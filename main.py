c9517739d9484ac4ab92c641678b79aa4a0365b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:46:27 +0000

    order contrast theirs corner instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
