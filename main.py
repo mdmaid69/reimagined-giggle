9239bb5bc2c4baaf9b570a6e4754827b565d6ee5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:20 +0000

    they who fold exchange recently

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
