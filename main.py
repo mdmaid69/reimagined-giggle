e8da9a0e9369a969a792e65f959dbb68c09c7781
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:45 +0000

    job danger why influence Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
