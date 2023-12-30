442f31223ccd402ee42f82b2f16edbef9b08d929
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:29:16 +0000

    straight uniform skin flood steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
