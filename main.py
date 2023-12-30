53ece93b9b923a59d6321db3e0ad2562849911e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:43:33 +0000

    street cross contribute keepfrom dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
