281ba3262b2bcafdd99963fdeee0e5c77fb08736
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:49 +0000

    crowded introduce dear possibly compare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
