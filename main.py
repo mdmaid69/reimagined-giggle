acd3cfe6bdaec89e4f3e4cff00bcf14add54db7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:34 +0000

    neither year shout sink object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
