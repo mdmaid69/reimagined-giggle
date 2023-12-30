bb7ee728d030a215e9e50694d900067a3abe0eca
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:17:31 +0000

    unusual blue smell mixture thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def create_user_string():
-        return collections.UserString()
+def get_array_as_frozenset(array):
+        return frozenset(array)
