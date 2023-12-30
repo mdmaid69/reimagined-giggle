d4f0e2cb41520507c88f1a79a068957842fa47f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:46 +0000

    fine confident from whistle trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
