4c4246500365be2e7024a1faefc66409e93b5900
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:22:12 +0000

    level tidy rich sting Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
