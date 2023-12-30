af965e0e5de9a15daa8be695e691b31650209bb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:05:22 +0000

    precise class room fast card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
