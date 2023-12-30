896cbe90d01d1bed12138e03ac26814b05b14a06
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:27:56 +0000

    add game form perhaps control

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
