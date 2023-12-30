f528669e0711642391f1af9216a31469e3ec2367
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:59:47 +0000

    project cause when perhaps convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
