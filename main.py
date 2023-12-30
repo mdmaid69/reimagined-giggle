1b9da75d81ca898b70c8f5890a2ea1b3336ac4aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:59:34 +0000

    declaration care pick deliberate guard

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
