ef629550b50100a017832e038ef02f6b612b2c84
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:39:50 +0000

    stupid it model offer length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def get_array_length(array):
+        return len(array)
