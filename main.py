ff41d6e7abec4a52ed6a9275b6710ab57bd52507
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:51:35 +0000

    cross getaway enthusiastic system complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
