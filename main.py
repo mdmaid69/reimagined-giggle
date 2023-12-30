6f5c0591dd7e37b3546763ebd2168f0aca440ee9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:53 +0000

    force wild coffee disappear somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
