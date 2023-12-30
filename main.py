fb865d765911e8c5cb590ce30ce527790dea1eb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:03:17 +0000

    glad man different she style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
