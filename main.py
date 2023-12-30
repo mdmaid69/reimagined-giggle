f25984f46184bd58861cafff1b3968dbcba4112b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:58 +0000

    takeout strike cry result consider

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
