73fc165386c32a0b0658fc4c7ae4c96a45766678
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:54 +0000

    pocket murder history goon mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
