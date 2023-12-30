90c9b7729d12b5d00d1981b88030b2f99df6e987
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:03:03 +0000

    active or often deliberate practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
