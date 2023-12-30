17b12cc276953e81dd077d4ff6a887f3252fc5fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:22:23 +0000

    brain disturb whoever interested innocent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
