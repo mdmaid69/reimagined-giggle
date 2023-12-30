cc47281b8c03e9cf30a60db23e7202d78c2af70f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:56:57 +0000

    bet behind long eager float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
