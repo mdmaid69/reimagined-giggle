a7a71fd82bef24e2899393571be93b45e1be5420
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:09 +0000

    include how remove between measure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
