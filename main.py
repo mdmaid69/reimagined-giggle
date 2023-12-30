fcf80b04345a4dc8a9ffe4f47f3452c9a28faec3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:55:30 +0000

    but rich giveaway Ss each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
