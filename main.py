fa458a684d04a9ada988ec64325b7f10b1514000
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:31:12 +0000

    squeeze settle before patient chief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
