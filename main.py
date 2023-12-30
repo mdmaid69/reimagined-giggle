fc7c0d12739c5f81fcfc476b0f4e551e5e2bd77b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:23:27 +0000

    turnon know go beneath type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def is_odd(n):
-        return n % 2 != 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
