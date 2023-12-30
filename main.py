942f4fbad4817d922bba4664c6aa8c54c6cf8007
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:17 +0000

    young manage danger present carry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+from collections import Counter
+print(Counter("hello world"))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
