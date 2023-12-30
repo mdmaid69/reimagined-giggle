cea53e0015fc13e0bc5830df2b63f7e8ac420a7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:25:21 +0000

    first storm say base switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_work(force, distance):
+        return force * distance
