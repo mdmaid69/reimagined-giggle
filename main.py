34d0ebd697ad409a74dffe161568b585e0ac4d0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:21 +0000

    commit holiday form all save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_average(lst):
+        return sum(lst) / len(lst)
