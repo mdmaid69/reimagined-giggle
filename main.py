63a9543d2292b4c0008dcec9d1f976107e46b288
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:05 +0000

    anybody destroy rely rise sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
