c003acddf45aacf287c7b2b730b6e9c46e16bc27
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:11 +0000

    wide bored avoid profit mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_max(lst):
+        return max(lst)
