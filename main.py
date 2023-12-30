0e5a3d9a9c7f6c0aaad1d784e8c362be024dcba6
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:35 +0000

    contrast definite claim legal insist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-def exit_program():
-        sys.exit()
+def cube_number(x):
+        return x**3
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
