eb34e2641dede9711ec4532f1dd2f3552c4024d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:22 +0000

    treat divide tense stupid hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
