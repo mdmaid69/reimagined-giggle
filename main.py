83e1f14305b21713de38687d1bfc129bd878ac79
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:44 +0000

    prize appeal list push introduction

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
