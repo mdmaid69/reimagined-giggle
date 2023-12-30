61166ec43c553037718b593a39fd67243750e50a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:54 +0000

    date 572enter platform one either

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
