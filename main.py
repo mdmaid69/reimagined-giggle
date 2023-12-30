7465a0e5eb56be169f1a38f235bf904f3cfaf956
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:24 +0000

    shape contents play past minority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
