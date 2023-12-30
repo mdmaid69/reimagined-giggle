2a5ae67d12ac8c5686bd8bdb310582f97b8fb52d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:30:52 +0000

    fold estimate centimetre myself Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_bytes(array):
+        return bytes(array)
