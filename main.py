e3e9cddf5144209e48822512d3e2bad54261f8cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:07 +0000

    dull belief gas calloff guess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_pressure(force, area):
+        return force / area
