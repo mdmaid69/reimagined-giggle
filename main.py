68bf3d233824f8805573c14e50172ed9af11b7d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:43 +0000

    punishment glass shop television hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
