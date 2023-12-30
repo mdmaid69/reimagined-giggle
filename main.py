0d9f62966489e11b4d8551711933d3950d3548e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:56:20 +0000

    gentle any pump learn what

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
