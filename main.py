9892fa9a78864e62d6b7d5dec641b97f04922b9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:53:08 +0000

    mind judge later off weak

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def clear_array(array):
+        array *= 0
+import collections
+def create_user_list():
+        return collections.UserList()
