4a6168611e056a11c6c28d007cb58c776ef78de3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:41:28 +0000

    reject talkinto complain behave intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
