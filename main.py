65f8660f1d238fb90a0c7a89bfc3c818bc0773d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:41:26 +0000

    moment report maximum long bottom

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+print("Hello, world!")
