5e77a4534b8a8126d4b3b11925a838b994fa1d2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:11:57 +0000

    manner measure born necessary like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def is_odd(n):
+        return n % 2 != 0
