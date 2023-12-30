ec67f0a2d3fe573279d9437433a6df3f93a510e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:49:06 +0000

    human 564encourage instrument finger complain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
