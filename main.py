069a6a0fa527ab889e7330f80ebdaab3834b4145
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:59:37 +0000

    or excellent embarrassed poison lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
