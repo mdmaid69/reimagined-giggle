d7dbe888559e1f6aa3cb668094bc6232e3f1221f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:08:19 +0000

    centimetre smell note truth doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
