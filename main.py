10df9feb5bac73990875dfe8674fa6949d20ffab
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:09:11 +0000

    short stir record goback(to) peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
