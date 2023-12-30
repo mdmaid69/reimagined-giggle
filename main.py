21316853783864a3b070f504ad1df8dbc8768298
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:55:36 +0000

    frame determined emphasize dangerous have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
