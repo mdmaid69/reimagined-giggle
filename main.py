46662042340c5c0b49263b7415bdfecefc01f030
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:29:54 +0000

    plant cope Kk pay history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
