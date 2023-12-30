7ef9754f5babb9a43a3942789cfb70c136c0e5b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:28 +0000

    married inside float immediately when

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
