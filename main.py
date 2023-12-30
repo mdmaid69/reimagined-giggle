428c8b823a72e5d6054bce51f7336177aafeba8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:00 +0000

    cap combine break base deserve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
