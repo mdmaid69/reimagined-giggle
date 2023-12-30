66211cfe2223aa1e8a2becf744a240db9408bd59
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:57 +0000

    past dress rise sense moment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
