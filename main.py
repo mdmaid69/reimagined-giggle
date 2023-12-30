0ac203481acd1187967d5cf0e9bad7f750e0943f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:02:50 +0000

    evidence nerve education yourself furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
