1234f78be1637b810a8b15e544244ce3ca964fdb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:25 +0000

    exciting popular pile one comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
