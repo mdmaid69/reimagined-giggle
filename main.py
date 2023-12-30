384f0b60e91e7be8a1b7960086e97613b166b6b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:04 +0000

    square shout interfere purpose finally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
