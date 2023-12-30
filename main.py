a4282cd82fc19f1c8d4b28ecabadfbf0a14951e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:29 +0000

    spill community turnup impatient listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_max(lst):
+        return max(lst)
