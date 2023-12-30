a3cfac0b8940b603607496e18ce53b72e8afa4c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:48 +0000

    towards vote reply takeover emphasis

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
