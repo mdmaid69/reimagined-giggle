94998f5b1356c2580a7b05ca127df9a9799656dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:54:12 +0000

    sign brave pity responsibility temper

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
