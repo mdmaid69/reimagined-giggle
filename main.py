5b7c4301d84d555a0cfd3d14a9f1cadc3a167fbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:38:48 +0000

    measure room talk Bb call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
