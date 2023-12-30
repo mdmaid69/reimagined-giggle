ed9c985d584827661a40f730de9157df3fcc2d03
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:33:13 +0000

    return classroom Dd slice pain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
