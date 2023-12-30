08d64e0413acbc370cf78da4ed1b34fb1d7c2f16
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:29 +0000

    throughout season sound smooth meal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
