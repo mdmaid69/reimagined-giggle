edad592de44f0aa619b24fe6e0a629859eb52598
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:19 +0000

    we calloff we note normal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_set(array):
+        return set(array)
