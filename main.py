2c8d2cbf8e59fbdac043c77e1ad60ca2c7d02ad0
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:00:47 +0000

    combine learn tidy circumstance takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
