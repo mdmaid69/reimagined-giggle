549e23857b4f7f15187a426118797ccb264a47ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:29:58 +0000

    central copy mine occasional sort

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
