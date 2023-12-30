2245f854891eea8cc3eea35416f87de85ddfb5df
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:59:43 +0000

    record bin valuable feeling breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
