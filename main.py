1138182be4e6820c31a18f918d7b5974c34e24fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:43:59 +0000

    after up they record far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
