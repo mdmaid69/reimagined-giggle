f25b4c7419fda93e69a023c730207b49651a2c43
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:17 +0000

    meaning language regular century leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
