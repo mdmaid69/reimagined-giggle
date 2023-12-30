204687dbbdf930dc860934b469f6047fa1b65185
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:39 +0000

    order keepup attractive special process

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def convert_to_hex(n):
+        return hex(n)
