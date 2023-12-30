a60f434e02c2ed09e693f2643006e01b150ba082
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:00:55 +0000

    kill when ring opposite too

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_area_rectangle(l, w):
+        return l * w
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
