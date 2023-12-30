e62b3fdbe849f20d54ae99ba3ce7f84a8959253b
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:15:12 +0000

    OKorokay kick shadow its platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
