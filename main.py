114bfcc5b5cde43a10f97d8ccbbe888c12b76ae1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:03:02 +0000

    though somehow everybody fall task

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import collections
+def create_stack():
+        return collections.deque()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
