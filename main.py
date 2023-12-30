0542f74400c60b9314e7482cf1b62ef8b4d5fb47
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:20:30 +0000

    bet natural business important exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
