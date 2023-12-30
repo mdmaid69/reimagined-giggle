0cac217295d4393e116ad0f60c685ef2dba6a1c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:59:32 +0000

    downstairs occasional result attempt mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
