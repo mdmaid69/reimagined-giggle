f96a97a82df4f983719e0218c424f9a74eb62183
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:45:40 +0000

    insist goup rest stiff free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
