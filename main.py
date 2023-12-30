b63a3c681ead5aa4001970fd9ac121915092ba28
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:44:29 +0000

    sting upstairs foot comeup compete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
