ad567a0cc0814f6003ab6bc2705c5e38b359bff9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:01 +0000

    apply unpleasant match regard challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
