ad99381001d7f753caf5a3e5d5bc0bca9993d70a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:19:59 +0000

    slice quiet search head remember

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def find_max(numbers):
+        return max(numbers)
+def convert_to_octal(n):
+        return oct(n)
