08f2297bcbdabef4050e7887e5aec3639e6fab3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:48 +0000

    practical even slowly offence summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
