396201907450b3e24f9575e77f484edc4ed352b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:47 +0000

    shame safe close powerful surprised

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
