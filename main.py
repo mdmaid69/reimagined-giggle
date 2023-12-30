cdf7a4a3bf296a30072de0dd1b3c43bdad4cefe8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:36:21 +0000

    reason that nowhere mind office

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
