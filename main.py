c9edc17b8f0a4a6117bf47400b2c8e88005eb940
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:35 +0000

    respect accept service before care

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
