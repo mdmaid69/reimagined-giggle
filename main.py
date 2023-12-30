2b73d525e1c1634c721344826e20aff725de26fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:43 +0000

    straight because mouth behaviour have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
