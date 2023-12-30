8594ac99a0fee722477d15daa44fab2f216d2884
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:34:52 +0000

    money battery light after single

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
