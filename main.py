a08386ae767ac7d0e45ddf6358682e5936e67872
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:03:21 +0000

    bet appeal tread pipe suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
