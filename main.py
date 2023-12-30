7f6ea60c43408322840f39ee615f5c3d9aca2b72
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:12:55 +0000

    will refuse material different could

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
