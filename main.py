99fc504a1c9a8df2d9d8e3639dbdb6199ac5f0d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:16 +0000

    upstairs exploration precise attempt dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
