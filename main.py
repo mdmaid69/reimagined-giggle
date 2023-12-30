9ddc3573d0cfa8bf886fc26e45ba57e75bc455a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:22:30 +0000

    trust drawing friendly pain waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
