d1d93fac7680544ff3d0de805f57ab2fcab5b516
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:49:51 +0000

    hardly home tune side regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
