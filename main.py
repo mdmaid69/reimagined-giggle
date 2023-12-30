9f98e6b927e603e46ae0b4021ceb57e75f61cd17
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:34 +0000

    sharp comment deaf drive partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
