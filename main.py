cc504a624f8829ab8e3b66af807bd4599d0d6764
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:43:55 +0000

    point force moral nose too

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
