259df0bf092e19e00d2df683813656bab3a652c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:32 +0000

    physical unusual odd consist delight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
