6a1f9b6a34662655198d11ee188913dfa98481eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:58:11 +0000

    my invite joke reply interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
