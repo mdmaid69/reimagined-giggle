b733edb191ee8740db3ac96ef0ef74cfb9bfb224
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:13 +0000

    through steam style think design

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
