c6cbdaaba2a2d3a6dcb64ab7181600969209c3b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:23:03 +0000

    except style material Dd autumn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
