15e738243702d49623444f68184522afef59635f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:56 +0000

    mark fairly concentrate point exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
