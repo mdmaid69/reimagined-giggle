692ff09b660dbbfc8e6f7d230931a05ba2aa3a85
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:11 +0000

    care know personal smile small

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
