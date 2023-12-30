544f689f13f20a8778337823ba8b42fe62b5d64f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:39 +0000

    makeup Zz bye easy order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
