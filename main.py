e5aca63c5de5d3286b98c2935d5b3f32c75189a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:18:45 +0000

    bother escape free stay rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
