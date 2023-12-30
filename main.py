daa12a05dd4afbaa94939415f3606cd21191d718
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:34 +0000

    respect listenfor official bus concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def square_number(x):
+        return x**2
