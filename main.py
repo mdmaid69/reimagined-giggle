d0642df187ab75ce78d0bd31ff52ea52cdc461e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:36:46 +0000

    face crowded thirsty there maintain

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def count_elements(lst):
+        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
