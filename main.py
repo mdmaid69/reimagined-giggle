7c5475c632b05395c3764ff4384d4fb4b4f695bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:47:14 +0000

    sail blue jump quiet downstairs

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
