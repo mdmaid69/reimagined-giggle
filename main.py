b76f3ed7049a0a5dc292df317c36e47127fd86f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:36:25 +0000

    load simple shoulder science rank

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
