54694d9652b483bd286f4b2c93646bc9ba06bca3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:45 +0000

    disapprove middle system language guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
