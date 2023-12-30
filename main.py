d8638254d5d86865da9eda0a96b4dae8047d230d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:37 +0000

    move correct favourite comein control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
