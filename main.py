077694169bf4d6b3d59cca27d951eb4e3faf52f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:30:19 +0000

    she pronunciation turnaway consult keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
