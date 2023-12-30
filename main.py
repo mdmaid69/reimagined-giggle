05dfff2b4fd4ffc6d5510da11f65b0fcc017b19d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:55 +0000

    anybody relation summer own finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
