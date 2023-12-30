3af41e5d488bcb3f255be6a4ad044b8c03e5bf21
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:41:41 +0000

    condition correct load winter secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
