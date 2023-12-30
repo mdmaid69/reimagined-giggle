e050ce0dcaf23464cbe54efc3445ac8fbfe74cc0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:22:03 +0000

    colour present passage legal partner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
