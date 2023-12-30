befba1b10380c2cb35a93de864e227ca2303b129
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:43:48 +0000

    single accidental whether destroy shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
