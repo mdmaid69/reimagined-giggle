7095f5a4fc78415bf7c1123b65f1c117814754d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:55:10 +0000

    direction activity destroy remain disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
