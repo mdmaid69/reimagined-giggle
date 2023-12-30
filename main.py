b4d7c6f81e5d29c7d06edc751c6444b388a543df
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:20:11 +0000

    hello when high so claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
