d80f033f69c7718edfcc0ed9ff721017c30478d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:48 +0000

    tie classroom mark suit thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
