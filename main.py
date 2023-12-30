6e31f4502f7a6320e851842297dfa4ceff76cdb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:29 +0000

    yet politics there blind luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
