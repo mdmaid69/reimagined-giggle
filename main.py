8746c1eeff852a33975bcfbc3136d8d20b0aafc2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:56:16 +0000

    shadow mainly feed to situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
