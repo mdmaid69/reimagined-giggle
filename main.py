4b681ca5d01dffa942fea48f93d901e855d67e5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:00 +0000

    science appeal face night autumn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def remove_from_array(array, item):
+        array.remove(item)
+import datetime
+def get_current_date():
+        return datetime.date.today()
