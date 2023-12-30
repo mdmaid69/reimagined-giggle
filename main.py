0b48814d2435a1b0bc136d0b7785578d87f4be02
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:21:25 +0000

    top outside hit match let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def cube_number(x):
+        return x**3
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
