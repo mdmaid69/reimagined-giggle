41bbfef9529dfb9d6f9ee19502508e5ac1186c63
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:46:31 +0000

    health anywayanyhow moral steady scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
