489afc1c870bec9649465935f847790e2a837edd
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:54 +0000

    slip call agree appeal complaint

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_string_from_array(array):
+        return array.tobytes()
