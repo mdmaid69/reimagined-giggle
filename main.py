a85e57bd0169b0817ef0058a13fd7296dbdd8dd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:47:30 +0000

    vanish load house straight rob

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
