d9163e962ca04a24a9c21bd54095cef537eef3ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:56 +0000

    top maybe tend area stretch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def sort_list(lst):
+        return sorted(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
