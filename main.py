edcb25c6b8fa519606cb401e3c685152900636fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:57 +0000

    town impressive recommend now remain

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
