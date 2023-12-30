196f87c6f27b408f72039ac3f8cd194ab7f150c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:19 +0000

    suit cry stay clean dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
