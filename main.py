d55c8a1a37f5aa739b2cd1d16717dc236ce094e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:15 +0000

    small deep language show tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_bool(array):
+        return bool(array)
