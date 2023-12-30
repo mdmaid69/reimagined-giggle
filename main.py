ddebceae11bd2e652d2b78548ab67e0cb969565d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:06 +0000

    tip phone vote popular poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
