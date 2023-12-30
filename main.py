5df6dc7f00e9e8fd86cf3c475473279045e50394
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:37:20 +0000

    score country but OKorokay sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
