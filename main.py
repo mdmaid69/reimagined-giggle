6ea162442551b505efcb8c43ca01247eda0a6083
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:17 +0000

    speech hold about tune occasional

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def convert_to_hex(n):
+        return hex(n)
