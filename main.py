09a0a892ed02e262c180e642d67bfd27b2feade1
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:06 +0000

    fit sort careful cheat onto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
