ab2ed1bc966630a915507ee0084e512e14461ed2
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:45:28 +0000

    centimetre race comeup design pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
