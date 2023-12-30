5308b66e6f0c17eefb7bb7ce97e598941a32b09c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:25 +0000

    suffer uponon everything crash hour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
+def calculate_average(lst):
+        return sum(lst) / len(lst)
