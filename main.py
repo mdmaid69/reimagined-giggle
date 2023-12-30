1494d30b84f6b66fa9a0317e0c361d753493ed1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:28 +0000

    write total prove stage spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
