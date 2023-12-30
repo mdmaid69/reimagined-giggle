4e4aa07e7b50c526efad5c98531c6e1eeb343178
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:22 +0000

    feel pull certainly guilty bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
