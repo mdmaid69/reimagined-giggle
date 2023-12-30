d718e3436a5b54e74a6911187d1d8c16d528bb25
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:16:08 +0000

    how innocent different sign unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
