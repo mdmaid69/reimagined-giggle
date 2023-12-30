21fce84655fb79e182ff49b6d7ed9fc02fddde3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:12:37 +0000

    office building takeback score fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
