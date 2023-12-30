421935aed96842dc4348b79c8b832666541ae6e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:39 +0000

    favourite tradition flow ago pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
