c73889e798e6b3a8999c1fe697003f9276bce298
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:09 +0000

    great over dark disappointed thin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
