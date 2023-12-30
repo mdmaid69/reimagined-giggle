31c3d88bb6f7811e6ffc094402e86968417b257c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:25 +0000

    round gradually getout well name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
