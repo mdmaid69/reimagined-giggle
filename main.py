e150239e82c5d8f3248f4fbd51cab5c9477cf2b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:01 +0000

    practise bother conversation shall off

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
