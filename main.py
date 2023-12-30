9121a462511e311e2b9d08f5fc078ad197cafb26
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:31 +0000

    war often but division clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
