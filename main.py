9aaaa59d0673b26703ba7d453908c116a2b243a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:09:00 +0000

    slow his light pass either

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
