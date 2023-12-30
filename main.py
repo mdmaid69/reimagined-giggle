cabd78eadbc26b462c4ec9419d4f52939a3a940e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:40 +0000

    let part honest will scared

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def convert_to_hex(n):
-        return hex(n)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
