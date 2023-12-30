64256a3e343f6664a0c1f80887a7f8e6383cd399
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:57:55 +0000

    dark intelligent professional afternoon every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
