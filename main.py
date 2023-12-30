483800f84da8ab68db6e994b64c4c36ec199ee9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:35:38 +0000

    squash edge affect relation silent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
