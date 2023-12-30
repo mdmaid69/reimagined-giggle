468e5a1e42ac0b519f59fced1c4ee7dca4222604
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:54 +0000

    her waste drink cheat evening

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
