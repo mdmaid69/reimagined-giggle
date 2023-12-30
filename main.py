9a83c2ab8ef8aecdd2e6af12d29f05cef6b3f64d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:11 +0000

    plastic like continue Kk worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
