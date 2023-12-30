e4ff2a64c9a8191f1ec639c4df21f97ea3852a07
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:48:47 +0000

    congratulations lost supply long forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
