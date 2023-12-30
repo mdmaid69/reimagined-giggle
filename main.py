a3a7922d8088fb258917d373d1ba2af80a2f53f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:50 +0000

    turn away search recover another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
