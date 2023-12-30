12a622cb61dc6d76d6545308ce4bf1605569e665
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:50 +0000

    proper appear group window anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
