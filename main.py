09d009b5861318b02f54613b56544878bf4ea61b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:05 +0000

    last little compare instrument expensive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
