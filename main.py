69580cc3441e2f5b430d101c0e1042d6924bc2bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:36:35 +0000

    danger content challenge tax female

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
