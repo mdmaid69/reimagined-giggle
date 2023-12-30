4ca23200e6f9fe778eb7f39fc06809a3aee53628
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:47 +0000

    special paper anybody store run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
