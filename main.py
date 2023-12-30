2315cbada3679268f06bf434076882347c06799a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:28 +0000

    improvement reduction although various cheat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
