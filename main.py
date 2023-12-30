c5b38738a9226a28e952da174b0ebd4047aa6e23
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:48 +0000

    shape warning seat mess needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
