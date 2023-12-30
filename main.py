ae0d619eb9cc613502f05edf0da747abd16539f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:59 +0000

    cheque turninto miss blue squash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
