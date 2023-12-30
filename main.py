2e9caa05a16441abcf1a7f3708e7ea2093b13558
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:05:16 +0000

    right hospital bringup stay figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
