0da586148a15b293def801aa3e328a57b3e46826
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:37:27 +0000

    body pride house among bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def check_if_array_contains_item(array, item):
+        return item in array
