5182c9a0c73ceb02f336289d812787e693c48c68
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:33 +0000

    talk hour both talkinto laugh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def change_working_directory(path):
+        os.chdir(path)
