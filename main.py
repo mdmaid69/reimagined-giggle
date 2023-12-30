de16d7e3dd35094b02de8af355b5221325b5187b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:33:23 +0000

    dress culture importance I ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
