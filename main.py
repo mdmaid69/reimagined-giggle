504aa23abe2b7ad4c0972ee1b789f6e2c73dc809
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:48:41 +0000

    turnon once building luck tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
