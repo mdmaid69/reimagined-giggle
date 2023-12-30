79f985fc7cada48bb7ef11c84572133d4c964d2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:25:18 +0000

    last workout mix responsibility tradition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-def convert_to_hex(n):
-        return hex(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def reverse_list(lst):
+        return lst[::-1]
