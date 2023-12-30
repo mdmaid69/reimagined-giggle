45a9415d51001b3ddce8e0f9a4f27af659524f91
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:01:46 +0000

    special abroad single convincing circumstance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
