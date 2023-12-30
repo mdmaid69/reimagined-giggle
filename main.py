bf4f44faee25cad06df0be0dc3d91ffe1ee93e3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:14:52 +0000

    tie cheap exciting party bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-print(random.randint(0, 100))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
