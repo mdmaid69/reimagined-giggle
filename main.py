d497db689ce601a184c5d7d68d8c4b817429d964
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:41 +0000

    soak scared advanced form time

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+print("Hello, world!")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
