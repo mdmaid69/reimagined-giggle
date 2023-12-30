458d1816a164cf7280d2006dbd699c8b2fe695c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:11:44 +0000

    type set free without unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
