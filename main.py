e9eea413b740e6253f5358b9a7baead51ec9bc34
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:32 +0000

    fill protest the innocent trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
