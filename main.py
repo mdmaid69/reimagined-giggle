4c20acfb8a78e74a33e7259201ff30463688cf55
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:52 +0000

    especially into story short whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
