e2ec62d8032036f9bde5aab8750baff0f7719f2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:53:55 +0000

    love under waste fire sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
