11e722821831b9a40c474bddbcbdd8bac43b370e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:43:54 +0000

    far side Vv clear maximum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def count_characters(sentence):
+        return len(sentence)
