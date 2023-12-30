9512ffead8af36ae8fc15dcd6acc36991e06d88f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:29:35 +0000

    anxious scale range this fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-print(math.pi)
+def calculate_floor(x):
+        return math.floor(x)
