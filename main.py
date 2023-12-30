fb73f4e6345b5eace2800be653fdd5ed37808675
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:22:26 +0000

    day circle so hard sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
