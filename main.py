d5a5a740785d37e138c5ca8118c2be01b2aa8ca5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:14:08 +0000

    getaway grand else public possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
