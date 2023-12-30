48e4a84e08bdd4f117da6eb96d91b997ad43f22d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:28 +0000

    through muscle marry chance pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
