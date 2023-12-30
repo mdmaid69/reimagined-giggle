f3b6bb1c092cb97ca36a9fe5e2a3beca9744bd3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:38 +0000

    goon distant tune giveout light

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
