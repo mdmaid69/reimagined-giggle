237c3a4aa9a72505c940b31fea0f452802aca0e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:15:03 +0000

    getaway lie generally address head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
