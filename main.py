146d95a17c83eeb7fc025e2dc657556dcb8546af
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:12:29 +0000

    pump talkinto all Zz finish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
