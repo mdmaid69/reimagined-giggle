ebb4457b6c436c82ba3f0d09ca99299e57bdb008
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:39:13 +0000

    jump production fire circle bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sine(x):
+        return math.sin(x)
+for i in range(5):
+        print(i)
