22bcd17703523056c3fcf23301d93cbf15087e4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:43:07 +0000

    left name undo thinkof protection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def find_min(lst):
+        return min(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
