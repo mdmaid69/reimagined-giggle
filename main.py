426070ba8f5541d9700414f70d37912c50d3a3ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:31 +0000

    street butter care passenger ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_max(lst):
-        return max(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
