10c5ba0683b2893621780f2cdb50d92e61ebb57b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:57 +0000

    wealth quality mainly possession over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
