17bd0c4da26be9e85e0dc6faf43ebf1635737f93
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:20 +0000

    past wild relationship ring holdup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
