8ff3caa72215ce9a9bb3b98446860d3b00602dbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:13 +0000

    platform deep rather comfort actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
