681c957ade679d84d3d19ffb2510da59cdf8fb2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:38:40 +0000

    demand appeal word wind plenty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def square_number(x):
+        return x**2
