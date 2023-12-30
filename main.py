f97b5fc2d2648b78a8d60a8547b6dd1e46fc2c43
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:33:33 +0000

    secret race Ii process father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import json
+print(json.dumps({"name": "John", "age": 30}))
