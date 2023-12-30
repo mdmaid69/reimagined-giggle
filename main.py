21dc3984c95eae022c965033b2e33ba3ad4bbec1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:48 +0000

    above carry such start name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def subtract_numbers(x, y):
+        return x - y
