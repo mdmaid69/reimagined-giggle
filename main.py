731c06f00c80117566109b4bdc235f2cd3dcda51
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:33:12 +0000

    balance peaceful flavour information address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
