e1ca90a4755eca6fa880f072acca060967645d10
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:29:50 +0000

    negative sister history burst process

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
