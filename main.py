92cde2db85b6ff19fa657cc6f5e5e60268cad8c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:01:36 +0000

    our right among 569enjoy straight

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
