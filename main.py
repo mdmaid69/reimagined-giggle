ccaa021be0ed98da740ec048413cad3a11fb4cb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:17:03 +0000

    contact giveback nowhere hit memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
