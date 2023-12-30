9597ad553d144e2a0836d903a4923e7311df952d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:52:23 +0000

    borrow treat lot exam night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
