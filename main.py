70bc9ba424ca53b8987bb9aba047000eec34b393
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:34:39 +0000

    boat company string chief release

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
