be1f5ed85a1b7e5ff392cb4dd7db555ff7376f0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:54 +0000

    no funny his minimum variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
