1ed50c53c6f4df77282ca9acf582b8e381e9aab2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:31 +0000

    bother stupid friend law motion

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
