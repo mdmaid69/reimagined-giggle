d3fcabbd7de1d4106e5f4510e3734f89d98f8c20
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:29:11 +0000

    hard smile let dress argue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def count_characters(sentence):
+        return len(sentence)
