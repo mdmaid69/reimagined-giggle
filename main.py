3d45b8ec4e121421781e6680fa53c708b1062cd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:02:02 +0000

    old so something power prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
