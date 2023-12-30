4b9354d1d23b903c47ada67bd6a85e8622ad0c61
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:02 +0000

    trust hold creature manner plain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_work(force, distance):
+        return force * distance
