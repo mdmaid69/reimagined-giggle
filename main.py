d3931da7737696d2ad319c30cb27a347d7c108c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:54:54 +0000

    turnon indeed interest consist afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def get_array_typecode(array):
+        return array.typecode
