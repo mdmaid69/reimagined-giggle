b59788de1ca7025ea24582262ebcaf5599f62d35
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:26 +0000

    tie transport above harm past

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
