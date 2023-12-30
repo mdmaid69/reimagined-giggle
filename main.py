ea87404ec6ac992dcbd0d051c95df0e5371a9690
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:27 +0000

    nearly arm drawing interfere objection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
