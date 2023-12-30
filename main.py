e29eb4b67d359b598c94228774e11818d3b9b09a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:48 +0000

    exactly century mind criticism represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
