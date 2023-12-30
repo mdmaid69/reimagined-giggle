ea33387cd7f8cb1fe78e25b529954f3ecca03784
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:15:49 +0000

    instead public party surface across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def square_number(x):
-        return x**2
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_int(array):
+        return int(array[0])
