07b7a519abd8515fa771c608e6e245887ec07b70
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:08:45 +0000

    call light reduction steam ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
