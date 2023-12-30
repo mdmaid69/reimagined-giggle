78266e750ca6d2338ca620d5dd9028c4f8cb5b0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:46 +0000

    careful demonstrate meeting when 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
