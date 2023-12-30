80484c702e193ceff006757d07ae94d171cb04c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:00 +0000

    cook dangerous human minimum history

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
