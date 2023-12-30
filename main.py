35e49495316df321fb586211b6963a9f63ad56bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:37:40 +0000

    study jump forget stop niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
