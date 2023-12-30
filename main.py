6ae4b90a5ffba00b2b7803c8487293cac1e3e3d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:11:38 +0000

    concerned pressure middle read dead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
