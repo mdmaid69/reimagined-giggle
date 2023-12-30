4c895426208551929c7001067d998aee25c054f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:11:52 +0000

    door object chief public breakthrough

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_area_rectangle(l, w):
-        return l * w
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
