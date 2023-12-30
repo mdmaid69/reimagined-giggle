6b3ed48f47e93d7dd1d8ae5b586c54b3aa40ee68
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:28 +0000

    transparent nowhere mind reduce far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
