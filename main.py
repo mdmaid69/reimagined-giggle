a90895791473844dae8477894702d13ed45081ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:46:07 +0000

    other cry apologizeapologise demand house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
