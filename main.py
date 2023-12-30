a0d03f9a27495f4a6534e285e8626196c4184417
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:47 +0000

    read strike forever nice on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def clear_array(array):
-        array *= 0
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
