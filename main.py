3a5aea9b034f35eb92d7f5770c9ab17ef200849b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:03 +0000

    different terrible total 572enter watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+print(sum(range(10)))
