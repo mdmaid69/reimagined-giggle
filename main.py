62aa0c73dde4bd8589f1776be1f8f52a0bb65ecc
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:38:28 +0000

    point breathe challenge share else

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
