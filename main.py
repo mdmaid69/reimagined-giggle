42bff056619344c01dfd8f24e4e440c5cdaecd5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:29:40 +0000

    nonsense impression admit else disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
