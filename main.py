c88e6999c345bed6d803779a2a8973cb16e379b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:19 +0000

    accordingto choice declaration throughout upwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
