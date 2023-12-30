805a054a49a5e250b8ad70d3e26b6943fa83c634
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:30 +0000

    stare hole present his workon

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def remove_duplicates(lst):
+        return list(set(lst))
