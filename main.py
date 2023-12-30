43e7b99fba23da7b37fe924ce30739f6cdccd336
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:38:50 +0000

    anxious convinced naturally increase everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_sine(x):
+        return math.sin(x)
