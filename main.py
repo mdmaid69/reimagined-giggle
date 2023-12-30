1f1406f757c4c389eae55fd52f148b881e2944f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:45 +0000

    shallow weak drive cough go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def convert_to_hex(n):
+        return hex(n)
