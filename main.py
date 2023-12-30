00aeb77634e00351bf636e03c94349daa5c91a04
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:01:21 +0000

    may information with claim ride

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import getpass
+def get_username():
+        return getpass.getuser()
