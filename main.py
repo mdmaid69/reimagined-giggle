ec7d70cb06226f6976d7d9d3bdc460556ff0cc0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:40:15 +0000

    help purpose unkind smoke black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
