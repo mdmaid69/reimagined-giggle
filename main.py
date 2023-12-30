30d892ced030f0da61a228763fcc31aca8327fb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:49 +0000

    offer deliver little side frequent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+def convert_to_hex(n):
+        return hex(n)
