2da7970c2499ad459aa714512c72a67ea91e1d6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:51 +0000

    Oo proof hungry convenient education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
