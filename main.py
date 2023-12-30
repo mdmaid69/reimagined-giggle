e46c475f096a5c622681b9364910e0e76e5ca50c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:23 +0000

    target collection along just ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_work(force, distance):
+        return force * distance
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
