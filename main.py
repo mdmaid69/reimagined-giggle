13971e2c7bb2f7cb65d506b26f5cc8016af7b010
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:42:29 +0000

    godown kind Mm hear rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def get_array_as_str(array):
+        return str(array)
+import re
+print(re.match("h.*o", "hello world"))
