094a82f64e69e44e60f2292395a89e397a3e92b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:40:38 +0000

    sudden impression balance shake ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
