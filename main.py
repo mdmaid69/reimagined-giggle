eeeb93d792545c9684d6d56a0c82b97ae7390c85
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:37:11 +0000

    sore ask stir tradition sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
