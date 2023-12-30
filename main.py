1e802be6522f551720b343790e828f4430766fdd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:03:00 +0000

    recommend better idea slip branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import datetime
+def get_current_date():
+        return datetime.date.today()
