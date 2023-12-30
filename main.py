6aa973b1c68aced6cdc0c5a4b678f08fe6f3de6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:30 +0000

    handle splash comfort drop could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_repr(array):
+        return repr(array)
