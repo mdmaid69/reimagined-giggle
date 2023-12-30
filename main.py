a2e13b8bb9a0f7559c91fa379cda58580b8e74fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:31 +0000

    wrap though shine genuine allow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
