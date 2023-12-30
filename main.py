d17013d5cbc6e1b1817e8d7380e66fb167079cf3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:49:08 +0000

    heavy undo meet dangerous keepaway

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def add_numbers(x, y):
+        return x + y
