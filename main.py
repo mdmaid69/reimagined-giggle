eafa0820ee56529b2ae3e21306e3c87e88f33c84
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:04 +0000

    legal Tt moral proper grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
