69ec5efe8c0ee31051698471413c25ef356548fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:22:36 +0000

    attitude fail yet vote write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
