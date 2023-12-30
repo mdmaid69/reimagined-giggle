b15e05f44d727175526b81cd23e017d3f9c49d33
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:17:25 +0000

    spring little sit business short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
