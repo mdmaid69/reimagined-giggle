d01713415ecd2d46cc4564a2b872d9c377066a55
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:11:10 +0000

    floor ring copy settle difference

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_floor(x):
+        return math.floor(x)
