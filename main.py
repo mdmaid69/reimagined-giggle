26f1f345fd5eaab640454da6f7676ea39d503621
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:59:39 +0000

    calm post opposite space wise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(datetime.datetime.now())
