335a690d346bc63cdbe8e02180c8e1e49f6b87f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:04:05 +0000

    appeal between respect runaway why

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
