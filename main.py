f15e6f78a8583f4bddd390a08fc149fcd46a4506
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:51:28 +0000

    content practice gooff negative somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
