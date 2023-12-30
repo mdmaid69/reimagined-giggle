a78ab9bc547cb9d154105da5f20caba3be7aadc0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:41 +0000

    scream worst exam explain value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
