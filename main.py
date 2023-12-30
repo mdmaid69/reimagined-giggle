8a090f0109e050a5e30aeb1abb7bcd5d83e5f922
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:23:58 +0000

    list thought deliver together slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
