e0c5e5afae3b33773b6af3d70f7c8c1287f124e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:11:06 +0000

    prepare birth mix group appeal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+print([x**2 for x in range(10)])
