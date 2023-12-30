4c61279cd42f9bb9ef5c1a37d1ff5f8217f768a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:13 +0000

    smile command delicate row Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_hex(n):
+        return hex(n)
