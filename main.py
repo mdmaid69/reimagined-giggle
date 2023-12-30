94c8a800f6597cd83263ce2ba4cc7b1824aad178
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:16:10 +0000

    will lean bury who careless

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
