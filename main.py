e276ad159a8222daa796d049e16896193d552891
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:43 +0000

    food cry repair lonely nose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import numpy as np
-print(np.array([1, 2, 3]))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
