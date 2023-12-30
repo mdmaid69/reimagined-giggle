c63c1b9a5f216959c00d101bd01d4e7a1775ea4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:21 +0000

    distant unfair pair throughout tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_min(lst):
-        return min(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
