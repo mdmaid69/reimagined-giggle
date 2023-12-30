fdbf1ba9601a27e1aaf4cc7a3274286fe82c2811
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:46:00 +0000

    spot splash young practice why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def get_array_as_float(array):
+        return float(array[0])
