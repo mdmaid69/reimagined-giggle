cb1039649dd63d0e53f5909b3b5957d5efb070aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:41 +0000

    tight offence fly breakthrough love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def convert_to_hex(n):
+        return hex(n)
