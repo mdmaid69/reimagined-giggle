ce86a39baf557b691443b271011915ac3bd0beb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:20:51 +0000

    569enjoy breakthrough fairly thorough guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
