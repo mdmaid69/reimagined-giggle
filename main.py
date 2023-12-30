6a7454235f0d2d84c82034dc3c4858c0aaab0002
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:29:37 +0000

    bush stone worry remember left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
