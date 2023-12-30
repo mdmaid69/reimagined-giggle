dcd05969cb95b310f0fc3029a636861c2a073084
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:35:24 +0000

    emphasize drive away display relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
