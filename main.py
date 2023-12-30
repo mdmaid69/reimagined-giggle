7f32b32723ce10f8e8d86d0fca274736ae20d02f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:44:22 +0000

    shame frightened talkoutof terrible obtain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import sys
-  def get_python_version():
-        return sys.version
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
