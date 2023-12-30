6b265a5d88baf5a2e31f6d3befa1f7c10a7ae325
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:00 +0000

    mainly write weekend belong pipe

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_current_time():
+        return time.time()
+def divide_numbers(x, y):
+        return x / y
