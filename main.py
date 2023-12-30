c22d8a243dc8f50b37f0c8b3f2979beac4a9eefa
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:43 +0000

    row business custom cut calm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
