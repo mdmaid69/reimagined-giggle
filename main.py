07b3528eeb5466b00a91da6a985a827fb08ed40d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:29:52 +0000

    about pure light share pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
