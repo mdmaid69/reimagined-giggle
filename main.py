ff1a7971dc69c242f05a1f2ad91cb2c68907f6f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:23:12 +0000

    foreign read raise sticky store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def roll_die():
+        return random.randint(1, 6)
