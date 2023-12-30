f5d9e3332acb2e2025b263d7fb3bc6c483960d54
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:37:53 +0000

    beat television arrive sorry best

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+print("Hello, world!")
