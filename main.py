130dbf7abfe70ad08c32e18939849a14fc3b9821
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:16:26 +0000

    from mainly brain trial comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
