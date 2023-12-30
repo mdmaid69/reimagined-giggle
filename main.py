394ffeb4fb3146861af9062ff2563a4078f72fd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:29:59 +0000

    carry theirs question strict instruction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
