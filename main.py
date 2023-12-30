a3e4170e9768fb2f2a16808716708bf1545638b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:22:44 +0000

    earth difficult conduct emergency whistle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
