8b2b3d2ceaab3646c9a27b7608db770c522f8236
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:53:15 +0000

    confuse teach commit bread leave

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_speed(distance, time):
+        return distance / time
