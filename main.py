f45a9da71822fdf0da2a554385b88d5c34d21807
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:52:04 +0000

    how tough card receive takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
