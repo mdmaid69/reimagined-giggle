bd786499346be25c54ee488fa18241663e223154
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:34:44 +0000

    ahead goodbye name shut chin

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
