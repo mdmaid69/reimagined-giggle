6e075ff8916cac62ebf261edc45c9a47fe6d1616
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:39:51 +0000

    safe then list miss hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
