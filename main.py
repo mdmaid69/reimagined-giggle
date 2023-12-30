358ad00441926cfe196712ce43f18def08bdf1b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:12:42 +0000

    set used succeed section example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
