ee44824ef80676f949e06faab17f8a429c3a6e47
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:22:31 +0000

    human free compete hurt note

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import sys
+def exit_program():
+        sys.exit()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
