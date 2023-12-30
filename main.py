ec6e00b547983cfb43e17d04956c039935de4c4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:00 +0000

    powerful sticky above afterwards blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def sort_list(lst):
+        return sorted(lst)
