85d879e6621f01578b92251d8947101b288bccfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:00:04 +0000

    wheel satisfied anxious slip easy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def convert_to_binary(n):
+        return bin(n)
