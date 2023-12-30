c295af39306bda95d8441d23b3f4b9fef15c3697
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:35:41 +0000

    exaggerate cover rarely mainly whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
