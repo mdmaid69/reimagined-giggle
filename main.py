12308a76bac311261650d6c516dc9f513ab31058
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:39:42 +0000

    declare close situation distance ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
