31beb3aa590c800813cb59b93400c5007e92e6b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:32 +0000

    debt point from voice object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  def convert_to_hex(n):
+        return hex(n)
