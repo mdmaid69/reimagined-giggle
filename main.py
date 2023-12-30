da821a791f6c0cc079d49e8b52bc686a93a17084
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:30:29 +0000

    law mass delicate equal easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
