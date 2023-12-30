61542ee3a77b274ac8302da594adabd9766394b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:09:53 +0000

    stir store shrink public characteristic

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
