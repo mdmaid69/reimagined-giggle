311a13fa05937ae8cc71305075f7ec8655a08f73
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:20 +0000

    roof film knowledge replace close

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
