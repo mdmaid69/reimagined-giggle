43589ef43eb360ac94fc3bc7c8fb18fb417a253b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:29:26 +0000

    only deal range Hh company

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def convert_to_octal(n):
+        return oct(n)
