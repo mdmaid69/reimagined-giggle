2248de03830c5388b2168c9356b17d8466281e03
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:09:03 +0000

    Mm load suit carry shoot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def subtract_numbers(x, y):
+        return x - y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
