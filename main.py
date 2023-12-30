1c272c59809ae20cefef5c8e74d1c19648231bf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:28:49 +0000

    expensive busy aim no combination

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
