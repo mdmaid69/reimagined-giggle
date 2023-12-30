d0505400709a90c2f02a2ad70e1df5d3a9b1fcd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:43:32 +0000

    leg mention powerful calmly friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
