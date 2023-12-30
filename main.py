1e0d9c375f050d5f5236f556f846812c6655c176
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:22:10 +0000

    scrap lose programme clean Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_item(array, i):
+        return array[i]
