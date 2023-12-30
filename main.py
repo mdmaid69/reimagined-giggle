c06f780abf7e41d1857b49005d46a99ef9392363
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:14 +0000

    book recovery brain run thank

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
