425b1ac24465d1972d3c68d237fc9e7eb427ed4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:23 +0000

    increase major hit nice responsible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_factorial(n):
+        return math.factorial(n)
