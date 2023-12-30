79c121cdae1917fc1e7acbdd8036be529a6aeed0
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:31:55 +0000

    Jj giveout machine shock gift

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
