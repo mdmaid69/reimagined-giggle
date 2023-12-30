88c8d7c030aacdaf24789c74f65696436dbb951e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:04 +0000

    state urgent notice something sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
