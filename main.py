14bea3014eda8a21b2181ccdd5b83e065cdcf7b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:08:34 +0000

    recovery personal practise keepup open

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import numpy as np
-print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
