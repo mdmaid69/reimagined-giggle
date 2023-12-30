0d2173b35dd32c0e17a0a201aa0c4ab3648654de
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:26 +0000

    very soak scream soil breakfast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
