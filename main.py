1622f3234a43bf003ccc2c27b89f3c6cf13c0593
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:31:48 +0000

    grand holdon miss position near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
