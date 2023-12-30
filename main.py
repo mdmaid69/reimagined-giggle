a83314cae197fe4c63c95e31e78137d7174de6c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:14 +0000

    final number idea talk pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
