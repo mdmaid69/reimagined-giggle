471929711ff7c1aa996183e3945935ed1b78686f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:47 +0000

    side telephone late completely create

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def is_even(n):
+        return n % 2 == 0
