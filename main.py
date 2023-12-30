8d3cc99c758e04a39296bc2bcaa1150ab4cc715c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:22:18 +0000

    comfortable stare indeed teach traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
