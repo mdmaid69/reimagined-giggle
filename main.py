a7a48eee7f909a0b0c6463fc2d6ffe238daa1629
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:32 +0000

    embarrassing professional breathe runover true

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
