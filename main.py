b37b85c864b85abcbaae732ee118377dd9d00d6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:13:04 +0000

    callout almost until rain broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
