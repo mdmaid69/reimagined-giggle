30883d86525a8c95f98e2af7977b2c98ccc95da5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:28:53 +0000

    together forward plain scale form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_as_list(array):
+        return list(array)
