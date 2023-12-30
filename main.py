2c21b93d6f20fcf0a33cd2abfb07fd1868fcb95d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:44:16 +0000

    always funny very moon probably

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
