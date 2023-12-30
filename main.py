5216c786ff60b07048a7e9614b5e92d4bfc5cfdd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:46:12 +0000

    confused ourselves grade press bin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def convert_to_hex(n):
+        return hex(n)
