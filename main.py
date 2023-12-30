bfcec21ac2546c997b0b3e7a99871bea592d7d54
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:37:43 +0000

    disgusting cry tragedy company tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def square_number(x):
+        return x**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
