b60373f4efb4ef5893abd03a570cfbab608eb07b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:47:02 +0000

    tape collect dare mess mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
