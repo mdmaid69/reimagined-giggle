4cf0f6305cd93bf1f8203e8c4f0ca53d4a2a785b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:42:52 +0000

    clean invitation kind punish convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
