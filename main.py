78a768ba4f1e5ae2cad2b02d07ad05fcbb6411a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:08 +0000

    warning take return bread embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
