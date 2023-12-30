8abcdab2caa50a2c8d014f0b59861fd54e259fb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:34 +0000

    sad between nurse Tt succeed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_binary(n):
+        return bin(n)
