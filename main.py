1b94d88000ba979cdbef6359bb261f8bed99f4f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:01:51 +0000

    bury head against spin wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
