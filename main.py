7a68d2dd1f55c67854db4ced09ceaee79a969d02
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:32:55 +0000

    bill waste die anyone principle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_min(lst):
-        return min(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
