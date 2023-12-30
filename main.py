a5143735685fed07bd7d3474b414d37671c8c9c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:32 +0000

    runout glad past dream path

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
