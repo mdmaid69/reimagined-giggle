4e94dc58c561f76f24adfe46e494c2f2d8e5626b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:55 +0000

    basically achieve pretty tone their

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
