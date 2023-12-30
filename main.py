9aafd04e18bf155db4b89c1b618721fccd80635d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:59 +0000

    dark fair he bridge today

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-print([x**2 for x in range(10)])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def cube_number(x):
+        return x**3
