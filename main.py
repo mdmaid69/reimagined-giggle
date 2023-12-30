fc8da742cbbe069e6807b83f62f1991688355c5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:08:29 +0000

    hour well row measure so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
