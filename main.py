4989554840e4296ed8ad4b3c8ea159cfd9b16ad4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:59 +0000

    tune search forget my glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
