0a2499ccbfa2fdcff3698289e643dff2a63ede60
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:15:50 +0000

    admire enthusiastic mark before sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_list_from_array(array):
+        return array.tolist()
