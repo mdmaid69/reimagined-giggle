d0815b29e38f9dbc62dd172eaffd293118e9cde3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:24:06 +0000

    explain base voice song every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sys
+print(sys.version)
