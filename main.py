0db6395fd583a40d19e247d2bfb54f30e446bf6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:19 +0000

    match argue hate roll rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_str(array):
+        return str(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
