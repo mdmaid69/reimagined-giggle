6c7a59eaa40aa3df74ad111728fd8a3509353ccb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:17:10 +0000

    question throughout reason bath lend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
