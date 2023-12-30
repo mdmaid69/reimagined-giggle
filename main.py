da9bc94417280aebeebbcc2dc13510728c3358f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:14:15 +0000

    busy several later foot willing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
