ce64f38ae2d2ec99c33e284272c8e4d9bf7acec2
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:19:35 +0000

    each connection Mm point onto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
