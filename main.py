ef801bef4efec0db33d7380900de0903748f36dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:11:53 +0000

    born happen holiday ring future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
