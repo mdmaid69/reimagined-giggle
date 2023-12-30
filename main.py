78a8b3bb5507433b414ea7712fc6992c03383183
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:03 +0000

    worst regard ear lookaround across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def reverse_array(array):
-        array.reverse()
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
