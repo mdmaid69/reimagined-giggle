19eb69cd1dd23f23e349b18a453016c4723712e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:08:25 +0000

    fit beat neat describe underneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_max(lst):
+        return max(lst)
