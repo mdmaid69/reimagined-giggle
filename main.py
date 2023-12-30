d13b7f7bcd7174918ed4f3cf9c25da97d46b901c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:55:24 +0000

    shame greeting difficulty computer measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
