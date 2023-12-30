6d70188523e5351af53ad9d3d7140c1a2d51cecd
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:21 +0000

    near rich smile attempt entry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
