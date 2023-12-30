e96b64d654f8d9bbffd10d370f24539502d6152d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:17:11 +0000

    good its interview stand simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+text = "Hello, world!"
+print("Characters:", len(text))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
