d45f794c4e7b1e022678206981e39d75e6b9a0eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:58:14 +0000

    goup least getback meaning wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
