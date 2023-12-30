3b3571cbad65dfd50f3e7c2c3b230374bfadb0f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:00:39 +0000

    threaten level roll slightly much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
