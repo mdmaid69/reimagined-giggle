5b8e81d0b9c74b56763e5b8841b5f8a4f407dd17
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:26:28 +0000

    tread wake adjective back debt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
