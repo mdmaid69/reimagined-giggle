aca228d38f80299e9245c41d8eff50677af3bbeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:02:30 +0000

    involve pretty subject present patience

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
