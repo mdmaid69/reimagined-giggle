ca4308066ee64935c4b3821041486a04d47c165f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:08 +0000

    holdup interview with maintain smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
