0e26fba5b058370f0d616ab6b898ecf34636d1f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:14:45 +0000

    duty meal house crash excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
