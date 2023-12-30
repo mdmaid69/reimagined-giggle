7cd938921c6631e10f228c844698284be688cf99
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:49:03 +0000

    food broken stare sport road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
