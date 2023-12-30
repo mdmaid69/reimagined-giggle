237cca2f59f04e55c0982abded52b9b89d08d386
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:38:15 +0000

    turn satisfied again throw service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
