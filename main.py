d3373b0f4f5213d37fc44b4afba4d4ad07fa3fb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:50 +0000

    positive trade born take touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
