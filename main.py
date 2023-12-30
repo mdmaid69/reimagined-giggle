77ee8e5073e942a1eea7684154bda61a1dcacec0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:53:23 +0000

    artificial miss pleasure typical century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sys
+  def get_python_version():
+        return sys.version
