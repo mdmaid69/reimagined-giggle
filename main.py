76c3775101a53a76f3b27febb02641d6512534be
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:31:24 +0000

    ring adjective real measure save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
