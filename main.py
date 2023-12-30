44da984b4d87de11875cf73f7ca28d971a8fd124
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:23:01 +0000

    later responsibility breakin in round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
