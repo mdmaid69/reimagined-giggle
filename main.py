1a5c6f4daf213d8383b33a58ac6f85d34a184267
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:30 +0000

    spot against challenge before must

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  def subtract_numbers(x, y):
-        return x - y
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
