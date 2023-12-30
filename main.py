04c66667d8a039cdf93bb9f5408fb2c5698df79c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:49:20 +0000

    confusing expensive state chat height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
