524355710ae4668b7cbd304abc857ce3fcc87526
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:36:25 +0000

    organization fault afraid involve contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
