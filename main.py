6992ed0031c3457b331856a690f3405862c2d6d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:13:57 +0000

    combination respect destroy load hungry

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
