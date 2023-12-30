bd572ea58667715d09725aa3caa0cf1eae859c3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:12:17 +0000

    show warning change comein cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
