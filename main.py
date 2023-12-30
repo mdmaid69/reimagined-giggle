25fbbd9e064e618f57938ddedfa0c03403241450
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:34:59 +0000

    upstairs stir foreign swimming silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import time
-def get_current_time():
-        return time.ctime()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
