a5e1dac89f18196a8b2986ac66008407c6034ef3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:31:11 +0000

    flow in surprising watch between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
