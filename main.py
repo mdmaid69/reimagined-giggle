00a415cf95fe7a3dd00c42867a03debdd64378d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:30:05 +0000

    problem ours material new usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
