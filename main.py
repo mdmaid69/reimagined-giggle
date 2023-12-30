65763391d7399b6b08d29d450888c65a2ea5f368
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:24 +0000

    where lean present fair steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
