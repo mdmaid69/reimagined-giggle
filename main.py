a87dd80247ab48d8a57fd99fb02ae996aa6b52e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:31 +0000

    moment scrape estimate difficulty exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
