e2884d507e7cbea9ac4f1adedf8e37c1c5e763fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:10:32 +0000

    slice friendly emphasize emphasize worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
