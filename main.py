670d88d9c0f394959d2f23c2800feae80c823652
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:13:20 +0000

    copy anyone final spoil touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import random
+print(random.randint(0, 100))
