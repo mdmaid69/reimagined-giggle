77db655401d3e598202bedd3c5c24c9ef26c2455
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:11 +0000

    may pause brain object life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def is_even(n):
+        return n % 2 == 0
