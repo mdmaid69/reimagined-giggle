e50808236a70a36505651b892596a4eb7bcf9570
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:49 +0000

    slight exciting ill answer send

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
