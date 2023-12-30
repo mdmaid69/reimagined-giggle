2200b5e2d5ddd33568ef8cc1cbed582ffa24800b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:29:51 +0000

    corner think hard agree seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
