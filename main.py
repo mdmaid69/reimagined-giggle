07aaaf9e8980c87b389c860256feaaf5286b7ea3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:51:33 +0000

    keepaway believe pair keepout stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_work(force, distance):
+        return force * distance
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
