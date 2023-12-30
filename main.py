9c33e1946d142bd04777a378d4b1fc1a96ba8365
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:43:57 +0000

    school connect no case connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def reverse_list(lst):
+        return lst[::-1]
