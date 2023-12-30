ae11151d83b1f0ddd6d3ffbe9adf4fe74d81338e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:59:51 +0000

    replace difficulty too terrible measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
