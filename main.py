6819f268f8ec08e60c78f0922cece81e3a81f2e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:57:29 +0000

    roll past method sticky contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
