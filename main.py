387a25766ba1c092ec60c7fd5d262eb140f9b70d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:48 +0000

    passage row within recommend Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
