90022bf5eb72b41f330323c7aff65e10df2ee43d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:07:13 +0000

    line different mass practice sort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
