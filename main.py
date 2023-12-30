cdb960277a51688c064e82ed1c08ac6883b4a41c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:31:52 +0000

    woman minority turnout impression cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
