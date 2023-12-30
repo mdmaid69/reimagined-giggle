73cd4ef57c57f7494f2db1dbb16281993b616c39
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:12:28 +0000

    row actual regularly either radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
