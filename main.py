c754ca522272e4199efb3eaf7bc4411594ebb342
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:12 +0000

    window extremely interfere turnback should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import platform
+def get_os_info():
+        return platform.uname()
