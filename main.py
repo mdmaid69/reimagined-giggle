c0e99a457d150e44204ace9b449afb3984a348a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:44:08 +0000

    message extraordinary style basic wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
