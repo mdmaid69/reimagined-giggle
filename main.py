6e43d8393f50a188ee2af94fe2c76f97e6d627a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:21 +0000

    control runout nice awake please

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
