f90cb48fb1d1a00ab47b53dda158b44a989f5e15
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:15 +0000

    intelligent thirsty among leg 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
