a20eb858ff05ae71c9ff83cc3b28d5c8e245ccf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:17 +0000

    special brain plenty realize total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import sys
+print(sys.version)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
