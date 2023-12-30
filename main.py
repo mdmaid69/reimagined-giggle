5621271472e121078097d1d5fc38ebadb2a2dac2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:20:46 +0000

    afternoon onto neither demonstrate impression

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
