1015088c69a7f8e13110282e7432a47c64c1d91c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:58:18 +0000

    shame favourite effect reliable getout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_set(array):
+        return set(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
