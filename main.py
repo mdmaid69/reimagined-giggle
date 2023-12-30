b89367d10b18d65edd65fbca079ce0c546c8bbf7
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:48:57 +0000

    standard slide slice callout callat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
