a42fe742c1982965342b0f31eaf0306a45ac778f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:52:06 +0000

    powerful high bringback love answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
