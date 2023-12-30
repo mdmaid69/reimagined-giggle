14f69bb3b7d3211c237c311a4181ee1eb1cd734a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:36:53 +0000

    cheek relation direct workon level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
