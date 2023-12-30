8bdafdf38b644e2813c49b76c928dc02bbb88342
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:07 +0000

    voice tall last ticket ring

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import time
-print(time.time())
+for i in range(5):
+        print(i)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
