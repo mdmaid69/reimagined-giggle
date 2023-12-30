a60e09022b03fd66401430641177c94956cf8073
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:13 +0000

    command blame it effort situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def get_array_as_tuple(array):
+        return tuple(array)
