3a7dfac8ea82969007c17de1d779792f9ddf624f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:44:36 +0000

    future unless pipe rare they

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def check_if_array_contains_item(array, item):
+        return item in array
