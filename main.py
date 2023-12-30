425c0b641a2cb203fd090a4e287b83129e7c03b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:48:31 +0000

    base work stop discover runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
