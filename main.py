3187f98319b21d7172b38c9f0f930f98f1d7291f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:11:23 +0000

    phone splash chance entrance safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_counter():
+        return collections.Counter()
