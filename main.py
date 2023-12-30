5471b0981e73a35e2abc653459e108bc9e899abd
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:30:55 +0000

    letter pin no shall health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
