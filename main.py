59472c81754d6c4a749bc981ddcfbe88bd9c80e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:16:57 +0000

    hold minimum spread relax design

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+from collections import Counter
+print(Counter("hello world"))
