d4b6b6087d1a0becad9ca1f2afb65dba910c0005
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:14 +0000

    vote path force recovery present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
