1bcb888721658d3ebfb2c21669e9d696c702bdf0
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:43:56 +0000

    any let unable regional Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
