7aed492cee569f859c0e1ed20eb9533877e77b2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:21:36 +0000

    last think bend task risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
