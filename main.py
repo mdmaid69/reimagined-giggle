a2d1b8b5e99a69d6a220275b7883b99147af18b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:51 +0000

    chat content sleep genuine board

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import time
+print(time.time())
