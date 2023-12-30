49ea799fb7ce73f0b623859255c4873083936ccd
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:59:26 +0000

    criticism object pronounce have great

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
