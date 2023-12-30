f8e251ca3e8ae551493e246c0d7ffe039d809ec8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:31:30 +0000

    post release kind speech mind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
