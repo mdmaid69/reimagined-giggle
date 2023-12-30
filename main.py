b4d99484f13e89977510a875b5728d964a73018d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:51:18 +0000

    concerning it city fierce slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import datetime
+def get_today_date():
+        return datetime.date.today()
