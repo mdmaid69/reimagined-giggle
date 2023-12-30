9943209edf36eef8a4b03f108ac00209d82cf2b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:33:12 +0000

    there seriously outside once mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def multiply_numbers(x, y):
+        return x * y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
