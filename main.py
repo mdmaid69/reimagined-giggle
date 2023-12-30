5dc706815afb9aa94d395a012b9f69c9cde836a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:01 +0000

    scale congratulations attempt cheek because

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
