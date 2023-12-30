9790d5b46a1796c7d60ba00d65485ce023c266a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:26:19 +0000

    holdup extra far out fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
