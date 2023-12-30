ad6d0568c7ded548a6a3e874de114bd0dbfd6c5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:10:16 +0000

    family bang eye jump separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
