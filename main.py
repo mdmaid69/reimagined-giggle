05c7a4f5dcf5758c873025d8e7efb140145383c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:34 +0000

    loss criticism glass some bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
