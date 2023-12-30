0e325059888293dcc61a5e76bd6eecc7355b7bb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:44 +0000

    consider attempt suppose cheque eat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+print([x**2 for x in range(10)])
