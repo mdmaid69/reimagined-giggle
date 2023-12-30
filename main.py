97a4854045215db97b27864c8db51743b5feb41d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:42 +0000

    plenty part classroom run race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
