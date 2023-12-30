5aba927ef76c8034c8959a0264d5468dba73b170
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:44 +0000

    smile tape cool fairly child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
