c5cf831c40772820f8b77599577697edea2221d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:42:52 +0000

    message consider precise bear circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
