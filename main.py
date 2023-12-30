8d468069bc1a77ff54bea61b035d0754ecb43c84
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:06 +0000

    takeaway manage connection slice pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
