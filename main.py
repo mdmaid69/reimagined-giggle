f2ed9521f549b9cbc2faf7f9a4cbd24e992ac794
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:41:25 +0000

    draw explanation brave unite hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
