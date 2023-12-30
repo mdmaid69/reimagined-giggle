8ddab17b50d316658339773806c4c8202755c5d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:21 +0000

    simple whisper answer instead runaway

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
