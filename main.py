3627e9d38800d291ed318d1b2a8ad59dab540453
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:43 +0000

    genuine talkinto rough estimate angry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def find_max(lst):
+        return max(lst)
