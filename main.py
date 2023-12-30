5f408511db44c8b3a661e677d25898d21855f4f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:05 +0000

    rush furniture insist challenge several

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+print(math.pi)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
