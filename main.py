ebd4afb2b74464afc39b10b1a8c13a1565733c84
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:13:50 +0000

    slight dull container strike mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import random
-print(random.randint(0, 100))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
