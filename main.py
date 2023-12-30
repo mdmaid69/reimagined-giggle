6a01dc9bc759adc167c1cabe20e74b8c0e73deb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:35:35 +0000

    whoever realize high polite dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area(radius):
+        return 3.14 * radius * radius
