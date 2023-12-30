9b0c3c4bfa07b74fefea5f00de773e32846310be
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:55 +0000

    manage area goup squash ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
