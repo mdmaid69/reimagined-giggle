3060bedb679a882d04e1eaaffcfb4d13d1f0e45d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:46:09 +0000

    sharp unite impossible hello prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
