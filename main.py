be962ecd85a0664e9d282937d6b8bb489ba1b0dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:40 +0000

    slide moon around cruel Uu

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def multiply_numbers(x, y):
+        return x * y
