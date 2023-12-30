1d37c10de4a666526498885bbef0f11b4e3b3530
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:33 +0000

    add hot object black basic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def find_min(numbers):
+        return min(numbers)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
