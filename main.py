ad4104a465ceec62db525cebb14e799d10e42444
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:13 +0000

    whether ever production solve run

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import numpy as np
   def calculate_mean(arr):
         return np.mean(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
