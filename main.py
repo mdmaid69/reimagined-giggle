53875655ed92b6be6c9228da5f81a01c6f70ff27
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:39:22 +0000

    impression direct previous quickly machine

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def reverse_string(s):
-        return s[::-1]
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
