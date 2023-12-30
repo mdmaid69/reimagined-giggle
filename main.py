5a617cf030d96fb60fcdb500e390ac32fd1ce724
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:17:12 +0000

    wonder actual attach angry target

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
