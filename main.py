2b03aed6ed5165bc77148beef191901e4e001b47
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:45:43 +0000

    welcome punishment brush divide help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def calculate_area_rectangle(l, w):
+        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
