6285b296ed1fbb8531ac6c46a742e495033fe67f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:26 +0000

    capital welcome pain niggaacademy hair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
