000a1336c261d784deeaa2f32bf6dfdc5845b17b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:53:52 +0000

    defend determined land score woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def find_max(numbers):
-        return max(numbers)
+def get_array_as_bytearray(array):
+        return bytearray(array)
