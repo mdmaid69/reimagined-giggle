5c500fd7342fb3bd16f2d1406ecdc0f9324a1719
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:31 +0000

    call tongue damp avoid feature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
