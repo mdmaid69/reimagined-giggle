cce1846dd158dac88ce8b0d9b07774bb43cc29ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:31:20 +0000

    rather modern impressed hard boil

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
