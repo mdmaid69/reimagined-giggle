9ed2e8bb8c9928e04f707836d56a6948a94fd938
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:36 +0000

    blind doubt save certain visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
