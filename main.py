8c17207dbc5447c225b2a2db389dcaf9f2f3c428
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:24 +0000

    comeback read demonstrate fact low

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
