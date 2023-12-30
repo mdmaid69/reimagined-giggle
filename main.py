5f81bbf20d215625781cdcce2a0181a543713672
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:53 +0000

    no noone leave section affect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
