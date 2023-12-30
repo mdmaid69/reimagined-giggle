2592d20658e1c2ae5d00e0d31189ceb677e0bbd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:36 +0000

    trousers closed drink here land

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
