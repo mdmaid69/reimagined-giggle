15c7e2c3cc162e13fe4ab2a2a05b28d5f5a82aa5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:16 +0000

    compete to lookafter separate move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
