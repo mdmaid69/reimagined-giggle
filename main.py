463349d5c31608bea7ad2ed4dbbdd69e6e2de3da
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:02:17 +0000

    easy issue guilty ever one

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
