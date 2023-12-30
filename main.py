a9b7144794513750cab4d78a0866267c52fa6756
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:50:40 +0000

    note pool herself bottom chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
