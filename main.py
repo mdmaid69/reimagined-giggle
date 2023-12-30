4406cd86be22599342785da01f75d857d883ae36
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:36:30 +0000

    theirs past bye regular workout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
