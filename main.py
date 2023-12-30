3816d2f101d981a703c25fbd9e0b9c0264d6041a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:23:24 +0000

    cut gift tonight human scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
