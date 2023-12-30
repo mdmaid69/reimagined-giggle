314e60ea75eb6626259202a0d512014dbefcdb72
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:57:29 +0000

    takeback accidentally through allow use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Characters:", len(text))
