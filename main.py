aaa10eae578168f8036edc547dd8fd2cc8fb378b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:35:49 +0000

    attempt raise something convenient rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_min(lst):
+        return min(lst)
