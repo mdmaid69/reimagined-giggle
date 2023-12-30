b217abb5d6b5280bc072a477cb633ef2eac28e78
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:27:00 +0000

    off quick bin valuable once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import sys
+print(sys.version)
