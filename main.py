5bebd8ed66f28ff084a5983a09de0dc22b5d538d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:32 +0000

    spray dull check head unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
