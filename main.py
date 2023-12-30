f48abc1db4ace99bb8d6f22bdfc65969a405d4c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:43 +0000

    wing pipe rich now achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
