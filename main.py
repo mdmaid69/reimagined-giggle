24667951f34f525b1156f5dcda9b67f8c8614bc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:36:01 +0000

    Qq stupid workout come involve

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
