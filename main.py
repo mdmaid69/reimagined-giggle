31783d6d91b05e5dae81751e9b622c92b4b66b74
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:36:35 +0000

    Nn paper past runover represent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_list_from_array(array):
+        return array.tolist()
