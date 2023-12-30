dff30e3cd284f1e6a1e90d6fb70ad1494507be3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:49:31 +0000

    hope Itself letter water contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_volume(length, width, height):
+        return length * width * height
