6b4a34de1bd56f97ff4b37fba63ed474beba3117
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:29:00 +0000

    they project crime clearly grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
