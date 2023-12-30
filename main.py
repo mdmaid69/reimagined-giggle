9c5e95480a285293f0987fe90d1c5a6c991ea654
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:46:23 +0000

    past steep private swim original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
