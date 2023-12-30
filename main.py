a2476cc98f2e9d5f650986bc4b480936af593d5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:49 +0000

    instead conduct extremely empty cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
