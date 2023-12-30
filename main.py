9c41e163c20b2da355382de290b737174c980b75
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:25 +0000

    point nurse measure principle wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
