96d65dec906d6cca6860161b2c17c6fcf4056f22
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:21:45 +0000

    set top once interfere broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
