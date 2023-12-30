739522d8f3a400363bf68559ffdfbfcf26c1ee66
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:18:25 +0000

    lend sweet pay belong charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
