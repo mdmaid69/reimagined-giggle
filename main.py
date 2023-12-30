21e8fbe03242723af07ab69f503b8e9d9a28408e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:30:38 +0000

    fine unlucky transparent eager display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_item(array, i):
+        return array[i]
