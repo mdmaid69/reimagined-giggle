89e1e44a16a7fa0bbd3df45386b8e7887cd33637
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:19:06 +0000

    godown character although fast necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
