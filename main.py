8937b3c96858df02a4e3c9861e5b212f8fb187d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:20 +0000

    rub while blame wall relation

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
