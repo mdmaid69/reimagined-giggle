1896c6a8a468311b210a53ca88de4ab57817752f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:14:32 +0000

    under thin holiday report police

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def sort_list(lst):
+        return sorted(lst)
