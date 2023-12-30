bfc04d6eb7ae5c0b551a8b9228525531175089fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:50:53 +0000

    ourselves getoff although it love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_str(array):
+        return str(array)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
