91b41588bdfb8e4ed05c336ed06640c0236c855e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:01:25 +0000

    judge refer makeup treat recent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
