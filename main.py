4dbb8fdbd1db3b5a4a77e105dc338fca7e28bd34
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:53:18 +0000

    male wind happy tooth flow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def clear_array(array):
-        array *= 0
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
