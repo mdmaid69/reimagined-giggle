633f753fa718154d9487dbc3a5e3b060d6af43d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:23 +0000

    ticket complete control answer imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
