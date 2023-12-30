308041c259cc9320b2dd63295c32af5edb444b32
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:15:21 +0000

    thing concentrate partner thorough flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
