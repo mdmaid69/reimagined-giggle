b8fd17a15f5b367e263f3d7fada5174c0edd88f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:47:26 +0000

    copy myself increase minority rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
