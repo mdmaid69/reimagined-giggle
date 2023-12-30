976b9f3f5241dea1d1d994de35cf96294471e2f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:16:33 +0000

    example will bend glad empty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
