2a26558701c73585c1895af8d62f1a16ed409233
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:45 +0000

    prize comeout final through not

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def create_user_list():
+        return collections.UserList()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
