f2673bdb7d4c49242cfea89e4c2c04a80e3ba5f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:29:18 +0000

    always interview special fix however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
