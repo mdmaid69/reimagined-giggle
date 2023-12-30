0713180a3f644d4861b9fac91b2531abbabd244b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:43 +0000

    last visit speed trick effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
