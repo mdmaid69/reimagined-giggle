691ad23c9609f31ccb29eaa1ca3fdb6dafcc0b62
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:05:52 +0000

    shock breakup hold quit Yy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
