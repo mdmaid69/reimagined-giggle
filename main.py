664562aba5249fa67de749bf4f5b1482e8c51e5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:10:25 +0000

    appeal seem deliberate complete large

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
