1de16e80b2c6f54eda925ae40b05d330649c6050
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:46:18 +0000

    letter quit owe scale bound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def append_to_array(array, item):
+        array.append(item)
