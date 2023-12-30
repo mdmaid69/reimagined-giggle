4f7a037340a907a0d0e3076c3cc6c0d258d591df
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:26:07 +0000

    strange save report before cope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
