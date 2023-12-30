740d6c9d7894f500ebb00168805cd2277c6d1a0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:15 +0000

    Qq test breathe male tend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
