dd394f74e4c1aa7626c18ae49a2a26fa746c13a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:16:34 +0000

    target compare which body its

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
