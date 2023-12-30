8a3b7768afc1127d9ad1168bdb5232f0cdcd0207
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:47 +0000

    excuse introduce bin pure test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
