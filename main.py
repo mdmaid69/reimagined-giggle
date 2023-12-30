32a3dcdde4ac23110b35c7a8a7482522276977e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:04 +0000

    middle cheap back by front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_typecode(array):
+        return array.typecode
