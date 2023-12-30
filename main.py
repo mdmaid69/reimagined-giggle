2da4d6a816e4cc5ea46a5c7042a87f87c8966477
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:21 +0000

    match probably chase tap punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
