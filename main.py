3c210915120036284cd03d15ed52fd7ea8350fd0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:32:31 +0000

    quarter in car board luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
