9a4daae2dc73071d40fae4b81cd2d0c09f837995
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:52:12 +0000

    clue too success future breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
