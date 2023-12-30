f8d87d2b6f2bebf5cc58828f20b5ba9dc26a9fa7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:21:41 +0000

    climb proper our entrance tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
