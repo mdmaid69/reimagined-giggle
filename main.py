c3009269a4fe07d3e4c36a479e6e8c933b767f91
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:57 +0000

    consist final attitude round turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
