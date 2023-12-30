05c01658e42e42e8e02ba05ecc4149c7abcab6cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:45 +0000

    loan brief string sort long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
