0269d1bc4b86bf7631c98b3102cd78d7ae6f78f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:47:50 +0000

    government check read exciting repeat

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
