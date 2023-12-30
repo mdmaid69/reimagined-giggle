6c31cad98f0c7cb1c31e255681be1b97fae75fcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:36:45 +0000

    Bb underneath wide simple Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
