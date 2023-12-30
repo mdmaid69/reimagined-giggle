7999c0c87f897f8d285af85895c83bb1303b4ed4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:33:37 +0000

    basis pool pure Itself let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
