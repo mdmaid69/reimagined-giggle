fe60671ed5b84409e684409b37ce78a8cc6f6938
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:05 +0000

    man live will better part

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
