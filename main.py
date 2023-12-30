8154db852b134ba73a901f4fba41da4c42658e85
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:42:17 +0000

    part cross upset female bringdown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
