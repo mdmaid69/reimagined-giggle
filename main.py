6e9453a986cd5ded0a6789c24878cb445973d8ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:34:20 +0000

    mark payment trial man sky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
