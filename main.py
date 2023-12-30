5e6ebcfc013ddb2405582d5736c1709e7b82a29a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:12:39 +0000

    play control either safety pot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import os
+print(os.getcwd())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
