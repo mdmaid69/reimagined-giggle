e3cfe75d6ea8707d63504b23739e971ab7bfe5bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:35 +0000

    woman convenient disagree provide wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
