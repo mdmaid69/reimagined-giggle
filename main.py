bb2f90375112aad58f11c475746cc8c7da6ba52c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:44:57 +0000

    past check against age to

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
