6c9a654afae9d5b9d9322a8a61d50fa3c9295cd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:40:59 +0000

    belong obtain mess this luck

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import time
+def get_current_time():
+        return time.ctime()
+for i in range(10): print(i)
