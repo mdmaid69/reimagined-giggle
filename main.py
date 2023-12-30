215a79db9b712d0115e980f234a31712042e64be
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:32 +0000

    ago inform friendly blind fair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
