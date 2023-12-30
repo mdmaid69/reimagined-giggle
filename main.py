1bea90150d0cf21e54dcf8ea566c21c04daa39fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:42:51 +0000

    speed lookaround shade compare just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
