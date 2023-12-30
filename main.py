0aa4d6bed72b63f34b1df49ddb5fab7cbdc6c057
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:54:00 +0000

    cold entry hard shout phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
