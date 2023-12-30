3d0c2b0e85a81e06e936a306da8f8c3715a227c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:37:40 +0000

    traffic estimate object give lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
