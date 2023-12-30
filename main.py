bc5a266a38d37525aafc22584037d4ab6bd187c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:58 +0000

    post attach without claim exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
