fadd3cb245dd9673ed156fc207c42ba7e3f36353
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:35:12 +0000

    equipment permission introduction brave popular

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_area_rectangle(l, w):
+        return l * w
