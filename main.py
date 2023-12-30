c8f9d81488867eafa148d89e3f568c46fd2de188
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:01:33 +0000

    calmly belong describe regret sport

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(numbers):
+        return min(numbers)
+def convert_to_binary(n):
+        return bin(n)
