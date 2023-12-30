b6b021b57ab50b5ae73fcb28ab3e01758f07f0ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:55:12 +0000

    lower several emotion kick safe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
