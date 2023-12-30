ebf2f26b2f0211878b9ca27cb73dff819a980ff1
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:53:03 +0000

    clothes takeback dare vanish maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import sys
-def exit_program():
-        sys.exit()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
