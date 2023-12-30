660bd52173b9e9b880ab986ce06dc16ced388cb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:11:45 +0000

    neat angry date sign no

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import sys
+print(sys.version)
