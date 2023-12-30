ed0c570db53a37fb038695c282e457bf7f5052dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:03:18 +0000

    pinch tidy present weak spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
