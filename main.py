cafb63679eb872e56feae3db0dc8fab9a6086329
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:30:53 +0000

    hard as absolutely notice share

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
+name = "Python"
+print("Hello,", name)
