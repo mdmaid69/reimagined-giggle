b1972f99b89c49b48c0ce66a1e9ac0c4871c14d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:15:43 +0000

    often closed limit accident care

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
