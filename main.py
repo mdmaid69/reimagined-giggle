0925b86b4c97400ffe943cf4533e5326a7616a11
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:40 +0000

    collect direct travel shop wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
