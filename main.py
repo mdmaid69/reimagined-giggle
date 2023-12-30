f6488fbf98b2b76e1c8559ba53e5c999cd504418
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:35 +0000

    spring cross learn equal play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import sys
+print(sys.version)
+import array
+def get_list_from_array(array):
+        return array.tolist()
