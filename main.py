ee2dac6357884044a237941b33d2373e615763a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:02 +0000

    comeover combination face pronounce talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
