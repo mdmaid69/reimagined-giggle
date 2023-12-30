fbe565cf412847fbf88e80b877246394b460004e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:33:26 +0000

    tidy complain suck perhaps fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
