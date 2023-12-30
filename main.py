d27f18ac9db8196e426e97fc373f13b1691a9d6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:05 +0000

    mistake bang look substance improve

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
