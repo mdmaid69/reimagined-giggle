3d8d8a7e8e4639fcf45076e7e1ae21535708a422
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:16 +0000

    sail takeaway neat instead life

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
