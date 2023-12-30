3c6efa7566dfa91741bd14bda8b8c18ba7a26297
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:02:17 +0000

    pure sit shut disgusting onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
