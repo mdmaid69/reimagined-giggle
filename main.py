fa3ebcca97424f7ed3f20fef143d58ad3cd3de82
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:18 +0000

    size term 569enjoy regularly about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+print(re.match("h.*o", "hello world"))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
