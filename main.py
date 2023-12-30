2f637136ed59dce09933d7d32881c526c58aaa6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:07 +0000

    situation callout soil arrangement image

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
