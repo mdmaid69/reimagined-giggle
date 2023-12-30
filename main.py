6d2b2850939169cafbffd9b0a8e35202b80f791c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:35:25 +0000

    hit transparent lean search destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def add_numbers(x, y):
-        return x + y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
