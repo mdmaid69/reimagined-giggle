1617b1161e78f829440644fff03bd3a1624ac9f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:33:58 +0000

    whoever country money detail professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
