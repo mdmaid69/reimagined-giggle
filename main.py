0adefeb780273e73259b5e5b5b7d42eb649b07fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:18:59 +0000

    time over shop dare colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
