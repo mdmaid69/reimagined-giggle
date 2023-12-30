a2b873b52d6c9ea8653cc14064bd06659bf35024
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:55:29 +0000

    cheat treat coat Ee insist

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
