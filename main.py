6843356b41f996f6df7b6066a6da539f0cdc6fb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:08:50 +0000

    sharp do likely search run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
