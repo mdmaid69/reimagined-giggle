82c8cac8dfa9847ee7c123127073d4276ad4d545
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:12:02 +0000

    talkback book talk sensitive into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
