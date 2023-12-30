11c83c2bb735c2cb58b44aed42f963dc71df6902
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:56 +0000

    suckme whole strike future maximum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
