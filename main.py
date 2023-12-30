b8e50e7e0d9891e2ea43055ae414823e3fd824d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:30 +0000

    lookafter lift state friendly clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
