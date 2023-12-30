7210ca32c125ac4e5e693dcc3b5c0ce69c48e003
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:01:07 +0000

    subject emotion flow book want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
