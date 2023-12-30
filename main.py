f99a21258f026dcd2035a4c2329ed47ca182ee63
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:28:38 +0000

    night far ago clothes stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import collections
-def create_counter():
-        return collections.Counter()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
