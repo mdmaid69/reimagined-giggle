bf317a76af6218d481d6eb4ee6ffc2a6ac46ff38
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:06 +0000

    spread height instruction on style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
