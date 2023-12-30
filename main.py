bab96f48433fcf66eef64c725c2b6d93e92cd422
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:28:32 +0000

    ball fire above telephone anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
