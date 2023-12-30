69d694aef108114661825498369bfbadfaf1f094
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:37 +0000

    gate pay drive uncertain store

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
