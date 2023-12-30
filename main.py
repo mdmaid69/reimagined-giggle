bc261a5243c5c038a487ddab575e614b0f803035
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:56:52 +0000

    whichever base exam minute lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
