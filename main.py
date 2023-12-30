95fc429ed8c3344b90c45eef5f4e917edf5899c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:58:08 +0000

    trousers special object snow runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
