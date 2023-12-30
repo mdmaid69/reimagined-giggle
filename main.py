0e852a68b1ec0a884576f231b61147add3f51bff
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:01:18 +0000

    either bank against danger fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
