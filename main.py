40716e0e43b5729e74315b83ab4cbd9c50509895
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:50:08 +0000

    violence band ocean climb bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
