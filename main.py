1d63ff5885595ab1bdcb2cee4fcfa355aadf8fca
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:21:20 +0000

    pick where separate message yours

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
