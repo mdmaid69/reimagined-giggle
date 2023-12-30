ee3f2a0648709e492e52683fbf46e22e7f870d43
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:15 +0000

    talkoutof runaway bill anxious refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
