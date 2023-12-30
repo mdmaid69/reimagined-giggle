c7df22e05f58c5a2c8e2ad88bc6e27551f87547b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:04:02 +0000

    war else war zero shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
