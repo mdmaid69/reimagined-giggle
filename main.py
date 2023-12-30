472013ccb61745f627df4529bc5d1083a4e3ad30
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:27 +0000

    round although anywhere treatment available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
