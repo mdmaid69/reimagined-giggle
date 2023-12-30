0cae5d86b19c5222f0d6c56b1cfb37360823ffed
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:32:40 +0000

    fasten tight finish reasonable confusing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
