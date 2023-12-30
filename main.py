03298ce10bca8d6a4d0d73cfa6cc08272da42737
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:25 +0000

    holdon slice conclusion instant family

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
