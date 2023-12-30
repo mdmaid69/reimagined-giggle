4a2ad581c3251aa237e37c85083ddee8a6310cc0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:40:27 +0000

    home transport shout more such

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
