3fe69d8ac6498f0376349cd77287f559d26e3856
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:22 +0000

    ordinary poison run excited scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
