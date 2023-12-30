2f49ebb86bee3d8193ed89272651ba376ea27070
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:10:39 +0000

    load dangerous grateful pronunciation just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
