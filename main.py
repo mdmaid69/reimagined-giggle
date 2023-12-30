4fcb3e125c9221d0c10159f722bbaeeeda141d0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:25 +0000

    increase wherever guess interrupt hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
