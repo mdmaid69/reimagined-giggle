884d9412cb122a72dc30ca61625ea1b3f7fea0ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:32 +0000

    gentle wish until total proud

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
