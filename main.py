df63ecac429ffd008f2b7d294996581600d49995
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:11:32 +0000

    scared failure area runout uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def get_array_from_list(list, typecode):
+def convert_list_to_array(list, typecode):
         return array.array(typecode, list)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
