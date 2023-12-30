e7a1ef7f92948ce928d771fd2e0d345a027cd469
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:55:39 +0000

    Yy centre education compete closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
