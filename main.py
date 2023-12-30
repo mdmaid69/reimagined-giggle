ba84302acabe2316d80cb44359531258d302c688
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:46 +0000

    university point set himself rather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
