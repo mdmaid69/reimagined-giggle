d5297181e17dac690c24e75851dfba6d0dd693f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:43:21 +0000

    tongue doctor quit behind clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
