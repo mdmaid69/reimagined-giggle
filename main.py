f504abe7d67d80e2e7c634b4c1b0859f8e0fcdbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:06:29 +0000

    separate everything factory properly character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
