db3fefc103fbb56a879b5c21699e2d36ba99fcae
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:47:11 +0000

    ball production runaway nurse length

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_user_string():
-        return collections.UserString()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
