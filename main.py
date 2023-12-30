d5ea6f09fdaa01ff4d0ce258837609a35c2fa2ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:55 +0000

    shrink forever decide shoot accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
