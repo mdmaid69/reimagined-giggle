9aa719bec093ae76422b4fc9a5faef14b8866d7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:34:26 +0000

    precise whistle blind autumn fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
