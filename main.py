7bc78851cc7b6becc4be54cd9c95d68bb21d0762
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:57 +0000

    Qq fault easy start national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
