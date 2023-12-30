e9ef53256d125111d53849bc5c690acc608a0de5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:53:16 +0000

    explain identify life conduct total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def cube_number(x):
-        return x**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
