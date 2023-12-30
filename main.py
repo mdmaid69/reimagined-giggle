b1ad694309aa746b8a6e69442df352fe499b3e2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:31:33 +0000

    drive correct live approval nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
