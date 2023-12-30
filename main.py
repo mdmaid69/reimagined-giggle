274521722c29e29141d720e22f3e2d01ba2a016c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:07 +0000

    question lie happen conduct damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
