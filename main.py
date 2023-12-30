36bb5d972c7ed66689846e943dc67a62351fef20
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:31:16 +0000

    hello shop excited call 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
