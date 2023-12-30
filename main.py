422660123598628a1c36342ecacca638688e9d6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:11:45 +0000

    relation tap address considering really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
