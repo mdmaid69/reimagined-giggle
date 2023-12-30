e82562b056ad6b84599c7196f3d07ba468e93ba6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:24 +0000

    moral sink violence holdup die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def reverse_array(array):
+        array.reverse()
