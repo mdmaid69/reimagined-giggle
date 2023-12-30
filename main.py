90e620db7433db0aef974262133b55f3614dfb21
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:47 +0000

    speak necessary more grade hot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
