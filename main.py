5171659b8cc102c8cb09e711409952ef539c12f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:05 +0000

    announce open beyond replace welcome

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-print(sum(range(10)))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
