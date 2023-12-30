0621251184c3ecf1a4f1193efc2fb361b9ae4e10
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:14 +0000

    year lost idea start round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+print(os.getcwd())
