28c4162cbbac79e82cf6c566218cb03836337391
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:21 +0000

    pure spot progress build Itself

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-from collections import Counter
-print(Counter("hello world"))
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+print("Hello, world!")
