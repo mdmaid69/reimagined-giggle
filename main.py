773e8963d8ab178ad74568ca81ef89cb57ed5c74
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:33:09 +0000

    excellent complicated stare people deaf

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
