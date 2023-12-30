e78bcd509d90a0638138ba3bb3dfdfc726a4426c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:53:48 +0000

    ignore rank head acceptable empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
