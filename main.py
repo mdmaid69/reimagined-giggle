84692fcbdbf92f9b8d8abd6ec3f2d4d3fed4267e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:37 +0000

    shopping go dry behave offend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
