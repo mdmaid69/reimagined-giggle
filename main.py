7124bf00e8d3fdc56bd18875ab1f73707b1f8e5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:56:02 +0000

    spite smart except receive underneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
