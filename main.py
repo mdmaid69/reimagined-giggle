a3e87edb0ce0d496c5a374898a1b2796a1558f55
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:15:40 +0000

    plant makeup problem wave variety

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
