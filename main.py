0044c0e02344e345156f6c6e9fb1ae075b2dd387
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:57 +0000

    beautiful in sight bin slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
