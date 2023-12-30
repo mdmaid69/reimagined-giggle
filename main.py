ed71d300805d2cf0feb784b639643bdfe1ab75c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:10:01 +0000

    friend threaten weekend floor conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
