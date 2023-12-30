15dcd9357f48c6ebdd3bc5e2c5401c2c3058dd1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:54:09 +0000

    healthy bound breathe world suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
