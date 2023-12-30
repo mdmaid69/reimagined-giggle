8a4d20f967fbb63feab1fdc496391e9d55ca22e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:11:47 +0000

    coat obvious people breakin mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
