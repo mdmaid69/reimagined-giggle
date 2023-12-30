c1e333374e1dd54c24667b17642e1b471167676d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:57 +0000

    length guess memory schedule near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
