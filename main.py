eac0b58c695206d25b6d97c8b9b4100a52afcbf5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:29:26 +0000

    us model comedown run little

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
