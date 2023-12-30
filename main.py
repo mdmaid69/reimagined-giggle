3cddf3b55b8caaef24228eef5e064091dd1e80ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:43:25 +0000

    previous preparation fat violence tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
