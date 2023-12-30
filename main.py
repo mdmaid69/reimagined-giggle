d20bb5e8cea7394abf6e4ad5de0a680c7cd1e0fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:26:44 +0000

    workon famous good quit not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
