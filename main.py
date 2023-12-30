beb694f3c783afab2bc0d5451133be97ee47f166
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:14 +0000

    interview settle thumb boring joke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_base_name(path):
+        return os.path.basename(path)
