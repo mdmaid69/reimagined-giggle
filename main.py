88f55d21921fd8e636359681004c91415fa84e1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:18:54 +0000

    tonight fall go wet before

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
