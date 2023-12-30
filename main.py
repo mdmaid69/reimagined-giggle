68aa38d174041a7f5144eadb788be5fb4e98d25e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:50:14 +0000

    door makeup bite as allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_max(numbers):
+        return max(numbers)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
