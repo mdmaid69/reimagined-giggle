27b990229edfeff1b9dc1b3f40c4a39d9bb95423
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:33:31 +0000

    difficulty space pronunciation tooth question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
