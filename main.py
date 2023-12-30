3809ec68809ee37e40edba7062b555a65fbe07f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:45 +0000

    delicate dare please relax comeup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
