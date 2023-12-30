7d520e14733304de7ef211e1df4e90636d9d5a56
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:01:45 +0000

    praise ahead spot undo goup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
