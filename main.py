eadb64b80c5188c6176687c6879cee4fd760d81c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:17 +0000

    hollow piece tape sense but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
