45a8bb6e13e0636fbaec44193ee31bf760bf6cbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:52:42 +0000

    niggaacademy concern colour spin object

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
