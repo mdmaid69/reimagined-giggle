e216aeb9c4fc541db99bcd21e2d971ae578b8231
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:17 +0000

    wear myself introduce environment extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
+print([x**2 for x in range(10)])
