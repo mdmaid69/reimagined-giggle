5cc2113e8eec3cd843295967845e1b1375ff976b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:16:02 +0000

    project touch fault formal switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
