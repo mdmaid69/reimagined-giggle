27a597bfedea3bd16a3ea29bfd48520930bf3267
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:10 +0000

    rain under table fear back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
