71cb9d65f356bcb73d8c20a5ef38583a3937f8d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:42 +0000

    yourself trick price hurry police

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
