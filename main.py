2a7cf0b060002105b7d962bde9be983a64c67558
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:21:17 +0000

    Oo sound knock callat worse

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
