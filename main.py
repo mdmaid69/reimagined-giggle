9fee1653d72fe9af04589eab8f30f14fbaef94af
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:04:57 +0000

    company song shadow obvious quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+print(math.pi)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
