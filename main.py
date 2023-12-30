a23bbf0d415eed82dc85577df9764e8ae2e02cb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:45:48 +0000

    entry freeze part principle comeup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
