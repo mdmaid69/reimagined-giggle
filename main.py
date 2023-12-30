328f4827d9581f56a25f7a229980f635d14447b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:36:23 +0000

    game recent refer poison touch

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_set(array):
+        return set(array)
