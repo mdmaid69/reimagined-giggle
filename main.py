1266e547b1653315d2fd9c1a40337e9ff4960dbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:42:07 +0000

    understand breakfast national target unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def find_min(lst):
+        return min(lst)
