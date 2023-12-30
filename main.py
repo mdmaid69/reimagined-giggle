c28ae5e15e50f5381c61b744c770dd27dc3eb659
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:03 +0000

    post conclusion middle swallow story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
