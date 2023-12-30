ae74c0b7de63f57b8e8b5b125751e0959514338f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:06:09 +0000

    pump lock foreign certainly active

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_string_from_array(array):
+        return array.tobytes()
