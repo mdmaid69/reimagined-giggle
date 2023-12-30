629113f184fe5dfff1ed15adb746244b73d27958
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:57 +0000

    trouble think plenty ask apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
