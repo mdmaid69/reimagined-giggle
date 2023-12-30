8887ecb5c12de5be15139dd341a2b92cc43febb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:02:06 +0000

    bite downstairs obvious action money

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
