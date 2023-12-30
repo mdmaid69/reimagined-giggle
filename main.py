5d406bcd9d7ff35a60f1838904c4cdc423ccdc01
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:12:20 +0000

    challenge familiar plant question shock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+print(os.getcwd())
