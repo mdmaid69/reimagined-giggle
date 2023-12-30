4470690155658469e49608704ae71865998f8130
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:33:48 +0000

    mix spoil crash anywayanyhow cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
