e1df1d11e22c9e75a572c83580edc38d9fc2df8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:57:00 +0000

    Jj wherever steal conduct cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def reverse_string(s):
-        return s[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
