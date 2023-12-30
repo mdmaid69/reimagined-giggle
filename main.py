669168bfdd229f071d0eb3f5f442ad0bd47cd5a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:20:45 +0000

    freedom forever talk relax occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
