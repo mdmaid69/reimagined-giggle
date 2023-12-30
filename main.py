24ad464bedc5021fbe6763a6566854d158da7fd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:11:53 +0000

    urgent knowledge occasional certain there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
