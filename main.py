d0cd26771807ab0a550ba371b1aff3ef9d8a41c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:47:11 +0000

    direct dare better at careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
