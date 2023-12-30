f67128bcb9407ddb3fab512870d8a7a6334a6378
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:12:10 +0000

    scare base education same well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
