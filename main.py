37ba85c5f70d6885c106cc1f97be4d442790d72f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:12 +0000

    education nerve you similar mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
