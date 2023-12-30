4fb0229f6215f5c978855d5da323ed75107d9143
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:37 +0000

    goodbye exam half tooth order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
