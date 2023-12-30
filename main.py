9024e0050546a8174e6784918abbdd5d85704118
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:29:45 +0000

    flat bother road around about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
