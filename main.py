b68566ab765cbd64f9fa08811d958e692595fa55
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:48 +0000

    upset save wait point base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
