1d770f7153f4d0fdc335c2c505d08104f53df497
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:24:34 +0000

    pump push offer unfortunate relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
