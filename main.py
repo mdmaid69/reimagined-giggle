8e46c9bac7f48525b009e17c8a8ebdd685f5523d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:49:49 +0000

    drop necessary punish bother must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
