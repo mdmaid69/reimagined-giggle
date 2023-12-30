a0f518ce074b20f6e5d7873b91097b20cc891436
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:57:17 +0000

    rubbish direct authority keepup occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import os
+def change_working_directory(path):
+        os.chdir(path)
