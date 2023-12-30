1c06ae82175168326624dfe691539f6539f0d109
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:31:35 +0000

    pretty prevent blind earn sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
