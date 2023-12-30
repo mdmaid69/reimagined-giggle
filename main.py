2cd066a5d316958d26a6eb574f25707cccbb7d13
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:36:24 +0000

    news wrong listenfor field country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
