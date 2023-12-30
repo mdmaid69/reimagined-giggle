1b85beeca52c3f254346d1f777f2bbb676757e84
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:54:27 +0000

    note forgive relative ordinary view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
