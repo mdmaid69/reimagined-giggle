efcff67fca4e025f6df55cdcbf41d8c5cd396a7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:04:55 +0000

    guard impossible attempt sometimes glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
