ae688c777d10cdad5b1a4300fcb91699410cff9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:13 +0000

    trade so neck prevent beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
