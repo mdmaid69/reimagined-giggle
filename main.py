ab894b49a1f428b8fd4c4650d79bce0b8325b7d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:48 +0000

    advanced constantly loss band shock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_max(lst):
+        return max(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
