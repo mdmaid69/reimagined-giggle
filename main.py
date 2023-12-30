213c2c94eff751eb556b0a084ac56e18e3bbd803
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:14:10 +0000

    bad makeup Ee survive case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
