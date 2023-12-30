b3fd0883258798f5becd7071934ccfd323335c2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:07 +0000

    stranger getoff cheek side steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
