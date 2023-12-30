2beae0d619ad6a0a5323d8024ef323413fc80d6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:28:20 +0000

    bear wish straight operation stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
