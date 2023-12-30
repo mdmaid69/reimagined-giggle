a1757528a120e83c91fe7ab7f42903d3cd3b159e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:38 +0000

    noone empty ours clue be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import time
-def get_time_since_epoch():
-        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
