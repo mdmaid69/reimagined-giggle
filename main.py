e75a9a786b569b35aac3c0ae1e54284be6c5762e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:33 +0000

    understand luck afternoon possibly turn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print("Hello, world!")
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
