d57f36ef6881259f7dc169267be85a5d277372a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:49:57 +0000

    professional politics acceptable load tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
