824188bfaa6ac2cde572f2d81571239ed7b6874a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:12:49 +0000

    pencil friendly emotion joke attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_array_to_list(array):
+        return array.tolist()
