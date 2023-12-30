15d8f09bbe8a4634bdbb01cea1df0fe2f8f036e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:46 +0000

    command round do gather demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
