ed2f027c9cb9ff3d7d929a558421d19e94c4e015
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:47:19 +0000

    petrol disappointed influence embarrassing account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
