8faef43f2c2ee5d472de3657d09229c85f740ef8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:26:33 +0000

    nearly to now satisfied normal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def find_max(lst):
+        return max(lst)
+def count_elements(lst):
+        return len(lst)
