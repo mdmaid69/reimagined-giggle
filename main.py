402d9e44ee1d8f243d06e5c5a8ac2f8aea234814
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:35:44 +0000

    mind bread type extraordinary activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
