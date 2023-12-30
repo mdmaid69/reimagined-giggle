caf8f083b461df1e563e5072886665234ad1320e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:28:13 +0000

    time familiar 567enemy on reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
