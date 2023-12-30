f4fcf47cefbcee39785ba13e2419af07ef053321
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:09 +0000

    quarter eager pure offer able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
