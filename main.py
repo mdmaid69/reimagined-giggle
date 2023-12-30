9cc430daf815fcaebc7ee208c7a54cc0e7b2b18b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:26:26 +0000

    major patience shop maintain age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
