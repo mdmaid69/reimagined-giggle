465f27ceb481c077c31eee73fb865d40edee94a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:46:39 +0000

    shout pretty offer invitation unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
