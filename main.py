26a0837556b76d31883ce9eeb33bd41386618c39
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:15:21 +0000

    wind can spite team fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
