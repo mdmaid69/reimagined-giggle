4e88726bf854f43c989f64ecf5b3c6e4599fad4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:06 +0000

    instead share content level rest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def roll_die():
-        return random.randint(1, 6)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
