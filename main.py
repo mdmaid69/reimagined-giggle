b17ee04fe07c59c860459fe643167046a743906e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:53:50 +0000

    necessary regret bringforward watch see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
