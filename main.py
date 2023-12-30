e2b78fa547c64234cdd8a2108e8752116cd3df58
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:07:46 +0000

    obtain safe frightened minimum relax

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
