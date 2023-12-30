2756903710c50c07bee45887d8f410f13fdef215
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:29:57 +0000

    thought politics pen towards but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
