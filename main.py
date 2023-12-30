704c3c970deb5755179ad2a1fd370990a92c2687
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:03 +0000

    neither Uu cover lookup action

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
