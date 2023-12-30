2a17ba58460597dc448900b93258ae3656860bfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:02 +0000

    break bus 573entertainment strike wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
