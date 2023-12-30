e6faa0ecd79dac64a52bd833e4d97721c91159ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:01:48 +0000

    steam pipe dress star mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+import datetime
+print(datetime.datetime.now())
