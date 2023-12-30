64d43e688c6e2f5b9fac644a5b836da1d7421141
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:22:58 +0000

    meet breakout later threaten include

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
