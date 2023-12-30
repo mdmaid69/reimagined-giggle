68b7f2bead57441183c8e104b7622cfdac568bfe
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:32:19 +0000

    home rule satisfactory getback happy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
