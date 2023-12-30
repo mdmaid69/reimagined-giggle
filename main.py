8baafa50e777aff46239227396299ec8a05a5597
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:57 +0000

    style frequent special refuse mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
