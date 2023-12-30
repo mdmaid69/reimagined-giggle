7c1fc2b6169bff01929a74f919546c4a48adf15c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:36 +0000

    reliable learn rise priority next

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
