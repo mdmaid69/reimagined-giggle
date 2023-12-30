98fe2bb30dcaceee3889da9a3309e33c2557548a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:58:35 +0000

    ashamed cause calmly down rule

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
