6616a68acefbfd956f5fc3ec5d96f20fbefa630e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:55:45 +0000

    promise crash nurse special loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
