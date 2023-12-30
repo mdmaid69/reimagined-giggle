0fbb0674a185fd066e7d1fabcbab2fcecf3c772a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:35:01 +0000

    before copy hurry affect wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
