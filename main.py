b222671a4baa29d246232c2d12b411b97c585442
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:44 +0000

    better list Mm feed belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
