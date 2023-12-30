24c747bdf169288e61f0f3e94fe79f98f75bb9cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:40 +0000

    direction board follow leave accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
