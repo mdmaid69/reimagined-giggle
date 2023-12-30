61ef2657c535fd2bc931e29c053e68e733f5ad47
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:20:21 +0000

    used fat storm desperate condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
