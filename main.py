813136a9ce20df571ea23e7538dbd07a39ee19d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:56 +0000

    dare best belief tense sound

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def is_odd(n):
+        return n % 2 != 0
+  def cube_number(x):
+        return x**3
