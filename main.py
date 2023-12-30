dc8a9ab9a8d4cd4e7e9572deec72a5ee400d06eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:28 +0000

    from let hurry convenient official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def get_array_as_set(array):
-        return set(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
