a2877f0a7060c7eaefd29e7a80bcd03c0332bae6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:38 +0000

    front science apart warning repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
