ec7a3a499fe48fac319ff6a18599e37e1e1949c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:51 +0000

    much close importance wish silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
