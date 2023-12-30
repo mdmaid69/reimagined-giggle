8182107db3f1500a95f79c9cf7641396cd258417
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:28:06 +0000

    while achieve stir quit wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import numpy as np
+print(np.array([1, 2, 3]))
