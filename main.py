1d8d34c7bfc5e3103b0cb0980e17187035379e5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:37:48 +0000

    occasional light final embarrassing hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
