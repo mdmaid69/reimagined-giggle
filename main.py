957a46f338564ad6d7ace7ff09b5fbf3236d8806
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:43:19 +0000

    noisy strict situation quarter complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
