ce7421425bec6baa7e4e47af4ac3b869b2695936
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:30 +0000

    market wire birth case from

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-for i in range(10): print(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
