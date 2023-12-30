6a3b989e7094d94952543c58efb5f84e09433763
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:55:53 +0000

    support schedule noone allow debt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-print(math.pi)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
