54c979555739f9ade754cc2db7e5db23cf777eec
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:10 +0000

    opportunity murder remember sight cinema

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+print(math.pi)
+def sort_numbers(numbers):
+        return sorted(numbers)
