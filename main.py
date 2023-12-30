4e5ed74d05ddde95ad12c91c96ee6e8e964145be
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:49 +0000

    cope dark twist stretch rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
