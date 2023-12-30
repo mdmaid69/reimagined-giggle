087a2ab19e4e0f420ac0e5f2bba7129c0c4ffcfd
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:47 +0000

    dark discovery bringdown believe tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
