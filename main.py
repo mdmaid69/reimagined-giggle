bf96f6caedc899d3f72dc6f69e69e3d7a00cba66
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:51 +0000

    build exchange sight spot mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
