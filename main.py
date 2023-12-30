da38bed185d7a1a437333f5074dfbc8cb3aeb8c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:36 +0000

    owe it serious greeting die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
