e2e02dc961171cb5fce1137527697f15cbfe8ebb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:51:03 +0000

    stranger copy organize section freedom

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_work(force, distance):
+        return force * distance
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
