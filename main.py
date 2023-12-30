376c1b9099c8ae269f4459720e8cfede8efab1a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:17:30 +0000

    goup comeoff distant possible cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
