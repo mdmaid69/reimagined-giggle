9a61ff5cb9284948ee8b030fe24907a6b33c2710
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:03 +0000

    shelter guard clearly main unhappy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
