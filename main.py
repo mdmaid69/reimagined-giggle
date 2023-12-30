59f9802a5044e21156e008f46a29deea60043b11
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:36:17 +0000

    building cover Nn mix strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_base_name(path):
+        return os.path.basename(path)
