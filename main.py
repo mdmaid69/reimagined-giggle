8bccd0f37faeb3d07cbd3ab971314fec53fc72da
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:14 +0000

    office runover both brown run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
