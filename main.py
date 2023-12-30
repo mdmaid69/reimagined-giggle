1d3eb4b1c88efc5bc816853bb9847a282a78b144
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:26:02 +0000

    sure perhaps cum successful march

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def sort_list(lst):
+        return sorted(lst)
+import datetime
+print(datetime.datetime.now())
