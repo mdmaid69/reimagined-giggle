1084566d8aab9ba573c2d03d3b0dab3d19031bd0
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:48:18 +0000

    wish wet splash comfortable cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import tensorflow as tf
+print(tf.__version__)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
