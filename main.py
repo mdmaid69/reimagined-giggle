4ec4b123b754ae1361805306dd64f63ea72710ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:36:30 +0000

    remark work every satisfied cool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
