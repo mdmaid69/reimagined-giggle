924b57624cb082adebf1669a29357071de81f3c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:39:00 +0000

    experiment broad crowd fork cover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
