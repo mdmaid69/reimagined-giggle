f54e62dc92e0330bee39e8f81015e979a4734490
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:55 +0000

    satisfied worth read form let

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_max(numbers):
+        return max(numbers)
