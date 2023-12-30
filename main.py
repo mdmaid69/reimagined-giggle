d62650111b54af611a1a50bf2f13e5de51a16828
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:29 +0000

    certain we business before vote

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
