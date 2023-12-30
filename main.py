9abc88bcca432b07c7cd8d025abb90f0f4eba910
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:15 +0000

    too comment frighten tomorrow responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
