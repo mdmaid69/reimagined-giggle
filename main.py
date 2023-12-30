91632a4b41f02c20f6d26bd2e11f7ec36f448276
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:15:30 +0000

    suppose street protect country friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
