aa3194043265c81e4e593e7cebcb065758c411bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:04:01 +0000

    peace home thinkover always sort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
