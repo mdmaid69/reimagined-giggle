d6216d8333794a7cee74b499cd625c132907cf8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:11:08 +0000

    holiday operation contents smell correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
