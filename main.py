7fcef4da1e964867812dd43f3db6126b9b102084
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:59 +0000

    example handle rain full method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
