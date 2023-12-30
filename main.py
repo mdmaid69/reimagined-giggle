88dc0fca4d52785d47bf2b9a7eeb7d392a5aaf75
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:33:08 +0000

    guilty often ground conduct friend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
