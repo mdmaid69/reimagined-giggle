b84da82563a756d7e3b61e3158d5fac8fdd13eb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:43 +0000

    react possibly until letter talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
