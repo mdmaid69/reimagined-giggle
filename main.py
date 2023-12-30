c802a861d1734410051624d8c1bfbda1544f551f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:26 +0000

    store cross quick makeinto flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
