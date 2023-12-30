c9a62e03807f7f503079cba57b654534cba36915
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:51 +0000

    interesting old hard try somebody

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def subtract_numbers(x, y):
+        return x - y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
