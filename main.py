24fee56828a096a00d0529a0ddd3f773825a6d99
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:55 +0000

    off joke attempt only plenty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def count_elements(lst):
-        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
