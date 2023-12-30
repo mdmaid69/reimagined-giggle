3c32303b4f47bcd17728fea6c9ec303228625092
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:26:45 +0000

    profit bank up operate Jj

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
