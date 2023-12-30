c15c14a4e07308df9ade5d3f886246c31b096d6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:13:41 +0000

    choose appear certain downstairs moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
