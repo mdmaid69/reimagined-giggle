725c7397e3b23b51921d34199bce0a7177857694
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:18:38 +0000

    rush telephone probably law picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
