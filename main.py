bf0edfdb90a10519ee54c413f0453049abf6c382
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:10:52 +0000

    again kill unless declare miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
