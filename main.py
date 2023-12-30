019a80fb8445f7f8888513d827e25b9d71183f0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:00 +0000

    open disappointed shoulder between worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
