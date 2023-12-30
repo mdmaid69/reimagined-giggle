596a0a038b063bde5d9153ebe583d0dee47e1f87
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:41:22 +0000

    keepfrom Vv justice complaint cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
