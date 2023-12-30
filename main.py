815c321dcf6bdd3172d0dc66c8db18817d96f7d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:48:34 +0000

    bang secret constantly beneath general

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
