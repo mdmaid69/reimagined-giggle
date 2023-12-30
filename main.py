0e80c004de860af20a2025fa37cca50a84924270
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:53:51 +0000

    hers warm pool dig announce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
