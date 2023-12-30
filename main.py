27f0e6754385bd06eeb6682e08c327d93d1a7d1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:55 +0000

    search juice medium roll answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
