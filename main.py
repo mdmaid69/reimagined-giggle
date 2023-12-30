3c980998d0a7c39c57bcb1c159c5daef8a41e2a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:23:59 +0000

    road crime so point set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
