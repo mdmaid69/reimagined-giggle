ecbbd3b19adc38cc3bd7931211967c98752fafb7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:05 +0000

    traffic criticism similar team just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
+import tensorflow as tf
+print(tf.__version__)
