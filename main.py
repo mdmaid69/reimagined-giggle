d6c995eda90677aecebc165bb30cf30473802636
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:14:58 +0000

    fear above shout ask sentence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print("Hello, world!")
+  def get_current_working_directory():
+        return os.getcwd()
