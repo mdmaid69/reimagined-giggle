cb8a0a8f6d8887e6d0d80ce579dd18f94465dfd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:29:35 +0000

    burn forgive coat study shop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print(json.dumps({"name": "John", "age": 30}))
