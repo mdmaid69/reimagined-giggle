62d6ff763fc8e9904676dc0a92184d181f41e043
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:32 +0000

    breakfast statement fashion emergency exact

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def is_odd(n):
-        return n % 2 != 0
   import pandas as pd
   def write_to_excel_file(file_name, data):
         df = pd.DataFrame(data)
         df.to_excel(file_name, index=False)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
