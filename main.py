c2701459255756fffac4ed073d68d2574a1efe13
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:29:45 +0000

    switch impressed innocent praise software

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def count_elements(lst):
-        return len(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
