4786dbaf499aa955165978db35b51e39928055aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:10:56 +0000

    strong proud environment dream gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
