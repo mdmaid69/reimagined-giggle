130f7c7ce0ae230f25db6deb694f970bf69dec95
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:19 +0000

    explain band bend disturb centre

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_speed(distance, time):
+        return distance / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
