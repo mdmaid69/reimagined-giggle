0071d1023fa6a24869592b09b232e6828050083e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:14 +0000

    personal until bridge receive spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
