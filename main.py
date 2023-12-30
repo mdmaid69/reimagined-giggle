47ab8429c8162ac3172c4b0bcf4394c890705d5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:03:13 +0000

    pretty help operation pattern surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
