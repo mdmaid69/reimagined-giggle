7b5efe209cc357b02af17db63c812e52b72dbc98
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:59 +0000

    tie rather short curious after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
