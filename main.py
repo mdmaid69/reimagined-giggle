4636265e6a3e4abf2f8b66f59a970ed5571f54ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:27 +0000

    declaration track want fail polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
