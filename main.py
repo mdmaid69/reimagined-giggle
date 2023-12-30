efe9237057ed1965a2ddc62c79315a55b19fe895
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:48 +0000

    Yy dance possible mess surprised

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def reverse_list(lst):
-        return lst[::-1]
+  def sort_list(lst):
+        return sorted(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
