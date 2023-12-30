b490c5dceb804a2c82b0dfe0756552a767428562
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:11 +0000

    care spot nor street exam

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
