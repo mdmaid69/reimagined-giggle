58ba08f475739f17af7a53f45dde9fed92c92ec3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:39:11 +0000

    warn occasional work swimming think

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
