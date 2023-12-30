a632056c0031d01e8ed7bb305f0169e184694a51
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:18:08 +0000

    entrance concentrate spring face certain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
