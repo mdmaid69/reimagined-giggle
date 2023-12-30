d185d2a094660d539df8f98c78afae97f59d0678
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:11 +0000

    operate tidy move principle onto

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
