5de34a2d6076845fd74861fc0b9425e4547a4cdd
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:18:32 +0000

    bank dull condition plate discovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
