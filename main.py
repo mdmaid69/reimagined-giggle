f9d9467f5a907f1255866378a8ba3df58f11bbf0
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:42:16 +0000

    regularly godown basically rub motion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
