a2082b57f473659d2a9347ff95d94f696ab6ef8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:02:56 +0000

    Gg sometimes right 564encourage little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
