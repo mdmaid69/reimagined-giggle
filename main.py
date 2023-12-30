fc611ae53c2ee0457b0b12d486d9d9d2c701f264
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:30:38 +0000

    poor amount only standard spray

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
