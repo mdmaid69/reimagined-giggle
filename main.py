be14780d249c6445cfc47b6d603f87bbd4a32446
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:09:07 +0000

    complete stone comeround swing train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
