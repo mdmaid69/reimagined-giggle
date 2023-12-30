6a994408b8f63402bf6b378e2fd75662dcedfb38
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:44 +0000

    tight shoulder soak contact rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
