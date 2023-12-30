3f5ecc72712cefdf5a2ab000a0ff714a7e0ebfc3
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:09:42 +0000

    thumb anyone callat flood contents

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
