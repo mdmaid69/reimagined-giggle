eed4826add7d75d0f0500521e09df54438366b8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:21 +0000

    sit truth compete rare getaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
