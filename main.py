24c32717a1068c6cdeeb7f9fc4578700ec7a55ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:10:19 +0000

    suggestion sad supply early treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
