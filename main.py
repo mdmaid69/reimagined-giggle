2a3ff3e828a33970249040b885ead8fe4369857c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:48:12 +0000

    trick match hope if study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
