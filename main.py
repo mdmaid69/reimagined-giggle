df54d57f0f9700d60cb89070ba8b7fbf65046556
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:31 +0000

    cold nor group progress case

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
