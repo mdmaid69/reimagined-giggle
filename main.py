b96d1f45dbd228690ed02d5aed0eac49b4ba8a13
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:46 +0000

    name shoot confident extra final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
