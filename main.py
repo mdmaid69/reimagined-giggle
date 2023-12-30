57f92cb955882341815e4e3ed87ba5bba562e725
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:48:56 +0000

    fair mouth display safety shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
