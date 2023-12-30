e2ad6e53819c00ebbd38b1d144eadd7c5ac5870c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:16 +0000

    wonder hate deep create while

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
