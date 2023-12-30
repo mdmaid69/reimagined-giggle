4efc4c004d65987f5fe99497de844db6620aa646
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:01 +0000

    deliberately back public towards furniture

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
