5c8c9ee63f1d6d6c4885e9f01ff811ecb3ea9b40
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:11:46 +0000

    off description mix material actual

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def cube_number(x):
+        return x**3
