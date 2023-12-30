bbc7ac6b31d98ce77f8c07649e12c4325523a65a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:26:58 +0000

    cope would narrow cheek news

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_base_name(path):
+        return os.path.basename(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
