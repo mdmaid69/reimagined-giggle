dc204fb0b83ff3c242ce8b8480e6eff0f363e8f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:59 +0000

    path another tomorrow discover bit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def square_number(x):
-        return x**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
