9a05b00e06ef9d37bb37feefd9e7eb6217324c10
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:50:23 +0000

    bet extra murder pour thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def remove_duplicates(lst):
+        return list(set(lst))
