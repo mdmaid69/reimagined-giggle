e98b2b564bf5a8c4a4202509c71bcacc8deaf894
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:25:20 +0000

    keepaway shake too shop mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
