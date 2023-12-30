24add4acb39a724cefd43f37e44967f334b62bbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:04:30 +0000

    deal receive deserve official interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
