3a1d0ef18884fc19c9924b5789aed4c7c468f771
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:16:12 +0000

    scare wild who Itself powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
