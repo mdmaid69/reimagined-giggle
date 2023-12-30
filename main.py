ed3fbf553770a62885a9408a422302adfd7e72d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:37 +0000

    match pencil until standard cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
