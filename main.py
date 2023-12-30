cd77586f2bb47c764f4f3dd750dc5ed9a6e2e5d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:23:47 +0000

    bother explanation level estimate book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def add_numbers(x, y):
+        return x + y
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
