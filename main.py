9567f48315b1c26acbc2d9e42b15a248d36b2f35
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:57:29 +0000

    terrible compare telephone rubbish time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
