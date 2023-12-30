832ce65bee1760369ece5b26f880e51f80e2118f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:44 +0000

    miss way strange improve step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import collections
+def create_queue():
+        return collections.deque()
 from flask import Flask
 app = Flask(__name__)
 
