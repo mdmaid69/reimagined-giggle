50b1dbe1bd582b0499212d3fca10acafbf979d52
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:32:41 +0000

    relative write motion Kk sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
