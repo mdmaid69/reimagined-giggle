af8733e5717e0ffb53f0a09ddc3051dac928e77f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:08:33 +0000

    vision lookup proper workupto relative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
