ac88ed949bbbf76b085465cfb78d7c8f728afddb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:20 +0000

    ticket undo match brother previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def convert_to_octal(n):
+        return oct(n)
