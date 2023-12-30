6c4a3acdb0d5d811f9ce02ab4646ee5a14700a5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:33:50 +0000

    challenge woman crisis this lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
