6af1aa1afb625704cb13f7640df2c207086cba38
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:55 +0000

    pattern condition protection hide properly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-for i in range(10): print(i)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
