c04d00f3bdfee11959e7c3dc25964b6d4c5ad0f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:52:43 +0000

    size lead copy concerning mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
