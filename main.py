a56f46471f6926aa47281b281ebd77b0cac13e16
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:47:56 +0000

    watch pride smooth knock strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
