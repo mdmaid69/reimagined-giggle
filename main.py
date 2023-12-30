62ef294ef3694e44276fa7d288bb3de974ec565d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:29:53 +0000

    opinion suppose breakup search account

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
