f9e1a1d2aec670fb88653633fb3f886d882d0e12
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:14:39 +0000

    trick tonight 569enjoy tip upwards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
