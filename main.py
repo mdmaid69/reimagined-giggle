366347cb00ba60d26273557eb58ad4a8db9f29df
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:23:51 +0000

    raise sit back list feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_palindrome(s):
+        return s == s[::-1]
