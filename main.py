083d82943119e87bff27a81ef491d9888a05ed77
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:12 +0000

    steal solve might extremely rent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
