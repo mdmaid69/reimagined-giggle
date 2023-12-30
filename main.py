caa9bc69ff5191b3fdb4a01b920df61e1a9efa81
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:03 +0000

    border later belief entrance week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
