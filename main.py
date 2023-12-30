efb1e499b1f89fe683ce25ebf3b353a87bcafd93
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:12:41 +0000

    lead doubt example grateful harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
