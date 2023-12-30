4058168c61650093283e5df401cef885bfd309ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:28 +0000

    reject takeback bored either custom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
