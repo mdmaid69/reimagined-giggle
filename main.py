5369b4db8414e933132ac40aa739e16a70ebe0bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:27 +0000

    tragedy habit floor pretend drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
