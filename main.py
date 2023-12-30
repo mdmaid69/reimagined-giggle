692ba7bdef8da9929f54a6bd40b1fd98ea9b3871
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:38 +0000

    properly emphasis deaf make disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
