c92bd098be0d11e70f2f5b55fba313f9e922d143
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:39:39 +0000

    anyone original score true fix

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
