06044d878f9c01565a3ed94ec27133cc069e5e9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:28:41 +0000

    free difference hear fire unite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def find_min(numbers):
+        return min(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
