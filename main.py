4b84dda4d390d05e41174bff240ab983c7e3b9e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:11:57 +0000

    comeround goup shallow accidentally regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
