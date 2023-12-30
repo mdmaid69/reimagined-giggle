1554fe75bab8710bb470678a3f43a24afe62fbfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:13 +0000

    unit immediately different reply unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
