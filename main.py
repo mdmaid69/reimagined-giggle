8b8baae4444550d166f2cc577521983af020c1e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:16:03 +0000

    across find comeout lower teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
