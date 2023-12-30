47fe15f85ee46ed9d729236aa7ec8f895580df8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:07 +0000

    neck yes pleasant influence dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
