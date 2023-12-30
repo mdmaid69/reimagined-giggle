171b77a44a9001ee6aaabb82b3c4a67fe974ae8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:33:11 +0000

    free criticism thumb stay badly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_sine(x):
+        return math.sin(x)
