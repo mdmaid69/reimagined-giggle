e8bd798b4ad7b5ec7a76088942ff458bab8fe9fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:34:30 +0000

    emphasize into else fear polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
