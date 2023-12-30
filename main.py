62d5a54b625d2fc49b855b0530e578a07e8970bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:57 +0000

    replace awake school save set

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+for i in range(10): print(i)
