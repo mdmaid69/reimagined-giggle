19f3b9ec442035f362fabd21f68057095baaee49
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:49:16 +0000

    stir cause spot challenge grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
