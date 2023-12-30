5602743196f49248a84a34da9f4de00019d3903d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:05 +0000

    hard like everything once believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
