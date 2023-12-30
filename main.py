602d248a9b4d6acd922a51e3c96811ad87da7697
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:43:21 +0000

    tax grand which before bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_ceiling(x):
+        return math.ceil(x)
