b52a25b23fb40d3385e701f09783bcec26d23fa4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:20:02 +0000

    get main myself interfere scale

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
