06751948997788d926975cb590f73b44a683cadd
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:23:40 +0000

    related excuse objection height offence

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
