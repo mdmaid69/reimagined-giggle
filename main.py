a26471a40a74905eba71a5b10cbd13eb2ba09193
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:44 +0000

    habit 573entertainment fault dark long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
