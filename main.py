5cc123bc833f2a89c50863957b5a2a0aa002337e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:53 +0000

    suit if advise fold profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
