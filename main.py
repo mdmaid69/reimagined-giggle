1470bcd02b6310be199546672189c195239e9199
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:52:49 +0000

    crisis demand ring uniform phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
