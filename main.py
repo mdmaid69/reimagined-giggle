60508a7bf28fbcceb4bd1d859d1bed0cc4928e65
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:45:58 +0000

    load enthusiastic law circle activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
