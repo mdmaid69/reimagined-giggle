1637b8845c12f6ba3ed1ad4c63751d333de36f1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:48 +0000

    pair near interruption himself beyond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
