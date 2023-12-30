9c2689c83b46b583961ec5b34b6c266fb228305d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:15:07 +0000

    noisy protection odd part key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
