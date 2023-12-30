0026cbd859db26f25011197e810d111aabd25563
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:40:24 +0000

    rid clearly improvement towards father

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
