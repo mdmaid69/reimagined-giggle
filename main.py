d1db45435f31f4672f51d164f0df37f4d90701dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:31:25 +0000

    hate sorry destroy pick fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
