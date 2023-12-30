a0f897ec330fd8adac998684263cbdea01bf1d45
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:07 +0000

    opposite cross whistle day hold

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def greet(name):
+        print(f"Hello, {name}!")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
