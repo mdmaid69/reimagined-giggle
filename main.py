96f21f0f74e38c9937611fd29b67a9c0ea036d5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:26 +0000

    hope positive boring it almost

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
