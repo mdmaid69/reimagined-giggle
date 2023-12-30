e495024db65e817b673cc48aaf63edbb9f8da386
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:55 +0000

    spill breath Tt few emergency

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
