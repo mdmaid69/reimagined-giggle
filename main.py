805c2eef95aa687acbb482468b050592be98d366
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:06 +0000

    fast soil usual convince birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
