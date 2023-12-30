ff7385e468912d310a8816a2a43a75f09aa45278
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:09 +0000

    now boil excited level scared

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import random
+print(random.randint(0, 100))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
