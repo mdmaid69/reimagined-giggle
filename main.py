13ef01b4805d90a4d86cbef68130e421868db11f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:43:03 +0000

    cut alone pity serve too

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
