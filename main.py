cded5f2c2ed9f84bc3277d110bb26514ecf7a63b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:53:46 +0000

    trap expression father everything base

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
