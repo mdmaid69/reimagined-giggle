e1b9f3227f39984d4904a5e0a84ba7094fe8dd81
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:29:31 +0000

    otherwise invite lookup insist unite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def reverse_list(lst):
+        return lst[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
