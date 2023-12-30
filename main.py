ff2c558ba54798523ef90b60dcb2f49b828bf8f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:07:52 +0000

    skirt comeout provide invitation successful

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
