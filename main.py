41ad1512a30006b2a87d3771f571b9e2a1c9931d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:19:36 +0000

    bar tone emphasis grand judge

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
