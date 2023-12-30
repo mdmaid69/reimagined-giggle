4f94c1355e2991974fc9ae684ea29a4b0998c1a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:51:46 +0000

    must law glad game may

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
