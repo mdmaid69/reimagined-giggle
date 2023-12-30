8ad1bb4134db7bb4a59f57e6e14043f3553af629
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:33:22 +0000

    makeup unable weather temper if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
