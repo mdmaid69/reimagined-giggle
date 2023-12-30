fb61faa8e45875e2ba1b92342e077043feec43eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:31 +0000

    worst basic introduce bringdown to

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+print("Hello, world!")
