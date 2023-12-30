c2229298c5298289078cf3c2bbd02699914cdf9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:47 +0000

    culture fasten come course dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
