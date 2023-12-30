40c0bd7b7bcfb8e97f28f7d538abe44756a487de
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:43 +0000

    thick tooth flat scrape pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
