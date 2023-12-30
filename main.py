fef77c60b22e4a033a90958968a5c690014f2d6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:33:47 +0000

    power wheel guess better ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
