a4cb72526d85e84af0eed3a335a6b07af1bba760
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:28 +0000

    house shallow miss test support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
