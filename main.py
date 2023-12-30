454c7282ab1d477efd6a26afdd9518e0abcd83c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:00:38 +0000

    exist plan load development deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
