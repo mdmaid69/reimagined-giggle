4455074467b655d0f7c2b03f22f6320b05f24e6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:44 +0000

    small excuse store Tt claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
