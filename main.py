74e4082a06fc3be6b0ca3d18c6b7dacd05ccf2a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:55:53 +0000

    exchange anybody brief manner hot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_max(numbers):
-        return max(numbers)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
