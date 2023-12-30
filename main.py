adcb6880bd3a53673522935ae484b3148e791660
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:26 +0000

    far sky pretty until everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def find_min(lst):
+        return min(lst)
