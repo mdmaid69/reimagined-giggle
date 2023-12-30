052ed7f02120a52c55cc02a35f2dff7b360651fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:45 +0000

    unfortunate day swimming under fierce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def add_numbers(a, b):
-        return a + b
+  def remove_duplicates(lst):
+        return list(set(lst))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
