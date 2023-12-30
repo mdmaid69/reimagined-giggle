cce1e402d904c90b12854d972bb0e1ba1c99e59d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:29 +0000

    trip by book demonstrate demand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
