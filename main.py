f6c250992c72c72678d38513907a4ba4ed4ab070
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:42 +0000

    church lie play scheme bang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
