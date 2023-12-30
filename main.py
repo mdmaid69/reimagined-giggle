c1cbf6b8414d19d775f4cd5074a947a7f2ca9f38
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:57 +0000

    better but remark air wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_set(array):
+        return set(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
