ed8bd768e1de860e954dcb9d695fd49f70563fcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:02:49 +0000

    degree up good difference second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
