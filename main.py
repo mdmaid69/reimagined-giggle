3f264d7d82f3203c4cd585b2c3d16b78da00eeff
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:38:22 +0000

    choose meeting soil drive stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
