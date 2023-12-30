55999c3308fcccb79735899bc002869a76dfb995
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:52:45 +0000

    pleasure memory serious platform talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
