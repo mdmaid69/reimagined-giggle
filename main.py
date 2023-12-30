c603262b3e7e9ba394c6843869af9b2b6d2e9781
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:03 +0000

    go coat ticket sorry now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
