ffea937db17bf4634e1a88ea7dbcbc4c8f0899d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:34 +0000

    make reason state time proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
