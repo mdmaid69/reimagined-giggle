a6544ac1eb0ca4eb9a97f3e31a9fb7370c877a73
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:02 +0000

    centimetre black turninto stamp still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
