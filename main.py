09f57568ad8c6fd9497d61a172d38bf78ea822a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:50:00 +0000

    dead paper bar name ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
