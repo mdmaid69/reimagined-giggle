e85cfa6925bc9af6af0128efb6ccb94167525b30
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:45 +0000

    visit actually serve extraordinary at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
