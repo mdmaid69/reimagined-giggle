8e81db2044c6debb2365b8b41fca141949eb2ee8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:03:15 +0000

    onto centre measure connect congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
