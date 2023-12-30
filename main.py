b4fb1d22ad40b8feb22dea3f5c06166d8df7f38d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:46:27 +0000

    impressive ready centimetre talkback success

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
