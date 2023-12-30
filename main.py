488820be8b09fb853cdab0f19448582089f6cdeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:12 +0000

    create choose however day smoke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
