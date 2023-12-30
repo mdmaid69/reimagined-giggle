ba813febd84dc1d71a0bac987a6c522814ea9d33
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:36:11 +0000

    broken really moral leave plastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_power(work, time):
+        return work / time
