9f67e7f0e745bef22e0e55dd49476956dddfda84
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:29 +0000

    free hour print answer course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_area(radius):
+        return 3.14 * radius * radius
