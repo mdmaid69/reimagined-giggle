dc79dd6954a73bf73803e8c710c22d521d7ebefa
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:25 +0000

    strike prize nonsense if beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
