e180b446436f0b37228c770cef25eec2b53c13c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:33 +0000

    wrong blame minute complicated reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
