6e267ee62434220f23f95b3f0260c41a36576e47
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:35 +0000

    spare take sometimes accidental trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
