9c783f0fa2d0bab64c01723bd8209ed3071e51df
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:29:54 +0000

    thorough copy wrong honest apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_power(work, time):
-        return work / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
