57a963628b2236307416c4c4bcbd0036f8abd6bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:37:46 +0000

    interrupt piece control introduction criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_tangent(x):
+        return math.tan(x)
