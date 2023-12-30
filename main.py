617dbf1a48aedc5f6e46ae4f6fc6e43e763b7b32
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:53 +0000

    pause stretch down enthusiastic crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
