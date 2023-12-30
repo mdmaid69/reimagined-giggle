5fbe7c9e716ab91e6c3a2d521097e105d028cda4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:40:14 +0000

    dry congratulations national position noise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
