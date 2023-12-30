7fd63884e67aabc7816c47e7b5a3eecdc1fd01dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:32:28 +0000

    start push war boil variety

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
