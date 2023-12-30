0a56c1b45dd2bcf618ec5cae4052c0ef6e82d51f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:36:40 +0000

    issue talkinto subject understand perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
