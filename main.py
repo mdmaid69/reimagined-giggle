7464eb4a3002a52457edf9cf652e4ba3441c48f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:01 +0000

    pronounce accept cool what second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
