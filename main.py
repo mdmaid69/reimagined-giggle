2f3394a1688780184d19668fb7d3456fc571ec6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:13:06 +0000

    drop thin quick bet write

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def square_number(x):
-        return x**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
