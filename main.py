871444f08a326451634db30258c200a00d94d572
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:29 +0000

    contrast land friendly 568energy ground

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
