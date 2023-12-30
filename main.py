62e216c1ddc48bffba9817cb0beef49ed009fdcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:28 +0000

    stand black explanation everywhere step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-print(sys.version)
+import math
+print(math.pi)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
