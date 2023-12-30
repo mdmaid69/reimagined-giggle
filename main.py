2e32d60059315da3a9d83f8956d60e234383f8a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:04:34 +0000

    reasonable see present well imagination

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
 import os
-print(os.getcwd())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_file_size(filename):
+        return os.path.getsize(filename)
