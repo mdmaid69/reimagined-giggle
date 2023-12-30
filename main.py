0a43ff27b52e9d5b81e0565e1c4d5098cc43937c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:29:16 +0000

    worth recognize clean copy add

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
