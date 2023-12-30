9b48634ec12b78b095af34bf3592630d6a44f9c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:29:19 +0000

    destroy responsibility last loss Gg

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
