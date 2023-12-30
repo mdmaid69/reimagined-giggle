35f75ddbd3fb261d6d00f185a4c70caff7ce5824
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:49 +0000

    life warn bath bite fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_speed(distance, time):
-        return distance / time
+for i in range(5):
+        print(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
