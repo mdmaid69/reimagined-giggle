cf3824eebbc34605a635d32c88776fd79202cc73
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:45:15 +0000

    exam gooff payment runover he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
