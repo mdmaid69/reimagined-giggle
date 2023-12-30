5ef5ef1539a067710cf8261d729591b5d14e406e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:52:28 +0000

    affect offer that suckme judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
