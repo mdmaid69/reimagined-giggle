7815d49c91b10fb3b12a1280847ffd2421a996c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:08 +0000

    shock test determined evidence first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
