25997b4692dd36c5a07d6582f6fd7460738275af
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:14:27 +0000

    evening please born curious sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
