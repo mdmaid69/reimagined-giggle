44d2df424a5cedb0460ade32fff60dee9c3e16cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:09:25 +0000

    dinner variety Oo actual taste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def remove_duplicates(lst):
+        return list(set(lst))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
