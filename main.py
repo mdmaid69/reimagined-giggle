48577166c297e0df20495f3da49a2cbef1f95fed
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:38:00 +0000

    lead quiet cover container shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
