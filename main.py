9e23e4097599e574f1daa9e3c54c529f8ecf5a0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:11:24 +0000

    valuable profit start nothing feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
