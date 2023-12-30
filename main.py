8039b3086b2e47bdf1dd6b9c51b09b56cdc5611e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:13:01 +0000

    whatever art push steep imagine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def convert_to_hex(n):
+        return hex(n)
