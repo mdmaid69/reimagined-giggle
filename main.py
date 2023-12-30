2e7a6ff84420e62dad04957ae581f422149a0242
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:46:30 +0000

    bite quick interrupt stand whisper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
