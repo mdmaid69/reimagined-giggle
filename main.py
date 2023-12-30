0b6ca18d13caceaaacf9268404ec60ed8ee5d60d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:13:00 +0000

    skill sound extremely obtain train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
