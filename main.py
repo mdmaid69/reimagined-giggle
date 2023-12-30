6a6ffdf7014a688cda9ce551c5350f0df85894a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:35:42 +0000

    smooth programme state negative strength

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+for i in range(10): print(i)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
