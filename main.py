3d83b3c25e29baedc1cbe1b191235a6c2e506b5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:41:43 +0000

    order violent steep accidentally better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
