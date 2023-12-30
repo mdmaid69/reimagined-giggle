37457133efd4a74bad7e5cbd88f3ff7237f52fd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:24:50 +0000

    unlucky deal pronounce art interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_set(array):
-        return set(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
