130b37999f6cca4debcb499d26105844110b1fd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:13:54 +0000

    cook rent possibility style hand

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+print(re.match("h.*o", "hello world"))
