cf980918733d51ad8d41020864b2e503a9466d81
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:08 +0000

    nothing crowd move dinner laugh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
