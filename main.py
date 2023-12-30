23b9cc7bc728b7eddc483d4622c4713bd31e05d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:07:46 +0000

    dear debt common person score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
