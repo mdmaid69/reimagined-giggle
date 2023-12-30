2f2cee0ee17a978e16a8c3e4b161186efd71e445
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:30:51 +0000

    doubt noone throughout Ff study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_string():
+        return collections.UserString()
