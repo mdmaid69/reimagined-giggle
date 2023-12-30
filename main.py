92deb8d7fde424da63d3bbb53d8a79639e3fd861
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:30:05 +0000

    crowd dry practice joke respect

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
