8d337f97fd08fe46a754cf5a25580c21c7abeec5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:15 +0000

    skirt talkoutof murder pleasure name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_sine(x):
+        return math.sin(x)
