8608b9575d62c8e829def812630c745a77cdb103
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:43 +0000

    listenfor all disappear if shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
