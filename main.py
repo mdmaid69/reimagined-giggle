c10982d55d10ab1dfdbfecea4b254a1966545224
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:39:42 +0000

    town head cross bite basically

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
