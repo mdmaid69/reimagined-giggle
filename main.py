6faa5437b4a40328314aa0f17299d7d0b7dc6af6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:03:40 +0000

    turnback watch nor language down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
