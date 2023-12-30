1982f6d134ea685ce698d50e0b5e47954af8bce5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:33:24 +0000

    which string scare ourselves comeout

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
