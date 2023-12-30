a81c37fed5d94ab7de51c1a53c454168279b8d53
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:34 +0000

    sink original male special sensitive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(os.getcwd())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
