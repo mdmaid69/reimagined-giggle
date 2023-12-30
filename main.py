df345af7f7081a8e612f62c5b3603d8e2f3b25e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:39:07 +0000

    effect environment beginning bringdown expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def delete_file(file_name):
+        os.remove(file_name)
