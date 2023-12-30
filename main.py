a5983da4fd54860909922bd74369739b29d82146
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:44:46 +0000

    sticky comment mine cap anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
