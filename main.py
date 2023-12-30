d99dfcaa070c9df384027dc4e181948678c9135b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:16:14 +0000

    bye behind you science gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def convert_to_octal(n):
+        return oct(n)
