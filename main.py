ded3082450e65249c8c4a5bd10d6d27c73dfb289
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:04 +0000

    she container emotion difficulty chief

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def print_python_version():
-        return sys.version
+for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
