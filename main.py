56832f568cafc11dd05c47fdcc56bf8d9efab26c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:00:55 +0000

    interested error cost everyone leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
