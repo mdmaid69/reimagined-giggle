66713ae2beaf4786049275c9b687e52ace89ea33
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:43:07 +0000

    used sleep attention pile strange

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
