9a4111ddcab1ded0cf45e5667d09996bdddf1c64
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:44 +0000

    hour waste help zero occasional

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
