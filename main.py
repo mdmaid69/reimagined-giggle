63b7e6b488b090827109ec0ec40f51d08b48f3d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:39:33 +0000

    basis convince point thanks advanced

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
