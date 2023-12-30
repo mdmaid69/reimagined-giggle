a6e3728c6534b5e5f32eb9d22c27bf4d2b0d1c25
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:49 +0000

    no accident pot front bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import sys
+def print_python_version():
+        print(sys.version)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
