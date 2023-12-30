9648c91560fd15adb3efba48c97ee44027a5f946
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:49:32 +0000

    destroy include control cry of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
