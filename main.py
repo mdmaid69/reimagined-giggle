321c10d14aae64cd39539ab8724046890d6ae23e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:05 +0000

    tall substance list apparently city

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import numpy as np
-print(np.array([1, 2, 3]))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
