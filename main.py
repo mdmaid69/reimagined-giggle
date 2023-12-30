6490dff673272b4a17654f4a597bcbbc577b3ecc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:25:41 +0000

    measure pipe 567enemy hear teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
