4729c443b02986fec8e989af8a12f683955ed648
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:32:25 +0000

    against announce group very small

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
