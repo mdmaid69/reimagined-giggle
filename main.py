a8dbe90c508f21874f15cd8f456c4ea2e952cc9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:03:04 +0000

    slow bank phone price funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
