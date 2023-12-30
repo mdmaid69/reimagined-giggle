f29a45ef7065511bc34fa1fb505fc6be99a01e64
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:59 +0000

    design comeover size time takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
