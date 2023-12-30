7ce3b3358192fcd1c9df0c90d703db2ff7d5b9aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:29:41 +0000

    callup sing 573entertainment seem theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
