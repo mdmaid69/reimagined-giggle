054cc857bc203a3e7ee73bc167d1ae59f3420ed1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:12:32 +0000

    favourite mysterious fork language report

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
