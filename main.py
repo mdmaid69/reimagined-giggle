067d300935f4df17c47fb95497e1d644d9b613fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:04:22 +0000

    underneath coat body takeoff pocket

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+for i in range(10): print(i)
