f29f9ae481cc3294ae955edb3dd0259af7be19b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:13:47 +0000

    getaway we design limit reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
