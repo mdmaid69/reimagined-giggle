122a73e66fdf0c3be9f2f0ee524522dbfe117abb
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:49:10 +0000

    love once shake display practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
