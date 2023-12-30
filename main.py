e6c3e47055283654b7faa2820850af72080af5dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:10:10 +0000

    usual sign exact breathe path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
