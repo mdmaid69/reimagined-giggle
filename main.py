a54911b4bbaeb107d7c57e7098bbb40fb8588c67
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:45:33 +0000

    suggest exist slow anywhere journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
