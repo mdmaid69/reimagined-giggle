6898380d58dc9bff410bb15b1dc574c35d2a69a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:53:43 +0000

    excellent characteristic pay full after

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
