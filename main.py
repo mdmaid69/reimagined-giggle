dba59b6f59ef6907dd2d0d72575ad5797d5fce50
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:49:37 +0000

    what remove fault tough principle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_acceleration(speed, time):
+        return speed / time
