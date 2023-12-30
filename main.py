2c3458cb0daa47118162e6747e99b510fdeedaad
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:21:24 +0000

    side allow related pay damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
