485126fd7e56439314d3ea8b2003a7de99d1182e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:25 +0000

    shelter sign lock direct stop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
