b6ec6656f2d3b958036ec52f6bf987704de4f3d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:24:00 +0000

    which roll grow criticize best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_as_bool(array):
+        return bool(array)
