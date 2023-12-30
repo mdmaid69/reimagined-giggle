7236cf8a603195a36739dafc33f4edea970ce931
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:20 +0000

    fortunately sweep acceptable priority yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
