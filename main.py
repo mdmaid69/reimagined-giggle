ae6dc178f8d66c7081af162341db49adb51f6312
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:04 +0000

    stir pleased fashion win Ss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
+import json
+print(json.dumps({"name": "John", "age": 30}))
