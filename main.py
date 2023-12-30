836c4081151c5d9b46b180ef76808253f210cac3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:39 +0000

    kill short dinner married I

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
