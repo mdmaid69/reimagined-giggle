2098c5c98ea01e74bfbd56dc461db1bd6ee107e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:02:10 +0000

    woman pump quiet back object

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
