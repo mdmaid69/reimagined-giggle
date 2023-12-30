001c0e003ed27f56f7dbf02c311b2d7c7bd04027
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:36:20 +0000

    bringforward makeinto respect dress Tt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import sys
+print(sys.version)
