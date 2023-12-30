d8e6b1ef7ff9477b52052eb0f2aea12db4dc4a7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:46 +0000

    mother deep swim explain intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
