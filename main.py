4f662615fee2fac66d68df4da9d7bf9862e0ef6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:17:19 +0000

    condition correct identify comeout runout

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
