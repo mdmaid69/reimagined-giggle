c3cffba7f1d292288c5420dd72a4b323fa80ebab
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:18 +0000

    tap shine discuss another job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def convert_array_to_string(array):
+        return array.tostring()
