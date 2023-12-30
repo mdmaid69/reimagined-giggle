8e9aed6f18ad4f4be889c2ebfa72728be388798e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:37 +0000

    string town so thirsty politics

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_min(numbers):
+        return min(numbers)
