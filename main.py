3fbc058e79c7247037f6980bd8307e588f63de12
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:11:04 +0000

    warm influence plain exactly memory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
