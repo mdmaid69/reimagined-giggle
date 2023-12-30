2e72f7a3a8af3132a216c7ef85f44da46da32e0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:08:33 +0000

    scale unlucky star mine mark

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
