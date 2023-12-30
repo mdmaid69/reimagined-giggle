77acc4209fd2d6d05b271b74053585f814130562
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:17:16 +0000

    breakout danger live contents order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
