a0ef77c6e193ee3bd7f3053226e961ca84d874d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:13 +0000

    only possible think otherwise subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
