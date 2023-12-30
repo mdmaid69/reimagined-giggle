b905eb45f68cb55c5e4047db72c43e47d5ba4f32
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:49 +0000

    smoke bill journey grow sudden

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def subtract_numbers(x, y):
-        return x - y
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
