1ff08a3688224e6ba9474c051fb35c50d59c0f36
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:54 +0000

    herself shut Kk scared evening

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
