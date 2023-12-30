3476a9f12744f53f47c888322c9ecd78305725f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:06:41 +0000

    punishment week pass hang bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
