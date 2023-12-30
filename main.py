d3a5ae62900f4c48c085d5e2dcbc992ea8010b58
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:51:30 +0000

    shake slow memory bear provide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
