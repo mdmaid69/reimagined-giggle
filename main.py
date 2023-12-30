2efc9e366ff8ee02bd4cd8ebc70c88323ca89e63
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:04 +0000

    estimate now fine excited round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
