cb585f632b665971eaebca854b4ceb5177fdfed6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:16:16 +0000

    blue modern high branch if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
