be989ddf989fde0f659971bb2abbeb44a0f17c12
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:00:13 +0000

    opportunity respect relative geton enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
