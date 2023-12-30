72f65ab836fa5871665b25da74297cf872106c68
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:08:32 +0000

    one clean prize obtain here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
