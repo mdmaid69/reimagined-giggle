473d802b288d66e804c5771ed3351ec56dc4717f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:42:48 +0000

    wish who pot search relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
