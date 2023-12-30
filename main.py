d2186ad9ede516cefd8904b047d7d3d82fb761ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:45:49 +0000

    soak pot help rid detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
