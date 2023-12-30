b2e854232bde956d636af2995a600d5e108d0fa6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:43 +0000

    comment themselves day interesting smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
