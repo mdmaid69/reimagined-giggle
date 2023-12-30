07fe60f537faea053096adb54c539008a3ac82d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:06:32 +0000

    amount strike object connected callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
