5dcba5f2d16cf90a07d488b0a88224fb9e906d5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:25:43 +0000

    different backwards instant crime crash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
