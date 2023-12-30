6dce9dc1be026934689d0506da1492fbdb7ce113
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:10:26 +0000

    software together rush repair lesson

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import time
+def get_time_since_epoch():
+        return time.time()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
