aa35ebbea4e19bded2c2294f2ba639efd868303e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:38:20 +0000

    score object song independent priority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
