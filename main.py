e2aa1decf18ca75ef75922609225f9b807c1e4ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:34 +0000

    pencil fast house press whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def split_path(path):
+        return os.path.split(path)
