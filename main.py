fa3b331e31db68849214ce4129e0d2cc9bf057ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:31:19 +0000

    rob letter trouble possession fond

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+for i in range(10): print(i)
