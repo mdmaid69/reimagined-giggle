15241c23cedf6e0df82d7e2bb6e130f4f78dbee2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:52:41 +0000

    score frame go car keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
