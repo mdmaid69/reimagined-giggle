3f82b077f53b177727f17c3dbc76b0db952253d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:41 +0000

    prove Ii minute cut careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
