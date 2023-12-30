962275be9e54f7ac7adbbd0ff7b1bdffb79794af
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:11:23 +0000

    better frighten hers impressive force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
