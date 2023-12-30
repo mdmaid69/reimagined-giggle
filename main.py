d7963e81b7694e7f4f55100141d5df852d99cc86
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:59 +0000

    quarter smile content join allow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
