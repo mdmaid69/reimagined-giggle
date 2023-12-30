f7a9f846ba4b3c3c0b8157e53b3080c2a56b1e3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:46:04 +0000

    prize person problem several flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import collections
+def create_user_list():
+        return collections.UserList()
