0ea3489e06f13cb73f578c0f90dd50944e0c5bbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:19:45 +0000

    always suggest heavy whatever introduce

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import datetime
+def get_today_date():
+        return datetime.date.today()
