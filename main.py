99a3f93b4c8eaeeae39b86d844e6e4aa52d3ff1d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:50:58 +0000

    glass study interruption situation high

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
