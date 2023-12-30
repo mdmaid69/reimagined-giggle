94a8e1c6e30fc1a8960c4411ac6dc495e6ae4be2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:18:45 +0000

    still joke birth sleep shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
