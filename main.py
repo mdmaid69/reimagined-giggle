f253115f200726e6f2a4400f26739a6faa2424cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:22 +0000

    song typical rub stamp maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
