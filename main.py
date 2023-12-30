96ea8569c0c29e0578c6a90bff653a4298b7e8d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:29:48 +0000

    immediately actually wish material benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
