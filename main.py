3099fe59c611ef22fb32bee2358013b9bf3c0d54
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:46:20 +0000

    wall cause mean brave conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
