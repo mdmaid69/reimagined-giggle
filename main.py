2ebdbd149d6534ffcf6bf126ed7c56aa99a75213
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:52 +0000

    real secret stop open load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
