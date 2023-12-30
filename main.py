40c1b8d2467e7f6111fc6778bad97cc657e63d33
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:37:50 +0000

    godown guard test politics repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
