1b8e7a056beddd8d633389065228b38a6314362e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:04 +0000

    decide replace list ahead performance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
