82684aa17ddc1b45a23d101e8265d786b243cce1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:22:14 +0000

    height remark organization steady tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
