d29210dc0d6cbac35bd7ad26876976afee271daa
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:34 +0000

    pull reserve human temper receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_current_directory():
+        return os.getcwd()
