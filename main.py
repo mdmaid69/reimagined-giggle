4e82261261225c46c902854cda8d5d0557b5df82
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:34 +0000

    still sorry at all usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
