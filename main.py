02db341f68853dab8c84aede93041a9b55947b59
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:00:09 +0000

    568energy firm kind tooth soil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
