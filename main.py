b5919847b2d5392ea2a37e81cfd6b51ef6369761
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:36:41 +0000

    fasten convinced genuine produce name

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+print(os.getcwd())
+def convert_to_octal(n):
+        return oct(n)
