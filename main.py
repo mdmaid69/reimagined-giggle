c742c71e7b019797170e495614f13bad3c0d6c36
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:31:54 +0000

    grow blame 564encourage laugh arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import math
-print(math.pi)
+def calculate_exponential(x):
+        return math.exp(x)
