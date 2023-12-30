62a89124037a7934674f7c5b20cc079ca3a3f997
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:18:03 +0000

    excuse open reasonable artificial squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
