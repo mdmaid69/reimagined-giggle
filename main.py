dafdfe3fabe00cdf5e28ddd6861d0ea62e84b631
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:25:57 +0000

    thinkof good dry small advise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
