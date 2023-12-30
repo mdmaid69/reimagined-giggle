06869c4323b191d4ba46ba6ee6bbe1347e2ebc5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:54:20 +0000

    distant nature punish identify sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
