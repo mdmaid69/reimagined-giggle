9a2668aaa5380bb65ed1942f2f54448f06b31019
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:41 +0000

    course loss contrast unless lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
