9f44169b65feb308cefe96daccf9e7b53b8f108e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:18:39 +0000

    lie Pp coffee waste rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
