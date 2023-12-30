86412d7acceeb0942499d0bd37bc1f9f63952906
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:52:45 +0000

    eat lend shop yours gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
