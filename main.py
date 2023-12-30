6c62fe76b6e1da66f29d3260607a777f8dee84e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:10:30 +0000

    decision mistake nowhere takeaway organize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
