e20e387532c1da0abd6f9c0d3c92b1fe22487b0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:56:54 +0000

    anywayanyhow smile disgusting but fork

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
