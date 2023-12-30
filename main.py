aa9f45ce8b8bcb37ccc92f572cc1267e68244d3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:58:58 +0000

    tap murder pass explain 571enough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def is_even(n):
+        return n % 2 == 0
