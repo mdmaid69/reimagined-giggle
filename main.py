add37d289332351dfacfa47a23b2dbd8f9e3d3a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:03:15 +0000

    deep share make weigh interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
