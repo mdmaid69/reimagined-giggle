e3183e2b25a9f07d3237c5b712a9a5a1a5af80ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:15:39 +0000

    welcome charge this person bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
