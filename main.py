2cd7e210ef3a9a424aa8a069a57fb6227721c2c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:11:23 +0000

    greeting woman mood every swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
+import array
+def clear_array(array):
+        array *= 0
