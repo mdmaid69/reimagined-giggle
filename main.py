c41cbd6fd866b0735ab0e533790ff4254ca882ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:57 +0000

    minority official her science talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_str(array):
+        return str(array)
