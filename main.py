5e4b8653b83a528392fdfd08fb9e3a4107614bf1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:37:22 +0000

    clothes relief same treat measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
