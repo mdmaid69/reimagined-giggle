dc35967a19d39dce0b0f2b44cec9c6f37f93635a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:00:57 +0000

    murder equal tour breakout fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
