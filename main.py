7bcaab9e1097dab5af20a1c613a9497205b7a0cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:22:41 +0000

    accidental mistake movement list year

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
