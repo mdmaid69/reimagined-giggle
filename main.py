cd3b06a7453167dbf59c784daac6dfe137b2e5ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:51:03 +0000

    impressive accident patience no accommodation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
