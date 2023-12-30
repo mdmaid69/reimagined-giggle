e4dedcc9d83bc84d9749bc5ff7be2f54990a4467
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:03:06 +0000

    children stupid firm command branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def append_to_array(array, item):
+        array.append(item)
