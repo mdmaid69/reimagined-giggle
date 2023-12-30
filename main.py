61fe8ff5e03256580dfa2e21830f89c9910e8b6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:33:48 +0000

    sort rarely callat agree himself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
