7876aac2bde45fce63dafcec96e6ae5fb8e09bdf
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:09 +0000

    lose exercise comment watch equal

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def remove_duplicates(lst):
+        return list(set(lst))
