f0b307ef35f77116aa705b86eb1553cb17ff2211
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:41 +0000

    range apologizeapologise sight around shine

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
