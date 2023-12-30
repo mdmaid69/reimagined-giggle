0e7015caceca5f0e8712b0f831b0e0c951d79103
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:09:34 +0000

    occur opportunity kind maximum copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
