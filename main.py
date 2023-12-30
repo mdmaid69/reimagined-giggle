c3c4c9c4183b49773a7981115fd09577e37c64df
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:15:21 +0000

    copy obvious behind figure children

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
