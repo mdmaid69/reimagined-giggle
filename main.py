b5b0033bc22aa32df62f6bf29ac3c56e7b028bea
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:32 +0000

    worse attempt tidy miss comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def add_numbers(x, y):
-        return x + y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
