1909ef9a4eb169cc8a22dbdbc914394ab63f42ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:24 +0000

    terrible this tooth language waste

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
