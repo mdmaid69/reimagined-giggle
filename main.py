34d71b9b43b0f0ecaae3103a8040f57ba9550aa4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:54:51 +0000

    law progress occur cry along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
