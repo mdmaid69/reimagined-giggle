f8271db7ddf1976748fe12332358d2febaba4381
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:33:03 +0000

    combine tend hurry possible keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
