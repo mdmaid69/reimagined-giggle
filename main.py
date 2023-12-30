ce26472fec5b503698e980a96ea49f0eeeea0758
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:53:01 +0000

    mail Tt cross rest clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
