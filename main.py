e7a9cd87b03c21af2471e087557ca7f07fbb97af
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:48 +0000

    accident slip example inside basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import random
+def roll_die():
+        return random.randint(1, 6)
