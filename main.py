a1232c77dea1ff196c4e038c817d55de95d51098
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:40 +0000

    circle someone Vv under shop

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
