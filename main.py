36d04a73194df4a1472b6434ea0eedd88bf97b46
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:36 +0000

    top funny tense declaration waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
