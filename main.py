fe5ef4c2ef2b2f83a4039542944f395f0516cf47
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:51:57 +0000

    decide risk regularly education plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
