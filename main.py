6318a2a59fb4b40775d0a7eab8cd34060d36b70b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:45:39 +0000

    rest stage serve Qq worry

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
