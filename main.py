30461e1afbb82e667df9c990723f63b96df94134
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:06:13 +0000

    concern better flood towards deserve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
