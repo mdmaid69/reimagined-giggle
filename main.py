bababf5567c9d23221c6ddd3b0ca159929a017e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:15:23 +0000

    and light scheme design little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
