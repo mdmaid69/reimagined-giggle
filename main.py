aa3ae982f5badba81ab5461e9ed5d246564ce657
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:44:07 +0000

    persuade scrap breakdown flood expensive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
