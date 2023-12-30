5f9070ba07c75ca5e6d5195439d8c70e0bfb57b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:33:39 +0000

    clear teach star both determined

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
