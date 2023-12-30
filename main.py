ec4aeb6f4ba8fbbeebb7b597b7822f2802a1194c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:47:17 +0000

    ground relative think Oo we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
