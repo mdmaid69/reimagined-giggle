be7119d613098bc6499b0a2191f45341805e54d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:13 +0000

    uncertain family partner unusual attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
