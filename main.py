0ca74f8436fca8de4b6d70986c2154bc62af509a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:03 +0000

    anxious know basis frightened work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
