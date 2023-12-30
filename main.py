24b94ddc4920049cd32ddc20e045ae89f7dac6a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:17:42 +0000

    attempt runinto ourselves rub hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_list():
-        return collections.UserList()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
