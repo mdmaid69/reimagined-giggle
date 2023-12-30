c62439810553fb93839495f3dcf7601a621f011b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:23 +0000

    receive equal call square hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
