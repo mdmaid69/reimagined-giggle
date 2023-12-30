e096d6cbfa4c2fbf6a1d803bfd8a1b0ab1064e28
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:13 +0000

    valuable perform justice somewhere little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def get_array_item(array, i):
-        return array[i]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
