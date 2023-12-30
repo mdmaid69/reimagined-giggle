c23ca8ded4141beaf84d069f73ea5b53e84d38d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:51 +0000

    around plan cry Kk large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
