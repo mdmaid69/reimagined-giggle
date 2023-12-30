f49de0bc9c844c67e248bbef62cb14bfdefeaf15
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:29:30 +0000

    pinch dress convincing turn variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+print(os.getcwd())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
