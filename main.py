3aa904bcf1a134b3c2fee3b6769b76ce8cbdda7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:00:36 +0000

    treat possible promise bite right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
