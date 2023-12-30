8d8d0a56f17801316244ce88cdb2197172b0d202
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:05:43 +0000

    success sensitive strike quantity drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
