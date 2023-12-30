8dd33c21a7d45f8cfe023e97246a2c87e8070cb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:41:03 +0000

    might pretty support write direction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
