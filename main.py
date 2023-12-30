34550ecd457ddb9a35a3e94a90250e7f402658dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:07:29 +0000

    side impression account loan night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
