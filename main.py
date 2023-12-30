158805727679df593f4a7fa1a8c191c5034ef6d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:12:17 +0000

    fold test stage ill mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
