08e87bc93af812151dad0db8b49832fe115986f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:21 +0000

    giveaway concentrate stamp conversation famous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
