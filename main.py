59b006f1fc8d8f05ce63ffeb83117c9128e5a2f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:15:14 +0000

    born interruption tragedy destroy slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
