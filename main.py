8595a2aae44937bdd511735c4e8901eb490f4fdb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:28:47 +0000

    179bite tap each wide real

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import datetime
+def get_today_date():
+        return datetime.date.today()
