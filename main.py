48a30e69f3654a552f4101e9e20dbdc10c18de4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:14:14 +0000

    firm force group pay sister

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
