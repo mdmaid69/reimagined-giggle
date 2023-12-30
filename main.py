8b20d09ec211b82b1a514c383ff242b4fcb6f853
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:13:27 +0000

    freeze cut coat earth so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
