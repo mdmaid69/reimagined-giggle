b3748ac59f6fa015bff316444b61cd8c42b71350
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:54:32 +0000

    ready goon below average wild

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
