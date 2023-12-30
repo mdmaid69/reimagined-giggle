ae3239ce1f74f5a3f343ef5d8f556ce713bfc757
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:37:44 +0000

    display get blue swallow without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
