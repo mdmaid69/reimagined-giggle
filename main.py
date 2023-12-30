e429c6fbb1e54e3954f856cd0e089a0e897efa57
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:21:27 +0000

    else young view serve within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
