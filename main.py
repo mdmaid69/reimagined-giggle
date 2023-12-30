06616aaf15eec84aa049e938b2c9f7597177acb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:55:29 +0000

    above tense dry buy slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def convert_to_hex(n):
-        return hex(n)
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_set(array):
+        return set(array)
