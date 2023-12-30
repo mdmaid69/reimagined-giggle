c8771ae5fc39875ede71f583e59bd74800eb67b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:34:59 +0000

    support invitation callat exist continue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+from collections import Counter
+print(Counter("hello world"))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
