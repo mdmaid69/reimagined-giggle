af9bf1a66d5c83fb26e96889d405ffbf9b940c46
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:56:34 +0000

    shock cause stiff blame belong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
