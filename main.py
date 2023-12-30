b5f3da6c442a605d84f32dd697a0a4cff737720f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:51:27 +0000

    artificial never risk limit exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
