7ec6e754856111cf3ab4f636c63b48ea27a78df4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:30 +0000

    turnon string accordingto 572enter soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
