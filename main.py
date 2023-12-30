9e07c6d52bf23865f7f10a79403baed163d0a802
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:47 +0000

    last inform serious offer thanks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import sys
+  def get_python_version():
+        return sys.version
