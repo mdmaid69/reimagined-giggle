ec20b0acb7a10489cab69958941e059fd7219277
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:03:57 +0000

    pocket gooff separate close bang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_exponential(x):
+        return math.exp(x)
