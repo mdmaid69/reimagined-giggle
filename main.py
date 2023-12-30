73bd923921f9e7d0a415c5f559b8457b25dc7b40
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:10:31 +0000

    explanation look none medium there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
