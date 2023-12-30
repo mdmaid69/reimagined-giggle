e6941486e729ccf7c2660672879443b31a00cbff
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:50:32 +0000

    dress question reach half compete

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
