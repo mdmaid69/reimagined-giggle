649eb1443311b0ab0bb42fffc6de9a16b05ecc83
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:46:40 +0000

    makeup same return there without

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
