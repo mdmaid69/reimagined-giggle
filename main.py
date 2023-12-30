fa36e0fdb5558159a0ff81eeb19c9b1f7e30f6fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:03:01 +0000

    rob Rr anyone message minor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
