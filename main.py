08ade1f7985257a2ede93bbc85bde01df4fcd208
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:24:12 +0000

    discuss shoot pause convinced runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
