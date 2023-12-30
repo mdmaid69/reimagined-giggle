79404511661c93ffdac57302246e77ec59a0d154
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:10:56 +0000

    touch pass choice appear obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
