cc779a5b0aa5ac88a7f60629bd9557921b2c75d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:57:25 +0000

    return noone aim operate however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
