18dd629932855f1e3cb4da9ab03460991bb2c609
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:56:22 +0000

    silent takeoff pleased brush make

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import sys
-def exit_program():
-        sys.exit()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
