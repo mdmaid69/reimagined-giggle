ce3a1746d937474413c5e59c2548182e470685d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:36 +0000

    nearly peace set fold criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
