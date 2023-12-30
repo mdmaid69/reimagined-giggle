8e000a0f32c00ab174044df327de35d14d5c0bb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:17 +0000

    bend return dress reserve some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
