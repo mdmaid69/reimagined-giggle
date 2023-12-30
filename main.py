2c37426f987f5d9abe54ba1ef1abbf3c9b2f4dd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:31:40 +0000

    size ought minor rescue answer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
