750a27a6cc3af5a84414882ae5122b3e723d89c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:57:19 +0000

    anywhere excuse cruel people welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
