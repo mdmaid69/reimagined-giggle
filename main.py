79521837ca66ef703b04ad5cb67ab1132c44b3cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:57 +0000

    forever cap different agreement divide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+print([x**2 for x in range(10)])
+import sys
+def exit_program():
+        sys.exit()
