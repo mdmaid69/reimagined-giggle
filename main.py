6426158e9624f242ea46e7fcab01b8c68b8c6903
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:08:21 +0000

    separate worth hollow acceptable waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
