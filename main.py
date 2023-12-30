a3186db84ab1116c19c3ecd76180b2eda893f322
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:32:59 +0000

    accidentally block present stare ought

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
