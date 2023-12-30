38762bb0feddcc0e2f4f9b7a551b5e142ac55518
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:59 +0000

    stamp popular practice fly joke

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
