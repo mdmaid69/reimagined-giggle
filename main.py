745cde894c0dc0e06bbb7d8de20c592c556803d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:41 +0000

    cost wave earth right surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
