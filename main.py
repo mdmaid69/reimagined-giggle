785946369165b2934500f87686d930ab63294069
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:10 +0000

    keep control clearly simple him

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-print([x**2 for x in range(10)])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
