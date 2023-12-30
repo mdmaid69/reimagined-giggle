ad2d5fce2826c78c9222f2b8c0a4b5292bd975a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:55:02 +0000

    warn impression can opposite cap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import random
-print(random.randint(0, 100))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
