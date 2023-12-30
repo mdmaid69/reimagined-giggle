22f0dc193d84ecbd62f40560c6ab3a114ca95a49
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:50 +0000

    trial high dick among wall

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
