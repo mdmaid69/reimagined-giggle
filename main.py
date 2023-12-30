65493212d8b9407e3a1f4b11a5a796f1479acfad
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:49:58 +0000

    goout sure container prevent spread

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
