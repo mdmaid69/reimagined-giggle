4116217867ae4b0f57a938f77e5dfabef4df71a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:48:08 +0000

    close cold opposite anyone theirs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
