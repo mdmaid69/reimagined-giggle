708ad49087ce696ac8c953d9e155dc678057519d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:18:38 +0000

    hole laugh busy scream achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
