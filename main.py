71d8014d3de0dd6f1958f721eaff921ef883f699
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:40 +0000

    cheat belief finally will ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def subtract_numbers(x, y):
+        return x - y
