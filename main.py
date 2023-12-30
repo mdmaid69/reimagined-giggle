8e1423c7bb0ba252eaf77b44fa70e186c3df74d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:31:44 +0000

    surprised clear giveout sorry save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
