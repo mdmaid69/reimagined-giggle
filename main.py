71dc0ef1fad1adbe1cf0e570c59276ccde416626
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:39:31 +0000

    brain thin favourite real position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
