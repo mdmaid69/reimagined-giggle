21cafdc0573f27ca5bb77cfcc501110db3687bc3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:01:09 +0000

    fear offer unable female 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
