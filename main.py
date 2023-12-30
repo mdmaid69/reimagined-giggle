4c7f36f00e6fdf23fcc8e63a78d6f42dcb7c9d73
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:19 +0000

    another influence close health involve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
