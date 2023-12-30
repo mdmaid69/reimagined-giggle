1f2a4ddcc00a6a72dd8abef763618ea7bed8fdeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:07 +0000

    slip fire trade trust hurt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def multiply_numbers(x, y):
+        return x * y
