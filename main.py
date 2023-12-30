8c0c191082c0c18fdcfae654ab0d33ae85092c3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:46 +0000

    work rub breakfast extraordinary have

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
