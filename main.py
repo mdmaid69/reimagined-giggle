8105230e809538a4dc1dc24affed0f217167561a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:45:31 +0000

    schedule above basic on yourself

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_max(numbers):
+        return max(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
