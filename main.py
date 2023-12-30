8427371aeab14e583f18b28c8dd82a49d3567940
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:07:22 +0000

    possible down their deliberate suffer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
