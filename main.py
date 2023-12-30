8cf193db429b671e258da9acbdcd7949842c3f5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:53:43 +0000

    hollow quality now error value

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
