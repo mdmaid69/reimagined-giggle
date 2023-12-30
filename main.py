ac4d5898dfab54a9a466bec160fb614c0f689eda
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:53:23 +0000

    declare centre begin pronunciation none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
