1c83c9e25bf149f9b95b0414e1e68e51b38a7071
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:33 +0000

    settle us stir arrangement Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
