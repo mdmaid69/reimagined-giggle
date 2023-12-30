447e2d8140b39c000c97360abc7b527f26abf80b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:30:03 +0000

    Kk journey giveout rub address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
