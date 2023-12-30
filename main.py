efdf1f8af6621855252dfdc261833d6630ae478e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:18 +0000

    pollution plan surprised fix pocket

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
