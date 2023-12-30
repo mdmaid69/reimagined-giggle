6c5eee3437f94ea90fa53527d80801b1183e777a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:53 +0000

    organization ignore tidy breakinto fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
