e1ddf95b7a6dd7ee4207b29f89febad791a454b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:49:00 +0000

    favourite position go married drive

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import tensorflow as tf
+print(tf.__version__)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
