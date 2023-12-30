a47e7329fff7f71d78a8952202d52cfc165632db
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:48:21 +0000

    recently invitation various fit pencil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_force(mass, acceleration):
         return mass * acceleration
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def clear_array(array):
+        array *= 0
