dd5a6e1de3d98c589addb64a5be91967886c5c11
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:46:10 +0000

    tool journey thorough affect spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_volume(length, width, height):
+        return length * width * height
