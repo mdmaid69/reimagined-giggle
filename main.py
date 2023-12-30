a2e3b8c228b20237d7169b9f92963c833563f32c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:57:00 +0000

    surprised mean lesson otherwise best

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
