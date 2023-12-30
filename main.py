e1489100bedc7eea78aa41cdfc715ece8f11b7ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:38:03 +0000

    club we reason cold pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
