ddbfa34db1a13c5c97e2f4603c4a4f0f79282611
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:09:40 +0000

    lead bet list shout lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
