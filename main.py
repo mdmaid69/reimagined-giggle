6beec08dc3e9b2e22494e06971f755321b133284
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:30:47 +0000

    love last trick love further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
