dc5e7a6e3c68a94f8a2f68d495106d2a1e94c7c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:40:38 +0000

    or passenger dress when blame

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def subtract_numbers(x, y):
+        return x - y
+  def convert_to_hex(n):
+        return hex(n)
