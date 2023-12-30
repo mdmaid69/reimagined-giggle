b7bc2a8d47f8de3ab9c7437c5760a1dc865b6d5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:46 +0000

    relative comeround little be fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
