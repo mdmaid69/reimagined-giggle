9fc0e1d2f472c500d2ad6cf25c2b4fd2414daed8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:57 +0000

    slightly hard judge spot beneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
