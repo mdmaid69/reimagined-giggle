c811ef3a4e8f3ff2d6da6de4badd9f73acd64982
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:57:11 +0000

    succeed fence tradition recent Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
