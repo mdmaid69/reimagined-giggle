14799ebac3d2ea6bbda18d329df83d0e40f89504
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:34:20 +0000

    bridge hand trap advice prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
