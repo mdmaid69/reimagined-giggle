f04cce188bf1db323c519fda0f92d1bfda974be1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:18 +0000

    opposite number become art unfortunate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
