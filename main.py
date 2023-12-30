8ecbf8fe920649b72992ea83ac90fdda64148fe5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:29:20 +0000

    dish onto cheque crime tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def divide_numbers(x, y):
+        return x / y
