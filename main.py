0867cf1591c5ab52ee2cbb29ec81aaf2eed31280
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:40:45 +0000

    luck talk exactly conversation minor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
