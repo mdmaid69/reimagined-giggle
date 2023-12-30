f2c440b46ac3d47e941abb149e7ea69ce12a09a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:54:38 +0000

    below wave ago bring before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
