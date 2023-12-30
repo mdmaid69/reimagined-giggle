6b3145a622c0eb1ff96c3163d4aad72bd339a889
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:07:42 +0000

    opposite unfair evening opinion boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
