0c6e33a24cc605f200abf014f4b17187ad13e26d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:02:21 +0000

    supply listenfor operate complain food

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
