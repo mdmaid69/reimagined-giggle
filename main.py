02080c0ed199ebb9a6019c8f3823ae9db0bb6010
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:01 +0000

    blame stop contents respect fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
