d81a8b44261ff92c92198da3eabd1e4dc1a643a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:43:46 +0000

    invite best share disgusting skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
