161661a1548d89457f5370bab666d709e90c2bdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:08:17 +0000

    union form can tight somewhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
