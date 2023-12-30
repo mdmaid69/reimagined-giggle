3d2c6ddb4a5ef3a7df4215e9026abe347d2e3f7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:54 +0000

    comment return short thanks present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
