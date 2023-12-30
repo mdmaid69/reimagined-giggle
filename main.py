d8ef50accd9d38e2d9af5410ee6bc8cbcf073052
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:35:45 +0000

    follow gradually stop lookback(on) top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
