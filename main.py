0cbde45b08c987bc991385ca744189276329e685
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:42 +0000

    error usual national impressed general

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
