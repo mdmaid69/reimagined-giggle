4e777d64d0602fbd0d4cb11682f9de7e18085b6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:27:53 +0000

    pressure help lie such influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
