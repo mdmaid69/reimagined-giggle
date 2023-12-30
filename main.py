22e3045d1d70f278da8a461c4ac3a589b4dc4b73
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:11 +0000

    little while exercise drawing strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
