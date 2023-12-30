00a079ccfb190a2c7ec5d7c4dfce77f2d006f411
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:30 +0000

    increase normal label male interrupt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import numpy as np
+print(np.array([1, 2, 3]))
