50ea15a528419624bd74d5b8f23a956fa3bc36b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:19 +0000

    either unusual skirt back ought

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def cube_number(x):
-        return x**3
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
