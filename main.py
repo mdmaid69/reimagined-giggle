d9798636829604bdf4ee60e7faed023f1bbae142
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:28 +0000

    fight motion bother us later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
