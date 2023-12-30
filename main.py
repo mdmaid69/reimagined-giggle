34184aedf47c240ff871f9f70d22ae1bb39252ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:11 +0000

    pair mine software reach develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_repr(array):
+        return repr(array)
