7df2574fac461750ea32a442103c43aa784e32ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:30:30 +0000

    lookup lookafter passenger put ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
