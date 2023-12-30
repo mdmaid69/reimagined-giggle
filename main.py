3b81e3ab97711de669191ede53b1581336d7fd8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:06:44 +0000

    hard job bother thick react

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
