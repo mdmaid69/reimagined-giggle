4af42a4b8a8bb98df59093cf5940b15707b8c93c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:23:23 +0000

    emphasize exam takeback no rub

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-print(time.time())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
