4de862c4f3750bdb966b4c5335a0b35194c0a182
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:54:40 +0000

    near by bet ring slice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
