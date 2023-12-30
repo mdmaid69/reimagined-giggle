10eaeef252cd88043db12c8651e2d6a48b801ec7
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:32:21 +0000

    bush whistle left hour case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def is_odd(n):
+        return n % 2 != 0
