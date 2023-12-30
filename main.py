01baedcc1f7204fa85ef3833e0c584fe84609918
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:04:53 +0000

    method period worth sail remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
