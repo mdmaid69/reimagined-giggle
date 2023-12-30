0f76ca1e87355b2d08dc8601ed76ab84ae20750f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:23 +0000

    noisy neither near thank makeinto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
