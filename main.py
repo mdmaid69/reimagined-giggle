62f5ef1d6415193096096d5e9faa0761400be221
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:17 +0000

    surprise object children strong bar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
