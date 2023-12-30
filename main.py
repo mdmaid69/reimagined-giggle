61634f573854dec8086b6d39da7a7294bd741e9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:27:28 +0000

    move sport shop circumstance rare

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
