9e7014e20dc1b3bda8f6508fe2b79a409a54b256
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:53 +0000

    heavy turn concerned lose unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
