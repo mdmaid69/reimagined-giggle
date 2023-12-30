3d3164a62ef7e512465af9e05581a361e70e6c10
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:02 +0000

    majority emergency wheel most argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
