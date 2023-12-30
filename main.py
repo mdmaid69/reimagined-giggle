e0b955d6c5e3fe08f68758fe63f8d8e8111bb7dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:08 +0000

    takeout turninto couple scared represent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+x = 10
+y = 20
+print("Sum:", x + y)
