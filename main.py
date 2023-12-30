2c68ed8d2148fb498db8fa692db1f47b078efc0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:41:26 +0000

    Kk smell contrast comment disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-print([x**2 for x in range(10)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
