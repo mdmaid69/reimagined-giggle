7496df3b3b5bb3c4aab5c6aa24952fb2d6fa93c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:54:57 +0000

    fork act second claim mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
