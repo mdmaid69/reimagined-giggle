05db27b14ffc2b496337559b6c77b41f2f83cfde
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:33:25 +0000

    alive violence comment compare innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
