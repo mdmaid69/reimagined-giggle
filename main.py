ad0483b9cb4853bb33857d4dd4a23ebe901e9b0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:03 +0000

    contribute plenty breakinto air worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
