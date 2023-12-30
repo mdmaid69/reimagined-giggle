a1ec129a47b3f5cd93ec78edcee063b24f865650
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:58:05 +0000

    someone intelligent exciting calloff eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_current_directory():
-        return os.getcwd()
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
