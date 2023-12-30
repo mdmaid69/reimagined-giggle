726fc9a169b2bd1518d98f21ac8cc9dce608248a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:52:03 +0000

    translate event bottom goout rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
