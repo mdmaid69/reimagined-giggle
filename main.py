1fab4764ed1dc99abb13265fe83017f54e93ee50
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:26 +0000

    tax sky Itself thought spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
