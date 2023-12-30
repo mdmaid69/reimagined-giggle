1c5cea15084f326574747ad765686cfc84e42045
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:12:56 +0000

    on share yesterday noise this

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
