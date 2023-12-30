afab1557607051256758ed1e1adda8b701708b81
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:02 +0000

    mark influence crash whistle space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
