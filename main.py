04c850d48b7e453c35778f90b81aa21b70571fd9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:06:15 +0000

    turnaway sweep escape type deep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import numpy as np
+print(np.array([1, 2, 3]))
