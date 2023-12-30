1930a97f874424fce0e75204c5ff6e4e55d732b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:47:16 +0000

    load leg happen famous custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
