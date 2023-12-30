e000c4063db0393c36436e580ca083e7a2f349a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:31:50 +0000

    prevent mess Ii square public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
