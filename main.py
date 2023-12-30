0022b4907ba38e92f4c957642dc90088a1a503aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:38:04 +0000

    interfere alive accidentally blame exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
