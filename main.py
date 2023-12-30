e06887e0b5ee38de69f54d5cd4883567b037bfda
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:32:09 +0000

    worry check floor burst all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
