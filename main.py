77faf8b88ddddec3f1b18d8b08ed7f8b617d031e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:02 +0000

    suit edge exploration while colour

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
