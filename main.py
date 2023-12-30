70e3ab11e03d860bedd840a0b43bd649dbf4f22f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:22:52 +0000

    mark possible sensible minority different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_current_directory():
+        return os.getcwd()
