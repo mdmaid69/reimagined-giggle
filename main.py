ff4d856683695ecb0ac6dbf18cbc7caec71ee167
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:59:45 +0000

    autumn steal well pity official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
