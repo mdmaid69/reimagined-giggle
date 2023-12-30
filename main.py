d4b1ce5568d29c3ab376d0a5bd863cf9a05537cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:12 +0000

    pain wind shout sensitive plate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-print(os.getcwd())
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
