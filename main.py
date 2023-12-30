9609d7f21437bbe69d016d007b0a931c1e6230bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:13 +0000

    line stamp wall fight lift

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
