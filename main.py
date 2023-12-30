340106ff6f54ad0f63a764f968fa2801d05c3fa7
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:25 +0000

    still approval balance refer negative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
