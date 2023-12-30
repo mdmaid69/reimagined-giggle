268ffdf62f15c733e1c11506c3828b98d991332d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:24 +0000

    provide few damage major dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
