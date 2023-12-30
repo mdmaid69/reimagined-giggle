f02afc146cc5e89588c5b1523e28a708df4034f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:35 +0000

    than possible ticket praise share

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_unique_words(sentence):
+        return set(sentence.split())
