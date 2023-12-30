bddb4e1b75ac6d89ab7e3768483a34a896052dfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:02:00 +0000

    willing order keepup shout almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  def is_odd(n):
+        return n % 2 != 0
