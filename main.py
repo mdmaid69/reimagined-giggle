4edb095f883697f116806a6974723fdb88e7dcb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:28:20 +0000

    pocket appear turndown danger bunch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
