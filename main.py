9fde068bb000b1366704d37d9cf4356a06d22091
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:09:08 +0000

    runout common healthy die real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
