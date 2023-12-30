7799f29c7725dda8d532ea57b3da373c71e68d99
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:28 +0000

    church fall cry past wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_acceleration(speed, time):
+        return speed / time
