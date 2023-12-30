c3fc5ba8bfd0a8aaf0799d8796cdf1e230d0b510
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:03:27 +0000

    will such rule coffee information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-print(os.getcwd())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
