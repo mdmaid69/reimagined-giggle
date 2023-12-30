322012e2529cc24e71a7060781ca9db7702917b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:06:42 +0000

    dead calloff ago quantity disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
