cea5476891f386d3de8a11a7ddbb89d2377d768a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:35:20 +0000

    nigga join charge within callback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
