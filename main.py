f621056671eca8a9dd64c0e01d0af80d1081ec60
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:54:11 +0000

    punishment touch reliable place hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
