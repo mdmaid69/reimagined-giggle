93d92a5bf8f25a39b436849a455deeb6e7cbe4a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:01:55 +0000

    listen issue if feature 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
