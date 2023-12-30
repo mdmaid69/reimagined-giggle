9a8f8db21c6f39bc7b5bbe71807c1c898b6b6ddc
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:50:42 +0000

    save wise war yours keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
