34991370f77080d1749eef6adac2117e6e2aa933
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:49:57 +0000

    convincing measure doubt talkoutof estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
