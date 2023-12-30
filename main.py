5645aae8c76617050e08e9bb42cafb86a07c2f1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:46:00 +0000

    jump thing ever slowly deal

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
