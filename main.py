df4edc38ab9e5d299e25a506482a7ee995f41c3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:12:42 +0000

    further rank war strict unlucky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
