2fdb8cc63abe67c317dc229e717e484ef6ed44d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:42:50 +0000

    calloff impressive wheel rest meanwhile

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
