b592896063052166db273711eef84ef0f710e528
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:04 +0000

    appearance practise start criticize test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
