023c4e778dc8752acc766e95342ccc057ba7e3fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:18 +0000

    pin modern moral hour bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
