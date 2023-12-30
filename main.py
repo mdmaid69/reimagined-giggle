3852929af65fb7fe0d4ea342fc5f02dbd73c5758
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:15:14 +0000

    suggest board lucky hello impressed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
