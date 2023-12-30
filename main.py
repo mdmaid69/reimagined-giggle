29d65b408e3f2b73f056752b94a74bfb316c8168
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:49 +0000

    anybody wise geton otherwise wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
