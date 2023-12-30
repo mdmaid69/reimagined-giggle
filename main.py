38e10a8eeeef4f4c51319acb4dbd356c403ed83d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:33:23 +0000

    air process action keen 566end

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_typecode(array):
-        return array.typecode
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
