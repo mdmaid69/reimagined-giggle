529b84602fc198c46eb2e960ab405f36eb112c05
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:27:54 +0000

    split weigh maximum giveback change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def reverse_array(array):
+        array.reverse()
