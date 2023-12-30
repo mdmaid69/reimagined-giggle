ba17ed64abe2beafef0672ec05138e7478a1cbd0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:11 +0000

    holiday form past male child

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
