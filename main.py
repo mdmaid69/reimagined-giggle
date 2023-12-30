56014ac61dba6ac5fd4581225509f5f5e45238d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:34:00 +0000

    reason piece good order pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
