3dc04e797834ee06edefbffb0d047774980beb83
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:03:41 +0000

    sight lookafter drive generous breakinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_max(numbers):
+        return max(numbers)
