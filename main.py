d12779d020b3ca05f6f94b7b0c1d7fdb3182a45f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:31 +0000

    somehow trip promise opposite brief

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
