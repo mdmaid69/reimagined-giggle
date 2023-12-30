73f368a55aa3628cde8cb8e626ef52d6e5641fb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:04:03 +0000

    need practise famous disturb exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def append_to_array(array, item):
+        array.append(item)
+def find_unique_words(sentence):
+        return set(sentence.split())
