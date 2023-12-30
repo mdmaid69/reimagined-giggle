00646f271510c1e35e6af8861a0b4669b3f12dc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:29 +0000

    once doctor club second along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
