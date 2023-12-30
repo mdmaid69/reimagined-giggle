feb09415802a67f248c6d5436b553845c413948b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:03:21 +0000

    load keepaway treat will pay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
