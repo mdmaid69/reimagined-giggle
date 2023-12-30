21cd6b29c53a04bfe55848d57b2ff470b4ec5eb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:40:54 +0000

    bright stop contribute turnon hospital

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
