6a2013c62e37f766c141e4e79b8e8e8582c6424d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:04 +0000

    rich either shut grateful bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_words(sentence):
+        return len(sentence.split())
