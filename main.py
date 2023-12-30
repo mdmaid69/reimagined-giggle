7824fe56cd66cb210f8e392d50b446ee13f1f158
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:00 +0000

    rest communication activity protection letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
