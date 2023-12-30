526e02cb9106bc3beb51a5381f81056f4d4d7851
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:37:56 +0000

    break about comfort suit underneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
