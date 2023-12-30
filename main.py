248b83c35ed98f36b98c14887c9c6cf9185520ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:55 +0000

    move late disappointed each town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
