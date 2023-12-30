d627024188750a2747328d8da1fd9d899bae5b2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:03:11 +0000

    nothing reserve correct age port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
