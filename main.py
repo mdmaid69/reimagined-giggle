d337e690c7142a0a43ba5770defffc46e847b5d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:35:28 +0000

    near takeback earth willing such

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
