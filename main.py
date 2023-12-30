beb64c7dfb497e4c50896f54e1cda7fa762b8c0c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:33:00 +0000

    cook surprising do store major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
