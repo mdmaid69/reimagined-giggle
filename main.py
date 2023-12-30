87857864c4108dfe31f5726771e9d6430cb6f60b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:45:03 +0000

    show instrument destroy some store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
