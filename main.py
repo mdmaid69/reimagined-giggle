792eb773a35a532af2f445fce9c835a9c54675f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:27 +0000

    track do search movement blind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+from collections import Counter
+print(Counter("hello world"))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
