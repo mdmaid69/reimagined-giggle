526567c47291c8795e91018abeca5055c0d86f0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:51:38 +0000

    just matter life related storm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Hello, world!")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
