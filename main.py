e746b6bc7597c0551054189e0a22ef158f1512cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:55 +0000

    cry exploration point warm whether

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
