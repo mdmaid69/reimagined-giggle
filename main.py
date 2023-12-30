00691fa941c5279ffed2d9b5d4cae9461b78264a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:48 +0000

    body calm actual abroad save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
