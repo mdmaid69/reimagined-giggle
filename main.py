a221d0a9ab4d50f5b8c0ab50dfb09b3ff5f10100
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:13:08 +0000

    count convince inquire demonstrate mixture

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
