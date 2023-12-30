a00f1d96515939a206b5b0ea183510fe68a26220
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:26 +0000

    display himself straight wave good

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
