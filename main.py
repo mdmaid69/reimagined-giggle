40e5b0d640f92396a9e0f196eb11b3e0b0f85a9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:10:39 +0000

    tax drawing out towards bye

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def get_array_as_bytearray(array):
+        return bytearray(array)
