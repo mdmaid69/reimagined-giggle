604a2068ff0c7bc5ac4ac9b541bdb304ed8a2959
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:55:33 +0000

    knock allow name flow brother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+print(os.getcwd())
