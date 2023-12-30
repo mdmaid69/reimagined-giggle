5f9335e1cbb058ca5e78c3ffd2989f548ba1ca57
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:10:09 +0000

    right price constantly purpose common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_average(lst):
+        return sum(lst) / len(lst)
