54b3a2d1a9d07db219912eaf1464fbebcbea3fdd
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:04 +0000

    lonely knock nervous future by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
