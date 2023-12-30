4a082ad04f870604d1f0e7903bec9e3dbdc5eb9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:57 +0000

    not ilovedicks sick wash mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
