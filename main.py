4612f8ad5579d2c16662495568938f2f15a5f024
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:36:37 +0000

    hole bye comment control his

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
