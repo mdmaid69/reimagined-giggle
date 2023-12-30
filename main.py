6d1e7e3bd0a218bf2c54319029681f7620f77b7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:34 +0000

    important 569enjoy bush hang suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sys
+  def get_python_version():
+        return sys.version
