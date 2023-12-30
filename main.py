e4bd02329b7267a0b542ff8acdfed1e2c32265ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:04:24 +0000

    uncomfortable our noisy blue plant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
