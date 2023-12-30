63c9d57637c1b4913210735598b5c746c40d9a9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:29 +0000

    practice suck emphasize broken autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
