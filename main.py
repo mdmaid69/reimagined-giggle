e452bbbe6450481d7e8c00d33103f117ab595517
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:28:09 +0000

    serve light love defend still

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
