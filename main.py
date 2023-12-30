05c232527c72a7fef69c64e22535a34f43a8c5cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:48 +0000

    about damp material let clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
