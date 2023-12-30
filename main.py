588e1bf884159563d159c8d83650a510d897fd16
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:36 +0000

    ill challenge work wise course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
