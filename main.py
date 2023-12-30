d7d8ff904ead1e94ea75f8a9a9c238cd73363fe4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:47:03 +0000

    front sit cook method worry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
