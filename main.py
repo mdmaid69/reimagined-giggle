60c2637c204d2e38a1592224278d59b571ab9371
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:11:04 +0000

    hard port respect breath negative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
