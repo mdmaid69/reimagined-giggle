6334c20a929404aae6ff83a9373bfd89992145d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:46:41 +0000

    throughout keepaway kick afraid whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
