8fa6104754900cc9346dcad80fa60b6981e30248
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:37 +0000

    unless extraordinary delicate exhibition pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
