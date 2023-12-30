b250ddb61edbe3ff2e5cddea036a07df0b400e6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:42:06 +0000

    modern understand mistake callback whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
