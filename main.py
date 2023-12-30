c7e55ca42f05e5bf81da77f24ad6993ec053393a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:57 +0000

    stretch paper blow Qq last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
