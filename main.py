826b503088e129c67e5fb0eda84911f2bf9eced8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:38 +0000

    Gg 565end open effect emphasis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def convert_to_hex(n):
+        return hex(n)
