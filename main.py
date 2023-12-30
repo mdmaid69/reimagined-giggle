5f44703e03bf0d91464f1cd47f5abbe1abbc2168
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:29:47 +0000

    house lookup expression injury whistle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
