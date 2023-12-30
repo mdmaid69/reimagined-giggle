ceb1013cae5808bf324d8428cda8128759a5424a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:42:59 +0000

    water reach black but remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
