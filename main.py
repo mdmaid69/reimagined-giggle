391c826f6706aa976bc44d1e6de38d3b3b8f69f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:01 +0000

    absolutely brush soft foreign computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
