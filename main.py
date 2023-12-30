46b4e701a3203daeb9197795f0dcf94618d9ea98
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:45 +0000

    own section modern together before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
