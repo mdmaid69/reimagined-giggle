d0148a5c6659314df46d1a4474ff8a9006ca496c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:12 +0000

    measure but bad length away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
