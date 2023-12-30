064d1398334e90197f9349cb6340037970f88dec
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:00:58 +0000

    better crisis position drive failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
