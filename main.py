6f2cf4c7777d394ed0eef36d4607bf4aa573066e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:29:55 +0000

    load centimetre closed entry climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def cube_number(x):
+        return x**3
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
