c87cb3129ad2018caf3fdf394c2662c7882031f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:21:30 +0000

    wing little damage grateful forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
