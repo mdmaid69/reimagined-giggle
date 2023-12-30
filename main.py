7a720dd267f89d461053b56d055f9654e4cdd591
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:05:49 +0000

    combination nerve event hard Dd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
