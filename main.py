af550b68a1ea665e71f673c258104f8419d07a12
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:33:45 +0000

    poison suck mean dish pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
