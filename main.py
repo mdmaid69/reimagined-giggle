d6cd604ea6813fa74eacccf0b93b13471a6245ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:56:53 +0000

    be contrast her run cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
