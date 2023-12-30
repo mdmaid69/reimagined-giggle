2164160c4de024d6ca8a35672a29a0b7c3367e68
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:18 +0000

    sign shape head branch store

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_float(array):
+        return float(array[0])
