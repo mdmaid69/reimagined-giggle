d694afc653578ea2ff72aac86eb05658b61ee757
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:10:39 +0000

    talkinto polite after weigh scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
