8b62124cfa2a278290d836febaf8d2ff07b8615f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:38:33 +0000

    smart check clear circle yes

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
