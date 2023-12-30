cf59758f9b1f861f47a7d66dec2bc4c432fb84db
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:40:24 +0000

    appeal approve sentence knowledge band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
