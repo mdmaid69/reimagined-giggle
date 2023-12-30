d4c472fb86131dd357a17f3410cbd76403e59d56
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:38 +0000

    worried attach temperature duty past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
