e7d30fbcaf8178184d9dcad6be89232eb432dd94
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:27 +0000

    unlikely call month receive butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
 import logging
-def log_message(message):
-        logging.info(message)
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
