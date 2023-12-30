047a742db3ad2983e0ee9d7a5b5bcd05b6ade421
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:32:49 +0000

    belief length swallow invitation oil

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
