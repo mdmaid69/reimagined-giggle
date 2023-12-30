06904f597b46dfb0142899ad98fe81521e01926d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:58 +0000

    trap boring demand organize bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_str(array):
+        return str(array)
+def convert_to_octal(n):
+        return oct(n)
