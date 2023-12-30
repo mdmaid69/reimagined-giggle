da430e2a5cf86160bb84db22d9aeb56e468dd7f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:22 +0000

    time save air brush what

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def cube_number(x):
+        return x**3
