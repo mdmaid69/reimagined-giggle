6815d49192d21e0721646fa6075b08a81621c3d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:23:48 +0000

    meal inform Ii speak difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
