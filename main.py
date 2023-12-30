cd5de8d3f377fec67aa35b72e6ee7cce25c87a9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:57:36 +0000

    along round limit quiet careless

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_unique_words(sentence):
-        return set(sentence.split())
+print("Hello, world!")
+def calculate_acceleration(speed, time):
+        return speed / time
