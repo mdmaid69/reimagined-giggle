949d08a1c131a4f61d5e958e292bc1bac9af4ae7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:29:03 +0000

    pity ashamed satisfactory most exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
