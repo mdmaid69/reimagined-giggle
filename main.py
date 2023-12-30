2cb80e4ddac5d0ca18bfa6671116cde822a087d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:39 +0000

    thirsty thorough quiet regularly brain

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
