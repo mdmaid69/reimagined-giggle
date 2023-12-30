84667fb521349e8c5e87a23241b7a85a1d9421a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:21 +0000

    behave sure length sign he

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+for i in range(5):
+        print(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
