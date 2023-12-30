94fb34aa5717d02a03736568e0020a39e44ce0ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:52 +0000

    division dig autumn shopping duty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
