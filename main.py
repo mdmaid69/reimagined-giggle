8d28d5821a60703f52f571fd5d7d26166811732f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:01:26 +0000

    form get deliver but space

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_user_string():
+        return collections.UserString()
