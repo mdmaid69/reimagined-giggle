5ddbea1eb3114a9d4dbb190892bb9061723fc3ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:25:17 +0000

    protect alive exaggerate serve fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
