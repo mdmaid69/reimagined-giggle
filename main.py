b87fe17f7e7ccd9400d88bea8bc59e122e9b306b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:20:12 +0000

    stop picture story such sticky

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import csv
-def load_csv(filename):
+def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
