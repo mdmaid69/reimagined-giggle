248e19b82c6210f6bc9ee1344b2538be8cec562e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:41:05 +0000

    tool abroad better wide special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
