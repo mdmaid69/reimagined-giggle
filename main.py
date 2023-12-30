5374d11b9d3b6637c6d87180e260c876eaca9974
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:29 +0000

    offend like cover bury kind

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
