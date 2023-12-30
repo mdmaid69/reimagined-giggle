3a30c8241629fdee3c09c928120e8911bdc6d957
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:57:08 +0000

    chance look please present interrupt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
