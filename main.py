f2964add86fec837378f338044e1d2f18436a62f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:29:25 +0000

    neither actually with unusual comeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def multiply_numbers(x, y):
+        return x * y
+import array
+def remove_from_array(array, item):
+        array.remove(item)
