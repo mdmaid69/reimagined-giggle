daf7941306bafbd2455e1469bb2830c9ef5d1073
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:30 +0000

    runover equal include mess bringback

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
