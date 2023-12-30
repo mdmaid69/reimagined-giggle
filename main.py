24e37a2612ee83aacc9a0f4a05555b791387ba00
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:05 +0000

    value makeinto price represent before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_characters(sentence):
-        return len(sentence)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
