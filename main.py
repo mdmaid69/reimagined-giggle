ce0455dd524c792d74218b6a52129b8bb7fa57dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:43:42 +0000

    fit lead balance safe achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
