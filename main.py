3e53a23a57ea49119f6370c37bdc9f3416c00697
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:54:58 +0000

    reasonable vanish target recent fast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
