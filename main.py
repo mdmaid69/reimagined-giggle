6a343795df7f07dac4bcba1b2548987816b2aa3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:14:13 +0000

    willing lead either bother near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import time
+print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
