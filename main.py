53378abd88e5e98253a92ffba6d9a9f394fe179e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:18:49 +0000

    Ll man Ww there word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
