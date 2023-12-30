8d882c365f54a37dc09dbe9edc8ef320a30841c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:31 +0000

    closely remember opposite meaning hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_arc_cosine(x):
+        return math.acos(x)
