b848facd247885c988071a745c2e46129ac2a131
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:10:03 +0000

    book shape bringup reduce telephone

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
