afdbd7869f6c4f1fd0c626b75f7f35ba8bd994b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:16 +0000

    bother service sticky pause force

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def convert_to_binary(n):
+        return bin(n)
+def convert_to_hex(n):
+        return hex(n)
