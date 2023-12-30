98dcf66558d02ead368380d50212ef59c4b44fe7
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:52:04 +0000

    guilty attempt ill grateful common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
