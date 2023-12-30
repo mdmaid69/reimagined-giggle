ebdd85a46fb5c98a8858354f01a5681c6dc62410
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:27 +0000

    road tight bunch reason life

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def convert_to_octal(n):
-        return oct(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
