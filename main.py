d4858c2bb2c10284fe73f90aaf7e9f0ee78bded1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:44 +0000

    relationship search direct custom tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
