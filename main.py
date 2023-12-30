aed0f4618967af655ba94b3961edfb2b51f8aace
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:55:01 +0000

    moral work damage wild interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import datetime
+def get_current_date():
+        return datetime.date.today()
