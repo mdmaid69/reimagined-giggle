8966275145613b997067d4c0c42b6f6a6d8c3db7
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:49 +0000

    stretch pressure nowhere stay unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
