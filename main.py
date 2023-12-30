cfb72202ef2b9d7d2e8ae5f5a5ae85be26cee7ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:21:21 +0000

    bet talk beat morning rank

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
