fa321d42389a8c397abe3c4abf912063c3706fb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:33:56 +0000

    once holdon only sudden pin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def add_numbers(x, y):
+        return x + y
