ecfa25d22c58316eb64c3e559ecc45786456114e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:29 +0000

    special cover achieve country safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
