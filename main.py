c782518ee067a6830833651fee239c7336f976fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:24 +0000

    mess translate sign concern exciting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_combinations(n, k):
+        return math.comb(n, k)
