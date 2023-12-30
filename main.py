5f4d8f0612552a4878495d9254eb01b9aa962672
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:36:37 +0000

    disagree pump matter piece goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
