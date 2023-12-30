5c207515180c8e03fbf04083b108e94cea37b560
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:35:55 +0000

    poison regular nervous post forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
