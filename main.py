5b01c97085f3a549042734a1906216d9ca33e11f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:38 +0000

    demonstrate lend bite cheque drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
