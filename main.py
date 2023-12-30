a9bc716d6300e6bc779501575fff4a4b507316a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:11:08 +0000

    warning risk control who tired

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
