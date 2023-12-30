f6ab590ec768ad9520bbe9a4ad132d5d66cc2ba2
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:13:54 +0000

    sort winter medium difference sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
