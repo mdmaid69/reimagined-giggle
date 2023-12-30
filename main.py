f543ec4ffc9d341b22361f5afd7d2fc0a782f279
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:33:18 +0000

    spoil bright 570enjoyable brush fence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
