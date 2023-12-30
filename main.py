9e6e47a92de379f3a64927fd9253698debd23140
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:22 +0000

    attempt receive tear trial moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
