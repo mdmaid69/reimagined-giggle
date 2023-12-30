bbb06e9160a440bdd4b75a2556345cf0a7b16394
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:42 +0000

    pupil design minority unfortunate callout

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-print("Hello, world!")
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
