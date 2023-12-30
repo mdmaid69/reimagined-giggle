f2e6b37d8ba459f76a99aa6c812f88f89fb14eaa
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:53 +0000

    harm pronunciation private payment square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
