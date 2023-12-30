998bd94e08ed639e20837875dc9b1fc7c6e7435b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:47:00 +0000

    shame normal onto stare usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
