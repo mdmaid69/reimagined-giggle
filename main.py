782c489dbf6845f02c73193ed57ee1abd83e6447
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:40:20 +0000

    be line rich street same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
