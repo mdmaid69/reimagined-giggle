c17c563b3255cda4d370846d956d6fdc585acc63
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:00:29 +0000

    there smooth quantity comefrom bother

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
