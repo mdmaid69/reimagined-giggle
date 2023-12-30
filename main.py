bcf457858f46c7f8cc147d4509e9ab439839d27f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:03:08 +0000

    without contrast go meaning pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
