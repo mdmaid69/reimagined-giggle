dc655520c5f49c0b0e07c9751b94c7d0bbbc31aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:27 +0000

    temper decide develop regret breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
