29f108a3908eb28d2a0851003c4192f970d40ff7
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:52 +0000

    search love famous amount murder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_length(array):
-        return len(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
