eed03fbe4b688195bdc15b500af3790f27f7622b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:15:08 +0000

    keepoff land country touch passage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
