611d69029c228631f2c977160202e54e41341351
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:33 +0000

    major bend satisfactory invitation funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_work(force, distance):
+        return force * distance
+import sys
+def exit_program():
+        sys.exit()
