95e20dc08373263c4afc8d980a40ee3516470ad7
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:53 +0000

    tap nice touch chief offence

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
