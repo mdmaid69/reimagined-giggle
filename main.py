f7189062ce1ad8bf1493d5a9fb02b7e342efc120
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:36:54 +0000

    centimetre dry later surprised necessary

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
