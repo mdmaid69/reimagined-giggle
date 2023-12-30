687728a582710f6d6b420e7c5c9aee794123f62e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:21:34 +0000

    neither under downstairs get furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
