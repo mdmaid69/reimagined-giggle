4bf2fc6340b7c292399c21523c15078cae001a96
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:26 +0000

    mine during brother increase shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_speed(distance, time):
+        return distance / time
