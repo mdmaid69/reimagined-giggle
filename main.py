166a62d964ac6f26ed95d8de72889beeecdfef8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:04:58 +0000

    under fetch quick swimming patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
