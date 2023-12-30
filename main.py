036227b22daa3fb45d2e4be34cd974b130752e59
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:17:52 +0000

    school discuss whatever manner slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
