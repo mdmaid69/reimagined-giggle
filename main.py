bfaab00e1032b8a61169f214c540a4012d353151
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:33:13 +0000

    stand freeze sticky laugh themselves

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
