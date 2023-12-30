7b7a5a8fc8a624ad9aba3d3b00fba6e356de2de7
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:14 +0000

    region admire breakin hardly system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
