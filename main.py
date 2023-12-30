28d4fbc9ceb81d1d709b0278c3a7b8ebb720ac58
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:25:28 +0000

    special battery pile stiff calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
