9da9f43ec179eb5259cb37db7533f8508e96dfb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:13:37 +0000

    ready drink secret drop clue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
