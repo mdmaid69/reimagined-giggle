4bc47c7ac79c96cfa3cf2c316100f3fb84ab41fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:44:15 +0000

    meanwhile cruel adjective rush turndown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
