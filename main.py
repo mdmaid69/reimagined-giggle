6579212c6ff9ad139580667defc272db0f32cf5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:36 +0000

    yesterday speech circle school fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
