3b70d39bbeb48f4c2ce4b709ca05c655c7390143
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:03 +0000

    fire play road record holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
