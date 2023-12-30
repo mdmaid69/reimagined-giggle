01efc53227a57f9db1ddab2aafeb4b31d1063ce2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:39 +0000

    defeat Hh passenger settle punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
