895334e81c910e9b24c5450cbda404065a2ae177
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:47:09 +0000

    allow no clear lean embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def add_numbers(a, b):
-        return a + b
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+name = "Python"
+print("Hello,", name)
