fd1b08d314ce543a280b816b91a5a235973da5e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:13:39 +0000

    speech occasional invitation cross case

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def square_number(x):
+        return x**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
