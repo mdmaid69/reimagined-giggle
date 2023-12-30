78282a09cd21d604f02259eeaf7f84ed1bab4fa0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:20:53 +0000

    wave include relief serve bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
