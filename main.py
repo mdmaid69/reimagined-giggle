5335cf01bd2fcf8837207ee06fd321ae63faeb2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:52:37 +0000

    trade answer injury worth movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
