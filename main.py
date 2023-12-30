ffa38c6e2cf3412df7e29713f67c1e9d9a49fc07
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:21:28 +0000

    shut fairly mark typical scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
