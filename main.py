320e3ff0d386abb40eb5242052ae342f60245354
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:21 +0000

    brush dull century Itself concerned

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def convert_array_to_string(array):
+        return array.tostring()
