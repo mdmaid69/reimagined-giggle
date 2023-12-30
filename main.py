e70324b92951e7ceecc22215fedc42514c267db7
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:06 +0000

    that unkind front complete half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
