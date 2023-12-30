09eccc9d48be1d8a69b97ec431113f0792b492e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:50:28 +0000

    impatient several consist number report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_set(array):
+        return set(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
