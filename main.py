01fcb975476fee73a977d4f9eb6490654f4178f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:49:10 +0000

    promise answer description flow offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
