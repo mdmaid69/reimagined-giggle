b9316a0447b6ef52a888babf8b3371d7c776e015
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:04 +0000

    central means wave foreign born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
