a3de96527e539dc1e15989e7d4f7cd06d270d92d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:55 +0000

    favourite forward sort visit anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
