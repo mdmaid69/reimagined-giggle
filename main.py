8779e2cd053661e9e88ac6e65efc6098368e308c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:42:03 +0000

    tap feature block motion duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
