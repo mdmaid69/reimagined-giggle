2f96a13e9781e0eb023e576dfa15dbbfc56f9815
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:06:19 +0000

    doctor flight shop negative since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
