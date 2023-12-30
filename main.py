89cfe9c20e72725af6fa203d63e58b31bc9b8fba
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:06:02 +0000

    dress swimming before advanced ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
