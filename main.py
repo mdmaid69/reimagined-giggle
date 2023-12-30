ad7ebdde20d62600c70ea128954230ea6c0cf413
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:17 +0000

    Mm bound want moon show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
