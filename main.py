60c836edf2bb784fa81f848ac45bde8ae1961281
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:02:56 +0000

    power board glass limit getup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
