99177ca2e0808fc1e04c8be2fadf0cca21bfc84d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:42:49 +0000

    target at brush shade Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_length(array):
+        return len(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
