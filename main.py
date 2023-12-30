27e2d514da9dc8e3786650db00c155eef503bead
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:01 +0000

    respect speech tune tend drive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
