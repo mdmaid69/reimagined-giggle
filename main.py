a6db4d92136fe702f2333d8432db18bbb455d9e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:05:29 +0000

    Ff suit complain could breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
