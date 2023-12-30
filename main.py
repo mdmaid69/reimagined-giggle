d0d6b55450da90e7ce9c6be9f78315d5eccd2868
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:34 +0000

    tape freedom on bit hospital

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
