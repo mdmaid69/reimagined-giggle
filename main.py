b0efaf93566667617a8d039b4a0a1366ec9b1175
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:56:36 +0000

    invitation moon turn action stay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print("Hello, world!")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
