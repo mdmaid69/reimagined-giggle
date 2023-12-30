d03ab94f8c8132fb384aa53c35e0bd69f00070f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:14 +0000

    dream more community back illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
