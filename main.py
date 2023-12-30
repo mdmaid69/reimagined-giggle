119878135eb6bf3ddc835b2cb618866128bf2351
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:56:55 +0000

    direct pride sleep there comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
