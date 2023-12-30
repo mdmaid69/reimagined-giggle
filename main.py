a3acab9a2f1ea823b636abc4e008401604ec2a94
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:38 +0000

    practise getback completely soon borrow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
 def calculate_remainder(x, y):
         return math.remainder(x, y)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
