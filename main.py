e0f9047e209acfc7bbcc8a5495aef3008f45524e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:54:13 +0000

    press how contrast same ticket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
