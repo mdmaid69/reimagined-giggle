1049d8473d7fbf1ea42853eefeb77d217bd853a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:49 +0000

    sort class cause hurt relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def square_number(x):
-        return x**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_sine(x):
+        return math.sin(x)
