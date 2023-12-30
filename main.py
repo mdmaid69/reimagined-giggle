bc8808fbd4bdfcec8248a562993810b8301f179c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:48:59 +0000

    temperature trip rush talkinto quickly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
