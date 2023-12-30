7065fcb3eba328c0d80a3f2e6ad93c81d9e1d9e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:05:39 +0000

    beneath loose protest reach distance

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
