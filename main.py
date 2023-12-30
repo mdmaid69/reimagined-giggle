33bc81ad1043904d1d285f84bb2f35ac5cc50df8
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:04:01 +0000

    stone other pleasure still carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
