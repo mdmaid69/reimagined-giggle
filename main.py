4fb59a41b401088a69121f49663135ca7925c726
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:31:34 +0000

    hurry 565end long spend beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
