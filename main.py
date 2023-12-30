21dc1b79e0cd006dc6e6bce4d5f8eae7986683d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:49:45 +0000

    visit introduce foreign fly powerful

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def remove_duplicates(lst):
+        return list(set(lst))
