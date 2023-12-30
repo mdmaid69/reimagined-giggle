e1e6beb13a6666a99303a16426ce03a4fdcda345
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:15 +0000

    dull edge cause father computer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
