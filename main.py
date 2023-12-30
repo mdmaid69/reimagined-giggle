a4f8a1455e02cffeabc35dfab77bed0538719c80
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:51 +0000

    dull equal physical fight undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_str(array):
+        return str(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
