91d6d497d438c4a7e2edf8701ba3d880918a5da9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:07 +0000

    brown discussion list whatever mistake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def split_path(path):
+        return os.path.split(path)
