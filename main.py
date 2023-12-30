3f5fc9526b5d1cd1c84e73300fd839d4e8129641
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:24:29 +0000

    go include shop whenever Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
