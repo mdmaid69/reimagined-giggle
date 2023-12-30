83c6abfa4c5f09b3ae4922e7bbf608f96ad7d486
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:57 +0000

    ill rich development so somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
