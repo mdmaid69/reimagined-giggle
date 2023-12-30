ddcf2ee80260c24238e9233de7a1e581caec7458
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:52 +0000

    trade deserve Gg fair recovery

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
