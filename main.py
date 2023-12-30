1c61059f3fdae6352966b1ccf3c6148e38e66096
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:44:25 +0000

    unkind moon regional safe though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
