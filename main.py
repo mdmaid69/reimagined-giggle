0e84a9ea58a9bcd268c76b57d40e4ddebc34872c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:22:31 +0000

    hair badly dead long moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def subtract_numbers(x, y):
+        return x - y
