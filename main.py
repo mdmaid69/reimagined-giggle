73c346ebed162e1923904fd5a5027bcc2d62148d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:48:40 +0000

    into slip regard steep fence

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
