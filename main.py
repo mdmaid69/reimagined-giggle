5265bf284c976997aeb046c6e0dda9a024fea0cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:04 +0000

    equal especially exact mean advantage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+print(math.pi)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
