de542049aa6396903129362bd2f141aff7a3b972
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:05 +0000

    across distance Tt meeting swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
