2469b0b4623a788dda60ae438fc0cee0c2828d1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:00:32 +0000

    reliable collection reach everywhere shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
