f264ff8e52e226f5746eb8763f2564622aae257a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:20 +0000

    intend mean sensitive thought instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
