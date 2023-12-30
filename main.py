15bd719c693d756743df763867c0b9f0e8014e88
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:20:28 +0000

    pity wipe accept previous high

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+print("Hello, world!")
