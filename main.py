bbd19237b75a439860a54e0fbde7bf65ce9b221d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:11 +0000

    better fork busy shade glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
