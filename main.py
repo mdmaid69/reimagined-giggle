377515be1db9c12def9bf0f90a84cf17ba5ad7ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:49 +0000

    best lucky upset meeting flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_exponential(x):
+        return math.exp(x)
