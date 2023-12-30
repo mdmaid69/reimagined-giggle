636d2632c0ea8d56cebb6cbb0987631fa23a2422
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:44:02 +0000

    fight otherwise pressure importance match

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
