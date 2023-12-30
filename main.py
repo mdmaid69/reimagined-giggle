9524de6a7076f856c47635785017a6076ca1ba2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:51 +0000

    high unit coffee otherwise building

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
