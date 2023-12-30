093e5c4e5603768aa31965c92f5c6109ab81c288
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:11:16 +0000

    giveaway crime minute upset public

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
