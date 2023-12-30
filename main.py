0fd6ee69699ccff0e2e6ea91c31bc55ac9bfaca4
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:46:10 +0000

    discussion in suggest after party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
