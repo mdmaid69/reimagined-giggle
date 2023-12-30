9f591b89c3bcdc39a72d8e674dde8f4903fc38fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:55:15 +0000

    recovery block yesterday even apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
