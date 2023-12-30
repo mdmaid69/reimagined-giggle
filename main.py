fd638609549a3b9b4e70681b7bae92c18240e100
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:14:35 +0000

    emotion national male lower accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
