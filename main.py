2cc2fb9de0fe36da899c53606e2b0a704aa3d592
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:43 +0000

    emotion coffee great clear detail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_speed(distance, time):
+        return distance / time
