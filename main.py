5b1097740806324be647f199170000990a65cd48
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:53 +0000

    too would good slice temperature

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_area_rectangle(l, w):
+        return l * w
+def remove_duplicates(lst):
+        return list(set(lst))
