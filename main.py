2802e4ea769bf9c8f7aef6811bd27e4297a43d81
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:23 +0000

    similar tend news call equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import tensorflow as tf
+print(tf.__version__)
