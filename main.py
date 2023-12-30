2ae0850eb8066db66a3eaf9c49976e73f2a5d22b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:38 +0000

    makeinto difficult prefer general from

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_priority_queue():
+        return collections.deque()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
