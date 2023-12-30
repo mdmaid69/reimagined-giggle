4c78f60430cbde4ab7cc22a973d92be1d794dabf
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:53:20 +0000

    dig bringforward slice uncertain machine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
