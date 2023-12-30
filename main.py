440eca06e6fb6731c7d3169e0a75c384c249bcb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:58:57 +0000

    relative difficulty day whisper material

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
