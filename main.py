78975e4a0f9bc1d11070948a100753b2dbd0335e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:19 +0000

    aim holdup urgent table dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_index(array, item):
+        return array.index(item)
