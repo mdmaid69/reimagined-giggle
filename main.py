5cbde3be750dc9c0c45892a62c11c6fb42fb401e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:11:32 +0000

    probably dry his want decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
