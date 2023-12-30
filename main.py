eb675c322b4e93c0ab2334beae5855106c6f8dcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:14 +0000

    sell number healthy protest pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
