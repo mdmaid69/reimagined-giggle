7c1a81f6ff3f8698f70b93ba2c0695faabed078e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:46:43 +0000

    wheel takeout responsibility book keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
