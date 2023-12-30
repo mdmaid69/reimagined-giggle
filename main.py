0ca046029c9b8e3032b5d969c887ce860ab75bdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:08 +0000

    still feeling half hardly manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
