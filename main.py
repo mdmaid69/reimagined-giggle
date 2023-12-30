906203e2c1377b370884a384b6a469f221bb2e31
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:40 +0000

    takeaway equal independent neat cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
