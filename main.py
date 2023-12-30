0f7f097452a9d94c90b4bfa7ccca8e73346c26e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:18 +0000

    peaceful beside become main study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
