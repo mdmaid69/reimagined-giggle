ca422ea5e4e108a33cf0098e61b6c1799a873e23
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:37:52 +0000

    single pity trial scare shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
