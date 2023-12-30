24f452e3c1f2174f60d8a7c8d00ee0b8becc1e0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:34 +0000

    interested check address joke fashion

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
