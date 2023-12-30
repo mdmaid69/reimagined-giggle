dfea94d4f63db12b922b0790b8f64f48953987f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:26:34 +0000

    nowhere sleep room sure cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
