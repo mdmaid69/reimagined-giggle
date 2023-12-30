eeea8e8cccbdc2936fd2819bbd7bd942cbef8204
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:49:37 +0000

    narrow fine computer necessary community

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
