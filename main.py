43fd81e3e7c2e1f3cc29b730490773a1231f8e42
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:18:31 +0000

    rest follow weather sure move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_volume(length, width, height):
+        return length * width * height
