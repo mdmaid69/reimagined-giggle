ed8f854fe652240f68a418f44921ca438bd9eb1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:50:38 +0000

    interruption glad rush talkinto lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+import sys
+print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
