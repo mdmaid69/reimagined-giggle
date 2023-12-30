dc3521e5817e16e4767967fbb59ea6bb28d84b31
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:15:36 +0000

    guide tomorrow maybe formal goout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
