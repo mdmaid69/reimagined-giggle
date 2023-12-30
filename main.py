4dea501da79d2bb6c2b9082df94d0471853fa3ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:20:48 +0000

    happen report 568energy city imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def is_odd(n):
-        return n % 2 != 0
+name = "Python"
+print("Hello,", name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
