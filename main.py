66e5141a6d15979ce7ebf4f43a7e870787867f02
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:22:43 +0000

    rank herself wind at within

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
