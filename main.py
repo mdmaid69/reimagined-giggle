07284e652f28f85c6e386cf15b5f3b55cdfa7788
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:45:59 +0000

    edge along form drink apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
