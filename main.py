aa07d20ebb1fd6cec8f0698ee86f957f0fc7df65
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:35 +0000

    point shine scared than sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
