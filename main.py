1bcd0c31cbaaf39bae87830e4c0b5c2f4a046cf5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:58 +0000

    switch unusual after brown circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
