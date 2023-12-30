5013a4ea960c3c360cae2bf47e9fc124a102ab49
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:51:51 +0000

    offer love could church colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
