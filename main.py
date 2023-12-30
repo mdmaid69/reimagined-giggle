a804dd72be38ad437f6e653b556e0fc0040346d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:47:53 +0000

    broad whisper exchange serious ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
