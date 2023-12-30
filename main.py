8fad37ec6321c44855f3e7fdfbccb604dd26bf30
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:41:39 +0000

    opinion shadow care spring interest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
