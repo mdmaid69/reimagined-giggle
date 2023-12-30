ce950d30336bbe93b5c7d9bcf6795fc9b9d28a41
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:23 +0000

    violence even vision centre gas

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
