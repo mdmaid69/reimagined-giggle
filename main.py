2dd6f1e308166c45f5ed61baa7d3343332cb9a18
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:54:06 +0000

    present shock city attach worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def count_elements(lst):
+        return len(lst)
