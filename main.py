01a384bc6cf507fe8684b06108c2d2e5bfe7afaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:43:02 +0000

    edge well she owe appeal

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
