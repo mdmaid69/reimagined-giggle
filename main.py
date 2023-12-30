ffe242125612141a756ce4a40cb3fe87baf8143f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:08 +0000

    comeround goon talk long ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
