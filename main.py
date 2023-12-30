b8d6d396f6a4ee2eb51569e002eae051cd4dcaf6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:12 +0000

    nearly song everybody force plant

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
