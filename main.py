bf7b8abc736b2b1ebda1a040e5c02b37f7f60d93
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:51:05 +0000

    opinion dry sure correct middle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
