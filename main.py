2d6bac866413265a576394a99e93758bf6649d19
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:04 +0000

    steady achieve well cruelty blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
+        return x - y
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
