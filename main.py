f3b86962e9fe43057b89320f2e8e8add6b65718f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:05 +0000

    from flow left smell sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def square_number(x):
-        return x**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_user_list():
+        return collections.UserList()
