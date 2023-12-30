9e272c772a3cffde252d5bfd32cc41c928d117b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:41 +0000

    aware live professional roof tape

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
