11489d20fdb8042f86ce221e04461a2aea8927c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:51:48 +0000

    father correct breath absolutely match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
