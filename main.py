947c199597cdedaaaf00eab00b3b09ca6688aa3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:17:01 +0000

    bury nowhere someone impatient popular

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
