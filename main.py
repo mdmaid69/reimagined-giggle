09a2da2b4c5ccd6faef08cb5942d9a5e1df0d699
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:13 +0000

    pipe crisis stir safety relative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_to_hex(n):
+        return hex(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
