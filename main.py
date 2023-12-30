f2e80083987365805e3d61eddfcf71416c285b5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:20:18 +0000

    long entry away exhibition partner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-print("Hello, world!")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
