821f0e6bcc6d96b8d1ed53dcf39fea682f3f3ae8
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:40:53 +0000

    boring purpose hurt permission compare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
