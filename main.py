076c44d1963fb319db1353c83d6c20d61fc8d4c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:05 +0000

    decide as maximum wall from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
