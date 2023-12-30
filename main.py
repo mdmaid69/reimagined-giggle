5a36b7b991e1e0827dc9e01305e8f91ae5adc5c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:29:23 +0000

    far design us far official

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
