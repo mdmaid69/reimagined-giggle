89bb79a7b76128b91caa9952b369508a634500ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:39 +0000

    exercise introduce convinced must call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
