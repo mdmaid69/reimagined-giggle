2daab280a8218d57fe915dc6fb91d569306fad5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:56:54 +0000

    closely offer through pure leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def multiply_numbers(x, y):
+        return x * y
