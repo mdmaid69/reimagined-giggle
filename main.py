03c0678470ec642865e301d3d723b7a07ef5fc64
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:05:07 +0000

    physical responsible type takeback suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
