89d84e3dc9957a6eeb04829a4dd9e28cbc55cabb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:11:53 +0000

    finish smart to yet turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
