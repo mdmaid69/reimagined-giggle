e3e9c0af7354d690e7edbb4e01c016ab506542d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:38 +0000

    onto compare climb motion proper

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
