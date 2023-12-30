70fdf070fb0bca8c84f16d5cbb3058291fa98902
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:34:15 +0000

    make pump phone ask scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_stack():
+        return collections.deque()
