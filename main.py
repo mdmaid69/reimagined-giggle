74583b0da8ffb9c4e901e8e14eb227192e87b249
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:34:32 +0000

    lay stretch thinkof fill slice

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
