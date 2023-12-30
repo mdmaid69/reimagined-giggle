3d25509affceab4701b73593eeab5d7745d80ceb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:45:26 +0000

    correct Ii measure regular arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
