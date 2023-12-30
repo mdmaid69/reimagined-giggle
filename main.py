312263090d5dfe88a393da39256a9aa7e9824b05
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:46 +0000

    count loan better sink cum

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import platform
+def get_os_info():
+        return platform.uname()
