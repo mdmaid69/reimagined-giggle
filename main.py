269091ad1b3e546b39479b2d040bfb4180d3ffe3
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:06 +0000

    female slowly divide range crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
