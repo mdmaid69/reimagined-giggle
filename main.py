06a293d2718b2d8811bc5486f8a7f350f40bc1ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:14 +0000

    smile lay section standard none

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
