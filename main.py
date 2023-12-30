54e21c9a67c550256834f5bb9f82ca042f57db1d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:52:10 +0000

    before dead nose organization occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def sort_list(lst):
-        return sorted(lst)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
