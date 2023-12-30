843bfeb8f750eb69a63d9e863f3fd263246e8e65
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:16:22 +0000

    complete congratulate comeout union fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
