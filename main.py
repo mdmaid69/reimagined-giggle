45bd732b256c30c7674c8b7b06ea30801b0e9c15
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:18 +0000

    wide turnon destroy value hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
