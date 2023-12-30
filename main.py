ebbfbb4bc24c8b4caeae0a83390df2bc7b861c2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:30:19 +0000

    left keep active wide thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_factorial(n):
+        return math.factorial(n)
