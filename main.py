ea46e58aa559fcca1bfbed0749198a132a990075
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:14:50 +0000

    soft every discovery wind interview

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
