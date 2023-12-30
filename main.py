bd0cef267c5aff07779510f83409e33c25d769d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:15:00 +0000

    start Yy please window dream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_speed(distance, time):
+        return distance / time
