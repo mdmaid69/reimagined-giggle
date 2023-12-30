a953f45f1977d02c3872ad079a0ecaa988bc9a03
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:18 +0000

    boat flight with correct fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
