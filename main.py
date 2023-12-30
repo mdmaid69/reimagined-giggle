b7ddc8adef9cd3f5d14124e4720a6c441fe0423f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:36:45 +0000

    freedom sell pure block block

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
