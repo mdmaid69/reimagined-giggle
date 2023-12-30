247e2d98dae1f8eb16070bd71a1a841982d7d769
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:45 +0000

    usual Vv goout watch please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
