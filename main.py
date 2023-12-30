23c56b6b5b3d2549b907e5c517ed2805bf5879e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:23 +0000

    closed remain pride holdout close

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
