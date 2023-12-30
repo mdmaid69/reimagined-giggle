63feb2a1cb30a49dff0ccf636ea3f8ca3ac8adc9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:03:28 +0000

    real love live manner tape

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
