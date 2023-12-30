6a39da1535ecdfe269977e8ce6495c522abb1925
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:29 +0000

    since judge trial environment catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
