f9a7de3d7c36fc76ec93c44468a7d183855e8cb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:00 +0000

    less band burst person generous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
