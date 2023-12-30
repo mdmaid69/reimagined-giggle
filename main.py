7d2562c829d7ed5435ff11b4b8bcd6de6caaf192
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:32 +0000

    name pattern gas change answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
