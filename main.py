8133ae3e1bf2961b3b1fd5e506e8ca50bc383efa
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:26:03 +0000

    quality important develop excellent away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
