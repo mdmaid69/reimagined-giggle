fbce5bb4626e7efe6ef18ffde4a31e6367ffb855
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:39 +0000

    satisfactory cold almost speak post

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
