6602639f7a4ab72eb40a5fcdae7803d4fde4a34d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:03:19 +0000

    plant fix imagine generally slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
