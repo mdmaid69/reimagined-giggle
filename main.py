68a42f9ea8e3780f756400853e92fce97dcf3c3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:29 +0000

    during mixture best motion keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
