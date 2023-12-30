be23abde77261c80a2c1461a997e7318ea9b33b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:06:45 +0000

    emphasis sorry world replace nearly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def convert_to_hex(n):
+        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
