4dd59754f3f9a49622b99000a736cb508d91a18b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:58:05 +0000

    shut breakin 564encourage must exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
