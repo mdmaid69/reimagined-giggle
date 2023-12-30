b8bec5938afbc53132a46c1a23682ad221351272
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:36 +0000

    intend rid radio machine not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_as_float(array):
+        return float(array[0])
