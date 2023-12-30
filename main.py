2c3582c5118704db5d23e0e1f603996b93deab7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:24:19 +0000

    track interfere earth day print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
