99226a7c80c9a2f89cd73caa073742f8e0b721fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:16 +0000

    together object mood grow communication

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print(sum(range(10)))
