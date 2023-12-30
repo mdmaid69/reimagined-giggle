d4cea392be293bb87d5a4f942c5912463335ee37
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:44:29 +0000

    uniform lie arrival add friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
