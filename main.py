dea83cc652a3337cb995d8da2a9c442f6c74a986
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:17:08 +0000

    foot water point bottom odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def count_elements(lst):
+        return len(lst)
