8b932e2e93566b98a7ceaedcabd3b82be21babe9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:39 +0000

    please Dd class 572enter regional

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def convert_to_hex(n):
+        return hex(n)
+print("Hello, world!")
