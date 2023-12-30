8df5a177227b3ed0ba066affca9541f7d43da929
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:45 +0000

    calloff force also friendly Hh

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def convert_to_octal(n):
+        return oct(n)
