0b8f7532dcc53e35560f775f00ff2fdeec1ff9d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:30:09 +0000

    Bb almost tap autumn rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import platform
+def get_python_version():
+        return platform.python_version()
