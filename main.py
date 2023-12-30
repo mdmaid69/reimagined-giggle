05aeeb119aaff94bd539156292b0add9c661afce
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:37 +0000

    song narrow effect holdout government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import platform
+def get_os_info():
+        return platform.uname()
