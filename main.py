a373d6a3f4a924287efcec2b1f81e709f11eb1a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:18 +0000

    urgent somewhere flood considering body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
