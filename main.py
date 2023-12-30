cd8f587ffb198cf9be481f3687c41a1bd76088d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:04:37 +0000

    taste sudden ear fly fresh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
