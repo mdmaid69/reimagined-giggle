29bb0c5bf45ee97199a055661c36f18f9edea23d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:27:30 +0000

    scream sleep sure tonight relation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
