9a31a9fabc584a8c823e5c769e86240453c21488
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:17:48 +0000

    plain move deep to lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
