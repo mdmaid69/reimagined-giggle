b68701fbb64e9050ad60ba76d953c946b1f57480
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:53:30 +0000

    also sound see entry distance

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
