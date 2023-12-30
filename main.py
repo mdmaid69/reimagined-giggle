3109a30f12e899f4332b5f9528d211d7c8ec956b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:01:05 +0000

    cheat and anything really rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def add_numbers(x, y):
+        return x + y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
