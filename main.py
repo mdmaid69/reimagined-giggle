489faf7c860fa05301a4760833aa2348bf54d0c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:12 +0000

    combination afterwards talkinto steer finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
