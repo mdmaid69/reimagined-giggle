b230053bf20fdc64d1f19f2dc7aa41e149bc058d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:07 +0000

    give flavour forward Gg account

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def get_array_length(array):
-        return len(array)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def get_array_as_bytes(array):
+        return bytes(array)
