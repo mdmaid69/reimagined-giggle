e5ba48aaab811042af6d754d19078af2f6cb1d93
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:39:24 +0000

    careful tense agree objection direction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import os
-print(os.getcwd())
+print("Hello, world!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
