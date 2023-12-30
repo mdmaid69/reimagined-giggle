c0ef8854f1e9a1a5a0c9b1e3f60b3a04c69a68c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:09 +0000

    ticket agreement name occasion rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
