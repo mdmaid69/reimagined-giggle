efb1f97b43dd570994e60dc3451dc2e401c9af14
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:07:12 +0000

    coffee whichever pump quarter corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
