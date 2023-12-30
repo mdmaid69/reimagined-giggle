f8130c604804b4f90e5293f2e126ff1d9579e024
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:18:18 +0000

    tip lucky breakin mistake now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
