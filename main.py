60746cdabb274c9d307f125cc3c0e72ee4497f9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:55:33 +0000

    cap aim immediate ball habit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
