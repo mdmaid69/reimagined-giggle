130251691b5a91146e88fdef4c00a7fbd9471dc7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:37:36 +0000

    then exchange partner importance give

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
