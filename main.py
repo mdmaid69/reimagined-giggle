6584d5d52a0dc3fa3b3aa30afd88df851c4e423f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:17:08 +0000

    pile myself odd pretend welcome

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
