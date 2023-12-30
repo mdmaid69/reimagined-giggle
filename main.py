8c94d7ad3d06c9b4f66ff45dcdc8035ef23e3554
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:05:03 +0000

    noisy tidy loose place plan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
