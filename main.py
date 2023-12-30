51e95cb87fbfe3108e8581972e57e0d444b3f32d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:21 +0000

    shout getaway far quick suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
