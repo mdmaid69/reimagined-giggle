702fe006ee283216a9bc169d70b8e5356841bfe5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:21 +0000

    could cook weekend goodbye smile

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import platform
+def get_python_version():
+        return platform.python_version()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
