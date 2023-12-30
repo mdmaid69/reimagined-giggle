92ff71fa11d01ed3f99cced62207143bc129e0f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:04:16 +0000

    sight list miss time tonight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
