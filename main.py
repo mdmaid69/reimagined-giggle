fed0e9ab1fc496b502907341f8bad39231a3e418
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:12:14 +0000

    goon artificial road getout concentrate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
