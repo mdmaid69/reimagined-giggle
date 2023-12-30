1726323ee8a69231491bb38019c8fb6f29636616
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:19:36 +0000

    mistake bottom lunch confident drive

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def clear_array(array):
-        array *= 0
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+for i in range(10): print(i)
