27b46250dbecb310c255e1b0464a3ad5a6e15f54
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:47:56 +0000

    convince brush please active behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_bool(array):
+        return bool(array)
