fc55fd951b5043923953cd5384424741447abea0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:24:30 +0000

    active attractive children task equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
