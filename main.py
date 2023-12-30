7e568b88a5e8d0680b5a9bbdba595f884db475e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:29:36 +0000

    impossible discuss live criticize national

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
