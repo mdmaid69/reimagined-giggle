9ffd2180f86eadf954538465014c76daa45ad582
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:57 +0000

    attach mixture unusual breakin recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import random
+def roll_die():
+        return random.randint(1, 6)
