6dc484508aaca51be1708ed7256b6937ebcea212
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:55:04 +0000

    but squash hear high tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
