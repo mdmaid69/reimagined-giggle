98a0025f9021c43740bc24ef4e76eded046ed37e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:22:31 +0000

    near worth often control ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
