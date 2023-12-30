2405601bbfebd551b2e247fdb42f245b2d2ecfba
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:29 +0000

    train such giveback afford apparently

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
