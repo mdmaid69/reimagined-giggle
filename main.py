e5f654c35882616c7221fba0d5684009f6e76490
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:12:19 +0000

    minute enthusiastic their drawing clean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
