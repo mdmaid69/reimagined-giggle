0d1a2068a9b82457a8b993d405a896b316ec2b2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:52 +0000

    keepaway heavy education bridge autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
