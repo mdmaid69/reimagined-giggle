6e48a2b2a964d40c242287a0e5ed07bbd474fa4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:12:16 +0000

    fork hair invite idea tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
