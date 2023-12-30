f8d2a5e56c6a446aa05778487bda3f7b324d66d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:15 +0000

    charge lot desperate damp meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
