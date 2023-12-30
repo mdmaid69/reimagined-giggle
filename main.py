0f4f5f8cb8f672b8198bd71a8ae2d515944fb948
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:57:15 +0000

    waste exchange wave Hh board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
