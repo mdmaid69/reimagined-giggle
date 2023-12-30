2315f76eb0b546f36c0c3cea5642fae2c0521b8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:34:03 +0000

    excuse that complicated confused hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
