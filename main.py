a26a0d0be46f5b4f581b0d55b143e076b24c2219
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:35:36 +0000

    town education shade one cold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
