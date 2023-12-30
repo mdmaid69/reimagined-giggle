4591160855c923ae0ef46bc62d3b556a90df03c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:39:30 +0000

    soil scream many peaceful appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
