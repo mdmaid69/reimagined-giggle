d3090635a6787fd34880f4eb1eaaa4569d0b4c83
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:39:07 +0000

    peace except female second heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
