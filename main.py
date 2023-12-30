b24c46f72b5c060b6bb87bfdb60a51a7a936e362
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:39 +0000

    evidence permission consider wild estimate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
