ac532c754cd85f908f510ba47667bc3c7d2f33ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:14:50 +0000

    rob complicated level plate price

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
