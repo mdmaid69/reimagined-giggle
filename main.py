2f3bcdd4321054b897f053235fbb809665750157
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:01:04 +0000

    similar division harm special under

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def greet(name):
+        print(f"Hello, {name}!")
