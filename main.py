ea82bb2fe19465a10f6b66e9e28432301b4ee0f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:22:12 +0000

    convince wall ought constantly respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
