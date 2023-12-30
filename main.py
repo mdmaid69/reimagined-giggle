79f94ed517b00fbd20deb0813302f3d4d76973af
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:27 +0000

    union suggest clean give closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
