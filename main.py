9a3c6bc6a39cd5528c2a156165491c859edcc4b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:58 +0000

    tour thankyou suit takeout help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_min(lst):
+        return min(lst)
