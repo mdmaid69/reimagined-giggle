4e67a79a50beccc5b628ecf7a3120b53dd2263b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:25:30 +0000

    order comeoff excuse duty niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def is_palindrome(s):
+        return s == s[::-1]
