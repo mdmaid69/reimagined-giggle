01454855300a6d73b0032ec99bc8afaeaa479906
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:50:46 +0000

    on rank comein city sort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
