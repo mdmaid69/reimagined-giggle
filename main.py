f0195101d9c2cebb20369ddc2b377ff07750469f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:37:12 +0000

    develop about here settle listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
