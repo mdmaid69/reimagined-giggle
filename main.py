cb10767496ec1532e3c72c05f0950c78b32c2f35
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:25 +0000

    scream how someone delicate better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
