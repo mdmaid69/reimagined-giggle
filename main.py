8e33146ea11732915ef86779bb279982af66a978
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:30 +0000

    moral spoil fact spoil walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
