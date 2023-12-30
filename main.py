6a16d6f69dea701ed1cd9a96f4b89ecb9887c264
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:02:16 +0000

    day over fact swim brain

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
