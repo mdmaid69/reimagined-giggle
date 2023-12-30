228b57e6f68b066ce0060d2627c13088a0550fdf
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:15 +0000

    inside every interview section bet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
