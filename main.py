c783163c324bb649cc8228d3ec7db8a548b40abd
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:36:16 +0000

    recognize suckme Ff praise shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
