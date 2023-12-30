bdac9b10ea916c2c1c9a9416a25fc1115f5c7155
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:17 +0000

    collect lesson life married we

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def is_even(n):
+        return n % 2 == 0
