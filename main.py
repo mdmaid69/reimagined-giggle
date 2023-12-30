feea723e3443e0351435a92c2d4303bd4ae10abc
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:36:14 +0000

    behind rescue artificial night pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-from collections import Counter
-print(Counter("hello world"))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
