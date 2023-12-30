2e6c7b9953c268bb35ae7da9fd132bfa6bbabae5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:20:14 +0000

    surprise completely simple winter related

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
