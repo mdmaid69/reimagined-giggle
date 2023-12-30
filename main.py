552d32e6160b149e56a5f8705ed3278bfeab7178
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:11 +0000

    measure go track inside steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import collections
+def create_stack():
+        return collections.deque()
