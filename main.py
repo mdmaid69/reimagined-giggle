dfeb73b50589fe6a85a0a3d9f73622a22c2a6130
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:42:15 +0000

    left comment forward wing behind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
