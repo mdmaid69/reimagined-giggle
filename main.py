653ac6bcc4c2dbea9d14e1ab80d06b174759fa48
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:44 +0000

    hurt unfair smell port tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
