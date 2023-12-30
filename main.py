b0f4812c454bbaf69dad729b4e9cbfda623f796a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:22 +0000

    move plastic knock fit bread

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
