46b996256ada9abf07bf321901764f4005513107
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:35 +0000

    difficulty sometimes perhaps train correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+x = 10
+y = 20
+print("Sum:", x + y)
