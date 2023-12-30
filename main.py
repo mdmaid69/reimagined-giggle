6e207a6967d5a65a4c734aea60ec942b5823b187
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:59:55 +0000

    thinkthrough badly talk which swimming

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
