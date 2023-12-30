5abfb126ce7ed30d393371a8502d425e385e523f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:01:35 +0000

    wheel rarely book beyond learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_max(lst):
+        return max(lst)
