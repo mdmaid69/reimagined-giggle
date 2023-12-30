b5394443c4e570535afa8aa4ccf3fbee32a81799
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:36:53 +0000

    finish dress lookup cheap earn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
