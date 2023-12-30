6ffa80bdcfb25fb1f079aed9b8704ca25ae74ce2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:31:37 +0000

    test upstairs case birthday so

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
