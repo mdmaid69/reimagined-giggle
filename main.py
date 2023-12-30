463796eb94d4f8a54ee12e88e5b15e5029404cea
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:54 +0000

    makeup arrive sit situation possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
