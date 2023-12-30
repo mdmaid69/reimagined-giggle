259b203794e0c9e71f940b0cc4a388d29e77a545
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:08 +0000

    yesterday mark open respect today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
