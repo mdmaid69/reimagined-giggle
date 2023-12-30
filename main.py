5f7954a3de41075cbe16e38d190e2945b9a1e13f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:24 +0000

    expensive across write century estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
