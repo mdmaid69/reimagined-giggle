82768fc80be064be659d9a2eca2c78f5854370fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:32 +0000

    height answer accident bottom fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
