d2c3f97923686de2c39339f30d416f6cd3f3d865
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:29:32 +0000

    round pure value die knowledge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-print(os.getcwd())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
