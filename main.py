3333d5d9429f9eb4fb9bc53b67010dc459e899f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:13 +0000

    neither with answer practise giveaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def append_to_array(array, item):
+        array.append(item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
