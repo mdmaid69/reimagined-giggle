46f5b6750d7936e817fe83f6dfb84e3afb116ee1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:20:10 +0000

    port could hair cross small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
