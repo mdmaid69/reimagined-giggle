8e4b0845439568777c7c8512f20b3f3dddcad71b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:23:19 +0000

    later mass flood steam blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def subtract_numbers(x, y):
-        return x - y
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
