e9964d8c3a485abb9e2f0b1a0674c394147ee544
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:31:28 +0000

    apart cheque equipment tour near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
