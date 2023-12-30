2941330c640bf4eee6e561488738ac61fb947e5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:28 +0000

    precise produce tax promise hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+from collections import Counter
+print(Counter("hello world"))
