e465ff0e2486854a4fc8f45ff0b54eb6701786ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:27 +0000

    after before mend lonely sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def convert_to_octal(n):
-        return oct(n)
+def reverse_list(lst):
+        return lst[::-1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
