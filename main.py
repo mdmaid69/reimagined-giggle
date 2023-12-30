f49c6dde83b80445f07efa4c903e19fd5e8390a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:33 +0000

    impatient fly conduct difficulty equal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
