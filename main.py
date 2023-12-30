41ea62c3aca7e04ea75e9662b5bbed15a53a0148
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:17:06 +0000

    dare union flight disturb holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
