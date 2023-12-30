25313e13bf4dba326625bbd694325a3f51a3dc3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:24:39 +0000

    side unusual dish bringforward praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
