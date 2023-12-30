a1298b22f5d5d9b72fc1d2d3f1d610f2797199e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:49:56 +0000

    over callup common corner sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
