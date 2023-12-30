fd7d6d4af7ed4afeee8b87ad423376b3e7f87859
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:00:31 +0000

    pin tool judge provide advantage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
