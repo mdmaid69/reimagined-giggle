9412256fbe81b822aafa924b3781980a1822de98
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:26:42 +0000

    prize silent gift bush stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
