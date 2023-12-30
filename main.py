69294b190dc34ae2b17bf725ca0c456baa81825b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:34:23 +0000

    view bring everywhere false tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_list_from_array(array):
+        return array.tolist()
