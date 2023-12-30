933865b46aac70da5878fe953dc41ae86ffe9ce2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:21:16 +0000

    bit rich soil hope crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
