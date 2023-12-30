dcabbd9c2b9c481e3907e9b99e14d7110f3aa7bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:16 +0000

    comfort backwards second interfere lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
