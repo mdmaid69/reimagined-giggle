50c7163ca7c04d8c8f7e1088f2f051364dc87a40
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:33 +0000

    our sink attempt behaviour decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
