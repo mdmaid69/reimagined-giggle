d36e36e98013476c596cdb7b6ac744ce085e4ab3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:33:55 +0000

    clear past set pretty identify

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
