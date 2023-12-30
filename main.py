7ca8200b1fcfdd527ae31245a9e44b26f3e71e59
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:35:15 +0000

    yet clue style spare unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import sys
-  def get_python_version():
-        return sys.version
+def get_array_as_repr(array):
+        return repr(array)
