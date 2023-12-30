d3cbee75ec39475cbb933d35d4caad07a95cb7d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:44:52 +0000

    care actually prefer short check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
