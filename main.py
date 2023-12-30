1e86cf0325d8d7c19d10aa940af2180007f0c3ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:43:13 +0000

    contents comment turnon suck world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
