aa1f753ac2c14f920a0491ddca929b45bd012181
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:20:01 +0000

    goup beneath fault doctor bank

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
