eeeb4f8d61532d7065a325498d4060151ef0b42b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:53:13 +0000

    occasional few dead test prefer

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
