5dda5d44866138b053f1df53e601c14ea9eaf1bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:16 +0000

    opponent whisper march church fire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
