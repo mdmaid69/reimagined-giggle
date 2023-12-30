4b4fc6226d57a4d0b43e0c00500c0c756b3ca316
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:13:20 +0000

    guess anxious reasonable partner type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
