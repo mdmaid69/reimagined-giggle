ab5e03c432071b4a195d173ca93446b2fdccc4b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:51:18 +0000

    ourselves well father about alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
