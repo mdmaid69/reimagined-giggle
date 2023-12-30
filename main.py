c0a1c7dc03f6649f3255d5020a4d6dbfdc72a60a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:58:35 +0000

    careless spill shout possible save

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
