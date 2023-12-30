704f8dd71d9a6deef75296b64e93a508c19fe382
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:10:45 +0000

    rise workout unpleasant star smoke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+for i in range(5):
+        print(i)
