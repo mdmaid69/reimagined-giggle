5f696d17e1a7ad4bb2cf1f102257b076c26ab957
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:55:56 +0000

    related favourite only communicate invitation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
