443f8fbe761d26a526baa0fa96b2fb5cfa740810
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:01 +0000

    future meal clue alone positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
