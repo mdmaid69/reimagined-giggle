f5bf80cb2ef4015889e57699a88eaa6e45350fcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:59 +0000

    wake getoff mean window face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
