90f18f44aa4ba380f1f83f0f3ed6bc45183161a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:45 +0000

    possession blind score skill what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
