3180cecc435609540c11c6424fc3286c2d5a3890
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:25:53 +0000

    hurry strong bag heavy principle

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
