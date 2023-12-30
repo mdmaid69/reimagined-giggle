5f590d452c6ac12c8ff575472f18be4d2d1c02e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:02:06 +0000

    bath broken see rank city

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
