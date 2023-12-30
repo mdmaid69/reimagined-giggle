e69a3917e935de4b9c72cf41e93e68757d13da57
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:03:55 +0000

    injury sister no OKorokay everywhere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_as_float(array):
+        return float(array[0])
+print(sum(range(10)))
