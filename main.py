eee7ba1fbdec9126cd7e7a7b9a66b374da5d6387
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:19:35 +0000

    face detail lookup better destroy

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
