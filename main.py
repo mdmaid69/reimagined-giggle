48e7983021b51892e0ec6bc0b8f4b351011ae0e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:35 +0000

    exact unfair exit plain round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def square_number(x):
+        return x**2
