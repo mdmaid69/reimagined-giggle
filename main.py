3d115aad7beb9474f80582126e063cd81e5b10e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:30:28 +0000

    back eat disturbing pity noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
