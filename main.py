98787addc17c28c96f5b95faab87eda83f99a972
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:50 +0000

    rich cross travel fall knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def square_number(x):
+        return x**2
