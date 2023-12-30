512754ad441c6993ccba9b1e0aad1ae416bdedf9
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:29:43 +0000

    treatment cum last respect full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
