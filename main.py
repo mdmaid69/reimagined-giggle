d55354f78524f03dbe60a20f3b347cac8f42ef34
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:57:10 +0000

    squeeze quarter muscle but rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_unique_words(sentence):
-        return set(sentence.split())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
