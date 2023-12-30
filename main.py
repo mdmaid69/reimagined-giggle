8b249bfff54f1ce60da547c96a4f422c022ca05d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:52:10 +0000

    complaint wave me lean sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_item_count(array, item):
+        return array.count(item)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
