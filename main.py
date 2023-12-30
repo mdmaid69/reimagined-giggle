429dad5cd83ee7e6f8914be6f1e8c72fdeb60bc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:24:20 +0000

    runover close compare door fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
