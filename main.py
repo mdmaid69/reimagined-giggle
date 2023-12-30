fa8eadb30e196cf22bf2ff29d74c3fd8412159d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:05 +0000

    refer cruel accept opponent duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_max(lst):
+        return max(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
