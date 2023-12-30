5db1b15fcd0404f1893990617f7ec69755d11175
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:35:28 +0000

    phone need noone issue problem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
