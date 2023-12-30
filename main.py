500b0d7b5c403447ae3e8cd473a82509726a6852
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:25:19 +0000

    sister throw delicate position medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_work(force, distance):
-        return force * distance
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
