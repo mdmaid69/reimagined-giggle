b9a5199277fb03b7833e30899b3b3d64fcf647df
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:15 +0000

    open fault key note constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
