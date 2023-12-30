57e7f62512cb4833d07579f0fe451d9397722d28
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:35 +0000

    value shadow first here bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def change_working_directory(path):
+        os.chdir(path)
