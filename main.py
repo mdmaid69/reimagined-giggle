9086b6a450ec1f1be302e5897b3c2fa529e36492
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:36:45 +0000

    to trouble patience impatient view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def count_words(sentence):
+        return len(sentence.split())
