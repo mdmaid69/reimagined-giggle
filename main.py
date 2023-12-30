afce8d4ccfd61893c2d85c93bc1636c3ab240ae7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:18 +0000

    simple match 573entertainment slowly trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
