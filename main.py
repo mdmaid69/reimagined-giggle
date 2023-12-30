99712fb2e514f1428969407a7fb72fb622db9c64
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:50 +0000

    people sell lost tend human

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def subtract_numbers(x, y):
-        return x - y
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
