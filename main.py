6f87e6816331f6ad9810a86824f876da1edd235d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:33:50 +0000

    direct increase world risk to

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
   import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
