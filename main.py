695fcc2d15746664131db5e48ad75ebefb95b19c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:18 +0000

    dry stick mistake tie image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
