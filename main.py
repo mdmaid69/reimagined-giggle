862c2b739150a019ddf584f3bd0c1e2e63b433cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:26:10 +0000

    practice this minimum man myself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import logging
+def log_message(message):
+        logging.info(message)
