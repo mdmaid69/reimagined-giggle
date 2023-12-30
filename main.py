4bd1759ab17fbdbd78abd1f61253cb8e67aec294
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:44 +0000

    preparation emergency estimate uncomfortable pinch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
