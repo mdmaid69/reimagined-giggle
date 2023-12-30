54f0aff548606c4b7cadeebfd76c727e9e2a7fad
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:10 +0000

    know survive collection ocean either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
