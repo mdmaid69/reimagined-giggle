0c886414f95f9391918e905dfd1c9504f69e85a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:52 +0000

    principle prevent programme reply remain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+def find_unique_words(sentence):
+        return set(sentence.split())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
