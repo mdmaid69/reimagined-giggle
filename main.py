0e4314e46508cf667d13fcbb975c33deb9a703ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:55:17 +0000

    explain music search burn pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
