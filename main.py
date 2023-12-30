be4f43d1fdd88227caa501fe7126998ec8d1c23e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:04 +0000

    fail actual cry Qq whistle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
