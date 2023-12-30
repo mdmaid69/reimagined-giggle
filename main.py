fccb8deee1363623045c430ebe5df2c51bc5c115
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:00:41 +0000

    pin far sing bye suck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
