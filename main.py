d2a86ea9b4fe2c3d6343b37b35ad811e381ca3d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:49:15 +0000

    chat square completely limit practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
