cb73b709d6216557ae87e3e10b2c0ce0af6341ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:02 +0000

    takeout 565end now length share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
