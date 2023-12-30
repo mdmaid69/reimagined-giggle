3d1e52c1ff8bbe4c62a94aa2d3a426cce751bc97
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:08:04 +0000

    will on move eager pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
