88a0fd71212d0b232b653042136b5613095a6d3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:21 +0000

    along pay distant on answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
