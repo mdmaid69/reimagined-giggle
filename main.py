c7a374fe116713be277dfb4c74f1ca7ee8feea00
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:18:20 +0000

    own means manner basis shrink

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
