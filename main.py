b2ea874031228147ebb93ca1cf10807126fbb3d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:36:52 +0000

    callat talkback consult mixture prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+def find_max(lst):
+        return max(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
