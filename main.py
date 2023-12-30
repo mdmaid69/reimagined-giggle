02fbdf913d74f59dc24d3d69b29ec7387a989645
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:41:22 +0000

    entrance public noisy mail confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
