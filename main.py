5aa38cc7a5c2fd20bdb70a7010feeeb6b9a5f498
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:47:32 +0000

    she slow shake truth silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
