6b452749170da1c37c567c1d14dac130689e159f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:02:00 +0000

    fix responsibility low serious exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
