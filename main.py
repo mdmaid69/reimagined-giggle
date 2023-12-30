349dcc74bf800cc808a8497d9fb5f953fb36302f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:12:41 +0000

    kick 571enough unlikely personal gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
