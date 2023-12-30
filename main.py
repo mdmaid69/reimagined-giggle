f2333f6c1319ea7647cc78086118dbe3c341f1e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:43 +0000

    steady on authority related Dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
