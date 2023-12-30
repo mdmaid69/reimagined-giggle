c411faab04d1b6ffa8148f9c2c524fb46596883c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:34 +0000

    introduction centre issue learn yesterday

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
