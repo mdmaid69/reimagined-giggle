fbfed61c9a3af6b23d92182f20de72794e1361bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:34:18 +0000

    reason juice solve effort quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import random
+print(random.randint(0, 100))
+import sys
+def exit_program():
+        sys.exit()
