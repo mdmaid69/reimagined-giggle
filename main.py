9ae0349d183725c77ce8a46a47d1d01e18520c3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:22:09 +0000

    subject guess threaten sale collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
