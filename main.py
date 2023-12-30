f33e87240bc6bf5a60ff1e384cb0890fb8e664eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:22 +0000

    spite human proud raw switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
