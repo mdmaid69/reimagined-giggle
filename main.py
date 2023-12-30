fa02694e71d23b41be8bc7ec739ec54b72b9a208
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:35:55 +0000

    neat related teach important design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def convert_to_octal(n):
+        return oct(n)
