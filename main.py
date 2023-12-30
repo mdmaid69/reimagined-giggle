a5d8ba09704ecbed75545eac2d613055ce4449a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:45:17 +0000

    too favourite share low giveup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+def get_current_time():
+        return time.time()
+import platform
+def get_python_version():
+        return platform.python_version()
