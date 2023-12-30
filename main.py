af26012f15605197673e3a4578e009c81ed8051f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:45 +0000

    uniform weekend trip talkinto half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
