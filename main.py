f1cfbc1b1ba95ad21dcc0f0e1a2fbd49bd404c0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:05:54 +0000

    description degree live achieve appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
