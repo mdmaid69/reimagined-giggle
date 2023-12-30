391d219d37e42de7dbe4546e7f41baf377c57f88
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:37 +0000

    morning trip pen invite woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
