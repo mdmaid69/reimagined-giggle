4214aeec018d119249d4252ce6ca1124e502d68c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:39:56 +0000

    stand now recent step spread

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
