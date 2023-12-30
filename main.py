89b9bdcc9a9a520846be1070e8897d6dc34b3a12
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:39 +0000

    release 564encourage refer criticize exam

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_floor(x):
+        return math.floor(x)
