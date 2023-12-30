a3cdf8058ba70aa70eacf1e4d524c0d64a89dc29
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:06:17 +0000

    loss already compete group use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
