d876e9a9be9fcb5940ad4540cac0c7fd7fe33382
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:30 +0000

    turnout land spill force intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+for i in range(5):
+        print(i)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
