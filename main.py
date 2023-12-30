dd0d5800f9767f823d1afbd87255abd0e6046a9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:47:59 +0000

    pretend stare clear ought floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
