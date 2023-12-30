296859a357332e5e0ee0a66c7e7925ff989beff2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:36:46 +0000

    list instead before prepare dear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
