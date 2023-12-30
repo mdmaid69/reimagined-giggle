ac511fd5a4039b6277a76246dbb9ccdc682f662b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:08:18 +0000

    geton touch present down marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
