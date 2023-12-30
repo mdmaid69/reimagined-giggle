8ffc41f1de97d30307b8dac06f3af6d34f4bb535
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:22 +0000

    early mysterious remain shrink bang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(x, y):
+        return x + y
+  def reverse_list(lst):
+        return lst[::-1]
