cffed023f321e76f6ba180bfa463bfeaea49788d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:22:36 +0000

    upwards busy tend wander react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
