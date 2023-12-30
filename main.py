ed08414dfd77a2046e9c569c6488ad7117301e32
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:04 +0000

    comeover under open maybe judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import sys
+  def get_python_version():
+        return sys.version
+x = 10
+y = 20
+print("Sum:", x + y)
