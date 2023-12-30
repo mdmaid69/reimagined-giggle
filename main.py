97233cc2ccfc6c842320d565960a2698a30cd630
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:44 +0000

    union already morning crowded wait

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
+print(sum(range(10)))
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_list_from_array(array):
+        return array.tolist()
