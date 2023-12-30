4afac28da0d7e7ded0a4ecd2db90fddd39fa76a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:21:06 +0000

    maintain show forget break first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
