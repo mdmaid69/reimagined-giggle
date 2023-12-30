0e7e577f5e0d61840330b302bef3d8063fdc7503
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:25:14 +0000

    threaten handle reply out crime

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import sys
+def print_python_version():
+        print(sys.version)
+def convert_to_octal(n):
+        return oct(n)
