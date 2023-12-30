7b7a6991054f66827f38e5d250d2444757c31517
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:17 +0000

    prefer 571enough lead tight finally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_typecode(array):
+        return array.typecode
