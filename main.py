1eb4bd2466a5db6a80e2ea2d56c34040d4d3ed68
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:21:07 +0000

    relax unlucky attractive bottom brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_max(lst):
-        return max(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
