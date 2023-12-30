7c4005e47cb9991f7b45a9bc86abd6c22841fa76
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:35:29 +0000

    move schedule through collect stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
