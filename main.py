ba47fac4001df5d7d9f17d1c160a16d75397c265
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:18:14 +0000

    study level near grow really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
