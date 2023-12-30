32a6ef703b27bd8b12ffe56c86ae6147f5fcda41
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:14:47 +0000

    forward what confuse creature forget

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_set(array):
+        return set(array)
