7a07e9f1d1df6d6ab190ceaca9ce8f25e567b675
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:25:42 +0000

    attractive group enthusiastic present view

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
