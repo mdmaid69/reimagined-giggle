23eedc551769b9a1f2e46247edc40e897f3d59a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:31:48 +0000

    turninto know usually face 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
