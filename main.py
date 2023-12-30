359afd4e71c140ac881b20a955f3c8c95b2b2907
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:30:15 +0000

    storm chin cost dress carry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-def find_min(lst):
-        return min(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
