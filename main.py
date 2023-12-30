f7f43235657ba0fe688a3c31998b78781097cef7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:18 +0000

    now money fierce rubbish inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
