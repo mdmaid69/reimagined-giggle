9c7ef9cd69873e3c89fb3273abbdf68d4d4a9f7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:56 +0000

    hour interest window backwards too

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
