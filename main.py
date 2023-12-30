05e69db44ab18950b0b0ec9c93f4656901242c79
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:29:28 +0000

    deserve hospital too improve Bb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
