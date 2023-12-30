6f756860d6392360bec06b0c18c71e57462ea1c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:59 +0000

    very hit target quit concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
