79913c1f4b95abef5fd2c8b87eb43114e0a5f1cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:04 +0000

    reliable influence rule age equal

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
