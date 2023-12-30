8e8593ec756f293d73e9ddabbdb658b9be981da3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:54:21 +0000

    supply address benefit wonder accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
