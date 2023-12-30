5345b76da9fec74fcd0c528fe866101d1768e13b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:54:32 +0000

    impossible nearly thanks prepare maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_min(lst):
+        return min(lst)
