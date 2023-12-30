2d17eb4e7312c9dc3a169ca7d86a405d2a228f93
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:40 +0000

    punish lookaround train emphasize result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
