9c70a3f0afcd0b6baac0cd50cace1807dc60a990
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:11 +0000

    lookup error approve along calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
