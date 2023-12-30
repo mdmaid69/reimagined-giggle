110fc7c60b29a366b0674889612dad0847cf3e4a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:44:09 +0000

    chief crisis bin exaggerate comfortable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
