670dbbe4416f4005974e7b8a6eced8e4615cc357
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:12:23 +0000

    row on scream long float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def divide_numbers(x, y):
-        return x / y
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
