eabe7ad8d1b095aacb3e10004a9507b18195a822
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:18 +0000

    practice bringforward comfortable estimate behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
