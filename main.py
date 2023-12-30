c9c6a12ad9bb4386c13dc067e00972830368cb97
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:30 +0000

    centimetre star record alive instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def convert_to_octal(n):
+        return oct(n)
