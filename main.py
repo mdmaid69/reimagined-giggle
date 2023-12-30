49b8aa2bd4619ac3cecb8a227013c2cb7bf888f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:24 +0000

    recognize reliable hit praise bottom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
