52de0deb14ffb0d2d667bb66f2fc05f3b25f5309
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:57:22 +0000

    middle care skin floor classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
