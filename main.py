949819da6f699eb6df2d7117eafe857014e08058
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:17 +0000

    actually label always fight attention

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def reverse_list(lst):
+        return lst[::-1]
