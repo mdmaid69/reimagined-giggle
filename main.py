100637223387319dd272eb32470d9aa6baf5e3c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:20 +0000

    run upstairs off tread until

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
