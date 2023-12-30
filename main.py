59aa94799b01f7281b04d602fe0a0786b227f351
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:26 +0000

    region holdon lesson unable declare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def multiply_numbers(x, y):
+        return x * y
