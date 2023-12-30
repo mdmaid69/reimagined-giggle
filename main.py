012d8cb0171e1de7dc066222c24ac7754573db1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:51:02 +0000

    mine behind wide long tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_memoryview(array):
+        return memoryview(array)
