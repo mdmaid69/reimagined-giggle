c3bcc912f82929e230420e6630e9e38d783baa60
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:21 +0000

    married gather control away mess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_max(numbers):
+        return max(numbers)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
