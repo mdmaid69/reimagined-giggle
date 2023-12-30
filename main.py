811d2061ac3bd89c9748a2165678120289e15f91
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:42 +0000

    translate pronounce door reply suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
