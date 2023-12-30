5dfb05bc107428c89f58144a5ac1842fb7762d93
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:40 +0000

    down fork regret nice near

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import collections
+def create_queue():
+        return collections.deque()
