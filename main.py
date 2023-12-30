9fdf880c07002a61558294433ffaedcbd0a49d87
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:29 +0000

    bright argue impossible blame better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
