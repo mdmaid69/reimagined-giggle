ce016aa132c7f96e1e2f6853d649adec83c3a379
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:31:40 +0000

    disgusting control occasion down exploration

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
