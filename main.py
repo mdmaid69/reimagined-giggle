ebb90195ab4d0513784a2ec20adcd9f30a1182d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:17:37 +0000

    explain start wind fat excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def convert_to_octal(n):
+        return oct(n)
