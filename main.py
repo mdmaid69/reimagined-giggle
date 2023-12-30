74c88d1aed9743053a34b62a3000a7df0a076f95
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:28:37 +0000

    hair say eager organization unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
