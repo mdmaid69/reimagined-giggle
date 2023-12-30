19e3005a39a862f818b7deadf4059fb569a210b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:17 +0000

    hour instruction nor breakdown clever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
