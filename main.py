0c5e91cd9e5e7f745f545ecbf3e2e87dce351041
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:21:29 +0000

    live lost comein dance admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
