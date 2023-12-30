e1951b87097ff33e38e86b2e543c15a83b5410e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:14:54 +0000

    usually damage beginning hello autumn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def convert_to_hex(n):
+        return hex(n)
