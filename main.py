a65c2489de8c2f0a60fa42d69c208e364fb40c12
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:55:38 +0000

    discover tie reserve weather radio

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-print(sys.version)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
