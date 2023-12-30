d2ef3cfeb32d908d784246501aded4d9fdab3f7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:34:51 +0000

    office violence thumb cough agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.time()
+import array
+def reverse_array(array):
+        array.reverse()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
