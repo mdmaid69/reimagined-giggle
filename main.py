986bed161ca1a4fffd17771c6d14cc33e758970d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:05:31 +0000

    crazy occur priority front schedule

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
