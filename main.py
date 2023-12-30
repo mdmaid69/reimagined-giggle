37a614be1e9c03eb4f08355138c032db8050bd39
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:35 +0000

    exchange century yours complete shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
