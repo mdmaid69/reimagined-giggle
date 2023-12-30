154788647c66296f20f8a9688e172910d832bad3
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:23:52 +0000

    comeoff especially take always improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
