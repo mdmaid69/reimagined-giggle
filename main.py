08c57374e1a0cd3e0c81789614d1b7e0ea43ec2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:32:28 +0000

    comfort excited reply know port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
