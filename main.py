850ae9b5f292fc6baf885e1d9c08309201c60b3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:22:50 +0000

    provide public support cover familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def sort_list(lst):
-        return sorted(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
