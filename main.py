56f383a9e338d738e1c0b410a3d7a286e79390ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:59:29 +0000

    promise waste gooff concern produce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_index(array, item):
+        return array.index(item)
