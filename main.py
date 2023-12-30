558163b3ffa814fe2891ccd7195c2d4083ae182e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:39 +0000

    ready callup introduction slight bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
