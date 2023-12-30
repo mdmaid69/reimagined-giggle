031e46fd259c4b403820edb8c61fd53f50985be4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:10:52 +0000

    lead copy spot clearly sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
