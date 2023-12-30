e2c8d77590b172cddcc68b35b51e62079a2e3be0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:15 +0000

    keepoff think spend person approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def remove_duplicates(lst):
+        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
