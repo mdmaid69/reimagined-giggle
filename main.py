ef2039890a5f84d60fa036251f4b3e54d24ac1ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:51:41 +0000

    573entertainment the fresh grateful meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
