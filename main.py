3c423ab51dc4be90f43e38dbd7a0c434f3c8eaaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:12:35 +0000

    temperature square finally break snow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def add_numbers(x, y):
+        return x + y
