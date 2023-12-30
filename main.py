e4deff2c1f5af813f84ae3901a19ba9e40912a86
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:42:16 +0000

    breakup start glad criticize bend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_itemsize(array):
+        return array.itemsize
