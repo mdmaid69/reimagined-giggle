fbb73467cd82bdf15e0c473cb3a0037f0b9291ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:27:19 +0000

    admire complain sensible long excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import sys
-print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
