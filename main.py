36dbf7e2ac539a923b1c1b63bfcee92f762a602d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:03:55 +0000

    claim collect holdup may communication

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
