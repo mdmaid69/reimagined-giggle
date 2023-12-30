3f7cb23d70b4e8e3271c698ae5ca56910c5957b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:40:56 +0000

    telephone fail why crash bend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_string_from_array(array):
+        return array.tobytes()
