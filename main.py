7908e7cbc64d7158405217331a72697a1a95b696
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:16:36 +0000

    straight point female promise soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def set_array_item(array, i, item):
+        array[i] = item
