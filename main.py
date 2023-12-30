7b24eb2b405ebd61099a32ba27414d9e8e674c13
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:39 +0000

    either could increase dance union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def print_python_version():
+        print(sys.version)
+import platform
+def get_python_version():
+        return platform.python_version()
