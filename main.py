26fd1dad50e655f943ed23bbc244909878d15fc3
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:56 +0000

    harm process especially dream back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
