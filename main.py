0ac5d156378028ef4f962a3f22b5927a53a3bf87
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:11 +0000

    film coffee borrow aware peace

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
