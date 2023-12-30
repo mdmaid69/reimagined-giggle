87157d73d17bc9055b9203f839856bcb64da41e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:47:15 +0000

    speed arrangement instrument form runover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
