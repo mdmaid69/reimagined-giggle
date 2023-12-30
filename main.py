9c018b3c3b5e191d89ee5572123efad29d1b071b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:10 +0000

    visit bottom Nn upstairs tense

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
