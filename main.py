7188c83074cc319497f33ddd76825facc6ae23e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:08 +0000

    lost interrupt bored awake realize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def is_even(n):
-        return n % 2 == 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
