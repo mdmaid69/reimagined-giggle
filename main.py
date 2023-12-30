1a5eb6889ae84f739f03341fd17e078482d3d901
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:36 +0000

    cross emergency disturb search breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
