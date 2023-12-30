ca352d32f3f7519037b4feaecdd79ff177e46826
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:31:59 +0000

    always in turndown private tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
