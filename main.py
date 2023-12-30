9a17550abfe5fb0e3d4eacb86e5a53c24f41747a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:25:03 +0000

    geton on used about break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
