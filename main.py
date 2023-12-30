3ed45f6c586abe231d25b69bd3e6ada26a5299fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:49:03 +0000

    should confident bottom so part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import numpy as np
+print(np.array([1, 2, 3]))
