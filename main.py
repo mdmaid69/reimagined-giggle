80c754637438b668cbf9ea0f2c6f55c46b69369f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:05 +0000

    century border patient course note

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def convert_to_hex(n):
+        return hex(n)
