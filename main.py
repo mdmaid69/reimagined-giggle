7e62b7b32c75f4a9026cab267985cd58b756efe1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:12:16 +0000

    platform pure rise against spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def find_max(lst):
+        return max(lst)
