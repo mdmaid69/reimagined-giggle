fb31ad0f468e018e71e45b2a2f91d71b1c71bf15
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:04:57 +0000

    open during welcome increase interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
