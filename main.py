bdd0f8d56e6a93228e250707d53389a6a94c226c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:43 +0000

    fun everywhere introduction finger rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
