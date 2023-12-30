b37c0e19b2a84f127152797ebd28aa3e3eca9a99
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:33 +0000

    pretend story sensitive rain bye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
