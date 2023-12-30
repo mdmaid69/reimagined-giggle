d30e91d4affa7bf049d0e852de7b6a0be73ce778
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:36:13 +0000

    piece ear thing never pencil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
