ea64a38dd796358f4f4c7409f1cd4b8364ea76fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:54 +0000

    passage tongue duty ring notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
