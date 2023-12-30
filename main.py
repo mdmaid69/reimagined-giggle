3a73feed19eb4ca277e5bc7781c6be27a8eaa12f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:03:33 +0000

    already cause let flow address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
