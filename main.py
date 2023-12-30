70f08131a15b868d2d2f464f010af0a68a6c0787
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:18:01 +0000

    bang speech permission nowhere shrink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
