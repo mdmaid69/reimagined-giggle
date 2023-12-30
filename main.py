a6c69e08e18f8e12947f204d300904d1b5aec391
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:59:19 +0000

    else announce beyond press tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
