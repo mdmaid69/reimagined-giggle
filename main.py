9d6f52c60f89cab3277eafe722ba22eb96e11726
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:30 +0000

    exact acceptable patient breath pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def find_min(lst):
-        return min(lst)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
