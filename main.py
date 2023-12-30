ea371f4a1b43ffde7397f9384a811b4b05621a10
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:01:59 +0000

    busy limit skill worst fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
