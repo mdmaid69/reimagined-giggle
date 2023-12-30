506b1bc7445b84b8527983f09bbb2889a1d9780f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:05:29 +0000

    rare Kk weak dish shock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def sort_list(lst):
+        return sorted(lst)
