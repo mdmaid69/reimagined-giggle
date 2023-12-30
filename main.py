ed4613d9069bfed57f572cec0b7731a76c1ac195
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:03:56 +0000

    come circumstance left appear sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
