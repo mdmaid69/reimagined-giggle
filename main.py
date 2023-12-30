849fcb58c91d6f15b70f71b2c1a891bbc0254f54
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:47:42 +0000

    waste stare brave innocent true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
