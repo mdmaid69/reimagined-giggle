593e91d4837a7c043458ce7fd98f6b8ea5a5c2d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:30:40 +0000

    through divide direct left home

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
