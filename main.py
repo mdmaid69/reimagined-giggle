603d7f0670b065371e13f7ebdf0fa18f2549a023
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:37:29 +0000

    up rub round order man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
