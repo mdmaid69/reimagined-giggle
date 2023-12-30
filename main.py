8131c70dca3a3e6c2eacf8f420300bc8e47d4b1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:10:42 +0000

    schedule pressure alone congratulations clear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
