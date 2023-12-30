347e79c01343d886f48c716c4c5a437018c0a13c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:56:21 +0000

    close sad cheap unusual chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_even(n):
-        return n % 2 == 0
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
