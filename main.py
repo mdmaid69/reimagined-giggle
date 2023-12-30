aeb803ec5e8f6373e7fd81f68e7de1d34bdd24de
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:27:17 +0000

    sail bringforward classroom alone correct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
