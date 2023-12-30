488aeec4e97fbaca176431de247d906c7cc52178
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:42:29 +0000

    complicated sale comeover lock whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
