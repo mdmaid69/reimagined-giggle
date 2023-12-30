1d0a4bcb2f3c66d6f65f5dd97fea93bc6c89bd47
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:18:05 +0000

    welcome project powerful keepout demand

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def convert_to_hex(n):
+        return hex(n)
