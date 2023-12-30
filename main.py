a82d3de2a027269f748e4192e2af9b2571def09f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:03:51 +0000

    long else present bye morning

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
