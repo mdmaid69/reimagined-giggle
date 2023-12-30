4c24cbc6bafcd59fab0dbd2b93cbf6bb67540e8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:59:32 +0000

    cross little stop allow bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
