c296f83250d14948fc34e3ce9b9c7e986f13724b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:47 +0000

    doubt confidence shoot underneath noone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
