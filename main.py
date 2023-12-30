29c023dac0073ce2977cfdb296067ca6364408cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:37:55 +0000

    hang pool rely sound connect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
