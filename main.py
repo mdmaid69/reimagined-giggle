c3a3b6f1b90dac6a382262ebd92b993c12df25bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:12 +0000

    order anything spoil appearance desperate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
