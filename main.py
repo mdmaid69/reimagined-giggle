dad34c73cf6bb6a715202d7bbc4cad0d370219af
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:51 +0000

    trust mend smell bang convinced

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
