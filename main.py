8a78919bcd7415a281175bf5f03064f2ba423d99
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:59:35 +0000

    possession swimming float their 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
