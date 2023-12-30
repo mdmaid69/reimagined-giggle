47100bfd65cd4ec9eafad608c6dbe3b66934e2bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:17:03 +0000

    relationship treat believe desperate towards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
