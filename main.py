c81c7ed57eeb29df758e464837c96467dba62884
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:27 +0000

    drive will hurry plate challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
