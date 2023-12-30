6634bad8ebd4b78c891d61630de78b7448b59684
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:36 +0000

    winter prove value impatient spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
 import collections
 def create_user_list():
         return collections.UserList()
+import array
+def get_array_index(array, item):
+        return array.index(item)
