ef9cf0b24b4f1460cbbbe42e2f85929ce22df8b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:45:15 +0000

    certainly innocent beside prize display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_area_circle(r):
+        return 3.14 * r**2
