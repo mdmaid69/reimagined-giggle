607f52b61e9cd5f982099e80719490ce4696185c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:49:44 +0000

    callback breath moment skirt goodbye

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def is_even(n):
-        return n % 2 == 0
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
