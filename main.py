e3b881b2c03f17a6cf358dfb0291020fbfbc3d1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:21:38 +0000

    stop home sort black advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_string_from_array(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
