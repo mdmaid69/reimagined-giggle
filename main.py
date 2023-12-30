d5cf477677ccf08f9faafd614022c853b61b8163
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:29 +0000

    expression along tune description fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
