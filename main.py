528a4a2599ef333675b064a1261e0c57ae5d18fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:33:31 +0000

    within shopping create sail immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
