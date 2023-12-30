79b90abd9756e9e8d6205ed7ea018f154535619b
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:10:49 +0000

    spread crash rough awake defend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
