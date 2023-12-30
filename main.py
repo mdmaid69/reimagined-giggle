0931e8568b3a0083a6c1c8f374ac4ebea0691cc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:34:26 +0000

    shut dangerous Ss approve car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_max(numbers):
+        return max(numbers)
