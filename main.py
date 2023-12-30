f7d5748226b4f502021049832e83b039ad795905
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:12:44 +0000

    situation smell century regional identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
