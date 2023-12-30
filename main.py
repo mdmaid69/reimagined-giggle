8be6fa60c40389854d354a0270303c6c8252d125
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:40 +0000

    lookup follow jump phone callup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
