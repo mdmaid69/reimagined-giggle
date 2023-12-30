2f413654581b28d069a4baca20a4d129ad496b3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:17:24 +0000

    hold calm result visit trouble

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
