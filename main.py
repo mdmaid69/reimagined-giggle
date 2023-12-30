03b238ddcd9d1a72cb9224fb89bd018fb83f4462
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:38:48 +0000

    exit sound excuse doubt extremely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
