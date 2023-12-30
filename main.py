5a1f9ee36aca7f5539a71cc8c9ffc67edec4e2dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:45 +0000

    block fond partner memory remove

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
