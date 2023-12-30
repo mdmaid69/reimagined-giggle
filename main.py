50ed0690da1c99b7004913caf5896bcbdeb84b20
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:37 +0000

    previous greeting 565end thinkthrough gooff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
