e84dbb1af9280facb0d876a66d657821a145318a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:06 +0000

    boring image bit wire corner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_max(numbers):
-        return max(numbers)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
