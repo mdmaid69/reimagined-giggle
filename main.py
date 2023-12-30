6a6ab74e3beee0fe3918f9bc93e3c1c963d232bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:58 +0000

    scared university about flat weather

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
