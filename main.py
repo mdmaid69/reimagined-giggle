58aac9e304eb6b01a5acb92821f4725fb7a2f06b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:53:25 +0000

    contain badly think holdon he

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
