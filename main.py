ed46ce8d3410ea136c57871c09a18e46b48a5f0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:30:38 +0000

    close cover quick fill work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
