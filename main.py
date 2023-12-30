073465821bd7b9e2dd662521bbb9203abec6c97b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:37 +0000

    past factory mainly sleep clean

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
