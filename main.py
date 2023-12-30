25fbacc317330efcc5736e2a795cabb1d073f5a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:11 +0000

    post bang ask match comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
