a28776aa00ec4e7054e15d5ce85c6d938c92070d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:03:13 +0000

    talk thought contain build main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
