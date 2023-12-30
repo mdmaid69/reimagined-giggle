16bc94c1cd61348ec276ef50acac4f46a292a8a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:25:46 +0000

    confuse trap wash pour art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def split_path(path):
+        return os.path.split(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
