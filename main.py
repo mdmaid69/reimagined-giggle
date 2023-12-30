0e3b5c6285e2f8018254aafb20755ab7ed2255a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:21 +0000

    walk leave happen conversation takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
