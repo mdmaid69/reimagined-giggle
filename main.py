e8ee7ea7adb83cc31f370d687e2affa18e6d1dbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:37:29 +0000

    complicated bet scrape live fall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
