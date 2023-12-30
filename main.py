a9d93996566b5666eaa14a4adf52712480356741
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:21:25 +0000

    back thinkover translate prize change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
