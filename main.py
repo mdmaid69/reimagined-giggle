18d385261e773af809a63f70a2b9c358ec7d38ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:45 +0000

    circle peaceful rain believe person

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
