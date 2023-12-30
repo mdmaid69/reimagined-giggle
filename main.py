5743274f16fd504668df0b574f282e0cbe3c0091
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:51 +0000

    one street bar scream answer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
