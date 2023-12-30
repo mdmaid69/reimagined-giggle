03a2b98ff92d8dce4212cd22336b57acf77265a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:15:43 +0000

    thought punishment special takeback eager

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
