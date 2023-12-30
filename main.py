897ca3af6c5e2d47770ffff4acc4e8c366c90839
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:54 +0000

    proof sorry passage progress mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
