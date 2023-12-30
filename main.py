e6056a74eeb18a4242fbd58f3eb9e0a1f35f06e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:24 +0000

    present something boring little dare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
