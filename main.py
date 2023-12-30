b4caa78a48854101036854d2b19bd5dec7d47703
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:19:59 +0000

    extraordinary negative cold wander straight

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def get_current_working_directory():
+        return os.getcwd()
