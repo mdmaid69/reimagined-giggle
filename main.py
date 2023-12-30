03f071431a32957d7a2c0845650610f05ff77bec
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:09 +0000

    scrap thirsty maybe forgive physical

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import random
+print(random.randint(0, 100))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
