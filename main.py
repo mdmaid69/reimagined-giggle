916995022da6c671c87cd311cd9b887351a320ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:39 +0000

    extremely bringdown be count call

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_hyperbolic_arc_tangent(x):
+def calculate_inverse_hyperbolic_tangent(x):
         return math.atanh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
