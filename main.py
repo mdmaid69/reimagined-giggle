f62f59aaefc932d46a96570230bfe68610de292a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:49 +0000

    bet town present boring middle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
