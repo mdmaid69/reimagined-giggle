4af5bf0233b797b1c8ee712bad48ea6f733f8a9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:52 +0000

    goback(to) as thankyou dark warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
