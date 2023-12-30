182336761f9553199a4d77654fd2b2b674bc19d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:36:18 +0000

    who stare difference precise track

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
