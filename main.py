5a7685d309eb0b0dcc020c5aad0f278d64e09d05
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:25:02 +0000

    mess trip official cope getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def delete_file(file_name):
+        os.remove(file_name)
