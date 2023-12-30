0ae48c591c0671131284371dfa58916f50dde847
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:26:50 +0000

    snow tidy slide another raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_list(array):
+        return list(array)
+import sys
+def exit_program():
+        sys.exit()
