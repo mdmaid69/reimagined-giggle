e185918eb5dbbb66c7fb10eb265795c148e43a7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:37:51 +0000

    lie correction somehow art education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_floor(x):
+        return math.floor(x)
