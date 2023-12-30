c4de2d3adb90d56fb7ed60ad79d06f8218607f73
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:06 +0000

    attractive famous along hand lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
