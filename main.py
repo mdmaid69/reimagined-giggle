04b19693c7ebe5e7dd14ae39fcd7403fd1e99fc3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:53:15 +0000

    this unlucky plain smell reject

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
