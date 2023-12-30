fef1a49ce45826e0f7ece1508cbac60b66ca4f32
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:22 +0000

    visit bear look completely settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
