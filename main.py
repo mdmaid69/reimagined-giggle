76b93ce3e43f58eaefe00a3b4ac16f2e4864c97c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:49:21 +0000

    interview accommodation 564encourage absolutely case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import os
+print(os.getcwd())
