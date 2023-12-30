62b463d99545dc4a6353641243db72a9b0953435
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:17 +0000

    but cross unite disagree wrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
