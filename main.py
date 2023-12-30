0d65e67cf6c15d6c1b63f69371da940ec1fdff15
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:47 +0000

    slice bunch destroy nonsense pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def count_words(sentence):
-        return len(sentence.split())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
