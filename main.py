860d7cc9442f34e1c5b850887a070387920d77fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:32:34 +0000

    record persuade mass similar goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
