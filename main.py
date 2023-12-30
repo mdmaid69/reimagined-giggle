6b92fe7003592c4f9081ea8fc4302fba540a6626
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:33:52 +0000

    young beneath completely splash dig

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
