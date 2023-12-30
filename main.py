20ef8e2ca9649abd5b3e59df0bc50e0fe598cfd0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:56:40 +0000

    concentrate ilovedicks cause average floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
