a497739ebd7b2077e6e79b07b59cf794144794c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:23:13 +0000

    know appearance callat unable clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
