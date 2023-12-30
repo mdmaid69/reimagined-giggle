23aac5ae1b78ff2d6a25c03321798e9acec6fbc7
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:15 +0000

    die discuss person far nature

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-def print_python_version():
-        return sys.version
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
