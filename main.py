9a561142769b77f42fbf03db27a970d3c413471a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:52:01 +0000

    niggaacademy mess regret could think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
