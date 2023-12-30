612b5da6be1b8a114c60252c7e411f518dab3b39
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:25:00 +0000

    Ww treat damage something recommend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
