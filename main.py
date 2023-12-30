ac11089d0894e0e63c77f21d0b2ebed21dc5d9d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:20:46 +0000

    should all talkback yet move

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
