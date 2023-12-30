bf1b4a4d2cf977c3be08fd9e16e18e5ad04300b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:52:27 +0000

    getoff trip scare trap familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
