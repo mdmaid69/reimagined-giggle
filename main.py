5409cb1f37dc7f8660cc919de51775536f576791
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:15 +0000

    sore marry comfort confused vote

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
