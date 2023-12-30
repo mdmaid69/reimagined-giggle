c0103c554e0d87a69438f5d530cda7947492a43f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:13:03 +0000

    circle combine connect thumb smile

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
