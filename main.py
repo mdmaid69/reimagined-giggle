2898c50b1d8c5a6976152fca1a399a7e5ac5c407
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:47 +0000

    mine Uu centimetre talkinto answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
