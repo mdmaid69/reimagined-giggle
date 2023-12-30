ae57910d23fd699c5ea5c548910f5f4231ca8b3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:08:50 +0000

    contact hate pupil country organization

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
