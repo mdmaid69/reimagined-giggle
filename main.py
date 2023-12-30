6154834aeafa73e26898b2eb95a71655584a96da
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:35 +0000

    aim kind match underneath front

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-for i in range(10): print(i)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
