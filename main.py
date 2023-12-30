bc1beaf45f10db01bea37c4644552f776fee879a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:38 +0000

    as present genuine undo improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
