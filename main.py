d50a127a145195b6f7074cddf76e6cae081613b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:37 +0000

    waste effort say hurry stop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
