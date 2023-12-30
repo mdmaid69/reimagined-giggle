ef30204b12a9e55af11c684a6f8390cc66cc90eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:08 +0000

    failure strange straight period error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
