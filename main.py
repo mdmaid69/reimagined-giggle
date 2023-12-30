c252fd966a9f832f94e0b66a8495275a4d0175ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:00:45 +0000

    grade she miss wire aim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
