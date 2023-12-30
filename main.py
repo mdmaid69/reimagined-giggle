7731fc3242b2542462bd893ba2036fce8a255b7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:45:15 +0000

    leave part laugh full last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
