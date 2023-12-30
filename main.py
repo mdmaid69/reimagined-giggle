912c25e1d5bf423ad09f94ec7699d98a4201b849
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:51:21 +0000

    keep disagree line bringup Vv

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
