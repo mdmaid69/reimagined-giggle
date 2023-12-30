faf60ecdf771ba90164616d288814d24932e94d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:52:33 +0000

    prize record except occur middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
