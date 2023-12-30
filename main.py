cbe58e95cb4e29c996aca0bb8eedea72a7575b50
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:32:37 +0000

    bottom control better apart separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_length(array):
-        return len(array)
+def get_string_from_array(array):
+        return array.tobytes()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
