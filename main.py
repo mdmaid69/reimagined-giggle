9249f5834fb4bb2f196428de7b94ea8de878f33e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:00 +0000

    month admit someone expensive present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
