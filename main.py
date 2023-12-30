940538cd5ce5cb3702164c9f9e1c1a21ab55dfe9
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:15:09 +0000

    for switch thorough what official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
