bc1f132047c02f27b79373b9456a03a1f39d3a14
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:50:09 +0000

    bottom prevent receive succeed Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
