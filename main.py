6a406d7cf7ebd2f478723afe358fe7ed798044af
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:40 +0000

    frame name rank mess name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
