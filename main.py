57ec4683f64137a6a1540c000f1fc62d8ee5c196
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:31 +0000

    control anywayanyhow congratulate teach far

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import collections
+def create_priority_queue():
+        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
