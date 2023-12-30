8e91a2bce10b2dc5d51bf629a5cbc403f253e3e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:08 +0000

    bunch single steam sort prevent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
+def divide_numbers(x, y):
+        return x / y
