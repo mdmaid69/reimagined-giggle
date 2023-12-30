c15a16e158d24e160574a80c758738a7b1c1fb93
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:06:56 +0000

    noisy or not confused shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_dict():
+        return collections.UserDict()
