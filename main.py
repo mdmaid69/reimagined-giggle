70a485b31bf08bd2cddccf0e6c2c0f8d37fc599a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:47:14 +0000

    himself world regret importance available

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def print_python_version():
+        print(sys.version)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
