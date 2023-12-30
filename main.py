27a7511d39aba7e59a478436fe8c64a0ab10dfa2
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:57 +0000

    heavy near joke temperature store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
