7ac6f1e1dbd32ba25a342c28adad5750768d6dc9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:05:10 +0000

    loan smile plan cough either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
