c4cc9b2c6f53306ee09e1d7bddef132be8eb9c72
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:50 +0000

    concerned region tired stone degree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_max(numbers):
-        return max(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
