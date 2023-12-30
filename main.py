f66e604bdc9cc852d91975f5044530a4f8fd6e76
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:56:07 +0000

    turnup nurse beneath live lead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
