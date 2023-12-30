08ad60c627f961cd020a11dc82e48ad54b3e1109
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:55:50 +0000

    favourite pleased nowhere marry mainly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
