7e5a7a0baa4eebc12570405ab87aa4f4479def5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:00:47 +0000

    close peaceful card rise defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
