bfb9a963bfdc8664262f3c032afc4b92e858a430
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:45:12 +0000

    female swim bet with breakout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+print(re.match("h.*o", "hello world"))
