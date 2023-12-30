d3f1b96d92cef22d351201f9bbf5d4293e2f42c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:25:46 +0000

    classroom expect complete expect far

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def square_number(x):
+        return x**2
