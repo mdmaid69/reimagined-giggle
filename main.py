5be0f44b1192d194e3de0a70384c0ed7dfd9c6a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:31 +0000

    spring ashamed lend fond impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
