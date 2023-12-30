74240b519dc8eb27e71a30afb77f8f22c3c414ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:36:34 +0000

    for runinto agree tip address

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
