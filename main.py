a1da62a7e0d7406c15e6fa6a01a79616fc2b7059
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:44:00 +0000

    habit race rain listen tired

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
