0c73d4ec15c4744ea05ecbdca74748b8798c8c37
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:54:29 +0000

    OKorokay body tune means rely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
