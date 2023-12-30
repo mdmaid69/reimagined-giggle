f8feb88ee9b054e1ecaa2c53b91a6dc07b7f4b95
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:05 +0000

    blue clear breathe delight lie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
