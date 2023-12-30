ee4054db2a8d04e9cc36def8fe078e98b474eae2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:08 +0000

    abroad practice wait effort tend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import sys
+def exit_program():
+        sys.exit()
