75ed2fd98e1f6be1524673e174d8351db61b5ea6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:17 +0000

    opposite off car different modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
