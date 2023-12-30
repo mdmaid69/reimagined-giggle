f27c06f48fa16907d78dce2a30cab0489400ae45
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:03 +0000

    tired name word sight heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
