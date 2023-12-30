5750c40d02b15d22c5d9604c706c781228a9753d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:04:31 +0000

    unfortunate without doubt interfere model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
