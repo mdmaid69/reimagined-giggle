57cde5af8cca810f99b66eee4712c5c5c799b0b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:58 +0000

    quarter second achieve past worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
