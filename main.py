7f225cddf0d6ee68de8ddb248e5985be60c29019
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:28:49 +0000

    cost comment question beside start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
