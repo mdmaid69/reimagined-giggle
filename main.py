89d888e3bd45e07e01c23d1a1564d64d5a833183
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:33:08 +0000

    anywayanyhow door cross path which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
