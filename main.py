86de4515c41dea7a80224aee61bda0cb7f81b657
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:43 +0000

    loud weather film die translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_array_to_list(array):
+        return array.tolist()
