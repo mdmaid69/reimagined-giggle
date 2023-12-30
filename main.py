12c35328fb7d27465dab7dd913b70dc86465e271
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:21:41 +0000

    begin flood test arrangement software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
