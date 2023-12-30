5f62403916e7167f5a2cae7834678d9050bf4008
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:14 +0000

    education traffic fasten spare complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def add_numbers(a, b):
+        return a + b
