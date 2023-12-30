b1ecf796f01d0c9d49107edb782d0c63610f35db
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:07 +0000

    fasten rough hurt bringup nurse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import tensorflow as tf
+print(tf.__version__)
