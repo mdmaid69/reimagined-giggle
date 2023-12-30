8ebb3c7fecdcb2b92d29d7fadb6efa3b5d7eb76f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:16:15 +0000

    bear flight warn help party

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-print("Hello, world!")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
