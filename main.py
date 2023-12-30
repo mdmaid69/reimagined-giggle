1b6f7b88d45ed4bc0e2513132a61bdad61d66d5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:55:57 +0000

    succeed worth practice issue pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
