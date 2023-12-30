dced56cec602ea3bd048e85e0506a68563a02aa9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:07 +0000

    choose fasten turninto offer matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
