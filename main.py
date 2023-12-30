ae2ae239f8f528c1b00bc71c5059079d0ec76563
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:26 +0000

    character beat gradually then colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def cube_number(x):
+        return x**3
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
