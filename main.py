51796ab4764d4dd887240398119906b1eba17161
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:17:48 +0000

    best number lean hole autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sys
-def exit_program():
-        sys.exit()
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
