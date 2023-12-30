907197733c84a1ad7d481d791e32f5b84a3e9cfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:33:59 +0000

    teach future part build call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_force(mass, acceleration):
+        return mass * acceleration
