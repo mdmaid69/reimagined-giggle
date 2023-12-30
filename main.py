9c17b92a41f2814cce15e56dd2b2818fb81e7204
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:58 +0000

    free live conduct steer keep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import numpy as np
+print(np.array([1, 2, 3]))
