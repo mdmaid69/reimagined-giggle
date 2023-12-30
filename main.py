33dc97026bb4c9a72b75d4f79d37fb61612536b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:44 +0000

    estimate dry embarrassed stir uniform

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
