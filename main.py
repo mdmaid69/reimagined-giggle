49be00286775824f53423ac4b611c7bf24e9edc0
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:40 +0000

    protect brave your rise turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
