6d9951d6d379070912435d7aa2482587a43873ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:50:53 +0000

    week pretty shock story strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def convert_to_octal(n):
+        return oct(n)
