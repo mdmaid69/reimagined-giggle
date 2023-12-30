b23aa8a1543d9240007fcb4b050741f03b2e237b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:45:41 +0000

    bound anyone quit sail centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_list(array):
+        return list(array)
