5217c106c362579ebf1c3d11a0a5ec7dd73272e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:54:50 +0000

    safety goaway confusing runout tread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
