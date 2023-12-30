7711aa42bc879324b80890b1b9bda89561146843
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:09:10 +0000

    teach nigga like familiar although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
