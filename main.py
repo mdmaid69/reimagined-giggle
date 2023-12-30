d46a04d66a60c38d9e86e67cc2235a4ca1a0f144
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:45:27 +0000

    angry sort though put lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
