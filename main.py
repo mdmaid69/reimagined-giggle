2f884dd52df100b96b50592189107eeb90196ef9
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:50:14 +0000

    recognize himself commit succeed attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
