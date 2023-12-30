3ba8c9b999ffc53ddea95fe41f4b9014dd81998f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:37 +0000

    the swimming lay first period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_floor(x):
+        return math.floor(x)
