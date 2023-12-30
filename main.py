cbca4b00cc3f39753c997a6c8890c123d2b937cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:26:22 +0000

    tape sorry excuse switch fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
