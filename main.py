53d889ddad0d0c202b4baa736f9bee0ef8600ad6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:41:12 +0000

    guess score popular increase pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
