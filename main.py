ccf3779361207f803a688b2de2d42c9015d4dedf
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:29:36 +0000

    silent cost switch substance drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
