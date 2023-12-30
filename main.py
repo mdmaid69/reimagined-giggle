dd74f2d78885dd045a7e1e7aef38cc0c76f0b406
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:56 +0000

    turnon morning eat bringup kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
