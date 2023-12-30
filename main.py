e30f74ce2e8568406d54854ad9565a38026b0abd
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:08:04 +0000

    total deliberate practise borrow record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import tensorflow as tf
+print(tf.__version__)
