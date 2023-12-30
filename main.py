c15f1c90a780a09046cd1a6c14c177d53869d690
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:31 +0000

    sort available immediate bit apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
