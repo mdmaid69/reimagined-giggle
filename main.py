0f6a395eee8f1c7c6aea9c17d06e1b0cf0e7ce13
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:14 +0000

    getoff attach failure provide arrange

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import csv
 def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
