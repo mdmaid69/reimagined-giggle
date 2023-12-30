557fc5a262433e5877d7f937cf79e55ba153ecb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:21:47 +0000

    next downstairs wind relative flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
