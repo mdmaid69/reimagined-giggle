f178f535ad8665e734a72d1df49aaee9a8dcd411
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:56:52 +0000

    deserve avoid sky greeting physical

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def reverse_list(lst):
+        return lst[::-1]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
