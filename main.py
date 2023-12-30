f97be10e0ae2ddd35afe5c120e0cb08ee3b12913
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:06 +0000

    place boring bridge rush like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
