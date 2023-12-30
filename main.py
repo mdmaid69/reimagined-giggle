f4ac90809afa59e097850d68b2df94d80f1e94c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:16 +0000

    sure language conversation image little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
