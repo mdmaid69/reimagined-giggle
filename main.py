0c3a09e30d760d39ddfb68f6edfdee2c778a2e62
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:27:19 +0000

    pay collection press fail nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
