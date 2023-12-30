ecc9b77168f9b20c7bc1c32cf683a5df7896344d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:46 +0000

    simple quickly unkind tall unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_unique_words(sentence):
+        return set(sentence.split())
