14332772c1753597c0345f69e73f389a1ebaf092
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:53:00 +0000

    lean runinto maybe during movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
