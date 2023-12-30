5a16dfa3adec681674c64f51d4a4903d4d85e290
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:30:50 +0000

    any invite worst accept duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
