1e5e55c61a4a23e021a0f05d85d7408bf4dd0391
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:13:03 +0000

    interview represent hollow opposite early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
