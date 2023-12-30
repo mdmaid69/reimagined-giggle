317f712d479fea545e6ac988616216cf35dd2d44
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:34:18 +0000

    out thirsty call improvement damage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
