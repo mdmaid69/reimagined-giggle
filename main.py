58c7875fb8b043ec0323ed7b8ca7f4a340b20eb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:38:02 +0000

    soft house stone seat trap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import datetime
+def get_today_date():
+        return datetime.date.today()
