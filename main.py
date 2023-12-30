a2e24c2a65197e1aa3cbf820e1b02ead14bb63f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:05 +0000

    573entertainment throughout profit keen trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
