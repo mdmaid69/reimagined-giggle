3fff4adbdb00015cd70b495766563f8a60c39429
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:59:23 +0000

    charge Hh word later force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
