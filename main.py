7df54a42756d66429994d277b6ed2393fbad7787
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:31:43 +0000

    by turn disappointed survive snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
