d5013be48a7c824efb1b9a8c0fe132e2beb10122
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:13:20 +0000

    several piece free bridge fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import os
-print(os.getcwd())
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
