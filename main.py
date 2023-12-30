fd8e9dc1c00bf37e6947d030d13b87f380ebe4e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:19:40 +0000

    message turnover need ear centre

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import unittest
 
 class TestStringMethods(unittest.TestCase):
         def test_upper(self):
         self.assertEqual("foo".upper(), "FOO")
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
