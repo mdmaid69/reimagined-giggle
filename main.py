f024b4d3b1e66fc8b780b49d21f185eb9245aa84
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:36 +0000

    breath wait rule whom independent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_hex(n):
+        return hex(n)
