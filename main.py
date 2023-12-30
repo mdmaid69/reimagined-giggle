4ceb1ede57e6e207ca836758acf0712c3145fcbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:24 +0000

    offer period straight everyone bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
