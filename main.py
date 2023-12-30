826e9dd96c44f58ad19be8a378018b8a178e12e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:07:18 +0000

    wash many need tight calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
