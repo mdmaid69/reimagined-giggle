4fbf1bb7cc6f20c4e41f8f7caabbf32480b09b4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:03:53 +0000

    hide dare female commit evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
