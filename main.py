6386c4872001c8af675d2d0a63f14d194c80ab2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:56:13 +0000

    pause priority Qq pupil if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
