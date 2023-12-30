f654c38f9f4b34437c19341789cceb850de3f500
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:03:14 +0000

    myself afford question actual work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
