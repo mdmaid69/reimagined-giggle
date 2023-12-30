4f2854d3988323a1a370317234a1d6375746c916
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:29 +0000

    clever young stupid plan finally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def reverse_list(lst):
+        return lst[::-1]
