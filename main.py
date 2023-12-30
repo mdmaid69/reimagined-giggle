a9778c38cb2671a91486a12f03751312c927f9c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:00 +0000

    break variety front evidence callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
