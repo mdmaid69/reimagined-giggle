7585ed307792e96efad0d5a8916e1d5418eb2a86
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:34:31 +0000

    outside develop transport sink phone

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
