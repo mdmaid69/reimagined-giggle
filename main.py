ca0255ae85af9e275a0e908a00ddfabf222f203c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:55:11 +0000

    fetch contribute foot bank contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
