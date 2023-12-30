56f9caf580073dcb6bc85e0a68eaa7b151ea1b57
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:17:33 +0000

    shoot contrast shelter bar bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
