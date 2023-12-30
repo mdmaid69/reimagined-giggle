d8bb6395a78e99d6bc70b4fb3b0cdcd6c8937ec7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:39 +0000

    story wave wealth I takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def remove_duplicates(lst):
+        return list(set(lst))
