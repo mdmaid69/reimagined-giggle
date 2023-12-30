b6ee57c283f6ec0fce0c46fed5442df2953a4d7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:29:46 +0000

    crazy cook turninto brave comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
