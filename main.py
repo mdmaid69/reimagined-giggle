20172e427a7e61de978ed48df7b7e26d4d32f58c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:09:47 +0000

    interrupt bang describe experiment estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
