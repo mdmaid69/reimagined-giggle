81c42d83c65a93a7852f809b22022492cd7d68e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:35:16 +0000

    contrast rise minority check mean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
