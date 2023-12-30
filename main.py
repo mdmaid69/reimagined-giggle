0c6f8ab7067b819b4195c0e83fbc3959281e4f93
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:16:19 +0000

    objection platform bet remain man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
