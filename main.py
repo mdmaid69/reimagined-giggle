7281d61049a7f2c5686e940cc79fca57f1b36390
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:10:10 +0000

    geton about pupil kind pen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
