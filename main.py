94dce3259ec1536791b48afc0eef9b5b19e9f5f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:11:23 +0000

    closely wander worse gift programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
