1fff7299b77be58a82148563b77cc99c2293a5ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:03:28 +0000

    seat shelter emphasis production attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
