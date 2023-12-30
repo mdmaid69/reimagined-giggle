4ff30c942b271cc1b5147ad5570fe9c2aa033540
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:57:06 +0000

    rich region now check ours

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+x = 10
+y = 20
+print("Sum:", x + y)
