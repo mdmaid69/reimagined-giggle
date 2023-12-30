1ebb04da908e3e4194ea09af7cd5cd92cd57f9ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:49:54 +0000

    hit convinced open inside ever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
