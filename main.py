7fe906e3a8e8dbc50f3d27489ef7627b92e37de3
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:20:39 +0000

    string brief couple instrument turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import platform
 def get_python_version():
         return platform.python_version()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
