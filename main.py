b2a1a6b0c8c9ca475d126014107047d5e8da518f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:56:19 +0000

    condition meaning post embarrassing last

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def is_odd(n):
+        return n % 2 != 0
