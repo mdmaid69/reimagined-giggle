a20c0c54870fb3ac9100da66e0abcb9fced00341
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:36:49 +0000

    ahead slice mind afraid lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
