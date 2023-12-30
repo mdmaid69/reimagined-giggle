7cbf2358c69a1b95c313e85f85688571f6dd4822
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:57:41 +0000

    takeaway mean makeinto cross proof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
