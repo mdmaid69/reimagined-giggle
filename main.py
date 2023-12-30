aaf18e54302b746d66bca0579839698a8aee7131
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:30:59 +0000

    compare nor distance direction mine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
