4c59de45920863c392a6543182c91beaf13b3553
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:31:39 +0000

    left swing issue tidy touch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def multiply_numbers(x, y):
+        return x * y
