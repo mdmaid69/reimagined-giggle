d9ad22af765bfa3fc9b4a5f85aa511b208e7b567
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:18:10 +0000

    that me strength press put

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
