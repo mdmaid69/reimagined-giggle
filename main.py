4baa9764dff0343a64838ee3cbd984fdc863235f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:10 +0000

    come content proud comefrom worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
