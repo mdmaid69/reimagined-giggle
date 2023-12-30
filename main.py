f347b66ca2a7dc21cfbbcdff26400b92d562e1e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:47:16 +0000

    deal nervous active bringback eat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
