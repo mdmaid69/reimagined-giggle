fdced3b7814069277aba898e0861aee400a02f87
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:34:13 +0000

    classroom complicated favourite square children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def roll_die():
-        return random.randint(1, 6)
+def reverse_string(s):
+        return s[::-1]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
