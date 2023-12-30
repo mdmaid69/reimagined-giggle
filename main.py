7e8740897b2e64ce636dbd735abef0cc7e22f109
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:22:51 +0000

    hope allow stupid personal capital

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def read_from_json(json_string):
+        return json.loads(json_string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
