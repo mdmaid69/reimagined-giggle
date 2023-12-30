bee6c508fc1950ef3175bf0eb3d766c2c53e9a24
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:28 +0000

    568energy dare wake side tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import platform
+def get_os_info():
+        return platform.uname()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
