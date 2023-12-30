b3b673d3be26a43b5896dbe2d640c53540d07a72
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:55:50 +0000

    ring meet themselves language bread

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
