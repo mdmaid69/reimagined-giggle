0322a00256366a48ea8ce8df3a5ae0e9a4535e86
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:58:09 +0000

    describe decide noisy block birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def add_numbers(a, b):
+        return a + b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
