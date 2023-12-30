699ddf1396d237daa0770bc0d013d9a380b92aac
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:55:52 +0000

    lookafter handle impossible rent shop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
