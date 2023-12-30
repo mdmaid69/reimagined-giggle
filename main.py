6c8d74524c7fc74b99ecc276b7a4d61eda199e23
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:59:21 +0000

    afternoon disapprove spring false put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def reverse_list(lst):
-        return lst[::-1]
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
