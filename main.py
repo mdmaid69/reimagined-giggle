c84d9f51628a7e3582cd3b47eabd1aac0d098349
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:31:51 +0000

    vision behave stupid become crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
