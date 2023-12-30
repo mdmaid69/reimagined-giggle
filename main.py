67bfea4499c7bd4d1f0f4535f40044564dd8d0a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:49:19 +0000

    of mine steal regard definite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
