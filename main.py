7ed66cb043cdca0ac6ad6ac329248a79a63b06e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:34:25 +0000

    cum think that guide fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
