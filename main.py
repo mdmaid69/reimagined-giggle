4f04da607c54a3b58dd9696e1df751deec7e12da
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:08 +0000

    trust shrink thankyou seem arrangement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print([x**2 for x in range(10)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
