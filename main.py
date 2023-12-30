a9e819e015bc069e9744ab115dd79245e8d5696e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:26:54 +0000

    rather noise foot that field

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
