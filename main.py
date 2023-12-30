4db9bd0381ac90abd4167791e5d94b5153a84acf
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:51:12 +0000

    benefit question mess middle aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def find_union(list1, list2):
+        return set(list1) | set(list2)
