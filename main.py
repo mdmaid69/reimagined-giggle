008614f62ff4dd56d1f5d3b4355757224e48afda
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:03:59 +0000

    high certainly along sound confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
