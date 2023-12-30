7f1c72481dee170089cd696f102e621ec5660d86
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:58:25 +0000

    sorry arm anywhere lesson bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
