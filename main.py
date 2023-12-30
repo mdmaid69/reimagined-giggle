d6b08da6ddd089f3fb4887bfa94095fb17101010
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:28:47 +0000

    pleased edge Rr it else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
