504e2142b9fdd18b2907a8ec71110813cbfb6c8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:04:51 +0000

    concentration concerning 568energy bend negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
