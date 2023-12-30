c09330bbddc405632f9e97175a3eaad1740899ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:45:31 +0000

    fairly interrupt beautiful on bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
