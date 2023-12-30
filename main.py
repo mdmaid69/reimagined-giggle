554fc956f5c41950c5e0481d7f580f55ccc80b50
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:13:17 +0000

    suggestion choose thirsty image absolutely

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
