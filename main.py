eb28f4db97fae3da7a9b0eb3f6d4a7765751c672
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:10:28 +0000

    sea pool explanation town equal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
