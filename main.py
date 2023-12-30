40d32fa2dea61f321419f20104752f751adece08
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:40:36 +0000

    tough afternoon right Nn beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
