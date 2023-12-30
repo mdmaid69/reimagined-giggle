b6579c134484b9c5ec48fa509d6820f66df3de46
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:17:41 +0000

    whom schedule man zero forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
