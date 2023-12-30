4c0bb20a771a0427d1253c26505570da4bc91708
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:16 +0000

    Yy then shock majority surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
