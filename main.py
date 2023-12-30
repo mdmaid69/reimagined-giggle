c745dcde6c2ce148707ddd9269a6976e87c2041c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:32:32 +0000

    visit flat sit mark strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+print("Hello, world!")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
