0dc01cade7a8b4467d0ff4cbaa1287bfc24fb798
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:08:09 +0000

    goon involve system runout frighten

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def is_odd(n):
-        return n % 2 != 0
+  def convert_to_octal(n):
+        return oct(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
