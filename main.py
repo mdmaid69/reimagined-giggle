994feb40378d004e5496c5f5e4242313391c57af
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:16 +0000

    old foreign callout cover another

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
