63b22bd0350fba7bfdf618fa1e9956bc0cf59254
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:46 +0000

    godown behind movement swim telephone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
