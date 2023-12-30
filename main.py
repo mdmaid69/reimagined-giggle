1afe11c35900164705b144633633019d97edbb52
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:58:18 +0000

    blind defend keepoff mess thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
