61f3615a9a87db18efe5918d2bad6b6cb5e0b56d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:21:24 +0000

    suckme decide bag scene loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_max(lst):
+        return max(lst)
