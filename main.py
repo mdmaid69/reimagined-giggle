979e9f4f16f665096cff014ab10ce63ec8f0cd2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:41:58 +0000

    shock kick please hour please

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_user_list():
-        return collections.UserList()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
