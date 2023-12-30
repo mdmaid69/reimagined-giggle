b9d945f6e07f35b648acd9e39029ffd02633c643
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:49 +0000

    total yesterday house allow sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
