e352b03af8a4f2c99b5379e2c018f5389ce518b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:48:25 +0000

    deserve come average embarrassed debt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_list_from_array(array):
+        return array.tolist()
