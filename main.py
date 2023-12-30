6a6ef671b1d2306e8472d7e44f8cd6f18cad36f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:39 +0000

    window shoulder next sensitive through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
