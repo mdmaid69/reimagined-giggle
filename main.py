8dba4b14f3adce31004778d0cb35226ef7b6878d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:34 +0000

    low neck change goon arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
