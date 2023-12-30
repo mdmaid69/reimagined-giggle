0ba709cad0bc4cefa43c4880e870b64029021ec6
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:59:09 +0000

    basis vote grateful frequent listenfor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
