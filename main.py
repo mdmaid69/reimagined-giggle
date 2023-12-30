e872c38cd2b8f037432769871f104cc18422af21
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:08:05 +0000

    neither bringforward print nerve control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
