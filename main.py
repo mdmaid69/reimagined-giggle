8ad97602b187541833a228cb484eb136b9ca21b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:37:58 +0000

    rare direction imagine pronounce reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
