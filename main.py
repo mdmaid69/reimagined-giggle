b838820dfacbdf6a694dbaa9ae686aedd1775eee
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:04:58 +0000

    breakinto shelter open that cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
