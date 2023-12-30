ffa167224af92c14c4cc09877343b092358125f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:28 +0000

    past meal male grow fresh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
