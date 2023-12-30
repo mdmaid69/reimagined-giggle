de8f6478b1f181483467b3b03dc60b152d40119c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:15:42 +0000

    violence contain progress impressed difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
