13e670b8e5ec3606f624105a927ef3ad7a291beb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:56:49 +0000

    estimate pipe nurse protect home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
