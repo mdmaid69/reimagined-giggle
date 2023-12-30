19fdb0df203222b45df39b2ae1377f71864b4d12
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:34:55 +0000

    minute expression progress suggestion bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
