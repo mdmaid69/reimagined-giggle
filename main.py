1e4c2e128e6fd3b73fbe2d9332c58a29080b57e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:03 +0000

    confuse piece available cover our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
