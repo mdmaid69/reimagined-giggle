87d9cbc00301ca18270c9af3c7d040a0a0f08974
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:40:48 +0000

    defeat risk stamp central confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
