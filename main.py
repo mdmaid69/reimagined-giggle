57bda490626e7a9cbd8861c0fabecae9e3201938
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:53:07 +0000

    phone avoid size silent hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
