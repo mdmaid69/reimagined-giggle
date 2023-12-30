e2e617338def321f6055a1ad278b531ddf6331b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:14:51 +0000

    holiday increase space 564encourage score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
