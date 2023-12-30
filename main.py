a9e327508e487110a8e1de2c49975a297bc1e385
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:14:50 +0000

    broken Ff reply desperate respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
