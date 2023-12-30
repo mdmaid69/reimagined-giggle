46f997d7ce94b9e006f9177906a6738a19606487
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:23 +0000

    thought treat reduction break lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def clear_array(array):
-        array *= 0
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
