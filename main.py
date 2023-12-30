f597002241fe63e818279e060ebb3a0a19e2f9f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:29:28 +0000

    connection soon rubbish used cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
