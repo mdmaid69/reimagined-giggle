6b5d20089982d04e962e1c7c4f46b89cdbbd51a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:17:01 +0000

    explanation meal telephone release complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
