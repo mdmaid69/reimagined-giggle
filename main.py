f611c2f19df178536e78f29f5535cf55b264929e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:26:16 +0000

    order often explain clear nature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
