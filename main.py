c946fa4302ee8913d050fd08c09ee189eb78290c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:06:15 +0000

    set pick dish long steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
