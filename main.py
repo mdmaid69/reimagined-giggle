b355e3cd91963a37ab9f2ab151fefbeaf690c2c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:05:52 +0000

    wing trade common agreement information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
