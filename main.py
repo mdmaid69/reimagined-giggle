83826c068f03f6ff1132164b282e6bac539276ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:21:40 +0000

    turnon chin reasonable fact rough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
