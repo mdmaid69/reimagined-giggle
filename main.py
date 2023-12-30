fae65d9f8590e2009cb7c102c90c3bde10a78aa1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:49:30 +0000

    fond warning interested over defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def square_number(x):
+        return x**2
