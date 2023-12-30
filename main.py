afb4f7820266f5324ec3f42ea744c8db5384548a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:50 +0000

    everyone forgive forward tired confident

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
