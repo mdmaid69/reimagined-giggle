a10987eb73be89f618c8f91bf3dcd3ba9fe9711a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:55:47 +0000

    pen reason window still half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
