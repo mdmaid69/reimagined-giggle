b5dd2ffce6b8f36aae3d503a8b0dac26900900dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:58:07 +0000

    name thinkof prove reaction recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
