85c23e8ccf2dca2b204192c553e187a2319442c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:32:07 +0000

    narrow like opportunity Cc difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
