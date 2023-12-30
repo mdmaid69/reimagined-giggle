b40200bcbd3a7c00cc6ec5f6897fb4003eda22cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:20:19 +0000

    declaration steam why dress throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
