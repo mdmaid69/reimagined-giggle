6b48d2269d7b9ec7b781d62744df9a152f354e13
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:27 +0000

    soon patience agreement human live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import re
-def find_all_occurrences(pattern, string):
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
-def find_max(lst):
-        return max(lst)
