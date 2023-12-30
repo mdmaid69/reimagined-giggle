a96b64b8ce8eb87a8171090b317444254f3e8af2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:46 +0000

    accept Qq field tape fasten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
