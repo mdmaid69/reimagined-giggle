4f1544294422035611868cc159f6b30392b4a021
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:13 +0000

    571enough only consider know trousers

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
