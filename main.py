56badcfccc45fbdd15daae3d75874efc9673150c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:33:08 +0000

    difficult impossible house contact cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_work(force, distance):
+        return force * distance
