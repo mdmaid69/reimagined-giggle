c42050af30662d3a76d52f0acc2b7cd63dc8a0fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:07:18 +0000

    bag turnoff flat noisy rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
