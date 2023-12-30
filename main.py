8104134647e793a0266058f99152b235a56a830f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:46:34 +0000

    back major loan petrol congratulations

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def clear_array(array):
-        array *= 0
+def create_user_string():
+        return collections.UserString()
