937427db7263ec8f105bae333bdbb9353dce6cf1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:34 +0000

    praise uncomfortable lonely general survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def multiply_numbers(x, y):
+        return x * y
