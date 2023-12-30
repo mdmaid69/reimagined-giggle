379400588e32ef98a5ab3e3672b4f727df365053
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:49:39 +0000

    prefer short Pp in what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_power(work, time):
+        return work / time
