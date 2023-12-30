9a9223ef49594b949e7c30a3db5c5f0f4ed9e707
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:26:58 +0000

    fear could feel 566end government

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def add_numbers(x, y):
+        return x + y
