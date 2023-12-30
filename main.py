560db5ec7295908b30bb59f25cda9a5c74e66a91
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:23:21 +0000

    mend each room minor similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
