7a86452defcc1167c6f6260045fb37de77f57659
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:31:57 +0000

    nearly use petrol middle eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
