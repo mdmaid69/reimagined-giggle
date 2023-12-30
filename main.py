464b8687078d5b275b3c5993e3c05278411435d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:58 +0000

    afterwards ear limit unusual tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
