60a796a5370264b09d7d821f98f10f1d3f98b1d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:18:47 +0000

    there spill cross increase ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_set(array):
+        return set(array)
