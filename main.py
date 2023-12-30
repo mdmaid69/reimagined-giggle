f1a75e40effd4ab266eb0f1c0b204c2417424c22
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:39 +0000

    thinkof wish flood slip miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
