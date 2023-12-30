c3f89173365246bed30c7be56e3f399247379871
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:24:43 +0000

    turnaway distant broken thinkover watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
