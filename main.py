9444fbce84daf41105032336488326122aa3aba1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:29 +0000

    patient dark on black surface

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
