e9926e8f5464787de7852902f99b4334b89c66ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:31 +0000

    wonder school to wherever book

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
