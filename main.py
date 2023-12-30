7ad318339a63acf00eb53b017c323cac9c5460d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:47 +0000

    sudden noise comeout form without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_string():
+        return collections.UserString()
