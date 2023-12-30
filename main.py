d5927988fe33f9d67dbe2ab5f292420509f67fdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:43:48 +0000

    hide unless absolutely market fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
