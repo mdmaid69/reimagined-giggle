2ffc9514ec2b8233adac3e977961d4b73dbaae3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:57:32 +0000

    birth smart file entry knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
