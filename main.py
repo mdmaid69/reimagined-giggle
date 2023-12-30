61d7084d3343f43b79c123605a7f2d3ae15f2b17
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:57:47 +0000

    bag pupil black present generally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_user_list():
-        return collections.UserList()
+  def square_number(x):
+        return x**2
+  def is_odd(n):
+        return n % 2 != 0
