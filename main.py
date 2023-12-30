84eb1362c47285f1214849f5316b3827a8d8ddb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:38:54 +0000

    this trust least pure regular

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
