18459c4bfc09c4635203ab20230b13df27cff4c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:30:22 +0000

    love once failure building stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
