f400107ca549e3097009ba784efd4c3883d23640
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:21:16 +0000

    birth thinkover noise tradition concentration

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
