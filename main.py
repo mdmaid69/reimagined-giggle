5c9d7dd6e41f5f015dacea2a873cd7366427c946
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:05:13 +0000

    shock share hospital holiday Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
