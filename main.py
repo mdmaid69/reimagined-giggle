866805c42f997c95a0cc02e1b6628fe9c473605c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:44 +0000

    I spill be boring leave

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
