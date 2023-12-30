6de917cb474f85c75cff12d82c8e893bd762d98b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:48:54 +0000

    admit travel us round usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
