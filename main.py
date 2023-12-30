69af5280411fff3cec5ae8d0a01c1ea608056a5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:15:39 +0000

    decide must cheque blame create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
