dc1088ffd27eb0c10d26fa1d245f2371ed36e163
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:48:09 +0000

    kick extremely rare runinto whistle

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
