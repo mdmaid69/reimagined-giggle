01a584150ce26ad4023161afe111a44832d2d2a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:37 +0000

    explain morning apply entrance surprise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def divide_numbers(x, y):
+        return x / y
