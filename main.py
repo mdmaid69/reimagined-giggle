10f0ee070176257f2bbf7ff8fed51e578793125a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:59:21 +0000

    brush experience computer there pride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
