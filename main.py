3f6771ac1ca33fa77a38dc0149196d4dbd5a9856
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:43:46 +0000

    hope Ii feeling Gg represent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
