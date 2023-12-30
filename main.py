1fd276ef9dfb844b9fadb433577903a281f84f00
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:14 +0000

    future formal ilovedicks part mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def add_numbers(x, y):
-        return x + y
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
