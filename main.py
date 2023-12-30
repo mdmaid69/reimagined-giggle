c3a222fab31b28797c94ce33ac2f8618f0337174
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:33:09 +0000

    fail turndown tense guess 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
