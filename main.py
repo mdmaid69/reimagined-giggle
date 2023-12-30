9a2fc38ea13c1eeb9ae244afeffef0141adf2ab1
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:13:06 +0000

    sick die old lead note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
