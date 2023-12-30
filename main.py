ac36e1727121531abcb318b7e9bba9d2f5e4a5e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:04:47 +0000

    branch depend statement thinkthrough large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
