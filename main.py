265dce18fccb652d3846f088cc86780e0b3028cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:04:01 +0000

    punish relative laugh damage indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
