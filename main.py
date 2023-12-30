3d787b8a7d43428d8613f071efc998d717142f64
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:53:24 +0000

    touch extraordinary satisfactory forever listen

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
