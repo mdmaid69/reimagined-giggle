f1290c9dd87fc53476dce6a548e40a6de2f6e24e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:23:24 +0000

    fork poison Ll wind very

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def convert_to_octal(n):
+        return oct(n)
