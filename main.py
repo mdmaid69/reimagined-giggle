86da1f48968e017fdf2e252b192e194f6b8bf05c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:42 +0000

    whether shoulder wild special place

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+print("Hello, world!")
