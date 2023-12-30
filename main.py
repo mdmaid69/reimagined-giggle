a21ee0e67ef2d01b560b3997cef5577a9532aaae
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:31:30 +0000

    ready active office destroy defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def generate_random_sample(population, k):
+        return random.sample(population, k)
