45f5e48796d64c3d750ffe716f03692a3171a275
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:05:55 +0000

    wrong man computer uncomfortable down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def convert_to_hex(n):
-        return hex(n)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
