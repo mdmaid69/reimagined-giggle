043160bc391a52a46f631d0370e58f9f9a5ff5ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:04:00 +0000

    convenient seriously shoot everywhere wipe

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
