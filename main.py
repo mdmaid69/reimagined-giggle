189b68681c9d42b9e489e3d4f6763fdbb96e6a16
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:02 +0000

    disapprove display lost shadow copy

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
