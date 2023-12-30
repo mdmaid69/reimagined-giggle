a30bbec46093cd6f292f10a118b08ce3d554c7ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:17 +0000

    relation rush prepare support give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
