8a80b0abf17aa2124cb79abb5822789014d1715e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:55 +0000

    goup machine interfere 565end block

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
