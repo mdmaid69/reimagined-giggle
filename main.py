81659a57b590a16181ffe562a8c93340c8a24d82
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:38:25 +0000

    slight please race question pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
