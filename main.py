48ab3cd1a452ba9fb9a997f5db073550690047de
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:38 +0000

    his allow miss opponent square

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
