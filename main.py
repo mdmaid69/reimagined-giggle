bf690f033ad09e6de6409cd91cbd33da4ae575ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:34:44 +0000

    fetch aim offer argument will

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
