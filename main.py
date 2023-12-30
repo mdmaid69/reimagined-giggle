98b0ce1fe92ef1a2170b78568794bb1313544783
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:35 +0000

    rest convinced Kk safe ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
