6630515c33f8ace8d7cf714f4a5cd5abdd34ba5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:07:33 +0000

    entry talkback hair debt care

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def multiply_numbers(x, y):
+        return x * y
