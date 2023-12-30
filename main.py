7834a4323f0fdabc5446981e74433fa859a2f1b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:15:12 +0000

    use slice mark flavour hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
