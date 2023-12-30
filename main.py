f8e0f6b2de6a18c351d0d566ac7f93dbd931a3e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:37:59 +0000

    apply interview neck recognize bound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
