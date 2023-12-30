0cbdb10b8e70703467c0ee68e939eca6109c55cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:34:42 +0000

    describe duty date male impressed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def sort_list(lst):
-        return sorted(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
