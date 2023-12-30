988e9455267c63a7a66307dde19ee4098eaab9b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:27:19 +0000

    twist speak turndown listen hollow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
