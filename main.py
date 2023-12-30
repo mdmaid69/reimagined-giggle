a4427eba6ef12b76a5d92fa558136f09c2107db1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:40:04 +0000

    laugh modern peace between trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
