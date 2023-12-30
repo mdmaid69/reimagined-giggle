f0bd08d80a1c103b84ecec0046af4a94a2f0313a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:54 +0000

    concentrate exhibition ashamed meaning pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
