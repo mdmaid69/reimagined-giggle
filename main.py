070db7fc808463e18a343b5b76f5756062a7f708
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:44:06 +0000

    house bread comeback fill heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
