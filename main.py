94555a4172506e39394d319b505262aa2a709e20
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:40:36 +0000

    apart majority Rr famous watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
