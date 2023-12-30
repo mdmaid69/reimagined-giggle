50cf57b6180e799e30ce487244f852f455a7bf07
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:00:10 +0000

    Ff lose bank seriously eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
