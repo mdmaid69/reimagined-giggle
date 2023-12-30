c30cf6eb756f6f55df759e13cae81ba5fef555b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:12 +0000

    any tap scheme mixture telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
