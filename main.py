6a8c90933ed8d321d544e3a79caaf6f47859b2da
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:08:32 +0000

    collection ground 572enter firm for

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
