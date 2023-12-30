3ab256fc4a3fa77e396397517d22358ae41b6717
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:19 +0000

    post handle cheque dry statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
