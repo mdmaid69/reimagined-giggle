577a7a937ea5089b8a918f726b546935320f6f94
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:05 +0000

    dinner scream may fine fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import random
+print(random.randint(0, 100))
