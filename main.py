24502baeef93438d4f84ca6550fa62e528f1ee8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:26:12 +0000

    suppose comfort me firm precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
