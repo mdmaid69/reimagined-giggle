366b9e2fbc3d80d8fdf4e990a40806276bc6b408
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:30 +0000

    comment powerful represent share precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
