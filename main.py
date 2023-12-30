d8b018fce75289e71735178fc3f03e9b735bb557
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:32:27 +0000

    promise attempt later union tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
