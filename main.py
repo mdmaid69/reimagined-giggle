846afff0612e6b590f02b160555a4067e0642e30
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:23 +0000

    behave breakout light club label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_array_to_bytes(array):
+        return array.tobytes()
