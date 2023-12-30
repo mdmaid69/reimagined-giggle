639a21a654f62c79c2f1a2496eef036b18e93b35
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:05:03 +0000

    row judge round sound protection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
