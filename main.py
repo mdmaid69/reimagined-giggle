806ddfa8a639845a6f68734c9313b1f171b424e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:32:28 +0000

    criticize hungry if still point

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def convert_to_octal(n):
+        return oct(n)
+def count_words(sentence):
+        return len(sentence.split())
