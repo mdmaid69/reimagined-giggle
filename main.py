734906b03769cc0c49250cbd9beb0dc559e760fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:50 +0000

    brain club dream really few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+text = "Hello, world!"
+print("Words:", len(text.split()))
