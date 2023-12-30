3ccc3dbb2a092893ffb6e618e8313183ac2b0660
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:11:01 +0000

    crash attempt hang question note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(x, y):
-        return x + y
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
