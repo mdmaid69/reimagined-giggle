5cc4e552d6867c26aa0b24ecaa92134883f9ef3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:26:01 +0000

    cross ring instrument situation body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
