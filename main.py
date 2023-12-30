83c089b1c7b78cb304814b318a9b1dd84bfde9da
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:01 +0000

    Rr charge curious protest favour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
