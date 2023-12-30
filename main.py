a39f9eddc6f2285d108ccb0af383431871db2b65
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:54:49 +0000

    describe preparation beautiful contact keepoff

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
