a648502668fb574305c505d9037a096a350f0fb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:54 +0000

    watch to limit fasten damage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def count_elements(lst):
-        return len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
