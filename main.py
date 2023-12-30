303537e728f8e55ac41caf016e1d81a9e0ff6ba6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:54:40 +0000

    tape level confidence runout wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
