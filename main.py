b6b2c50f4882ab71432e7a3e52cbbe8ef3977c3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:16 +0000

    up exaggerate flow fashion upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
