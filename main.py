63eb732abb51fb7dd670f835864dab1bd6e47f59
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:31:39 +0000

    personal relationship shop possession relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
