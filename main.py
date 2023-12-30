c7e4a34bf040a2d1a8ad39485ddb363d5a0e21e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:07:07 +0000

    piece snow lean broken between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def add_numbers(a, b):
+        return a + b
