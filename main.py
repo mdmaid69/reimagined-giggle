34f4a5d596ab411ac265df6d37515d771ec4f833
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:46:13 +0000

    worst public broad big calm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def clear_array(array):
-        array *= 0
+def find_min(numbers):
+        return min(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
