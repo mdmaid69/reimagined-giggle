2925b12971a2834abdccddcec98720bf16c8246c
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:39:31 +0000

    ourselves dry congratulate car father

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
