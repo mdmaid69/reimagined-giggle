17b3ddca9d2ca7749ee107e0cf91c8ec42332df8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:30:45 +0000

    different appearance lookup government trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
