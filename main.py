3a0c00cf8cf51e6c803356112ae61d2875c68740
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:15:13 +0000

    mess word contact near difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
