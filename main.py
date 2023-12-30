41687f50614aa2ff593cfd05625ae0fd3d88bd3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:31:52 +0000

    holdup react wonder comeout goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def find_max(lst):
+        return max(lst)
