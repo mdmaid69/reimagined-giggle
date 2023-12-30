c961f33d19029ac0e8ec53f21b972c3757403683
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:52 +0000

    sky act runout underneath centimetre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
