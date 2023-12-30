68dc131ca3f222aaf026664829dfcb176b147fe1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:27:34 +0000

    thank tax sink answer concentration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
