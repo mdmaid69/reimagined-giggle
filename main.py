d16a3f6d4d130168cdf03532078aa9033e9f5f7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:43:12 +0000

    heavy cool congratulations Oo sit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def append_to_array(array, item):
+        array.append(item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
