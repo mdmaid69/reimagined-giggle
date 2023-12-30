25c0f3a9c9d5088834155af79da7f63b11410d95
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:03:43 +0000

    exploration callat attach opponent side

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
