5a1c4171c66ae8f46ed90eb5b265ee3b7f405b71
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:45 +0000

    tense clean draw sound earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(lst):
-        return min(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
