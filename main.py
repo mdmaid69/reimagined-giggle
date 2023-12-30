820227d990144baa0c70d92abd399b3c6a629403
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:01:03 +0000

    storm bringforward anxious passage on

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(5):
-        print(i)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
