b00e36d59db2d39ad56d49436609e977f1593a5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:08:20 +0000

    sound trouble dear giveout brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def cube_number(x):
-        return x**3
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
