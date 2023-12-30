94222f74852926fee27984cbe333af58017c668f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:16 +0000

    prepare away poor sharp Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
