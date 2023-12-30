bc20456aed9ecc49df7e9b6a3ca857d563a7ba6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:04 +0000

    list strict take plenty attractive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
