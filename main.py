ee198febf343fa791092fbd7f6db5c22cb819ba6
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:21 +0000

    quick figure correct shop cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_floor(x):
+        return math.floor(x)
