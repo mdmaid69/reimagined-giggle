e05c7077fbbcc8bfc8217c4486ec55fbe0e85aa5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:35:25 +0000

    balance after swallow check murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
