4bb9ec57355f8069f076d06c6faf46c8da38b535
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:17:50 +0000

    welcome ever fond invite rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
