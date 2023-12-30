b6b176d1b8ef4fcc294ec0d2802e01d249d58a64
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:09 +0000

    downstairs return getback clean seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_octal(n):
+        return oct(n)
