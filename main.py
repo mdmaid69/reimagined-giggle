b7633088482deab1cba7ed7214468c90f42068b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:36:42 +0000

    wipe ill before fashion control

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
