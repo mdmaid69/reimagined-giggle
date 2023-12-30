a53a581089f55676c91bfc0a36970bb6d8a4bb60
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:57:53 +0000

    their idea us read possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
