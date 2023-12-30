cecbd1676b6cbb7cfd93df27088525ea886a151e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:01:43 +0000

    properly consist only base combine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_min(lst):
+        return min(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
