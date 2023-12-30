ea862aafa5af6e3c2012d7c39a7844e6e58086ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:08:48 +0000

    cook appear neither convince fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
