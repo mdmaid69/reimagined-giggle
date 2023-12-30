1651c1a62345039b4d329ece42c28836fd1a45a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:54:18 +0000

    previous war roll effect test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
