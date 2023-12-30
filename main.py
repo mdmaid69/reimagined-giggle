ad1ce9c41cfcbbfaccbad88838aa1de4bfa90c97
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:11:12 +0000

    exhibition worth address somehow cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
