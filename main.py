8105378a3c9d1d8ee171e8e526dafc7d00c7411e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:22:42 +0000

    meanwhile opportunity spot concerned mention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-print(sum(range(10)))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
