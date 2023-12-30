f158f653461b9c9c31631a9c82615090d47c58f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:43:52 +0000

    cheque foreign feel lunch gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
