f0283b50effd028880e37316bffa03fc00f633c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:44:56 +0000

    hurry mistake visit discussion again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
