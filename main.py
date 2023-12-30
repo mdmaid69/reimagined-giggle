001e7c84ebff7a31e1cd4aa6af56cedc7e8132a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:22 +0000

    sting workon lose name deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
