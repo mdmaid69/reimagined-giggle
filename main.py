6ad84650a24d1106a3561c6b85b5b75c8b3f52a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:54:03 +0000

    develop protection lunch lay from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
