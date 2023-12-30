d3c847dec4c5ea8b2cf97f3418ab5f91fbb9f76c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:24 +0000

    family among rather watch sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
