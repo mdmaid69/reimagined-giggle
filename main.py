0d0f8e1970b754f0ce1d8cd12e400d8ad37c2c7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:43:20 +0000

    immediate dangerous leg couple loss

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
