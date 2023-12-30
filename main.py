3c8a7d16db48153ccffcaf18c6556ef5e44f24a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:45:22 +0000

    tune evening total dish forgive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
