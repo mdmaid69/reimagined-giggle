ed2e3aaff9a465dfdd7efc715e5f77d8dd365a26
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:53 +0000

    wise fast risk flavour strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
