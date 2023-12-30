8eacbbe7aa065970a08466dae6f4a72b2f93179d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:54:39 +0000

    bury any stranger always impressed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
