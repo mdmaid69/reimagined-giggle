63067b45cdf81e31bd749ca2470e9aff90099801
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:23 +0000

    average experience calm beside anything

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
