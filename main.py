78753f24ac97adad170b030d7bc6bc391e7b05cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:51 +0000

    dinner breakinto thick hospital lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
