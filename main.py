7479e387c43df331594f5b6607bc3aa4ef22dd92
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:42:31 +0000

    shop sport want class maybe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
