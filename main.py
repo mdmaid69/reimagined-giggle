00a74507c765a85e5c30b99c4ce5634f11254562
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:58 +0000

    argue neither description art please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(10): print(i)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
