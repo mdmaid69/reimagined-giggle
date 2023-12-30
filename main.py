250bddb170c0b23c83d1c6eea42fdc22b20485e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:10 +0000

    immediately him distant injury previous

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
