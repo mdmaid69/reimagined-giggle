0c54a48cb86e29cb2b692708669f802b27c62902
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:47:40 +0000

    window sure report callat fresh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-for i in range(5):
-        print(i)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
