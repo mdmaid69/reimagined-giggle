7a0f078c873d47db9727cc1f7f510a3cd45784cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:12:57 +0000

    move punish tip none thank

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
