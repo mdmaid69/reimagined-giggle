8c1ebce7900725a775c2fead93e09f52dc74f1ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:38:25 +0000

    short wake 564encourage major callup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+print(os.getcwd())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
