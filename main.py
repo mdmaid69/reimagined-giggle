a811f4a6d7f6389afa6419e8518a7e5ec2938c72
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:22 +0000

    complaint young tool concerning else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
