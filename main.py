48646719f76607a56c901d4e272492e2a692aaf9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:56 +0000

    unfortunate everything thank wherever help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
