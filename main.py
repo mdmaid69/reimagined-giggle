6620efd160f9fb5626bea4ebcd104678cb5d93ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:21:14 +0000

    argument drink number odd pronounce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_acceleration(speed, time):
+        return speed / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
