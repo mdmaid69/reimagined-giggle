96802b0808e94dc128ffbe48ad226fd9c6581a0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:23:34 +0000

    carry over kick print put

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
