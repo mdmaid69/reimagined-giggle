04ff982fcb1afd9acd75c57de2be9fe78ea010bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:26 +0000

    recovery unhappy cost relative always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
