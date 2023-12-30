221e3cb5baebfd368910cc629f11a4dbd8ea3bfa
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:37:19 +0000

    person substance protect strong search

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
