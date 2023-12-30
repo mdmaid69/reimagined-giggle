3f520e86eca9fd5faadb16c5a193897ed87eb5cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:51:18 +0000

    obvious club comeup suck swimming

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_hex(n):
+        return hex(n)
