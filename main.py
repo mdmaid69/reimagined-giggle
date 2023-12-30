ee8483a5a4e3999787fb5a7b9f9b12bce6b731d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:35:24 +0000

    pupil recovery loud shelter scare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
