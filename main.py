3354f1da4625f5ec7bd4b99c7fc803bd30ad55d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:03 +0000

    official long operation stupid whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
