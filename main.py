ab86cec810d38f7248b1d3cc1bfbc6d35f4d95b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:37:39 +0000

    broken scrape some deaf pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
