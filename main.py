df7e9c8b58896922a06f4d88b905f55553d2ab88
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:15:20 +0000

    hair stir advantage sweep spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
