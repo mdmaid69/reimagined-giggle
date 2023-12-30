b92f02bd9b6479e206797518aea802c001d0d59d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:59 +0000

    damp several happen Nn possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
