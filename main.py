83671c74497cbadeee9946b1e4072892da626ddb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:52:07 +0000

    behind whistle why movement forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
