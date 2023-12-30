81bbddbce2715341346548fb22639d8bbf901070
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:43:35 +0000

    workupto mainly needle meet experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_max(numbers):
-        return max(numbers)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
