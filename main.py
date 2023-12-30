1c6bec264ead6a95a87faad1cf231ba771b9fd00
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:58:44 +0000

    slice dream round attitude little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-from collections import Counter
-print(Counter("hello world"))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
