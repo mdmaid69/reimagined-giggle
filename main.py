5e8979ef5a0ca35c11110a684d0daf29a6e98a8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:14 +0000

    plan payment wrong means welcome

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
