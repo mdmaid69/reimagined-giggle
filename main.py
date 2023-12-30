84952a8245206906d890af0013c831bc52d4ef87
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:38 +0000

    ring murder about close result

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def add_numbers(x, y):
-        return x + y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def multiply_numbers(x, y):
+        return x * y
