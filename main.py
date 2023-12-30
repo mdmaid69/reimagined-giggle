3a721b5e4ca934b2e319f18b9f65738b5fc087be
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:10 +0000

    takeoff petrol again 572enter peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
