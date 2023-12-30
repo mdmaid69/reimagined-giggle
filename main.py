bca856f949d19e8b1478b55eea41881b17be4115
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:46 +0000

    relative nor within Uu success

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
