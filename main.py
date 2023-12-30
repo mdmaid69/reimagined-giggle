6a4aee86f6022a81f5d503dd28cd7e80773acdf1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:15:08 +0000

    swallow stupid pattern swallow protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
