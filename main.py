cdb9c6ca81825437748de829f4b3f48918fe6970
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:01:05 +0000

    straight guide high beyond shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
