ef1b1d18e606ad622ffeae5a42cfe750ec296398
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:01:24 +0000

    people makeinto visit actual company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
