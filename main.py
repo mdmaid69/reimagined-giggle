7b6d0dd86861a43df06c37f97da165732efebada
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:21:17 +0000

    pause Itself because sight bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
