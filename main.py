b1674adbaa46c8c8a80b55666e893e99344b6b96
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:11 +0000

    confuse cost sometimes arrangement remain

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
