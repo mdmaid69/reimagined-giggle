953c96f2b86456a4ea97c255378a140f874efc4a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:21:56 +0000

    address share seem amount difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-print("Hello, world!")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def convert_to_binary(n):
+        return bin(n)
