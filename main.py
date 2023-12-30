9b9750bf8f38ea45088a7a935ff6ba017f04c7f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:59:56 +0000

    fashion sale report unite passage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
