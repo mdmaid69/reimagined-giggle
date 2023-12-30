eef00f99c66b1b753760ee096ea1204e9875637c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:51 +0000

    become sale evening sensitive aim

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def add_numbers(x, y):
+        return x + y
