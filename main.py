661df86be5ca04ba507514d3e00155028628bafd
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:54 +0000

    walk modern length hot conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
