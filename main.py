8e70a3ec9eacbddde3e76a757fb2f5f628991897
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:58:04 +0000

    quarter frightened most Cc invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_power(work, time):
+        return work / time
