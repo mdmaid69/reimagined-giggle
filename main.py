52422e6ce62571905cd5d068bf5e779bc27b6a68
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:42:04 +0000

    danger dangerous exaggerate criticize fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
