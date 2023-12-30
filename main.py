37bd8f79ad622279d7caa597a97b2679f1f402dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:40:40 +0000

    weekend control tidy reach refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
