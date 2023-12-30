7db22d9bc7d693181cd306307ac8aaa76f66e8f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:40:51 +0000

    deliver call burn manage cope

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_floor(x):
+        return math.floor(x)
