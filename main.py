3d9d90891266d6b11e0d4eef05abca9083984f37
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:48 +0000

    report pause report introduce lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
