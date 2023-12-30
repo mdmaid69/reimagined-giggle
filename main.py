a2373e6b6b09148bfb9969bddd2314068b1ff0a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:34 +0000

    culture hurt string polite fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
