caf8850048057cb1026ed17e8bedbc9a232ce11d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:47:06 +0000

    mistake after warning over save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
