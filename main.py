c52349395b7a6dc77db618ee2f0ba6c0040bc4fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:56:17 +0000

    favour meanwhile pretty comfortable allow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
