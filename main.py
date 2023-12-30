1a2aecda62eb2b79e7e2d9d1ac4d972cdc9a62aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:19:55 +0000

    concentration pleased bad use plastic

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def convert_to_octal(n):
-        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
