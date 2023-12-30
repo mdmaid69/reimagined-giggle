80e0e43d8e07cdbff7ef1c78b348d7e6653eb373
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:48 +0000

    pour kill interest knowledge oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
