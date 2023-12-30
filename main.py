5a5f8fbe4bae3a1123d3228c30669014a90ffa30
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:33:30 +0000

    leave upwards cap deliberately breakin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_pressure(force, area):
-        return force / area
+def cube_number(x):
+        return x**3
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
