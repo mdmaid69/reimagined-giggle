ef44e6d8f0294b692b4a8ba1c8343fb1fb5d25e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:46 +0000

    in thought friend such dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
