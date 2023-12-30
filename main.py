ce9e582ad9493afa7c0e8b50f68e8acbe5240d9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:42:35 +0000

    claim motion lookup apart print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
