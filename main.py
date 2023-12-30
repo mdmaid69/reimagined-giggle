3d1b4b2193435daf6d93a1bdbe2bbc4cf938902b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:31:42 +0000

    discover meal brush goaway false

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
