887aa1114e8c8bda83f41c9cf9cf88da0e6f2af6
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:28:24 +0000

    force and makeinto warm consider

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
