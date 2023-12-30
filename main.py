246924a5515c69c27dee2efad13e3f7b17d46c23
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:00:58 +0000

    only previous run track fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
