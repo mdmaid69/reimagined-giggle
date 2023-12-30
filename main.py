4d8456df99da4d5da91b4e8c539155856c4295ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:28:01 +0000

    reply sleep score ours inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
