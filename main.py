07b61a9d7f82cd0b767c306deb85d35c4721f797
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:12 +0000

    fit breathe measure grand print

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+print("Hello, world!")
