b59485ea2c167f86413a1340c866198ef8c4daf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:27 +0000

    difference group fit remark cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
