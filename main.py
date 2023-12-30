e899c2fa7b321979dfd32790faf71a26bf69fbc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:14:38 +0000

    now enthusiastic left second eye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
