2111df7848ea2a1f6bfc868b4fe2ca4c50dcb2eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:36 +0000

    delicate advantage run no bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
