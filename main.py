75be67ee9a27670d84aff14e85c5e5ac488aefeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:27:16 +0000

    takeout except learn scale high

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+print(sys.version)
