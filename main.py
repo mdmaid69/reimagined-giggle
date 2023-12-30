83cd5c6d1e619179ba58bc947d99f29bafbde3c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:44:48 +0000

    please cost original neck flat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
