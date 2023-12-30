09f36d8b837d34c894c1fd7f835f030f1caa34d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:58:23 +0000

    if thing excellent Bb breakdown

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
