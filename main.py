e9e1ee6ab42767fc305d5c9a81f92be7cb89e871
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:44:22 +0000

    interrupt fine wet opinion Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import tensorflow as tf
+print(tf.__version__)
