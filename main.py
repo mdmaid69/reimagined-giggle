c5f0d6e63511fcfc6957aaaa863ad4f3c678d898
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:56 +0000

    journey comefrom approve quiet chief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
