18d3fe985ac8bf9430d35458bcf423b4954378bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:36:44 +0000

    experiment value strong mind lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
