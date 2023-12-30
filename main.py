0f22a5c7673299c097ebc18ffbe5dfe47e294e35
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:31:14 +0000

    obtain expect pocket protect something

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
