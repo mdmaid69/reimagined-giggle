a8a4e1fea47d9491a9c61f304fead9ac578a2997
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:58 +0000

    combination smart offer calloff project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
