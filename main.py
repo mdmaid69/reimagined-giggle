9945464c472c37d71a6ea8edb60b48aef98f5b3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:44 +0000

    calmly basically lonely afraid bath

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def reverse_list(lst):
+        return lst[::-1]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
