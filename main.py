51f7ad6c1e20ada7ffda90d0f3744863b7ffb7d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:18:03 +0000

    attitude settle forward run demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
