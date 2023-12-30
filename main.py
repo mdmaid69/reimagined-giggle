5286e233a5ea455139a22c3e098d68f3fdf87bcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:01:00 +0000

    argue proper imagine majority muscle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
