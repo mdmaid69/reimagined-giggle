1486237aa90d4f249970d2a12670b46aa21fca07
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:47:38 +0000

    clue thank national impressive wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
