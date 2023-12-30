8ad87faae8232c1681dbd33a393883a7d90edf2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:55 +0000

    talkoutof silent run ground waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
