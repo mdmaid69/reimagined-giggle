3b659bf71454e4043dec35f88969b8e0f4b94cbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:11 +0000

    top sound space shadow strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
