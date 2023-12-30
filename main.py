9e311e7d6f29bd4bcc5542af9e6fe152db75ecce
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:29:12 +0000

    wing concentrate confusing record turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
