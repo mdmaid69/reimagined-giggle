85e9bda2556a64935df7d904e5a6f1110a99bf4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:37 +0000

    advice luck cheek dangerous deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
