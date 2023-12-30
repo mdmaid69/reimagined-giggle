326544857ae01f4e0b12349a0003b88a7e4e0e5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:47:34 +0000

    right game colour mad yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def convert_array_to_list(array):
+        return array.tolist()
