ea7b3af5454dbff1d18aaba0df8512101adc28c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:17 +0000

    practice tear effort include exam

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
