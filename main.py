3ccdff5163425a111f7bf1a55deeb8a0e9881c77
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:19:36 +0000

    active paper large sweep tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
