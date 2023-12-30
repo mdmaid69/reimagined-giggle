6b81953685f58ac288cf95c92ee3894dea112fa1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:27:12 +0000

    think various gas none too

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
