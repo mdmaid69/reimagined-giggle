400523315a96618ba1c178cb06098d7a514496f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:30 +0000

    anywayanyhow takeover land happy OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
