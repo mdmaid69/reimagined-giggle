86326ca190b72b75bcce82d19ed84452fbfde17a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:04 +0000

    ask tonight prize steam most

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
