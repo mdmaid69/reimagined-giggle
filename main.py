26f04a481bcced766136cc8cc40976be9c552ed2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:53 +0000

    reaction furniture club small ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_memoryview(array):
+        return memoryview(array)
