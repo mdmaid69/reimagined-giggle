f5d9bccee0b58b8bdfad4c8f42011a7eeeebf947
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:49:27 +0000

    greeting since taste last be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import sys
+def exit_program():
+        sys.exit()
