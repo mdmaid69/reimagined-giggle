7803c87876464b21b2cf1ced8e44852a0c3166c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:17 +0000

    choose alone father allow continue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
