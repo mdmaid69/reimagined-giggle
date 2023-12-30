eacfa6735e9279d31d652ba6dfe1a0fa3924aaa4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:28:34 +0000

    trouble sleep design comeback smooth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
