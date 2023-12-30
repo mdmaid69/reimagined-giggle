bcf0ca3030cc709a53eba1e26fa562887eb8414e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:22:20 +0000

    under ground under breath beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_username():
-        return getpass.getuser()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
