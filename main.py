43fb07e4ebde7e59a96ebbb10b36d13fad8814ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:53 +0000

    respect pour go rest catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_power(work, time):
+        return work / time
