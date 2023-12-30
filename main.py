40a17d71441af462092422dc8c4388317e26eb3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:00:46 +0000

    forever getout Jj react relationship

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print(sum(range(10)))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
