9012f6884030234aeb51ccbb4efbc9d131ba30f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:46:19 +0000

    circumstance mess type shoulder record

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def reverse_list(lst):
-        return lst[::-1]
+def convert_to_octal(n):
+        return oct(n)
+import tensorflow as tf
+print(tf.__version__)
