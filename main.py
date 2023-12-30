c22c0d560a8dbefc0c3f9f600644c04dd1aaa153
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:42:33 +0000

    prefer region whistle wait plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
