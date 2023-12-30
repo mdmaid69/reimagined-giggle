2965e19b1c21e2c73115bc19d03409035d06e775
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:26:39 +0000

    emotion moon usually development tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
