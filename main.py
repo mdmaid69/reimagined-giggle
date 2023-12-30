b7d4d0c75a464584dcf379156f6e22f514277a78
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:47 +0000

    plan goback(to) dangerous high become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
