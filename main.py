9653657617ce95d2d731e18666bd0b724e1db7b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:24:53 +0000

    recent stick chase law hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
