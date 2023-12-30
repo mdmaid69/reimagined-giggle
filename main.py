5ebb0da3e60a459cb29d61576d2a0b35dc7e5481
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:33:01 +0000

    future Bb smoke discovery autumn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+for i in range(5):
+        print(i)
