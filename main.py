fc453da4ca712a178711adc99331abd0bec36135
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:41 +0000

    legal sleep drop afraid mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def is_even(n):
+        return n % 2 == 0
