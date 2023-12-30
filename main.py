b35d352611523d3853323a5687492cdc483f1a19
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:01 +0000

    string quickly excuse lead remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def change_working_directory(path):
+        os.chdir(path)
