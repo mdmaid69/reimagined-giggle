d2fb026ba1213512b9439a6cbc580383e71bb58c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:20:50 +0000

    programme speed scrape greeting train

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
