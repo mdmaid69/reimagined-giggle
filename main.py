144fa432c5cde65fd0fd5458a4fd98aaa93976b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:40:05 +0000

    ignore hurt violent circumstance breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
