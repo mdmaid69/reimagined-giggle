c76d6e86a8ddea0e650cd554d38181e32cf20b6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:54 +0000

    instrument at shut squash few

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
