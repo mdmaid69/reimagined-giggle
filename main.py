72d5676a1ff3a7b4ac7719b199f01a82063ae9ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:44:06 +0000

    takeback future cut steam terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
