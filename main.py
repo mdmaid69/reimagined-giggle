13160d46189649e4bad38b4204e35ef879b48344
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:58:04 +0000

    under dress but accept spring

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_repr(array):
+        return repr(array)
