e643ffe35b33400e79eaf249415493852b17929f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:14:25 +0000

    regular breakfast far abroad new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
