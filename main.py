003ccdef65fab40f44a4e6fe4683f353b8be10dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:28 +0000

    clue hang turnout pile except

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
