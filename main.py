df08c0b71b8fa79f145c370f1f40e62820afc083
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:47 +0000

    card scale whoever lesson change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
