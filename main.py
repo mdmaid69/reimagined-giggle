b43d39340a21eafd2cd9ffb22d2da37404c540bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:50 +0000

    butter everywhere birth waste quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
