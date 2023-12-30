fe635917ca54d99b9ade714fc76f3ff59f988d3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:59:25 +0000

    variety inform night punishment fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
