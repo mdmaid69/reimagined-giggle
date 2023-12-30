2620b894239ea9658591058ef0cf1beecdac803c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:10 +0000

    habit bag surprised close mark

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
