6229db89daf12699ab579f31233ae34bad8968ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:03 +0000

    otherwise either otherwise home container

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import time
-def get_current_time():
-        return time.ctime()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
