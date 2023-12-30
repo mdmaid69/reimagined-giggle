ec3a572c9246692cb0118bab6753aaa83a9f2816
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:08:26 +0000

    direct mouth also wrong let

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
