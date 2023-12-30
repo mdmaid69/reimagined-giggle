6b88c11dcc1d8cc20dfd8672e7f340cfde33d55b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:57:20 +0000

    clearly work stamp noisy discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
