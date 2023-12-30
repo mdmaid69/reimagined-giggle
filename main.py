356ee83e491f8983e753975fc1438a477f438f19
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:04:57 +0000

    in still only right answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
