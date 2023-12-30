44a8b159aa33fb98547ebbfb79551685f06166f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:28:39 +0000

    amount house damage bet intelligent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def cube_number(x):
-        return x**3
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
