ca35eda91c21d5b787ca244973da5cd0a0ac74f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:43:42 +0000

    earth sink past indeed pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
