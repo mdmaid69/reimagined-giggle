937079a3082356c4d7ff1351fcef5a8f1bea02c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:27 +0000

    cruel land themselves interested rain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def remove_duplicates(lst):
-        return list(set(lst))
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
