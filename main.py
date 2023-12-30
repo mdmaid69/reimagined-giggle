7009cbd7dfcf1da07e635f2908c9cb375b785a75
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:28 +0000

    forgive silent reject climb course

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def is_odd(n):
+        return n % 2 != 0
