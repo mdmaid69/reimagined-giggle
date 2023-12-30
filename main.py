bb79c793c8e33ffc41c2c5c88e38b712f726d6bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:40:42 +0000

    last like certainly street something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
