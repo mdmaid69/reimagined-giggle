3af056347ac9bbc135f9c779ce3175ecfd85eefd
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:16:27 +0000

    some interfere people confusing sense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-print(sum(range(10)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_typecode(array):
+        return array.typecode
