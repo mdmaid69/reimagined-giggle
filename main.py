494e85e0c48beaeb237fd09a8b9d519fa43dd0d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:57:31 +0000

    us nothing development punish shopping

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
