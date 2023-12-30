ccd4f7e332284c02f095b5105b18d4d085c6167d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:09 +0000

    hand takeover smart pronounce again

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pressure(force, area):
-        return force / area
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
