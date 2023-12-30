0a0772560223dc7ed64c91bc4df41e13952f2325
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:43 +0000

    set interview physical comeup even

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
