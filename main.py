93071b58cf2905cb52e1300b0b00a42f508794c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:48 +0000

    miss blow everywhere above free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_max(numbers):
+        return max(numbers)
