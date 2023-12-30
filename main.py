ae4094a47ea1dbd2b4c899e821832071129f3648
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:58:41 +0000

    burst custom bin arrival idea

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import time
+print(time.time())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
