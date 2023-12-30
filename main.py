849c487fe53658f406cdb89eceb53455f30a122f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:48:00 +0000

    attractive oil variety comefrom contents

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def calculate_profit_margin(revenue, cost):
         return (revenue - cost) / revenue
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def reverse_array(array):
+        array.reverse()
