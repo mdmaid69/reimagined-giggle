6442e0eb52955083e5efe9498f5d8dae2ed858c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:24:32 +0000

    inquire organize neck flat late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
