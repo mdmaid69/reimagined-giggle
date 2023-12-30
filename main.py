cca1346c836207d7a5ac99c43894890abaae50ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:26 +0000

    ashamed simple fill advise otherwise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def count_words(sentence):
-        return len(sentence.split())
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
