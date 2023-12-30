512f3c6ac672204d85315d224de50ca03b880aa6
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:36:48 +0000

    Tt space treat crash copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
