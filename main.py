a0da39c02fb498be66f666e611eae2dc84297a47
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:47:28 +0000

    male front ready surprised different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
