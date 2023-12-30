4d85d12f2a24580449727446adb02c10251e90b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:31:18 +0000

    foot frightened report violent possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
