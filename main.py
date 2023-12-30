5209772d8e27376aa240c950406195bf6d538979
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:29 +0000

    tidy impressive muscle survive friend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
