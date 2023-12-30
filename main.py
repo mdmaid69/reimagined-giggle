7ec721719009b524a5f532ad0904f771e4c957e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:13 +0000

    regional hurt tie black dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
