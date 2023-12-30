c3d1f9656da33dc855ee169e2477f9cf8b31ff0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:09 +0000

    contain everybody result further defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
