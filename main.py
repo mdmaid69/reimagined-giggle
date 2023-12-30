e4a50eae37de835b1833557e81c25dec4e778c85
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:31 +0000

    comment drink money have ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
