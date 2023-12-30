f35063dcdfbff92728027b4f3db0ba2bfd27b763
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:54 +0000

    row unkind hang apparently force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
