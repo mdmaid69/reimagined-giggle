e6d0d49915d02c9d2a2ade3b7ea84952c176de9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:41 +0000

    then roof sight maintain pollution

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
