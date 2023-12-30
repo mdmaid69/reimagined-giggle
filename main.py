89914f7bd180a4eb0e46fb10937624ec06dd4e7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:56 +0000

    lookback(on) basic doctor test Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
