624ae3476f8b5221ce98fdfbffdba2e2839eddd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:55:42 +0000

    whistle fine neck ashamed rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
