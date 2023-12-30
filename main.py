bd5700d00eae1338995cae9b65071487bdec0f14
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:54:03 +0000

    wild communication lonely behaviour respect

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
