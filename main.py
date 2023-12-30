5c5fe8dfe8980ebf2023b086fabe810272f5b4f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:18:24 +0000

    advice fault passenger lead reduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
