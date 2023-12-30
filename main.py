73abfec075580ca22e81167a36f519e748e9f19c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:57:59 +0000

    zero stretch anyone begin relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
