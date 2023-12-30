17c22bfb37d80bcb08c358ae9bfb65c715986f7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:58:25 +0000

    regret other waste light Zz

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
