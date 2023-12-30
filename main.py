8ded98f7f624ca045c1e3b030d3a77200030a94d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:36 +0000

    less tell half move hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
