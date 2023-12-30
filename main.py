17305116926223d8a2215915ae3eba101a1d085f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:43:31 +0000

    reduce Ff first aim fold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
