b73edce505fa97ae54755ea07049ec3dc22594eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:33:11 +0000

    field set 565end offence mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
