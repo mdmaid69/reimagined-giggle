343542edc8237eb292886c271a60139a350ebe74
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:11:42 +0000

    passenger clear record neither wave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
