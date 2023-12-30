4793dd6d11882b67c6ef9617de1ca8141d1a020e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:41 +0000

    correction while book accommodation come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def clear_array(array):
+        array *= 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
