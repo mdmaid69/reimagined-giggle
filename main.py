f9b06dbf7e1b87c7762733348a5243fc5ccbf261
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:32:25 +0000

    without future himself juice apologizeapologise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
