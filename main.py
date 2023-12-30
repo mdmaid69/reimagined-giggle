099d32af825df5d0fcf27223202da924e34f3c99
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:42 +0000

    cruel and square attractive breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
