529d3483d22be4c391a56f77b5640c11955f09ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:23:02 +0000

    confident although crash increase fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
