05dc16d926f8a89aab34b3682444fa3689a098c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:09:06 +0000

    dead goon inform plate more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
