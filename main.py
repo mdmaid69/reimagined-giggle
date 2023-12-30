126fbd98c787d5656e19a2397f9d6986dbea842c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:34:15 +0000

    emphasize oil downstairs estimate guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import sys
-def print_python_version():
-        print(sys.version)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
