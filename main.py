01203e6cab2fe81065fbf5a18b2c3cc715981cc0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:57 +0000

    natural dance takeoff please wander

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_min(lst):
+        return min(lst)
