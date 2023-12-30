90f1b987e0874c43bc292ce80ec580a25cc4ae05
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:40:16 +0000

    mistake oil illness hurt for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
