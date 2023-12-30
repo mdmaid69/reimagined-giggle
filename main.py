9622e725a1bf6d683c97b4ecff97ca057c46fee6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:12 +0000

    thankyou sit to centimetre distant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
