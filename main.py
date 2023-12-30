8273b2d266bc2223ce4968752cb91edae79fae19
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:35 +0000

    congratulate fairly draw behind home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
