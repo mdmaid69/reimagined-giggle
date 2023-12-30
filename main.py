3902592d64d00c198a51817aba8bd699ded5e2d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:33 +0000

    fill centimetre trouble gate dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
