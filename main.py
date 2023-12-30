d7767b445c706f4b4e116a4c5a67addaeb409a06
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:02:35 +0000

    terrible release car grow find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
