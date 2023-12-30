9f141bc4fe49ebe100d54fac87f09b92d3addfab
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:54 +0000

    suit limit far vision poison

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
