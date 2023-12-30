3c5840ce64408b96fc9a990451ebcd6babb60b25
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:21 +0000

    smell plastic you law goon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
