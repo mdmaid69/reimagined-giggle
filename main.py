32d501349ffec82ca4ed16b94bdea383b005ada5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:55:46 +0000

    hit idea whom honest opinion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
