564511246a5150647bf4b3bbc60d53f3864eb004
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:40 +0000

    respect gift create development there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
