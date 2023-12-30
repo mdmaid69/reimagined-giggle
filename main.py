be5d061e61a5a2ff83b677a4bd0dc182f2ebbc75
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:15:18 +0000

    cut strong fasten single achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
