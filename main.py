ae611ebbeece0fcc31058787fca4e0591bcd84b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:02 +0000

    sad trip Gg threaten smell

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
