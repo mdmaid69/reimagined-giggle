402e2003a7e26fbe3ead03ff15d105e8940b714b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:57 +0000

    cause ours rough 572enter arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
