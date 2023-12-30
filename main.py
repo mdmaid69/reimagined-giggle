c7f55f3d2b765ac52711010106f7150177242cbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:10:30 +0000

    uncertain dull right track dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import datetime
+print(datetime.datetime.now())
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
