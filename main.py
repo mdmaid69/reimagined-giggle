e9194187621bd12c3b3d9262bd326544f300559e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:43 +0000

    plastic foot long spray pure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
