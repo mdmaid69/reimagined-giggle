5ae16001759c0e57ed93205799b66687990874fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:46:18 +0000

    enthusiastic gather Dd gentle problem

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
