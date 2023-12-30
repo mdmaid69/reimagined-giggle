36c577e5553c3c62647e051ba797bcd38dd507fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:40:47 +0000

    giveup record interested think world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import datetime
+print(datetime.datetime.now())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
