40bf0ff824a320f1694bab6f6cd1d37b01172e7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:04 +0000

    bin damage phone definite ring

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
