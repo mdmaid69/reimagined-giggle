50edd6a3c7ec94ef223398ab2cc10a9c65a77f1d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:28:53 +0000

    war estimate event tool however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
