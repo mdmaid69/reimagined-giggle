5a2ae9d8bac5e75f070e0cb630929ff733fed6e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:23:52 +0000

    fall giveup workout road church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sys
+def exit_program():
+        sys.exit()
