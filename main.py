66b91a1b76a472c5eb0a2b312579434f7fb6f339
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:38:28 +0000

    habit everyone odd mention unless

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
