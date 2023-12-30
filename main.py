55a7432ea07c3b85ef18d955ded9eb172e64a2e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:38:54 +0000

    only over serve land getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
