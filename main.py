2f6c435885bd70e384ccf7878e28f0b44c90a7ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:57 +0000

    bit reject borrow style thirsty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 i = 0
 while i < 5:
         print(i)
