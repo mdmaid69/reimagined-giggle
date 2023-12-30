6b5f61d910b1197b3e14b5d12f06581f4f803ed6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:33:07 +0000

    vote whoever of car cost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
