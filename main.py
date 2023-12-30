c2a8c16fc78082c4ace9cae2dd86a5a121c674b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:02:21 +0000

    fail gap trust meaning floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
