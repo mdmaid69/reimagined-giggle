c946196a3c06181e8cf0dc8a6f464186d9179404
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:47:39 +0000

    rain another complaint illness fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
