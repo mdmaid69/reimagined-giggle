e8d070b0a22809df866e0dd4f483d9b958a49030
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:31:32 +0000

    weigh right gooff shop reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
