93dfc6fda85d408e8dee45ea88bff5c16c9b34a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:36:44 +0000

    refuse well payment shoulder try

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
