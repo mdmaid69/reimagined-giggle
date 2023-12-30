423531753d9b0f438f1a2cadcd421e52ae6999fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:58:14 +0000

    great thinkthrough Ll switch grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
