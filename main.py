7265c227d845aad060d1e793546d68118ec4c191
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:15:23 +0000

    actually risk gradually undo conversation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
