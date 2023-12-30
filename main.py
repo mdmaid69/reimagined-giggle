26735388295c7a8a9642ca7882946296b096abb7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:03:29 +0000

    remain front different but home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
