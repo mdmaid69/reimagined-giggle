d26a7deb1b7ca9f22a52955c78614c56bd73ff8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:17:30 +0000

    tread cheat comfortable confusing now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+print(os.getcwd())
+def subtract_numbers(x, y):
+        return x - y
