b49d80f369ecdba08f0d3155ea0ff3973899335a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:43:44 +0000

    reliable please jump nurse theirs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
