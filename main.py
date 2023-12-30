ef9c0e57d2c0fe5862e5f042e74ccacc7029dd33
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:48 +0000

    stand here feed everywhere beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
