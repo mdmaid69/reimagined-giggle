e9ea096fbc5aee1ca57d97a295d78b76db71384f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:13:25 +0000

    afternoon if uncertain sort OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
