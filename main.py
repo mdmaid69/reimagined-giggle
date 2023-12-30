da712bc3d857ba9ed714a8f352deff2df50a56dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:29:23 +0000

    566end live straight check murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
