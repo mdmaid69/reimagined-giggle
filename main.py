132e054a09cc14d1af354be33af2572691329244
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:33 +0000

    display ashamed hurry backwards post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
