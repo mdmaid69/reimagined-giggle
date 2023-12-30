55f30d2d36fc60488c43768f67bb07d92333c9bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:03:57 +0000

    organize either feed develop generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
