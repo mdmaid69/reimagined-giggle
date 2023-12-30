0abc297967f07a0cfc279db8577d6562d0071829
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:08:14 +0000

    speak nearly name mean tooth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_frozenset(array):
+        return frozenset(array)
