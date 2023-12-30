36cc2c5acf72c95a467274ffc39885e83d5741b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:42:55 +0000

    pronounce once thanks insist to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import numpy as np
+print(np.array([1, 2, 3]))
