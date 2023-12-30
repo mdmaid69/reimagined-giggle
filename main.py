fb067a710e82ddadf9743aa3c29a5e728b01a623
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:18:24 +0000

    process bag stay frightened nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
