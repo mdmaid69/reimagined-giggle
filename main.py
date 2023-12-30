2fc78079bcb0caa95fcd014b32ceed26988e7ab9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:10:24 +0000

    Yy thumb get sweet tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(numbers):
-        return max(numbers)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
