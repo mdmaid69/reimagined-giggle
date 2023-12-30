138c7e509d51cbe4890e0c8a6aea425bcbe74bcf
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:07:30 +0000

    necessary takeaway order stick below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
