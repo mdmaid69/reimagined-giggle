2397b5fc40300ca3b3546a37cababf32cd473369
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:53:45 +0000

    home best 570enjoyable idea upset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
