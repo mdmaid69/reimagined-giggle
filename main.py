6611e98f32c91c4149700fbd72351337fdc79df6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:53:40 +0000

    lock tall gather dish perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import random
+print(random.randint(0, 100))
