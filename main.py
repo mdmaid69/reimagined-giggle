ebfe3811215966b7947fc04b05262c93b740c17c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:53:55 +0000

    patient strength fond damp purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
