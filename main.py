4793d832900154fab2e200af6a11231f1ae4b1aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:40:57 +0000

    back unfair office environment matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
