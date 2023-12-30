5db2f06edd56d6c458b625bb42b028be0aba3cd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:21 +0000

    judge pure project promise negative

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_area_rectangle(l, w):
+        return l * w
