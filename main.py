5e264d876ab54b366d7a88949a737735adcb18fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:33:24 +0000

    same sensible joke true until

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+print(os.getcwd())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
