2e5285f1cb290b963a435ef77576822d6f3dc2ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:50 +0000

    hold none unit dress tough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
