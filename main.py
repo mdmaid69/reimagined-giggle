267673cf318eb2c8096167a287dea8107fbd8f95
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:45:52 +0000

    herself impression present wave choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
