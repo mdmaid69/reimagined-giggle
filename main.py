95a0c49f5aea36db1f8aa9cf74552a819291e83f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:05:04 +0000

    better human issue land Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def subtract_numbers(x, y):
+        return x - y
