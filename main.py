499d7bda9e53dc4fc720c18becd8b6b222941d4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:34:47 +0000

    whole bringdown birthday afternoon dinner

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+print(os.getcwd())
+text = "Hello, world!"
+print("Words:", len(text.split()))
