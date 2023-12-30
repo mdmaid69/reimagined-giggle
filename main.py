1c0f3931f006b54a7d1735eb98b15ebd3fb79c55
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:27 +0000

    occur tip lesson radio need

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import numpy as np
-print(np.array([1, 2, 3]))
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
