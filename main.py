6407ecd782de42d649b42c5023d980874aa8e7f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:37:31 +0000

    charge her slice team character

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
