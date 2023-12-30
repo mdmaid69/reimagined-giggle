c4115335d4a730e6595fac50f38b5815d6925abd
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:25 +0000

    snow return create love forward

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_stack():
-        return collections.deque()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+print(sum(range(10)))
