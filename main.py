4b54cb518b122d128d04b6159f474b26b468586c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:37:59 +0000

    turnout basis defend exaggerate schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_current_working_directory():
+        return os.getcwd()
