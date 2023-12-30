3221da1982f151f91ab300f7a96f481bb6e1c3c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:14:43 +0000

    woman coat discussion scheme wet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
