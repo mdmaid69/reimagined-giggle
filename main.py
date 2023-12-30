7bd42c64551177e03073cac7b1a0a5701624af05
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:50 +0000

    giveup concentrate unite off communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
