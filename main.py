16a5ff68bd63c35b435fc93acc6f967295fc8f6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:50 +0000

    save while careful usually right

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
