81a733b4382aaab6fe9f2c69afd9e805dfae65bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:28 +0000

    fortunately concentrate soak reply breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
