e5142b7d02cca6ce366d8a8f51b2e53f3e4ea9e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:39 +0000

    nice yourself dinner herself available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
