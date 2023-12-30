66352a40806d26e5dd6b4c26bac7c22c6c4a276a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:09 +0000

    born contact slow extraordinary foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
