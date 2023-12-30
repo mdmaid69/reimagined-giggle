ce62d2a51d22b850890e1e72ef03bb00f1693504
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:27 +0000

    179bite short capital woman unless

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_work(force, distance):
-        return force * distance
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
