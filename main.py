804f67cab6716462fe3f5076786e558d2570611d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:02:56 +0000

    bad offer remove available blind

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
