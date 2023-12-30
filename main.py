97ce877e2b58f8ce02bfefb77bb5df13126138cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:40 +0000

    discuss during lower discussion couple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
