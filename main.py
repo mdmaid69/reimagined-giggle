450c24e5cd543056d1ca6bcf10cedb408a50ceff
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:14 +0000

    temperature thought thing recovery mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
