68eb76d75df1a20f4ce1f8e1b448b9f3e0fe6abd
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:04 +0000

    damage constantly as check everything

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def remove_duplicates(lst):
-        return list(set(lst))
+import sys
+print(sys.version)
+  def subtract_numbers(x, y):
+        return x - y
