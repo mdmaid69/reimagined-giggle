065bff608454187d1562feec555a8a5be539337b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:58:29 +0000

    badly fun minute star complete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def subtract_numbers(x, y):
+        return x - y
