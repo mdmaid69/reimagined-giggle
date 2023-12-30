ebc683f9952379d227eb39a7cc0b6251747cdf58
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:34 +0000

    thick owe lay smile damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
