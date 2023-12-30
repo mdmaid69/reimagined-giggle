22c07d6f6ec2bfa965d65835a71e86767e6c4191
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:27:05 +0000

    turnup solve variety though object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
