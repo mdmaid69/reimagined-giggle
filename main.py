b81ee3913af149de72045764f4121cd1bdebf4ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:31:03 +0000

    through takeover Zz moment proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
