d233e2c1935225342c85c4ff3d5a59d951934ab6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:41 +0000

    passage poison skill drive form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import datetime
+print(datetime.datetime.now())
