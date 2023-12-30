7385db7fc7bf1fed08d24315ba3c733d52fce151
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:07:27 +0000

    money film ground tough unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
