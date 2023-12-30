37a5a13591f44a7f591549a22e48fdfbc3aab2b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:17 +0000

    oil them sweep change air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_rectangle(l, w):
+        return l * w
