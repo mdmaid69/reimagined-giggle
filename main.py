aca86db60d05d64b7d912725a12736e428e8b611
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:07:13 +0000

    fear share extremely water bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
