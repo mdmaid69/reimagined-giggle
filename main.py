3a67366ed30d16cf1222ee3eee144e2f5ec5263a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:03:07 +0000

    turninto central breath minor 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
