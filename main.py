2b07bf6cdba1158392e795aa1f4b9b99f834e947
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:16:46 +0000

    communication police boring tooth telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
