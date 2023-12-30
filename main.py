3dbb17676b121b086a31a96efeb12661e7d0fc68
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:11:40 +0000

    few shall about black push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
