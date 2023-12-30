738e5af8c0f4e957ffa4a61a098c0cc553f3607d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:23:59 +0000

    fear but pocket fetch read

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
