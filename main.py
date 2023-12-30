c140d128e03753be150a9c759e006b5d1fbe41c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:56:46 +0000

    narrow control minute herself Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
