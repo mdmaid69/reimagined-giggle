3cf9eac587f5deb5ef672a0140eebdfb111f01ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:40 +0000

    expression sail clever contact deliver

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
