2469ed850337c62ac1d0a17ae4d808a3bc240efc
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:26:57 +0000

    before used workupto think fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
