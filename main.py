3733e52ffb61c65fa9645936e6f7a4b07804489b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:55:16 +0000

    bye wild shock trousers tap

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def convert_to_binary(n):
-        return bin(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def change_working_directory(path):
+        os.chdir(path)
