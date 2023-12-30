56b1239191b01fa01a7ea05f49b56a6a21f8683d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:54 +0000

    collection suffer badly depend twist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
