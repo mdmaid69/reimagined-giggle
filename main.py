5cc85fdb1c7a2b771f548b2850c79b1543e9fb4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:51:17 +0000

    match straight share hair new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
