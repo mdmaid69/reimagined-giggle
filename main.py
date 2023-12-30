fdd300894e214b485937b9076e9de1931362bc97
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:26 +0000

    family lucky heavy picture switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
