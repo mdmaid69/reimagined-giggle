a59cb9cc59cc7bf544dc15e748ef4d5922118c61
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:14 +0000

    sleep roll project that reliable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
