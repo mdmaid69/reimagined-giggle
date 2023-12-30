db95c2a8eab85bd8f8951cab7197a86c7481991b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:18 +0000

    sleep do appear complete commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
