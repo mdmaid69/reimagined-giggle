3f32acbca2ec16443c624ba6d1fd48d445e5790c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:16:14 +0000

    getaway shop brother classroom adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
