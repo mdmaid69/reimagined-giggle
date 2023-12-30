4fca2b294fe99ce1c6aa1b3b072a61bc92a7b2a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:55:34 +0000

    back autumn broad worth belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
