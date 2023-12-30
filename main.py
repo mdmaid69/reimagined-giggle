4dcce603b8f11a14b3fe03ec564293d1f77eca32
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:31:20 +0000

    bath throughout drop wonder equipment

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
