77617ffbeb00d5b19209135ebdfe98392f2a5469
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:35:59 +0000

    petrol someone ball material pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
