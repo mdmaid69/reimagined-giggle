d63ba796e784643eb56c44db0f148791eddf0b12
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:09 +0000

    keep since influence trousers mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
