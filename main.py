11c5719cd686c9896c96e3e20d0f502432cc12b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:38 +0000

    organization equipment tap Ii take

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
