3078fe8e3afaece789632cf8d5b871e88a411853
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:04:13 +0000

    crime not stamp whether stand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
