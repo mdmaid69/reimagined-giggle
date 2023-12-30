9558e604118cf5326a927b64f87d577762f14ed4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:06:23 +0000

    takeout prevent attempt various sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
