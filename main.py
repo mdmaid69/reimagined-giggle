f14260f7fea879505ddf64c2d685062305ee3f2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:00:20 +0000

    class situation swallow walk accidental

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def convert_to_hex(n):
+        return hex(n)
