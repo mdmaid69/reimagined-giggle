96174579da3bc50e0f036fff696c31d3e2750eaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:08:09 +0000

    factory say remark file swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_item(array, i):
+        return array[i]
