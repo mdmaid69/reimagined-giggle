336505502059675a62c02d635984951023687d46
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:44:12 +0000

    detail snow watch may talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
