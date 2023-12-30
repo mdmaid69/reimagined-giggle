6913ed32ab0de59c01b347677aa2d374ff65ed54
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:30:57 +0000

    explanation tie please temper dress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_unique_words(sentence):
+        return set(sentence.split())
