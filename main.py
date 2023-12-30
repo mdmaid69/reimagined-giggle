bd96dcceef7425c5fb2a67f3441592feab6fd071
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:23:47 +0000

    side connected sleep battery drawing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
