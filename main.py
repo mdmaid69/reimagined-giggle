d33cd39e42daeab719fc29a7d24335964fdfa15c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:50 +0000

    rough float jump exaggerate release

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
