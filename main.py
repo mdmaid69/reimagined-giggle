4d7bf8d23b72fa96df610ef88af712dac384dff7
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:40 +0000

    accept clean radio once anywhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+for i in range(5):
+        print(i)
