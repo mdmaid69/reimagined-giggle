3c95894008a23dd7107b2d5e1a560695e2fe872d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:59:29 +0000

    pool direct unlucky plan ball

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
