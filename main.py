fa999d951c022262bef919b0d8f3edb146f4b2d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:15:02 +0000

    image band hold powerful formal

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_min(lst):
+        return min(lst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
