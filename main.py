c8549ec33c9978908e07068800c9e16e8ac0105a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:56:34 +0000

    Kk future settle ready straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_current_working_directory():
+        return os.getcwd()
