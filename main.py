2c21c5d14ef642d27e705ee9322004b693b443d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:02:37 +0000

    pressure affect unfair glass platform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
