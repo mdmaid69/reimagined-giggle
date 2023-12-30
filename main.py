188307d2e3a25af9a89acb459bf52caa93256fc8
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:53:07 +0000

    big occasion dear recover punish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
