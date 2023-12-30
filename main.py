bcb396d30651b73c58c0d4528104e9bf0fe2dd6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:48:57 +0000

    official date fast recover somehow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
