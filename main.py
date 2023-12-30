a5a6eda8fa27cab2927ddf78e3c69812faf1260a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:54:34 +0000

    ability move scheme innocent finger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
