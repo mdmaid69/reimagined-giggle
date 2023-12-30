f104ddeb35ae0e2ce93405527befe35593106c2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:31 +0000

    study small work floor left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
