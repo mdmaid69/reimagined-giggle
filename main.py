e145864947c1ad7f90cdfd5369a84b366fafbb89
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:53:50 +0000

    Gg prepare lean half concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
