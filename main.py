1d4b718f4dc14ea2ea05dcbfe8c290f004ce7f30
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:28:26 +0000

    break not instrument convenient father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
