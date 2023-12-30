e804a54278fe08fbeb6a2e03b281f4af9a5108cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:45:41 +0000

    unfair exercise all rate order

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def print_hello():
 thread = threading.Thread(target=print_hello)
 thread.start()
 thread.join()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
