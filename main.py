b6b85a12b92053ad82c87c4c39f4003b07f10756
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:23:10 +0000

    survive ring recover container persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
