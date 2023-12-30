2867a8ce8b6352aed6f842268054247723a134e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:13:03 +0000

    university use each information sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def convert_to_hex(n):
-        return hex(n)
+print([x**2 for x in range(10)])
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
