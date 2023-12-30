9a3e9bf966452aa07993750a86608d574dcaa8a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:04 +0000

    dull takeout used mine payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
