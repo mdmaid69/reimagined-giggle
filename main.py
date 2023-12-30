e42a57fc7baa54294c265bdf23ff5aadb338404d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:35:59 +0000

    tone wander turnback contain practical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
