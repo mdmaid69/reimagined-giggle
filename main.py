87732f497aba53be9be62e3ae89ca5efba7cc4c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:06:02 +0000

    condition beneath period judge path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
