4a969b85075bd672e3afc4d1bf09c0b9d9644543
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:39:54 +0000

    wind suckme ticket easy way

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_force(mass, acceleration):
+        return mass * acceleration
