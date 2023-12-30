c76c763d5c078814ce1ab0912a51b0b1fe842915
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:41 +0000

    perfect young chat soak load

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
