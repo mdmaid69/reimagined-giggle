22825d360c8bf14fcccee7f3cce9c2b2b90a3682
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:50 +0000

    switch really week stone disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
