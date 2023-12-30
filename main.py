f40f93e620a27931f9b56c23ebebff61b98fc810
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:23:24 +0000

    pump complaint cold embarrassed time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
