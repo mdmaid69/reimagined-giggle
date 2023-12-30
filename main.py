a0123b82398fbc8bcebab45523c0837bae53b2d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:43 +0000

    turnback leg slight Jj underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
