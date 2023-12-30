0f35e66c5ac4724ae841b5209eaf76936af97140
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:11:55 +0000

    base will that transparent damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
