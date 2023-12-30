d6a32944ba3e508d96f006ece3400bb89b1d3ac4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:00:28 +0000

    argue inquire side suffer hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
