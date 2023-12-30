b3e271eb3c7c6490e662e264049f95c8b9e58441
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:12 +0000

    away poor wing and bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
