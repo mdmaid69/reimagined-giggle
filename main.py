c90dfff50a4dd60c88f6964c67b9694dcb659641
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:22:42 +0000

    drink visit long name average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
