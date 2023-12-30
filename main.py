1b75c358990d2acab44f0e11524d0242372af38b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:00:05 +0000

    responsibility go eager comfortable company

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
