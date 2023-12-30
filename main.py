c64839845829d743a39f55517b3795862f091a3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:49:01 +0000

    mess estimate safety cheat whom

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-for i in range(5):
-        print(i)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
