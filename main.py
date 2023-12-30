892937fd2132407090fa6bf4d944b5a92aa64df9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:13:05 +0000

    do into rule somehow bound

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
