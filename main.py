2bc04e09cf089c3b75434aa049c5622b5ad30c22
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:41:50 +0000

    skin anywhere walk boat concerning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def is_odd(n):
+        return n % 2 != 0
