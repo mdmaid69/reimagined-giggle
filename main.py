16f043c20a738223c3ad253d799ea1dfff23323d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:59:12 +0000

    half explain somebody more turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import math
 def calculate_greatest_common_divisor(a, b):
         return math.gcd(a, b)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
