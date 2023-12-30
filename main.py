ac535486cec94d5c527277d2f184da8474230767
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:21:04 +0000

    vanish them communicate goup still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
