3a8d9fb1cb45fcaf8c9a1b7c98af4fed46b06960
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:21:59 +0000

    late picture attitude to send

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
