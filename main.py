7d58edaa78a6054ec31d40b1fbc4d76bee798170
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:08 +0000

    steady lead field desperate height

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def add_numbers(a, b):
+        return a + b
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
