5a66e1a752c207a62b185b8a015e23c4fe54773b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:16:59 +0000

    treatment exit cheat tradition maximum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-from collections import Counter
-print(Counter("hello world"))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
