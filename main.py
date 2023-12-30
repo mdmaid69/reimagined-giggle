c691ab5fffde6dd435cbb3171bd87149580cc4d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:23:35 +0000

    keepfrom whom blame confident hand

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
