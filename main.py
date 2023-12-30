306c32aa7a227449a523cd38b0f2e52865583d99
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:17 +0000

    material punishment Yy dear lucky

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
