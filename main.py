e98e397f01684126ee709ae42b7b8d07b0b46b01
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:36:36 +0000

    day proper sort occasional opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
