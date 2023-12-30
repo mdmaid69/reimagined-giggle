0709b2069544e9653d08a049ac395c3b475bca99
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:26:49 +0000

    they 566end should comment thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
