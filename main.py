575c16e5507f4f71fa0f842600b9e94a87519824
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:44 +0000

    trade not present beneath Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
