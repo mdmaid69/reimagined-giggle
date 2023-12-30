9f0f73dddacf38fbd10eaff709067e541771aef8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:19 +0000

    plant bringup list bus production

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
