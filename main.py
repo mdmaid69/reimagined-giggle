410fec0c5601a9696d8bfd57156b93b561ccbbfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:24 +0000

    achievement each that whistle try

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
