40dec470ac2a94918b300d4534d8b15ca49e2403
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:51 +0000

    uncertain complicated guess disapprove Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
