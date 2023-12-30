a202ecaba524a4cb7efef108ecba68aa4e288729
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:53:39 +0000

    lesson section unless might smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
