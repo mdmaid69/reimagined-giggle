e17c2cb4974538b35c330263afd05ab603f0f59c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:59:32 +0000

    terrible watch frequent shake Ii

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
