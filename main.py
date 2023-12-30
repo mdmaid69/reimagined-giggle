0d5041f45a2d393abd0fe9a4c8ec272ae739d828
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:22:53 +0000

    me great seat organize frighten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
