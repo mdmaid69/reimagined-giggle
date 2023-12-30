d6f8465926cd312f9dc5d02bddb365414010f92b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:33:44 +0000

    far suddenly advanced whatever meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-def print_python_version():
-        print(sys.version)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
