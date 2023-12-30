b44f6eb63ac1cbdfe2b97e60cd575ec5a933dd9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:10:46 +0000

    thing find turnaway runout car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
