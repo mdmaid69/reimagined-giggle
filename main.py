d44b066b28a1b2ecadf1e52da71707e1875d0807
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:48:02 +0000

    afternoon cut influence period thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
