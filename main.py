88bd252d53b5a22af3d2d91ace31d1e145154cab
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:41:00 +0000

    impossible bury straight not story

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
