0d45699045d33f212983586c3f6bde17b0d4db95
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:19:47 +0000

    twist scene cheque apologizeapologise finger

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import sys
+def print_python_version():
+        print(sys.version)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
