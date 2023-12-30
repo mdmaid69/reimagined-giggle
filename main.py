58d441b386837f647140acd5625cc463c47f2a68
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:57:07 +0000

    should chance sink light design

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def create_priority_queue():
+        return collections.deque()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
