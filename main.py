b05126bccd25bcc5ccdcc3b7f98f4d79c8884540
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:34:25 +0000

    lot father software turnup soon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
