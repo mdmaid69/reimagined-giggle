83b0057a0cc6107e6fb5c599aa348e836bf6c248
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:02:52 +0000

    minimum present blame opposite address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_even(n):
+        return n % 2 == 0
