31179c017fb2b9b883305e08063946e316ce9e24
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:10:36 +0000

    interested you commit shadow unable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import tensorflow as tf
-print(tf.__version__)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
