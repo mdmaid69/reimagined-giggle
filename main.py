785a3d1a0c7875bd709306a6c550edd234330f51
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:53:48 +0000

    long speech pupil evidence community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_pressure(force, area):
-        return force / area
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
