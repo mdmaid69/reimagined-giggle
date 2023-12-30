959542b7b594391af862b959ecde5979e98e4804
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:01:02 +0000

    necessary trap very rain except

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
