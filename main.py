ad74691c64d220fffa590c6be2ecb8052c0f9f74
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:35 +0000

    equal capital people region apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
