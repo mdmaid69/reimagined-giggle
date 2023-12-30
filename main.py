7918b2277cd29aefb0c45d97718671d2e45efb53
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:19 +0000

    house meet reply steep pump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import json
+print(json.dumps({"name": "John", "age": 30}))
