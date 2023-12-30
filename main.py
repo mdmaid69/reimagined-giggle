f1881b8da44d02b0cab90d917c64de6c41ef64d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:02:55 +0000

    mind much exciting object regard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
