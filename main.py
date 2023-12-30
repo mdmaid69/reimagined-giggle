7f73f490e24ccd51067a10feda0818709e0aceec
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:23 +0000

    callup pile chat necessary circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_max(numbers):
-        return max(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
