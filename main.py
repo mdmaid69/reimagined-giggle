8ed29494e3abc86dbc9959814af3bf791df55c1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:18:05 +0000

    geton speak key indeed break

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
