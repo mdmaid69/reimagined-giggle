0cd034099434d1409837fcbcbe1aa51640c9459a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:29:34 +0000

    hurry minimum notice reply correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def square_number(x):
-        return x**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
