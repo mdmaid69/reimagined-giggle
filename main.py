ed4c71a2cc043a4ed5a8399a068b01a761d83d06
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:16 +0000

    film later moral battery completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
