cfe42b71bad7b36939193e54fc04e53e54edc360
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:31 +0000

    unlucky unfair system alive tend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
