bd85cc2eddf2a04f7fa276a0c6dab49517a68ba5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:07 +0000

    action Tt regularly think emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def calculate_variance(arr):
+        return np.var(arr)
