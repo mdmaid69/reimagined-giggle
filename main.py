5ac49a39ebdca131dec5e93b47da046f00e44910
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:33 +0000

    Nn tooth possible almost owe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
