fd5b49b5251ef65323b773b59ceef358ba784b20
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:08:40 +0000

    surface machine blow direct amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
