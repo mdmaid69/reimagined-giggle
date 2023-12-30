624fc2c948e2d70ac7f0ecc9e46bbb9a87865a9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:09 +0000

    often would error deep wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
