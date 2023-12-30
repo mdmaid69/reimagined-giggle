e4ec6cf58681812bdb4050a934b258d4c23cbc46
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:18:30 +0000

    risk familiar cross taste rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
