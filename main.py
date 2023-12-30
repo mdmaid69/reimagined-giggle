0c3ebde014abf9c332de28901708293e908d8c99
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:54:32 +0000

    exist firm dark valuable push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
