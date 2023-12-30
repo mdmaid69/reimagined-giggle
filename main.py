dbd944fcecaf3b45bca2127caed4357157812071
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:10 +0000

    sticky yet exchange safety middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
