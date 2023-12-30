a2bd2d83e355f15fa9eeaa4f8d9e46af5ef4d634
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:31:09 +0000

    least guilty ride slowly science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
