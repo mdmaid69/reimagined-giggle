67276a8cfbf8624682ede9ba4df3bdde5bef60a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:32:23 +0000

    heat worst situation trap thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_current_working_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
