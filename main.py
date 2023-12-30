ead011c799de38fe2bcc7e0ac3412d0600849a52
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:57 +0000

    real would complicated operate road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
