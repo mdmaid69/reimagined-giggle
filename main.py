23a93e22db5c9bdaf13a061cb035034a7876105a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:39 +0000

    Jj round father favourite length

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
