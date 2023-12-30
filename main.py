676733db4225e69fa814467b5d4513eeea48f33d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:29 +0000

    fetch sleep dress degree takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
