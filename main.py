6b96341070620d13fb52b675701a8ac302228aab
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:03:05 +0000

    pull butter supply born temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
