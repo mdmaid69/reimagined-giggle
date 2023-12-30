bf387c4e1050a9eba3addfedda359172efbb7487
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:39 +0000

    maybe correct ought bag pay

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_acceleration(speed, time):
+        return speed / time
