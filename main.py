8666d097d13a890eae4a82f8d50658a9002ba8f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:22:21 +0000

    Ff hurt ring truth manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
