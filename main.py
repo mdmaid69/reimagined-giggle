09a7abb8b66e966d1294a23119859dcb8c4efec7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:41:47 +0000

    gooff mine copy scale mystery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
