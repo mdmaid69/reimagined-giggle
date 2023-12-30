8b844929e19903afcb57ffdafe6fbf6561f0ac3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:50 +0000

    table press lose mystery someone

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
