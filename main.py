c6174d85dfc2e4634a6231262a22c5a0941fea37
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:27:15 +0000

    read and storm fond proof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
