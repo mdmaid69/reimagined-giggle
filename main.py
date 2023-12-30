34acca76580842379c7e6de0999849c184e4d8ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:51:36 +0000

    go happen cause several slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
