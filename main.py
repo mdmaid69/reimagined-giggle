8c959d42ce6889bfa83d75fac717a038f0e3ad5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:25:19 +0000

    across imagine save excuse precise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def convert_array_to_bytes(array):
+        return array.tobytes()
