76a32aa19fec3f9c0823a55b486f6823fc73a28d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:57:08 +0000

    perhaps rent bit dish minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
