ada6efb1685cd8918662d8a8d3794d493869e2b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:17 +0000

    art whatever concentration high feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
