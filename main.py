8dda963cb51155ee22ab3b5ecb5bbbe8e448a44c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:26 +0000

    from band discuss getaway trousers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
