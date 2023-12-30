3697850e659bb5a392ad898d412ef3a46cbe1861
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:53:25 +0000

    tidy pool peaceful open sea

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
