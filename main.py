14ddd04e8389f6f8ca54ad2b98a20b659753bea2
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:10:01 +0000

    fat shoulder school grateful wet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
