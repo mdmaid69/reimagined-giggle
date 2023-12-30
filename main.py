426f0c4a1b68f579cf7c6e382467e2f8d7783397
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:01:37 +0000

    determined satisfactory mark factory bother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
