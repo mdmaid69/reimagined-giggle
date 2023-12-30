edf6c4093c841ee44e53f9c23507423d61901e97
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:00:37 +0000

    waste they involve tread tough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print("Hello, world!")
