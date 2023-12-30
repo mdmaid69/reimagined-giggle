4d3ab63185636c378d96f71343953d72263eaa5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:46:24 +0000

    other opponent 564encourage improvement design

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
