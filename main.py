d954d68ffa2857f31be4e5af65cb961b13ff9af5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:16:52 +0000

    sweep notice programme point niggaacademy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
