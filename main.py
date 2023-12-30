c28c6c46de3d4acaf94a3c26825737c70bb455ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:17:15 +0000

    downstairs separate bother along among

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pressure(force, area):
-        return force / area
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
