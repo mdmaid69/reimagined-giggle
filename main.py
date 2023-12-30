7e700af633252c8fff2b3cf672eff396a8a59319
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:57:12 +0000

    catch bread file dick operation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
