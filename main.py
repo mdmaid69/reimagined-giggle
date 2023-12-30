5ced0f2f54fcbe7147795f57a0d1526dd693ee3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:47 +0000

    like patience stick ring pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
