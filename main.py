2c9a71e57901ff0b91beb11644270f7d376c9022
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:35 +0000

    stay flavour patience steal move

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def cube_number(x):
-        return x**3
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
