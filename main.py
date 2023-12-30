d66cd847ac8fbeab5085dfe9a64bb91cbd12a34f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:55:05 +0000

    occasion leave teach thank before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
