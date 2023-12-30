bb2d0e3947a71276dd26d74194165cdc13c2f7b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:01 +0000

    instant arrival meaning drop Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
