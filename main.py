af7f213ec69f645860b65b6cf561ee46166fd386
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:38:30 +0000

    programme threaten judge estimate pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
