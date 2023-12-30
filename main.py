913611de08e99805cd394f72c22c8f40b40ddbde
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:21 +0000

    reduction out quit somebody her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_stack():
+        return collections.deque()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
