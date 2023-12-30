58cfd67cacb596b00b902297576f044bc13a2309
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:01:58 +0000

    surprised various cry mean best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import time
-def get_current_time():
-        return time.ctime()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
