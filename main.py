bbb19a387748c885db1e74be44bb4261d37f8d14
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:52:47 +0000

    already branch please keepup slowly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
