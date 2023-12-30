fa9461cc330bcc4cce42e303de56c6562035e700
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:42 +0000

    warning brown hold professional obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
