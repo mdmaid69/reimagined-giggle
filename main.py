3f0227bf19a18fe91302c62953ba4e46def5eff4
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:16:14 +0000

    off public satisfactory love scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
