eddedb00a6367d4c0d73b9205df4dba524de8cc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:56:11 +0000

    entrance check satisfactory example down

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print("Hello, world!")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
