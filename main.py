ae8a4a8c9f6feb4cde6c5201942c6a3e9416130b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:33 +0000

    afford gradually touch list gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
