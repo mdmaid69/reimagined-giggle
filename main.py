0cc7ca9d1b569dffe54de7f12f1c6a54c63aef64
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:48:59 +0000

    clothes fall list sense world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
