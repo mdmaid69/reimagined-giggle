7c087ec46bd7df8ec32c1e440ca09213d252edd2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:45 +0000

    566end giveup will defeat seriously

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(numbers):
-        return min(numbers)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
