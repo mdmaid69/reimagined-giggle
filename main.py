718d3a382fd3504ec0a9a332b7c5b1c11260dd9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:45:04 +0000

    community shop time solve left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
