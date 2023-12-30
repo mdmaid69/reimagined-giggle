e5ab1468e11b28ecb5b602e037cc06fff7f59f5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:50:33 +0000

    lend objection raise judge friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
