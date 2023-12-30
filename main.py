992bf2524431e5e7e840a9173923263e7b0d9c8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:52:41 +0000

    set tool comein left get

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
