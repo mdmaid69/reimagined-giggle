66b6d0d710b86df31ecc5bb5a20bba4b5bc84f78
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:22:17 +0000

    Gg innocent special load poison

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
