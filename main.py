6820b364291898031893f93241296f70bea000e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:58 +0000

    once snow quarter broken bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def remove_duplicates(lst):
-        return list(set(lst))
+def square_number(x):
+        return x**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
