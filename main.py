2ad0e0038cd36b835d29cf0bfaa3ae6218fc13c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:38 +0000

    glad nor care fast recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
