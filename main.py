2b956630f982a8b06166b9e4bc969761bb13d040
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:14 +0000

    pin fasten performance rush thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def get_array_as_float(array):
+        return float(array[0])
