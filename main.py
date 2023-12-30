024537bef5a41dbf30f01239351396a5d09fbc50
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:46:25 +0000

    gap involve tongue typical indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def multiply_numbers(x, y):
+        return x * y
