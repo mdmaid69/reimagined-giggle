777788e2dcb40fd1b28c09d2c6bb66472573411b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:40 +0000

    equipment dear weigh block embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
