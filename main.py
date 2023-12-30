9aa8831b8556e727e5540059c6a81849096f292c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:54:40 +0000

    search consider gradually dick awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
