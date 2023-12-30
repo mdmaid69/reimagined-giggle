5fd6f234c46d73d8ce6f57167ab52286ea1cf0cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:38:12 +0000

    throughout to runout coat material

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
