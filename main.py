3d9c191480ed2f193cd8a912d3fff7bf50fb7aa3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:03 +0000

    line collection indeed live realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
