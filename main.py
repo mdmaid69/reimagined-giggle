89db691d3dd6075e004119eafb070f5560619399
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:36:09 +0000

    strict he reduction come nearly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
