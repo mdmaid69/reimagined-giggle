7f5ce0111abc0d8cf7af816af32d074e44a0f4bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:10:58 +0000

    respect store so unfair persuade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
