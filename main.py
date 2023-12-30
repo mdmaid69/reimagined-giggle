8c5fbb2d63bb8302e7dfb29b62736e33373d770b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:14:58 +0000

    any violent market hope breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
