27b6622b616f90ed1b5e765a0ee8835e948559ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:11:25 +0000

    part visit at attract relationship

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+print(time.time())
+def cube_number(x):
+        return x**3
