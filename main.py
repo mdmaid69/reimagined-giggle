07dba416c4acfa0cb4e9115d14612317a62425b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:05:08 +0000

    stupid frightened stiff spray instrument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 def calculate_current_ratio(current_assets, current_liabilities):
         return current_assets / current_liabilities
