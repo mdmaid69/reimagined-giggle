9266f2331290b24c1788c2ca7e212d990fb46df0
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:52:34 +0000

    fold undo service strength discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_power(work, time):
+        return work / time
