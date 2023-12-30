e38d5c67c20842479ba526599815e9f2ef0ba230
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:04:14 +0000

    turnaway town tax persuade much

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
