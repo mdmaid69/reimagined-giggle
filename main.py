418ed9fde3858f28ed801f1043b1aff66228e453
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:42:30 +0000

    valuable scene deliver with surprise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-print(math.pi)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
