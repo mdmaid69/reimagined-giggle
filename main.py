99849da0b12a547a6d0d0cff927dd1fc5ffb2fae
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:29 +0000

    radio outside rather say office

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+x = 10
+y = 20
+print("Sum:", x + y)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
