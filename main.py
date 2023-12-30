f732c6d141cd1063b978c539b1fe01e0a78a2d48
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:59:41 +0000

    comefrom light birth basically well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
