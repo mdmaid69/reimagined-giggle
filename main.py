1da4df7110134955c28a43fe1a895546b8c81358
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:05:38 +0000

    recognize hello all exploration bury

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
