4fa1bb4a0fee8773f3da1360f2372abce9c75e2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:57 +0000

    label tap over smart pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
