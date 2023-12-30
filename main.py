f2262bc7bffa2a5790e167f50426c70ab0ee6b24
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:10:40 +0000

    whistle stamp nowhere freedom able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
