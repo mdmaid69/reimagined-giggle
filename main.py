6eb7007101cb318dcb0e093d19d44bc688290708
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:47:05 +0000

    confident oil wild its sorry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_typecode(array):
+        return array.typecode
