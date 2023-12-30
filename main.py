54896712d61d85dfe366bad120555b9ed253a489
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:18:44 +0000

    freedom design sick kick mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
