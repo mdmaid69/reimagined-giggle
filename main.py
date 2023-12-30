d61589d5d358f7061af611b2619a67e82785bf02
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:30:47 +0000

    load quick scrap 566end swing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_pressure(force, area):
-        return force / area
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
