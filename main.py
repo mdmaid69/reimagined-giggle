b6635d32bf15b4b63470a5b73db46152ae52bd39
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:08 +0000

    invitation warm only sound serve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_list(array):
+        return list(array)
