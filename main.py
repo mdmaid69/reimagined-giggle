f5e8f269fb6c8f9a90da489579a13939e25ccdd0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:58:24 +0000

    door before collect risk autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_max(lst):
+        return max(lst)
