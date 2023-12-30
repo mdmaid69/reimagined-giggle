d1b32acb27dffdcc2e7708cf72ce7928b5e4f268
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:23 +0000

    lookup through spend callback walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
