974d6f1dfcff3d05df13da8fa084bbd4cfc3d827
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:43:48 +0000

    tune mean check blue 566end

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
