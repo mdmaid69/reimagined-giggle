0bd1ce03735e8693699e2db383b2e409f35369fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:01:29 +0000

    scared crash each reduction afraid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
