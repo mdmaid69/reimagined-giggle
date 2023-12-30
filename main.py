dd7e65586097f4106d71dd8150f0f76e5eadcb6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:23 +0000

    survive sorry honest accidental please

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def reverse_string(s):
+        return s[::-1]
