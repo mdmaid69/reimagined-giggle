6c7684b8181be743895b3e71fb1f5a3ba33891f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:53:39 +0000

    tradition record scrape dick nice

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
