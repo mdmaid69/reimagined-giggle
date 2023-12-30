039ac53f31148d7c7b0c370fbf7bda942c57d66f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:17:38 +0000

    meanwhile pause expect ill invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
