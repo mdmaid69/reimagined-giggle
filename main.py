f8393b95888505e0a74e785d13698f83418e03a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:59:48 +0000

    fresh seriously tread pile fit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_speed(distance, time):
-        return distance / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
