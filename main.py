101ccf6446dc04ecb17f50c7993591fb41132a0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:05:41 +0000

    comeover patience length slip comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
