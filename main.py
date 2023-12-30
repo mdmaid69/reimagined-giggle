7f9ab4854f1862dac968a199a343ec03a507ba98
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:35 +0000

    value university splash provide petrol

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
