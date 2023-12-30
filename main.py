87d8d3e0615833547033c35cdecb332fce6dcde9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:26:57 +0000

    whenever supply offend cope Jj

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_speed(distance, time):
+        return distance / time
+for i in range(5):
+        print(i)
