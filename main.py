47b3352fb3d7af3d06f068823ae53a616edd4c59
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:15:48 +0000

    cover possibly back add material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import platform
+def get_os_info():
+        return platform.uname()
