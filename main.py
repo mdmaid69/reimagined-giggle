f577c4ca6feb9b1c103a088b8ed2e078e1e3baa8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:01 +0000

    production expect final conversation getout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_min(lst):
+        return min(lst)
