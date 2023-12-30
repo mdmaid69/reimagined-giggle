0fd7e3ad138a1d72e44d31d0b4f6f935687ce4cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:02 +0000

    turnback weak happy the depend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
