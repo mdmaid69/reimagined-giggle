241a3da2481cd0bdd0b950d8e466c798ba766cf9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:04:24 +0000

    approve slight spend cover guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
