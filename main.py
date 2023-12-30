621c7293dd2bba36638cc44af14d92818e57ee9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:05 +0000

    upstairs breakfast pile disapprove depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
