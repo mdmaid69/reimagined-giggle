1a6df3f8830645f18826f8d4982fb6d783e2c4d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:27 +0000

    keep gap second combine supply

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import numpy as np
-print(np.array([1, 2, 3]))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
