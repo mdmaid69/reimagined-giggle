9d3740ca739003b9a23882da91419a7bf1c36650
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:02:46 +0000

    crash gift mainly interesting use

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_characters(sentence):
+        return len(sentence)
