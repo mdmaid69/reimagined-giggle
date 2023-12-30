83bc9e27bb8e46304d0b4dc535e73218beab0a51
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:52 +0000

    father excuse example long really

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
