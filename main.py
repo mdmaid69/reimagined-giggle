0e9a4709ea209f83d770822ef763ae42333453cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:49:19 +0000

    head crisis offer bath behave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
