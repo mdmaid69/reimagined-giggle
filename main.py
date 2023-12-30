c727e6079bc8ee8ad280bc979a0cb79d86edcc70
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:18:40 +0000

    load come raw study exchange

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_work(force, distance):
+        return force * distance
