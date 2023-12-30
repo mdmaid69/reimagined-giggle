7dc4788ec9306261497d22dbab8a39c489ae8a24
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:48 +0000

    recover book afternoon language simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
