51037e416c7483974ca262513b3f5427a6c15122
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:09:11 +0000

    knock meet ready piece angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
