ae97f18b9bbabfd9ae4b4440efa61fcf248bf088
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:52:18 +0000

    before used contact formal claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
