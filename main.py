316ddc3edfeb8db39eb2ca3f2e2b4cf53fd94347
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:31:07 +0000

    expression fear hungry threaten squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
