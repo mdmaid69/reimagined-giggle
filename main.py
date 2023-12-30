5253a100a4e4bddbd4f4badfa8f220c6474fe617
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:00:02 +0000

    present birthday instead show improve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_max(lst):
+        return max(lst)
