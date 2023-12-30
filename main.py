4a88219be2c5fa560fc2dd54587a321047548ed0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:11 +0000

    break strange Hh course slowly

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
