c45ec706e6991cf627c239f17a40621f13aa89a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:50:25 +0000

    further no base choose shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def divide_numbers(x, y):
+        return x / y
