21899faadefe51fbcd13cfc41b53ae4faed9b349
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:53:41 +0000

    rent I open round advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
