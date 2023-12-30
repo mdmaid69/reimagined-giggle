b5fdc44bcfbd3b80e430383ee9b07864a44ad7ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:21:22 +0000

    I guess wave eager listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def clear_array(array):
+        array *= 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
