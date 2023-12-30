2e4a2ee3df2a3afcb1ded20cd326fab386e717b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:17:21 +0000

    early bound shame piece runaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
