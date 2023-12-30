8c3959536a19bb523e86e7c7838b70c48984c905
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:52 +0000

    mind road tool fast strong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+text = "Hello, world!"
+print("Characters:", len(text))
+import os
+print(os.getcwd())
