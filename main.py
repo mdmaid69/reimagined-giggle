f4cd382d3dd8a92ba487f52670be6781a73cc056
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:02:15 +0000

    scrap runover mess stick neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
