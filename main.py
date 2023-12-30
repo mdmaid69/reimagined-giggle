731b8413b9f5d3c1dc5a68e7a0881764a9dca08f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:02 +0000

    land part work say while

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
