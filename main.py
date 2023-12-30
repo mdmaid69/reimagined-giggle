c92af460a826f6ef32ed065156a9fe1f0e383a74
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:09:04 +0000

    sign suggest knowledge runinto correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
