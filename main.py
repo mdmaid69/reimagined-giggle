8115faba4f31d4fe068d741d0f4027031b42ba18
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:53 +0000

    answer clear string please before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+from collections import Counter
+print(Counter("hello world"))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
