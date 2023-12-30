5fd76615ed7173c697b264b125486b37e2b8769a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:11 +0000

    software thinkthrough professional spread likely

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
