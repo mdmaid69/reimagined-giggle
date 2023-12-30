b9f46b14a17e9b01fbd5e7fe1be53a47f8146a4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:55 +0000

    taste this wherever neat uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
