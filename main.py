475e01006da76febd98dc34926ab7e65dab382c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:21 +0000

    justice fight thank today behave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
