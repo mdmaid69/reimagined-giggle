d4561638fdf2f8e202f20b2f157e3896de836517
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:41 +0000

    copy choice sink wet offer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
