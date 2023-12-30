80f93c66235c9c29598cb2b9737bb4bd4152a61d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:51:23 +0000

    live our bet conversation takeoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
