574ca945348da2d6674065eae338f826c82e1a69
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:05:32 +0000

    personal beside start exchange splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
