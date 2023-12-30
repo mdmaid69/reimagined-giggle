f03de2a1174233952a307dbf2f18fbf9db2ad945
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:49 +0000

    expensive replace child wire mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
