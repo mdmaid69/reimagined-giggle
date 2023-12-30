8fabecf8c8781f0a0f9346b1ebeefc247cdc8b27
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:35:27 +0000

    creature test thank suppose university

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def remove_from_array(array, item):
+        array.remove(item)
