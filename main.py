6af3a5beeab72211c508f2513a6f049a42888494
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:59:17 +0000

    abroad include shut travel uncertain

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
