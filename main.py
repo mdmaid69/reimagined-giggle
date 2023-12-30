72a1bce7180ef1cda977ce1b9ee0b8bb02656ff2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:52:14 +0000

    abroad tip grateful talkoutof reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
