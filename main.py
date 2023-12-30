c8370230a9e00c31ba42976383de7be561f94d77
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:57:05 +0000

    boil half write neither raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
