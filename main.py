52456415042a31bd16d9e8520fdc5b32d1344e95
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:52:26 +0000

    instruction television fetch open my

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
