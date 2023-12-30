bd1b16a208c8e56f3b12e97245458ed23279e5e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:11 +0000

    winter divide count almost again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
