e62178d90f561e98f75878f530bd3b90232550e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:25 +0000

    conclusion valuable practical children Gg

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
