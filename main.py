66255449dbddc179a3a145d9b758fe67d27f0f94
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:58:10 +0000

    please shadow see Uu increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
