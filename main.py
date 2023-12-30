41f4bf608b125246e7c69427f02e63d26b860760
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:17:17 +0000

    tour flow though demand tie

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
