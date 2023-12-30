2c7c726fe8c7919dc9b3805172ffa6fd5d412e47
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:53:37 +0000

    there track travel as slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
