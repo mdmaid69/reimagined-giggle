6d34bbd84ba8eb748814aee60d38436f8fcd34e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:44:44 +0000

    permission perfect same deaf breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
