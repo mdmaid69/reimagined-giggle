9e14b42753a959d6e74dd73006b3bad3b2f4c676
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:57:20 +0000

    mark scrap complaint slip storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def reverse_array(array):
+        array.reverse()
