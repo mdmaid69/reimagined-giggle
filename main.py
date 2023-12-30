64bb6ff0948480070a56967f46baa0a847a7f185
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:37:04 +0000

    favourite gooff on room set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
