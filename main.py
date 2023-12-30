77b047626155b49a70be793957e796f24f36d997
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:10:43 +0000

    getoff satisfied police slice dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
