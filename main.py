6306d207fb79092f67bcfd87d5ba4a4a7e7a19e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:56:51 +0000

    advice represent pay centre roof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
