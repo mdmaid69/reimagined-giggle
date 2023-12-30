d1e0aef446a33b87dddeac13c28569aaf570540d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:41 +0000

    giveup respect nonsense wild wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-print(datetime.datetime.now())
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
