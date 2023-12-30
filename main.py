edea17a867f534475d850e7f4c3bfaa7b4752111
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:40:36 +0000

    recognize march thirsty funny burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
