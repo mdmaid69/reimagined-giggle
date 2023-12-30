3428c3628c9282bee5be580e768caba2c2fa1e34
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:48:25 +0000

    anxious bite right combination design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def count_characters(sentence):
+        return len(sentence)
