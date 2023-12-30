5cc49ad0a25803f41485564c142957cb9e81f657
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:09 +0000

    evidence fair tip better him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
