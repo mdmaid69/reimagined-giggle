5ea9ede33de1e4c12b94ae8d34b52171cf417da7
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:30 +0000

    blind earth admit loose contain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_volume(length, width, height):
+        return length * width * height
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
