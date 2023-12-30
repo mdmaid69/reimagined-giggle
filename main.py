c8b797710c658074271fb37837450390dfc16f85
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:07:50 +0000

    university twist film name medium

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
