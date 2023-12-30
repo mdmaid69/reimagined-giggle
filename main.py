7fe3bf2570d7e131e0ae1e1c5d26656d05a694f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:32:43 +0000

    objection contrast indeed brave burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
