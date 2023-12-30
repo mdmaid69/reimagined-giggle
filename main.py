15cfba2b96a7359dc8ee04115b4e4c6f6d9e1133
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:35 +0000

    announce medium crash bang ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-name = "Python"
-print("Hello,", name)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
