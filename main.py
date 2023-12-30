5beb99b963bb85bc96ed81047aaf80fa5746e66b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:34:43 +0000

    whether satisfied shake guide coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
