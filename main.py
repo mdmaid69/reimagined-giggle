dcc016e313c46459b4196ea444062070b3f68465
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:48:41 +0000

    ready key warning true learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
