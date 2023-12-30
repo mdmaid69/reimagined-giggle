83683e31d33d800f5e5d1a75636e4ad635e6f9b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:28:13 +0000

    rule depend calmly away carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
