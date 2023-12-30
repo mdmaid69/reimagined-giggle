c83ee479a9ce8bbc4fd387d737ff972106f11268
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:22:32 +0000

    song surprise fault through opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def reverse_array(array):
-        array.reverse()
+def get_string_from_array(array):
+        return array.tobytes()
