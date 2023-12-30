6839b19e9fb4fffa12bb9e3ba0774e6ba2883a7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:10:10 +0000

    person sticky chin start personal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
