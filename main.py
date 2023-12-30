5e41d541b34a1e59df21367c1ef8731fa3829786
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:20:28 +0000

    combine thought ashamed would certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
