6e7ce567fa37506da12bc538f8f91713aa4e5b1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:50:41 +0000

    circumstance certain news because necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
