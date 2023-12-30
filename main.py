1284715fc6f79053188a97db303eb18734307752
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:15 +0000

    hurry unless future eat cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
