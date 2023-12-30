c1dfcba2c80b3b8e3501670c569873699d835012
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:54:15 +0000

    fork offend feature understand dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def count_elements(lst):
+        return len(lst)
