846f8c6c72731f6ec42a125cfb3d49c54c3c4686
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:42:32 +0000

    switch lookback(on) field mouth spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
