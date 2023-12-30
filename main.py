7703fae7e2909fe49469e077d4a44ab2efc1b441
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:53 +0000

    left door band mend style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
+import array
+def clear_array(array):
+        array *= 0
