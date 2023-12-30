c1c6c1b3bbb34ebbc2d8a71327778c0fb000eec1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:50:56 +0000

    feeling giveaway try benefit offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def add_numbers(x, y):
-        return x + y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
