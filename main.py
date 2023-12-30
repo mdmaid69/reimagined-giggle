554c69753351d9a67411fc3836ecfc3e15317c3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:26:20 +0000

    appearance while within claim Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
