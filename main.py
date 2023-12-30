81cce9a21e628aa113705234e207638a6df5b2f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:10:49 +0000

    switch change trade comedown invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
