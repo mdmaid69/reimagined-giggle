ca258917bb954b344f02d1231a1b6d48a05b9192
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:33:48 +0000

    against injury fairly kind fetch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
