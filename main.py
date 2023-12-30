1fa9a50d94129c6ef232936b949767b97d59c3e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:55:38 +0000

    mark cost upwards discussion help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
