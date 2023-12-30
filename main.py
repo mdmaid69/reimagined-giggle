7aa54fa6fde4f0944ef165718e8432403a93793a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:02 +0000

    out office improvement check stage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_index(array, item):
+        return array.index(item)
