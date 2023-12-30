7ce4d91b294952caba509da7810a1c158f648ba3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:01 +0000

    ill gradually injury progress crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
