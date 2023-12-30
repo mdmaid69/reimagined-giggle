6f00981a90a9d9a98dfc7741573fefb3975644ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:07:52 +0000

    pronunciation city move go touch

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
