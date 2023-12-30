175a9729787b08de3bc73840547965a496afa87f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:54:30 +0000

    bank formal report satisfactory similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
