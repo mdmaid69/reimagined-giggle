a80cf4847d286e78f38e97dc858771bdb67f0e60
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:58:15 +0000

    suckme belong my angry still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
