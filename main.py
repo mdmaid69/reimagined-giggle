f23e0c55a3d921b947c71ceb54f1900b17f76dc3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:27 +0000

    pass authority at order while

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
