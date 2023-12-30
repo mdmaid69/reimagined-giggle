afa95436736b809d8dd49e9d72b3f14097a61594
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:58:03 +0000

    immediately scare light dark fear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import os
-print(os.getcwd())
+print(json.dumps({"name": "John", "age": 30}))
