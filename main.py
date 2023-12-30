a777815bf885bbfd24edc15f01314ec7de164bcf
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:32:03 +0000

    approval twist error ride free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
