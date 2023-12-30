47eee750eef8824b1f0ce517212ff92d9b4b99a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:39:02 +0000

    takeoff walk bar keen juice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
