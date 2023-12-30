d3938643e58afb6a039d60b6ffd0d332cab3c9a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:55 +0000

    fairly although turninto our sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
