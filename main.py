6f8f56251a73c11aa976ac6ad74264caca0975f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:20 +0000

    comfortable declaration ours of godown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_index(array, item):
+        return array.index(item)
