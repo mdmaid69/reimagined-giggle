4d68f9ddfdd6e71a92f2c8e97d9ce76a97f46a4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:31:02 +0000

    shut willing away complaint store

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
+def sort_list(lst):
+        return sorted(lst)
