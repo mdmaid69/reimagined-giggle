afa222008776f9975f173c8096cb193666d73c59
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:42:17 +0000

    free rubbish disturbing line exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
