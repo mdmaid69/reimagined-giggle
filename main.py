a0d244ea48ef6f305c37ec50a583affaca2ccb11
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:53 +0000

    switch suffer mix down town

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def sort_numbers(numbers):
+        return sorted(numbers)
