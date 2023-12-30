c2120204ad224b62edb96500d272c984084281b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:56:05 +0000

    shopping check minority bury closed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
