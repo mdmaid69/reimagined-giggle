4923b52d16b671dba5e7fdfa1ed3abaf24870bd0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:45 +0000

    stupid who expression hospital speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
