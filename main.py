0fb083d8ece5bedd79133a8ff52cb8f25a2e49e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:35:52 +0000

    score marry callout keepoff apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_min(lst):
-        return min(lst)
+def convert_array_to_bytes(array):
+        return array.tobytes()
