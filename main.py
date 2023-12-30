916becf6fe0f2419c91a8c86497d906cfaf97d8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:03 +0000

    such spot healthy demand famous

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import getpass
+def get_username():
+        return getpass.getuser()
