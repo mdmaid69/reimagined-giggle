3898c7a8cb5f7e48939f1c948fb2d40c71363640
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:49:27 +0000

    argument punishment lost drink language

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
