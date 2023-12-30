ae04d1fee8d56507a8b89772317c019684643c64
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:44 +0000

    flat take strong little search

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
