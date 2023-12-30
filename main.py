1573d020d607fd7b587d52a0cdcb3359ecc62eff
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:26:17 +0000

    hope go breakdown underneath new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
