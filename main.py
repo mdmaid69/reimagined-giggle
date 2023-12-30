69cc44bf7ddd25caa0f39a3bd17c0faa8d8e3281
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:51:11 +0000

    window wind block repair workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_min(lst):
+        return min(lst)
