0e1f4ffefa44d68f8227e6ee2e03cf051d210fbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:26 +0000

    card praise disturb type they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def multiply_numbers(x, y):
-        return x * y
+import sys
+print(sys.version)
+  import os
+  def split_path(path):
+        return os.path.split(path)
