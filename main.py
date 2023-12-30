7b233eaf497f1b74a287c5d4d8b22ad1aa46fb65
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:08 +0000

    goup smell interrupt ride pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
