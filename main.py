0b08b9d72413917893dd592fca136dc523d8aabb
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:58 +0000

    break force shout whisper measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
