3001b615f923a3cf6cd20fa9ad90b714c0da29bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:44:12 +0000

    on least inquire criticize plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_speed(distance, time):
+        return distance / time
