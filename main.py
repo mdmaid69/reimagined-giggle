2cb3096d9ccbf4cb78f31148d309ad6dc4216706
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:53:46 +0000

    pot typical moon proud board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
