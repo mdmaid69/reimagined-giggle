6854500516f0ccd163ff42bef45427de938584ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:48:26 +0000

    scrap time length list display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-print(os.getcwd())
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
