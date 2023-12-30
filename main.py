d98d4c26b26a7d7e986409737b7088b0582d7f86
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:57:07 +0000

    rain tap turn stand pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
