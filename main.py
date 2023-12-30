d1d3241fb28bc834afb7f37c27b583a779e31e55
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:57:03 +0000

    order divide confused walk improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
