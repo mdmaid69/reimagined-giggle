5785d5309dc76310018f0d9abf641812c3000d9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:18:20 +0000

    mysterious sick active along onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def square_number(x):
-        return x**2
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
