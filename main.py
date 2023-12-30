1b7d79134840e9e18a2ebef213f1430ffd46c45e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:16 +0000

    chase road wind spot move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
