b1b2ed63075f90151f4e72108467534b30d4ba6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:58:54 +0000

    able swallow late understand match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
