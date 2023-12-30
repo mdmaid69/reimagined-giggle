474ac915806c84169c09d32109475dfe8acfff89
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:50 +0000

    goaway precise set team insist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
