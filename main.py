b4fb6a05046e8eaeca7eb389c08faefcf1f96dea
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:46 +0000

    appearance recover report someone stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_counter():
+        return collections.Counter()
