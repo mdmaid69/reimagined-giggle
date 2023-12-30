b2d3b4b49ba286472b4f82d9a69d48cf33e4a653
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:38 +0000

    independent breakfast friendly harm no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
