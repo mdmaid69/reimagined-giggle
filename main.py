76da3e9593ab1cc0cb368b67f2cd4b40f973d779
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:20 +0000

    storm runinto thumb exploration long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
