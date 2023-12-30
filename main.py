6a0ebbdefbe60b3f0b351279c4946b78f3aa9194
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:18:18 +0000

    free attempt real label backwards

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
