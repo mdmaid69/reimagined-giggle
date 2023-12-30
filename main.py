f67bd3bfa4d4f77fd1089297ad299c65a5fd5231
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:00 +0000

    age awake tired introduction order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
