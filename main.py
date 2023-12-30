6cd7a02b8ac52ab8fed03d7aebe7335875b7c9d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:55:34 +0000

    themselves married load before traffic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
