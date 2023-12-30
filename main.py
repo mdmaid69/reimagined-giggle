1ad80b7ff6e312491845f927c165281f7e59ba3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:09:13 +0000

    area shop knock stiff criticize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
