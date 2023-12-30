b5e1f8ba8fa766ba79c233a04e7068b3b2322e6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:06 +0000

    activity moral mark my ashamed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
