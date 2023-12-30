ef51005641f668288fe3fd83bcf3a54e71272557
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:16:22 +0000

    again suggest like people dish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
