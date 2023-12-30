db20b5fb35573e57403b4dc6beb9f164b0abd7b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:42:42 +0000

    draw factory have building if

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
