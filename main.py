bcbd42d74096406f6f8d334d8c53f75969bb8396
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:04:01 +0000

    chin by glass second plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
