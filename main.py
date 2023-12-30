5e45e80a049199e29868e9415a8f3b9dafe30343
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:19:21 +0000

    protection delicate contain grade workout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
