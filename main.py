951745c79a04d0ec7d2dc5b32f2bf94b26926ddb
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:38:23 +0000

    bear raw man accept squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
