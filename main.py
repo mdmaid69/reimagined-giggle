9efa44c3ac2886eb8b36641b74c86ba06d7b2f69
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:23:11 +0000

    terrible shoulder neat niggaacademy brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
