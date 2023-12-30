07b1b52a16c49589c05abf0529653f266a60044e
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:37 +0000

    clothes spot crash noise certain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
