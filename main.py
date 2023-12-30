f86c0bdde14218b45620c707757b44d4baf72923
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:36:01 +0000

    plenty check able ours best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
