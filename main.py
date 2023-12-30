fc8987216728c2f645c3425c0ca9625d412a2794
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:17 +0000

    friendly total underneath tune crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
