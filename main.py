ac8589a8ca3a590550645fa56a93ba6dbb338f7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:47 +0000

    scene calmly own national turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
