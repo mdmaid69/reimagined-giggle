9ef81953a05e10f442907bacbf690e58695dbd9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:09:49 +0000

    man coffee fall distant how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
