f241623e5af517ff4efdc5407fa66301e6c40e07
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:41:32 +0000

    talkback put unite forgive equipment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
