40061dce0404662c9b9e41f3a9413bbe892d8503
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:16:27 +0000

    connected praise raise suggestion weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
