62cbe1890c167bc1d1f17ae6d1b3693fcfecc651
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:25 +0000

    ever might discussion range characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
