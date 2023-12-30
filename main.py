b45f762be4ea951e76fcbc0c1a353a05d3e3cfd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:24 +0000

    yet adjective material today home

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def subtract_numbers(x, y):
+        return x - y
