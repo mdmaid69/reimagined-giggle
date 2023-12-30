30c80e1b8f14bf8c0dcbcffafafc72a6a1b5e3a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:57 +0000

    train address goup brother around

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
