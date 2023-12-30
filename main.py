dca165c4de47ab54a3aa8745b751ea5dc7931544
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:02:15 +0000

    hate right understand your level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_item(array, i):
+        return array[i]
