817e7909df5ced33a4728cba22edb8ec59c8fb8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:14:30 +0000

    attempt guess turnback swing absolutely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
