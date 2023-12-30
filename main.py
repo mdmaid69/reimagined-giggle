f7713be36739fb814fd89fe5bd59d35b34817dca
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:17 +0000

    angry guess about high measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
