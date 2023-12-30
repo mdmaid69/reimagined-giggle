10b087aa83248fea5b004cd51cb1216750a4975e
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:39 +0000

    rub offence drop part sale

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.ctime()
+import array
+def get_list_from_array(array):
+        return array.tolist()
