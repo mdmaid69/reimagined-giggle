e05b2e641e7726a88fd64b436d9b7925e3322a87
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:48:24 +0000

    increase summer meanwhile bored light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+print(os.getcwd())
