c15b5f1da53fd4a49999c4be308918f8fac53877
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:18 +0000

    even kind dick simple plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
