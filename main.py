5642a6f605f3793ca84e3ed14bd5923dfcdc71e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:27:12 +0000

    around woman sport forget fold

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
