3fd1b8f3cfbf04d15400fc6349c1176f1690236f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:57:42 +0000

    hope flow break circle Jj

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def add_numbers(x, y):
-        return x + y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
