9068f2a83e067eeb2bcd70e95437fba6dc87afb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:03 +0000

    activity explain permission storm calmly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
