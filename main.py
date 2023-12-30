05ceceed3c8574ffc0d09126ff0e3981c74fb211
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:00 +0000

    fire hit crash name afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
