4b28b5ffd5e2c118c554bbff962eec6620b64e9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:50 +0000

    way apparently preparation instant straight

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
