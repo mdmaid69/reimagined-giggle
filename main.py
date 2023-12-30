662e8b5342b201fc4edba7c8abe339a9c68f06a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:02:58 +0000

    model broad at study smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
