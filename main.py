ae84c22c33d7e489cc0cf68ed794d63e1a40fa95
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:57:24 +0000

    pity hope embarrassed upset professional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
