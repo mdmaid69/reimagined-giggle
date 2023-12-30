b33a8ce901f700c473b21e7620f93821a34c92c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:55:52 +0000

    grade home somehow project possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
