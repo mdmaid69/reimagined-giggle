ba82336b5cb3f7a5e5f4f7efdaf2160b4cd89d57
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:14 +0000

    wind reduce Qq impressive big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
