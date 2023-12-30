ce57ba017b7acba3b59cfd6adbdfd542f042daa9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:14:52 +0000

    now doctor whoever command mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
