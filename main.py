f1ac4627d2fc6308a35b08620b7e6993c8a8f7d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:31:22 +0000

    tell stupid previous supply surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
