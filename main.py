a69541a75b354668de94c8d1694d571c317bffb8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:03 +0000

    table immediately long tax fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
