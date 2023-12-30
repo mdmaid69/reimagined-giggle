a98fb57caef669a3dbde4443a946badc97959655
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:25:41 +0000

    normal measure while indeed throughout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def find_max(numbers):
-        return max(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
