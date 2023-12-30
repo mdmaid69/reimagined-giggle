838ffffe45a39f1250ad430973036afd3aa2936d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:18 +0000

    without lock lead smell Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
