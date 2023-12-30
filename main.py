f999d5dd4da42a209c0e6af6c1ae1270e0f9d9ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:13 +0000

    best turn spoil complaint whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.time()
