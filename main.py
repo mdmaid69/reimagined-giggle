bf0e05767a56d9c268fc767c5d852dc41e440c96
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:47 +0000

    willing shock calmly pure sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
