c16f8dffa583f69fff65d5cc7bd2467165d39ce3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:34 +0000

    foreign scream shoulder she split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
