ad3969c446491487382929188944ee3db3fe9a94
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:54:46 +0000

    serious pleased part sleep introduce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
