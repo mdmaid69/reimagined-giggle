1f0b6baee595867ff8ece9fdca2ccb03ea08313b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:54 +0000

    calm alive odd progress fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
