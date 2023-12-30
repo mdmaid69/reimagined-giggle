253a4ec2699a8191cae52e4405616d35be8cd3d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:33 +0000

    cause down only belong drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
