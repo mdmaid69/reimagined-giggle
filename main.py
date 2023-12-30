dbbbeb061216758881950f9e67fc6c4d73055bd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:30:59 +0000

    pity argue slow Dd left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
