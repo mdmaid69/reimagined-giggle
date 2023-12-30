8121d42e1754eeedee20492663cdd17f11052935
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:40 +0000

    guide half impossible niggaacademy stir

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
