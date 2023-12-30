e0052c88a761f9270ee877a33f6fd0fdc40ad5ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:08:39 +0000

    anything happen getoff search pen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
