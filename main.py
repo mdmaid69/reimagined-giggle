15ead95c098971a881a13e8ad610a91550077b23
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:39 +0000

    trick brain mend wear rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
