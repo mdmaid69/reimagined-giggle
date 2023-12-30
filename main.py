8c020f9ce143efe401d02334bb985515132744ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:30 +0000

    inform receive impressive hollow may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Characters:", len(text))
