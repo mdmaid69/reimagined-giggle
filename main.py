beacd7bc9119ea48086ae766b2e6a60ed7124b71
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:01:42 +0000

    suit clean shut pipe sort

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def reverse_string(s):
+        return s[::-1]
