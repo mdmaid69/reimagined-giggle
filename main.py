ee49abbe8bf68a9f81445de7ca31a23ea1b49690
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:52 +0000

    move generous game suit yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
 import os
 print(os.getcwd())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
