2dd4edce0ae758ed193d2576c48e8215a999779d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:06:39 +0000

    book beginning chase pronounce computer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
