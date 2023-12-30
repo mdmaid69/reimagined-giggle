6d78a26400f9d456760eca478a0770fd03710a64
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:02:36 +0000

    steady turninto lower many Cc

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
