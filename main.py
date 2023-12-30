69a2f7d3586e107c7428ab7792489ae4106d3816
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:24 +0000

    turnup makeinto Vv mind last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-print(sys.version)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
