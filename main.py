2dc9e8bb091e03813f7cbc1ba10fe47f8a785bde
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:05 +0000

    deserve fork at from value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
