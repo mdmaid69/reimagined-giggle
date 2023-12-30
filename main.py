dc15dbe48baeeb32155e17b16ad14154138c29ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:56 +0000

    interfere love definite exam rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_float(array):
+        return float(array[0])
