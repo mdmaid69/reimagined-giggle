0f5b5de2d28d414b886554d6f34d802ac17238b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:24:21 +0000

    action that tie balance father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
