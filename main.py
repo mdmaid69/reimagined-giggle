7f669593b967d2ae6c980d9cf4dc39d1825947e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:49:49 +0000

    comefrom back open compete sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
