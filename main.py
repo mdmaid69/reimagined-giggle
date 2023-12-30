f7b91697622df0a573ae757d3a383c5b14ed173b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:50 +0000

    except boil sting steep flood

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
