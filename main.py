c3b7038f52d975f97c997208d18fad3323e5e7ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:48:52 +0000

    equal hate mad good influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import collections
+def create_stack():
+        return collections.deque()
