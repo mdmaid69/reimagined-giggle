749ba32583795899fd55635b50a1dde2622b9aff
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:27:36 +0000

    sometimes simple interesting choose cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
