b02d30f55324f392d86925a53f1fd260618dbe55
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:39:13 +0000

    today praise getoff season passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def cube_number(x):
-        return x**3
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
