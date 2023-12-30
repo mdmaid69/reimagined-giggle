fe24ec0e09c2244effd22163d024daf3a8f7663f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:10:05 +0000

    form father within across cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
