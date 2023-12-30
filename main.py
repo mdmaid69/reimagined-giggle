6258e33acd62a2ebe512a82b472481b90db964cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:49 +0000

    slice cheek comfortable strict object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
