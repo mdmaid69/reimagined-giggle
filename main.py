1cebd5b8176b4b0a6789235da70952de8507dc0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:06 +0000

    money clearly approval praise 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
