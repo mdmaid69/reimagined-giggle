d7d4d270e13a87ea98264507143d061f65fb0efc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:40:19 +0000

    for turn day new lead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
