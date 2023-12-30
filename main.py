1e7faec5714e87204bfc5a94505b8b5d3d157a8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:24:15 +0000

    pool kill exchange the shoulder

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def greet(name):
-        print(f"Hello, {name}!")
+print(sum(range(10)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
