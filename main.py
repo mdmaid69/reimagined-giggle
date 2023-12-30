1395403543ad08071f80993ebe9d9c61889de25c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:56:40 +0000

    model apologizeapologise freeze apart prevent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
