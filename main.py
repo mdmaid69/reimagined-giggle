9abee524f4e76d87c9dcb6b1ced31f03425422e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:15:31 +0000

    bush free shop callout Ss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
