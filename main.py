8b59a7a8bb6ad0ecf34e0575def53e5a7c094447
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:17:45 +0000

    contact plastic decide free although

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
