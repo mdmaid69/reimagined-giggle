a8f6081f870647fcedc6e26bac9288e16c775290
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:38:37 +0000

    familiar petrol intend walk fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
