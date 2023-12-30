e22410088ed63701faa054c1ea31fc46341e6c34
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:16:57 +0000

    now workon recover report close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import sys
-def exit_program():
-        sys.exit()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
