c0ce0e2c4e88a7deaa3ce2f1983fc3f5674229bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:11:16 +0000

    cough question secret beautiful practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
