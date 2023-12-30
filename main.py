a9ec048d981df013c89c56122538f23507bf75ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:09:38 +0000

    own plenty traffic official tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
