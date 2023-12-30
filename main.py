88b05309bd36ed280bf2a543bac8a3a9ca91e7dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:47:50 +0000

    welcome mixture smart destroy thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
