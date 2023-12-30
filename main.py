0292d2318f86c332707c2d4bd6373da72e17ea21
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:57:15 +0000

    apparently comeover slight substance lead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
