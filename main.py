ba7238db7d23bced0cb4db0e8148f346733fd32f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:38:34 +0000

    reasonable snow meeting opposite stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 text = "Hello, world!"
 print("Words:", len(text.split()))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
