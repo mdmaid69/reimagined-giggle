e314fb86724860fee5090894697e213bbecdd263
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:51:51 +0000

    earn scared waste Qq provide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def split_path(path):
+        return os.path.split(path)
