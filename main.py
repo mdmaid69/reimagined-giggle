3bab792c13a770be64c1e51e602b69001d25eb3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:32:03 +0000

    warm comfort crazy serve skill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
