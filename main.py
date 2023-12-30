800f05a198b31c153b60e1610d4a573ee4fe4ef2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:07:16 +0000

    term excuse fold plenty accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
