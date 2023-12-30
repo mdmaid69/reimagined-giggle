bbc05a2a9e2700a4a0d0b8c70e64a32d67437741
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:47:18 +0000

    your married mess emphasis offend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
