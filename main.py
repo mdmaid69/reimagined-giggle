75f70fe3e9959facac0f2c5390ded51e7f83b00b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:07:18 +0000

    danger adjective promise discuss frequent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
