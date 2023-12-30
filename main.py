96fb3222c2d353deb70488e6213bd314f20bda43
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:24:07 +0000

    talkinto wherever scheme getout OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
