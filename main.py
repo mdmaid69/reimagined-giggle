9eb8c5f164700ae6b6d8594326da47e0681e448f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:17:11 +0000

    trouble gate make this exchange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
