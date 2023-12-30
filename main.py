0370e88714cb72a9072008e5e5c15dae3b9f579e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:43:28 +0000

    project mouth means intelligent getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
