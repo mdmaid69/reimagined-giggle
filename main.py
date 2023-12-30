e938f432a7cc94c7f94c6bde4af05ff04d3592bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:59 +0000

    quarter rid except turnout combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
