3c35c335219a9937b17c7e885a6cf267c7a23335
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:15:01 +0000

    dull bet spread present limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def is_odd(n):
+        return n % 2 != 0
