d5e650a027776c3c3c914d00d7a92277f25fb412
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:58:21 +0000

    than learn radio limit apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
