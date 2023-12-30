a0816e9e334c9f2a146ec8340bf94548edf581d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:07:10 +0000

    wind me theirs card limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
