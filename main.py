39a35f99a141274b505a034be5a37dc30a1c7996
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:18 +0000

    further collection gentle upstairs comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
