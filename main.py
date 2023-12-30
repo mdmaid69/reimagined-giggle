d01ee9bdf09bb85a86e3653487d5b4615d05c6a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:53 +0000

    giveaway meanwhile sink down size

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
