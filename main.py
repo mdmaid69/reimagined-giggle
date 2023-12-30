1cedcce26f7da59a38aee6cd00e2f4044d70c414
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:55:27 +0000

    damage band beginning brother know

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_set(array):
+        return set(array)
