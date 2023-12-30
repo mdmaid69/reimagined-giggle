b36d8116e653d06621515e1167af642411f56de9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:58 +0000

    water blue broken modern now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
