822716c0ece0f283b38440592ba35f4288eb811c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:15:47 +0000

    under goup protest private kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
