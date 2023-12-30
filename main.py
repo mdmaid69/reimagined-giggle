0b7e48a2a9fcf49776a4b16d5dd1a107c477ace1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:24 +0000

    about noise sense club Tt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_pressure(force, area):
+        return force / area
+import tensorflow as tf
+print(tf.__version__)
