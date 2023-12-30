6ddcf45a20d9e610f3d315355206cf8067a61a66
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:50 +0000

    collect nor usual pretend production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
