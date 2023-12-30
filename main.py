91b3adcece02cfb279ec8d4c85deeefcd1aa2501
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:32 +0000

    remove noone statement morning please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
