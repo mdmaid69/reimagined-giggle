64580e9bbbd135d4cc3d0e06fcc154170c6fdb26
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:21:02 +0000

    snow dare tough nor light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_str(array):
+        return str(array)
