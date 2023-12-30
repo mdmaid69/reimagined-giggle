d71e86f1ecd3dffeb8623f07424e785e7aed2089
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:08:07 +0000

    object sister dress purpose all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
