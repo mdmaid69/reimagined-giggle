5d55707e594229c363c9a97d84c4915165e54998
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:27:59 +0000

    communication temper free relationship coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
