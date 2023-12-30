8d2c90b0a0215bc7c3da499f3f8a64988b0c959a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:22:14 +0000

    light confidence long away frame

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
