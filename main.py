c458d0d69cf620247710313c9009def3d8e8d217
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:28:19 +0000

    pair scream bottom apart suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+def get_current_working_directory():
+        return os.getcwd()
