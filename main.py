f35c97ad84afaffcc34451f5c432e8abf2977f8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:54 +0000

    eager extraordinary age shape what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
