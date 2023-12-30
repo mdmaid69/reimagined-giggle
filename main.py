b214aaa7cf20f538579e3a28e3c9fafe37acbd68
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:32:00 +0000

    brain equipment our better correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_volume(length, width, height):
+        return length * width * height
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
