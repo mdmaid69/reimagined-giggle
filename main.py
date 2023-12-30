45139975fc812ee2007c687fadbe91aabd1b81a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:31 +0000

    drop that picture key path

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+print(sum(range(10)))
