eabeda258575cfbe30a84fb403dd83756338f763
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:49:06 +0000

    schedule row really dig imagine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print(os.getcwd())
