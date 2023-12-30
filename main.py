d4117b7319eb8087fb11062067ba194e93dcbdcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:51 +0000

    dear goup give easy live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+def log_message(message):
+        logging.info(message)
