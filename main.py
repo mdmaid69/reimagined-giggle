1e65a3d262356db8e8c1af9a0e8059b69671a5a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:05 +0000

    family typical shoot course rather

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
