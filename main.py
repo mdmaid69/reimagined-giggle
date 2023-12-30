10ea5764799285a8a74733629923ad6a0cd7da52
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:22 +0000

    machine walk lie school centre

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
