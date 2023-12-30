cd287b5a914e7526f620b287fec63584d4809ac7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:28 +0000

    our another delicate which battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
