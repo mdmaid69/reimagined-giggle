78d2a6cc9e586fc66bb56a4b923a46bf5c8d3b62
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:24 +0000

    unable change join present brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_list():
-        return collections.UserList()
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
