baaaf44a18cde8f3ec35c5c8aada0dabf9115a71
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:01 +0000

    rid mood city worried near

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
