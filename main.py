1e5585e6a2d5e0b09f68cf321f379dfd145dbfed
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:03:50 +0000

    loud giveout oil fix suckme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import platform
-def get_os_info():
-        return platform.uname()
+  def calculate_median(arr):
+        return np.median(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
