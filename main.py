c4500670016821e31100c7ebec73af06cdf0adc7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:42:34 +0000

    letter most unlucky home reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
