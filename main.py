ca4552debc8cc0f75d9be6cebf7a43a3bbf30e38
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:13 +0000

    passenger section sure male stupid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+print(time.time())
+def calculate_power(work, time):
+        return work / time
