78ac77d577ff9b88a6d1b4c373230a36c9497d54
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:39 +0000

    suit culture young question yes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
