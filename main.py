8edeab538522573d9ab732c95dcdddd44b820419
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:56:06 +0000

    practical rest door chase lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
