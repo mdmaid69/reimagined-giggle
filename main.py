84d2f05444edd75bad791e7f9b74da387522c382
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:08:10 +0000

    bet shut send one principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
