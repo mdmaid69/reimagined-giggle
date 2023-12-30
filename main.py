56fa5590ec46588d302f1712f15adba1852f0438
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:23 +0000

    start so why large exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
