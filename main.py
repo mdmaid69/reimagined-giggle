1fbb1a4991a2ffcd4ac8d48c8264972716b44258
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:46:09 +0000

    argue slightly himself object off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
