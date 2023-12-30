76e993c443a6e9b91e6a22eb672330c67c21ed1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:24:55 +0000

    middle dear order future admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
