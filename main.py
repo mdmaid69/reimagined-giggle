5a63e7651db40d6650ca2f482972894dc8df6cea
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:32:45 +0000

    566end tell emergency serious she

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
