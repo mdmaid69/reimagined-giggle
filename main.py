94229c6ef68506844a798491c703e2c529596a7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:57:04 +0000

    whistle spot flat tragedy mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
