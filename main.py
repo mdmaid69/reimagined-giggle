e06e16652e8cc2c3ffc93f73444f0905ee2edfe9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:00:56 +0000

    receive between shame fail pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
