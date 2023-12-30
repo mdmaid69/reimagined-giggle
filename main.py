4b0a9fd2121fcf0e0dbd3f8ed66b32384707b01d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:40:22 +0000

    advanced very test protection measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_slice(array, i, j):
+        return array[i:j]
