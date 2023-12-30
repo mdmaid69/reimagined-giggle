09579628dc28e4a1c9c302adb62bae029391fa2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:21 +0000

    birth turnup shout evening ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
