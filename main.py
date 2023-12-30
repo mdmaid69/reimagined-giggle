b5a0a31c0cede867348d7a219a5fc92ae4b74b61
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:11 +0000

    obvious dance message majority juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
