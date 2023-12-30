f365c564aa7e74a3c6f569cd5752baaf707e1961
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:18 +0000

    bang degree successful sail pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
