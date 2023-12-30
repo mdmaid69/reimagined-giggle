eafbe15c849e7df756d300500504ded5b36e199b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:29 +0000

    journey runover taste combine well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def reverse_list(lst):
+        return lst[::-1]
