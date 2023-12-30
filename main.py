dd95c67e9c85fa51cd56011424a6bedcc564db64
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:51 +0000

    dull plant head hear common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+for i in range(5):
+        print(i)
