713cc7fb08374a3d44537e1231ce64369f69f816
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:13 +0000

    level interrupt edge crime plastic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def convert_to_hex(n):
-        return hex(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
