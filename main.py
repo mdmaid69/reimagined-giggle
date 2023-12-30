cf35e935b9192eb78e8e627e4a62fb47435cee8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:12:02 +0000

    but ticket gather this jump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+from collections import Counter
+print(Counter("hello world"))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
