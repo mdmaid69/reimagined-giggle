f9b316630432e8fe0d72ff229a8a346e552a5e26
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:52:35 +0000

    tie interfere fairly treat Kk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_stack():
+        return collections.deque()
