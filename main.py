9ce1ae4f31f2532e60cd34b840cbca05a8b86b66
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:13:43 +0000

    hollow early lesson suffer fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_item(array, i):
+        return array[i]
