3ffedbd76b73951243498224184c20d19cd1c982
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:03:03 +0000

    tooth but somehow excuse main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  def convert_to_octal(n):
+        return oct(n)
