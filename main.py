cfe5545eb1ad909d9e6afdd41cfb5b822a02d3a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:56:07 +0000

    look quit Dd turnback passage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
