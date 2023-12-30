019c4a0c5c4b533d3243e5eefa608e34cb481fe8
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:35:45 +0000

    interested tap moon dark own

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_odd(n):
+        return n % 2 != 0
+def subtract_numbers(x, y):
+        return x - y
