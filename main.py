707321b14061b708a0deb1c7962919c7a1f03f5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:27 +0000

    tense rain Pp step drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
