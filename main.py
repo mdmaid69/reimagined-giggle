c21f4c6859ce73c11fcb0d0f5338c2ac4a609054
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:47 +0000

    act split back keep offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
