ef12491090ca9813c08f7ae0112920f9092f19a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:49:57 +0000

    name successful information couple one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
