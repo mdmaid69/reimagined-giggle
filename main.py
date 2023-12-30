5141058b259dbd970cffc4a374383979950e1b7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:09:07 +0000

    expect little cover comment profit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
