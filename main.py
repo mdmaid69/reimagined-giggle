784307fe873bba13ece03687c413037434a63ad3
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:39:34 +0000

    reasonable hole stiff explain them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
