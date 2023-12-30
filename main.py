05c926893273b4c93d4835b740f3e352fba7f0ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:49:45 +0000

    evidence vote square partner decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
