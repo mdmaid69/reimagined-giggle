e51ff5dc4c5f13dd724ea3af4d0925901325ef35
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:13 +0000

    moon partner gather better read

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
