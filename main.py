f7a7aa20565424cacd36e1f52b172a43a3004561
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:10 +0000

    describe feature something interruption word

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
