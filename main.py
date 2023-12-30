5b8bc17dad2733ce8002e50e2405def84d897287
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:09 +0000

    unpleasant home dangerous outside total

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
