e64a4934a26d7de056a93f722f6c7b5748b00d81
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:48 +0000

    uncertain warn unless age calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
