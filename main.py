7624b7f963f6869bc144d53904d69e994733119d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:53 +0000

    afterwards reject then shape until

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
