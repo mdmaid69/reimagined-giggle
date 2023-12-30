d8db9184c412f69458a07338fd6bef2601bf5b7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:36 +0000

    lend clear suit fetch say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
