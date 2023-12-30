31ddf8e4a8f174f50cbfabf364b9b22200d4154b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:24:34 +0000

    size takeback hate immediately taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
