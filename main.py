d2ea0a4d027f69d268b8a00e55b8367194b9f259
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:13 +0000

    afterwards generally but allow train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
