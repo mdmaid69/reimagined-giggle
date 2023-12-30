acb00e1c731619f18a125a5726a5f7f0e2bad094
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:16 +0000

    car score car poison earth

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
