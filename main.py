3b0b3e0941c8bf27d65ea5820670074907bb7316
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:22:05 +0000

    offer regularly star inquire account

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
