fe1626c18f78c9751bdcaf18dfd5b71fa8ab8243
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:35 +0000

    above clear lend only this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
