8e0884399e2874f1165b946179906fa967dd5db1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:53:41 +0000

    fortunately mean confuse lesson ourselves

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item(array, i):
+        return array[i]
