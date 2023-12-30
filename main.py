0795e951d9cfe8b08f866a53d688da9c399292e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:32:38 +0000

    sorry heat wash rest unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
