45bcf70b22d93d2b5edf99f3fbf80707ba2e2c34
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:16:37 +0000

    passage you experiment otherwise care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
