1a0543bbbd8e5b360da70b5e3ac7ac4d0b6f3721
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:29:44 +0000

    fast some figure receive permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
