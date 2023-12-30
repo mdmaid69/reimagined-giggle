e3ec3861a36002191752fa1e3f44e02ca8706096
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:21:32 +0000

    course please brother avoid such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
