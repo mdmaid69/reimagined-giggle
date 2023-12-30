841a968867da1f34313e84891b25a8321c35b110
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:37 +0000

    comeover shine worth hide damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def clear_array(array):
+        array *= 0
+import array
+def get_array_itemsize(array):
+        return array.itemsize
