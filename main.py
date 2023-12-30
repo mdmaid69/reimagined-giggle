e2867a44b9d1a1a3d938701313e58176bbd5449f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:39 +0000

    opposite bound task smoke motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
