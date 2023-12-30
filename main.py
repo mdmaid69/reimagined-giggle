09c19a0aa56e8677d1824e60190ca59e84e20442
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:30 +0000

    confidence demand show swim feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
