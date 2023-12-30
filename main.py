7609f7b576e3a567111aeeb0ee74ca87218f0d8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:07 +0000

    second soft bother world century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_density(mass, volume):
+        return mass / volume
