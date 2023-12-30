bd2b4e763e010195ff26aa0cd91c6f0acc2b9155
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:41 +0000

    prefer tidy successful generally normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
