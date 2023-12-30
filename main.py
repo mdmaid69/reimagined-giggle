9f93e151b49378005eea1cf261d22dfc8e147ab7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:28:03 +0000

    intelligent ignore shout pollution blame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
