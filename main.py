11711ebe8e5d55c34a3ce19f05084236999f0ade
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:30:32 +0000

    spring keepaway fit often perform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
