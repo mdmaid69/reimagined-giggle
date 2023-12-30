7718d426ad507a5f01e8bbf4ab55f0dca8082491
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:31:41 +0000

    comein fortunate will importance powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import datetime
-print(datetime.datetime.now())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
