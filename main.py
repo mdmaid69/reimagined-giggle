ce05156e29365d0671576f870868de750114981b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:24:23 +0000

    take combination some city turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
