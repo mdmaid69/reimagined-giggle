29899db339af7f3ffe0dd54a546f78611b6e6880
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:23:29 +0000

    determined who turnaway getoff suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
