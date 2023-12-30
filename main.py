e45ab007008b8d442c106bcb5496bc038f0a3007
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:17:42 +0000

    wing greeting under bet kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
