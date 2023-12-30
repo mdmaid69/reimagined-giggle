3e753c03ec30a3c5b5f7a928355ec7fb60922ca5
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:04:38 +0000

    mess taste reject hurt month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
