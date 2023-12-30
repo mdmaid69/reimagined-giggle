ff2899c290d2173330f282b3e8c56eccc23798b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:25:16 +0000

    strict decision free tie our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def subtract_numbers(x, y):
+        return x - y
