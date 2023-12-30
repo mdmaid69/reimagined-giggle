16e7c72aa7508ef5cafa729f3dc28398f4d05b64
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:58:27 +0000

    event pour fail fence further

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def convert_to_binary(n):
+        return bin(n)
+import sys
+def print_python_version():
+        print(sys.version)
