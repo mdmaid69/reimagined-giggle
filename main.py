8e72615dc5d5a61f07df06c88247ecfefb510d1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:10:03 +0000

    cough tell risk thinkover miss

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
