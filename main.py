e89979f543a18ba8f8753244c8c2a5df4ca71abd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:59:08 +0000

    whenever get suppose whoever fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
