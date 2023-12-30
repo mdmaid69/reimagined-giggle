a52d0fd46959cb70bc85d077fb3629f8b9d0496f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:34:49 +0000

    measure suggestion near present object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def add_numbers(x, y):
-        return x + y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
