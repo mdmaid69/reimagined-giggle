e0654559b797fb8ec1aa08e9967fa868661bc197
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:55:00 +0000

    return appeal lock tidy damage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_even(n):
+        return n % 2 == 0
