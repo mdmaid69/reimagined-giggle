ebeacb514795d4397c7ee7596eb8f3680a6fc1ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:42:34 +0000

    exaggerate guard holdout transparent shadow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def convert_to_hex(n):
-        return hex(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
