0b8b337aada50ce821c5d45af42aac74840f2ed0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:46:10 +0000

    prove spring probably second meal

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_int(array):
+        return int(array[0])
