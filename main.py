8460244ce25648a6e56ae08439ceb6f311fa4b3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:19:51 +0000

    goout behind lucky goodbye progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
