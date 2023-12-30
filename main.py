fe303af89f75dcaa66fc67081f1573279ba3ee2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:51 +0000

    contents smell hollow firm rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
