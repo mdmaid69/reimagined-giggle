898f900853839cd204a441e8f249cf6451b155c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:25:30 +0000

    production Nn his middle today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
