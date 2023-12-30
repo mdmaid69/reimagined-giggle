da3651d225b562ea776b2be8a59e4db2329af2f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:32:10 +0000

    safety book temperature style centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
