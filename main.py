cdb3e95f370e9f7ce7aea3e88193e53d93436e96
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:49:54 +0000

    tool whatever which handle later

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
+def find_max(numbers):
+        return max(numbers)
