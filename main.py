1ad9555597498afa24614ce3f03a5999dcd7d111
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:07:54 +0000

    smart typical shoulder ill any

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+def get_current_time():
+        return time.time()
