e779da3835bfa20d0447b5d8b09e5151b5d16837
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:13 +0000

    remark 570enjoyable goup embarrassing broad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
