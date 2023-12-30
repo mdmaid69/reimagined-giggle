d296cdf66482c007ae5e093f4546e0926cb585c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:30:09 +0000

    mass wet collection relation classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
