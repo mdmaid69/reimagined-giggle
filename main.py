99999fcb850f2cb445553783f5f864acb3a7468b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:47:46 +0000

    its especially embarrassed comfortable threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
