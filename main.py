15e16955cec41c4ef4a88ac4702f0ec826242ca1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:00:05 +0000

    edge afford match straight shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
