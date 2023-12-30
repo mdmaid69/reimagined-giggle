bc233792492091765a33190c0e32ab70009257f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:50 +0000

    connection thinkthrough under treat drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
