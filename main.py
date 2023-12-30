0d7d96561e283c0e493956e05b3428fcda7290ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:05:00 +0000

    wing improve rent often view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
