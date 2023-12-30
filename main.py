83b0e63ec1fc7f72eab4f60d25b3672964ff7565
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:34:08 +0000

    finally noise form affect happen

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
