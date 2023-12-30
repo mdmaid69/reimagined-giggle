db465271f42705458ed3f933782a5c50722d5528
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:04:46 +0000

    bother limit sign what argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
