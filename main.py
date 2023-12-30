a025864a1977e71d84ca0c1d86a45658f72d2c79
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:34:27 +0000

    takeback week through thick eye

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area(radius):
+        return 3.14 * radius * radius
