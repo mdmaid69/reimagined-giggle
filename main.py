9245e83e71a1823547ec54d022acddcb0a3e422d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:49 +0000

    breakout reply legal label false

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import datetime
+print(datetime.datetime.now())
