2dcc50b617b0ce372bd4d4c47f1042addb79b84b
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:04:31 +0000

    fork draw handle opinion Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
