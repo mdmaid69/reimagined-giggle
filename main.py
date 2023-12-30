ba30f68de5daed084dc75033149791f3ec197107
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:39 +0000

    put model mistake prove rely

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
