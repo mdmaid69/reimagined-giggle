d64c3575a0c7ad265a950e207c7529ef576b032b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:53:45 +0000

    form measure bunch fresh report

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
