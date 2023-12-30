5387ed9362388f53cfd80016df954c64d6a079a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:52 +0000

    push draw only meet fast

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
