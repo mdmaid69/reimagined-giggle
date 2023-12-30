10f819f43a49e6c76018a0747174ac811c6936ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:13 +0000

    increase break jump record butter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
