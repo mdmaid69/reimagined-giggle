d03a51280e17a7673dbb0ce38eaaa77e22a3c1eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:02:29 +0000

    sink earth Uu pot machine

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_acceleration(speed, time):
+        return speed / time
