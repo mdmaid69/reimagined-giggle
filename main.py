99742d15359eceb30ab42a617dedd181eae98a37
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:41:13 +0000

    original thing splash sad discussion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
