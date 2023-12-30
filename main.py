37eb0ac1449616111398f738992ac89190d62ff9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:21 +0000

    refer my live dig before

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
