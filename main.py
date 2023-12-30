42299ff66f8623a67e05d6d59922f75e1f467c10
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:34:08 +0000

    later still after everybody Vv

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
