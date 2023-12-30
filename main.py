f17064ab8963037641136f74c25567a1f5fd81e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:10:27 +0000

    female smell style future boring

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
