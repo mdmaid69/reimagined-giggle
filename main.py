32a18a67117d1157f5010c78fee2bdb7644d7654
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:48 +0000

    prevent culture ever our scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
