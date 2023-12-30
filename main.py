ea66a47ac9b07c4d93d13a4c80ea65b609535734
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:10:54 +0000

    nor gift tap save godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def add_numbers(x, y):
-        return x + y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
