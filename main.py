f30a1f1721e34aa5442cf01bf29779cb5b5d37d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:11:11 +0000

    need 568energy family public ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
