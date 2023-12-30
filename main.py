3726e4cb1c82a421226d60eae4840b7f425bc99f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:01 +0000

    war uncomfortable suddenly loss comment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def divide_numbers(x, y):
-        return x / y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
