542b517679ccf450b221a40096cbc704f309f684
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:02 +0000

    money involve rely lookafter shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
