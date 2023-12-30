9f7f558cbbfb86ef8f72d740c4e55a61ca66f385
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:27:47 +0000

    fairly seriously typical tone relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
