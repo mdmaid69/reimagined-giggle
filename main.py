39757cde677c0d8a84e6837496ca2ef1753f65ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:43 +0000

    trust objection persuade future rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
