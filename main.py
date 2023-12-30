e027dec144b05604bd53adcd9641e8e3ac31df2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:52 +0000

    goback(to) reach wake flow pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
