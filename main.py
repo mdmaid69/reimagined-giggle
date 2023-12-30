931bd9af3f193768aa5127c7ef4e63e4af05bb3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:36:49 +0000

    message demonstrate punish ashamed suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
