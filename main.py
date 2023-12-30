629aabddbf79cb961dacd3eacf1a930eb580c766
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:01:38 +0000

    kind temper curious urgent read

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
