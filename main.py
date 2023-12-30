bdade368d67725f48d62dd9807f36a6272b3c83d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:15:31 +0000

    touch another him approval moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def count_elements(iterable):
+        return collections.Counter(iterable)
