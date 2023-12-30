10699ae56aa5f1cfcef04b8b10ed4b0e04a760bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:28 +0000

    object spray beneath throw recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
