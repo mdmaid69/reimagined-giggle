f3b24795ed03885a97bc2b20937f8898f7e909a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:09:30 +0000

    us cover fortunately visit Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
