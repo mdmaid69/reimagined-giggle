c6b76f62a431ebe7f44c31826ae30bb7794ab576
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:35:30 +0000

    responsibility our post correction inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
