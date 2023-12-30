b04d5901cc11406a1f52ba33184e3e5dbed52000
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:33:14 +0000

    shame head consult open runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
