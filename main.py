ab9907f645e24339b0bc0c91462c86e8142a5d8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:27:05 +0000

    little crisis dish butter exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
