b343a65d738e5eb95073ee1e50fba5c467ab62e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:20:23 +0000

    plenty tap happy personal organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
