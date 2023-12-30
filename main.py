526a165c409ddf367d279b6e171058d93cde39ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:52:42 +0000

    hardly Gg of argument passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_interest(principal, rate, time):
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
