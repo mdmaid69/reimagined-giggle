051d545d1d2f34f4d82b32b728aed491ff8948c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:10 +0000

    any instead sharp clean manage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-print(datetime.datetime.now())
+from collections import Counter
+print(Counter("hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
