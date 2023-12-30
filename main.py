1b6aeff5b807dda06e5b98699638bed6b3e97898
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:30:41 +0000

    far honest same tragedy who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
