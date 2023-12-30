2af325053be358f84733a5b3328d5ec523512a4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:12:28 +0000

    blue perhaps breakin safety trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
