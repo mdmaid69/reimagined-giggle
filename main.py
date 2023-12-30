759475c55a408495ff9eec316ac8d51fa26eb9bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:19:51 +0000

    shock shine people issue walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def is_odd(n):
-        return n % 2 != 0
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
