19bd0c987de78ff52e13e9cbf593143b24816755
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:19:40 +0000

    sick poison otherwise neither best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
