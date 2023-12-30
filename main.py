8e04174818c0b01b15c0da31d6b931db3f54357c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:52:47 +0000

    flat nonsense account comeup reliable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import tensorflow as tf
+print(tf.__version__)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
