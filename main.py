a87ba204c64bfdd6d36224d1e6283fb2ef0cd07c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:23:32 +0000

    point disagree safe regret moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
