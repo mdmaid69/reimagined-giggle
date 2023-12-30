13914bb7361c47d97dcce80f8c7b5886ff15bac2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:36:56 +0000

    Oo cruelty complete trousers underneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
