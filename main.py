d7b0b7736b80832388fd1e1f132e70d5b90d299e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:37:54 +0000

    quiet study float lock almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
