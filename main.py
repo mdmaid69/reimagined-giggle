8de4538cfaf5a4f3b3be0b8bb05dba883ab84167
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:15:57 +0000

    sore sleep soft violent fetch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
