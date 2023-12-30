ddacaf2161d08fb6265fbf2686cfee1211fa03dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:06:57 +0000

    makeinto project dish anywhere success

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_array_to_string(array):
+        return array.tostring()
