72b5faeeb48a3759a80b139cf763f5b2d2288329
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:27:43 +0000

    sail now on recently until

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import sys
-  def get_python_version():
-        return sys.version
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
