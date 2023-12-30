6da5968f716f7442ad35018a4094c599442f3f94
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:53:01 +0000

    Ww see pump behave consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_bool(array):
+        return bool(array)
