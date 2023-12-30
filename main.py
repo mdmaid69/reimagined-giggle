b6cadf5930e8e5a08f529a3b458c4b5b23bee866
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:39:41 +0000

    ago protest estimate agree while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
