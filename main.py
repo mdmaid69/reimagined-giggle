720b945a8505cf8fc4d0ea113f1fa7c5bab5779f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:54 +0000

    show Itself inquire rank effect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
