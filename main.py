5b82f91795a8d849a5d236450ff53c53565b544e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:58:31 +0000

    beat turninto time miss gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
