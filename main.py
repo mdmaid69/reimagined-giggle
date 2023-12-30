bb3633aa04ec87aa18b08f91f5de64749d8100f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:37:18 +0000

    slow disappear stiff spite declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
