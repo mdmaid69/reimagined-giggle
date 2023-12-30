b211852fbc0a57881fbfc8202e74e247bce71177
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:26:11 +0000

    exercise opinion bang Nn worth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
