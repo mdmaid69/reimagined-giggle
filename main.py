b2d0d8ca4b2648eecae54cacd34374179ab0d89d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:01 +0000

    interrupt taste on lot pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
