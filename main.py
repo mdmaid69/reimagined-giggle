474ec56f031d345521098185e5d61d75f6f38973
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:31 +0000

    speech open very lock Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
