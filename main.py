2f67a88acadc8cf0a19e4a1df33710f0a08c0636
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:05:06 +0000

    pencil drink sting fire confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
