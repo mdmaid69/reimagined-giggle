21838588be09d3fb320a25187c3411abf552886d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:01 +0000

    shine mark book clue recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
