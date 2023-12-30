4c4331a2d57b4b7cc144ee52af55236a4c749a79
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:26 +0000

    transport aim far remove responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
