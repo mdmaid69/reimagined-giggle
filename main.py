37c12e6925e35b0b3c9ba6d5cb8cc0ab5206926c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:23 +0000

    soak splash figure beautiful print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+for i in range(5):
+        print(i)
