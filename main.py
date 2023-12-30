c7e8a842ee5237144df3120e8d2d42189c5cfbdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:44:36 +0000

    aware closed regional warm herself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
