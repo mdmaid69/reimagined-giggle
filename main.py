b3bfc3d4447774de1b680bd65739d42c126f3a34
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:48:27 +0000

    switch Cc backwards guess continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
