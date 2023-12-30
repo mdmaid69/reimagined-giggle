dee750692faa28a7e410ae096ddb652b559c1b46
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:39:53 +0000

    long object half already work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-print(time.time())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
