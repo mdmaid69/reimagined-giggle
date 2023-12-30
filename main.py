d7ca0ba8f5f96126f74fcf3e7a666ec29f223ef3
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:01 +0000

    free length arm if add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
