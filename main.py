84716eb1fb74bf4a9bc9b75a47995b5aba8c98ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:29 +0000

    give runinto gradually wet Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
