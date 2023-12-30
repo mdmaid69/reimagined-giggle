a6ccdfafaf5e8dfe3a3fe8b07ba5c9df31db5a42
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:28:55 +0000

    lift pot turnover exact brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
