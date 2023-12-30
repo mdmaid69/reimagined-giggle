a240690db3e33aef56fb7b10929e89fe3497a3cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:38 +0000

    steam practise conduct clothes call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_work(force, distance):
+        return force * distance
