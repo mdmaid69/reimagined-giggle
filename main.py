534df04cf1f4ea4649074bc0655ae92c874e6a25
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:09:47 +0000

    join fire picture follow single

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
