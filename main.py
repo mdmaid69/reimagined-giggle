83e28c4fbbb24d6a279ad6ec411f0c83897fe2f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:58 +0000

    gradually short embarrassing trousers among

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def greet(name):
+        print(f"Hello, {name}!")
