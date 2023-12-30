061b0d1abf7840c9c5afa9bea23cfcbed461b61d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:54 +0000

    disapprove good respect around pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
