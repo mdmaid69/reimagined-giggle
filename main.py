eda3af931f8d784f3ef11924b194a9598b4bfef1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:43:47 +0000

    soak position moment name slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def reverse_list(lst):
+        return lst[::-1]
