93762b3d53bc9325de12e0de23b09a06338fa161
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:38:15 +0000

    considering mention schedule idea picture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
