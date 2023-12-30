7605fd4ef2a432d68296c618f17394fc2850306b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:01:12 +0000

    very feel crash tour niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
