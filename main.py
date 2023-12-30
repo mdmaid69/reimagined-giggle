5f78d9391c3223e0accb87286b14d7a9bf010862
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:05:24 +0000

    help depend experience onto argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
