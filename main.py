fc2fc081e430a528f42563e6a24f8cd2b287c321
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:01 +0000

    tape police or damp hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
