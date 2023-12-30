4651b8ba7c76af83870e70bf0a5a9d3300a0e252
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:28:02 +0000

    copy bang sure copy I

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
