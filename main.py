16bf002844df9ed3c20c36e4b761fa42d4de2760
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:18 +0000

    advise begin among approval first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
