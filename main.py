1d459cbc2898477606559d3b8cdfa8b89760ad2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:17:53 +0000

    purpose punishment unlikely rest upstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
+from collections import Counter
+print(Counter("hello world"))
