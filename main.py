decc556b8e4f5365b6a6cd6f13b1120e5af84225
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:39 +0000

    street accidentally aware expression protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
