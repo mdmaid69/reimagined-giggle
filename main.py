fcc6a4689734d46fec4eaf0ac3b26322a41d3b7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:03 +0000

    shoulder steal probably share fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
