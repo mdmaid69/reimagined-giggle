a277ed04e3c8f1baa4fdac6d4a12bc227195afa5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:02 +0000

    pleasant callback scene classroom standard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
