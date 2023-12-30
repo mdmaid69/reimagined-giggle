e5e4df5f72dccd3c71a807ea198b91652876c346
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:15 +0000

    measure along belong concentration seem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.time()
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
