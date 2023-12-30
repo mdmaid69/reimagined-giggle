1d7a268173217730f211095c69bacdc0b3c986b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:51 +0000

    century tap frame bring finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
