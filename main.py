ce383dbcf12bf513e2a127970dbc80ece1b6e40d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:52:03 +0000

    illness target punishment pool foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_stack():
+        return collections.deque()
