8e2e0ff332a2388ace775123e76a536c090a3ac2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:30:53 +0000

    repair onto professional aim street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
