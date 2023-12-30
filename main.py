19cdb08f998202fc18442eee12c5b5837f02d744
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:50 +0000

    share phone callout image please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
