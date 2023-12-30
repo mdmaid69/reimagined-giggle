5b42dc3ca679e5e14204168451d54984dd75af52
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:39:41 +0000

    straight birthday whistle punish hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def find_min(lst):
+        return min(lst)
