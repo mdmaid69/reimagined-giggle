00d845916c85bf149d3dc3ef91587129c074d26f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:47:13 +0000

    frightened cut dish stop success

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print("Hello, world!")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
