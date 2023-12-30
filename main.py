487ae55341dfd4acaf8d93d8e1e78dee58b1ecec
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:25:46 +0000

    callback generally gap manage tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
