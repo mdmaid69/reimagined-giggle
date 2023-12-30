862eafd0711d9061b491c27d6fcb9f81c10624e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:06:27 +0000

    pocket view runout name compare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
