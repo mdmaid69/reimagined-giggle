c77ccb684d7b63eeeac4b7f93125a8f3ec3a0235
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:26:36 +0000

    beginning neat rough no regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
