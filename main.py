7ccfa2caa7283d7811c14568925561663352c1f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:19 +0000

    go further jump spill luck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_typecode(array):
-        return array.typecode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
