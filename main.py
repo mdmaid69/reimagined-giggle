96d59c1c8764f33cd13b7d808d97a8b5d11c548b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:01 +0000

    first public single minimum although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
