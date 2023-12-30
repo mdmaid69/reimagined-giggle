037d813f0e7c1c87f7686bda7fd882caf1f0e389
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:31:53 +0000

    towards once open shout use

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
