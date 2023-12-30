7b09a6f24231cb0c6195af0a2752696a51ea5cf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:25:26 +0000

    behind impressed failure without concerned

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def convert_to_octal(n):
-        return oct(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
