446af5f8c4b1f931ee1dca8c1d3bdd23000da73c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:39:09 +0000

    active nothing please money stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
