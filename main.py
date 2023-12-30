1acc0f0de44e12f718625a8a652d85282cf639da
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:28:01 +0000

    difference some decide accept half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def split_path(path):
+        return os.path.split(path)
