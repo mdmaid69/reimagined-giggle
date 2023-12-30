f3cf356e542c08953e4f27685daead69bbbdfaea
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:52 +0000

    rob fire doubt politics television

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
