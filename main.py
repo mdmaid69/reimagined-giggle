8ac0e534ddd3b7b876677ded81f4ca85d52d31f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:49:33 +0000

    content boil creature respect fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
