0ea20d0eb65ebd23dbe5095b40f3fafcc78a058a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:38:36 +0000

    fierce dish typical trust sweep

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import datetime
+print(datetime.datetime.now())
