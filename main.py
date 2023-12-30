5820fbbdad2de7377d0de7f2063adaa720758878
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:13:35 +0000

    constantly start plant married powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
