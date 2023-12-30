f5439a34e18cb6f13c81f479b0af13c25bd0dc40
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:10:26 +0000

    influence still mysterious willing lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
