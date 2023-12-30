7d48b7bc975447151af9db697297fbe5d179f06b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:57:22 +0000

    second contribute carry plan few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
