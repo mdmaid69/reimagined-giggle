9b8fda815216b6fe731249fd324ae194fb297746
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:44:20 +0000

    Tt possibly truth fact edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
