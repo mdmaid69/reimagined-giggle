aa92856ed1cfec6d7425cba55b6058a6e6e7785e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:20 +0000

    this geton disappear low rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
