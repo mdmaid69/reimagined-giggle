3c21df07130996474a25e6013c73643f4343ad4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:56:25 +0000

    best wish impressed splash friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
