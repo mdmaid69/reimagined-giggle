ddc31d4beabe9f9b1d7ddeb8f704d66c0aaf2447
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:14:23 +0000

    little correction shut cheat meeting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def divide_numbers(x, y):
+        return x / y
