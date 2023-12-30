63a7514fdd97e10d1d02a96f573f8e5ba12908a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:48:04 +0000

    future whisper impression responsible lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
