cf0de07837dd98c3a342f94059afdd85d3e09268
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:24 +0000

    opposite pattern forgive unless such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
