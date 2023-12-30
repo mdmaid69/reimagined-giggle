0825ed2d88672056836298e29482df0a15c7a9a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:09:58 +0000

    weather stick forever advice innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
