bc857b865d8f24e55579ee0906b521cb2d857e53
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:57:50 +0000

    pretty listenfor instruction Ee lean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
