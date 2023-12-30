9fefdbae00fe46f42d806c23d3b550f176413c58
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:23:44 +0000

    recommend bit everyone receive health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
