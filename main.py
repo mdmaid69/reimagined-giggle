8d1b109046e52c97e296db3b3a31713a2f7220d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:57:53 +0000

    feel foreign mistake advanced along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
