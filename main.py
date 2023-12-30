cd9df1fc8347460c83fcff1d61cc09aa7f0ac19e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:31:17 +0000

    murder trousers brother workupto cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
