7b228e74500605a445e50d8f424592011a24d6e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:17 +0000

    beat loud piece flood limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
