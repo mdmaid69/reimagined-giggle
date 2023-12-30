31205ebe2b8941ccd3ec81eda956f7cc45088f4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:02:53 +0000

    correction wash false contain wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
