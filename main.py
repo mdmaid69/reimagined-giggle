88e1619e9fd0cb99d6ef6026a768f33b6b5217f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:26 +0000

    yours extremely just mail performance

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
