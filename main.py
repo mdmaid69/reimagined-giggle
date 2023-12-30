dd8669dcc7ab1b7192387e3d188c6ac6ccb08e1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:42:40 +0000

    cheat operate world dinner threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
