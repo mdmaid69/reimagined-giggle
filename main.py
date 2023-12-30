a11faf11e94cc82ebdc08147d074edc8d5578efe
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:09:18 +0000

    comedown start accidentally swing short

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
