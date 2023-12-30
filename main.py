b558c4692a794bf2b600ba9cad9cccfac0f82c62
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:27:47 +0000

    language on silent warning turndown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
