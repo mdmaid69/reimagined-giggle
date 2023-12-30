ebd003b27ab91f7d75149c72f99175ac9d07d34e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:11 +0000

    connected charge deliberately something form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
