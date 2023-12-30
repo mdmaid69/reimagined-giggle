59e2b12ba2ba7433d65480fa5d212a260d798b9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:25 +0000

    innocent explanation culture where reliable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
