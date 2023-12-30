cadadea06cdc22ea2a57788e3b949fba5e4369ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:14:16 +0000

    comeoff motion soak birth reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_typecode(array):
+        return array.typecode
