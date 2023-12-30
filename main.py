517cad0083ee685aa8ed665c7e58bf4d7fe65d16
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:27:51 +0000

    mix change inform onto everywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
