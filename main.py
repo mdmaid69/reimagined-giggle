4d1614964522f1a6ce580cc889514f2852557317
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:44 +0000

    rely almost admit traffic introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
