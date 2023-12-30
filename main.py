898189904e8f3dce240f1abbfe4bbade505e4f41
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:50:29 +0000

    face ticket string dance live

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
