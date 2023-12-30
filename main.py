349b51632162966539ddf5fdf3a689f5c9d89c22
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:44:08 +0000

    round thinkover steal please instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_max(numbers):
+        return max(numbers)
