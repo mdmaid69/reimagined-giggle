f2de1ccf34e11de51ca1a18e3f8f6418a7299404
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:18 +0000

    good command sensitive crime defend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def count_elements(lst):
+        return len(lst)
