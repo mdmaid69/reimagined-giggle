b5e9673b3df964e7c130342707758ad32df3ac34
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:19 +0000

    recognize protest takeover level would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
