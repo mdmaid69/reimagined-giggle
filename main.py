1f4ccd0503d2b70dd1f9936b92154505fd25d5dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:39:28 +0000

    appear combine hate confidence possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-name = "Python"
-print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_float(array):
+        return float(array[0])
