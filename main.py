ce8781ade3c8ee3abb4bc0add8b97cfa12488726
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:52 +0000

    whistle shelter situation precise 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
