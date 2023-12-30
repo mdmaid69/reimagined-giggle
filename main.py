41c3dcc049ab3f922f829856a685904f5d689c59
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:15:52 +0000

    suck life properly radio plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
