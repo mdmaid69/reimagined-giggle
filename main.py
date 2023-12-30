53cddbb1d1fc20ef45bd7cbff5ee83f342e07b2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:14:50 +0000

    duty turnaway over poor little

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
