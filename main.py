5ee1374367efa7b2a3f3dcb332e195df4f67ac12
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:47:49 +0000

    shock fierce calmly compare invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
