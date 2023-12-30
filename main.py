56fde06167e6812468fcf0cbfaab333a335033fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:44:24 +0000

    fire settle whistle task close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
