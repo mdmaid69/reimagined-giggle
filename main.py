af56df2a2b3eefc4f16e36100da9f94b9465e41c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:17:40 +0000

    pretty trick Kk basic swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import sys
+def exit_program():
+        sys.exit()
