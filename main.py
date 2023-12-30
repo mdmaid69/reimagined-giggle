2a65808b10a87d267d206673b8a80611906de56c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:23:52 +0000

    fresh personal final workout light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
