4eed9e9b21cb14fea5dd10f0d7a9dea37db2a285
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:59:30 +0000

    schedule because ocean normal world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
