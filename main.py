2999927b5492ae4f70ba7e7d99560ee6ad3c67bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:40 +0000

    comein bye anywhere pick however

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_pressure(force, area):
+        return force / area
+def calculate_volume(length, width, height):
+        return length * width * height
