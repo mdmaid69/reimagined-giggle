3426624bd337a2069280095ec64b1c11ae593792
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:06:53 +0000

    pretend average bit brush activity

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def cube_number(x):
+        return x**3
+def square_number(x):
+        return x**2
