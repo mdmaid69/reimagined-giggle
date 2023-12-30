fccd0e1ffb1f34adf4fb4dfc38a7e91fd01ffa8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:51:17 +0000

    giveup crime characteristic row careless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
