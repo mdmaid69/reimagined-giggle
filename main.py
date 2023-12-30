246039541424439797bac3add8751764539ca6d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:28:59 +0000

    terrible answer match march famous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
