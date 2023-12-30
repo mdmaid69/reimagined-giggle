bf4b32730309993073321c723a23d58081ad44c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:01:00 +0000

    various home punishment angry wheel

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
