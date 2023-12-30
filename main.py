dcf3cea2df3eefa19f9397c7f8c5f29c0b036c8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:36 +0000

    benefit tip after except runover

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
