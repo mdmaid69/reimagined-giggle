0b7aff556f76f36836f45f89a20b367c98de50f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:53 +0000

    home plenty ear explain describe

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
