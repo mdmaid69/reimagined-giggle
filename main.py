3c070761ddf427dac1f9f2fbcd8b3ea9a531a9fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:44 +0000

    who voice giveback rent refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
