9e6137fdc2c0e8b3daccfa4359bb9c0c69a0e364
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:00:07 +0000

    how flat off chin turnup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
