45a4445b6162c8d6c5f0f537d310634933cb3f39
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:31 +0000

    comeout base weigh open danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
