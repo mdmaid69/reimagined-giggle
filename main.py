222385862fab29894d7df4a23bd9bc8b2b211214
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:58 +0000

    single collect part will communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
