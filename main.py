2d1e3c137fe56bf7f07188cb1a02603cf692b117
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:05:15 +0000

    listen where thirsty become at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def reverse_list(lst):
+        return lst[::-1]
