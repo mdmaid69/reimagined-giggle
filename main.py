5b107e3d53e0917da859f057a520c9ef0dfbcfc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:11:30 +0000

    bend intend himself bother thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
