c50c83ee42edf688be29402c1aae8b9b5779c3d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:48:22 +0000

    agree several return fine flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+x = 10
+y = 20
+print("Sum:", x + y)
