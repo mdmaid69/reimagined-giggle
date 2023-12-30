73f8aedd1d02c7c5eaa803caf181b2bcfab0ca07
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:43 +0000

    much trade connected doubt 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
