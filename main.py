6a5046f6890ffb23116549d04adddb6618e018b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:19:45 +0000

    stay many cover suppose sister

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
