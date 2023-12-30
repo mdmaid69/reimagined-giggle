90c40c2e331b744bc68083a2c939a7da9b8a9940
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:28:58 +0000

    runover separate head pleasure blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
