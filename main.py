05b0526b175517a6f14137d921bc24535660d1ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:11:31 +0000

    have mess hurry each press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+print(sys.version)
