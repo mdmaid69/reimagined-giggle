21158113bc620ee2e6851dfce13e9712d3d84f7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:21 +0000

    sing draw part again switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
