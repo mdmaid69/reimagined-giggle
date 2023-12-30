800ede11804b3e080909838a130c52de171bf178
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:52 +0000

    crash before take comeoff common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-for i in range(10): print(i)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
