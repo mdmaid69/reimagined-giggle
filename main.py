a1af2aac359b38e271e0206f01dbbf44f28bb413
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:01:39 +0000

    close motion disappear lookaround speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_as_repr(array):
+        return repr(array)
