e467c596e6022abf233639bb99112a0dc1661f36
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:15:27 +0000

    responsibility country behaviour swim chat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
