444a1c29bb7de256c3b1646d5ab978c2037cdbb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:00:12 +0000

    true empty deliberate clear rent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
+def is_even(n):
+        return n % 2 == 0
