986193b673da9a91f07e6c2ab1359bbb0b390525
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:09:15 +0000

    close woman over home force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_typecode(array):
+        return array.typecode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
