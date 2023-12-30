69dd1f84bc80ad44b3d84e9065ebf3f196daff72
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:22:14 +0000

    wrap close favourite example nor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
