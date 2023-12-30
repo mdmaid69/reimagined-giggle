e65bfa9fc5a0b6436ec8a4a17cd6f55f0e3af9f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:19:30 +0000

    hit basic avoid suckme lead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
