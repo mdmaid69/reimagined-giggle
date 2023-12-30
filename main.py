1109b5a499d44987a8d7e90142ef61aa5e328288
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:32 +0000

    sensitive sign hello introduce in

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
