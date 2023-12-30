36ed55130f77d81c2d8032f637535f1e56843cd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:30:24 +0000

    this tip giveaway connected schedule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
