fba904531f370cb490c591d471e9c5484d3d9f1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:18:26 +0000

    discover deserve reduction extremely fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
