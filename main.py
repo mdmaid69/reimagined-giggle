afc927c9cca6af7b8e083b7e3b4deda3ed6ae609
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:23:25 +0000

    fall attention course bend watch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
