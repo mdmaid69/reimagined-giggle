596d0441719911c19fbb4c67ad4a0592be9b2d52
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:44:52 +0000

    funny report level minimum place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
