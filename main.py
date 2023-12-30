3a90e84e6ce6bc72a71fa8bc9ca7dda67f11e579
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:25 +0000

    comein simple steep include turnoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def is_odd(n):
+        return n % 2 != 0
+  def remove_duplicates(lst):
+        return list(set(lst))
