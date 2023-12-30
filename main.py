2012b0f45d18ff4b1d8f86a3c0fd9600143a25ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:38:16 +0000

    familiar ready ill completely chin

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
