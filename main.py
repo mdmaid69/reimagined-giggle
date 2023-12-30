1c5b0d9678ba0b216b7bc7b6b6e07f4104230955
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:44 +0000

    between by high design 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
