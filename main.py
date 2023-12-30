2f8fbe401e81b0b438b8c053858e46fe030cb115
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:07:16 +0000

    their unkind impressive style shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
