7ed063d7b31b60b5380246a36c9537d81dd6df0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:22:02 +0000

    answer whole join confusing now

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
