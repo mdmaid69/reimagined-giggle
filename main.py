6acfc9ce1bace1fbcd8150195d6735bea41a6ca9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:29 +0000

    getout section town separate claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
