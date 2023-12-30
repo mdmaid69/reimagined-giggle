37564ca228990b387b1b265caed58e47a69ed60c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:58:18 +0000

    mind prove completely leg seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
