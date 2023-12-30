45a798d1840a1511c77fa236c6f545fe3f1e4835
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:18 +0000

    duty age gooff car delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+print(os.getcwd())
