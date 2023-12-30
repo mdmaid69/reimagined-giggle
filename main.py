6f67820fc4d6187f1674756fa44e683ee4009620
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:52:50 +0000

    community practise tell theirs far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
