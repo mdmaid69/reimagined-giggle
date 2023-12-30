b368f37e9fe5983113ce34673864d9b99a72b944
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:45:57 +0000

    shadow as strike birth shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
