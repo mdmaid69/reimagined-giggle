6468893ffae6831b857302dfcb2ec457d8a57cae
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:02 +0000

    police succeed ask next full

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
