10871eca2c0fb8c416fb67008c77a52ade23c55f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:07 +0000

    flat achieve thick punishment minority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
