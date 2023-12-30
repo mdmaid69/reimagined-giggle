8e0eac7cfee62197a82a170ffa979b7705a1c6c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:19:19 +0000

    ocean letter tap action often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
