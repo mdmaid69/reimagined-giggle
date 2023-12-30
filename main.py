9580a1bbeab315d0276636de8d34735428a5a7aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:45:04 +0000

    distant same Itself sense success

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
