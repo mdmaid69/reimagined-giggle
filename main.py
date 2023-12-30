5e0818670c171c656875021472f9c3a8f0f8a7ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:04:12 +0000

    interested later stretch bend market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
