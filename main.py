bccdc46b54ee75b42140687d721f937f31cc5b3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:40 +0000

    exciting apply until danger in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
