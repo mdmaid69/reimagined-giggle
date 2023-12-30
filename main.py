2b9e6f56ff8477785cacb423ba552deb75001fa2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:38:00 +0000

    sale capital embarrassed habit plenty

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
