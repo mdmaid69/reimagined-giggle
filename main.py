569a4d8a4eaed51e88e19f71ddb293c89de2ffec
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:53:02 +0000

    pen reduction relative unit scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
