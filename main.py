5c72db6848e72a45eb294f87553047f8fb3acb55
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:56:27 +0000

    wipe narrow proud gather first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
