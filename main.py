6bf93048b33bfc80ccb226a2119d54a599aa797f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:00 +0000

    room before concentration march close

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
