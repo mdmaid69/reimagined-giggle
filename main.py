532c0eff9c7d99d623a0b98dc8292ec35b97300e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:09 +0000

    goout true still trade loose

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_min(lst):
+        return min(lst)
