8da6bd91a3e2c6fda9163089e8231b5157799f97
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:19 +0000

    news lead think backwards impatient

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-print("Hello, world!")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
