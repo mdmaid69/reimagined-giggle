79bb24d0f3e5d422bf8ece0696dcb80f25af818c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:27:57 +0000

    search contain block sore Mm

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def clear_array(array):
-        array *= 0
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
