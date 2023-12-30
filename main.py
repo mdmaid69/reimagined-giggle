690b5a5eb1fa9c7293dfacf8e80cde183bb28646
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:51:53 +0000

    quit type adjective contain culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
