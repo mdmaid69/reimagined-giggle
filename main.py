1a8565224941763a28f0f024e537cfc3e26c3084
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:42 +0000

    steal complaint unite loud rush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
