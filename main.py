537f2a1c2efd31fb58e703cbb2b196ffa2e85cd8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:16:29 +0000

    announce various air which broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
