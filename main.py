03c962782a2c8ceadccb7f119deec37fd392d163
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:39:09 +0000

    heart keepup mistake search runout

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
