aa1860d13a98c4f1d42a6c8e0042877b0a5bca95
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:29:20 +0000

    its probably where down quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
