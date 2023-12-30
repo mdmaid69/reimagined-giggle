f9c1a831d08320b52fd1be50f24f820360057467
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:11:51 +0000

    damp embarrassing soft intelligent 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
