c6f5b5ea63d58ccdf50e1f18922f55b80aeb2192
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:31:35 +0000

    above uponon out pick low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
