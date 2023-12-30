c75aa7d042451f2b11875200c253553ca41e254d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:24:03 +0000

    advantage rough sleep principle himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
