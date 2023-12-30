e1738e51ed9b78922095d75140090bccc313cf9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:12 +0000

    awake bath standard suckme contribute

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
