cd05454ac59e82fe6ed559b342d39c30595b5479
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:23 +0000

    somebody earth sore left passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
