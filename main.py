8fe7c57bf702194094586b37f33cc0d1bf2c70f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:13:17 +0000

    179bite shop deal empty quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
