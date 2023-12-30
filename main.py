59015707a725376cfa3fef02c7e3a5d77480f67b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:52:33 +0000

    container advantage announce develop 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-for i in range(5):
-        print(i)
+def get_array_as_bool(array):
+        return bool(array)
