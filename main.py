25080cf4d387c860cf8aba1ec50d38e4a8e04043
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:42 +0000

    kill office wheel arrangement sorry

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
