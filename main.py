83b704b95e89a4507f426d9634725bed1ecc046f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:51:09 +0000

    cheat recognize extremely 566end have

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def count_characters(sentence):
+        return len(sentence)
