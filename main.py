5b0851d32d3a2456afee5e02bad61718c6f77686
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:16 +0000

    happy either calm its definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
