792f2d14ac6a441569730a5c95a94acf817e3b1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:57:01 +0000

    thankyou chief try like ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
