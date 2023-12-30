a4a3a9a73eec89ca17794924be9749de8688647a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:56 +0000

    few pay contact spend shrink

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
