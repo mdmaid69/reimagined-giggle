2c3d22bee2e0f0e0f0d3ebfbf503cf87eeace602
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:18 +0000

    crowd poor soft programme base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
