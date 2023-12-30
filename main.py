a1ee86bf3bd8cba9709ea8f8fc452048a637f478
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:46:49 +0000

    still afford right bus forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
