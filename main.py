2c98fd273205bdac10c9be54097f8d8f37dd055b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:16:09 +0000

    rule advanced phone with tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def is_palindrome(s):
-        return s == s[::-1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
