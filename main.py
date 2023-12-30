60a3c4b326b9a7d2cec9ba620f418a642b1bf2b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:50:15 +0000

    usually better earth coffee hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
