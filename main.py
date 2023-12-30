9394a0dec89f4805ad0390975d91039a8606498f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:02:56 +0000

    extraordinary thumb unkind bit attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
