8e4d1e30f39f4b2290a819f5ccac8ce4b99d0685
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:59:42 +0000

    strength waste persuade thirsty work

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
