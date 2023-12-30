cecec46a9d26a4e524c5077299058c1833da6213
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:42 +0000

    record watch Ee rich introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
