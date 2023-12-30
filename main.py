e35d0bc6af487230d2c334b73dbbe681de58c8ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:18:09 +0000

    mix combination unusual admire rare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
