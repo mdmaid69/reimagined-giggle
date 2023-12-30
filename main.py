75d83cc1436688fca58dd89d780d3511c56a8e2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:06:40 +0000

    trial dangerous tap stretch whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def calculate_mean(arr):
+        return np.mean(arr)
