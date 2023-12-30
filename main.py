b82a7440d0fbe726d657c3d4d2934fb1ab2b565b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:01 +0000

    suggestion concerned division between forever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
