5e2bcfc474d0b7723f792de773543c8863a598d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:59:12 +0000

    joke contact tune pin march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
