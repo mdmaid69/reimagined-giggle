5aa4766b8c98472a7c7155bc6a0ce4d207a991b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:53:12 +0000

    last dark wheel wear complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
