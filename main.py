a268f5a49c4e5b79b017c56135e65626d1ba5ec9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:08 +0000

    condition innocent vanish suffer free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
