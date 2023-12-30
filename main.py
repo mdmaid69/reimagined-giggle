80d5ce389dfe1fbb7b55d7d1958360560fd8ee8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:12:56 +0000

    he fear file every rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_palindrome(s):
+        return s == s[::-1]
