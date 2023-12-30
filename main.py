37b44311356ca0e76a02c7dcd93afc665ae8ae44
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:20:55 +0000

    sink naturally pause stamp left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
