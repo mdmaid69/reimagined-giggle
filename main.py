0f7d2d0c9cc1e255f0c599de57e93fcb3c8149be
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:24:40 +0000

    fill some murder recognize fun

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
