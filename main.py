657375ba662583025b713be47cfc4f56cd19bf49
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:08:56 +0000

    reliable tragedy trick hard event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-print(os.getcwd())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
