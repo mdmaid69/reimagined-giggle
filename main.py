e89a987fe785ce0d5eb18d7cc96f7c735a019e58
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:38 +0000

    chance advanced serve keepfrom giveback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
