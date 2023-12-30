5c5f5b9d1011ef2c51e369addf0f94264245cc34
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:11:36 +0000

    really unpleasant business last worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
