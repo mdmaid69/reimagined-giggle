d8357b59f5aa5716256910403e675050de0bca05
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:33 +0000

    guess minimum complete deliver distance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
