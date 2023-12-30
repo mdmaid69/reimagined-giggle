04de43a530b01f88b4dddfc158f6a6215365925e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:37 +0000

    cut favourite substance several breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
