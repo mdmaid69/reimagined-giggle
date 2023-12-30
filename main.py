59a18435ff2cfcdcc8e3db275e7f45e0c3b020f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:24:40 +0000

    other invite opportunity maintain right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
