0f402acab0bc8a3571b2755bf3e197b3bbaac1d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:07:32 +0000

    they giveout serve godown drive

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
