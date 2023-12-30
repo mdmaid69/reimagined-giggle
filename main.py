3dd922ea30661bc6af1cd333ea6725bcdaf9d9df
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:19 +0000

    Tt mean mood case replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_octal(n):
-        return oct(n)
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
