02657cff050a71555c80eb36ea9319bf7764ae86
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:35 +0000

    runout preparation copy strong mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
