a5c790869b9f220d8ceedb923626d41f68ae5ed3
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:16:36 +0000

    proof fun edge near tense

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
