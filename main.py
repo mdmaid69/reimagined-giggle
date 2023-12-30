9be05eee825e9d31a27a9be7f6a586df5398ea34
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:33:47 +0000

    exit trade dress come also

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
