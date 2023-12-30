774d804cbe00bbc7976812cef34f848923095a1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:55 +0000

    hide stick hurt development sick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-for i in range(10): print(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
