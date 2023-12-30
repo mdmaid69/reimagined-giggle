ccd29cddb4dc22b0d3111d7c673207317ea523d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:35 +0000

    relax strike destroy actual hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
