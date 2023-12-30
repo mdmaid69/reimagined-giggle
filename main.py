e4355b398c364ab21793b05ecc85d8348ea0b52b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:12:35 +0000

    weak tie tall next may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def subtract_numbers(x, y):
+        return x - y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
