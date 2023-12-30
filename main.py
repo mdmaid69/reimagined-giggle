4b8a808f778c1738df8eb93703fe68e32fd77174
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:51:52 +0000

    why confuse final disturb disturb

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
