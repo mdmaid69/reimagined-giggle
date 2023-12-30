7e01ce1a74395fbbf67674bc6bcc1a12e1a4f631
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:29:30 +0000

    easy flavour flight ignore tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-print(math.pi)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
