35d4328d2d22640f27203ffda2f41a2d47dc29e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:17:52 +0000

    careless switch powerful radio cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
