fb980f5e47c5b6b82bd8a5285520d3078e34ea3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:07:10 +0000

    shine silent claim vote back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def square_number(x):
+        return x**2
