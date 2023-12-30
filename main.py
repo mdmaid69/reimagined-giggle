35cd0969242ae469a5fac9c43d719fef631a0da9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:34 +0000

    hair press turnover dull post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
