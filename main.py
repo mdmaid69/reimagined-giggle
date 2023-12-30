233b5b075044d968fbabd6ac405769e46f95d789
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:03:03 +0000

    explain accidental thumb excuse round

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
