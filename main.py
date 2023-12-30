21dcbc3d6cc4adbc7aa0a74f9b7e36dfe6c7cb8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:49:52 +0000

    permission taste plan create too

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_array_to_unicode(array):
+        return array.tounicode()
