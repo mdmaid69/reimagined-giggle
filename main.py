dc7bebb21dd9df25711df6c146ba1fc8508c2f04
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:47:07 +0000

    wing murder century deliberate friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
