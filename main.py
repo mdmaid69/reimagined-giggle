2445926403a255f0e397649fa4c2141bd6491122
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:20:24 +0000

    necessary absolutely represent stretch usually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
