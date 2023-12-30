4c0d9023f6b0ba851488da88d121ec22ff73c491
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:20 +0000

    really official measure target release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
