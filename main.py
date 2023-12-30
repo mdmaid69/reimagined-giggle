bae83a2c15784a29d45ff9cbc83b00956d661e7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:47 +0000

    goon justice principle transparent between

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
