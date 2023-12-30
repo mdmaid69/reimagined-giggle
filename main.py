2bd14ce402505ac207ca9b2cec289a63e7eb2f4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:39:01 +0000

    drive firm ourselves considering case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
