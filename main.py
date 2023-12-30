79f6a393a2133183603d0eff6ed45e896dd62db5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:59:28 +0000

    total personal scheme common away

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
