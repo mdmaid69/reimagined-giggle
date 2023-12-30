862017fb61bc1c1c45f9c87cc89693030f00343e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:15:28 +0000

    573entertainment payment load cruel soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
