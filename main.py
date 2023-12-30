81a0bebfd6e7feedc94ebc030a5f6e9aeba86561
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:14:28 +0000

    small along chin error breathe

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
