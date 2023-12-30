f98f0c102d16f16603f62a3a4990b5f08efc81b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:58:56 +0000

    to length excuse form fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+print(random.randint(0, 100))
