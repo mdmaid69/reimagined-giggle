329722cee17b065b152618b8d25adf2af1d2ac02
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:26:02 +0000

    far break aware choose next

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def is_palindrome(s):
+        return s == s[::-1]
