161ff0ad2d6817c8d0e79e3de43f35b9fd789640
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:59:12 +0000

    bring aware naturally disgusting allow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
