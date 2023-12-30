2caf42527189534eee54df84767e33547aa699b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:40:33 +0000

    light dish before completely odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_string_from_array(array):
+        return array.tobytes()
