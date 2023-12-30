c8070fb382e0b60fe9caa2ef5e26f2fc61c5e63e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:51:32 +0000

    certain onto brush operate punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
