8305d0d32621fda0a5418fb82fee4b36efbb2bdb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:48:30 +0000

    tip fashion past prize travel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
