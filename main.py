70007f30aeb77b73625fdac39709ea0a594cc94c
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:47 +0000

    smoke why here you flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
