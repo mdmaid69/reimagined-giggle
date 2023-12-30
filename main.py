ea43daa3012fbaede6d2e417e2ae7214b567307d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:43 +0000

    tough aim regularly guard estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
