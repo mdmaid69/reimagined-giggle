aab62007e5a5c07e7c41bf0213db7f95de9d8e4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:15 +0000

    as suffer board rest go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
