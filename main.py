1f1bc6316f79376f9a46ddbbe79100b8d8ca45d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:14:29 +0000

    sleep anxious opposite none start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-print(os.getcwd())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
