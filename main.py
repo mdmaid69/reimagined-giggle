81ca145705a3e13a56e6b40e96ad8e5a6fcfaaab
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:21:25 +0000

    runover present sometimes action tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
