5b850fd822fb870b9cde1a433b5ca0521e95f7a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:26:37 +0000

    pause centimetre pause wise draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
