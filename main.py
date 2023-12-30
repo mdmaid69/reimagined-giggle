573ba5179d3cb0dc8751a0a90e978f7df4aa4ee6
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:17 +0000

    muscle runout let shallow noone

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
