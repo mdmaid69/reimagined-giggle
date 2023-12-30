c702fb4d9a68a4438f5a5893c064e45292ac86cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:56:43 +0000

    round friend talkinto one remark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
