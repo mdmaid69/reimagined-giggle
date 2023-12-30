d242b4d3189b3cebed1083a3cb9ac068c5cb0805
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:42 +0000

    with famous sudden alive fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
