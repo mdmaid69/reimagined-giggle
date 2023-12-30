e4f8bb4bc90db6daad2c0c76ac52e72f3e1223d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:19:47 +0000

    runover convince responsibility frightened apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
