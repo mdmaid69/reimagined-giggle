49488fbb0c28573a559fa4c745bde98e3fb8432d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:43:27 +0000

    what proper risk comfort crime

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
