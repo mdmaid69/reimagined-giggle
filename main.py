29afd818705215ac7ad01e8bdfe1179bbee5d57d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:46 +0000

    but gooff matter concern Ii

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def convert_to_hex(n):
-        return hex(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
