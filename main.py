5f046a1d2e38f6b7e19340cd59bc64916bbdd347
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:57 +0000

    trade rub play reach anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
