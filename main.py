4ca52d4a272a1da44a106d0e290a51f52942eaea
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:48 +0000

    slowly song principle intend awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
