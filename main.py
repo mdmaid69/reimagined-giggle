39ff0e148f38854bc8cc9703dd4dcfe26c1f0fbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:14:18 +0000

    lend sweep wear bunch bush

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
