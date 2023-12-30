a9dfa95510caee2106d74106883f0ccadfce2788
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:23:47 +0000

    pattern not around mind anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
