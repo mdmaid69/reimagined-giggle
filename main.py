03c863e9e105fb86c5387538763a87e0c9f6abec
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:22:45 +0000

    tax our definite deaf skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
