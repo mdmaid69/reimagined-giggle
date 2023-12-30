226c93ec8f573bd10f3de2c0dcf55c99a594f45d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:04 +0000

    sticky here pause by secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def is_even(n):
+        return n % 2 == 0
