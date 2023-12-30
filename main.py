af00fb9bf099510191121a09196fa53ffc3e4aba
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:30:48 +0000

    contrast stone find television trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def convert_to_octal(n):
-        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
