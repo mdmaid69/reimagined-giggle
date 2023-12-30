2ad136c807508a11f60da96f2213fc3d8588949d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:31 +0000

    everybody evening bad fence promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
