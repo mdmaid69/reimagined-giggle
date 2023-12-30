ac518cb87c890682b65d3ecfe0c249990980b0d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:52:33 +0000

    any innocent fashion trick into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
