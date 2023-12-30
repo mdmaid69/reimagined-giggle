ed2da99c7c0df9fbc383153a76bbb28569f3829a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:57:18 +0000

    exaggerate bag exam copy around

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def reverse_list(lst):
+        return lst[::-1]
