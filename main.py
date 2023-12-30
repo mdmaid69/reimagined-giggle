a3a0f797f8794c14adccdd2bd672b30a265bf12f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:53 +0000

    ocean wander Ll stir season

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
