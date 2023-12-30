784e4c75eaf5d174355f95ba6e77707e5d48db4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:41:57 +0000

    getback lean smile stupid promise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-name = "Python"
-print("Hello,", name)
+def count_elements(lst):
+        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
