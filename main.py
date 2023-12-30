21ac56cd084637da65ef6c7fd68915c7af60cbfa
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:53 +0000

    rough drawing boring play of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
