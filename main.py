20be52a54ca52024ae1f8f35acaef66b9c53a06c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:17:56 +0000

    shoot practice opposite actual anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
