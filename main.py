38d2cfd89c9ff682aa3acae07b2c87d7c070d6cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:08 +0000

    available tend sense our cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
