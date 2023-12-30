5b7620f019e8c3bc9bde7cc04b8d0fd93705cfad
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:15:01 +0000

    doubt comeround pretend heart below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
