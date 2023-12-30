e157d74ce7fb9c7c78bce9e71b50fa7d9078e168
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:10:03 +0000

    both stretch pleasant butter safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import platform
+def get_os_info():
+        return platform.uname()
+import sys
+def exit_program():
+        sys.exit()
