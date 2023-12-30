7123aaac243de9bf7f8493e10ad52c92efa8cfc1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:18:51 +0000

    fit pick computer confuse increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
