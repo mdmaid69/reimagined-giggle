a0a4ed8d21b8f5f68c688bc782811513436fa7f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:29:18 +0000

    rough whether snow entrance familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
