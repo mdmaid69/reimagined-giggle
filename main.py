e3292825ae9cff2abc85a738191f2fd61382aaad
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:41:55 +0000

    usual mix joke union new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
