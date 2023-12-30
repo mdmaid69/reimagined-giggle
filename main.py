748ab5bd2f37820e1b91b75063ba4ff94e2fc0e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:22:07 +0000

    instead thank confidence pile dinner

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
