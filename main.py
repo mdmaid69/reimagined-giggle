3c7014499a36ab16c21f42ad6f1e245ecbba22f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:30 +0000

    steady yesterday leg loud start

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
