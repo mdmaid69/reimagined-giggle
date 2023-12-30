c8d84cd9d99a389358aa6bde32f2082a29f7e601
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:39 +0000

    worried nice company limit present

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
