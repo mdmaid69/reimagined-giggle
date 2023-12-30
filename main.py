730003516ad662cd71382ad27ad4400cdf3a8fa5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:51 +0000

    minute please murder consider comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
