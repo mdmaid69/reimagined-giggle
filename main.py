5d9dd83fae5a78e21c496d75e682fa5923c44181
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:59:44 +0000

    thin equal avoid company acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
