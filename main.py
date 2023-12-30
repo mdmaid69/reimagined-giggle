7db3b5b18e8e5f703ecdf91c6377852656db7183
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:49:40 +0000

    increase laugh conduct emergency wide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
