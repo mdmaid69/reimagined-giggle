a94b66c10174ffd82a6b12a33fd25fd43571ff38
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:26 +0000

    handle trade beat especially sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_min(numbers):
-        return min(numbers)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
