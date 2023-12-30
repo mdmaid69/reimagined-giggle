44cc25210e5a825c640bf3543cdce4ec2c53aee0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:16:33 +0000

    bring memory away turn demand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
