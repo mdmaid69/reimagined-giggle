b42ae296b2d88ead4b221c2593a8a76f628ae95c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:15:09 +0000

    find open underneath private content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
