e8b27d50671826916bbd46314eb3fb5ca720b8c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:30:13 +0000

    case light since forget agree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import sys
+print(sys.version)
+def find_min(lst):
+        return min(lst)
