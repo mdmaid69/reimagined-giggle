1d018a5e2631d3ae25db1c3b4b0c414167ede0dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:57:57 +0000

    offence beside interested term minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+def print_python_version():
+        return sys.version
 def find_common_elements(list1, list2):
         return set(list1) & set(list2)
