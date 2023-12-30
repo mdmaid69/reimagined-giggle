f363aae8e27977c61c0fcfb998701f7c26315e1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:57:52 +0000

    ours advantage damp in play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
