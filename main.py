99baf6c6de184b42c658aa3da0ea34ada90183bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:56:39 +0000

    underneath scale remove deep much

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
