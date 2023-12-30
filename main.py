e64a6529dbc0b8ceb366a3286c3da09bb23b6903
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:50:42 +0000

    upset failure sweet contrast equal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+print(os.getcwd())
+import getpass
+def get_username():
+        return getpass.getuser()
