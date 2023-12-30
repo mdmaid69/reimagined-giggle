ec8662c566038fbe535197eb13a8557a6c7a9082
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:31:51 +0000

    interview half meaning goback(to) passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def multiply_numbers(x, y):
+        return x * y
