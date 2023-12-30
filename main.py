4a2a844394d777f8fc78d463f334291c2083cc6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:39 +0000

    mad immediately back brush amount

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
