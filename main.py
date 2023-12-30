abf8cae40759bc74dfa981a1ebcc34b93018b748
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:56:38 +0000

    reply before answer absolutely scare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-print("Hello, world!")
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
