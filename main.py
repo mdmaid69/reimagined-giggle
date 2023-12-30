322a52cf11ae713575346f1849fe546112ef654e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:24:03 +0000

    honest comeout awake material furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
