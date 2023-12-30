967a9ef203eb33408cc3c648abc8b9907fe37e61
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:48 +0000

    comment chat nature bunch vanish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_min(numbers):
+        return min(numbers)
+def cube_number(x):
+        return x**3
