9be287e66af0ad0fdbf487c2ffaf8ce7c97858cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:26:19 +0000

    criticism table start order old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
