4c0b7583315c0505a1d0d639cb69d8497ae52b18
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:18:03 +0000

    rise turnon bring swallow opposite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
