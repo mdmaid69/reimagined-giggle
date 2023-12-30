64b09c02af250299faf0c643b1faa70a091e2cbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:57:13 +0000

    love brush busy watch steal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 text = "Hello, world!"
-print("Reversed:", text[::-1])
-def count_elements(lst):
-        return len(lst)
+print("Characters:", len(text))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
