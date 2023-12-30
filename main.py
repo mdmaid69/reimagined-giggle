cdecc2a56c17263f44d99f92dae05e4437852d6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:00:27 +0000

    favour content string no whistle

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_set(array):
+        return set(array)
