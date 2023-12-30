083f707a0e3b7d5ae993c8c0051f06f5755a9dd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:48 +0000

    suit detail wrap wide 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
