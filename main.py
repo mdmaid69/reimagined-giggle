b4486b0e20b0dc20b6e0ffadbfd0a5ac405d4ef9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:30 +0000

    high convenient later tense keep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
