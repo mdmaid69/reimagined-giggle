ab1a6a7e908dd543a118a31e0dba7f7beb5c50f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:46 +0000

    try count trick copy here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_size(filename):
+        return os.path.getsize(filename)
