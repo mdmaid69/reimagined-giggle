88547e04a2a47442a6474bf4489c6e9e48e23175
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:46:04 +0000

    year telephone special definite artificial

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
