fb9ec04dafbacfad8b67ec24a2823cef3977753a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:01 +0000

    prepare listenfor easy sky increase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
