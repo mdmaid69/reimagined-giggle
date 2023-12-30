144ecca78b06b284aabe646f12a3f5a8723fafd9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:38:48 +0000

    stone pen plan lookup talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def remove_from_array(array, item):
+        array.remove(item)
