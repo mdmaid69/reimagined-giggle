c6d26f95c4c3b2e1b67ddbb0dc0275ce242fdf37
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:34 +0000

    woman near blind aim hide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def multiply_numbers(x, y):
+        return x * y
