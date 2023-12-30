aeed21fdb922b13836c5919c03dab7157e33d602
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:32:27 +0000

    wide taste 572enter result argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_str(array):
+        return str(array)
