b454a9278dc36ffd1f8af5a4f5f24e83cab60761
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:03 +0000

    example name permission sell brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def add_numbers(a, b):
+        return a + b
