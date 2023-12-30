6c6b59fb655de6c394ab808ac4cf12af9ce10c6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:45 +0000

    turn point unhappy concerning explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
