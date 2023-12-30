c9d3eab046a1af6d184ce2aaa3fa178c831a9d3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:00:31 +0000

    accordingto wing get boat downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
