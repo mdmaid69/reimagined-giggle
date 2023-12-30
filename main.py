4d834a8240031fb6db4c8d31e9b81a2fadb2e7c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:48:47 +0000

    pure fair fresh Ll cut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import heapq
 def push_pop_heap(heap, item):
         return heapq.heappushpop(heap, item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
