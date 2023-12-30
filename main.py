a305abd04e48424f33dc1730933c610ec473c044
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:58:35 +0000

    design should rubbish pull butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import sys
-def print_python_version():
-        return sys.version
+def add_to_python_path(path):
+        sys.path.append(path)
