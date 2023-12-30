975be6c1a1669f623af437562da826af1d5cc7f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:15 +0000

    myself mine space test offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
