b42a8f1a85afc05fa45628dbc36f8b5137dbdd8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:58 +0000

    muscle deserve bright dinner unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
