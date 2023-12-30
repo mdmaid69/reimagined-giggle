90cbb1944e7560f7f7769159a1499067d2c788a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:54:44 +0000

    entry cool measure mean improvement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
