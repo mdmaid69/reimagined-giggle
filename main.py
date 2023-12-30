4c27c2693cc1cb661e207733b4b4315b9f5aab16
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:35 +0000

    theirs taste cross concentrate goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_list_from_array(array):
+        return array.tolist()
