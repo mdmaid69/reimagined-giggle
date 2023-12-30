2ca40a9611ea27564635a02b711d94a6b5f053bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:09:46 +0000

    know danger recently favourite rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
