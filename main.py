32b641f4b48e61dec8fe05a9d38b0d58ed33d10a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:26 +0000

    cum rise without guard careless

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
