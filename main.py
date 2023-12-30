104fde8d5771dd58e3d460c630385fd0c82c1eb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:05 +0000

    walk considering develop blow tight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
