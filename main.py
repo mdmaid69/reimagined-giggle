a0ce92c6c50638ae21bf0c25f75bb16a7a48bfe6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:58:20 +0000

    achieve keepoff not anywhere part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
