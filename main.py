17fe33242fb704d069a8af6a6de05eb1e2ed2c59
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:14:52 +0000

    plant match out head coffee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
