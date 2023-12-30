31ad97c6f745bc9c7f83e1a1ec23928be05400a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:57:11 +0000

    bringback comeround interest prove rather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
