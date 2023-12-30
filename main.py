575f70ee7a9068f77e5467073f5442f5ecc3e0f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:49 +0000

    temper spring hospital surface patient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
