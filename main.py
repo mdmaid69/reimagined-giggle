e94deee46780fb4f246e05d7341552440217e0cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:39:39 +0000

    true please must delight protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
