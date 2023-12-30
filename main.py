4fa8ecf2a01a7339abc6a8c186c1c1d0b4716b25
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:22 +0000

    large plant company only physical

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
