64a8ce3f271de5a863891ffdc1413727a4fad827
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:02:52 +0000

    stick please getoff later round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
