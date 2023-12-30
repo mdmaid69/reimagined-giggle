82605ed02b25cdc0da6103fb57aec3ced3883a7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:17 +0000

    load dream someone experience reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
