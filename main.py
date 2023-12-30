bc00de097c444867a9831868a46b32228dddd2c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:14 +0000

    typical sleep ourselves ourselves grade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
