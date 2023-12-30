6e92335834a2a62627cd1325a86d40d628551998
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:21:02 +0000

    settle involve smile young deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
