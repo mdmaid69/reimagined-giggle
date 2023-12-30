8d8bedaf9270ba9fbf5318bd7d10f484e3937f1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:46:42 +0000

    woman repeat dare none tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
