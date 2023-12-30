ca1a441e01fd7b94c20c3d6c091dd39c2646300a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:21:45 +0000

    sticky language shut dark stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
