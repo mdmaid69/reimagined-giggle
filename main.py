203b9ef2b67a444b46c3d1a368246f1d0887127e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:58:35 +0000

    need warning importance doubt lookaround

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
