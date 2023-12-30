5c92da8741e63fb415b38e5e462838c1f0629b3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:32 +0000

    brown clean record comeout appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
