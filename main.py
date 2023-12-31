00da702ec06bfbce08fde93d9383fd0898eb9c17
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:02 +0000

    tradition holdout brief journey glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
