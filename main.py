cf7a61451ec9a64e8286411d856978bc7b042f3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:13:10 +0000

    pain murder we poison bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
