1dfde774d2f13757062f1044ec1534e47dae4213
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:35 +0000

    by keepout tired ticket family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
