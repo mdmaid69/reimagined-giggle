259acd6adb257e1f6da07565c749b729b804a7fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:54:00 +0000

    happy minority mixture rest live

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
