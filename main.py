cf425f525e1389f92c04bbc795eb51897fa74cdc
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:33 +0000

    heart exchange transport lookaround nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
