2df6a10c6cb52a19a232a244f0afe035c28c430d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:18 +0000

    justice makeup because breakthrough discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_contains_item(array, item):
+        return item in array
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
