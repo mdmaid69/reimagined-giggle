53ac41261d98281a468f3ec6f456b3cf10e2ebef
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:47 +0000

    score switch keepfrom cost either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
