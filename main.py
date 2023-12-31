581a70c42dc7eddf46dd0522def746d5ef2ad75b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:41 +0000

    develop bye dick advice content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
