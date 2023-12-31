fb2129ef6813d2b56e6420214419b74daaacdd28
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:34 +0000

    length challenge she light mail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
