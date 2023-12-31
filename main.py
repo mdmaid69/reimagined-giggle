42656ade78966a0c53ec5c8f37eb4b02469374d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:46:55 +0000

    drive into smile track convinced

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
