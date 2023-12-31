4f9eac7a45d410e36f53859426caa93de39b2d77
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:51 +0000

    performance guess relief operate private

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
