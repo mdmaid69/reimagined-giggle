6f38d269f4e87bad2a5c192ae1e25a50c0e99f40
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:00 +0000

    branch instant lean tread load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
