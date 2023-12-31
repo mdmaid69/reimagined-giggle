42cce4a6ec7e19cef27e3956d5e5489d7ea207d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:56:24 +0000

    account wrap point like Bb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
