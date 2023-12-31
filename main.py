2561b9af6a6b492a52c0194828b0ad3ab84aee25
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:25 +0000

    tap tend design teach equal

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_list_from_array(array):
+        return array.tolist()
