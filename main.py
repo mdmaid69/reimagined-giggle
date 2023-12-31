f6cdfbc6abfb9d679738e7ee99adf1970343ced1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:56 +0000

    watch luck poor cruelty mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
