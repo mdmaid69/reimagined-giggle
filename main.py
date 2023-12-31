1e4d0d858319b80243773e20b27371136f5ca9a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:07:03 +0000

    gap bringforward our give pity

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_area_rectangle(l, w):
+        return l * w
 def calculate_density(mass, volume):
         return mass / volume
