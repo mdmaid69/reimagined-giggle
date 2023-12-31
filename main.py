2ade90f3fb42271491d93a8dd41915d4f36dea6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:56 +0000

    care bet skin play deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
