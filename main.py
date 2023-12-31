33cb79beacc70bd426d02f233e037551797f880a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:48:27 +0000

    justice same wind threaten gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
