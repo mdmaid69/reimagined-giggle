e88250da787b4b9390913a1362f4beb2ab5fe9b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:24:55 +0000

    computer bend plastic lookback(on) their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
