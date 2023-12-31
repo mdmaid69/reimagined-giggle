c9374ac9d19d2947ac81b1a545979a3758aeb055
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:14 +0000

    fence carry dance tear reach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
