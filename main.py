6afdc476081a2a71afc2f7b590969a6be253da1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:21 +0000

    science pinch bringup phone typical

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
