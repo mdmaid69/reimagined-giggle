3a07fa092a70273d058cfa33ed719e7cb136e773
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:00 +0000

    trick excuse kill tongue swing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
