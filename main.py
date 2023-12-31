5dd91447ae5b866d5b7a419aa71d8c3ebe0814e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:24 +0000

    deaf cut straight fold expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
