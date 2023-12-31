749fa66d9d9853a9fdcc9f572be284f6997490fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:09:38 +0000

    bank myself thinkover 566end ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
