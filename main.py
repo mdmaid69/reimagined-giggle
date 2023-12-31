3a88399f04a67b301dbdf44c472b4ac8801b7de3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:57 +0000

    note person our protest confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def reverse_array(array):
+        array.reverse()
