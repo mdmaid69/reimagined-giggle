10c7ac7c389e49240826e4f1ccd5dca7a5908d11
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:35 +0000

    strange error display various yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
