2e59b1c7b66a59e547b7c5de42cb75b40ca8c00a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:48 +0000

    boring board uniform school brother

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
