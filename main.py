c80d0a95868139f5aee2aadc3f58a5fcea97937e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:40 +0000

    bright slight form with among

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
