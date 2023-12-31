aec80a95480e34314984ad2c8969a5b5a4658034
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:02 +0000

    wide immediately in nigga sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
