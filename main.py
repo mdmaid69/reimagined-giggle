c55ee56e265130b51562efb78ee371a69ece193c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:59:16 +0000

    copy beyond face male tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
