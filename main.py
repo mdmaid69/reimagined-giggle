37f43f5c5dcf273561c32d5259d95f099e3c88a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:55:55 +0000

    pot priority it break nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
