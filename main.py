c50cc26eb670d900fec52ea03124a9e2d02d28f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:04 +0000

    blue either runaway translate delight

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
