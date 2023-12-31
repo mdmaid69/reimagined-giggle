35333bc28e0b1083c0bf0e3f704ce40a14f6f468
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:09:42 +0000

    interruption goback(to) post national between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
