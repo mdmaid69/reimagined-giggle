a0b1cd6549cca20a360a921368411d4cdcf37409
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:52 +0000

    view once admit unlikely swimming

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
