683065598ff277d95846cff6bc7b3b1671410916
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:34 +0000

    shoulder arrange talk study include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
