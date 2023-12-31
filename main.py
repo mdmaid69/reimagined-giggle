171c4c23ed69e7ed95e6079a6fc14fed750ad4dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:08 +0000

    clearly attempt herself payment splash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
