2d693f7b5de52566f8c4c4ece98506ecb6af36a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:59 +0000

    better recovery much shoulder hear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
