b332cd2ebabbcc4ffc3f34a9532f23595bf8cf73
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:53:18 +0000

    pattern spare music tool kick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
