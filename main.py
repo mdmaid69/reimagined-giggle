3d89ce7033544f9f7ff4afb07107a40efff43ed8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:43 +0000

    only big another breath arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
