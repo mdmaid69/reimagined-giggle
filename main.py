f3182789e20ad714d89215b24d4b79f2cbdbbeb8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:52 +0000

    succeed wander display above lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
