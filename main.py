a977ca31966fb3e7e591911f995cfbe2418ba795
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:41 +0000

    wealth influence them rid goout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+import platform
+def get_python_version():
+        return platform.python_version()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
