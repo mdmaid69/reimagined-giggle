ed5c21a3e5ee409686713647b201b9d3dd157a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:34 +0000

    least slight far inside fight

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
