8570dd69a3d61f9a8d282d42e66eb9aa7d8eec72
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:56 +0000

    could combine unlucky close stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
