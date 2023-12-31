1c17900c80d26f488b8e3e37e5fb8c1b1a427f04
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:40 +0000

    enthusiastic phone turnover ours group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
