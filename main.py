587347205c6dd348f8a7ad3d06c5989a233609e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:51:43 +0000

    570enjoyable increase lucky explain minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def reverse_list(lst):
+        return lst[::-1]
