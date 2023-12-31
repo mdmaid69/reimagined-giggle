c5c9d5524a231a32a7c74b6ec7dc67f48cd32cf2
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:30:11 +0000

    be independent reach like workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
