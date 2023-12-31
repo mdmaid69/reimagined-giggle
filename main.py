a43ff92b96755609f56e452143bc19c6eb068553
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:01 +0000

    reasonable nature past get pass

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
