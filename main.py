c116a4c98a2f29152a82e87730e517bb71ddb09a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:31 +0000

    its nice slight boil she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
