f309ac294137f07f6bca9146bd8f46e6ed4961ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:44:22 +0000

    press exaggerate holdon independent point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
