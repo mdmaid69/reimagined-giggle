2dc65860f2a5dc0d6a517b4b176233d826cac6b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:18:37 +0000

    order tap rob report complete

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def find_max(numbers):
-        return max(numbers)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
