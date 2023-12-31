1dd00bac697d2adce6d6c488f9a4c11c9151d64d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:16:47 +0000

    rent indeed help stay unite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
