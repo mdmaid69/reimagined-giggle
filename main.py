1021575ab9970c7f9d149cefc9f616d57b755ef4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:57:45 +0000

    light original closely joke possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
