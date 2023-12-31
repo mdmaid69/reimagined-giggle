5d108acd946a9baa586d22785db0db705c9152f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:27 +0000

    become about suggestion period awake

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def find_min(numbers):
-        return min(numbers)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
