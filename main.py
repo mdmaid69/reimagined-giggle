1f252e7205f6779128412fb446fd77abe6971f83
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:56 +0000

    clearly healthy total through top

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_int(array):
+        return int(array[0])
