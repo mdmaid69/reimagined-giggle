19e38fb63dd541ab4210030d6772e26ec70b3b2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:25:12 +0000

    ticket happen anyone empty brother

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
