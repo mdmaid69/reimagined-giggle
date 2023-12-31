e8c329cdd37d1184b79aa7d245038c3b3ad6fb40
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:26 +0000

    production abroad century spite extremely

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
