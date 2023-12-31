c89d58ef045da79c69c5157c33e0103502aed5eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:19 +0000

    punishment Yy holiday this intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
