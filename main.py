f93e8ded095c12ed258e0beb743b91a6a9533fef
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:07:56 +0000

    type other powerful every charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
