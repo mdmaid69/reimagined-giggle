3308a658772b2f3483b9cc87f7aa7e786f57728a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:22:36 +0000

    nature stiff one stop they

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
