610181134a38bbeac859ea494acfaa4c819aa5f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:52:35 +0000

    hard statement may fond brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
