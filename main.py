eefc48fca2511ffe5b3a463110e6bb2d5d28c487
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:46 +0000

    behaviour behaviour agree bring dinner

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
