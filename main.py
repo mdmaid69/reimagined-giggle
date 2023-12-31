fd62b899c8f970ba32798eadbba482fce2581415
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:41:17 +0000

    hello expensive meal breath innocent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
