3241b8a72eeb33f2f7e0346943dbbcd8f94bb9ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:36:17 +0000

    blow trip fetch shade public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
