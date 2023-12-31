5139c12acdb08fbf08dde0fe3ea35b7ffcb57ed7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:36:36 +0000

    available breakinto congratulations Zz space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
