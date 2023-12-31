9868ae4eb8195b2b39ff054ff1335d5fbc21c68c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:55:19 +0000

    extremely ride inside breakup there

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
