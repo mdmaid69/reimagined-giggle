caef7e43ac1b7028991c3301b1dc2945316f534e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:48:42 +0000

    error plenty tip getback flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
