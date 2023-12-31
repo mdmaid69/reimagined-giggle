90e5115154598d289e13b59863ec08e6820c1619
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:31:34 +0000

    thing more niggaacademy pretend study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
