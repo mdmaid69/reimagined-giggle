a44bd4f157d11a06f589702da1db6765ef727af6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:06:17 +0000

    answer another tend drink tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
