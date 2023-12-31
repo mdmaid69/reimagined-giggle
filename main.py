02ba0bfe73b98ac0fa8f9a56456504aeef25c0ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:02:04 +0000

    interested thinkthrough soft connect raw

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
