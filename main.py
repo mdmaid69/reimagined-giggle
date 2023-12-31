2096ce9d80148680895aeb973529051f94b8ba74
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:14 +0000

    takeback towards band cross build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+x = 10
+y = 20
+print("Sum:", x + y)
