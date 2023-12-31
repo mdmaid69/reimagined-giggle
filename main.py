8adade9e7e16eef2ffccbfee5e6bdc083b9ae979
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:46 +0000

    too wave back public hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
