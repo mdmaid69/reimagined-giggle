4f90ae5d0505b317751fde1881ce88f68bd806cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:00 +0000

    thirsty yet probably except ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def remove_duplicates(lst):
-        return list(set(lst))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def add_numbers(a, b):
+        return a + b
