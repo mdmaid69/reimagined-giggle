07d0780156612fc5b9107321f117f16a8ae0bdb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:20:51 +0000

    earth majority secret bridge trap

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
