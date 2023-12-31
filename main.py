fa35d65a6de815d7c311f386980c20e967cd27a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:00:03 +0000

    wave father put track takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def is_palindrome(s):
-        return s == s[::-1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
