054babaf5477c7a91df4ac357dd16b176b2baa32
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:48 +0000

    expression do clean loud slightly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
