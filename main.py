2d609f76f86d477f32c7e4c923271e45a09e5ca0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:50 +0000

    length then laugh promise lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
