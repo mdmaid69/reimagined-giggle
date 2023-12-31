8cc9181115f4344766a23a0b23590fed14dc8675
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:00:20 +0000

    hand hospital write somebody sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
