633a0248fb1613935497606f2d588ea5ed280b21
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:12:28 +0000

    safety uncomfortable switch Pp zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_power(work, time):
+        return work / time
