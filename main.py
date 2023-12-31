c9634d3a6927153fdf745cb69887923c4c1481ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:24:57 +0000

    level nurse happy wing rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
