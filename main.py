49ab5145e9168cd27c8d49b7ef27c02ecab8daa3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:45:57 +0000

    active matter look forget deaf

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_acceleration(speed, time):
+        return speed / time
