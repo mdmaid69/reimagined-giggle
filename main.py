3054e00c17441f7bf97135dd159b57001cb5a24d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:01 +0000

    term length look near destroy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
