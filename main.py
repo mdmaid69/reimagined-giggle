176e96ed60f76ebfeee50d01ffb2e0a3d65620bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:38:23 +0000

    payment soon attach price further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
