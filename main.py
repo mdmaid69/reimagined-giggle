9070d84d98edbf780c7b1444a5459ab046c966e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:15 +0000

    comeup my win high climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
