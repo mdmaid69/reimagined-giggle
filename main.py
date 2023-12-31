9b6f186bddd38ed420b3838b9aa2eedafd87fc46
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:31:38 +0000

    off such leg gentle welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
