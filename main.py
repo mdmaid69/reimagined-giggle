6a0cfe17a59236b9795b7b8c225fe497baafdd48
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:56 +0000

    turn limit unite crime now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
