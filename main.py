52b58e15f5d9af1aecea78351d45cdda82518751
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:14:20 +0000

    wild slice quiet commit treatment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
