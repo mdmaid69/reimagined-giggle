f81b1dc677243ec680b848b7a6254bee1994a455
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:41 +0000

    talk 564encourage stamp choose finally

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
