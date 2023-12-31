3a9921189fe37b5ece21241f1e413484b46f9c9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:23:33 +0000

    pretend Ff temper ashamed joke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
