d67f30b8a82fed3902a176f8231d2d1dce9e02aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:26 +0000

    love quickly message kick brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
