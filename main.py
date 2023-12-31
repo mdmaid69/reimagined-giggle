00166e9c14cf7120fb7ce9b4090b5d0a0ecbef72
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:50 +0000

    burst small image face ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
