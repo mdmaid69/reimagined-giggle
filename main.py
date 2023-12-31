c589c7ffa0b0a5ff3248e57693c2f6020a1f95f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:08:06 +0000

    matter win name deep get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
