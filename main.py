d090600f321f96c880c7cb91654b95791cf1412f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:43:47 +0000

    which choice especially embarrassed still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
