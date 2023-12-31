19da36097bd581ec212f1fc1f2861e9c1ab6bd8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:19:05 +0000

    limit explanation hold children their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
