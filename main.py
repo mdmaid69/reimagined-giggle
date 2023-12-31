cbe06f4793b628c6ee9d8d88e1b2baf9abecac98
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:01:31 +0000

    already official slice wide slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
