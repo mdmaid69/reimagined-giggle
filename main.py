90a7aa7791dd93e7c3f8876094dce5db92d22ca8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:19 +0000

    lift rent protest experiment general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_power(work, time):
+        return work / time
