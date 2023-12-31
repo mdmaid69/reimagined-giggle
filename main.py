833a54df27786d0b33243495d73db10a31eb16a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:13:33 +0000

    wave strength middle mess look

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
