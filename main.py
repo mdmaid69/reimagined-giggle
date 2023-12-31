4b99a18e675a27659fcb68d3c9e3215a569bde5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:19:46 +0000

    impressed shade do squeeze almost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def add_numbers(x, y):
+        return x + y
+def reverse_list(lst):
+        return lst[::-1]
