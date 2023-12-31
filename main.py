802735015bc30424e832881395b7583648ccbeeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:57 +0000

    never rest bite pass comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
