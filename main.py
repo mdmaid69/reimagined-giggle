3168b07ca3ea7334f553dd7409208fd2d5a1df34
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:51:26 +0000

    down invitation fast stone hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
