97f46b19d811ea6f0f019bfb0fd9cfa802df2609
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:11:58 +0000

    both turnover evidence try judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
