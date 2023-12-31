7d14192ef9d0ea4e56b6fd5f5533ff9466cc8db2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:51 +0000

    split right left action thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
