d5d63893c97453cdb7eb2c4dedd77d9b42252e94
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:15:14 +0000

    total strength side company waste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
