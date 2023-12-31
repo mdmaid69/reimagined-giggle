98df35d4e070ba8c132cfda6cd3df32201428f0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:00 +0000

    attract bang silent knowledge present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
 def calculate_sine(x):
         return math.sin(x)
+def reverse_list(lst):
+        return lst[::-1]
