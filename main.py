35d56ba5ca63c797561774bd0d72a69ab03ab2ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:32:35 +0000

    dark sign judge excited miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
