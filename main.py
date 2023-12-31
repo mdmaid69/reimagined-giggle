b7a036158a239b71b50823a0eb55fb69697a0125
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:03:37 +0000

    hollow cough ticket ashamed snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+print("Hello, world!")
