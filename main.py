f9e6a0f940b728e58155cc686cf5c3b1084e77fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:41 +0000

    pity at speed doctor section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
