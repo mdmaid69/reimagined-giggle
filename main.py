c48f7a6d6d5e82bc3e9335b69dffa91e10c014ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:55:43 +0000

    move downstairs show funny cold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
