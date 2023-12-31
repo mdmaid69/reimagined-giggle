70782be391f5d5a4a855d313c9223da046a5a09a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:46 +0000

    their future separate ours rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
