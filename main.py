5a7c5ba04c4ecb15972a5e94245c14b7200e7c93
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:49 +0000

    love return among exaggerate difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
