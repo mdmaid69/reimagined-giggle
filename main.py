63b64fc9be167a55f69ac87f96e4f8dbb464fab6
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:54:58 +0000

    anyone kick house sky complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
