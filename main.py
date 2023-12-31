6dfb8861969a976b77c9e032aefd38753cd52422
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:39:50 +0000

    impression whichever much tense who

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
