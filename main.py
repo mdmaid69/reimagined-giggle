55efc1e98e463ac1d77e7ee48ae5a0264e56377b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:10:00 +0000

    explanation takeover night discovery trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
