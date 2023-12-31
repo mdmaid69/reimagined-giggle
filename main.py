c6000f34ec7488125c28094f4c69d6323b73f53b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:05 +0000

    smooth full find thought behind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
