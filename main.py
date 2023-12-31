5f4ff60cfdcbb6146d67a7f4ce990541c64f6866
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:19 +0000

    comeround Ll 572enter dangerous cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_power(work, time):
+        return work / time
