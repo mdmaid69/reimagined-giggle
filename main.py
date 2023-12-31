ca9f1e08aab809d9243da7f53d6fa9cb261be01d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:06:20 +0000

    runaway callout should surface keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
