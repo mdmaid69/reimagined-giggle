d778f38cea92db9f64eabbcb5c92d7c777e093f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:43:04 +0000

    tired track lookup disturb disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
