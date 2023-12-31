14963cd45e68da9d518d8b58c3451d9c319d0486
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:19 +0000

    choice university strange sign ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
