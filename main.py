be4d4b659744ac6fc1387cc626f8bec90f6f9446
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:43 +0000

    suit report body anxious cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
