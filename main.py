95df6b1d8a11be6bad575533a2b107fb5b162985
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:49 +0000

    achievement fortunate hide have uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
