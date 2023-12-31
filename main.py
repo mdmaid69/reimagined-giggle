8ac77dd2dbfca5f06c5dbb66514a11cf71d64328
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:53 +0000

    regret gate high low battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def square_number(x):
-        return x**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
