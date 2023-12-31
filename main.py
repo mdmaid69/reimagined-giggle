7340efe51ec6f0a47567abfa07d0f786ebbe2f28
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:29 +0000

    giveout lose previous furniture basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
