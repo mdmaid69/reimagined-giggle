17664b5cda1c3755f2129cc92c35db9f85f51e55
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:21 +0000

    holdup state broken talk comefrom

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
