ebbaca94a658af2a6ec4edbd069b963c6a3e30d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:21 +0000

    watch stand where name sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
