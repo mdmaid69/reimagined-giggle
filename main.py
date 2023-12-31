eade8c98848992b52bddec5c34759e3d809ff9c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:39 +0000

    normal church yesterday run proud

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
