36bda2b3a35e49c7bdd18d55a50c28819237c5f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:08 +0000

    clever let pollution whole surface

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(sum(range(10)))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
