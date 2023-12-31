ed2f50b71070c77493a9c91410de885a2961dacd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:43 +0000

    born risk order remember mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
