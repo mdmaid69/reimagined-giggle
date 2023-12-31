97b09a1edd1d0ad0a2868430a1cc2222fc2ac530
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:16:40 +0000

    record yes make weather fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
