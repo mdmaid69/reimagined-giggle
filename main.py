0d8a25cfa2e7fba35b1d697e94cb7474f08c4e88
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:52 +0000

    remove takeaway rid buy uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
