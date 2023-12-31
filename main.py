fc8540dfed2aedf2fed81fabc4e3a4a3aeb263af
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:15 +0000

    teach reduce branch hold sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def check_if_array_contains_item(array, item):
+        return item in array
