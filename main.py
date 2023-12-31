9524ced4940df9fff3861c588354a52a3da62a60
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:20 +0000

    feeling offer general settle hot

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
