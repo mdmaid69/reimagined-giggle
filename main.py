08da2beb4cb3673df64eac417fbbad8a3c6faef6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:04:45 +0000

    exploration tidy emotion lock pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
