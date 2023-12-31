a7a22b8e95a1bf80353083cd025702636a6ae0cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:38 +0000

    scale mess succeed style rubbish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
