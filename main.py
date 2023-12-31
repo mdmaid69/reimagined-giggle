30bcbf2ab78e6c9dfe068f43810f7ba1c6e7263f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:21:57 +0000

    nose dear unlikely your very

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
