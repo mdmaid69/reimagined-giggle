d8f2745a1117a237037ce91753cb4a34d3fa61bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:24:49 +0000

    image wonder beneath giveback sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
