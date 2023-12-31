0e9118c9e92f1085dc0275ad808c61f462f06066
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:01:20 +0000

    season shout godown loud school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
