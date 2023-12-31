228624d450abc785f787a80c9d0dd74af4e9320c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:03 +0000

    persuade anywayanyhow limit comeround hold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
