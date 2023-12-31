1e3427130344a7cfdcd903718d1ceaaffce67ba4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:27:44 +0000

    pronunciation exact sort half back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
