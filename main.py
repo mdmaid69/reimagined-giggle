ca8bf153a525a761dd50e8fe26a6b04d5d1ce15f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:04 +0000

    Nn badly turnaway basic comfort

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
