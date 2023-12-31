8695b1b79fba19e7dd8c3abcaaafca0e24ff39f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:58 +0000

    male meanwhile across giveup ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
