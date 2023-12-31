0fa45fbd8a89642f6f8e95e753cceeccde72aadf
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:24:44 +0000

    combination consult move read mad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
