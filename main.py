96ce2f36765a830bd569bb480ebcde5fae19fc38
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:19:46 +0000

    she demand compare hello quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
