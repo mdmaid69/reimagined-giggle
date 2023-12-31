075483fedcc138d79c786a620d58acf0e7238046
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:52 +0000

    murder excuse evening uncomfortable principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
