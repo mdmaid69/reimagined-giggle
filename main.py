3fd9acc0e5b2fd532893e3a1f00867966ff3b17f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:15 +0000

    mood means miss fix university

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
