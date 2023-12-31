2477522bd13a2b7c7ef4e53a9c8108cc09871ea3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:25 +0000

    fetch branch 571enough discussion defend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
