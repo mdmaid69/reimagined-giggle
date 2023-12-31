6812aa3e63ecd4e346e872596931f20e77d5a1f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:24:22 +0000

    mess smart remain confusing spill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def is_odd(n):
+        return n % 2 != 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
