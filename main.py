6d44398510e4145ff526aa7da61314d3909dbea9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:37 +0000

    basic habit like entry mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
