243ae19c54e733a179ee6e3d20ab17cdd8c106a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:46:01 +0000

    address make immediately tragedy succeed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def divide_numbers(x, y):
-        return x / y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_odd(n):
+        return n % 2 != 0
