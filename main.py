52bae3a67c495ef8a18218268af8c86144d62247
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:56 +0000

    judge team guilty no accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
