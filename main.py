13f83c7befee7a5a8f7134e31de8dc2d9bdddd13
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:45 +0000

    firm left quiet comefrom regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import platform
+def get_os_info():
+        return platform.uname()
