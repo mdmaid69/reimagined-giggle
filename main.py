49db6489db1e938acc2f8e69ea18854a4ffb90a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:59:38 +0000

    constantly listenfor fire proud then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
