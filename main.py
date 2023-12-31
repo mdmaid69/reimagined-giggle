1a348088f482b9631806a6d8382055ebc032fb27
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:15 +0000

    patience fault tear relax 568energy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
