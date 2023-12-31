105fde4b8d98e506686b6a47495ed227bdeb7ee3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:49:27 +0000

    able as list great turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
+import time
+def get_current_time():
+        return time.time()
