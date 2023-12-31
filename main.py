2e84e9fe5a3722b6d89688dbd389e2f3b7ebd9d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:51:28 +0000

    dick her thought downstairs accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_work(force, distance):
+        return force * distance
