44de2810cbb03791caee2f9c953684eff0aed2ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:05 +0000

    combine avoid paper blind goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
