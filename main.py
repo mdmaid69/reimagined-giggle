13e88579adb300750db7709a84b1e4722a852a63
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:14:11 +0000

    raw fault conclusion cry exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
