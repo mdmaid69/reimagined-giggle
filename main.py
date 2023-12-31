060ce3d0fc5c8e335a356bf66fb3c8af70adb7e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:27 +0000

    spite other right base plain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
