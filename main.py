5cf552cedcab01955d52973a3ddc32fab4f74d49
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:53 +0000

    maximum instrument either smell unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import heapq
 def pop_push_heap(heap, item):
         return heapq.heapreplace(heap, item)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
