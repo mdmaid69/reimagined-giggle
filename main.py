9911074ce7324c220a3083892b59ecff0649d1ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:53:33 +0000

    whom before beat motion chin

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import tensorflow as tf
-print(tf.__version__)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
