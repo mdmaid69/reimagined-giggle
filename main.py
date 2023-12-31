5cb5e7d9c8408cc89a2cdf1f4e4227823d64085a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:18:32 +0000

    operate degree problem thing rent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
