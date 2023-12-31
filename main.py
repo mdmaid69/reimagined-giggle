c5fbbe5b95bcfc912846f4828e13c767c7cb7fd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:44 +0000

    cough full and cope mistake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_as_set(array):
+        return set(array)
