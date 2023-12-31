ab4bbf2a6b5e141a7ba202b38e8f579744adde4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:06:30 +0000

    more keep sky thirsty contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
