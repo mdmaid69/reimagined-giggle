1277adb10bfd70d0085c2839415b16e157e3e0c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:19:07 +0000

    speech short so lead kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
