292dbcd062ebfcaf6cfc33a5d4b3d06fcd939032
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:17 +0000

    string raw easy generally determined

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def is_even(n):
+        return n % 2 == 0
