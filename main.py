6f3802ce43638b6af00ba8be1afd37e92ebe7474
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:45:51 +0000

    fire shop accommodation murder market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
