27db0c362639a69782335596db68929ff769988c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:18 +0000

    scared you whole impression peaceful

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print(sum(range(10)))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_item(array, i):
+        return array[i]
