1c921c52acd6a17bc0b5e47a5d6893071dc46db8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:13 +0000

    treatment over collect face keepup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
