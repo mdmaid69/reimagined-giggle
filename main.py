2aeb5c3d3c1024694c2706036125e73dca7f28e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:56:31 +0000

    replace yes have autumn on

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_stack():
-        return collections.deque()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
