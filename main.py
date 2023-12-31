2ec0e35e3b9161ed16a987257230896f53873ab6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:59 +0000

    wave realize pleasure trust rise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
