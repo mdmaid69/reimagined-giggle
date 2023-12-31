3840985b1bec102bb9e54050c3bc856867fa4306
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:34:44 +0000

    himself fortunately fairly use floor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
