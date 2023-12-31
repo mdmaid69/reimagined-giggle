5d9bcbf175c1585b4b3738e919508d4dfb3cc18b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:24 +0000

    sweet exercise here shock rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
