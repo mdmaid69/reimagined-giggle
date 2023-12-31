6e73d29336acd03928abdc0f93b773edcf985580
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:18:37 +0000

    dream freedom quarter juice violent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
