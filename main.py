e0caf18513b5f8688eedaebb073ec8d1f6447248
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:57 +0000

    show round talkback born steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
