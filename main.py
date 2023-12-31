6f0a7620896f94ed3088a700763ff1bb1a3bcb3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:48:44 +0000

    habit uncomfortable afford care bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
