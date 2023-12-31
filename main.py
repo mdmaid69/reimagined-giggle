3bcf6a9c07485133f545c19c018277d2551cace8
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:27:28 +0000

    avoid each vision control evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
