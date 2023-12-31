a735786ef6c37e3b3da0f9bd7043c08f72f89e21
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:27 +0000

    tip once direct apply form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def convert_to_binary(n):
-        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
