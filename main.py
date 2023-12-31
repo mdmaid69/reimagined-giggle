1b571e4ecf7f6565457c94044c77730b70761e32
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:24:31 +0000

    offer 573entertainment thankyou secret valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
