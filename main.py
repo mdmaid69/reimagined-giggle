906015a113478c153fe84c364f8fe67aa8658d40
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:02:55 +0000

    threaten towards fairly expensive appearance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(10): print(i)
