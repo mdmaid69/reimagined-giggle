ca76b3a548b232d2cf90c2de7a70816d36b3aa00
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:21 +0000

    start nose she recommend Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
