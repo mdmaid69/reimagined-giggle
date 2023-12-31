d7f808a306b1fc83e44ae1a4ad495da394f0fb04
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:18 +0000

    commit wild Ss table comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def print_python_version():
-        print(sys.version)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
