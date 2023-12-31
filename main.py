9d187d4ac491af5586ae87f5bb2662a6f31feeb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:09 +0000

    trousers whoever building only general

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
