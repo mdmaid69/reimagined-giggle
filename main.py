47400ef6f40cd8ccf8999b7459075638ac4a4c6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:33:00 +0000

    Itself fasten hurry comment which

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
