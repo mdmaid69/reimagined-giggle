1dcd002b0523c4ef8109f4b6fa467c3674f10130
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:18:07 +0000

    distance that character regional deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
