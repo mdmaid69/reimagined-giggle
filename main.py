79a4fdffae41977c18608bdf50ad7deade9f74b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:42:37 +0000

    downstairs extremely lead receive mean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import sys
-def exit_program():
-        sys.exit()
+def cube_number(x):
+        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
