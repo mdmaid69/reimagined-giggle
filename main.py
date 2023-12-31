40d0ac3da947051a0a0e9efe3147341b7a8af46c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:02:19 +0000

    during thanks bath violent miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
