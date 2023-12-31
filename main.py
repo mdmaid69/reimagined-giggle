1421dafda0a9b34b1efff6b427b710f61a9f9044
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:46 +0000

    hospital benefit pool consult distant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
