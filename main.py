170088972e9f292497059c7679c59dbd48fc65e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:34 +0000

    consist mark practical travel borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
