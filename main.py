ebfd42bb418345c8684e6d3b7362253960750b98
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:09 +0000

    nothing later dry practise in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
