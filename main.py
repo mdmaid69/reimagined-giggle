2db20ffa517beabaeeb6189808a4150e3441447b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:53 +0000

    improve name information contrast doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_queue():
+        return collections.deque()
