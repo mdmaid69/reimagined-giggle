777a9386cc485b1817d700b0acbec1087f04cef2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:48 +0000

    skill fit study minor whichever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
