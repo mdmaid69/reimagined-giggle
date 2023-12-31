3456613b3ac782a53b026e998b0546eba9a9fdbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:20 +0000

    share fortunately phone schedule light

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-name = "Python"
-print("Hello,", name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
