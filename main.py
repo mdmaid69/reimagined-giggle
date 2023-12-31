eb324d513b2eeccc4b5767d110f0d3bde93337f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:50:39 +0000

    large 564encourage situation Oo answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
