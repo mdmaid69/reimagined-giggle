2a9ff27d792b6184ca90c689654caad6a82c54fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:54 +0000

    guess burst undo goout yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
