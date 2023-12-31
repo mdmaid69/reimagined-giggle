818d5350a94818d6ee044a819325bcaa109778e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:48 +0000

    may roof accidental win OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
