bdd040b0b4ac943b988efbad65cd4599d97aabdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:47:36 +0000

    bringup beneath earn trap during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
