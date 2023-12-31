be5fcf65ff8d94f6a36c62e978cba7d08413a3ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:26 +0000

    surprise worth fall mind reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
