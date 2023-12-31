5d2d2e1f9c6fc90a884bacc48332231a80be351d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:50:17 +0000

    possibly suit Gg occur sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
