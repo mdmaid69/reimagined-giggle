2735945b1d6a7828f365e7be681ee6d0b94d0262
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:52:56 +0000

    sore increase get nice unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def multiply_numbers(x, y):
-        return x * y
+def is_odd(n):
+        return n % 2 != 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
