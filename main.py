dbb7573fbd795b2a60e0bee812093ff1d8e516bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:57 +0000

    estimate holdup nose behind second

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
