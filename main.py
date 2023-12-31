19423691bd184a114a58992037032de2975370fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:31:20 +0000

    concentrate slice show turn good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
