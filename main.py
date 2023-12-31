59bb241f306e7998eab1c664bc93e70f2ccee3c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:09:11 +0000

    confused excuse limit part seem

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
