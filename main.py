40dbf10b5b3b91507197b71bbd24bce756900d0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:57 +0000

    wrong score explanation rescue spread

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
