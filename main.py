deba387ff26f486bc836a0be1b49172541ace536
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:22:26 +0000

    contact wide cheap behind well

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
