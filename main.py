0782b75f58fefa9cf50a92de5a2c66c299f7730d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:40 +0000

    nose claim print breakfast ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
