3fc0b120c4556c51ecf32ec19b512a10a9b9482b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:08 +0000

    spend later lost forget at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
