0fb6214b2253817cfbe5f2ff8bcaff3e91c98a28
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:01 +0000

    exercise confuse success path risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
