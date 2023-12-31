edce8b7b2384e72dfc2ad85a65be6afd41b016ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:29 +0000

    sign minimum show neat defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
