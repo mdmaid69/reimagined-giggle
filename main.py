dc22a3a9e236747976e8e5c3162269475f542ed8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:08 +0000

    please steam music morning spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
