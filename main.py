e28a858215d6413f9c69eb771a864439c4758e39
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:59:44 +0000

    acceptable book shock quick schedule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
