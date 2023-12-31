f8bfcf31bb40c4d00614f3344559423031aee138
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:32:12 +0000

    drink reduce treatment act many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
