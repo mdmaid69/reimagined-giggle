b3039997e0b7df6ec07b69758562faf7987a310a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:57:37 +0000

    bill story beside own keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
