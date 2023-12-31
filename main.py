be0903d65f97da4c57a07e8fee21f9d430e9ed0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:01 +0000

    more important callup recent arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
