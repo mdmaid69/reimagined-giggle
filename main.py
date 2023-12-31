b6f272cdd8f23f680aae7865463aa20b901cd33e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:52:37 +0000

    steady getaway term contain have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
