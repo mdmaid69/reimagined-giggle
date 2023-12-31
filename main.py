444b3e87e1765653396e60af5fd7ff744164f7aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:16 +0000

    gas stranger completely already evening

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
