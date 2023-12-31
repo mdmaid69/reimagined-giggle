002dfcbb54f76406c0864443dd1e59b8166d7de9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:36:43 +0000

    ahead issue callback central during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_as_str(array):
+        return str(array)
+def subtract_numbers(x, y):
+        return x - y
