28f6016846548361f90c4250395a0a4831ba2773
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:08:11 +0000

    quarter conduct work breakinto coat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def sort_list(lst):
-        return sorted(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def is_even(n):
+        return n % 2 == 0
