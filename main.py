d037a3596b6a6012c0a88960981abb4c0d5a46a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:37:57 +0000

    idea fairly swimming post next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
