f90dda49ec81d66d4f4e89fc7428f4670a6d7c1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:21 +0000

    proof wealth until production original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
