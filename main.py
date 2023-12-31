8e2a6b8aeb606f6d733241448947ae4e2fc14d1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:52:43 +0000

    attempt stage only hope congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
