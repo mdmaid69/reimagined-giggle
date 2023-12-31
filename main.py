48d0d63aad81754423f9873c9634328d2243be30
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:49 +0000

    hold neither impressive play usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
