ad5b6fa3ceacc7c7e2f8ed43f442cc46e8de8176
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:58:50 +0000

    pick range contain mark best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
