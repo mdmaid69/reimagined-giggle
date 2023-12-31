587fcfd88f6682197e5297cb836bfa18d99db3b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:33 +0000

    talkback run whistle offence usual

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
