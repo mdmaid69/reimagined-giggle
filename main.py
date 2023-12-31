a518ff89168fa9073f080cde5011afba6d05cd0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:49 +0000

    shade Oo set behaviour method

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
