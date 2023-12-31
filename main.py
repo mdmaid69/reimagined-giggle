49f3fcd72139d54be4ae085883d8df6eff44e82c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:17:23 +0000

    amount float turnoff joke last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def is_palindrome(s):
+        return s == s[::-1]
