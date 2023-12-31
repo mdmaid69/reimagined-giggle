c2899bb6024522e10a8cea80ab0feb8eeed775c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:34 +0000

    course introduce give sweet hour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
