2505bcb0d0f789f310bbd2b4c5f56b828357e306
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:44:48 +0000

    sink Tt book drop brush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def count_elements(lst):
+        return len(lst)
