0850d7d59fa474689ab4d09fb24371592ff6ce2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:34 +0000

    equal outside return crowd impossible

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
