2a407ed1bce5f751789deed6042f8187829c0bcb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:49 +0000

    account country rush understand book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
