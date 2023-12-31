d93b56c092044045287e579ca7e865b3b665f198
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:03:59 +0000

    plate start free runout prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def add_numbers(x, y):
-        return x + y
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
