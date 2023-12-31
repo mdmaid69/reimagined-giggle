94421eb650cefdee0d20fa5ce3a1cafd121caf17
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:38 +0000

    see powerful emergency bus ocean

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
