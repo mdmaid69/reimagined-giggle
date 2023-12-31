d94218b59c7630d0fb684389669bcd48543ad549
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:36:43 +0000

    bar consult colour squash friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
