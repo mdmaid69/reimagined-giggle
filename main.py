ed4828988dd83d37949fb07b1413bbf35de296f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:41 +0000

    close like ear smile early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
