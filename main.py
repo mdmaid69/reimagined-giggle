56ef77c9b5d5a9c43409103ae43f65e0c48159f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:37:52 +0000

    start flat general help during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def remove_directory(path):
+        os.rmdir(path)
