06fa6002b1f4ae769cdb6b18b73b7f25f4550b84
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:27:05 +0000

    usually different square press exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
