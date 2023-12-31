87b00e77e338c9d0b7711226ed97c80daecbbcd9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:59:03 +0000

    apart water snow getoff will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
