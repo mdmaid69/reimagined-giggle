3807f3f74363f75f12723e3731701cdfcc6dd190
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:45 +0000

    think century every back but

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-text = "Hello, world!"
-print("Characters:", len(text))
+  def square_number(x):
+        return x**2
+import logging
+def log_message(message):
+        logging.info(message)
