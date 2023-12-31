84cd6b22e73a21d12c631a340a23eee40fa6f120
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:42 +0000

    part accidentally sky interfere judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
