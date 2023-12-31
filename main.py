d85e3475364fcfc4deb243b8c59db0cf65091a87
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:00:02 +0000

    personal impressed approve natural no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_power(work, time):
+        return work / time
