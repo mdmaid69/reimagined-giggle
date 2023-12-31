5fed1a19dbc2aa90eeb2b468478561bbe771c922
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:59 +0000

    disturb body rescue sort warning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
