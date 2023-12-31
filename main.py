71045d6b7f1c13b0151bac09a26e52e16916dedf
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:21:14 +0000

    quality single behind determined record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
