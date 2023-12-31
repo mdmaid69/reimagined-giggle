df9c86c1418344ded61ef83db9206bc7784725b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:07:12 +0000

    sound remove force culture her

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_power(work, time):
-        return work / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
