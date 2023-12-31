74e20aaa1e8302148e04362cce1941311e8ab117
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:53 +0000

    pin wander comeover call short

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
