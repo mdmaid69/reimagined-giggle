99939a84b52f425a6efacbdb2b664c1e6cd7bc5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:09 +0000

    aim ready warning glass payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_bool(array):
+        return bool(array)
