3c641dc666b83be739c39e7c8fc539b116d49531
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:43:14 +0000

    rid everybody Ss tradition name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sys
+def exit_program():
+        sys.exit()
+def find_max(lst):
+        return max(lst)
