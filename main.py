e9f75d4e3af45ac204c80a6834a3b163c97d2eea
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:05:37 +0000

    Ff instruction hers split explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
