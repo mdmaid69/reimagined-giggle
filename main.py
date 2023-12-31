7c6c7090a95132187a0ad6f3e71f7fd504616049
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:16 +0000

    payment squeeze operate situation generous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_string_from_array(array):
+        return array.tobytes()
