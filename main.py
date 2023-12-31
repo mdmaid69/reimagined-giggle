6c13733a91a862b7edb91c28131eb1687e68d368
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:18 +0000

    market awake separate repeat evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
