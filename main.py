4259b16a2fb9218fe5cfaf3866822dbbe608b58e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:01 +0000

    mess fairly declaration talk ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
