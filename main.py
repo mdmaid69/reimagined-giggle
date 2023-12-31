940b9eaf928a2766545eb45adafe021ea422c060
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:49:16 +0000

    community gift still snow minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
