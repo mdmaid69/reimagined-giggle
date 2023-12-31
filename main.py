96edb5d7ddae48e084c89c6994a412b46f728425
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:04 +0000

    owe his command married zero

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-print(random.randint(0, 100))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_acceleration(speed, time):
+        return speed / time
