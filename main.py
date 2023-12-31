64d90feceae3d70840f9c04eb74e14fcbfd10233
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:18:02 +0000

    cope cheque cry afternoon yours

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
