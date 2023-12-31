c641405ede6222ed08b2d009d2d4e82b0bd325e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:08:59 +0000

    deep previous he breakout collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def remove_duplicates(lst):
-        return list(set(lst))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
