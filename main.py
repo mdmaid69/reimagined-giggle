a742465d18e5bdc92d43c90ab2f479eb41359fde
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:23 +0000

    butter destroy occasional funny beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
