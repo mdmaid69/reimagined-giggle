d8e3f2362300549d4e11dc5b9ae01f9e0b24c767
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:27 +0000

    combination square mention callup roll

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
