34e32b9177e030a0ec795376fb43458080a773a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:13 +0000

    land deep decide authority ever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
