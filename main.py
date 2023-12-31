60cf94e74eb49de6cb57b2da9f20c11de52c1762
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:20 +0000

    announce this class fall bill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+print(math.pi)
