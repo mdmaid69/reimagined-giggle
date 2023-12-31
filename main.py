7b6b42a306acd79c58ae3c126ceb1c103db1b6e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:15 +0000

    whatever cross difference during sink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
