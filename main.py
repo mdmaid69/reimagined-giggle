1b71601e143b57a194d2483c9776b3330564a6b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:42 +0000

    careful sport favourite whenever anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_area_rectangle(l, w):
+        return l * w
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
