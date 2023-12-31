9e72fe29b5e12c4ab037e82f9052eb004cec8dd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:00 +0000

    whole dangerous decision exchange pronounce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
