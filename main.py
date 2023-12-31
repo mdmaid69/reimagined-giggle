6112cd090c1742c4d3737777f5b63dbf6793810b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:17 +0000

    do even length rubbish behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
