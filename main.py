824ee994307b5076b09773f8fd0fd2f781c1d6d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:28 +0000

    nonsense deliberate change search convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
