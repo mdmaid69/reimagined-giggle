16a9c5745912c97674592d7ce04be384f8fe1bf8
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:19:34 +0000

    petrol us whom pause future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def count_words(sentence):
-        return len(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
