855ef4abfd86b193c3b7580957595c173d2fd9d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:24:42 +0000

    length judge visit trade keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
