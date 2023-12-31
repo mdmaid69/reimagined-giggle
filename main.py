db5543e236db0a3da9014ed0d05dcd55a4fd55a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:56:05 +0000

    mind official colour play impatient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+def exit_program():
+        sys.exit()
+import os
+print(os.getcwd())
