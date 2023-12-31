541f523dd9cabadb9df1730d28bbb8eac0b76e15
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:44:13 +0000

    gap complete maximum further comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
