56b2277e315765483b53df4cc9c7068459801875
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:47 +0000

    main congratulate list protection illness

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
+import os
+print(os.getcwd())
