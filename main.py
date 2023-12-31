af939987449e474e6fd8aa01bf1f8f191deab4ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:49:19 +0000

    snow vanish pronunciation hard holdup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
