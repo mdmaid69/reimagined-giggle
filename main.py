dd69e1faecb42074f00d7c2ae8f9fd614a133bc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:12 +0000

    argue low taste deaf answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
