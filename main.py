b1da7d498a20b303b01a38019e6fab32a3f284d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:03 +0000

    soon control less past now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
