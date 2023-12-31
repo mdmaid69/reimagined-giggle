4a079eb3f0b788a09a6a8fd978727c6f3189a53c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:14 +0000

    that still book debt view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-print(math.pi)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
