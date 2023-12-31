50f151f1560da8f64845762547ecb57860135c71
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:23:17 +0000

    method substance great characteristic attractive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
