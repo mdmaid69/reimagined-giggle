592bfce976faf31b5038aa78a013930fabaf300e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:22 +0000

    congratulate hide apply past performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
