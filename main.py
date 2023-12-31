d4698a1f24646c5688576cb16bc952f57b796fd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:59:23 +0000

    whenever tall thought level freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  def convert_to_hex(n):
+        return hex(n)
