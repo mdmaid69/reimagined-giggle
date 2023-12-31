f04d2a2eac63cd2979ca58ed7e36def0672b1d97
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:02 +0000

    meeting more promise several offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
