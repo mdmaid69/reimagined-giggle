f96df4d3b90edc3e8a83ed22b7adf895f680b7f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:32 +0000

    frighten pain positive advantage callup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_list(array):
-        return list(array)
+import tensorflow as tf
+print(tf.__version__)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
