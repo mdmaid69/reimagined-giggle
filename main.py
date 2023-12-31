c49416c152ffb8b5eeffc176d285ab22ce2fe253
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:48:47 +0000

    fear blow convince succeed whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
