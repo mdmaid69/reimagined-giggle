6532fa079a890ca4c4593d07c7985f5f7d3b414f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:18 +0000

    most hear her able traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def get_current_time():
+        return time.ctime()
