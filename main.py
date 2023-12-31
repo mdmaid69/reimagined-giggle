ced0feacef4a6088a4311a687386f8d6dbe264b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:05:18 +0000

    begin emphasis trip shrink relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def is_palindrome(s):
+        return s == s[::-1]
