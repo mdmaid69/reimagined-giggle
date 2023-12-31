fe9b19536a7bdf823764e0196a3da9ba8fbbb407
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:08 +0000

    wipe mouth stiff tie either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
