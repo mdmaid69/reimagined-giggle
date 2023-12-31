4ec58615de732276d82673ce61bc27f8730d14e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:17 +0000

    whistle practise track risk flavour

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_length(array):
+        return len(array)
