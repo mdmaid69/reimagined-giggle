d9b5390dd0cd4807f71bd61f225a15620c712aa5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:21 +0000

    exam welcome later table herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_max(lst):
+        return max(lst)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
