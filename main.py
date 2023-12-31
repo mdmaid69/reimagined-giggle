f1bea41764f89fee556665bae1ef0cd2c91b7c4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:15 +0000

    dress outside demand tax zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
