5642feaf420887f55f6b258adb826eaea3ac0341
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:27 +0000

    getup past serve someone hurry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
