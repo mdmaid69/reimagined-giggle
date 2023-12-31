aa379e11e48ee6406a66daa31db450abd00d9558
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:45 +0000

    wash comment better hard embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
