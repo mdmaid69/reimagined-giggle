e30eb8add3b9049b810248da6e72499ca5fd6c3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:24 +0000

    damp when that knowledge simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
