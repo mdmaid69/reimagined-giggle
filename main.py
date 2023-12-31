9bce02c7ec5a311e503052fc38962df553639a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:27 +0000

    content supply arm keepoff spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
