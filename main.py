b78c4f95ac2b7c6d5a1ee2e9616d7106659df22a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:47 +0000

    design hang mess excuse spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
