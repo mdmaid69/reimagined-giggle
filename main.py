26d1603b6626758b671a6f5ce7df121dc3dc3e74
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:02:35 +0000

    imagination guess will meaning goup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
