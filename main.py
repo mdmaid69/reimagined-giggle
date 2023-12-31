93c66d606d1053e6908a24db5087a334b5793e8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:59 +0000

    question past bright agree completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
