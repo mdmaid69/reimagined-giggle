dd7d06bda43b73af38afc877b9104ce1e94fedbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:54:43 +0000

    far occur drink dance head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
