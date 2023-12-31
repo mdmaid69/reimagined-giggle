cbeb69e5949ef91cdea1a91680d93d4157296ce6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:33:04 +0000

    male completely indeed stiff port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
