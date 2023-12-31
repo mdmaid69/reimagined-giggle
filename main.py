6aa42a0f160082cacb0cfeadb9b32a2e0b2c6a1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:06:20 +0000

    weather question surprising cross before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
