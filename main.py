3ec29df40dab48e5c3a54d8add380ef11dfb3780
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:07 +0000

    turn scrap win pronunciation child

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
