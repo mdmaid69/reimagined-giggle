7ff16021b14e2b6014ba9301bb17598342fcd3e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:58 +0000

    smell combine turn importance see

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def cube_number(x):
+        return x**3
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
