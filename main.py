5a2b023d3407bea3a990aa0545c2097318c450af
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:34 +0000

    ball rubbish final stick at

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def create_counter():
+        return collections.Counter()
