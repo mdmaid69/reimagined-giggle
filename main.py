2f5805b482df6f6f75e712f520ec340b4eb7ec30
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:37:51 +0000

    discuss ahead discovery length dream

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
+for i in range(10): print(i)
