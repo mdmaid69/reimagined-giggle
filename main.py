67eb0c35966cb6b91b90b2df1b556b0d3a7d8143
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:46:53 +0000

    actual goback(to) mail calm downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_set(array):
+        return set(array)
