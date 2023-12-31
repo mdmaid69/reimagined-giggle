a5d39df6fdcb2de1a6e0a22e5eea14cc56fc0567
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:09 +0000

    short trip television flat earth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
