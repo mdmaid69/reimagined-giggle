5833ffa903b060ba3f482daa049348d8c335acdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:16:01 +0000

    treat typical downstairs recognize schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
