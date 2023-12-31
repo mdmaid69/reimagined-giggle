af2e700a619b1c657a2e8b97dddaef19429a9e32
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:38:26 +0000

    base example calm friend service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
