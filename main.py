9b605c2218dfedabd613c43144f1097fe39b4d86
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:41 +0000

    shop vanish continue open myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
