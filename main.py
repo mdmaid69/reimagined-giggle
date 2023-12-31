2929900d35f6657d731841f37cf9863f8e59dc98
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:48:27 +0000

    cry black worried ordinary relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
