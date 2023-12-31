ae3cbb4c08a5e67cd9cf88c772ca93860987a226
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:33 +0000

    extra big future can would

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
