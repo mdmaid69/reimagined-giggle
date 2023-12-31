638389dfb3f5e0900d7ea803a111dc1e195011ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:10:26 +0000

    amount embarrassed appearance even impossible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
