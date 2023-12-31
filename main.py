3601d270967d159b2bba333982c9ef8383f57dfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:20:55 +0000

    crowd bank light ago summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
