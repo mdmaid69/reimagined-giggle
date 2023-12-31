283cd8ad513f3bd55e517e92bfcbef8c1c09d4ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:26:51 +0000

    contents tear Cc takeout refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
