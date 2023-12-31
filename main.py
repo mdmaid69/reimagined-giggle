cd9d43a2ef44bbf99cdf873a9cdb9cae082ee0e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:59:30 +0000

    dig front art least include

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
