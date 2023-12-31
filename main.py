398234bf842a8decee4f7211acf12908cf5c3fa2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:22:21 +0000

    turnover put occasional apply main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import sys
+print(sys.version)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
