9f5d16d3b6c9620a474f9efcdeceeb2e7d713617
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:51:16 +0000

    runover in pretty thought arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
