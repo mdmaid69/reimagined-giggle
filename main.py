807a25d2d37d7eb33c6cc388a254475c9e81892e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:57:38 +0000

    present practical constantly price scare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
