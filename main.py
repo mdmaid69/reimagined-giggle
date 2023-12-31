d442aed942031197d2e07049dd855320220513d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:20 +0000

    well watch temper theirs level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def reverse_array(array):
+        array.reverse()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
