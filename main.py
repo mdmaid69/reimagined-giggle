3a6c71380b3447942e8d4a84865e0973ce06da66
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:23 +0000

    keepout cry drive turnback glass

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
