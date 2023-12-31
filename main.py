8da9a7048654b6846a4371eac3b8ffa1b2740f1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:35:47 +0000

    shopping remove style reaction once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
