d9e048734e4210c8d31180e9dd1f1bc45488a537
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:38 +0000

    get Itself smile contact cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
