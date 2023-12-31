969ecc0ee239d9f043fecd7cabb6f9b1f38e5176
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:08 +0000

    benefit whichever unfair score fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
