bfd7a0924c3e8baeff0ab5167f0a4456fe765b2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:01 +0000

    thing because remember splash may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
