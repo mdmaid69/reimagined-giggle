2a59f0edf37deb81ce7a66d4b943a7d6c08c7286
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:20:54 +0000

    stupid pretty produce safe goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import tensorflow as tf
+print(tf.__version__)
