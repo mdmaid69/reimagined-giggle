bd0446ade0d98f34e31c583b8f35f6c3715d9717
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:42 +0000

    regional squeeze bother chance round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
