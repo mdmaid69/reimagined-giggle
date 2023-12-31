1f626ff499867c63e506fa3164d423dbbc80a39d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:06:30 +0000

    ahead spend circle bang while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
