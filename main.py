554b3040717f0e835481f454c8881459810b12cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:06:49 +0000

    difficulty ordinary harm than foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
