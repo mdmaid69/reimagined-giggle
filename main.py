8c606b754bebb1e881ed86398d9aace5c9f51af3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:04:45 +0000

    late around she culture makeup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def log_message(message):
-        logging.info(message)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
