aa7d2ae05eee294844e05a339c124064ff128e2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:27:59 +0000

    beyond right target by winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
