c4aac4696e38b08442b37c3c69b3b8528bb423b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:50 +0000

    only art unfortunate compete organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_area_rectangle(l, w):
+        return l * w
