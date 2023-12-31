8d4055b2e5cea875fa9e5ef1aba1ba2497702940
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:03 +0000

    guess mistake trial design bend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_speed(distance, time):
+        return distance / time
