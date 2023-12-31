97cf5300e25752fd83c96a1c5276f42d41087c2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:09 +0000

    worth black content later push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import math
 def calculate_square_root(x):
         return math.sqrt(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
