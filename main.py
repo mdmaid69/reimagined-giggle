279c24d2e1598f388ca6b205053a45de1d42c61c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:21 +0000

    scale hot protection underneath name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def convert_to_octal(n):
+        return oct(n)
