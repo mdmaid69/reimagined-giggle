9a0a538bbdf5f60274d706183fc1b9704de6e2dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:29:41 +0000

    tense bridge pleasure build shall

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
