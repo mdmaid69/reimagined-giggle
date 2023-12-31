2ba34c363c2df4e442c868568e8a89f91d3e11fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:57:48 +0000

    succeed lend beneath beyond activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
