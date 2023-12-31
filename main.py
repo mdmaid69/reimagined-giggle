1b55804b07ac0d6d81a3d2cd428e8fa6e12674ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:33 +0000

    break it claim sing crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
