09c067bfe2312fc93bc7793274566a78fbe23d44
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:24 +0000

    sad collect embarrassed deaf original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
