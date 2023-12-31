251538908ea0b834ff85ed2b7b53cf273a90686c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:12:52 +0000

    level breakfast payment human month

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
