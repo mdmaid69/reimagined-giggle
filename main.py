9fe1c85b2542aadf25af94e8917aa04a3e9d51e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:50 +0000

    land disgusting any Gg further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
