2c0cd8f03419a4dbe2a96e99f3af5c913ef7f590
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:06:27 +0000

    report advantage amount where low

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
