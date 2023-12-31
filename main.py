bcbae40b3964f01cd0fc4921094ad3f02d2f3259
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:32 +0000

    bringback march possession slip bad

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-name = "Python"
-print("Hello,", name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import re
+print(re.match("h.*o", "hello world"))
