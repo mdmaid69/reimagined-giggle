a051cdf34eca0557d0969b09ab9bef97583a8aae
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:51 +0000

    spread pot guilty star close

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
