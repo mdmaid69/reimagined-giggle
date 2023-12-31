ab73eb63e3742fc7e7ecbf7d889f0fa1ab0bf5a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:16:08 +0000

    shock goon its this concern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
