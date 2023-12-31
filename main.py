1b87387586355c3d47137dc4bebd3d971bd31c90
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:20:47 +0000

    why reserve another grand scream

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
