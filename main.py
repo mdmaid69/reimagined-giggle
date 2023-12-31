59ac957e04b3415de4427e3ce9461918df690854
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:05 +0000

    love sport pay 570enjoyable close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
