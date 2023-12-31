d770a4c6649d197936bfd59d3c114fdb535d9be4
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:49:34 +0000

    which test average politics production

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_rectangle(l, w):
-        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
