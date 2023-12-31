2df69121d56f505701b262115196618e30acf835
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:10 +0000

    famous middle explanation bin left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
