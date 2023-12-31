47fe01af9a0a2a9eba8d912e2f04d4b5ed8e7655
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:15 +0000

    danger shut loose recent string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
