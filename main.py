e336695f2bfa271cbfb402f46bc618da69df9bb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:40 +0000

    soft back slice cook keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
