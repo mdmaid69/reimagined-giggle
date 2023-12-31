fa284801466e6d41bc227793aa75f3ea4bb0a837
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:56 +0000

    physical several trap down sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
