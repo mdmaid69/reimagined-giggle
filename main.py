0931c7004bf28b753e23d38a573c3bdcac318d2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:14 +0000

    across must difficulty guess disappear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
