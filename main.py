3c8f8a6850aaf8d850d91d463aa9780043eb122e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:39:11 +0000

    comfortable can real bend justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
