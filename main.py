1bc55e258c801e11e8a66817c3a6cb32d6c7b47a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:35 +0000

    mix sink call Kk book

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print(sum(range(10)))
