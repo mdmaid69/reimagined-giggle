8670fa8aa5ea249eeb9cc2db29042335186714ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:59 +0000

    obvious clear admire form fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
