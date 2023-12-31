b29ac1313f6acfb22c46483b846047797daf01bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:28:16 +0000

    who raise beyond compare think

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
