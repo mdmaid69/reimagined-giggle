d46073882c32748dff7c3198aa6f386ff020d231
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:57 +0000

    formal bet dance able computer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
