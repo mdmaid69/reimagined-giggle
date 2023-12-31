c65997af2e85edd07855eb984888c49a482fd345
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:06:28 +0000

    lend listen communicate consider way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
