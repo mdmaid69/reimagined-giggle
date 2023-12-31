a94fb317280960f69d439d8b496c6dd34f9389d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:53:35 +0000

    hate could ask record pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sys
+print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
