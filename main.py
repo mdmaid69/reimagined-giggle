169bb4acd4f8ba541777e66e6a668d694255324f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:19:49 +0000

    table life shrink sign earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
