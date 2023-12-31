9c27cb5c61714bc2e3e072ffe70d87a7dd013e36
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:56 +0000

    gate almost heavy minute direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
