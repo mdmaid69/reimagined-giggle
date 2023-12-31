52e2b6a664321fc565c38978381da680cc11a983
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:25 +0000

    would accidentally offence error juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
