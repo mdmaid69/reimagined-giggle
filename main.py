f9536882c79b1effe0ee543c52fbcd4e2509043c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:47:59 +0000

    sure mark fold feed rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
+import os
+print(os.getcwd())
