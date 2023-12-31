6eae4d2168fb61ab89eb60b4d4d97d011ab3298c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:08:37 +0000

    shall part often back spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
