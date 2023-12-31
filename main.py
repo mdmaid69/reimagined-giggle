e83ee9244b9ddca07c6c9f349f4271fbc2e9c299
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:50:11 +0000

    job trick translate uponon yes

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+print([x**2 for x in range(10)])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
