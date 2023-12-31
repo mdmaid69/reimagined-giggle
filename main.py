2640acfa117afe9071491a4d44025771d27f9e44
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:43 +0000

    cruel physical price imagine chat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_work(force, distance):
-        return force * distance
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Characters:", len(text))
