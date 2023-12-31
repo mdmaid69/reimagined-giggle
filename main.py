370174f0bfb7a34d3a9f68847fa5cd470a68a4ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:14 +0000

    black judge afterwards silent rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
