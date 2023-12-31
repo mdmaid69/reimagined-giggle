dd1d60a5c64de6184b7d048ebf5d02b5c52952c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:27:18 +0000

    backwards rush design street conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
