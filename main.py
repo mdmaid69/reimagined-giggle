8957e056a2f314f999cb17f2e145a8c4d6e82cf4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:04:20 +0000

    grow ocean centimetre Kk every

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
