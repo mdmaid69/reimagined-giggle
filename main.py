9b6dd96fd6d7909e03d429170165411baabb4903
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:53:56 +0000

    release exercise place national fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_ceiling(x):
+        return math.ceil(x)
