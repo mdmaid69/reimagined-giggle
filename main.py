a00c8fd473691499e5b41880852c4143ef7626b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:47 +0000

    goon history problem base ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+print(os.getcwd())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
