9c0801f972785731cb747bf90e2833bca556c5b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:09 +0000

    matter sick area lie attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
