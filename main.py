6695d391594e24910944836128e8de0d488d2a87
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:37:50 +0000

    picture sky birth glad aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
