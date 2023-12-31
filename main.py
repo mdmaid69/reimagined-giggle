88f0107138ab6c643dceae7c8899523535c02f1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:55:01 +0000

    house anywayanyhow bottom ignore professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def reverse_array(array):
+        array.reverse()
