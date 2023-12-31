0ae8402e632899a5536c7cc18b0f47afa7728e2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:35 +0000

    press classroom unfortunate insist once

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
