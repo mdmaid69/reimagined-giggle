44e71e1a3d2ad77a4a522a00abc19271bc7b7210
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:25:45 +0000

    sight content identify question world

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_bool(array):
+        return bool(array)
