5e4fa7d9dc0b403f1991d27c96e2ce35126b3778
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:07:43 +0000

    like secret birthday though Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
