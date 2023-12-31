3dd6dceef5646bf0a2fc883e7e27adeedcf371fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:53 +0000

    instead book immediately Ee state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
