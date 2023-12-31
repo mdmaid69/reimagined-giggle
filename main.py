4fe9260929d197923a36b68db3142fdba136f1a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:00:32 +0000

    work body exit crash sight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
