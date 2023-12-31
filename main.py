5202b0e1f42f8541f1a05389454590c82e94403b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:58 +0000

    possible cap fork gap desperate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
