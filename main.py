b88ecb2e96ee63b169e7658a8fdaa6835608aba4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:46 +0000

    belong hang admire bury passenger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
