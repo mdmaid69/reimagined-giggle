4badbbd6e8669e67a961fc3466c2ec8eb6e20ba1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:41:34 +0000

    improve star kill confusing guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def convert_to_octal(n):
+        return oct(n)
