59dd78b8e8d684898aecd7f855e4833dcd20ce5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:36:01 +0000

    dress line design trip weak

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
