77cb396d32dc88db5a0a00d4719d76f682e4b0c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:06:49 +0000

    proof order answer backwards convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
