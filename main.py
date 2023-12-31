0bfadd07402534758137c280ed9c33530d47244e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:36:57 +0000

    silent familiar flow sad bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
