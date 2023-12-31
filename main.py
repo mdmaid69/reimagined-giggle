def220301749a0db9929d6730cb249b2c3ca2d50
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:46 +0000

    terrible owe comeoff issue reason

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
