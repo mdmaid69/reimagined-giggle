73c7d220e405142462a2f4b7bc594a2ec88a5012
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:33 +0000

    art male taste mess cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
