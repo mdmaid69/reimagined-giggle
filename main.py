ab9937ebbbb6baf493d6fee7c41ba46e1e26681b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:53 +0000

    design use lonely purpose thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
