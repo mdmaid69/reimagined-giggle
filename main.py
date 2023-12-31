cd547873c718fec17fa14bec650fe800bc3944b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:03 +0000

    medium bin rush money accommodation

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def append_to_array(array, item):
+        array.append(item)
