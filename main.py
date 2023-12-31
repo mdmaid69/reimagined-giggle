316672f35c498be8e6caa5336cd6d620f587b912
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:33:23 +0000

    right increase design rely set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
