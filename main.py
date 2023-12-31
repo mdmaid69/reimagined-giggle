b99b7c475cafc6f218ce5cdb1c07ea9c459a45d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:43:53 +0000

    dig life whistle party nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
