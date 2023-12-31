42457d4979ea94b9d75685537c263abc3ae1d00b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:41 +0000

    they because turnup nonsense godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
