314802c081780330b639c80b59f314157ef8ca8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:31:01 +0000

    reliable spend part mean wave

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def subtract_numbers(x, y):
+        return x - y
