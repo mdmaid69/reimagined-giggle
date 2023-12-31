c5b6123db148de5134e09a7294133c0083ead820
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:12 +0000

    passenger play push discovery hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
