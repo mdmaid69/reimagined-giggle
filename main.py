b2bcabb0742024688f32fd829c2105f664ce59ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:42:50 +0000

    listen swing church long innocent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_array_as_str(array):
+        return str(array)
