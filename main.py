f68abc13a672c726081176499b95323c6b9fa5f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:51:17 +0000

    estimate along aim between thin

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
