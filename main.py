ea205d41b0bd8387eff076ad75cacd5f8c289252
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:25 +0000

    follow getaway possibility workout basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
