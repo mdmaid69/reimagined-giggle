2a3a53eff7e674fc672c1b64f9c7a02cd90bab9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:06 +0000

    inside effort detail rise emphasis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
