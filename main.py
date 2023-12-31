a94dfb41eeae264952e67ae773d26a6f3c9f474a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:54 +0000

    full image careless platform vision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
