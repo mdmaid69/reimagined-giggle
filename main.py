2e4ca3bd02848c0741186e51860472a416d496e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:40 +0000

    fresh image meal giveback luck

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
