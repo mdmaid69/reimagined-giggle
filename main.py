d68b8cb3fd59e43e5e13c25faeb59eabda357f4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:23:21 +0000

    interview sell sort environment everybody

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
