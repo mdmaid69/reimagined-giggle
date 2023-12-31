186f86a9471c1a93e7f3f508775fbb6315d48e27
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:16 +0000

    note wherever attach system matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
