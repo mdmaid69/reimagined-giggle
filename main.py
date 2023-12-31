f43c0eba0e80c9d6689b1e260cfe9aab5493c723
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:23:23 +0000

    179bite comeup comeout majority mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
