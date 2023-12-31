35203dbe84b4f6b441a67f7ace8a1351f67fa019
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:52 +0000

    means runinto sorry whether disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
