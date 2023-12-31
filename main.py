a188bef327ca7e379b4eeac68eba86793df30e27
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:07:58 +0000

    curious cheek long control steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
