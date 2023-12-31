b3b1b5a7a1352a1464b59a043a45f5798cb28434
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:35 +0000

    show throughout protest string probably

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
