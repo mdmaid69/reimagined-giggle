ec42709bb31eaec5767b3cc911139c7a5b6d618e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:06 +0000

    brain search disappointed lonely cold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
