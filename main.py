0d30cc85aa8cba7774198c4df9585664f6f13885
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:53:00 +0000

    breakthrough turnup modern attractive glad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
