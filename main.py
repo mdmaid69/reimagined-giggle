2c5a3630e197da321b5d3b1b5fdb0ba3ab9e3217
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:55:32 +0000

    available zero snow suit play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
