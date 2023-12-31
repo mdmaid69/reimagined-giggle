96675ee7e9d836084128d1502d051118d8a27cac
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:30 +0000

    spill design amount off battery

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
