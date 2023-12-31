e1a892bf3144d087d23d29e37c0592bea8435b9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:52:52 +0000

    describe comeback priority decide goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
