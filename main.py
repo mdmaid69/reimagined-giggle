e897f4faae3ae358c30bf6d76b88a4e0153020ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:33:04 +0000

    score water odd ignore soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def square_number(x):
-        return x**2
+print("Hello, world!")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
