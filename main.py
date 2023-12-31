81703ca86554333f41f33ad85bc02b943a8c4fc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:23:08 +0000

    goaway urgent anyone we cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
