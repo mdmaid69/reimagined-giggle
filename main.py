10bf34c59a4ddfe42e4be10065bd8ba5d4ac291e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:41:52 +0000

    soil interesting male comment agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
