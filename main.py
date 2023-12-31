4a48b7179bd7f2253ec65cdd4010edd95142a924
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:51 +0000

    first goout quality ask however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
