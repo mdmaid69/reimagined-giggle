e599130cf5f164de2d4ffe6ae80a651dd7427d5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:04:37 +0000

    mass keen hope increase profit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
