47bd41710efb1288d3486312e3241873ac197d63
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:39:47 +0000

    scare dare pleased large so

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+text = "Hello, world!"
+print("Characters:", len(text))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
