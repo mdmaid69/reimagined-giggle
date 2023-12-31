310a28be50f438cbeeb19bd8bc521476dc02b9e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:27 +0000

    lucky our high love judge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
