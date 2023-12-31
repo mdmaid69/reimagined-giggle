ba1a7c19614e7eaa2686d60674734bb955f08b12
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:23:59 +0000

    approve duty subject dress flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
