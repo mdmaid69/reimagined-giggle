7c545ffceed40bfc7a08349ba980f31e6581ee7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:11:21 +0000

    happen proud high battery or

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def clear_array(array):
+        array *= 0
