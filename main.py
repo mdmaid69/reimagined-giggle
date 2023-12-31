34522307a21ca1a93d46f8ce7c8463d258bcf45d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:56:58 +0000

    through through wait gap professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
