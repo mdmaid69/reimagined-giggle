9cbfe2c51a39033a28fac7b0eeaf5c0f904383e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:36:22 +0000

    passenger but young me slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
