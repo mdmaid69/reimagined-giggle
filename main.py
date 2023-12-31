a0f523ba4ae487eb56803bdbd36c6f3553270123
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:33:04 +0000

    form where get Yy second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
