5d7d1564b35a76f1bd54e03c581257e9bf799ae5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:24 +0000

    relief imagination female collect someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_float(array):
+        return float(array[0])
