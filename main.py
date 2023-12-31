491f57b766b94c137aff10c535868f5ece5cf286
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:02 +0000

    steal left chat form expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
