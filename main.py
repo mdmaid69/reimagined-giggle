ed6ae4e5f23d478b4631b03854fd0082dc21f208
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:08:46 +0000

    offence figure notice steal thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
