fdb7cacd0004b105ad384b7148172bf80e644fc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:40:51 +0000

    breakdown loose disappointed probably country

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
