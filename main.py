e25eebfddbbdcd263ec43060d5eb4e972bc13b38
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:14:29 +0000

    feed prevent broken ourselves speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
