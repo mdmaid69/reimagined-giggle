326e5d91945ff0fd91e388a8fad8adbb1d4b52b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:35:23 +0000

    charge mix valuable world pure

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_density(mass, volume):
+        return mass / volume
+  def reverse_list(lst):
+        return lst[::-1]
