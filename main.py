a90e01dde672751ab78925eb6107d4fa995cc29f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:47 +0000

    store fix turndown treatment fast

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
