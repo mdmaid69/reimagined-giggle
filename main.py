15d068f6a9bc426899ab4136ccaa41c1b677b65d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:53:11 +0000

    approve finger chief bringup damp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_union(list1, list2):
+        return set(list1) | set(list2)
