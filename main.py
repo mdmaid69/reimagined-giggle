5ad8149747cc0294ea8d5e822c3a81442109dc48
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:38:54 +0000

    grand imagine section getoff off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
