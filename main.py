679f1bd414d63a7874498d9658713f4a170f59ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:42 +0000

    group private gate slightly performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import sys
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
