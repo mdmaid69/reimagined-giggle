5caed47cd14aa2ef7d8f66530a80cbc9a57d6777
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:28:00 +0000

    march again method loud report

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
