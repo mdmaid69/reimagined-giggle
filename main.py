343aa84779a4301406a0443022a81fb60643e0ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:26 +0000

    entry boring minute turnon understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
