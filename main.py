e6f14ed413ecf0b7f29aa535a1750dd1f0147945
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:01:14 +0000

    belong carry so peaceful no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
