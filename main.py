59efac55ecbd0d9af07faae3f42ebf84734008ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:00:59 +0000

    within journey attempt character scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
