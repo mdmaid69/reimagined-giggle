2ca2a67765abe3bf899dea7d7266439df5343668
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:21 +0000

    Ss travel plain general destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
