babfbc971a8ab39f1d00b4301da56622ba2ee442
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:32:41 +0000

    unlucky design test poison Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
