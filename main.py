6c90a60ad06d61b60e85ea8a8ef393124b5f2013
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:42:26 +0000

    to far 566end prove interview

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import tensorflow as tf
+print(tf.__version__)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
