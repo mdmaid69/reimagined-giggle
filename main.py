ea091cde922d3510a017591bbe379b08a2b5d1fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:19 +0000

    will sign myself react wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
