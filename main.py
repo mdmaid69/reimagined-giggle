38714a2a99cd8da9a119bf2a452b2b9f92d61fd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:11 +0000

    ashamed pick invitation neither result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def get_array_buffer_info(array):
+        return array.buffer_info()
