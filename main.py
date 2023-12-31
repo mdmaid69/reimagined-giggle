491b8c42c1d5cf10c1a252a70b28ec2503c34cf8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:54 +0000

    pressure thinkthrough character head favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
