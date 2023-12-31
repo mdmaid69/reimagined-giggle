d388d06911c0adba24ba5d224b0e8395c2768d98
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:04 +0000

    prevent perfect Ee money before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
