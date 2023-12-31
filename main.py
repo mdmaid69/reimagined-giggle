bdb0c962ad33528ddef6fa58fda28ab101b095ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:37:50 +0000

    equal much bound continue that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
