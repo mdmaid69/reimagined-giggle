cb00dfb0b641d10d93bc595e59b5e6571cf93a6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:07:15 +0000

    why split solve field ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-for i in range(5):
-        print(i)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
