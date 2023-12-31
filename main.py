1e7a9509b4b2567dec85f40604dd249b21eae9d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:58:48 +0000

    solve Ff stir connect stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
