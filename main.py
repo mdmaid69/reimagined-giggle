0342eff519a4b75c2eb29c0514b3dea0c7a02fb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:41:08 +0000

    perform dance crisis our lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
