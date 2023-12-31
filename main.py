80d0d777058b006a3313d5f65058816108089672
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:12:24 +0000

    brother wash innocent translate under

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
