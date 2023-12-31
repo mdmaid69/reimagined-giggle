372e0559ae07d4fd84f04c9d4063e33d639f3743
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:29 +0000

    television sleep already fair uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
