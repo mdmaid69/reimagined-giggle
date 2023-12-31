312506c11aca15443bccbd90a6a0335a95f1e192
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:09 +0000

    name reject around free pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
