86a49d37bfc7ba60d06ef7258cf81a47f47f1a17
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:45:18 +0000

    interesting medium sort attach yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
