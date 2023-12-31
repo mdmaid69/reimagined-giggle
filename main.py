8048752e96c1d3ddc1b366f1d0d0bdd6b603a0e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:56 +0000

    easy record Gg it flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
