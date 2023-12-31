a6d1f149a9a83ca48ea61b0c40d160a57dcc73b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:50 +0000

    firm split report Nn content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
