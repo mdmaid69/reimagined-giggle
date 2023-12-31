28506c98a9afbf20a0bc4aba7530695b2f4285a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:14 +0000

    store approval prevent murder impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
