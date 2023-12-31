5d7bf0499ee928142fb9a12cd9b07566586fea5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:08 +0000

    through mouth surface leave communicate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+print(os.getcwd())
