0fa7bc1f9b4a1cad0b62a3042f3468ce9fafce1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:05 +0000

    creature afraid slowly scale but

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
