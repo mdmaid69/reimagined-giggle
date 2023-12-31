b0ffb151a221ad902f07aca8d78fffc26c7d0329
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:53:49 +0000

    Nn arm possible innocent naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import re
+print(re.match("h.*o", "hello world"))
