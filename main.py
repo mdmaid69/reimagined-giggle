c9d1d9a49618e3e5720ae9a5db458115f02e009c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:53 +0000

    object face step giveout over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def split_path(path):
+        return os.path.split(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
