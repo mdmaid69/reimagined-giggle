32258927986cd0374edbfdf7b6010e22b9c6f131
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:55:26 +0000

    human ourselves uncertain fill pinch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def find_max(lst):
-        return max(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
