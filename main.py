b66f8ff1196401c5143e77309857d1b7e0814c5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:06:04 +0000

    most exchange length bang cost

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
