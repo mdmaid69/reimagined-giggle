d1eff720913badce36d0ca7a66ebc6fab483f46b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:41:14 +0000

    tight attitude insist account music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
