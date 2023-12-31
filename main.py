3994d6f9a1fc36359b7a105f1ad125e119243121
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:09:39 +0000

    stage nose pretty remove press

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
