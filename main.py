da90fd97a350fefb8c920526967f3f71aa3b9f77
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:15 +0000

    doctor likely getoff science holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
