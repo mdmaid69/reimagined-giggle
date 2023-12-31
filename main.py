a3d754e0669f6a208fd31ec4b82d40f790015398
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:04 +0000

    single success cost correct 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
