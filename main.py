336363784f41c6eb1d1d75f2d630c005fa56abf3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:31:05 +0000

    not arrival wild cry increase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
