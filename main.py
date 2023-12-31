e992518674d4d7efe09cdba1b6e68d0ee7335660
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:58:30 +0000

    awake compete level forgive under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
