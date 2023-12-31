2046690c8c305f1b576f5f2f4dba30f0dba6a9a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:41:57 +0000

    quickly feel early where public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
