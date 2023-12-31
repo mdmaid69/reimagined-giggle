5b20f4765d70628de1258d6b7a8ed22a38a2bc5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:49:20 +0000

    conversation deal rise shame stay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
