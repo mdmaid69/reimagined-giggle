23c4f989e1ef65fdcebbf58b5e674cf505a8f7a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:03:09 +0000

    also ride gate reply among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
