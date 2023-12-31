616273864596decdb65340525ec1733fe8044866
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:21:00 +0000

    least impossible shrink such comein

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
