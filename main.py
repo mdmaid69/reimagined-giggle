182bc9376d01512b092481041e057658f3af4101
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:14 +0000

    decide mood tooth serious leave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def cube_number(x):
-        return x**3
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
