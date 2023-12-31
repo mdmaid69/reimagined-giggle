0e25801b8b1b30cd2da7006feb5d31cd3ad66976
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:20 +0000

    568energy among reply total split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
