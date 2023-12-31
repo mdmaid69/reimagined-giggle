e136a4946468aa8f25537e7b22ca1149c6d3b7e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:32:02 +0000

    factory benefit small clear lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
