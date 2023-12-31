6f2ebf1128c3209e30c21bf623728628026111cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:30 +0000

    accept race involve cruelty 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_min(lst):
-        return min(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
