67e0bdc183c0dcfbe420dbc2e0271f2eafec4c1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:45:05 +0000

    report eat poor talk music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
