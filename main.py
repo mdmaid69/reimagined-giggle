f4d574d0bf810a462335dc74ba670a69315bf15f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:33:11 +0000

    disapprove police clue translate police

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
