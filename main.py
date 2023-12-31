2ea1ae91f554363d737c9d9ed40bfc1830024d1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:21 +0000

    straight line noise petrol sing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
