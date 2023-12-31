9a10c1e3a8d944219ba3f6b264c64c9584f99582
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:52 +0000

    question door interfere safe with

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_acceleration(speed, time):
+        return speed / time
