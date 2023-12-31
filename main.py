9f31854d10db25a5b509722f164d876beb9916b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:23:47 +0000

    purpose so down stir describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
