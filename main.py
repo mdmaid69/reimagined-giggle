30eb242b3a96e1f3098b890927cddd6d99bbba12
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:39:37 +0000

    hurry develop estimate tune custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
