ec216b1afe36118aebe571300cd84315aa2b9ee2
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:03:38 +0000

    worried properly healthy meeting hit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def square_number(x):
-        return x**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
