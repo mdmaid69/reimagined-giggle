ead2fa31a91afa10edc2ba5effd8c192a8be6a9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:20 +0000

    protest pipe slight put occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_speed(distance, time):
+        return distance / time
