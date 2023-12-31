d42ed4d53c90304e01e1bb70096041f88bd54cc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:42:44 +0000

    search doubt room cough equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
