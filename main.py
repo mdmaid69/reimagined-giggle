396d270d4414b37b2a61c3187c4f17ac245e53e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:49:55 +0000

    grade joke cinema limit mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
