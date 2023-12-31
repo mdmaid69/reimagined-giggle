3277d53cad1350e2ae755811c1b8d97d8b49d725
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:22:31 +0000

    throughout scream experience gradually on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
