8ddab0a0344431d39699d78368612291420776f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:53 +0000

    charge introduction turn rent tall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
