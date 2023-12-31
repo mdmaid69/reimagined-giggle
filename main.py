c7b8b443ddab11c674ee01fb85164dd047dfac54
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:07:32 +0000

    smell top above snow sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_current_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
