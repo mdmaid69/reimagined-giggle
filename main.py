ab7f549e29c29dd41fccdcf360667dae5a8da931
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:41:14 +0000

    find distance blind arrival smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 def add_numbers(x, y):
         return x + y
