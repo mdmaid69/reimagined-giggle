4892fd1dea1b6f9eebbc0fe721a283c7c74d8d46
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:28 +0000

    past stiff thin comment recognize

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
