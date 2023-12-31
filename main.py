1d5d9621e3c9a2bb20c401618474b184de53faec
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:26 +0000

    forget recover male pleasant rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def exit_program():
+        sys.exit()
