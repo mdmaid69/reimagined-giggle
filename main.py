57d0e4a505f9223b0c846d69012b7a847586eab2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:32:07 +0000

    face special goup safety complain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def change_working_directory(path):
+        os.chdir(path)
