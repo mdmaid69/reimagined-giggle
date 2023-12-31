efeffeb0ca8dc0877124c97e5b6be0a08d60c2b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:09 +0000

    happy nearly perfect fortunate yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
