5c5cccdf5b83aa4309278512227f984b4fdff6f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:54:48 +0000

    food repair public would Qq

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
