5d3c979c07ddfc9550a2c7e24c1f92b0d72b32bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:07:18 +0000

    traffic rush earth centimetre pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def convert_to_octal(n):
+        return oct(n)
