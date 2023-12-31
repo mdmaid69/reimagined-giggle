1c21e459fe2e5c6045ece51eca2ad0f66e72fe6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:43 +0000

    miss exchange soft cool difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
