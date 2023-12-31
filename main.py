4981d56e2a56c2667eef510c675e81f8051c6e71
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:01 +0000

    blow broad friendly relationship whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
