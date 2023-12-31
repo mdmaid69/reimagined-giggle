823c89a3491ac08b9aa74bf63b6c4d04ec06bdcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:00:57 +0000

    love eye regularly scrap file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
