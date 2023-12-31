49f0577e4c2347c5be96076bc6f0f0551319024b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:51 +0000

    getup estimate clear careful reasonable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
