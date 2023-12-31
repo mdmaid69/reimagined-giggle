3ade63fcfb402e9a2f449791353bbb7d46e9b215
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:55:55 +0000

    brave across minimum holdout so

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_counter():
+        return collections.Counter()
