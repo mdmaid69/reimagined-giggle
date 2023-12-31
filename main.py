79021ce98482a321243e897b4887f7c17d2a3833
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:55 +0000

    estimate step all then truth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
