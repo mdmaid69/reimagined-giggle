2eed923f56fd5dd8e6821873725d8ac1903acb18
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:29:14 +0000

    paper people left bridge wing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
