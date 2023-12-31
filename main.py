035ca8ae7f5b43f3132e947cb9ba1085ae336f30
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:39 +0000

    moral clothes congratulations large study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_unique_words(sentence):
-        return set(sentence.split())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
