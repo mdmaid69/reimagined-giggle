ed5f78fca292404f7e72e43b63b2300a8f24128e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:22:25 +0000

    outside Yy group occasion move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
