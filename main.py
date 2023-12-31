622622965abd84821235bba5ef483760e5feb390
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:27 +0000

    score study OKorokay start warm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
