89c90629d3aebe36e02050675b12347612123e2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:21 +0000

    picture boat stage 570enjoyable I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
