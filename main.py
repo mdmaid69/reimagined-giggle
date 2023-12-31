6f6f1fd1592285f948123ddc1ced2d06dd9b5dcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:42 +0000

    warm will example buy about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
