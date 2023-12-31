7c0901c60d798a783f5e06b4edfb6ba7f5afe52a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:17 +0000

    spread case stop add have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
