8cce81811b0b44e7b52ffbf2ae2b3ceb2acd5f3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:55:58 +0000

    tape tend inside cost score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def divide_numbers(x, y):
+        return x / y
