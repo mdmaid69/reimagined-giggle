13b6a1c15611c6b638c97fdfa775c2a9b4b9d22c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:17:10 +0000

    completely finish rarely afternoon confidence

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+print(math.pi)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
