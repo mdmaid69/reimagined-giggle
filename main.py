6f667aa794baa03b53802caf3ac086ba1018a885
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:34:16 +0000

    before equal fill sleep exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
