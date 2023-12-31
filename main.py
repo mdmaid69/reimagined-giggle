10531bb708bda04218caf413d374150d2d1c0db5
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:14:49 +0000

    clear empty protection her shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
