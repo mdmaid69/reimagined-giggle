24831c550b27f9eb91f5ce4b01bab823fa121759
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:29:57 +0000

    shake outside fork bother careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
