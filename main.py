8a1af78894bf25fbc325a919c7189a9e31d99943
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:04 +0000

    people female row angry ability

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
