b1e57f06b0ceedee4b0608ba28040a7b7187c79f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:21 +0000

    deal convenient Cc we reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
