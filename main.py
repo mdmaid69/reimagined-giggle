7db7c913d9df7f36c4097c81665f5e39900ca9bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:05:51 +0000

    sink uncomfortable available 570enjoyable try

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
