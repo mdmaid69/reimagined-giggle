59ac190a572af64595c0a6d60a8c5ddf33c2ca34
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:11 +0000

    connection concerned advantage please bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
