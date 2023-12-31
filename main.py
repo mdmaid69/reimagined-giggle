a988400365fefe4145c370c6484c6da003415a82
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:26 +0000

    even themselves praise effort patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
