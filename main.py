e0dc797bd7fc1245f94f72d95a885f96b42e9ea7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:46 +0000

    left minute positive fact anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
