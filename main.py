51a0884225b1ad6e5228e4c4d412a95433520a24
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:12:49 +0000

    stamp turn change community welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
