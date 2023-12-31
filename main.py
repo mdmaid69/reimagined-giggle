f5c0a1e2ca2b932527cc2bbe7727d6c66bfa49db
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:22 +0000

    rule month protect smoke combine

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
