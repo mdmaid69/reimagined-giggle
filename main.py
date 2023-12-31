b84bd0023aa3943b0af164b0407ecf20fdd230b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:15:30 +0000

    strike window demand safe account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
