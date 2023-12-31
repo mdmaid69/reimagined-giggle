0a8eb301f3327cf74e195d6639c153ff23ac6584
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:01 +0000

    search government 570enjoyable shout 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
