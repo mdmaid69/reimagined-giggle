18278a0ef0bbcd76314a75f35b4402032118479b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:39:14 +0000

    present skin shrink bother too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
