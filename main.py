691c9645c026d256c9e7018860273793765b1675
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:16:26 +0000

    priority brown herself 566end and

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
