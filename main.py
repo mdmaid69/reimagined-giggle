a6a5521cfdd2a4a5d993750cba240952c7407597
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:22:20 +0000

    act job bound catch insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
