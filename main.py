8b9df249e1a6574ab89dcea48fdbc56fcb741e2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:17 +0000

    carry accidentally transport physical characteristic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
