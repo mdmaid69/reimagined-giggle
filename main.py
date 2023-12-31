dac93fcc47133e819609d8b6fc1a6e62e3b31920
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:22:09 +0000

    unable deliberately light give worth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
