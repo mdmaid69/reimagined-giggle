00159c88ed5fe0a385c9c381b69a580690d3aec7
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:11 +0000

    interruption favourite mark second power

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def divide_numbers(x, y):
-        return x / y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
