72019a6880d9b71a1cb5a2e3510fc88430229320
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:19 +0000

    have smell content recovery Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
