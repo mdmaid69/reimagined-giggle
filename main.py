edb5c1c132f112fe2fc758a4502e10e832231e66
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:15:36 +0000

    fire last ill notice godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
