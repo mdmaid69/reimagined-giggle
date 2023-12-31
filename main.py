9559c54143ce34acbc4d8f772c4185c63d65aed4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:05 +0000

    warm hate join return excuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def convert_to_octal(n):
+        return oct(n)
