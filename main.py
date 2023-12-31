0e24baedae6316a6101f4da656065414a07bdbd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:54 +0000

    future boil measure giveout emphasis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import json
+print(json.dumps({"name": "John", "age": 30}))
