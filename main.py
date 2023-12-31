c2b02570d89df395896ee1cc8c5bbf9137e02267
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:04:31 +0000

    dry year late sing telephone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
