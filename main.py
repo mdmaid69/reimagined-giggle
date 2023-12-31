c4aa0805e563b09a1bd53b8dc20811b807cf05ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:51 +0000

    mend prepare equal concerning check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
