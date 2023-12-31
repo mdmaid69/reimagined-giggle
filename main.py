b6ba4f553091f2ae621d527d4ad18bc34e950e43
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:55 +0000

    wave speech month slide open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
