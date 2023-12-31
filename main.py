97338270ad5fbf743037026c1ab29719ae55be18
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:04:56 +0000

    refer hide basis dry broken

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
