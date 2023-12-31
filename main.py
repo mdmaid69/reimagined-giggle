f6688282251d358eeb2a256242b423b5d87beb91
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:32:25 +0000

    dance describe ask small help

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
