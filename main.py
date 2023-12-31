f4446a1c85db33fed5cecfed461b089c5fe1cd16
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:50:26 +0000

    calloff big people sister improvement

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
