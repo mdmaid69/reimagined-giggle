8c73eb8827c4c44ac55e277fa4e4f6f542a1d6a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:38 +0000

    sting manner holdon frame anybody

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import re
+print(re.match("h.*o", "hello world"))
