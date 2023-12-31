6df530dfa1486a5f8e016dc54db83705054aa31b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:59 +0000

    count bend blame drive keepup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
