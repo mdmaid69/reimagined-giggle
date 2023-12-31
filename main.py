8e9a1cd0eb8a59b5211c8a7a9d85108f128ffd89
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:20 +0000

    section cheat free tired popular

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print([x**2 for x in range(10)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
