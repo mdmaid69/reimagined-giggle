d6017f27f012257834359e7609f18e532febb401
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:37:43 +0000

    offence company should method or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
