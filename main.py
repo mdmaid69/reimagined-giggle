e76e0b326f624c2b22d6a5f681ee69990e7cb5b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:41:57 +0000

    immediately shock swim worth agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
