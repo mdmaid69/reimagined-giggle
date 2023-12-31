3104ebdec8ab2a3e0f4b3f2615db7851b3eb793b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:15:41 +0000

    nor display aim book shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
