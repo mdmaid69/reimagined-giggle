38769241ebb230e7de595853aba36ab6c0451940
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:37 +0000

    certain up honest human myself

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
