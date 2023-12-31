d508c0216e32ead1beb11226118d97ee8132dd51
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:22:31 +0000

    ability school unable size heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
