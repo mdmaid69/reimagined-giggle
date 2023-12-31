f52731ae52ef78fd19b4c42255065a0ecbeb5b8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:06 +0000

    sign sometimes plenty sick explain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
