000714ad8b772ada1c427d494523854165a51cb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:43 +0000

    exist lucky perform partner across

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
