2122a0967aa971713850bfbda35e5ccc2b52c43d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:55:18 +0000

    equal power ignore before direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
