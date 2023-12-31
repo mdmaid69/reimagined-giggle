ecaed7108c9b26150b405266ffc6c7025983a48f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:42:55 +0000

    life position too lead platform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
