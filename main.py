ba6e5d15d89567188871504061a6404b6de75df3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:29 +0000

    side value wonder hurry shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
