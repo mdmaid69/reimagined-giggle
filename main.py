8f8461b28407f2dfab1fc66fa7dc5b9ee1c2a089
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:32:00 +0000

    dangerous plenty low church right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
