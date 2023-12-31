8bf9550fe5a5826ab7fb197b34e1972d07638840
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:31 +0000

    relative her top why shelter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
