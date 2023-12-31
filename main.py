874ad2bcb4bc850b1c5da16553d5657c512aedc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:53 +0000

    thirsty care sick separate bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import tensorflow as tf
+print(tf.__version__)
