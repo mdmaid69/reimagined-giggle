81e34a54e63b33a3a2f5d6ba00d43d75dac66d78
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:19 +0000

    phone combine urgent grow part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
