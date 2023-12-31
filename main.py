9852fb6bc9b84c5b67d6f43fcd6c4b3ab535886f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:05:24 +0000

    issue modern past about capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
