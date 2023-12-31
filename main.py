1cbe02926f9a5e5d620d41b8ddd8f838d409e811
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:53 +0000

    nowhere message throughout remove compare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
