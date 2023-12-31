bd471ecb8dc9d9132e2543969ea0465caa455923
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:55 +0000

    birthday ocean collection sense somehow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
