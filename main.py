ae18adec3ad7febd67ca68efe6a5b81408025e92
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:48:09 +0000

    outside behave set settle consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def remove_duplicates(lst):
+        return list(set(lst))
