0e82829825bcec24969a43ef40b5a92962107893
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:16:11 +0000

    expensive breathe quickly crash speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
