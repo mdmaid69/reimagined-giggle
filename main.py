44a7d51e794542dbf835a4c32b1b6813b6839613
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:12:58 +0000

    central punish visit brush practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
