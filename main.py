656ff47cd0c92b6ab095c19aec9f91a5a142f688
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:49:32 +0000

    earn outside keepaway any study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
