901cc5baeeaeb0b32f512968ff24cc19d836633f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:27:48 +0000

    taste question whatever fair trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
