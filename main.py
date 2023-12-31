4109d3d18ebff0e0d23d32e45ba678495a97cb06
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:08 +0000

    across wish wet head let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 def calculate_pe_ratio(price_per_share, eps):
         return price_per_share / eps
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
