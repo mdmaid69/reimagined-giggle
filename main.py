42a1abc3917e01f5f5320883d4b6d8101f6c61dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:20 +0000

    recovery back in 570enjoyable 564encourage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
