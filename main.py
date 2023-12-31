a2447aab490d56397403f59f99d620ff5cbf5ef6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:21 +0000

    rich university recommend break determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import numpy as np
-print(np.array([1, 2, 3]))
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
