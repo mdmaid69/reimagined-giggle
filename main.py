6162e06f34d48984993ff38475374ca97c950fb8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:17 +0000

    die promise runout beyond beside

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
