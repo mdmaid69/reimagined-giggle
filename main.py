3c03e26fff515f4f9f6bfc1a71cac5f27ce55509
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:29:45 +0000

    brown equipment start circumstance available

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
