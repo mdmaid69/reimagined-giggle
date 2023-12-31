7bc3156c115cc64bc851e4e4f02d384756130791
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:51 +0000

    disturbing own argument final may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
