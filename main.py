7899be39993fc33b135f94f9780fb0fbab0a5253
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:26 +0000

    firm Ll wire warning up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
