3e9ebf7d1e1fa87ae604681c3529e3a10eb5ff29
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:37:51 +0000

    inquire discussion broad behind enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def add_numbers(x, y):
-        return x + y
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
