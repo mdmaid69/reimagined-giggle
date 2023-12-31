7b4aaafbac4d9feadcf4f2428dce8d8a59631dd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:37 +0000

    everything of do noisy another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
