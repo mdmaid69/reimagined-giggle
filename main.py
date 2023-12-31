933316e92612e05cdc605c2a16309ceb9d32d664
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:44 +0000

    handle grateful scale fast wild

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def sort_list(lst):
+        return sorted(lst)
