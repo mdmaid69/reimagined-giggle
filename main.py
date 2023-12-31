fe556b0a8f543cdaabc5091f61e25cec15e288be
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:33:06 +0000

    yours hold study include foreign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def cube_number(x):
-        return x**3
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
