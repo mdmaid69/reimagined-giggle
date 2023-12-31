b2777bd259f4cd29abb8a49da7d63be8b2241594
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:50 +0000

    appearance hurt think storm smooth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
