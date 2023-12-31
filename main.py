ce5cec44efc0f819ac085cc0ca9ba9e12b552805
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:30 +0000

    central snow difficulty battery shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
