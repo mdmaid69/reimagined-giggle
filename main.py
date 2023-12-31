3f26788759ab20e126eccdd1764dd07c495d82d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:42 +0000

    clean accordingto passenger weather only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
