073a5436991c60d1431571e563538fdb161ee5e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:29 +0000

    description crisis quit accommodation declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
