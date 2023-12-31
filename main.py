10ccddc216cf4ec1e0a2760dfab9ae6638c0fb54
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:16:42 +0000

    they begin rare fork violent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def get_array_as_set(array):
+        return set(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
