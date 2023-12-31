6847f07ec465bc523eb454b4bcd99982965d7f50
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:37 +0000

    surprised silent natural further blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
