891177da050fda2f7adbec23a131ae0789a1d557
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:12:34 +0000

    aim anything perform quantity turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import numpy as np
-print(np.array([1, 2, 3]))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_item(array, i):
+        return array[i]
