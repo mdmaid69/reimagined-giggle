eb784e36d0708c37f3aa482150bec94373c412f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:15 +0000

    but problem entry basic probably

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
