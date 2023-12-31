4e9831d35097bca041be2aa25548536771d9eae3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:21 +0000

    across reject again press natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+import sys
+print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
