530cb0ef64837af4c6d927e8c326202c4d5f6a0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:31 +0000

    sound grade scare importance worse

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
