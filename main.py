b232c553d610090e246abf920d8c0cb35df527ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:28 +0000

    disagree deliberately attractive breakthrough health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
