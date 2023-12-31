550c1b689cf5e3c7c3a2f994da23a0ca271e004b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:40:40 +0000

    favour spoil division everything convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
