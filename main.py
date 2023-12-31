402d4da5537f889e327121fa9ef6e78374e4a4e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:25:20 +0000

    spot add 564encourage less married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
