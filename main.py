9f39874ff4beb6570b54f8b392d33cde8a6d3b8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:10:34 +0000

    do throughout instead lesson ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
