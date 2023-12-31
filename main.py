33613879ba6f946df326a7245163d704b39de111
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:08:58 +0000

    stir worried quit runaway shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
