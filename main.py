d2b71eaa14347018d85440f0f4b032d31ebda2a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:26 +0000

    drive earn by shop skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
