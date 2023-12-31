3fc89557c0a5d6531f85d493b72ade4a705c1966
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:37 +0000

    comfort port awake trial bridge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
