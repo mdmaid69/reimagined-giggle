47e7472bb2d88c94d87f036d70224e1a34ddf3a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:05 +0000

    fence clean giveup method underneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_max(numbers):
-        return max(numbers)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import tensorflow as tf
+print(tf.__version__)
