9d947977832aabd89fe02b37dc52523b83bc484b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:32 +0000

    declare over bar them rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def remove_directory(path):
+        os.rmdir(path)
