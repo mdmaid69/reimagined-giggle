a18728eb864628b6bd777f506117c8981da9d5fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:30 +0000

    attach rush belong roll similar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
