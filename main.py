9c31e6eb960a38a8812aa3be5662273990e42ec7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:46:51 +0000

    event listen rub sort breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
