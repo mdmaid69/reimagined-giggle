84ee7a49d24c581f7518b689a4e33675dfd7b258
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:27 +0000

    rubbish takeaway of children art

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
