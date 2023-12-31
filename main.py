fead9a0c59a5721ae20f747c5cdbad88064fb38c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:20 +0000

    attempt experiment unlikely appeal such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
