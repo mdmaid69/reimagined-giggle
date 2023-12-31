df3161e1067c716b307a3f74a22da040bc1fa55c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:55:57 +0000

    shape amount born prepare favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
