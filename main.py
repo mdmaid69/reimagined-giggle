c609d689da23a12c25a60c006921d58ae8cb57b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:15:16 +0000

    vote space curious weak matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def square_number(x):
-        return x**2
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
