b346ebd3475b994d5d0c5035092cd35b9dad665d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:00 +0000

    everybody grateful bit another mine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
