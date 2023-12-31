2cd1ecfdc4b9ac01a00e000d3d5dee4b10c1f0ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:37:08 +0000

    ring platform straight spring mind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
