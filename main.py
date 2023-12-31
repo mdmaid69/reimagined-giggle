a0b961a06781898bf55046de847072d67117ccc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:58 +0000

    talk door thirsty Ww march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
