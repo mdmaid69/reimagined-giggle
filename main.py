5537d0a49336798b2237a07d9792f65c4ca6050e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:40 +0000

    adjective movement disappointed will wheel

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
