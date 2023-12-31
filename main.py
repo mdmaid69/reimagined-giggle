e0915654239df7797f560b7091e0602e70ab0ca7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:14 +0000

    neither opinion frightened breakfast travel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
