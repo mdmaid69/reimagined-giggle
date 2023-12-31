e44bd98f0e5b9b0e95dea9aa1bd84946b33add4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:30:41 +0000

    takeaway forgive underneath standard father

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
