1f351c1118216d500a1a7b3f10c02716a16ad137
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:15:28 +0000

    group sad bang arm hollow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
