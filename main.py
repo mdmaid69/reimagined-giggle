ed758dd3f9071b99d594eade46c35ff1a56939a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:59 +0000

    she affect surprise education ground

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def exit_program():
+        sys.exit()
