0a7640b64954706fbebeec552ce3d65a0a0cba94
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:51 +0000

    challenge callout bin direct leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
