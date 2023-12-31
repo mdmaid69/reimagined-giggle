6b809ecca606df5adaf4a8af22630d4f6927bd36
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:17 +0000

    middle bridge city wish classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
