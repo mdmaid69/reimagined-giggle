6c70db9c845dfd3318372b4aee5fb82b61bf1f0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:50:00 +0000

    front splash culture tip turndown

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
