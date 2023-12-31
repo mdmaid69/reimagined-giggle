7fc6e8f459c3cd479ddc687fe604434c9d5c9ff3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:40:51 +0000

    bottom die duty behave free

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
