5d9ba14e0cedb284a0a9d592957b317c16529601
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:35:48 +0000

    write mistake sensible getup introduce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sys
+def exit_program():
+        sys.exit()
 import re
-def find_pattern(pattern, string):
+def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
