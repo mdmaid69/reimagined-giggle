009ee12ebfd417f48f43589ea893e7099861e4c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:08:59 +0000

    mean relax face lookafter once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
 from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print(Counter("hello world"))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
