f33368b4a8626a35460493466166c279485d147f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:07:54 +0000

    environment defend our whom foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
