60fe17ce15d1fa0f89b200653e3c2afc2b0f018b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:00:47 +0000

    zero contribute crowd with unite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
