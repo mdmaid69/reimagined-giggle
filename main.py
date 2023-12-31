10b915dfdd3bdd95a42073485f805e4f09fadba6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:40:14 +0000

    hard comfortable wipe protect famous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
