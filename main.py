e00fb066d6094956d05d920101338f45960f8f2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:52 +0000

    become getaway reason coat before

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_odd(n):
+        return n % 2 != 0
