3c5d97ace817d16f59fad953a5775c0157122327
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:20:44 +0000

    they job still accident patient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
