9d8abf8e7d39dadf07d0b0e075962ac290843e32
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:32 +0000

    undo protect party urgent switch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def divide_numbers(x, y):
+        return x / y
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
