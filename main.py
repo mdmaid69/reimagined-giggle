25a8cf60f65629fabce4343cce5ec47eb5d3dc93
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:51 +0000

    pass inform thinkthrough satisfied company

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def square_number(x):
-        return x**2
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+print(os.getcwd())
