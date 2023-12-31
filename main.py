060ae2ab819b5e20343de4d82b08b4c5c57f9ec9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:08:35 +0000

    bury opposite will tend secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+print(sys.version)
