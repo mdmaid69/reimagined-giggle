bc5678f82662fd7d65c7e0654fed0686d3342bf9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:10:41 +0000

    here weekend agreement soft with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
