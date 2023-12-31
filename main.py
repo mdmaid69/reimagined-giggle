48d3d9b17f5f6e59f4f3354bf9e602b642aeb6c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:03:12 +0000

    should strong well hide comment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
