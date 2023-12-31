ce9ad20a67e1bce4046679ac9e6166ae33aa5bbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:54:40 +0000

    surprised beneath him breakfast share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
