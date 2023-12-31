07e0b8174b81aa9a17e3a0a23932ed6222097a00
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:47 +0000

    sister equipment body admire Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
