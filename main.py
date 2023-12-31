f208f20f0a2407358eee96c1f1ef2460d3a59c29
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:20 +0000

    flat tongue tread contribute world

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perpetuity(payment, rate):
+        return payment / rate
