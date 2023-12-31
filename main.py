52d77ef8e5a687827b260aba33d40df69ec0de83
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:36:17 +0000

    health shall kind me spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
