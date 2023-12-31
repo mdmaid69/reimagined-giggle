1703d45f075c40358494fefa4dccb6b013b15eb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:55 +0000

    immediate couple swim instrument own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_work(force, distance):
+        return force * distance
+import os
+def remove_directory(path):
+        os.rmdir(path)
