3c4ca51aa07c3e619377870b1851b1675d7f19dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:09:09 +0000

    project interruption lot contrast fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+name = "Python"
+print("Hello,", name)
+import os
+print(os.getcwd())
