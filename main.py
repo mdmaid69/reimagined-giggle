78503794dd4cb983ae5bc08f220581ab5118656a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:22 +0000

    goaway lucky play test fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
