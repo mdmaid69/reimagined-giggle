4178cc74754af06a15680f231b7391b2e38e1958
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:56:34 +0000

    strike strength danger apart rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
