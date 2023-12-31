9b77540d06ff6c5373155029db152f070d8f9fcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:08 +0000

    swim public desperate exchange exactly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
