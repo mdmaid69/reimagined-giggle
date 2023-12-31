1480ab046b2c82eb16cb811c68217c5951d0780c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:18:13 +0000

    means drive respect family whoever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
