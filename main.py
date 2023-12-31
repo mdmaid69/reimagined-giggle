a71ab0f5ecffc017f0c42b79df9227909df1d1ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:25 +0000

    space pay escape throw use

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
