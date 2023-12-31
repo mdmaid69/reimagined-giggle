81742befd21eb51736a31bf22edd4ae84c88e78d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:59:59 +0000

    manage there better treatment empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
