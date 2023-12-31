695c253208134efb965ed10ac64a5839a7346762
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:02 +0000

    force pure deep pen slice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
