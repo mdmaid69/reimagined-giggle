7161a7267d8f6ba99357d3c37c9c5cc5d0fe4bbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:09:17 +0000

    Mm crash powerful inside skill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
