658ca0b5f5b5a96896a5b5bbc654f3878d1cf5cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:32:41 +0000

    beat clean goodbye cover price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
