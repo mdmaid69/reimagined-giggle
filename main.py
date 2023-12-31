47fb1aebeb8c198dc92fd9391a5ef16378db7769
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:02 +0000

    means Ee paper both become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import logging
-def log_message(message):
-        logging.info(message)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def convert_to_octal(n):
+        return oct(n)
