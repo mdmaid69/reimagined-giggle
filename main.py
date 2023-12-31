214aa281e9f9d81dc1be4c1b9affb115d5c27f98
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:54:29 +0000

    sink earth clear disturbing bear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
