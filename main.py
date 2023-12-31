7b76b48a5c7cb26832d059692434e17a1e95ac6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:17 +0000

    nerve great trade go chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
