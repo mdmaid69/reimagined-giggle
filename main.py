1e154d8c56829c0ae001e66be2f4d1312a9030ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:32:10 +0000

    label error at much chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+name = "Python"
+print("Hello,", name)
