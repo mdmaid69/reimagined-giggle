cddea8e809c70212f84891ad7a2f37d282f2fd9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:09:09 +0000

    unite curious several thin interest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def cube_number(x):
+        return x**3
+def find_unique_words(sentence):
+        return set(sentence.split())
