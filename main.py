040165ef9aeaa7ec0e0a0f9706fd302df5f389f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:49 +0000

    vanish present call objection calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
