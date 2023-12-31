dc097ca6a9e99fb4c92f3e9d5ff33eeaef12986c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:33 +0000

    door spend comment interest wash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
