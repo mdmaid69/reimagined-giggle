e0e905f5ca14e81ad209bbc6a13838fe960f6385
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:50:28 +0000

    comein pay flight life bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def find_max(numbers):
+        return max(numbers)
