c1e8335edb8343ffa1a6837cdc82aac9d8ac75ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:35 +0000

    satisfied money condition government bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
