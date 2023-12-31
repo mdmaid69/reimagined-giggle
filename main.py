a149ad28fcac5105eac2e9b525b6afa40e941b54
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:59:12 +0000

    bag experience region free none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
