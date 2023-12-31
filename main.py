6482fbd0a4932a0b8f8dc08cc0bfb2542b868383
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:31 +0000

    scare will find punishment rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-for i in range(10): print(i)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
