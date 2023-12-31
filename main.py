caa6169e544194cf5d3d4568d8505c2ce256d1c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:52:25 +0000

    could announce else possession obtain

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
