3e57b28c3420b5ddf32147f9b239f39b0c923112
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:17:00 +0000

    last wrap smoke past fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import array
-def get_string_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
