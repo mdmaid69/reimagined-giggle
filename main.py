3724082af8d39d1717463898613c9da8514374b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:44:46 +0000

    goaway watch badly band convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
