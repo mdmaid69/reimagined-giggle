78de9cd947d6fa41310f9b7b8feb86266ba76cdc
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:05 +0000

    cross cause except punish butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
