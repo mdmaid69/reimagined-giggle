90b3a3931456abcc800ae5c8d7c4ee9ea58fddec
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:08 +0000

    workon undo equal sky commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-for i in range(5):
-        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
