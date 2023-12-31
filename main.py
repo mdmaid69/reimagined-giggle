2268abe54b20d5b109227e6e0efe6d553c144677
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:05 +0000

    search somewhere thick lookafter suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
