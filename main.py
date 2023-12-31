0abace9538675888041b987e4f138bb5eb20a8cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:20:02 +0000

    goodbye connection ball callat shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def roll_die():
+        return random.randint(1, 6)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
