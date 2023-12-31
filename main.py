77b3dfe5fc56ef31d94eeb3849bbae19958632ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:54:45 +0000

    over chance star wild name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
