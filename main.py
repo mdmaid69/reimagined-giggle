36624223f967e8b7162b7fc618ab378b4029e07b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:56:08 +0000

    profit sad bridge turninto gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
