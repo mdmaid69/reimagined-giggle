40dacbef060bf498a54b492c4e4c31a65d8e4b6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:10 +0000

    uncertain usual back bus shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_bool(array):
+        return bool(array)
