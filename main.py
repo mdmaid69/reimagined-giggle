a1d5ad69479e30f7127be89d4acf30ff41cfd90f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:02 +0000

    underneath makeup across later serve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
