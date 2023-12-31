c2fedfb4f090026489e1f44ce178e67e16df12ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:46:41 +0000

    club stop shout content face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
