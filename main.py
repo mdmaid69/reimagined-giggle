c3d64903e78dd7bce1d77fb85bb592813c09b67b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:27:44 +0000

    lead claim ear track very

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
