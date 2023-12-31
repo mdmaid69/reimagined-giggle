b268fadd8dae706cdc318b26e7c34f7a4c6da991
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:24 +0000

    damage characteristic gift move smile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+name = "Python"
+print("Hello,", name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
