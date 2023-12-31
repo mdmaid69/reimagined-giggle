001d4acaa4b966814d8da1f562faab65ad6c39c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:32:53 +0000

    surface confuse shade wish hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_string_from_array(array):
+        return array.tobytes()
