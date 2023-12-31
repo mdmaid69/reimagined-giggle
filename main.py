43a438989bf0fe9c9228f9dfc585baf402d46297
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:42 +0000

    book sing bunch figure inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
