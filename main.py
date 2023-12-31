05a2ae6ec981e34388d98d86ce04c0fd413a6cbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:58:28 +0000

    rush breakinto study pity platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def sort_list(lst):
+        return sorted(lst)
