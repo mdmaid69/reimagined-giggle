8e2e8f5e3796ef051db60381266f3f3f2c00ac24
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:22:59 +0000

    workout loss instead form hand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_set(array):
-        return set(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
