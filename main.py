37aefef64b843dcb2e95cec6ba507fb2cb16f7c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:44:22 +0000

    dish between degree cut deep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import time
-def get_current_time():
-        return time.time()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def square_number(x):
+        return x**2
