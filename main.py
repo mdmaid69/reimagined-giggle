92e9fa1320cc75094dfe42ee429dce9a757c5b97
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:10:53 +0000

    murder pot silent regard step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def find_max(numbers):
+        return max(numbers)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
