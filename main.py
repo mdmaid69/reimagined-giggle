d97498ec12a4a5c6f2dbdea02cec2152b8211e59
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:48:42 +0000

    knowledge keepoff purpose increase pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
