42f2962ca1250cc5233005161ba68db48bbb1279
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:41 +0000

    loss loose geton length tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_index(array, item):
+        return array.index(item)
