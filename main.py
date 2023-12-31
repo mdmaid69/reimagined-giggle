fdb786e1c02065dd679362d87813244ef93e2f0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:19:11 +0000

    path fire instruction Tt move

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def reverse_string(s):
+        return s[::-1]
