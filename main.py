99cc71971af9b2eaa2fbee7c6422d3551988eb54
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:59:31 +0000

    scheme lock Kk towards easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
