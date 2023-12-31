467b9b4d73c6624daeadc338aea1c7bf8f7caaf6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:40 +0000

    union how brave split trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_absolute_value(x):
+        return math.fabs(x)
