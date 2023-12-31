994c386a00847c16b57af8b7db041e3e754efc5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:55:08 +0000

    fire lower board giveup right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
