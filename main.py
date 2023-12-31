dc9bc5533df4aab47e644bb1e80658274ef89530
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:55:34 +0000

    realize apologizeapologise satisfactory imagination emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_odd(n):
+        return n % 2 != 0
+import time
+def get_current_time():
+        return time.ctime()
