5302c57a934fb0edc871ea42c0ca98a22d618a03
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:30:21 +0000

    free herself geton believe history

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
