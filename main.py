c777aa145aaa874718f87ff5574f01f1c93f52bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:52:34 +0000

    proper seat desperate between fork

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
