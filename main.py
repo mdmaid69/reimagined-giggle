e3940a1c3b35cb4794edc2cf2dae1679da366422
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:15 +0000

    chase somehow condition hurry drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
