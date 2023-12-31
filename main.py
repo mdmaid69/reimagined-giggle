2c7232e7b7d3f33683a56c359e23e4d8d94a3cf5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:06 +0000

    polite confused she rich why

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
