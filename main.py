d6f75e861d857c34274a69f67a8ac4a06b6bb9a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:35:06 +0000

    bottom upwards shut urgent cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
