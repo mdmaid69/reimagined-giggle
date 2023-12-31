80f1aeab267c3f3b089d915709d34e52f760bda4
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:46:26 +0000

    equal future stare miss dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
 def create_named_tuple(name, fields):
         return collections.namedtuple(name, fields)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_typecode(array):
+        return array.typecode
