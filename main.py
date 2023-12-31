eef7c90891fc5d62324de14bd4029a9ad84fd901
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:23 +0000

    build instruction error journey spite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
