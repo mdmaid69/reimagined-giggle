7f8c7137635296e130de8f512e3185ba4537955f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:56 +0000

    impatient by completely turnon possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
