5c9ee91ad74229e9ec9d9ec15ec5f574e05520c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:23:56 +0000

    work close will someone style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
