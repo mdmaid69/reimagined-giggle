2138f5b952a5fcdaeceb8b5a37cbcebe4bbaf9b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:36:09 +0000

    so head bang fairly official

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
