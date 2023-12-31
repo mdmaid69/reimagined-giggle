79e99ba66328499c1296665cb6e7706c40110545
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:29 +0000

    difficult smooth unhappy awake trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
