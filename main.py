cfa69a07c6cb1309df14f8d0e69138c86196d0ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:24 +0000

    exciting bus clever cheque spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
