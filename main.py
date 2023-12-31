e806c7592a9f174a9d7b63afca81237ccbe59a05
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:04 +0000

    float right nowhere exaggerate identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
