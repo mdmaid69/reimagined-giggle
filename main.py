1fe339a90d58b7af8240962f96a341902a6e81b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:23 +0000

    manner rarely imagination relax sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
