2d9ef50844bcd7678f4c3cdb87d3a0d9a6cdcf83
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:05:14 +0000

    view pleasure hurt embarrassing region

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  def is_odd(n):
-        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
