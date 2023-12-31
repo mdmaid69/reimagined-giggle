37dcc3fc5a01e9d0dab0cc4b28434deef2aca558
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:10 +0000

    tune size size meanwhile fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
