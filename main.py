b5df8cca5dd908ee5edf508aa42efe39e58f25e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:03 +0000

    Hh become wing pool warn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+print(os.getcwd())
