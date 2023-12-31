a2bfd03d7da5ebcc00c886a22ee3614fda96c6d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:38:15 +0000

    score message cheat colour now

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_set(array):
-        return set(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
