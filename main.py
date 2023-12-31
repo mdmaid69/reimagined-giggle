3a163bf5fb1d61ea086ec647b76b896e1aff0b4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:38 +0000

    usually loud voice so deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import platform
+def get_python_version():
+        return platform.python_version()
