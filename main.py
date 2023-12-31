dbb47d8bede51f998a1a28248d7aad4853269b3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:00:03 +0000

    worth supply always hot apart

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
