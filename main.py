6ee57a5849ceaf03c7c93ce2d7327521fcb04725
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:21:27 +0000

    unusual lookback(on) bye breakinto course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
