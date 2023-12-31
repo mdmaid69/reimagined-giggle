469d4af78fa327d714f55f37a06f4c6e40b8058d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:25 +0000

    deliver breath start show appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
