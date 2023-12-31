325b8e1da9f59248c39c55f2a0586547579ce2e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:52:30 +0000

    gooff term together will expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
