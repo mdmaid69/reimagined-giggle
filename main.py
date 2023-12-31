fdddd238606e6f872d92ce5cb1aaff6dff43cf12
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:58 +0000

    taste little embarrassing squeeze exciting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
