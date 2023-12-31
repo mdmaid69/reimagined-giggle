b3cd5d8702fa755c30492bc3fde1dfb7354982b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:37 +0000

    deep false fall worried fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
