5aa00f118c1ea0f31368637315ad920d7d7eaebd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:07 +0000

    lie onto search scene free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
