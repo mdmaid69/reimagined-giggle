13b8496eb419e24892835e6e830466248c62578d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:37:31 +0000

    closed poison beautiful spot runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
