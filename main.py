a42a2b5658686d6b68917189d374fc34cfd8121d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:43 +0000

    occasion Dd dress who about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
