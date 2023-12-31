e3377d40b59c3598b05d8009389f4a9a6fc541d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:09:41 +0000

    shopping 566end week abroad container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_numbers(numbers):
+        return sorted(numbers)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
