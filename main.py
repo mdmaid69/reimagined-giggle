e6c44e5c14ed5fa4854cba7cacea74fa328cd87c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:28:24 +0000

    personal fond charge needle hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
