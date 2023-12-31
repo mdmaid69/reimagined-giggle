24b2f8acf3dd03ce26a05bf9f15f3b10f9f79684
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:21 +0000

    sit comein fire offence tell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
