c888651f9e261133740bbda31a9c70c2a650f309
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:36:57 +0000

    boat measure exist quantity inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def count_characters(sentence):
-        return len(sentence)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
