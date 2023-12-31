8043f46915e1edf8d37ff782fc92c0726a9b0140
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:38:14 +0000

    pollution why argument disagree eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
