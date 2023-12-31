b16469c0969ac669eb829c12c53f77050f286ec5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:24:11 +0000

    change sight cut respect usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
