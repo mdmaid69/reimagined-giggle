9d648ff4c6b4f600baf0c5bedfef87de95b93d30
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:33:34 +0000

    wake watch characteristic listenfor like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
