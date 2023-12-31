9e2aa7b2aba81328cbccabf8580785f074a2e725
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:30:15 +0000

    nervous our speech whole stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
