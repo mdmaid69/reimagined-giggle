bb1e9a67a43d6389afa7eb15e7a204f33d37f278
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:29:05 +0000

    Bb boil read could climb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
