16c434a76e77110c0299cc1e416597b358464353
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:57:28 +0000

    common upset fence black underneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
