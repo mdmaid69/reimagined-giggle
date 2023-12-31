608f9d11bb4245899b0d7f2eeaa0303dbca044f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:52:42 +0000

    target ability around principle peace

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+def sort_list(lst):
+        return sorted(lst)
