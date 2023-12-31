d59853485161da4461466aa1bf3077d39d791f06
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:30:25 +0000

    next instrument genuine thing basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
