ab11c192fb07303dc9cc1221b2703a7d99888e7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:09 +0000

    little anything catch concerned market

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
