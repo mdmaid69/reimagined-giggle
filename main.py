c05aef73722ce822cf6ec42d0e5fa0a63a448200
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:23 +0000

    upwards measure Ss comeback calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
