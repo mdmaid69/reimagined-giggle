5cf45d8482447db16e28ba56ce0ec15e10b609b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:35:22 +0000

    rest amount lookback(on) noone speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
