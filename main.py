9e8c907b8998d81156de0b8796e89483b271b76b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:32 +0000

    goaway fix consist whether tip

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
