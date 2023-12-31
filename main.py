18ffce5b2e5f5cf5752029464ae489aa1f601f8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:51:49 +0000

    interview circle responsible her approve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def reverse_string(s):
+        return s[::-1]
+import os
+print(os.getcwd())
