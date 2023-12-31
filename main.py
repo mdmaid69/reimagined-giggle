099931712fba862d32b6fab839a5860beb650b08
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:04 +0000

    agreement profit deliberate general excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
