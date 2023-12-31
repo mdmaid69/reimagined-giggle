527e2a7fe74be82175368f4cebf2fc57c6555a79
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:28 +0000

    secret moment angry several birthday

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
