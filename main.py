05eb2e3f236ee7489319aa7d4ea300bef92921a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:18 +0000

    repeat little load field activity

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
