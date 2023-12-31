215e8f2566d0653b00715eefbf35fdd089a6d65b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:18:15 +0000

    treat loud Itself excellent declare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
