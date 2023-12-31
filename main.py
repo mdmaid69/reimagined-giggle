7a4aa75e19eee4d83caef8488cce22a493539472
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:23:35 +0000

    about star spray sink peace

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
