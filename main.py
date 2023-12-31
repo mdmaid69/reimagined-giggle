e96384fb0a83fb0c847bd84e4b68bac596037f6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:16 +0000

    bye plan mess feel edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
