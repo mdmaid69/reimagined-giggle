e761374445a768ed9f4b9db64b2ab9974fd35ad2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:53 +0000

    shall somewhere flow serious live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
