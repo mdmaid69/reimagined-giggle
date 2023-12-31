884496e8848652521abbe3115d62e0211423f8b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:55 +0000

    rub throw imagine follow sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
