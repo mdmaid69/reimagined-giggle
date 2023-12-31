187e4a2236f81ac5d8590025c00ad3c256dbae4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:00:39 +0000

    skill why scared store become

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import tensorflow as tf
+print(tf.__version__)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
