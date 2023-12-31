bf460d6f22269ae07f0425d65270a09a4fed1a61
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:07:41 +0000

    lead mean keepoff wash generally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
