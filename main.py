669bc5203de540ec6f7185cd8b8c4bd81ceab89f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:38 +0000

    buy spite fine blind appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
