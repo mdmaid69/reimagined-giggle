ceafc618b88288ac7fd06cf53825e223ad02d10a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:27:54 +0000

    history sweep custom throughout today

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_absolute_value(x):
+        return math.fabs(x)
