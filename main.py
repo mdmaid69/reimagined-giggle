0f33bc5e4663d616e92ceea392e74d6de73b1df8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:02 +0000

    pour file water tough public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
