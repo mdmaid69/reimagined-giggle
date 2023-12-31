2d1d50a77c9f5429ab024a69f7bed1fb135114e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:09 +0000

    closely 179bite call wrong challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
