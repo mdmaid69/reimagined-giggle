1f8c5b3857f397c2b62fcd319bb1652d66d86e8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:23 +0000

    course kind deliver distance receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def is_even(n):
+        return n % 2 == 0
