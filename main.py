5dc469e1355db25d005133503dfbc140b78d9c94
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:33 +0000

    mail offer immediately meaning turnup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
