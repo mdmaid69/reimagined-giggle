b6908cc5c5604d6a4d532ffd4e6af4e7b9dcb7a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:59:59 +0000

    on belief 565end worse watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
