35576422ef591a0388f09f6314e8aa8ef6fd436d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:52:16 +0000

    dark but within since trip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_even(n):
+        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
