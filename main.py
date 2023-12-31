b5d0642346b5077d0671a8257aacfef963adc6eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:56:57 +0000

    deep afford test foot final

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
