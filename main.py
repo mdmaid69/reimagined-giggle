3bdfc139be4d07150f137c054691f0fab13ba7d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:32:29 +0000

    sudden either eager straight declare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
