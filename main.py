fb2b4df7f3058f51f608ac66a41ccf0f8315a5e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:10:51 +0000

    572enter gap shoulder possession bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
