d5c22dad540dad0d6c513ac13dffc42553b325ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:25 +0000

    address though hit pause study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
