4b8c3faa11f9ac9542bb2db2a5e8380162ff2e59
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:37:49 +0000

    set praise home react crowded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
