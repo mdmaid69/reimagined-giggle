1916b635311d7fa0cd70d506235d3201c685f263
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:03:04 +0000

    treat defeat flat block famous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import datetime
+print(datetime.datetime.now())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
