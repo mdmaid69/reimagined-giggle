869729e69c8b0465e9c489cc9322824eb93b8858
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:40 +0000

    crazy piece brother mail fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
