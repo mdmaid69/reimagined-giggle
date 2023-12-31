23b600f0de090f385766eaa05b567b0fa7a5aae4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:05:47 +0000

    can holiday collect declaration message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
