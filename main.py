bdc113f81f3e95b62986211bedea7c64c773a456
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:49:54 +0000

    criticize account hurry short rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
