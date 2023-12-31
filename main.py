189c36e0cef8ececfc3164a7b0b9158c98f1f141
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:24:10 +0000

    dance whichever pain talkback produce

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def is_palindrome(s):
+        return s == s[::-1]
