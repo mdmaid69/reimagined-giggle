2bfc933a8dbcb6683b203e9530424cdf1878bfde
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:16 +0000

    Ll uncomfortable target very movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
