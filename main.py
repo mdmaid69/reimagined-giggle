26a332280caeaa1915dba4e1b637f22b170ac824
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:55 +0000

    Oo pretty stiff copy point

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
