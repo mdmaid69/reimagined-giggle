6a2fa8a39cb526a78296511aa612064fffb15d10
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:22:47 +0000

    smell spin fear claim bill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
