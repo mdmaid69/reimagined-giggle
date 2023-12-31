61f590bb685e6d21eb97bd676cb9e8597765a15e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:30 +0000

    borrow announce exit nervous study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def convert_to_hex(n):
-        return hex(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import numpy as np
+print(np.array([1, 2, 3]))
