667fa081046a00788f99a60770811fdd60f2e8d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:48 +0000

    brain task autumn repeat permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
