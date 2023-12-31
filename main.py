407030c9d94cf02a6a2d978ddee1d0b7aca68556
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:22:29 +0000

    offer majority catch getout seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
