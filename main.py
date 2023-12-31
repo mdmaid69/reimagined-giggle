14ed44ed60a813cb6c1305685f84fd9ef94cca79
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:42:10 +0000

    relax Ll common learn develop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
