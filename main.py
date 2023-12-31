d8d13da09660a0ffec7747503035caba069d5d51
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:28 +0000

    possibility willing murder major brown

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
