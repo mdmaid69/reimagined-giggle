3960f66d15ae8d7d6280c497ff78962ba88e33b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:03 +0000

    hurt fold protest exercise strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
