9460fb67b388bbdac89b19360ed89b795cf5ab8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:10:14 +0000

    any male worth trial chin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
