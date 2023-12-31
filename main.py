8c977c76c3d063157e6260bb48e22b79f206aa0b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:48 +0000

    fall pattern during my giveout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
