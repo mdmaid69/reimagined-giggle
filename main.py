0bb483890fd7ca072f2ee07a77b1a549efb95829
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:49:01 +0000

    spring few pain meanwhile dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
