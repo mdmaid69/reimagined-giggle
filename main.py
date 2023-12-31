e9f569e3a2749ae90fcc76fca5b840df3011ebc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:38 +0000

    moment distant personal violence address

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
