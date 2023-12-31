c6fbc21570cfd50a73a321517db7643cf42f3c76
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:36:31 +0000

    method cheat pollution practical lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
