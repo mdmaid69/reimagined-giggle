276ab52baa0a59ebab4b7494f9840a296be0633b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:45:58 +0000

    awake opponent experiment past than

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
