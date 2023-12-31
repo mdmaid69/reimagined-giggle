91198d6b0f06bb23795a703f05287c15e8f99dc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:15:24 +0000

    exhibition dangerous empty ashamed keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
