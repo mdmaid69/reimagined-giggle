af1bd28500de22036d7787f3b3eed39ce99d4d2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:51 +0000

    learn free quickly fit concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import datetime
+print(datetime.datetime.now())
