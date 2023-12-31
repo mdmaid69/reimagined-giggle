21bacb08e5a8ae54d1a7bbbf446b167b36d65cda
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:27 +0000

    weekend rain confident whistle it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
