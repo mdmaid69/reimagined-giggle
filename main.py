edde7f87df7a42e50e128243fa6a65776e3b48ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:44:18 +0000

    march play pay offer getup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-print([x**2 for x in range(10)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
