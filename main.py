45c7ba8b2b5d54813d26a78a312c48adf57d3b2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:33 +0000

    edge lookup murder tight close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_list(array):
+        return array.tolist()
