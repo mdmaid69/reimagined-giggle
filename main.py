b23d643b7ac3f57dc07ae696a755b63e677ff76e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:43 +0000

    comedown female use tired image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
