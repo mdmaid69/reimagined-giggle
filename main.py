98452aee19ac1238e231a9c2a6bb1ec64a8cb3f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:26 +0000

    male hello only embarrassing determined

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
