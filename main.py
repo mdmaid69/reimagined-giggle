aeab64e5a9c1869b750774dc417e4c020be9d10c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:18:47 +0000

    lookup crowd steep guard climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
