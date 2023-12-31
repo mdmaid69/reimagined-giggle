8634ebb2f3d45c0e68e815b9a7873557d3fbcef8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:11:28 +0000

    drop outside mention talkoutof problem

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import tensorflow as tf
+print(tf.__version__)
