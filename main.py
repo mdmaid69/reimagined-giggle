05d1f3707573e246e93645f05c69becb8d088b57
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:52:58 +0000

    easy round draw lucky travel

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
