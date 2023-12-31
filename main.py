720933d33812a9c8abe4957055037b7409d52b7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:08:42 +0000

    combine separate bear rich phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
