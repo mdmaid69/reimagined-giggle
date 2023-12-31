8d3c5fd7a14588523721e21af0b91728a56e09f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:11 +0000

    dry string empty comment art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
