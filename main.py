bf77ed7767d97be66b261b0328abde8a3fc1cd19
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:23:22 +0000

    happen division thirsty also kick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
