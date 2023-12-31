bdd16a51d4af022349d7a9fb28aea0cc1d58bd9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:37 +0000

    arrival mean wide uncomfortable prepare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
