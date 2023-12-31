7e264f0f930b1c6582d6a46dd04cfe4f38ff32e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:30:19 +0000

    wild test print disappointed tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
