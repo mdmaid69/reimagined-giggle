5cafb3debc41402c8ceeff42ffb408d3b05c3a2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:18 +0000

    possibility pick slice advice crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
