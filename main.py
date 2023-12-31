b7b14d2f7a9edd6d1c8ab5287aa1e432f2fffad9
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:30:47 +0000

    unusual interested underneath friend private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
