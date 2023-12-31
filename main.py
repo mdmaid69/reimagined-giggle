d4b3bc306acdac5f53268e171138159ee5ca11f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:47:43 +0000

    treat ear land curious wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
