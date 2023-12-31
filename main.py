bb16b36be2af12bea060dbba53d217941379e6e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:17:48 +0000

    flow tidy off flow within

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
