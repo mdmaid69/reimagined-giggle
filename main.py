cd7bdf280d380307dcb77992284c6f98fa8204df
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:18 +0000

    that only rescue increase smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
