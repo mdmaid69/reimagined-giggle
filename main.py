4a84bc1d38deb73d7da02e182b7288854f17017b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:58 +0000

    crash smell lock square born

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
