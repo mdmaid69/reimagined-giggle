c45fa96e985d8c6cfb7bbaca0040ace598b184fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:24:26 +0000

    practice breakin exist whisper also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
