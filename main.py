546ff14bb92b429b1b9a2ad61ff78d00ae2d65a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:36:13 +0000

    giveback rent swing sick paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def count_elements(lst):
+        return len(lst)
