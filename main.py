602dacb48681ad06d3aadd1a2cb24733fb905238
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:44 +0000

    Ii tradition communication stick wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
