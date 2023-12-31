f133cf5d7eb0482781605ec2d901ccec9f01cee7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:06:43 +0000

    boring direction occur long active

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
