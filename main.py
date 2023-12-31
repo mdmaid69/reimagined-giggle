47b3d487936fa2adedc148f3981ed2bbdf22989e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:39:29 +0000

    one gift hungry pool lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
