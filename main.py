23bb0dc4f9d454aed57504d0a06f9be703613baa
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:16:27 +0000

    fast bridge extremely argue worse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def find_min(numbers):
-        return min(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
