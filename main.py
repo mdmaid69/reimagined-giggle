3ad8039dfb4fb83823518e1acb528615f27f1f6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:17 +0000

    slice winter typical favourite out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
