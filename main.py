9f7c3fa322396249db7873d37fdd5b2747f11a95
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:14:59 +0000

    medium rather prove breakdown head

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
