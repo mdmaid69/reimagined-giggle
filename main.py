fd500af5f4938a71ca725fa2e5d0a5a0669a0b11
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:48:04 +0000

    rain before round look line

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
