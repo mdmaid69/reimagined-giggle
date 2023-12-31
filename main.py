a7427965a7f5b87266daf803035f91c29f292f28
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:56:57 +0000

    neither drink thanks push chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
