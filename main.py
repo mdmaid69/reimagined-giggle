8266376137911a1d9245480ff2e133aa9e1675a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:00 +0000

    possibly port basic allow week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def count_words(sentence):
+        return len(sentence.split())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
