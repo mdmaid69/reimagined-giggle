f4b2a30027c03a19059664afa4c715934169f4f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:22:40 +0000

    plain 573entertainment character slip complain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
