4103fd8ac6bdad23751585246c45c1b767be8df2
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:42 +0000

    reply turnover narrow birthday air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
