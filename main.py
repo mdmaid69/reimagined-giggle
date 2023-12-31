aeb678b1ffeb797225d444422c86fba549e2c804
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:46:49 +0000

    fear describe broad love miss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def cube_number(x):
-        return x**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
