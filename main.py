6c4259807f419d73ddccc4683fa131b119a700b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:37 +0000

    all shopping feel across eager

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_set(array):
+        return set(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
