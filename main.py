78200d3cffffe841d06df91bf0d0a58d43e91f30
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:35 +0000

    may compare position form comeround

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
