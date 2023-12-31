6b40ed03106cc08c9f059f83a0236523d0068437
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:32 +0000

    between convinced comment collection miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
