cc7ff8ba546e1835e401b5e68cfe548aa6c3ed3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:52:49 +0000

    little hold destroy hour comeround

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_min(numbers):
-        return min(numbers)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
