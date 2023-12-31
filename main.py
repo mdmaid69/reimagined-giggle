a61eab0605dcbe377a3308d059358789635ceb16
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:36 +0000

    refer command perform bringforward appear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
