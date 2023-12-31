96d856a092b32aae66d6c36f06bb3d2ca16ae604
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:11 +0000

    makeinto your mine opposite pretty

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
