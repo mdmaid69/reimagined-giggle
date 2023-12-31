43033baaed402d3a79b382abeeddbfbf0f346d4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:13:40 +0000

    stretch reduce soon within arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
