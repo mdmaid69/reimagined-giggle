572aaa6329b56cb422cb08543506ed07d942e342
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:50:32 +0000

    since possibility remain strict find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def convert_to_binary(n):
+        return bin(n)
