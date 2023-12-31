1ca985e28316a0903c34812f7b107b6e43398453
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:37:48 +0000

    smoke love laugh concentrate fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
