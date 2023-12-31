3d43c542ff8d218a0c49b06c193c513e54d09133
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:20:09 +0000

    curious young feature taste example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
