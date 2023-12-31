ca317c41abc2e9471a145d210757aed777f5a8e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:17 +0000

    point appear pupil shadow hope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
