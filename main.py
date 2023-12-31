ebefe5e927845c3902ae4731c2f30fb563b97e5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:16:45 +0000

    Pp callup downstairs custom gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
