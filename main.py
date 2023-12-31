f3e3a91f3a7089c016ffb584d6f54215616c9fc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:36:51 +0000

    silent party block there tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_sine(x):
+        return math.sin(x)
