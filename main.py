ad8228d2b996d90aa6fe1c400a91f68f3f8892cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:28 +0000

    comeover bet steam concentrate spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
