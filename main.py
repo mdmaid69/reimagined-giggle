12dbc9afe56107181283751955f592ddeddde534
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:23:02 +0000

    stop message your stop dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
