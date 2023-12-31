ebbcd70e68980be27ca3adcb5cf1a154609042e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:13 +0000

    trick fail sharp search himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(a, b):
-        return a + b
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+print(math.pi)
