8f3cedc780d3db05f7becd5bce2c9f89b2795465
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:08:45 +0000

    wave workout weekend breakin reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
