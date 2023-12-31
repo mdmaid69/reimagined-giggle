2021e5d952a0e3f392a3aab529d86479bd3170a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:24:55 +0000

    worried stir wait stretch relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
