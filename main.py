9d053a62f264914430f66292c141b141232fa67d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:03:59 +0000

    young stand bringdown charge likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
