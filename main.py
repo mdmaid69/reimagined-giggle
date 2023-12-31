0274622b709cfe414a910982d6d32c42c2e9fd8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:03 +0000

    responsibility cum transport friendly swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
