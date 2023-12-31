7d027f4af43e07f9153fc7961725b1668dec420a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:24:49 +0000

    complain translate raise emphasize form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def count_elements(lst):
+        return len(lst)
