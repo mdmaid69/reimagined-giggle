b08dbd351e248e483c51f78304d9e73c96703d8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:10:20 +0000

    ignore congratulate attempt contribute comeoff

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_volume(length, width, height):
+        return length * width * height
