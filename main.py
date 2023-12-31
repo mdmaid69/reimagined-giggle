11ac60575c9712a0d6e0a3063aed03cf9a634a21
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:50 +0000

    hello spring genuine style spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
