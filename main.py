47bfc1cbae2ae755721f70e3e9aa36d1896d6dd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:34 +0000

    office size travel breakdown average

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import os
+print(os.getcwd())
+for i in range(5):
+        print(i)
