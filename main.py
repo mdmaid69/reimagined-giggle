b9ddae2ba1083ce7ed83c34e950854898324e337
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:52 +0000

    defeat careful bit suggest unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
