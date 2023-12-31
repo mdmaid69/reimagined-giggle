7efe13aa67a8380fb75afb1fd2190699464d3ea1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:19 +0000

    fit mainly final quick workupto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
