b1be35d87d5e780a46c5cf69d6988f39b480e25e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:29 +0000

    radio feature mad argue invitation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
