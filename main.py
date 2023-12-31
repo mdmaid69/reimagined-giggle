dce5252022b2f1d24e2417a5b6d055180eceeeb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:45 +0000

    worst dare expect unfortunate middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
