171a2820314206d4b7aa1fd17dcef9610e7aab0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:52:24 +0000

    worth lookup tap share accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def get_array_as_float(array):
+        return float(array[0])
