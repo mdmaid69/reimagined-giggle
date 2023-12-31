66121a2780ead6bc257e7f511bad27688eb2c660
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:21:13 +0000

    keep walk rich geton mood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
