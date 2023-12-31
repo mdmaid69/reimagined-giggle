25fadb693cb39c5fff5a6a3304d7a199ea21b71e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:24:10 +0000

    whether time loss hate little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_volume(length, width, height):
-        return length * width * height
+import random
+def roll_die():
+        return random.randint(1, 6)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
