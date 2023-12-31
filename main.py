969518b4af7a229f0ab8f3d6938afaaa6375cae5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:06 +0000

    top before enthusiastic me record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
