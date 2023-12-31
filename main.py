fdd3126fc2ed3af1bc83a0ad437ef8da16e8e1c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:19:11 +0000

    giveout pretty cover cut some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
