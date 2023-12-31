f05083a481e167d0436bafb3c38e899b90d499ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:40:01 +0000

    tall sky pair obvious distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def add_numbers(x, y):
-        return x + y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def is_palindrome(s):
+        return s == s[::-1]
