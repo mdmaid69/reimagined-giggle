b7dabbd82692ff561c91848321756a446e86be12
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:27 +0000

    soil drawing special comeout consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
