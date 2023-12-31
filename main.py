51088b77ce647f80a5313b32298b8ba6dcd5b9a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:18:03 +0000

    birth equal detail old as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
