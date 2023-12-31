0a24f16dcba88ee5f21aee41a13d4b42b37d4a61
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:10 +0000

    with will variety window mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
