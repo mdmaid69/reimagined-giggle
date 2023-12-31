ebe9084efbe1bbe927cb577df40121e06f715dcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:16 +0000

    natural time eye anybody relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
 import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def copy_file(src, dst):
+        shutil.copy(src, dst)
