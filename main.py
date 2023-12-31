3abf71fe1a355977158cee43328c917dd7aaaf79
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:06 +0000

    comein impossible interruption listenfor born

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-for i in range(5):
-        print(i)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
