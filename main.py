57cfce8643019c4b820c350e321b756fa22ade51
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:11 +0000

    expect brown software lead extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import random
-print(random.randint(0, 100))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
