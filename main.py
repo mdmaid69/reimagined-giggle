6b003bf284140c81e7f3a16876d15bf1d46d9f9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:25:20 +0000

    apparently move allow print excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
