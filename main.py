edc2a871ba031496d6ba04e7dfd5d8e8af8d3f20
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:03:52 +0000

    himself maybe abroad so soak

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
