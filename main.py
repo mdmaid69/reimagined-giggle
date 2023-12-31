6939d578867260c550819dd17cd6edb9a5edc37f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:31:04 +0000

    trousers defeat war walk lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
