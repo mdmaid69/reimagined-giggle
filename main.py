40a8ade8a55a64e02f98288b28481783e414f95f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:07 +0000

    crazy wrong suit world country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
