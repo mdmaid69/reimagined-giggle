c85c128cb43f405093efddef0acbdc8749d9d139
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:06:16 +0000

    makeup stare broken test fetch

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
