d044dff282722b772f0f6ddc8f582f187f603386
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:37 +0000

    dangerous spend curious tip style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
