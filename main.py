598ed419881c2604659acf96e4da383ce0e2730c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:07:52 +0000

    content front influence drop much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
