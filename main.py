b6b5f43ad0ace224d5609952b580d25caaf8f336
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:56:20 +0000

    hollow quiet sink relationship repair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
