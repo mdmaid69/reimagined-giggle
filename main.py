3b77a183f5137adc76ff59c879ea135218ebdd2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:37 +0000

    connection rank downstairs admire cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
