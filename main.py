8468d7df08fb0070ce709859bcd2e1812af611e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:52 +0000

    kind earn develop once please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
