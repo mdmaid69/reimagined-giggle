15168caae31d5dd143a61fdf9e4236807ee8801e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:00 +0000

    admire ear connected back age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
