4948f131a943f1aaa3e63e01782c196eb51d973b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:02:01 +0000

    at area punish possible report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import time
+def get_current_time():
+        return time.time()
