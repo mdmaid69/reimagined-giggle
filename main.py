5b3a01a2efc6a9a5140efff68d4f7d283d1b4b1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:58:55 +0000

    shame speak large object mess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
