0b9363bee41d7ca21986fe4cee8512b653d9ca58
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:34 +0000

    thick frighten without into deserve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
