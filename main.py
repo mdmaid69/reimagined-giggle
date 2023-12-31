6424d920ac89d8f1bd34e6aee0ed6204aafc7e78
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:29 +0000

    have giveback possession shoot loud

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
