acd7fa3a6d0fadc3f08ffff786d7043bcb32ef6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:42 +0000

    address substance shallow exhibition rate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
