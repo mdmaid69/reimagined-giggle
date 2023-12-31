2c3c8649cfbecc93da65db831ca46233abe6c951
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:14 +0000

    catch cut rain pipe friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
