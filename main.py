7d3afa20bdae95555fd25cf46f719e340acfac68
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:44 +0000

    year over murder fire charge

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print([x**2 for x in range(10)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
