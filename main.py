40fa963e8b7347be53db1e468d6a770a19b61332
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:44:57 +0000

    other command slow believe Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
