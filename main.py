28a3e9687a23183ff2d41d6c25d3ed63495763ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:09 +0000

    interest case muscle lead protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
