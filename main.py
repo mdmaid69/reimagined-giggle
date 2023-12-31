8132356f0132b4d3c12cb9d43ca3ec7a57191610
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:04 +0000

    gate Jj stick eat relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_acceleration(speed, time):
+        return speed / time
