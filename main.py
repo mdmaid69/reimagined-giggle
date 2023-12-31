0802a85a44febdb90510693dcbe41a45e487015c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:27 +0000

    listen news unable recent satisfied

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
