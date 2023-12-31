78930f19f162d8e399a0479653650f20da2ddc9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:02:23 +0000

    open whenever field glad telephone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
