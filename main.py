92a8d39f13da8689a0fd48efbdee01c577d30a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:18:50 +0000

    flood maybe miss voice land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
