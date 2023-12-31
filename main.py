5cc902e902a3ad4215f115efe0a25676f5661aab
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:45:41 +0000

    world live interrupt theirs cost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
