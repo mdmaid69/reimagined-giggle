240b0e60ba57179250cfcafba087eb0d08d176f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:14 +0000

    agreement born survive fairly talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
