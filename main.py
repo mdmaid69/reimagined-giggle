988f7007411484b22e59d4a557e1b56e2282596d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:12 +0000

    down cover mark break occasional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-print(sys.version)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def sort_list(lst):
+        return sorted(lst)
