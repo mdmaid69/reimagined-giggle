02b67fc9727d3c81e00f740a3c47e9275801cca6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:22:56 +0000

    male worried street star Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
