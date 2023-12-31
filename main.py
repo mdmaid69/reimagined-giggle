58c2e5cd405e540448b79da702db3d14476d233c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:05:04 +0000

    pot expensive band unable mess

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def convert_to_octal(n):
+        return oct(n)
