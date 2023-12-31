a873c2d3eb82357059f631930bcb529ec2bd760d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:39 +0000

    whatever gooff influence brush somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
