52a065d0f63e0405232af04d48773f788fde2e58
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:38 +0000

    real care house lookafter near

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
