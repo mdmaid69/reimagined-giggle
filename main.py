93753706990b6f6508ac0f8b036750cc64de27ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:55:15 +0000

    swing instruction organize calloff violence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
