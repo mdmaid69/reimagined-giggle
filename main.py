4c64a8ee3703e8d594cc45bff7a0fa25244e05c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:54 +0000

    present result suggest borrow degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
