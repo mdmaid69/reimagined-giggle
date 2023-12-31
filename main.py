0f63bc0a5da51951c657a7d101408864237f4fed
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:33:26 +0000

    quickly goup legal unlucky lookback(on)

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
