3fbb2020608b3726005ba8c983683f4af95043d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:09 +0000

    catch plan refer wrap special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
