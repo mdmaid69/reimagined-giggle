db1367a908cc12220f7d025f4970140cb417decf
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:53:29 +0000

    police uncertain inquire building confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
