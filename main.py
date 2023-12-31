ba04ec5f573ea39d51f8a65b14510c2108e13b91
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:33:13 +0000

    nerve emotion average operate view

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
