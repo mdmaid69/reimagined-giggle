d557ccbdef73fbd9a75fea7e0f50eb4dc1a770b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:03:42 +0000

    through type temper someone snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
