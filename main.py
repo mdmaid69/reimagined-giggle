35a49ee61548c636d4777211e2b3cc32a383a794
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:14 +0000

    replace flood thank acceptable journey

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
