d9271f54f31589023466ba81d1af0442c1a81f29
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:32:22 +0000

    breakfast check responsible of transport

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
