9f33bec37b2c426e8149448a1e6807a1d622cd82
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:50 +0000

    straight choose union light except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
