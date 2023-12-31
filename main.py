a8af8e5084ac45843137eaf7d2cf9cecbf6714b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:03:45 +0000

    pronounce unpleasant few tired also

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def remove_duplicates(lst):
+        return list(set(lst))
