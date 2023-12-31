a0cdcfc30b5f521e6dc9117019c3b81fdf520d91
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:51 +0000

    wide speech pipe speech there

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def remove_duplicates(lst):
+        return list(set(lst))
+def greet(name):
+        print(f"Hello, {name}!")
