51fc3a72bbb62bd293705437dd2c50940742db54
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:35:17 +0000

    so everything throw bridge century

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+name = "Python"
+print("Hello,", name)
+  def add_numbers(x, y):
+        return x + y
