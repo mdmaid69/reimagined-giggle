5474a7040340fb643df3f60e9675a554a14d5876
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:49 +0000

    still patience tell stop seat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_current_directory():
+        return os.getcwd()
