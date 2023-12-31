d511aac4f08fe77090e544f2b631aa330fa39c42
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:46:04 +0000

    tidy length radio polite organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
