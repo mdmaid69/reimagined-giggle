c051c719454e03baba3b3d965596331107ec8975
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:58 +0000

    plan mistake train dream ordinary

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pressure(force, area):
-        return force / area
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
