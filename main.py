c907a7a92641a3e1edf81f42493db76a3afca239
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:32:19 +0000

    careful suit exist safety confuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
