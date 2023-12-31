5610652c49573b17ef01d81670816584f1fa694a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:51 +0000

    few himself blind least before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
