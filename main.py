ace84d5ebd50566679b9fab1073a0f1ebbf30b2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:38:49 +0000

    ground break back anywhere lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
