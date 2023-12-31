5b427b2ce0e1300f0fb7fe067f19e99680bb9ab3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:36:22 +0000

    murder pollution bringforward about cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
