4cf3c093e17953a6fe7b8510480b880c9bd5a2f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:33 +0000

    giveback jump 568energy rid clothes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
