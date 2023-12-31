fa77953ec79fba7a60dab00bea744d342573feb8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:41 +0000

    valuable squash objection careless preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_priority_queue():
+        return collections.deque()
 import math
 def calculate_arc_cosine(x):
         return math.acos(x)
