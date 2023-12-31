1ac516c185f42a80499bed6b435b1e2befe554f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:50 +0000

    objection rid mark chat herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
