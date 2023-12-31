606e967b6aa5121f1fb78b50a4be8e2423705395
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:50 +0000

    university your meaning forgive except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
