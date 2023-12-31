c2d6c15e71e83700acd3a77d7ed6d0abba4e2b8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:32:23 +0000

    big trip eye method task

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-def find_max(lst):
-        return max(lst)
+print(sum(range(10)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
