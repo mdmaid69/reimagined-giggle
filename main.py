b2f93c480477d4a43ddab220121724331a9fca82
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:20 +0000

    complain wonder consist lift month

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
