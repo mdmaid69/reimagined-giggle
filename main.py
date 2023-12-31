aaaf6ab6b212bfefdd16fcc613e381e3afe30041
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:50:04 +0000

    middle afterwards relationship crowded damp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
