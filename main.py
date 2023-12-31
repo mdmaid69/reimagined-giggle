228113e50a180580f0740f90e8c182a981f30cff
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:35 +0000

    wall false afterwards land agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
