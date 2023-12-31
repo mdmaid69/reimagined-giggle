86086369e12291c91ef52a13b4d1b8cc5b493444
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:03:48 +0000

    shake finger dish breakin crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
