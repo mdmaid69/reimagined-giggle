41ae4d562ca6ba09fc47fdecf68c932b21c66c72
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:43:07 +0000

    before before set collection turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
