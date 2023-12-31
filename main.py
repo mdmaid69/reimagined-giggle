ec8da3b5da0b017d3932f432e192435b751c9f2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:48 +0000

    temper payment stop deliberately divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
