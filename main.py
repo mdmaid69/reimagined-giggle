4078e74a076216228e6eea355784d8d46d99f1d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:56 +0000

    case service fix think generally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
