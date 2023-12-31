6b4e37f432d260ac4b5dc5356a0eee2dce483d40
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:01 +0000

    listen spoil tone high notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_date():
+        return datetime.date.today()
