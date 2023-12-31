765926488f4273621564aa20262810e3c09fff4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:59 +0000

    tall someone immediate thorough talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.ctime()
