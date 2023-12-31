cbeeaeb62a4bbb2a3220d5d917666bc7ff9c7e14
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:06:03 +0000

    paper business feed steal in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_density(mass, volume):
+        return mass / volume
