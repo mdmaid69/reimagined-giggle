e7b511753cb11fb6aebd9063f059dab27e6c1642
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:04 +0000

    call teach equal state dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
