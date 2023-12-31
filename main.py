d1cf2311503cba07a42c9ad6c57be4bd12a9abf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:29 +0000

    pity turn neither brief direct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
