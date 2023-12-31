106f1fa08c29a1c688fa304b4915e8e1548de3e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:50 +0000

    real related discover could party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
