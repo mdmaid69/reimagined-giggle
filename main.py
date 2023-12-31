a229c6018dae64a56b50235b3e137e163d685615
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:46:06 +0000

    though slightly along confusing complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
