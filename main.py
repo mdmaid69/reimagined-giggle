a3e5733337ec126be82ec9b21fd95747cdbe3228
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:01:50 +0000

    connection combination declare ilovedicks bar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
