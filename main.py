48985efa3afa5bacf72e1fd9feb7bc691f4dcc87
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:13:35 +0000

    care zero succeed door undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def convert_array_to_bytes(array):
+def get_bytes_from_array(array):
         return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
