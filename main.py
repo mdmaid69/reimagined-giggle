b33ee8389e43dd819d94a9c13e3fb5fe1179b055
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:57 +0000

    getaway failure equal ready wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
