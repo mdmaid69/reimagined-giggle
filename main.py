77b47b6015fc4b2f8aa2c8816d60a5434c3c4e87
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:32:18 +0000

    scale show each recovery Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
