3f6f32238c44d1480440e183ba78e6f4ceb833af
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:05:56 +0000

    squeeze use face square official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
