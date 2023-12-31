7114cbc330e13df4f36958d63c6d5299a834140f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:16 +0000

    mind recover date reply here

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_density(mass, volume):
+        return mass / volume
