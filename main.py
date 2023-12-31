6b01eb28e400fc3dd839bfa7afcf063dce3ec935
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:15 +0000

    buy loud freedom discussion air

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
