6b01a7b157267deda7800dd9b3169fe8abb7e108
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:44 +0000

    lookaround fair shrink summer fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
