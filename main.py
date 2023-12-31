6c8c25a9c18170b72ecb295980989c3b5708115a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:43:18 +0000

    Yy they part opponent drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
