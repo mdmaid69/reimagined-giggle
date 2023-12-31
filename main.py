a121622546df8264e54a7d671bb54d7ff1ffd61f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:07 +0000

    either dear identify love design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
