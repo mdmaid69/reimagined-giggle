85d3933d7235ed35b99aa730f40e4e01a253aa84
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:31 +0000

    model favour band transport wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
