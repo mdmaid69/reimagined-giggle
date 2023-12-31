90074f733c45b5ed80d0e3380bac916bbe602bb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:05 +0000

    bringback complicated drop look dull

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
