73655a6e1e878546f6316dcd0d7ba30ccc81593f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:55 +0000

    check view fond maintain hope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
