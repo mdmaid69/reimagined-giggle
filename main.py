c9708a28a33b9d5fc62414f756fd17b98d2f5fbd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:24:09 +0000

    home culture suit goodbye warn

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
