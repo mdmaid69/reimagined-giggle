61f1726f7968784933eed853dc8749c2fcc85a95
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:27:50 +0000

    fault mysterious result doctor guide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
