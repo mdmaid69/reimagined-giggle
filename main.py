bd55cd053627fe1667c10f0f1a3494bc7de49411
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:31 +0000

    destroy deliver usually basically birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
