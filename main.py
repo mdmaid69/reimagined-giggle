64fed784a365d6a4972148a11e6d6a7ede11a0aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:07 +0000

    absolutely spring comeup temper entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
