9f4cbc33eb487a9cde4692a579c0c24fd3ff85bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:45 +0000

    free approve matter cost shop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
