340a1bbed55f48f0078499eacf625bbb6f9bc6bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:02:59 +0000

    any basis wish goback(to) affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
