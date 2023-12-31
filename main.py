d6970d346a2e62d52379da70bd85c6adef8e4640
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:09:25 +0000

    can key bunch off thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
