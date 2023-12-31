a88267c0e63405bafbd7fa74c90f92c290c6f0ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:33:15 +0000

    talkoutof typical except suit taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
