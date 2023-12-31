3688713f7ce7c5099ad7234b7cac612dd30b2cd9
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:24 +0000

    undo loan minimum expression beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
