dff617aedb433d7343c9d5dbe018db9690133413
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:17:15 +0000

    yesterday ready know Yy regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
