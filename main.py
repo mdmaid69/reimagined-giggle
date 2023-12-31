befab809094b7c58feb7016ee66f004b32dccf2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:10:03 +0000

    backwards mouth foot guard by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def is_even(n):
+        return n % 2 == 0
