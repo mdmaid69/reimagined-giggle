ecffe3e20367f298e3d85685562196341e1e3aa6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:36:55 +0000

    572enter pocket thinkthrough inside order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
