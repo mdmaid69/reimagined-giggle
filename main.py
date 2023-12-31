e5785a2c74598afaaa08bf3e2d2bbbf2d2cac18f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:35 +0000

    by majority weekend convinced again

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
