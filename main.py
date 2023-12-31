98ba4454d4cd61a4685eaf84bdea163a2391af2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:56:12 +0000

    that suit dress software miss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
