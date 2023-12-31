6ac912bde1d5cd2770412a2a10b24ef7717946d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:57:06 +0000

    responsible to freeze careless exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
