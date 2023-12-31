90e6f2ab4c05c680a67d4bb06c2f1d3af18e43db
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:30:24 +0000

    change Pp trick disappear why

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
