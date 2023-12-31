64c6154bcc5341fd4b41c47b9e3b00c9b99e8cbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:05 +0000

    hide smell impressed roll side

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
