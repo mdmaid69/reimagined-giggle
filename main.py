a97cc270b08f04104fb88b9d673eac82fda39cc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:26 +0000

    our sure damage position angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
