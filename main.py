200ab3d08b861b2b1f89149963c62bf97b1fb522
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:10 +0000

    wash except polite false mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
