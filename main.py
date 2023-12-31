79a226050eb9dcb38cc79248aa4c329b08c3f62d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:48 +0000

    well race risk vanish middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_repr(array):
+        return repr(array)
