cc87fd26d79f140b10009e1ddaf6d5f7b99929da
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:47:10 +0000

    cut emergency somewhere recommend everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
