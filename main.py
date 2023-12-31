4f80ff95f8b9ad6660fd8f1003812ef52b7d83de
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:41 +0000

    about charge goup someone really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
