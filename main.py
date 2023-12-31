7d907b3339ee049b2e0af1b662eefcafabf27109
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:33:19 +0000

    turnback card which over range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
