8a02f30fdf726e869287d882895806dedc5a7f68
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:23:21 +0000

    estimate fire wealth cover warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def square_number(x):
-        return x**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
