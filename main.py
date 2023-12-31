2e4139aecc59d300cee81e613bf5a61450a91c4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:57 +0000

    lead family ticket congratulate collect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
