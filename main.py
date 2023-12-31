c5bd9ed1c606d91f5472aa0e38b5f3da05bfaa55
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:13:16 +0000

    compete flavour view impatient follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
