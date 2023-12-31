eac021ad6b3d6c7165da8205f0ee15af20fc93bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:44:16 +0000

    reduce machine seat bottom low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
