808db28d2f972ebbc756245159ce6ec89d64ae50
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:07:58 +0000

    Yy improve at takeover hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
