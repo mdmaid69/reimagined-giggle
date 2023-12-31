3ff62096cc47470a3ec7839fea5ac7301e867446
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:09 +0000

    himself fall hungry used slice

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
