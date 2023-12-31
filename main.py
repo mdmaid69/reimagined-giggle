800835fe284bd75bb945ae4efe601ef97142cc9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:07 +0000

    waste them familiar cum ill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
