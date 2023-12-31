25b120b84032d1b9817546899f21901cfd1367e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:47 +0000

    criticism whistle clear serve spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
