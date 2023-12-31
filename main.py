62ea915f1cf6c404df0e787244a08d59e793d9f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:32 +0000

    war quick maybe frightened dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
