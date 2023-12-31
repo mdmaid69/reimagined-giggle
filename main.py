f844b7b2a2559160c9aa10ee35486d1ba3ad4189
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:39 +0000

    Qq murder wave thinkover happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def is_even(n):
+        return n % 2 == 0
