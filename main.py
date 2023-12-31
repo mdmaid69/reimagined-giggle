16ced0bde1c75b0202a3a57a0a35d4abe4e36600
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:09 +0000

    hate Mm broad worse progress

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
