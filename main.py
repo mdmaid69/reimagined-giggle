62bc1e804d2bd7b2344ebd7f28ef742d94ae27df
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:16 +0000

    crowd murder estimate muscle underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
