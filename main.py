5e7ccbcaf4cfae5f8865e4bdcacf23f24ccb5fc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:52 +0000

    past seem effect accept back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
