2eaca9e16b319bdc8717b06ad0e2c033a94696d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:04:39 +0000

    split behave relief brush sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
