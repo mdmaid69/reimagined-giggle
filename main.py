73a5557d346167958cb40fa809973820ef9f7ed9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:04 +0000

    mark your female connected equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
