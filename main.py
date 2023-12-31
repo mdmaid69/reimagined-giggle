4047f79f1ddc54d7f451eb8aeeaad3a41349fe96
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:45:39 +0000

    trouble convenient feed calm holdout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
