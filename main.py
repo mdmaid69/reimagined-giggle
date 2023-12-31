f40eb4e5c0b24df1670085cd54d7dfd493a23fa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:31 +0000

    cost embarrassed high already centre

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
