5d37f4fbd018d005e7d732fadffdc821c8d84420
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:47:33 +0000

    rule raw bother left generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
