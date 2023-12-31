b8b34d11fa1e04d8e68c30969f732b0fb0ca8499
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:41:50 +0000

    edge pretend him throughout Zz

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
