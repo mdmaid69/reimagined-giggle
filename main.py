00b7e275291c8a7db725fa2d64e5d57bd8d0199a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:06:18 +0000

    circle study keepfrom purpose concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_as_set(array):
+        return set(array)
