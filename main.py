a06108e14f1226fdca0bf8b71773b1e6ba5151b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:54:05 +0000

    whistle male pupil comeover very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
