f5bdc862f2407a970cee99db928ef2232ce16959
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:53:30 +0000

    trust different either stage grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
