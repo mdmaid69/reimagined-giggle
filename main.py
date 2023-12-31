855268367cd883b807afb47dc519a7be71a14c3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:49:09 +0000

    boat tip advice pay fat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
