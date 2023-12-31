e04e9c5c3eb50e1a66adb8c88682a90b543cffa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:41:36 +0000

    nowhere conduct but wheel male

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
