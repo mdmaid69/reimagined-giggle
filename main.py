a47ff76d1e715503c9dd8cca23c7067322ea6033
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:30:41 +0000

    content butter next tax along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
