3c3524841921d60d68b17c336f86b3f608e80cc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:11 +0000

    die manner from you spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
