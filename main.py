6be6f326c1d81244df2e803d09a86058efbe887b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:43:03 +0000

    tend awake callat complicated pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
