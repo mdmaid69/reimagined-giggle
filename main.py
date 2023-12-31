1857476edcb7030d281acce21d1e8e9223fdcefd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:37:01 +0000

    development tradition weak fence rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+def exit_program():
+        sys.exit()
