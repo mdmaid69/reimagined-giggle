6711f0116083a3d8fdf2b64a32b5254f743cfa49
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:22 +0000

    callat usual goodbye hardly turnout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import collections
 def create_user_string():
         return collections.UserString()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
