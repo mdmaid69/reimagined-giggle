3cbf6bb7651e225b2a700db0d7d012056e7d9981
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:02 +0000

    price offer sleep above whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
