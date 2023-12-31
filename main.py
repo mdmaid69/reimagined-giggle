f08420c12b3b30fbcfa460b378ad0293c7c5eeeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:23:24 +0000

    nor month exam combine quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
