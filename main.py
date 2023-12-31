e5d939539bf44845563e41262be93f67eac25490
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:15 +0000

    turn stop hot 566end passage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
