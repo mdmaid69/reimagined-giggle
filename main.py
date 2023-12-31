5222692fc9d44e70373bc0fd4c82d53267adc9f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:42 +0000

    sticky cry variety behave hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
