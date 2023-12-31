1964463078b63f4104d844c20fd3098a159393ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:46:20 +0000

    flood let possible out circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
