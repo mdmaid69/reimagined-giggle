c3b466556a750036aa62889eb7eb5df1767cc816
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:59 +0000

    basis fork convincing often influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def reverse_array(array):
-        array.reverse()
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
