837daeba731cc6c42f5b75e548876dde3221828f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:51 +0000

    under now worried mine cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
