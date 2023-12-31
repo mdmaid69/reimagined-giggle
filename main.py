b46301132286bdcbf6a7b00158a7eb9ff26f4abb
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:34:11 +0000

    handle trial defeat body abroad

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def sort_list(lst):
+        return sorted(lst)
+def add_numbers(a, b):
+        return a + b
