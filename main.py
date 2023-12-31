87f9d43b8b4522ee59c8ec1c2a4da28c8b12f3ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:13 +0000

    zero together break cruelty underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
