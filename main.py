6c4c3830d8e3909c431255d3c679963539a2ea4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:31:37 +0000

    wish wave scared dance compete

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
