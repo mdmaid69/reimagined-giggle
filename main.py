16a1c6ffb3c4059b5080ce82b9592fcb42d911fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:24 +0000

    born record worth book achieve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
