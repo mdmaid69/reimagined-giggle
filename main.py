44f2c439ae8aa39c9470ffde59b2dfc19373d854
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:30 +0000

    relative favour circle completely proof

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
