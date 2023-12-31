2ff415f96ee8833527126f15ebddf72a8f79e999
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:44 +0000

    squeeze loose tear great round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
