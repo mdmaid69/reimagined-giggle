f324b2d65a0af2b7a197764be0446883bf09ea2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:11 +0000

    combination badly drive responsibility contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
