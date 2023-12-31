c7f7039f6dc53ac602999d15f3de70f76901c09f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:56:48 +0000

    start offer hear train black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
