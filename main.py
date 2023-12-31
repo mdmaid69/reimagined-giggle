a4a5ac016cdc4e2a627180e7f8fc5d748a3656cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:54:57 +0000

    pin comment getaway I calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 print(tf.__version__)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import getpass
+def get_username():
+        return getpass.getuser()
