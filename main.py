2c8b69b3c70bec71823af82ebdb22250a66517f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:51:17 +0000

    advice slight my accommodation relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_priority_queue():
+        return collections.deque()
