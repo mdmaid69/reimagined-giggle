0f0d70ce537ee276a904b6a5392d3ae1edd4a1dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:28 +0000

    neck less scrap please turndown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
