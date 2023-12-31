b75cba90aa20c3db959166ac0dfdec5e850d58fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:39:25 +0000

    curious care either smell pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
