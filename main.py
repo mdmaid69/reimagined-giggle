5a6f4468f9ba468a45a9caca4a153e94d16006af
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:49:49 +0000

    yesterday development hurt food burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_array_as_bytearray(array):
+        return bytearray(array)
