3165812faf57ccb2736456dfc1b0c2c6c28ed257
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:07 +0000

    represent private convincing uniform fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import numpy as np
-print(np.array([1, 2, 3]))
+x = 10
+y = 20
+print("Sum:", x + y)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
