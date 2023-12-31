da114d8823294b1b2eca20c5d38b44ccc1cc8f1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:22:15 +0000

    payment lay start search heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_time_since_epoch():
+        return time.time()
