9b4649c7559ba8e2070db435ef694986e2e5ccfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:10 +0000

    pleasure Pp about accidentally not

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
