5d275dbf52f78a160ef112d683dbe7b4c1612225
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:49 +0000

    whom nervous visit pretty Mm

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
