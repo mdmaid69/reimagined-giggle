8252771a11095f29acb00c307b03c71618ff609c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:59:55 +0000

    polite tool smooth everybody uncertain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
