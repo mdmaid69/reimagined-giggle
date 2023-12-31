00f963ac2a5963b006caf70498970999a99976be
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:28 +0000

    interfere cut basically face keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
