39436bda2ce7d56a7add7a5754d1ee0e0c16c0f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:11 +0000

    autumn print false vote along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
