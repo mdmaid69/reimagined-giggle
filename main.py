6c5ea3dae9a6454b935466cda45024377f67f449
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:18 +0000

    issue base scream beside friendly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
