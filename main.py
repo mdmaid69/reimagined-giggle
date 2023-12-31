3283d3b5bae65500cb07933d90e50d67631f8cfd
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:11 +0000

    like deal light which fast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
