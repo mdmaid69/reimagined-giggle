c7e7e9f6328fc98673d05f230853d0634cf5c208
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:16:12 +0000

    responsibility force pool century letter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def sort_list(lst):
-        return sorted(lst)
+def check_if_array_contains_item(array, item):
+        return item in array
