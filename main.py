cbe8a27f379af6095cf11da13bac0aadff498583
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:41 +0000

    public unlucky talkoutof school different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
