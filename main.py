f99752ef434c66e8f7cf05567580b829d83e24ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:38:59 +0000

    572enter pair definite discuss father

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
