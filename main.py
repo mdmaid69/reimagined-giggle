06798d00c573cf118dd35a6a2813dd01caa58fee
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:40:39 +0000

    every file strict land war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
