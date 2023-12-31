b5df466cc6076434d595575f8f5a89ff94585720
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:50:10 +0000

    typical spend interesting control get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
