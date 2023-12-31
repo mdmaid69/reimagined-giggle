5434834d2ed6b5513ba4815546eb8a4c2f8cd309
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:22:08 +0000

    succeed pool prefer bye in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
