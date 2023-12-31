619335802d1fcd18bf0435a8de48434f6161eaa5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:24:36 +0000

    stretch result snow wealth again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
