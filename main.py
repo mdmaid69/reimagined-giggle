7f59953d063f5eb7e31c13ed82a0aaa89eb9521a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:39 +0000

    inside aim below schedule safe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
