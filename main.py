bc92d988e6cb25cc70a0f6540a0e4dd7a9c8dada
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:37:46 +0000

    use correct age defeat tall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_power(work, time):
+        return work / time
