8572218755e9b111bf8fe0225f44ae48a32812d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:25:24 +0000

    wrap attach polite far study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
