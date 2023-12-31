b1b3ee13ec952fef1fea0b032914cc20cc4e759d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:53 +0000

    enthusiastic fast kind confused disturbing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
