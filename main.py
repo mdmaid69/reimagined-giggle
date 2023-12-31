2e96aa56317909507c7300adc19364d1d6c6fa3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:27:17 +0000

    emotion behind once alive runaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def count_words(sentence):
+        return len(sentence.split())
