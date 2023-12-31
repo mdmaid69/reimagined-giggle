65a5d859824f8121725a7929cbc98250a275629c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:51:14 +0000

    occasional either illness complete unfortunate

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+from collections import Counter
+print(Counter("hello world"))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
