5b3d45bf5b437d3691f61960d24a79d0bf5f30a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:23 +0000

    afraid every cause aim difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def count_elements(lst):
+        return len(lst)
