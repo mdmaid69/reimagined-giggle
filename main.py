f60f1a7f9d74421585114c6f679644497c30da18
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:00:28 +0000

    unkind crime offer geton involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
