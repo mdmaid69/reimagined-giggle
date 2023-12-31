b471d23709f797c144712e75be30d16ed312062d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:32 +0000

    stranger worse style dance hello

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def is_even(n):
+        return n % 2 == 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
