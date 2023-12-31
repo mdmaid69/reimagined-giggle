e42f228c58f87e889cf5e84855c37547be9b6899
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:07 +0000

    container exchange command window talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
