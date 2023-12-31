48bdf812ef99b21909f131533447cea9f4b2eb03
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:18 +0000

    base broad kind dry well

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
