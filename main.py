729a8f5b885b2bf36b5249a8a8d7db8da358b90b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:10 +0000

    turninto considering he doubt keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
