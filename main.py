d70a598dddf6a4b415d331cfda2fd97838c01834
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:59 +0000

    water 572enter convenient gift bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_characters(sentence):
-        return len(sentence)
+import os
+print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
