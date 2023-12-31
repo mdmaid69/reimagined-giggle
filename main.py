4c7d3346bbdb2bd8fb73991e82d04035ac76bfee
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:43:18 +0000

    switch shout entry clean laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
