c40c2bc0de64f0530144c3102e10c89287319f24
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:13 +0000

    worth group grateful goon creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
