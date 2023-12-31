51023529cecc7f354d728425ffda5a4577d996d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:09:16 +0000

    finish contact spare considering yesterday

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
