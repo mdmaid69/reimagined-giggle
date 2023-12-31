3b3f67277f42c2a03fbf18a627c32ffb3d890e19
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:57 +0000

    exaggerate sore gas measure brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(lst):
-        return max(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
