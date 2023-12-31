68bde5caaf1106c8ad8428117b7ed9c273fc2c83
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:06 +0000

    art noise clothes switch feed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sine(x):
+        return math.sin(x)
