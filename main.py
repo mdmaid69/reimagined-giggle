1977bdb88b90275f0adb9f98af02a8048ac318f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:45 +0000

    friendly Ee print various thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
