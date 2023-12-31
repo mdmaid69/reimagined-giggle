69ded1fad19c489a46e718c4b68651a147e18518
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:15:09 +0000

    beside any trick absolutely traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
