bfb7484ac12bc71c0bfe0d2eda4cf9b041e3ac1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:46 +0000

    impossible in shout happen reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
