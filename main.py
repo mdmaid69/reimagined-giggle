fbc106033ec75a0eeb92e0ef33fa07c308a6c53b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:27:25 +0000

    name purpose empty above reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
