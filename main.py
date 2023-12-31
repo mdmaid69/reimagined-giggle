fc3e863b2a8a7631e77cb280dbf4aeb58ecd6659
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:22 +0000

    find sound giveout public downstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
