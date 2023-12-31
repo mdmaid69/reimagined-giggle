22d3700b13725df24a5fd40db121b8699d19429d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:20:20 +0000

    key after piece range rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
