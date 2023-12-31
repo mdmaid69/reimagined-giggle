dd5f163b2d6f31769832cd5a730631cdbaa918c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:27 +0000

    bath thank otherwise past lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
