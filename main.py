1bb202100de0e3f1a6fdf0ee023c50d8ffd657b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:12:27 +0000

    care runover join various throughout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
