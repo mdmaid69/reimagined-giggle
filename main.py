c6ffa2d6068172f4845f9f2cc4232e94f58969b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:58:22 +0000

    keepout announce yesterday large public

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
