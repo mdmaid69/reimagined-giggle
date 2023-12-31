6cad74b852cfcbdbae44ed787f4ae34a4981f485
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:21:44 +0000

    black gather contribute big obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
