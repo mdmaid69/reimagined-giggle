f105671a4fdbd04e19101f44306bf507586aaa6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:57 +0000

    with roof giveaway organize discover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
