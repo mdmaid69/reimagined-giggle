4bf82c1bd11cb5e18d77fbb3c6a854984f78f438
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:38 +0000

    person effect open deliberate connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+def square_number(x):
+        return x**2
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
