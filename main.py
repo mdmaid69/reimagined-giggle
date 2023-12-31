3aa1aa8184d039867c6e4046d413e39b3fcc5563
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:49:44 +0000

    shoot school risk waste conduct

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
