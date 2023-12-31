d71651b21a4d4fb42ce68b834c8845ddc646be20
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:08:07 +0000

    bringup begin discovery smooth copy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
