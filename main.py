19853a93b0d3ec024cd10a2b720184129c17f3a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:25:49 +0000

    congratulate post blame way introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
