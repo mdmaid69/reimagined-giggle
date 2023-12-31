7fba0e9c5b37d6b5efa04159d229164e83645111
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:30:26 +0000

    lonely convince vanish arm after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
