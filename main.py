0b7b786752f84c5921c4e87330ecafc7e581ef01
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:38:18 +0000

    behave flat meal empty few

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import platform
+def get_python_version():
+        return platform.python_version()
