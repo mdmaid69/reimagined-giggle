05207f60674c61340041358d8757c515616c2d4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:44 +0000

    breakfast beat listen suddenly may

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_octal(n):
+        return oct(n)
