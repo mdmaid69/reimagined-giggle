9f704afa18abe81ffd446a5706d5dbfb39cf1700
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:00:51 +0000

    condition contrast average behaviour single

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
