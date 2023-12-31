8f05cda5b11ccb69814bfb1f592bf2f4dd0e0ff2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:47:36 +0000

    recovery culture transport foot account

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
