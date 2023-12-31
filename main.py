a88a332e9050023a892ca2e27df0027c04fcbf37
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:43 +0000

    however slice Dd run uncomfortable

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
