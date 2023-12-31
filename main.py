b7541b574aecf89aa69253c20210b57bf39f3842
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:19 +0000

    feel visit fun collect after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
