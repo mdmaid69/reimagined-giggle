f3b8b5eb0972aad5fb576151e90253bd9b23f433
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:29:17 +0000

    rest worse statement phone season

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
