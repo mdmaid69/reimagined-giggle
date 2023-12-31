f48eec1fa4efc48dd7b9b877cf957eb792d2459b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:48:02 +0000

    strict list compare maybe runinto

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_area_rectangle(l, w):
+        return l * w
+print("Hello, world!")
