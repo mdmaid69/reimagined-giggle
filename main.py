a58e3a2643ceb1e1e9500cc492c363e28c1ca768
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:29:03 +0000

    hate platform lower experience wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
