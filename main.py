47c6911d552e9ab89ef6c5e533bc4db9bbee6bfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:03 +0000

    recovery replace left right lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
