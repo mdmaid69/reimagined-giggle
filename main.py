e60d1692ea9171043226bfa085093082e5c43483
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:36:09 +0000

    blow rid afterwards sink like

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
