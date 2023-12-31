e4a47096ee7473f6e2cad454ba804d6a0dd130a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:58:42 +0000

    experience goup press comeoff help

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
