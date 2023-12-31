6d83eb8e4b9463a276b57373004c30dde9579b08
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:25 +0000

    sure position clearly smooth cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
