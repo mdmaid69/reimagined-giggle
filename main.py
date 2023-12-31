e83f253186031afae0c5ec0b2c0b69bcb0011856
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:50 +0000

    downstairs free whole steam suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
