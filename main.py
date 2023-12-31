34086805728c561d457d23f0512eb6618791ec99
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:16:52 +0000

    practise complete hate habit believe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
