502333deed3acc0b9450ee457ebf029f4b57f454
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:03 +0000

    capital expression song blame trap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
