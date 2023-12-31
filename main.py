3262ab40b3c79c652a55873040650e9696a092db
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:27 +0000

    various valuable practical judge finish

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+print(re.match("h.*o", "hello world"))
