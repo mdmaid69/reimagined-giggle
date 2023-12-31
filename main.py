0583429824ee2340bcd9da1fc2657f058e3fec69
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:22 +0000

    understand reason sensitive together wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
