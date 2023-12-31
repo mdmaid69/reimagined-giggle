0e28e721875cf4e00bffb236b91003fc80e01555
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:06:03 +0000

    innocent hold introduction 179bite silent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
+def convert_to_binary(n):
+        return bin(n)
