c2561db3bf3b2ae20e73ddad043b12befae3acb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:18:11 +0000

    formal cheque hole wheel light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def roll_die():
+        return random.randint(1, 6)
