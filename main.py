5bbbf0195d6fcb787ea5335b3ed50ea8ea04ba16
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:24 +0000

    complete except ill stand see

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
