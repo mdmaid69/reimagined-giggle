97b517c08e07ebbcaf3efa45a4e028b08e0ef30b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:16 +0000

    suckme fair matter means post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
