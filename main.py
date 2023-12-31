395fac7cfd3ef39b2807bfd6ff4ce3beb7d25685
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:49:04 +0000

    interview beginning television apply suggest

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
