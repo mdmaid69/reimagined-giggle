d0cd0a340d1ca2951bc17cbf6bf34ac5b5ff652d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:49 +0000

    search plastic solve hand wind

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_list(array):
+        return list(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
