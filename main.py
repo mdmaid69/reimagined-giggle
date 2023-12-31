689c72fe57429efd1367f9fed8ac2d43cee40ec0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:49:21 +0000

    once lookafter shape giveout success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
