b2cba5767ca54615943e215d60c084db84a35593
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:41 +0000

    pour getaway smooth nowhere throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
