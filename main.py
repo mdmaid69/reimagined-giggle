739efd909dff1e83c30cef6337ed86f56e845cdb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:08:29 +0000

    makeinto cost notice objection unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
