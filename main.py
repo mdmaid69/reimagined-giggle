2c1aff41809e8cd85dcefbf7b76b77026ec91c80
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:33 +0000

    appeal comment suit pour work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
