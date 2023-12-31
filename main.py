94c720292fe865c61f876f28eb78d84a343623a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:35 +0000

    wind exactly set year children

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
