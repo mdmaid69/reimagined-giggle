53a4e63d0d31ba393849f17a7add2bb0cfb4642f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:25:35 +0000

    anybody Ii corner special pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
