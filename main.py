8a9a39d2f7bbfb5ff3510d748bc7470c228c1116
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:46 +0000

    concerning with amount ever thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+for i in range(5):
+        print(i)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
