6b7a89fb5f9ef455f6744b18c7aae042c7699774
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:18:22 +0000

    year damage best contain takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
