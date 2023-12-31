36d2646146aab81e4f079323954ca51e74c2a081
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:39:30 +0000

    lookaround unkind rush university fond

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
