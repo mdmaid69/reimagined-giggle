53a19b15f5c813dfeffc9040b6b8c71575d96859
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:12:48 +0000

    fetch gas physical excellent greeting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def square_number(x):
-        return x**2
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
