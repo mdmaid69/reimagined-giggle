0a5fc4c347ead3d4aa6fc9da4432846dabace14d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:59:17 +0000

    relax rain its sentence type

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
