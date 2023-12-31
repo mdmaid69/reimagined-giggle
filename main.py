2c842627efb573bb735117fa2d06861e0149cb8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:06:29 +0000

    special necessary married hope tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
