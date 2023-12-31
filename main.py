b6a71d9fc45f692ab8eb974da96712aca4fa43b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:18:15 +0000

    attempt concern place name fill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
