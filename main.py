5c6be2414b5be3444b49c58b63cc840d94b87c53
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:11 +0000

    file while interrupt church thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
