bc6dd98e4e2c7e914982591af035571c964f0498
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:26:08 +0000

    confidence practice offer minority fit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
