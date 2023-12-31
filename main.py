b166b50508c50d5086f21c0a0635e6a110a28734
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:41 +0000

    high miss plain strong bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def square_number(x):
+        return x**2
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
