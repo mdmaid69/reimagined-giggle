2135e9ac323ccf019ac97ff9574fef2fef36a8ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:08 +0000

    freeze empty comeout air settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
