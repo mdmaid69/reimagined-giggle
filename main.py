576fce81c9ae7789638aef89589047ff1d8147fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:49 +0000

    deliberate programme fall right measure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
