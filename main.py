d254305ff094f1ddc62aed4614f9f07cd4bc8468
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:41:35 +0000

    trade knock lonely interest immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
