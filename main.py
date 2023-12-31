893b5949d17ded69cc6947c19215b1c20750e745
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:54:29 +0000

    report hope government reach tool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-for i in range(5):
-        print(i)
+def get_array_as_bytes(array):
+        return bytes(array)
