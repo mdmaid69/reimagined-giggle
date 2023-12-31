efbfac6fe6fa9bf7ed4e82b0a61d1437acb03c07
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:16:31 +0000

    tragedy smart major thin wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
