68e3399e6ecaf7fe5d4dad0f3e23cd7598e28cbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:44 +0000

    shut none slice our method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
