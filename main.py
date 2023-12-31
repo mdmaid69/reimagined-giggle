c6ad4517e327c14524198d1af466d6d12e08c4f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:43:59 +0000

    friendly 564encourage invite react final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def square_number(x):
+        return x**2
