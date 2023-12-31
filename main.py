1321efb41bb38fed0651d167260c91250c12ff32
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:16 +0000

    chat angry burn ring well

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import datetime
-print(datetime.datetime.now())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
