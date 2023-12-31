70012da68f7ff986ac34ebf2d23dd89e49e23774
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:13:02 +0000

    degree order steep brush gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
