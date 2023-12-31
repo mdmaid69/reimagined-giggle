bb28a0809506c24e54a07dc1e7704e2fe42b2f59
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:32:45 +0000

    harm respect eat inquire spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
