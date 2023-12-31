6da4241df526a60fea5a973a5cc228f141a1ba5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:57 +0000

    bread sign colour lead rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
