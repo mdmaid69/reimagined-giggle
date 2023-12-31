50670d191f745bbef1a61c0bc707521a50c3dd70
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:03 +0000

    return consist quantity this probably

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
