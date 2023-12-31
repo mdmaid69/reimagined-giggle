e0ab3adaf1d231ad2039d28575b87103c6a934f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:17 +0000

    hurt keepoff throw comfort eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
