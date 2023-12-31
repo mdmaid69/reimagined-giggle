3fe415ad83756ac0516cc86c4980b09b9366ea12
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:40 +0000

    connected share original rather single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
