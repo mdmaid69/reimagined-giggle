421423eda61464ffecddb62508254731a5509787
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:09:46 +0000

    travel lower formal confidence turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
