86cc260ac5034b82a1cf1595a1c94298cb83ffe6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:43:37 +0000

    mail warning welcome less consult

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+print(sum(range(10)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
