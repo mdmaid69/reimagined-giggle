596a225425ccc95153a9cfe26fe62100baa795ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:27 +0000

    kill occasion lookaround one to

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
