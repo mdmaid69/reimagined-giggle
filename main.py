e5e3c40b618006332bd396115a2ea16ad89218da
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:50 +0000

    slide chin satisfied pressure stop

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
