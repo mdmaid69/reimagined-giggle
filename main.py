d708897dd00b7a086b4db25a377fe8132760de97
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:48:53 +0000

    loss now organization kill uponon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
