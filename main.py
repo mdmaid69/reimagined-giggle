5fbe139149d8a266d8c2386848b62c95fab29c13
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:49 +0000

    direct accordingto hollow conversation interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
