99c2f5decf3d46d44aad4016e4e343fab2962cf3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:02 +0000

    wing far maybe forgive include

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
