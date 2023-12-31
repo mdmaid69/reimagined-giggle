d767450a1cf501508634da5580572d2dc93903d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:49 +0000

    respect draw discover thumb his

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(10): print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
