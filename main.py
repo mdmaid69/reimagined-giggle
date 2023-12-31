a10a7eca09851354b268103b364a955b63db4422
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:32 +0000

    music thinkover exaggerate I carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
