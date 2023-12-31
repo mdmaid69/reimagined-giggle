b894d16eae9001d65c813a111480264f31c7102e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:43:30 +0000

    still extra break break gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
