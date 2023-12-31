4ac9de0df415f8427f8369779cdc812164f0fa2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:58:25 +0000

    contribute drink progress whoever turnback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
