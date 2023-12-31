26abca1cbb81a4fa5eee0f549548a76e91e29878
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:09:43 +0000

    interview and load save waste

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
