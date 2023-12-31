7023b25967a23b945a55500bc692c15ba2224fdb
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:13 +0000

    interest breathe part frequent approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
