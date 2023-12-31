daf83f2f1acd3136f9ebf509febcf775a5eee2b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:22 +0000

    marry important if become travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
