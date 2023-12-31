84043987e4c352570a00f6dc57572f6e06f3d198
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:20:24 +0000

    approve invitation behaviour dish giveout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_density(mass, volume):
+        return mass / volume
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
