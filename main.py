010382120772c148bef72fd04b203708bb9e2cce
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:37 +0000

    score bury pronunciation worth sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
