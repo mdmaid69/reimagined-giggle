ba1d0363961cc40a3b614b6d5329901c9dede334
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:05 +0000

    mend wing right clothes tradition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
