684ee0cf11de777027f7af6882572834209c1fda
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:21 +0000

    best much basically juice involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
