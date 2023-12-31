90de86a462c628ed36373f25949792473d757273
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:20 +0000

    rub appeal relationship upset married

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def subtract_numbers(x, y):
+        return x - y
