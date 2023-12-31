276e193ae5556164fcac9491642517e3a7401e83
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:16:35 +0000

    neither quality aware regular search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
