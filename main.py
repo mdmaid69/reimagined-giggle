d01fd85009ae9bdf2f85ec49c25d9c30042f86e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:54 +0000

    street thick brush uncomfortable disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
