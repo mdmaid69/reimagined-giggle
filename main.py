8d107c959650f1a2bc3f9271be3a2c585889335a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:32:40 +0000

    takeoff somehow callback store represent

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
