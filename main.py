4dbe327671598ed8c24790889e606aa09ce9499b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:11 +0000

    flat travel pure myself environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
