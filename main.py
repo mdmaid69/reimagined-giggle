4c2460c48ee122d31d4705bd4b9ae07b91166622
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:08 +0000

    advanced wheel bored fairly write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
