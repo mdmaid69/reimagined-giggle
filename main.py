c7ce4e66d7f42fef3b4b1fbedb50ac9fa318505f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:22 +0000

    tragedy thinkover Ff want term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
