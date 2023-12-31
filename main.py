b509b28430ce449c2967b880699f4ea6f5483a60
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:17 +0000

    so shall copy shout past

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
