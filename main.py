bceb89f46523ba5f486b4629e28050dd967793a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:40:25 +0000

    dick work dead relief language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def cube_number(x):
-        return x**3
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
