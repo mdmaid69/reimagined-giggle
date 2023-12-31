286e73788c1696357c82edc78465838e9cae4c0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:21 +0000

    division among either balance why

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+  def sort_list(lst):
+        return sorted(lst)
