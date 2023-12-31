abb504fa59b8684bfbf20715eb2b918154645ac6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:19:59 +0000

    pile vote taste theirs nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
