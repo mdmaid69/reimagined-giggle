5fbb01b58c8fc00189295010ec154a4f1011ef83
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:50 +0000

    chance gas book let Ll

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_power(work, time):
+        return work / time
