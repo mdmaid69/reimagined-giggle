a6fb57e14c3879c01eebd48a4994a9520b5d89b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:35:15 +0000

    already slice rubbish intelligent present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
