8a09a527123bbc62becb3e0b1949c23fb536d1c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:12:17 +0000

    home claim though peaceful active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
