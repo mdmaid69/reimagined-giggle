7ce67532e8db29da954f18d7ae380713b0aa1751
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:35 +0000

    invitation gate we pleased make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def subtract_numbers(x, y):
+        return x - y
