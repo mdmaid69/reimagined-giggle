54ec7062d6e838c7ce82b35fdd2c71bd48677b83
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:26:03 +0000

    stare uniform building image Cc

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-for i in range(10): print(i)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
