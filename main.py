6d766dde24b451f8ee147fc19c7cd8948dc902ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:22:25 +0000

    destroy breakfast fat really strange

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import datetime
+print(datetime.datetime.now())
+  def calculate_area_rectangle(l, w):
+        return l * w
