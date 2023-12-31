f9a93e2412ca58192b38e36ba5e2048f1dc93325
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:41 +0000

    relative glass create wind minute

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+print(sum(range(10)))
