1caa56c6e758aca818cdd4173a1476fdb49e6ede
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:21:10 +0000

    insist copy friendly confuse plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(a, b):
+        return a + b
