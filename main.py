f93bd126c46df188e2852de7f13c3549dad36d7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:42 +0000

    bill favour eat opposite contain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def sort_list(lst):
+        return sorted(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
