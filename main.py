f99132832d401f73a18f77dd8bc9cc84401db87f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:11:22 +0000

    wonder sure expensive base move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
