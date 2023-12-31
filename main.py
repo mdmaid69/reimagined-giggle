07489cc84f9e2389477e6b36a2912e8b5774e78a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:23:05 +0000

    emphasis test scale character extra

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
