5d1a736a7a26187002843f6d6d935d1a4321be9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:52 +0000

    breakout shoot rain left variety

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
