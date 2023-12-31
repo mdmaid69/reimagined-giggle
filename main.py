83a56dd700901227a28e6e5aef179000078e372c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:10:53 +0000

    prefer discuss post hold bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
