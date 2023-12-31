46ede7273b3ab184e81b67b50907017d17dabbdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:18:32 +0000

    too area organize deliberately phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
