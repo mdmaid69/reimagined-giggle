a4183069508d3429a9ed4a693a16fe73227afc84
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:37:16 +0000

    snow each cruel react mainly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
