8109ab56363e682d6f3652279cd781560f659a94
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:37:04 +0000

    load twist recognize breathe phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
