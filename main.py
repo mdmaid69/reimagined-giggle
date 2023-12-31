95f74034a8bdbeda975706b91aeba42929b2c116
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:14 +0000

    where ear while top heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
