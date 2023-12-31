de9365e3a1b6905743e6d1f557d876e3d947bb61
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:37:32 +0000

    cool question flight woman on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def count_words(sentence):
+        return len(sentence.split())
