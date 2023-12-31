9fca7c0ba9ad094f7923c2aef182f798ed04f886
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:39 +0000

    battery fat lead spring combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
