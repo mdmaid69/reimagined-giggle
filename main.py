5066562b6846811ba45417d5daf34881fcd0886c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:20 +0000

    mention over anywayanyhow damp push

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        return sys.version
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
