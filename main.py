7b9ffd53e17481e552651634ba43b5a9cc07a606
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:58 +0000

    positive Ss and lean seat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
