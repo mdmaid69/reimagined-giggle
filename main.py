bac6137f367ab765209f980be446437ad8089e36
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:11:08 +0000

    detail tragedy fasten broad occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
