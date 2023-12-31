f9282511952603f444b9cb6a800ced2e878198ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:15 +0000

    although blue getout create bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
