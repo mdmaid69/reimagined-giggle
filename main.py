ba5c2bf665a1bf6d71ac1bbabcc001349123b87f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:36 +0000

    either switch pleased makeup ball

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
