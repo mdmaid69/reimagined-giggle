ecab12e6f447f7efc883b2001330455185cb1b0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:14:45 +0000

    Qq why Mm in whichever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
