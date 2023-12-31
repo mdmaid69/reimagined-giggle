95f4f61b27c243d2a600638cd85ecb42cfc9e55c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:04:24 +0000

    achieve moment decide getoff meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
