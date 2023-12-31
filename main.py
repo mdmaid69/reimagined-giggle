640ae45fea0f5ade9a5c5912443d8ea82bb8b338
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:28:51 +0000

    deaf long afraid beneath act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
