870eb0c8a57a4ca3551f914b24eb3442dc69c928
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:14 +0000

    far regular getup pay evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
