6918c805e57cba41762ea7159b2dd52f4a5028ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:29 +0000

    pencil game escape flight table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
