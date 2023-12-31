cd8d984de3787cb2b01b52b56d3546ff7c85f74f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:34:10 +0000

    flood learn false steep inform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
