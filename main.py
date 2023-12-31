19e36445e783b80dca587e7900c0cf36f2c9e3e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:47 +0000

    answer regional cheap town bus

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
