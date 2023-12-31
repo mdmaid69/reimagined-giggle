5774c10cfbc2cb5630939b06e776f91e18554c56
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:24 +0000

    contribute shrink talkoutof none us

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
