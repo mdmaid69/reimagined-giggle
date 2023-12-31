a3a2c954cfb587903e87640425b5fc723b25639c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:28:26 +0000

    make fast Rr lock confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import platform
+def get_os_info():
+        return platform.uname()
