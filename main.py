7083c92b5237b9626429a36e099e93f16aaaf795
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:12:44 +0000

    myself complicated satisfied another trousers

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import unittest
 
 class TestStringMethods(unittest.TestCase):
         def test_upper(self):
         self.assertEqual("foo".upper(), "FOO")
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
