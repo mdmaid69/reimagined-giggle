662155a574b64fb8300dcfeed23ad6d5af1381e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:55:48 +0000

    wash rule stir injury whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import sys
+print(sys.version)
