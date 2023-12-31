60355edd12e40f4255bfbf8e0a7ddd4a95314943
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:32:28 +0000

    Tt discussion imagination eat she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
