6a1cafde3d3a669f9bb597140c5dc80a6db2b782
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:40 +0000

    write worry value theirs ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
