1e9de49ed1437549225f3ddf3053f4644bc9b6dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:51 +0000

    miss birthday clear there prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
