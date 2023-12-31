26b9e3fac65805abd6ffdf914e23c161515a2874
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:00:38 +0000

    arrival happy live direct nonsense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
