f2a141111d0db44c33aaeab2c76f138873cbe8bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:41 +0000

    moral obtain sometimes party easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
