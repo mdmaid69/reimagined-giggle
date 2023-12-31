aae2c9104f05285fef259df54d03c3323a8239d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:22 +0000

    shelter 571enough goaway her dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
