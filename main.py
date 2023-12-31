e263c88c8504bedad1835da8d643a62e8d15e9a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:17 +0000

    leave attitude figure ours round

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
