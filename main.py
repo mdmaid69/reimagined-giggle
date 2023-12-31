d81133238d17575f13db74b8cc1133ac5980790a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:17 +0000

    play supply sure drive see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
