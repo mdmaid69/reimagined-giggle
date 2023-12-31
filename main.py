ebf4c053a8dfc01da244e5116c487cfc3952a48b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:46:43 +0000

    equal careless born exchange ring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-for i in range(10): print(i)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
