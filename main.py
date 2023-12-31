a5ee3c0f9c2084b6a8f157ac5cc42474b71d0a51
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:23:53 +0000

    long watch him plenty head

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
