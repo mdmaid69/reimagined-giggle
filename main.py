73a8614a5cd40d5a1a3398df83fa8e0e6d030fb3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:48 +0000

    dare slice weather bet welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
