d8664156ce0c816a9a24e6765db9f8efdbe3e0b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:13 +0000

    comeup evidence bin ours chase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def square_number(x):
+        return x**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
