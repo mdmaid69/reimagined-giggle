3d0e01f2c29d8a15a884ea65a99bf7d023d49dfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:40 +0000

    high deaf generally high comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import time
+print(time.time())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
