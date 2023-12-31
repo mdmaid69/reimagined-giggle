16af8545ec0344abe4788515edd9d808cf287017
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:05 +0000

    goup shut unless comeout block

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def reverse_array(array):
+        array.reverse()
