4ada37f95428ef5b5f8eb424763be3ecdaf02c5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:31 +0000

    private catch instead bright perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
