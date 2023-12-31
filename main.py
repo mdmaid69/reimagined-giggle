6cfd6dc38f4cc3af1c78e994c5335c4fc0ebbf55
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:37 +0000

    OKorokay pressure valuable drive probably

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
