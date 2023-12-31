0795f15d1add57a8788fbb3ad60eb82082893562
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:15:52 +0000

    limit cross deliberately us success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
