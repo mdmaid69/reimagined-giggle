8088861384d8f5a6cca0cab93e2999f6c3f37975
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:32:24 +0000

    worth check let secret commit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
