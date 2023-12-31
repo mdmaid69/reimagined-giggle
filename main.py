6161f47a6ff63ad27eace340cc4eae635ce58704
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:47:40 +0000

    this deep callat laugh shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
