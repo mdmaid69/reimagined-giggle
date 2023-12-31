9045bf2bb5f9b1beeec6b78f58d85893ce096bc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:35 +0000

    example what look underneath choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
