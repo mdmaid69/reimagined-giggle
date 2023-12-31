5ab0aba6c70406c6ce8f53659ba28e8bf55bca88
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:05:43 +0000

    callback face sure combination comeup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
