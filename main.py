cf2c83ec21b58b589c4911148d93edeffe54ae03
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:39 +0000

    congratulations bit dry tone apart

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
