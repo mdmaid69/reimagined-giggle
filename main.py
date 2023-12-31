1a11208139746fe9b84b185c2195962f0ac8047c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:03:35 +0000

    everywhere corner deliberately force about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+print(os.getcwd())
