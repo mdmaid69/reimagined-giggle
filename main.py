83d17f7f4e7995b676e83717dbe6660369f210a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:07 +0000

    point television tie control first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_volume(length, width, height):
+        return length * width * height
