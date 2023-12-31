5382d74c714af55dff406a8c939db9389ddd782c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:21:36 +0000

    song office shout wave worth

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
