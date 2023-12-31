b8befa7a278c908eb1eb44523736cda9d8dffb6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:25 +0000

    probably quality independent bound note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
