0bac29596f1b2e2d951c1ee2cb7d872ad3d8784e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:02:58 +0000

    goodbye lie takeback software bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
