9579d4734ae8f337170b76f5141affc7d4a58b33
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:10:02 +0000

    deliberate embarrassing needle angry on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def is_even(n):
+        return n % 2 == 0
