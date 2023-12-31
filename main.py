f3bfe9550be2514902824d6ef394248ba4d4bcb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:28 +0000

    grade advise hollow debt true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def find_max(lst):
+        return max(lst)
