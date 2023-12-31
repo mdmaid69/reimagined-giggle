3f7b910e3805f46bc36b737734f100e4a81fcf5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:49 +0000

    free knock keepfrom even negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
