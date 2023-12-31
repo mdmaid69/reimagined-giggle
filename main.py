6d367050e325360558fbce8e0bbc725735e65393
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:18 +0000

    exchange tread clever challenge still

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_length(array):
-        return len(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
