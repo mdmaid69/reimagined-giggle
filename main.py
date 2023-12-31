d35f1194752109a4ce0ec2a751f6a38823406191
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:15:24 +0000

    dream chin although brush land

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
