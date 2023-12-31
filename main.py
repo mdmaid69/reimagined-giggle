79dfac71ce0ff6ee4f994ab27bb544bae7ab65ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:03:30 +0000

    measure pool their achieve refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
