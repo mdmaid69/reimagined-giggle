fb030ef7642e81f66af6f88f4e268c4b1c7c44ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:23 +0000

    record collect pattern constantly interest

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def cube_number(x):
-        return x**3
+def calculate_power(work, time):
+        return work / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
