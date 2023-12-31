c6bcd5de8cb323f9c3f051373ae9249889e901ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:04 +0000

    seem moment chase turn fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def square_number(x):
+        return x**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
