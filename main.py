124de354a40c29391596c62373e16e1532a0863f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:41:30 +0000

    question refuse firm little twist

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
