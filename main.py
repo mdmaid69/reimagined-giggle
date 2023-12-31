07ce6ceb183e41f9dd6d76c3e6f60486dcc5f253
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:53:39 +0000

    scared near interested permission bet

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
