7bbfad619a1a546520f9d01cea3ab0646127f9c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:02:32 +0000

    certainly demand dull limit report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
