f16fb091c000cbf058755074d6df174056ce4efc
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:45:12 +0000

    help maximum entrance nice unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
