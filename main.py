d49e51cb07b7a29c5fc8dadea7fed3694941c767
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:01 +0000

    path wide although pupil deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
