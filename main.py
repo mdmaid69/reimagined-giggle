74e6929b7081bba410dfb8f0cd69dfe1a2f3d173
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:44:34 +0000

    usual increase closed value force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
