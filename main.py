852185f3326fa2e991d853ba498fa4036e0d1ffb
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:00:02 +0000

    wait my disappear empty price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sys
-def print_python_version():
-        print(sys.version)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
