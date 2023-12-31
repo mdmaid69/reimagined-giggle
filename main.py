8c5cd73a94d8cd30402ad7ccf4bc26c47ea6cd1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:41:09 +0000

    show exhibition suddenly advise instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
