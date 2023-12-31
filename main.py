090edb7c474395bed570fe8b62a3a00d8c594b32
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:45:08 +0000

    dream carry space notice squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
