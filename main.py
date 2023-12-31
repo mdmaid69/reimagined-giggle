fabf66cb73373a57629f807e056e2923daf2735c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:47 +0000

    drop choice yesterday trick judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
