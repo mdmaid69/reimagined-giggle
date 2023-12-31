f4abc257dcf5997d083ba6562cf51a0684bb5283
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:56:57 +0000

    boat actual standard beginning stare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
