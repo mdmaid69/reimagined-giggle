d2112a5f1b02859caec5be6c48891c27c2b6f94c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:25 +0000

    hope taste cheat couple interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
