d80dea138446fe08ec37c99c29c11b3eb08b6f4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:47:41 +0000

    offer mess wide bite successful

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
