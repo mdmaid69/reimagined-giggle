5b94f2efc0b37ebd843324a8bb3d1a07523b6cf4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:53 +0000

    trade everywhere doubt swimming alone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
