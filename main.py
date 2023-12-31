005c1832305dd00618c93e944d2635ff349fb181
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:16:13 +0000

    split relief block safety worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
