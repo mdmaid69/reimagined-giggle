a30339029c8f5e974c803719d79cf5d1218c9407
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:04:44 +0000

    protest discuss so Ss charge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_list(array):
-        return list(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
