a3583d294acebf230b018a5822e692fdfaeba370
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:03:15 +0000

    tight face cross slide concerned

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
