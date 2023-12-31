2904310b4aacc4113c5da9ed0433b70116b5c2c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:53:03 +0000

    reason that for reason his

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
