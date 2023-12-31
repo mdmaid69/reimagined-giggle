b533d9a3c1d6a7237e32c564bbab3c6749748d25
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:19:50 +0000

    effort getaway wing appearance trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
