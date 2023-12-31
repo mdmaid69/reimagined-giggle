f23663e3bd2e4cda9b6fbbc8af3a287f04a3f8ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:02 +0000

    smoke confused tear interfere this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
