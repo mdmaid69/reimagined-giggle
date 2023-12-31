d1621c20f06b6fd1803b6bf4336a8b7214727492
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:54 +0000

    badly suckme form cross joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
