69eddd523a17a792ff01b180f1cec070afb94991
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:46:59 +0000

    road breakout backwards fix lead

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
