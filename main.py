f10dfca125c4fe3dd10aefb42ab174d8d3cc12dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:58 +0000

    not home row spare since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
