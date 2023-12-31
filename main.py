c542916a41753dabb852728700432d7f12caf128
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:07:48 +0000

    trust play address sure move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
