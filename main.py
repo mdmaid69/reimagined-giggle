a95f28a60e0b6cc19ecafb8b2f0a361535066cdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:41 +0000

    raw raw blind boat ring

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
