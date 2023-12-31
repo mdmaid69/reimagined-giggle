6798bb83d4e249c41eb11703892c871e92d0ab05
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:38:59 +0000

    usually beyond conversation catch crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_min(lst):
+        return min(lst)
