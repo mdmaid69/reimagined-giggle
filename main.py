f89df9c7492458f47e1b63370a142551f1f141df
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:28:11 +0000

    gooff house approve nigga listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
