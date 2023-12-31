36565eb7526a7562c7e3a50063d0bf237cb189d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:45:46 +0000

    exit discuss modern back television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
