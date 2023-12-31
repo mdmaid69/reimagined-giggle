ea8f71393c2183e141124385ee694dae144978af
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:23:26 +0000

    play careless shop order limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
