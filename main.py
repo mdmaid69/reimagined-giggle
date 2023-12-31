576541fc0b53c9ad6a1483bb60207f30fa5a8254
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:26:56 +0000

    content do Ll maintain else

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
