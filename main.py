3816bfb1f54e63cd4e6ea9121cafbf4ff57708c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:36 +0000

    makeinto shopping land method upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
