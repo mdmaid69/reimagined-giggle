2934b1257790ed4bf0be2a0a20eb33803ebe6ea3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:50 +0000

    progress throw card train thirsty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
