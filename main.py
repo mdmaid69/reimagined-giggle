1fac1fac9bf79c7a45eaca6fb578112c499f5140
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:22 +0000

    reply telephone bottom lead cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_bool(array):
+        return bool(array)
