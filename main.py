25c8387b5d158509053454f79662a44f1f671c3d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:00 +0000

    properly show OKorokay event difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
