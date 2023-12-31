cb12a9e384a92b5605640fd79725d543d67cd856
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:01:05 +0000

    demand block no succeed yesterday

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-def count_words(sentence):
-        return len(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
