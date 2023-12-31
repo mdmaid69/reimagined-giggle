1bfcdad96e2dea3a17a6125189c80a694b3082b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:04:50 +0000

    without air join up hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
