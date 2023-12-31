313e9e681a9435833b23a9a3e621288a1a4731f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:11:22 +0000

    sense perform state hurry high

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
