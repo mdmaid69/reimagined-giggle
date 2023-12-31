1d405cc56e9166180435b19739da0b40721585fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:45 +0000

    try certainly gather generous fail

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_str(array):
+        return str(array)
