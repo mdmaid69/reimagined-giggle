02c53598abb78113a4d759395d1626715f5c8aa8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:59:01 +0000

    start cook behaviour 564encourage makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
