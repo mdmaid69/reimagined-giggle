57e7fccec40734f61f8186fc696bc9d8a7acdfb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:46:32 +0000

    strange last slow square bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
