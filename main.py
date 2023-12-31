fdd6c45e12f91eff0260038c76b072e797725b9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:33:40 +0000

    drink trap accidental next except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_complex(array):
+        return complex(array[0])
