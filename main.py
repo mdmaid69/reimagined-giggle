86ef8b36ffd48173cb14a0f841fa2cd2d4252b4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:00 +0000

    worst tear act speak distant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
