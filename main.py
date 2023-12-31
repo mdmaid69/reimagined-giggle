9a063ebf7342519287ed0152434ef059adfa195d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:49:46 +0000

    turn week soon chance realize

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
