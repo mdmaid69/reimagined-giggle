739e6b17b50052d8490ea87b7eb900e719c786d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:55:05 +0000

    sting manner start trap serious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def convert_to_binary(n):
+        return bin(n)
