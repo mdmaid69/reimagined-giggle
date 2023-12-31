58e8eecb3ac2c25ae9466927fae59927bb7eb914
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:26 +0000

    shout solve area object complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
