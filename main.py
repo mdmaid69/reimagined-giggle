dfa850b3d50bdd5b978c5e00cc610c159bee540e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:30 +0000

    hide minimum hang more healthy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
