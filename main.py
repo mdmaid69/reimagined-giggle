a58cc8eaa09e5b3911752bdc07d0e0cd73e94f6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:53:49 +0000

    basically contact snow flavour certain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
