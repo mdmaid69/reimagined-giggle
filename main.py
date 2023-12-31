207abe0b80a094cf700a3fd8917d4cd5fffe079a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:44:56 +0000

    slow attempt waste play inform

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
