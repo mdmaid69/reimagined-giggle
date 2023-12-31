7b11d914884de5e59b469fd818fb5bb4c52b3879
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:26 +0000

    exercise confuse too twist loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
