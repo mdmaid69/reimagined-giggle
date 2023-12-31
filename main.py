dbfc37d71eebc71daea00f6875f7a96473166892
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:35 +0000

    pay false spring nothing cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
