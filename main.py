cf57f6554589aad4327ff869eb8ae6c3d03d783a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:46 +0000

    unlucky market finally cheap punish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
