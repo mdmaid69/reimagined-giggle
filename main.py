8e513696bfb972215dd3bd9d5cfad269fda48368
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:56:01 +0000

    during doctor know entrance thought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def reverse_list(lst):
-        return lst[::-1]
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
