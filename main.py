83c7e48b8fb8016f0b099d479b604589fd6ddf6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:38 +0000

    contribute fierce hang everyone fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
