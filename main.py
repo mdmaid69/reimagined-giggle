d5f498bb3925787b6e097e9b66cc2e974816e2ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:21 +0000

    comeout twist afterwards fear total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
