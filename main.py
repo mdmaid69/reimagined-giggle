3b29e5c00247cbed121ed9d99dd82afa5634637e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:53:00 +0000

    still same sad honest play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
