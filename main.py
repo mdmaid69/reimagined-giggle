e6bad4909ed416ed1aeb97bb3178f7e732968b30
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:42:50 +0000

    treatment would mistake build shallow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_list(lst):
+        return lst[::-1]
