0a74d9a087a01ff652098c12541bd3a6aabf1c49
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:31:30 +0000

    doctor burst shade sometimes could

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
