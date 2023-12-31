7ff6d06c6af257a53b0c73b0a2c071d694355104
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:53 +0000

    father Dd regret finger popular

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
