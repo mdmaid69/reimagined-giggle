bdea2f710f11b6c2607383f020b986fbca3269c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:33:25 +0000

    young think exchange cry company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
