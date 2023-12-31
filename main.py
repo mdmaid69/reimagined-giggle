1b1ee205363acd573c14588fd9552fadf6513423
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:52:28 +0000

    wash improve strong continue medium

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
