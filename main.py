9898de046b441677d14cd74ac1b4531d2726cab6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:32 +0000

    direct few left disturbing winter

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
