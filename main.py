a7da73a538247b962e2974805b780d29b3c4f351
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:31:15 +0000

    practise nigga conduct debt mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
