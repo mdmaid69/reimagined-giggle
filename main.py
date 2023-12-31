ef30f4e2919b6eec80209d3d8ba742dac3ef1d90
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:32:59 +0000

    feeling holdup separate but male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
