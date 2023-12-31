87d515ff6ed033e10d78979f901815799e7d3ed1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:06 +0000

    gap agreement slowly no sweet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
