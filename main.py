81392aa363df29a67d87ff8cc947b95a11420458
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:37 +0000

    fat yours strong disappointed early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
