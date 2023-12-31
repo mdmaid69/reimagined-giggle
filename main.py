871e566b8dbbdccda7e35c5ad14aa666f8f154e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:59 +0000

    however fine term weigh hate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
