40e78873f5914a6bb462f8e0d98c75468d2c9f30
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:44 +0000

    challenge effort all description say

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
