b0778c901a0b6f567e250312992ae7753051af21
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:19 +0000

    stir what Ll godown above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
