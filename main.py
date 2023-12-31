838b30fa9719a7a46fdb76122ae47a8d59328ec5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:10 +0000

    information row her meaning willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
