74d1c20b384d495ab9615f6966f57b6492536ed3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:06 +0000

    gas division shade union breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
