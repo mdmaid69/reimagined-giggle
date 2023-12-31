52b848f56bc27d312bfefeac786d5ef64f53ab0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:27:42 +0000

    plant private tooth understand free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def sort_list(lst):
-        return sorted(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
