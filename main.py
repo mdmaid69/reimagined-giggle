3a8fdc62f37b140efa57af8dcb57fee6bbe35db2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:42 +0000

    whistle stranger supply claim cinema

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
