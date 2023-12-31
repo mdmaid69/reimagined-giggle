6660f0ead657060710bbcb6a167a8e19268fd694
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:13 +0000

    unlikely entrance young rather instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
