f102e2a0112fe94e5fe9b00e1b5335fe21711f20
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:32:12 +0000

    goon holdup wash plain week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
