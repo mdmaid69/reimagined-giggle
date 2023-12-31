baaf88aff996c8d15091f561ac3a5be8eaf79c02
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:51 +0000

    familiar congratulations normal ask reason

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
