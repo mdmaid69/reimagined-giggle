bf5a336b7b1e0b95a290c10f67df9bc89f5e2c7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:23:26 +0000

    warning injury thorough light hurry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
