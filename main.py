b88d23977d2b1974144fc4d86c7f630b413c4d30
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:11:48 +0000

    contact warm event new life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
