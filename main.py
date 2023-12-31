e3d016cf5565e285e821d9e63242df74d46fdb65
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:26:33 +0000

    responsibility serious now punishment spin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_min(lst):
+        return min(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
