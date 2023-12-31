a60bbc2da064847fc2d698e17ad49bf365a834bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:08:50 +0000

    extremely especially conclusion nor important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
