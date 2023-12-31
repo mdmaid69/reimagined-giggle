bbccbef1951b3c81962cfe1e0c7e28eb799d8b73
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:08 +0000

    describe freedom build react learn

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
