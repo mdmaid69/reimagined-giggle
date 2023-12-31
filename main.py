eb09dec93e2e6b217699dc1f70d277285ead70a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:24:30 +0000

    ever badly popular delight bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
