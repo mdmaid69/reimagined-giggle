e3d6c6c4ca53ef9bc905494cd425540d7b508e20
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:48 +0000

    definite pure damage recognize refuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
