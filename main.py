dfd63211a67beed3b3d1dd5c8b9f1b6349733dc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:56 +0000

    much suit beat memory listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
