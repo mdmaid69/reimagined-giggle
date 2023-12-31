16fe7c99db96e5b9dac05bb095a805127db800f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:15 +0000

    now really young mark receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
