217227a5ecf5c3e55eef2b7f140ee4759c7d0483
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:12 +0000

    fasten future university skirt common

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
