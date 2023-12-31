9601e814c93f807c71451e7ea74d4c066c687f01
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:38:34 +0000

    petrol record us noone age

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
