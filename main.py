c8193a2781cd8b1f80b1d6958fd6d001edead6b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:31:05 +0000

    hard possible spring emotion arrangement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
