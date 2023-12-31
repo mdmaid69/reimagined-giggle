233cd728e1346f4e8ed6dea3ee23cc8446f77573
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:49:26 +0000

    far Ff either tall split

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
