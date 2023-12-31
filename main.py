3929a5c964d54b1252fb66a5d1ea14b0d2829bfd
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:46 +0000

    follow manage job tie instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
