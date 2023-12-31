ad0254131cb35a3cb0524d97cdf030a7ed3a3b3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:24 +0000

    free practise aim injury difference

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+name = "Python"
+print("Hello,", name)
