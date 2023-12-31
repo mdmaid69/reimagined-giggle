9709b8f80988865bf11352f093db60177c709b64
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:42:03 +0000

    today invitation skirt necessary meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def convert_to_hex(n):
+        return hex(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
