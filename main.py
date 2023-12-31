68063d8cfd59b60e67aa487d57609896612a006c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:25 +0000

    dance realize depend herself scheme

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
