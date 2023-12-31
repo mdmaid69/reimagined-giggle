c57aa6c3af8e2beb4b140e1f0ed1d8af09d407ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:37:03 +0000

    sure whom getaway score pleased

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
