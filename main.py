664e0956af2e3cb5c614a77991de6c515d5ca7a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:05 +0000

    share memory sound wander week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
