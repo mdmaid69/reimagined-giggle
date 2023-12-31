e0502fb95670c4420d2c5172d6ce2a2935ee0996
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:01 +0000

    wild do illness comeout face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
