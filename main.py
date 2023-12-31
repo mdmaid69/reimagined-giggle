efe0b650695a7ac3ae95c49aa1159f19e574c04d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:47:00 +0000

    minor criticism blame mail Nn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
