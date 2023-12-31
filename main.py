23bfb47e01a20066364621c7cbcb04d69acb6092
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:48 +0000

    wherever few disturbing shop discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
