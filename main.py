5f7ae4650165f6e29cd0ee7d69ec3136362f2e07
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:22 +0000

    reasonable window square standard trousers

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
