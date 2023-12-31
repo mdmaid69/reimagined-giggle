1d4af58ddf1f56729cc1f8b6e20ca39afe5b8d40
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:34 +0000

    worry here whether call than

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
