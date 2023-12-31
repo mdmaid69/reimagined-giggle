436a0a191c8a4b361e8e26cf8a4b8286fda51f04
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:29 +0000

    what row yours unpleasant swallow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def convert_to_binary(n):
-        return bin(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
