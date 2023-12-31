7b2c8d489d10473cb32b01e153247699a9745d90
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:13:03 +0000

    contribute design pair discover swallow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import sys
-def exit_program():
-        sys.exit()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+text = "Hello, world!"
+print("Words:", len(text.split()))
