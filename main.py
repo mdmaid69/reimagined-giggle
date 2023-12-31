cbdfb2ad6710781fcba412615c57721645449c27
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:32 +0000

    makeup mystery sink convince treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
