77333c55468640fadbd97255ba9f5b6ddba00d9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:47:00 +0000

    soak split pretty natural weekend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_volume(length, width, height):
+        return length * width * height
