0edb866fecc2e1e56b4fdafa71468ef35087e5fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:30:05 +0000

    switch effort really lookback(on) 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
