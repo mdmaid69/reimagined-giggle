805c83a66714c66360dfe0eeb622dc33c89da49e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:08 +0000

    pin busy teach announce talkback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
