c2bb8799c84767025ebf706b5d71fea0ca98ee96
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:27 +0000

    here pencil second find forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
