afd0cc9e78f369594c83ae6280a0c4e372ab0774
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:48 +0000

    insist inside face influence OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def add_numbers(x, y):
+        return x + y
