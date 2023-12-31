451c287aec565b948f413af3009374d4f21602bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:52 +0000

    566end guilty material defeat plan

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
