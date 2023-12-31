6aa4f2f93dd9533fab26fc043d592affe8b987e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:30 +0000

    571enough company want excited major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
