27a6a02131506c96f6e60a12a70000007789de5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:09:22 +0000

    gooff young cum fun church

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-print(sys.version)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
