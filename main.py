6228d65672d00dd611ddfdbc2ba45246cc0b0480
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:46:13 +0000

    behind birthday urgent view trap

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
