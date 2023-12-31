005b16151b328c95297f33e3e764e0fccb0cd6f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:57:22 +0000

    negative along actual gradually corner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
