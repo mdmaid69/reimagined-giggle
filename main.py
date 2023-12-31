93e6d45560596b3ed7e759ef71a01fe12c99e597
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:33:11 +0000

    flat breakinto deliberate spare breakfast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
