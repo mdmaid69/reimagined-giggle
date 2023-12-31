d6d5cd38c37f7b65336acdc794a1ed91f9e92373
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:04:28 +0000

    smell shelter pot club getout

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
   def plot_scatter_graph(x, y):
         plt.scatter(x, y)
         plt.show()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import datetime
+def get_today_date():
+        return datetime.date.today()
