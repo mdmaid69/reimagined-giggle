bdbefb9db00c940b197ff4ff8f9bab89bb4991bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:28 +0000

    interview burn waste minimum fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
