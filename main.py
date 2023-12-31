e8d2e64e7406d21fbf8120898dfdbbc681f1f99f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:09 +0000

    tight regard experiment nowhere sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
