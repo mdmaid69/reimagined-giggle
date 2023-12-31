69e5b25b21113832bff8d644237afc30928213c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:07 +0000

    agree cheque fail if freeze

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+print("Hello, world!")
