a9e4516aed97a5150a2e972958042b03aff59fba
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:38 +0000

    view stir frightened appear hers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
