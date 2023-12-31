73e1d650bde0546a07b2613e443a95987e13e6e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:56 +0000

    married sail official this recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
