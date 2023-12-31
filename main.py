b264643009105c1460ceef1c44dc7530b7082b57
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:21 +0000

    forever whoever breakin argument shut

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
