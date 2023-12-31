dc11464aa74d82d391988891bf7b9660c49dc53c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:05:19 +0000

    environment nurse dry scream thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+for i in range(10): print(i)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
