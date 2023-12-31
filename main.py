21070b059457d1278b396b5d597da3b9f35763a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:55 +0000

    fall nigga easy life taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
