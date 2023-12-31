addc517cb071a5d0fee8972934c01f48a0c6c3e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:52 +0000

    treatment union since branch mark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
