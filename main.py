dd3a2a8646191c286537b4b9c0b83c781239bb98
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:57 +0000

    famous expression oil hurry exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-print(math.pi)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
