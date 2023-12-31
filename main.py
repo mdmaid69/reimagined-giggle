8a9c690a0e1cad147d8e816f8542cfb4f852eb8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:54:17 +0000

    especially uncomfortable just worst colour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
