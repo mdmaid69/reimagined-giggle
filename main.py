dd3b85f92e683631593f4f0b2e47bd30c6eb010a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:27 +0000

    for makeinto great wish odd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import random
+def roll_die():
+        return random.randint(1, 6)
