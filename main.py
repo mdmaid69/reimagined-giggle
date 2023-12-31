6cb101346049a32dc835db928c78b181d2d6efd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:21 +0000

    earth enthusiastic divide getout day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
