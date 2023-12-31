45ee0bfcc1bd442a6fe609989fbc420e70e91409
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:52 +0000

    identify even book wide natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
