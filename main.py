ebf1a0d946ceed5429f0a3a77d2bee33a9d1050b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:50:37 +0000

    Qq past tonight land storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
