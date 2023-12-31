983d514a9d29e6d75ec8498bae9059015e439ae2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:41:08 +0000

    relative seat tear involve freeze

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
