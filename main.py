91e7c60150ef35c77f7510740d3c2f6cf463552f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:35:28 +0000

    hard office lesson refuse unfair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_octal(n):
+        return oct(n)
+def add_numbers(x, y):
+        return x + y
