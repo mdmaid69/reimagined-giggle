33a46f1ad05eae5d0eaf65175237f877ffe6db7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:40 +0000

    country production feature opponent teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
