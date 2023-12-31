30b9e881a5c33bfed806f3c92a6d7f982bddd59d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:14:28 +0000

    nerve praise birthday clear every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
 import collections
 def create_user_list():
         return collections.UserList()
