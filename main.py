c73bc39e43ac1b2ceb37f7dd2ac7326ad5005bf7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:55:13 +0000

    split hit because die inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
