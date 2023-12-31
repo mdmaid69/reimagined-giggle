ba1043986046680f34f4ebe012443f2d9eb3f783
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:55 +0000

    remember whenever coffee reason importance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_float(array):
+        return float(array[0])
