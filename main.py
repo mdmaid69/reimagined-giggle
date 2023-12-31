74e331b817f37f7caffa355d223a7744915c2be5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:42:11 +0000

    spoil whistle tool special rid

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
