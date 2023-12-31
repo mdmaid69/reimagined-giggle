5d91f57d256bbbfa47c41e6d2856aaca45febbfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:00 +0000

    delicate beat secret appearance help

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
