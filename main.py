1b924254c1f47e85c9a701d80573569dff92e7e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:12 +0000

    he general bath shame both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
