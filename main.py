f60417d07b5759d13faedc3eda5b0523512001a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:39 +0000

    name dinner cause exactly middle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
