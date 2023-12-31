032dd2d70f3f87ff26b1ec41d798f2f56164628d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:23 +0000

    mother thirsty violence steam busy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
