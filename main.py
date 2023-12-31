91818e790c842d8a5d8c2faaaedbf910a9596ae3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:20 +0000

    feeling declaration model battery punish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_string():
+        return collections.UserString()
