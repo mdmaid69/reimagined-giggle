827de06b4281323a494ecb80dc3b65c791285210
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:21 +0000

    matter cheat quality gentle branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
