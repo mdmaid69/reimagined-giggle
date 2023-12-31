caa8ce36adac7aeafff9208c043b9ffbfb9df458
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:17 +0000

    bringback traffic lend last thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def square_number(x):
-        return x**2
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_str(array):
+        return str(array)
