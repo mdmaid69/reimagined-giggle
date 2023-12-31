7e4116fd62dd1f432d4881586fd4ed7e1c9cd482
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:22 +0000

    information safety shall copy scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
