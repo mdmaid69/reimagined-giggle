b575971ce508ca374fc19fecbe2ef716bf6e6d8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:33 +0000

    programme second might noone there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
