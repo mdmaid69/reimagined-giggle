171dc5e22fd0c5d64eb7dfc6aa8531429e945a59
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:50:00 +0000

    deaf separate dance age violent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
