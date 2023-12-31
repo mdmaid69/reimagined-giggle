96ef70e001a83348e0bcdec367c06345d9bf240a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:22 +0000

    again minimum this force offer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
