e197c9c190a7bd44a91c4604adc70450f2f0e5f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:04 +0000

    method us respect prepare description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_file(file_name):
+        os.remove(file_name)
