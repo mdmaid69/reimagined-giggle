212ddb97f96778a34a4c8a4cf17f1ec2965946eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:36 +0000

    congratulations damage space sign soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(numbers):
+        return min(numbers)
