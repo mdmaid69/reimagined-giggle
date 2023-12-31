95b30dd2e6f1ca0ea3d9069166e83aa7fd15d0d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:09 +0000

    pleasant translate inquire string provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
