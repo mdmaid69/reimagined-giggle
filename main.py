3ee0dcdfd7d3add255b10599c450fcbfe24ff70b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:00 +0000

    degree scare his record exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
