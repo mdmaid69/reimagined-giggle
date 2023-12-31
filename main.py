8205ef2a0d4a19c2850564c83d0d392f5ed13bd8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:39:41 +0000

    adjective gooff discovery opinion preparation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
