b5a9e219e5d95a654c3570f6e09d1f0509ce42f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:33:34 +0000

    correction go takeback confused match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
