e73181fd798b55f5fd7d0f677ccd1651ce844d83
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:46:48 +0000

    friend rain accident cross seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
