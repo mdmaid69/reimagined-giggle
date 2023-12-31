a9b6a200a4acd9dc3dc3439054fd41328e22b730
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:35:57 +0000

    slightly Ii lot meeting now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
