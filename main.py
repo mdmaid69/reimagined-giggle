26aff1c34c8e515058b0f5b368173ac2b1235f30
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:35 +0000

    stamp mine sound shoot spread

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
