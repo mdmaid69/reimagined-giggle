5026ee728df1023f3a56c09973bb66b05da4e895
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:26:27 +0000

    open sometimes quality loud drawing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
