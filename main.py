9405ca1861398330fda3c9855ee12eacc970f478
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:45:48 +0000

    path shadow church improvement clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
