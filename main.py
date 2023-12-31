d2f48ce0ae20dc776074bab2f71b5f9d6a45b2e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:02:08 +0000

    long theirs even sort blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
