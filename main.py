120c79e3e2a6759bafc88a2cacbc64f7e1b7734d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:57:20 +0000

    fault long left tense direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
