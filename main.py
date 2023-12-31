5b58d0a6ab191a073148ebc742fd008a31c36d97
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:49 +0000

    bringback instead instrument human escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
