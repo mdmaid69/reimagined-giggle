2498ded4a2603744e6460a1824df84119fe14920
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:21 +0000

    roof force Ww shoot better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
