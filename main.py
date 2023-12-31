ebc2279cec69820ed4528964e12b266c8aa98f88
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:45 +0000

    female all easy book read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
