fa7144e584db345170b0a37da23d08bd1a33b70b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:23:48 +0000

    godown 572enter sight agree run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
