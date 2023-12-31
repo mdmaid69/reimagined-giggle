791eec960ddc392a93fc442e2bf5b06637014fed
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:11:28 +0000

    announce embarrassed better first respect

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
