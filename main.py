4dd1bd80cba280d69841b7e5648a57e555174df2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:37 +0000

    skin by pass holdup jump

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
