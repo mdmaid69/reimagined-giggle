a1af7ce6539a9a3f66857f27297ba770364c1a8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:55 +0000

    hard different run pride far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import numpy as np
+print(np.array([1, 2, 3]))
