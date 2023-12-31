557fbe41356612ed259290a00b090c54363e8c4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:28 +0000

    control practice appear exactly account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
