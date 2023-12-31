1f55b857b823c61eab3868939f547371d6e73b64
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:06:11 +0000

    sweep to Zz butter force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_item(array, i):
+        return array[i]
