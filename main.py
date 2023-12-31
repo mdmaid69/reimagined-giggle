d3b83d15e80a63b17c1e521b6ae11b3df4be225a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:16:12 +0000

    issue only guess long taste

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_area(radius):
+        return 3.14 * radius * radius
