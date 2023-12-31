4f4cf3086a08455f5011afb8fd0cf5d129b5aa51
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:44 +0000

    bar bag right lift forgive

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
