d7b4a1c601cb8b4d365552cca616eb01dfeec58a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:27 +0000

    natural look pupil large father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
