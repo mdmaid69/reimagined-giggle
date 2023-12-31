9533bf0c80a4b19ff580f2c89cae29a7da0b60c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:13 +0000

    calmly through criticism do happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
