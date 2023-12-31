ecce01f18fc7fa36aa63f575b489ee13c17fad17
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:47:25 +0000

    spare patient onto lean prepare

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
