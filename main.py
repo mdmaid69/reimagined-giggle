093d887f6d4bf665d6debf991bd76580de74f7b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:13 +0000

    anyone score risk build suck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
