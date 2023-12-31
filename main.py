e0672242c66ef29fa93004728f1dacc3c75d74e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:15:16 +0000

    difference dig file wet bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
