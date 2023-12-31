ed0d4a71af768e68870112e6cae22ab662daecee
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:10 +0000

    agreement takeback properly smoke tidy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
