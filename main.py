de642fc24f60050ac5db8ddcb07649d9a554d606
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:30:49 +0000

    herself dress turnup violent freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_area_circle(r):
+        return 3.14 * r**2
