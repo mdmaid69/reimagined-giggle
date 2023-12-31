5fcaee27ee1adb84de65c3899d2cf7a73c524733
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:16:57 +0000

    effect disturb love boil town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
