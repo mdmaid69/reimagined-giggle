c8340ee2a86bb2e10c4cf2222e30ae281f504bad
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:43:41 +0000

    style invite goaway know picture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
