007136590bb0501d2ddb304f46f45243bd164bc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:54:59 +0000

    walk swim offer connected major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
