071e8e0d04bec96789fa976c809813c45c7555f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:23 +0000

    runaway illness nerve unkind usually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
