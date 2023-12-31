9205498f5cfef7f268b3b7b38ca15a84930372e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:53:11 +0000

    brave foreign certain spill in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
