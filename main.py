80645522e4bbcb35799f222ee20ae761436053cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:03:04 +0000

    control anyone gooff warm well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
