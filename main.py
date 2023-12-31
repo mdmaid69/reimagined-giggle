96e93c18e4106b08cd95cf5e2636c1204f6ccdcb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:45 +0000

    hole frightened mine comfortable otherwise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
