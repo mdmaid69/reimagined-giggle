d62a3784efdf7cae5cb09f2ee3efa48c8663d702
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:29:26 +0000

    comeround tool suck speed child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
