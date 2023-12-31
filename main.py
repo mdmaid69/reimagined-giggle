0906cd6d5727d10605533c58eacba99074a4586f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:12 +0000

    lookafter crowded dick shopping fortunate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
