8ec7bb3f66fa534fcb8a984259eba2b45728fa85
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:34:52 +0000

    odd round leg trap move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
+  def convert_to_hex(n):
+        return hex(n)
