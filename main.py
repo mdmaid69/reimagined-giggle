bdcb458d64615821e35966698bb5ccf2c8192a16
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:20 +0000

    train deal thinkover slice nothing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_area_rectangle(l, w):
+        return l * w
