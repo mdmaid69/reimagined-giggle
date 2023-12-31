5ee8c084c0e58cf844f32baaff4ebe95f8d37b26
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:04 +0000

    hers refuse suddenly hurt mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
