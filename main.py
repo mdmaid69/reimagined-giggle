7e1588085452a7723bc67dfdad417c101c55bbac
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:04:11 +0000

    prize keen branch congratulations afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
