6c98ba2868d6670709b9745c100cdb13ce010ac2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:40:24 +0000

    Oo heat tape house proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+print("Hello, world!")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
