b28407d2d5135b33c749eb1d728103fc698422ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:18 +0000

    inform rate die too direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
