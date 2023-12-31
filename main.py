c43275fb97e6829c3044e59dbf3a1767c69ec3ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:52:33 +0000

    shame used cost let scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
