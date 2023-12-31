56a46392b543925097c468bc1cdee1c55e352ab7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:33 +0000

    identify interrupt cry sleep accordingto

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
