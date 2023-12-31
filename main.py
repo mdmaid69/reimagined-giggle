3382f359667d356e0a1e1b403d9831a8eb281ba0
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:20 +0000

    pride shock old exhibition somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import os
+print(os.getcwd())
