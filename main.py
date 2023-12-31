1574ffb36122d0fb355a55e2e27d29c8eb651396
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:40 +0000

    machine class material bath bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
