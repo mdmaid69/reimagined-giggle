0866038be4e1554a1756d7f1d5f7fce6d7d2c5a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:56:20 +0000

    cross mess try child congratulate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
