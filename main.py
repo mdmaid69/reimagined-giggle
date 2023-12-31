88271d64adebfbea98057afa691e3d97bfdb8ef0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:29:29 +0000

    office gas attention afterwards direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
