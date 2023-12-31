510b7f66f7afc22bd008741c6a4d3db712da206a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:25:38 +0000

    payment earth force stay bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
