5b037250cd7eb7a5bc5b80932a512b4660b555dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:10:53 +0000

    surprise Yy nor drive reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
