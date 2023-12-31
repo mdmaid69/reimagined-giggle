afaa2ce05538af54fa79b6b07c6c23d14be5a096
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:29 +0000

    Ss direction muscle goout runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
