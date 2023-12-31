5829bf3262052e01bf2bdda552fd3c8ad7235380
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:22:55 +0000

    able shock underneath deal effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_str(array):
-        return str(array)
-def count_characters(sentence):
-        return len(sentence)
+def clear_array(array):
+        array *= 0
