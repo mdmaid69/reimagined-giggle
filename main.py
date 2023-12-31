e93a28a837a466c4fe15ff995a8eeb6e027e299c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:58 +0000

    coffee project remark age open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-print(sys.version)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_rectangle(l, w):
+        return l * w
