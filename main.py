5e5d6152652b98a71b8012daaf3af325b0c04ec9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:59:14 +0000

    chase fashion key every transparent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
