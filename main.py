bbfb560b260f4f4218a8c8ebe85320900bef9708
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:22:52 +0000

    except wish married ours stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
