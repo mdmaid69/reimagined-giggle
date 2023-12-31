ea49129e59e2bd49db751cf334d17a00f90a620d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:30 +0000

    here lie although gooff just

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
