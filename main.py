3fde2135a5b2ddca2876551aa9f029cee1740c40
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:13 +0000

    laugh rescue loose Mm hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
