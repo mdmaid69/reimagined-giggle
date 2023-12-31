1e13712ddb71477a111c83e04fddd30e07b0ebf1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:48 +0000

    steady comeoff OKorokay bank amount

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
