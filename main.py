b4240e2610087985051066ad94ff13aaa2b40098
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:24:15 +0000

    port hospital deliver break load

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
