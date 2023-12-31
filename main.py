d56b683283bcfd71a067745c9f287c2c16bd6906
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:21:33 +0000

    uniform second fond save limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import sys
-  def get_python_version():
-        return sys.version
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
