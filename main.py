47dd8803f5934b6784f92e9e11e44cd1dacfc3a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:06:02 +0000

    survive makeinto border confidence bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
