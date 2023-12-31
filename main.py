31fe3af8bdcff294ed286a247cb04e0c2a704ce4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:40:30 +0000

    their she storm badly short

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
