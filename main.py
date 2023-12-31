bb420f616f0ffab9f38fd486a646d3ceeaf0377a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:09 +0000

    finger test apologizeapologise sense difference

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
