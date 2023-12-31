f938fa4b441b0c6ac4fdbb5d68be870c239440bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:57:22 +0000

    situation use 565end difference importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
