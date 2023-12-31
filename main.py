2ff5decf86a8af9af7b7ab4358e5331c082deb00
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:49 +0000

    one age sentence whistle clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_float(array):
+        return float(array[0])
