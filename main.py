39ee85b5e842279e9533dc57f7e21f46c3339dc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:03 +0000

    none board me able life

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
