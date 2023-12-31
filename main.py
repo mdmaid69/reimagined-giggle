a78d1705670060c12a2f966ba8b1ad6ff3eaddfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:11 +0000

    begin well Oo upstairs intend

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
