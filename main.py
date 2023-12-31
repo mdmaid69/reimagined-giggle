948b65ceb323c8b5cfa67677d770ed2fc35324da
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:56 +0000

    thinkover band handle niggaacademy lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
