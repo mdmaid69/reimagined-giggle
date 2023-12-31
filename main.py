850c12e0fd6ed055426dc9e1758cf57c81c77e65
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:42 +0000

    as disappear physical behind reason

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
