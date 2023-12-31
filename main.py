0efe0b762ececdf1ae31b7566d621eaa2fb4e7bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:55 +0000

    cost measure wonder powerful person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
