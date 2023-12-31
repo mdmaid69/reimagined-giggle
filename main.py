b1358e5eca846c40ae5076d67735345f2f5db613
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:51:38 +0000

    city ago recent point together

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
