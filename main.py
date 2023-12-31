3e9183e2f387a918a2f3048141b04dbee2a4c640
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:38 +0000

    project giveaway place theirs peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
