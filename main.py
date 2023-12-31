7210c529b945261c9311160db52f5ebe68640311
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:25:53 +0000

    miss love official such better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
