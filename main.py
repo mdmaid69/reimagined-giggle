2ac8110d8fa9a4a823d9a5c1a4634d5eec25d7c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:14 +0000

    scared opinion bye delight very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
