343055133f4fe1ff69ca706cdca9eecd4b3738e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:35:40 +0000

    reserve remain demand direction throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
