60f949f4c4d064ed89edb62fa1dfa08817da3258
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:32:47 +0000

    objection rub life shake form

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
