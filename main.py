dfa299d8c4f567304c45142a02a6959c6d5df3aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:18 +0000

    minimum aim fall fashion trap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_max(lst):
+        return max(lst)
