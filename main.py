f61149d043bb9cfdb6edb864b5d57d51f89f7d51
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:21:45 +0000

    patient interest separate guide task

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
