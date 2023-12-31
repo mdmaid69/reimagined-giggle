0fb92f639f2384bbd465b568bac7abead1abc4c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:05:23 +0000

    place holiday read concentrate conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
