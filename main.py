186e5c6a314653cd483e4a2633e5e36e18e5239d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:13:21 +0000

    equal yes argument father identify

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
