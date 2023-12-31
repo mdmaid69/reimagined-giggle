f0b44c9458d9ce413506cb41f4b6f4332d3bc546
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:16 +0000

    gate book warning replace punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
