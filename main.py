84dedf6b853a2896dae1678477fd6ba992817ba5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:03 +0000

    forgive heavy change track someone

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import re
+print(re.match("h.*o", "hello world"))
