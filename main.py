91f20d830f69b426c48582e6d149d3fc5698f424
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:16 +0000

    any imagination 179bite exaggerate skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
