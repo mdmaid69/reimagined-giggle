7543d6ee8bcce83ba1af88ab1cb275dbaf9955ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:48:33 +0000

    smile hard length vanish callout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def reverse_list(lst):
+        return lst[::-1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
