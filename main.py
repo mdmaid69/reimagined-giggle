93225c89dc435812818b0dede471502e547e8ba3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:58 +0000

    little arrival pleasant spring bet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
