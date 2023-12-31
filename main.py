713e5d3cb77459d450801771ccdc218c00b278b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:56:20 +0000

    error plan catch contact branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
