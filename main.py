d192849e4389b428edbca5da7eb7ddb1e220d8ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:43 +0000

    anyone unlikely force tend roll

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
