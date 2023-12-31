015c3bb9d42cb3d92d12348a41576f9083ffe830
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:22 +0000

    point report pronounce Mm rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
