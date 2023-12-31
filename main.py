b477517ac07737a089051b0c5816845222ad9dff
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:34:50 +0000

    ilovedicks inside clever uponon stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
