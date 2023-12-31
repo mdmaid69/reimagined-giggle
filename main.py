dcb2494ecde2f51457048121bc51e25d7e4dbb5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:14 +0000

    foot fly effort press downstairs

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
