7bd5282e4bb7541a84ae8fa86bc4023069b32629
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:47:15 +0000

    loud deaf silent past comeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
