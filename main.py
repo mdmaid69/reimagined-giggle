1528c125f2c2aeee641c4c59b7910995eaefa6e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:58 +0000

    education century burst fly ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+name = "Python"
+print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
