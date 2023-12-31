a1c7175a52b86d3bfdb4a2ae58ad83d73bc7621c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:29 +0000

    appearance someone city majority 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
