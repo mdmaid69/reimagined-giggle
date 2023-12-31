fa7718f048567c151b9a7947acca17551f69c1e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:16:23 +0000

    build division runover pride otherwise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
