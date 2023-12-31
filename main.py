4b8e9f1f17abc4bd91301262f2d1b53e1a253c42
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:03:10 +0000

    fierce plan skin friendly tend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def reverse_list(lst):
+        return lst[::-1]
