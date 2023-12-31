321d2c4a01db9c1271afcc78d1a7913153df3929
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:30 +0000

    blow laugh peaceful short sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
