4f33c40115d6c5768c88c3308b40a5cf8e1577c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:18 +0000

    butter stamp workout team call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
