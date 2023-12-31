26ef448d48f750df3e15424adeb90a3e269e16b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:02:15 +0000

    square nigga nearly in Cc

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
