130740842fbe0727d46ed03b7646137270fb31c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:48:36 +0000

    roof spread point tread favour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
