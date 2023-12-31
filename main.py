e0473ffaebfc00a4303132c5790b541f1c4bca97
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:18:57 +0000

    material fit attract tip mass

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
