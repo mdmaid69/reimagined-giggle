f49dc2aefffa80b3772e0eb86a6c17781d78579e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:14:07 +0000

    breakin someone song approval bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
