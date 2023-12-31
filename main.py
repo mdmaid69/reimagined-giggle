537c525c2c30ced4e0b32344f79ec85be91deb56
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:43:39 +0000

    issue possible easy might smooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
