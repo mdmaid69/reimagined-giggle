ea89e44ac9c4a214b04a675b4072d2cd9ffd40e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:16:28 +0000

    attempt properly combine lot geton

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
