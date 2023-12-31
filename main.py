15aa3e1123da0e852708f339922a58ca0ecc981e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:49:44 +0000

    goout to upstairs couple improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
