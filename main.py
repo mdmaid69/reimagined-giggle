1b6eba66fe9f5adb2b53773e93798258b3c17b6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:38 +0000

    attract relief arrange interested calmly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
