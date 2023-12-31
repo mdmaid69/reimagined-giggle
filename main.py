4e2bfa2df95b0879fd0e27f3ff5f1fb17f5bf3a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:38:03 +0000

    steady television wind disturbing someone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
