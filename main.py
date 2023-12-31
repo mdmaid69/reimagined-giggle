dbca00fd4a6bd3f6b3d7c8519ff3f83c82db3e39
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:17 +0000

    murder thorough classroom size pronunciation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import getpass
+def get_username():
+        return getpass.getuser()
