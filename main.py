136102c56cc52c8c642e2f7cf22db5b78b3cb990
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:14:37 +0000

    correct could workupto normal soft

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
