a8de50659ebf138fa1fc19da2bbe1e3d05da244e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:14:11 +0000

    go receive check by average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
