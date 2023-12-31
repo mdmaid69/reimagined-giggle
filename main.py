e325fee9d0d436647428ba6749450f954232096b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:05:03 +0000

    public plate combination getup morning

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
