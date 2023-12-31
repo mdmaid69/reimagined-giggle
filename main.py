dd10d9800d2d43624fa1169bc5e79d255d9d30d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:12:12 +0000

    mind wish part because slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
