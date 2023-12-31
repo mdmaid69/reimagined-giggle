da04f5f9f33f2295b5a301e8f49fba9c8efb1e9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:58:21 +0000

    leave bus depend reply workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
