aa28b06e0729edc7095dbf5ebe8b98eabb59a1f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:32 +0000

    friend label perhaps wash scream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
