90f6eb2ea6d87c9c0e07c78fe859043ff2a9339e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:34:58 +0000

    interfere still spring evidence cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
