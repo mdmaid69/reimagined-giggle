7aa8446dc8cf9fb1eae5341b72efbe95210ed1db
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:57 +0000

    summer ashamed ilovedicks pocket nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_index(array, item):
+        return array.index(item)
