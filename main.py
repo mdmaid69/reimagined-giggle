505fb3d236e3bebe6c42c6f47969e100bab318c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:30 +0000

    need plate ability serious satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
