a6bb739a7fdb1d44933a42cbb428f684ec45679f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:28:36 +0000

    ours one computer university gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
