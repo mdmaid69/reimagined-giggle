49ea880bbc94b7dcdc4d6fd10685ed34a4173493
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:09 +0000

    apologizeapologise possibility calmly delicate Hh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
