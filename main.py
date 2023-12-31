af9766758937b51730b78fa163746b85d73d631b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:12 +0000

    concentrate charge suck serious hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
