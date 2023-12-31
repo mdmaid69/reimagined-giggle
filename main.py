27178ab75109239099c5caed561f87fa5e65fdf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:24:44 +0000

    comeoff present leave point limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
