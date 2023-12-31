28cee50116cfd424776f57af9315fb2095797ac3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:59 +0000

    exact tap moon force every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
