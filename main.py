92f04d5947439dd32fb2c89cf371f4e24ac1506d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:46:00 +0000

    fashion discuss advice equal minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
