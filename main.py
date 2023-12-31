8cc729965b7160227dbd9c02ce929003405fef73
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:30 +0000

    nonsense bye butter off bill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+for i in range(10): print(i)
