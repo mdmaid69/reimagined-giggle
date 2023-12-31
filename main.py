24284a40bdaa3f00bf070f1ac4887ed65c97c9c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:04:47 +0000

    talkoutof problem compete unlikely hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
