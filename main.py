1408f04d629aa452c15493a899bf4961996ac95d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:52 +0000

    eat Cc concentration fear anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_speed(distance, time):
+        return distance / time
