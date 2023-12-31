d2c635b39d77aaf01d68e0d654b840f85d215c38
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:42 +0000

    region gate disagree slice war

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
