aa927ec5889ae255b9e1cf55f3b23ec475b782d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:23:43 +0000

    sting brown because brain flow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
