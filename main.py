7702adaf3c50821d0c68e6684380d47127d36bcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:41 +0000

    form or pressure information transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
