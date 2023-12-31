a1b807ff5e27b8723307803d5b25c25cd6b2568c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:40:58 +0000

    underneath bunch getout fall hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import numpy as np
+print(np.array([1, 2, 3]))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
