c1c8800ba6f923b6dbb651526218243570c60c4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:05 +0000

    foot strength discuss post professional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
