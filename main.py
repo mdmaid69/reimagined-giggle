9c49b34d651d875e3443b68eded42e83c5ff8a39
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:03:01 +0000

    hate mark walk dish stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
