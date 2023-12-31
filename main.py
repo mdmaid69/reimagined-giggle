294813e41cf74ec9ab97b8eb39ecdea1df4339a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:28:22 +0000

    interview fix mix still actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
