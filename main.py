ac49f7c3c2ea9050d7cd04c1e889cc2266572a3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:39 +0000

    cruel nature score impressed side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
