680872e67a50e3fc12a3613f2855dae41e0ccd25
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:53 +0000

    prepare rush substance actually target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-print(os.getcwd())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_volume(length, width, height):
+        return length * width * height
