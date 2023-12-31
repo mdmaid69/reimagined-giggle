547d42ba4ff36f5072cee005dd25d6166af5e93e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:19 +0000

    question tone morning cut fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-for i in range(10): print(i)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
