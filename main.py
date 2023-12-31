8cadc7beff4591ebd49080ed0bdea709e47ab6a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:49 +0000

    search joke office give hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
