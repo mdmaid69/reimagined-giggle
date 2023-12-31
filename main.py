15a2aaf700a3d8b0e56318d10f992ff55918478a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:55:32 +0000

    roof gift formal computer runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
