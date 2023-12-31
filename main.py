89c1455cbdba82c07823612eeabfadb1472fa04c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:35 +0000

    wander regular bright thankyou promise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
