cf1ae3934c5fa3391e6061ab03100e90510505c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:42:20 +0000

    treat row separate mistake warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
