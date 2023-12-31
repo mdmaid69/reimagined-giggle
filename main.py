097ca8be3a6330845435f745180d0b44e432b77a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:14 +0000

    arm accident 565end confuse rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
