d8414ba0ad103925234abf6c0e92feedffe82a03
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:52 +0000

    mistake out objection level awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
