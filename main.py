4fbd2343ea227f34c128be4a929b434d7d2c9472
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:47:19 +0000

    appearance correct offer cheat frighten

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
