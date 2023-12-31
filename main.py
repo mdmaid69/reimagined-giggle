3c61103b267518cb2cc9e08e378aa277522b8f43
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:45 +0000

    sick score do shopping owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def cube_number(x):
+        return x**3
