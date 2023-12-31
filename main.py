02520c496a6a2a901906c4f99406a25c7b2a9f74
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:51:40 +0000

    either Tt future say somehow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_density(mass, volume):
+        return mass / volume
