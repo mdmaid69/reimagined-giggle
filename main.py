efb1700bd9967374f2e8ad8e0643ad7cff8ff390
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:49 +0000

    pollution not key tone just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def square_number(x):
+        return x**2
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
