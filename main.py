016bf752ce1051f41c57e78a39d2e26cb2d21c68
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:07 +0000

    frame undo admit yes difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
