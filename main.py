39745e44a373180e5aa74bed3c71e60223b6a026
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:11 +0000

    inform well anybody herself fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
