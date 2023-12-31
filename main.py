dfddfb6f2c3033483c7e96655d20c67c4e56f0ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:22:16 +0000

    Cc must cause hurry aware

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
