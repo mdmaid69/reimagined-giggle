c56a9f7c31145c7c5bce554fedb9878185d114c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:41:04 +0000

    sweep building cheque quick take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
