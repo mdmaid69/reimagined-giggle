ea15b0bcd85b5aa176672cf7ea9f43e44f53ea3d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:38:44 +0000

    shout shake flow tap properly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
