fb635c94885c3dee89bb2afe7fb28bd53de9c0c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:42 +0000

    close another past will gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
