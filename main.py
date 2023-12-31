1d9787c979da88d90f62504359db638fa45e0691
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:36 +0000

    yourself over heart ourselves lower

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
