35656a3487763d3ee1ae28d5c321dc5ebb3d63c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:42:39 +0000

    lookback(on) guilty sail sort doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
