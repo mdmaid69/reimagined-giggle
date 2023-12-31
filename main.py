ee4f6d108982505daf858986c0b1196a5e813f92
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:19 +0000

    date mind city woman either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
