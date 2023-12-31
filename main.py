87eae84fbe8f1b7c17d233f10d9fad2a72234285
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:51 +0000

    discuss morning noone fortunately same

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
