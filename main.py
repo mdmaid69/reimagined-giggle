265785406b102e5fb38ad5d3223d4b78b03afc24
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:02:00 +0000

    from body clothes grand exactly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def reverse_string(s):
+        return s[::-1]
