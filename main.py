75206ebcac972bbb73d6be1dc727cd70e9c77557
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:01:37 +0000

    mark steal cook charge attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
