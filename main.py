15419a014e3f0e785b3e84c153649376c8ee05ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:36 +0000

    my smile safe love week

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
