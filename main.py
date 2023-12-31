804f7721a0c60490ed1e74b9b13a726437c4e140
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:46 +0000

    private book concentrate shop maintain

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import collections
+def create_user_list():
+        return collections.UserList()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
