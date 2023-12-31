ba2bf28af7d954214752b74b4b3339cfdde3a5e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:26:13 +0000

    object traffic week sail secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
