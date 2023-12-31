0ada0b083714be074c4d48b9fdd34a05ac0fd1af
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:01:02 +0000

    scale judge further object well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
