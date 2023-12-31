eb2b25bd29e5a0d9256364a70bc5ef7beea610e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:42:29 +0000

    contact community or connect move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
