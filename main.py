a1298fb4bbf2dc5142c6b1ef0bdacd2f6f1608a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:44 +0000

    month no noone unpleasant tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
