99093c7325dbb04a57a4fed4bdfd60040d8ae8d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:04 +0000

    father geton themselves share his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
