de54007de1c3916191506fd4355a82991ac2e219
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:51 +0000

    know guilty bath system lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
