e18317418ab85ab8545f456346b1afac92a614da
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:30 +0000

    chin nice purpose crowd disturb

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_power(work, time):
+        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
