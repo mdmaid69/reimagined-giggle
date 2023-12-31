550ea3beca7f23fb710add2b52025dbc91490f8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:57 +0000

    react pay increase voice hold

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
 import array
 def get_array_from_list(list, typecode):
         return array.array(typecode, list)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
