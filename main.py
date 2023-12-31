9b05a6b1c8718d3bf72750528cf0dcafd7c70780
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:26 +0000

    book danger wheel but hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
