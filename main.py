631961dac4c706d2c9f715e92f462d80ea24270d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:40:38 +0000

    modern communicate callback mood design

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
