f16f33a35cc71c83fb30edf6e2044721bba960eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:38:53 +0000

    frame feel feed repeat character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-print(sys.version)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
