fc185384c7357bf4a9af27513969e50d10ae93d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:13 +0000

    shallow switch possibly hurry trap

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def subtract_numbers(x, y):
+        return x - y
