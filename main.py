05c0c81759d47584c8943c1e023c9a1a10aa7f76
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:57 +0000

    precise children state keepfrom law

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def convert_to_octal(n):
-        return oct(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
