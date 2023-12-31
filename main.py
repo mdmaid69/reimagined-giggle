52be001f6affdf745dfd787ecfb569f383cb2f1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:50:10 +0000

    real type shake born close

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
