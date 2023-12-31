343b3f11a71b96c6a14c277188dd9489175c037a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:58 +0000

    basic far shout succeed your

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
