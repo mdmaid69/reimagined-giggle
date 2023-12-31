9d382572a684e68eef2dc2a863269588261d9a00
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:02:20 +0000

    concentration 573entertainment declaration find recover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import tensorflow as tf
+print(tf.__version__)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
