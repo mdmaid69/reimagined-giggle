a179f90d67d8fc5e81b9787c32e776899af8e3b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:25 +0000

    combine read home deliberately breakfast

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
