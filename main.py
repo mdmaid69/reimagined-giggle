10d8fe4921120fca50849356a10921fbc3d6e663
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:11 +0000

    fortunately guide account treat street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
