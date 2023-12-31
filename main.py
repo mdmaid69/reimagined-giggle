dadae224d4fde6f47dca9ddfe53d297dbf87656c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:53 +0000

    pile freeze content consider wealth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
