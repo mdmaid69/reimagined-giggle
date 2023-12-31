4262b3615fc76201c1b3ad7a2db9d362f9873cee
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:35:06 +0000

    because family part sing comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
