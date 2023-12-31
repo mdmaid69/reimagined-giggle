39d6c4aff59dfa0b24dd3a9a5d38c0d788dc7583
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:34:48 +0000

    flow expect measure quarter own

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
