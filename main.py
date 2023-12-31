cddbf5acbf0fecc34117998758332b047034e037
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:31:54 +0000

    little exploration possibly trip love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import logging
+def log_message(message):
+        logging.info(message)
