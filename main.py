194522fcea16bf1b259b0e1fb4921aacd3cb6151
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:40 +0000

    area serious weak smell file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
