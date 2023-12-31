ac6b26669b290beeca6ddd4dd1af1e47f34f9559
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:38 +0000

    film great settle uncertain thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
