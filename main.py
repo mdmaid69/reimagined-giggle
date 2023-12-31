d772516f511ca3af258441c5e691239d3738bcbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:45 +0000

    wait downstairs bend part switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
