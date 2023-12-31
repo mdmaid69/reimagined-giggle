06120c4b3e9fb09eb85342bf4e060ef542613d2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:00:06 +0000

    same small suggestion foot survive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
