76c17889d6bd3f175017672cb68479ee67934c9a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:00:06 +0000

    considering shut famous combination contact

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def square_number(x):
+        return x**2
