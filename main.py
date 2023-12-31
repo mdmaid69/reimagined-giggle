e9744f831bf9ef830df87a06da728689e5b985eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:59 +0000

    steer war send uponon late

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(10): print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
