0138095ec903c31fdfcf8cfd3d7709236b1432bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:00 +0000

    breakin bear contrast receive church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
