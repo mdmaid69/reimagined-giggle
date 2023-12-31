ac453054621c6d411c3c7eec7820ec6de6071937
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:40 +0000

    in copy uncertain sense somewhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
