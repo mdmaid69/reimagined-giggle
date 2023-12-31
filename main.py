f14ef747bc3e3e0d4d492d22267815b71cfacf0b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:28 +0000

    pretty goup theirs scrape doubt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
