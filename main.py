5111798ff2164e15761194f5145f5943ac630c78
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:37 +0000

    smoke hold ill thing goout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
