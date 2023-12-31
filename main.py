7488b58d3e800f21e8e3df103dabfdd42938780d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:28:23 +0000

    while independent approval taste anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
