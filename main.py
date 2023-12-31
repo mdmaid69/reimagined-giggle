debb0714b73244a74e96dc5830291198278ce506
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:55:34 +0000

    gradually involve completely defend exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
