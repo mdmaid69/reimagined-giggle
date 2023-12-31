d52b5103e99ddc9a2672ad2866befa066ae636d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:53 +0000

    match meal spin whatever tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
