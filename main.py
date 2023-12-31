dbd640fbe4e428b7cb62055134b3a8da5352458b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:55 +0000

    recommend 572enter wild tradition stupid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def reverse_list(lst):
+        return lst[::-1]
