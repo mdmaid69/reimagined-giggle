15880abb2be039e1826b60936d26b6ce2603af7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:25:13 +0000

    cruel goback(to) substance see freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
