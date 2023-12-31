7abef91632a3e30a201d95bdebd2bd1c63231974
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:56 +0000

    fortunately long whoever petrol relative

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
