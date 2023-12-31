7245bdb6343ef71f9c8146c52b195ae8d655f6a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:28 +0000

    type born evening factory to

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
