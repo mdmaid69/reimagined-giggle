62dc2ae0a97f4abfa0e5e783308a42d30d483a27
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:47 +0000

    limit ahead degree sometimes happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def is_palindrome(s):
+        return s == s[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
