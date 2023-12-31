e7eb115ca701b1db7a2d6e04ee1918948a003426
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:10:18 +0000

    loud centre or responsibility shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
