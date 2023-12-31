1c0c2b860b5da5fb723ab7a478185a585ac4ec83
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:21 +0000

    seat do stop reliable 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
