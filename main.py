9d773c235972312f6ce5bc46b0ebb6be0ba2be55
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:16:55 +0000

    before perform hide physical doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
