fb7915507c14b92e4300d584670540fd397ec984
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:00:18 +0000

    dish go aware quit cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def cube_number(x):
+        return x**3
