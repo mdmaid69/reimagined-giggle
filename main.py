82c8c23effd8023c8edf5952fac93a12f62477b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:43:31 +0000

    fasten home proud if fill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_array_to_string(array):
+        return array.tostring()
