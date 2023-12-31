23680190ef251f2f5cd0a6220f83cdf61af8701f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:55 +0000

    back rather achieve view light

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
