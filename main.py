70e2389194ab9ee30abb317682c9924212a799a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:50:20 +0000

    our uponon figure heat estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
