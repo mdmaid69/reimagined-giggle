79a0c3673b6393978c0a22b90fb8156fa7e6f6ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:13 +0000

    original smell both any total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
