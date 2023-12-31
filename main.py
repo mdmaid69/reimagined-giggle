7508ff8e15bd93e6fb83aeae9f8093769d2af003
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:54 +0000

    equal exaggerate impatient strong uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
