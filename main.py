986adb7ef57cc6e453447686814ca2c957f08a55
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:14:51 +0000

    silent rain cost behave produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
