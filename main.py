ed7cde06413119f53de8bdf8fb7ced78d9a3c1db
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:40 +0000

    listen tall load proud show

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
