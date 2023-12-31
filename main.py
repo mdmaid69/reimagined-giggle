f4801d90ea45f1c3cb519f126b5eb03c2c4fd3c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:05:28 +0000

    dangerous top light old accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
