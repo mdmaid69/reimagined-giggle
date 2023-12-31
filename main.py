04ff84eabda010ea0d815bea87c329dc0a11861a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:41:56 +0000

    recently but busy colour raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
