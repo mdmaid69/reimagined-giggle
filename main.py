5ed197a3d1bec4642816e4ef4483b69f258f1949
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:24 +0000

    indeed our count either discover

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
