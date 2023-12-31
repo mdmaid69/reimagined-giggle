34adaff18bf6b58028e5123499f376c74675eadd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:05:25 +0000

    combine play left keepout later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+for i in range(10): print(i)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
