5524d7374e8cbb4104855be6ce9704dcc252a41a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:26 +0000

    declare long valuable spot gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
