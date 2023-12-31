0902b57bd5f569bf859379ae2ca72ee6e8dbd945
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:52 +0000

    contrast information goup goout 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
