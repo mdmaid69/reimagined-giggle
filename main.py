7b4ba2427d9e8c54cdc72908a1487d17fb862c36
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:01:15 +0000

    deliberately me every lead culture

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
