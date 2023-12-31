c46f67d6696df4ab60a55ce9fdb128c5b37bf475
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:17:54 +0000

    wake tour discuss letter here

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
