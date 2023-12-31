2d5d31c5d3cd071f2bd9cde465ba81ade41f5863
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:25 +0000

    shake persuade see comedown dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import datetime
-def get_current_date():
-        return datetime.date.today()
+print(datetime.datetime.now())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
