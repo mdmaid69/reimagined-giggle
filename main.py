d6ad46204b3cb4909eca84a641fc7305420ae8b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:55 +0000

    nonsense fall right there only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
