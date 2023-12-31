45ec3c2a88e7ae5bcc13d77609f572349164af8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:53 +0000

    expect feed priority long disturb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_max(lst):
+        return max(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
