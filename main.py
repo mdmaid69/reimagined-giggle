59347d5ff3c12b39d04fe975bfdc8a737c0d7ad7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:46:40 +0000

    catch better trip whistle gradually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
