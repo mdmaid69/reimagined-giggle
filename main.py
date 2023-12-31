aa2f14c59934bdd68377ca0d1e8735a85422aafe
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:19:33 +0000

    air somebody embarrassing roll formal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
