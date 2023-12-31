2e02469219467363c7602717fe93e4e6df989156
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:01 +0000

    damage nice entry help modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_stack():
+        return collections.deque()
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def delete_directory(path):
+        shutil.rmtree(path)
