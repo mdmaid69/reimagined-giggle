4cc3cdadc6cbd7b345602c92de56d43f32ca4e86
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:37:50 +0000

    bet arrive spot say notice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
