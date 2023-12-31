5e3969192bb8b96b73a2db495b7a4ee93c71c093
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:18 +0000

    talk figure habit little familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
