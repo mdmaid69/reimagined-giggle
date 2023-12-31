084885264b3bc2464b529b52e0dbc8a5322ef332
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:25 +0000

    tap direct disturbing hair wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
