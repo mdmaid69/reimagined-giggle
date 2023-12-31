3a8e39c6d62bed88f2ad209e16453f060a5b45d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:45 +0000

    sea dream crowd tap Pp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
