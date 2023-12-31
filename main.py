a88a864204d01e3aeb88dd017ac9243c504fca55
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:57:11 +0000

    call cheque story behind card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
