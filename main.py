bd36be39fc27bb6ba80e39d96afedbb23111bfb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:00 +0000

    impressed change rent spend perfect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def cube_number(x):
+        return x**3
