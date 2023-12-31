784e9a14118f6e199961d5d9b6959618d74fc42a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:55 +0000

    finish mouth pinch model matter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
