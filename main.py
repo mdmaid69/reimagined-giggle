c69f54b0c15191b32803a01e36373d9325bc8e15
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:28:52 +0000

    closely light mark before characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def set_array_item(array, i, item):
+        array[i] = item
