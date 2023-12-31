492815776ff4d9282d8eb56ff4c42f65910fe2fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:47 +0000

    common blue afraid claim comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
