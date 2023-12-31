2661a48cc82751a4066a7f7e77a903f3f7275c40
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:54 +0000

    connection scheme deep little consist

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
