7e229008f32024cbf0cf23a56c871a6898f29a06
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:16 +0000

    fond talkinto previous could cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
