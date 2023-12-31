3e966ac0efaa2c77c5b8a4e26fa6721671ae4623
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:30 +0000

    straight country confusing dream shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_tangent(x):
+        return math.tan(x)
