ce940984a6382297f4da0737157b230bac3e434d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:34:18 +0000

    punishment plastic nearly slide your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
