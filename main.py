25849855d03296db74716a34bb50e2696c5452d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:36 +0000

    church both love recovery winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
