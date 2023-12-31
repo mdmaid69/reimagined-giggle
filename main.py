5c76031d14083e111aa00138e5b8134454ab31bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:05:10 +0000

    sort pity ride support rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
