0a557ebca4dedd8e25213926020199ebb8e3f1a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:09:51 +0000

    holdon stare race connect present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
