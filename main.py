9a7cb7809f6ce2949b8d1232037ad9e9a5e9db60
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:55 +0000

    fashion but better cook emergency

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def count_characters(sentence):
+        return len(sentence)
