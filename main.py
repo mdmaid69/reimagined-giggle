9a6cd0f3cc34a02f9454754e4ffebd73d3c405df
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:54:35 +0000

    organize book along knock cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import os
-print(os.getcwd())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def square_number(x):
+        return x**2
