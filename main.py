94b0b6d79cfb1d052a32b576169bf0e156047988
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:35:29 +0000

    low natural awake neither doubt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
