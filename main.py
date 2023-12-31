1547464d45aeeba0d1a9acdfca3a5cad930a5f49
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:18 +0000

    bang dark exchange in fast

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import collections
-def create_queue():
-        return collections.deque()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+i = 0
+while i < 5:
+        print(i)
+        i += 1
