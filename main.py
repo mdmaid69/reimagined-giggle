10a56982988cef31e8b74e1175df4b5a9b6e3f28
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:20 +0000

    getup do smell through exchange

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
