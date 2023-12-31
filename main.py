6fc8c46fd83cfa3b03ac31358558e3707f8371c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:44:21 +0000

    hurry pen damage wave warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
