da3e5125d5962632c8f45634d42f6f6cfe537ae8
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:33:16 +0000

    include convenient buy whoever hurry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
