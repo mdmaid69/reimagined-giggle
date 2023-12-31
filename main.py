e4e040e9bf6df14c9f199d0caa24fefb37fc0366
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:26:27 +0000

    reaction hollow callup maximum little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
