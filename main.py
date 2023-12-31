3607b2592dd174e9d1d4c86f307736aea9e8f88a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:57 +0000

    careless not introduce declaration book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
