34222c0807f97f8d6b08b473fbd08e5dab9f8568
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:01:01 +0000

    fire precise report right sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
