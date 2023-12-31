494bf17b698a35f5b622af376be8a6d72cdac5da
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:59:51 +0000

    realize old movement swimming common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+print(json.dumps({"name": "John", "age": 30}))
