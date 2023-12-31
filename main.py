84ff9321ecdfa32e8ce9e0bb4786820dff8f0e5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:42 +0000

    man wise original degree whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
