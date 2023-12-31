909cbc5e51c1294522407f800094aa0dd4159041
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:18 +0000

    hurt idea pupil shadow murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_base_name(path):
+        return os.path.basename(path)
