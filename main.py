32f371b13d22fe60df926c0cad87cdec2eaa987a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:01 +0000

    mail instrument since include empty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-print([x**2 for x in range(10)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
