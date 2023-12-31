72265cbd740d4bd26c3255998f3daf635f6c2625
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:52 +0000

    tend cry listenfor platform within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
