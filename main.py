207e1eacf742d6bcdaf526f36eb9094e7fc3ec89
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:32:49 +0000

    uncomfortable slight perfect belief life

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
