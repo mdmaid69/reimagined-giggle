5fd20379f02751f10e07fd6b67ac65f838e16b43
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:35 +0000

    entrance match stranger shopping cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_repr(array):
+        return repr(array)
