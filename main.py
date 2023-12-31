97551164e7feb827723433da654f484c8d78a237
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:10 +0000

    opposite hole speech check result

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
