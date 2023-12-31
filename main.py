37b1f06534281a87e9a6a417f88eee518feda5f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:24 +0000

    Yy favour mess somebody pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
