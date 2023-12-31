a7f7eff388a2e004617da81b13109a696e1b3d8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:05:29 +0000

    ever care copy our better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
+        return hex(n)
