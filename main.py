1eaa66e18c048c3103af06f8fbe89c43d44e690d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:11 +0000

    wind sing light interruption trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
