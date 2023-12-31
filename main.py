dee69f0ecfcd693d5997c053de597fa3081dd60b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:19 +0000

    discussion plastic tough opportunity issue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+print(sys.version)
