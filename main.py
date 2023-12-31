a1c79733a73c3a7cfa553c6a1fbb75bd8b064d94
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:01:58 +0000

    mysterious back black form nervous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
