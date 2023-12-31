ca49437931919182fac26d777ccb73d885b4b4e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:56 +0000

    come bringdown comeover concerning comment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
