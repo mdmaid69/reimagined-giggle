ac330d3ddcc528fc2cd2377ce2766eedf310467e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:05 +0000

    help message connection common disgusting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
