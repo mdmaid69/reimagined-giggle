f191776538a0b4e85a8acfb20a43712415b0d435
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:34:43 +0000

    seriously sense club rush part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
