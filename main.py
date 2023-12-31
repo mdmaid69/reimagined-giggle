f9b0432d6551456ab92028de8909a126ef7d6b14
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:15:09 +0000

    eye spread again contrast turninto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
