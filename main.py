6f81596ac14c5606e8a589f8dce908d97b9a0c2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:04:16 +0000

    Nn substance drive impossible well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
