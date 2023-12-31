5ba3a9d0d709c43091430425a90a5bcbe5a32dbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:54:09 +0000

    splash country store chance shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
