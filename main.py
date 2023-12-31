b131f8db076474b4512a1523d1cf640dc2f08cb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:36 +0000

    recent police person spin however

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def convert_to_binary(n):
+        return bin(n)
