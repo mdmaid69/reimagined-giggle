03aa35aa3faa169e0fd15b38a0de3bb5ba46ce30
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:15:52 +0000

    mad spend so education tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
