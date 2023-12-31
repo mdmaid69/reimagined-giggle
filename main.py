10f605acceec60ea777563270a68986417ab51ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:29:49 +0000

    off name lead quiet plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
