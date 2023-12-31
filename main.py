4dacffc12450734ccee94ff2b5996e14df831014
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:51:05 +0000

    shallow chin cross science perhaps

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def clear_array(array):
+        array *= 0
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
