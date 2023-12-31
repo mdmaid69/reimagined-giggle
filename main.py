454fdf9ee756e70a42836db44bfc725cd3690733
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:04 +0000

    square pile sound patience seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
