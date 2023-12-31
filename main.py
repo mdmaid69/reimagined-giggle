1a3a80584c7346b1335337d0d8b322bf0d8bf6f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:31 +0000

    Bb trap back smile centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
