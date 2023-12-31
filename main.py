1ede265a156a6615e981f0dc1bdbef67ec5961b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:56 +0000

    spite truth wish stand strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
