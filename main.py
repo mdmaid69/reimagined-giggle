985d3dae388061a576b337489ef25710731a0aa8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:09 +0000

    admit picture accommodation embarrassing experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
