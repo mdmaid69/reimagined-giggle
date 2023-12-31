da6ca680e6d4d6f7eac3f9b36c10d58d1724ebe6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:07 +0000

    favour switch bury knowledge object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
