3f088e4f9f383b5455015b49235d3b4a8d5d37ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:07 +0000

    trick embarrassing tip it earn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_acceleration(speed, time):
-        return speed / time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
