cbefacb064a4e96adc0f4563a69398cf9f7944ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:25 +0000

    birth prepare runaway organize nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
