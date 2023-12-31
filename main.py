e7dd366ef37f79f3e68a1d807e5dec3b7709a41c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:53 +0000

    in public write suffer appeal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
