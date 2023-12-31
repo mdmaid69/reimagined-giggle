b22e5e542b59dfe68aaa79357a3da79183acd15a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:03 +0000

    roll stranger trap personal precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
