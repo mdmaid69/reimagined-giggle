786f615bff3b14ad6c8d6452b8b5971400b43f1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:59:45 +0000

    inside average problem work apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
