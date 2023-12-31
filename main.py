ae06de299276f67aba5e699597d7c84b2f0fcef4
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:58 +0000

    branch experiment train ahead exhibition

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def square_number(x):
+        return x**2
