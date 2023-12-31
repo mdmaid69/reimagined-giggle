bd8d8295ab60b498552d6ea6ac90de31478f4e81
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:58 +0000

    habit treat extraordinary convinced ago

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
