815cd023aa988d4303c1805f6c4f7af4dfbd7531
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:29 +0000

    ignore breakinto backwards approve summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
