b994da7509eb4ca41e228b45506abcd2114114be
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:24 +0000

    shallow war snow behind joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
