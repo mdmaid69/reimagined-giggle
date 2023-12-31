77702fead10222262ce8ce6c1672d2e91fb00207
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:11 +0000

    fairly there identify fortunate height

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
