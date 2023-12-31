7c9da0502ab774cd43991c4f9377e50d0d0a40d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:54:36 +0000

    warm declare deliberate warning mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
