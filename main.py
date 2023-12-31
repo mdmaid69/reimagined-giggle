3eca80cfe227e1b8a10d0eda1cac887809bc3512
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:57 +0000

    shoot teach example believe bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
