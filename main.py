22e9ee4330840a7e9bd7b343b63ae5fae5d191d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:34:39 +0000

    suck last gradually mystery teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
