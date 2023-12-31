2323e4158d59153580b247dbc5be00658f18dfbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:27 +0000

    mainly man wide long trick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
