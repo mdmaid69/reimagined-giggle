31a1ea8b78dd9442c44d4791accae79eb1c202cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:59 +0000

    little store excuse believe ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_string_from_array(array):
+        return array.tobytes()
