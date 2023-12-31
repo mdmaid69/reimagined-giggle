082e24d36333365359f70318a7cf9d1ca0aac548
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:22:13 +0000

    section although fault hope guess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
