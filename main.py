6ee9f0de9c673e18fe2285d5a265127e972cbaa9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:49 +0000

    squeeze keepfrom experience callup notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
