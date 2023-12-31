3d2de64531b776daadd0559409c3f861482a7303
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:55:50 +0000

    official getup eye crisis simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
