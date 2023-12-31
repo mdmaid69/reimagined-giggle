8e225a9608c838a43f309111cac5e985f80570b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:02 +0000

    blue release by necessary OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
