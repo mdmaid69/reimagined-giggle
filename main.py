e81fc9d52835b5ead04681f438ee54cf5b4259b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:21 +0000

    tragedy official lonely unusual hard

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
