afbe171c7189e0b64c2e687110ed0e98d52678a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:35 +0000

    basic label style thinkover be

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
