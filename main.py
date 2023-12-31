e0c8f49c60d14e04d1c7ffe29cdb431163f3ea9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:11 +0000

    should dig once pair spite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
