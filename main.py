8c792e58ae45ea98e99a2d9b00b89647fdba6c39
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:33:24 +0000

    trouble phone home gift workupto

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+from collections import Counter
+print(Counter("hello world"))
+print(sum(range(10)))
