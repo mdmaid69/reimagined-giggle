08826344cbe825b09efe98e65aeb94162a0a9b6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:41 +0000

    upwards persuade art easy backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
