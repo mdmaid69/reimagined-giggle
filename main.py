83b8a0272e9a9963512c3ef6a5eae1aa20707e67
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:40:43 +0000

    read large gate act change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
