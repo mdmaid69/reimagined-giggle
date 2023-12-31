fa3fd1e4ab7684d4832c5ae2deafc0641aa651ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:05 +0000

    spend zero temperature along on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
