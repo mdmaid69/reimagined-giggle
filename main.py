7b25f51614f2ca1efaf14c9d499240be23049376
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:12 +0000

    extremely immediate unfair chat crash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
