c784ed587b480b464a95dad90ef2676d356d7f6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:42 +0000

    approve free keepfrom bang 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_index(array, item):
+        return array.index(item)
+  def remove_duplicates(lst):
+        return list(set(lst))
