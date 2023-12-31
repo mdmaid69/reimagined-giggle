52cacbdc2c7262064c6849ea8f2fa2fed2056dc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:10 +0000

    door desperate clue fall rest

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
