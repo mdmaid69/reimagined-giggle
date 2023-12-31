bfc020cb1b656eb109082a7f79e9b078ebb39d70
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:58:04 +0000

    near disgusting object number view

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
