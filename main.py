0b779b7869b09d8819c1672989d9b9472d5ebef4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:49 +0000

    breathe old everywhere favourite remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
