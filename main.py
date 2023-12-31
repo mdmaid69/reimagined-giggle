a5e65732616edadd6ce8a1c79581063884176a7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:17:10 +0000

    professional left day care mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
