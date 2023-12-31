1a68512256cd6afec7484197dcdab09cdded86aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:26 +0000

    exciting woman communicate recommend slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-name = "Python"
-print("Hello,", name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
