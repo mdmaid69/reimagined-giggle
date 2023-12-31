1b8670e65492b3f56991298a3cf5a51b8553b364
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:10 +0000

    cry block possession takeover attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
