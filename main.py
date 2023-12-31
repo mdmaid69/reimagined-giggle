ee784fc4ca29b6f983761a63ed0baad56af8ce4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:22 +0000

    offence Tt seat during smell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
