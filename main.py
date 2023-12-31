83c5eb74d2a8d2dad49be00846178ea62f625f4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:33:27 +0000

    board yet worth set himself

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+print("Hello, world!")
