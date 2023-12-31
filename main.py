c826202507024c97d8023542e837ce0940eda780
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:05 +0000

    otherwise fall sorry lean must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
