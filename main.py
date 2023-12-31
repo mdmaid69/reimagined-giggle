b50460a89b1f7979963b672bfb0509e453269fd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:39 +0000

    effect thinkthrough card address fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
