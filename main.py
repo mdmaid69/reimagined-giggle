4bf5053bad601788adc2947330562506caf8324e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:49:02 +0000

    swimming female depend join scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
