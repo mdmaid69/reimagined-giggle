82d49d2c20bda504d5a3151adfa5d31a8b847085
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:04 +0000

    available in frame sing size

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def reverse_list(lst):
+        return lst[::-1]
