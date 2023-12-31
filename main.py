b856732e5770da3460297b0248f5b45f14f2d7ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:59 +0000

    turnback respect union alive record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def find_max(lst):
+        return max(lst)
