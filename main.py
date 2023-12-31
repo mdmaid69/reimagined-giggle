631d4e04828b6cdfc15bc493d2cbe81e8bc5d494
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:39 +0000

    right everywhere centre rid into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
