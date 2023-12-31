33f3a0ae7614020a557080759f2769ab4734329f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:13:40 +0000

    wall succeed arrive disgusting turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
