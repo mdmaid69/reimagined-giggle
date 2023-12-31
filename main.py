adc1bc99f6d11e7f9e8d485d3bf71470eb488f2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:51:35 +0000

    change criticism pressure feature classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
