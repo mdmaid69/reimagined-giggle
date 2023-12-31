f7da621637f9735ce914c68fbb879f9be1465ffc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:29:16 +0000

    fall lunch goback(to) accommodation race

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
