76d23353dbc3ad033496430d17558bc1ca879a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:14:30 +0000

    dream public furniture attempt cruelty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
