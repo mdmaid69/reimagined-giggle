841a0407815056d3846fc7c1dc37d732250f9491
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:24:10 +0000

    suckme damage sea teach arm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+print([x**2 for x in range(10)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
