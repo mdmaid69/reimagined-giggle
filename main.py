d0b08de14495c1ba94c9223ad3371029dd9af7c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:03:37 +0000

    reply angry fail maybe perfect

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+print(re.match("h.*o", "hello world"))
