ec565b994a1fe371e23bd307ebd360e82374855b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:01:03 +0000

    constantly pool large level cope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import time
-def get_current_time():
-        return time.time()
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
