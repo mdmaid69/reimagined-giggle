916c9da8327fd8bf8cf98dd355836dd218eee4be
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:29:51 +0000

    566end talkoutof downstairs sink colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
