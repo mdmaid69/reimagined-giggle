8baddbcd2e97cb622f0510e95721a8a58757cec9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:46:11 +0000

    hold strength family phone game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
