c4dc012beba1813bfacbc4680db75b955cf4b97b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:15 +0000

    aim except repair store one

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def square_number(x):
-        return x**2
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
