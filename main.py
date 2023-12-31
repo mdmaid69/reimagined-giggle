c235185fce59d6442f3c05e3ef94683cecb4eae6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:25:06 +0000

    poor neither reasonable scale direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
