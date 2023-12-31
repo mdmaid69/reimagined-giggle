23918b2e5c3de1563ce3bfd7fc6c570f65a0d56e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:38:21 +0000

    promise offence where sad balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_error_function(x):
+        return math.erf(x)
