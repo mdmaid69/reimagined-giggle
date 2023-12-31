2b6352b3cc78bc1bccf78712a428b4f69a83dd37
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:30 +0000

    confuse mark late shoulder aim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def greet(name):
+        print(f"Hello, {name}!")
