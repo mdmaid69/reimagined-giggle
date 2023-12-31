b90d08cea19ee2355d7ee4a949d62a95dc0f5f3d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:53 +0000

    violence many quickly Qq taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def divide_numbers(x, y):
+        return x / y
