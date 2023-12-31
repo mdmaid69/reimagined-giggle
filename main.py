18946509209d78e79dbcceeac8486ab0cfe38a3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:37:41 +0000

    exactly generally secret ill open

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
