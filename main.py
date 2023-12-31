951808fa37d3badfcaa7d379330c8b6548a80d95
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:36 +0000

    yes no display money dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
