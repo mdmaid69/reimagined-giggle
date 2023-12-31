816b52d714dc33d230050479ca21d2bcaee3b9c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:47:07 +0000

    snow university circle front turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_typecode(array):
+        return array.typecode
