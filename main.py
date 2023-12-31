af0cd05818e8c6c5b7105d7796a32e7405e7d0be
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:23:56 +0000

    justice suit smoke name bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
