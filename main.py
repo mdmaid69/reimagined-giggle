cfc314c27779395f06418aa0bca7258cd36d218c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:10:14 +0000

    complicated book sea slightly suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+for i in range(5):
+        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
