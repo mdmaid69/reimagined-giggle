edf0b14ac48cf7cb38f4c99b64bf83f5472d08f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:53:25 +0000

    receive smart unpleasant hospital downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
