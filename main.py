0be21e386f8eef0c88b794aca82efcfb9cf8bbc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:39 +0000

    trust new regularly right straight

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def sort_numbers(numbers):
-        return sorted(numbers)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
