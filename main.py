1418b466e8fa815c7c77437ef59853f43e417e6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:10:07 +0000

    free build area issue sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
