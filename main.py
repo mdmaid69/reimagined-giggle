3830b5a25488da0b3029f1da8bb5d2a945911d56
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:53 +0000

    condition pair side mail head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
