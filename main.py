bfacd376d8ffcf30e62f00e97113e9233688c004
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:18:46 +0000

    takeover somebody correction relief show

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
