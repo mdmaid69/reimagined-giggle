ac8148bd989c4c30dfe0cbfbe812aea74b8e1685
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:07 +0000

    switch pump concern schedule talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
