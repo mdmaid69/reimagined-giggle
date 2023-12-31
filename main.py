d56f8c8276120b54e2ec64aa2c464da2063235a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:01:19 +0000

    near somebody difficulty snow star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(lst):
-        return min(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
