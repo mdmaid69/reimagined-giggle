388792a6e5f50f88900605146fa64efb2c7fb491
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:21 +0000

    slide child split straight about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
