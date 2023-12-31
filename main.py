db61c0400d2172168e90ff9ff0a4142dd3df8bf6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:26 +0000

    breathe recent love second beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
