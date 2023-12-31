dad5b137db6fd4be72155fe5a424fe073b56e51b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:24:57 +0000

    effect send funny away entry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
