002ae37b73112a3d251dfdeba2dbe2c107822df3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:16:55 +0000

    pot getback continue range workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import collections
+def create_priority_queue():
+        return collections.deque()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
