98d34e023ebfe5da5641eff42714dffceeacd6c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:16:02 +0000

    new lookback(on) second negative offence

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
