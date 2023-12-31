0467391bb4b621b38c87c937b180863fab252a14
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:38:26 +0000

    their before unkind turnout either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
