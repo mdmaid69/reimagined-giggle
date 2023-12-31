31f69be27aa50515c04a44448af6f57105bba9cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:05 +0000

    mail convincing long soon account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def square_number(x):
+        return x**2
