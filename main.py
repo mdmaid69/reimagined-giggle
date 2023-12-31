20eb068c9c4fad50d2df4b0a4f09de4d1b82ca90
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:02:57 +0000

    prepare wise skin dick organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
