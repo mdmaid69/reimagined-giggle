473a178b5f4877aee8a4db3293f5f93f3f01ead6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:57 +0000

    Ll more shoot produce safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_work(force, distance):
+        return force * distance
