e970b3933bcbf80e04fc663a05a521d3aaad7334
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:07:38 +0000

    claim scrape public what buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
