ef56f3f51df44d29a5361ceb00c0391da8b5e09f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:43:14 +0000

    practise substance trip shout relief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
