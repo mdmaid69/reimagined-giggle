219f2668a7dbd3eccb8a4c587bb0f9658a186153
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:10 +0000

    complain circle scream table naturally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+print([x**2 for x in range(10)])
