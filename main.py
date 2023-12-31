15b1698896e3233b650284e5ae72e79f2e5ca23e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:09 +0000

    limit spill rely method lookafter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
