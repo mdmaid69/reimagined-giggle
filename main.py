269b54d79bbbde0a93fb10e6514b3ea85fa247e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:07 +0000

    wherever heart left Ll direction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_queue():
-        return collections.deque()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
