ed7499c65ad930b8bbde7025282abd85cf2fe0a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:38:36 +0000

    although goback(to) machine slip mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
