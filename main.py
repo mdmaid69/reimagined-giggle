01f2c8b88333c11b60da5a8b761651f7d9fe9999
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:03:58 +0000

    squeeze instant finger disappointed guide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import time
-print(time.time())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
