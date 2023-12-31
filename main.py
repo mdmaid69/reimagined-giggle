1c6f1a4f7136a032b6a43f60ed0d0ef61a96d88d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:44 +0000

    always scale away destroy scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
