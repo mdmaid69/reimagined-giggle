aa47f191088dce3ea9ecc25559cf2b93c232e5a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:47 +0000

    keepout Tt demand word Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
