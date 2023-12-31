c81d4cbac303b5754ac33ff23f5cbdf908084575
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:33 +0000

    love convinced backwards well nerve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
