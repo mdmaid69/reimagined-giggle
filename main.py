341749e80cfa23824fc6be977df08e60e1a9584f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:34 +0000

    decision mail eager explain lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
