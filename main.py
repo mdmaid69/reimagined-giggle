7204cf345cb18aa2a5a8f2ff980428d85e9949b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:12:00 +0000

    dear union human react receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
