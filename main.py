f605f2063695f04408b884919a55220cb1fb2776
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:30 +0000

    balance entry somehow open unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
