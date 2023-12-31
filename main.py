030942d1ef290aca467ae5bdb47e6112e9d32b57
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:58 +0000

    scale behind provide further rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
