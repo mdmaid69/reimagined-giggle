590221364b99d9191786b93b00a6e3eec7ff63c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:02:40 +0000

    take hold capital once pile

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import re
+print(re.match("h.*o", "hello world"))
+  def reverse_list(lst):
+        return lst[::-1]
