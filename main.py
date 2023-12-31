7daa6bb17f3e74eccdaa9436c92dd174db40c5e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:06:46 +0000

    funny goout whoever wave slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
