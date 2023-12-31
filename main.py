dc9784fc4e8578291f882e103c3a0652872f9af2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:30 +0000

    official but separate list giveaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 n = 10
 print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
