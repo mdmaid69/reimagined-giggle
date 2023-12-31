566cd59bc380a6df17d1cae01d50f1bed7dd27b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:51 +0000

    hurt deliver scare song public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_floor(x):
+        return math.floor(x)
