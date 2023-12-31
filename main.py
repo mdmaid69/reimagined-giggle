68e9dc998e27adcbf9188fc73d3b606146abaea0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:18:18 +0000

    extremely belief wheel joke negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
