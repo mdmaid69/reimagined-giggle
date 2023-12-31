9400ade51b40f28f2d0a8214cd94b5df78ee62f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:29 +0000

    understand warm room city trade

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
