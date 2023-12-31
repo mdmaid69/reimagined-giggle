9ffc331402c2f99668d490107897af4ebe7519d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:36 +0000

    nearly refuse exercise wear suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
