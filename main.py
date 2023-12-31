a6d5b189236b1851ec05828b69411af481ad1a60
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:26:55 +0000

    poor sink medium period book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
