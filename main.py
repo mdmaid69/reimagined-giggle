6d43b02ef6820b960045b32fc646137673ee3346
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:46:40 +0000

    interesting left pool understand compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
