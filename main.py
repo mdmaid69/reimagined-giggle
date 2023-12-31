d4bc3079bff4f4842f88bef91b138c285800a359
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:14 +0000

    breakin exchange this expensive mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
