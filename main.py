babaedf19a1d3b198ad8a14dcdb6f869d4abd439
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:00:57 +0000

    door 567enemy rush breakthrough anywhere

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_power(work, time):
+        return work / time
