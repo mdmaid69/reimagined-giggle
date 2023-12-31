1957be35fcc72186fc62eec52e69f54127622944
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:08:05 +0000

    congratulate kind job message project

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
