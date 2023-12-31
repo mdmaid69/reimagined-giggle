9e6fd52bd25199c60388b49ae01583caad595fe6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:51:01 +0000

    wealth closely aim face note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
