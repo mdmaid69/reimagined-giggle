5bfacf6bddc88d05a5ea116183c5111ba0ba2259
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:20 +0000

    mix upset contrast business whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
