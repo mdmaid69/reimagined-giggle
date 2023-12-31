2422fab7b2f19e0515e6f2d7e29c9ab523f4643f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:49 +0000

    leg task make runinto react

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
