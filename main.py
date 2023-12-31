37dcb923bcedc0e7e0a55910349e74d7e2ddff1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:29:40 +0000

    meaning matter after negative owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
