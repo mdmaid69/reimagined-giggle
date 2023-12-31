7b3eb40f76443b1bf75d1682a231aa7fded005f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:33:48 +0000

    back comefrom describe divide risk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
