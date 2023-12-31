0afb7b3ddd716fbd8ab498c96305512e8cdcf5a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:56 +0000

    clothes time creature rain there

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
