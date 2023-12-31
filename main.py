e521aa32132df1ff0f8379a0f6a2b87221ad2388
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:24 +0000

    shout maybe second gooff practise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import time
-print(time.time())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
