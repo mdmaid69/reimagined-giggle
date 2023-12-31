d2b8d71d864fd76938e34ee954268be3cba1d663
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:09:11 +0000

    rent target present please medium

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-name = "Python"
-print("Hello,", name)
-import os
-print(os.getcwd())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
