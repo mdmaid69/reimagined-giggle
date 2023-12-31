e3502a1967d6751a27927cd19cd943d0e39af2af
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:02:58 +0000

    unpleasant feature board check foot

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
