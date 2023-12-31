2123eafa0a3297c722533c69292d95efe8990054
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:38:48 +0000

    difficult cross project turn future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def cube_number(x):
-        return x**3
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
