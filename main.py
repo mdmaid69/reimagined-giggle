804c3b00dd6cfb0503705db91d6824e9b5fb2f30
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:17 +0000

    understand wheel create government speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
