a3d1d6853dc7e20b951d87cdd890ca123a1c72b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:52:41 +0000

    deserve transport offence attract priority

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
