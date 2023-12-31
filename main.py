bbfc984fecef9f5a4f4303a1086bf3960c7e0e58
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:13 +0000

    best field name stop offence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_speed(distance, time):
+        return distance / time
