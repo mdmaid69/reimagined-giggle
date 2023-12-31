7be44741c7db0bf63577611251d2752350d79297
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:57 +0000

    possibly godown low involve squash

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def convert_to_hex(n):
-        return hex(n)
+import sys
+print(sys.version)
+def multiply_numbers(x, y):
+        return x * y
