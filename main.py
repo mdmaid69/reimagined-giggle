75cabbfe83100c98520e56533f9cc7f96e28272b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:41 +0000

    stamp emotion by occur into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
