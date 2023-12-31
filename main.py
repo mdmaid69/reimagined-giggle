38f8a5ef29d3ede36ef625038be6a5eb8d615714
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:45:21 +0000

    destroy crazy stranger apparently satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
