14f372b58815bf28f93e5abc4f850a6d1a64ef51
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:02:11 +0000

    dear sell phone mess practise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
