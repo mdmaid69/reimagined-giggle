19e309ff702f6bbc34c016779e1f66d647579a17
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:02:02 +0000

    give admire murder Mm press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
