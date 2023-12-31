20fdebd9f8c0d838f6953a7e7440d8bbd50e1781
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:57 +0000

    touch sensible home I harm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
