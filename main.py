9b66484fc2103e4612da340ffd61935f5b0e0b4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:19:15 +0000

    very stare recognize simple one

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
