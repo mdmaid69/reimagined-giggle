d6706b352e5803ef25841e361d35520afb3702b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:20 +0000

    accidental sink sleep your deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
