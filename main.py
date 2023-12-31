29a3930c39acee215e70b9537665b1ba96762cb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:28 +0000

    spend remark or declare free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
