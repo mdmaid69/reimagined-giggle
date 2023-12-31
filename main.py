b0a54270c7f26513baa49859afbd19e0845a3923
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:53:27 +0000

    group embarrassed follow letter fat

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
