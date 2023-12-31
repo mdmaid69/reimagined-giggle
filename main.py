d7ca40598c2ed79c9183c2ca059ee9d698c1cafa
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:34:28 +0000

    show straight worst last consider

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
