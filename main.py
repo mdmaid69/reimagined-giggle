cde2fea23cfa331e7626773f6063ac260f5870e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:34:49 +0000

    no determined only worry effect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def count_elements(lst):
+        return len(lst)
