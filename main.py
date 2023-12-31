a85c20e90d5607cb7eaa345252d3721c84803390
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:51:01 +0000

    law rate attach form left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
