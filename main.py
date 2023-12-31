29a3bc39cae8103df845e46c4fd9009cb8db19a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:27 +0000

    year clear owe question fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
