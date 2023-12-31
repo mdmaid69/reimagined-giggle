2709977f2e226dd8e1f19dccaa41cdc20b827dc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:38 +0000

    slightly several OKorokay any accordingto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
