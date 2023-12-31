a46881ebf9798a3261325fc262833f142223a993
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:35:07 +0000

    purpose dry off whenever nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
