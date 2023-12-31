dc7115e62668d0f1a87225ecc60f39e3745782bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:10:32 +0000

    turnback wave gate survive close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def reverse_array(array):
+        array.reverse()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
