ac6d66fe75f883ad0ab2d59ec8d7e4ed898f2aeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:43 +0000

    near dick crash bin sorry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+print(time.time())
+def is_odd(n):
+        return n % 2 != 0
