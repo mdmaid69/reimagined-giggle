32a5e9e044bd0176cfd403154e5db3428cc66382
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:21:38 +0000

    warm pain offer minor feature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
