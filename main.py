e36a27386a1bd5286a02e48e0c695ea5a1c8eec3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:57:31 +0000

    street half appearance dry roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
