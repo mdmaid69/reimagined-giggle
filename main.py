c6f4bd46159dd2df7cd3179a4fefe283e3bf362a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:54 +0000

    injury passenger place range slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
