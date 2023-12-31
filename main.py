3c0563c99e131537abde48a27a63f4781c8a659b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:37:06 +0000

    prepare unless keepoff direct drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
