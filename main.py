74da8837b619f4360e91df5a7cb4aa219d8cd68c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:30:39 +0000

    improve argument squeeze object object

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import time
-print(time.time())
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
