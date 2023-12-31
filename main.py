1015f30db4e7a49c46fec04fcee1fb160c249a6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:55:04 +0000

    survive whoever scrape song however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def square_number(x):
+        return x**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
