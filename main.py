9209c1499e79c2c8e45b47070c8e663881861a68
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:36:22 +0000

    opponent touch excuse what expensive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
