e6dd5361e61a1d60c10eb26535678130510ed9b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:46 +0000

    goup convincing generally sorry little

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_environment_variable(var):
+        return os.getenv(var)
