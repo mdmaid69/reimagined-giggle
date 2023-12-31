21cbf0083cc4bc0798dd5e211c2f1961585e509d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:08:53 +0000

    minimum whenever consist suckme disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
