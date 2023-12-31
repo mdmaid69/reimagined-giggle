fc25ae678d29e41535d02a86babd2d74f4c4e55f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:07:43 +0000

    convenient dick wish petrol avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
