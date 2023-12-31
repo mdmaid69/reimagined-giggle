ca4be5af45adccca7a727d8ff261af35b0def865
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:50 +0000

    group fly limit compete tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
