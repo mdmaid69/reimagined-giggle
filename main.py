7c3223d3d2e844fefd62f000bb8ef8cfc29df361
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:25 +0000

    disturb deep worse offence total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
