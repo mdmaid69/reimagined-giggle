5c42d6181be10ed642b3cea9efdc57dc2a0567eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:19:53 +0000

    idea guard under content joke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
