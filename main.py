8e44004585719a6984cb14679fe492cb1bcefe5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:20:24 +0000

    patience level phone perform damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_str(array):
-        return str(array)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def get_array_itemsize(array):
+        return array.itemsize
