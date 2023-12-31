aefae7b3aa9cb547e198655986858389bf7e1f59
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:10 +0000

    strict criticize set luck least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
