b8301d6015a1de3072dd71b741ec906046c44faa
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:18:41 +0000

    morning body loud profit thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
