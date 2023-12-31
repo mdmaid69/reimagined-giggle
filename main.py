9822dd8e77a23d89a70c7e1eb364f86c755074f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:09 +0000

    broad though crowded walk soil

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_exponential(x):
+        return math.exp(x)
