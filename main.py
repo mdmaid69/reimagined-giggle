ef335f70a1d71ffa24bee6566f8d829cf772d302
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:49:12 +0000

    unlikely search instrument freeze scrape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
