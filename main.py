10ac904a22ee690ae0c2e7808b9c44bf9a67fbfd
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:06:33 +0000

    contribute excuse hair environment clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
