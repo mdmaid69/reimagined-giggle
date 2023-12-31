fdaf651eea7a4feae2d55d18925bf8c2146a4b26
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:35 +0000

    Cc factory minimum leg deliberately

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
