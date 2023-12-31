a0d02aae2b7e441c85887829e646278bdf5b48f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:13 +0000

    them consist it scheme keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
