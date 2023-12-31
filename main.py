e2085b0eccd0e54067ec4dd5baeca85cdcd48013
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:06 +0000

    surprised tell preparation wave easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
