792f022e726ff40a497b6482459cd1b6c25612e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:50:58 +0000

    mysterious bringup Yy lookaround ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
