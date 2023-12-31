61669aed36ac5ad69473fea532d33f7f9d3414a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:45:25 +0000

    goback(to) once slow each Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print(sum(range(10)))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
