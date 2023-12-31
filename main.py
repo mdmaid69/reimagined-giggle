0ac5217f98d4f4da9d9ee1d5865b67eef8fb5b02
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:42 +0000

    demonstrate contrast influence callout under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
