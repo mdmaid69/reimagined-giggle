2d025ff1aab94ecb54f52abc52a524f8c81e88b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:22 +0000

    advice congratulate write friendly wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
