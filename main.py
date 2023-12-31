5f2f4e8ccd8cc38c1f67453f5fa4a47bdee291bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:24:39 +0000

    term sleep hair impatient cost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
