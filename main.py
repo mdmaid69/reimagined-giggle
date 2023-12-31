dbbe07c450a897c75410c2fac9ef8877d8694f42
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:03 +0000

    my road nor swallow surprise

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
