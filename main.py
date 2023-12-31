796a02dce21a1126b5d39ff9dbd302e0b34c64ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:39:08 +0000

    top spare trap himself twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
