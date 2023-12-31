493ed6fe0f5bd6b54fd4cb96d580d29caaa5f4b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:09:28 +0000

    off tip brush mine negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
