62f3a3fd9d5410a990545ad6e0badb0daa2527a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:18 +0000

    bury draw sudden hollow familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
