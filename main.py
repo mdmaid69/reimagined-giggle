40e3f012c46f5b1680410bac541eb54eafd07d4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:16 +0000

    gift valuable foot avoid thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+print(math.pi)
