47ebdbf2c2b2e8dddc404d8d731325bddce5602b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:33:41 +0000

    disgusting attention late world unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
