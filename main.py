f40d8759f4ed6eeaf5bb9206b41ade6bf03324db
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:54 +0000

    trade afterwards foreign him someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
