5dfc992e9056abdb035dd85a4a4e6eea21e2c5fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:27 +0000

    under suddenly every breakfast storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
