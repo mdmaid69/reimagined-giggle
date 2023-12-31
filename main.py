f7da31722b1b570c3d44e3552a4678920b9909b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:59:20 +0000

    brain boat science such centimetre

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_work(force, distance):
-        return force * distance
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def calculate_area_rectangle(l, w):
+        return l * w
