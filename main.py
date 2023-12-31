fa8ac086416f2d585048b3a40035cdc9e404aad9
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:33 +0000

    balance outside change coat tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
