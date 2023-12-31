a14cd59cd5738bf0c2887859c871c276b46bb4e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:37 +0000

    threaten mass comeround city base

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
