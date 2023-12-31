4fdbcb3aedb424027755dd05a517250880b412a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:52:12 +0000

    sail report task coat reasonable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
