1257c9a89e950727afe2de8fa190894d37725e9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:51 +0000

    replace communicate story priority fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
