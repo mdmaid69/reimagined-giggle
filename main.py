94b2a2dcf30104188a14da23997b64539ed66074
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:35 +0000

    trick small practical temper state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import time
+def get_current_time():
+        return time.time()
