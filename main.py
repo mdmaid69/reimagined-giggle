66312af4fb2024f6bd56e258cc0c4bec7f989771
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:51:54 +0000

    couple Pp false organize turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def multiply_numbers(x, y):
+        return x * y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
