55f66fe7fdcc8279d04fe4f92e67ea7a209e0b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:44 +0000

    switch connect occur subject live

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def append_to_array(array, item):
+        array.append(item)
