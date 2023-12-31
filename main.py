aee300a2c64ecfad0dbafbb863027a8330507d4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:37:06 +0000

    forgive prepare like this beside

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
