e330444c31b3fe4be4b9db49d1af1188328435d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:52:04 +0000

    heavy lend miss slide under

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
