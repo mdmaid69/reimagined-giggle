4b0859f850040c0b27e50219d9d454a5d39a0a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:01:05 +0000

    favourite hers flat nerve learn

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def exit_program():
+        sys.exit()
