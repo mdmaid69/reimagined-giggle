fc03ede8c254dac427d35f11afe216c3d0023060
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:08:21 +0000

    confusing type second finish risk

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+text = "Hello, world!"
+print("Reversed:", text[::-1])
