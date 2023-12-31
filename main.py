92a3166f74731c8dad2a7a1a26b3ed3f293b2260
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:29:56 +0000

    cover regional start once fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
