c5fb0bcbc3170e8b281e7b9de111eab00d436add
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:04 +0000

    mess proud Pp round without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_min(lst):
+        return min(lst)
