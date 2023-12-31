09094bae1cb2f836e3983233c4f5a01cefd5aadd
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:47:05 +0000

    whom plain band pay there

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
