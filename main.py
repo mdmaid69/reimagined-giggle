a518c370cc5297d11d87d5925a8c4abbf1a906f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:28:51 +0000

    fail movement impatient false game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
