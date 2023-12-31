83bb65cd60830ec4007d8a84f32d38b56d965e26
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:00:03 +0000

    waste listen use mistake keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def add_numbers(x, y):
+        return x + y
