ee832f883cd0fe87315ef9f9058828c297080827
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:05 +0000

    tired completely forgive Dd private

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_area_rectangle(l, w):
+        return l * w
