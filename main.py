45c3d0b73a02f900fe4f1dbc4fa7c8872c8317a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:24:51 +0000

    prefer burst sign rob circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
