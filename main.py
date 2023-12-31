670d8c31e98d0b163ba117d179f22166b15103c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:17:56 +0000

    interview upstairs mean risk catch

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
