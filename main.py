7ad8aca282b8f225643c8c0c23351e00a8e6b0db
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:18 +0000

    quantity serious previous help experiment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
