36d299f6fe943574ad5b4e1a284f7aceeb033129
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:31 +0000

    frightened out pass stay second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
