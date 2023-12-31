7839048fb8a9ccb56b5ec58a1ae246c9376ccb32
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:25:34 +0000

    invitation neither dream deal return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def convert_to_binary(n):
+        return bin(n)
