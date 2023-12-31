471d8fa8f0192e8c5c25fa7c50cfd252a14e318d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:09:23 +0000

    sorry people sticky bag pay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
