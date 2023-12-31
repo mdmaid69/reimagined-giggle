27e8a05bd64d2d1e457695538d590167c0d733f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:53 +0000

    club care love wide wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
