f5a3ecf0616de7fa3bd5b81e2a6769114105452d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:42 +0000

    stage damp type often area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
