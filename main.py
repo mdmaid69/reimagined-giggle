6a53ce7964e7ef014d442d657dfdd24cb1f51c27
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:02:57 +0000

    piece grateful shopping scared frighten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
