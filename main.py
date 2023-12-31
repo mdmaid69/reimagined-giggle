0e890957bad1fef255a69f93a2f61529a500d88a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:09 +0000

    hard over welcome heart happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
