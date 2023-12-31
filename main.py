e88562bdc20a3db77d66cfe8ccf3ec4e48b76cc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:43 +0000

    total generous under record burst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def remove_duplicates(lst):
+        return list(set(lst))
+def sort_numbers(numbers):
+        return sorted(numbers)
