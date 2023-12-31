60fcb17e54f0c55817809bf79f5d26e6805ccbc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:55 +0000

    mean lie part until hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_arc_cosine(x):
+        return math.acos(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
