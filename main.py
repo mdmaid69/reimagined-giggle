b2aca242510b7a6e8753c62385c8cbb9935d9948
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:59 +0000

    advise stone central inform fast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
