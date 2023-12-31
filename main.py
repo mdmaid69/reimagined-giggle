9ce3394d7ad0e9a1615cf7aba51eaea3f437a8c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:53:57 +0000

    walk arm quickly hair inform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
