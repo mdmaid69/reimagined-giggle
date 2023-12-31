b466efb3c0b57241d27523e9db5be6426457fe16
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:25:13 +0000

    trap win content bet try

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_min(numbers):
+        return min(numbers)
+  def square_number(x):
+        return x**2
