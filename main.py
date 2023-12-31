3788b211801692a065556e85e1c3a827ab81697e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:44 +0000

    support shake crazy piece office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
