8787ecb8d876af05fcbf5f69f9544bd7286ef5e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:07 +0000

    goback(to) workon improvement brief else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
