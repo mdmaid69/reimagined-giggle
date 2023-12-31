8ea219f923948c60c337e0d4cde5cde4ad77eb46
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:48:13 +0000

    upwards count born difficulty generous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_even(n):
+        return n % 2 == 0
