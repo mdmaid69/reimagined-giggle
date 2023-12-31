ed0585f416e40b7c5fbe10d8cabd15b8f4fca656
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:22 +0000

    laugh thought off mention think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
