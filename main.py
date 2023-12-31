84912d2a8eca7cc3aff573ed3f1748c95cdd7f36
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:38:09 +0000

    throw dare mysterious runover we

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
