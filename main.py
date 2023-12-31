288f5348a3f73a3c496b8a7c49a5d88257e6e511
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:53:14 +0000

    bar so breakup division later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
