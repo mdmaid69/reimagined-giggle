9d32091ac0204ba4d95c7afa912d14866a713c1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:15 +0000

    main like hide sometimes face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
