e0d5b2ad035bcadf3994d4e179310634e64409ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:03:59 +0000

    cry stick create polite slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
