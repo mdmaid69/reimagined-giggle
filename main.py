c763708880833f0f83212c08a2b49b901e455371
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:02 +0000

    please possible unite blame estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
