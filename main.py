b5ca9159655900ff5838f763867e3c3c83ac9a65
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:21 +0000

    this close ourselves copy foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_area(radius):
+        return 3.14 * radius * radius
