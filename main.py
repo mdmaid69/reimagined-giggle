c5070d3bc257db4696477fb860498c5c29144c45
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:02 +0000

    herself rest breakthrough wind ocean

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
