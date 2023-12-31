86af15438c53e000b46e2b4e5b0c63904d859508
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:56:47 +0000

    sensible move wash importance lie

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
