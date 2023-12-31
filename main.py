9d37711fee9769ed052e3e57dd3754957ab938bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:31:32 +0000

    table hollow mark wait stretch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
