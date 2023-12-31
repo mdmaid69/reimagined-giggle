dd64a772382353d5aea19562514017b2aa4fa33d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:04:57 +0000

    claim place Ff personal behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
