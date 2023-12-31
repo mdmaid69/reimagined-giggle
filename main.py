08448e5788608a0d28032985b68d7634061d2001
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:04:56 +0000

    count usual appear general comedown

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_pressure(force, area):
+        return force / area
