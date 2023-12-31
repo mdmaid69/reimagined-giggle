e81c9680d53bdc85e79389c62af1e26371fdfb4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:00 +0000

    be approve runout contact joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
