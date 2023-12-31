2ea647006bbeaf0636cedb4c97740099fcb89353
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:46 +0000

    hand with how any warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
