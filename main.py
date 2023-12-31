3bc2606114687b3e93d5b47b0bdccbcec469431f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:07 +0000

    defend rest other boat comfortable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
