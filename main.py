7a113c4a1c73d4a9d83528081fac010a0c799e33
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:05 +0000

    attach ride earth legal prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
