45bcdafdd36bad16cf1446c5dc4500e3c2949a79
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:38:49 +0000

    cinema fall quick price poison

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
