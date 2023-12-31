058b9a8e3ea8ffdcd4f9c6370034386422045cb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:49 +0000

    generally chase joke cheat plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
+def is_odd(n):
+        return n % 2 != 0
