6701302dae7d7c94ad357a1394014d0c0b8c356f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:37 +0000

    total direction sudden pair distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def count_words(sentence):
+        return len(sentence.split())
