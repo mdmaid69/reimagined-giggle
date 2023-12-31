4f1f1ca9d9a9aa737c7cae316002e56d6946846c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:50 +0000

    lunch scream word situation tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
