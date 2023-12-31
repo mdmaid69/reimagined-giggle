d01c500120307b2a74b22b51142a9e6f736425d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:32 +0000

    cost confident group fortunately burn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
