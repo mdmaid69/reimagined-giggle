d279582dea2f1daabfacc3c9c1d53e8455786062
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:38 +0000

    still long few blow radio

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+import re
+print(re.match("h.*o", "hello world"))
