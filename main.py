9da9f790917761eaf3e79ed813c91b4ef827b313
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:36:12 +0000

    recent important idea defeat trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
