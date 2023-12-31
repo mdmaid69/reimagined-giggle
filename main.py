317bf1affd10a099313dba4921f0fd6fcd021328
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:25 +0000

    instant breakin address sight schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
