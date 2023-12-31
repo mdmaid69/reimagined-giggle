1e7522856e731aef716e200df62d2a8b4bf25a89
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:08:52 +0000

    wave rather Bb direct objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
