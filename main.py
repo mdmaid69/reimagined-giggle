6ad864fd533f4aaaf9f8f70fc7ff3749ba022da3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:35:00 +0000

    natural suggestion warning sorry catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
