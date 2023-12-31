be4302170b3f9e115ada5a5db7962cb1b9b04040
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:31 +0000

    each broken congratulate exit suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
