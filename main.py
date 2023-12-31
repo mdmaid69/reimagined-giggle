6c8d19848e528c0184d40fe87501dc4cbb27dda6
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:47 +0000

    suck owe narrow runout police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
