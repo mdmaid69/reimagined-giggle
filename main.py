1902b4ff0d228b7172f81a53e87bceabdd02a1b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:57:03 +0000

    bus money fit share exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
