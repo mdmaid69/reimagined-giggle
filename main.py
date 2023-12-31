df2340f890ff3dc9f2fcde1b68b30372a7ff269b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:36 +0000

    image recognize cut unpleasant city

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def multiply_numbers(x, y):
+        return x * y
