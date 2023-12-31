e11e533c69b181d8343be8bc303ea8e361319556
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:26:39 +0000

    touch exam exactly couple building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def square_number(x):
+        return x**2
