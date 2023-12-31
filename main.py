a3fd402441545a92814ae53cfd896ad0a87c5089
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:39:26 +0000

    same clever demand difficult steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
