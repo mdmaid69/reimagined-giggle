961c5828c53f2894ce9d6114338ff8f04159cff4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:18 +0000

    hour attention trip exist refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
