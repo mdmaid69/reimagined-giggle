4b58270a82aaa61918a7e1d52915ca34116ef515
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:43:11 +0000

    dry lesson may cheat face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
