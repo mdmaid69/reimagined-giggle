2ac954e42650c03b08c9e8e4b2390900b07d0035
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:24:58 +0000

    judge disturb your pass formal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
