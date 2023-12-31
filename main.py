602ee1efb1df09fb316ec3509a409e51ee8cf682
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:25 +0000

    past callup be human workout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+print([x**2 for x in range(10)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
