3237cd1c2753cdc01db4e62d77a02fb6ca3d09ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:54:42 +0000

    after workupto past convinced treat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
