bd9ed4299e654ebcefeda8ddbd4242ac116d8625
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:33 +0000

    flow lay Gg again getup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
