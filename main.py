2ad7e0a1aba5f575898269e524dda9b3fb80f321
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:09 +0000

    turnup callback loss square another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+x = 10
+y = 20
+print("Sum:", x + y)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
