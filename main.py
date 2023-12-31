e9194143420891cfa434f1a49283f0da4fc0e30a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:00:29 +0000

    interview around accept cinema giveback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
