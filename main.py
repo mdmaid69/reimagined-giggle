a5a63bb41be85223eb1ab60b18605a1a8adaf7d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:07 +0000

    goon like store spring increase

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
