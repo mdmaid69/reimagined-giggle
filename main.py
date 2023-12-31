7c5fd6feb37464c1ca8da35b6a48d189b20742d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:46 +0000

    uncertain hardly among uncomfortable could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
