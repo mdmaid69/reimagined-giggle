61b3cd0c909b2bd9a4c78dd0d5a0e31aa6c0d017
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:13 +0000

    steal regularly service practice supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
