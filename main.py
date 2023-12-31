57db389b1fb6b1e0b90babe637395acfbe8695b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:26 +0000

    occasion shout breakin memory deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_typecode(array):
+        return array.typecode
