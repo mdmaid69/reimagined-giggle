4149981f98de40b45ace2edb5fe0c68abc0c2fd4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:19:14 +0000

    replace successful Kk tooth actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
