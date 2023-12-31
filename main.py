aceb192bdf122b8c4816a0ea686798e770810a55
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:19:21 +0000

    group frighten year arrival fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
