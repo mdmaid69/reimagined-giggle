b401489208fae7987ecf32ad67347b701aef42a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:36:47 +0000

    turnaway maximum present exercise any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
