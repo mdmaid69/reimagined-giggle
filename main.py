1d5afa84c46ee5d1d2833703de681b5227c97b15
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:16 +0000

    interrupt thick swallow dream father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
