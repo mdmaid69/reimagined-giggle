ea8d1df1359d1566cc197d3f2f34140d7c7cbd30
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:36 +0000

    pretty clear responsibility dry lookafter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
