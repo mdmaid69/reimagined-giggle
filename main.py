e56535874a5ad116951b7f99ebc2d5a66889f3af
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:26:36 +0000

    matter story ordinary ground attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
