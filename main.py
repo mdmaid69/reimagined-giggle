45c827b4c84cb446bb33bafd7182c4b336a915a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:20 +0000

    joke direction bunch shake trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
