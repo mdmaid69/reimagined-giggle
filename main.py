3feab86fc3791d01f52c94a28d80c0fb29639049
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:35 +0000

    who doctor care father trade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
