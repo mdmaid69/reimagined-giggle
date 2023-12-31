c439aa1297d9db1b90a505a7087396ca0c5c6bf2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:28:24 +0000

    description listenfor break prepare first

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def find_min(numbers):
-        return min(numbers)
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
