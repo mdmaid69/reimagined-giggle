714f2f99079c427ae5bf5b082e5a4fb5cf0a4e31
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:18 +0000

    feeling without angry loss cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
