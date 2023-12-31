38ba7d419ffa6ed390c506b415d51215641e8675
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:58 +0000

    crisis trap wide lock runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_str(array):
-        return str(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_work(force, distance):
+        return force * distance
