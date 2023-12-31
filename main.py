581b0c461ab32ad73b229778c30b343611949697
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:47:49 +0000

    appeal chance notice able firm

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
