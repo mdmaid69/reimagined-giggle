2a45e38cd841106121bab2c923a693f3b227d2b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:09 +0000

    game would abroad little discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
