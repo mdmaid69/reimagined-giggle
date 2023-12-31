9c0d914eb468f21d765d9478c42ef380c0e30c52
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:04:10 +0000

    clearly fence translate word discuss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
