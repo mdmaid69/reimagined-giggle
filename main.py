c85338ae9ed671dea129d50cc306846078d6a9dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:15 +0000

    not build chief journey suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
