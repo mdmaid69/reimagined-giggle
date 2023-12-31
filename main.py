fbb8737b950c5179ffe2629f9f80ccbe32c98ec1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:32:56 +0000

    wind any recover usually whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
