bddb55456c916229566b23b03ca99949c62a6db9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:55 +0000

    under ground abroad couple settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
