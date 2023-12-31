5a3764bfd175d8c629550eb4034ebe896a0c4436
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:19:37 +0000

    there obtain hole objection charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_int(array):
+        return int(array[0])
+def count_characters(sentence):
+        return len(sentence)
