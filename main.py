9591f6645a24140a26eb5c427d2d4ef19c1a2c4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:04 +0000

    steer play maintain cause except

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 def calculate_speed(distance, time):
         return distance / time
