54cd03abfc87d808d02f15276f31b46abf721997
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:52:22 +0000

    cover supply store theirs friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
