c6fbacb394a43c62ebba76a0d17859b996cab1a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:13:21 +0000

    roof pool weak attach underneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
