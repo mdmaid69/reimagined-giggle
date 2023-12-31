7f462a8ebdc5f15c069c280e596660eda0f9d96e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:56:11 +0000

    lot lower sharp bringback across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def remove_duplicates(lst):
+        return list(set(lst))
