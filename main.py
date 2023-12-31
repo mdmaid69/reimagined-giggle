44df97b8a7f10a5beee0c9b0d6f97dad6876b5f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:12 +0000

    poor probably often talkinto order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
