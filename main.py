bfc5af83d26534e52ef1f1f424585b4729f57d0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:51 +0000

    reasonable bottom unlucky pleased into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
