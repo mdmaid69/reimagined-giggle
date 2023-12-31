74be4a2a566295af00e53560b123de551c34bd5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:11 +0000

    level sleep general trip tight

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def reverse_list(lst):
-        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
