38c7872d85e753e0fff0062eb9040fa6623fe86c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:01:00 +0000

    underneath steady now last decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
