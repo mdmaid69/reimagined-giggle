a5dfc112a4e7e2733a305a3c399c4bc76ee69f8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:30 +0000

    action exam switch because turninto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import getpass
+def get_username():
+        return getpass.getuser()
