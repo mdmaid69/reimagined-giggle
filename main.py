00f635a74a6c5424e4f7025346a66ecf8dd36c03
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:27 +0000

    stay quarter success autumn comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
