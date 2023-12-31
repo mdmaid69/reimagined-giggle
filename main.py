d464cce8ae6f47a148940fe61bdbcbf9b14c3392
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:32:17 +0000

    model problem comedown far something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
