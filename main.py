32b78174594e133a96a4ae67e578fb1249f60ccd
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:43 +0000

    heat 568energy smile afterwards tune

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
