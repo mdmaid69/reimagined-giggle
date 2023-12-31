9a2f3ebc67ec612cb8c6dffcc5bc33f02c1257bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:13 +0000

    judge whistle dry they callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
