368fdd7d6b584823854af20f373a9e270ba500b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:24:20 +0000

    much keepup mess body breakfast

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+def is_palindrome(s):
+        return s == s[::-1]
+print([x**2 for x in range(10)])
