3b5f9419ae60305f71f94d663ca748a130a14216
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:05:27 +0000

    runover turnaway miss join dare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
