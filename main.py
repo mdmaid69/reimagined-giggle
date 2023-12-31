8fa0ed6167de0a9ce66b06a8d3acc4bc55dc35da
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:01:21 +0000

    problem total yourself estimate up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
