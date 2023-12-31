52e38c38eeadff464c2343c8887772ef9c31fd14
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:48:40 +0000

    though tip opposite either guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
