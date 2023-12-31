11f19ee54fc0848b633ec8391db44c2bd2349d18
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:30:15 +0000

    burn since base him where

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def subtract_numbers(x, y):
+        return x - y
