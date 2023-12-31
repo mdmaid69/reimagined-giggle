88d660ea3b17e6b28652cca46205e7ec12b13acf
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:42 +0000

    straight lift him especially dance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
