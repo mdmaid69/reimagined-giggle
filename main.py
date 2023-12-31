972f78f9e5c96f827359da89e89191950dcb5a55
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:24:12 +0000

    mood suit give uncertain flow

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def is_palindrome(s):
-        return s == s[::-1]
+def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def get_current_time():
+        return time.time()
