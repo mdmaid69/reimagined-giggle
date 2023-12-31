ca8bcdfd4b910fd291b404fd6157597704664eaa
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:50 +0000

    fond hot confuse patience boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
