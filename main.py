c3fcf8dfbf95522697c8d977e93ec8043d1691eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:24:46 +0000

    goodbye grow begin accordingto abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
