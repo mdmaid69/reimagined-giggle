094cac83d24a8024ec4a7fc08f0c73f052a335ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:28 +0000

    file interview favour in last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
