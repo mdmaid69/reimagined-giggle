70b1a979d09ac99aecdff803a62cd16ead07fe3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:10:34 +0000

    bread advanced surprising work cold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
