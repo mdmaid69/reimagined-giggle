e67d3951670382ac859669f2e7407876664dbd32
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:17 +0000

    stand law 567enemy everybody on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
