d6a79337cb31ddbca156cdbfe1bbfc5117f163ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:05:35 +0000

    relax rush today scrap bag

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def convert_to_binary(n):
+        return bin(n)
+import math
+print(math.pi)
