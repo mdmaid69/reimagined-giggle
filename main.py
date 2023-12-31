6dd48a751253e26270f67781d7efc977d8c4fb26
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:15 +0000

    past joke ear getout round

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
