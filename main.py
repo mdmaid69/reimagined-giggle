67f821f4f0773e228154b44fb3da271e7f0f922f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:43:16 +0000

    dress good cry scrap fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
