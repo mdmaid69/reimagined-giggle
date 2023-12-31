3274fdcd5a1765ba009653114e162508396e19b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:40 +0000

    stage appeal theirs clean original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_speed(distance, time):
+        return distance / time
