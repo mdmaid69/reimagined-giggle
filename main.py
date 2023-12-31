fc7c4ef4c619c9e5b0bef033336e3308e4e56045
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:38:14 +0000

    although forward touch fire although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_directory():
+        return os.getcwd()
