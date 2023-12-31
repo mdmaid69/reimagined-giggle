1dff28a2d3c9d5dfd41ac5b235d31e01cd10a7b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:36:23 +0000

    until improve settle fork joke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import random
-print(random.randint(0, 100))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
