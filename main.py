22178de959a26ae09e597f86f9467e85c1b75dd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:40 +0000

    image should regular stop usual

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_floor(x):
+        return math.floor(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
