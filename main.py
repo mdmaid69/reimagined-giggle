ba713d20d200c12774648d43b0d858c9f29cb0a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:55 +0000

    sign sleep cheque confused before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
