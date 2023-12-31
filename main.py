ca5cb32663ea381bbf4a7e253f77c9d9cb0aa6e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:33:59 +0000

    sport main everyone wish impressed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
