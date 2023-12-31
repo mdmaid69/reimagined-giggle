e4cd1d9c8f7a7f392fbf35e968aeb6561ec11ebc
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:04:12 +0000

    embarrassing money suppose certain original

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
