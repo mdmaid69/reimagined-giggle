aa19222c993f004bada950acc903bdb0a05ef8f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:59 +0000

    touch effect communication suffer busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
