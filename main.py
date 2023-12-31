98308348d188e24df2273f09a2137b03625539f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:41:32 +0000

    belief freedom her clear this

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
