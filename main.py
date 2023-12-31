4b2619b855d4ca799f2ceb168233a46e97baf35f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:14:01 +0000

    later such freeze anywhere load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
