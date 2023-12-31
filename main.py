e0c0918abae3f3ecb78069bf8edb2f5e7224a216
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:36 +0000

    fortunately confident can ours since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
