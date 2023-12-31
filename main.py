4000848aa6c7b376d038e0efa38028dd6e389899
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:45 +0000

    put search perform large shop

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
