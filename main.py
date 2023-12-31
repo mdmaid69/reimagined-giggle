10ae74dee45a0bfe06087013563d82a79b482fd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:49:36 +0000

    wonder our afterwards on such

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def cube_number(x):
+        return x**3
