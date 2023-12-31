78f54744c917c7352e0a78dc4f57d5c7fa93cc96
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:53 +0000

    trade trade market determined swim

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def is_odd(n):
+        return n % 2 != 0
+def convert_to_binary(n):
+        return bin(n)
