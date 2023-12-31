59a14ab2a538496d9d3c5052d56e71e96504aa7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:09 +0000

    crash strike home hand blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
