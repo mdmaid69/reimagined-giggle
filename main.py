1fff128d9690c67b443c7f0a605983fdad281c52
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:16 +0000

    appearance delight winter completely takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
