12225b8aa8a284b9484e9a0207d6851c26f7f1e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:46 +0000

    win present life repeat excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
