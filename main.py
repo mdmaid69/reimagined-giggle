13f269c18191e813d540faa102e676afcea1ca26
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:36 +0000

    considering medium first afternoon present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
