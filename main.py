01e0a242121ab902be877b9452b31f6011708324
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:48:34 +0000

    buy pronounce operation tip blame

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
