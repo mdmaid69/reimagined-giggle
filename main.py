1ba79e0dfee353de18e53681395a38851d480ae3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:44 +0000

    race urgent play Mm direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
