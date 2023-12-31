57ccd234ccc7350c0c149eddf7a94eca76dda9a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:00:12 +0000

    choose train less run voice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
