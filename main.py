3ea05edebe693b751c4f4b7bed1d1fe73f2b602e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:16 +0000

    handle scrap hear only into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
