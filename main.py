15e8a5a891c69fee229fba1c3c0af3b8d342f86a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:24 +0000

    respect plastic steal turnback hurry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def clear_array(array):
-        array *= 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
