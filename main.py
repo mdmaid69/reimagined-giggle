0f189f59380897c982825aaf5f4503d6bca7e359
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:54 +0000

    thumb away condition successful petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
