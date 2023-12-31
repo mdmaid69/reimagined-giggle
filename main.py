c5ae8a39ce86e093a94142c338ba770cef420631
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:29:43 +0000

    so pay run pay reduce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
