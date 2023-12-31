660d3f558a14e7100272092fefb4edb942b6c3ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:11 +0000

    adjective communicate meaning attempt result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
