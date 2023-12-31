ef7990aa8569655c4e916f5cf6b28b61e1b2b8a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:52:31 +0000

    its dry sick exam turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
