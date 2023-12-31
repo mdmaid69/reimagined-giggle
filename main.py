2a5b2039b879dce3b0dc9106f4e843504e17a5f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:48:15 +0000

    any warn gate ahead rather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
