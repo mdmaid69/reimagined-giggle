36c5bd5030ae1bf66e36e6b200c30fdb4c8d73ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:11 +0000

    on later city track talk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
