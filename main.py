9037298c3e9bdb2f98194ae54f0309e6b7acee90
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:30:02 +0000

    regional tidy kill noise cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
