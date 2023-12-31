7531f2a627e428231abce383abdb30fd00e16534
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:38:10 +0000

    post anxious late fast mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
 def calculate_remainder(x, y):
         return math.remainder(x, y)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
