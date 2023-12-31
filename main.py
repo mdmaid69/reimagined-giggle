1d038d38b07fbb4b68763e3520e12b6213c342c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:26:27 +0000

    keep operation pride see centre

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
