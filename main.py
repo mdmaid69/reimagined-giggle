9162a387f4e4342e66db3b751b54dd68d613240c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:22:06 +0000

    favour any education error keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
