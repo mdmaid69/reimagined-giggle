299bd364865477fce7b815bf695ccfa38ecdace3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:51 +0000

    partner land stretch talkinto shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
