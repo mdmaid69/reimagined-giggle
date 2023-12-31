17739985e0c5e1c23baca4059a17c9e5d7151a65
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:06 +0000

    record clear proud achievement succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
