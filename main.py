8d3643b124935ccc56fc62a916cf187a6acdd23a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:33 +0000

    regular fairly unhappy finger comeout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
