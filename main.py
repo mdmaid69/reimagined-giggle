8b25f58b1b01f80fe944299169046fe06333403d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:28 +0000

    turnover narrow pronounce appear aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
