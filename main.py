5bbdd9e3828ccf9320a7d7a1defadc04f70e0cf8
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:27 +0000

    shoot now very protection Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
