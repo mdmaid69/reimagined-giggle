74d3ecf3c8b004c71f62944e1c7c7ca7e2b9ec35
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:11 +0000

    turn both come rest positive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+def is_odd(n):
+        return n % 2 != 0
+def find_max(numbers):
+        return max(numbers)
