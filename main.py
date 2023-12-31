8b26db27ba7ef1828781b57ff165d8dbdf5ee2c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:43 +0000

    satisfied skin well quality giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
