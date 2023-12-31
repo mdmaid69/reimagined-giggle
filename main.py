c6578a12b0ea1ade644a5216cc1d18fa4a6f94fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:19:36 +0000

    level emergency pretty together hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
