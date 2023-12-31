89909c77148e5d52caf80c9dd37b65c13689d2bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:42:06 +0000

    pressure rare tone today fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
