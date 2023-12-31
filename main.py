ae6119d24a49c101b7b0d87e7532d51ef061e97b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:56:35 +0000

    lucky right capital this decide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
