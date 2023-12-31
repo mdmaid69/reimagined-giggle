c6335f02485474ca4c98c82d4c8a74385462ce3d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:39 +0000

    tie blame patience turninto after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
