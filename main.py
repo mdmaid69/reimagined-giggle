26945fc3c4a285e838dc6329d128fdd52b1b4e37
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:16:18 +0000

    pile no air weak so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def split_path(path):
+        return os.path.split(path)
