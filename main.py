78a23c5ab902f4ba6b4e80f20593fd73efb22f2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:37 +0000

    neither giveback deserve worth drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
