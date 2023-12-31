a18f8670e5e65041a2db562da70dff006725c008
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:00:45 +0000

    edge shock neat water concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
