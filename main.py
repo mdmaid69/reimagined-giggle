787bc5c8b6a3b66f448028af27bbff7af0852f71
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:53:12 +0000

    thankyou achievement share card either

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
