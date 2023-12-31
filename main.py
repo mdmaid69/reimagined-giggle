44e51d9ba4cc15d6a17578153efa1dfb910a61e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:12:37 +0000

    support translate sometimes contain rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
