b28749459cb3b004f19fddf6eb719c3e863c341a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:25 +0000

    seat raw strike funny disapprove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def square_number(x):
-        return x**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def clear_array(array):
+        array *= 0
