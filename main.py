cdc1d10f16ac38e7e3404a9d1eec37562d6b8bb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:15 +0000

    welcome question no dead increase

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
