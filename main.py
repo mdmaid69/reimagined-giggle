2cf75f53baa042ef3321c95f0104b89ae2ad7b67
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:32 +0000

    at light total skill partner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import platform
+def get_os_info():
+        return platform.uname()
