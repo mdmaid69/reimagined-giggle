28c5deb0e44d68ddb79aeb4d279d4bb5b5067302
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:24:51 +0000

    help idea switch string sensible

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
