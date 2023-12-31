cfc410b487b153c641594aa9d565d9b3c069347c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:41 +0000

    supply like underneath hate child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
