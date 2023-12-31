5aca4f1c090b4c3b1c6ae7fbf6f5a8f20d6ad18b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:46 +0000

    climb position pattern bringup reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
