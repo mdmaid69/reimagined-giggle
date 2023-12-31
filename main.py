e30524c76d156b3c4000a9b3e725fdf0661aeeb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:42:21 +0000

    dangerous voice afternoon breakdown mood

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+print(re.match("h.*o", "hello world"))
