b31aa4a8cf004277db534ef57adb748fd6278cce
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:13 +0000

    programme content frighten mess once

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
