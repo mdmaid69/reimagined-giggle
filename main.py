db9780a6cedff8d4d190fe2d32ccbc2ed75d15a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:51:23 +0000

    means after method passenger about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
