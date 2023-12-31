26a739b84736191b8dcadd63efa2f9619dd62fed
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:01 +0000

    crash go same dare afterwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+print([x**2 for x in range(10)])
