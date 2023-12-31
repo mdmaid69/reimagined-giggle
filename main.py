8bcf95ac88919dc61309836244fc4e4303a2b416
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:03:34 +0000

    case noone home sad stupid

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
