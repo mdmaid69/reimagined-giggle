b6ef570d0f4ec580940110a9427c0d22d733a97e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:22 +0000

    she lie sticky content relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
