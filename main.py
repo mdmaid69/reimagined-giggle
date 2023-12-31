be77392134a2312497695e2694c785d92b604acd
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:35 +0000

    here home scene combine reduction

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
