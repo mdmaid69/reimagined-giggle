59db6b619c82e5b60fe385c42a787cc326a6c19d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:55:22 +0000

    tidy charge butter sort tradition

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(sum(range(10)))
+import json
+print(json.dumps({"name": "John", "age": 30}))
