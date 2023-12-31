b4fb560d1deb514367d651305d70be4785873afe
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:56:44 +0000

    interfere statement wealth turn within

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
