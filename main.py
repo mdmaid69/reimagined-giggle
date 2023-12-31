7c36a71b0141145cd4c579e1fbb4989f48d9fd4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:43:30 +0000

    brush continue smoke no seat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
