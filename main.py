e4b1e9832c0515eeece007a550008da19acebd28
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:38 +0000

    lonely disappear lose attention comfort

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
