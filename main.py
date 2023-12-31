4553a1ec16d7488ab478c7befe0f31da347a2995
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:34 +0000

    judge hour drink cool 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
