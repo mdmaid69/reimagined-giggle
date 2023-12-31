fc2f341b69204f441d48d1df9ab38a0d601b5b25
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:50 +0000

    detail protest estimate advise news

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
