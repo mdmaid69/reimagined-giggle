37c99cb33f4a6e7047a352efec974f81b493fbee
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:29:07 +0000

    test trade pupil respect past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
