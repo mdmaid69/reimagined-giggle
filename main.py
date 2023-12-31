47247204880c92a62c8b3050e262851e26d05b37
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:23 +0000

    butter idea fight custom keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
