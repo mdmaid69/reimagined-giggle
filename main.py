4edd743b0656c56cb21047d9df7e322e46a437ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:52 +0000

    plastic please plant level spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_string(s):
-        return s[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
