5f29e7c999ad989e56909663583f090cd39c5132
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:32:23 +0000

    attract same above window anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
+  def is_even(n):
+        return n % 2 == 0
