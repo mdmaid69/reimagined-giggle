b97c8c141563ac687127189b1abbb812263bfbb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:18:16 +0000

    smart either group hope cry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
