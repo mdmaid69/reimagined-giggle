467fc2234f8ec63512ef43627ced525dab5fef50
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:08 +0000

    succeed pass Jj fashion worst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def add_numbers(a, b):
+        return a + b
+def sort_list(lst):
+        return sorted(lst)
