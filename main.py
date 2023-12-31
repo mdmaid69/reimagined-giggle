6b8c3b64774c3e2319a4c76c2583b9c38897d869
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:02:52 +0000

    hardly cheek share quality whoever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
