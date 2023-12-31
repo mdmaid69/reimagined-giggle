8d45413197dc3b4302d58793235e46f616686f2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:39:31 +0000

    564encourage relief absolutely whole cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
