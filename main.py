e345ce2fcf41b973d65aa680e326a6cf79913065
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:44:23 +0000

    moment grade Hh wire love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
