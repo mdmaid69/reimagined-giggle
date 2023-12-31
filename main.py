556e3e5c92caf88be5298265162e1ec1e3dbd228
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:48 +0000

    downstairs ahead lookafter punishment watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
