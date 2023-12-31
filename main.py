83106f39bf8a34ac5915e1a87f0dd2ef9a22396b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:29 +0000

    568energy except battery slice clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
