7f84f39b489155446846cbe7b410a3f7727bd002
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:59:32 +0000

    smooth around store fast smile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
