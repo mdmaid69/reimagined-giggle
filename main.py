d8aa96c91457f92b7b2e1e7cda2f9345a5541900
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:44:49 +0000

    stay cruel front break report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import tensorflow as tf
+print(tf.__version__)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
