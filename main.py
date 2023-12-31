0865d06141f936ae502cfa858537b3bbc9357528
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:34:51 +0000

    walk arrival train unite bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
