c1a9eafb6779c709f832809e658be17fe624d632
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:37:38 +0000

    improvement lift coffee among increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
