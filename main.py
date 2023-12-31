71ab8cb7864aae59e384ab130fbe9d87b2de62f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:28:58 +0000

    male entry right dare unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def roll_die():
+        return random.randint(1, 6)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
