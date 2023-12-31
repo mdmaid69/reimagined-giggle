ab3bc6491abff8f322176cc3b6d26e6cf2b7264d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:48 +0000

    situation drive foot city identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import time
+print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
