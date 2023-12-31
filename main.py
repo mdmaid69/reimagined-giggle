62dcbb3b1def6f6e09da84490181b2ff9eee0008
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:46 +0000

    task translate past equal shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
