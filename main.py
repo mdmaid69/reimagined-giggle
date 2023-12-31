651a95caf38e94b1696b26d4e53c0feec8133407
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:42:02 +0000

    sick act manner negative beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def multiply_numbers(x, y):
+        return x * y
