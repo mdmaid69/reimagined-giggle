77b41648fa9b48e14c135c42850684bb3acbe8da
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:53 +0000

    classroom far trust odd reach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def convert_to_hex(n):
+        return hex(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
