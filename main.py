5ba8549ca3b7003345d2f084b94e1b2f1715fac1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:54:13 +0000

    charge community win tend Ss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
