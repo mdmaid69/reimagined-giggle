f66731a340e9207292931e6fdfde1bc96b195106
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:33 +0000

    delicate programme poor mix length

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-for i in range(10): print(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
