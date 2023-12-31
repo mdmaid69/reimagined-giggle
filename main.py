f87e3c8f02c356d65eed80383b419fb56fb122d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:05 +0000

    ring past flavour bit excited

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
