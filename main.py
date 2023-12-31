d3c995bb0f14f8d74f9ca7f37f1480ed1f47f864
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:04 +0000

    OKorokay friendly flat whether delicate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
