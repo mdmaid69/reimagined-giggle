ac11474c9642509b0c3de0c9a31ec1e708c04eac
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:12 +0000

    concerning odd holdon their familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def set_array_item(array, i, item):
+        array[i] = item
