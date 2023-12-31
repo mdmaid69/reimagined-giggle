3c99a20ad8cbd6bf6a42ebbcf6aa17a4f86de584
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:24:54 +0000

    Tt closed talkinto pleased our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
