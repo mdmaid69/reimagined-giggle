d97f6f232b08eed596cdb0b96ae92c20299f9077
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:11:00 +0000

    cum success responsible hurt related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def add_numbers(x, y):
-        return x + y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_area_rectangle(l, w):
+        return l * w
