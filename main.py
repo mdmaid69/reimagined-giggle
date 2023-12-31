c8b9d621507cd4218b6dfa0341c02ecc89f7ac49
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:37 +0000

    fire important brain stare funny

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
