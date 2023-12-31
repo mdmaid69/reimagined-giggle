62ac2825bd2c4049a0ff7777da0c1f928e82568a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:45 +0000

    spring bear relief style carry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
