411a2367fd8aac7ad23190b7b7c453d8622e7ff4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:12:27 +0000

    maintain duty major nigga debt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
