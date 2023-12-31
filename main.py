885d830e552cc2f33bdb3c1fce8fa9d881bd790a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:25 +0000

    past slide Oo quarter strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
