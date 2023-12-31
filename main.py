8844a0da808fa8ca3e776584a9a5fd2a5abc3056
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:43:43 +0000

    fit bunch block demand scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
