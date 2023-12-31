4ff17afafa6e134a1b2903497890211bc0980ebc
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:10 +0000

    send him report relax violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
