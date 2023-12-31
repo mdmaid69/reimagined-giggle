bfefccc20e466cc9383fb20775c0297965dea2be
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:38:05 +0000

    suckme pull carry but matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
