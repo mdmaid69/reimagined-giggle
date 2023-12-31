6718204d14b106bb66f65cf7044f51131f23ef9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:02:21 +0000

    565end shape completely sensitive pretty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
