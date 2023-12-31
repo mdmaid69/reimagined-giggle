8ea5d671e3be40fd71a94558bdb32ba689885eaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:01 +0000

    detail handle explain heat lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
