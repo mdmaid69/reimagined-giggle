3a404ad9cd330893c95d38956c2ac917bc2f9d13
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:52 +0000

    physical doubt boring still say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import sys
-def print_python_version():
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import sys
+  def get_python_version():
         return sys.version
