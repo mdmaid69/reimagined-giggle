6b651fcf7a0cd2f997c210a01524af2ae1051d34
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:45 +0000

    square month star wet knock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
