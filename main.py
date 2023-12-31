e9eae1a0eb3c06997bda66567873f01051ff1fa3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:57 +0000

    lookafter pause mad personal sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
