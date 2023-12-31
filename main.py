b4c0d62ba4eb0d1843a7e7b4bfaade0c59dc7c98
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:44 +0000

    tonight eat communication present supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
