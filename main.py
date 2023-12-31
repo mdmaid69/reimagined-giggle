754f9e9b5a65e9023094eb5d40e9062711e1d9e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:38:21 +0000

    tonight bunch lucky suggest lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
