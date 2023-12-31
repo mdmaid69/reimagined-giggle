6fa71b7ad9369368004c1618a18e8b95d5116078
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:33:42 +0000

    my mess wait trick little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def sort_list(lst):
+        return sorted(lst)
