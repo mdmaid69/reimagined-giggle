a093e44d0353d106357f4eb7ae0a1282248e5a4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:33:46 +0000

    callout workupto exercise manage no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_area_circle(r):
+        return 3.14 * r**2
