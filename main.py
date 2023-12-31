e80db80076e3eade1f0912f1f4193d3f85a1bc50
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:54:21 +0000

    afternoon common report angry study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
