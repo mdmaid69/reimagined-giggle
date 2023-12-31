f1a6a2924e0cf06654ba5c3f299efe123de52904
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:27 +0000

    maximum watch complete variety unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
