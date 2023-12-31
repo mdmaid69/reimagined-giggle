f3b3aab15df413c755a81be778c81f884a185ff6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:10:07 +0000

    water willing instead model deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
