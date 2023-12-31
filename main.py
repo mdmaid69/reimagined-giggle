5732ace07e4cda30cc039d5a188d658b1673d38f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:37 +0000

    warning the that mail bit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
