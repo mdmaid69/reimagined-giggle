af72a1bca5b49dfe81314f7e06fd2676100b38b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:51:21 +0000

    order help appear science grateful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+for i in range(10): print(i)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
