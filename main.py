cb8de424084399cb897188c20a01d1bb80204b83
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:09:53 +0000

    call perfect thanks long turn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print("Hello, world!")
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
