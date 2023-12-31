45729514a9e003fd1b9297eec079634b5f72a5d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:12:26 +0000

    lower century sweep calm turn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def convert_to_hex(n):
+        return hex(n)
+  def square_number(x):
+        return x**2
