d5631b0ad680f74fadb65b7cdd5b6c8c236bb674
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:09 +0000

    flavour door evening first discovery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
