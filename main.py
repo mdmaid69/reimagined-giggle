22d27f28e7063e0d2b06245ea81fa12542978fef
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:23:00 +0000

    grand post book confuse comment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
