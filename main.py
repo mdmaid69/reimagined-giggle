94b7102230cc98382e54f66b04afd3ff58035883
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:43:25 +0000

    short goup build build Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
