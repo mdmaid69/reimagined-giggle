174d2953bd0db7fd6588436dfaf0c714053c0292
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:08 +0000

    post criticize Tt take further

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
