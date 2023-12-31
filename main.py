f228baa9425b1610144f546121c88b2118f447d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:40:22 +0000

    thought genuine test steep pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
