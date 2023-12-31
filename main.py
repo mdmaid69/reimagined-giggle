c3658188e4f86b179b9aa88e89cdf9564ce86ea5
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:01 +0000

    bread room disturb stare bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
