2350e3f794b4006c567eed292fb86425e3da7d3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:35 +0000

    Bb beautiful mysterious perhaps beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
