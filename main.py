f1f82ec1f49cf364b52ba524f041cd94de395269
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:23 +0000

    skill object centre mine fashion

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
