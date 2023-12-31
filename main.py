56c3c4546a318babff63c5ae73c442982ffa2dc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:28:26 +0000

    letter doubt along nerve rank

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
