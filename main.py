546f589ac259b6ca513cff267663d2964c41b866
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:55:43 +0000

    pencil cry turn only line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print("Hello, world!")
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
