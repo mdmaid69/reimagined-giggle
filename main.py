6b28991834d027f7baeabe8825be014201e2dcf2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:06 +0000

    glass thinkthrough early worse again

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def multiply_numbers(x, y):
+        return x * y
+def square_number(x):
+        return x**2
