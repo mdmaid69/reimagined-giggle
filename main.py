6d12b9332e2f30b14da471f32bebe2596b9a2d6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:02 +0000

    part fairly than date blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
