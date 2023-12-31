5042c7df0b86f9653e3496f5fd8346ee26910eb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:04:41 +0000

    bringforward party open everyone above

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
