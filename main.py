e9acb92581feb213ded89703c6acd8ff727de366
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:04 +0000

    dream path live charge legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
 print(time.time())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
