995446d38f63f7d7deb867b5e2cd4705dac38d79
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:08 +0000

    under wait mean range hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+for i in range(5):
+        print(i)
