02e7d7b68f057e91af8d1834fa71657d7c95a088
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:05 +0000

    arrange slice community cross skill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def is_even(n):
+        return n % 2 == 0
