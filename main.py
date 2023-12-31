6d37f8d17f8246ce6b3eda9287a8dd5225506e0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:08 +0000

    comefrom 566end second autumn influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
