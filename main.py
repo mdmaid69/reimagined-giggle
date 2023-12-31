a709b8f014b7e0f4d36b912ec0753fb700779d58
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:29:06 +0000

    around takeover towards disappointed free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bytes(array):
+        return bytes(array)
