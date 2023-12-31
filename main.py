da52d1bedf9fc6db8b1ac75eebf8a0b795256cba
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:23:15 +0000

    artificial factory left alive rest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
