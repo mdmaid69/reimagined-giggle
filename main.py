efbcee87eff31700333f7b508b09c710526f51a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:43:02 +0000

    back pour fasten peaceful evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_repr(array):
+        return repr(array)
