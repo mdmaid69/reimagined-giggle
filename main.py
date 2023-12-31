fa5006ae4ac1fd9afe030784854bfb77952809ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:00 +0000

    coffee straight goback(to) authority up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_itemsize(array):
+        return array.itemsize
