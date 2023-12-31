6c86ddf184b4a8e066f2b063c930e27cc06616bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:02 +0000

    yes new tip complete error

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+print(math.pi)
