9059d9a7c9f76c634bacd5c28ab4778108853dad
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:26 +0000

    neat rich anywhere benefit play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
