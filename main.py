48e88c42a28d2856926486a96304b8517f236bf4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:51 +0000

    climb score fold miss spend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_item(array, i):
+        return array[i]
