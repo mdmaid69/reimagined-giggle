e970e0fd5d4c88293cbe09a8660eec9549fba772
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:32:37 +0000

    hear day Dd jump compare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
