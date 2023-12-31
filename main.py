4131f7b5e2867cd18e4b3bd910fa497c284765cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:54 +0000

    copy means glass wire total

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import tensorflow as tf
+print(tf.__version__)
