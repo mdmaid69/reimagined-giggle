9bb1ec996127539959090820addc6271e97abf6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:59:24 +0000

    neat none water below along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_work(force, distance):
+        return force * distance
