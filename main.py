0df78847f9121e9b4f5a983cf035160f2b1f6054
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:21 +0000

    show point message transparent fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
