6735e81cc6d0d5458d802b37edfad4b700dbce80
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:23 +0000

    proud boil favourite light prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
