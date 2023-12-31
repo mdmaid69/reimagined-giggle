61a51f122283380fe6c3afece0c6e938c27030ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:27:55 +0000

    lock wherever declare remove crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
