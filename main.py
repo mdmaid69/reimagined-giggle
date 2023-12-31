b252a2c53a1a9b2483b67ddb32d0d8b06e9faf87
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:45 +0000

    beyond declaration class drive clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
