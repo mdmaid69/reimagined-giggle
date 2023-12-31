36de028deeaf546f6937826edaf06a62e10d1a3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:41:36 +0000

    hold entry finger by heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
