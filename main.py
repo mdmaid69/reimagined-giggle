196665b2379e1249f849da264bbe40cc3d5a574f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:05 +0000

    lookafter race state concerned occur

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
