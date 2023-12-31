c244108841475163364d0437505d802e32fc0655
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:17 +0000

    priority even black discussion creature

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
