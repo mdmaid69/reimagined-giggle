f9d6c23ddc71d80d8cd8e4e7a41b97d33799180b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:41:21 +0000

    flood fat act print mail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
