34a0ca359f9b9b99d54198e63693858152b7d9b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:33 +0000

    cost deliver nervous prevent single

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def find_max(lst):
+        return max(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
