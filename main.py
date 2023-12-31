df5612a44f28639c2598851290892128c76d5297
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:31:03 +0000

    weak open rare turn dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
