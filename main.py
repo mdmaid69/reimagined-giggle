ec28d7617efb4b7088301cc9030bbdf37e0b5fa8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:00 +0000

    pocket proud yesterday advise under

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
