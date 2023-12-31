7ccbf62388140eb9c9f44dfd7338658e9f75b474
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:02:43 +0000

    worried pump holdup steep broken

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
