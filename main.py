edc38cb3588b89040b9affe6ef0a1bd8aeda61ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:10 +0000

    steady bridge spite present turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
