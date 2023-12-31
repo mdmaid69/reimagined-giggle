4029ac106054d8e1b2f2afff33b816bb352b225e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:43:05 +0000

    pure direct behaviour seat rush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
