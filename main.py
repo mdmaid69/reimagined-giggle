183b18c70f41b5e164d405c87117c1f8b7cdf9f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:39:44 +0000

    their duty mess bread avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
