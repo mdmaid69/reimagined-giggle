f1cff6636a316aa0cfceb5ce1a46fe427669e7c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:47:43 +0000

    post point right hot formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
