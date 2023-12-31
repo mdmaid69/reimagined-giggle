6f1b2b6a05b995b02f73b515760eb74d0d386686
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:36:58 +0000

    basis game fear taste risk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print(sum(range(10)))
