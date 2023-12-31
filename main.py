22010172887b125274f667fb56c0901dd0f6e789
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:32 +0000

    every listenfor direct recovery practical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
