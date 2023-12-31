f57ebde7a55f16652fb229130a235a3125d0a0e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:43 +0000

    popular mess getout awake sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
