980f42c95a81561561409905159e405b520105bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:55:29 +0000

    believe knock content block second

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+x = 10
+y = 20
+print("Sum:", x + y)
