f90d950c605ab91a1fa2b3ed9deb9233479ed53d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:47 +0000

    warm makeinto breakin go region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
