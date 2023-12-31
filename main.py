fcf7d382b4a8ed33ac067f526ce28e09fd405f13
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:00 +0000

    break general responsibility heavy unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
