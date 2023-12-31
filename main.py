208620af3acb47a35b7cf0d74e46af5fe2e68dca
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:43:40 +0000

    sell scene colour service best

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
