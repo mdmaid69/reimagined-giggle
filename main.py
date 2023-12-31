864d422d27f4e3e1c52dd61d53709797627e287f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:38 +0000

    innocent excuse disturb sell onto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
