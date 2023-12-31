e04302b85fda548e19ba7288cda4a8f8960bc94c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:16:36 +0000

    comment whenever repeat tip rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
