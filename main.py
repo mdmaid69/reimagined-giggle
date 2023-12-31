94d9a57dbcc9d27d4a8753f6449ac3821fa1dec7
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:06:52 +0000

    breakout sentence shelter communicate though

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
