07eed7f426811aae85509490789a462ca62c68de
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:56:21 +0000

    refer perfect hot child rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
