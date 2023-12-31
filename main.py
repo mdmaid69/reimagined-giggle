05ac3efb987896dc3dd054b48aabc6aa2389f74b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:55 +0000

    rely realize charge Zz anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def get_array_as_float(array):
+        return float(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
