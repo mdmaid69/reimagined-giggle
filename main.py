d0160836aba4e604653267bfd80f11147117684f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:30:53 +0000

    force force place modern underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
