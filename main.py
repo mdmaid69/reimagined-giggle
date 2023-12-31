e4eb79b1344e6bcfb38c887b6ff910f857c9e9b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:42:57 +0000

    reply turnoff crowded shout meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
