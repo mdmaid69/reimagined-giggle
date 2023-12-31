47b88a10a711470275ac1d2c458973fe46e024c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:05 +0000

    wrap disappear dream tend container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
