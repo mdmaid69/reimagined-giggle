027e76e3581ad2ce9e48b4c542f5ba1ac835467a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:59:50 +0000

    only sell shop body closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
