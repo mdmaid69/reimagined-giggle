76f5e8ac71392f23078cfdead69af328214e4963
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:43 +0000

    load almost look improve border

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
