87613ff9280d479ae3ce8ee3a5ea966d981c849d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:05 +0000

    shine unlikely gather fail introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
