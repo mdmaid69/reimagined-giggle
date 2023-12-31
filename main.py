461a8d87074f5b5d97ce5fcf455f7610c831706e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:22 +0000

    survive hurt command mail factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
