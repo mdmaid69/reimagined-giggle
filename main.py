1989d886073db5bd1b3953a52d54215074187225
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:09:18 +0000

    attempt certainly cost declaration modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-print(datetime.datetime.now())
+def find_max(lst):
+        return max(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
