db4ec19eb1977fe77faad9c3f114a3e9c9e7c54c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:42:14 +0000

    patient fairly upstairs support mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+print(os.getcwd())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
