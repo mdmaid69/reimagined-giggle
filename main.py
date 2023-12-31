b78e0993154f3b0afe66505eda924196cd07f570
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:51:43 +0000

    more keepout march comeback left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sys
+print(sys.version)
