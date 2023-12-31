83fc720963e06ed77ff80bf8e95f8769c3fa7b17
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:43 +0000

    most classroom comeup stupid squeeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
