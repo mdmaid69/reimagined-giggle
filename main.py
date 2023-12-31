96f07c98f5cadc61133986946737b7b645ef4a3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:34 +0000

    noisy nervous whatever try glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
