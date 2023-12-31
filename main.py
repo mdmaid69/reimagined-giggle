f36968f47bb5923ad65a7b4b7e6fc187cf5d1fb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:00:19 +0000

    whistle shopping excuse range eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
