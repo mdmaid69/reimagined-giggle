c6f170dde8ba857b3777cb7863e78e464443e8d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:10 +0000

    wrong church history damage quick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
