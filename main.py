74688e54418bf95ca0d79760eec6de1c7bd8d22a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:10 +0000

    otherwise ourselves phone let on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
