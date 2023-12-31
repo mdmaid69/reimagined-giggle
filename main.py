3e30f9ffbe5ec709bd25da2815c57e8775090af2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:12:18 +0000

    smoke more part later care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
