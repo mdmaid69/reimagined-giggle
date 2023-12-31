24a761eabc8f267ea6f6b2c305c04803c69b6c00
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:47:57 +0000

    sleep person similar first top

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
