5c1a26a468f490dd250cb5bdec61e6e01ca28437
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:02 +0000

    will home fork permission sea

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
