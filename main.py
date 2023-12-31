f178b4a665420be01633080abaaf8bbcd172d2f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:43:09 +0000

    next space problem shout land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
