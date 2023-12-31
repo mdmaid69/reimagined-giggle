f5b296426bf0badc011375751386d026799cc88f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:41 +0000

    ago regard wall design safety

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_list(array):
+        return list(array)
