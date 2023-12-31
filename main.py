8f4d0dda018bea5fa492ed89ecb081fa9b0348b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:07:55 +0000

    pipe foreign street fail probably

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
