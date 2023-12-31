4961f1b2c0d38b22573366b01610b6f17305f088
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:37:50 +0000

    funny independent used judge flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
