292101f5264be83e86d458372c709d141e8a37ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:55 +0000

    past wait middle song next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
