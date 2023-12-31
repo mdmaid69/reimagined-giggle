8cfd092bc5c8afd560367d7a225496561088b446
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:40 +0000

    minority behind difference soak accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
