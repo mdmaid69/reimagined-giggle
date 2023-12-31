3dc41c8a8cd6aca0d9ff035a793b38870f2764ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:47:13 +0000

    herself prevent result test direct

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
