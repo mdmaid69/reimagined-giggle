5c2e1f63945b719538fc61e715ca45375ca61c9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:05:06 +0000

    quickly home middle suppose hospital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
