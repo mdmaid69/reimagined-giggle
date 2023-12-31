6d9f8be3e5f38746af509ac05b16fa6b122a3182
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:54 +0000

    react old surface rely country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
