21faa7cb269f540f0f23d4716ba0ac4eeb5a5b73
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:06:47 +0000

    Vv before stop translate day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
