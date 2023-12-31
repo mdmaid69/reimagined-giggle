081aaf0556f7800eaba096c06e4140944719b81a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:58 +0000

    round understand sound separate square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
