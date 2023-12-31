6ab510d602921336c7d88a89708eec38c61d8604
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:17 +0000

    measure ahead squeeze temper sweet

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
