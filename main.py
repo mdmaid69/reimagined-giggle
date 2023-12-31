14db937ad6069666cc6ed264ff3a49c69e76ac27
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:44 +0000

    respect factory unit kind trap

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
