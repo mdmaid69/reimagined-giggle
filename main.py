7eade768618c94dcdf571d103b0ed7561d118eb8
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:41 +0000

    something little report environment straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
