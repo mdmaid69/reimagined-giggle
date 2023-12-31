55d6a1f528515fa29f588bca64db31993899e649
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:14:51 +0000

    tap rather exit speech regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def clear_array(array):
-        array *= 0
+def find_max(numbers):
+        return max(numbers)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
