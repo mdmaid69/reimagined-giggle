9a0f56cfb23f9b9c683e397da9b6e990f5aeedaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:03:45 +0000

    niggaacademy grade impressed instrument table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
