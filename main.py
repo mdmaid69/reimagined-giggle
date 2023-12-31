baae2fb59e8928b4a0ba3fbbc6ae90e235d4f708
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:21 +0000

    treat represent knowledge claim bite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
