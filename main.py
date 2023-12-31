0beee0ee5a795695b995ed049b58dd245e733acb
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:28 +0000

    about division tell method detail

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def count_words(sentence):
-        return len(sentence.split())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
