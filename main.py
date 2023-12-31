374401fb67d427d83e06dbb14fdb8eb4bcb77197
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:04 +0000

    judge squeeze pause interrupt boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import re
-print(re.match("h.*o", "hello world"))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
