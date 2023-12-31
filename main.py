9ffac220bc584d188aa25a647ba38d6f615644e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:01 +0000

    murder brief when interfere position

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
