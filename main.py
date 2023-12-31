a81e48bfdf8946380b7b922965e1e7639348f1a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:37:29 +0000

    keen crisis attitude again break

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
