e442dd510513c2ac5e4d8331afbbb05ab1d18799
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:57:48 +0000

    conduct then wet flow another

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
