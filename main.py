a6e1dc00b4234b1c7d4a04a74ce90b52c6519ba3
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:56:38 +0000

    prize slowly shape measure than

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def convert_to_binary(n):
-        return bin(n)
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
