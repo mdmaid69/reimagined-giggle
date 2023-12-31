b7b995ae76a29b4244ada592f55f7f783ecd55e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:31 +0000

    pinch stand eye content care

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
