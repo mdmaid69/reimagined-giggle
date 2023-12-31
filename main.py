56d276548cd69ac26ff9ce2f9c1212e71afa4e2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:24:35 +0000

    freeze gooff produce through gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
