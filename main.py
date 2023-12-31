55ef074bb94d493e1aed1eac5096d3e89c2c341b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:38:18 +0000

    shallow backwards impressed week responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
