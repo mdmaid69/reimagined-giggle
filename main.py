fc77fe1be3b5f46673daa0c88fee3f8354030f9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:02:27 +0000

    miss blow workupto dark reliable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_array_as_list(array):
+        return list(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
