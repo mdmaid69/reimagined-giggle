dd37ff0f4d5719cb1f45f25c91cd730e12f3cf58
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:39 +0000

    basic demonstrate for search enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
