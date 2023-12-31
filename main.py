9626d90eab674b259f0fc705bf4254524ab6237a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:45:32 +0000

    wherever anyone flat about treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
