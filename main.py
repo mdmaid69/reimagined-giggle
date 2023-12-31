3c81124c261c9e5200abaf9590efa9fcd2a22c42
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:21 +0000

    careless sink platform lookafter relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
