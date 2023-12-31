32bbfc5c40035d73f1ab223081dc68c425ad6800
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:30 +0000

    game beside we sleep name

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
