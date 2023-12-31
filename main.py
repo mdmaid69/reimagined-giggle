2aed20a68e773e3dee7b1a16bb40dfdf0c158dbd
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:02 +0000

    later forever lie content concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_density(mass, volume):
+        return mass / volume
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
