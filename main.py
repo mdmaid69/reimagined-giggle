3baf4775319ba0c866e0eca3bf03799f573dc6d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:23:15 +0000

    whistle keepoff put project defeat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
