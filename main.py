7f90455f0e866e63f9bf050a5efe1468c3f348f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:12:59 +0000

    born wash glad past pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
