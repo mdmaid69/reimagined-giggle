d732d04c40d33f2a14834554db92a30df11451e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:13:25 +0000

    brain tongue nothing break so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
