f08fa7bcede310eb8cf00bb94c7db213f59803f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:02:40 +0000

    sometimes distance dress occur expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
+import math
+def calculate_tangent(x):
+        return math.tan(x)
