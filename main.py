f8a0a9184e2d478483517d2995871a1b385e09e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:56 +0000

    him high cook education squash

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
