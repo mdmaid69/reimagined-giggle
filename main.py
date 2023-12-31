dcdc8d4d3bb721d7b51483516b8191963d37f040
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:19:41 +0000

    protect regret next love fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
