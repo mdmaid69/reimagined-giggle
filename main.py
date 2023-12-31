b5d28b24e0ad3faf34fb3018b146cafbc939b247
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:28:22 +0000

    break bear follow get free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
