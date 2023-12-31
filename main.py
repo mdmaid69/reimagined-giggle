214d7821a9daef1e6396c6528235d7668729b5e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:55:17 +0000

    level slide bush determined damp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def convert_to_octal(n):
+        return oct(n)
+import tensorflow as tf
+print(tf.__version__)
