efd3289a4a6d9294adce8c51f328711f0ed5d46d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:53:50 +0000

    film stir we commit turnaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
