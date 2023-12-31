4089f73b04bfefd3e548148e82fc2952dac97b13
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:39:21 +0000

    occasional stage cool target phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def count_elements(lst):
-        return len(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
