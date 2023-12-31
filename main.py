be002ad854fe97edcd6224e8b2103e5a7a1478ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:32 +0000

    Ee majority clear clear Ss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
