aebb182615cc0452f8094308e3c887d484b93b31
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:11:25 +0000

    heart play replace light interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_counter():
+        return collections.Counter()
