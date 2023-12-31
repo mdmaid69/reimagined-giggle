770ecb6f2d4397408f8cf23cd4eb694eadcea038
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:02:46 +0000

    knock explain type seriously silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
