bdcddbb5ac56f6296007a227bedf371f3ad885a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:37:22 +0000

    spend entrance affect pair crisis

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def cube_number(x):
+        return x**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
