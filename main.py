ecbab11fb8c34272b5f7c14efec7430c223ba3ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:25 +0000

    broken except wish probably peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
