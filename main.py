071dbf078da2e0f85ff36696580cd31e104809c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:52:41 +0000

    unit freeze environment upset tragedy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
