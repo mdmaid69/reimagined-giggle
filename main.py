7944484c56d6eea0e3d41b183168b25a9f4bc236
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:27:10 +0000

    material walk law here steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
