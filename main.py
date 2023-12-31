d93a7b830d6179514c2cde43dd7b986fe75a2a9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:32:20 +0000

    turn steer shoot tread vision

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def add_numbers(a, b):
+        return a + b
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
