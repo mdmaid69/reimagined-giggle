7f69dfdbc3d7990b59e23426f08dfb919fef3baf
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:26 +0000

    let hide expect firm describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
