8572ca83c193c3e9443c5b60ab6aefa529b8b7d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:40 +0000

    stranger blow most talk ball

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(lst):
+        return min(lst)
