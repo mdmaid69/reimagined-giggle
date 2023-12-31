92cdf4d0b9300bc0c75e1209a67a632ea52a6ff3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:42 +0000

    performance have roll beat track

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
