519e91ef85c2e6cc628510394973c9e44bd77775
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:00 +0000

    throughout left bother connection discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_typecode(array):
+        return array.typecode
