c38d80c5a015bf1775f76549a43dc75ea260af84
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:53:40 +0000

    programme spring wealth start unkind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
