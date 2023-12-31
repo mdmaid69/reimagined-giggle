5078cd9428afef6bbaff652e31990e3093dca78d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:38 +0000

    common opponent flat heavy snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def split_path(path):
+        return os.path.split(path)
