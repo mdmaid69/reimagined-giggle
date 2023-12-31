af3820a61aa299b7a0a1d5fef4b8f074d6b6a8ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:34:02 +0000

    great there stone wave star

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_current_working_directory():
+        return os.getcwd()
