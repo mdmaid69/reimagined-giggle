4ea93b8cb58b41804533e74edb63f96308d1f839
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:06:29 +0000

    turninto glass change worse sink

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
