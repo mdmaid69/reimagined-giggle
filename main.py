8b77d3c0bbab0d8dab237f4b8dd0a62bdc820a24
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:23 +0000

    question conversation rest recognize suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def convert_to_hex(n):
+        return hex(n)
