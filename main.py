1044500009c4e3dae0d83e3dbe1820caa9dacb6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:20 +0000

    different would keep unfair keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
+  def is_odd(n):
         return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
