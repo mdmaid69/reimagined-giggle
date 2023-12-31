d3ee9dd23e813e3758c0c5cdc9b5b9251b58adba
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:50:32 +0000

    congratulations thing bored month only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
