c6bda4c5d40782a94d5dad71b2e1e9e7741df0da
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:44:46 +0000

    authority owe improvement anybody characteristic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import time
-print(time.time())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
