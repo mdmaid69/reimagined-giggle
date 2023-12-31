95ae053fe5f587d340b8a0506fa64951763b5907
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:25:41 +0000

    address callback generous night break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
