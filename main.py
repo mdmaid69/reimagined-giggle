61d59fc3395caa7c0510b92a15d7aeb0763e7164
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:03:50 +0000

    slowly nervous advice quantity where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_acceleration(speed, time):
+        return speed / time
