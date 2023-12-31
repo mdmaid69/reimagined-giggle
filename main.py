52d22698d4e41c2ce880f31276bc0e9c17ca0d2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:58 +0000

    now note mail silent cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
