bdfea1b4f6dcdbd9d7139fdd6cb547e77e4d39b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:51 +0000

    fast basis contact age splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
