2eba744da8609ee89a0ee98313a80a4164d997c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:54 +0000

    unpleasant oil certain dare computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
