3a7b88ac7becbc270b6c9e1525f357d4f2cc1c26
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:36:20 +0000

    foreign you ordinary object welcome

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_str(array):
-        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
