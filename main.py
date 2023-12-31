84b3f4ed8e5dda463327378fa7362f3202646b4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:05 +0000

    surprise study earth anxious ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
