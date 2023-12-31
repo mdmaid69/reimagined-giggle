42305e858b623ee9dea8196b0bf78f7c2999a845
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:27 +0000

    mean start limit interruption now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
