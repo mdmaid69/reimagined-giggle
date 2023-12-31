3a8d1556372725083afbf6b6ec578f6259230e4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:08:41 +0000

    bound noone corner claim ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def find_min(lst):
+        return min(lst)
