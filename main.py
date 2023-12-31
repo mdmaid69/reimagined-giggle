c4ed1513adf77ed2eb2304db8c46324878e0603e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:37:58 +0000

    beneath smoke figure excited of

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
