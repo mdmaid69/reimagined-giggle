47b0cb3c187572ec8e177648b86624a8d4186b3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:52 +0000

    congratulate far friend always strike

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
