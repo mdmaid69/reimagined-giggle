976b4d5d9d4fe9d5ddc98a0e5c2be23af8999696
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:20 +0000

    chief estimate deaf ours middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
