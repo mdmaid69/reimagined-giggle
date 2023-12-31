2f5f4bd95d465cd27ba8fbda0751a64cc7a5162f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:26:41 +0000

    form tip special only already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
