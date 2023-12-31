08087d869c87585ca8fee18297f5230a9ec38bbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:45:17 +0000

    innocent tread national contrast father

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
