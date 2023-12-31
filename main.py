3ac8bd951dcc496834409366230412634e7182a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:59 +0000

    board run ask perform comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
