166245e4c1cc1f3a17451e6073315e600e64ec4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:37:24 +0000

    purpose reasonable match terrible abroad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
