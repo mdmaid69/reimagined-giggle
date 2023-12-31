090e856bbb9bc2e600c427510816609225ad32cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:18 +0000

    border book minor improve bang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
