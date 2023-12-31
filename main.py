028656e5eaf418e6a91648f4d3b2d62d7bc8d89d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:49 +0000

    circumstance string search raw occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
