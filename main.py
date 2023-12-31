3e64cf2c9bd156b999292b00983e9363695181aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:37:18 +0000

    sign probably bringback general damp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
