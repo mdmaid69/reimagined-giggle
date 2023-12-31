8fc6702fb8aa4ed658d4b305f66499aa8fcff298
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:54 +0000

    scared concerned boring deep wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
