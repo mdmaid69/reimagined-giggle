835e924daaa7da4aaf84bbfd5abda27d0db5a631
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:47:58 +0000

    bringforward pump permission comeoff clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
