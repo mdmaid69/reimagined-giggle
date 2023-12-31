b37b2f022b7c76f8abb06f915315b8528b005072
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:55 +0000

    believe special prevent early thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
