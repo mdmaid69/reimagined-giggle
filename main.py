7d129fc6b1b03a823222bf23dc97af542b01da79
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:48:11 +0000

    suffer take dig knock myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_stack():
-        return collections.deque()
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
