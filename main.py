de0a3dbc36289e41dfedd7cfbad4a6386dc6fdd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:08 +0000

    culture outside near through share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
