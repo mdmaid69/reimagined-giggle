c3237117cfec3164be941c8b20f8e592e7fbd322
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:58 +0000

    cope unusual might cap pleasant

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_sign(x):
+        return math.copysign(1, x)
