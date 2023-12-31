d7a7e51229307c02e21e7a6696ef6d3da761ecce
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:31:30 +0000

    cruelty basic possibility wipe note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
