ca5dee48d39a3b0bc868d76ed6bd3f1a85401ee2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:10 +0000

    gentle join worth talk turndown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
