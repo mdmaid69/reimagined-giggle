81498c4d395900c8a5aff2f0c1dd9781a5d7e724
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:04:04 +0000

    attention information best trick Oo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
