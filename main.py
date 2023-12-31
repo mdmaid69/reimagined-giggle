e42fa01c4e6198333798b65e58877cdd87b794a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:52:40 +0000

    rid exciting callat must patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
