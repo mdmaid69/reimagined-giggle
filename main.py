e20f0b1516419a7e9b3918a3a5680af35f2ac7ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:00:57 +0000

    chance crash rub feel opportunity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
