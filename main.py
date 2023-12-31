1deb68efe43ffce59304311b5cb8c48406fbc44f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:36:13 +0000

    other opinion before whatever fold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def is_palindrome(s):
-        return s == s[::-1]
