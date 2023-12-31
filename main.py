926980a4cd923bcace4d1ddc7b05505c7cae0c78
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:36:33 +0000

    favour smile doubt better judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
