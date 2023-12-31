aad34faac767089ce3abd374f7316481e6a3e425
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:11 +0000

    related dry before exactly Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
