2ec26fe3edb7fe2e2df784498fb4cd52f1fc806b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:27:04 +0000

    likely soak workupto blame story

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
