3bfbfc3de7111c92c86b567a0403eeef1f79da55
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:49 +0000

    weak own artificial exam amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sys
+def exit_program():
+        sys.exit()
