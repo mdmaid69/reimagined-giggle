deef3fc3756dcaaa44ccfda84a467bce322bbc6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:38:55 +0000

    twist flood complaint independent apply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
