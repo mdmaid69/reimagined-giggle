95537e1035db2701fa205a0971f77b130c1cae1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:07 +0000

    forget party typical street beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
