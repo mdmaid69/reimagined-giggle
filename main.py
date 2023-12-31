8c228dc69562875e2add4caa9aeecbe79d9faefc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:29:45 +0000

    lift both sleep contain mess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def is_palindrome(s):
+        return s == s[::-1]
