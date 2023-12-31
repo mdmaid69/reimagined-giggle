92bf2eb34cba5d89ee68c84204dcca587092a2b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:57 +0000

    born because time test soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
