99861eb52615b67dc143e630935b1fd6389b26e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:55:00 +0000

    place tune gate cool majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
