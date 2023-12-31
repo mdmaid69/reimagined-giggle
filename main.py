4dd37f73f249996441ecbd68e409efc9e32149ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:51:24 +0000

    slice trust could broken address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
