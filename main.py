511619f35ce30ba59be15e4895c9af4e852a26c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:33 +0000

    pleasure scene slice hot boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
