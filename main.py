d736900d68a3173508cf96b0b592ae5537c39eaa
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:54 +0000

    hospital dress keepfrom no confident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
