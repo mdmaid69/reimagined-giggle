3487d94f44dd3aa6484114ec766dd74476d0c35f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:48:30 +0000

    regularly hold consult change man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
