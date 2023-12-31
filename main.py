8f0ea3b7c1c5fa8864691d4548ab260dfcb00a35
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:00:24 +0000

    suffer principle floor distant break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_typecode(array):
+        return array.typecode
