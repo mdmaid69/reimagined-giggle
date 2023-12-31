a227ad9282c7a619fc25c4f79109149cd8093f3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:45:49 +0000

    roof display noise skirt rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def append_to_array(array, item):
+        array.append(item)
