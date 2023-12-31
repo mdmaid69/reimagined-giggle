d5ca4a488277beaa962d4c5133ef503993c4b8dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:25 +0000

    turnon cold fairly drawing deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
