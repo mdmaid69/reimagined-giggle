57f31e831de7fd8d0807b92a9a63a6d20e56b139
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:16:35 +0000

    butter own remove couple choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+for i in range(5):
+        print(i)
 import os
-def remove_directory(path):
-        os.rmdir(path)
+print(os.getcwd())
