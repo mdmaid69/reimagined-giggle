d632ddf551a75daa47a93c8f79d0ee88e62d406e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:06 +0000

    clue weak fold never almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
