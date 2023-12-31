ee9fc66053f3fbed46fe8631802ce834a477340c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:22:45 +0000

    tonight bush talk excellent side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
