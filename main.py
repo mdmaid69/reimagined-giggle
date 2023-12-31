6587fb166e86640e1fd1ae0d3cd74841e5e70873
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:05:09 +0000

    breakinto short stick desperate healthy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import platform
+def get_os_info():
+        return platform.uname()
