e1920f33c31bae34f2b6ce8deb274c8770316102
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:33:18 +0000

    wake sad purpose fall sometimes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
