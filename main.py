850154d949bcf9b46559bcd891f11fb6fab3e3e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:21:02 +0000

    behave better flavour possible in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
