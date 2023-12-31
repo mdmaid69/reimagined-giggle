2347d1b21fdcc028acd547aa767975f3f141612f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:05:19 +0000

    begin demonstrate cross remember powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
