463a19bd58698ece66170ae35e847e65c709301c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:43:50 +0000

    inquire continue crash afraid correction

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_item_count(array, item):
+        return array.count(item)
