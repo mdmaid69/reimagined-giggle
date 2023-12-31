1193026d08cf7c5a6d91e655f979014f9bb0d6f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:36 +0000

    spite mix deliver woman bend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
