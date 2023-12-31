eacc404cd912aa9387f4bbf97a93a96814b7b3d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:17 +0000

    joke nervous measure check keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
