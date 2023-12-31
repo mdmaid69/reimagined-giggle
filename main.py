8e5876586dd0ed59dbac6e0a9aa032feae3223f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:20 +0000

    afford calm the him rich

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_repr(array):
+        return repr(array)
