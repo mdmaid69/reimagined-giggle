01d55944c9da773ed7355ee258f745437eba6391
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:07 +0000

    them pain lookafter whoever taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
