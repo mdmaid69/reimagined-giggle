570c2f22b2f7d939fcd0885eccb2c9da5f77779a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:03:50 +0000

    mail break over swimming invite

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
