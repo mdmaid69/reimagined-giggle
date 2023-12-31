865534927599715a9a47ba65e21933e5b803ddcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:58 +0000

    book takeover branch steady confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
