1366fcce325af87de60f5db040096555c75e5efa
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:02 +0000

    dear comment change goodbye group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+def exit_program():
+        sys.exit()
