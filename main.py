e8729804ec79fc7c1886ca708c8efa7c501fa4ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:39 +0000

    shop impatient 572enter reason handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
