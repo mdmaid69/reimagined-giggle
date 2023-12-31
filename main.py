7ed614bfa8f496aa84598761b06fc2d8fbee68ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:42:41 +0000

    talkinto wish rush circumstance finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
