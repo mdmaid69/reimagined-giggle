52649e80665abdc948cc57d445775400acd112b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:22 +0000

    apparently violent raw out typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_stack():
-        return collections.deque()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
