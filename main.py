bf02880a137b39ebe1bb60bb74d0f5408d380b1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:38:09 +0000

    trick perform protection uncertain shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
