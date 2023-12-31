e33d70c15590fc1ef22e5cf7c6cf704a35289f87
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:57:52 +0000

    gas nowhere black fasten mystery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
 def calculate_work(force, distance):
         return force * distance
+  def count_elements(lst):
+        return len(lst)
