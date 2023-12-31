73b920ec0d7e386540dc41d19c9b24c45b3e58a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:56 +0000

    scene far wait everything hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
