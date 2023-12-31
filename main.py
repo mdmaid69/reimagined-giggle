e6959fd7aeb38f2b97ab01542d00f6f14295a968
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:10:55 +0000

    send bang change month all

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
