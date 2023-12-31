10578f9c2bfcd7c4777d9e4f71fb0213bc8e7892
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:25:30 +0000

    probably afterwards either steal race

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import sys
+print(sys.version)
