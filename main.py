47587464ff78ec1b921253871458cf281a5131e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:42 +0000

    die herself lucky sleep free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
