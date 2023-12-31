c72aae167dbf587d1933845cb089004f6fd21b9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:01 +0000

    opportunity sticky justice stamp free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
