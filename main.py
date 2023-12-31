04dbf4ee45a4aecf130c80c363fa1b9ffb707c2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:41 +0000

    ago trick couple private spoil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def sort_list(lst):
+        return sorted(lst)
