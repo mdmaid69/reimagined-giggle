848b91b43daf8ef558427a81971fdf753f9f6210
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:30:35 +0000

    power waste maintain sentence trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
