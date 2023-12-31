cde2f53f10cd8be86400d1db3e4cd6ddc905f9f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:53 +0000

    foot report platform excited because

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
