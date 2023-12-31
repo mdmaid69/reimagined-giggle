b9846520d6316b51a75e35958c12b24a23bc309d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:05:38 +0000

    arrange pile she always whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
