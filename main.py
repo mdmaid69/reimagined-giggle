950441a394e27b7603ddf54e6494b3daa51ad2eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:54:28 +0000

    exchange spray shopping dress foot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
