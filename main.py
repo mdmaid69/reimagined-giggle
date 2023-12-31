bbdf75180ab281d66d12552cb70fa3ba9578aea9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:08:32 +0000

    next sink major strange clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import sys
+  def get_python_version():
+        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
