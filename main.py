228c8e78f9428d2112390a872071a243996e696f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:52 +0000

    wake interest everyone top decide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
