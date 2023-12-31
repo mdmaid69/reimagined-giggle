daab033031112d5e99ed2cef81b668115d095a39
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:38 +0000

    estimate speak steady wave admire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
