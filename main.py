e49c241eb101b81608bef99a3fa36650f899640a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:19 +0000

    threaten fine busy mysterious opportunity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
