482eaad410755d236a84dbae96ba0fe677125083
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:02 +0000

    tell sing cross impression arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
