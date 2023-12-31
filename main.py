d8600ad4db485a1951ac91a5dfef90f8abb35d28
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:28 +0000

    defend smooth something regard sale

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_min(lst):
-        return min(lst)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
