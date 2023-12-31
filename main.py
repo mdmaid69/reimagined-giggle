3a58013651a1a2195e24f1e8ae22991fa52363a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:57:47 +0000

    neat politics surprise mention comeup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
