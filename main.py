4ae58809b610bc93ef40e65233fb4e2955bfd1f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:16:32 +0000

    bored available deep march view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
