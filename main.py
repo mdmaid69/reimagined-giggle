f22e78784e1952e44eaa8ed457a9e17809b8c53b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:49:59 +0000

    trick measure politics ought floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
