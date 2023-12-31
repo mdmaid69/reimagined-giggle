90c4f37bd009b94cf9bda558a8e823bb0bfa89cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:27 +0000

    dark would satisfied crowd defend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def reverse_list(lst):
+        return lst[::-1]
