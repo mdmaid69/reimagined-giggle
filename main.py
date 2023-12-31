fc1c78d611fb08b10334c439fda39c06960500fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:03 +0000

    target official treat pump maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
