0ded3d19b7ec6893d11437be25dfc0ea7e3bad98
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:05:27 +0000

    compare thin learn hers wonder

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+print([x**2 for x in range(10)])
