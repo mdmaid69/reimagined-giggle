040cece03d639f59d4618fae893e8f6c86ca4a03
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:08 +0000

    street expect free come Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
