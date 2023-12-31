8ebdd4a6b619c828939da2db4280b036557157e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:25:30 +0000

    them demonstrate watch steer control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import tensorflow as tf
+print(tf.__version__)
