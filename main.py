f336e314de9ffb94d5b2a153d0ac85c873d57575
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:07:48 +0000

    evening schedule decision contribute money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
