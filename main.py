5e7ee55b5cd83c273d7813c9d841ad436fa3e443
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:49 +0000

    give familiar rough rubbish belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
