cb9948ebe835d3668fc70cec087653a516389ecf
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:13 +0000

    rest write loss ground demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
