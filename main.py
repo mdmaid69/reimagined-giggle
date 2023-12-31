2e62c2511abd8523a681d2c4617c4fec987765fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:50:29 +0000

    content strange best disturbing straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
   def check_if_directory_exists(dir_name):
         return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
