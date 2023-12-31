eedf033b0f8dd14fcdd70bba44e854f70136bef9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:27:15 +0000

    serious me frightened wall interesting

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
