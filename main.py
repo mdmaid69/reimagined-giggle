d07c9cf878e02b54a30bc4c0fe9e4c16619aafd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:16 +0000

    conduct judge regret agreement bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
