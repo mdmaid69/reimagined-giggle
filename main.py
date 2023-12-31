ca0fc5516bb21bc4743ef15d4bcad69a9164527a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:10 +0000

    reply she lead 571enough recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
