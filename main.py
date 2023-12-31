1ea42e6527418030d87e5207f07cb4e5dc09acd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:01:55 +0000

    face Nn suit strong field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
