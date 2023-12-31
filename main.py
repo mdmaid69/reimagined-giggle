9ca7026d4dd95f8970b2dc017483f940fde50e91
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:33 +0000

    through runaway meet fresh imagine

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
