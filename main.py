8427951a07dc2e2f5688a92b9ebdf731fdfaf79a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:23:38 +0000

    order by ought thought impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import os
-print(os.getcwd())
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
