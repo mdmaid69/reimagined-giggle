3af7cd4b3324a5ca17190e0d30824ee7420125cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:59:13 +0000

    near second summer culture serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
