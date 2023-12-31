19d6ae4ed64ebf0f9081afc60e1e8070b667cf5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:05 +0000

    refuse suggestion careful opposite apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
