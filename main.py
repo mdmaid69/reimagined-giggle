b0e24556e2a90d7294765551d33502ff6110e134
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:24:01 +0000

    promise share activity pure whether

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
