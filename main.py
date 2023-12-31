eeb5815babea374b16b705b973f9ec442e8ddce7
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:03:36 +0000

    sensible old complete walk high

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
