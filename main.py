4b294ae58b71b0a3d05ab378c83e3cf9f4ba99bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:08:31 +0000

    sometimes not possibility certain separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
