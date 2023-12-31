ffaff45c9a0b50ebb0f6e5106df75b1135bc158f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:18:05 +0000

    single how method at impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
