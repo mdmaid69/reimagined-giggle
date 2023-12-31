9927b5936a9f756e525c2584132de42d005f3685
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:30 +0000

    friend boat holdon state offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Average:", sum(numbers) / len(numbers))
