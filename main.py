bf307a41dba1ba8540f04d5432fa859b65d36259
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:35:30 +0000

    issue memory figure pocket fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
