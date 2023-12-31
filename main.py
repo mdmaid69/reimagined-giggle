46d9272c6f997b57326cf82ffc5d37689f4ac98b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:05:12 +0000

    switch prevent loan creature tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
