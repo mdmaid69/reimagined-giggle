59d68ae56bb6852119fcc1dd282e09b76aaeed62
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:58:35 +0000

    school properly mark Ww vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
