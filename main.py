736c615a74b6a0fabc1ea5416f2e2d0395c94adc
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:14 +0000

    comeback grateful his surprise help

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def reverse_string(s):
-        return s[::-1]
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
