7250d09584f3f59c988327ef647610301a9b1428
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:45 +0000

    train manner reply noisy hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-print(random.randint(0, 100))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
