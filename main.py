c220212bb1edce47e5917aa30647883c35b2b6a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:27 +0000

    government worst distance in cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
