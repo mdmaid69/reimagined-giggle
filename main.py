0c3f8ccefc255ef121a5ae7741ff4190021bea6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:27 +0000

    turndown middle runover shut frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def delete_file(file_name):
+        os.remove(file_name)
