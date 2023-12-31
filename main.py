3f28e71e27b54436bb71fe8b1c951f79494c3a26
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:06:33 +0000

    practical head attempt wing deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def reverse_list(lst):
+        return lst[::-1]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
