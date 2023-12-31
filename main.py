052024747d55eedb0b21cad23dd2d0509b3bd309
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:35 +0000

    television base disappear lean injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_area_rectangle(l, w):
+        return l * w
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_error_function(x):
+        return math.erf(x)
