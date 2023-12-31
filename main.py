ee08aab5e8f560b035a2ae0ad588df7b60a760f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:05:53 +0000

    city appeal wave estimate commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
