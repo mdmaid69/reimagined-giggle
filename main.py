4285029986cb85db3150c018834bdf1a9d30c0bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:37 +0000

    plan for advise same live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
