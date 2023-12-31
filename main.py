ebaf3a35a377cd31923b7c5053cd59a3dfed101a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:33 +0000

    holdup valuable considering lay letter

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
