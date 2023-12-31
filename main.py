14e29c1e9431a9aabacca1c1ebf88eb1bfb3c1e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:13:10 +0000

    formal recognize steam telephone actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
