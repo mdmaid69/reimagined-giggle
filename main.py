9793cbd95cfa02536b2764bb4b169fb40474edb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:17 +0000

    sister punish model reason keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
