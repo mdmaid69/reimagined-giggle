9b886a0ea46d498816735629bc1994a7ddfbb105
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:41:48 +0000

    regularly unit frighten modern thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
