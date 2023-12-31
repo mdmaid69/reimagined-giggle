1603502e07b12686f07b9f9b09396fbe9683b810
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:21:31 +0000

    wild prevent sit perfect twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def find_max(lst):
+        return max(lst)
