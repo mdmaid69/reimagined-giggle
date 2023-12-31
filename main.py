2412dc18ef758290f3925b93496397b887237eb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:04 +0000

    advantage sea actual anywhere offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def convert_to_octal(n):
-        return oct(n)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
