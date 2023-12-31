45ff89dfd7ce5ab6e34eac6cbfc7623e8092ec6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:30 +0000

    extremely yesterday shame ball team

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def add_numbers(a, b):
+        return a + b
