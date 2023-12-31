b617fb0b6f66af445bb67e3b715ae080ccc4787a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:56:57 +0000

    rub plant delight congratulate water

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
