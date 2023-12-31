233ad94339ea1ab60c69a632dda57503401b2562
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:31 +0000

    push doubt wise method fat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
