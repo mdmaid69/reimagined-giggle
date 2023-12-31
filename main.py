29f68838301ab11354583a3faa08f174ae6237c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:43:48 +0000

    on study rain memory store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
