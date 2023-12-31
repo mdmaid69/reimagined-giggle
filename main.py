af312c08be1fb3a741801f06dcc6828829875ef5
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:04 +0000

    preparation set wrap callup cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
