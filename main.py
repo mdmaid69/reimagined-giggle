83568568c9377f8240bb811827383bfb917174d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:29:54 +0000

    tense turnon spread bang breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
