28b8eb94ef5a0fd83d44eab2bd050bd0ab776788
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:11:20 +0000

    together relax recommend talk warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def add_numbers(x, y):
-        return x + y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
