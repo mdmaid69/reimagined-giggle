004138496d4ab7dd9be24440395f92014f75d130
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:47 +0000

    formal limit spray complain run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
