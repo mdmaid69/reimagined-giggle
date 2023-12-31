f2c2906a557d6f0f3f0d8d12564a4d3ac8874868
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:49 +0000

    go not spite season cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
