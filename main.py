3f16eba6158443494aa6f7520108602039e40b6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:08 +0000

    only for down boat means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def square_number(x):
-        return x**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import platform
+def get_os_info():
+        return platform.uname()
