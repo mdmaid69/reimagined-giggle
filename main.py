65bd0d2f1abac67d663df21fd228f40c88da2254
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:01 +0000

    this goon present from rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
