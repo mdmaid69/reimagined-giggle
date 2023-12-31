3da3696c39c79774e4e5992f9990f75b7750616b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:34:48 +0000

    shop kick right visit interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
