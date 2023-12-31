e5e85412faaf4242ba8c6686deb6d98f23474c61
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:25 +0000

    punishment manage thorough tradition sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
