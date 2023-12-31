a9db5fa3733c76d2098fa0d054aceecb46a42208
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:44:14 +0000

    passenger abroad frequent recognize probably

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
