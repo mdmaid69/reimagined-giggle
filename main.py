1be6e18cf004e5e07654dbc3c27530b7ac850ded
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:58:38 +0000

    instead row deliberate eat position

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
