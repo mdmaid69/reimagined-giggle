353b71fb1bd9789bd4f280a56fa6fb6841658f53
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:49 +0000

    back talkoutof lost stranger wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
