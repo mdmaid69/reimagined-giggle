d008d67e60387d483500ac55a6f6599f4ed5cb62
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:57:20 +0000

    dry just chase snow deliver

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
