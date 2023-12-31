ede6509d5c41c62ead5d50c2b421313d996546b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:33:53 +0000

    account good run country typical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_area_circle(r):
+        return 3.14 * r**2
