ea7e5b182cdbcd22afcb275c34a0099a486cbfe4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:16:00 +0000

    wide wire book drawing suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_odd(n):
+        return n % 2 != 0
+import sys
+print(sys.version)
