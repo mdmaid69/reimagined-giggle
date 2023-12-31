cfe7bff0709d196c5242a220d2b2a2e2ed08b64b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:05 +0000

    less definite especially clothes fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
