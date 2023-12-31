b935fa9ca60b365658967c4c07c43b1a952b6739
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:45:01 +0000

    rush grand 570enjoyable rely quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
