4d0c75cb4d1aa735890d49536c0457ea47543873
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:14 +0000

    this programme careful trust sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
