7432e08e32344098e5aaaddb3efc504ad0a028aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:30:33 +0000

    store drop type thank Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
