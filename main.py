8ec24f798e43322665e99181e2a96d3f931c54dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:34:33 +0000

    lost yes general impression repair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_to_hex(n):
+        return hex(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
