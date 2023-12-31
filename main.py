d8a9e525113fbcd29336b00489f2be78a6a8e9fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:05:35 +0000

    practical excited as well cool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
