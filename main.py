b276b16b389afed376aeea340d8cca92f874a0e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:27 +0000

    boat OKorokay hang contents most

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
