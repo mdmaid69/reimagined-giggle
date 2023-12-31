07937d036f026a46b3f9221d56379acd32610e57
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:12 +0000

    shoot right sail connected cheque

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
