e388b06669954d2ce15dfb020ebd3f4df19f8eaa
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:20 +0000

    runout rubbish march size meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def exit_program():
+        sys.exit()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
