a254da20ae6cb15c5984fadf9d62149e8bb5de1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:38:05 +0000

    succeed godown kill lift care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_max(lst):
+        return max(lst)
