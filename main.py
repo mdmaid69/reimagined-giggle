b977e0fdde6dec820ef6ec07ed7c0527967f82be
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:05:47 +0000

    minimum company pretty choose voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
