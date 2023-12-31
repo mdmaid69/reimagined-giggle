ccf0b113596772805d5b89f85b9eaf32cbd036b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:10 +0000

    sometimes rain world calm discuss

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
