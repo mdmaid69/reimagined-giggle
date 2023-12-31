8a012ac453011ff1af4b342a7db46bd46d50c5e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:19 +0000

    official pocket anybody crazy job

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def cube_number(x):
-        return x**3
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
