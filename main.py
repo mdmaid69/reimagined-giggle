2c64faee2db32bec1fbfeeb06338b2a09d102ab0
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:38:25 +0000

    draw whistle cum spring neither

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
