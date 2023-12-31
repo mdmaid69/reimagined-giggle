4263e8935f7d4512aa6e08d892c5a6c86cb7437b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:41:29 +0000

    form preparation strike design either

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import tensorflow as tf
-print(tf.__version__)
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
