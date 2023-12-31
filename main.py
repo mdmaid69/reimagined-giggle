9cd127ee26b027992d77dd53930fabc655976912
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:54 +0000

    condition lucky about purpose accidentally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def square_number(x):
-        return x**2
+def convert_to_octal(n):
+        return oct(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
