0ceb7b17e715f2d9093abd034ff23cd1a80e4602
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:29:15 +0000

    laugh respect impressed reasonable phone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
