70c9309f17e1c351e45f6e377ba7c9118b1fc8f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:43 +0000

    evening rather equipment only when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def count_elements(lst):
-        return len(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
