4fd4c6dc4ad667383e359816a32b3809d3ab7c42
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:06:22 +0000

    store during concentrate store match

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
