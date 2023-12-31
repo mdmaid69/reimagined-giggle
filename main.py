4e1737fbe7aba986fd5f895f0de8ad9f01da3ff1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:00 +0000

    patience neck one table formal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
