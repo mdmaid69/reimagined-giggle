a58c1fa5b6d8b82282a59f1973a57b694e766fa6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:05 +0000

    expression consider foreign frequent convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
 import array
-def clear_array(array):
-        array *= 0
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def append_to_array(array, item):
+        array.append(item)
