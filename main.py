ba9955b61ef10618134cdf0696b85d680cd69e1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:55:54 +0000

    callout up sleep interfere cold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
