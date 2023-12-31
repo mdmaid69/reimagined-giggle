af8e69167d2bcb71a07ddf079c2547905a5a9a46
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:55:39 +0000

    disgusting contact benefit sentence uniform

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
