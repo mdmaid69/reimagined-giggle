2acd7a8351787b4c96cad82e586fca6ef20dc437
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:09 +0000

    minimum Jj air callat indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_string_from_array(array):
+        return array.tobytes()
