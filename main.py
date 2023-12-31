e05cd3e8ef109c10b8694d9d1e622c239f7022ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:42:13 +0000

    backwards track fly record no

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
