bc5d854a24b8022f79aa5a980db7c4f61cadace1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:32 +0000

    approval centimetre stranger split pretty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
