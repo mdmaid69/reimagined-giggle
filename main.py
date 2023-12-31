c74f779aa8babf7d41c9a5ef58a9708c631b6618
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:16:24 +0000

    bank cover face ago wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
