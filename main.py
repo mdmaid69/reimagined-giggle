eee497260088ac6b61924acafa9ceeaea6101240
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:03:19 +0000

    plate takeback keepoff old fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
