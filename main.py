fc095791cf87042d954c1feca06982d862121547
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:58:06 +0000

    rush claim sweep congratulations interesting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
