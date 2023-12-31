943a51bd5cfe7f8c8bac9854356300f035fd4d9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:45:59 +0000

    violent ago Ll address punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
