d5de1b6a6a30ccc1d51c436ad9dd38ebb21aa58e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:59:07 +0000

    sky score mixture leg brush

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
