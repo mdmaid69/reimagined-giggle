6ecb2e1ccbd19633a0c9bb59c2db1236b1b33d8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:45 +0000

    manner programme scrap appeal foreign

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+print("Hello, world!")
