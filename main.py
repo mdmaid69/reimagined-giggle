11a86e0f6d7786624167afe574f397aae13b86b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:22 +0000

    temper hurt system human negative

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def add_numbers(a, b):
-        return a + b
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
