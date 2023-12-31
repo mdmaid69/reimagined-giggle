69c4a0347e8b89d4e2d8de89853bf48f19c48cce
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:01 +0000

    proper building capital steam joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
