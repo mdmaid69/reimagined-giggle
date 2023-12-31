6a7e3f36f7b47c5d897366285a17be3ea899847f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:05:12 +0000

    thorough design swimming tap report

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_min(numbers):
+        return min(numbers)
+  def is_odd(n):
+        return n % 2 != 0
