589ab5a329037cbfc8f8147de2cf28779d29a801
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:45:45 +0000

    union suckme safe dead constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
