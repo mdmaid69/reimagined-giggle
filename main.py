40430bb9c272176e7d046d89035b8970d5f3a755
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:05:54 +0000

    the wander clothes disapprove Rr

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def square_number(x):
+        return x**2
+def add_numbers(x, y):
+        return x + y
