7bcd42d53a11d6f6c4bcd58899e65596b2f1db7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:37:02 +0000

    measure club choice view affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
