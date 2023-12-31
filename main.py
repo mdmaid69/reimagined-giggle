73c31748c3d9ed267da5c9a777cc4448d0455495
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:10 +0000

    accidental ilovedicks want hit neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
