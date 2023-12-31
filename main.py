15bdf03a1edc140cdc141cc403be6b27e59e6140
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:02 +0000

    on thinkover broken spread hot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def remove_duplicates(lst):
+        return list(set(lst))
