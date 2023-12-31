190dfd494476e388c01c02dd7ecd5a17a4b6f287
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:36 +0000

    powerful inform hear previous environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
