0c664aad27a4cb74c3d1ee37bf57db6ff5da1ae0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:47:56 +0000

    innocent join save clothes wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_item(array, i):
+        return array[i]
