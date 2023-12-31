bb67d7b2bcb9381c0883b04c2aab60f6a3e79ac6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:45 +0000

    relief unpleasant sink collect show

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
