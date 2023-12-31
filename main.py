b3a1d1a7aec46e6c314193bef4ddf7633ec2630b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:12:00 +0000

    definite risk phone something discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
