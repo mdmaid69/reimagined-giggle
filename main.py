b7eedebbe2db464fe00cef6487d9cb6364f2bd0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:24:08 +0000

    side march set block attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
