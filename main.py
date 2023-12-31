adbc3a6a2139850c3356527e4e7574906ddb134d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:42:06 +0000

    walk police dream minimum at

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
