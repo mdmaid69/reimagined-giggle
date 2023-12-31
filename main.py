5c0399a158a318e0bd330c3427c18360accc6fe9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:52 +0000

    light performance sense slip consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
