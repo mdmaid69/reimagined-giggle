fa8943619dc6ebc91d293f168e0d9bccb4b948b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:39:23 +0000

    along opportunity school company surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
