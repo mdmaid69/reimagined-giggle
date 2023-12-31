2cfd1e06c97d93aa9ef0098f57a7986dd27f3ce2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:30 +0000

    language building attract fill joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
