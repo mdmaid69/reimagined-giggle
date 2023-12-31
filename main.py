6ec7aace3a4d80ce7efc2de1cf27c62830113516
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:41:46 +0000

    confident want steam start ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
