c9ef9351acdcd11edcdfe3855034f23d8139afc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:42:05 +0000

    maximum sleep although unusual shallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_list_from_array(array):
+        return array.tolist()
