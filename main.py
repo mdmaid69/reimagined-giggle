55e7fce5106d2c3e83e51ef5f5f081be334ef78b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:13:35 +0000

    city cost far famous finally

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
