92794c24f40fe3d46929a9a8a60383904d5c7d66
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:33:21 +0000

    push impressed have relief holiday

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-name = "Python"
-print("Hello,", name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
