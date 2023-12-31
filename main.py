3f4fea18e50251a965a305eb43acd50b21b52d9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:13:38 +0000

    tap disturb rich dry sing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
