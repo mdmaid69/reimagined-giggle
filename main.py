0f3de9c9742d05ddb113f1cec88f4f2d2046b98b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:08:01 +0000

    pause constantly live display side

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import numpy as np
+print(np.array([1, 2, 3]))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
