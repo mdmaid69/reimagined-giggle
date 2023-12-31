7d67f81258c1e404503bb47fa96dff1c47b0e2c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:47:36 +0000

    talk child string flow female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def multiply_numbers(x, y):
+        return x * y
