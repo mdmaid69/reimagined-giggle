82fe6cf4221fdf52288d503cc4084549a98b2b4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:18 +0000

    error culture contact fond frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
