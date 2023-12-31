30ef790b3909ac44b0f30aa71f18586866a128a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:10:42 +0000

    on pour ask interruption yes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
