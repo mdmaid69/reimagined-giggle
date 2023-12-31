f0b2907a4c561f2583b8863342a185ae281f909f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:03:45 +0000

    design she play gas company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_min(numbers):
+        return min(numbers)
