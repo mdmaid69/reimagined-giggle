0ffb37941a865c563da9bda694ae9daf3367e0b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:33:57 +0000

    careless skill top breakinto equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def convert_array_to_list(array):
+        return array.tolist()
