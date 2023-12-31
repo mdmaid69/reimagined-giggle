32f7450625001deed91c5516d140e570e5a300c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:11:41 +0000

    judge neither down excuse high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_base_name(path):
+        return os.path.basename(path)
