3e0ce27ec1eb128200311fb158e6b47bbe4158ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:59:23 +0000

    information flavour comfortable pupil address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def reverse_list(lst):
-        return lst[::-1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
