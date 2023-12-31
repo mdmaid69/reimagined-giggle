964e6e34cac936d45a3427380190727924bf3af0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:16:15 +0000

    gather food support break try

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
