b158fdd5e69164f285d5875bd42f08960d39d85c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:20 +0000

    form pencil fact science drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
