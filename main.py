6c79c4d068ab23178d2f72000e8ff9c2014efd38
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:26:27 +0000

    combine mass please takeout life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
