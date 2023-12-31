fb4a1aca6e01db25b577e6d0e0973de321ff8d8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:49:39 +0000

    sorry tradition surface thought completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
