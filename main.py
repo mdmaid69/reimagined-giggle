18138934f34acac4fa42cd4d0dc0e3e169daa55c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:34:53 +0000

    cry seem please tomorrow instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import time
+print(time.time())
+import os
+def change_working_directory(path):
+        os.chdir(path)
