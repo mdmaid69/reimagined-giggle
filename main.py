f4f21b1717aa781df8ff62cccdaf6ecade0777e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:06:25 +0000

    measure win suit everyone programme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_pressure(force, area):
-        return force / area
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
