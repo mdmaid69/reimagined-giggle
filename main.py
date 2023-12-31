217c547605ee620f6bdfe64f3235fc3ff3e3055d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:33:05 +0000

    principle new away likely memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+for i in range(5):
+        print(i)
