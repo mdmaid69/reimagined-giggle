2c931b499d071b4a9fb53029c25ec8ea89795ca7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:00:16 +0000

    interview declaration storm ticket choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
