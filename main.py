a07ba5a1ea81b1b5ab551cb3005d4edb8dbb4105
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:31 +0000

    breakthrough distant money touch within

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
