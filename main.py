71fa16b120bcaca3f99f7ebad0a577bd7e3ab7d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:06 +0000

    sad needle over emphasis again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
