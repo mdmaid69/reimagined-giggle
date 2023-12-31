85e8948a9eead06fc907d4cfd052b54decfbd049
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:24:13 +0000

    mind tooth worry soak protection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
