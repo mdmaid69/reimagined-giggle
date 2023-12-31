64dff5f4176d3932508e7597a77ca40af329d9b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:52 +0000

    standard doubt payment enthusiastic in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
