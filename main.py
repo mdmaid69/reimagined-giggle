938b2a2c09ef38b6569abb634a4594978a031be9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:38 +0000

    description twist know least wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
