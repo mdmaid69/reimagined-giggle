3a8532da50cc873c1ef70d7edb089e5ea9b0b5fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:17:15 +0000

    straight turnon strict ability smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
