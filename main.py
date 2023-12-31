eebef38dec88c520cb4795d3b6b81e72cfeb3b4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:42:57 +0000

    earth workout wire belief interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
