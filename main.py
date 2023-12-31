014a7ed812df6e40492078e64883b1f42f591f0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:34 +0000

    sure once but punish nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
