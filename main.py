bf3e5a71d607e5a1bd989b97605f888a6a0734d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:29 +0000

    bridge power would rule mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def reverse_list(lst):
+        return lst[::-1]
