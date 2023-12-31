bbb91a58be48baa934a01a2e11fc85d3e502aea8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:47 +0000

    involve either table lookaround speak

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print("Hello, world!")
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
