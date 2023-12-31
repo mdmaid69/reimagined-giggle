2757aae7dfa987d35dfc74731e31cc3b5a53c0aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:59:33 +0000

    deal wet popular below characteristic

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_min(numbers):
+        return min(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
