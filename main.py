ce76654389dbb2362fb3bbdcd1a0b75cc34b3584
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:27:52 +0000

    statement that go pen tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
