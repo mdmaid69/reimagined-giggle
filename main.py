254eeb92530ee26aecc5e64e164ce46d5565634e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:51:49 +0000

    phone breakthrough mail low change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
