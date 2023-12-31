e262eac6846639869e581f1f1b208417b44fe2bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:58:05 +0000

    sight world threaten hit temper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
