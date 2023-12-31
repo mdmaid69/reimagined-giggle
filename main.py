618e9bfb6bdbe8f55ee23fec56b233782aae2963
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:08 +0000

    phone clearly whether poor left

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
