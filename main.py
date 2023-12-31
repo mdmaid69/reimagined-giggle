6a5a250be349f44a08caec0181c3751f435232b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:04 +0000

    last joke beyond under mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
