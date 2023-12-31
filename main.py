80e7f95f28734a18e2308c98ee077c5f775a5cda
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:59:54 +0000

    Vv plant within collection between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
