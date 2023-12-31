405b98a5c5bbd2b1658ab1fd995717c73b70f8b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:21 +0000

    pinch room glad murder swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
