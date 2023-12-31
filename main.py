0c77a78bc3008bbd61be0b0c7e96e0f77cc6de5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:33:27 +0000

    point minimum bottom butter gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
