b8758943bb31682304a2a68a812c088dbdd2d925
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:38:24 +0000

    difficult money jump shade turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
