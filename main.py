e234b5cd31238f228040935ab4678e6b2efcbc58
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:32 +0000

    thinkover guess threaten direct port

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
