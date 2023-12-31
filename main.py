66dfbeaab297699cc716c9251ac280059a565161
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:34 +0000

    crash say back manage prepare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
