cfcf2f8757cc8ffc6b126042a1f6eaf40a8aeec2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:04:05 +0000

    example although ask tread practise

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
