001acb9aff483c89aaaf320da3184acdaefdbeb3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:24:43 +0000

    finally image surface answer me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
