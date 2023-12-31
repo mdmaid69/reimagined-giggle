10a2a1d6201433019ca29abd0edafe37aab485b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:33:22 +0000

    alone forget achieve put fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
