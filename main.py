fcb9aa1dafe55f58b1acd1ec4206a502285f9ee3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:01 +0000

    whistle copy police dinner clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
