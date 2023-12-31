4a116f287acf6059bcf4b385ddc48c8346975f4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:51 +0000

    swim dish old off attach

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
