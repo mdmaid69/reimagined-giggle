79a8618d684e1d149f302c625efad1bed29fb788
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:42 +0000

    story history getoff shut open

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
