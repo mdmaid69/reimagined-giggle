9b6677172525e7d290a6c668f00b79210a31a690
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:41 +0000

    month embarrassed nothing pot be

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
