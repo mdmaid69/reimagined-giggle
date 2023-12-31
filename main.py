829bfb2dca1428d54b464294b56d8bb3595b856b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:35:35 +0000

    rent fill on check belong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
