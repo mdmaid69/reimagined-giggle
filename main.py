8a1e91338aa0b4306e595ac1faa8df70ee3e4c43
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:15 +0000

    bank always like she yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
