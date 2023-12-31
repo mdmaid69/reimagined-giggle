d4284c9fb6792029ab107b08d157ba25fe1833f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:33:13 +0000

    failure disgusting put drop some

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sys
+def print_python_version():
+        print(sys.version)
