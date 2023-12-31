6dbc4bd7c0772fb5ed9e4923dae9ef8c15e291fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:48 +0000

    study 572enter meanwhile cause television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
