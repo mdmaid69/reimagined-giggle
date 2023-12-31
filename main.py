c773aa439896cea46d1e539e62789393374e8f19
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:35 +0000

    occur detail distant before Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
