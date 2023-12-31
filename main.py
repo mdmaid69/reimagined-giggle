e5d4dbfa7ded109f93c823bad16c7e19a6942b8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:31 +0000

    surprising wall time mess interesting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
