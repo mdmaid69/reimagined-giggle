5d331c17d056d511d1f87ffebd5275e37f9b11bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:48 +0000

    injury insist understand take suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
