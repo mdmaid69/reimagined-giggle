583cd0ce9ef428e58ec115348dd9ef0c94eae852
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:47:38 +0000

    behaviour thumb favour goon wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(numbers):
-        return min(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
