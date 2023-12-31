8557b7c3f4107965b845653eef347186b081b18b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:39:26 +0000

    fail present scream either address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def square_number(x):
+        return x**2
