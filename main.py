3efc45d174843fe8feedd28777e16ff9c45aa55d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:38:04 +0000

    fly closely another advanced statement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
