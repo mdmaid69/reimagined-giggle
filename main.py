e3d7d211149e45e782c2f76e30e0a5f160a26f16
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:45 +0000

    behind false tie discovery strong

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def is_odd(n):
-        return n % 2 != 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
