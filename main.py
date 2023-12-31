4d66e65dd712ef514266a15c7ff2a9c3c1eaff24
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:43 +0000

    shrink empty disgusting bringup Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
