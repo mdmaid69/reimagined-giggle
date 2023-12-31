3c4143f5bcc81d8c2d19162d7c6da3639b03020b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:06 +0000

    weekend bang move workupto bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
