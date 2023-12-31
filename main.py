522e96fddfcb567687e249fb10b90acd0b5bcd14
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:44 +0000

    thinkof plan guide relation will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def cube_number(x):
+        return x**3
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
