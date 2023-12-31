68f5f222a9519c64146d754def501c55b270b21b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:51:28 +0000

    yet rest shallow roll platform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
