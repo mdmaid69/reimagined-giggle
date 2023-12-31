3fc438bf7fcd64af6e2a40b8ab13d17407d0de3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:31:06 +0000

    mainly average slightly sign day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
