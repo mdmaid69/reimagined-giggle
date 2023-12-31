ab66527c85d00b5cbc51cbd134ca3622ce42d289
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:36:17 +0000

    mysterious convenient decision strict deep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def cube_number(x):
+        return x**3
+def convert_to_octal(n):
+        return oct(n)
