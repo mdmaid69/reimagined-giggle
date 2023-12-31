b3e0a078e3d8a426826f6d201039879a800afdb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:27:35 +0000

    sweep calm rest operation Ee

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_min(numbers):
+        return min(numbers)
