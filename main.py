09507a8a1f39c57331f4dc903432e6391339e2c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:01 +0000

    off stir regret you pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
