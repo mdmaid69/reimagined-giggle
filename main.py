b83c9fa2793aa09045d08942a09aec005eed71e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:18 +0000

    yourself pen weather listenfor 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
