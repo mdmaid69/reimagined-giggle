e3124074776c28938e4ab2fcb2eedf3a0eb13485
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:44 +0000

    front stir geton forget black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
