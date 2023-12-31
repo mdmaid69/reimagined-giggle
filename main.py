0ce161acd50edb09b5778dde709503b6fc7e0ff2
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:13 +0000

    better lot brief guard majority

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
