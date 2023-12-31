078f73f78940ed1fb740b0daa60e6d56bc7c21f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:40 +0000

    sell national measure upstairs intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-print(os.getcwd())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
