933ee59172d8d70a04ae6eb9911989533f7b7d3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:57:57 +0000

    praise around terrible mystery computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
