ba82e149b691c460caa30a228463f36edaa30ee3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:40 +0000

    ready accept unfortunate sticky anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
