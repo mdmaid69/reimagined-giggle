d67662228e63f183ca5469406792c8ead96547f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:25 +0000

    platform especially stamp mine find

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def is_odd(n):
+        return n % 2 != 0
+def find_min(numbers):
+        return min(numbers)
