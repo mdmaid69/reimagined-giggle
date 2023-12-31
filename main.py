6a71feb00743e6cb5bf5800f09347fd11136da7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:55:11 +0000

    come shade confused now exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
