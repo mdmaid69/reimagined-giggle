3dd5e9529592a0a294c2519aa926cb4da304f52e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:02:53 +0000

    listenfor identify know telephone marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+def convert_to_octal(n):
+        return oct(n)
