75dfbcbc155d5711a872f3f7ff8f008efe188139
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:07:25 +0000

    rank another sky serious cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
