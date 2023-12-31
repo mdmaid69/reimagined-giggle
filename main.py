69702459f4939a54adb3d8090238058b30e4cf8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:58:17 +0000

    raw criticism second train skirt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
