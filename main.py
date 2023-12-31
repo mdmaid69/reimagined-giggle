0de5f2794cd2464135189e0670b388cd5942dcc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:52 +0000

    hit hurt form lift action

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cosine(x):
+        return math.cos(x)
