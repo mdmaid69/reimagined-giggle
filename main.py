b8e7e44b34c53e3e342c0a321f7b21281d050813
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:20 +0000

    neat who shock somewhere maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
