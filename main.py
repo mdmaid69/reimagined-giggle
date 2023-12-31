2bf1037c4e5e0ab755f38965d6456f0e52ee402a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:21 +0000

    include no question danger person

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
