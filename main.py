04e417fadd0b489a75e0ae5f01cd570f4a173c0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:39 +0000

    pause die handle dream region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
