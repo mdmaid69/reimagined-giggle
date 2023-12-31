ddebc951e6bdfbd406f390cd09825122b9ccb86b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:43:24 +0000

    near still wing definite deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
