c1f3ba887f8bec39268a2fb6572d6b55e79fe82b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:39 +0000

    shall family seem wave safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_set(array):
+        return set(array)
