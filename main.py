d296b67123c31c219e66f5081f927afd9e8f34e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:01 +0000

    late change laugh arrival myself

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
