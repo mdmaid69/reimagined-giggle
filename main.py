e71fc4ba8b9d2ba63f78300e78d52fb806cfc240
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:37 +0000

    theirs uponon concentrate flow class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-print(random.randint(0, 100))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
