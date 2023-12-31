52844861d9b71aad2584d9281618b9de8a82f18f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:21 +0000

    building sweep mean protest laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
