c4566e5457408123c57d88875e841f17e91b65f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:38:31 +0000

    advice climb worse trouble breakfast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_list(array):
+        return list(array)
