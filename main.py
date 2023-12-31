41eef0a31d0706f88839635e8a59ab661fa0f86f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:21 +0000

    arrangement equipment deal slide dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_base_name(path):
+        return os.path.basename(path)
