90b0beabcb32a63fbfbdb0a6adfe8412372f2854
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:25 +0000

    switch draw few would gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
