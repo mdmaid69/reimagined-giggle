b2dc3ed2f1e046cba24d1719ba3766900e9bd828
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:32 +0000

    confused charge much offer fall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
