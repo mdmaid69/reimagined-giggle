9d8accbd6be5a09796fa6c016a339eb94feeb516
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:24:57 +0000

    so moment exciting name successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
