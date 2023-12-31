d0dac0a8163a6ce2419d8e666d47532484b9a9dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:51 +0000

    bet production meeting relief lock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_current_directory():
+        return os.getcwd()
