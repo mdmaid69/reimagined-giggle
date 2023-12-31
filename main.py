c57fa80accf2f9ceb7f6964e20bee3db2b668bf2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:22:11 +0000

    branch concern teach understand do

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
