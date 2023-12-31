e839dca2490f8ed44fc2a2e517d49b829947f218
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:45:15 +0000

    move if immediately recover beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
