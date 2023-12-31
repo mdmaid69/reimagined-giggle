26e80108dea13d3414f5736b05eb496b6943abf8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:46 +0000

    original between promise need period

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
