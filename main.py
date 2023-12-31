ee248078157866b1b1751ef3f2eece7d1e205be6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:39 +0000

    advice close defend fact deliberate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def get_array_typecode(array):
+        return array.typecode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
