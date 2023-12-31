34a816db1fd8b90bf36d265fe26251571b35aa6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:25 +0000

    scare rough speed entry than

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
