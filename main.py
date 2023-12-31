eb84e6146cf4961d270aea615f4acac0e8dfb2d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:04 +0000

    insist bringforward disturbing feature tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
