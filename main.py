6f3a23c914b6edade310e04f18222b6e7432a688
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:47:22 +0000

    practice charge violent patience minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
