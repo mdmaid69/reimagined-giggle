91d88218c0f7c5f111c3b5ea9d7caf9f3f657d8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:28:07 +0000

    dangerous succeed switch original swimming

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+for i in range(5):
+        print(i)
