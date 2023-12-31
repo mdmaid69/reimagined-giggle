662c320e4bbabf98a4d798d7225679be9baeb294
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:11 +0000

    attempt term paper exercise fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
