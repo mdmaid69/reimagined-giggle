e05ac4770a28ce5404e4672d74671d6985a0e068
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:10 +0000

    buy rely sad line comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
