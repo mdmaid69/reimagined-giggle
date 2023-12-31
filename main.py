4a98db79d82c3cd7a0db57e74b8768866962690b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:43:05 +0000

    wear right damp rich thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
