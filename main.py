d1ff1df74d25fe562022c5d853d2b60e20894f5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:47:19 +0000

    sense fair nonsense regret original

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
