bf8faeadd56e27fba4fe31a2664b01a23999b92c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:11:52 +0000

    sticky condition to through guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
