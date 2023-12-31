81657c93e6a4345efe5dafa0f7e63857156e7fdb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:38 +0000

    computer from turnaway tear block

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
