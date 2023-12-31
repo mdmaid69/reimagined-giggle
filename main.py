4df44e5c09a0360a15663ff78721a106a6558630
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:27 +0000

    description drawing determined police waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
