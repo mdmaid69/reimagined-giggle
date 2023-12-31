49fe7de6d788d8a7a3fe5eaacab8dcd54f5b3ee2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:33 +0000

    deaf on chin petrol operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
   def create_directory(dir_name):
         os.makedirs(dir_name, exist_ok=True)
