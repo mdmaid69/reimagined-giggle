fc5859a205837562edbb701983cba4567353a158
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:54 +0000

    many takeout thorough scared pride

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+for i in range(10): print(i)
