d30b983b805c3acb42ce721e9b7bc4305770b9a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:21 +0000

    suckme trust service wet responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
