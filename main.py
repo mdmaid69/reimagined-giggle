00c7764e863d6c15aa256d2145fa7f77e509d173
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:31:43 +0000

    like against convince degree still

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
