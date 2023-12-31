f758b4efe53e8788b3822741c2d27da3a24d4fa4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:53 +0000

    characteristic Bb guess print strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
