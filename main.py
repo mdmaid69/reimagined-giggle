6bd823d5d4e166481937292903343876ce7c7814
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:59 +0000

    there concentrate generally convinced grateful

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
