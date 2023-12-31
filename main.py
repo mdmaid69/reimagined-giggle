c16198740156918bf35eb5567770f8b1801b6620
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:10 +0000

    say dig left damage pile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import getpass
+def get_username():
+        return getpass.getuser()
