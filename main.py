43d2bd15dc75bd359d630989519d741bbc30d4fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:46 +0000

    aim various attitude Nn safety

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-print(datetime.datetime.now())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
