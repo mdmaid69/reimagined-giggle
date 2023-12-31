1223b3749d1d094d4fe194004fceb792348c35f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:15:43 +0000

    shout recently consider enthusiastic principle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
