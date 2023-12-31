f78a033c226dfb1d4a73714d05d956735a03b072
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:37 +0000

    ever arrangement hope juice friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
