8a97a54c37161521f1c565c05edd71a1aa66288c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:33 +0000

    polite 569enjoy anywayanyhow foot search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_density(mass, volume):
+        return mass / volume
