f445841bec02678f832bb5d0ce14402fb40bab67
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:51 +0000

    related name convinced tall common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
