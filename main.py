71ef47486cb35a5a969947364a27571277d0e217
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:35:31 +0000

    present mess fortunate mess decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
