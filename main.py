2b5f6adca7bc164642d188ac6071f5a21236e3d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:05:08 +0000

    into create getout record pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
