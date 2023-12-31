9bd565ec4dbf4982e46f45aed1d58703812125b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:46 +0000

    stir direction whatever unable doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
