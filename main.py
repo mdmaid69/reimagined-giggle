a164bb316e2f3433088f5e182853fb1d371cd97f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:16 +0000

    majority hard splash break distant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
