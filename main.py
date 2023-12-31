9e7aeea1616bf9d24efb731a907c94fa990863bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:26 +0000

    freedom contrast capital themselves he

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def get_array_as_str(array):
-        return str(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
