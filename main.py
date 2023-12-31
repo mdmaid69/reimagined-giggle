503894be6648ed3893ccb506c50be22be76af274
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:17:32 +0000

    learn disappointed why prevent history

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def square_number(x):
+        return x**2
