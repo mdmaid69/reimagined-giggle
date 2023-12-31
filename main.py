665869b83a547e69d0170be73fd720d3b26567ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:54:38 +0000

    sentence vanish attitude tongue naturally

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
