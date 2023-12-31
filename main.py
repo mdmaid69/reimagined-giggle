040243020568bf85d64a9c4b8e0106f13dc441a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:39 +0000

    use book promise argue like

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
