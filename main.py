addf059b37819fe95c7e05570028cd4bb364e54f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:34 +0000

    union break mixture steal it

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
