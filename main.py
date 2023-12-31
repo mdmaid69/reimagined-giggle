8e61ad0afb2a9c111d4f9f1af3a5166a581f30d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:17 +0000

    rely still spare pass lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
