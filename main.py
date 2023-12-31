011576d896d36385c5bd4aa00028a0233ed292d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:17 +0000

    clean sound weather meanwhile down

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
