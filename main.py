fcb5d7acf3c53273b3d9e149153fcfd9faafd3be
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:18:28 +0000

    yet schedule bend city reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
