b8a0e1f82900d6ed44d8bbefdf7dffaec1d696d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:44:50 +0000

    responsible breakout colour centre claim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def sort_list(lst):
-        return sorted(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
