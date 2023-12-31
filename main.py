178bc6998c6a3732d3dda7259e62f40de7ebd1c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:17 +0000

    already familiar class exaggerate worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Words:", len(text.split()))
