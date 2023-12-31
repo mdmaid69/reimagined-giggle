443500db789c17d9dae1c4f4483a7debbfbcd840
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:54 +0000

    wear 572enter guard luck finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
