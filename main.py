9d1aacc5e11b6835ccf1e0a46188993b21a65819
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:41 +0000

    onto comment downstairs round stop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def count_elements(lst):
+        return len(lst)
