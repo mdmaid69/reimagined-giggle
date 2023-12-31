466a8c8960ba6b7080890b08b8b44e81e5e10cbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:08 +0000

    track post fire worth boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
