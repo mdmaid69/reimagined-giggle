46717e13d0342cf808962418fa32bbac5a11b56b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:59:02 +0000

    understand direct 571enough neither measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
