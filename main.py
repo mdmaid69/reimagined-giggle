092b5c44b3a53421e891b518ba2943990cde8d7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:08:56 +0000

    spin turn pride abroad offer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
