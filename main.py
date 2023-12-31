2110d9d59ed98f61ef4ecb3ae1ae087cd15fe64b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:31 +0000

    relative scheme concerned correct responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_speed(distance, time):
+        return distance / time
