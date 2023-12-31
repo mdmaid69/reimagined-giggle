e186840ed489e3f9c33362cfe4710345a0ceed0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:33 +0000

    decision command thirsty protest last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
