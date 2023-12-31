917d6481c05aa1f2ae147d2286c96b9b6d97217f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:51 +0000

    slice concerned pure away evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
