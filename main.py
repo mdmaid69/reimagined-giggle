62894a40ccde0b95b50c983c04b2060292fe6414
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:26 +0000

    though book cross keepup now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
