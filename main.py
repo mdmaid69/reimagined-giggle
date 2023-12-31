829123f91d79c419e6aa1c39c2988c9e12fec5cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:52:13 +0000

    flat generous transport fly sport

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
