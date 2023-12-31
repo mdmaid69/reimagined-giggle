0ebc44a7aed71bc896a9d4ca080397db22496096
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:16:58 +0000

    emergency fill strike frighten furniture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
