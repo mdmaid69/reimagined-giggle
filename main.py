80247bd5f19d80acea6699695bf85e01e64ff499
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:53 +0000

    good arrival future week underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
