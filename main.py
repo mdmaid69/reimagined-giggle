beaea422721c88e4b1de764261d810e1eed7919a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:08 +0000

    couple round luck hard move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_speed(distance, time):
+        return distance / time
