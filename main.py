9b21981de72743202301badf79009a41a087e2e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:02:51 +0000

    reply window sort trust correct

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+print(sum(range(10)))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
