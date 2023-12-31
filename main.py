826545fb6e5db1975872664405f17c6a22aa9a9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:10 +0000

    quantity owe roof each state

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
