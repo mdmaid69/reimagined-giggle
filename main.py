b868596d7adbe9610bdd9dda9ae32ddc39ffa3e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:55 +0000

    test exam goout top want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
