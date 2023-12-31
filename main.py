8237ff710c441164902b982e6fd381e51c7361a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:18:12 +0000

    acceptable free always quantity consider

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def is_odd(n):
+        return n % 2 != 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
