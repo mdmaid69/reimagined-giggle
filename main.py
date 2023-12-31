1f1d4a08896a8389458092990881108ec339fc72
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:09 +0000

    mind card protest plain lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
