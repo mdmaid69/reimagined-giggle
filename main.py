8f7d916cb3db2133decd6cd062b6ac80c63a0757
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:18:24 +0000

    squash separate open suggestion crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
