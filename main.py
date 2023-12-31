57a1e7e973b246c2e7089c8b571f5b90240cdce9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:32 +0000

    programme might bunch summer simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
