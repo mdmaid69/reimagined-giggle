bafd0a061bffae2819c2c75b763e04508ca8f307
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:10 +0000

    expression before getoff original fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
