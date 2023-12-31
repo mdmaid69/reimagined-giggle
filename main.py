499ac295a763482e38a741b132c93c3ab26df822
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:23 +0000

    round prefer why live design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
