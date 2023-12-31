34b8dbc06be95d4e60e8b19cb5350810e17cd6a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:29:24 +0000

    invitation thorough heart go back

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
