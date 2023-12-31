80a7eb9520771a8c668a0db34b1b6178f1cd530a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:01:05 +0000

    deliberately fear 570enjoyable bend equal

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
