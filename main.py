4c4c692af481210340fc3afafe879aa3ea32bdbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:43:42 +0000

    thinkof attitude criticism far boil

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_work(force, distance):
+        return force * distance
