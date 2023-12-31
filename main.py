c911ea14b6ded6320d66518aceb04748fe693993
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:53 +0000

    call hope brave Yy summer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
