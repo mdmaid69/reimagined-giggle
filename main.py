40e313e241e200da88064fab7cc91034b7577566
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:51 +0000

    turn fashion believe language sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_min(lst):
+        return min(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
