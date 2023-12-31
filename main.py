2f698b32042b122e0998f9f84040598b69b817a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:38 +0000

    giveout willing couple ask hear

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
