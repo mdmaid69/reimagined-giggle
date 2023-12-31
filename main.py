5478ade897bf2d7513a480a57e631fcaf98dd45c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:53:08 +0000

    violent this until later turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_list_from_array(array):
+        return array.tolist()
