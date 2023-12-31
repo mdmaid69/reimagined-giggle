37c8256fc3cef546765681d3ebdfe3a958a23334
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:24 +0000

    thinkthrough march accidental uncomfortable bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
