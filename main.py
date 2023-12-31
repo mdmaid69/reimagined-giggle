39b109b3c695a459e98f023ab2fc4542a0068d1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:52:32 +0000

    patient deliberate cum hole slide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
