983ee2bf37695b976780b68aace6ad3271f9a575
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:43 +0000

    relationship please level occasion keepout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
