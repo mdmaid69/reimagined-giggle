6250fb9b97fe6890064d3bec727a40facb9bb48f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:37:52 +0000

    convincing process stand at row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def convert_to_octal(n):
+        return oct(n)
