2915106f6e20c753b4bed924ea497645128b31d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:34:13 +0000

    comeup pencil damp error yet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
