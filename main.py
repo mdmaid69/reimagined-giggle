283ee878e8fe40d2c9f2cba1bae679b54bedc70e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:58:47 +0000

    from creature pump grow extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_floor(x):
+        return math.floor(x)
+import platform
+def get_python_version():
+        return platform.python_version()
