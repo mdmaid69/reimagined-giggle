0f51e74036ea7a37f9b3a1a129f69d626ab68a38
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:55 +0000

    wild slightly godown shop imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_string(s):
-        return s[::-1]
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
