ee403994d4c0795ae18b3304bf58a8d5e4fac318
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:13 +0000

    contribute aware duty sore why

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
