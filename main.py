675ea0e2e3d5ae2da25f8127ded5d52a4fd6602c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:59:48 +0000

    modern since just your hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(lst):
-        return min(lst)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
