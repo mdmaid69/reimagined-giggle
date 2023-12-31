6b4b0e99d8523e46bf177b1b1006a48025cfb079
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:24:53 +0000

    walk goup principle limit reject

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
