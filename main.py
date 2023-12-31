8f7ff5552d61095351fb59254f7cab5666b26301
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:09 +0000

    flow summer shoulder arrangement solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
