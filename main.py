174911c541fa2bbcf86e340f68ff41a9d954f6b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:12:20 +0000

    comment question supply on display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
