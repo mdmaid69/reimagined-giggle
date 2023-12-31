4742d64f6746d4c8eb1a7daae792c6f4a9b28d3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:42 +0000

    wrap over up year except

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_itemsize(array):
+        return array.itemsize
