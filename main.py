e24206fa37d66cd814f870d83ed7747935a951b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:42 +0000

    murder pool neither maintain angry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
