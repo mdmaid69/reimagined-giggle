2fcc713d28579ac7d4fa8999626e2f6397f82d8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:43:53 +0000

    rarely replace traffic through much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
