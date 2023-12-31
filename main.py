3aae6a8af090dac270008732e7b660e18dcd8a4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:00 +0000

    spend challenge address sore little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
