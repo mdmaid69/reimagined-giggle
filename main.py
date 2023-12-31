3d29cb8e8aa8b818020ea1d44adfb7b75fe83035
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:12:29 +0000

    weigh simple substance debt upwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
