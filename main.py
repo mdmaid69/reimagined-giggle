c77bb397b8d2a9955201092a2f39e5ca73bdc55f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:53 +0000

    pour pour would badly door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
