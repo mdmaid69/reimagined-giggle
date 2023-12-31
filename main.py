96a307e364fb824b87d52d9ca950ecc0a0c3091a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:45:36 +0000

    interview protection fairly care gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def count_elements(lst):
+        return len(lst)
