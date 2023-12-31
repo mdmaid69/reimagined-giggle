0c579b329996c9244a50ca29227d6b7b7b118cfd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:15 +0000

    season ashamed where violence swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
