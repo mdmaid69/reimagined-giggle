5c0d5324d28b37445e2a99ef1af64dd6e64784ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:48:11 +0000

    572enter game invite wait promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
