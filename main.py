21a9ea316668fad07d6d8f4b2bb9ae50ad11cc60
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:42:29 +0000

    mad tune organization smile wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
