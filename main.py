d40d026c6876e847e06d1237cb9bb2210c28014c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:16:56 +0000

    black arm judge limit rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
