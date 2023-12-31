4f79f621fdf8306b893d6a0aa572d75e46d061cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:27:27 +0000

    answer charge broad situation meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
