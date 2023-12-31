6b727f3acbfc1cbde3d791403f356d9257d0c343
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:12:36 +0000

    score deaf conduct pretty over

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
