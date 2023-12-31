5aec4f2846a296f2e20a9528ec33837948a3b6dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:58:02 +0000

    post pain this trust ought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
