e9e48904fafa71256a277c190eb3f8d6e733cf03
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:08 +0000

    cover plate sign unless only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
