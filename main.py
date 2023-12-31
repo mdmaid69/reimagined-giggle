e4e2faa7135707835046fe662b12e4e285380e9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:07:46 +0000

    ring goodbye evidence different share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
