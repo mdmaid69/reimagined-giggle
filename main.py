da420e696ed51727045fdf889fe80642a1b413ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:44 +0000

    equal more marry cost broken

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_max(numbers):
+        return max(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
