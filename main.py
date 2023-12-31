f857d22447acdef2f1f6fa24127e7769280f64ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:38 +0000

    hard container goup deaf pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
