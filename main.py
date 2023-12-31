3a3504815725641e98567bdb4b86b056aa14582a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:48 +0000

    characteristic very drive meeting married

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
