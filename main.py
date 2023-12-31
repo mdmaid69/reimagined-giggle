eaf1370f6306b00fc40ee44aa6647edd67149380
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:07:40 +0000

    test wild birth invitation emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def reverse_list(lst):
+        return lst[::-1]
