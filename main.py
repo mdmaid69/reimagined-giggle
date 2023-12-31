10021922e6047652f0404772d9322cdf90eec5e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:36 +0000

    smart reply tone side suddenly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
