ee71e6edbc09d93469078e3c4eb398813adc0416
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:51 +0000

    half relationship hope nigga term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
