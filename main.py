ba129beff62dd62ab6e3526b929e13a0f41ef330
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:03 +0000

    finger frequent score paper reserve

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
