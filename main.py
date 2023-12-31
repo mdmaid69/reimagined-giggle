5bd1ab0327ddc52e823242c21da959f67d58466f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:00 +0000

    matter protest you underneath pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
