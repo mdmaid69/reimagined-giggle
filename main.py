3cabd2c9a1601945800c86a2c681d9734603f98b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:23 +0000

    minute trust name separate bang

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+from collections import Counter
+print(Counter("hello world"))
