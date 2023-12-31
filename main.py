a72e0492b4a2b389728e5a597ea01dc19a1d2b81
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:43:32 +0000

    slow nerve everybody increase freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
