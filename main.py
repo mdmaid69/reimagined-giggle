09e949bbf4a1d12ada1333ac3ad41184f1f2fb23
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:19:10 +0000

    law wall active turnoff enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
