68eb94ab9eba142ef4b34061d70c05c3c8bd55d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:48:24 +0000

    operation unpleasant crowd passage exactly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
