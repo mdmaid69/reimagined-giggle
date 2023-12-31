8d669d3ea64c4b89f3665e30619619770889d083
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:57 +0000

    wrong couple definite but exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
