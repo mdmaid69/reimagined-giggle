77de3eea98c4a9428a061115872f38e839d32f28
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:09:17 +0000

    figure strength shall file purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
