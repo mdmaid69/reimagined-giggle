6961702d9f127997abcf8652cb106db220373e4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:03:59 +0000

    contrast high wash value kind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
