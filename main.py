6bf6bf85a08f976b7cc20853857bd523638b8524
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:38 +0000

    offer think change rely throughout

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print("Hello, world!")
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
