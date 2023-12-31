a77086dd96bc181a9f8d8c21e38fa8353a12b2f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:32 +0000

    laugh everywhere several while estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
