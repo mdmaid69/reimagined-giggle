d564792d9277ed99c2b7380130afe3d3bca292e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:46:40 +0000

    such possibly fashion breakin there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
