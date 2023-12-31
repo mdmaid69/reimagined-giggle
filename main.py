85e9067dda151dacd444d2ceb3679af269f37913
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:09:39 +0000

    clear weather call report fact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
