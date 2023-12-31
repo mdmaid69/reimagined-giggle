be593467bc3f4008475ed00677b44c969a515272
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:57 +0000

    any usual takeback explain production

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
