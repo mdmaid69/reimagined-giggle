6940caf08a12f1e5406ea8a5c47e3e702c3af34e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:41:12 +0000

    smell repeat suckme or blame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
