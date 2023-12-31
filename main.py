ffff8981ada37172649502ac8d4c864a67473f03
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:42:29 +0000

    light slice congratulate dead fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
