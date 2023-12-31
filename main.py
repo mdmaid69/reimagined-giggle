e700e7cd811b367126b3d88eeb7a6c2884431e41
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:41:38 +0000

    broken count basically them comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
