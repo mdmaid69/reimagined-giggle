b27d14f158a863f9a8f2c30e7170cce0de586b50
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:54:12 +0000

    shallow question Oo several yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+from collections import Counter
+print(Counter("hello world"))
