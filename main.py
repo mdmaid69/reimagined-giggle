b282d52d6627cdffb1a84bb627f2c73442dc2689
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:13 +0000

    mood country meal commit interested

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
