0798e212954896d09f523c9c9075b44eb873a617
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:19 +0000

    cum cost post partner view

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
