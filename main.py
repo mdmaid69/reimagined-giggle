fa5c1e587554b927fcb4edb46f2d4f632ae400f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:48:33 +0000

    factory office difficulty already shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def reverse_list(lst):
+        return lst[::-1]
