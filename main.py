a03126be063693d5ed8820b8d5148b6d81351f88
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:55 +0000

    conversation shut stay behind dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
