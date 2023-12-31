98fd1a277ce1fe68251c3693eaf07c77a33dab19
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:49 +0000

    broken keepup start fasten form

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
