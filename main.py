800ae0dad6fcf15697d7dee105a1c2409d2e48c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:21 +0000

    report system obtain word town

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
