3ffffc5a5e13b0e94017dfd55debebc3b9a5df5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:55:35 +0000

    still measure give offence blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
