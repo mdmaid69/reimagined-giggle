cbf55fe5509200cc6593db655538c87424843839
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:24:26 +0000

    degree fond say contents agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
