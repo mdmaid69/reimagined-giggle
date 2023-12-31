dc22ede7c200c777a4d1e7af66a234275ec6b127
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:55 +0000

    brief affect available whoever OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
