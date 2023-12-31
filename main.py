e77e8197210a5abaddb58f9ab8a91536383820d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:36 +0000

    accidentally couple Ff case pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
