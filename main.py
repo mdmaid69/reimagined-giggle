63a4b6226c25058f0a83726d8f1333f5ed2eb100
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:44 +0000

    damp chase set wall walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
