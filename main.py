6d6ec68a9024e97c56d107a4e21391c0977b7ccd
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:45:09 +0000

    heavy unlikely promise below aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
