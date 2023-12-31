cf0ce6f1a47ea4bb34c7d49528ed79a51eaf3ab7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:37:41 +0000

    interesting young favourite spin worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
