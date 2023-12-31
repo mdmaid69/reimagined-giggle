b6849b9d2f726cd2f48754db9c04bd9765e4f443
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:26:48 +0000

    miss complete travel nerve against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-print(sys.version)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
