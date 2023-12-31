aa191e0ea0ef11b1d6104e64a1c0b37c783a23e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:29:59 +0000

    introduce emphasis extremely Kk deliberate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
