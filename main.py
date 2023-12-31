5820ef9bf42a92b8eadb9558d92f95e32740cd09
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:14:34 +0000

    hold press air perfect search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def multiply_numbers(x, y):
-        return x * y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
