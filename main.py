f0e7a3cbd7071227a10d066c0d5f0a4ea4397096
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:00:36 +0000

    have disturb ought breakthrough father

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
