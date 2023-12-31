b44880fec63813696033b90002b07e8284d86c24
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:35 +0000

    left section polite importance reduction

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
