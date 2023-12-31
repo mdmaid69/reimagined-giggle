ab0c82e6f8351723202de984ac7de1631817baa3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:41 +0000

    with learn meeting own certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
