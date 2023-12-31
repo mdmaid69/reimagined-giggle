6656dc69da5e501a8808cac7b609e437d6872ade
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:40 +0000

    hurry cover danger bit collection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
