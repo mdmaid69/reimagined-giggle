1a38bb4ebc9423d0c85ddde96b6fa1dac954d33b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:46:46 +0000

    be tonight worst book sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
