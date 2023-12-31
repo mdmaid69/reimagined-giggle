7ae58e31b94685017a5e924f7ee624eb1cf30abe
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:38:40 +0000

    turnon outside clever immediate example

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def multiply_numbers(x, y):
+        return x * y
