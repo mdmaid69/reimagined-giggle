7e57413be275d870f788090f6aae6498b3efef42
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:39 +0000

    minimum collect side Ii accidentally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print("Hello, world!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
