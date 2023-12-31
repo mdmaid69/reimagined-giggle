28941f1d9e0f144463b432e99ca47a952999a509
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:37:36 +0000

    test purpose refer neither study

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
