bd710d2738240fd63a75bff10a2001f3a19d8d54
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:02:06 +0000

    wash thinkover stop already ought

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
