111ab594b916087bbda3f9e76b984ccd611d81ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:03 +0000

    hardly sister scrape odd port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
