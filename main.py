185142a5211f464c138732f743ca350749d90fb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:38:42 +0000

    breath turnover post prove recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
