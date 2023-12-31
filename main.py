8c5bc5b238029c15482a9a0080eb64766d2de76d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:15:58 +0000

    confused future obtain idea shelter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_even(n):
+        return n % 2 == 0
+def sort_list(lst):
+        return sorted(lst)
