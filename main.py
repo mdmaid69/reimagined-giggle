d67a1075ae86ca36a202199a79ec41725ebafb32
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:51:47 +0000

    become tongue think once fork

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
