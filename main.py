0f4bbdce42a21635235da69c03de4a567d2e07a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:40 +0000

    beyond pleasant family medium characteristic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
