979d4d201979347ceb1dc150656a9aeb7a2cc92e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:48:03 +0000

    blame out operation approval aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
