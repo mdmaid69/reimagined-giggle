f15a48ddc2d1b0b8a5975182ca892a35034aea37
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:36:54 +0000

    realize probably new tour upset

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
