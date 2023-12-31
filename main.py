efe7b758e82b3e1bf87ac0f71b950d6b64b5dca3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:03 +0000

    duty lose adjective cheek hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
