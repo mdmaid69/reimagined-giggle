0869461112b505095ab20e7f058806b171b9aa20
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:04 +0000

    sentence lean mystery present study

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
