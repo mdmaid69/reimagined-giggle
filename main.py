be9079005a1909b24a1b686e2b76eba3940c0242
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:48 +0000

    rubbish your turnup pupil comein

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
