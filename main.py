7c4065e17a2eb80da1b5a20fcd8b46282560a088
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:03:03 +0000

    Ff purpose whom object guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
