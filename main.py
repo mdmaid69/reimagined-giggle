ce142c82ea83ca1e05d9c86a4540331d9499f0fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:22 +0000

    problem relation grand ignore sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
