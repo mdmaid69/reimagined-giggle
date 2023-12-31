0f61dca9690da0327ab4723abb33714afecd5af8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:11:52 +0000

    raise doubt only strength exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
