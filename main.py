8ad07281392ca1263cad2dd759229fb70b27b8d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:43 +0000

    odd introduce neither square pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
