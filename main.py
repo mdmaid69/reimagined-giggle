6289b883ecf13bf4c83ea9a2650e56915c3174fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:42 +0000

    regional regard authority suddenly begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
