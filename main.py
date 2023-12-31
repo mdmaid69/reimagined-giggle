ba0d4a5709c5ed319811151c3d798ce070ea7df6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:08 +0000

    touch side state burn trial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
