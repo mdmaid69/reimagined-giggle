00478ce7e65d5edabd235b119b47d7d599ff342d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:06 +0000

    roof spite safe cheek easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
