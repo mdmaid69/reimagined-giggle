0b9512837f8b1eb8478b31a6e2778b6350e6774c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:37 +0000

    nowhere press cause trial know

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+print(os.getcwd())
