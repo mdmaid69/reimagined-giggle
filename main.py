dfa1cb858510bcfc965591d01d7f6f4f237c48ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:29:41 +0000

    fierce front turnup until treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
