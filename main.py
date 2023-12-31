df9e19e011d4c92620bcd582cda5566abb3e778c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:15:06 +0000

    pause preparation definite piece sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
