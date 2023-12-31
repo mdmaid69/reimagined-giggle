847295124180a5838efcdd8bda273abed1c9e49b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:51:35 +0000

    blue keepoff family all answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
