d070dacb30c6bf520f9db70ba4af5c9872d39ea3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:50:48 +0000

    frame figure introduction miss accept

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
