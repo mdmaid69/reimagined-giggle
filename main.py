3ab8488aaf44852126d79b1c9917ae8e631bf21e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:47:55 +0000

    sort match emphasize just refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
