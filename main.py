d2f90bf534e8d37fe27f1712d47dcd82a884321d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:56 +0000

    among left country criticize pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
