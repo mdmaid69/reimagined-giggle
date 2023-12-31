dc4c1fd7fb2235e1a36c2a2fdc42f8b36fda63b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:04 +0000

    listen various deliberately means themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_as_set(array):
+        return set(array)
