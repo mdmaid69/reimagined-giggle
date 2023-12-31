de84feda863a2b5760b339d82cab15253a1a93bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:41:08 +0000

    floor genuine tell them oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
