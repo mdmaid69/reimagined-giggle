c2446874749be4d8b8efbded3028b80cd89f4aa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:26:58 +0000

    sea Pp pronunciation condition formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
