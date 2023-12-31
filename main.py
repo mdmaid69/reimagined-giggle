00d5a4fa9642e9d0878344ea7a74ddca6c6b68bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:07 +0000

    dream turnoff table especially square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
