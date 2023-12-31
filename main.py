2b7ef32f433a75c58c5856bd393c33a290063921
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:47:53 +0000

    statement imagine allow talkoutof organize

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+print("Hello, world!")
