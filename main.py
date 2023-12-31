8d3a0975f8e0b51b617689115e028a028d02a28e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:45 +0000

    tap I dick combine branch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
