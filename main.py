1e7f9cbe1ac380ea4682620f4ab5f913faaeedd4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:23:46 +0000

    machine rent knowledge that dry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
