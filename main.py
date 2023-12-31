31b34adb939ecc0c3399d037f07bf5e703c24492
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:27 +0000

    sort common brain scream welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
