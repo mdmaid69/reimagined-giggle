5d8395c188f02f32daecb3adf90f051969093543
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:03:59 +0000

    someone firm sound teach children

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
