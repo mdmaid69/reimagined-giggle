fcb8d1dbc439a8888ad24dd7c0a1c9d4d6f438a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:28 +0000

    split load pity indeed 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
