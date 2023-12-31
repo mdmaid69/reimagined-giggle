8e554e9ad2e9fe903555fab0fa00c337fd73bb1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:07:31 +0000

    stranger legal uncomfortable over argue

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
