979c3466376ddb4fd23b4d4b4e78f7f86f83dc0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:00:26 +0000

    face obtain accept even keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
