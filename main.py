1fdcdd2248ffe6f87dbaf6235a5aa988d2364827
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:56 +0000

    set pause Kk private manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
