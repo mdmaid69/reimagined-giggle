9cb36ba7d19dbee99be9cc2e13ee1826bad1b8e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:38:29 +0000

    think deal getback unusual country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
