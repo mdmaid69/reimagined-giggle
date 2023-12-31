f2c4c1f9d68dc597b5eb1a718abb7ef4ee8fa241
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:01:36 +0000

    risk mind spare beside near

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_typecode(array):
+        return array.typecode
