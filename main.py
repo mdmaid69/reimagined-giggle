31f46233e86e8de4d78522900ca5394871a55c9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:22 +0000

    lonely suit them warning press

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
