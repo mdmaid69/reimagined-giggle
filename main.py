e3f8af76a004a34e41a4a234df2bb7c7fc74955c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:45 +0000

    land their wrap thick direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import time
+print(time.time())
