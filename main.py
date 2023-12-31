d13ec9b2c507e463eb8ef9f878e19f71f81d9059
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:18:12 +0000

    shout mind safe let happy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
