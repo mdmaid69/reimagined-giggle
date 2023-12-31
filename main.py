c4b41e0fef2f2a36e87ed1bc6fb784677d266efe
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:01 +0000

    breakup cross pull describe defeat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
