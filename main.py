4cde9900c6fd729ab949f92a0d7912b947d02c1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:51:23 +0000

    test tight permission immediately relief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
