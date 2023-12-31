e891d6d0f38605a42f57843d5d8eb9f7c2b6d603
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:45:45 +0000

    tread nearly injury onto block

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
