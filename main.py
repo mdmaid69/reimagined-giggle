90450ee94f1f5f058e64309449bf99f450067cc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:50:37 +0000

    clothes compare glad waste divide

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
