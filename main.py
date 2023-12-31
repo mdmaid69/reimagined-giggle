cfa8329e0d1e8cbd1788470cfadd8415a7d62a22
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:26:26 +0000

    by warning children wish close

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
