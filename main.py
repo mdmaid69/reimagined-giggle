2c79227fcaf606237b1eeff0cb02d1efee81e209
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:54:57 +0000

    combine finger cope off whoever

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
