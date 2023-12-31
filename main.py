0093d255edb916c2a052ccde0e52322dc1623706
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:46 +0000

    freeze old takeout mend neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-print(sys.version)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
