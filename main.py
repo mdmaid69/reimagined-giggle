7b9c31b49ce45fc6892c7056faedf1bc6f90e7b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:09 +0000

    claim part twist sentence ear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
