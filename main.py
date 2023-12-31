608e6737050b011937e6a55690bbbd51a32d9861
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:04 +0000

    even near breakdown area listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
