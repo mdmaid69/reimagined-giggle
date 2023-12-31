0bb1b96fe93ff988636718994c960c70f19040d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:44:28 +0000

    wire drive song space forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
