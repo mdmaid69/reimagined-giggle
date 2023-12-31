61869b3c13d9618b84e5f6ef683168e54f94c911
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:20:39 +0000

    score more lean often ought

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
