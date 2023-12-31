a8a0b024d90beb38e4d9c848495c9b997251f44f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:28:37 +0000

    disapprove hand feel operation story

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def cube_number(x):
-        return x**3
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
