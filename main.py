43b688b5ce1e9ae56a7c3d097ca72f20f0144963
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:34 +0000

    exaggerate fortunate memory score lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
