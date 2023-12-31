5e9808c76c5b5e37c3e6200297751a8b224ae2b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:17 +0000

    even force burn pile 567enemy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_index(array, item):
+        return array.index(item)
