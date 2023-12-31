d3041cc2bd9d7ca168f097e06df18c84003a9cd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:13 +0000

    shout miss concerning sleep joke

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
