48442aea03309c978d07f672d4c7dc484835f185
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:57:36 +0000

    deliver print hit clue scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
