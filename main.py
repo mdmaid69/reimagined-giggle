7fcf944ba0457505d597a1e07daadd48e5775868
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:34 +0000

    ocean fast bush teach great

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
