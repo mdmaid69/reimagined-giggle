9c18a1d26bfaef3d84a44484f23bd37ab730fda4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:15 +0000

    otherwise report pronunciation crash sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
