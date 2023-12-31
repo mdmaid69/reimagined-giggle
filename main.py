3c0f6ca1909a74d63eea9bf32c71c7fe66d19af6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:13 +0000

    profit pleasure exam warm sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
