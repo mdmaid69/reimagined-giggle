48593a2827e94d36c2a5f87f615361dc9bc77074
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:06:29 +0000

    cost ourselves exit birth sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
