3d7d9074371a4481ee4f784523f890866220830d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:23 +0000

    accidentally without copy sweet since

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import platform
+def get_python_version():
+        return platform.python_version()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
