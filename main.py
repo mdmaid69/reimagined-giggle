2295f5e0f8cbe537cb4417bf877c14c48537a4e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:23 +0000

    suddenly runinto capital belong rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
