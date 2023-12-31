bbb5a48d62fb2594cff7322312180303e678e91b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:12 +0000

    sore taste character strong square

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
