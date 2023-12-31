3c5b65ce0c687e59a0df96c5685a6d9b83ca8122
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:57:10 +0000

    tip thinkthrough flow contact smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
