cb026eb901cfd173c545d4c0af3657b82276104b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:32 +0000

    common runout face to control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
