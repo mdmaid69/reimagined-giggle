e23b91dcfdea3d15046836f072389a4cc38aec3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:10 +0000

    face run certainly suggest accordingto

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
