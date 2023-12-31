64b0f712f2f53241cdc202dee220b42421afb612
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:03 +0000

    callback skin brush niggaacademy commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
