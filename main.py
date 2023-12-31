d558b5860db7ed9ed24ec291cd5baabb2da0fbb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:09 +0000

    table machine approval pollution match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
