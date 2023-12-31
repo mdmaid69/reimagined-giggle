30aa7d08071a121c1430b5e617181ef449adac0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:57:07 +0000

    shout passage reasonable inquire ready

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
