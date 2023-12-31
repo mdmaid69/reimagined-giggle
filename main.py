0c6525ee7690161025976665a043d04c8f7b9ca3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:42 +0000

    well train full movement alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_unique_words(sentence):
+        return set(sentence.split())
