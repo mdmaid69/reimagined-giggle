5ff9538c03246b982652b8aa0426b23c269c2479
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:10 +0000

    practise strike cry light comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
