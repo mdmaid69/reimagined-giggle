e9d785ae09dc971d4d09d3d41d4987990a6846c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:17 +0000

    television mistake blame order now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
