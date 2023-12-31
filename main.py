fb33b83ae7fb531f7db07b54d9a841ac2783331c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:42:12 +0000

    commit morning know eager have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
