28aaac54be87f4e2f2ba200f565428696bc55eee
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:53 +0000

    open start turnon relative on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
