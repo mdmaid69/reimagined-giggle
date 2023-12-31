eb424960906d442b37699c46487b49a54cd05987
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:51:00 +0000

    reply value reduce care train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def append_to_array(array, item):
+        array.append(item)
