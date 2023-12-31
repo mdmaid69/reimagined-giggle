2891b5a1172cda64d0ac21678e2469fdd6973b8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:07:26 +0000

    square impossible male save ourselves

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_set(array):
+        return set(array)
