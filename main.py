25cfff9e47d9be76785cc00cdbd503249b0aef2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:37:32 +0000

    able blame break already child

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def convert_to_octal(n):
+        return oct(n)
+import getpass
+def get_username():
+        return getpass.getuser()
