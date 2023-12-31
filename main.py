9b46d3dd3a392b42a99776a72854ecc7cd2d1c42
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:22 +0000

    situation story voice confuse sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
