fbcc9edba00d91c4eded829a011871ecd4126476
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:52:34 +0000

    tough wonder this unit project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+from collections import Counter
+print(Counter("hello world"))
