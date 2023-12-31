f1e504a40bfea0d0edd484efdf38030d08a80577
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:22 +0000

    team sorry brown start before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
