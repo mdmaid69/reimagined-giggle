d791d598e9d46dee27bb38c09bf013bb29e11f25
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:11 +0000

    fear pause try admire dress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Characters:", len(text))
