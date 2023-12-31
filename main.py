720275fd2a31060d8d577fc966bfe38bbb14dcaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:48:56 +0000

    spring next cross ignore even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
