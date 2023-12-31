89cf1f45138e540e47df26024d376b6c6d12afc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:39 +0000

    sport ring upwards brother compare

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_from_bytes(bytes, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
