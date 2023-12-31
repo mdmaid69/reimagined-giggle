6cc97663fcdfadd3bc4997ae5ed6425128882155
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:39:52 +0000

    last opposite design handle attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
