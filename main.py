5cbba8a298ee0d0b3dc8657c07fd363fb9ce6cf9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:18 +0000

    please hear present guard worse

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
