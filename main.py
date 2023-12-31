1b37970159f62d527512ac01ba2701800f1532b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:41 +0000

    power under scream attempt light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import os
-print(os.getcwd())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
