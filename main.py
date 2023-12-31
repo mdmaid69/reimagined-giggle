262d15033b03056fed3baad83572ccdda031f77c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:15 +0000

    leg goup embarrassing thinkof culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
