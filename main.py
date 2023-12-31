076589db3b2a49980bd2cede0b0e8ffe9748175c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:35:53 +0000

    house pretend surprise well sort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
