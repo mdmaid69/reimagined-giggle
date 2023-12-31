6681f1df1cb415e4ccefd2fb763558492d93853f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:27 +0000

    nurse will bottom circumstance aim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
