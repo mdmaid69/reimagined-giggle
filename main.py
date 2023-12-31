eefdbf3aa5651911811c4c7fdc23d70d129d62d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:48 +0000

    process last narrow conclusion too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
