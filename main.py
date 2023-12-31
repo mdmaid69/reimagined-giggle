ce69f76c7578371a11324c2a0018aeb808459643
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:42 +0000

    murder dream equal several relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+print("Hello, world!")
