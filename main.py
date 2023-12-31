adba905f576ee4b060c23e1bce445d66b562ddee
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:19:18 +0000

    already slip ordinary worse order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_characters(sentence):
-        return len(sentence)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
