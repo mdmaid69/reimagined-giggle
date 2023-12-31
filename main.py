5a92c487266f2aa73004f028a9a48db74b7ddc2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:59 +0000

    square neck cross bite cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
