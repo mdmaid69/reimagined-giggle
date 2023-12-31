c48e6694dbdf58ea81c71f36dc516b17427dc8bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:49 +0000

    embarrassing apart punish substance cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
