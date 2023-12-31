fec857b7fc426aa8a6b8d90aaa360977c3d5da53
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:33:25 +0000

    turnon runinto drop anything single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
