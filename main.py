ed9c6ee4cebad7ddd57e27884626f3d66c973cbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:15:12 +0000

    Cc behind complicated motion in

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
