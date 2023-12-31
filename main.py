f33970c9f244f21be68064c9b33c5e683bf71710
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:03:09 +0000

    thankyou fall night tidy cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
