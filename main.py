da2ff0d9d5db11f512a65d831e86b9d5d0a15b5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:45:24 +0000

    grateful yesterday possibility wipe guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
