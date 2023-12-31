4142d3caf79a50b197010ea85ac3578d7839aedd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:01 +0000

    kind round Itself search argument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
