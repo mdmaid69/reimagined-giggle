ec3211778bbdc28395526c86f4c43f6cabab396c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:15:37 +0000

    food bound curious fetch majority

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_bytes_from_array(array):
+        return array.tobytes()
