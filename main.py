c6b86d9fb108ed2f9b87b625d015a5a49f04c212
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:45:10 +0000

    evidence gap foreign journey force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
