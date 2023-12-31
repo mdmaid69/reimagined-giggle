4956dcb54798a81d613c145b54b819fc1f0203a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:55 +0000

    interested science organize journey block

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
