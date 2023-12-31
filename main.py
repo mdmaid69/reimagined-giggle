7d7922fbea7714b3ba72d0be4ac42d9d76abb313
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:25 +0000

    can alive street measure hospital

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
