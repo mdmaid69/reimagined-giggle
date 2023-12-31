1d2f71321df96b9017a1c1be572bc2a83177437b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:56 +0000

    avoid insist lucky legal various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def reverse_array(array):
-        array.reverse()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def get_array_as_str(array):
+        return str(array)
