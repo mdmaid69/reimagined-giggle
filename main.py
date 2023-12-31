80c7a966325bf44e33c8d182fd410e9c70662e76
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:36 +0000

    hold find programme national size

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
