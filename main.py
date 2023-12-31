48261b90da0b911fabcfe7ff17cdfa237b418fd8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:55:41 +0000

    life bar flat blame breakin

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
