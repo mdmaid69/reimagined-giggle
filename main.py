dab5bd15cdfc3a3d97a4b25e993d22e399ddb8e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:57:59 +0000

    apply possession thorough deaf characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
