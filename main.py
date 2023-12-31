4d62b5f13a5fae62f94db34ac0db210beb4d95fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:34 +0000

    talkinto happen although refer bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_even(n):
+        return n % 2 == 0
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
