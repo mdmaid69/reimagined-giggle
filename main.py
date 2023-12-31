6d6dd57fa75d96752750f44079514d718a52470e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:36 +0000

    own poison cut dry admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
