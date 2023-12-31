22aa422cd908944f4faafc8f5da08be825ac1f93
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:10 +0000

    or impressed crash disapprove description

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
