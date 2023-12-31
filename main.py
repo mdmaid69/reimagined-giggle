5b39005346e023a26ebea7b89c3b34194a39bada
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:02 +0000

    mark purpose case freedom spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
