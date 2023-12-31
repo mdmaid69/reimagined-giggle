5eeb2f96d2a4c25af12d8da161a4e2300d8d0dd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:41 +0000

    personal another sure second back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
