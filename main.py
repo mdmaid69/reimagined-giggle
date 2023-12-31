2f74ed39bd61366322ba20fcf5e1ff9a3d20839c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:34 +0000

    like secret sit curious broad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
