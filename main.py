e0c213328493dafd0c41ecbb4e5131d96e9586fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:48:14 +0000

    appeal fix arrange intend grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import collections
 def create_user_string():
         return collections.UserString()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
