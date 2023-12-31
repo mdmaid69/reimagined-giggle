c5f3ed6d1de1b3998a1786842b02311b15bcfdda
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:25 +0000

    very fence comment prevent work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
