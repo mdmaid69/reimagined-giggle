936c50bc4454f1a5dfed502ff4765ffb0f79178f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:13 +0000

    light breakin fortunate tomorrow neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
