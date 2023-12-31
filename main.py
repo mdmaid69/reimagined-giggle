7f55eff23a3c34a9ecf4cd00d5a9e1bc4303fb5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:30:52 +0000

    behave reliable memory interfere rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
