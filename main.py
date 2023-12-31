052b6e0fd57c3d9401e62b4a225a23a2f1f5650a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:46 +0000

    squash thumb present offence evidence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def cube_number(x):
+        return x**3
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
