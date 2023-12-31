e1fbda3452e7f0d64a7dee5aba3a7155f549fdeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:22 +0000

    close always die comment sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
