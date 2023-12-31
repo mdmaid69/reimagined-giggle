3bd41eafe14b9c4600632aeea547f730af86239f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:32 +0000

    brother develop live refuse evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
