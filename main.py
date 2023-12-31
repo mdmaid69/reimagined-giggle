20fc526801b13d4c66b4e70b0a8f9c94a12bc763
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:10 +0000

    equal bound load bend wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+print(json.dumps({"name": "John", "age": 30}))
