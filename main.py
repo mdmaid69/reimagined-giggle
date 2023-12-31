bb4537cd574d39ea7ac81bfd05b5c8dcbc677c25
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:25 +0000

    present on flow rescue hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
