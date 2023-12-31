b4754b7decfe529a77235d0cc28618706cb02926
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:44 +0000

    interruption print expect suddenly hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_counter():
-        return collections.Counter()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
