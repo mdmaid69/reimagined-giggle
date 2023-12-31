dbf8a6755243f076f5b43dfe6a407ff2411e5621
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:25 +0000

    rub hope hit defeat scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def divide_numbers(x, y):
+        return x / y
