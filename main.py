294d4a3e31ce50da73105d53318908ecb4b994c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:17 +0000

    smooth cheap calloff eye why

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
