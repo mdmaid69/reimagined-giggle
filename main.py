e693e9d6a3e1624a8b689fafeb26f442d78be8c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:25 +0000

    sort off belief loud else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
