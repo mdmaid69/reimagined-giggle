8eda128ac994a6e97b268339e5e8df74a3e19b8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:48:07 +0000

    invitation agree off thumb meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
