a3b48e36fea7746786d7cef04dfdd04585280769
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:37 +0000

    model treat whether Zz any

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
+def is_odd(n):
+        return n % 2 != 0
