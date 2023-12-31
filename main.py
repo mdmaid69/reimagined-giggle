41178c456a613351edc6cd026d13cc89c3385ce7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:01:47 +0000

    risk drive wheel hit come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_even(n):
+        return n % 2 == 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
