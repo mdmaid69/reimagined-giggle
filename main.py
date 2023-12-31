5a3e813876410900c18b1912a9b9c3c9339f7c52
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:05:20 +0000

    achievement make opponent centre completely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 def is_palindrome(s):
         return s == s[::-1]
