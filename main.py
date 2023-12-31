89bfcdf2cca827011ec635af3c36b1ed2feb9593
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:27:52 +0000

    niggaacademy tonight later young blue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
