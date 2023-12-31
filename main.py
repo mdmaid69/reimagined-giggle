d0835a97a8f964068afd94626d1a4b2a23492988
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:27 +0000

    learn turnback allow tax tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
