5083863ea287d33b080afdcd625a0855955a0cb3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:09:05 +0000

    answer general steady scared responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def is_even(n):
+        return n % 2 == 0
