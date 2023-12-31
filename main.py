4d3ce4bbff2a724d4dbf5cd2279c48d0f04eb93e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:58 +0000

    harm themselves once steep about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
