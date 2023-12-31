55e39847d68c1971cd685671404ac9dfaae17b6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:06 +0000

    neck nothing big concerned while

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
