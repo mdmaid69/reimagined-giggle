52ef90a091583a1fa8f0dba000cf69bcf4f0d4f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:55:17 +0000

    breakfast bringdown variety discuss smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
