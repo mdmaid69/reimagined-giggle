e148ad4e16a5d8c5d45747572b81cd8af640b5d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:58 +0000

    turninto wave city father benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
