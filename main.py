32b15998a6e57f9e1abf832408f877350c8e57ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:42:25 +0000

    piece innocent custom agree health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
