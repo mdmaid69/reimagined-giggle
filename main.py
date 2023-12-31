a481c992b59290f10a096b5d136215e91d8b6127
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:55:44 +0000

    goodbye trial promise wind would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
