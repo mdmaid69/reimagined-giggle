2f53aaa91b606e1c7bea3f701aa591cd9dc2a8e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:40:50 +0000

    thinkof everywhere speed on unit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
