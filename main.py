226c005b747312bfbbe3537e6b195afab0daa41d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:26:45 +0000

    account belief weak lead tape

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_density(mass, volume):
-        return mass / volume
+print("Square numbers:", [x**2 for x in range(n)])
+print("Hello, world!")
