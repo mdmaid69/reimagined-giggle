345a3dce25eadbad4258c91c18f99036940a7380
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:57:22 +0000

    average since deal clearly anywhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def convert_to_hex(n):
-        return hex(n)
+for i in range(5):
+        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
