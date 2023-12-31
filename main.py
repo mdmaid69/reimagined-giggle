d7c2d41e1ed0f57acc2f7260a8805249f2ea24bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:45 +0000

    sweep between surprising deep close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
