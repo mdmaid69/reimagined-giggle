2e2258a08572203bc8092c00fe35eedfdda18de6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:58:45 +0000

    prefer comedown pump children shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
