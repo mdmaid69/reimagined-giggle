a7bd221c52de0177b4289eb3a36e5facbc2bdfa1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:49 +0000

    awake or ticket medium break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
