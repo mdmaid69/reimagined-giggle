07a263b6ba81636c0f498c9c3a9129c659fb486f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:25:10 +0000

    borrow sad lower unpleasant church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_date():
+        return datetime.date.today()
