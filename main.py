ec5045cab56c26d1d72b8958df1c5f5f9f8dafa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:25 +0000

    freeze turn sort low transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
