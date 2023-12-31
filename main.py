ef7bbb1dc113ba3819fc941b837769657c7587c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:19:28 +0000

    transparent in shout wash nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
