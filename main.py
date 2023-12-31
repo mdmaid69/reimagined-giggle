00791e75924e3b0f4b47ff13e133883afce41b60
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:15 +0000

    development Bb twist bite getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
