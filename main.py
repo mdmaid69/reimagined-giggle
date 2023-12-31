5f087d86721022f802defde417ba0439f16c13a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:45 +0000

    base hello class eat excited

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_hex(n):
+        return hex(n)
