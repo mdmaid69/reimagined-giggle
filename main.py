d8d52e067b5f572260bbbd7df8fa8c8d6216e760
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:46 +0000

    recover accidental progress wild over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_python_version():
+        return platform.python_version()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
