af5d981d9298ea163507868b43f6da1caec83646
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:43 +0000

    motion above easy pride town

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
