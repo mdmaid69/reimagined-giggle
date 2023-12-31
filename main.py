e67b89011d7ee89e786242fe2c8a92bb8226c7dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:27:34 +0000

    bury lend miss both work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def convert_to_hex(n):
+        return hex(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
