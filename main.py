fe94b6bbebc79534f402849f6fa82dfb004c1916
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:45 +0000

    choice clothes pool plastic tidy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
