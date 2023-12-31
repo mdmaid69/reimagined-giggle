a98ea861f0c3acfc26afee449a2af3f41b963408
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:08:50 +0000

    street downstairs cheque prevent habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
