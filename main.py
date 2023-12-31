c92c575b7d5889bf9427df3946ffe7f3b5f79ac0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:49:03 +0000

    unable mind crime period guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
