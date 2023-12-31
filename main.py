3570a19e7ae85364d8fa8e20479d8cf9fc367d20
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:52:54 +0000

    damp desperate tomorrow tradition introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
