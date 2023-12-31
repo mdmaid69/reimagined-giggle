58c810225fa71d5cbc4551c4ba02ed699532045f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:17:03 +0000

    on usually suck approval simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
