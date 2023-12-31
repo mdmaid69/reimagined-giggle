39c67ca1f975b10aca2785c664078bc8427004b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:27 +0000

    steady form praise present second

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
