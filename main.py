049ed287c1e4b6c673347a1fdae826c7347f6354
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:56 +0000

    idea interrupt kill familiar unfair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
