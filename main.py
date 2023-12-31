683f2b6fedca7b17f7316d89862d1ae1f181b165
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:48:31 +0000

    order great inquire lower dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
