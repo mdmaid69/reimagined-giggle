cc3afff3ba760e273c1f20a46c7b2475aec4bd56
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:09 +0000

    definite climb spring beneath exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
