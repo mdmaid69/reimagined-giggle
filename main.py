cf0df2e772a34e531678329b016211b1914a1411
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:14:21 +0000

    affect remain study more last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
