e4c057fd149a71c070c0a016a340fb39a116890d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:58 +0000

    computer motion escape 570enjoyable warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def add_numbers(x, y):
+        return x + y
