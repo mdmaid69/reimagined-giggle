a8ff0a2783b52e1da2bd5931faa175770dc13c50
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:41:05 +0000

    fortunate reject flood juice hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
