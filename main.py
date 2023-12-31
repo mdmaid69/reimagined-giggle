28faa88f9ff707f1840363a79ed9c265b8004267
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:07 +0000

    behind project lonely getback afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
