df832bde0438c9bbc6c84a183b6c24bd7260e610
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:51:36 +0000

    ask experiment fence fast organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
