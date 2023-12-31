bb8376145c8035bdc66e9246564e2f26d5c06419
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:37 +0000

    Dd house seem close test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
