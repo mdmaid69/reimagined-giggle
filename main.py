adc765113c6529779307af6ef8d1c11f9eaaeebe
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:47 +0000

    clean smile communication style mind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
