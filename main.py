e27aeaa0ceea7294922645012ba3ad12df3d2571
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:16:32 +0000

    justice next tooth godown shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
