e7948bf1d108d8b6844555535a5971ba502c071b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:05 +0000

    equal type lookup perfect accident

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
