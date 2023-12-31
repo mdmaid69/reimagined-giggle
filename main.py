bbcf49812727137ab59975da276bef887bda9546
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:21 +0000

    place share steady force force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
