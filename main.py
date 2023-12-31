f177f05ec07a7ac55b4dce62303c341400aa8035
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:00 +0000

    getback look brush keepaway piece

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
