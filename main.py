9b4751601d54a7ac16367298267d618fd9940f08
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:37 +0000

    doctor control number play stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def exit_program():
-        sys.exit()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
