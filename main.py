15630a57c15b73d3d622cd93c8928e3eb06869e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:26:12 +0000

    considering store translate difficult pool

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def cube_number(x):
-        return x**3
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
