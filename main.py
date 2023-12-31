beb2366364808295966131769887d204ffaf80d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:43:28 +0000

    learn help our handle flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
