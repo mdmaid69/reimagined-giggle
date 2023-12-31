31cf8b08670e26e02ed330f70706c7d1fd4ab4bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:01:25 +0000

    notice bang means shoulder closely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
