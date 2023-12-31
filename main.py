be7e732b8b80713a6048ab4cdca82f6a05eb9bf1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:50:40 +0000

    keepaway why watch practical yourself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
