81723b343c7abb42a37ab5acd939ccb931d4560f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:58:35 +0000

    drawing getoff by Zz further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
