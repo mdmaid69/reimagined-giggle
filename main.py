d8a0d15802ae1fa8ec9c2277232499b0d0014743
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:22 +0000

    write escape repair alone ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
