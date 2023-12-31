748dd672961f45b36936480bccd7064afe3c7aa2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:45:51 +0000

    train sign unite smooth survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
