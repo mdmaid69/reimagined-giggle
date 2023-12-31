4551b62ac950b2e6d4e85722af1e2c46358c97b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:01 +0000

    cum wonder slide least OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
