47fca37f79826eed0fe6d8fcfce8f7d9f3f87bb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:10 +0000

    occasional thumb unhappy damage few

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-print(math.pi)
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
