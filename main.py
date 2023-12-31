0d14ae9d8a7890eb299c80da5b2897f36e5a08ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:16 +0000

    block colour ignore activity careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def greet(name):
+        print(f"Hello, {name}!")
+import numpy as np
+print(np.array([1, 2, 3]))
