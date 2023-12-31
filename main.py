a0ad6c8d0648ea137eaaf8a0c6e835e327a237cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:11:23 +0000

    carry common throughout mention there

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import array
-def clear_array(array):
-        array *= 0
+def get_list_from_array(array):
+        return array.tolist()
