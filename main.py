b3c3a2e3ffd3042e9611c074448b3ee1dc0897b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:31:43 +0000

    change recover introduce fetch greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
