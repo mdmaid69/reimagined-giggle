ccc2dfb0dc02cc4df2ba8d82f2a051174105dd0b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:57:32 +0000

    join downstairs arm borrow weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
