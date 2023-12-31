ddd997008f2a78e598dcbd96c7586f9482442a28
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:57:14 +0000

    important her calmly include square

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
