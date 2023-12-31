86c4c4b4e2d386305fc45b01ee57ddc951f6e917
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:08 +0000

    left edge open mend apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
