300275b4a5e89ff2193f09440217a9171f4ab281
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:04:47 +0000

    deaf hit alone content brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
