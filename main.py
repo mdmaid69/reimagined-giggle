f2840472e0fa38b396ad6847299ccffa514cebca
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:47 +0000

    smell today company over instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def multiply_numbers(x, y):
-        return x * y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
