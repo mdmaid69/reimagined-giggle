e96020fe32cd322f78345742067b5dbe01437f74
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:45 +0000

    politics protest ourselves temperature somehow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
+def calculate_speed(distance, time):
+        return distance / time
