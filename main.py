3a8f13472716f1a0a13cecff15a22cf4c5b359e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:48:29 +0000

    report along church radio comefrom

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
