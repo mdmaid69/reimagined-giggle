524db37e229b0d81ac156b75c7e1fbc4ecd366a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:08:38 +0000

    teach hurry they now outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
