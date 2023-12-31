16e3c837fa77c0a260d04b67ba0e5baba6643ff3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:59:17 +0000

    practical plan reply mainly black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_base_name(path):
+        return os.path.basename(path)
