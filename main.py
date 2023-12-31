9553ac51dd62317d770075bb2cd32c6e7c2b9a92
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:03 +0000

    range hit callback noise crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def subtract_numbers(x, y):
+        return x - y
