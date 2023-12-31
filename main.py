149dddf4bc7987d6a7c2ac356f3d34025ca483f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:52:01 +0000

    nowhere today under other name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
