77f02ad88dbcc55f8447e24fe36d8e7c75be22b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:15:06 +0000

    trouble upwards damage scream pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
