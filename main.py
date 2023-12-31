65aed180a895078b17afc184e96e21795e17c594
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:11 +0000

    rough plenty serve conduct mark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
