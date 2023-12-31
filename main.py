8f591f1bd9814b40346c0971fa72d894d974a4e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:05 +0000

    off afterwards constantly majority appearance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
