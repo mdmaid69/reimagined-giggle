fe28c69f036cc8d02529c3b9ee641c0cbee5fb66
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:07 +0000

    ours remove swing half command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def square_number(x):
-        return x**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
