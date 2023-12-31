ca95e8aa238a18b106e44b760452c42cd9d7104c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:22 +0000

    unite question bush hit keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
