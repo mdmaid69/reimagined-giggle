2b1bbd68838a2f07e4ebc910e05c70775c13faae
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:33 +0000

    return argument perfect mood contribute

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
