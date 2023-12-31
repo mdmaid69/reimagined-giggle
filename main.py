2eeb617c2213b33d94c5a25152cd2f91550d826e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:29 +0000

    support today change Ee gate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
