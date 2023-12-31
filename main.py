ebdbf9bcdfd9aa34b6a0c874905adbefd767032a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:30 +0000

    the permission love Ss want

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
