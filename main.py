488f46d8b1ac163639425cb98ff36ed07a3fbecd
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:57:14 +0000

    underneath stamp busy state choice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
