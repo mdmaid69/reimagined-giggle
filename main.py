55961d8ae2653ed2fef8c1d5248263281385726d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:34 +0000

    form teach successful switch interesting

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
