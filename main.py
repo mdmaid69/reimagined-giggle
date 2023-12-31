a3925b5c05016ed5f000d51bb0f244da55ccf06e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:39 +0000

    transport squash burst another near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
