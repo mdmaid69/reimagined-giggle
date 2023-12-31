e024a274e7fffb6615fcd2141971df4bbafd52f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:47 +0000

    pupil clear theirs public central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
