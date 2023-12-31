5db41fc9a31cf6b7389638487ca93bb5f11b52e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:59:34 +0000

    law expression possibility simple comeback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def add_numbers(a, b):
-        return a + b
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
