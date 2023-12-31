b7cb6024e886a3a380773a421d8ec605886ede62
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:00:42 +0000

    secret behind respect base injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
