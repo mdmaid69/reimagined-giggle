d4f67fad04c1b5b48fc3deaffdd55e75f5328469
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:42 +0000

    organize stamp broad should welcome

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
