6f789f45aa949f5ab25c04547a7bf5b5dcd46fed
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:40 +0000

    stop recommend final thinkof tear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
