752d6c8931c460c94a1398c47ef912831cce7d81
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:14 +0000

    bury public square little mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def sort_list(lst):
-        return sorted(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
