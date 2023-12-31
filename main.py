4fd7801aa3f90627d644fc7919606234fd4d25be
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:36 +0000

    achieve great likely callback always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
