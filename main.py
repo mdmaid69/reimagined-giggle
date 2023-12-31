c4df2d49b392a2bee8bcb7d08595fe1e25d66dd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:22 +0000

    minimum grow instruction agreement belong

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
