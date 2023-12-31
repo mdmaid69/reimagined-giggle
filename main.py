75d3e60efa52e0c84f754a24850ac183af78db4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:22 +0000

    Mm willing always absolutely long

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def add_numbers(x, y):
+        return x + y
