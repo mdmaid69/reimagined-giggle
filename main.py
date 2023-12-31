dbfce6ccaab90090d12a9bd7c5acdb248f998bd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:18 +0000

    connect survive love error twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
