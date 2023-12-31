3ff9c47fd0349a48b31e0ef62e6527dfc703e9c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:08 +0000

    ordinary pressure mistake dance round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
