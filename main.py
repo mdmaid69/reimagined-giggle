38907aa5f81866b4161774a36e6043d1318dfe76
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:50 +0000

    attempt tidy unable few responsible

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def create_queue():
+        return collections.deque()
