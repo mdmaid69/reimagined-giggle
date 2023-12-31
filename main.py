212255b3100a07bf17942099dbd5341033322672
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:48 +0000

    exactly Ll surface basis theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
