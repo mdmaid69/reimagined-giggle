1c8f7ac460b14437f50dca2ab6b84ef593d5dd2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:32 +0000

    bite burst note custom city

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
