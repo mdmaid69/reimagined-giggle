20e1ec5f2b5781474694bfa8bf14d2ce120546ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:44 +0000

    sharp also awake remark physical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
