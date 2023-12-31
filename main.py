fdca606feff1e707383af9d001b84508256ea7dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:36 +0000

    might comeback big model direction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
