2b77c161d648c7803d7f24d2ac7cd59d1986b746
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:04:09 +0000

    pause case cause example extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import random
+print(random.randint(0, 100))
