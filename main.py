7348d29560e56d59873f95994f017ba14285117e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:18:49 +0000

    behaviour impatient feature takeback dull

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
