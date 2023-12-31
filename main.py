960bea55e249f9a205ecd08760ad0a7bfb93b234
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:30 +0000

    suit friendly mistake tight thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
