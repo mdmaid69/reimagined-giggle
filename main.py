0f804cece8aa536d80064b4c267512bc72d22cbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:21 +0000

    her fast drop quick whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
