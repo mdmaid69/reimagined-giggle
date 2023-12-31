5c75b8d42f03e2c71a7c93f8d1018ee25b729288
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:28 +0000

    gate below walk trouble letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
