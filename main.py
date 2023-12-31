ad35167d8144e5ea777f46c605a42ee22878dd8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:56:11 +0000

    around open principle by neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def square_number(x):
+        return x**2
