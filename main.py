1ac2bdeba831b5f3f51c2d8aa17fa0e8b0146a65
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:47 +0000

    pile community thirsty stand familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
