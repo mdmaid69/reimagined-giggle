270a5f331cd8de4be38b20d66a24b7246032dd81
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:28:43 +0000

    season judge cold sort unfair

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
