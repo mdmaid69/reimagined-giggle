f4b1142d05b261bdd889ae934667af8b45b3ea6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:23 +0000

    priority rich closed content contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+print([x**2 for x in range(10)])
