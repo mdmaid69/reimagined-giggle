f64153b869fbf963e71d0dccd32cfdee40316c3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:00:11 +0000

    young whatever pause throughout comeover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
