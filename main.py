1c209daaa925632fc7e76c7668f55cb2e3a79c3d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:44:55 +0000

    minority rain accommodation exact 566end

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_acceleration(speed, time):
+        return speed / time
