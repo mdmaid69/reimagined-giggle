06a8d00e604610d2926c01f88500b11f374298a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:22 +0000

    limit myself secret clear exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import os
-print(os.getcwd())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
