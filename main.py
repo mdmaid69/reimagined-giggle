520e5c03cc7460e341ff4a479755e8c916a9d963
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:23 +0000

    Ee twist present march able

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
