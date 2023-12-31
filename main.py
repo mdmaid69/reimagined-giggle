44730c636caaa9cf1177d1678b255a153c4f6f29
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:33:09 +0000

    upset bread experience over rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
