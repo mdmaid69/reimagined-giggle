ee4b717a09d19849ec4031ccf4d5b26f8333b252
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:55:59 +0000

    hope compare close attractive everything

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
