7b6ba49df80c281ec3c0af11339db6e707c7245f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:24 +0000

    part good loose form fast

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
