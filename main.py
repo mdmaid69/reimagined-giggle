e7aadab41970ada2545e9ba678daddf8a0657d01
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:39:03 +0000

    disappointed bet limit hide cry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def convert_to_hex(n):
+        return hex(n)
