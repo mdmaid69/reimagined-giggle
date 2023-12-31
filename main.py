e9322a2e00f380be03a01291a90cbccd69772ae7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:03 +0000

    escape squeeze far immediate field

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_max(numbers):
+        return max(numbers)
