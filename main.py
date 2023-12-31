47e2ec2f1ede641506e19fefdd294dc4977af616
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:08:29 +0000

    compete spot contrast church flight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_min(numbers):
-        return min(numbers)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
