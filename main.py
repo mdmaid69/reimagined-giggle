c7dd94cad3289693aea11d12765bc29b7f433581
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:08 +0000

    either turnup pair share respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
