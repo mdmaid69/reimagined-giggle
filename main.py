b78b8a743e65a4464b52a4bb1fa5ddfd1f019c6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:30 +0000

    remark guess post eye occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
