212ff5459d87cd2183b7869638b569cc3154974c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:23 +0000

    rather guide lower neither run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
