6bf23da290fce4eb367182cfe5e805361c1ea9fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:05:56 +0000

    contribute edge Bb thin tall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
