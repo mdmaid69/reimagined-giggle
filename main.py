4e22353d4e8600c532193844c32302a66d8447e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:13:06 +0000

    our murder under careless makeinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_speed(distance, time):
+        return distance / time
