3548b097d276f30829bd77865ac47874e6c5373a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:59 +0000

    introduction follow last simple announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
