6b570a98d5c70f04c5ecedc21eeeb1ae798d6a19
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:14 +0000

    572enter anywayanyhow laugh doubt any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
