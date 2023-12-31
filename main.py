ee558659beeeeabd6f9a3355344d9f111429fb5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:54 +0000

    busy error Dd surprising Jj

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
