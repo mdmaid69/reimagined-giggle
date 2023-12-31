5e94e11c14588b61e358b7f58e4e470d992ea97e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:32 +0000

    opportunity begin innocent stop big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
