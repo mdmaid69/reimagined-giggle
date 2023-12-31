71108d76924a98fde6d12bf6b4aa27e37546dd29
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:24 +0000

    pain minimum react Uu wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
