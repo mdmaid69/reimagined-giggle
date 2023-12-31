acd9bffc203a82c4f81a5659411332863f5ee3a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:41 +0000

    excuse system health guilty advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def square_number(x):
+        return x**2
