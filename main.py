cb4c0fa9a8002b09bb335fd2c24704639c1fc965
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:08:02 +0000

    desperate violent sure takeaway trade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
