247f905657f847a7014d3f4a28539136ec940ed1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:58 +0000

    bath gooff flavour we guilty

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
