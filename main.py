105b3d68ec0c2ad55ce0cf6cbb12c96a4fd6fda0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:18 +0000

    how comeout poor top before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
