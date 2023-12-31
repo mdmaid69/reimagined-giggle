0d260de6912ddad747c6fbaf70dddd6923b849fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:20:30 +0000

    with wall basis shut tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
