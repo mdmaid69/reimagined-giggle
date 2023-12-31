88c7c7b9172317dd2573c1ecaa732e5905aaf900
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:09:52 +0000

    imagination bright mysterious ring sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
