ebcd61e800594e88a1943be8e38705875bd3c366
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:54 +0000

    theirs under there exploration offence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
