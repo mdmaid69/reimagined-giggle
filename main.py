2aceb2f2b581fbb64fbef4b1d1619b61949fbc2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:44 +0000

    steep refer threaten cover somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
