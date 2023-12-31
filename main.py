2e5da984607fcfaa8e9f162e485139319d926f8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:52:44 +0000

    search when land pocket sort

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
