9212fc86b9f50e199629e2b927ad83a7c17284a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:50 +0000

    turnback careful union reply unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
