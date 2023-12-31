e3dde1f6f0163325d6868a5cb346acad3cb305ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:22 +0000

    mysterious better estimate fault ourselves

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
