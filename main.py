b1540fb0b33b99d27620c99a6659572fad0adee7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:40 +0000

    sometimes gap official future gentle

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
