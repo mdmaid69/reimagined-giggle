07f0d40c56ae46e2246e9073c8c34f4652505b34
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:10:57 +0000

    exchange speak full education emphasize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_density(mass, volume):
-        return mass / volume
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
