ec5ff43059eda1c728cc5969a4f0d6605f2771ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:27 +0000

    natural any material off lift

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
