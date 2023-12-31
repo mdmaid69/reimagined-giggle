435ca5d83da68fb71c498724430f67d819da5660
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:28:20 +0000

    well pupil tie judge size

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_user_list():
-        return collections.UserList()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
