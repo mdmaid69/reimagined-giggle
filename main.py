7103c59e5aee890d49be3c7e8ef49c612ef07fd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:04 +0000

    door colour until criticism sorry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
