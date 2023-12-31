fd30d1b6554b3e0f447165f835171dd5b12ea80d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:38 +0000

    turninto police season complete workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
