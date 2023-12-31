ff9064f7d16e29f98135c78d6c5eda01b3f51ba4
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:04 +0000

    set value play contain usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
