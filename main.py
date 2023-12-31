3f5940e77d7751e3b82aa30dca9465e123f6bd46
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:45:20 +0000

    programme shape drop order last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
