707b47ab3dc6a968a7393afe57597b7cfbbda239
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:52 +0000

    possibly hard ocean squash eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
