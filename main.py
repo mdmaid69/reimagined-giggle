b0daa97065e245266500c818a011ba68d9ed62b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:32:49 +0000

    pollution declare protect rescue tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_power(work, time):
+        return work / time
