e86213d857ea500a60d82f28f24eb8c3f97d1798
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:10:43 +0000

    manner turnover feature exploration fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
