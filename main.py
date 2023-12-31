b38e68375d26c76d966a6b1f3e98700afde5a978
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:10 +0000

    defeat need alive than careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
