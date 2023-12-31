00dd7f5dc92f7d2929c08d27d4ad5cddacbc57b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:40:21 +0000

    thing wish burst one ours

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def convert_to_octal(n):
-        return oct(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
