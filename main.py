8739ece3aa2a726b2e562160732474a12ecf461d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:48:37 +0000

    mark float physical cheque partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
