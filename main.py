39bf22738275c65e54dad8ea8967437003925fc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:12:57 +0000

    excuse whenever beside service sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
