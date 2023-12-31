68fe3e854c7cc0a8588de772da4942c87002c113
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:39:10 +0000

    familiar friend classroom increase mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
