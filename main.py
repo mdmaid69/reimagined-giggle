9cc85b65f64a0f167316c2fa1190fa62600b64ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:22:14 +0000

    closely improvement switch bad shake

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
