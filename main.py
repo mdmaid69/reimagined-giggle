7e154c2337144fc6d08c20aab526e6b389ef2661
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:24:11 +0000

    their married shop mean answer

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
