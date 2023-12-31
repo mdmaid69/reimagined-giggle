56abc9e1d3435a8dbc257b4193822cef2610f487
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:28:30 +0000

    difficulty worried broken luck introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
