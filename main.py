7920ac54ccc8aaef7885b550bbf0fc8ab281d306
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:23 +0000

    cause ticket enthusiastic still hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
