453a8e83464645c1511c6f366fbd71bc71c03121
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:02 +0000

    eye communication fail load nowhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def clear_array(array):
+        array *= 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
