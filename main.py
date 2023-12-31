97f929b7b0953a7fb477080023a0023335f706c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:19:37 +0000

    difficult hit total smile willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
