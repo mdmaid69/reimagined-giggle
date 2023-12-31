402e60c20d1900fa555f00e1d48aee16eea3318b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:16 +0000

    belong Itself below normal ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
