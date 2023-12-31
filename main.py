22438db83321b26931386f5cf750fe7c74b86e88
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:45 +0000

    genuine shut hurt separate nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
