cffdd6295a95fb702ae961d58f4eb4207fe4b447
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:30 +0000

    fight 566end food at reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
