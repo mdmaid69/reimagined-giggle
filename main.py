5bfbdaafb88075e346a8470fd9d333840de7be9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:43:58 +0000

    far strike answer goaway most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_logarithm(base, x):
+        return math.log(x, base)
