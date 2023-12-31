95403999f6f47e42e535089abcfccd878ff97ccc
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:35:22 +0000

    colour off only season emergency

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import time
-def get_current_time():
-        return time.ctime()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def count_words(sentence):
+        return len(sentence.split())
