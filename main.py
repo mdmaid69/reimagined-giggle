51933c7647ec555e7cd1959cad95633a2a3930a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:00:32 +0000

    holiday kick when present trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
