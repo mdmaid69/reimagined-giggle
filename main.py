86ad0ab2591cf01cc919fdfc08d359e91b0dac18
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:31:36 +0000

    moon black sure joke before

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def reverse_list(lst):
-        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
