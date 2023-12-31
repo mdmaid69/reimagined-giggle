69ca3f5a575fc7c7e16ea2880741bca4b3356bb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:20 +0000

    pattern most though upwards in

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
