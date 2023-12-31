d672b8708f3a48a9bcf0b8a99d96733a2f9aa925
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:00 +0000

    talkoutof previous uncomfortable turn interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
