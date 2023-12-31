fc0a1ec9e4d5a82ae9c57dd6e778f2226a9a80fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:02 +0000

    its radio sale tragedy hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
