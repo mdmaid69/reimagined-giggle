1f125569b1f5d95b405a005069e8eb11eca22a2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:20 +0000

    check set improve 568energy strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
