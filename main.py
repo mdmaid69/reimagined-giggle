000c698ef1e43ddc1cc03f46289003b1bc2546e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:38 +0000

    scream report correct block pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_floor(x):
+        return math.floor(x)
