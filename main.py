d61c593fa5e274118e59ffe02442f2edac2689f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:43:10 +0000

    careless mine on themselves poor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def divide_numbers(x, y):
+        return x / y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
