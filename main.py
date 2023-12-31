70fbf9873b158ab4521920e357c242208b0d6f59
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:21 +0000

    solve handle nerve bang serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def square_number(x):
+        return x**2
