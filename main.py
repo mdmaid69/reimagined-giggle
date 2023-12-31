c42c601b001e599efabb5c0ebed7eee017111048
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:37 +0000

    Zz company minimum back pretty

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def square_number(x):
+        return x**2
+import array
+def append_to_array(array, item):
+        array.append(item)
