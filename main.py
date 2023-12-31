13c1ae310dd2c848e8034968b358bb7db28f8183
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:19:32 +0000

    record below mark pure estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
