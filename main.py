9a2be83dfa3b1a45ec4e94262039856558e87279
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:03 +0000

    genuine rush country tall raise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
