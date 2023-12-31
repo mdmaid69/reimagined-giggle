01c845343792f9b7abde1c1b345e3c09481c0210
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:27 +0000

    Ff fit class advise negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def add_numbers(x, y):
-        return x + y
+for i in range(5):
+        print(i)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
