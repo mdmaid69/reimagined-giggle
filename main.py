11d9d4b6e1601527cea11d23b01baebdb64bacc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:30:02 +0000

    short picture correct recover bother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
