c59d3c2e3b5b5cda7dff3b937cb42a8a317e0d4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:05 +0000

    thank slice intend takeoff generous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.time()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
