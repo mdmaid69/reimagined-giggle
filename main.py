16be9514a7d21e0ab9763dfb9bc0323e173ffde4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:18:32 +0000

    goodbye sound invite sting contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
