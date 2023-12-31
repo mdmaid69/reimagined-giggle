99442a2811e87ef83f74e23318d68bbf9e76384c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:41 +0000

    life morning building upset 179bite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+print(math.pi)
