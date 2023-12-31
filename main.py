d54334b83bf5f07a8c690a6be23fde18d88fc2f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:40 +0000

    interview advantage measure fair pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
