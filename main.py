069ed231a9dda66cc0b3d22757cf5fb4df205ccd
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:24:07 +0000

    meeting you theirs confuse decision

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
