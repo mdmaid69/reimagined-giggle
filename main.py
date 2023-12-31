f7f2e69b21efb290ab728a6a3a7f4a19080d1a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:39:36 +0000

    fortunate mystery there work passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-name = "Python"
-print("Hello,", name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
