e0de12ad886ab04e285d5e9465af4f428959b78b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:46:09 +0000

    move question talk part wear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
