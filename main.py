74b1080404f4a1c52f92eef02fc6fc25117a4455
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:11:10 +0000

    university discovery generally keen justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
 import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
