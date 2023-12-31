0348e1fd16739d3500638135653fca5fc8320f57
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:20 +0000

    normal Nn people reply major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-print(math.pi)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
