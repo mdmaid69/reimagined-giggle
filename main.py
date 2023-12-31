1a54f45cc55d80f70d19943a8d7c439a46ac5559
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:08 +0000

    passage repair new aim it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
