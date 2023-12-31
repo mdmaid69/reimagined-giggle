3740d7c5c33602f6f9ba9e77521bf0e1457e651a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:00:05 +0000

    radio mood wrong chief spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
