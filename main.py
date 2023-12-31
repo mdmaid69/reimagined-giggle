c0ca254d9a05d9e04fb30ad98eddf3f030286321
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:15 +0000

    well passage advantage own effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+name = "Python"
+print("Hello,", name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
