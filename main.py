2cd4ae2fffdfd21065f56a7df186e59237fd0d23
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:09 +0000

    secret one arrival this much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def count_words(sentence):
+        return len(sentence.split())
