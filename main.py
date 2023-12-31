fe4e0dea5179c31968eee6c0925f4e01da13462d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:45:41 +0000

    runover eye brush live perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
