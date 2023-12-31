e9f0ab672e3e42e6548ea80107a8e7a4e8d0ff7a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:36 +0000

    how read any safety church

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+print(math.pi)
