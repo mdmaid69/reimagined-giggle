d0777029c000bab69a6b36b1a3d3d5d130ad4d4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:52:52 +0000

    agree work deep callup perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_hex(n):
-        return hex(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
