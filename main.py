1d143c4c034435f590a1c109b705cef7e661fa7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:44:36 +0000

    disagree perhaps run progress match

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
