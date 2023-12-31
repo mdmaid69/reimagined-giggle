e0bf8f08077391ca257eefa13dae97fec3b6c890
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:08 +0000

    sweet dig sore worst seem

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
