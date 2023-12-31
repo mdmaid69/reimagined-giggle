8cf05b4f8d3ff0058cc2b9c8a2390ae8775dcade
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:07 +0000

    base rush normal Qq fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+x = 10
+y = 20
+print("Sum:", x + y)
