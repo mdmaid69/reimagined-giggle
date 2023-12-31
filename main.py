145a960984b7a2d4640d74bfbd652c09745a5101
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:35:22 +0000

    low display lower thorough light

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
