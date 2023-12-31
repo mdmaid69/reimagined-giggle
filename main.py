f53b9c8833199d924b94caff5c4582b904898044
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:33 +0000

    announce add cruel behave whichever

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import collections
+def create_priority_queue():
+        return collections.deque()
