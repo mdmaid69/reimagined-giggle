97dfde870e9222f84ad08d4cc4fbed46a0794ce3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:48:09 +0000

    waste raise fond niggaacademy possible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
