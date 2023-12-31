585ee922530afd21892dbf11827e0113f1da6eda
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:27 +0000

    top importance house ashamed pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
