064e19f2aadd3da9f6acf55ca78d8bc94679f4e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:10:25 +0000

    cost home wind teach mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
