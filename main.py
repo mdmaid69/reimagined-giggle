b4d49a3a333459827f3088464fc9afcfa8fccb7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:52:27 +0000

    polite public constantly refer remember

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-print(math.pi)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
