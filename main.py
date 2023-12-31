6fdb2d9b26397ecb825cb823effcf5bab9d2677a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:37:47 +0000

    matter scare plant rob battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
