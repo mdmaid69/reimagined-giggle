63d8bf86119a95e6860d8e95153f4cfe49606dcb
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:16:59 +0000

    float shopping use either side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def convert_to_hex(n):
-        return hex(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
