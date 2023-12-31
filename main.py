258e712c36c8422c617384e5bb2bb2dcc1e63887
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:31:49 +0000

    can cinema sink mother subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
