55c7bbb9c123534ccee1366f0feecea7881be383
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:25:45 +0000

    wind geton passenger introduce train

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
