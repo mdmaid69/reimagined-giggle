6e16d8d69a8a5c611eca69e3dc80d656d9eed2e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:12:01 +0000

    tip boil fortunate speech hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
