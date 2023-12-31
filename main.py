d54215d1baa80b9f6eb98dd813e00349cdcc259f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:15 +0000

    big claim knock holiday factory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+print(sum(range(10)))
