fabce4fae1a5b4b507ec596f2e406875e433f0e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:36:50 +0000

    offence possibility develop acceptable wish

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
