f543b4af89c1bc5982d9c3796fa317706e1307c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:16:56 +0000

    much niggaacademy bringforward rank exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
