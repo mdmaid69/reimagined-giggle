71d31fb9aaabeed0ad84ba22705fd6959ff3207e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:43:20 +0000

    pinch interview attach order top

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
