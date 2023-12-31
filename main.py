7f48ab78c31266a7f3dc119593469ef0c2e61def
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:04 +0000

    convincing delight general important keepoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
