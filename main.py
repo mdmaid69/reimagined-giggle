3a76356e43d37bce71e1cf02d9ffc5ff290ddad6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:32 +0000

    freedom godown turnon everything earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
