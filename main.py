30c70b69ed5702ad08077f1d0bcbb08076c3cdb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:46:31 +0000

    lookup find private wonder precise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
