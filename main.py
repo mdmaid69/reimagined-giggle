17a4eccc9bb0ae2250aee715a03c58414e9c7ef4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:40:31 +0000

    admit play plate only ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
