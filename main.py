e6f1d692e5c137a7995654f829a95da3c2276c50
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:57:55 +0000

    about regional smooth increase guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
