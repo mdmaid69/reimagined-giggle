160da83ce0ebc84095e1ab7fbcf8756b2b7e38dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:49:32 +0000

    solve spite sorry actual hour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
