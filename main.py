7a3c7a7b1948b259b70ca89f1311f06867769f22
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:16 +0000

    cause smell bunch smile invitation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
