be811906a93647c61b3b462f58a03a7ef2ea3052
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:17 +0000

    model breakdown error right skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
