086dbbca19ac561c380c136f256aa2fd9745c1c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:36:35 +0000

    question Pp priority should smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
