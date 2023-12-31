bb673dde596eb3ca7001f5335e01711507459df0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:02:16 +0000

    next away whenever mine warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+print(math.pi)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
