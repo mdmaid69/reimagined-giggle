46fd7586bb6d4d901903e37bd2d60a3f70a7e147
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:38 +0000

    lonely back pretty then start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
