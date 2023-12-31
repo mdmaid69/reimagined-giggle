6e3fe68745fd11254a22651036732238bba4c88c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:15:39 +0000

    apparently otherwise to sweet uncertain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def count_elements(lst):
+        return len(lst)
