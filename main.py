575346d4ab5445f634970dedb1b2f3bb63e637fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:17:18 +0000

    argument mark front sea service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
