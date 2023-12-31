772b4275fb5afbecfae9187f63200e9e2cf982b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:37 +0000

    music exam leg nor exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
