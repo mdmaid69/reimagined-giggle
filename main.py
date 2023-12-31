4519215eea2893d960cfc5b17557f0b966181f89
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:28 +0000

    round comfort harm interview fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
