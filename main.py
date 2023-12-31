04893b2edb36fd354aaecba69d65c30887696bb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:10:11 +0000

    organization wind tour exchange previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
