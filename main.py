ec713284d5dd261db0cf54029f40effa4345fd57
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:45 +0000

    convenient store judge crowd belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
