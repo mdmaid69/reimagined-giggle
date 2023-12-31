6bee20bbd65aab1443e118a60b1e2a448bbbaa33
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:19:19 +0000

    over giveup sure worth he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_speed(distance, time):
+        return distance / time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
