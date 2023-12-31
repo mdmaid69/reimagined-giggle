1f855669c65f1169097df0f27769ce4e054c189b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:54:47 +0000

    need slice platform 179bite comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
