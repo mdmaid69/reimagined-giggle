135359f6612d81514884ae5b41b542561e46f529
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:50:55 +0000

    general laugh ride equal again

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def clear_array(array):
-        array *= 0
+def subtract_numbers(x, y):
+        return x - y
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
