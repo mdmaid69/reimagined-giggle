f7efd001dfd7ba2f44620109c14097f44f72fdf2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:13:13 +0000

    style work best back fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def remove_duplicates(lst):
+        return list(set(lst))
