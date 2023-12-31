3e8b368f28457026d26bdcf90c584f197421e067
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:59:52 +0000

    married cry juice deal stamp

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
