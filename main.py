06cc0d1433ef0fe1088de7f284589f314ed4f0af
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:51:06 +0000

    relax exaggerate file return characteristic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def clear_array(array):
-        array *= 0
-import collections
-def create_queue():
-        return collections.deque()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
