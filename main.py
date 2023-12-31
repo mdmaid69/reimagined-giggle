41f82ff6f1586431bf97e1d54f9e34a29b0fed74
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:14 +0000

    area ticket Nn scream pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
