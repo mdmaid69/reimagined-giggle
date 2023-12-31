9bf3f187538d03d8b1b2661d35254c0bb3a73395
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:26 +0000

    medium surprise by or cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_exponential(x):
+        return math.exp(x)
+import sys
+def print_python_version():
+        return sys.version
