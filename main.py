ce18e4ea772d979336b2bb942a2f172543d40218
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:57 +0000

    congratulations there need clean you

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
