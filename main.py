307d7e829fa0951298a48e00e150f3210f5dc4a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:20 +0000

    accident famous cold callback burn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(10): print(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
