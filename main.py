1b74652dc055609bcc6be289c80c09c54b08c783
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:09:03 +0000

    miss pause turninto black appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
