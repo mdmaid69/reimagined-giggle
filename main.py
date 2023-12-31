7b2888e3b6c3eba91caeb378d6aaf3605b951743
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:57 +0000

    soil cross everybody classroom progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
