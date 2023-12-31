8a63a4012ffa81d7db0545852721bc34fb401f0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:02 +0000

    priority giveup quiet safe into

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
