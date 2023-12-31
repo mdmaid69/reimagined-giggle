741fa8d88a1eb29182eb2f3ea9674b1f74e47bf7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:17:09 +0000

    568energy turnover exist length near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def reverse_list(lst):
+        return lst[::-1]
