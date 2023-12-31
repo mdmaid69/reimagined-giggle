07a944ffdbb8acce75cefd4f30f87d5b72161f38
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:56:14 +0000

    mistake finger hurt sad news

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def is_palindrome(s):
+        return s == s[::-1]
