705b04a11ac6c77ac545bd29eac086cbba499b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:40 +0000

    familiar fetch flavour onto responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
