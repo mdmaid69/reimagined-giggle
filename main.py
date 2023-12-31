be973a21c2d058b7bdc6319c2184a7dd939d3f3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:46 +0000

    wheel eat build person discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
