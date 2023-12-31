73aa2ce807e649500b4067008fb6a17712c3ab0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:22:48 +0000

    square turninto raw print compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.ctime()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_set(array):
+        return set(array)
