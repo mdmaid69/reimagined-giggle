08087e45ae27ca1c68e81bcdbac8a8d19c2e8dda
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:08 +0000

    almost blind because cross large

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
