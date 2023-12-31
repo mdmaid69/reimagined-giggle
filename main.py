a87301861e7e9efea867a965e07a06ead18c0dc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:56:03 +0000

    judge body nowhere offence blind

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
