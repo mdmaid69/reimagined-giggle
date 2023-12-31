b357c2fbd69582b7d37e2c8d3aba68c0776431c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:54:19 +0000

    open history shrink me support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_work(force, distance):
+        return force * distance
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
