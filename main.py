5fe9581c9d7975cf327b5948a0f37ed9fd9d9a85
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:31 +0000

    aim confident persuade coffee that

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+def get_current_time():
+        return time.time()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
