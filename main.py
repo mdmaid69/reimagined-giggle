f1f24f15323b8a8081fbf13f05829bc42c9692fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:30:40 +0000

    commit wonder pronunciation style wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
