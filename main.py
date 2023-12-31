bd4d4649c5b46bea3b85812806f9042df5dce855
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:36 +0000

    little goup affect content bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
