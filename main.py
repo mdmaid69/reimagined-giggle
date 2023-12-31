e01d8fde118f91078a9b85825eb3ab7ddd90b51c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:56 +0000

    guard wherever save apart tend

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
