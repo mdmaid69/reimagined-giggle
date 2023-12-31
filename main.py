b3b250fef338112737452313ba9ef959f3a495cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:59:06 +0000

    minor warning bang curious gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_tuple(array):
+        return tuple(array)
