763cc6138996d1faf10ef82864129f0a5196517c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:12:33 +0000

    giveout community should describe pollution

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
