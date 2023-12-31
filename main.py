8d770136923f581e8d256d2e4bef82071b4d155d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:32:18 +0000

    precise difference rid valuable fasten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
