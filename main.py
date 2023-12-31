4b39ef338e8eef5fbb11d58f763723b1050abe37
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:27:10 +0000

    goodbye report discover wash top

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import time
-def get_current_time():
-        return time.time()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
