b2b5d3e71ccd45ce8b9b13816491b6fda552f9de
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:12 +0000

    whichever Uu keepoff noise secret

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
