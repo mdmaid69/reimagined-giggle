f73e9bb35c6fe5d6d97a98da21b65062bc6816a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:41:43 +0000

    tear hello share hello Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
