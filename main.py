0dd2822109184b505ebfbe7a8e00149ff2f13403
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:29:20 +0000

    group evidence pick about cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def clear_array(array):
+        array *= 0
