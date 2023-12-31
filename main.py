38f93614952cdf83f224f749855ee8d24555468e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:52:44 +0000

    equipment choose pair considering train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
