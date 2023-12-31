107c5a6a55d8a2bd559aabce6ce2b43310a3fe4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:38:22 +0000

    blame store satisfactory below hers

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def divide_numbers(x, y):
+        return x / y
