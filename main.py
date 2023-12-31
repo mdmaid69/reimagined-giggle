47db8ef8cf179910ad0541af2e7be1333020c1d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:05 +0000

    scare comeout moon turnon myself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-print(re.match("h.*o", "hello world"))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
