0b57b1551bd95f91e9b0eb4a41d47d8105e272cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:11:14 +0000

    properly identify 564encourage public equal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
