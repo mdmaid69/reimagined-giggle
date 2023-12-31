cc152e5bbfa2d7e1339e4fc83250172e81ddc6d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:22:55 +0000

    dress breakout height ilovedicks remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
