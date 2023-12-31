ee03264ebd5ed1e4c267936fdce0fa7fb51c4d57
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:32 +0000

    opinion capital survive confidence flight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
