8830207fef98d427d29b9a9d049331b59e045f92
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:44:31 +0000

    bunch important careful follow past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
