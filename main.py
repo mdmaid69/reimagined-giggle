5d184373e390ee45164d95ee03b516bc738590a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:57 +0000

    Uu object wing jump lesson

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def reverse_array(array):
+        array.reverse()
