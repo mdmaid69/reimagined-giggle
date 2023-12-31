897e77f34913d3f622684c5dec58fe1d0352a806
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:53:33 +0000

    concerned warning mind rather danger

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
