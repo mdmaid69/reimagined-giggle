1716ced9a03ace1012aab5704c89021091ea8994
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:10:00 +0000

    news main recover breath in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def cube_number(x):
-        return x**3
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
