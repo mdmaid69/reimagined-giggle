94df9a6407f75e9ff98afef8b52374e8d8f65439
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:17 +0000

    mainly tired actually expression mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
