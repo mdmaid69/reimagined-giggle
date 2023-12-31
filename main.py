aaf9d6f612f4d396ccc383acfb32cd3379be87c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:59 +0000

    luck disturb on trick sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
