7733bc3a046771b478021826d02a4261488ea7bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:35:30 +0000

    task dance thought ordinary cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
