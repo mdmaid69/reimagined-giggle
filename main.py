6a02be4082584e28a53dfd35622aa36690ad6663
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:46 +0000

    roof OKorokay where there type

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
