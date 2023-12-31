80cceafcec0f4ea0fd922218c361e1b860928531
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:58 +0000

    miss course brain freeze new

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
