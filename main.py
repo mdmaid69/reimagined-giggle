deb88d776fc05dd472972ddc2c2ed87e58a44a2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:06 +0000

    yourself welcome limit table time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
