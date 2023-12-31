6b234112cc84e30e6cc5aed251d5fe89dff8c565
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:04 +0000

    meanwhile content keepoff free willing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def add_numbers(x, y):
+        return x + y
