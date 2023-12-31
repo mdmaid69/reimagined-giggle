ad765d82bb93c962fe72530950724ac1d4d812ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:37 +0000

    side happen precise well create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def reverse_list(lst):
+        return lst[::-1]
+def sort_numbers(numbers):
+        return sorted(numbers)
