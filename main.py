375e6985781d82af3d6cfe6ba92c3434619c40c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:51 +0000

    goout badly view even exist

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
