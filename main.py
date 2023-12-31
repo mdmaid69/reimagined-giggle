271b621a65f8e35eadd9dd96b9b3c61ee2e98718
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:07 +0000

    post ever blind benefit last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
