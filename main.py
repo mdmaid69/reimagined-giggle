efbe087f991d0f3089dfb6663d895328448665b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:00:45 +0000

    measure either decision care stop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
