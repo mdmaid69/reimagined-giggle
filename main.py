bcb9e3f67454a4f2b68a4b7b1629a1d27d1826ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:59:49 +0000

    all whistle various correct firm

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
