5bcec7d5e3c9927e9e5f31c803ce1439409bb14f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:38 +0000

    height lose authority beside split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def remove_duplicates(lst):
+        return list(set(lst))
