a2fb849d60fc2b788478e4a5497d769ab94d444b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:21:47 +0000

    century row borrow arm mystery

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
