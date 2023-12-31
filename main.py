8a3f027fd502d88ff93c1d5f272011f769a07afc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:01:52 +0000

    mine television spare holiday ticket

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_bytes(array):
+        return array.tobytes()
