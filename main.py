37ab2e7c49623ed06f97f03fb972e684dd517d62
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:06 +0000

    light somewhere again smoke since

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import sys
-def exit_program():
-        sys.exit()
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
