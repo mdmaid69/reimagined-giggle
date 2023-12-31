0806c900860cca592f552ca0f2fd69d5beb72cda
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:48 +0000

    otherwise dish die interfere break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
