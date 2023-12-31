24f85579b3e9270436260fffbef530dbe136c5c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:42:55 +0000

    sign recover problem sometimes shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
