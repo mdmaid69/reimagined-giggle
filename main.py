f767a571c8e1702ded5d198b06fb44327142ae55
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:20:49 +0000

    sharp ourselves demand hungry arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
