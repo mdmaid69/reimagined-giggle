b7f04b7676fcd72e0f6183747cfece158862ac00
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:59 +0000

    news church excuse realize rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
