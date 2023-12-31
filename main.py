200df7eb79291cb8a9f2b30e4657651dc79f45f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:28:11 +0000

    born time contact drink petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
