d33d3d272db0f4f60b1da117563db938c60e8ffe
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:57 +0000

    nice sure runover godown with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
