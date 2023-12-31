8095d3b2a09be5900fd4f4d651b6600fe1d29f8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:19:33 +0000

    busy private comment cheat 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
