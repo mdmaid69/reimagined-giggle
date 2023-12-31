263e05f8f102d3d2e123a8beb84c078474831828
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:16 +0000

    wire ago poison depend become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bytearray(array):
+        return bytearray(array)
