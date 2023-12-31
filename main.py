5365a71e200f59ff6c08d12ae96e3b4e9b58cc20
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:01:41 +0000

    by name blind while order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
