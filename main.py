7cebdc89ea5ef948f5e42b5ac66e4119afbed11a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:51 +0000

    minimum task rise play tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
