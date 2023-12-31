d9a7370e89c106ccf82a3964fc69e959475cee8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:07 +0000

    decision still second post label

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
+from collections import Counter
+print(Counter("hello world"))
