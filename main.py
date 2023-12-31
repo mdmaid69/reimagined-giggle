4626de9d374743cdd4add17c6c6b0ea15b11ffa7
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:31 +0000

    treat suck spread godown comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
