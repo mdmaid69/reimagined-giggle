695122112cfabcfb9036f2833f5e8f4a21ea89bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:12:46 +0000

    murder file play disapprove satisfied

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def square_number(x):
+        return x**2
