1ff5da9c4bac08f651b2091f6d5a92faca7732e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:05:09 +0000

    film satisfied already who safe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def remove_from_array(array, item):
+        array.remove(item)
