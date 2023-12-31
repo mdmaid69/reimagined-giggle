8d44c9b22db3b21ea6d3325ec33834aa4de44355
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:49:19 +0000

    179bite last flood lonely rush

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
