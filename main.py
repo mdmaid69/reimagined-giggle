6a1386913fd949da81d014a8ce3ef7c823c9d5fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:08 +0000

    confusing no failure giveout set

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
