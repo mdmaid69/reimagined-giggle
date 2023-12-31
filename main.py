deea35fd2c018f7c161792359c200091644fa4be
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:39 +0000

    rush he shut blame lookafter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
