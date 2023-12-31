4a26330bfa2c088fdaaea0de4c64cbf85bff3812
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:08 +0000

    drink snow measure right speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
