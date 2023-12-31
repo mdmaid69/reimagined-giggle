e430ff472fc7ce214635deb4addd0e3b3bafc8bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:00:40 +0000

    advantage interrupt holdon funny knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
