4cc4b132d8fd7ff668824418a1e3ea17f9fbe9e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:54 +0000

    include cut threaten human shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
