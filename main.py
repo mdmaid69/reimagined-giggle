4afb0cab67a8365cea38d9070a0d22c456fc8975
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:31:09 +0000

    performance natural sweep breakup drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
