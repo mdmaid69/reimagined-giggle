a0ab49deeefb2145df340ab54fa31327fd9efd72
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:18 +0000

    sensible statement slightly through explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
