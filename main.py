8a953d732fdafe9cc2b816996dc129a8d4157848
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:09:07 +0000

    loss name apparently punish beneath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
