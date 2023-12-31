2d37d2185be85ffb81e3d11437fd450483eedd03
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:01 +0000

    steady usual purpose teach so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
