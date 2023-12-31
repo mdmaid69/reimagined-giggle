d4c98dc82adcc4ef9a9286c7b16fb6c83bdba2df
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:14:54 +0000

    wild giveup workon model force

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
