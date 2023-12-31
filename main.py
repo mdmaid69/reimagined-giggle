a463fc891ef4aa4e885fffdfe871d6daf26da51a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:50 +0000

    breakin punish mistake yes broad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(5):
-        print(i)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
