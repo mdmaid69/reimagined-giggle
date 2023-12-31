a86a49d49c9cf7161f9d1b74310524839135dc86
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:46 +0000

    creature free change ball circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
