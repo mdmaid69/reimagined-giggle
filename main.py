a4b5f38eddae39f06d8fdce3eca2a034979289c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:55:16 +0000

    peace ride shopping lower plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
