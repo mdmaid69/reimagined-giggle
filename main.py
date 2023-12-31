d5c10fe030b8e9a122593a7fef0c998f7e0023bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:36:22 +0000

    attempt prove pretty list getaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
