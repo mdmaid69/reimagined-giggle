56394f04606546379c57c888bc1f01067b2554d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:20 +0000

    sort develop suggest matter imagine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
