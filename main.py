32bb7ca97a47bbcf6bd9e9a3f876eac961badb55
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:06 +0000

    way rest urgent getup pronounce

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
