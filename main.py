d6f2fd4a09d6cf0ad013e6a2b15b729ea125041f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:33 +0000

    criticism swimming wander slightly entrance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-name = "Python"
-print("Hello,", name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
