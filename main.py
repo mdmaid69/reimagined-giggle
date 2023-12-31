64093de8840b2112068af35b61b1d8b41d178531
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:44:16 +0000

    goon become tradition corner town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
