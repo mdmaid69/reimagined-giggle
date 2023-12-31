1c2cd8fb9f8cbb75442e54801db7998a6a775a4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:53 +0000

    show move waste sell breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
