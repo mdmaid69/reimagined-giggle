f025f4c1095a62c6237d1d6ddc511d0ac4e1b682
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:48 +0000

    cough powerful conclusion pupil refuse

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
