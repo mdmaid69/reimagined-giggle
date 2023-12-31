3801d417944965e494cd0e4c0797b8013a9ab5a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:36 +0000

    shoulder listen corner threaten blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
