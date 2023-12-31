9932280e8833ac3228a71130f5dbd0a06bf5a15d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:21:26 +0000

    sort since pretty rubbish takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def is_palindrome(s):
+        return s == s[::-1]
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
