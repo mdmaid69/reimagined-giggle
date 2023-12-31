eb68ee303572e08968d94163b3a5b5a122785988
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:54 +0000

    soft break heart onto eye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
