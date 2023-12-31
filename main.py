6e4a5a1ecf5ea1a022f16e768495ab6685b7415e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:32:33 +0000

    breakin contrast action fault thing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
