cf92f4a16d4df48da7a70833afbf7862a62a2f08
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:52:45 +0000

    second large close shape convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_list_from_array(array):
+        return array.tolist()
