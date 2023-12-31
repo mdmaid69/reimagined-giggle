9fdfbdd0f36be2593614053048dc2cd7a52cfb4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:27:11 +0000

    rather luck no politics perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
