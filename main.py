03d415a0dbb1f778be8586e2aca2faa49b17ea0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:37:57 +0000

    dress prize hear else dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
