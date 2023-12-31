d9f9a0c057398dcd8b10dcb390da0cf10bc40a45
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:03 +0000

    time name Yy badly him

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print(sum(range(10)))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
