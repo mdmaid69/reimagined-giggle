b9a4fcfaf65ed1297f080362a92ef8fd9ed55765
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:24:32 +0000

    treat communication talkback appeal software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
