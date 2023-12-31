fe5f8c32097aead6da5223c1fedf97e0dff8aad4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:41:48 +0000

    definite pride soil getout entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
