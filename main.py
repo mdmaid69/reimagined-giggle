2c7fd9d187142e0245c4ea98409187fea9f23ea5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:06 +0000

    back obvious shape skirt comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
