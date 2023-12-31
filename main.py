6e6a9018cbdc156125fb910505e4621d05cb9e17
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:46 +0000

    them trade touch crime principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
