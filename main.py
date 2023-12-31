65d53552ed90da351a2b23f56c2fff2620f4aa31
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:42:08 +0000

    future flavour bin tongue build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
