1cd8551a4e5549c613dc9927e0fae9f5f95a123d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:37 +0000

    freedom deal promise combination repair

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
