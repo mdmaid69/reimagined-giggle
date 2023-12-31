016207cf4219ee37ed19aba85577ae255032a915
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:43:47 +0000

    pot impressive play seriously sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import json
+print(json.dumps({"name": "John", "age": 30}))
