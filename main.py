9576efdb600a382cbb6dec03d509bafcb994e24b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:26:42 +0000

    office in pin thirsty steam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_odd(n):
-        return n % 2 != 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
