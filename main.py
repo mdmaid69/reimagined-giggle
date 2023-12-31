90dfb615fb14170989bb3ade3832dec5f902c22a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:13:23 +0000

    whom persuade continue rule traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
