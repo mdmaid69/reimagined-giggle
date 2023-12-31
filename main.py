ab507e12b9025b82932f47e49debd9caa772fa23
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:50 +0000

    unlucky risk inside market majority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
