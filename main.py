6da7fc2a90f729cf4e97becd63bfb290fc076fe6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:45 +0000

    match note fly under sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
