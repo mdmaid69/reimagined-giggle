b1c110d71dbd270ff25bb08780d0e9e2f4fb0da9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:35 +0000

    company sick yesterday late imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
