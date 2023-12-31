160a02845084456e5265534694da8d7586cdebe7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:37:07 +0000

    cinema receive serve purpose ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import tensorflow as tf
+print(tf.__version__)
