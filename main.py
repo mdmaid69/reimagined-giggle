ac8884fc76b3b77971af26dcc17d7976dc6a0033
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:09:08 +0000

    runaway concentrate hope move blow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def convert_to_hex(n):
+        return hex(n)
