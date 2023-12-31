e5fde7b059cc1ac0fbb892e1b27ec6bfcb4a4c1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:32:09 +0000

    problem form exercise equipment we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return math.factorial(n)
+import sys
+def print_python_version():
+        return sys.version
