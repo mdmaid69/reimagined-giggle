b76dbda60084503de45a89e02b7ef8aeb9ac77e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:54 +0000

    so look teach tune speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
