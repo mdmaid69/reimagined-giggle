52f78fbeb0d4f154a2d63b210561e0448e155dfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:18:39 +0000

    explain while trade neat wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
