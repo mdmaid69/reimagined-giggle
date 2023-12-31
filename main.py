7ebe403fe09f7747323e59b0bcb5ba13dee75673
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:21 +0000

    place of notice protection seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
