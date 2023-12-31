ed0d4815e7f4c4d55f5462050aaec028054be679
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:58:29 +0000

    brown confidence frighten snow though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
