4fe94f7c81bc182504cdb975b3bd3a9d4c495749
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:52 +0000

    close through month combine drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
