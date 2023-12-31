cade180aa51c7a685413f66546503dbdae3d996a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:07:46 +0000

    answer they maybe make next

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
