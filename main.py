5be44f8bb25cceebde128654f7fde8c021d4568e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:26:34 +0000

    568energy like lookafter hurry wear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+print("Powers of 2:", [2**x for x in range(n)])
