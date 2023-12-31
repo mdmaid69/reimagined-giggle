21655af1ba10bef3bafbba49078b29a86e2c66f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:10:27 +0000

    probably read along what tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
