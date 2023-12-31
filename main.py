66bd347d41c39ffcff9073556245d68fc4011cc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:59:52 +0000

    previous on until steady bus

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
