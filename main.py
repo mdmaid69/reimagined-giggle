7f6e8092e79b07da16068198f7bd5275352faba8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:09 +0000

    godown so noisy wipe chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
