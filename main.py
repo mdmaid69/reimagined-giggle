230d79c6ff5e7e1dc8cf37064746573d99286856
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:43:54 +0000

    here smell people form fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
