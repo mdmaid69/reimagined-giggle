c41af07006fb2f64cf571fb6af342440fc0880f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:18 +0000

    birth increase separate cause mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
