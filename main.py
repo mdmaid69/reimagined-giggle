e08439f05177c156b066ff60e769da897d0c9f91
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:29 +0000

    mad bother mess gift positive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
