abcdf7bcfc709dc7eddf7b87a87388855e643bca
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:03:55 +0000

    noone another high sale respect

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
