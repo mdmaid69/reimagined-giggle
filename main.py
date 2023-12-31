d4225287d667270b5b6af7b8c85b9f0ffbbdb636
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:58 +0000

    correct match classroom nose whisper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
