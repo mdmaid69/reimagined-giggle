f0ff8d32bb3e8cc758a0aefc68ba09eb0d6680c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:44 +0000

    rub stay rest divide card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
