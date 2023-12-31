14ef5a7f0aa8ed36de8246f83c43516c2da99fcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:41:49 +0000

    body community prevent record include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
