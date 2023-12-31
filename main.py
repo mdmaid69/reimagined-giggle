517e28b7d3dccb3a17e81ccdab986e94ffd943cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:09 +0000

    snow glad join usual now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
