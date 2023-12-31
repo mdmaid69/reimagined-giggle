b9ecc6d90470cfa51d9f5dc0432721d0a5bc6c3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:55:05 +0000

    strong impossible conduct minority room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
