e85176d61100b3c51c1c8655731b84f36c6b7d7a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:49 +0000

    jump spread watch problem getaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
