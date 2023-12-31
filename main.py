3a418afc90d635bf58de33d6ac0178b8f6afa601
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:28:57 +0000

    sail talk uponon fortunately continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
