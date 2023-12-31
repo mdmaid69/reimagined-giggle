d475ef33dbf4314e81828b919a383e3e340b075e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:27:10 +0000

    reduction order radio true jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
