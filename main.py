5d77a03cae32530d78c13b4a16ac6735cb40fb7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:42:40 +0000

    squeeze test physical development treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def greet(name):
+        print(f"Hello, {name}!")
