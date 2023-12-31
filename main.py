7c24ef074f55f72c228afe9a98cf1aec1ee6b3b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:04:24 +0000

    sound discussion government comeout pot

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+print(math.pi)
