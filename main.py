70386d05c0c35dd5a456dadb97b54d695b445e1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:39 +0000

    peaceful between apply either tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
