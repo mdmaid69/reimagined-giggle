5749a0109b33a341ac3ddd3a90506155b64d6028
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:21:43 +0000

    step busy breakdown mail once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
