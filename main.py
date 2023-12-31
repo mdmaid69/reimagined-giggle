3ef7a066e73eb45d33a14a6fd6895a0958c3df93
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:33 +0000

    cheek order comein dream new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
