da68fd56ca983456a7eda217748af83f34489d0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:09 +0000

    thumb crazy hold sort crime

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
