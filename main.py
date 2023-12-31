e07d1fa900205e43553179da9fd79e070ec22a8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:24:01 +0000

    blame hair decision not interest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
