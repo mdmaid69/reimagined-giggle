e3eed80cb5451d8fa8f199a664350c82fb1ba067
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:07:44 +0000

    plastic yes home face sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
