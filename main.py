add277c042fb903f0950348a3cb1f2dfe1eaa5f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:06 +0000

    pride practise his finally me

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_speed(distance, time):
-        return distance / time
+def clear_array(array):
+        array *= 0
