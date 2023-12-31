78af9ca7b8a16870d0ebb0a149987832d79410ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:55:01 +0000

    list he delight flavour poison

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+print(sys.version)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
