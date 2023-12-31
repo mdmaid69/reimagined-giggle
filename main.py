f686aa3b2194f44ec22590c12b579d2b3d1fdf40
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:58:42 +0000

    yours corner warning male cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
