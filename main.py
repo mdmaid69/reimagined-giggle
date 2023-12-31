3db2a5ad5ea4dceee8571b4b00b25b7b756a965f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:52:02 +0000

    takeback lay reject future hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
