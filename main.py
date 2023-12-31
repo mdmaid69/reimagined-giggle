51887ee48cada1d3b4715d06d3d54b699c4d3e38
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:22:05 +0000

    turnaway grade job circle middle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
