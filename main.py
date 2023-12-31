b803e605f11f58cd9ba970dfe362a73d3b65c45f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:12 +0000

    last owe hers Dd machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
