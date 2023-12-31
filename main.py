6963eb7b0a140348374685cb089ea455a77577e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:14:19 +0000

    connect ahead glass sharp medium

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
