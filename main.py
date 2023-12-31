8b0708bccc346c596c7589ca831cf92efd8d0b94
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:59:04 +0000

    from press finger worst example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
