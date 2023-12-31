eb3801ebe3fafa4bbc9020042a403340ba52a356
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:08:54 +0000

    party intelligent shallow float clue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
