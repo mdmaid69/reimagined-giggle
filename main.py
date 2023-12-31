7ca8a8d1b8a370598f10e115bb9cfa6d80e6cf7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:03:51 +0000

    when once plastic prepare plant

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_list(array):
-        return list(array)
+def cube_number(x):
+        return x**3
+def convert_to_hex(n):
+        return hex(n)
