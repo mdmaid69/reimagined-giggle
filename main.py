8e7923dbbd666103981fc5ea0d4d6aa7ddbb805e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:56 +0000

    child accident keep upset people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
