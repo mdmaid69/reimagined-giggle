e9439c80d9c64fbaa349f28b1b501e4b40a8c0fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:13 +0000

    cruelty advice pay involve original

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
