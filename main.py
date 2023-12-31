ca15bc59de5ec0bc1864e60d560f360d06490a2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:46 +0000

    admire example contrast appeal contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
