953dab25b55ef7d3fb98800b6b2d35ef5de85174
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:12:52 +0000

    call week effect compete kind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
