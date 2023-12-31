98a98992c0f271cc537e58cf882e4c9f04d5210b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:05 +0000

    straight afternoon interrupt father dream

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import re
+print(re.match("h.*o", "hello world"))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
