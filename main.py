863327c087f1fa44ad990e8508bb83866d14bc8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:13:39 +0000

    stretch minority less table remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
