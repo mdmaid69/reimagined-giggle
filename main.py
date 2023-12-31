8e979d74998ba228b5a78dabcfde62b7d64e1186
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:06:07 +0000

    like opposite other bang often

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def reverse_array(array):
+        array.reverse()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
