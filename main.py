8d11993d17c4b58a4c1f26c84eee9475d60e3042
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:30 +0000

    rich poor on train apply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
