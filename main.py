f29eafdeec4c66f726d5cd108f66427cba40b3de
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:21 +0000

    bit punish fear whoever information

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
