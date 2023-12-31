fa73b4d1bd332cbec1202fd8872759b7405890cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:00 +0000

    impressive wall rescue progress conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def find_max(lst):
+        return max(lst)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
