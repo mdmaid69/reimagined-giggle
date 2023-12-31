bd39c58a0ff8314cad748e8d7d09743e34c24c8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:55 +0000

    fail make rub bound estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
