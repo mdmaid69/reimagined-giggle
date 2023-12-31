6cce775312d624b93037caea5d482288c7435ae7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:02 +0000

    value tense flood any beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
