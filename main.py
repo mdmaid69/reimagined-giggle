681a24f821b4b4163680c23ca3055ab3f87596e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:38:42 +0000

    accidental scared dead rest fast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
