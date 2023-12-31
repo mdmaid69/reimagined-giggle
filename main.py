dd20632607c0d26d69a63b5f411c782a39e4d9dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:30:54 +0000

    book often care more interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
