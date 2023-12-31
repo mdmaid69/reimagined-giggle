2d53aa18af29d05b2d5d10727656f51c80a90305
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:54 +0000

    family official brush within wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
