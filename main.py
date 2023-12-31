80d94cd3e71cdcc8668c3cae19cb895b372552fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:38 +0000

    land hurt disapprove ashamed she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
