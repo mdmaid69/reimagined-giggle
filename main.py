d62902df2c4078e192776451b5a6972e1fec82a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:05 +0000

    colour sing circumstance mail plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
