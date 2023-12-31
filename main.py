63bcdafe032e891758898f63472caee10e0fbd7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:41:55 +0000

    breakinto without immediately waste exercise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_array(array):
+        array.reverse()
