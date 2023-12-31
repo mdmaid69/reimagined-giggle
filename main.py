1019c6b6396b2123c99fb9a396bac8a26bfe3a6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:16:25 +0000

    perform mention identify later seriously

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
