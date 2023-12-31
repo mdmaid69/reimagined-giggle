c1253267542b8e2509bf95e48ed1543e0829686b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:41:29 +0000

    free concentration appeal content at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_density(mass, volume):
+        return mass / volume
