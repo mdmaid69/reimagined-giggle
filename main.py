79543ae6601c5971bc64ed56e0d74523862f40b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:17:04 +0000

    under reasonable edge increase unlucky

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
