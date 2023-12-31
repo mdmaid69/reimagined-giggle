c689b048b8f1750d83ca096341b3aae3110a79b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:04 +0000

    Dd smoke interrupt low word

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
