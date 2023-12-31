6b59d1a648a02a9c0eee8b214e66e7c943ec7174
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:04 +0000

    Cc workon visit start performance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def square_number(x):
+        return x**2
