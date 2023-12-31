6de63633016fb839d7bfbe6a371bf21f973cdb88
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:14:20 +0000

    swim front speak begin towards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_power(work, time):
-        return work / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
