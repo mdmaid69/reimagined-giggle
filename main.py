3ff24507a2d206f2cfe1f14745dbaf1c51e68b73
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:50:16 +0000

    tape inside sudden commit embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def convert_to_hex(n):
+        return hex(n)
