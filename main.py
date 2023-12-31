65e099b262d1a4394dbbcad994fe98f8d24178ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:35:42 +0000

    expensive suggestion ring cause course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
