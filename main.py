5f070c2642f651a17578772ca6148fe9cffbca92
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:58:17 +0000

    goaway human myself relation spot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
