3482577a8d92a2093f69b545a10f5f1a6c713a0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:37:38 +0000

    real space excuse close frame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def cube_number(x):
+        return x**3
