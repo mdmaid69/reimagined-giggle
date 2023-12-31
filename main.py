20119c1f635900be721850f0ddd40e60968cf423
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:04:07 +0000

    through store chase fierce thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
