43e72a9b86a55681a7888275a07a05d4221a41f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:31:34 +0000

    pure within influence thick health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
