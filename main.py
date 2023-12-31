2243f9362217aab987e0eed1c0ec59e25576f890
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:08 +0000

    backwards move apart door morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
