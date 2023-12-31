ef15a490a4a3a33c6ac1811c2a40c6342d01ece4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:25 +0000

    cap while gap exercise just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
