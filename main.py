34e0109c4b474b393cf7b67b4c21f9a76828454f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:29:20 +0000

    shopping instant affect very organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
