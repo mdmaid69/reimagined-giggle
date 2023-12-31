2271bbe32a829e08b5776e0c3b36baadbc5904b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:38:42 +0000

    bar regularly truth become hurry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
