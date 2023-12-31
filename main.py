ba5a4348affddd6735474e47def279ef26553756
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:00:35 +0000

    usual less hers until before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def count_characters(sentence):
+        return len(sentence)
