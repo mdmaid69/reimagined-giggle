495dd8af04513402d433629a747024adc43415d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:48:08 +0000

    crowd room cool rich defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
