9e7043325af625302555246b171733fa9dd8129c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:44 +0000

    block lot proud consist habit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
