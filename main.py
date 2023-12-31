3ec90fec53c1706e43a5cb410c55fd9d11e6ed5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:44 +0000

    deliberate teach exchange step dig

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
