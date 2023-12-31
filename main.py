c3e85fa8ca8ddea4ed12de4630ec0522cbdcf39a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:53 +0000

    suppose become guilty personal forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
