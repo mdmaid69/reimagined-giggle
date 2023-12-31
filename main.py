f723de85cb8e9bb9ee9e4e013b19a2e2b739b961
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:58:27 +0000

    situation traffic minority formal either

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def convert_to_binary(n):
+        return bin(n)
