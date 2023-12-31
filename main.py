8c04867800dfa1c0dbc82c7372f38361a3f0c895
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:34 +0000

    tape play down beyond connected

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def clear_array(array):
-        array *= 0
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
