1d9e6e6545124b80c8d34fce1f7601b4553512e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:34:04 +0000

    positive trap card fail wise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
