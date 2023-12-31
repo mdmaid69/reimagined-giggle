db4f43de542b88d8ae4bf3e9b82633fb0b056b6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:40:29 +0000

    niggaacademy show town on dull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
