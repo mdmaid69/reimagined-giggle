645d341518f134a1b51ccfd3ec9f0424139761e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:01:32 +0000

    return brother could goon culture

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
