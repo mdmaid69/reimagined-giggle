09345ae6ee7ee8a4bc4e39587816edb599fd6069
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:17 +0000

    giveup path switch manner glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
