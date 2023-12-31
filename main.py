aabd18e81bd678e57e545ea41ca11590eccc6171
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:58 +0000

    there unite problem public division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
