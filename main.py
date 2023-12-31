898cfc6b44c4cb74f95aa5775eb3a09f288cde96
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:21 +0000

    sentence medium clever doubt cut

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_power(work, time):
-        return work / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
