2bb1bd4dd55adf3116ada13999eba74c2983829f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:04:22 +0000

    raise Ll last thinkthrough direct

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
