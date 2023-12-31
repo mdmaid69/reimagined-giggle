22cf9461e7ab7b95cfa4281c35dce29a089f4efc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:51 +0000

    address event use through test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
