97f6820e300ed6d4c3802b1f7e886ec2cea5a773
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:19 +0000

    food I agree shock soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
