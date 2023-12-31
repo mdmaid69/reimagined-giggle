3dcd473d5b229b87edff217ac2a746cbe1e8790f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:03:11 +0000

    lead soak skirt wash treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
