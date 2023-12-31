f4ac4e62a875a2820527287bf632f3f61960721d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:58:21 +0000

    anywayanyhow knowledge spring replace unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
