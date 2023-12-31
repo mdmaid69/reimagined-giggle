981d6f42f1dba762c20072dc54e54245e430d9cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:24 +0000

    may woman nurse life straight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
