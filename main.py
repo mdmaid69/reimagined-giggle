5a94e176bb7de83d1a73e7511d4283b8db8520f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:16:41 +0000

    embarrassed come also through if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
