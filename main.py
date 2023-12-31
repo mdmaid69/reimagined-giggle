c1079dedc15da8358785ed3d7bc9b915ccb50401
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:31 +0000

    slow now active suit lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
