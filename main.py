bc0272236df301bf12c35cfe6012a393d3cb9038
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:38:16 +0000

    trap drive cause support suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
