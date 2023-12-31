10519fb53b3e6c9d4dcc7730fa34e07f0795175f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:08 +0000

    scared attention impressive geton speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import sys
+print(sys.version)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
