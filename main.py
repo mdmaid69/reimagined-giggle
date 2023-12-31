38e17c7aeb3f644ca9768ec7527abf8dd193a20d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:54 +0000

    work cover female calmly game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
