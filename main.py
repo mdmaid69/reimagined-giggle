6feb251a0c71028587ea9b43358481b133662baa
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:45:09 +0000

    church climb week behind birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
