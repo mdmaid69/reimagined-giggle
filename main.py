b9e9ce2b86a016678490f7b28daec5ab5b5c1d8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:38:51 +0000

    majority tall nose flow half

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
