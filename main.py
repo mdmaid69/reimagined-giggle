e9eeffe533fd585bbb800ee6b36322ff2360420e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:07:39 +0000

    goup better keepup load once

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
