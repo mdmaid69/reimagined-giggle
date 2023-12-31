52363c9fcccb6ece54c29cdfb5575ffa98c15943
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:30:50 +0000

    pretty see correct unhappy actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
