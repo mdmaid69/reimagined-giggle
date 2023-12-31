ab744f1fb9bd240fdb1516cb967dc7b2b33b46e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:07 +0000

    final fall mixture drive unlucky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import sys
-print(sys.version)
+  def convert_to_hex(n):
+        return hex(n)
+def is_odd(n):
+        return n % 2 != 0
