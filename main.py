b7bb47dbae3189bc5533928ed54dbb85fbfd7b06
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:09 +0000

    look basis wake get mistake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
