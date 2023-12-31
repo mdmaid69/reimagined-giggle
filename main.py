76997022cb57547ef10a75cfd4861949b8cd19bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:51:17 +0000

    admire band convinced feeling goon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
