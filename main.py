e9ab9b0a53fbbd18e3c1479b14f8ab9164888a89
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:17:09 +0000

    learn man suddenly bang impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
