d299ee3311d9cb2afede8651903653b9066a83f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:30:15 +0000

    round evening successful easy usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
