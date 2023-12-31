66dc850866d3c2b9997c2693e05526f3669b736e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:40:24 +0000

    escape from switch wonder cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def cube_number(x):
+        return x**3
