3ff7eea396e6987835066674040b61d6dda71101
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:50:23 +0000

    serious steer lookafter order tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
