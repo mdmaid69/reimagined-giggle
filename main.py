926e773d763d6d9ab6dd501819bb550ce1d11228
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:13 +0000

    over Mm chin swim somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
