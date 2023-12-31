58cb8ba0f00a7e6d5a9eb07f6aab6e5138fd520b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:41:42 +0000

    wall smart turn taste further

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
