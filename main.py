12e137d8279c03411ef62115035c206258aa30b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:32:14 +0000

    water lie here present unlucky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
