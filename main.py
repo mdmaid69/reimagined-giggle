a8da86b5c98dc2eb61972ee24afe27c29c711671
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:33 +0000

    drive stick well from temperature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def add_numbers(x, y):
-        return x + y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
