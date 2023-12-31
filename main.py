5c8e66e6b2fcd6a5364ea981c393df9ba4b29629
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:49:48 +0000

    sister worry thick pay touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
