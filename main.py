e505fa7b286536dfc6700a0948af39de96b5d857
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:04:01 +0000

    total loud table emotion warn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
