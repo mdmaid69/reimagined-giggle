75ced1a616b74f393942f69e99caa1549dcf1244
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:33:21 +0000

    holdup cut pressure basically frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
