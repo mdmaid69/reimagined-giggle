6a9475397d6c50de5a9d03e39844f154d394da1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:43 +0000

    ago door off trap wish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
