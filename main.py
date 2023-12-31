4e7adc4073eb633cc31fcd0b01325761cbdc1ebb
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:12 +0000

    interfere deal opposite share new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
