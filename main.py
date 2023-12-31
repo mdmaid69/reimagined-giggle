d8c3c2bc3599e35e69a04a2a2556aea841e7c277
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:48 +0000

    best sort mass instead answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
