7138aecd7aaecab489ffd79b3440055ac4a4e93d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:00:13 +0000

    check length wave mean healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
