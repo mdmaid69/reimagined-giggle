586502c833de0fb98a07a04f954655b4fb4d7a57
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:20:37 +0000

    anywhere fairly fault emergency difficult

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
