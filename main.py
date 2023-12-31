41b79ce773858715c555d4465d9bd47fdc6a45f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:02:47 +0000

    rank react different noisy receive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def get_array_as_memoryview(array):
+        return memoryview(array)
