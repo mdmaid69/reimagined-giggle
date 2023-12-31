d190a41d045ed673bfb44e7909b936897316545f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:56:20 +0000

    picture demand equal look around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
