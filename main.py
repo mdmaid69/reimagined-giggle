e79fea37dc95a7e6eca6b70e69feb964bec79e7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:01:52 +0000

    spill instead intelligent strict duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
