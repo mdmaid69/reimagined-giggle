0b098346e1673f93588b6859633b30a420c491a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:33 +0000

    smile help instead no seem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-print(time.time())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
