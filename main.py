fdc61ae66e2f879ade64d5e3c0019fbe8f4ac8e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:36 +0000

    emphasize Ss tonight punishment conclusion

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
