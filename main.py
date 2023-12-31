97d5f680afdcf93b3ab41a12f5d155bdc97eaf33
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:01:45 +0000

    intend pass characteristic agree damp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def reverse_list(lst):
+        return lst[::-1]
+  def convert_to_octal(n):
+        return oct(n)
