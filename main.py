43e561db970a9e159c10c8d6c034959745394d74
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:59:30 +0000

    action past getaway bend fact

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
