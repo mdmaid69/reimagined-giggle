39dbc7eb9356c0cb47f3d954745bc7567498301c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:21:48 +0000

    clean disgusting tape circle help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
