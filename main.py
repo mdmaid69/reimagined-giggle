23d389974c67446502250029f1ce248ebd700e6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:32 +0000

    although keen aware relief take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
