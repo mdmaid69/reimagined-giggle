3d72c4f719891ff8fd87402afdeede10742647b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:39 +0000

    when meeting neither proper man

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
