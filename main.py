9ed4d7b303be8f20d2f693a59a1b4adcf5c770e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:18 +0000

    work sharp imagine mixture cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
