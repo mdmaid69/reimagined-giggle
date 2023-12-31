6593dd54608b2c4156972f0c252dac340fe24e68
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:28:49 +0000

    club rest actually profit identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
