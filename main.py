e2f4c0995fcef1de46e434d63e216b38b9ccbf51
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:39 +0000

    exaggerate argument goout computer hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
