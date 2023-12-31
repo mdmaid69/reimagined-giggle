8e5dbe3c078dc7f68bef7a526726fefb7ea2016a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:07:29 +0000

    organize some nervous chin upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
