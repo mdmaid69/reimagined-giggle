9ee8d03d5c92fd2d51c6c172435da7770c769cb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:33 +0000

    speak rather bear trick soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
