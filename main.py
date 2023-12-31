03d54ff50d1d6c4f1733e250c794d00a64b71a2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:21 +0000

    cross loss similar 573entertainment type

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
