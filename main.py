276e59ecdd3618f6ef59050f3393fa1504713116
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:26:36 +0000

    turninto transport soil fear lesson

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
