9f326e5916c0989f3068834298f8039b834eb91e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:57 +0000

    whole cum nor sorry quickly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
