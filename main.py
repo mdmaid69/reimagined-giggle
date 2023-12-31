129b9edbabd980cd4169054242de41e6d24f62e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:22:14 +0000

    present bag interfere bridge argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
