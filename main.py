ca64d44f4b098af31cc558c8f09af8e31beef9eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:19:52 +0000

    frequent switch than forward pity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_work(force, distance):
-        return force * distance
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
