1d1b4a54b3397ac723ff4b26cd2a4793a5614831
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:34:29 +0000

    correction again every opportunity choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
