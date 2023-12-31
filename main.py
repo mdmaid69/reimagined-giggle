e4e38603986aa29e31163407dac5dc0a24f5bc57
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:20 +0000

    centimetre all mark club on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
