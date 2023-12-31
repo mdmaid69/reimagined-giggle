a1edffa83ce45bab2c62e84f8521e05d2351e0d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:34:28 +0000

    walk carry along left opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
