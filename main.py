e8156eeac0a480e4a5b43a9663505e9a92c98336
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:15 +0000

    season achievement hair refuse book

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def square_number(x):
-        return x**2
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_power(work, time):
+        return work / time
+def calculate_area_rectangle(l, w):
+        return l * w
