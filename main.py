4f2154be47e027ec9eb33182cd507cce6d4b71c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:57:05 +0000

    pressure tone before wing but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
