4fc39836df2febd8c0afab556dfa06d5bd6f054b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:59 +0000

    Uu comefrom guide cost size

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def exit_program():
+        sys.exit()
