e295a97d513df66cc966d5b7b2e6b693e984d059
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:00 +0000

    ground touch love night half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
