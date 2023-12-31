23f3ab07830f49cd1251bba9653030bf3df24add
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:15 +0000

    himself phone development correct contents

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import re
-print(re.match("h.*o", "hello world"))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
