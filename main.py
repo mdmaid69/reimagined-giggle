d45f83af16aeb880c86177da851bd4583dbd9518
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:32:47 +0000

    hit excuse bury deliver recent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
