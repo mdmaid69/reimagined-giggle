0912bd402b54b90add69bbbb4cb8c365d9564480
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:15:58 +0000

    factory frighten convenient climb minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
