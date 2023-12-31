24c340d39cd722c8f372e18510274d5e08b7b368
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:27:08 +0000

    fix them computer gentle steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
