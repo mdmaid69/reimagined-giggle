aeaf92bd835d57368ac1aec1f57ce3808f841241
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:19 +0000

    relax comedown crisis keepaway eye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
