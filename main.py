2b45f945f4c10c1f6182c3156aed27185d059f0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:17:22 +0000

    motion Ii interrupt light arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
