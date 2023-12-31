6de5b0a9f145ae743827b4b856a1c9e8d81f85a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:30 +0000

    radio lower powerful bad yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
