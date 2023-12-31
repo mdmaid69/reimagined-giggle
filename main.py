7fe23adb42a8ff845efc6e731b6af82005240fe6
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:26:21 +0000

    manage yourself bread catch outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_as_str(array):
+        return str(array)
