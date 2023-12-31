3d4fb1c653510b287b192b1b1270e90d2b4c8acc
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:21 +0000

    good proper large expression matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
