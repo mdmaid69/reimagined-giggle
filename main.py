403ce58d7d81cf0b2197fdb32f5b26ce5c5e9451
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:41:10 +0000

    across class real wait belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
