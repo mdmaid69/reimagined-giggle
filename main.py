eaeac92c283f724c4c4b4dba4d2962b8dba891c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:00:24 +0000

    know decide play plan ring

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
