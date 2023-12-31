f250def594152b5e26b9d0065ec48c8a91c379f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:20:45 +0000

    why total power value wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_length(array):
+        return len(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
