d2996e3d096b22a50cb271c59ceca64de86af97e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:02 +0000

    flat land platform loan legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
