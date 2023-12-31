96d0ac031af9cf493f37eb193b4a30aaed026402
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:58 +0000

    floor combination whether letter reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
