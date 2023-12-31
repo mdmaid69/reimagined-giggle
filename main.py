368856f10fd08e99d6d2c379021cb2bf46641450
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:03 +0000

    disappointed easy consider power slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
