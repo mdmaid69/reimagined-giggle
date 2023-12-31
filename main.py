7c8aaf395842792225404757d66f9b3e9f2ac8a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:39:37 +0000

    beginning hide meal wet face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def get_current_working_directory():
+        return os.getcwd()
