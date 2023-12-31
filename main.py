4bfe0835b3fc64f57b4379dbe28aaf476f25d57b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:23:27 +0000

    spill admire of loss some

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
