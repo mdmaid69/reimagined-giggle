2e0100c69ea9f7ce0c1eb7781abaa8e3271edac9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:07:28 +0000

    abroad statement project meanwhile borrow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
