7b23bc62bea249660fe9f905bedb2e130538b149
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:57:30 +0000

    shock also traffic challenge target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_square_root(x):
+        return math.sqrt(x)
