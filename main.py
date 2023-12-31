1182baabfca752a4f51559b43fbf7ee03478d638
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:16:39 +0000

    official and weigh imagine ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
