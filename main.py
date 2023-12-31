a59a5d439c44567a0f9f6796e0f36cdc6c8d19a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:42 +0000

    blame time arm cover replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_sign(x):
+        return math.copysign(1, x)
