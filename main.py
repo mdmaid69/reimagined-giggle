7811d4114697b9e42f2c4aa9317cdf0f33ade850
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:06 +0000

    free should turnup make father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
