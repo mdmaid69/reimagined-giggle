a7028054f8a77c68968c4e6484b1f082242bfc08
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:40:53 +0000

    sweet offend bottom gather extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
