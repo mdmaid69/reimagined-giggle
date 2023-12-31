f5524c08bf6cd72779c6cd3e6cb24f19732110db
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:04 +0000

    target how visit Ww sister

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_user_list():
-        return collections.UserList()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
