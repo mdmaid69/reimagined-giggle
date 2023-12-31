b20e5487466b6e4168a19ea62f01ec4dbcf10796
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:54:48 +0000

    less closed his mess occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
