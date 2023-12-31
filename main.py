bea86052ee570e3cdcb8987ec8d1f1cb0529a752
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:39 +0000

    yesterday gather father group friend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
