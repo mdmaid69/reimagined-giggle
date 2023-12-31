063e054093a4044032fc271b1d5e57cf0ba385fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:35:38 +0000

    somehow club tip passage transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
