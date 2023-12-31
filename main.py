0d57d7fec7be6c32c32334ac72f179540954592d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:15:21 +0000

    tax connect track also branch

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
