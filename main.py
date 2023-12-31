fd93838b7b381a6dd2783e4c23c13aa544bac786
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:37:48 +0000

    achievement except various profit achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
