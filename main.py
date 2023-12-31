ed0524d5171530a09b0e4ee382b9a8f9e481ec30
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:57:27 +0000

    disappear advantage me few through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
