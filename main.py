77b90cd4ccaf0108d73be67edd52324fb55b5a49
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:25 +0000

    burst likely ball 572enter safety

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
