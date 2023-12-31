7f4763e9718e476e916a44663c6c640a5bc78cae
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:50:43 +0000

    escape 572enter relation risk clear

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
