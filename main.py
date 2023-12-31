432557fa0688f1ae671ac1918fd7d82095d2da9a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:56 +0000

    fast workout couple convenient heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
