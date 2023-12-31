cc009c0c460c623f31754b46016d46210db50d6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:58 +0000

    actual equal sure something practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
