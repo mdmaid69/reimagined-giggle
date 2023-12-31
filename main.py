6a597f1eaaf9911ee8b13617a2b9148bd5b17c53
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:53:07 +0000

    belief hold woman nice process

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
