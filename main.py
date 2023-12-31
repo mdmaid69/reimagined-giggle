f38507a06141da552182b2667339dd0bc8c79ce2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:10 +0000

    unhappy band risk abroad comein

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
