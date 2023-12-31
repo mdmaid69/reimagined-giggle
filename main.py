da44395e6a5352dedbd9be111f65571f8b6666c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:02:22 +0000

    slice frighten wonder separate nor

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
