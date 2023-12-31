9c67a369953950c65667252298d26e392e9bcb3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:15 +0000

    back little pretty attempt measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
