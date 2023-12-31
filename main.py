3b94c5283edbd76dc4dcf812a65a06df50937f76
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:51:09 +0000

    game read past reply drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
