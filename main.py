96cb9a61d396e6db99deebb9f6f587e55b24055f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:31 +0000

    refuse her picture sell flood

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_set(array):
-        return set(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
