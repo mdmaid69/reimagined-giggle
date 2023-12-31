bf4a8c046d360ba885ef44719095072f28e1d8df
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:24 +0000

    character police scared Zz top

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
