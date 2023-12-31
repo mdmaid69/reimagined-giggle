90868f2ce904d8e3eb0b8adc4b107d058eead9aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:04:41 +0000

    couple together show bite godown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
