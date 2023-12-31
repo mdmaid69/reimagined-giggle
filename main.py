cd0e28eb813cab4bdedc32ffe262fdc006f8cc94
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:13:44 +0000

    top bread wherever blame support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
