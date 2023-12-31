76f75cc4da4a3718b7d615d0a42c4771dddcac54
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:18 +0000

    weather tooth generous 564encourage gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.ctime()
