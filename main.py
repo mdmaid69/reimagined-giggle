f046bd6b8e0b5dc7e3f09742513ee76e2ce0ce2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:46:10 +0000

    kick spot regularly match comeout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
