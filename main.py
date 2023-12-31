f9674c9397126287dc648010660cd92749352fe6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:21:02 +0000

    yes half here for look

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
