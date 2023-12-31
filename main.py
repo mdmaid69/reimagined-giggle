94b479da61219880214be4b312858fb236c4fd70
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:13 +0000

    holiday rough tax happy fit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
