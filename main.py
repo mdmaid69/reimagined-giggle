c56adddae0dfb75ea1e93dbfe21b314e6b8cdeda
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:33:53 +0000

    sound goodbye contain holdup hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
