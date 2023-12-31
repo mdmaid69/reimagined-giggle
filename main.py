a0688d704346925a7b12956c7cadee94fdadefb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:36:35 +0000

    science listenfor unfair relative or

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
