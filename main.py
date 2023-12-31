c9742315186d6774b8ab7aeaafa1e10d4f070504
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:45 +0000

    involve strange winter thank shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def add_numbers(x, y):
+        return x + y
