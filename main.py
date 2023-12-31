cdcd336e19cdbb2d0ea044805e89b1f5b0dbeb8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:37:41 +0000

    noone year excuse own respect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def sort_list(lst):
-        return sorted(lst)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
