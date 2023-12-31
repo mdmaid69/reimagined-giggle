583101e6af4b19280aa670e8c6b72da4b4c47cd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:02:16 +0000

    pure turndown opinion one pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
