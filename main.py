d33d0382b9dedcab8a0dba35555fb1c5901934eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:59 +0000

    partner scale embarrassed tongue cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
