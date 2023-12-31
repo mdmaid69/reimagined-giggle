3613f3e878e4b48f590b521819d2c17da8bc77c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:33:07 +0000

    fear far mix attractive profit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print([x**2 for x in range(10)])
