a869d127a51f4e9fc055bcd68da95653bb3eea90
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:57 +0000

    fit transport bend goon order

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
