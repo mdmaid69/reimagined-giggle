0c7e853aa68e6d539fcbe2dc2dfa2f4b7627932c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:40:48 +0000

    ticket kill happen remain fairly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
