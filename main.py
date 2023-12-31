49134131b9c1fd3e2fa1e14c64b4e013fccfd643
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:31:44 +0000

    message trade score rate unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
