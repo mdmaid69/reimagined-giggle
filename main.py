17f57182a041184cef941c0f53e92dd3acbe13a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:43 +0000

    do regularly split spring agree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
+  def subtract_numbers(x, y):
+        return x - y
