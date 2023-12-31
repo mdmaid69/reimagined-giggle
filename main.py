d440f9854cb6344100cc665a9e6d8d99160743e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:14:15 +0000

    lost better correction classroom create

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
