6c7d8451080ba891c0d6e1333661f9471ae6f1b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:56:44 +0000

    slip offence question off fix

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-print(time.time())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
