51bb7a2bf86c6ea8a10fc62ed6a1d9bb9fb01c8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:43 +0000

    large equipment wing respect congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def cube_number(x):
-        return x**3
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
