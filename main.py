e8af5cde8f7689638b63ee61943fe509659061e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:40 +0000

    general land badly table press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
