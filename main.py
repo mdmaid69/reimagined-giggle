bb51a65e1da27c7052a81995d80c29583ee2e592
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:20 +0000

    fear expect pay suit brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
