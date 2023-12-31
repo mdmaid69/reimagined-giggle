8e664dbffb081c2a1a17db4cf7f21c030e5ac6c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:50:38 +0000

    in callup finally can spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
