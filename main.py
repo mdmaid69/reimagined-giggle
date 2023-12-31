9f979102acf152acadafba0b20e564b73333ff1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:10:58 +0000

    keen turndown immediate left sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
