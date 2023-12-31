1615659068549fa17c00fba9a320a62b44f501ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:39 +0000

    reasonable spread long turnback can

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
