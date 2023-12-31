ee035fd699f347b5fcda8edcf2f5223c60383864
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:03:55 +0000

    my talkinto last geton transport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def sort_list(lst):
+        return sorted(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
