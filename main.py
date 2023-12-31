9ab888a04610cee19d20e72afb8dcec40b30702a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:58 +0000

    year right neck various tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
