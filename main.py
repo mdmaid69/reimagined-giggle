72efad60ae0a39df164e95e9ce69b5e634b6ef1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:27:48 +0000

    famous blame cold stamp dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
