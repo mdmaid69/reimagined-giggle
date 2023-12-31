2c9c9fb9b559e5b7f7be7bc772d6546b89355a67
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:19 +0000

    lead change bright eye class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
