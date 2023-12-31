9f51d4e1a764dfab82a17e75c93dea778d74a621
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:01 +0000

    waste reserve past sometimes exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_volume(length, width, height):
+        return length * width * height
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
