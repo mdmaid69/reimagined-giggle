d38a8efc561f6baea4a6ba5168f02e97917d3fc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:04:09 +0000

    nature confused fortunate fat steal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
