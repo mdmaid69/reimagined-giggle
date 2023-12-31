bc9c1a14915bc52332731e20be9e7f090ad3f700
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:37 +0000

    down miss over distance black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+print(os.getcwd())
