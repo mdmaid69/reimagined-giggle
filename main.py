2d09d41665ac3c0942add2622bb77d82ec642815
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:23 +0000

    sight breath tone sale popular

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_density(mass, volume):
+        return mass / volume
