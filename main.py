94aa792d9b6df841c9c6283463e98c6aa0151779
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:32:57 +0000

    Ss runover recover trip habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
