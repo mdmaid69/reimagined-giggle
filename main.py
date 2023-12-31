f0c06eb113b9c0ca02262d60ec7729a49698fd23
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:44:51 +0000

    and perhaps phone brush during

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def subtract_numbers(x, y):
+        return x - y
