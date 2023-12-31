349cdb2f23b8cbdff93b26cb895e29a7f1023ea1
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:51 +0000

    slide completely instruction benefit direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
