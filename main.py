d132ee9697f3c235be0fe5743b2092e46ad63f9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:28 +0000

    hollow mean talkoutof picture completely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
+def find_min(numbers):
+        return min(numbers)
