78ebc8b55eb79ec0256a7144c065e11b6dfe9a71
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:42:29 +0000

    obvious we suit strange dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+print(math.pi)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
