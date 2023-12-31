df09752c008675480c3bd000404aac4a4ce9a1c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:54:58 +0000

    mainly accident switch realize die

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def count_words(sentence):
-        return len(sentence.split())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
