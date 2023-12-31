afec089546766c10cbb574ec560e77d764b0c488
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:15:35 +0000

    grade wake keepoff probably whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
