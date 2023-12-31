317fc83eda04729c25f694083035fec55371dafe
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:48:17 +0000

    date shock place eat 568energy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
