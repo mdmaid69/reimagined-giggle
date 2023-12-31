e6aba15d04a12b6d2f3968b327f60cbb007fb6fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:05:21 +0000

    anywhere acceptable account recommend hate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
