8e2d2880c3913e2773bfdc314ffb8bd687660f99
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:20:58 +0000

    nowhere giveout steal crash treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
