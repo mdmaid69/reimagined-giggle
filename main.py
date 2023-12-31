6738b3689a38a286057b28871fa343549b67af17
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:30 +0000

    personal keep production ought importance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
