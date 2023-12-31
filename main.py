aad84ac3823076c2e1e92330e29474b891b3914e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:30 +0000

    book punish certainly storm hang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
