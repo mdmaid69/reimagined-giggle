dfeb328c7e9fbdb9961a6dfd99311b7dbada5653
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:27 +0000

    last rob bill may plan

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import re
-print(re.match("h.*o", "hello world"))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
