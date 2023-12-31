dd39123c10d9dfa744f1e534446f0d0326f8445c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:53 +0000

    still welcome glass expression language

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def remove_from_array(array, item):
+        array.remove(item)
