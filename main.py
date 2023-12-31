f9c7bc324948186419d99bb56fa0ca3acf594113
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:53:13 +0000

    rather result shine go shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+name = "Python"
+print("Hello,", name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
