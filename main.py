074d2d21b7df2214182b0c754bb426f7bf0405d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:18:55 +0000

    bury shine original steer adjective

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
