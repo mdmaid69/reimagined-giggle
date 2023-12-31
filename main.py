4510e05793b546f5b4136e0e869f13f253044cd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:16:42 +0000

    time standard let cross regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
