223cf47bf8ee1aa5b100d2c5db68ec21dc5d7de8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:57 +0000

    thought whistle strict send sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_item_count(array, item):
+        return array.count(item)
