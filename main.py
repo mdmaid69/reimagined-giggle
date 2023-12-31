03891bd1d80c278f5d21b3c3eecb3540224f9cc7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:43 +0000

    loss runinto instead battery can

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
