bc9c1d9715263806fffc0b33e9edff4d8355397e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:40 +0000

    either each cum complaint best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
