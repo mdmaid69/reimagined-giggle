ae75b885bf9008c3165444619310df7d6ab185ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:48 +0000

    bother herself into spend passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
