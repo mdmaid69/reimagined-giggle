c61f51c5943386f392f22e00e6acae50e0dfaba8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:25 +0000

    wave forgive intend right fine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_string(s):
+        return s[::-1]
