e2b4500d7f0e0b272f516bbe3c8e6026b67d8dfd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:09 +0000

    release learn gas empty shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
