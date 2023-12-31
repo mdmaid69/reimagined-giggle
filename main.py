2fa4f082949bb11a99be02ec88ebfbc38de9d5d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:55:00 +0000

    letter friend above Itself now

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sys
-print(sys.version)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
