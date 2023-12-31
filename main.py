2e6c5e19758d057541641b7d97da0b84d67fcf41
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:32 +0000

    press person such by black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
