4d8fb5c99631354c07e3dd59c8b24f56453a1f78
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:22:07 +0000

    cut takeout take party takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
