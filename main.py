7f41f0cb4417f4e14a030967e42802e640279231
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:59 +0000

    lock mind lookafter deal part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
