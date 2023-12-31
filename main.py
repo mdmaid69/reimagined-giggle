bc0467498a2d9c69d2d38d5a782c1e3d792ac737
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:51:37 +0000

    dance argue comment whenever stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
