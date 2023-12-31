d5a165d14cc4448de35b6c3ecbd1b9c002d0028a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:59:35 +0000

    innocent concern direct detail cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
