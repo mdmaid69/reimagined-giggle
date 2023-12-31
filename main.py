4d9c0956ea15f1805ce064eba1b3ccd585a6776c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:39 +0000

    everything everything tap who line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
