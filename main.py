f411917cd0e4f0b96ee685a2a196841864ffd4d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:12 +0000

    song unhappy delicate noise walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
