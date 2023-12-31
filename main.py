b581d7f2b9d03572dc95b9945d27236eee3aac72
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:38:40 +0000

    tear bang thinkover push relative

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
