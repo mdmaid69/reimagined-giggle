15b81328436b4f00f9424aa41eb258b5be582e9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:09:07 +0000

    sticky busy tour goback(to) desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
