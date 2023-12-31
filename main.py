024360fa3db17362622f6a324ba60ea4efa43ed0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:44:10 +0000

    expect sink walk straight cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
