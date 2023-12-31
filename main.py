461e5da0a2c1e46ea3ee776a94cca719b9ca6b34
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:40 +0000

    likely float bar broad everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
