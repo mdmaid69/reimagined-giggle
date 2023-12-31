91598350480393348f51ff57d0a7738f96f17047
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:25 +0000

    child love science travel giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_odd(n):
+        return n % 2 != 0
+import datetime
+def get_current_date():
+        return datetime.date.today()
