3b76bc52fb400fae5e0e828e70ed13997dc91d95
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:46 +0000

    middle true send birth firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
