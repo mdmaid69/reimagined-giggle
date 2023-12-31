c630e33f6ffe0ae69aae9876f36244195875ed7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:38 +0000

    broad surprised dry some suddenly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
