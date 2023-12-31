4fce86c29979a1fcc20fbdfef5f2be3068d9a26e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:11:23 +0000

    intelligent burst goon unite 567enemy

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
