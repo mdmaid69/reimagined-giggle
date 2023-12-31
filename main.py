6b5701da8cfbfa510823d26e8083533bb562cf06
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:54 +0000

    defend shelter stiff quickly after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
