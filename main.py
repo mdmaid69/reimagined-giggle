e178790e3a3b37e92df64789560309939a1f87c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:46:57 +0000

    sick gooff fat public arrange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def cube_number(x):
-        return x**3
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
