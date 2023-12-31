21d150c9dd02df92d5ca250d02e593e57cc864e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:32 +0000

    holdon dry up shake runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
