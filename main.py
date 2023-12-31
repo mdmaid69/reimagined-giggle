bd56892c69d25d38801e8808c4421563eb1375f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:35:40 +0000

    entry care relation down spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
