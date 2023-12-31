1a75f0a51d8952eaa1a86bf4e96571170aa870d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:38 +0000

    joke basic possible old completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
