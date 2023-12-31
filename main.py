cc12906d2412c5fa1a9306420fdea05b4163e9e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:48 +0000

    backwards light laugh forgive dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_odd(n):
-        return n % 2 != 0
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_max(lst):
+        return max(lst)
