e56913f917a6ae888418a40d7dbd082f9be754ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:49:01 +0000

    slow crime spring mixture woman

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
