feb467d7ec90a5c4b7a1b5b05d1469aa908b9b2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:07 +0000

    distance butter catch work hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
