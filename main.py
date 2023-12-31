e8a0211b6eef6d3f19d36e7036559264499c2c9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:56 +0000

    respect blind sore job long

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(lst):
-        return min(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
