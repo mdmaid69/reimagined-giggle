b0e8a3cfffc7c1c37d12f373cae69f3ac5c44780
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:42 +0000

    date total pollution fall concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
