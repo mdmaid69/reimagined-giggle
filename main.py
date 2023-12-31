a4cd764aae3001c6f57804a77c8bc7a309b9eef6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:45:59 +0000

    hang wander ought wipe row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
