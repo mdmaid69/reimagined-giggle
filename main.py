6a3421b9bed086a4acc8adc1a5919e09d8c3eed3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:09 +0000

    gas manage satisfactory cross shoulder

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
+print(sum(range(10)))
