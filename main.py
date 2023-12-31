1cea138815b67569665280cbb0f6bbaa69a8e062
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:22 +0000

    find manage pay side realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
