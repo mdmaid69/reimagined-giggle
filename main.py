65a3b4f694bcdd0fe7bfb7fae9e577f8991fc225
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:48:36 +0000

    ball mystery century ours tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
