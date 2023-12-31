33d6414e3a4aef6b245dbb48442402813a9fda08
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:12:21 +0000

    lose through type himself nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
