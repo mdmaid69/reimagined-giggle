d450491732c8f0ae11d44fc5e129828ac13228cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:13:50 +0000

    accordingto task winter swim strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_numbers(numbers):
-        return sorted(numbers)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
