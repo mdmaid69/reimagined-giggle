f28711b6f88e2707a54a4fb6507604a70fedd7d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:16 +0000

    store except win after lesson

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
