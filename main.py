7fd78113b68f37ade25d870e654be191fb24473d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:36:54 +0000

    custom grade for create pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
