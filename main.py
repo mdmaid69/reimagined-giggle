d7a30944982ac474e616015a1093216fb084332e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:13:55 +0000

    alone male thirsty print pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
