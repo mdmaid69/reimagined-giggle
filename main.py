d64ddb662922e3d94edba4eb59f6c42a577f564f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:39 +0000

    display completely inside through comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
