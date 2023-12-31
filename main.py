d2b77e760b35ecd5881f4e6aa9343f9332c14b83
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:07:02 +0000

    pity false closely consider punish

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def sort_list(lst):
+        return sorted(lst)
