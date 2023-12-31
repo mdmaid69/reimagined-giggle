c593081e9846c0dccaa69e88d4dbad9329359855
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:07:38 +0000

    before criticism half suggest fairly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def is_even(n):
-        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
