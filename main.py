44a207a9b53e6ffa8d5e60da0fd4e0b456a50172
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:39:08 +0000

    everyone boat speech upstairs general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
