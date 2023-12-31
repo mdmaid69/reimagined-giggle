f847bfcd441a4cffc1c4b4dba7ca38625e46288c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:02 +0000

    around swing reduction it value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
