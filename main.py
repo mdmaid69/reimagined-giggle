3bc2fbdfbb14ad43df6e566309be7fa866a0755b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:29 +0000

    soak breakin brain fairly forever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
