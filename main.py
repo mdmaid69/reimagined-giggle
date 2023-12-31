69f0abd58b21de3e6ac83a9201cd5ecbb96ab47f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:45 +0000

    giveup downstairs bringback search circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
