846f134ea5a28cfb79fc8b254447a6a0c3748a78
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:22:57 +0000

    recovery test unless object team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
