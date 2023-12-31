6d05b63ff957393bb51e3904b4d5a0698495024e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:06:01 +0000

    smart estimate giveback suck trick

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+for i in range(10): print(i)
