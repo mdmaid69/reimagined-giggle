6057f4728207b28edfa46abde1fa130f680c7010
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:01:23 +0000

    mother sight may unpleasant quick

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import random
-print(random.randint(0, 100))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
