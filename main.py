6fe2378bc1230eba423663b9e10e679640f9cd31
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:47 +0000

    age nice lock whole negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def reverse_string(s):
+        return s[::-1]
