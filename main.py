b080a4a9ac36ecb079baa33bcbb7d89ecccc439d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:31:45 +0000

    reserve process all Jj shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def square_number(x):
+        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
