ecba8d094046edbc2002f6d801773c32d4adb040
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:33:53 +0000

    high yes attention tooth official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def sort_list(lst):
+        return sorted(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
