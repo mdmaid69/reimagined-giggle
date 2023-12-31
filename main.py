7eb78fb322980428376afc94ab98bad589a81c30
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:08 +0000

    shape priority fence acceptable outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def add_numbers(a, b):
-        return a + b
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
