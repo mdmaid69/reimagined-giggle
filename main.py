bb99495bd0e01c6f847e2b541d8ad4853d179a2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:36 +0000

    drink thorough everybody create word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
