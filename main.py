ee1d07cb2bd6532dfee22fbecf1693d1e3aa8841
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:47:05 +0000

    description state switch reach fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
