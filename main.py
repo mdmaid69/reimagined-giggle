23db93d31d3a08f348f57bc1dc5d88c11ce35b49
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:10:05 +0000

    round warning apparently better gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
