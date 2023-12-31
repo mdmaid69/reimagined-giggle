20076993b14b6e2cc8485d5363420968f0f1ffce
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:08:05 +0000

    they whether rest hole truth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
