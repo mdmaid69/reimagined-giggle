253ebdd432f63750d5c6dec3e6c6e4f1a767abc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:19:59 +0000

    proper Yy deliver fortunate natural

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
