9e8f1547108e0208b488b1d4964b0b015bf66301
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:15:45 +0000

    blue order comeback worry shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
