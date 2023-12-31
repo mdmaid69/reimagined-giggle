4de7ef9c831f35c2e0ae98c506f80fc5f5d0ca85
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:17 +0000

    month round lookup invite total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
