7543a21b0b8bdc734e8ba56d3725524849dec53f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:15 +0000

    combination pool chat talk so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
