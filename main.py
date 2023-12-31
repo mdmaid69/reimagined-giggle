71dc53749c0fa8e323fd2c55d009fb89ba3d8b6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:43 +0000

    clear attract attempt unite fight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
