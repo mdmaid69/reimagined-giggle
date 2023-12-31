45b114577a2d1b46c9eb0f8b6ce1189bee85fb84
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:13 +0000

    fasten theirs goaway well cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
