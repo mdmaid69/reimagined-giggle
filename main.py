f2cad0b1512ece72e459c71bbbc626165d7d3ef3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:52:38 +0000

    decision talkinto down niggaacademy scrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
