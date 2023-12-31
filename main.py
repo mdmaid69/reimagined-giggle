93100689b4db2e93e8d7ec7585464ff22866b39c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:06:05 +0000

    tooth involve marry slightly neat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
