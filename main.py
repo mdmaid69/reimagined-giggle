a0542cb15129e1061bcf00d99c1470dbff76a9b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:13 +0000

    miss choice relationship trouble maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
