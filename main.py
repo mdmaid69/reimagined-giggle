3e39dedbfd7a21250aa98a2706fa9fb465a5db7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:09 +0000

    offence callout beyond chase clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
