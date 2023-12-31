02326daf92b1ec577f6e50d0399795a81a99b8cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:42 +0000

    impression challenge lookback(on) receive put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
