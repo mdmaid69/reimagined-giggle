64f82975786c68a1472826c6b3ad000bf63d78b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:34:37 +0000

    odd acceptable up tight slide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def square_number(x):
-        return x**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import numpy as np
+print(np.array([1, 2, 3]))
