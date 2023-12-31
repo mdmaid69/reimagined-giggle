482e01272b3ca44e36b7bee39e1ed62f679452b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:50 +0000

    holdon bury goon target forward

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
