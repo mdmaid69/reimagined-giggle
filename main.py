4815768d217271ea80e35de81d5b1eda66fd3f22
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:22 +0000

    pair case once emotion thinkover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
