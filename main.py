caf8e16fb6a4c9d51704447ae1cd945222146958
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:02:33 +0000

    physical reach recognize mysterious direction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_counter():
-        return collections.Counter()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
