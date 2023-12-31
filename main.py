f45e8af16b576b8bf7dd9b7c40a05ac01d9d06fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:39 +0000

    along bang set breath surprise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
