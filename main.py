edb38e7691e18c2d316db6145abece1ee8a0f691
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:14:20 +0000

    measure comeup kill rule error

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
