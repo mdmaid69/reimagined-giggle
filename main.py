51dee5905402664bc9331bbdd5b7abe90cb187ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:32 +0000

    Jj field stage damage star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
