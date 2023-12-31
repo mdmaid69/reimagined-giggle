ef53c63eb13c90a89d911c2788754672b5706ae0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:54 +0000

    definite godown pot season wing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
