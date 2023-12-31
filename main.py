286903fb3fead97727ab212205952941106a8606
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:36 +0000

    godown wide develop lookback(on) temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
