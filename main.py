95bb0962a6fc2c14f3434901303b7edbd2bbea73
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:30 +0000

    run basic tell unit switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
