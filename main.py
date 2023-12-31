c6b1c8f8b065cd2517f70b3d51f73ac8e9bbdf34
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:04 +0000

    favour cheat load mark simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_directory_name(path):
+        return os.path.dirname(path)
