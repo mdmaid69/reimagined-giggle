b6f3a791788d3297ebd0b74789d2b2742049abfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:29 +0000

    method wish nervous wire yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
