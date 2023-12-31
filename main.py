748e399ceb41ecf37d06b921e45a9edb751997b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:07 +0000

    know space false content firm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def count_characters(sentence):
+        return len(sentence)
