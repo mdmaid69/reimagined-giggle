5020c507699cc087bfed89d8dcd92ce82af5512b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:02 +0000

    available section light turnover powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
