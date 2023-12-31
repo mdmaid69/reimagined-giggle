bdb69a4ba12b7004aca788a9aa31cb7a089ffef6
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:03 +0000

    share increase scheme its rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
