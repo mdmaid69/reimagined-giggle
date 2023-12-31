0a4b9c4d0cda9118759ada659439560340fd7a51
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:20 +0000

    forever doubt bear slice get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
