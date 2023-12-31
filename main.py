e5a4a3f4cdde75e7b89efc683dd1e5a323b1bb21
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:41:30 +0000

    wing science grade young minority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def clear_array(array):
-        array *= 0
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
