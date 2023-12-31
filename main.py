300ab2cc28a92e6b5554cc0fae8736287241bc0b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:55:44 +0000

    push motion thinkover Tt measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
