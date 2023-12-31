447571a22fc5c899f1d08cb832684ab94b19e7b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:22:46 +0000

    scare ashamed another peace work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
