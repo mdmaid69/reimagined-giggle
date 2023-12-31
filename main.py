03f6373126098607b244e7785ea01a35e5ba152f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:03 +0000

    today recommend land round match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
