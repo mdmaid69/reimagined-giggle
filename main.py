759925e4df61c1317f05c6bc01eb9bfc867297d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:24:48 +0000

    contrast means consist future after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
