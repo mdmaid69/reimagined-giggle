92d81116f45185f52a9a6884b305149a11347a50
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:34:54 +0000

    protection honest ordinary description relative

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
