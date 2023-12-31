fcbbaafe6619a33b3bad1c3f1e25107636895e38
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:19:16 +0000

    kick disagree too shade expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
