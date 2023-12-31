9806e1a3d6e6a2d5339fed78d0cf8da8bc6a4ef7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:37 +0000

    impressed dish love at his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_today_date():
+        return datetime.date.today()
