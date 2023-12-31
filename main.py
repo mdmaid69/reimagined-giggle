66ef7b3c12c5f41abfcc5e43931ab54ce8a67170
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:37:12 +0000

    workout back disagree fortunate issue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
