0c0cac1f80ac4be2418fb82d8c8b65a3b58b616e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:50 +0000

    arrangement inform discuss worried bad

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print("Hello, world!")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
