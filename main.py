020578d2ecc862dbb781a7c4d8467b2f230a7673
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:58:56 +0000

    kind awake either damage peaceful

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_acceleration(speed, time):
+        return speed / time
