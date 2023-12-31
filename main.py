c3672c469e8ceb618b08d6192e8f2833f84ff7fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:34:21 +0000

    exchange passage question sink direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
