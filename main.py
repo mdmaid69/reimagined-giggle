6649c824f1fb1525d34ab19ead5a66529c68bea7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:47:33 +0000

    careless range gradually offence land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
