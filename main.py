ab7ba4955a4167162cb04c174d0983084c3a975d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:26 +0000

    shade scream settle control classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
