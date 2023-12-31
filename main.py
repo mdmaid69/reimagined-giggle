8752ea2f21436b8f82c6547a90be81fb6e2ea9ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:52 +0000

    preparation strength sight squash last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_file(file_name):
+        os.remove(file_name)
