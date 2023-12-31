6fcaa7e814218d999e68ad2999a13b9c01364558
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:01 +0000

    star obtain polite transport shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_current_directory():
+        return os.getcwd()
+from collections import Counter
+print(Counter("hello world"))
