b5aeb6ad336d89c55104a66f7fc3669d6d5f6567
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:11:18 +0000

    music 568energy task cough ball

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def convert_to_hex(n):
+        return hex(n)
+  def sort_list(lst):
+        return sorted(lst)
