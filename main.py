d72e10809f7ec76ca4dbaec20506722f92642df7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:37:14 +0000

    soak brown content correct suckme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def cube_number(x):
-        return x**3
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
