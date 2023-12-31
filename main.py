41d7cd83df838e82298ecb2e74a6ee461ef20a5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:10 +0000

    future class pause shout help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
