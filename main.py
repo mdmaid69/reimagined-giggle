3f72d9cf5d173a03d3cf96dd977c2dc62fe595c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:25 +0000

    by congratulate sweep train reduction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
