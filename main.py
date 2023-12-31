6b8f46f17b21b8a2f2a3a887967eecc8bee0673f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:45:48 +0000

    drop bound post very instrument

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
