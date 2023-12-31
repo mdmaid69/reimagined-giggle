f2d186b599f492424dcbb9d90e33b7343168cef2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:41 +0000

    likely anybody play unlikely frightened

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
