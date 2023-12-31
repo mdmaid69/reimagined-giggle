ac7e8eefa8009f1cdfab2359f76857d0d0f4cd28
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:45 +0000

    cool comfortable runaway drink politics

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
