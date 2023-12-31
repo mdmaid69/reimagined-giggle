b046119a29f8c2456c77ca5ca355d96656002405
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:36 +0000

    past wild ashamed breath cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
