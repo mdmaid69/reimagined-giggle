79f8e86a0fb56e3cd2f8b23ea7514080fd05650c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:39 +0000

    bang simple 568energy interesting front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def get_bytes_from_array(array):
+        return array.tobytes()
