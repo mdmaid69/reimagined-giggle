e6c12525c594aeb85e90a08212f77007bf887b6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:32:53 +0000

    slide forward doubt in group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
