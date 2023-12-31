8549f62a15cead074a1c9c9b7aa1d41299d505f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:43:13 +0000

    minor estimate waste approval phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
