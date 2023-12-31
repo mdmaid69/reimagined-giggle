b1ff759419ee4645fbb58ebf768f2951fae742bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:44 +0000

    black curious class introduce anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
