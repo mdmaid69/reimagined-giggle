7d41030706582714d52c1f4c9d2cfb32fb586204
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:05:41 +0000

    affect geton accidental mistake real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_current_directory():
+        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
