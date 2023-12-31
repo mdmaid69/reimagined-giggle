3a252bdce63e0e1c19c2a33a3dddf500bf24f134
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:16:35 +0000

    object claim kill male giveaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
