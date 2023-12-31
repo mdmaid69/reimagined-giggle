99d2d7ccd91c33561c28e6b20d551f1160fbc84e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:32 +0000

    sea sure off best should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
