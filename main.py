abda2047787ae05314f8bbcf4756830e3f3fd0c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:02 +0000

    confidence tense order flow realize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
