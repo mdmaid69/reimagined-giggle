11c27ce84ee9559f3ee67538b3c5b22244b6d700
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:12 +0000

    dinner please contain anybody block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def find_max(lst):
-        return max(lst)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
