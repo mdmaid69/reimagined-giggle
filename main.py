74da4fda25ad1365bc43fc323cd8201fd36d409a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:09 +0000

    clear comeup target grade because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.ctime()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
