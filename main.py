136d2f5ec69cc4cd586bd99ae1df5140f077cb8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:02 +0000

    immediately wave myself purpose brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
