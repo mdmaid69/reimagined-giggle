0d86d2e48bead587f69f9b8ccb461acb4ae7efba
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:22:47 +0000

    thinkover old rest which say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_odd(n):
+        return n % 2 != 0
+import array
+def set_array_item(array, i, item):
+        array[i] = item
