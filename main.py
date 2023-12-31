6b468498fbf9b0dc9861da289a547f4127916d5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:02 +0000

    tense his 571enough country soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
