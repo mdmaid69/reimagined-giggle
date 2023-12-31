52473b080c3fe5292efc19f7bccbe5839523f39d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:27 +0000

    why tall apart keepoff never

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
