52c1aed9eb53d321f91e655f95f4a835c8398c59
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:23 +0000

    recent giveout standard row fairly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-print(sys.version)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
