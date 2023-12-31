ffe5a472e20ac3a4f2e2ac2c135200284174441d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:00 +0000

    kill world today nervous mistake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
