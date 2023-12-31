4278234fbe80af77f4f098d2c114654790844424
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:27 +0000

    giveup needle backwards forever record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
