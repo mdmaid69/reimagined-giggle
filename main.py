3cc2a1541cf5c791fc6d55e38fafaa6afb2fc631
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:52 +0000

    politics front quarter him on

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import time
-print(time.time())
+print("Hello, world!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
