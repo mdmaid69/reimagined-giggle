60fdbdf7a11fb0b41defa34bf4f77f30d2368ee1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:19:39 +0000

    goodbye sweep round goup mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
