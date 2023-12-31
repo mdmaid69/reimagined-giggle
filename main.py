0059b26b1e31c07bda7ab6100e3eb132a5b5cc21
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:46 +0000

    turndown pupil neither mother matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
