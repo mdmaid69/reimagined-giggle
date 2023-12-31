d3e34c271f55af921fd3f39026873648fb011d08
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:12:48 +0000

    later 565end close impossible compete

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+name = "Python"
+print("Hello,", name)
