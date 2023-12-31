5d4ea043f7a74cc21928921d9a8fd8349df18488
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:29 +0000

    condition country empty bath traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
