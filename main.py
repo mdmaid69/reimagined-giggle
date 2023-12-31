baa78b57050e19afda4a766f1797192c667c076f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:09 +0000

    turnup environment wake become ashamed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
