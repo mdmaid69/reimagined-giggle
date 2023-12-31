cced826aca74eebfb122f985b82badb72710eb39
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:19 +0000

    out proper police stare circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
