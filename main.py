eb71f44cfddadddddb02aef70a8be72752388c90
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:34 +0000

    present shop practical journey exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
