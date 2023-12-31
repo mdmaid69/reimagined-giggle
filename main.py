85845b603ee46fbce3f5a78ed340e4234387533f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:23 +0000

    against moon guess one public

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_max(numbers):
+        return max(numbers)
