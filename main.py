db56e7d6bf3350fd6dd70af34c7677d1029576ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:18:53 +0000

    day match next there him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
