06bac1782735cd33295d016d3d2a566c9d5027f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:01:22 +0000

    case pay confusing Ll increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  def multiply_numbers(x, y):
+        return x * y
