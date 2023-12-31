14a82cdb67d5b1f6ea6d1abfc7b58cbbe7d4dcc8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:35:28 +0000

    order bound share will suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
