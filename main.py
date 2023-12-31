8385877219b7a233782eb1e9d6edb80657d38c7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:30 +0000

    Ww chat many inform nothing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
