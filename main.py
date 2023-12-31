08d95c2d0d5d8f143711289116fe5c54d6118e87
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:48 +0000

    divide legal obvious worst threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
