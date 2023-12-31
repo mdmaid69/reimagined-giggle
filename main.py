ff1f34fba7af866f50f4ebd62e5a315016c8ccb3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:03 +0000

    personal full team relative entrance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-print([x**2 for x in range(10)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
