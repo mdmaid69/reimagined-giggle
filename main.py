f57de9db9f0aff00d28c9a503da39c146ddca848
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:07:38 +0000

    take dream event once hold

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-def is_even(n):
-        return n % 2 == 0
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
