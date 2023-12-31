d348a562b320538e8b35b4dd460393badacf1166
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:47 +0000

    talk equipment shrink block unless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
