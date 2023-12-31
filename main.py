b87c4b2d1cf31e133ddf8d496697a7364d2ab1ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:17 +0000

    cheap lose poison fasten total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
