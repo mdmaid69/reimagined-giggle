19e7091391a4b31f09b381667e82297ac84fcefc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:20 +0000

    try late desperate level fortunate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-for i in range(5):
-        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
