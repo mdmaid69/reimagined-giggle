c435c5666062671ecadd81f8112c83fb1332e2bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:13 +0000

    way brief bother fetch invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_current_directory():
+        return os.getcwd()
