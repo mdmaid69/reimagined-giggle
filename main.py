f4fbf40c00d45c9065d6c879bee257dfb06c2522
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:58 +0000

    steep concentration permission cruelty wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
