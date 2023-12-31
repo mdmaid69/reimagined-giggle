ae2b613b3cac243c83dc191f0bbeab389f4e3e6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:11 +0000

    street divide wave shallow protection

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+print(sum(range(10)))
