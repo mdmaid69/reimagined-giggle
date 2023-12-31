81098a10c9746839f5fb9e33a93ba15354ca43a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:17 +0000

    exchange season pull lay along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
