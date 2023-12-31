d306a2f95a8ee4bf6188f6d0a6d0c8f4f4511ce6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:16 +0000

    succeed clearly purpose life people

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
