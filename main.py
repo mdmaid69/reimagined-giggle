75f0e9db3afa8b6f35bd9905cafe462681f449c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:38:04 +0000

    impressed weather love different drive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
