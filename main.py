82119866c24165a50da7e72adc988db1caee134f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:03:09 +0000

    talk human murder direct minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
