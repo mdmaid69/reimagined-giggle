3394769e9c787b258aef5fd91f646a0e140748fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:43:29 +0000

    shadow of forget unkind deliberately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import platform
+def get_os_info():
+        return platform.uname()
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Squared:", [n**2 for n in numbers])
