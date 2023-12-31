2dc77203064eb145cca65312ed1ff59ce01155e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:00 +0000

    estimate miss read down children

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
