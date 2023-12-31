ad054a023a98b1cb9626476e772b88d8f76e5c5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:59 +0000

    boring rare make other state

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_list_from_array(array):
+        return array.tolist()
