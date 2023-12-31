2335b9bfceabf23a3b7fbd2b1039d74942ab6bd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:02 +0000

    cross goup however money match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
