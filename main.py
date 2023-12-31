090db56e3148cc93ad19bad7b25142d1f514f9e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:52:09 +0000

    Kk custom experience takeaway foot

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sys
-def exit_program():
-        sys.exit()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
