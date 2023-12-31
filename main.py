c8d744811ee5ace9a461b5ff35ad38740eb32789
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:20 +0000

    opponent against system bringup slow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import getpass
+def get_username():
+        return getpass.getuser()
