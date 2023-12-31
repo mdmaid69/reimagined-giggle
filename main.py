b2bbedbc9a9edee310b616c25922fce312edf06b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:22 +0000

    his art comment human rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
