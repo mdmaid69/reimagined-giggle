10db22b009ab34b4e5639456602ae60561a624d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:11 +0000

    mistake forever pipe unkind club

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def square_number(x):
-        return x**2
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
