ceb9ef3e2d7a946ee2362119edb4f1d5a11bcbb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:32:36 +0000

    anybody point coat this decision

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
