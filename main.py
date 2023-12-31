561c21e98ab2473e55125e768d0e943d83fad350
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:26:55 +0000

    nice table can worry belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
