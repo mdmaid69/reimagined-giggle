c8bff837d630496927da5000753cd67288de6aa8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:54:04 +0000

    floor busy themselves fix account

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
