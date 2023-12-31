cae852f9b2680f6ff107f793a94cf30936790a66
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:50:33 +0000

    satisfactory store still gate word

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
