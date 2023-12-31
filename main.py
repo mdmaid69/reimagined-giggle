69f4a838cd1727cc395ff76e3d9cfecdadbc92c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:31 +0000

    history future complete plan wear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
