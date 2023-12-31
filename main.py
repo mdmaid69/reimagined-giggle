925f9972dfb874286135a1114310625800f92fec
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:21:21 +0000

    hospital perhaps angry odd however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
