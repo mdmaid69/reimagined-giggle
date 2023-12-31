d76508a661426c41def045bb9f503710e6f9ea85
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:45 +0000

    acceptable blame occur sea once

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_density(mass, volume):
+        return mass / volume
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
