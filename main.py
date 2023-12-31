e746a53c073fa62c567ce3e9eb1d82d4ef1958d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:35:32 +0000

    street could side practical juice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def sort_list(lst):
+        return sorted(lst)
