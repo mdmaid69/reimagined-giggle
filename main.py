772893c1162f74eb5b2f0a06ba38e45362affb5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:12:23 +0000

    confusing tie row react wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
