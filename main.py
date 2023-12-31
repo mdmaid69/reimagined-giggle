6bcee719e9454d5d95c32fde043acdc70515e117
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:06:39 +0000

    stare welcome task be tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import logging
+def log_message(message):
+        logging.info(message)
