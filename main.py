786142ccebcfec8569ed9585d97b08d804e81a3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:45:53 +0000

    class divide congratulate admit front

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
