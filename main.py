e51a19f4ab037cdb061e1084d7c3d23ff4c4b83a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:05:14 +0000

    let may Rr able needle

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_acceleration(speed, time):
+        return speed / time
