b3e8582696c412bd89ee8b1be62c9a4e22987588
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:32 +0000

    regret confused from exercise getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
