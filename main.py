76a3a542410a9fb10c50a5733e9d3ff71d8db346
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:03:43 +0000

    all between neck perfect fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
