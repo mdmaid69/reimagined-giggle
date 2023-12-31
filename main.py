4e821e643a1573bc15783720fe74b3381a17b411
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:04 +0000

    plan hate respect difference empty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def convert_to_octal(n):
+        return oct(n)
