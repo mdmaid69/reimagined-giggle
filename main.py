acb06d3eaf65fb75b9e0e19448f605c618c8b53f
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:21:48 +0000

    squash lay cinema embarrassed abroad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
