553576988eb20c1e8ba52137d5a767b58bb1ee68
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:33:51 +0000

    empty prefer happy choose design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
