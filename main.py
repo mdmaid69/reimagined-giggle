035317d260266cf205733ff1b072b063673e5366
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:51:35 +0000

    when sale for remember emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
