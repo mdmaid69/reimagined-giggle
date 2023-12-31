ef9b74fef1cea3b56065699a8f894f835170cd6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:36 +0000

    difficult once choice may class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
