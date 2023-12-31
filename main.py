3dbe4c7c8da0d85739bc21abd57146c7c27f40ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:29:24 +0000

    lower sure trade Rr winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
