5946d80910fbb5243041b8c58804a436292647c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:08 +0000

    talkback pool swim meanwhile false

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+print(math.pi)
