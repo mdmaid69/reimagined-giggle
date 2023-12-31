bb5163ca8c76edca9ae66cf32d88c8e9f0408e30
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:39:46 +0000

    fear flat ever inform original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
