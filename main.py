c0c208826f1ae3510d898fab6a4b946ad9421cba
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:12 +0000

    interrupt tall situation pain drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
