356bd59db67d79fa129c7f6eecb74f764ea868ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:09 +0000

    survive trap contain ever basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
