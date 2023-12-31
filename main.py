1c1d0914c22b686b846c443f12672b6e6d45684f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:12 +0000

    need maybe hole identify development

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_elements(lst):
-        return len(lst)
+print(sum(range(10)))
+  def is_odd(n):
+        return n % 2 != 0
