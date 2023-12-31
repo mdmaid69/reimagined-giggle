c8b49828804f9f9cfa863ee148babf2589b0a0b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:59:50 +0000

    exploration computer tap keep trousers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
