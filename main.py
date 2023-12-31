72abdf10e8379ff0ec7d1c925f02950bc08cd765
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:50 +0000

    star special squeeze dangerous together

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
