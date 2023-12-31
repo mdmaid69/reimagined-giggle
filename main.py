d41759c99c00cae417c4870f7181740d5aa6db72
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:51:17 +0000

    suit throw issue false mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
