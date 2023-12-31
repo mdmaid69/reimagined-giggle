db1190f5312b7c8d0a63aea5741380a7bdce0928
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:43:20 +0000

    Bb prevent city action neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import json
+print(json.dumps({"name": "John", "age": 30}))
