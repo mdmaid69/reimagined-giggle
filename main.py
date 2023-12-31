355e3e7fe1c0c8c631584c7d6d4e14bdc4fe1756
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:35:42 +0000

    behind condition file keepup weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_typecode(array):
+        return array.typecode
