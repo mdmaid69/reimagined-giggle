4fba9162bf5a37721776d535ceed020cccc886ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:17:16 +0000

    rather juice workout form discuss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
