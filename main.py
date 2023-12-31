e12be2fa1d75092f45b6d62a283f88e47d962e5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:11 +0000

    whom further thin estimate touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
