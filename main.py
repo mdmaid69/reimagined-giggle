f0f9af71e71b1a97523b5157343a0c3ae4476c4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:52 +0000

    cost keepaway test fold tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
