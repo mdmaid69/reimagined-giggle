9079cb561cb4e084aa8b841b417ab76d9bac7d2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:50 +0000

    visit willing occasion port story

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import datetime
+print(datetime.datetime.now())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
