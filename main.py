66c9e13849a95c29c871050290d6363c22509cbd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:09 +0000

    underneath actual hard deserve main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_string(s):
+        return s[::-1]
