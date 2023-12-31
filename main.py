b28074cd46b6ed961cce21fcdd3bb78c56e645c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:37:25 +0000

    perfect pay tape emotion 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
