ce04feed3e691006f049051390f31c29ddd8f61d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:50 +0000

    meet wake beautiful touch start

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sys
+def exit_program():
+        sys.exit()
