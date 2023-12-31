5c5cb2bcf74fbe5e7812d9d6c8b20d688932eb53
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:56 +0000

    secret recovery risk swallow moon

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def convert_to_binary(n):
+        return bin(n)
