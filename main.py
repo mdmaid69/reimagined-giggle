69b02fb8942c2f232638a8b1db9a25179ac03e7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:41 +0000

    watch public slightly stupid smooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
