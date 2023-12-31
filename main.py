45a613a2e811ac6ab1fa26351be954c275ae1b60
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:08 +0000

    blind defend attract perhaps joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
