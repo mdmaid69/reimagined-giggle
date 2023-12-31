55f739df142258637ec99d5974ac049a26dcbe8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:08:59 +0000

    actual rarely ignore trick profit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
