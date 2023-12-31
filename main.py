17345cfe28d86ce97e500710e659940811b206b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:41 +0000

    slice support getaway injury sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
