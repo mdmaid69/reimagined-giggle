2f451ab8e23c193dd89cb4e644d3e656fd358471
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:33 +0000

    detail success motion ring remember

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
