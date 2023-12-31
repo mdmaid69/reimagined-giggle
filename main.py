4659016bd50429222744acc1618b00f2a5b9ceed
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:50 +0000

    range action usually acceptable even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
