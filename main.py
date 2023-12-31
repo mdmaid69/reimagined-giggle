821ccad1bd2d1eaef25443adeae86d24c2fb81a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:52:32 +0000

    independent star regular lot bus

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def square_number(x):
+        return x**2
