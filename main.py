a9c40310af0d3752d680e83490825a82f2c849ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:52 +0000

    help survive birthday circumstance help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
