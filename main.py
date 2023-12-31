595d15947b29c0bcf1fcb93ec3dd8c6a5365dbcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:31:09 +0000

    track down kind accept talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
