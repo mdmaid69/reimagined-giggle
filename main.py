af1dcbacdcf401b33401b81738711b6c598f612e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:59:14 +0000

    yourself formal climb consider choose

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
