7206dbd56872db1e2e0dceb93f2f6fd799587d8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:32:22 +0000

    church nervous defeat load official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
