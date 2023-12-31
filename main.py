2c98d550f972e8185105188f3729c6ce4c32742d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:03:22 +0000

    frequent loss hair bring shut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_max(numbers):
+        return max(numbers)
