fcdec1677a8638cd15b00156a9964f41e3091eee
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:04 +0000

    ear fork boil light threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
