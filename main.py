7b77b46d93d6ccd50a9b2fbaaf29632c43efafae
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:49 +0000

    failure announce appear clean comfortable

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
