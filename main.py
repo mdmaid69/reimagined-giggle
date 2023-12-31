1a83e012bf80e31f86b5149e9fb8fe98407ae401
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:45:11 +0000

    environment away part exaggerate bang

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
