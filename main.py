70d68c0da8d26883c2257eaddee183a2a256f9ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:24 +0000

    passenger seem software motion can

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_work(force, distance):
+        return force * distance
