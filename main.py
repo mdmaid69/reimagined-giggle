31bf4d82f01cee85356ca99d04ee688610fc0af4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:52:40 +0000

    active objection maybe needle choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_list(array):
+        return list(array)
