f55cbc160a9b076e064bae1305920634e1861486
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:07:06 +0000

    prefer hospital watch centre band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
