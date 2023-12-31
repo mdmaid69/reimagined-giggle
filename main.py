16e4731a2e4ab4cab0212dc8d2c928cda487c4df
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:58:57 +0000

    story shallow contents temperature petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
