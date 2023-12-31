0df9dca887c1b0275bddfb800ecc25b078073fd8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:54:19 +0000

    wide mood knock custom light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def find_min(numbers):
-        return min(numbers)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
