ba93605bfbb1d4345cb5db0373b2f562bdf65b12
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:46 +0000

    possession enthusiastic wish right Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
