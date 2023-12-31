d243f1a649a23f062f4feb368876da3ee235ac18
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:43 +0000

    towards tidy which ring dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return math.factorial(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
