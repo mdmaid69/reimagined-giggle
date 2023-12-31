194199ae7c2c2758c3dcdf60fb0cd8024f24ebad
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:32:38 +0000

    be protest argue alive pretty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
