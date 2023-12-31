b78cfacda68197510a7a5ec7d498c60c407445b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:27:46 +0000

    improve reason word obtain emotion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
-print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
