f6a4e07e1492794b7fba2836ae8a4b96ff860a02
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:50 +0000

    lonely occasional narrow tax Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 def calculate_work(force, distance):
         return force * distance
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
