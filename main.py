cd6873b9835b871c7c7d25f5fd81ee2cb0d7fa8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:48:05 +0000

    we stupid into lead study

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
