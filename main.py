e2a3aa014b1525d3753886472dea4371cd0bc738
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:55 +0000

    exaggerate peace model spring by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
