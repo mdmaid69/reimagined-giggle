ed1d33845e3c429cbd5215f2e1a19fafea81d158
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:25:03 +0000

    wherever secret bringdown between union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
