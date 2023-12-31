27c11391727e6c966a6aec4475522262950b41c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:54:11 +0000

    curious need tall human rare

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
