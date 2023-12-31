0fef97dcf7428eef3c0e8467792a3341714feee4
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:59 +0000

    bunch method body there branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
