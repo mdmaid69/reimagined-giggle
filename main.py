22f2ac93393b9a49ff6314296849a03f7f7cfade
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:26:34 +0000

    regard thanks everybody independent steam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def count_elements(lst):
+        return len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
