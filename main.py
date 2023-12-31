408d74cf032c7bb85181b65c30477f4eec3e2430
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:51 +0000

    back round exactly remove control

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
