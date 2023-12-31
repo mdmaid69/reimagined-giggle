fe947e8f3c30c6d670aa260c74136a74a892c674
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:09:02 +0000

    bend let goaway period turnover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def sort_list(lst):
-        return sorted(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
