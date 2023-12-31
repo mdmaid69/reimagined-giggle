50d6607df4937afc2b9ef3769faef735c4ed564d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:24 +0000

    comfortable afraid work own but

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import time
+def get_current_time():
+        return time.ctime()
