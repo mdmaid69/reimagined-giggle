9023ec8639fb53ca468fad90adb32f13455c71e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:03:51 +0000

    Cc kind interrupt ticket story

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+print(math.pi)
+def find_min(lst):
+        return min(lst)
