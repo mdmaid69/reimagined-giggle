2f8238f079b1bc8f7595ec853efdc82c49b2fe69
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:08:45 +0000

    scale payment afterwards illness private

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def find_min(lst):
+        return min(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
