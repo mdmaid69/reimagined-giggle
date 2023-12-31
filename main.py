d3ea516938e6d792987cc8f5f580ab1d9e4d41da
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:13:17 +0000

    address finish sore past there

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
