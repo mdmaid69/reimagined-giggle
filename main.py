95d70a2702d165e72bc12f4d15da74828fecd293
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:13 +0000

    nurse several worry moon may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
