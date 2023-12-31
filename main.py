42ff08a8eb54d6782faef063242746b0974c461a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:01 +0000

    partner both trap criticism full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import collections
+def create_user_list():
+        return collections.UserList()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
