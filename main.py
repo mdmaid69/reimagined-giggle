e0a13ca2f4d7ffb37793ce78dbe14489704609e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:53:32 +0000

    under excellent willing persuade contribute

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
