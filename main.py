9202665bfed2da517324e56cc9ead62d55c97e9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:23:33 +0000

    level none congratulate must interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def get_array_slice(array, i, j):
+        return array[i:j]
