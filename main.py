0b1d0393ce82a465f5541ef70fc9836de3283cf3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:47 +0000

    trousers broken our turnaway appear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
