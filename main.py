7c4bfe09ee5c935807148482fb613b13f3a6c47d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:43 +0000

    let live tend pleasure start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_array_to_list(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
