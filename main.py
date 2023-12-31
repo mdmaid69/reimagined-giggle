33da3e2f988a1e6e674847ba2595036333e06230
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:14 +0000

    top news reach necessary runover

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
