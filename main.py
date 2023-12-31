849c80d95b2691614ebafa2998a50d974769fc67
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:34:04 +0000

    claim breath flat place hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
