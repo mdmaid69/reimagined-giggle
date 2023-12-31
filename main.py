6ddf5bdc10da1868b1e378d385d663865012a78f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:01 +0000

    light protection shock arm should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
