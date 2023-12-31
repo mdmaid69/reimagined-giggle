05b0d532e0a50ef3ec3b08bc873451c9164fb7d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:36:41 +0000

    street country stiff substance scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
