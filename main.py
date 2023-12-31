340d42e455d25c566eff028ea24b9166fbc874cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:36 +0000

    block bottom rest introduction hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
