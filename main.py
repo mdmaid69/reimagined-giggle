6a6c8551ad8a61de2ce7098172ae055295a7df9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:33:09 +0000

    major think against objection complete

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_min(lst):
+        return min(lst)
