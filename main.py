3c2e82e55cf14556bd6e1c67ab0541bffe455fa4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:44 +0000

    because operation hard hair trip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def reverse_string(s):
-        return s[::-1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_min(numbers):
+        return min(numbers)
