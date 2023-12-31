9ee39301da5ec86f17e010356df82d9ec4396906
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:15 +0000

    group him makeinto sink clearly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
