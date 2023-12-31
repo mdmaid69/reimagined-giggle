0e50c66fc9e414a850f04e135cea5155db84ebc8
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:00:32 +0000

    preparation keepaway shelter busy value

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
