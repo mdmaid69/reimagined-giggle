3fd33285fde4d4c75e998f9f0dfdc2448a7967bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:04 +0000

    mainly check frighten bunch downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
