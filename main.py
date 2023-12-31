9a3cc1744cac82f65254cfa9e2bbef08e5df8a95
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:20:26 +0000

    away waste except high shop

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
