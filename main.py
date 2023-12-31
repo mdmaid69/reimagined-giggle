aba62f237aaaeeb56a4f430e7810d51e53a6a153
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:48 +0000

    mine flavour aware twist hurt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
