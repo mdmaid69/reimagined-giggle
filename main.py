dd8ed4ac416ee55a122c0947d2827ef00113cbea
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:57 +0000

    nose natural beyond off file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
