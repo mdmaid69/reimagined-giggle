ba60d63cc2a70dda171b1243548372ad13c9d866
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:21 +0000

    copy steep steer last shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
