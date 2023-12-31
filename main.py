8d3a1ef0499fdd918f1128f905de9b7c98db5ac4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:04:37 +0000

    on empty disgusting turnaway his

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
