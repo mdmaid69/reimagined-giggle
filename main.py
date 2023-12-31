44e7349e6a28657d99c17c65b49d772c8522778b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:04 +0000

    angry favour teach trap better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+for i in range(5):
+        print(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
