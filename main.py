3a6fca7fe4f650597b104e083b99583136d02b65
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:17:02 +0000

    head odd gradually arrive release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_stack():
+        return collections.deque()
