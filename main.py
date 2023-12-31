9613999d5a5385511bd8954495352ef78c753aec
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:37 +0000

    scene open nigga emphasize room

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-  def subtract_numbers(x, y):
-        return x - y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
