4749531ecb534dec04934b3d714e538a0caaf245
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:12:52 +0000

    burn congratulate health brush prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
