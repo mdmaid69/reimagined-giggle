0d721694eb410b8e3cc65504b9d82219981b4143
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:21 +0000

    getup hurt geton minimum sight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_str(array):
-        return str(array)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
