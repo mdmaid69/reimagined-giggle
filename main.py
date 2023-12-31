f82dc18e881eb057373f28dbdcd31a549051075e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:04:41 +0000

    window own because design silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
