9d38e93051c55113747363278499082ac2788eb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:11:09 +0000

    today although board nor relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
