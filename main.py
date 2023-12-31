db45f504075a988dca652021143f7c346bb7fa59
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:36:41 +0000

    male wave black definite town

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
+  def remove_duplicates(lst):
+        return list(set(lst))
