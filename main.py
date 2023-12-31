47d16cf18f2c07504a5a0cfd5b0757ba316b7ddc
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:18 +0000

    tongue right imagination moment open

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
