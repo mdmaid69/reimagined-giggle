f64c17f4e095206bad45061c1bbf10b489f88f25
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:14 +0000

    should apparently hardly scene crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
