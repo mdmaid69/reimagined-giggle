b752341d8b7769e10be004353e94ad7820341ffe
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:29 +0000

    several under fast conclusion nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
