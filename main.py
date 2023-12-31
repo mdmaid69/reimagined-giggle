81acd28ef8cc65d9be468a7214e2e95bee27f8e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:59:21 +0000

    powerful herself race load port

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
