9f285d3f8bdac9f17cc2db36511721712ad292a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:44 +0000

    callback recommend spring pour eager

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-for i in range(5):
-        print(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
