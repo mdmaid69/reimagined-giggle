a80fec4f8069ea90a3e337f36e79cd05c8ff32bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:39 +0000

    commit uniform affect hurry unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
