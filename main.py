ed36a7e64327847d5cdcbc688eb4a6b8aca509fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:33 +0000

    goup line quiet reduction light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
