dac821d9ef7220256495b853fcb76c096d54223c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:57 +0000

    sink slice goback(to) this nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(10): print(i)
