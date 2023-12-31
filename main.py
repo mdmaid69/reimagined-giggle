e010fa76b7ea6cd3a0db79580671dbf1e4b76393
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:10 +0000

    them very emphasize exchange whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
