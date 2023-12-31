3ef11c2a847c4919d1c7478c5d62441e6ddf55e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:35 +0000

    street familiar above well thanks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
