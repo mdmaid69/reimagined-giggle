1955249204466a0a71aa350948c4831d00d2a3ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:19:15 +0000

    easy measure any compare so

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def count_elements(lst):
+        return len(lst)
