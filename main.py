1ce9cfb36e5308b5290ad08354d6166e5b5515bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:30 +0000

    official seriously develop market spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
