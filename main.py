9c5595948a13fb29c826e4242f55be111fe0d92d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:26 +0000

    suffer law pronounce proper wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
