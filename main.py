552a0e394d5a07834021d3899b4713e03b79a14a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:01:47 +0000

    fault public fill children hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
