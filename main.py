680fea5b3aaef78b00c1dfec4dec5cb969ee4902
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:31:40 +0000

    runover cheque century duty respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
