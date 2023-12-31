523bd0bab0b6e2bdb6d04935ee6f0e96de63fa16
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:43:19 +0000

    father wake turnup tone body

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
