fe3527dd0b0589c81ef66d14850ae0f5430bd925
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:23:00 +0000

    impressive connect rely live happen

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-name = "Python"
-print("Hello,", name)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
