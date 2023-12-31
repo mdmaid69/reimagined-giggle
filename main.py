7679c7e0c9a8821e12485462473fb6e612d6d195
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:35 +0000

    Mm holdon somehow dish sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
