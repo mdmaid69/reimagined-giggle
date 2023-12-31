05b61f491ab74e340004bb6de0b25fdc951dd388
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:32:09 +0000

    major schedule formal formal amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
