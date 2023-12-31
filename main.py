5978a8f2c3420a4266a5d80d2b778cf773c69b7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:13:03 +0000

    zero cross front string trap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def sort_list(lst):
-        return sorted(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
