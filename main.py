6eb6030e38398725e055cae603951e9584d11942
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:28:34 +0000

    last change disturbing and bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
