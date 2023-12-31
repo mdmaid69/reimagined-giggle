41c4895f2e37c5c61bda9698effdf46b43c21d4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:34:40 +0000

    argument mend scrap murder consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def cube_number(x):
+        return x**3
