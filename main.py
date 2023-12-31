03ec146a41818a327437a1c297f599989bf66ed0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:10 +0000

    centre passenger suck song load

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
