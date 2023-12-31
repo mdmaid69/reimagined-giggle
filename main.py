3399e73ade23c00b5291605cb213165a2bf98261
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:20 +0000

    wash careful hello collect shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
