dd0d98c205c98343ff2bfc31b75933ed948e7240
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:22 +0000

    patience ready speed regular back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+from collections import Counter
+print(Counter("hello world"))
