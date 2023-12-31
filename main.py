c1d52997007dcc58c01c0ea3841183c3882e261a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:25:47 +0000

    circle bar recommend arm coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
