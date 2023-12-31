8a89b750319c63483452b2e69e365d353215259f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:49 +0000

    suddenly thought bye suckme edge

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+def greet(name):
+        print(f"Hello, {name}!")
