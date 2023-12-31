6d3d48b3f2ad810e3f48cb966c46393362a6960d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:27 +0000

    proper mystery prefer major recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
