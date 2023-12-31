021f5defd47c1ebc7a74e99d8dd52f53d461bc56
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:27:40 +0000

    plan 564encourage wait become blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
