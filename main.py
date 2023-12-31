b0cd71162c903857a3c8b0e1963b765465943d0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:26:52 +0000

    age voice land and ours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
