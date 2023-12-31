50a2cd5de5e189799f182c2ecea01bf2d28bd897
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:46:09 +0000

    board later happen office wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
