6d53cc80c3500ea1c3fc9ad49db7ee2ec791d0ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:11 +0000

    admit disagree bored broad poor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
