2ad5a207008435a8dcfb9734f953c39ad23c197e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:38 +0000

    tight same blue nonsense complete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
