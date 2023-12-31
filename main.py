7bb9517829a55903f8aad05b45dba3497a717acd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:32 +0000

    immediately sort explain stamp proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
