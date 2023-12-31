affe483ded9647368ba5dff699a2d67e35ba63c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:58 +0000

    bring none club use suit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
+def find_max(lst):
+        return max(lst)
