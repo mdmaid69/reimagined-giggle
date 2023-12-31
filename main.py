ba5727225b8e5384f0c3f9dbcdaa4bdf6e583e80
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:33 +0000

    left steady brown laugh in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
