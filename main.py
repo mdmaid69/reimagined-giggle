b1d035e2a3f9b2de77cbf3a936e50fb33e24c39b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:24 +0000

    justice stop confident well comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
