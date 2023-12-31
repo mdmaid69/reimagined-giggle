95f3eb27907cff75dd10286feaec229ed33bbfbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:40 +0000

    skin by workon owe aim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_bytes(array):
+        return bytes(array)
