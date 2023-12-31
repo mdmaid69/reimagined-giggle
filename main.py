114c671751ab64d7f6c946be38c7e96f50773d7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:53 +0000

    land only hope speech estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
