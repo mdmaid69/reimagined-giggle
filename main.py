babdac336eb8308440f9702931ee06450609c091
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:21:55 +0000

    middle measure turnon plan mainly

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
