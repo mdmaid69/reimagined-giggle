ddc144a240da200af2febf5be4ecc4e48368c1a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:06:10 +0000

    medium nearly stir but mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def count_words(sentence):
+        return len(sentence.split())
