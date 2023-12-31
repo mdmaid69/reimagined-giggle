dabe524a3e251922493bef390cd3518737733dc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:38:30 +0000

    impossible reduction yes season problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
