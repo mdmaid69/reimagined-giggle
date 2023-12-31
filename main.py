9716dea569d59b26cc69ac05190f2c2f9d15fb4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:03 +0000

    train keepup knowledge possibility excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
