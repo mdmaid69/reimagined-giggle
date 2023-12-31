3afadbfa45d4adc0464e741a1b44b9ce30c09fe7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:11 +0000

    will independent neck laugh embarrassed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
