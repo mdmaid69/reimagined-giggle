fc14e2568d98d724bbd4743e70002f3c18a8cfa2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:11:14 +0000

    previous offer period Kk foot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+print(re.match("h.*o", "hello world"))
