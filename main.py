ed3f84260e42e30ef886f4c2303df9e909df14e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:10:22 +0000

    explanation swim ride invite seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
