869318c88cc221c130865ffcaa00cbec6353307f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:21 +0000

    danger lean black plant arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
