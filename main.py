6fb81c5d7890449ea46c025243a22d4362702dac
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:25:52 +0000

    turnoff spite within warn least

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
