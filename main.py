646a3448df58fd5ffde141a8258b284659f13b05
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:37 +0000

    under along excellent 564encourage translate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
