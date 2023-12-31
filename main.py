3450ec8c84e58e854f31b48775211e97a3f23bf8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:12:46 +0000

    passenger base nothing street worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import platform
-def get_os_info():
-        return platform.uname()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
