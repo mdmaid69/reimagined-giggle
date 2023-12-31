6c868b922583767b54ee5a7dbd2e7172ef995eb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:44:09 +0000

    yet strength while still uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_list_from_array(array):
+        return array.tolist()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
