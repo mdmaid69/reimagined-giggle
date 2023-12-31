6f3db3ac09ae22602787d063b59988442a30f0e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:37:25 +0000

    Oo lookafter tense comfortable listenfor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
