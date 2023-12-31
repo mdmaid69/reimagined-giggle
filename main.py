8302af4d5f35d8ce51c81ace8b4e26edb3b78383
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:04 +0000

    pleased telephone under show either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
