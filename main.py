9e31394a73f95f418b34a63ea4c64a302f91bdaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:24 +0000

    myself market threaten walk whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(lst):
-        return max(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
