f746ae99416dd6066bdaef1e57a7f23ede7778b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:13:47 +0000

    part just us relationship level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
