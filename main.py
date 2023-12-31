25d051932af8a2b69a55695e2a167a224072d081
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:06:51 +0000

    fasten them project passenger anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_characters(sentence):
+        return len(sentence)
