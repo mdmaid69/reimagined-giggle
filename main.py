465e60337b54086d8700324968077f07e685e81e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:37 +0000

    claim benefit listen reduce murder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_unique_words(sentence):
+        return set(sentence.split())
