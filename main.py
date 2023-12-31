1c1684ed14fe9db5e06f3cfffdafbdf47b3df391
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:55:30 +0000

    wish shrink hit successful sticky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
