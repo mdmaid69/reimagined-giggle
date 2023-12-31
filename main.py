e32763d7ee44c9f05270f97b4ea2bff181064ab3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:48 +0000

    coffee decide confident change switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_str(array):
-        return str(array)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
