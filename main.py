7154d3a933ef7bf849d53a256932770f947172db
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:46:50 +0000

    object necessary choice cold kind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+print(time.time())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
