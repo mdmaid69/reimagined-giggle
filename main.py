cb771cd12e41ef29316da478c91993618f4e0d5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:18:06 +0000

    connection display suit breakout statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
