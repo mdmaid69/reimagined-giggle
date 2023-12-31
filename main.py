9aa75226dca29fa6b8cec350245b118809a3d5a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:03 +0000

    balance funny partner original thanks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
