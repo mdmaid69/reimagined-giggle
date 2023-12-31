5f1ffa8a0c2830c0d535f42c4fc4eca18819b37c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:56:33 +0000

    tread lunch wish various dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
