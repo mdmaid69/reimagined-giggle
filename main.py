09fab8b86fe31b854b7e9a5c260678a116427511
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:20 +0000

    busy write view Itself pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
