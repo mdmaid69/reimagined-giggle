6fd4ddc764740ed2a83c2e4e32bfed86c99d6ec3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:31:44 +0000

    stop worth instrument office bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
