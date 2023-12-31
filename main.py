50c974198705dcbb8b4369cf1c7d8bb14ee15554
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:30:37 +0000

    strong friendly division face could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
