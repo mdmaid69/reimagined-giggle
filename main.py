ee985013df0fd085c6787563053a464e907cd717
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:25 +0000

    basically equal lookaround much check

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
