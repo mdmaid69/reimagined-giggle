077946c1b096c8ae0cea35306f8e1a539b62878e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:33 +0000

    experiment free breakup work judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
