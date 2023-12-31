45de5e8c1dc6d3012025e1d7dd1b8bd151996f2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:10:54 +0000

    start gift damage disturb exhibition

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
