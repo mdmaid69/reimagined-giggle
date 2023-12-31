f0384cf33b2dccd5aef798f8c979bd8d3b97cfd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:53 +0000

    welcome now might appeal factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
