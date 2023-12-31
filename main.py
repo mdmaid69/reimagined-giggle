aa56556fbef2726b50debdf8e89af7f9d7db6e31
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:31:20 +0000

    share appeal row please hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
