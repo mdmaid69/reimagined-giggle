e917bcd1a9741f880ad5283278d9b9c1a0f4aa56
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:43:14 +0000

    quickly ability post temper lay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def convert_to_hex(n):
-        return hex(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
