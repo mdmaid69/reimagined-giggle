59d6451d38b291a53bae83c8cddb2822566d251e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:20 +0000

    married dry accident build drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
