4e863a796e5e70984dc9985de0c8f62d955ff7e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:43 +0000

    build brush scrap power furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
