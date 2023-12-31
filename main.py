5e881ed6b1d656dfedb7f644fc18ffe44a474d75
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:46:09 +0000

    reduce artificial 573entertainment enthusiastic shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
