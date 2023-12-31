33678ccbc58845e4baca9ce5a8a606b2b9c10c3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:54 +0000

    scream reason pupil steal criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
