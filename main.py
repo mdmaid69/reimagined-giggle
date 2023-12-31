27fc10b5880b36c7cafde1f469cea3fc91989d3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:56:14 +0000

    gas improvement write guard achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_palindrome(s):
+        return s == s[::-1]
