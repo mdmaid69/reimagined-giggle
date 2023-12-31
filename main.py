6f5266bb674c7945111e678e54366f8d64242d61
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:46 +0000

    because wide age at achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
