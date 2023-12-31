2c48eb55f767dc810330ae6c7f5ead5afa40cf8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:27 +0000

    surprising pattern excuse case his

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
