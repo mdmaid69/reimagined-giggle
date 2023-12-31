71b498cd8b5e76f006d6fc3d8682edb6a8ed7ba4
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:56:53 +0000

    miss sudden trust test protection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
