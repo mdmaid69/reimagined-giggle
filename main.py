03598209d192377a1396c972c09f3d2a8bcbc9ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:09:10 +0000

    warning steer previous want breathe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
