8e186820864925b73b36d52f280fb1cfbb80d414
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:49 +0000

    cold honest live serve muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
