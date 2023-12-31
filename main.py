81127437bd7a1cc4c6fd6c2baa2d03f4ddae19a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:54 +0000

    fat determined tidy generally in

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
