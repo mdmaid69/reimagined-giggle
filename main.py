e8012d3f635ad836f9ff921096808b6927c86c56
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:59 +0000

    young scrap pay boring chat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
