fb6a8527627f26b4930611d91eabe14efba05ccd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:25 +0000

    afraid thick clothes scream supply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
+name = "Python"
+print("Hello,", name)
