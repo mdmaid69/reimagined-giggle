8b209a43f49370cf9d6f1a266ddf5ebe3016f9d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:53:22 +0000

    means attractive tip female effort

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
