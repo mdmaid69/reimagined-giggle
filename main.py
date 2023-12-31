7f66f551f884923547f47dd1e6e86702dc1a2e0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:12 +0000

    cause fear rubbish whom all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
