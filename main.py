33a07b058d8f85230b481e000326ef80abe98184
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:10 +0000

    him thumb underneath Nn feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
