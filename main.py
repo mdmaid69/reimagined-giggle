05dfa33d3dc0b871e865340480af77d5f7b87590
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:25 +0000

    hit crash forgive dress Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
