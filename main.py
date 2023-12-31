7ecbb30799e730e7bb73b5ac5023f313e9f62edc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:53 +0000

    range attempt flow bend meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_acceleration(speed, time):
-        return speed / time
+for i in range(5):
+        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
