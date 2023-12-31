6750041459a9e5c27dd1fe47ddf5a61d413a22ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:29:06 +0000

    exchange front yours content ever

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def greet(name):
-        print(f"Hello, {name}!")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_min(lst):
+        return min(lst)
