868ddb0b55739d740080392329d467f2ffd601c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:44:48 +0000

    before watch region side call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
