e1bf8482894fdb98a75e538be7c0121424b72af6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:09 +0000

    match information present flat bite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import random
+print(random.randint(0, 100))
+def remove_duplicates(lst):
+        return list(set(lst))
