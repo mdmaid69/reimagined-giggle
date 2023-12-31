0ef73bf39b79e7b13499561285dbc6481f442cc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:40 +0000

    desperate comeoff seat fresh involve

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
