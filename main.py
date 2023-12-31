72e0906b65e1e876751f39a06c6670df7cd42932
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:39:25 +0000

    male might change serious storm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
