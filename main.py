09953782b6ca1c6f7757cd55503fdaf5115d8f56
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:31 +0000

    wise negative afterwards thing lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
