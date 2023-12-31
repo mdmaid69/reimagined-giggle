778372f32ef4ca882cf157f1fc5bc3621b235506
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:13:40 +0000

    means settle hit Ii prepare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
