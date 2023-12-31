83e15f3e850d78e3d46099a48610a4146faeafa4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:55:12 +0000

    back point right nurse before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import sys
+  def get_python_version():
+        return sys.version
