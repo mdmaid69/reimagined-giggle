422639b06541fa9848618d798d7930fce2fc6527
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:47:49 +0000

    comeout fit popular loan breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
