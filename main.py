bdba412adc32e05be7e522efb3dcd097c2dd08f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:37:11 +0000

    alone loose its bring stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
