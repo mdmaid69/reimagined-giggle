8190f95d342da0c39bef7f3b68cacea9071e1a83
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:57 +0000

    answer breathe only married kill

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+for i in range(5):
+        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
