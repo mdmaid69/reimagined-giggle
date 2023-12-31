ffea03a4cc02262579309c84a6705a99d3b47fbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:25:40 +0000

    terrible measure innocent see nose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
