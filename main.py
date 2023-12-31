7f21d237e8081c70e7b6fa5227c105482a786ca4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:59 +0000

    near work dare quickly treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
