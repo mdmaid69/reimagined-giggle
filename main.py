a5aaa79c0a17b27ce450a1bf8154154da70054c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:19 +0000

    aim key round medium wherever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
