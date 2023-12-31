f40595002abdb3753109035252db1eb858847438
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:30:21 +0000

    earth funny branch secret term

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
