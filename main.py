3e4b0317834d036b07e7aef738fac6af7ac2f659
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:05:21 +0000

    rain dead trap hurry oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_work(force, distance):
+        return force * distance
