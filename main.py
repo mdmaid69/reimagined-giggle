2b92ebf7c5eee97f5f0384665d4cd982512a14bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:48 +0000

    deaf reject who wave opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
