e94263b62f9826a5b7c3f22ddaa892bdc165b797
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:52 +0000

    work will phone public sure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
