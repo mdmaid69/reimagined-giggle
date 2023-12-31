66b13838274a642c2b4d8f193eb8f97a9e894129
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:51 +0000

    load disappear expression straight mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
