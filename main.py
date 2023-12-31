162647681b9a507cea939330bbf6fd113a85a137
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:56:03 +0000

    rain mad free deserve hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
