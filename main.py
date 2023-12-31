7428aa950123b7f26de004b5d69dbfe2aab4e793
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:10 +0000

    fasten impossible slight argue downstairs

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def convert_to_hex(n):
+        return hex(n)
+for i in range(10): print(i)
