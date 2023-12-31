5c5b760a60ab61ce873a34b9b21603895a19a4c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:06 +0000

    absolutely achievement tax care exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
