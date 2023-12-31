b845587ecc1b733dba5236e6e6bc9b0591d867ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:00 +0000

    pile fetch usually calloff total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
+import sys
+print(sys.version)
