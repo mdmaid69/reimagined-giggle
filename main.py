fd745101d627d05fba929edef2b4e1d020c540b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:19:06 +0000

    order gap bath unlikely pride

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
