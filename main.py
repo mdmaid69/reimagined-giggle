5ed78e2779bb6e9fc5375149e66156c18da8e6ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:33 +0000

    service cost whatever round convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
