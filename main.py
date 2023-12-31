5d818d86a93aa6d63bbe4651fba1b58a5f4fd29c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:57 +0000

    be separate name pity because

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
