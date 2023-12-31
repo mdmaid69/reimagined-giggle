7b697e527aa80cc375899dcb4999347f626f21f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:37 +0000

    unless share step study pause

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def divide_numbers(x, y):
+        return x / y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
