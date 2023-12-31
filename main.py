e43088ff3e57072a0e55e3d56d5cdeb4b4ec13c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:28:02 +0000

    choose Ff company food woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
