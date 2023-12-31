6f8bafec2f9ffa8f82f9d15f05c1f815d7fd2dda
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:52 +0000

    star great all lookaround herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
