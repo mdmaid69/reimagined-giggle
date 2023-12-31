173b94fd40b56ff2d15ab2a20540d88b5aa96d5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:37:38 +0000

    weekend upstairs that tomorrow consider

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
