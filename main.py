7b3668933dce3f65bb0db9627c68da09e021e4da
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:11 +0000

    professional refuse sale create somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def find_max(numbers):
+        return max(numbers)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
