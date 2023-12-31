7ea37c6c12583853b993b9d459e71f8bd335782c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:43:03 +0000

    begin along find height soft

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def split_path(path):
+        return os.path.split(path)
