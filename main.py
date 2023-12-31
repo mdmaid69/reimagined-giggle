4458c67209925243fb1f6fb8fe6a31fcb190f964
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:15:48 +0000

    estimate juice tight stare off

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
+def find_min(numbers):
+        return min(numbers)
