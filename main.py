43006808b5b7e72a240dd4ef1fb3d276249cfc52
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:18 +0000

    after shoulder help shut trial

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_repr(array):
+        return repr(array)
