77dc09aa60b8cc14605e496695740ded7240c520
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:36:19 +0000

    unless patient fairly happy workon

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
