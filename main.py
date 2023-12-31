a2dcf944cedfb3f6e7fdd7bc9d0cb4e61579a8b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:06 +0000

    needle him his thin push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
