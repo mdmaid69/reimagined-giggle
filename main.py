f922579937135eee02b2031d822fed4accd7526b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:01:18 +0000

    bit pretty swim takeaway fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def find_min(numbers):
-        return min(numbers)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
