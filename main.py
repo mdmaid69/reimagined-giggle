3b8853ae8fa52e711e9107c67f0166c835c5fe4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:53 +0000

    whistle mine before female extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
