438d9f4f06963a908e94a18f71c9b579820ded91
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:04 +0000

    deliver storm main offer squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def add_numbers(x, y):
-        return x + y
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
