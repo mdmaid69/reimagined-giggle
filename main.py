ae133ff70d56e6f176830f9a6edf9351d14b860a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:09:25 +0000

    anywhere pressure payment scare combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def find_max(lst):
+        return max(lst)
