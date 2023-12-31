4beee304413b9644e6ae2f02264dd064b76741ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:21 +0000

    speech arrival he insist copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
