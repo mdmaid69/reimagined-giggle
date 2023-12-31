1f9bb46d2c668b0d2edc87a8253f66776cb090bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:13 +0000

    light private study contact answer

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print("Hello, world!")
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
