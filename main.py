1b7f34fe055a2551b5646d4eeaa06bd4dae08045
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:48:38 +0000

    soft intend impressed accidental open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
