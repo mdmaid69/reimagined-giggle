752d390533d1c2be25ec0fa1c9dd5e4e41b886ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:13 +0000

    because sister service think late

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
