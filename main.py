c6ea914b5f3bcb7c4a8c297026e574be95f9193d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:19 +0000

    cough visit silent win check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
