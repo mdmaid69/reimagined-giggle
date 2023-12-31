6be603aafa0b7328c0fc6ed4c0424ba1c94ac47b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:50:20 +0000

    bad confident this duty blow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
