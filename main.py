c5771f4d081cc19d590e3495bbf955e18fc6d3f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:59:40 +0000

    right never worse crash should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
