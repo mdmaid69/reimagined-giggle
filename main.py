2e81dc904b6eb012e174dab9b004c01633496dcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:59 +0000

    steer sad often family sign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_density(mass, volume):
+        return mass / volume
