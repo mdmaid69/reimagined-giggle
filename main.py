2397fb0e546dd7863275466d0c01edddbb26b8f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:25:55 +0000

    bright thankyou awake trust petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
