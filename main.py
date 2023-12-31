129ad1e3a7396f75bb62046b3649f66a53a1b937
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:45 +0000

    lead vision look now 573entertainment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
