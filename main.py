be08859a75dd2a875149db786cdedc3ee6219cd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:06 +0000

    exchange tear Rr track round

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
