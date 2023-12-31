85118269e71691bf3c8bf97c3fc9173ae642d9c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:01:05 +0000

    face custom apparently tense direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
