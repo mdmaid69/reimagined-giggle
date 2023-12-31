dbb2ed64831835744326721b0f413934c7bcb188
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:59:00 +0000

    advise average breakdown drive word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
