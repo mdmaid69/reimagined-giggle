afa332e14cd5be0f7dd898ae201a4f3d1385df12
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:52 +0000

    breakup raw print stupid mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-print(re.match("h.*o", "hello world"))
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
