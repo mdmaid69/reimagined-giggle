293953a9d4e6bc0ec080f49e94471fdd6da809c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:29:54 +0000

    history earn holdon miss bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
