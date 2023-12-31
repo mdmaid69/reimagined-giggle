17da058f99abbbcfde521826aef0b14cc63d41ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:06:09 +0000

    could takeover whistle inform pleasant

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
