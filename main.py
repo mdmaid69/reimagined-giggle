c73bbafae1d2994316f5622c987e11f11df87cad
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:59 +0000

    mind say few wave congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
