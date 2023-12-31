755eef1d6790120230c8584895bbf447318eb362
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:29 +0000

    same control lie invitation favourite

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
