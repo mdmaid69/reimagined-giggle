b96400c9d7635d4ecfd2d1fef03db9309f8b4c9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:52 +0000

    frightened excuse shop perhaps mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
