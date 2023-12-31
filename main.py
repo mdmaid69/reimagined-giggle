a14d64cc1b6517e02f214e3e421ba8cd4b42ae02
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:34 +0000

    565end cold comeup spread soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
