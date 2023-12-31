d404b808aac2cbcc2de92fe0ddcf31564587030b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:30 +0000

    each Cc should meet begin

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
