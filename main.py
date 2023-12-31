741b5e6762576277f44262c6b9cf2acde004b79f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:33 +0000

    gather at excited key everybody

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
