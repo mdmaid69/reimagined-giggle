7f7d2a7ea27afc9a1ddda1908662ed3334dc1296
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:27:32 +0000

    small thought dare comeback flavour

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
