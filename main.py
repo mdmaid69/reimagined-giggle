45020a15c0a735eecdfd11b9d3b8d6fc2e7f46cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:07:46 +0000

    couple fine fat train within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
