f6e539e685553db813a6b09614dde2c29931044f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:27 +0000

    taste equipment beat yet sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
