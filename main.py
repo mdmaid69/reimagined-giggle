2c291a629d1eedc4af38993e241a507e142c5677
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:31 +0000

    suggest door face finally keepout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
