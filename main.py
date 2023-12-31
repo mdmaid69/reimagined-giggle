dcd41a4b00a4cff44f870b07254495a483983700
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:30:00 +0000

    used live close turnout Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_floor(x):
+        return math.floor(x)
