a699ac17b0074e33bf5a9087f90d324cdd5c9bc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:12:43 +0000

    effort damage uncertain contrast bury

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
