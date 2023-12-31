f924d14f827f0c5fe065c688de4257cc864503c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:46:54 +0000

    report wander boat offend change

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_prime(n):
+import logging
+def log_message(message):
+        logging.info(message)
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
