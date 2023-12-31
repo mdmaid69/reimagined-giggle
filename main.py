de5a0a1ceab5d021148f1f06353ad37b2f16516b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:49 +0000

    cost disapprove ring first narrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
