1e2e7e54c03b3c43f1813df54bbbf8978ec67701
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:16:02 +0000

    preparation tradition surface high trouble

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
