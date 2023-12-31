68e63785d30b82bf3dee3af31ec971ea0d49ce59
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:47:23 +0000

    share somewhere intelligent until rank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_octal(n):
+        return oct(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
