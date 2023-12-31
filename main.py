8a793c4f8a5a706d7b7335033405323695572b46
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:37 +0000

    drink meaning could hurry slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
