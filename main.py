46ba780882b99af41fbef401359c5b9bb5e0be89
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:37 +0000

    better same ought provide shoulder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def add_numbers(x, y):
-        return x + y
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
