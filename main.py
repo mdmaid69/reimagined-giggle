02e80054b2cef1e43fea694647f930a434e0fcde
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:00 +0000

    wide breakfast politics crash quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+for i in range(5):
+        print(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
