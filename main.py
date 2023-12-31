c6b6cd3113a138ff4365f0ac217c46c3795a74ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:38:02 +0000

    lost honest offer deliver side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def convert_to_hex(n):
-        return hex(n)
+def find_max(lst):
+        return max(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
