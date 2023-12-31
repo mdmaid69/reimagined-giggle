44351c7eec3c8984e3eb5275a90c488dcc05522c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:38 +0000

    idea bad message limit pair

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
