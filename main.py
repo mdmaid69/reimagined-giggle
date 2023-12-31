d4ef8263722a30fb965e76cf8ac5bbb5f83c11a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:11:34 +0000

    point pay nearly content amount

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
