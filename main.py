59ad1a387d6517b04f6511786832cf973477771d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:06 +0000

    injury smooth delight yourself name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def convert_to_binary(n):
+        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
