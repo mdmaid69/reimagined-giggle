5ffa2b0fc56eb7263bcc25a10d8fd56a425253b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:09 +0000

    correct rank container summer uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
