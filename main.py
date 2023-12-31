8691e858941bebf8071eac3352fa57df07ca9c97
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:48:46 +0000

    mess separate something paper tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
