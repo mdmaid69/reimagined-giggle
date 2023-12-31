7124442fdd2bdaf584408a98de7d0ea1c5314f57
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:12:54 +0000

    corner warning spite introduction start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def clear_array(array):
+        array *= 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
