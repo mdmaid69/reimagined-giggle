01d46be9e22b9b19f5beec32c9e94ca54bc2c257
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:50:12 +0000

    rather generous afternoon everything illness

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(numbers):
-        return min(numbers)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
