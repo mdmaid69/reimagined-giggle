80a347696dd99b3b5e1494913ed96322d48bf240
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:02:39 +0000

    necessary male of pattern quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_min(numbers):
+        return min(numbers)
