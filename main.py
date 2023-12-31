0a7e37656db2784d8b1513f89f94f89f994a8756
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:52:30 +0000

    mass politics danger yes doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import sys
+def print_python_version():
+        print(sys.version)
