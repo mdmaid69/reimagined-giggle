4a50c0878e4803eb9db518793e65e0c61bd71973
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:28 +0000

    just freedom work maintain wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
