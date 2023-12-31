8d78429e5f2a0d62c0fba784ef24b025b8e74ae6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:35:49 +0000

    Jj relax accident life operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
