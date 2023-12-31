d17bc0caf2638d4c957beab39772f228aac60c19
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:47 +0000

    wall hospital schedule build question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_length(array):
-        return len(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
