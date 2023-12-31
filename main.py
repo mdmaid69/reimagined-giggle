5a8b84929c54664fb4badaaca91fb6b11e28f5ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:33:25 +0000

    patience independent present tooth money

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
