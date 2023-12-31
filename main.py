10af856996d4b22e349c06c9980d716896bdbec3
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:08:32 +0000

    branch message wheel wide bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
