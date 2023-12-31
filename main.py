b12f5d1368f6b42090e8865e9af230e48fb91615
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:33:47 +0000

    thanks powerful steam whenever collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
