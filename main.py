f9aa215d1af718083eb6ef1e4a3e8e0b1646f888
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:35:36 +0000

    mouth emphasis yet bury slightly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
