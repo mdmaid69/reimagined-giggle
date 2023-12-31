fefa068b9508a4ac1ad3fd52ab6ba1df22366c74
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:45 +0000

    paper question radio chin bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
