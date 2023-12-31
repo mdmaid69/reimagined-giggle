00e7bab003228e2fc3f69dbc0e03f3ba1da5bd14
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:50 +0000

    burn comeout talk complaint scared

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
