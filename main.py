81ab7367a2ab9a02463508cdd159beae0c44c0f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:51:02 +0000

    frequent dress entrance dinner patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
