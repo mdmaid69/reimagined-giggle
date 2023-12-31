c91d297deecc8cd93beba2a33b0d05fc3d199612
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:22 +0000

    lookback(on) ask fresh number dark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
