b24ea39e9b0e3c6cf94fcd924e7481e792bf3cbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:06 +0000

    there smooth out anywayanyhow address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+x = 10
+y = 20
+print("Sum:", x + y)
