60438ce0c83208058690e6b3e0985ad21c1fecd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:46 +0000

    brush bar unit advice possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
