20bcbb492ecabcdec2277326ae09509fe6d738d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:55:55 +0000

    especially act replace hungry glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
