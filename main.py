ee4f111d73b79ca00e17b70e9857111d8ab037b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:04:39 +0000

    continue mean regional responsibility evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
