8df1093a9578ce90877c8d112bde2008854439e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:57:31 +0000

    radio same turnon thought crazy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
