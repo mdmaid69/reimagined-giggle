b8f96a2090210c89f01ca618ff4593fec89e56ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:11 +0000

    priority steady store kind group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
