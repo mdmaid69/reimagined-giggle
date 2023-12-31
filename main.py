2eb458f12200e16f4e25efcffaa4b3ff4d84cd1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:00:53 +0000

    round taste trouble minute charge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_unique_words(sentence):
+        return set(sentence.split())
