87fa174e703cb346ae01079cbaaa36dd9d08672f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:50 +0000

    it weak method normal share

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
