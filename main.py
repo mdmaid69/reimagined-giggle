4fd380e487c20241d4d81288db4f50bf446e9dcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:33:20 +0000

    goback(to) owe night force public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
