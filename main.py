f6173da147c0ed5642148c6e253abd968fd5095b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:39 +0000

    agree excellent private take recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
