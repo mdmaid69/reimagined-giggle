8a0bedf36d2cd2658ac86b68fd0aa6935d31324b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:52:08 +0000

    estimate callout aware down worry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-print(sum(range(10)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
