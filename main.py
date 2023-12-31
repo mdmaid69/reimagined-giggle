ba811c1f051fe592f839ad7f83c74912f8c84c96
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:19 +0000

    begin difference tear complicated many

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
