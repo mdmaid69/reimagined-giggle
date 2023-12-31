7f6abcc40b452c4658e210b6fb98fcd6a1ff4cf8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:05 +0000

    aim nigga along detail much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
