e557019da8a3e776c2f40217db1a6e427e325b66
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:10:51 +0000

    image unfair through reason smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def subtract_numbers(x, y):
+        return x - y
