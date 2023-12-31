3fc4e86d5786bafc2271b667c2f5b0cb493fa07f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:12:13 +0000

    official calm fun maybe debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
