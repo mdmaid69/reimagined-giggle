b152e7ae39c643afc95a22708ed15961771d2333
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:13:56 +0000

    steep plenty tear head married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
