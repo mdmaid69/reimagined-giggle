97bcf08e39f454322f02f21a1bfe28e3a8ee3912
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:25 +0000

    original permission Vv live fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def clear_array(array):
-        array *= 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
