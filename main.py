d3a0e09d8f11c76d4947a96472ae43e47a0d2848
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:42 +0000

    vanish complain lost guess appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+print(os.getcwd())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
