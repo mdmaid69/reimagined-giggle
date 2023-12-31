ac808c44afd9b2af0cf2e02654ef20735491e0d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:43:22 +0000

    path unusual habit comein light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
