6007f27be5cae9ad2df2e7113de0cda79d416c80
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:36 +0000

    visit may wear lunch firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
