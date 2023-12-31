6f083b8188fe628a18ff9c7439563833a988ad23
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:24:28 +0000

    obvious neat bring eat obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
