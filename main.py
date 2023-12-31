4adaceb8483524af2ddbd20d53e105e2651afe94
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:21 +0000

    only turnup approve joke trick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def create_user_list():
-        return collections.UserList()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
