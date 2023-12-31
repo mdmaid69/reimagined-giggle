4c058baede5f77acbe2129e8a135d7da0dfa7b27
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:09:11 +0000

    minor instruction pump pattern state

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
