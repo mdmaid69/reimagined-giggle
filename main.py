a118337f3ba83fc6ce040935335906d6595d6378
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:03:30 +0000

    translate old century Dd wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
