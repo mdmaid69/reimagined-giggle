4fbdf83b51f230bd0331cab60638f2dbffd8fc84
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:26:43 +0000

    abroad material declare exaggerate shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
