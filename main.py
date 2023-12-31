fa97fd690f4dd28b15f2d6b76d570bdb1bfadbc7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:36 +0000

    borrow shop concentrate shade wide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
