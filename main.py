2f4f3df324c4c4c590ab139aff160a015723c452
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:45 +0000

    round small brush exciting talkoutof

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
