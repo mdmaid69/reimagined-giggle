81592e54d3beb865995a2a0239babe62792fce4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:03:00 +0000

    567enemy knowledge tense hers throw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
