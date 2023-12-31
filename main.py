5cb53b399cb9bd333dad3529800375d235168ac0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:36 +0000

    nose mark declare better support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
