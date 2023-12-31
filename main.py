9ced173dd57808050c364c9c215f8785bfb01ad9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:38:55 +0000

    ride talk work probably free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_set(array):
+        return set(array)
