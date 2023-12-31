f2e973754be3c66e2ce5b3d7428ac4244856cca1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:43 +0000

    sure add seat discuss right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
