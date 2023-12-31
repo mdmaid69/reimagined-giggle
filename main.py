af5e0d0210784a67049d1ce8bc4a545efb794ec7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:22 +0000

    see way backwards suit perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
