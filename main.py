41d3cea3b9248926b90ce15708cb77a43819332d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:35 +0000

    treatment finger round through standard

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def add_numbers(x, y):
+        return x + y
