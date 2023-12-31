91f7d59e5c3b72e7ea8a797890fc08bca97e4708
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:28:46 +0000

    regard hear lucky practical bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
