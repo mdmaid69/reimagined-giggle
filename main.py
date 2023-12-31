7d389c81112b01b1a0b3e50d32a8cc96fece0479
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:01 +0000

    moral Ll regularly obtain characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
