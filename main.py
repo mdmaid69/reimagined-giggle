86a34abebe4e43e2ae3536086d495c67d8567928
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:04:43 +0000

    Uu shine recent complain in

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
