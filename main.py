598a1fbf4c05343605c411deca754b68a3f7bab1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:08 +0000

    square raw danger breakup letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
