aea919cdece4b176d8466b9c0caa3714455c8069
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:02:29 +0000

    prevent disturb measure container giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
