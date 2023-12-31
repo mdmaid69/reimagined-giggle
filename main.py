0dbf8eefc81793b2c8fcc70950b26380a01fc203
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:04:32 +0000

    worse any exchange this factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
