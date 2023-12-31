ed803ea29a632afbe71097a09cdb8f391c086ed0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:27 +0000

    relax clue their arrival nowhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import collections
+def create_stack():
+        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
