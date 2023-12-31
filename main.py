f89cc92e35b5c8ed549460a2c904187ca4348085
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:42:41 +0000

    Gg introduce this suit generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
