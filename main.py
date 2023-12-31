18911495246d6ff11a16001c864f7b5348f3abed
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:46:03 +0000

    sensitive punish name invitation anyone

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import random
+print(random.randint(0, 100))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
