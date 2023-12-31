bd86a0e001d15b5eed92645523f9ae3dd57f8c90
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:51:28 +0000

    union unit advice break near

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
