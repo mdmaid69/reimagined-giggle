d17f9b9844e787c76f6763652e75d0807def5a93
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:29 +0000

    separate related scale emphasis search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
