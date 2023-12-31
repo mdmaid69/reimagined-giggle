3adf5b3190a8f191577514c39aa638203c0f2528
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:34 +0000

    pattern scheme satisfactory thin rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
