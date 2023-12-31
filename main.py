c6141dbcc09ccbaf62e8beee6d6ac6fe2420a7a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:49 +0000

    pair object song anything suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
