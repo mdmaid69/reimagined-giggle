4a1d5a4fa89ccbaac23278fd7d6bbb03f1ce1bcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:12 +0000

    respect quantity control male represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
