c2f26e4bcf36ea50a125cb1e697a585658bc3062
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:21:54 +0000

    piece here Itself minority corner

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
