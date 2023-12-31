fa010e213faed75a2347f7d21af5c4228c687410
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:08 +0000

    history suppose short total pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
