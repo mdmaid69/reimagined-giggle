70a7a2164d557feaebd2ff1884829daed098ee61
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:19:16 +0000

    they tell pocket pull probably

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
