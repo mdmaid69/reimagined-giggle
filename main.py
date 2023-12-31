1d2ecd1e82ecf1ca8808e430505e0ded63139e54
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:39 +0000

    appearance demand disapprove splash comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
