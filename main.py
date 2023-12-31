f5065f8b0e19bbf183b5321229aaa24a32070912
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:46 +0000

    squeeze carry straight keepaway church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def is_even(n):
+        return n % 2 == 0
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
