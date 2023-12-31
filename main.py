54a737bf374260d0a0fe4fda0c7489be0f0cfcdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:24:27 +0000

    hate suit giveup programme path

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
