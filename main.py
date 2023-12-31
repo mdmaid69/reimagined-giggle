ee0526c9dfe0b010e21d1d6f7d132061d57e50ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:30:20 +0000

    gradually sign rubbish cry somewhere

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
