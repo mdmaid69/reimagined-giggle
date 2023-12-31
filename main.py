e7e6ace85a40c4d6026f0bf8fb84280d6adfa86c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:25 +0000

    season exit relation drink excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
