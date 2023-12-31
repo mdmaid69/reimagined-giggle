26c386e234543cfaeee4d921d66b671ae75e67f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:55 +0000

    increase understand squeeze loose actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def is_odd(n):
-        return n % 2 != 0
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
