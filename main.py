909725cbf2de68fd0a4e9147e36554ed29677cb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:39 +0000

    favourite boil offer rub thumb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+print(time.time())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
