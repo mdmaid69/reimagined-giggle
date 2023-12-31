3a2dc86c3a32b8e47f5917a8c25ccaa874327d93
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:47:43 +0000

    car basic window anyone furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
