9c8e220635ca7244f2e8b5174861ddde84183f37
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:26 +0000

    nigga runout unite cry talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
