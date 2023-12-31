ccc689abe26f0f64ec4d966fc83e6fce1c9c6330
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:40:13 +0000

    consist base threaten forever sale

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
