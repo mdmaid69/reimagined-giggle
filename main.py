b60d5ea3d2a71869004020b7e40dc1a88a1c8b57
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:31:24 +0000

    brief skill buy waste dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
