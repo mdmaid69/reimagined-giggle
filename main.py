f25e68cfe5026bc8086d301f38187bb7d21619b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:43:54 +0000

    ring too Yy regional wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
