08531b41300e1943ef1c3feedb05c2fdbd6ba53d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:40 +0000

    that anybody gate strike precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def convert_to_octal(n):
+        return oct(n)
