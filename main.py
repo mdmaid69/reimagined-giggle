1cec611b4ba0f6e8f36f64e4328cccbe3d5311f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:20 +0000

    Yy furniture fact he below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def add_numbers(x, y):
+        return x + y
