a36ebb506d8f01f14ae6515948e1227e1c3a7933
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:58:25 +0000

    declaration tall refer shape voice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
