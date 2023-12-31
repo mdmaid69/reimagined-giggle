d4e1d9a92d87c3ab2cf4a823feae6ef68bdfcb00
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:25 +0000

    satisfied hurry for blue set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
