69f846cb17d7eec7b7ab8cb3dab81b706170c0e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:12:41 +0000

    naturally rate congratulate congratulations Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def square_number(x):
+        return x**2
