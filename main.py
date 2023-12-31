9a18c55e0a3e705e2a9c98f73b78c94593aba929
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:26 +0000

    polite father trip pen far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def convert_to_octal(n):
+        return oct(n)
