7752c0007e054468270863516eab1a118baa5dd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:40:12 +0000

    steam gift edge this tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
