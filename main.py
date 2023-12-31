51e71b3156f485ffc2d38fb95819f362e573104d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:30 +0000

    drop guess base visit father

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
