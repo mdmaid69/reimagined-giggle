21129e63154dfbd58a50dd3013a3b35d47de736b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:37 +0000

    wish snow tool understand like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import tensorflow as tf
+print(tf.__version__)
+import collections
+def create_queue():
+        return collections.deque()
