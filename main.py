fdce30afecb9129d928c8227372ab88bf8e81d4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:10 +0000

    generally common serious bag but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
