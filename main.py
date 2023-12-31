c9dc57223b6aef12a4f9cc2e14a00704a5e7ea37
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:13 +0000

    collect method building today score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
