7fae48df1f73b79b0ac45bcbe875f46793f71d49
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:48:15 +0000

    attempt by paper last succeed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
