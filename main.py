8f9c5c6eb480ef11b829458830d61d2bcdae1292
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:32 +0000

    turn getback increase immediately turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
