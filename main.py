fb6f95eecb2cf1968a3227ad3ec3ce475eac15b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:32:58 +0000

    unkind unkind dry already off

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
