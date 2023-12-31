e22a9e0a2e752c07b3107d0baae8c645712230f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:49 +0000

    most yourself sore reasonable second

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
