6b98c482af617726c8e5740822825a2a4fabeec5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:58:54 +0000

    save comeback example practical sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
