f139ccab2f4a52715151c7e2a36b99084f88e881
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:38 +0000

    earth beyond wash wheel long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import platform
+def get_os_info():
+        return platform.uname()
