c2fd1cb8b8b3f905570df032dfdacc544601ce96
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:32 +0000

    keepoff happen notice mend fill

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def square_number(x):
+        return x**2
