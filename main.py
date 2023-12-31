03e05b83507faf83456d068b6955698ee87c85dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:54 +0000

    view spread home expect university

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
