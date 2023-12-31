965eaa4c52beeb6805df4a5b5751910a1e79984c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:11 +0000

    suit persuade beyond ahead crime

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
