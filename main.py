a83cf44d588bb178f4b3a11d9cbdd88b53a3567d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:48:46 +0000

    hurry innocent explain release culture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_even(n):
+        return n % 2 == 0
