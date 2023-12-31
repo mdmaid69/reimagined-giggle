9446e2a3a68d2b9a4b5dd8167ccac99637eda225
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:29 +0000

    whatever hear stage Tt stick

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
