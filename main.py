7d5923b327626992a498a0a7dfe32144d95fed33
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:47 +0000

    petrol dry profit attractive passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
