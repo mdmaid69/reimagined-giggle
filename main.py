705c4cc5b67363d146e48c08295592037c4ac56b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:25:06 +0000

    lead careless disapprove ordinary dark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+for i in range(10): print(i)
