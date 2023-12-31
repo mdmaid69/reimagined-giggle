dcd08769f346848952d803a82f77597c08403fe1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:09:20 +0000

    ability honest turnback neat laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
