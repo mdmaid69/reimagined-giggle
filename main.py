7714a66de3f1a82b0e4ebf3bfcf2f70ae0231449
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:58:30 +0000

    around detail let anybody swallow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def cube_number(x):
+        return x**3
