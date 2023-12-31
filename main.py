fc961243160914bc85be3afa1e397cdd913e0d9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:35 +0000

    giveout play treat cry even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
