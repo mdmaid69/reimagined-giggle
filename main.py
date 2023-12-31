5b2b5660076c5755bb25ceacaeaebd5349e0d99d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:59:45 +0000

    risk ignore rub march help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
