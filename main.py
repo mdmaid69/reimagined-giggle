76bc25de6b09759f0fe28ef5b7fb9080fecf7889
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:12:20 +0000

    doctor lower makeinto left sad

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
