8e353d9a8226ee73051cf1da4143e40f2a1ec473
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:54 +0000

    hole cope catch good nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
