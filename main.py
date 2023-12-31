0d1fd1fb6b7733ad54e201459116a336377a7c70
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:20:15 +0000

    soil fat chat talkoutof nose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
