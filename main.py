d99753675bc66c21f4aa656ed52313a31f64fafc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:01:18 +0000

    direct mother marry bear stamp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
-print(sys.version)
-def count_elements(lst):
-        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
