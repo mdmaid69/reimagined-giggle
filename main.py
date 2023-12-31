6b744f631dca93c9abe189eb84f268ca14ca2cde
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:14:46 +0000

    close shout how waste fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_work(force, distance):
-        return force * distance
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
