a4cbe1b4bd8a3a867df6f61966720a8830590f2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:06:40 +0000

    quarter original future react bring

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
