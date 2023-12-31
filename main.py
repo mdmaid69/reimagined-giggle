a6b84090c01c6643970838b96c38d591cdf65569
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:41 +0000

    contact yesterday step my carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
