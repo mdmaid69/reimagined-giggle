8684b34a0906da8d44b00ab8a6ebae6f95eccea8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:26:14 +0000

    complete back show her bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
