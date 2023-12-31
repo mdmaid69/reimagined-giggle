bacda83cd61b69df3ed5d9880dbb25d8b83ea889
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:21:56 +0000

    city ever concerning Hh deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def print_python_version():
+        return sys.version
