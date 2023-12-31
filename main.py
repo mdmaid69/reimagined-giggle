6a401ff1161480e44e2ed9db9fe98b8353c2cd3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:44:50 +0000

    mention different thanks decision creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
