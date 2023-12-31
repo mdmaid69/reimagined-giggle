2c65f2e5543bb1a8ac88d722d2261957f2cefef9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:03 +0000

    sound quick trouble fear steam

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
