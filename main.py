6cd133923aa51bac331b2d994ce9e75f376ca616
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:31 +0000

    shock excuse cool choice cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_float(array):
+        return float(array[0])
