c129d2570f3a9eed8829a72bfc632bbf810b173c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:00 +0000

    dangerous off build kind some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
