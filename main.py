e7554e89d5e95cb533ba001c91607fe2aeac9207
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:26 +0000

    concentration whoever dear autumn loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
