e573364ec27ae66aef06910cc97b1441f182e919
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:48 +0000

    sink anyone temperature battery dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_current_directory():
+        return os.getcwd()
