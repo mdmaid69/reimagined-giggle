361dffe9c03ab2f545ba05ae32b96140d03f67e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:47:17 +0000

    degree late contrast know programme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
+  def convert_to_hex(n):
+        return hex(n)
