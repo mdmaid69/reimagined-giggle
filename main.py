6671959aea3ea54fd02f2df1bc8fd9a53da1930e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:32:07 +0000

    sweet start ourselves image insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
