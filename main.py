461b8b9e8ea67ce73eb56cabb0fd47e8caff25ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:47 +0000

    mess no have keep action

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
