b874bdb9dd433f187f02486cbc5c74f805d7caa5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:38 +0000

    table goodbye judge rely unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
