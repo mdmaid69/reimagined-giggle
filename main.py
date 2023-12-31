14f08b5c161c52e88496cea6e6ef394181ae8c05
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:35:40 +0000

    backwards cost conversation introduce sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_min(lst):
-        return min(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
