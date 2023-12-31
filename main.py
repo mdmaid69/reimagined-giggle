d547fada1fe5d91281d86e5cd0de53dfd4298864
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:41:14 +0000

    commit skirt poison remain brown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
