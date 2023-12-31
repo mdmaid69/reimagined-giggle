bc25f861185ba06a353d3c1a0ded10ec6000ea22
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:23:06 +0000

    slide ear kind familiar identify

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
