b0655a075194dca66a3726288e8b722ac60ed8de
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:24:25 +0000

    change inquire squash load luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def is_even(n):
+        return n % 2 == 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
