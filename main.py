4e27b0d2a5a30d5180334c2760c2f052faf2c8e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:22 +0000

    intend owe calm account drawing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def get_bytes_from_array(array):
+        return array.tobytes()
