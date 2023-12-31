71783f56c2a606807463a2d0c38d298432e63226
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:52 +0000

    release outside approve Dd return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
