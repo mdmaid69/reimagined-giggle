fed761f7dc76c235320245e9b81c308e46e65df7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:33 +0000

    afternoon fall zero immediate opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
