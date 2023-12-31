5c846b8b0c42e8236064400224d66da68698c6cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:24 +0000

    crisis our earn high popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
