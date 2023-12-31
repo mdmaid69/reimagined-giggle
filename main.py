e79eb961cf7324216c405bfbd19ed1d27ab7bd76
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:08 +0000

    workout snow peaceful comfortable survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
