731c31c3bf115807e8cf49c4059d2a7d577a9eae
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:29 +0000

    boil ring exit wish roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
