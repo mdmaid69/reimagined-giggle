37566cf748e28d263b2493d4d313ec89cd07f523
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:41 +0000

    easy praise complete turnover lean

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def create_stack():
+        return collections.deque()
