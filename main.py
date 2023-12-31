b3e05a8f8b89f98bdde385bad789e9bfbf62d811
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:54 +0000

    steal ordinary bringforward respect story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
