91a571e5a6e075c7e6cb06b542a82b9054a721fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:36:30 +0000

    hair spill phone stamp cause

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
