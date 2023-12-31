e7dc967cdfb27e6f43db51012a3ac4c96013d44d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:39 +0000

    worse giveaway custom sorry separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+print(math.pi)
