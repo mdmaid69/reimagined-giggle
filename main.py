42f0d24111c4399a3903a35a2511ef7b88d4f65a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:16 +0000

    start damage thinkof flat raise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
