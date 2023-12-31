f65c4cc5e488bc068e295b0026aba55e29b52b3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:58 +0000

    steal school loan meaning charge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
