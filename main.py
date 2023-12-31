8365b654a6775fc5411ffdcb5c7f62f470eb941b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:23:16 +0000

    twist arrangement brother study practise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
+def cube_number(x):
+        return x**3
