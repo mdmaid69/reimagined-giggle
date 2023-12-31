d97dc4888aaeb62b4ce324059210bc1a63d803ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:34 +0000

    temper attempt underneath physical top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
