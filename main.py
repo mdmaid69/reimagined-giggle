2e5cd323ee4e4289e3df6916ceb10e02e213389d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:46:12 +0000

    run estimate strict push previous

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
