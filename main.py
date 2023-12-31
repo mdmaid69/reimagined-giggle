fccce70d7163d0f6a5c51efa39c81601ae8e7ea1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:08:14 +0000

    tired criticism surface agree expect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import time
+print(time.time())
