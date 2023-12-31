d89882cabeec4aa80ddd4b9ae99da70be46256f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:28:51 +0000

    past ill freeze gas petrol

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
