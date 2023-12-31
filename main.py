c6249f0631de49dac6631d8fd81b470707a78ffe
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:02 +0000

    pour naturally picture shopping 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
