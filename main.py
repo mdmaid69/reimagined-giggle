35675908e75442d44185680e01e93976dfb66e5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:52 +0000

    second family talkoutof turnaway more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
