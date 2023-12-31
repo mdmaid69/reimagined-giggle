1a1e8d524217286d34ad1d31bd6b0f7e790d229d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:54 +0000

    active best reduce theirs turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+print(time.time())
+import os
+def get_current_working_directory():
+        return os.getcwd()
