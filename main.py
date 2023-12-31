78fa063801d94fda6c711c24673e24fe59cc1b51
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:09:19 +0000

    badly interfere relief seriously similar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
