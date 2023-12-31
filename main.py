bcd52dc0839eb31aafc5dad961214ce032bb6a1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:15:08 +0000

    contents get machine extraordinary tradition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
