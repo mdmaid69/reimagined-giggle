c6ca04bc4a352a856b1cce9a3cde8afbc5a18514
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:42 +0000

    us upwards freeze outside among

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
