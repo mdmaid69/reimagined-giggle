c0d94e22ca6ab1f38c31e3217116816cbe06a0ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:16 +0000

    trick watch bringdown road height

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_max(numbers):
+        return max(numbers)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
