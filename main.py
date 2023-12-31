a80761130aa5d15f5e31b30d83bbe896166a19f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:32:34 +0000

    imagination interested listen prove active

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
