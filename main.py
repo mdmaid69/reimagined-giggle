cd638d531af3999c7f767e9a6a8ceecf4ce042ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:53 +0000

    patience often friend train him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
