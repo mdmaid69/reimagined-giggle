4407bd8c3a724abe202199c88456ee6aae482a12
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:45 +0000

    always right control transport rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+print(random.randint(0, 100))
