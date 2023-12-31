21999f8694737c671aa74bc148f78d4454b3f6ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:49 +0000

    home tidy communicate earth towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
