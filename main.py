2cd4c84c8ad725d1f8d4d974d6681e028ae5080e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:36:51 +0000

    tidy scrape star getaway complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
