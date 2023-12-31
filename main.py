129cd3ab0761e28f5a77544526fd044b8eacd36d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:55:47 +0000

    poor tooth down middle tape

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_min(lst):
+        return min(lst)
