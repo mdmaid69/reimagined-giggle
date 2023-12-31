d52d7c0cd27dc86674711160b99867225a3fccab
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:33 +0000

    create smell talkoutof study wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
