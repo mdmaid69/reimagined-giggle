e8f5c1ff298aafe4aefbeb037a60cbe133dc3bf5
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:11:27 +0000

    contact write amount rescue platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
