d263d3e66249d4b912db77524deb0a7ca196842c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:26 +0000

    learn know prepare see name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
