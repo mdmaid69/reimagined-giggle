a7b29c37fe47b4b6628a1a699045d337b4ba9bc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:31:54 +0000

    hand not hit dead apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
