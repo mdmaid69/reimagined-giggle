903e6c44785c59d22ea8a6e1149b61b66d031257
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:39 +0000

    put shelter preparation paper sense

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
