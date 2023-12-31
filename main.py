cf94a92aede4bc353c4d88ca8911e2853561b6f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:22:57 +0000

    abroad young opponent image nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_set(array):
+        return set(array)
