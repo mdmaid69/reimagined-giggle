cc1fd5c0a35e91ce939321387b5c70a83b689955
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:04:22 +0000

    talkback deserve recently schedule answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
