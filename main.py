bf15317acca289ec8b21de2a56669af43e438251
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:23:43 +0000

    tool connection activity bite male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_current_date():
+        return datetime.date.today()
