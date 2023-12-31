1b819102ed60fda47d2f875df85e614bfca28f15
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:24:57 +0000

    whether steam sink protection space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
