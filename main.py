d18996e389e83200a01dc304ea5188a9d931dd69
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:59 +0000

    swimming tonight mine mass turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
