1b08f340b1fc390542c616934ef95fc703336d99
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:34 +0000

    something spill side do pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
