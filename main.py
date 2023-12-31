b6f06c167c105e522f94240b83501a1a8a2595cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:39 +0000

    walk look suppose quit talk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def square_number(x):
+        return x**2
