a817d8cf3ae6d0496419230dbe6ffb1554b90b67
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:39:43 +0000

    wind stranger splash mine scheme

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
