949f92a6237e7776b618264b710020bcc8711fc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:59:18 +0000

    reason central reject long object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_area_rectangle(l, w):
+        return l * w
