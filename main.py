f2592605d1f753e9df8739118460fb342560b878
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:24 +0000

    little pile may convincing rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
