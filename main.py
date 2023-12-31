81c5039d41827b94ea77eafe5163c384b342245a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:38:37 +0000

    fold day trick temperature zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
