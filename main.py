b5d10ed30c32e4ba0c0e571abcaa20234448e6fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:58:34 +0000

    seat guard there 179bite protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
