97aa48ad959f69f133673d9f7e162d6246cb66ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:21 +0000

    herself division easy discuss home

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
