764a8e26df1600fd51d5adce1429e886e58d65a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:10 +0000

    your visit Bb figure money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def append_to_array(array, item):
+        array.append(item)
+  def convert_to_binary(n):
+        return bin(n)
