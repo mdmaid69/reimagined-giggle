40c9ffcbf149adea256545d14551806500124398
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:30:13 +0000

    replace patient get finger principle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def sort_list(lst):
-        return sorted(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
