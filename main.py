6554960b700885e8e43bd93c99ca3f788745d1c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:11 +0000

    compare anywayanyhow police common declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
