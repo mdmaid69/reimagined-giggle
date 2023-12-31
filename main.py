4aa7868709d22e1ee3010b96fa43154b26f3d6d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:29 +0000

    study ilovedicks sense repeat use

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+print(os.getcwd())
+import time
+print(time.time())
