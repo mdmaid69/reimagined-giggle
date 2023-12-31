ffc89b637378eeba6727571e7a8485a92c029dbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:12 +0000

    about week meet market violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
