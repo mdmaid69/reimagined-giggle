b3574ff1ec9240e40df784795e321892d3486ae1
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:12 +0000

    charge nurse thought dry stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def sort_list(lst):
-        return sorted(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
