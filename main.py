a988340165b09232f386158137c42a8121a9dfdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:00 +0000

    raw production section book behind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
