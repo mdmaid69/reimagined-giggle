4721711e78aa5a3a36309078e9eca8d0796ae9ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:14:53 +0000

    sign appeal improvement repeat off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
