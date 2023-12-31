1ffc28c1d5d13b9653e40d856acd7261475e046b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:42:49 +0000

    so hide live load display

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def reverse_array(array):
-        array.reverse()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
