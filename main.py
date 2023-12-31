6031479e609ea29d2a25152b1c5a9435f623435f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:37:48 +0000

    cut beside spill bar professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
