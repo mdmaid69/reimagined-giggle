f0c1e0e7fc95efbf40d2906fe816673e76de4bec
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:21 +0000

    air pour rush free eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
