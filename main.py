8e0fe8f9c76f151533cd05431aff5acbff40cba6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:03 +0000

    take face include suggest summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
