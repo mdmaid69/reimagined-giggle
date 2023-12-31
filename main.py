604681c56cd8b487943aa33aef4d167b8cb1a840
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:16:28 +0000

    mend brave stage compare decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.ctime()
