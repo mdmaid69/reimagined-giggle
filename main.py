e664453f6f2592ebd8d1b008b29527ec58b71baf
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:51 +0000

    worth estimate mark figure firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
