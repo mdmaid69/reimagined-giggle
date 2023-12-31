c9255139ef812d2068088e17d94bb40f97eae747
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:39 +0000

    experience relax wise later break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
