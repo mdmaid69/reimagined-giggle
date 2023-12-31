fc297e84f1014fe321b320a43606730d49e64362
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:05:28 +0000

    rob juice excuse broad everyone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
