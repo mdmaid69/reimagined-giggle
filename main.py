9c9483678f8657af824d34fb710bca94b95d4277
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:37:35 +0000

    turndown count against have Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
