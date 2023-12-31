f2ac60baf14a8b81d4c1024c67ec6a6abd9eb5d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:22:37 +0000

    want count skill feature comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def count_characters(sentence):
+        return len(sentence)
