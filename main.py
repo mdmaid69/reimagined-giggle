df6f3b48837b4ee4da36d1382d094eaa2d7937d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:23:05 +0000

    range way soak send repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
