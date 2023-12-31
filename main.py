20a4c9b3774892867483293f5c1e73d68734c08e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:48:31 +0000

    relation bet marry rather break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
