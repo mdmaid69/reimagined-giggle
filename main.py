55dcbd6b927af96671497e87e51e725632a28b11
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:09:05 +0000

    improve spite attempt various common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
