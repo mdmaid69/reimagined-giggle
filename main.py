383f05e9532c2744bb471e5a40c99336f30ad682
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:45 +0000

    whatever university satisfied cut load

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_max(lst):
+        return max(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
