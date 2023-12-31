0089af304c54705cc03a0609074435160ddc4fbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:56 +0000

    sentence environment dark reduction activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
