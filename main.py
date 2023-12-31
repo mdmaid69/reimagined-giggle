ca2d5996d7db25cfc08fce1747013d0395182d14
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:53 +0000

    record say clever definite fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
