d1053ad8ce354022132ab2e75452da929f00a441
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:14 +0000

    speech comeover sight glad best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
