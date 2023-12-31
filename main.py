d4a28a8a129b1202ae7e9785ac6f49c63c726e7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:03:19 +0000

    rule turninto almost spring stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
