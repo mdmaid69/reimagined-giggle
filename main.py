752b1528935fdb75b8d917ac29b7491242017652
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:19:25 +0000

    tip stupid negative purpose hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
