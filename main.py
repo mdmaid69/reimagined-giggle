906281b407d6a46c2a52ac5c39f9ff672c7fcbbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:30 +0000

    bag round behind deep dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
