8bbcd29c4e36ade43c4c079c021ad7fbe50eb639
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:06:29 +0000

    dare female pronounce serve thirsty

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
