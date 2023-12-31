7eac438ac5f79e47459faca75844bb78960a71fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:31:14 +0000

    up picture plain wealth trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
