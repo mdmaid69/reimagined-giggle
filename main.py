89232e3f1edfe3148c56615482f0b123a7de1ea7
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:02:12 +0000

    Mm famous build gentle save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
