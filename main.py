4aa9899d308196d68183a67fec9bfd5d8db16070
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:57:13 +0000

    day unlikely cinema somebody trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
