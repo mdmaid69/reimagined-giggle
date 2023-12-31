1b973cd2c143502493e7bdebab120619b1386aa4
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:43:51 +0000

    hollow bend spare bottom embarrassing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
