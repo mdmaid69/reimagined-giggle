11fe35d225eb8881bbdf7a42ca3f577d37e990a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:52:24 +0000

    Ll few imagine combine sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_current_working_directory():
+        return os.getcwd()
+  def square_number(x):
+        return x**2
