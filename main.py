276da02b3150de16a6ee2e90914e2acecbb85f13
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:56 +0000

    alive Uu medium wall suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
