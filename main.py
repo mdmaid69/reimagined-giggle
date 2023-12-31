93b4b055f26e79f7f111f3cb261549cdc074c8c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:39:48 +0000

    meaning concentration goodbye crowded considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
