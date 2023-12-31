1f82befa00042e915f989dde681a31b5a407fcd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:12:39 +0000

    clean ride recover clean record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
