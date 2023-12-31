0a089a4caffb4bc265f2c024e8f1f76034f8c198
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:39:28 +0000

    gradually shake talkoutof attempt house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
