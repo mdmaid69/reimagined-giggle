2e8a2cb0d8d32b191ac7f991bb6cbc9223dcf4fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:48:29 +0000

    radio emphasize hello target sight

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
