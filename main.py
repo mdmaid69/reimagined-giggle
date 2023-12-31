43b993597eb4e1b0f79294c5c34cde885a11b64d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:27:43 +0000

    let concern turnover separate hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
