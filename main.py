74658271fb95314099868a2fdddeb11374dbdcb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:35:02 +0000

    test contents mark total listen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
