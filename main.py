b1d8955a84dbd31b8cdcf39aea855250ab46012a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:36:16 +0000

    turnover mess go any enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def get_array_length(array):
+        return len(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
