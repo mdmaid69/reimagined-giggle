fcce0b3ee714f9ab887735cfef08fcf6e75ed9ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:53:34 +0000

    limit storm post more pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
