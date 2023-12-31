5e9ad2d52522558e3686211d38553d0b3f77cac2
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:31 +0000

    man look final mad apologizeapologise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_min(lst):
+        return min(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
