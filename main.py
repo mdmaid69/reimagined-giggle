4e8eb0bc2fd5933bbf961d5cb8f61c3b79937d32
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:35 +0000

    freeze measure platform error getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
