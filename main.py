01f16fcc6f4792c1b15e93e05a1b918731677f54
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:08 +0000

    deliberate fierce thin pencil holiday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import numpy as np
+print(np.array([1, 2, 3]))
