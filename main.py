d8d5dbfeac36199c30ba705656f2a20f1728686b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:52 +0000

    reliable official callat section push

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-print(time.time())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
