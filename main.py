fbafc2cac649fba69bb53b8ab9d2e9826b94ca80
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:13:23 +0000

    ought interview again regional water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
