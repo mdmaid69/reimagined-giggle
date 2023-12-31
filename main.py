069e4216254b716ea8eef777976883beeca237ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:18 +0000

    convinced lean spread colour field

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
