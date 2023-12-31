6d79e6ddff03cec09168a7d12f9bdb30a2549927
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:01:45 +0000

    bother borrow emotion door opportunity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print(sum(range(10)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
