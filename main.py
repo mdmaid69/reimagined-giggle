18644acb8aa69d593b68da2d33bd66c6fd4d4f19
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:03:06 +0000

    regular surface out success private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
