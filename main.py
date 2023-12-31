53c64f153be495fcbd1d4b0e7fa9b8bcec9eff87
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:11:40 +0000

    time alive wide lookup crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
