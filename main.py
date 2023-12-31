81db061eca5d207e72ecc291781f5a81b2f61f84
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:03:41 +0000

    Hh aware house upstairs string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
