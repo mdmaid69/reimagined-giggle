591901aa9cbcaf3eacf2d91fa284fbc31601fa99
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:00:13 +0000

    target delicate close point stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
