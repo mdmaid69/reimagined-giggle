fa8b4a4009b3b300df3fa0a50f1cc826f2273cdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:19 +0000

    weekend comment care dear improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_working_directory():
+        return os.getcwd()
