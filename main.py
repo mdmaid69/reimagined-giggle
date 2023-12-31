efda44f8b39d59eeb54fb638d028c861db8ca26e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:26:38 +0000

    point cheap business ball round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
