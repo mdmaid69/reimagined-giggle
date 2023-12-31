ab85fc0a7d4b5ee4b756846f4dc5169a84b6306f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:09:06 +0000

    would form escape lookaround number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
