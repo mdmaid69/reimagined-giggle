3d28ab34aece7533780328119ebee64252730748
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:22 +0000

    sight school bus watch valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
