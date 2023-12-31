acde55cb5b7cec9c086aeb7bedbe2aa37f863494
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:35 +0000

    skin until unite read strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
