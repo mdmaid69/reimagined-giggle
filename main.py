cda03e6e4feaff138f6ce4039d2ab711c4613c95
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:03:45 +0000

    grow mean makeup tap great

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
