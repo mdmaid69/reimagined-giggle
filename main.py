ab980774243f3f9186d6ad117cf1cb8edaad8e54
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:19 +0000

    reply feed beautiful try steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
