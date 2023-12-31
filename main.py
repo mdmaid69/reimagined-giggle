aac759ecd640809e961bd1bffb5983c27e4f0f63
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:15:33 +0000

    small study sale comment learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
