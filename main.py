2790ab0913285bc03a96214f84820474768a7444
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:29 +0000

    moment opponent pressure they affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
