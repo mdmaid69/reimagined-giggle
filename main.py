03e4ee45cad85f9bb58a25466b8c5b4f8c566f35
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:56:00 +0000

    damp over pressure take form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
