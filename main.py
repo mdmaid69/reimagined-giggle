38c5bee09ce4d8b73739f0bf2f90753dd7390ea5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:26 +0000

    in getback approve promise be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
