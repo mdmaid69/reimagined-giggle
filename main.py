a77a589cd1ee1d931fff40230ed2424dbe153e8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:23:57 +0000

    immediate consider tip face forward

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
