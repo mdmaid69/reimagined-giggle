1c406de9f82a7abd2e7b64e7f38733aa9902ccc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:09 +0000

    shut turninto die classroom shadow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
