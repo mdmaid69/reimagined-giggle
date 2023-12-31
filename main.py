dcc395bc6d4efdb52b7f90f4efffe0cbe1d2cdd8
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:02:06 +0000

    mix class no thought week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
