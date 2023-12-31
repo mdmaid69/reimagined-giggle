48096b6bad564f0ce04a88d3ef0189ced3e544dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:10:35 +0000

    transport everyone good supply against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+print(json.dumps({"name": "John", "age": 30}))
