15d95471008bb155ab79769863f2f405616693f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:32:57 +0000

    coffee pool except rarely Qq

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import collections
+def create_user_string():
+        return collections.UserString()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
