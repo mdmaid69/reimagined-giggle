7760acf31c1a3587d3c3afb0ae609a630d0cb098
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:52 +0000

    cry total unless crisis forward

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print("Hello, world!")
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
