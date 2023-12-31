bd5c5145cda8b5225c7affeb829a84f8fa7b2062
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:07:26 +0000

    division her satisfactory escape light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
