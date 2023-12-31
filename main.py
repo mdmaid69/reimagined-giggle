d0a327ab6e38b45909f05d278dc027951cbbb40f
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:04:33 +0000

    female plan report plan disappear

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-print(time.time())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
