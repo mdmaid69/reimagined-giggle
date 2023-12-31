3989189dc7d6298e52c2a1605732480c3a99a50e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:08:16 +0000

    speak late quality noise can

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
