b18b66819a5b832a10784ad0a3b8e345871d1adc
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:57 +0000

    excuse shock pain funny fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
