45a229618796ae60af2512a5d6129a0f0582e9e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:08 +0000

    piece bringdown money interview agree

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
