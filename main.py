a028f2b51308915482003fe37ca43117fb2bea9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:20:08 +0000

    prize fall sing border eye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
