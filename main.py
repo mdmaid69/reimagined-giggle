8f7cc2e9d21fe43eb7a8d9be77e0229104361ee5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:04 +0000

    but second punish takeout sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
