bd0795eb60fc677a6a53a5f17f4dab345424fb6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:45:41 +0000

    sing bread purpose regular give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
