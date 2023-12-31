7d2ccea302ef86e24a5d636f51d903337520f48e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:51 +0000

    maximum awake raw repair watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
