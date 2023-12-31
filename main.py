38a4f1e2965f980710305bff8354299f3a3c271c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:37:00 +0000

    fit serious exchange depend fear

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
