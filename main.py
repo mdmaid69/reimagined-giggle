bc77ac8609239d699796fc7dd4defe412b72559d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:45:04 +0000

    roll block tune speak bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
