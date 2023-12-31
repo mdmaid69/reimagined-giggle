bf6efc5da47ae9e2ed0c50d5b0693b2a63699ca8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:27:46 +0000

    measure undo poor conclusion skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
