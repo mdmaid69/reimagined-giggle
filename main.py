806a460445de65c3f2beca1003b42a6f18c2001a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:11:33 +0000

    furniture classroom pool answer able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
