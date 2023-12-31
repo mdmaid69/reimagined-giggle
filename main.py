cab607b6b12903e82c012d165635fbda72544806
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:07:10 +0000

    secret although crazy bringback Pp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+print(math.pi)
