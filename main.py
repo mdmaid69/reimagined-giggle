6adf8bf45adb1282ce43072a3e19bca7317968b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:12 +0000

    warn habit prepare even undo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
