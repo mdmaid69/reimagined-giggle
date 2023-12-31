99ddb1ab143ef70c6068c45c02f94f26880b43d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:06 +0000

    picture odd progress contact undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
