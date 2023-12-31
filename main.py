3ed3d0971ede92d119ed16bb3f1650c3a4feee52
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:11:22 +0000

    tip study blue brown sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
