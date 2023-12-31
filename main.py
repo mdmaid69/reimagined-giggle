c25bcc5130264e050307ef4ad29b23fc4d1c537f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:36:44 +0000

    direct advice sport reserve hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
