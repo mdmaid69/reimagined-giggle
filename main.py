010a9047922500106f4665eb322aa663496273ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:48 +0000

    Zz stranger over last dick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
