231b00f73aff5b138f695153214ff3fd6698d4c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:46 +0000

    accidentally some freedom regular closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_area_circle(r):
+        return 3.14 * r**2
