4d2f61ebf3ffa7ee8862e2e0662b529d8a7fd143
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:10:00 +0000

    bottom centimetre keen we attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
