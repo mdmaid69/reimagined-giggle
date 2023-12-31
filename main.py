4fc7e2d48b338b29bccac9bbe3984d79b7ca51f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:30:46 +0000

    far tip turn spring immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
