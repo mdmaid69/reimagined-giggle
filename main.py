445a529696dd04b51437635bacf3458e356b694c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:19 +0000

    guilty ever progress something skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_slice(array, i, j):
+        return array[i:j]
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
