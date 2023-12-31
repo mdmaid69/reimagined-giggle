6d12b1cb19c71830e504041b0289e73cb46df1e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:43:08 +0000

    breakup party needle advice backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
