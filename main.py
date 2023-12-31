97bb18e489beb7bcbc0fa0daa56ced47bb145901
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:29 +0000

    build glad flight shock there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def subtract_numbers(x, y):
+        return x - y
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
