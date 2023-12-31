9eee7ad7584370ab69e34cb4dc17d6489299bcc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:56:56 +0000

    earn generous fence pretty doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
