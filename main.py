68c855a2f6d608576a3d149d5faac3b587114fb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:38 +0000

    matter shape grand schedule block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
