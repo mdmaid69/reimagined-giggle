4042aa414e4a1370e92d93830bcddde086b8a1d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:55 +0000

    rid formal border share enthusiastic

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import collections
-def create_stack():
-        return collections.deque()
+def get_current_time():
+        return time.time()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
