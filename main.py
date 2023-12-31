11ca5f8ff962f21bc8c3cca286c8646d39c9ebe1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:43:37 +0000

    better apologizeapologise call backwards describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
