d93428b21051365fb6dc3099f687288b88031716
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:06:40 +0000

    front swim trick brush build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_power(work, time):
+        return work / time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
