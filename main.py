debbf1a5a3b8fa11b2718c2145212ef45fcac339
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:09 +0000

    interesting rarely receive free forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_index(array, item):
+        return array.index(item)
