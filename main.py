591e20c2749550f5a87d100add412301959ea997
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:04 +0000

    original passenger rule considering dead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
