9d5e0fe52075c6e964b6d1e0a407132ee17c2919
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:53:40 +0000

    warm difference destroy though except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
