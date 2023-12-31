f655c32a5f59f5c9d1f6b77ec0c50108c6d6f1e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:36 +0000

    communication freeze slow really what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import collections
+def create_priority_queue():
+        return collections.deque()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
