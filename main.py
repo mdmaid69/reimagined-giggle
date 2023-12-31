fe9bc70b90977161d099c06d8c13bce95ebfd87e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:53:41 +0000

    arrive change well powerful score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Reversed:", text[::-1])
