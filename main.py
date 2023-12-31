bfcb0a8334b086edf10bc858ac5692289763ac1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:36 +0000

    even move advantage hospital track

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
