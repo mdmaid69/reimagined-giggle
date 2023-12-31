10774ff72cafafe4546d85af37a80d6c0b96bedd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:26 +0000

    funny pretty education comfort dead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
