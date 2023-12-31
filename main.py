b2176b9a0d17dc2bc8487d26b57aaf35b5a8de48
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:02:21 +0000

    seriously spread full expression good

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
