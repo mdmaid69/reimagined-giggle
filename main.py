def13b96b4c7eb808a68412a5ec298310b61d1a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:29 +0000

    make last begin birthday holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
