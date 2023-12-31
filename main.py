074ceee87a4d108131983c5f3f6ce8d668a17f61
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:21 +0000

    delicate interest laugh environment prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-x = 10
-y = 20
-print("Sum:", x + y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
