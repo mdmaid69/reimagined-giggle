8648c9c5ca05796efedee59ddc066a762bb9426d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:17 +0000

    middle nonsense fight hair used

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
