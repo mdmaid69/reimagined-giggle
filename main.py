10224605e81e0c1c44624294436b893adf642fba
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:03:36 +0000

    chance pretty goup level lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
