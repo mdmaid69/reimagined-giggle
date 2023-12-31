cc5bd8d5400373d8a003070fa0af5c8cf78859be
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:45 +0000

    future face issue direct inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
