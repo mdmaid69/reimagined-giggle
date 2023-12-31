32ac45697ddf4afccbbd803ad016efaa6ab799fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:02:27 +0000

    570enjoyable towards rank who under

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
