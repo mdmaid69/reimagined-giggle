2ae374ea63497d54b5a1655131d859a3eeb85e8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:30 +0000

    chief contact fresh yesterday you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
