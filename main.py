cfb86f3a8fac9d820f5d111e5e537381df513571
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:46 +0000

    pride safety description opportunity early

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import time
+def get_current_time():
+        return time.ctime()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
