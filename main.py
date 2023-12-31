93a11d6ad5d1af3ca2a369661655fa70e6f18ad2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:18:34 +0000

    married likely start 570enjoyable weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
