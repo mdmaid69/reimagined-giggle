aaa5044ff0d45106b17010e970e11774b24b475e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:58 +0000

    serious breakinto Hh hurt quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
