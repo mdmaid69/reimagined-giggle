3a8346fba5c3f96dee05c3f41e10f65f507cb822
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:56 +0000

    knowledge report meal slide chase

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
