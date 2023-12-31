48fc5bed18c3f4af59c959b91607a6b95fd58f2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:31:51 +0000

    joke various blame field discussion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def square_number(x):
+        return x**2
