9ca27ab073d360ae91923fbaf5cd18442b07d512
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:01 +0000

    you affect obtain light bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
