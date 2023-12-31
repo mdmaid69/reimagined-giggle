6c9b4a78e0e98bf6567fc4ce5f0df81e9af8841b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:09:35 +0000

    tough politics bin event wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
