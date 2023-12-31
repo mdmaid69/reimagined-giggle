fe6e77f5aa82fc1e4fbe5cb6246fe3c31f1d6a50
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:03 +0000

    miss either niggaacademy ring intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
