a222332657491f3af91368efcd76a59099afe5df
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:11 +0000

    famous storm accordingto plate reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
