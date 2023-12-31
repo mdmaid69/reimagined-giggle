725501c0674afaf2292d5e78b902fca4262a46b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:12 +0000

    whether tip port sweep comeup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
