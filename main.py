4eb22269a4a470ec24132709ed57b96cb0f9dde4
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:32:26 +0000

    prize recover far generous record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
