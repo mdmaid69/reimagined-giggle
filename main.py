40a9f39b65595511448d27a072c09a8e127ae879
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:36 +0000

    for only study Zz smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
