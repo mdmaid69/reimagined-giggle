bbd50009772e10ac0380614aed18edfb60c55e9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:53 +0000

    family maybe offence set love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
 import array
 def get_list_from_array(array):
         return array.tolist()
