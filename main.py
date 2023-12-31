7e936d8882ddf541c7642906295a5bc539f86041
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:15 +0000

    pleasant injury letter fault body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
