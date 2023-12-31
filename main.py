5213c296eb6d62456989860942447011e05197e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:36:38 +0000

    water nigga law even search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
