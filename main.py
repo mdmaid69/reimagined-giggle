b37488b6659ec7edc3385511bf7e90de62eb0618
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:18 +0000

    divide lookup among hole patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
