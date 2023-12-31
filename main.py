95b81fbc30ab11b2878544611b4d7d63d45cfaaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:24 +0000

    hard choice pleasant game loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
