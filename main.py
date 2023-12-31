c98e9723c94503d621cb7c2d4aae0e8d7a51d62c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:32:58 +0000

    influence emphasis wash fresh aware

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
