3be109d6502113b9d12e16eb97b5e8c49ab2204f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:23 +0000

    unlikely container unkind together frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
