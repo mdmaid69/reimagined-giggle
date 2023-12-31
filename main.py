6dd9d202998e6e26348e426c08321dbb2bdc59b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:55 +0000

    feeling future later basic workupto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
