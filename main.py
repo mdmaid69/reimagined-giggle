c046a41c69ee720b7acf3f74c5de77d3da64c787
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:53 +0000

    seriously entrance variety wave matter

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def reverse_string(s):
+        return s[::-1]
