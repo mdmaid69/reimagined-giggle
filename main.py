11ac1667676d07c0026e713195acc13ce3932012
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:22:17 +0000

    minute laugh dare address surprise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
