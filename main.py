270e08dce9cf3b2d3b7f4c9e308b611252577e8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:42:57 +0000

    disappointed trap spare service patience

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
