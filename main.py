4148d1ffb9117c098419ab1a6de33562c0bf0b78
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:40 +0000

    problem factory bringforward basic wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
