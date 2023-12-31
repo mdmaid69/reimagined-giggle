361e7632c99ab1c110196ee30a8f343b28df5799
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:30:52 +0000

    Nn speed bill congratulate throughout

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
