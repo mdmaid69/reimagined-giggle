164ee3fbfe53785433de4cd48e71696be7985f01
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:52 +0000

    means file free drive tape

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
