72a071bfe325efeaef2227ccd5e3e7557306b544
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:20:28 +0000

    pinch use fork trade idea

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-for i in range(10): print(i)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_density(mass, volume):
+        return mass / volume
