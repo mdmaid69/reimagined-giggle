4f7cea65c35ecf3755d0f60baff296f4986d8cbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:46 +0000

    delicate theirs sentence heat correct

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
