e00d03221795c7d6d937ccd4a02ecb15fb742242
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:24:01 +0000

    make mass complain cool physical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
