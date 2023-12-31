c7964915d90e5197898a368717df04f39345c814
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:46:53 +0000

    practise slip drawing joke pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
