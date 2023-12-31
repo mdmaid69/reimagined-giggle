a85081ce94596c3d3b2712406b266cf3bd14c7a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:39 +0000

    public chance quality vision nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
