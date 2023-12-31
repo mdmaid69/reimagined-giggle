f7af7a9fa942bbe31c3297485d4e4834134bd3f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:44:53 +0000

    makeup evidence admit worried bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def subtract_numbers(x, y):
-        return x - y
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def convert_to_hex(n):
+        return hex(n)
