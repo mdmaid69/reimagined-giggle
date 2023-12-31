919e8541866358a3d39c8ea05783507ac0216544
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:33 +0000

    late regional in extremely thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_string_from_array(array):
+        return array.tobytes()
