16b93a28e68993a235c02adbdce30c104868d4a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:49:17 +0000

    waste feeling pain look distance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-def count_characters(sentence):
-        return len(sentence)
+def remove_duplicates(lst):
+        return list(set(lst))
+def count_words(sentence):
+        return len(sentence.split())
