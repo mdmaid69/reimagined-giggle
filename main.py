479009e94b8fec0dfbfd8f322bd7bf856461a520
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:12 +0000

    community rarely giveback guilty maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
