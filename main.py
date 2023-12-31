464fdebda22b165fe212e7cb1f5f87e1bf81df32
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:24 +0000

    weak wear with education very

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_max(lst):
+        return max(lst)
+def convert_to_binary(n):
+        return bin(n)
