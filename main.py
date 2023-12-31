07360211b46b369f99a60a59d60441211f7a2189
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:39:49 +0000

    circle task related indeed sign

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
