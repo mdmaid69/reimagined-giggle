599c2a124530566835d1722c0b1970d94a724370
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:50:29 +0000

    limit shop conversation live which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_speed(distance, time):
+        return distance / time
