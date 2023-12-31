8710a4d201671caaeda1b7c7df192b136d3ba267
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:42 +0000

    interesting duty note impression priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
