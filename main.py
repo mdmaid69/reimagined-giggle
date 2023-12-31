dd1da955ff7889a44bf5246c24e5882dcc96ec34
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:45 +0000

    exercise left hard light nose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
