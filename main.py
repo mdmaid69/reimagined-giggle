31845880bf0cdc5f522382d0528129e4daa2265f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:59:52 +0000

    hard feed neither immediate wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def split_path(path):
+        return os.path.split(path)
