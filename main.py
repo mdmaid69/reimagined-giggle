5bafcf3ece6cd3825bd5c2d6e11508c11558f78d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:07:02 +0000

    perfect argue you still post

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
