864f9a6c716a2191ed6da3d48ff77d795084d0af
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:08 +0000

    die square waste home later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
