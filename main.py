2cff41193444fad0a900b3d133f0bd85bc2e9a05
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:30:43 +0000

    politics suit lead dark else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
