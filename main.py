4374a683b6375d957a95545b6788eeae870251eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:27 +0000

    ignore notice sense even about

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from collections import Counter
-print(Counter("hello world"))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
