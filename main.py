8c0b4d621fd4d3a41d136dd1be55a14be6fcab94
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:30:00 +0000

    music take introduction programme sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
