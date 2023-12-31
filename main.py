8c271f34102a88fd3246b9ab32fa1786ccab9b21
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:32:37 +0000

    bad rough debt so noise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
