aa502b0afc6ab1417d4a2be6c409a43efd2bf6e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:40 +0000

    wire ignore compete confusing break

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
