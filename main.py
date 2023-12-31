6ec94db711bfe4beeb48ec319e7a9ed2e17d0620
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:59 +0000

    kick flat desperate raw giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
