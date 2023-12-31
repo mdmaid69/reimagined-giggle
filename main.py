77ec48425c6c745ca5d21543208f6bae6c4eeb7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:08 +0000

    centre table sleep in 565end

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
