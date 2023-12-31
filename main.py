9755cfc3275198ae8c0996c6263bc54d2e637b9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:51 +0000

    imagination clear pretty immediately within

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
