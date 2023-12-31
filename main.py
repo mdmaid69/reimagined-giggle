911bb9162b1a89c89af8fef011979080d7082482
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:19 +0000

    match do suppose suppose deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
