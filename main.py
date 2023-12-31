a324ba6ba33dd46caed3e02bc5e5bf10506b498f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:26 +0000

    throughout improvement wise turnoff schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
