4b82a6db801006b2dee76ff6cb8a55696fad86a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:02 +0000

    566end collection advise note even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
