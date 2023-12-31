b72fbe975ae3ebec382ad824bfbc67a5378bc55d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:56:51 +0000

    pupil copy past target pleasant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
