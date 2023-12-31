14de1f2e6cac52d2f57c5523abc809f7f10a0277
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:29 +0000

    wake compete respect cope whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
