8038c827b73c77658a86158f24467a812bc967a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:48 +0000

    active hospital sister dry weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
