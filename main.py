563872bcc4e96ea691652f0183b29b0cb431a10e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:46 +0000

    short event sit last new

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
