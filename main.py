799af8828cbfb0f20560c3c90c418c2867ca0f4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:08:34 +0000

    classroom kind commit bin brother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
