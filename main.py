2c7ecd7d32d34c697b06a8bffc1deaed257f66a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:27 +0000

    school disapprove fork noone region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def convert_array_to_list(array):
+        return array.tolist()
