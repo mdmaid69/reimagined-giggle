65d45e4bfacb7936af758922da45e5140656b239
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:53 +0000

    edge at pretty coffee thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
