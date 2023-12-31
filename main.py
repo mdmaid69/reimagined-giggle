5113605032e0a0bacc72f3b2e4d1482f1633dc75
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:23:41 +0000

    pile foreign minimum frightened odd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
