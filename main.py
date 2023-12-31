1819db5e9b4d7739efbb10f407ab4bc1c12732a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:40 +0000

    climb another between swim trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
