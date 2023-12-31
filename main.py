4a0bab0404e38e6f27547fbeb43c063baf86873f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:16 +0000

    bang activity band bringup state

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
