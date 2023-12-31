8c62f430dbb6d493b1a2293b27c5e81cb11b792b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:06 +0000

    firm stop nose passenger suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
