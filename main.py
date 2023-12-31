d7ca40c07dc47c20ed00dd4d9abdc2ba63601613
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:17 +0000

    arrange affect wise relax cost

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_power(work, time):
-        return work / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import tensorflow as tf
+print(tf.__version__)
