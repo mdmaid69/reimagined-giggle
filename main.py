2ef2c7b8985cf0c0f3a5d467fbdb3850db090e29
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:31 +0000

    previous up school trade brother

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def reverse_string(s):
-        return s[::-1]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
