551c7cfc6cbd2c3a303063c0655193d5b1f18981
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:27:25 +0000

    lay tip before last company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
