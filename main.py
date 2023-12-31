1fbd49ad64b41e561063487d0704b44b610e1bae
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:35 +0000

    yourself regret increase split date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-for i in range(5):
-        print(i)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_work(force, distance):
+        return force * distance
