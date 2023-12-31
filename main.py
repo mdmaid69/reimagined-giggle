41c87da068b34b98991b70d2ea0120d873eafc45
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:57 +0000

    ahead suffer mystery concentration start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def exit_program():
+        sys.exit()
