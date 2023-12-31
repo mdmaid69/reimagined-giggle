25e2f8a755a2ad46d7fa4f07df7c42bb9999eff7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:02 +0000

    stop fat advice brave surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_exponential(x):
+        return math.exp(x)
