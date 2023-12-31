8552a0d37ac13c730de06564f2e0549318e7509c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:27:46 +0000

    sky art stone divide praise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
