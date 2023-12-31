39e30bbd6d03b0912db18870856e8f716bf4532f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:18 +0000

    far exciting discover fat object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
