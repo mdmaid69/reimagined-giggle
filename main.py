395e8ebc7c14bcb73308c993ac9fbd5b95ac82ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:41 +0000

    reply fork lookup affect drop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_pressure(force, area):
+        return force / area
