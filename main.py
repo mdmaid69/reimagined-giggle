791d5df6a5c732ffa434136d5ae9bbea12e4fed5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:32:36 +0000

    by boil top key come

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
