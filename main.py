0c18f2eb7d1fbd27b7070ab690e3ea99079abc11
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:37 +0000

    choose fault seem bringdown message

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
