08c7c1cb101f9c76224716948832a25741dad32f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:02:33 +0000

    season trade style closely dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
