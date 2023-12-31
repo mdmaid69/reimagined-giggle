642c6b02ca9eec61a249788e449812fe68ffcc8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:59 +0000

    idea find pen hit pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
