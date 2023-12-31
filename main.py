99e4de0af4d559d38c2f83890c663a8cc3affe6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:36 +0000

    shadow steal bringup above keepoff

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-for i in range(5):
-        print(i)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
