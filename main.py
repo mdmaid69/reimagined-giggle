dd734be858338635b9419c50ac4114f0d4fcb91f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:05:31 +0000

    container cheque tragedy rob paper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
