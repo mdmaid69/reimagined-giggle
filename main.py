dcd447c6af3db66403d841049988758e14380116
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:08:33 +0000

    must frightened hurt steal miss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
