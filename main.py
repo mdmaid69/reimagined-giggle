7102e1302b7f6d5b521aab759ccc5442c6a0243e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:07:39 +0000

    land go hold please law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
