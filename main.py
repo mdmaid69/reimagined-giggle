df044e304728fa459fc6d0c319b2218e467ff4b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:02 +0000

    really workout content sight difference

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
