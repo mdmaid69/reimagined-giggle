10ee1dae64b6367cb6cebafd7b3caa1915268a3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:42 +0000

    takeback visit behind thought even

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
