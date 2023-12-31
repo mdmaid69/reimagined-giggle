f7487d4a66db25a8e557a28d562cf070f51bbe65
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:46 +0000

    profit language shock stage accordingto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import datetime
-print(datetime.datetime.now())
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
