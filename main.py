dceb457ccbbbca52ba07520d16ea598c3e44ee29
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:46:48 +0000

    dream track live downstairs practice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
