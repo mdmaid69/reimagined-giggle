9f100fd93d1b3d955e233df781a63d567928bb8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:18:22 +0000

    takeoff then swim closely fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
