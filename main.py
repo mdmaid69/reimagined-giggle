cd3849422cc703901fe3aa6d203ae3378039e176
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:12 +0000

    month content force arrival well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
