36d392ecd43fec6f843d43a8eeb9f15d85c86cd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:25:47 +0000

    vote position rather high contrast

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_min(lst):
-        return min(lst)
+for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
