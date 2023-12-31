da1efc81d61e848abccdffdb80797fb0de97f9d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:01:54 +0000

    once consist repair depend play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
