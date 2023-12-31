2f026b5806b0078dc6e2aa717aa0560a94f2596c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:14:17 +0000

    less eat desperate chief display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
