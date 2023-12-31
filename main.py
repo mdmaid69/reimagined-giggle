1506a9250439116b53d9d541fc704388b1829f61
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:57 +0000

    take smart floor no shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
