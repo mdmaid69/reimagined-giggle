e7832e19e6457fbecb12334e7609623c134b45a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:37:22 +0000

    laugh union achievement interview meet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def square_number(x):
+        return x**2
