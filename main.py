1a9163e2c7442fa06f35594af7c0e5456bbe1043
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:17:35 +0000

    fashion lock convenient workupto fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+print(os.getcwd())
+import platform
+def get_os_info():
+        return platform.uname()
