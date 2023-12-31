6f5bba5eabfb8ab994e08e8c7e759833d3c8799a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:07 +0000

    life tend turnon worry wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
