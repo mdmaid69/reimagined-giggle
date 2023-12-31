04e6a3fd39aa716bebc048388d71053ec28d37de
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:38 +0000

    stupid round no mystery environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
