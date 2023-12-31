7a6c59e2df367e7bc86cba3d718245f09306837a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:44 +0000

    punishment skirt advise this limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
