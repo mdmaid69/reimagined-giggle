569b3c6d91a61d138722745d50c5f39ea5a281d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:53:27 +0000

    tip flavour game centre or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
