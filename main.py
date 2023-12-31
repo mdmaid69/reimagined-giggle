bae1f1fb184b25370333e53c501152c9671a489e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:30 +0000

    sport flat major sound likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def count_elements(lst):
+        return len(lst)
