000b403a6d6b2b7050702a482334c255c32682aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:53 +0000

    schedule consult miss succeed workupto

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
