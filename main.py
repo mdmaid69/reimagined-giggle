b62cc5b444f4fd9d41f976c7ee4e264d085d8a2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:55 +0000

    rank complicated find clean stupid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_future_value(principal, rate, time):
+import random
+print(random.randint(0, 100))
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
