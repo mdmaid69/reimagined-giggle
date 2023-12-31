6c193c1493466b6bb70add7eb70283c45263f667
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:00 +0000

    smell zero even ring yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
