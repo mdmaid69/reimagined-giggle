607e1fb3dd7136fd8bc743c5344ae6880be4e93b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:49:40 +0000

    possible position see should reaction

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+print(sum(range(10)))
