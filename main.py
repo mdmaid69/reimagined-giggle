d102ebaf2ed58566d97c8849216665c8cd546d53
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:55 +0000

    modern idea strike neither expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
