09222f7703cc8b0e927517344dd205371b5f509a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:38 +0000

    hit its lesson shame exam

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
