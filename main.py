17246272214d0f38d873fdab4847e4259e89377d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:59:20 +0000

    finally spin apply scare bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
