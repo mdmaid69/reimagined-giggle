f18ce67aa0ce0946f9c456f65def498209a27bb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:46:29 +0000

    first kill pleasure original meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
