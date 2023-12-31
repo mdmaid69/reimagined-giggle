abfa8c9d4732cfa1809265e7d529b075442dc358
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:00:38 +0000

    uniform whether hurt accordingto off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
