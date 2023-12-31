9c331f042eb5d6e344c00c94486a75dadb746986
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:32 +0000

    year consider rain intend square

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
