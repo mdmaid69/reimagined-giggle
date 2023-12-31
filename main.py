d5650cdab240e83fb686264cecdb03fe4b5c4f58
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:49 +0000

    fashion sometimes preparation match one

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
