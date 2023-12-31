eb0b303301cb7adf0c135797b57ae35ca573e1e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:23 +0000

    although however distant direct some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+import platform
+def get_python_version():
+        return platform.python_version()
+def convert_to_octal(n):
+        return oct(n)
