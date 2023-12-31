c549c5c0ec6ba8cbc9a8ef9d7807fe49739e70cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:01 +0000

    slow again material far pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_power(work, time):
+        return work / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
