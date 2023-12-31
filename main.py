661134049580679b8410401e645f96b8c50f0df9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:56:50 +0000

    wealth equipment brother coat wrong

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
