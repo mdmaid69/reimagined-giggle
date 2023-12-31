8665129b58b809fede0ad835998507c190f11225
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:13 +0000

    familiar expect same equipment project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
