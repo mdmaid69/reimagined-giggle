08b8f441c4ea29c2386e4bda1aa6b20f48d8d4dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:44 +0000

    undo mend still married simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
