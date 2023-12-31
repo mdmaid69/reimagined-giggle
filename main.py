68a527d93c150493b8b724c4a0626eaec25a54a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:08 +0000

    turn run wheel information anywhere

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
