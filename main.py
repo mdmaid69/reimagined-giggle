57c4b839a1e57f89f9e18422a1dd0bc9b4fc8aee
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:57 +0000

    account now exchange now poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
