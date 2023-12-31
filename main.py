f7ba8aa0fb2be515c5f32658cc28a556eddda800
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:17 +0000

    of immediate plan excellent black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def square_number(x):
+        return x**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
