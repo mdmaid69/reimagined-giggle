3d1ee9ed9752bf100435f75c288f60f521588ef7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:19:32 +0000

    finally crowd party transport good

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
