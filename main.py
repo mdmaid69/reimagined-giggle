5cae5b80614523cb5cdac92dc328c1e2828f5125
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:28:30 +0000

    until government neither fold dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_max(lst):
+        return max(lst)
