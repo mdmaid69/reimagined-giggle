2cdc7c1d369e6ecff2be9cbfb86befa372c17be4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:03 +0000

    bring silent 569enjoy child experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
