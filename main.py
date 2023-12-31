67e8f5ed6b88839412b5e658da81d6a99c8f21eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:57:58 +0000

    present hour judge uniform party

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def convert_to_octal(n):
+        return oct(n)
