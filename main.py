c96617aa4df5d12071c8318ac0a93bfaaf2e85cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:19 +0000

    exact slowly direct wall runout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_hex(n):
-        return hex(n)
+import collections
+def create_user_list():
+        return collections.UserList()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
