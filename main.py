e47e337b42a77b583ca3e158ae695473d379fdcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:55 +0000

    regular forever pick plan start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
