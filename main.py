b372b47ace8e5c8ca891ace0cab46868f82309d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:06 +0000

    he lock effect jump relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
