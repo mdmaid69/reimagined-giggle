fcc88c46c6d173e9b62d738ba6941ba0941074a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:18 +0000

    challenge match distance free cap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
