81677f043bfecc419c37a5e8fccd56ba5f604a8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:22 +0000

    takeout test left path even

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
