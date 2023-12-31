382fdfa1a462b58513e1d472b6cf6b20c4f2692f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:27 +0000

    scrap command with keepfrom many

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
