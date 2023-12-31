ccbdaf8ea81a8d630e878fe2e5f1eb41ceef8f27
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:35 +0000

    whisper offence basically water file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
