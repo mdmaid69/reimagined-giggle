3571aba7a3ea51f712e0bb407b86a03fa217764b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:19:35 +0000

    principle talkinto conversation obvious sight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
