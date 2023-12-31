705a2ac656cdb965f19fbdacbd153acb36a13607
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:28 +0000

    maximum up behind Ee demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
