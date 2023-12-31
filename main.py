f65bc24fca24ce32cbb2407e5183c492c0164ae8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:00:27 +0000

    shoulder fail solve date communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
