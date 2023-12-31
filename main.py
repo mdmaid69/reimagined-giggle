8a6b8f10c2bc94bd208497ff234a1150bdb89164
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:34 +0000

    jump gas point free statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+print(math.pi)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
