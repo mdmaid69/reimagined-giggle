7c7baea8f938d33d873cc1dc63372f5f35fd89e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:26:45 +0000

    dress feeling copy cause lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        print(sys.version)
