c01e547780cdded0164e297335225c560fab3ff2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:38 +0000

    male communication tip perform otherwise

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
