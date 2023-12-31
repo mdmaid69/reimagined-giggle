902ef492e4010ed6c63a2b29f18a0c24997821b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:16:10 +0000

    practice ring dear high yesterday

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
