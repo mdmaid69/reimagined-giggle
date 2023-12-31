0d5b78f9bca31467f4befc7012ece8782986dd67
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:31 +0000

    someone thumb reason fear conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def remove_duplicates(lst):
+        return list(set(lst))
