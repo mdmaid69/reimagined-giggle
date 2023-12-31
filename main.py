a09d2b538c55649cea5d94a0e408ed8151c91a36
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:59:27 +0000

    trap correct bound minimum laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
