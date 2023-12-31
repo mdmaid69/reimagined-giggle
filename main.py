4e0b825072841b43938cde839642046ccc99ff22
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:25:04 +0000

    shelter frightened complete list miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
