0add9aa7a061e4c41820b1f0a1e51488b942a355
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:10:59 +0000

    paper boat right identify catch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
