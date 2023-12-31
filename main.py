d0a86a2613f21bf027e06d30f47350c214f29870
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:03:25 +0000

    simple country feed answer under

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
