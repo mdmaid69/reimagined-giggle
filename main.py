74b31a62d62b209533b21ab9c6deec47462dd563
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:48 +0000

    cruel possible getback turnaway study

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
