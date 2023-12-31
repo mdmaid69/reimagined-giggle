d27e91b1b27e8fea43f3b2901e509b7a0c1ed09b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:34 +0000

    find tour operate boring open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
