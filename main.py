3194216ec00d9cce9fcaf7baa53746d7cc75c0f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:33 +0000

    abroad measure preparation dark risk

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def convert_to_hex(n):
+        return hex(n)
