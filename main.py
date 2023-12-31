dfb612c816b183591b2216cb4af803105c6352f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:49 +0000

    date original natural serious fence

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def multiply_numbers(x, y):
+        return x * y
+  def subtract_numbers(x, y):
+        return x - y
