3a4c0411863b6aa3687b80d74e33412f652c14cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:59 +0000

    zero sting passage pinch where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_set(array):
+        return set(array)
