e5083286913b95dfc393e279fe285e9e5fe8f949
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:05:29 +0000

    community search include into neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
