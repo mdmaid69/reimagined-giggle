16cc0788c597088eb8ec42683304bfbcab1f4f4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:01:27 +0000

    heart dry instruction mixture coat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
