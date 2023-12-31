064e8840767205a25dd5f576521a1018b3aa1386
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:24 +0000

    community improve result kick situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
