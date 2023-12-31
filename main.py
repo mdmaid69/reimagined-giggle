5ba57d4508d326d6eaa500db4dc0e53d17400c27
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:03:47 +0000

    view building invite considering hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
