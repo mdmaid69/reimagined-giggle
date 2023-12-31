a8f721c03834e186fadfcd1ced2e14835addf8ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:53:24 +0000

    ahead chin for convenient plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def convert_to_binary(n):
+        return bin(n)
