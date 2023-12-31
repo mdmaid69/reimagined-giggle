89c99fe4c69dffb9d255f43354efbfbb8ff3f3ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:02 +0000

    production exit talkback game skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
