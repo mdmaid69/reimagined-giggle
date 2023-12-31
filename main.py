9dfecd93bbaf7c8d29ea806a9b6290a642f108a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:33 +0000

    dress single around positive urgent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
