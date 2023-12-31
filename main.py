3f55261017de9d38377a7dcdd6799b375db74bb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:10 +0000

    Mm lay related past fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def convert_to_hex(n):
+        return hex(n)
