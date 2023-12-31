3aa0f176a6fcb82fbeed2feecc696b42ddcde44e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:38:02 +0000

    probably exaggerate bored cheat face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
