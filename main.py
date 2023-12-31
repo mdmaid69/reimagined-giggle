6f917c75d404867b441aa5b4874317e9c95752f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:55:11 +0000

    relative boil cover split study

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
