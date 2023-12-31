9dab3b43ec2c6e05b8bf83df0e1defde08b662e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:25:21 +0000

    direct minute bar fly compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
