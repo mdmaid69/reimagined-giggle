b72a5601555d8222bea8652a4585b9e88708eaca
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:42:49 +0000

    shop watch start fact surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
