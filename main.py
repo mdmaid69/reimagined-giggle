58ee150693f597ecfd7d8de909aca32c4c60736e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:57:54 +0000

    skirt violence appeal copy love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
