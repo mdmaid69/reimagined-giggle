a57b42c272df193f8e651975e30a926629ed7066
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:31 +0000

    slip whole alone Uu do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
