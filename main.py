f3246de1c8a8283f38dcf998ec533c41d63b5415
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:21 +0000

    ashamed laugh such each probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def is_odd(n):
+        return n % 2 != 0
