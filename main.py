6488f5f4ea58f3fdad11e9dacf0b711d8020d051
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:34 +0000

    kick dance food swing experience

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_density(mass, volume):
-        return mass / volume
+name = "Python"
+print("Hello,", name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
