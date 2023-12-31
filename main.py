7976f523da3c55a7a9cb9f8c4f49f667f01077eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:46 +0000

    this hole if ashamed estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import os
+print(os.getcwd())
