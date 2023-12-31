4fb97b8df6799c19ee128a9bdb22006df7cc042a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:58 +0000

    finish wander film cover already

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
