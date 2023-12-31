e80437af08a980b1a246b02ab340813924a46674
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:49:07 +0000

    control shout tap clear far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def split_path(path):
+        return os.path.split(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
