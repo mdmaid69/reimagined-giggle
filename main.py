c74334604e09170459533361ece781dd0b7ed27a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:48 +0000

    relationship early grade breakin getout

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
