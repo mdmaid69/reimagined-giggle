81d8d7318aa5859b9767490bbe2ae00ede57f6ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:03:17 +0000

    real drawing exaggerate catch sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def count_characters(sentence):
+        return len(sentence)
