ddfeacd3f437beb7d860aa4517509ae07bb74048
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:05 +0000

    apologizeapologise nor soon important flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import time
+def get_time_since_epoch():
+        return time.time()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
