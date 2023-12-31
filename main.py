2266338dc62f2923b7a0d9a7ccd2ba2d8f56ab2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:48:10 +0000

    extra holdout file their clearly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def is_even(n):
+        return n % 2 == 0
