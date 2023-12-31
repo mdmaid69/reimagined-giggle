35670b60d4a3918808ccba836d38560ccdefa108
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:32:43 +0000

    lost anxious run high public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
