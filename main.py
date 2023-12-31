3bed96097bec53ac7eb64c0987dcb3901385f021
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:09:46 +0000

    everywhere large somehow air pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
