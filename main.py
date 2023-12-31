62e8237330a751dec4ebd167d58a77e930b52311
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:42 +0000

    sink wild in evidence sale

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
