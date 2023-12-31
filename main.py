c52e58ad6490adce816d3292cbe87c66efa5a1ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:42 +0000

    store tough just lonely protection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
