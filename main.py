def5114fd9c50653738cc854690b376c39145be6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:09 +0000

    shout dangerous area talkoutof song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def greet(name):
+        print(f"Hello, {name}!")
