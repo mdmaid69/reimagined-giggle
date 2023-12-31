1028a22f39e41b8364b0b10d221e785720948503
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:50 +0000

    shine letter contact truth sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
