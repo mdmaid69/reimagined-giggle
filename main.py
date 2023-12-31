71732e9f1827b200d5c69411bf6a06ecfef2da74
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:45 +0000

    aim late example idea beginning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
