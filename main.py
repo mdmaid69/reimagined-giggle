794497bb6de7f9405ce353f3666ffc39fd6e16df
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:09:04 +0000

    confuse rich pair Ll bored

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_circle(r):
+        return 3.14 * r**2
