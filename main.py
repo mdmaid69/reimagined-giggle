37c289960a85b4aee0ed0e292a366746bb2fdda1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:47:18 +0000

    tend dish cool man estimate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_area(radius):
+        return 3.14 * radius * radius
