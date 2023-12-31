2057774418313245e28244f4502b306ca8b44c55
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:21:42 +0000

    whistle way sink march fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
