17ed59000d9b9dbb180e96ecad82dd68d0eaff13
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:09:59 +0000

    connected method bet take beside

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_pressure(force, area):
-        return force / area
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
