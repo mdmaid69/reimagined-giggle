925315fa454673ac3c36551637e9c40c0c143e64
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:33:17 +0000

    present set goup range organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
