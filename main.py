5bc516957dd96116dec7fadb63c2357ee99016a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:36 +0000

    computer occur divide above improvement

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
