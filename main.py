4b3e6ce6af5b55be8d6c76602af458bb85a92c8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:31:02 +0000

    careless probably picture man run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
 import heapq
 def get_smallest_elements(iterable, n):
         return heapq.nsmallest(n, iterable)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
