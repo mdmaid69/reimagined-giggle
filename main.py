4bf9533c8f4fc5e52d76a8a64eef4378e48a2bf1
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:07:05 +0000

    open judge pipe desperate advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
