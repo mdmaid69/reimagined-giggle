855dae28f9c69e278355abfb542dfb588745f371
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:45 +0000

    nigga occasional since pollution reply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
