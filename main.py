26ed52ba2de6e91126a0436ca1517d301334bf5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:30 +0000

    somewhere cook light 572enter cook

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
