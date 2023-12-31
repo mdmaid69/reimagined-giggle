bc9ba275bddda716c783b31b822b03590a4437ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:00:43 +0000

    solve thorough wash operation purpose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_power(work, time):
+        return work / time
