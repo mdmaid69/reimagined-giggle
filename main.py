1926f964751839b33b100772f8b4c7486afe7ec8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:59 +0000

    theirs company tape frame turn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
 def create_array(typecode, initializer):
         return array.array(typecode, initializer)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
