9cc091684c09af11d96a3dc4cf899730e6b13d8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:24:35 +0000

    pump lookafter major arrive lookup

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+print(sum(range(10)))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
