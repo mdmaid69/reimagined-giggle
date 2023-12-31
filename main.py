8f5870ae5e50cd453f363d2a8e485ab7429f6c4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:14 +0000

    rain injury onto thought undo

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
