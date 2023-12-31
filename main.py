cb64121704ab6ad027ccc9528c64feb87de5c2d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:36:03 +0000

    since ask underneath base objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
