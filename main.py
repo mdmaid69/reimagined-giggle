b741f991dded14449f072283d31eb38a159d989c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:55:22 +0000

    switch rid goodbye exaggerate top

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
