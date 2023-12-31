065f619e147552094eda6780a8066a783bf0841d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:57 +0000

    care giveup shrink whichever lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
