886f92e6e48398853fe89d4829eed3babcb4482c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:28:22 +0000

    later science I ground minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def exit_program():
-        sys.exit()
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
