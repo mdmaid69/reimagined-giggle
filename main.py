1523e5abdefe37d5d8d9af80ff3d90e89e2667bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:39 +0000

    either happy matter worth process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
