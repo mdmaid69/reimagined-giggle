424fe19dbf20aaa9be0d8a1a318d673703f859f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:20:17 +0000

    trade school shock brief exist

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
