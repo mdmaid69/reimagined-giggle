e757f575f2562062cf01b1380b70be359a966dc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:53:05 +0000

    visit practice score pretend welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
