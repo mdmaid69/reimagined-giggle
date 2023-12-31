b9dd56902d593e0e767fad35ce219116ebf96c35
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:29 +0000

    innocent play cause dick image

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
