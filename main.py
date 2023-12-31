af564b01eb63e43dd1cf0908b8406dd88ff00e35
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:02 +0000

    freeze workon shut gas bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
