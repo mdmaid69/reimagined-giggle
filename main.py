fd251668f16b143b7d83e9739233e1f79613d845
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:07 +0000

    line repeat base break pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
