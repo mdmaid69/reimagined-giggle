3e9bd52f342f98a2665ecff5c52039a98012bff6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:59 +0000

    grateful everyone equal violence trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_sine(x):
+        return math.sin(x)
