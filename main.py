ca8b75c283d082fc8d914aa4c897b79f1a506f6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:39 +0000

    file deliver hard invitation last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
