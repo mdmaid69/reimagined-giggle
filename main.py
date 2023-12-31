d8d08bb6a74a3e1c4479157f1af290e19de0cedf
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:10:50 +0000

    decide pay boring Kk goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
