0a95eb6bf31c1ab275c89e3f9288f936a82f0bef
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:22:49 +0000

    goon sail hard march sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
