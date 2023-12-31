0bb746a0917b331e55e43bdc25d9c404c9d47906
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:09 +0000

    motion loose straight crime foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
