d32e8240e8f45e4a4f25d42e8d6a7e36ae424ee5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:05 +0000

    luck seriously praise lot solve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
