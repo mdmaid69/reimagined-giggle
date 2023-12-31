bc545d69cdc530a4401dfc9c11db5b8751156606
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:33:30 +0000

    gap dig criticize bet fetch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
+def calculate_power(work, time):
+        return work / time
