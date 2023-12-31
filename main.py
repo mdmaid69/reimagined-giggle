5cefd4d7f1a85fb1b7f173707ee166f759181ca3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:56 +0000

    climb threaten pollution they whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
