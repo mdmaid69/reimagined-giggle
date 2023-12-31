26916f07579d37a2509367c24d829b0cf8a97253
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:41 +0000

    help run small Qq abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
