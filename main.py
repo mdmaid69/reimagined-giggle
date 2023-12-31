dd42c98dc5a4faa752c906a8c26b3ffbda64389a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:17:30 +0000

    tidy unable discovery face difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
