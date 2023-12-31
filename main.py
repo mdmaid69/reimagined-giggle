1f16c75b0161197d944ab01bc313a165156cfc2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:39 +0000

    exciting priority down event morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
