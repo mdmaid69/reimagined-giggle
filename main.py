bd86eb1d2ab32bca04797484cfc3a7aa2d53fd0b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:27:57 +0000

    certainly improve fire actual dance

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_int(array):
+        return int(array[0])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
