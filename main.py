a876a8f77396813e8f7da68cf9f7cfdc5c777fe7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:28 +0000

    stare around stone snow runaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
