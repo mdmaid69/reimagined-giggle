81fce34dfe0e1e020f4d55f51aa41f31c87e553d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:30:34 +0000

    discussion tired boil chance before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
