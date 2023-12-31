1beb060058f1409cb93a0e6a35dbfc77103e11c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:40 +0000

    whole possession delicate shelter when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
