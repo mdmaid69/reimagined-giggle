7d3ad6ac72ea36f992cc929f8941deef3942c2b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:42:22 +0000

    tour plan value stretch evidence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
