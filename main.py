57f1c5793469ddf58bf29de15a6b70bec80219fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:51 +0000

    conclusion realize activity attempt pleasant

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
