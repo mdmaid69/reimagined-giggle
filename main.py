afc5e2c045a717797977176cb98ba386d81b7d91
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:10 +0000

    perhaps treatment swing trade off

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
