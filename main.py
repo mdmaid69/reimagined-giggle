aa129a661191aa04c58e46dc9664bb93d3a2be09
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:14 +0000

    tongue bit family avoid violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
