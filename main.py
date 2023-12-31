e1fe4951f1356600ce0983e71291cffb4c74ba62
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:30:20 +0000

    result workon already variety body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
