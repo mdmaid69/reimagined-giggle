76ec5a1b394ece37fc9419b13b637bb425474801
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:55:36 +0000

    copy hit model achievement instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
