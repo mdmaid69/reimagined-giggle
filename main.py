b6b67d7189bb48e55f692fa24093d5f8f3ef68cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:08:31 +0000

    book neither community whistle well

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
