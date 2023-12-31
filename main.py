49d744c12337c280aad53bcfe7d89461aff0bcd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:44 +0000

    him together grand absolutely sure

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
