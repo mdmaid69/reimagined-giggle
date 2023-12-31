bedf93202b2cc31eeb672dce5a6826dad18ee711
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:32:46 +0000

    better skill piece attempt miss

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
