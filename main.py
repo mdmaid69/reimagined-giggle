c0d6af28a35928205dd3403e35d0a571b2d54644
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:18 +0000

    anywayanyhow stop mend about advise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
