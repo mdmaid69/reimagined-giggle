6a8b131127dbf9caab8063f29586d2db8875ff61
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:48:58 +0000

    translate pay hungry report make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
