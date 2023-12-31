1ed50683a5b0de53661ddbb51d1371dd46f17b34
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:31:45 +0000

    harm squeeze opinion quit contact

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
