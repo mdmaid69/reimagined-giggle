fdd8d6019b1cc5afd6e17f4f96a0893b0924529d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:42:20 +0000

    profit although warn shock upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_acceleration(speed, time):
-        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
