38995ce8b527e885f7660ca2efb1a579efd2d428
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:33:08 +0000

    ourselves increase stupid above space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
