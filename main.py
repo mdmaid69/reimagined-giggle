c17f491fd81a9144e93ffae94af18fd6741906dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:55 +0000

    police cook treat exam separate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def is_palindrome(s):
+        return s == s[::-1]
