b126dd4dd209def8c649c03973dbdd6a1d6b039e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:16 +0000

    become body classroom empty base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
