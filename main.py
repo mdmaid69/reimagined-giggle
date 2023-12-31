5abdea9f4cf0909e032ad5e3e875f5907f908ff5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:34:56 +0000

    pile cost thank set owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
