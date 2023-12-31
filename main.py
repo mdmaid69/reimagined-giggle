0ed4e47c5f560396fa451d49c9a983afe1ac3268
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:03:36 +0000

    lookup contrast split wonder obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import random
+print(random.randint(0, 100))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
