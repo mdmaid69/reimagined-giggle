4b958c967897c9dccbfcc382b0fa87abf1bacb45
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:10:30 +0000

    pity everybody bend worth fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
