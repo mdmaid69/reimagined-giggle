3323e79c6fdd1cdba27df78d3d8597519a458f80
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:46:35 +0000

    doubt flat runover money meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
