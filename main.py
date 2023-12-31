ccb9f5fdb1860d768aaeeac3a86fbf3858a763f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:30 +0000

    clever free copy rule sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
