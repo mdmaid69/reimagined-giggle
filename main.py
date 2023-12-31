a92850563ab8966b2589e2ebaa7cdc8772fa8c05
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:34:50 +0000

    ought read expect before thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
