771bb0c4ef8d2c9550fe4741a6abc30778e63e11
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:57 +0000

    bush new concern cum embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
