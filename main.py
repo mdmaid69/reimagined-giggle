75ac101eb5055e3a25f812b847b091bf6a150af5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:13 +0000

    live question make minority copy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
