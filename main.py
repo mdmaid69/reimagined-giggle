ed7eb3815c76c58dbe53160192d8aec3190569f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:35 +0000

    walk anyone clever dress thorough

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
