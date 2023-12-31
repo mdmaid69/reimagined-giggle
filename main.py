a9eda1a04b56798c015b76bd24fc688c718db7f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:01:03 +0000

    party rank turnover bus when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
