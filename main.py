58c6dc434ce69025125339f12e09a83150c28d60
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:21 +0000

    disappear extraordinary stage condition brush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
