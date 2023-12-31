3948e7446f8197f253c1ef34eb2cdf1bfb4132a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:05:48 +0000

    physical although rescue unpleasant shelter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
