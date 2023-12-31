cb13d3fd145aeac445b2fc5beaa1f05805b8a37f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:16:50 +0000

    unkind issue free more difficult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
