2ab863c33998875bcf84ae8ac45ca9f217226007
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:17 +0000

    he disappear gather break extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def convert_to_binary(n):
+        return bin(n)
