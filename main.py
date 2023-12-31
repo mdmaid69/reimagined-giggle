48c62ac64c9562f64d752635c3457f6cb974db2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:00:23 +0000

    programme under journey health cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
