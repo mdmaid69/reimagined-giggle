396815a24df667ecefeb3a5a43a3bdb67384cc1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:56 +0000

    share turnup skill surprising eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
