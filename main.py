607c8ef3e84fc072478d4a0bc29a013e1ede636a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:16:43 +0000

    organize pencil trade value try

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import os
+print(os.getcwd())
+def find_max(lst):
+        return max(lst)
