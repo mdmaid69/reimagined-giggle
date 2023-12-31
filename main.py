105b0f355d247f84b73a8a6b3c57376a9e858c81
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:48:58 +0000

    company exploration certainly male clean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
