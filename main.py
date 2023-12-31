0961b993848fe48874886e5abb0ecf745a5779b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:59 +0000

    mysterious over unit put put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
