4b84298bdf40f890a5605f672a32a68c3bd9433e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:29 +0000

    public once song else wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
