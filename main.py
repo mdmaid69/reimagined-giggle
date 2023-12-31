9b5313ff6c11594db08339db19fc995f0d5a6700
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:36 +0000

    watch aim right inside nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
