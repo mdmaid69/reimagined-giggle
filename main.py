6b5fcf068e6f35a6337f1814251d8c07d199e65d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:05:33 +0000

    shoot but cost form bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+  def multiply_numbers(x, y):
+        return x * y
