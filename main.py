1a24b2e5f28bfad8564720c2b1c361ea909ca813
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:00:45 +0000

    water fairly money move undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
