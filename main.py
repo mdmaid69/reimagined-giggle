b338eabc64ed12500c8da00e275adbbe84eb4b12
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:18:49 +0000

    guilty left safe tough development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_set(array):
+        return set(array)
