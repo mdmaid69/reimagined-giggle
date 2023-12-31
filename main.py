94ad8e79fd8387c0e52ce9edd21259940cd1f5a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:33:35 +0000

    strike drop remove comfort approval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
