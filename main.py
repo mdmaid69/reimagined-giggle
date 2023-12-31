0cdeb860f79baaecf87137b8905b1e15585a5c34
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:49 +0000

    worth succeed crash drink name

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def cube_number(x):
+        return x**3
