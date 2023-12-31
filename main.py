61a7d8487c1fb6645bc0bdb4736d64919af2bdb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:10 +0000

    cap space promise cost post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
