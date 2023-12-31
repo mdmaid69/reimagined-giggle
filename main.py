3bcbd2cce44b8f2aafbfc5067c95d315dda4557c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:16 +0000

    Gg lot slice wheel computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
