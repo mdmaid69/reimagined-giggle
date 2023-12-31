42eddd4135e87a9c5e020befb72122568493a03e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:25 +0000

    difficult mysterious minor believe blow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 def reverse_list(lst):
         return lst[::-1]
