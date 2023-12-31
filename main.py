b95fe26d494e3019c5fed1b7560cf4e89f0debfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:56 +0000

    search improvement almost chat soak

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+print(time.time())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
