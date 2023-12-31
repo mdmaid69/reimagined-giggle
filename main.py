1f6bf8c351843a5ba84cfae922a05385e71230ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:55:38 +0000

    fit yes search there goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
