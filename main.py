432641ca4b83082f71339bd8b4928fa43d1fd961
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:01:20 +0000

    distance score comein children to

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import random
+print(random.randint(0, 100))
