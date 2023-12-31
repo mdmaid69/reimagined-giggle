13053fcc9c52407ab11ecf42d3f19d0d7756d656
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:21:14 +0000

    show content game earn number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
