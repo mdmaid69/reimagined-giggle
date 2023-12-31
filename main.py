beb9168cdd7bf9ecb7eabd580d9ca429bdd03814
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:07:26 +0000

    giveback pair common square hurry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
