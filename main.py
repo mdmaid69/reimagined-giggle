11382f21bac99eaea44546e8ca81e3af9f0ebc53
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:06:09 +0000

    tap table interruption important near

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
