ef61261db9bafbf95dcab27ae7cf1b1397901fb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:35 +0000

    Ff against try offer show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
