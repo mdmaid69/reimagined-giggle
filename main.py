9d8b5db4fa4caf5032cebea67dad1941a6387f3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:22:31 +0000

    load evidence guilty decide pure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def reverse_string(s):
+        return s[::-1]
