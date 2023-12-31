eadc1b9fc7d89173fc5ec8119ba1c7bda38e6aff
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:34 +0000

    name obvious month wrap bright

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
