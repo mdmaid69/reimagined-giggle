9dfeb0987dc561ffdad7f9e37463e3ee0073840c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:21:00 +0000

    praise speed board want culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
