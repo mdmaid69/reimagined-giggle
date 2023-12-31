998db455f7f4781d26af3168b2eab7fabd35dc74
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:09:38 +0000

    comeout frame size go plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
