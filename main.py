c354242795b926474960eba38d6caa3755e7b0cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:54:49 +0000

    common fork waste regret time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_string(array):
+        return array.tostring()
