345013345e05505c6d0d9d68349c18291931f93b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:17 +0000

    wipe underneath belong lock effect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
