df945e3e91ed6a5be5e36945235bc78c28b9dbb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:21:03 +0000

    present cross test early target

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
