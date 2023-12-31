a2330fbe889815c68737afdf69a34377f9fe81b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:43:08 +0000

    make present plate Vv apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def iterate_over_array(array):
+        for item in array:
+        print(item)
