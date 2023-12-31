52cce91aa01cf54a2b6f2d6c995afee38dc68b26
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:59:02 +0000

    conduct remain black war reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def convert_to_hex(n):
+        return hex(n)
