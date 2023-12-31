cdc9b7131ef42b9968611bae2ea367ecd2c7256f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:58:03 +0000

    size bread raise chief contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
