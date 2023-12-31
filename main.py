5878b2e7b0a5c1e2348f02d2609d41dc78016c9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:01:08 +0000

    dick cool return swim onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
+import array
+def get_array_as_set(array):
+        return set(array)
