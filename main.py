964325e4d1bc0f34ee511a43c05528a0f55161eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:59:17 +0000

    success pause motion book store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
