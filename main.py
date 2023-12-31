e5aedd817c017828a86580e81086ef594791370d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:15:34 +0000

    anyone exact regard bored draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
