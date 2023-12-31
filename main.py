fa04a8d50418ad108c6406f94ebe858dc7b5be49
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:38 +0000

    wind grade relax bang not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
