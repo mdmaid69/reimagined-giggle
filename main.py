6e4ee601539116c17bc08e3eaa49d288e0b0dfd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:26:57 +0000

    when notice Ee discover really

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
