8bfbbd3c32a299bf59db9f13d0c6dedea899cc6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:44:40 +0000

    seem design set connect let

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
