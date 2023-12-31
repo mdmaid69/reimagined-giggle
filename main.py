bf5f79ad78db5d57af026e65de2d10205194aee9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:58:59 +0000

    split still fix terrible friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
