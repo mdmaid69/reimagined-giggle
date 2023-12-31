15774dc69e9ab1c29148423e952d988487eefc6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:29:55 +0000

    price dance beat arrange apparently

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
