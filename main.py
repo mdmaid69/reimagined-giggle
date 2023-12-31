477a883361d44c02fb3bca1ca8f3f04964e20859
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:41 +0000

    whatever abroad way copy tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
