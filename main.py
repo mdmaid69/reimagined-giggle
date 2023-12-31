bbfc8d3f597ecf45586fe3970498ad8c8234c149
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:17:23 +0000

    winter confidence mark team kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
