0348d6b8221013613543ae582550e08b28974ceb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:03:30 +0000

    lay motion noise much proper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
