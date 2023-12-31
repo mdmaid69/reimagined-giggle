b0a562cfcecfdc63987e05ef9c19fb731da467e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:04 +0000

    turnon rush private pipe loss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
