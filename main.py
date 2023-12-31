c44e8f4a2e224b2380fc010d470a40b9e58daa1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:49 +0000

    Bb glad late cheque limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
