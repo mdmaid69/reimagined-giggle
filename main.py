6c473c4bcfe1ff68eeb12cbaa5baa1769df46025
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:29:05 +0000

    comeround zero promise many breakdown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
