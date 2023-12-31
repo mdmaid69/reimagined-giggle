51c672b816c18399f2b6280815a1b70b81798211
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:31:07 +0000

    else later squash lonely tip

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
