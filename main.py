5a0bf4721fc26d002231b472d22a498adf186ac2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:20 +0000

    speak cope means bother spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
