82e486b438a663f096a9bb0b823555e66285e76c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:05 +0000

    check slowly bottom boring unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_repr(array):
+        return repr(array)
