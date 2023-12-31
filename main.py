97cecafc638b361ebd21cf5f1f858389c292f58b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:08 +0000

    fact study wing style back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
