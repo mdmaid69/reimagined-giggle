fa233baa2a81e4c82cfba50be2a49bc64126da69
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:04 +0000

    connect Ff otherwise freeze skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
