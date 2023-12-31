55b300cefc38b3c1cca90938279d9e1c34b3a845
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:31 +0000

    undo cope precise violence shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
