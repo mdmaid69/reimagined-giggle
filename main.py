145f1ec5713407ff6dda631adf6ecf2ae8aea3b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:50 +0000

    plan smell fork exaggerate science

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
