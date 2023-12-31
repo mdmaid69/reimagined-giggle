f7f2df6eb3b4faa5e923415f1492abd823e6ed98
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:32 +0000

    soon shoulder fail shock refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
