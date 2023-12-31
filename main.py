66afae1a57b3f37763e70b3c4e75ea6aef8893c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:40:20 +0000

    confused pay anything soon room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
