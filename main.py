a62a08da7a4fbcd97bfe5eff75f3d508b7accc4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:31:57 +0000

    fine sign argument entry push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
