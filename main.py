f9001a2f34c9245452814dbdd7d28eae7ad56db8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:38:38 +0000

    lonely right normal accidentally embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
