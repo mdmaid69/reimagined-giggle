4984e66a5668a93be840c0c5bbbb8aa71f645343
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:27 +0000

    search instead raise breath day

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
