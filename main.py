c962e0a4808d92f3569458717e6fc158d24d0ad9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:22 +0000

    guilty whichever platform face rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
