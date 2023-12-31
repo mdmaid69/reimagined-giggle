56940b3d5a6d50548e534f78f960cd0b772020f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:02 +0000

    fashion transport me past either

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
