51e8cfed5456def58363c2c3e4707aa2f2f7a9f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:19 +0000

    comefrom deliberately pause spend get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
