ff10a2495f2606b2433127ca2558f63779c92445
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:55 +0000

    breakfast position rain convincing trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
