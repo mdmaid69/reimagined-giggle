504e1359a140cd68fea27684639e71cac1645428
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:00 +0000

    almost centimetre scrape escape ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
