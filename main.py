8ef0a8befc550e3b56f0087c12e935e415870739
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:40 +0000

    boil plain sport lesson physical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
