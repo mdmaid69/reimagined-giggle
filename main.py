449b169fc12071001b970b542b62cdf860d0c085
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:45 +0000

    accordingto bye suffer near last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import numpy as np
+print(np.array([1, 2, 3]))
