8228a6ce197c8367d815ab137dda48e0befe955b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:33:42 +0000

    concentrate remark light mood talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
