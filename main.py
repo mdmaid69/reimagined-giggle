61b6733e6b84115cdd511223ef8e8a3410d095bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:16 +0000

    wave another constantly shut Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
