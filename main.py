58641ae60fb04dfb1c4a8a9fac25706922b9e514
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:43 +0000

    perfect teach idea tread corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
