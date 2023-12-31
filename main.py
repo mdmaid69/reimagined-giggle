5a1a210ca5523762bcdfead66ebe941b7216e973
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:25 +0000

    discussion confident remove fold charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
