1aed7dab8e0f48e1bb08205a31db68c53acb3db4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:35 +0000

    middle sorry evidence punish final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def add_numbers(a, b):
-        return a + b
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
