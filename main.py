431479cfdd09ce10748040821f644f0fbe65e538
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:59 +0000

    visit each switch wide culture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
