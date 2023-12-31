8b20f9ea20f747caa544f425d58f4de8578773b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:57:12 +0000

    unit unite hard direction direction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
