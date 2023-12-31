c550dccc05bd29be9614590f10b27cb2c4cbc236
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:34:50 +0000

    manner water go part party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
