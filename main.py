49ba6e292d5a2c596ed2211d7ad6d4b478fb0cd8
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:34:15 +0000

    ride this pot smell punish

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
