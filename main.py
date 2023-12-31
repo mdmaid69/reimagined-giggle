7f59bc1b330741b2cc0359fa920f9296588ea231
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:41 +0000

    snow include cry walk future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
