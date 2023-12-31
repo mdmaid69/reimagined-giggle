b146cbd6fc716ec83bfa3f466d2187b7733070f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:33:32 +0000

    instrument far deaf draw watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
