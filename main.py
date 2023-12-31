d8022620e4e5a112f00f8b36672f94c51972a80b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:40 +0000

    passage after much equipment attitude

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
