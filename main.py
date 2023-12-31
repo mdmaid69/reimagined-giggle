396d7faac4f7cf750ed80b6c4d8b8a6bfe7800d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:33:36 +0000

    increase degree music shoot possible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
