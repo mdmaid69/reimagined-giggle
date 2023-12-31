fcc41d2b4169d88739ec600109a14465bc658345
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:00 +0000

    week news 564encourage Pp protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
