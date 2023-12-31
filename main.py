80f0990e6d71a84995a3c16354c1d82d1438856f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:33 +0000

    repair mind wave will dinner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
