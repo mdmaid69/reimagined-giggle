865650ff7edcf70b0575527cd99c1b671c46528f
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:27:38 +0000

    into leave lot doctor thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
