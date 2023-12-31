2311009a4d0f30b52077ed4b7654a59c8078566c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:15:14 +0000

    sharp record everybody mass bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def square_number(x):
+        return x**2
