ad2dda1eb4090e8793fb11bdda64c5df65f81099
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:57:50 +0000

    fit passenger bet temperature gradually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_work(force, distance):
+        return force * distance
