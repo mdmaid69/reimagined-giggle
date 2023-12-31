bd82eb7a9c941d28b670aaec348cf102bf8b10c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:56:03 +0000

    field view rid content theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
