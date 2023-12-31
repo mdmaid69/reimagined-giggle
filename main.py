2f7289d6e184de76ce3825763c6e0c237d250669
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:05:12 +0000

    concern story what paper dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
