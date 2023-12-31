e8dc4e5ac33a0f1129941a83633f135d6a6678f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:38:03 +0000

    smell bit science store afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
