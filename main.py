2d03db7e14fc0f14d2cd25abe3cc3b805ba64e59
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:00:42 +0000

    somehow shade able tense coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
