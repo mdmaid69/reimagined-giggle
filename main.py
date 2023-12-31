9090efc13a1045d71fd947d1687d3ef8b07ab3d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:22 +0000

    also comeout great slowly represent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_current_time():
-        return time.time()
 import collections
 def create_priority_queue():
         return collections.deque()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
