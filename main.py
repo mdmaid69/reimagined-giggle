a41473537a5107f40dfe7ce4b70627a3b79514ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:43:18 +0000

    needle concerning life prefer shock

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
