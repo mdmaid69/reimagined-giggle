efc8bec08c00f73622e0a41def6212d691514757
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:47:42 +0000

    whatever town finger bury education

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
+from collections import Counter
+print(Counter("hello world"))
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
