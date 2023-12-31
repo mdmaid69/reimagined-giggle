1557ec22bc5e929875b2ab1775a91353fe597069
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:37 +0000

    order string juice even steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
