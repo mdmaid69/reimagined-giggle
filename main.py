8b33d89e342fcd60378bed9be877941bbbce4c74
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:37 +0000

    less threaten record allow live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
