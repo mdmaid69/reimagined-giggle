05d13a2403904afa06fb909f4baca3bd9b89d891
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:05:31 +0000

    thinkof threaten typical otherwise beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
