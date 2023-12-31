6ab96594bc25b911b138f11983dd72fefda3e6e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:50 +0000

    fly appear tradition gap relief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
