b218a650ccb3ee14847830a2416765b6fbabbbe9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:42:37 +0000

    pump steep front alone rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_length(array):
-        return len(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
