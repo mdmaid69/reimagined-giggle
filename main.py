3a0edaf9eda28f19fb0c8c061a4c1d617a7f9d4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:48:38 +0000

    workout explanation no dead giveout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def add_numbers(a, b):
+        return a + b
+def add_numbers(a, b):
+        return a + b
