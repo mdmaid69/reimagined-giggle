45d2f5e38b2b71bce5a36427cfc383bb6f0114bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:15:40 +0000

    instruction event idea whether suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
