cb6e313534633c41f4be8588e74cb99e4280b9a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:58 +0000

    noone there table situation air

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_pressure(force, area):
+        return force / area
+def calculate_pressure(force, area):
+        return force / area
