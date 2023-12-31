8bc07e8a02bb80486c8bfad8979c71335378e9aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:56:54 +0000

    refer emphasis past truth friendly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import re
   def replace_all_occurrences(pattern, replace_with, string):
         return re.sub(pattern, replace_with, string)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
