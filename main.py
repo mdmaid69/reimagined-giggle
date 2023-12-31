b9560bd336c0a0b1f4147230da04bbe6f3066623
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:51 +0000

    less weigh design hard mother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def sort_list(lst):
-        return sorted(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
