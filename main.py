f4fe7d9b7ebbde42090cc906fb00d31d7675a43f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:22 +0000

    business fit vision mood fly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
