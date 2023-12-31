5eac65c85cee045de12d72b0875d9cf47a079f1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:16:58 +0000

    sad read fast crowded unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
