afb248496385a546570e3f5a12300f1897223076
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:30:31 +0000

    wipe scale she concentration put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
