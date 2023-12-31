20fb63e733b93863abd6379651b222ddad91deb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:10 +0000

    more pride attention hollow immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
