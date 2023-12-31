999341350ebac31b48bb5e6731b1427c0b27a35d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:38:26 +0000

    male undo scrap noisy back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def convert_to_hex(n):
+        return hex(n)
