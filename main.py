262087cba4b0be2ec97c67e1d987e79c62a02bea
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:57 +0000

    scale complain spoil fix really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_characters(sentence):
+        return len(sentence)
