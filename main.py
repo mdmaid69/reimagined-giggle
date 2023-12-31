ecdfd12639684711c6a32553afab9314e4ad1f3d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:55:26 +0000

    burst deliberately father mail rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_repr(array):
+        return repr(array)
