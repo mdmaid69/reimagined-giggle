370e199bee90ede6672a4958d0ca24fcd52bd269
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:12 +0000

    blow choose money university opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
