398ce0529d1e97edcba3a6bf53093cec56515ed1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:53 +0000

    storm against university describe suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sys
+def exit_program():
+        sys.exit()
