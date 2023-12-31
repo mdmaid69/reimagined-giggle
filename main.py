3ec6d2a6e0443c4e3b66327a1b839bb582d64f1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:26:33 +0000

    health intend bend holdup side

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
