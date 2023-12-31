e3c63ebfaaf3cbebd7d156bce8a580da15db1b80
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:01:20 +0000

    father inside window partner recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
