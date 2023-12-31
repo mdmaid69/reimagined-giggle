6ad16498a4877199469dccc550f788b172108311
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:29 +0000

    fear exploration however takeoff exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
