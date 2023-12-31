6404cb45f85242dce06a2bc576b4c38c859a468f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:48:58 +0000

    bit question soon goout danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
