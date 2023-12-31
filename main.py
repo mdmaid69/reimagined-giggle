e5aab07390cbdd908fdca094ae18afd007a482b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:05:05 +0000

    reply careless confused cough search

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
