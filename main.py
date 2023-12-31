82dc92489381cccf8deed1be7d977ae96bda1495
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:02:35 +0000

    get port evidence visit love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
