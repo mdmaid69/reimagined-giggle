69641606173c46292d481e6bf7e6bc71d9d97ed8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:17 +0000

    trade more wild how shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
