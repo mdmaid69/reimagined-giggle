d962ebca9f547fc43eb977e55aadec0f1f416274
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:20 +0000

    callup comeout that tall poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
