650325363a295b76d3388ddb0eca73131632b9fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:02:07 +0000

    day surprising 572enter rest weather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def convert_to_octal(n):
-        return oct(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_repr(array):
+        return repr(array)
