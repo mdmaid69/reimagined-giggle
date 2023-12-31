f1d51351e083b3bb85cc26bfe65292b52e5b6b62
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:54 +0000

    party report within radio disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
