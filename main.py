8c55f76a79745d2798a5d8bbabe9f2b322b787c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:38:40 +0000

    scene their path rest interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
