fdffd9371c287dd0a2b173ebbea134a261466ac2
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:45:31 +0000

    from even yours reduction replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
