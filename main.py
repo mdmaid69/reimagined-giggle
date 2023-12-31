8af4da29d6507d3b58d160928aa70ebf39b667d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:46:47 +0000

    to entry mention open depend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
