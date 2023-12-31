a8b7cb95b73b4391f9f254ce3e680b3a8ed56a7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:20 +0000

    case unless form image Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
