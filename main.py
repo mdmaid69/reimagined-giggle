7cb9b37e221489a92d9382d25b7bc9b7e82b99b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:26 +0000

    hope priority ability vision price

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
