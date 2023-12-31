253f789f190e7b1b5deed3182c786796ebfbbd7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:38:57 +0000

    understand age issue meanwhile build

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
