87857d2519f494c5fac51aece603db66d33adcdb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:21 +0000

    567enemy draw square cut there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import time
-def get_current_time():
-        return time.time()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
