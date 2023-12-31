399a980b9fff5109ab0577c229db2c418f82fcb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:24:33 +0000

    against Ff direction somebody hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
