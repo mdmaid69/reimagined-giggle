10fa41947373c02159a7e6340355df0e92702174
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:30:10 +0000

    abroad listenfor offer book stick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
