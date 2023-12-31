458e11fbf1f16c868d5355657b705e51fb16777d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:09 +0000

    basic wash human arm consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_str(array):
+        return str(array)
