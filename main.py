5dd53b35bcc282c9fff04562f56810514af3312a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:57:16 +0000

    instruction Qq welcome cause square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
