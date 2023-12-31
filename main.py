d7b28d9860c60df731721ee8f90b90a86ff3763b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:28 +0000

    window difficult whistle embarrassed spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
