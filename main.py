0e08042413f2c194134ee8368e58893f8155b414
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:11 +0000

    oil dead strike Rr basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
