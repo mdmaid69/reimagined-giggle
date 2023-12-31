1af547024d9995a831c08c83f1aeb32321659c2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:28:20 +0000

    steady Oo gap scream between

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
