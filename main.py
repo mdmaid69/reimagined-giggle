8c814f8ea758d8e9025220d74705e6def380168a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:31:17 +0000

    difference test pour scrape knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
