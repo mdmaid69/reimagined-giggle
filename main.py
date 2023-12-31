9565ffd923172290fe4ee264919e0cd5fe4129cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:33 +0000

    run pin Ii wrong unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
