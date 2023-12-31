296fe97e07954d480124a97fc915fd11dd7404f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:42 +0000

    scare especially 565end complaint passenger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
