8625206433b5347c2b4717d39a11bbfbf6630411
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:26:34 +0000

    discuss modern hospital pretty claim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import collections
-def create_user_list():
-        return collections.UserList()
+def convert_array_to_list(array):
+        return array.tolist()
+print([x**2 for x in range(10)])
