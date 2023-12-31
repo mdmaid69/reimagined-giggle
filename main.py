582a7527f122664a0bbd0f618bd870401f98a2f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:15 +0000

    news private defend container guilty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
