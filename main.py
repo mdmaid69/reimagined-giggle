2d5afd5dc79fbb8128c33c2ea2b3e6731d07b351
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:11 +0000

    risk brave spray advantage meet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
