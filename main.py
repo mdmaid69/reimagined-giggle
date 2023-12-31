ee0f5a1a46cbf3d39de43da710f86afdf79d3faa
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:26 +0000

    phone always indeed possession let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
