d993d4109f4b190bfa869e1a1ee017c247ae61e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:27 +0000

    care giveup kind criticism such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
