08467ffc093d47196067e3655ede97b0f6e3b8b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:48 +0000

    borrow shoot giveup Hh upset

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
