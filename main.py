61372af18c7c2e3646eaf29ad73ce613b976c5f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:41:58 +0000

    interruption keepoff tense bring brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+for i in range(5):
+        print(i)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
