81f6d4b12a61229636f7cf8459666007e73bedcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:36 +0000

    operate accept face of however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def is_odd(n):
+        return n % 2 != 0
