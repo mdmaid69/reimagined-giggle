47de59e796cf09c20ebb8f0c250b76be01bba661
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:19 +0000

    scare through thankyou by born

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def is_even(n):
-        return n % 2 == 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_unique_words(sentence):
+        return set(sentence.split())
