eb50d3c0678443381a7928bed0a00654e28a3b63
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:11:08 +0000

    worry insist chin provide forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
