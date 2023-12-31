6bfc0b41404036a47e5b744d540df1c78180c395
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:12 +0000

    broken pot keepout right willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
