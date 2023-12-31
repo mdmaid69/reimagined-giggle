5a39ac1e59fdf0ff1660945c442e2865bfe01291
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:04:37 +0000

    shout hide apply sing measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
