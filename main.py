e6db99779325b28a0ddba8cdb9db8ebc567fd388
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:34 +0000

    tour emphasis stand suck talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
