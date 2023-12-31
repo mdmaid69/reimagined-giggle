7f9a979bfa5016079cd77d9a2c90d2a75ee4f57c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:22:27 +0000

    hang breath drive more authority

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
