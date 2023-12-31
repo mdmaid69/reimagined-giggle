6719d9eef088e797aea55bef7b479710deaea5d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:21:26 +0000

    consist lean judge single right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
