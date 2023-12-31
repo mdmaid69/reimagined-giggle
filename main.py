835932b31399f8b897cb56d6c879b93ae0500f61
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:32:42 +0000

    feeling always finally advantage consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
