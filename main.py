5aa14357586511ccb41b40b14c2f096532eb9480
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:33 +0000

    song ago rob discuss neither

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-print(math.pi)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_speed(distance, time):
+        return distance / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
