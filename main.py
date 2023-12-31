7034f5a40121936874c5dda38340966121b787f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:07:17 +0000

    keep fall programme keepoff Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Words:", len(text.split()))
