d191d0dd4f4f3a6c2fecfe30b5a848ecad731917
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:30 +0000

    mother book chief together slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
