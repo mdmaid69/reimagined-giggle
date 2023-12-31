698c114d3fcd7fb2f5f678719c42763e05ff587f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:15 +0000

    important spite forward world sharp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
