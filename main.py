d51ad26e2b6328f383b1379b12d03f1db3951696
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:39:50 +0000

    this as turnover lay charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
