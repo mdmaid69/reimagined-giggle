1deb5ea73e549aeea3bb7fce9c530709884ba8d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:27:12 +0000

    control save although damage accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
