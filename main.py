499a4e7cbe1b87f4be61b9d79575019aaf6af354
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:55 +0000

    getup winter loud skirt serious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
