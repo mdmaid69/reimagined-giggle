9da13ac3b29fbcbfb0f0a4db3486c0f340b4183b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:05:54 +0000

    regional completely feeling risk find

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
