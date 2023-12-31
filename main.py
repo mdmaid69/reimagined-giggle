762068099608863189a2b1ac399919e5bc55902e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:48:03 +0000

    stir whether spring natural second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
