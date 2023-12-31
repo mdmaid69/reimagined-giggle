940406f8bb38b9b264530b45aab63ba7e15c3c1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:48 +0000

    lead operation inform report bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
