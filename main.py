d7c3fea2f3bb9fa360db5e33aabf163b896a8e76
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:12 +0000

    wash brush by onto curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_sine(x):
-        return math.sin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
