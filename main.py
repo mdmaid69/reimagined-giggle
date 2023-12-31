412486bb2bcd000fd7bb65e2eb2913fb04467b38
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:40:59 +0000

    minimum charge may machine wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
