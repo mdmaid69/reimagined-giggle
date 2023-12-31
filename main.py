b72fc035029bce7c1b9f88bdd824821c4b91a5e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:22:32 +0000

    conversation class point address mood

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
