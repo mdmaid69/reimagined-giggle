9fcbcc9d83af4db961bde2bbd70bd457a7d37af5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:58:00 +0000

    fence pause minute trade bread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
