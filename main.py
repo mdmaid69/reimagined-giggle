f066e045005931a52cb24c56d0fe98d1bf68b03b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:02:30 +0000

    breakup reason care event poor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
