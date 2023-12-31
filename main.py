e83cc56a6bd8611ae80832ab28c0091baf5585ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:51:09 +0000

    lunch one something of conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
