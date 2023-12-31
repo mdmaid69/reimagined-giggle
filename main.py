618b246f89b1c9367265f36ba801a1c0d7e847fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:15 +0000

    yes visit generous case us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def find_min(lst):
+        return min(lst)
