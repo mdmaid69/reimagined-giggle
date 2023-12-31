13ba207d239aa9287754d332499915bb0e2e57a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:37:02 +0000

    minor natural Ee open weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
