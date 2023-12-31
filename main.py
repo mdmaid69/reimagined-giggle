8b3f0c2d151f6acbb272709560ec2bfce9a2631d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:36:27 +0000

    public maximum better beautiful afternoon

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
