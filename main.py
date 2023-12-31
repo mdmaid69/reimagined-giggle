979c5d73e7f42e029fea5d81ee74c7e4a1d0584a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:49:50 +0000

    practice fond yet study deep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
