7e1d94dff9ed2a47cfd09e52483d42377205e920
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:50 +0000

    soft deliberate through affect urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def count_elements(lst):
+        return len(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
