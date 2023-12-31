20392cf471f082da5ba2de9d66d1fdacd0b843da
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:28 +0000

    government slice smoke story total

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
