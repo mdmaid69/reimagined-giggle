58b927c5ebb4693ee0caff6c0badda836394849c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:46 +0000

    shoulder standard dish false control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
