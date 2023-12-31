b3b64568cd085239ca4d834ca00f9051ca8a9567
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:28:37 +0000

    below us pen calloff play

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import sys
+  def get_python_version():
+        return sys.version
