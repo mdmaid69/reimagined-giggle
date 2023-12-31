1a42121c560e4a1f469baa10375c3c042d2c5c83
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:16 +0000

    pass remain fierce busy where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
