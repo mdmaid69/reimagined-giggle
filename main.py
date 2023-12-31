603d355dcac9c9c04f9bc2921db488b8ae05754b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:59:27 +0000

    stupid safe slightly glass stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def subtract_numbers(x, y):
+        return x - y
