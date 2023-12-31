8e30b78024a665ec056e4e471dbf1fc28befa0bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:16 +0000

    declaration opponent plastic crowd basically

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
