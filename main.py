2cd6762b2e4d3a2c84e0ef22e42c0e499aa49a1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:00:57 +0000

    jump indeed dress fail tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
