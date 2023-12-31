d167864899b6d5abf6a2b9043dcab8eb14fc4a72
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:56:57 +0000

    exchange attention crisis Rr rid

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
