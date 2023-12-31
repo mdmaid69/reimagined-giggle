ea335ce46d12c31e886978d95a4ab8fdb9773bfd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:04 +0000

    makeup report personal check story

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
