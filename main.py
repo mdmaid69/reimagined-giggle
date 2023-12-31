954ac2e86fc6cd374db04f6a3efe790285e03017
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:06:12 +0000

    easy slip bag sure admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
