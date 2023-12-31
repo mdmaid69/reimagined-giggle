499172a7f920741ce21ffd9e5a5bdd4e7686867e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:35 +0000

    exit sweet steal false bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+for i in range(5):
+        print(i)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
