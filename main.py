3a670b74283adb8f4627b8e944685b534a62db99
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:41:18 +0000

    thick guess back rescue walk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
