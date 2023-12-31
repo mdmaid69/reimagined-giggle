d9fbbe909cfdba61591407f7f392e5f3f487cebf
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:08 +0000

    eat rub copy used because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
