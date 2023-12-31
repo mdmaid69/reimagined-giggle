56c21e98f03dc0a9739cfc8926575b2e575cfc0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:13 +0000

    fresh activity must wash once

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def multiply_numbers(x, y):
+        return x * y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
