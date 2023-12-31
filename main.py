c6b90075dd4144abaf5ca973c68f606bc2dc2c30
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:10 +0000

    take rain authority supply accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
