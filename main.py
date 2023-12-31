5e92a33bdf941d20685c1b9b059a4e88a4c3686e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:53:04 +0000

    ring sticky upset trade figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def clear_array(array):
-        array *= 0
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
