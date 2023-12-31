453751c087f2a5ae45a289475a83972c5171899f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:40 +0000

    me dress soil Rr compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_array_to_bytes(array):
+        return array.tobytes()
