cc4eb40f29a39b93432865700c1de64673737020
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:30:48 +0000

    cheap slightly save there expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
