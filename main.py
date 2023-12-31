3bc34bb5431083f61efb1faf28d30a8d72ead74c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:09 +0000

    lock control worst dig familiar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
