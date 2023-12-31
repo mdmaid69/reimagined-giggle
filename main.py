290c49d54c850e53b851de50513e3fe491773a25
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:32:33 +0000

    activity length operation divide drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
