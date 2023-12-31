5a3964edfeb14c5c42e3118835ba664eeb1d72b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:33:21 +0000

    Ee value second sticky succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
