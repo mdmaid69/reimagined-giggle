d2450da53aa198b90ec959a2f75a0bcd01116b2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:19 +0000

    mark bye better morning worried

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_typecode(array):
-        return array.typecode
+def iterate_over_array(array):
+        for item in array:
+        print(item)
