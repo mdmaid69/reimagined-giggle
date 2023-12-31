7dc06f33853ec4b740df7fb9cdb975a45ba468ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:01:22 +0000

    throughout always dry sense getback

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
