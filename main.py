6a34959c592d9b42a6226d34606e16177acbb05e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:21:34 +0000

    considering quit phone scared view

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
