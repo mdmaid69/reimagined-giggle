8bfecc500187e247e3c68060c762b3750ce65ef6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:51:52 +0000

    whoever communication impossible wake shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
