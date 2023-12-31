7bf8381f9edb117be7b73acf11886b9f63f46fdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:59:48 +0000

    rather collect buy view telephone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
