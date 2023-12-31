c667e7a53f31d874cedc3dfdf294b259a1b6a776
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:21 +0000

    573entertainment draw shop breathe learn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
