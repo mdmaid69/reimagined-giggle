a68c5f1935d03f73328585104e3d276cc9916a32
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:47:11 +0000

    best disturb bath nonsense for

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+print(time.time())
