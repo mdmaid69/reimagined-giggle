2cb26d876e192ccd628b32c2553dc53a7fef2d9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:52:17 +0000

    pump goaway hospital warning whichever

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
