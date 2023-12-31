2a75a954a8d8f18d0201c1594deadb6900e71df0
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:27:01 +0000

    or spare comfort interview watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
