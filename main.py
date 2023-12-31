76f091c72b28b9dba3383593286e4b836a0a9809
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:11 +0000

    please put ourselves along supply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
