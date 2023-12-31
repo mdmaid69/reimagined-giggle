b53ecefacbee1b76513ebcd4757a6cb101985b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:04 +0000

    whatever already study advice bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
