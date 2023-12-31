b7607997eb1c6d835ef57885a6602986254eefd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:10 +0000

    floor reach drink would tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
