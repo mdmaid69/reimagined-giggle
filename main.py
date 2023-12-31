52f406a534fa91f1a27a5b0ace6c1fe3617ebdbd
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:05 +0000

    shop period road criticism paper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
