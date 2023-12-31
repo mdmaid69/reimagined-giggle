1901dcfd7619ec8a70fb2cc53abc2239bbe187d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:03 +0000

    success relative person figure body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
