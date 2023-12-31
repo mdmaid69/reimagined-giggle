703fca4a4557c04b060137a90ca968425a7a67f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:54 +0000

    lift spite appeal yes major

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
