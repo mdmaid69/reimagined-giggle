41c1ede8ca7b76ee42e6e787c7fd75d001c7ff87
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:04:12 +0000

    eager school closely die put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
