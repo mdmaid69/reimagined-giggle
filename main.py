df5053ceb41cd8bb06c66768e7b3ab8e9935a246
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:24 +0000

    bill sweet frighten polite powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
