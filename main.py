ae1a209cc1e077f862c38619a916b2e4c8da89f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:34 +0000

    opportunity himself takeout example proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
