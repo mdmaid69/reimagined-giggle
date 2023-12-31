c7fd321f7c99667464b94dbd4a952a7d567a953a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:24 +0000

    divide fit round neck regular

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
