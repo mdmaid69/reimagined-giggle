316d7970fc59681b75e2a4d1be831a66bcacb587
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:10 +0000

    change few develop ready neither

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
