fe203e42aa5db97fd1c335e09fbe3a59299eca54
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:17:58 +0000

    close flavour well slide strong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
