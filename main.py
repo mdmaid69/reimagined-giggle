cf2cb8b62ee330461971335b0cb9d1ea45572105
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:52 +0000

    field stop minimum unfortunate unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
