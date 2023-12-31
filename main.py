98c08190b7fb1b12404e462b3e40faf19a72d834
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:07:04 +0000

    model exit among owe chance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
