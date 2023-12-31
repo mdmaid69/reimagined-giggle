183d9fa32b143b3db4f6af5dde5db3bc16cdc0d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:02 +0000

    hope thumb year reply Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
