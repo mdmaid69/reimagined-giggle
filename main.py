64aeb1e8867f032dabed751d0fab643ff0e99226
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:05 +0000

    matter confident big burst crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
