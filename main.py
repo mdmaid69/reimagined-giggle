aa1c63649b1d286d7d67f8969b38d1e38a2f966f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:55 +0000

    if you lookafter test water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
