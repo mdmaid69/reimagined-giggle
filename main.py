ff7cf75d56bbaeb763a5e2b046e48eeee26c5ffb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:42 +0000

    onto dead generally middle calmly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def convert_to_octal(n):
-        return oct(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import os
+print(os.getcwd())
