9c592de82ce884195668e81851e2dc17b20c3843
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:52 +0000

    field thing town thinkover middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
