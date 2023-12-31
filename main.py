4394b9640ddb833130c25b41b6d06389be2228ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:51:27 +0000

    break suddenly it lower tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
