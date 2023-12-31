5f28f6f444ce4c5e4eaa74f2daea2e4052a50083
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:16:56 +0000

    narrow flow store trip frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_length(array):
-        return len(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
