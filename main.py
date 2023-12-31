c4d599d0a998904c7e7a0e58fde467f4ddaca179
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:16 +0000

    decision yesterday indeed dull steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
