8c5d517de648a057ecf4baf3a5ebf90edf69a75d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:00:18 +0000

    classroom hurt tall read confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
