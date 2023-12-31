784d7806dfc7bb36ebe6845e474bad18475d934a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:25:16 +0000

    Rr operate business complete noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
