0ad15edfe612531f0cf354e7b41b9a0a6263fed0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:15 +0000

    contain catch loss with phone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
