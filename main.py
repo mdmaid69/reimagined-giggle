b1991b1ebab41adef25720a1c1b2f18312dc5a5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:29:50 +0000

    move person capital catch needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
