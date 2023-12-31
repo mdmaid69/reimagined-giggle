88875f98be327e437eb383489ee73fb1c47c00ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:54:10 +0000

    hang some advanced original difficult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
