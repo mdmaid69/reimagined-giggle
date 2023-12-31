28be5064e2c310520017d103d37395174005e07a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:47:56 +0000

    use opinion it relation confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_max(lst):
-        return max(lst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
