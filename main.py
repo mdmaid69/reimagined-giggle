319f40e9bb923ca802e3f37fd044c5e098326ed5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:53 +0000

    letter similar responsibility straight born

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
