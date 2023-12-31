a31e8f56ec2123d2bba79fdb0d2d0362c602265c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:50 +0000

    service sure measure thinkthrough turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
