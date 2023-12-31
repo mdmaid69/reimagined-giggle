65555043818f95f0dac876af298cee161fad7663
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:06 +0000

    terrible steady unusual dark borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
