1f83fd6bc1033255080bc178cb4936c692fa86f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:20:55 +0000

    nurse block train sail damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
