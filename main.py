045b5783a0eb495bf46134b39bfbfe77b7a258a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:13 +0000

    alone here calmly anybody criticize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
+import time
+print(time.time())
