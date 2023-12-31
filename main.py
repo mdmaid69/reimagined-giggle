d8a33cb6a8ccfe1b96a8d81922233eac9b412739
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:52:22 +0000

    touch today along careless sometimes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
