9c15fe8b7e95deefb46962f3dbc0125644037063
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:23:37 +0000

    tooth food male offend easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
