9777d55b022154d8a9ca2861473190c79d6713e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:41 +0000

    sort occasion month night benefit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sys
+def print_python_version():
+        print(sys.version)
