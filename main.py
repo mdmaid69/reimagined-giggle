b399168a6bbf86e678b27a38499683e83678443b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:49:22 +0000

    book be mine play progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
