26577386e5b0f91797d3f9af2516dbbe5421ae65
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:45:22 +0000

    pressure prepare write violence subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
