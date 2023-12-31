a9c8484e29f21c6897c806459fe208fd228f3819
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:40 +0000

    everyone wall variety regularly 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
