552b633d70ca31a23714edf05d5989e36363f364
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:55:04 +0000

    coffee ring entrance touch home

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
