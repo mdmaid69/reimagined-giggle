107acd3973a7dd025c0ffac238ae314339ced333
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:44:55 +0000

    compete share square cry with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
