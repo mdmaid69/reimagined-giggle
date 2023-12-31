6f85b5d1311e9aa3267d178a8bc521d0bb968400
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:57 +0000

    receive exact hard past sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
