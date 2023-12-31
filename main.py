1b5fb1b6dc633fc6efafd031d6c80655f9e3ccaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:32 +0000

    come how ignore explain unkind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
