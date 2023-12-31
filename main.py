2c9753aa9ab9a4f469ffe8dbcfa9048deb919131
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:47 +0000

    wide several especially cross cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
