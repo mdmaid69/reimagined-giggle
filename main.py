b9e672b8778981214b03d2d7f9d236db28962946
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:07:52 +0000

    increase reduce dull mysterious science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 def calculate_speed(distance, time):
         return distance / time
