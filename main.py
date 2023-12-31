beedbe66f667c62e03858f7fdd3cbdbc12d0097f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:08 +0000

    rule bet opinion present degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(5):
-        print(i)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
