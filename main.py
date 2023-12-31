393db10b128b426098658dcc748a6eae1ebabba3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:29:12 +0000

    greeting or count convenient likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
