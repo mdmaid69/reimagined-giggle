83bd0945153fe9bf07a54cc2d47f18b292162296
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:37 +0000

    pupil urgent imagination majority give

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
