40683227a148688cb6d85b5524b5bf3ccfdecba9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:46 +0000

    say find direct hold game

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
