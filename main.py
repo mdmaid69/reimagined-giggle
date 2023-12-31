5e7f678a59f266354aa8a6cb37b5ed54c3339d23
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:22 +0000

    march exam tense measure include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
