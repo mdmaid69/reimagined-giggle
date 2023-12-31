9550817af9b2802511ae41e803880937b212d928
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:39:22 +0000

    confused suggest ready brown entrance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def square_number(x):
+        return x**2
