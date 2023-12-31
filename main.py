6e7aff35f82d106e32e380d981bb538f065f5177
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:34:55 +0000

    afraid convincing takeoff pollution impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
