4ea42a0788ffab2dd74ef1df3eb42bf2a3ac83cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:12:21 +0000

    control decision breakin positive dance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
