13e6a39a775611d80c1c9062a2c06801e6ace813
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:32 +0000

    bag than break sink follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
