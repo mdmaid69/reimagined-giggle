3da437fdef81a03facdccbde2a5a463903957c42
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:45 +0000

    lead admire pay workon brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
