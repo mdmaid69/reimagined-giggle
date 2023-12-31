c05ca74200770edddd57a7fe5ba9ee9aa27487f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:35 +0000

    be certainly public disappointed care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
