9cf44cee9669876ebd88b3e4d9ae6758561fad1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:40 +0000

    makeinto dare willing lose leave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
