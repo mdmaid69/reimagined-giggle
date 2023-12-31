772b6fb07beaf13604f42515b9628ccf92ff9068
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:59 +0000

    line lookback(on) total tough obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_acceleration(speed, time):
+        return speed / time
