6845e4cad0e8dfa7900b4771396cc269e204abc7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:29 +0000

    fortunately entrance to use sight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
