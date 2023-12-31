eda23dfeff31cc1645f580fd4e7d5f576655d065
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:20:48 +0000

    meaning slice judge bread unable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
