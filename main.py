21bb6325c36c71bea280155bb75f6b3297cb50e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:02 +0000

    talkoutof sweet row another wear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
