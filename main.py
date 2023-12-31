c95d53d5b4304046d54ff73f6ce7092d795fade9
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:35 +0000

    the join once send slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def add_numbers(x, y):
+        return x + y
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
