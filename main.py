9ed124deecb3c3bd7791747c5a59f64a423d8b70
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:10:22 +0000

    564encourage normal crash cruel exercise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import getpass
+def get_username():
+        return getpass.getuser()
