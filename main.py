8121af4ee983564227931220b8142bca02a3c670
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:57 +0000

    sale defend uncomfortable spring uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def is_even(n):
+        return n % 2 == 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
