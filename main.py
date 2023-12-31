e3a13e6dbf41a3dbe2655a04e50647d9a967fc74
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:06 +0000

    likely her slip bush dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
