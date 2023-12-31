da08ad06b0679535e399f4a4f1f4caa753e96c00
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:33 +0000

    wide confuse dig treat phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
