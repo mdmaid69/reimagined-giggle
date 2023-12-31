a48e083638e56f304dc9b362770d11a2a9b94b75
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:08:38 +0000

    my black welcome 564encourage voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
