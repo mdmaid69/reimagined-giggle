96c258205e46c823b5230c4cf3170394f2b49049
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:34:03 +0000

    callout purpose brave suggest art

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def sort_list(lst):
-        return sorted(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
