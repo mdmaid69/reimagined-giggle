735bd8dcd2ee273fb9d1d05890b0dd1e8d30222d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:00:58 +0000

    later lookaround deserve during whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
