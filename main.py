e3a5dc53e4f989e9123363b34d2d651fbcb8d61e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:25 +0000

    through division issue listenfor yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
