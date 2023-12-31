050b21b246dc9b2d9d4b47342dcd42ae58af1d5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:43 +0000

    back whoever standard shock statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
