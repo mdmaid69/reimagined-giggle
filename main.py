9c74d3e9ca752304cd1b6f92ea62be17365ee6eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:57:35 +0000

    cough but unless forward hot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def remove_duplicates(lst):
+        return list(set(lst))
