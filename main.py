84dbbe8d500d269d45dd46d82488785c395c2eb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:15 +0000

    stick about measure book joke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_acceleration(speed, time):
+        return speed / time
