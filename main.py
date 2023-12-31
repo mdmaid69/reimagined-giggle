a3cb4f196dd2ca289c0fb8f316c7aff23574da2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:47 +0000

    mess used surprise middle keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
