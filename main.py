29367f66779b2ecba56869d830ff24194ae21ad4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:02 +0000

    see manner still shake black

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
