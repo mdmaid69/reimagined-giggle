acb7db0ac67a14754dd055ac9ed74c89aed58045
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:55:56 +0000

    plain house close opinion close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
