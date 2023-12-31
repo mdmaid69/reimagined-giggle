f02e72a7bbfcac65fe780e774da2d21279e0f856
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:45 +0000

    maybe well visit space Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
