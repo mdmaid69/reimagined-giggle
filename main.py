104abc7393b0cc144a6f48649f9ceb4dd1d8001f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:10 +0000

    free instead wake scare machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
