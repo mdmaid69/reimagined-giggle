3848fdcb6c8ae7e8b51fb96d053bef99606a016a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:59:37 +0000

    better practise basically central new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def square_number(x):
-        return x**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
