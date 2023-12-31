9b6686ac14608b319d3b5888518c4aa2e3bf0c4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:56 +0000

    Cc getback content basic cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
