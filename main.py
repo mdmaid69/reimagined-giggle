8d5805df7c216bf5681158cd7dbe765890036092
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:07 +0000

    birth home shake bag mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import sys
-  def get_python_version():
-        return sys.version
+import re
+print(re.match("h.*o", "hello world"))
+import sys
+print(sys.version)
