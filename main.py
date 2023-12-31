58c5f76f5c03aa3a9895c08460aafd5b859a561d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:05 +0000

    Nn money impressive impressed true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_sine(x):
+        return math.sin(x)
