cd9e2b7ae7e85a79654d6f48b3d6737187c3d1f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:57:43 +0000

    interrupt activity beginning nature create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
