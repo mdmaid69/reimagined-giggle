8eb9696e4891968f6ddbeeb06f1b01a2a243978f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:55 +0000

    convenient negative would production prepare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_item(array, i):
+        return array[i]
