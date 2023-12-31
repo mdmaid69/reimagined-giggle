f13c24255c5df7832786a65905876a175b7fe527
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:31:56 +0000

    develop trick share her fold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
