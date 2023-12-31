c3a2f66dea21be9c0c0e12b564b93fed97631984
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:14:54 +0000

    never swallow obvious male otherwise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
