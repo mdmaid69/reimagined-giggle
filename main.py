a29d7c9387c6f87410f09ecde3d06abd9c34ddcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:43:26 +0000

    drawing identify moon plain classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
