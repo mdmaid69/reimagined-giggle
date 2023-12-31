650421730871301bb709fe64ba08a3bdf997356f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:35:37 +0000

    receive another it development maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
