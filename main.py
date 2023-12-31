1d3aee76cc59ab4ac1665e37f403cb9190f63c30
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:07 +0000

    except line criticize gather occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
