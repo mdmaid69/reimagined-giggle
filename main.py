108931c215ebbacd6f72686adcb2e25ca758f225
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:32:45 +0000

    drink excuse circle unlikely story

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
