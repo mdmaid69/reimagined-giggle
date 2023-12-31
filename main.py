fd8e8431d7279c43d75437b2d0e100bbe61a66aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:47:14 +0000

    mix onto expression print bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def is_even(n):
+        return n % 2 == 0
