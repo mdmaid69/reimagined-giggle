ec94d7be77cf01a391237da99553594aa9fc7745
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:50:29 +0000

    clue that subject empty identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
