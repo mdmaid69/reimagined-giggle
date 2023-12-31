d1bd03ffaae665be6139edf2afb613911cecc651
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:34:25 +0000

    waste arrangement smell hear hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
