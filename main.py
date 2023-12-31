44244ad9eafb8f9ba9a7b611ef772fa8360f404f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:03 +0000

    some rank illness interested almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
