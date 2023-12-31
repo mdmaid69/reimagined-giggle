7e63d38f24253e8a85380b5386180bed56f71c53
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:37 +0000

    hold strange herself polite some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
