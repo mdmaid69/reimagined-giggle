00a5ee6ac9a37291094ae1172b9fe4bec4506b09
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:34 +0000

    takeaway cinema shine stranger shake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
