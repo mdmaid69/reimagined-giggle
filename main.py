520b486b6cd1cf2d2b146daa2d6b697ce145416c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:02:22 +0000

    breakup remain they Tt draw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
