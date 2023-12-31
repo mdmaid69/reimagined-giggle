661e7c9625726e8ffd67ada545ddfad58e4b68db
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:47:20 +0000

    intend joke unable card tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
