f41a83ee5d68d1cb445ac3660ce200b346dbf155
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:14:26 +0000

    official shoulder expect produce left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
