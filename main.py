79d80fdbae47511550474e7925aeb200f29dd12a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:16 +0000

    spoil funny themselves middle tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
