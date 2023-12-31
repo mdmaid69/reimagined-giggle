27fb76219020595b3a002666be7e332351087974
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:57:42 +0000

    lend noisy pretty anxious constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def count_elements(lst):
+        return len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
