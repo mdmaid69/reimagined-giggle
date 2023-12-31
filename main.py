93dd6a6d869f3d4be0e51b0bdce61b161007afd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:03 +0000

    sore after fire argument movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
