048fbc7bf3ffb313260916452f891a32ac2e44df
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:36:17 +0000

    lost reduce phone freedom date

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_str(array):
+        return str(array)
