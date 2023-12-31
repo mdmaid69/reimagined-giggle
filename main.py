c1b7cc562958f51dcb6c6b08930a5c4dd73fa92d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:06 +0000

    back very advantage ask perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
