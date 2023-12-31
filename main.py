a5c800d33ef3ca00060b9c0cd47135c44407a151
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:13 +0000

    remark practise advise then between

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_power(work, time):
-        return work / time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
