16e08f3b0d2b25562af66ec4666cf87065841cc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:01:08 +0000

    tour finish gooff scream play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def divide_numbers(x, y):
+        return x / y
