7a8049e4f925d287ea056ac7027f8b6b796e44c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:27 +0000

    character fight giveup sentence film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
