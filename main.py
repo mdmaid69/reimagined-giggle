f5287f251656517acd504e757beeae8cb6c54980
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:40:33 +0000

    school design make type line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
