b806cabbb65ef363a263c10e63e73a5a3e1e5822
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:07 +0000

    fit friendly cause order breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
