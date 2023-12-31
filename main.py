d38dc8c2c60e91dc279704e0bf67e536ce32a67e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:56 +0000

    yes know rush thick lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
