3a3ec671db07d95b3e06ff910352928e4d711191
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:38 +0000

    gooff attitude way consist study

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
