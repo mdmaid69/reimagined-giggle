f93ad26d4fa0180a1960785fb5f8e71541e27076
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:28 +0000

    its thin everything plan turnaway

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
