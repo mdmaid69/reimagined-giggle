7ce792b2f95695a03afdaadb818a8a148b8ffdac
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:21:34 +0000

    switch deaf police union minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
