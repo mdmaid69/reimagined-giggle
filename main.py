218f51db2347b31d61e956ea5ba1fc45d2505216
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:51 +0000

    copy early attract money disagree

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
