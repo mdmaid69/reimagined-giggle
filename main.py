07fb11278ecd87287690ea00ea3ee2d38a3b1157
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:34:26 +0000

    tool cry ahead rank comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import time
+print(time.time())
+def count_characters(sentence):
+        return len(sentence)
