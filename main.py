01da0192496273a9834d9905bc248a290f27dfb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:46 +0000

    brown direction quarter criticism direct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
