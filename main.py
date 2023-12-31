8dace4082b668e274ea0d59a91ce5ef5ae43ec5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:23 +0000

    own male building snow turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
