98d13ac6af090a910aa0c16bd434ebe2b4945ae9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:42:46 +0000

    temperature traffic cum view claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+def get_current_working_directory():
+        return os.getcwd()
