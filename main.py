b135363983dd25081ca96570e6c695fbf0563c3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:15 +0000

    close single declare basic either

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
