5b35cbb83d2e65703a7fb1278e1bc06d7764f7aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:24:59 +0000

    forgive tall outside circle temperature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
