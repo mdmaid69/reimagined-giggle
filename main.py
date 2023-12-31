e1e4c1a27811db880f86ac6bc26647a8fb143e92
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:55:16 +0000

    description dig muscle sensitive tongue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
