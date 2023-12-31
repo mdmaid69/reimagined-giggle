9f3c66116775dacfcc113c81cffe9fa397a65fb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:15:21 +0000

    proud expensive commit out stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
