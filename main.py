dfc8139027a8ddaa9452dbeccea03093c6ca3aa2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:51 +0000

    birthday arrangement quickly list track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
