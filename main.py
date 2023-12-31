3399c6adde604ece3b98ab0dd479795b14af2a3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:19 +0000

    shelter circle fire connect ocean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
