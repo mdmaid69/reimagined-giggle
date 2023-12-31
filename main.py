ff5c0eb11f8db88fb3db3425eddde4d5d4b13a5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:51 +0000

    laugh stop bang speech insist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
