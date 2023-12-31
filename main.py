a4e8ff3b855a4fe4aef1fad83063a9d4b2753ca0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:51:22 +0000

    Yy exciting pause many defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_power(work, time):
-        return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
