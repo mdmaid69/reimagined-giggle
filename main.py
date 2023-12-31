8d88980d8d35bf472b581916fff28f164a299de7
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:47 +0000

    narrow raise trust ride shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area_rectangle(l, w):
+        return l * w
