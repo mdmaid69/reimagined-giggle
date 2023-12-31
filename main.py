a682154232f2ba3e475cb350c28287855d4dfc74
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:28 +0000

    line switch field room trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def remove_from_array(array, item):
+        array.remove(item)
+import platform
+def get_python_version():
+        return platform.python_version()
