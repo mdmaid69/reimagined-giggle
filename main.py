583e98db6dff3e8c44fd4c729e3f060d88094e9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:32 +0000

    affect no who succeed dance

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
