0ebf877f4e93b8e7857fc58cb8227b1799409ca5
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:24 +0000

    mainly I to freeze Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
