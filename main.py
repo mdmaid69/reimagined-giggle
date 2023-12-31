2c1433ec5982767de158dc566674944787acb721
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:44:45 +0000

    soon comment complete relationship stranger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
