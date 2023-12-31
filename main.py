37d4c6d027986e5c99855c3a89d10b93b8d14079
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:12 +0000

    conclusion label tap laugh connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(lst):
-        return max(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
