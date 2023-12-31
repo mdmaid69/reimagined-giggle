7958d5c49edaec9188addd2ddec1c76803976314
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:10 +0000

    stay set valuable takeout mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
