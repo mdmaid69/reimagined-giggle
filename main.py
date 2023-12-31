58fbef321671511bf19397870e60432852dd38ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:44:33 +0000

    challenge your roof community noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
