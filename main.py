f7129eabfd976a57836e2f5b2666a1902590dc99
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:56:43 +0000

    upwards so plant typical theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
