9ab807727ec177e7c6231a95c2515dc441e669c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:25 +0000

    gate film maybe teach convenient

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
