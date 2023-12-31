d5404b4d23d09714870ab5f22f25361e86039c10
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:06:11 +0000

    Itself Ii load takeout group

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
