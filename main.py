886742ba0f1e07f042f839da9cfb4e030c88415b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:17 +0000

    freedom voice Ee divide getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-print(math.pi)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
