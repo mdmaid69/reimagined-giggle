d7d416d03835a382bec6ccd74a7f38c0eec8deec
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:50:29 +0000

    once discussion excuse I important

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
