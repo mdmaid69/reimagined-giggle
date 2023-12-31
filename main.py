317a7651c0220c168ea379adb4f1237dd0814196
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:01:58 +0000

    joke dangerous shop fear doctor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
