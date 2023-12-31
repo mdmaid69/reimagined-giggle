62043016e2856ae7724c6f2cd90351c6fc971a31
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:33:29 +0000

    beneath swimming sky music holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
