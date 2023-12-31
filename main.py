ff1aefb93f4586f608d5ba9fff5c7e8fc67fe723
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:36:07 +0000

    university Dd skirt offer live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def remove_duplicates(lst):
+        return list(set(lst))
