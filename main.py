10b4415fbc466afc297e400c439082ccc84d4a6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:31:50 +0000

    back before ready satisfied cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
