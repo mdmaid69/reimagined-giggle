5550792f643e604fe4723fd3a0f9270494f32bc8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:32:42 +0000

    battery disagree have fat bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
