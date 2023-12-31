689b0001184b3fc4e26a5eac6dc040d80a3ab617
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:03:47 +0000

    dinner official remain suppose dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
