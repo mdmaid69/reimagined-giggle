dc7d5beebd3fbbb7eb86e07e62d72574aa05c915
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:55 +0000

    thirsty stick confident lunch uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
