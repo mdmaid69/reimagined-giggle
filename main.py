97286db94d76e8e0930bfc64c4b20df0a222e45f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:07 +0000

    now substance holiday friend share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
