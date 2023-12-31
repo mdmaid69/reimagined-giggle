486a036f6a1626db152cd5997db2c9f53abf6075
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:38 +0000

    direct stamp breakinto hospital single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def add_numbers(x, y):
+        return x + y
