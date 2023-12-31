55c3b731b4d0a2dac007a009cdfb51cbc77811e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:34:08 +0000

    familiar jump concentrate bored organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
