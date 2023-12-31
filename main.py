5fe00af4aa20cbed65f418606e93554e8660612d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:40 +0000

    name concern steal then desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
