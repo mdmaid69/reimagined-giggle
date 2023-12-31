b4042c58335fbcec9a2b3354bebc7a54421890c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:28:35 +0000

    circumstance stick giveout hole marry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def convert_to_hex(n):
+        return hex(n)
