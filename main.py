6c0ccf9139db3cf0813147525d0284648d596ac5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:54:32 +0000

    my secret damage confident without

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
