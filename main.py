008a57df15c3acc6987d2019cf8cf2f016cc0f6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:22 +0000

    open load protest path seem

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
