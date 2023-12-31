2435ad760ee632cb41985c387890b5fe2160c84a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:33 +0000

    argue tap anyone raise future

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
