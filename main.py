718bf0499919bc708c5e2859e14405213f4b16e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:45 +0000

    else lock container answer left

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def remove_duplicates(lst):
-        return list(set(lst))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_min(numbers):
+        return min(numbers)
