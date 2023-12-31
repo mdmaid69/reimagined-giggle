ffa244f3858152edc90ae98d8c647ecd8737f3a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:06:52 +0000

    desperate meaning smart murder certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
 import heapq
 def pop_from_heap(heap):
         return heapq.heappop(heap)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
