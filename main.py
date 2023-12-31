79cd0da04d5bf6b79b8004acf1af079e7d86383a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:43 +0000

    impressive could conduct press onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
