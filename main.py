c5d6fc4e6c71bf77df109c5303f61c44fd05222d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:31 +0000

    point communication chase record prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
