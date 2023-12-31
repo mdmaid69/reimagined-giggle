a55754c1be3b2e131cbd491b202c3487d292bbb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:24:30 +0000

    she tidy characteristic blue see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
