bf5da7aa3ee1b955970d28aa7cea1929370c76f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:35:31 +0000

    building rub when cruel drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
