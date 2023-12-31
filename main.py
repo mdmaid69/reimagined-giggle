a7cd425080d5477cbdaa2ad26bf4f1d553572728
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:56:42 +0000

    effort rely realize history partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_list_from_array(array):
+        return array.tolist()
