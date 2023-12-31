5ba6cdd3067db0295e5a28e7a077008e7c68e48b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:52:09 +0000

    yes exact against dress scale

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
