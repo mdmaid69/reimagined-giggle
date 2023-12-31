897b5bcf4d99ecfc2b1a7b97ef02286ade61e3bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:17 +0000

    system better slow firm give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
