27584ac46076f9a9fbf00f5971506e27867b065f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:59:09 +0000

    beat factory surface wish slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
