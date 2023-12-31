fc8d605c5ce905f2c80b13480e9b9133e09591b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:47:49 +0000

    giveup someone push Qq against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
