e76859ba9f00d960e71636711af672224da6146f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:00:57 +0000

    his mind plate reply morning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
