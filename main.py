7f435c969d52455e6552724d21f15253edf11dca
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:50:13 +0000

    me under degree shrink expect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
