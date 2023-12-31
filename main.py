99db5726a744be3525c7fbf693fb226d01d1d72b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:10:26 +0000

    boring earth contact should breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
