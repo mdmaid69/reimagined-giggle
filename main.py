519038aff2d171d42931ce380fb20b2d95ab0763
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:56 +0000

    sign exploration reach wind long

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def change_working_directory(path):
+        os.chdir(path)
