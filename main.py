466d5c142c337e91d588ac7d291f09704126c312
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:30 +0000

    pay season wise play let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
