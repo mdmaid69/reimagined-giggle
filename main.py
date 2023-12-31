499c876e2e830a25031b9da6d3e86e27676e76cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:52 +0000

    tip union circumstance happen finish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
