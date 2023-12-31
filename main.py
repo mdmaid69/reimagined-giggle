6c144ff670c0ff167e7a040ff419d7b58cdddef3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:17:17 +0000

    holdup stone charge Hh nerve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
