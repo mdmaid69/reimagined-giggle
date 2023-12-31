ba0932d56066e1aa9394a11e275b843cee929ef7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:44 +0000

    keep size whoever boring die

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
