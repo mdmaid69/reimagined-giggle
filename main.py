872d4f6106476e87fd86830adfc5b2ba604c363a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:41 +0000

    straight embarrassed flight tonight accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
