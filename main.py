97085df632c97288e08ef73ea206e8487edaa28f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:02 +0000

    giveback recover regret unless money

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
