3b0f24774f29e97bbc58a1a2d0a125f613f53e2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:00:44 +0000

    invite die control funny downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
