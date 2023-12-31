e177adebff3f4cec8e0f5387fd66fda27791bb21
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:43:31 +0000

    ordinary raw confuse start with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
+import platform
+def get_python_version():
+        return platform.python_version()
