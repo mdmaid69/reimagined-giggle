ebf358f54f47bb2badf09e10c868f41c87ea0fe1
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:48:00 +0000

    spring rest sky negative telephone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
