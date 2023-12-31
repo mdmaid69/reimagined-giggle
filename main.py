c750cc9b62be3d0d36ebc1c20c14b9d135d40638
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:01:27 +0000

    live unit conduct square their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
