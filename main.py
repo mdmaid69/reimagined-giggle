09cec4227e505a0278090d851725bc9a869977f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:06:08 +0000

    clothes range bar label live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
