de6bbaaae99b49b39ebb8fee0cbf2281e4581877
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:23 +0000

    pile platform pressure peace Kk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
