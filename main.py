8673db8876867518ceee2fd9f5f382ef653ef1cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:06:47 +0000

    few transparent takeback vanish out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
