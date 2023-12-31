3f6d9f0adf9a4e3ceb884ca4a9a7e68dc9f0d48d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:25 +0000

    build circle keep interruption underneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
