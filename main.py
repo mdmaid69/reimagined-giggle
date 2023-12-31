1a2d89e4181d1db8c6698e0c9acf25dc75e32345
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:19 +0000

    transparent rescue damp fierce we

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
