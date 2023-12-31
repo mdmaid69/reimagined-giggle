905364adadfe5343526ac7ab0394ba768faa7ea4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:53 +0000

    smoke objection row answer clear

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
