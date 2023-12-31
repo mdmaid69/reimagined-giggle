ab36985c6c7eec2474f25f141a137be1705901dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:14 +0000

    stiff send passenger pretend sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
