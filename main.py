3c3cae6f7849127905f5cb1d0d02eeef86b8c13a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:20 +0000

    instead cover no order repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
