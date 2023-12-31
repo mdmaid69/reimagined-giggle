4f5c8e3374129f53afc1568ced7c73c1ef041891
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:45 +0000

    here passage discovery prevent calm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
