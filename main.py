d2bfe96f305f7994454d0ba1064303f15ca63418
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:36 +0000

    bringup upwards marry decide breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
