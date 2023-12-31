ffd64fe2178c2bc0374ac2eb87ffce530a24bde2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:25:51 +0000

    demonstrate them also clear light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import array
 def get_array_typecode(array):
         return array.typecode
