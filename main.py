e4130c5e0c8583bfbe428251a1e73d1cb90f5516
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:06 +0000

    action brother proud long tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def reverse_array(array):
-        array.reverse()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
