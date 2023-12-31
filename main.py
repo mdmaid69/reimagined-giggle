5178aa2290b141d584d1f74a88afea033ca07087
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:41:23 +0000

    spread relief every hair vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
