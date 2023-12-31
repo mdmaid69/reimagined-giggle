8a3f2b43d94800a879bd83da96ba27750708c210
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:06:56 +0000

    dry left single spare corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
