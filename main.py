5649b72073b951c0f3c89c9c6e867e7f7ee1a06b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:31 +0000

    rescue before in ignore regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
