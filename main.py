b20a131a0efb4484b25cd0f530a898988bbe9065
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:07:53 +0000

    noise pass cry shut band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
