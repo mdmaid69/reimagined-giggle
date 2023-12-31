563955953d6808ad2139da4442ec536f2ba9649d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:45 +0000

    dear joke muscle moon powerful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
