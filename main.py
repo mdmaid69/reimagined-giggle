16b81479482f775329e3eca5d2a39d573075f049
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:39:09 +0000

    pronunciation brain hard tap season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
