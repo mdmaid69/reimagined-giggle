b999ad866805d3cb22fb1465bf14136cf2d18d26
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:14:28 +0000

    difference their translate right start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def square_number(x):
+        return x**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
