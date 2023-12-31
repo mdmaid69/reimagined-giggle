80a3b2b325fffcb276de524dc1b607dad582d12b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:01:27 +0000

    deep willing finger official succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def divide_numbers(x, y):
+        return x / y
