5c60098a4ab5e882a35a7aeb9246be55dbaeabdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:49:35 +0000

    group physical circumstance concentrate this

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def reverse_string(s):
-        return s[::-1]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def greet(name):
+        print(f"Hello, {name}!")
