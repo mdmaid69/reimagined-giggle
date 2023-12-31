c14d5f973b00a9d019b18ae7b0fe02a44f4bc7a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:02:37 +0000

    happen alone number word command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
