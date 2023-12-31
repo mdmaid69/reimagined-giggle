6a1606aa8a23eebfd775996d9d2b20cdef33c95d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:07 +0000

    Vv arrive better smile special

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
+def find_union(list1, list2):
+        return set(list1) | set(list2)
