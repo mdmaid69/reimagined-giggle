4cd42736d33a12e83199c949852d9f3b91c4e53b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:00 +0000

    exciting someone unusual whatever word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
