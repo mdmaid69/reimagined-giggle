4383cfc76b4e1e9a39c9da80b313b0f8e53e8f7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:57 +0000

    juice table repair square touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
