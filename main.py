a05c1d59605a22430efcbcc3f9244534a1888d91
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:17:02 +0000

    best programme limit mystery mean

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
