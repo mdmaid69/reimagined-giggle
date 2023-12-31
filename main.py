116348688bdaf9f50dee8a4f19ad0498b0b38eb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:09:49 +0000

    feature combination thin us back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
