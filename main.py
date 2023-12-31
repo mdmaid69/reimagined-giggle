89789927087a2a2d80c409b2686140d7c69d1eef
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:42:44 +0000

    mistake support prove correction direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
