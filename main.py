a83d4816e3cbfbbd17b9689c9790c09320a54f2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:27:33 +0000

    price control detail match gas

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
