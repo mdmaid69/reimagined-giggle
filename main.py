5498f8d2a3cc50cfcd028d980345784804caa428
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:16:18 +0000

    cheek measure admire high most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import sys
+def exit_program():
+        sys.exit()
