aa94b0996ec261a2994e1ffa31241d0607fce568
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:31 +0000

    trial doubt either hurt nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
