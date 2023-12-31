216e601da47c4ed792a78241cc883de525bfb59f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:04:10 +0000

    cover scrap estimate taste murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
