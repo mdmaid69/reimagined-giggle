69cea3752b446fd151c89f0bec9fa7112059d220
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:01 +0000

    software score middle date deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
