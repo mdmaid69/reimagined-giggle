f46c999ae4d7c8c0c5d3c3e47bcbb31d6da34b17
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:23:15 +0000

    force contents this debt disgusting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
