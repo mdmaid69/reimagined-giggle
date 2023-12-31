e95425d397fd18d8a9d96a3c24b1b2aa114b57de
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:00 +0000

    base inside rich turnout central

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
