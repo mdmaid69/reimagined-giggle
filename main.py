31b3c6754c6e41877791c98d187c573f06f44a86
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:48 +0000

    make smooth thinkthrough punish plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
+for i in range(5):
+        print(i)
