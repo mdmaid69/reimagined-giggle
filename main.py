a24d03d15c9ffb918a394f93b7ca1d10d059257a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:25 +0000

    impossible black suit feature their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
