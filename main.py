e440fb85b4ca3ddca42fc5b6cd49d47a84761419
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:40 +0000

    ordinary trip style giveback pattern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
