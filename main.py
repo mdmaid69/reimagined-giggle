537651644d45b4a52b6cec28c17c56d4af963869
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:16 +0000

    what far sort scene make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
