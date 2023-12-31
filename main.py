d2d444486644da87f2c5cb298b8516add94ff378
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:40:50 +0000

    petrol information house giveup used

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_cosine(x):
+        return math.cos(x)
