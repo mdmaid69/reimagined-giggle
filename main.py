a718d26b4258d3d0cbbc5dea38a369e68c4c46eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:16:39 +0000

    completely there he rain produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
