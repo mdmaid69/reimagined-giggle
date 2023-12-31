89a73d1af2ebfe641fd07301444c528dcf4eefeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:53 +0000

    side my sky copy none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
