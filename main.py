110817e0a6a6c2f3bedecac32c103adff8de1a0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:48:51 +0000

    mistake breathe bang small practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def add_numbers(a, b):
+        return a + b
+import time
+def get_current_time():
+        return time.time()
