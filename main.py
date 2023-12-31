661bc62bf9f44579c5cdf8ad32c0679cf8dff80e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:54:20 +0000

    anywhere Gg meanwhile embarrassed love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
