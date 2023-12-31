5fa68e4f9ffc5379de8d26dc57565cfae7dd60c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:03 +0000

    depend bored present basic simple

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
