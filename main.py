4d3f813f70aee37aec30b890cb409f8799e56b8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:31:24 +0000

    experiment pattern century tidy towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
