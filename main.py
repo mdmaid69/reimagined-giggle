f9233da51bdded9d5e45851f55b3699d7f97b51a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:32:24 +0000

    left vanish great track disappointed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
