bd5f9f786816dab89a3b13aa3d30a13ebf5c2dca
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:57:26 +0000

    through temperature practical defend slow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
