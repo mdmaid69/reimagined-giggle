123df1d257df7f8af72a39fc9d9e84ae9870e76b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:48 +0000

    flight equal scare day accident

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
