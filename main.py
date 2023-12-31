790a9e1b99e97d171f7bd6606826a20aa2b9e846
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:20:39 +0000

    become into head worry nurse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def subtract_numbers(x, y):
+        return x - y
