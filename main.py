6e214b0fa01188377efb51aef24381ce1dbefdd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:48:49 +0000

    in strength afterwards sentence head

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_min(numbers):
+        return min(numbers)
