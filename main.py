5b95e5d66b4c41344e61b71cdbae67838edb4c80
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:19:58 +0000

    takeout extraordinary total minimum fire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
