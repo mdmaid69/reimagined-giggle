13a576f330871df90978945608290e2990b10ba6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:53 +0000

    disturbing all slip manage interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
