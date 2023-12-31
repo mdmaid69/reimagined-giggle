9b513799a06236749c98cffd92ba12a19a330094
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:59:36 +0000

    570enjoyable man aim perform star

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
