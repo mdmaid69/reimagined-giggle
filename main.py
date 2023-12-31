dbf92dd78ebed56d0610b02b0ad03a8d66621039
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:10:06 +0000

    since though fall depend soft

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
