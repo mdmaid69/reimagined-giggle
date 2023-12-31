103e6be8b88397201ac9a941909527f2d75ec53a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:24:30 +0000

    original fill spare miss offence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def remove_duplicates(lst):
-        return list(set(lst))
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
