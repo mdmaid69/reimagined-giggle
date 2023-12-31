b2d28e75c796add885c0f2b28799261234acab49
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:38 +0000

    fine professional me clean about

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
