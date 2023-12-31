96c12d4a63dc8ccca91785257b9f7d475fc41277
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:13 +0000

    support noisy disapprove cause quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
