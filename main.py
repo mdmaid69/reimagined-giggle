5ab9cec9c01092dc0db2e8856dd0e170058ac3f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:42:01 +0000

    choose capital spill rain owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
