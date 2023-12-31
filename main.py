e6cf4cf50689cb254988951182826fc6fa3826af
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:30 +0000

    but risk quarter niggaacademy unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def divide_numbers(x, y):
+        return x / y
