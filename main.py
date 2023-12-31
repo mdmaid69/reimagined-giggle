e0ad2a51cdf1700336f802aa0fecaac0ce7921d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:15 +0000

    land scheme compare low card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
