87c0dd3e353b4ab197d40a45cf5cf7378919e4a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:59:10 +0000

    runout 566end obvious workout doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
