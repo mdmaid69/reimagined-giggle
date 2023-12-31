5de3b236a3424faf4552aa898a201e38b548b5ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:48:09 +0000

    another Ss plenty object alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import time
+print(time.time())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
