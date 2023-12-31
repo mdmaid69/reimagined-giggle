db09135c934d7a6791987a97c9d7ccb4a576006e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:48 +0000

    cool talk content everyone Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
