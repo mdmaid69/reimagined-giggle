2468054f2a01921995763348cbb16ff95ae8339e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:25 +0000

    Ww emphasize criticism offence offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_circle_area(radius):
+        return math.pi * radius**2
