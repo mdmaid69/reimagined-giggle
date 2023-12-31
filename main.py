cdaf54c25400a25e130664cd47e384fa446c9385
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:55 +0000

    patient shelter push report public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
