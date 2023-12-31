bf46627bdc7699110af840a6a2c62eb036e41a04
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:10 +0000

    cope heat street entrance beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
