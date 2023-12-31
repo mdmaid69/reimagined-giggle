0e1c1eb78e0d6e2dbd008aa064dd919eaaea962f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:24 +0000

    copy address share without admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
