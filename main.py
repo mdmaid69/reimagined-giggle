37da31a81b482ea372b9f2bff2177edee8bf45bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:31 +0000

    such uniform communicate buy list

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
