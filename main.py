0e380900b42d59e0154e90a47106db6b5014d6f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:28 +0000

    day nice if thank takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
