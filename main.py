51889845611e5e6cfa0a26a3449bbb9857430d35
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:46:34 +0000

    grand glad among expect hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
