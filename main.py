ae0c16774c2c4cb2af070478931d09159c5f68f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:42 +0000

    between sink free difficult hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
