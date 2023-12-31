787b4235cff71d0526d535ba61ce1d5bcdcb22ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:04:19 +0000

    recovery ahead cover environment later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
