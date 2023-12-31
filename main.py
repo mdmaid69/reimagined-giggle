79c7ec75de2ca7c385b215861525c772fbe21f21
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:37 +0000

    cap round respect exact relation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def delete_file(file_name):
+        os.remove(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
