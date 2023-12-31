9d236d245a9341a58e5b8f5a966525b80d260248
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:31 +0000

    public trip school from pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
