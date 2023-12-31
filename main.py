2dc84e0024bf6928d31fca08dd0b169a031f5214
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:51 +0000

    straight condition private contain tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
