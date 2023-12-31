24ef112dec3bb8d73a7a4abc6017e8fd7c938830
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:55 +0000

    cold neither edge square influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
