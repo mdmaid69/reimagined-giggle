66469237eed382a0abb90935835d8587b7660904
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:27 +0000

    nowhere past flavour hardly learn

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
