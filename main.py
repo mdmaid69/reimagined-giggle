889efa868ee835232b545645b9bca24ace987922
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:51:03 +0000

    exhibition sometimes tonight table compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_item_count(array, item):
+        return array.count(item)
