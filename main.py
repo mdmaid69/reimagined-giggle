d3ffc8633cf6e633dbfff0cfa0d749a4d340ff3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:40 +0000

    sell obvious excuse extraordinary excellent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
