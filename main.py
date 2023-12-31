da384996501e1fc3774a21897922c33b9b4a6e92
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:57 +0000

    dream ticket divide seriously onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
