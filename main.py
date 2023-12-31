197aba4b1674ef44b95a679960c36626d620465c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:50:14 +0000

    turnout female really car bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
