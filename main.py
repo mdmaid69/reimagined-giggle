feb465dd4e1b480944c80dda0b2ab97d60d3fde3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:52:30 +0000

    address talkback cheque dream snow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_union(list1, list2):
+        return set(list1) | set(list2)
