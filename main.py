d933bc35c2a5f45f04a34ae54450ee8468334141
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:19 +0000

    group concentrate bridge could guess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def cube_number(x):
-        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
