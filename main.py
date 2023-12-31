fb1e13be260911c8fd9f2eec9bd98c908d3f27d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:16 +0000

    run figure conduct form lower

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
