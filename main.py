2d41f853a8e2ac9535f97889ada30c18b538f628
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:52:27 +0000

    fence positive before bring especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
