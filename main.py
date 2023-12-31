3a38e8ac9f163feda397a57a0aff5424d740dcea
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:24 +0000

    sister arrangement check spend tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
