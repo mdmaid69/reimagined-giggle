7f8d9b2742239ba23cc1e074decfad6902fa981a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:19 +0000

    community what stamp exciting future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_typecode(array):
+        return array.typecode
