57ffb819ddfd27fd5fdf00f4522fb0ed596ecad5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:20:33 +0000

    uniform strong I best tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
