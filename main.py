dbdffbd7c6fdca39f7af69c1ed955e4cc3e020f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:20 +0000

    injury town afternoon usually himself

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Words:", len(text.split()))
