7bcec9fd16f0e8cdcfc6b94b7068cda55fd71d59
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:39:23 +0000

    congratulate callup power raw this

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
