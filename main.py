15fc18e75650b94a60129e72495db719c5e897f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:44:16 +0000

    believe invitation comeback rely sweep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
