d8811736d829efdfc75b1b388f067002f0b49349
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:27:03 +0000

    noone container shopping mystery ride

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
