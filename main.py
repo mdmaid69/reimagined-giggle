c6230ce5b94c53eb374d1a1cefae0e6c2e0be8df
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:38:49 +0000

    passage eager offer blind tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def convert_to_hex(n):
+        return hex(n)
