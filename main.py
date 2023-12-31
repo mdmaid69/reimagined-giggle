f43630c71f90fa3a2836cdf6f09515d4e6b5e3b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:40 +0000

    boil unfortunate mouth drop crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
