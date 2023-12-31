3c1589bf55f105ab910fa99848be897fe2c99d14
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:39 +0000

    outside 573entertainment return trust street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
