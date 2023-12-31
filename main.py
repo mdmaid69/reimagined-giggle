5b966f25740c7994abb6aec3cee527f3e38e3694
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:33 +0000

    keep choice century ask claim

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
