c7f3cb9b3d6d8ecd634a7392740b470746402e48
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:24 +0000

    date along tap learn nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
