164e7cdb65d5ff0e29634014b8f084c5921259ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:03 +0000

    play wall period general use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
