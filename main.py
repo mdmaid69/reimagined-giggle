6ef6a26eb30648ccd7f805f1fbb395684006dd89
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:31 +0000

    tight surprising strict path 567enemy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-for i in range(5):
-        print(i)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
