062e0fb5aea1b0ab4d90bb9480416861744e194a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:12 +0000

    above niggaacademy noise freeze emotion

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
