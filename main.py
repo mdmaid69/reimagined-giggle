3a64d0efa28d40860247c50cba327c33cef33f40
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:57:56 +0000

    feel high kind dry design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+import math
+print(math.pi)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
