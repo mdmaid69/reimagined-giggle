5c138946ab55327e18e64f9e68844c160eb71817
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:43 +0000

    OKorokay discover search whom situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
