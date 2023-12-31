ff261bde52935680694929186b09a50bf10166b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:23:15 +0000

    possibility top across swallow length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
