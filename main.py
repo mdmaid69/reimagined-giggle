e30e62b106731c3909735e4da44fe7bc2d83287f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:02 +0000

    school judge your thinkof rest

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_set(array):
-        return set(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
