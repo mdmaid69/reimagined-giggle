d75741b58c37f4e4afd747bc5f59243301a3186a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:14:21 +0000

    approval use goon matter meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
