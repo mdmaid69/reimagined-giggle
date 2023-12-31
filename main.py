2952300e281ac9af2421e16708fee8b31e9220ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:10 +0000

    pattern true watch visit hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
