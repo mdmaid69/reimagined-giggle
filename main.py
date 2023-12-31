2900bd997cb10fbb1baa2f0cb23fb2e6777f70b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:01:29 +0000

    duty sort division straight here

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
