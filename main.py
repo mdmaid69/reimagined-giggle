5c9d4b5f5544c2be2ef938bb5ba972a2eadb00c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:55:50 +0000

    apologizeapologise common edge lend up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
