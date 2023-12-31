6fe79d0b6319783a2ea8ac9dc64f14ff0ac5575e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:16 +0000

    number comeout mistake allow touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def multiply_numbers(x, y):
-        return x * y
+def get_array_as_float(array):
+        return float(array[0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
