31ff7a00ef37f49b5b6f3b3e67271be33c6877f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:55:28 +0000

    difficult interview order innocent pressure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
