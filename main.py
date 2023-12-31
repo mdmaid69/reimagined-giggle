6b2b2622d1c681bad57c87263f19bda13bf83bb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:56:56 +0000

    luck embarrassing disturbing love trip

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
