bcdaeea2cada64d91b7eaf18b5d7a8065a07a3fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:55:51 +0000

    bag have interview hold mystery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
