701a455d88f66daf9c11a49adf5e2fe5898dfce5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:55:32 +0000

    town shadow busy keepfrom under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
