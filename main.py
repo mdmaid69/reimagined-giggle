ab4e819de6cdec11e3661f038af17460027c4bab
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:23:30 +0000

    insist book say Bb breakdown

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
