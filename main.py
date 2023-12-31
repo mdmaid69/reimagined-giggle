618975458b5039f63e62f8b3579e4a281547f650
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:13 +0000

    lot unkind fear mark stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
