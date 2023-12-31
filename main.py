7c02b3f6ea603ba64df354a71c60283fce73b770
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:21 +0000

    worry shade bet anybody as

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
