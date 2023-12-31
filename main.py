c7ad4bfdc94001d56ba870ec191efa483aa1b71b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:20:34 +0000

    branch black sell awake below

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
