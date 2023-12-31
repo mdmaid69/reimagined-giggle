2fb14484266c6a1a029402ab96f0cf35cf1467b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:01:37 +0000

    tear fortunate cum method shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
