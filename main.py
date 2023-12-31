3edd1e77c484e73730bae14315e651c0e7a3dd19
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:33:47 +0000

    class street rid certainly alive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
