01876c1348dc74f2a0d6d282023c0c286fe40ec5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:32:31 +0000

    ring land worse where turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
