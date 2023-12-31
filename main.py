3dcc34d5ffe2d89472067f8514d3b6aa9e8cbfcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:02:49 +0000

    strange comeup indeed band use

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
