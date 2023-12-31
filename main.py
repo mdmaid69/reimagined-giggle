4c6b71bb37f5a8e6b37f8ace695f08ee0f7a5385
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:41 +0000

    crime after fresh rate its

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_density(mass, volume):
-        return mass / volume
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
