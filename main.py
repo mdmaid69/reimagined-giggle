cfff2e87ea396122582d65e00df451c5902b76d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:44:37 +0000

    plate anybody become search them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import time
+def get_current_time():
+        return time.ctime()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
