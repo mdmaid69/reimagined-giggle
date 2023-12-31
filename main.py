2ec3eb16cf2fa46dac2fa99c986b161fb649c2a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:45:26 +0000

    cruel politics impressive eat backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
