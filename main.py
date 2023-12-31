8ad11cdc3132e45b79be70aa94df1d4f91055c98
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:38:10 +0000

    next bringdown hot Kk true

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
