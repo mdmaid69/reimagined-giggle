afc6e745d77781fbd09c4c0ddfa18c76dbf639c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:14:32 +0000

    score favour light payment seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
 def get_list_from_array(array):
         return array.tolist()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def multiply_numbers(x, y):
+        return x * y
