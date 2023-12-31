9b38b96a5ff8e56d802150d2b2e9eae292350493
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:18:32 +0000

    hit height sore turnover time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
