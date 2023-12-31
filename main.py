de1c1e96e26a44cf854935eeffc108c15fddadd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:51 +0000

    in anywayanyhow prevent even wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
