cb4a4e1608743116c546faa8b9953945f34f4cd8
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:22 +0000

    row own bye luck woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 def get_current_date():
         return datetime.date.today()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
