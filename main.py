740c53cac105bdf86042c99d37b7b9afb12437eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:34 +0000

    Jj fierce deliberately rub damage

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
