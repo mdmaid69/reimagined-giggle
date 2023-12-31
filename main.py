027f2e4593076392805726597174f70214c6a140
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:08:18 +0000

    at slightly traffic inquire trick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
