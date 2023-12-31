1a242d37a8422cb83bb7e6da26ffb90527f8949b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:53 +0000

    consider see statement someone breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
