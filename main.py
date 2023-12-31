d431e7d30678542f8c6a42880f0bd24681314db0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:04 +0000

    earth price another account circle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
