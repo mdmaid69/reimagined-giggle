4d443f8a5614b7b072ce60060ec3fda3fd560622
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:36:07 +0000

    work display type throw tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
