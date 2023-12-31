16806adf9f34e31a6752144f39a113557ab78db4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:00 +0000

    lookaround tough none fear handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_queue():
+def create_stack():
         return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
