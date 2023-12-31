70117960264c0bf1711f4af95b2c45a01199aaac
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:17:14 +0000

    mark neck push positive risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
