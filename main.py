88465e86adbd91692da7e543e401eaec4e3fff14
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:24:24 +0000

    joke company accident thinkthrough judge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
