27a90f8fcbfcc23bbbb645bc891125f5aee90f9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:32:03 +0000

    telephone bill charge criticize star

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
