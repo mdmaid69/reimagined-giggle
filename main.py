18e0bd8ad1736d67ae8182fc450720f5a0a91ebc
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:11 +0000

    Jj bush comment catch address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
