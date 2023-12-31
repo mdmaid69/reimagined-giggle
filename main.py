952356f61ea7b14c65c306ea7b4c4cfca94d4468
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:06 +0000

    state radio bin correct exact

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def divide_numbers(x, y):
-        return x / y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
