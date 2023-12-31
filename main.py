731f71018a68c23180e8f3267bc6b68546649b32
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:11 +0000

    squash welcome peace face pump

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
