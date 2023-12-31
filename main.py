6315c7601fdd82f3d8ecd2c0fa17c7bbb697160c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:17 +0000

    satisfied within confident angry rain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
