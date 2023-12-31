543c85231b004f3b6574f49fab94710d4208b240
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:27 +0000

    dark neat difference cum boring

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_power(work, time):
+        return work / time
+def convert_to_binary(n):
+        return bin(n)
