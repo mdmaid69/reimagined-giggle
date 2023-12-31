d5bbdefd510c436c8b09b93e2237488ea64b9760
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:23:46 +0000

    smell drop smooth friend previous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
