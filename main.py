8a66631cd426b3ad68c557adbca4ed9cdd0f5456
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:59 +0000

    tour dry learn guess round

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def add_numbers(x, y):
+        return x + y
