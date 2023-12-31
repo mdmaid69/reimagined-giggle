5ffea9151fa2c79a55950eb77321b90e2243639e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:56:55 +0000

    mistake admire gift beautiful crash

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_item(array, i):
+        return array[i]
