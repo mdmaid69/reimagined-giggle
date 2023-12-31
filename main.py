2de64fb53d0fe189a2adf1efef562aad3e871cae
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:45:17 +0000

    least ask set trap show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
