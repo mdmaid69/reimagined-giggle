eeb1c697c3656a0914bfc240e860fd0493d5b7a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:55 +0000

    shout effect hello fight tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def square_number(x):
-        return x**2
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
