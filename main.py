30fa212ebb5082a18dc50a27b2927cc4f54ea720
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:46:42 +0000

    load hot broad stare me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
