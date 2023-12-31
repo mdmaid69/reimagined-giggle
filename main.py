2fa9a87868d3764fd64e367d3514db6e574ccff7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:41:46 +0000

    thumb deaf for bother day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def reverse_array(array):
+        array.reverse()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
