c8db4572437b85cd048faddba1ef6302aec38ce2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:37:42 +0000

    repair to fasten plain glass

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
