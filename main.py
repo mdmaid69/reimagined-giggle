c7d0f065973a222c0cfb09e6212d5127eddeb7ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:43:49 +0000

    model such inquire ready welcome

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
