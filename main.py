8f9b6b7c819bf2f286aff26a709693c00f79ab23
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:16 +0000

    behind belong sleep confusing schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
