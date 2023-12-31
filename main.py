adfa8eac91fe256bbba6daaa91cd7610c24bf7c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:40:41 +0000

    birth bite roof eye getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
