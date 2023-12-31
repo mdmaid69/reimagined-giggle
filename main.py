c63ffe724b8952e108e8ea70c9d6cce25139f30a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:33 +0000

    exhibition month freeze secret deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
