4a0bf027186d75a86a33ab21d6709ee0b8301d95
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:57:50 +0000

    image hit workupto wonder bringforward

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
