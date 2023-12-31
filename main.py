4bf3183d7bf864bf9b7cbea6d20c400007cfaf29
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:27:52 +0000

    tonight bringdown damage ball occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_itemsize(array):
+        return array.itemsize
