234db4cb257aeb31a839b43dc113872016a9ebcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:53:34 +0000

    goup arrange success Yy polite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_binary(n):
+        return bin(n)
