de6c1d71cf0bfdca715e42615f10405f874fca48
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:12 +0000

    work test prepare trade luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def roll_die():
+        return random.randint(1, 6)
