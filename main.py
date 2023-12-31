21eb961d842a47eaa1a1e3292017675826e71da2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:22:19 +0000

    wind crime situation like welcome

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
