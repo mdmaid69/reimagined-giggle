27f611c4fbf42b2654cd6ceebef07684cf2ce75b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:51 +0000

    outside share project quit support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_block_size(file_name):
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
