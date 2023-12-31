2a58d4a43a7e3cab68556a79cbd54fc190965de4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:20 +0000

    goaway question turnaway attach careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
