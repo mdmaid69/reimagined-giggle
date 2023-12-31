dc04d34f77b98c4f597d4f203c1fd1e6a7c6e050
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:21 +0000

    getoff back smile aware somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
