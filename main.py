c49fa7a429e52ad7554ef2a4db1fc63443a2a0bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:46 +0000

    official strength event approve several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
