db76a23b020b19947900c2b3f2ca7933225838c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:19 +0000

    upwards recent lookafter pen bill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
