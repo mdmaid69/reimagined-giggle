c060e26ef93959d85d24675e1febaad1c720d853
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:44 +0000

    foreign great organize tight cheek

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
