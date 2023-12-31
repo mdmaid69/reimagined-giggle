be4214aadd274a455eb7da59316a553f2c6cc25a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:10 +0000

    while large miss shut score

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
+  def remove_duplicates(lst):
+        return list(set(lst))
