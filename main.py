6ff6f4730407ad7dd44813c5e21028d26aa4b698
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:18:20 +0000

    bend word gift protect child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
