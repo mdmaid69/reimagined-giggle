db60ca16d463380bd1ab892e027145a15d204104
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:10 +0000

    plain breakfast personal towards urgent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
