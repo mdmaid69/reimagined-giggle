8f2c0c339fc4a170228115cad93501d95a634a1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:14 +0000

    dangerous Pp of travel bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
