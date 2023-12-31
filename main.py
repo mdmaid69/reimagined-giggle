f7d7c273d884b2d009a84e471b5cf92b8015bf17
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:24:01 +0000

    rank base takeoff search aim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
