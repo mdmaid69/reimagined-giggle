1f0ff24b510f3181f9debe644148247a01af69a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:35 +0000

    lot last message book bye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
