f89da36c02c03c896b180c80736204caedf7c3d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:14 +0000

    thumb evening lucky lookup remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
