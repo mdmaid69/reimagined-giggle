b436ed014cc8888aaf706594eed74cc9cf06b94a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:42:05 +0000

    clear shape train check wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
