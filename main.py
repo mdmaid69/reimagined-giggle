90be01c6616112febd6eb52f607aca8c4211ae18
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:46 +0000

    wherever blame worst fortunate 568energy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
