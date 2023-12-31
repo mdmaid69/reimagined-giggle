74e5dcdcc6dc6f10042aadc24d5eb31335cbe2f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:00:13 +0000

    giveout solve definite movement cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
