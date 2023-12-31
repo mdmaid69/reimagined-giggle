b1b499e321c3e798df941f20741029e5f4e920bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:27 +0000

    squash shelter write forever play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
