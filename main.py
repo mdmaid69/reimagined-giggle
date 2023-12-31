c30b744503695a60511a2b1a7f50e93b93e7fd16
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:09 +0000

    really letter pool sign over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
