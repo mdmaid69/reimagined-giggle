3714bc6c3e0e4f6c1893b1be3f26e76e2996235b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:31 +0000

    lay government mess past warning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
