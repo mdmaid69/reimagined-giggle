8919e7334db276b47a17c2ae72f0e3d186fa3343
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:25:09 +0000

    weather drawing demand basic word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def cube_number(x):
-        return x**3
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
