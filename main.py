fec95f4616f2981eaa5b426ea992dd02edde2d9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:39 +0000

    love maximum lend point trip

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
