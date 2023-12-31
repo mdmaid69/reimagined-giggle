5e3e6c1f9dd6339c4247175cc29f584b6ae00fd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:04 +0000

    destroy wind regret edge dead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
