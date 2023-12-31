ac520fb9e2bb5a0455878e96ffa635447b96c5ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:26:06 +0000

    deserve bang scale company week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
