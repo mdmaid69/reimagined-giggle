a5041daeaca30ead051b89e013607cf8ed352c2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:30 +0000

    trouble cut makeinto total suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
