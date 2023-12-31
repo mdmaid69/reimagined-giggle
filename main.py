b6668696518cb52eb82b21a41f5843dd1d0bc83e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:54 +0000

    fat wet together city familiar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
