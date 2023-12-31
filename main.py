f55c0aee786c4ee3171b88d131eeaad5fc59e287
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:27:10 +0000

    great head tough will expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
