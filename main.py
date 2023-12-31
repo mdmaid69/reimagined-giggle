41dbc9ef3e4275df4d6a956a5bd55336b5641006
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:43:36 +0000

    warn forgive black unlucky turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
