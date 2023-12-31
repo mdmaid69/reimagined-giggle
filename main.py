aabb30ed96b217dffdf803d9015277c903d71f2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:10 +0000

    raise naturally whistle shock furniture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
