12ca7b17f897e94f0daee09ce4d235653e90d025
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:17 +0000

    flight nearly begin study Kk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def create_queue():
+        return collections.deque()
