3b70d543003efdc91ff9c165575be7427d784c92
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:27 +0000

    takeout closely bush healthy pause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_set(array):
+        return set(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
