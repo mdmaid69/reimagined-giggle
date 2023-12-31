89864be22bc71b21d74e5b538cef1b5e3900b0d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:02:03 +0000

    comment rather last emphasize play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
