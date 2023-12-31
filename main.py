6607a44ee0b029fb47336d915e72f7f342deb093
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:36:25 +0000

    comfortable tell immediate dead bet

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
