b408e8eb9483bea1c8dc6e7e2796cc110c0b44b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:25:17 +0000

    remember tip of guess design

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import time
+print(time.time())
+for i in range(5):
+        print(i)
