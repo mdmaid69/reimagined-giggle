27a9d773dac82f8df949e22511c8325bafeb6f26
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:47 +0000

    plastic service such worse mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_density(mass, volume):
+        return mass / volume
