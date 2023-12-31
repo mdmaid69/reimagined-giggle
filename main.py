5dc14cf5cb797f943e2cbda9b5b80ec987bb2fc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:00:52 +0000

    room workon test sudden bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_even(n):
+        return n % 2 == 0
