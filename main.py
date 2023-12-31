acad76837586666e0e2ca9eb11402fcf7e0f3437
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:16:58 +0000

    negative duty rich attractive compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
