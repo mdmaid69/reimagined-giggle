437a631bdbfc4b7db872f1f8c9df3f6b82d21f34
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:42:35 +0000

    good ignore fear congratulations during

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
