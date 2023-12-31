6e288e18aa522c030d70f1b02bbc6f30ec5633ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:15 +0000

    criticize Dd dig tax target

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
