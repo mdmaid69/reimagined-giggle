9b6df879b1cdee838a548c2f16149ce1d9a71f1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:36:56 +0000

    own bottom just appearance trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sys
+def print_python_version():
+        print(sys.version)
