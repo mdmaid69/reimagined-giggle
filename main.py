5440a32821b8bf96877cc67369ee3726e078628c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:39:55 +0000

    once coffee brush cold emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_repr(array):
+        return repr(array)
