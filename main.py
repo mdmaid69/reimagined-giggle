44c18faab38118b8578564a679ee5ef1c486cbb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:09:40 +0000

    though cook scrape certain desperate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
