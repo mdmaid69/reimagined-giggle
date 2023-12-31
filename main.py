be08239e698c446fcbaf2036276fb40d372a8122
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:15:52 +0000

    that wipe night correct thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_acceleration(speed, time):
+        return speed / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
