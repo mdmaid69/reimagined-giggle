f7162c7f563f8b1d5de0c4b5453cfbe12d720e5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:22:15 +0000

    needle unfair teach explain computer

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
