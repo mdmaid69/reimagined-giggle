fbee62c13a433a579456a62a9c7e2a4825158538
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:51:25 +0000

    description base car breakinto takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Reversed:", text[::-1])
