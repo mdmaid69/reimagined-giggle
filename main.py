19aca8e09fbc0adf102ea2f005513afde6b0dfcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:33:02 +0000

    small again Dd danger my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
