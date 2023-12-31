dce9c95e6c4c65339bab2d1b56e4da42ed2d8b7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:02:00 +0000

    valuable passage birth human judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
