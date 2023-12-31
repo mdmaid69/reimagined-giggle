257333f1d037025b949471b3d0764262127907b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:46 +0000

    hurry fail inform tidy change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_min(numbers):
+        return min(numbers)
+def add_numbers(a, b):
+        return a + b
