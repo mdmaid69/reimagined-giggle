8ba5d696ec183f02b27e213bb046153aae4c7b7a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:09 +0000

    hit payment all fit fetch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
