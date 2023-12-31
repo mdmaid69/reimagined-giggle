db042c1793f3c9151a75d7f7de11baebceeef96c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:57:11 +0000

    telephone ride worth house pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
