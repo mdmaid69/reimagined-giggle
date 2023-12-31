2d810e4fc7514a1bb406f2658eba5ffd9888e315
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:26:46 +0000

    cover owe fit catch debt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
