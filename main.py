ffea361daa3922a77dbcaa0e91831d4e14ffab42
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:08:24 +0000

    suddenly equipment think aim runinto

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
 import csv
 def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
