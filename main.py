c4c46021563cf9194977a82816bb29bc7808e4af
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:37 +0000

    learn charge confuse display description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
