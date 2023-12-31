5f17d5dd21f108bb5a2d40dad160e9b83a0a9376
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:07 +0000

    form safe round argue weak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
