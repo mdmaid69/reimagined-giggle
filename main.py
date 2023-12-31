1378a9ffb1c06a068ec7e2ed125bedb899bb93b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:53 +0000

    appeal happen comedown exact remember

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
