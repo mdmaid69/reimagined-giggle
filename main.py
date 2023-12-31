e23ef3d596bd35eff19e785fe9b033fa47d1ef58
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:06:16 +0000

    formal half this square border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
