063269b5a276790b0ab28ec63276f9556b3fe575
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:03:25 +0000

    its sale pen reach secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
