8c0e3c348bd7be9b11f6daaf3f33c33300c9e95b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:17 +0000

    born charge describe go sea

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_int(array):
+        return int(array[0])
