1bea9ad56cdbabd542b362287bf941ff96dd910a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:03:13 +0000

    minute still comein generous rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
