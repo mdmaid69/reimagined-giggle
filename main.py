b69eb6de27f17418a3c001563db17d3e2e9912e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:36:53 +0000

    subject responsibility turnback wonder press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
