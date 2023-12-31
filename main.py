2a94b9242e61d77b25999e0c9a894845c3d95908
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:19 +0000

    material goon previous name admit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+print([x**2 for x in range(10)])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
