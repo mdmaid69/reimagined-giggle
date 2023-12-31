e6d64a1a21588adbe67777605cb9ff38272b12c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:05 +0000

    form short maybe hour in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
