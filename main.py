172578f7ced9b7dd8bce92e435e6738f8107f579
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:04:45 +0000

    basis study conclusion touch dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
