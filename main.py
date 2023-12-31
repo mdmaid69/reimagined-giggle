79435712ff2de3e611137699b90d600ee3535ff8
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:46 +0000

    scare show correct private fasten

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
