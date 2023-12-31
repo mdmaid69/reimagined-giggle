fbbe272f3e3437220c12edac5df77eca51f71115
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:56:44 +0000

    distant shade payment left trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
