1b851914f54106a1ae19e789d64ea151a8baeebc
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:56:34 +0000

    obvious scheme joke deal realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
