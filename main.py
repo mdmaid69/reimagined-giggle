21d6911ef331352873a729ebe840f8724fa60d0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:18:38 +0000

    firm swim dark usual nice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
