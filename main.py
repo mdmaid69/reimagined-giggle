4bce35c9b189a335762fc92f619094b72d978241
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:57:01 +0000

    find exchange forever declaration reaction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
