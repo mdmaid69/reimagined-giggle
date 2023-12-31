dad1634f094ed449ca5ce3e3c44ee57b28dfe69b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:52:25 +0000

    Kk table exam tend appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
