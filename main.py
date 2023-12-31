fd318ba4320dcee26c43b256a5f2bbd187ecfd65
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:41 +0000

    flat authority leg generous nurse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
