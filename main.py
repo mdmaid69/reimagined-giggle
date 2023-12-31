2fe7a854d97fb9eb768d38743b8f23687b94d1e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:08:10 +0000

    intelligent point Gg holdup without

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
