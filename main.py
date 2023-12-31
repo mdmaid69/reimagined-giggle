e51cef3f5e3e5dfc4ea6c5c582db6bdcf8752fb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:58 +0000

    perhaps load zero advice influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
