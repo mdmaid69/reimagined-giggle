d5ac9f3aab51e9be690fcff96e10978fd4e9c998
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:06 +0000

    accommodation moment they unpleasant rest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
