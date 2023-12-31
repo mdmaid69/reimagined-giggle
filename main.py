5a2872ee7fc544abe50a53bc73880550ecba5f8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:28:19 +0000

    reduce leg declare breakthrough along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import time
+def get_time_since_epoch():
+        return time.time()
