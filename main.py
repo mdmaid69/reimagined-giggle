6ca43d20a77ec144aeb5dedaa55bfc38668b7020
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:18 +0000

    approval ours pollution mean example

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
