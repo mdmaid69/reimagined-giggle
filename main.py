cfacfce84a4ff7e7be653637dac049b8bce5024e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:15:21 +0000

    raise protection Qq book lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def add_numbers(x, y):
+        return x + y
