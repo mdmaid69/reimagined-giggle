f9138c087240e65530d517378baf276dbed06e28
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:07 +0000

    see protest choose young eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
