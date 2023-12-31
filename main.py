56a3b4f1527a86089c28760a318249b350a6c879
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:21:12 +0000

    great trap dare include miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def get_array_typecode(array):
+        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
