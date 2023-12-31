dff01677b7fe09f65ccf5bff986f4a22cd91f242
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:38:35 +0000

    relax rough now switch vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
+import array
+def clear_array(array):
+        array *= 0
