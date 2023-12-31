46cf652612f5842585b2d2ebb9030cb7f66f16cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:34:19 +0000

    official challenge unite minimum slowly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
