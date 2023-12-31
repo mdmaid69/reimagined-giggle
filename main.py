4bc2e7d7959187baa10a87034958d8872fa0052e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:29 +0000

    negative bill comeoff correction increase

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
