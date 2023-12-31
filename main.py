643a0ca4b39aaf50c5e95d486bb3f2f34289a266
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:26 +0000

    innocent complete permission temperature 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
