998d89eed2bd43509936864fd7da4cf90041a4a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:33:15 +0000

    system somewhere be connected deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def find_max(numbers):
+        return max(numbers)
