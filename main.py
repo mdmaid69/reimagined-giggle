5ee528e89cabdb89401ff3b7313a166797261045
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:17:47 +0000

    experiment ring pour swing nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
