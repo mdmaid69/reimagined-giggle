505eb08d59328daea8e90a3724a3b7fe21139a75
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:49 +0000

    own waste and scene negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
