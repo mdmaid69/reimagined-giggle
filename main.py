6ddeea4d0c0610bdc0bd6bc514e713ffa1b9f937
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:33:14 +0000

    forget anywayanyhow manage broad concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
