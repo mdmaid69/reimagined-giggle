93829ede44ead4d0c52fe6f4162b589ea313865e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:10:21 +0000

    intend accident so unit slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
