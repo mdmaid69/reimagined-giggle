aabc17a7d716d6f4261b1ea48ccd2e4e34b93270
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:23 +0000

    return reduce hate though introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
