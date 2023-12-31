388efa1951f438445a9db03ba955e520bf2aca34
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:10 +0000

    myself along tired crash wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_gamma_function(x):
+        return math.gamma(x)
