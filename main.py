4387feebf11522ec155f5c6c1f0d7d28f01055f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:57:58 +0000

    story voice attention lonely copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
