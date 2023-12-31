94b1a1f7c8a4d4a27751945291bcd00fd7c6e4b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:43 +0000

    sure section evening popular rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
