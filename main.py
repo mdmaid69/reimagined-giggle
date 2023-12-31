fae37e20476648e02392b9c16d4bec6e3305532f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:59:00 +0000

    family shall warm various long

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
