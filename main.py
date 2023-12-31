793cb7575bdd883a0606f9c75fccdfc320a6edd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:30:26 +0000

    also climb recognize thing correct

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
