e6fd74225a401ab8acbc75c7a659f3594dfe27e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:16 +0000

    bend interrupt fast forward mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
