4be80d52437c05dd60d159f8954223ffb759d43f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:39:12 +0000

    lesson except next thirsty pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
