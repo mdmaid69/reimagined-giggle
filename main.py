993bf73b739b0ea42d97efc9f8475a8957edbae0
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:42:07 +0000

    comeround involve personal rush empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
