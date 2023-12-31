6326183adf308a32e6fc2bb2fe7f8a5202e29846
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:47 +0000

    confidence twist shape police burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
