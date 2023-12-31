45a1b4af19a65f41df7570335a01607351f6744a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:57:20 +0000

    talkoutof disturb help warn proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
