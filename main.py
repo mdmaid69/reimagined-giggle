1dcea49a339e15a0e177f7e06e242385829dcb03
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:00 +0000

    rise drink maybe straight prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
