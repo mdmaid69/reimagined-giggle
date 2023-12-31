eb98b22f23cce187605b800641a6402926136494
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:44:35 +0000

    strong experience unlucky reason explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_base_name(path):
+        return os.path.basename(path)
