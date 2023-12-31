267ab474139b6d0c14ba6c00d7ed73d9c55d89be
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:49:38 +0000

    record 569enjoy mistake train plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
