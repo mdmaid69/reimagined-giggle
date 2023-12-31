df7d6322816cc288784846ffcc7696f6a941974f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:52 +0000

    still uponon juice prevent silent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sys
-print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
