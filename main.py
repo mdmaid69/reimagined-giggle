6784bcdaed2fd7903c2917e50f203335d92651c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:47:36 +0000

    direct hard preparation bang lower

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
