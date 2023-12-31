9f302bfaf75e6fe2aecc3e2f87333c266493bad9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:51:45 +0000

    later relationship comeout card polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
