f838cd6f46d91de9a63916e7081bea8469111d8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:25 +0000

    yes collection passage school tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def divide_numbers(x, y):
-        return x / y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
