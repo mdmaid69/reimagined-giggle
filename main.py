258e07fb5c538c45d274feebfdd61472baaac10c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:32:50 +0000

    well same stretch do warning

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
