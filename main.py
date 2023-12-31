4096e2133aae61b361b10eec4f0133aff088af63
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:35 +0000

    only more backwards herself similar

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import collections
-def create_queue():
-        return collections.deque()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
