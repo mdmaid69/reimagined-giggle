956c98d4dd5f216b522641152af17009646ada70
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:50 +0000

    praise communication use much smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
