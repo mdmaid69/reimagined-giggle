f66d865afaa6efbfd705272ac7be5f0b86cee18a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:49 +0000

    even deserve climb standard hand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
