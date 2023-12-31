fa236f3c3796ccff5f94d1e497b3154f1771f5f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:03 +0000

    heat Jj disagree regard lead

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
