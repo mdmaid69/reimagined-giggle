1fe362180af405d022e7236a0739a0e83d667cb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:08:19 +0000

    clearly mysterious issue 571enough gate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
