f9a3c18a790e3d6f9c051dc93f380e0435bb9cd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:57:18 +0000

    embarrassing wear busy earn welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
