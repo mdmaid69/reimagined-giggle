1e0b5df8c906af7f03bc92f795eebddab903ea82
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:36 +0000

    wear present bet own rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
