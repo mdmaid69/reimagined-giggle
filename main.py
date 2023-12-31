9cb4768b6a54df3954522745b1dd1d32aa29ae50
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:52 +0000

    us marry police I even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
