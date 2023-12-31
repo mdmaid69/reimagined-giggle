2a23fcaf0031b17825761bfbd0139d35b7be81ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:27:24 +0000

    condition race deliberately lookback(on) compete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+print("Hello, world!")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
