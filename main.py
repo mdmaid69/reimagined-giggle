9aad209672f8a6c9443fe7f15908773d9fe32321
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:49 +0000

    wave school getback talkback takeback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
