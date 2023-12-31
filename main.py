c550157e38d52a4380edf30dffdf1e3de039305d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:37:24 +0000

    nowhere improvement record sink grow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
