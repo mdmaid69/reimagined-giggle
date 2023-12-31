2a63d97eaf7668bf9fc9678fa0bf6da4fc911e79
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:53:52 +0000

    guard entrance turnup standard wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
