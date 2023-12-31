8f3c2f3aad821aca7d9360210ea9fe6d9aa1d771
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:31:25 +0000

    grow fear OKorokay skill physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
