7502c2e98ce96258acc5a80ee63ac91d59b97ddf
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:48:01 +0000

    mistake support offend low sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
