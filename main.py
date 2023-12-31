f7157f54e108e57c8fa2eb7659f7e1ac89287434
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:15:21 +0000

    mean delight crowd reaction bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
