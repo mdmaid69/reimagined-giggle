373ae9da6927c41d4b4c6c094748eef7236f3d2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:10 +0000

    ever crisis under workout frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
