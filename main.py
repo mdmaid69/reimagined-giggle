0d57b364551d20dead564bb55b56007fb9c433aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:31:31 +0000

    rush exact turninto frightened 179bite

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def add_numbers(x, y):
+        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
