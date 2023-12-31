537054365e3408ba3e1ef03602696281e3220002
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:04 +0000

    public rise special hear care

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
