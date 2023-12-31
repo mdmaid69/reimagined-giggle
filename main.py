b6925dcc087633870e63db8f08c7c8194a8f0577
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:07:13 +0000

    winter edge turninto concerning store

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
