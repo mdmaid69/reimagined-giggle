79bc4f820088475d43590d30372b1c939add0f8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:23 +0000

    lead apply whoever nothing forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
