0487b13d3e3779c38a3052788fdfa9040e99015e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:55 +0000

    anybody negative remember moral right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
