6f7a206f3c2bf127261e9c39a911d588617bf8d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:56:43 +0000

    giveout mad them Qq Dd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
