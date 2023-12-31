b0f2fb54551d2f40e551865e6b2294e08798ac69
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:50:58 +0000

    callout most quickly buy group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def calculate_area_rectangle(l, w):
-        return l * w
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
