6fb80be9a8b891565208c341c5438fe266af5a18
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:04 +0000

    central subject rubbish safe kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
