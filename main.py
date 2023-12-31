3462ac9f90ed5043c6e061ebb011a7e11ad4493d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:52:26 +0000

    human both expect manage deserve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def square_number(x):
-        return x**2
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
