45132f0c1aa44919d60f910e9bddae664a2c30bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:06 +0000

    workupto goup gate class risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
