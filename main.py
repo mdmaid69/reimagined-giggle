96fc41753d5c679f93d28d75892b32077903957d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:15 +0000

    young again sink sharp clue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
