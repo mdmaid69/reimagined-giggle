8ba9d55e58e7c6ae59e9080a3f8819c0ab33d68c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:57 +0000

    miss along slightly juice shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
