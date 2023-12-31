d970919294f69fe8718b7a1ce526cf2adc9b578a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:49 +0000

    manage sign harm although backwards

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
