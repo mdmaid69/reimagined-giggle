4774f1f2ac5d57c40c68ecf50373a02f61458a2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:17:20 +0000

    rely along power success class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pressure(force, area):
+        return force / area
+import sys
+def exit_program():
+        sys.exit()
