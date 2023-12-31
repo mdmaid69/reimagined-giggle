6181efe4ef2c211528a33492ad59a442bcd9729b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:28 +0000

    my back cut church bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
