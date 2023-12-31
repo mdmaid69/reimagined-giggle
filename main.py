89736c46008245aeb513b4773244ec41dbda4480
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:48 +0000

    watch street left runover average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
