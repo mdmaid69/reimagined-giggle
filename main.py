9173403bcfa8a989fc811aae8d38096035a644bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:37 +0000

    bottom tell apart close godown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
+  def count_elements(lst):
+        return len(lst)
