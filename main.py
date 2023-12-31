999f3843e10126273d280ab6e941987556002af6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:12:50 +0000

    married benefit attempt weather comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
