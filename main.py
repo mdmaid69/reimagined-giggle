f709a2f6c7953b78960003becda3479991f975f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:10:28 +0000

    near since smell priority consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
