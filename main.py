3f83af3e8a7060c66947521d7b713260e1adbfbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:28 +0000

    wealth mark set decision produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-print(math.pi)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
