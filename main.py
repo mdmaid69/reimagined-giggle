ec910e650f8e7682fe57255a3ba0cece608aa460
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:52 +0000

    breakin stand you sleep accidental

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def cube_number(x):
-        return x**3
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
