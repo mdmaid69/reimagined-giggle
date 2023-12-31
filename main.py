e98c7923f0dd82bd0c024914b0057d2c22a81910
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:52:52 +0000

    collection fetch check shallow top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
