db3a2dc1e8f301aec1a917112c0aa1cd8aea5d24
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:06 +0000

    finger stand comeout wild rub

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
