9dcc879f36d1801494ed4e6123d6e743c180425d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:25 +0000

    wrap comment respect scene brain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
