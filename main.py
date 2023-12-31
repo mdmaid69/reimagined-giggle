ef29809a4a525a8fc33e73e45280fc776475f992
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:50:04 +0000

    female plenty compare comeoff range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
