a54e9920048b594630000a56862ff4248dabec75
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:37:14 +0000

    giveup search weekend giveup unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
