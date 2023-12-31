afa5da027900e0a7ef6caaec4e9fd0610fe11189
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:21 +0000

    perhaps vanish somehow pause apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
