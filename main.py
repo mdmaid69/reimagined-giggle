4b34a555e2ac8fa4da97d23a1aff840a41111f31
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:47:01 +0000

    hear somehow innocent wall as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def count_elements(lst):
-        return len(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def split_path(path):
+        return os.path.split(path)
