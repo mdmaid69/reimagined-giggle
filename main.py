9d10936e965b9b4e0314ee37d16b73e3c4ccafad
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:34 +0000

    be raw really goaway callat

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
