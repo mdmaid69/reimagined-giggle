1a23a3fc0b0bca1a906d718995ec77f3f4fd657b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:09:23 +0000

    Ll light water birth lookafter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
