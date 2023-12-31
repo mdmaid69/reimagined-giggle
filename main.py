d06a331b946ba2d3be541584f08bba957d5b2f87
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:53 +0000

    unable destroy prize trust somebody

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
