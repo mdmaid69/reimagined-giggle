8724efc2d1eab9d6628960f1842555cc96642156
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:02 +0000

    about scheme responsible rise legal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
