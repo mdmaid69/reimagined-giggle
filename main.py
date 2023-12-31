96d3585f5fa2d3754073aa63a507323299eae985
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:19:42 +0000

    turn disturbing store peaceful money

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
