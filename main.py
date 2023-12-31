3ae3ebf936071f6f8ee94dd7f84deee66d16d239
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:04:25 +0000

    rob yours greeting communicate ear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
