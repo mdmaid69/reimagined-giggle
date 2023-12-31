0890caba6b14372a8507c644b8382c683b64fbf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:21 +0000

    intend cap content before comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_pressure(force, area):
+        return force / area
