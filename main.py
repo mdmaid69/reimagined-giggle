ec7a616bc5c141a505df26234e1b9a1448e86462
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:33 +0000

    outside combination shop base develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
