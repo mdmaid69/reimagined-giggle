b9e168cf99fbe2c5b533076e69f1aa75dac8065d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:47:09 +0000

    careless 564encourage if boat clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
