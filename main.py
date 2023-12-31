e2d0fe72c7e0a3ee7e0b0210410887d23f79447c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:40 +0000

    her room desperate profit crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
