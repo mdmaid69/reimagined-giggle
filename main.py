02fbf0560b76b4d50f6c9872a1b0da25635ca350
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:34 +0000

    love behave pause really suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
