a28f40d38aae4c5c79b42979c623e84706b6d451
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:23:44 +0000

    arrange satisfactory nurse memory plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
