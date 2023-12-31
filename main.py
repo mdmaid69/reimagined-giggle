f46cf14753f334e08aa8abdfb4d4eff656ea8351
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:48:26 +0000

    fasten face hard hole closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
