9693be41cf5c4946a282da17338378d16b2391aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:42:44 +0000

    change hit sometimes major fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
