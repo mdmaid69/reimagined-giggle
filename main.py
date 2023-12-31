72fda75a4580fa3085c9f5d9e7587a8f9d75ee84
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:41:48 +0000

    goodbye unless free song wise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
