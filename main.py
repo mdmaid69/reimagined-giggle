3e63d94020e1e467065ec7e17889df819c131521
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:20 +0000

    urgent voice declaration shout Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
