6ab082289d5f6611ae74c22e7c9e47f61b63d6d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:23:35 +0000

    stage win turnoff include responsibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_density(mass, volume):
+        return mass / volume
