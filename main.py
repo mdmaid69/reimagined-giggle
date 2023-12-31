fb41f67aee2e2978907c48b9302c03563145ce72
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:09:06 +0000

    away steer ahead impressed beginning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
