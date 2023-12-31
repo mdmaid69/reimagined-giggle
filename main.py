3784b0c560cd3d301ebb8e8f29671720555778aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:36:24 +0000

    mind impressive rely spend any

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_area_rectangle(l, w):
+        return l * w
