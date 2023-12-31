43528d79e3d9f225af0442ca92dac2da847eb45f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:56:11 +0000

    concentration giveout care programme over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
