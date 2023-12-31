4c138e6a2d44242f79d36c9e818bdf95cd57eee9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:44 +0000

    at test imagine unless 179bite

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
