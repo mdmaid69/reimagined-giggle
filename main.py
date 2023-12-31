148a7721852c8fae293c430a0ee55e82dd66fe54
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:04 +0000

    face crazy squash 572enter total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
