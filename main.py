4984f0f1cfe0a609cd9a13e49e00814cb4c44022
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:02 +0000

    pile influence free account introduce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def count_elements(lst):
-        return len(lst)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
