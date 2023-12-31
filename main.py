8143b4db75552ba632c054f53583a6f118985ad3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:08 +0000

    room finally confused science 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
