5a5aeaec2e3a42ae7ddc3eb5d999183bd8e8f96e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:10 +0000

    plant fashion swing lead measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def square_number(x):
+        return x**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
