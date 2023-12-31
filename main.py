77205ed706a7e845637d8121e44d7f9bd6388762
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:26:10 +0000

    instruction mistake underneath pause promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
