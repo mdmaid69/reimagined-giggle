ec1689a6ad850a22b61ba846dd7b035d1fcb836e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:02:50 +0000

    occasional reduction sick Ss intend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
