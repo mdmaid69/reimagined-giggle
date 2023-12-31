fa791a99ddede64910262666e7da40fa55ab7c87
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:42 +0000

    majority sometimes motion fair fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
