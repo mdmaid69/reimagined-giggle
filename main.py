912be9dbb227a7529d4a10ab739f26afea7fc994
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:03:55 +0000

    agree opinion duty expensive please

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
