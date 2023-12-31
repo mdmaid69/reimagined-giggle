08b4ebc5d45cde76d9d49eff5c40cb1482dae71b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:22 +0000

    rough little track thought any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
