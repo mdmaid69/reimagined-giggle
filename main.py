2355b40b8aac287ed45bca5415423eb1effe9456
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:53:32 +0000

    pile 568energy before visit desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
