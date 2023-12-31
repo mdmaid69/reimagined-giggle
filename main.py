e89a77caa101eb4d67202a196b67315f9dcee7bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:04:08 +0000

    scared turnout ear science mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
