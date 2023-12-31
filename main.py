e8d2c658a92bbf01fdaeb8025ddde0b525ba0253
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:55:53 +0000

    in turnback willing deliberately sweep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def square_number(x):
-        return x**2
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
