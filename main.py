dc1ec39273f702073f642ab1fdca9f02ae23ed08
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:32 +0000

    getup still rid model introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_typecode(array):
+        return array.typecode
