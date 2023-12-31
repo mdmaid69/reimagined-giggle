788aa6fa51dd1f33b47eb0a351e799659b6b7c75
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:13 +0000

    away tap swim idea love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
