3156fbd74894749794435778aea3e108ff66f73c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:16 +0000

    nice tip dear off notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
