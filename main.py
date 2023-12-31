e10d76f2c06d226650ca914d7da1c3494788d219
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:37:00 +0000

    content cheque Nn course refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
