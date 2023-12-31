b8ce1cdebb583eac854cf98ebc63b7f3a872891b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:57 +0000

    task love mess compete call

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
