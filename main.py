2d49cac881ec83817953aebdfe17055078daa282
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:30 +0000

    suffer tour gap scheme tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
