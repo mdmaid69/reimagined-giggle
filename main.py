3ede83a24668883da110a6df1e4e14e412ccdc47
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:06:03 +0000

    fill insist wrong 569enjoy level

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
