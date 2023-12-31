8c400b0c70b23f0f4ec9d7fb8f4c17523eb25ec1
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:55:04 +0000

    busy crash view letter unhappy

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def reverse_array(array):
-        array.reverse()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
