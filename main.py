dcfd4c045ebb963daeb2a36f2d040ce9d8c59b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:38 +0000

    professional if late piece politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
