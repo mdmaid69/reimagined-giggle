4c3db2cc7c4b9f03ec958a3bb03f673155df5c47
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:42 +0000

    wish stick television pleasant total

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
