5e835efd8cf4da30326d5e79bdba504153b60296
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:52:53 +0000

    Nn action gentle direct cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_item(array, i):
+        return array[i]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
