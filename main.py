df18c060f1e8da5ed1a499e588f63822068e4567
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:39 +0000

    tidy order crash but minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
