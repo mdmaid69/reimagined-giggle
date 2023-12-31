0ff9943265b9fa5f8c6b20b132a45f687b6c25f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:33:29 +0000

    juice tap silent mark sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
