578fb6f2c0ae2b9d6b261f8e43f828d35224a439
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:55:41 +0000

    together sting plate concentrate expression

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def subtract_numbers(x, y):
+        return x - y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
