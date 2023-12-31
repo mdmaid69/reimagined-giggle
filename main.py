de4e2f618d2b986f6027917b0dae5ec6eee6101e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:32 +0000

    sit suck tidy message eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
