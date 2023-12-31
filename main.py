e6aed4abc43ae4db1c2273482792e3bedccc5c15
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:44 +0000

    save tidy present wind convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
