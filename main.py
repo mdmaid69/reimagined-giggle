79d8ca1792d7b2676186fc522ace06fbd0a50998
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:45 +0000

    moral evening rob home cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
