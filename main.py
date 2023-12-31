46074e1271023a67bdfbda056e599fb2f5b0cd01
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:41 +0000

    match now power region home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
