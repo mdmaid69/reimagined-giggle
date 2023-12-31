780e8ea912a0592c26d6019926e6b86f8f96094a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:49 +0000

    minority dress eager afterwards through

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
