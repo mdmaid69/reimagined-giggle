bf3b908d5425f415b24a8b412dc6dd537e813d93
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:56 +0000

    lookaround basis lost sad while

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
