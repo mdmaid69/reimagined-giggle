46af78cacf5398158b80ad49336416f6b5be75dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:47 +0000

    turnup though weigh boring plan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import collections
+def create_user_list():
+        return collections.UserList()
