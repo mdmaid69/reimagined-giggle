dedc51fa55eb341ca9f76ff2e62e17e4137d656e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:12:48 +0000

    window big wish breakinto phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
