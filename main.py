f0d217cd6a7c62555619827753a0244be96e34c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:36:43 +0000

    lonely payment no convince attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
