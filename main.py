78789b4b3a535989c436876d72088465dd8e5020
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:51 +0000

    justice show keepup clothes put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_max(numbers):
+        return max(numbers)
