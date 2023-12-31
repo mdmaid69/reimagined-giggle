0a0f303279fd9a9172aec410b73f7fce34d4dc12
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:00:08 +0000

    far connection terrible method memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def clear_array(array):
-        array *= 0
+def find_min(numbers):
+        return min(numbers)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
