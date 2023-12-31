dfe37d1e020387f2065866a1c40a367df3eeb9ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:06:58 +0000

    extraordinary spare play bit seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
