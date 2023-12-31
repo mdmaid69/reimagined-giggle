eb8c834a9a5593006f42df1c591319618474610c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:00:35 +0000

    half pronounce town trip interrupt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
