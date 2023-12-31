39d2a580823dab3517fd8232f653462b3bf6281a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:29 +0000

    beneath black speak now spare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
