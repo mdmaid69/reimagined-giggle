912744600186483c523c2836ccd3546c04378fa2
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:37 +0000

    vote simple clear false suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
