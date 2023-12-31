9532bd8d4090695daefd992b4ccc338cf6c47101
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:53 +0000

    Ss reply forgive conversation slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
