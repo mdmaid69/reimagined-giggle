c7ac125daaa1d71c19c930ad7f4a31a7bd9a4e79
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:23 +0000

    fall suckme grateful rare pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def square_number(x):
-        return x**2
+def calculate_circle_area(radius):
+        return math.pi * radius**2
