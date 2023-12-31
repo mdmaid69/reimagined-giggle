eecf6cf9aad06ab8a033a33cf425a0172384f277
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:40 +0000

    mood usual high Uu size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
