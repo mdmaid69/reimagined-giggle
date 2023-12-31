9f2f4e883bad21b43e622fa57d728f795fad3346
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:25:28 +0000

    turnaway measure middle telephone display

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
