318c0ca2123bd1945ed0389dba322e46b29cda7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:00 +0000

    discovery joke extraordinary unite move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
