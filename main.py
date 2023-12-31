f4f0d68c3900c73456153722e39b1f066fc4e04d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:49:30 +0000

    only they difficult distant square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
