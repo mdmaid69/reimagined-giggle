80c4cdb37db88581be761cec4316161b33fc81e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:24:34 +0000

    report worst Rr mean tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
