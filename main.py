f4b990b6629bf75954986fb85781bd9fda3c1c1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:27 +0000

    settle increase each follow rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import getpass
+def get_username():
+        return getpass.getuser()
