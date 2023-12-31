84ee5b9e089c9eae3748a8bb623f6d0ccd87db42
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:21 +0000

    raise contrast full cough before

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import tensorflow as tf
-print(tf.__version__)
+import sys
+print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
