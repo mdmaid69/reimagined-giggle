c913e51cc31fb30727b249bb4eb57f3eee940c2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:12 +0000

    excellent somewhere knowledge regret mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
