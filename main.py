416ec255b5045dc997cd61f2911bdb2871d1596a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:10:23 +0000

    beginning takeout mark popular like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
