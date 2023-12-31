d74fcd92772ebc6d003548616cda99ec86ec6f9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:46:58 +0000

    especially even question small question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
