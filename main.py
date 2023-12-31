6aaf13d851d8d954498b80e9d8679e0f5c47b8f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:47:20 +0000

    second reason rain punish subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
