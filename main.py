32ac4f044f5853edd8ac40bc204bd5113e2fa595
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:57 +0000

    minute little use themselves drink

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_volume(length, width, height):
+        return length * width * height
