7eb101d94c40f08911a2eb168f9c4f7d61268dbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:00 +0000

    something attempt command lead course

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
