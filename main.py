05e8cec09a08a55314fa110b2ac03fcbc591bab8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:31 +0000

    goup care burst order any

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
