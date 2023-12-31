906d9fb0100048d1ea81f60102a26b01528897a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:31:17 +0000

    variety mysterious clear every guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
