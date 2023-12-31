6e48561694da9cc8e271ef419c83afa9bf05d9a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:28 +0000

    workon how carry unusual concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
