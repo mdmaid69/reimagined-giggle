05a868c2c9f967cffe83da6cdfe1d1a11c62ec17
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:07 +0000

    cold try shade pleasure late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
