037d4e33e27d52ce0909ade11cfe72f59ea0ee95
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:31 +0000

    skill street yesterday air thinkover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import re
+print(re.match("h.*o", "hello world"))
