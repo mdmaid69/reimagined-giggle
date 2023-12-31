ef88225c9a7a4c2f912b2c740918cf2a858382f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:34 +0000

    around nothing advanced run man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
