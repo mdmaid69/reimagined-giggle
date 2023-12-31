88efadfc3a77d9922b220c392eed53b6f3477bbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:09 +0000

    who identify ago mean base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
