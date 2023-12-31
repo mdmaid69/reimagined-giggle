fc88bbc1cdfcbb3266bfbd42584ab421247b74f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:05 +0000

    offer guess freeze kind contrast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def count_characters(sentence):
+        return len(sentence)
