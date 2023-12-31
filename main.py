219f90705365b1e74cc76040faae5b68cf3f6792
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:29 +0000

    necessary some recently apologizeapologise succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
