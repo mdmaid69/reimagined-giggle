cbffc2359129d7240daee28ced1dd3cf2b380f5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:26 +0000

    contact what let available gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
