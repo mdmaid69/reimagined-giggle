56c905e9cfe59ecfba776641397c37af91e9d7f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:01 +0000

    load father leg pump brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
