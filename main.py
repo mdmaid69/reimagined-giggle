c735ee5f9065d2dd42fe4ef1f0571065e5a2b858
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:25:26 +0000

    light deliver worse stupid help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
