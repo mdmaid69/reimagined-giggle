e36deb0f9914ac536a8e1d11f667ebcdee8c102e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:24 +0000

    hit word difficult doctor knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
