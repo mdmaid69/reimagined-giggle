b5bf2be17bff1edff7ef4e0e0a8f91610dcdd657
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:07 +0000

    distant phone eye treat sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
