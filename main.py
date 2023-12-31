5c942555068a80a275a0e2cb2b5344515cbe62d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:59 +0000

    careful free goout breathe dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
