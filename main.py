b85bb9beb5ccdee8590797166970eea692f37e43
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:58:26 +0000

    lookafter tip division occasional action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
