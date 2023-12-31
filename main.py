317eac716b9e92dac9d3cc3fd09bf0886f86effb
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:08 +0000

    damage absolutely wise narrow general

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
