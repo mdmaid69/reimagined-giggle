6ab04572bf80426a894c96dfdd35d0a1c2262a3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:17:40 +0000

    rather takeback bunch afterwards write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
