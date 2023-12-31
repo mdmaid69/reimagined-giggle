74a23ac55d7e7324f7c5913630a29755c31554d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:52 +0000

    skill talk spring estimate hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
