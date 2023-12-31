8b545ff53346e26f0393a4b5c4a5a67e11657320
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:35 +0000

    here exit sore rent cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def is_even(n):
+        return n % 2 == 0
