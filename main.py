9ae3486407674abce6873d8b372e05bc3459f86b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:26 +0000

    pretty appeal Pp exhibition goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
