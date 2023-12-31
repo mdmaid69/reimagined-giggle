763f9fdc2e6ec6cdd779eabb7697ffe819e2049f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:01:24 +0000

    unlikely chief pleased leave event

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
