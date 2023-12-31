dfeea0a9b3b7d05dceb0d4df9d743117291de1cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:28 +0000

    rule few town apologizeapologise loud

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def convert_to_octal(n):
+        return oct(n)
