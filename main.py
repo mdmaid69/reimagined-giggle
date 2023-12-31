d14716a6d77957dd270705ae20fe87e7a2eec12a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:46 +0000

    spring badly sure either switch

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
