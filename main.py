930938c62dca4dfbcc0457c4b7a05d01946225ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:59 +0000

    quick replace pity life wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
