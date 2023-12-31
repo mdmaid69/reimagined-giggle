7556f852433537b6d52fb5bca880996674eef763
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:04 +0000

    though vote them peace here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
