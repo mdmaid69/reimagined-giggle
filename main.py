d290f2b1013ebf4d2a48568420ac508fbc8f6073
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:22:43 +0000

    behaviour wish rob count idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
