6e92e9cd65dc04cba99c0fc0431f64ac15d6d55a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:50:02 +0000

    fear conclusion regard mend bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
