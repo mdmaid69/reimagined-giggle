38146cf5408008b89dd0f2192ea93efc109216ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:27:53 +0000

    minimum I stop know apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
