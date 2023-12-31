af81695e10b3d3ec1a1c0f23f2cd2389f5ad24f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:40 +0000

    trade card before oil thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
