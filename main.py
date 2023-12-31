63b7329cee63053df63ebfca5705ed4ae1f8be4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:08:50 +0000

    suffer official think gooff concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
