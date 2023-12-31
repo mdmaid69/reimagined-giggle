183aa667e7a4e22054b7632248ef62320dc3dfc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:07:21 +0000

    roll want worse story week

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
