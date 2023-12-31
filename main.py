10e72a5504e0d8124afe4ca771f59f630c9dc49d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:21 +0000

    wing union similar region style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
