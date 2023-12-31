1298489f8c0e645e75ce48338ca137783b622b4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:15 +0000

    message miss paper trust settle

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
