558ca345e33c97de949682f7c8cf1c64f696ec80
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:47 +0000

    guard new obtain keen write

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def count_elements(lst):
+        return len(lst)
