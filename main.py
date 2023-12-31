e83bd8c35614c57c5339e2893e71ed0ecee8dba8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:43:53 +0000

    urgent room collect phone quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_speed(distance, time):
+        return distance / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
