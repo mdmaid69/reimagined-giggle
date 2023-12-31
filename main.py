558e4a4ca55a7fd2e5e43c6589f77d47bddbd62a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:31 +0000

    write speed important container plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
