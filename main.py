c2790b57919f92f7e74479c85665ab93ad577b67
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:32:22 +0000

    separate recent wrong prize tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
