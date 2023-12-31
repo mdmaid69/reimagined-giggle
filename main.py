f46a76aacd6a4fc481497c6c6794491bb22ae165
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:14:52 +0000

    throughout reject discover transport professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def get_array_as_str(array):
+        return str(array)
