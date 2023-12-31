d983e8fa675e0171e6b25df76577b4867c60930a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:08:17 +0000

    tip used behind classroom forever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
