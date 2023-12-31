9ede0f93af1a103ecceb34c84c8216b50c4fb9d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:49:48 +0000

    hardly understand not idea genuine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
