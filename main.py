b6c1c6b90b6b719dce28ac54e83e25c0cf621e5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:54 +0000

    the steam effect best everybody

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
