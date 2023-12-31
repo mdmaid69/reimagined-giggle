9995adf9565843cdba962819c947e871cd81350b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:07 +0000

    design sing occasion rubbish owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_directory_name(path):
         return os.path.dirname(path)
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
