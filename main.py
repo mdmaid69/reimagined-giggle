ba2a806459e374b5c0dafd2970b8bd8245a4acc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:36:08 +0000

    occur shoulder quickly authority gas

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
