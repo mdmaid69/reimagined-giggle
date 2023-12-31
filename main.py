72a7a8f23879b56957162d2eab043d918d38905e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:56:49 +0000

    reasonable discuss minute under kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
