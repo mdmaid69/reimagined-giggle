21487b590ee9dcc94f065debcf66da578d36be9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:10 +0000

    game dry serve so pain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def add_numbers(x, y):
+        return x + y
+import datetime
+def get_current_date():
+        return datetime.date.today()
