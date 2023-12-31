3b56ca3f5465c764f5c8a415076e5c52918136c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:23 +0000

    young explain takeaway tongue anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
+import platform
+def get_os_info():
+        return platform.uname()
