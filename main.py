dd42c67fdf4eb02ceaccaa81b7bca69120127de9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:03 +0000

    Nn cap around goaway lead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
