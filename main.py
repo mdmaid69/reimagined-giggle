57f43e61ce96b5500f7222f37b0b8c1ade19ae67
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:52:30 +0000

    Jj complaint keepaway related fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
