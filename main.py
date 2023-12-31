b1e575b07b073982dd70875981b01163e6de90db
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:17 +0000

    Pp attitude leg dress test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
