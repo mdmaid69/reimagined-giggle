31d6808b973633d97a9c2393b706a5152fadba72
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:14 +0000

    television command responsibility love 565end

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
