0fc0b2648782fe109c60332b175b970523326bdc
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:47 +0000

    ashamed easy thinkthrough oil judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
