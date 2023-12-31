9e03da20d86b37d8b2c19938661eb3b12b8541da
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:44 +0000

    possibly familiar afford patience goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_as_bool(array):
+        return bool(array)
