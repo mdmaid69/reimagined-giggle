4663df97559e84f32bb16ff06af8d1afe5193f65
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:24:44 +0000

    whisper petrol special rid swallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
