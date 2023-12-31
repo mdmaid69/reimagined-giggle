90806b758231e7c6bdb88fc4f095e167a977fd3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:23:32 +0000

    offend sore cough declaration accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
