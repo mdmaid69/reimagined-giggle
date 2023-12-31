c666c014199239716ec715d44d2a0a570c239d41
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:30:38 +0000

    unpleasant show purpose meanwhile towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
