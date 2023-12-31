13a13f2feb3e3dfa51108c5f20994eb59ea1b773
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:12:35 +0000

    pressure upset belong judge sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
 def get_string_from_array(array):
         return array.tobytes()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def count_words(sentence):
+        return len(sentence.split())
