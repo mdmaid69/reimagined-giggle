267051623e5595edbbb33bf97f15d420261fc98b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:40 +0000

    plan car true firm like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
