357c1b10b1dae2a2c9ca0cba1974d3ae49bbd773
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:04:40 +0000

    brave coat aim Ff colour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
