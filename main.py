f96154769045b762e5c0ce060146902c23d41444
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:49:46 +0000

    study meaning note ring justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def subtract_numbers(x, y):
+        return x - y
