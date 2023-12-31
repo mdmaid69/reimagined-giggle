22d7ca4ff75b24ef3f7c1101cf51aae86c5edb15
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:32:28 +0000

    really like company group lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
