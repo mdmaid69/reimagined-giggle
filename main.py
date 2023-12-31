089e632a4f7fd79d018301385e249a87bb9489dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:51 +0000

    guide pretend money trust far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
