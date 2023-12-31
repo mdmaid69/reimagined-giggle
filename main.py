68ed3408a32bda75f276ada756c0c281f733c964
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:52 +0000

    chance watch touch sentence excuse

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import time
+def get_current_time():
+        return time.ctime()
