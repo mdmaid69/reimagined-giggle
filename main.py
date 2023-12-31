e5fa84e6ea65a52cbdfbb34ea1de06958eac8767
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:26:03 +0000

    accept excited approval collect 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def convert_to_binary(n):
+        return bin(n)
