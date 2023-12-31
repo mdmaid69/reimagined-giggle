cc750921e7d0e59925f238f48663dbbeddde08c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:55:57 +0000

    clue dry bet space often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
