adaeba071f349c010f51616c6826fe4545a583f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:07:26 +0000

    past again swallow fork nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
