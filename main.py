ded81b5610b41a56af9d7aefbf3452a3c6177e6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:41:40 +0000

    hope tear fire brain exactly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
