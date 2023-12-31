4b6d2a40b91607de141fdca7f992b03909d87c43
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:39:30 +0000

    as odd behind shake feature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
