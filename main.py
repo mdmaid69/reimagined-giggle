26f7c3621c4cee2cd97edda0a20ca15cb9e6bc2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:55 +0000

    public answer approval join attempt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def remove_directory(path):
+        os.rmdir(path)
