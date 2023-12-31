36b78fe8f523f6d9656449896c2ee8c986d3ebdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:30:58 +0000

    failure second suit ourselves they

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
