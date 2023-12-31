77dcb0cc436e443ecd67fe609861142351104046
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:20 +0000

    every goout bottom maybe undo

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
