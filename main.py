40f58904aa0d9bb515f135f0433efc3cff4300d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:04 +0000

    instrument fair able frighten quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item(array, i):
-        return array[i]
+def count_elements(lst):
+        return len(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
