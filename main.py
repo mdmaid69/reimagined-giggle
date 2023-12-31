ef2c1fa1e6b7b34aa8cc3cd0a65bf6a098d24d72
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:49:40 +0000

    hold point mix window recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
