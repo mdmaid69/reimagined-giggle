e7683dc7ef754272c0413eb054b032396f1575b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:47 +0000

    know stamp window rate free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
