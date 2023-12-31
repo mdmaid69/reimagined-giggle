e10d07ef84c22d10727788d8c3dc3f4bf0ae1144
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:52:59 +0000

    week vote team attach spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
