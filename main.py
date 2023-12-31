b70d5ad74f7f87ff26270e452754005396b24837
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:14 +0000

    happen workon undo opposite relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
