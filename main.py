617d78d79910c08f5e126874b8a299dc32c42157
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:17:17 +0000

    why road sleep plate benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def is_even(n):
+        return n % 2 == 0
