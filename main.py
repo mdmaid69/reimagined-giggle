6f8cde4db17466e1154dc655afc4f424dfe44562
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:13:21 +0000

    runinto forgive communicate command nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
