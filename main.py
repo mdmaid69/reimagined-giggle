1cedf78341e888a03e706dec969495650cf38257
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:50 +0000

    deal wall attitude high rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
