bffec8a2f702af7be53c7ce762eaebc63abd5c60
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:29:58 +0000

    go habit office male move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_int(array):
+        return int(array[0])
