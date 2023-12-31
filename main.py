3b50ed5693b7bc42316b77721547e9b028d4e114
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:38 +0000

    form thin beneath smell suck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def count_characters(sentence):
+        return len(sentence)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
