d0d31b493dde30f30944c79308579b2f9989de28
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:41 +0000

    see keepfrom turnoff absolutely drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import time
+print(time.time())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
