d3c1f82af4b3ef2bf8391ae2876bfe0f8ea466d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:34:55 +0000

    transport later use willing care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def change_working_directory(path):
-        os.chdir(path)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
