407cbd3e464ed80ad2e06b0ed17be19c9be2a519
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:58 +0000

    blame question score day swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
