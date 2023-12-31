d58aa739b970b0c94f2235eaa0f56883eb539765
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:03:30 +0000

    telephone tragedy cheat debt road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
