660511f65695fde396da468924a3af3efa375ae4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:12:27 +0000

    tap consist pinch once rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_user_list():
+        return collections.UserList()
