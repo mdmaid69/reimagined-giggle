9fbd56b9aae5f9c4ae65fc34d5b002635191a492
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:24 +0000

    correct slight land simple official

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def square_number(x):
-        return x**2
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
