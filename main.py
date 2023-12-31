8c58fb04f500bd4a927ed2d286b488cc90a07e83
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:29:39 +0000

    appear address nearly proper trial

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
