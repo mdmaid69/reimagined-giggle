00878a838dafd8095126f1e5aede5f7e7248435e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:38 +0000

    patience steep speak square bet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
