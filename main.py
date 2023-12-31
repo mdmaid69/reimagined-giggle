3f409f48880d746f73bfb9c64eb29e33843db385
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:14 +0000

    responsibility clean meal fight bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_floor(x):
+        return math.floor(x)
