7caa40ac101d2dcbdf61312257a4ffa4753e0ed8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:23:54 +0000

    total polite correction relief already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
