6eab46fc2dafa752bf678498b24b48f1062dd471
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:14:57 +0000

    reduction fashion boring though stir

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
