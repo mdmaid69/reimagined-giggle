628a747020e2e750ce3aea58c17fe005dcededbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:51:56 +0000

    body later kind manage television

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_queue():
+        return collections.deque()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
