82fe04ce6cd93f20c9f2ba902436fcb752d64032
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:23 +0000

    talkinto confident embarrassed identify chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
