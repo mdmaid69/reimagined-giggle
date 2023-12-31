6baf64f9dc966948f41f802536c4bcc2a6d47dba
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:07:37 +0000

    true obtain draw become makeup

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
