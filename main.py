3cc852d5c676794e61eda1f448542e9b1b97f8e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:05 +0000

    actually opposite mix convenient string

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
+def calculate_work(force, distance):
+        return force * distance
