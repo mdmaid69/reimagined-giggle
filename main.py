2c0c5a6fd44340d07986c6e51216ee4db39b4eb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:22:54 +0000

    pity hide fair tidy tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
