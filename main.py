3c8d535c4c4be094f31e70c53a4334a0ef3c754b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:59:12 +0000

    street swimming 571enough temperature of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
