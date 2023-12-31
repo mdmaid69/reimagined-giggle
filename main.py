a99e727b71c4739bc0f0701e26b7e7e729651bcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:46 +0000

    original than large pass phone

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sys
-def print_python_version():
-        print(sys.version)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import datetime
+print(datetime.datetime.now())
