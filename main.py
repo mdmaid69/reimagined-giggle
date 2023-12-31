09adf89da484b34234b18763643b0e5220e29d8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:11 +0000

    various tidy convinced beautiful passage

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
