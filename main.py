a8398b78e7aa90113bb134ccf7c516766f59480a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:22 +0000

    stare niggaacademy ring play noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
