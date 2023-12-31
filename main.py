5a7e490a65983652659f564dbdde42d5c098042c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:23 +0000

    fair wake place meanwhile worth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+print(os.getcwd())
+import sys
+def print_python_version():
+        return sys.version
