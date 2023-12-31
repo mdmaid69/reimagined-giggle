56ec97d087bae1772829d685084daed065505b98
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:10:28 +0000

    shadow business calloff store form

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
