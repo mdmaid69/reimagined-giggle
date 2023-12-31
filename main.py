1d0fbb41a514f373d4c5e499d6493ba7dcfd1236
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:58:13 +0000

    system judge help produce equal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
