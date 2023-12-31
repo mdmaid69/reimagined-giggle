a50a026115a7b2d8a6d1730fe3d9f28e7e92c245
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:57 +0000

    occur broken shock unite urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
