33caad4a7db6ec66926756def14c6e2a3b2bb9f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:16 +0000

    butter container road favourite feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
