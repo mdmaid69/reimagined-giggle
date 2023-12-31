acdfd6d813e31b61210013126010b5f3d3ed8728
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:15 +0000

    lesson reasonable surprise office and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
