f009bb1533282f11046acb71a95b728169b9c1ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:45 +0000

    hope search peace failure after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
