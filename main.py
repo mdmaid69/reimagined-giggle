412d89390d34227fb55241f89472d0ba0044edd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:54:27 +0000

    rarely tool swim cry live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
