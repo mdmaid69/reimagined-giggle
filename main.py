a6cc029d10e41f844c943e8d7dc84551ada58714
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:50 +0000

    software responsible oil still contents

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
