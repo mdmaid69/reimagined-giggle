07ec4cc3fa8f8215340124d138750433977ea3a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:14:19 +0000

    defend sale try hit organize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
