8cc63da1cbcc3677305ca826b7e333b6a655d809
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:11:01 +0000

    soak church upstairs science urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
