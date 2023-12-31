c035f8aaf195dee3e47883f687e513df9aea1bf4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:50:17 +0000

    guess murder eat blow reply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
