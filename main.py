3d75ee66d07ba75a2eb2fa87d07e4e22f6f2edcb
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:03:53 +0000

    bother bunch fire bin uniform

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def remove_from_array(array, item):
+        array.remove(item)
