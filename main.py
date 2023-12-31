c5393bff461fdd4cac59e402a307ab9937dcf426
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:54:53 +0000

    tap slow harm recent law

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def convert_to_hex(n):
+        return hex(n)
