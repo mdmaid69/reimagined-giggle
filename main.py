453ad86ac449123183ae542fdc845f99a9c2dad3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:02 +0000

    when popular behaviour coat closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
