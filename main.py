4d519663b67934ca244a650f364e7efde0230935
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:28 +0000

    convince sleep part otherwise truth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_current_working_directory():
+        return os.getcwd()
