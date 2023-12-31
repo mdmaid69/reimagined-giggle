6de68abea1875c32fc158c9b0374c1ebdd627ea4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:28 +0000

    get themselves experiment perfect Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
