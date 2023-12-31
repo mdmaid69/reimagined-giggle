6f0d0bc66244889b0c1bbcf10ceae1e7100f35b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:54:07 +0000

    improve contribute fashion attempt lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
