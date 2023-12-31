9d24b584f0298a82f1e141f58e6bcfb702648077
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:22 +0000

    cough only settle fire themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
