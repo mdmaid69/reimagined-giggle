267ef829ed1f67010716549f05b1803e7137b7d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:27:46 +0000

    along gradually stone party key

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
