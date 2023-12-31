b944e14ff2a8ad8c5f993fe469312402d380fcd8
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:58:46 +0000

    sister television reject suggestion suddenly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def sort_numbers(numbers):
-        return sorted(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
