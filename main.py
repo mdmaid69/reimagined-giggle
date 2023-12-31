ec3bcbcfaadbc1ca839e233e5a21300b85fa220a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:49 +0000

    crisis appear but cause man

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
