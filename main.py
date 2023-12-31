554945f42104a6cdd9cdf634d07d5f77ad950cf1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:23 +0000

    cap machine sight smell sad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
