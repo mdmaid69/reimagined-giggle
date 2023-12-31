4d0e37fe133fc90feec9ad8c31c25fec9f3d10c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:39:51 +0000

    basis impression clue people 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
