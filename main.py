45f6c2c997b4ecb9306a165745eef62443388c55
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:18 +0000

    shade yet ago cost range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
