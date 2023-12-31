042a2f3adff262c57424d634b41e03250a5a2a67
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:12 +0000

    practical live culture sing danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
