f0078f03667c4591936f42d402f4da3ffbb28fe0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:44 +0000

    section order generally eager arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
