5ae942e1e3cf748c1042a3b7db4fdd9cce5295bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:15 +0000

    him need choose plenty study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
