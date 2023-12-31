6a4f2247896d3b105e4804e47b26244db48573ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:04 +0000

    smile reaction quarter question hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
