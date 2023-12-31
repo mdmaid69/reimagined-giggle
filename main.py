c3bee6f93666de6f8bf6b22cd1d29e918ce64ee4
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:49 +0000

    goout fit move love step

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
