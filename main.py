5868d26b447472298daa77f76099e9d4cab440b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:46:51 +0000

    used knowledge soak affect whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
