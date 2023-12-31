dc2c42a96a91c3bb9d11753e3d64a3dce4099ed7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:25 +0000

    I high better dead stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
