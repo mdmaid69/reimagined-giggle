e0f59294fdac784e0caff820eae786620c0f283f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:41 +0000

    gradually search left section improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_array_to_list(array):
+        return array.tolist()
+  def multiply_numbers(x, y):
+        return x * y
