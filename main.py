e242958c4aa2c2525e6f3a1966d5a15e30ce36cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:50 +0000

    turnover skin fit path proper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
