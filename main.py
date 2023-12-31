f9cfe27189b5115f3179010ede3ad3963d2e2358
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:31 +0000

    line alive freedom blame swim

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
