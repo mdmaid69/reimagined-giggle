5e6407bdc39a31495239a2ca2bfdadcadef5b1cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:06:38 +0000

    reason discuss clear off please

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
