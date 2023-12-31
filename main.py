c164d4e2ed9c44bcea8f52aff2e56d871511cf4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:17:49 +0000

    poison impressed cross fat 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
