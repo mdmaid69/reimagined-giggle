24fe1f1ae66f09faca9e70a63489d4080cee5aab
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:50:45 +0000

    pile trap mess means between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
