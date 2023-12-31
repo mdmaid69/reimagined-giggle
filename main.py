f0fec5746fdfa3193047e040d3ae4df39c752ab4
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:23 +0000

    broad soil tap giveaway sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
