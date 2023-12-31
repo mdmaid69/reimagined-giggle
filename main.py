a6111fc2e62bbc9380709df241b40e6cd387510e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:37 +0000

    bottom over hand there cheat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
