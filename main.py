3da2108f011947ee3e00def69e16f061922b6667
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:26:07 +0000

    finger needle situation thing pin

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
