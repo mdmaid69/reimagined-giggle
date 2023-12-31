2a0ae9731fe1b226617e27d25fcf9e6d4405013b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:01 +0000

    usual birthday definite holdout Nn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
