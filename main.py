6b437544dac54039a7b53a366445b3120f3b27cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:40 +0000

    party fight nor uncomfortable children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
