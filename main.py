d01b95f101284aaa4e358f05f244a1fb40942065
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:37:13 +0000

    terrible perhaps offence grand favourite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
