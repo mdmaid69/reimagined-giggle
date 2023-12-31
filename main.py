e5af2f60fd869a5df46714b851c816835a888e1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:29 +0000

    injury road preparation inquire breakout

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
