fe363189d51d4fe0aefa6c4a41e27f73a9320910
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:02:11 +0000

    music trust keen well never

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import numpy as np
-print(np.array([1, 2, 3]))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
