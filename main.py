244c8973c612fa8fa33d0e3603dfaf71e9d81711
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:40:43 +0000

    healthy excellent dry fire week

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
