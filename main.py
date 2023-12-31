fcb2c004b0b8bb9fcf2aaa6dbb5e93c1ff6158f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:05 +0000

    suck connected rank this physical

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
