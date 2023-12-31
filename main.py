f657b137b6fb0e84d6ac4abf520b6e4137f5e821
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:40 +0000

    spot fond relation runover major

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
