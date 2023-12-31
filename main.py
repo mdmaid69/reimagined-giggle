c54c63c056b10ce671fd5b5f3ba2b8a0d250b59c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:19:40 +0000

    forget brown progress rain exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import datetime
+def get_today_date():
+        return datetime.date.today()
