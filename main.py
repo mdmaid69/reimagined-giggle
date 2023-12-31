4d1c6240f9a884b0f82b3c03ac072c0d94040e6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:04 +0000

    lend avoid hope city direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
