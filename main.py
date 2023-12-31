55e29ef27ca72f277a37d400c47bca8378f0f71b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:09 +0000

    pattern slip because choice term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
