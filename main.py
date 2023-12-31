25a97bb8af310905e3e47a5c808745735c75a973
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:00:11 +0000

    hot rain know used offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
