f35bd3cc7ba3b80e7648e6806885ccd4085c978c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:10 +0000

    conclusion pride better squeeze frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
