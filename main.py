4020092b388fdaf8ee23f6804e033757af96bd14
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:32:40 +0000

    room tap mess cheque tool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
