a54937e12fcc1b58e242ebea39ae6f603676f4c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:26 +0000

    why still shape free equal

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
