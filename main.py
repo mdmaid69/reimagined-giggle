95d646816ca1541e54814e6939a45fe57f3ba2af
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:43 +0000

    friendly wheel familiar argument absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
