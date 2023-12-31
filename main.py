6411f12bce9da94c4baea53216c940d91daa4b04
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:35 +0000

    lost free bottom store sense

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_max(numbers):
+        return max(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
