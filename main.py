96f083d506db3826f17cdbe7283215d86af24ac5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:40:35 +0000

    name frighten problem manage effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
