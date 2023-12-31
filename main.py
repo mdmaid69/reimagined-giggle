4dd67f859829d23f2efae55f212f6fdc2abd6bb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:14:37 +0000

    love account rise female point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
