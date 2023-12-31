d1c59c2b5970648d1c0cb7b643daf03a27ae643d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:37 +0000

    translate silent message whistle leg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
