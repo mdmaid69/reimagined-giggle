4bc58a22725fc1eb0df694d91ef463d2a7252413
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:38:24 +0000

    clear as myself reason you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
