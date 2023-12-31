0e1a9f80aa07473791224f92cb99c8bd365a6c61
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:11 +0000

    could reach amount proud challenge

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sys
+def exit_program():
+        sys.exit()
