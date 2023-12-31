0f6765acb08b3e1a9cb12a41427d00dd58f68b8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:19 +0000

    spot pressure talkoutof variety deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
