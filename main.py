10e2329c5f0218cf62ed95aaac2a944880c20236
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:26 +0000

    union various hour relief breakup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
