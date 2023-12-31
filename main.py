629611a370448306dc1c9bc6051202491f1abacb
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:22:14 +0000

    mean afford each honest luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
