3c6fec0446a8f577a4f3946693e7775f06404a39
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:31:29 +0000

    basic mood great decision health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
