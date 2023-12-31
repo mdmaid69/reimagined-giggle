8aa271a1a30ad43bc132d8868fc7f2f0f546462f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:08 +0000

    comeover towards afternoon trick sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
