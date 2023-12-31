bfbfca47c8e38fd3acfa21e9c77f9e13e3ae310d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:35 +0000

    easy hold exam full flow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
