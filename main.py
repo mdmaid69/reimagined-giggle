707150b2c08bcff906e9c09311f51d02d92cd3be
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:53:51 +0000

    used advantage flow wet way

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
