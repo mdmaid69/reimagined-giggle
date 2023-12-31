211a17bcbd0b1d45421e22b5fb1c89507ba9abf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:58 +0000

    flat fairly in aim goback(to)

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
