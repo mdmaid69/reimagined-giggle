329cc4c3a02ed669922070aa977a82adca96dca4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:28 +0000

    interfere fill sentence difference course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
