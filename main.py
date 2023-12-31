56a0b24496ab9b046db117d6c7ffb47002781edf
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:53 +0000

    long shut skill born under

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import datetime
-print(datetime.datetime.now())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
