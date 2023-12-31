ba0c7cf62200654ef98463c07888c1cbc519c7f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:38:30 +0000

    stone mess rain fail radio

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
