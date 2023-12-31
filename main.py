6b0967146a30ceb996d33d9344269c6c5bdc542d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:23:40 +0000

    record Rr near whom low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
