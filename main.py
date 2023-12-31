cb804a2f6aaf72f6f36015ff771163c00a246b82
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:57:12 +0000

    protect minimum pleasant comeover region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
