be892918908174ee8b666ffa060ab2f9013bb741
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:19:31 +0000

    back lie protect care callup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
