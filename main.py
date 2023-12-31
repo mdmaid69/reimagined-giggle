e55580031cb9a482103eedd98476ba7787e9a7aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:34 +0000

    callback circle average pupil game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_max(numbers):
+        return max(numbers)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
