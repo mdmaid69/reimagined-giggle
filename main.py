0e2b7d28f8f9084aed111793da8beba6be760733
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:53 +0000

    wave reduction home swimming contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
