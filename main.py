3f95a659bdccd8721e70ee5ef187c78eda576705
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:10:16 +0000

    roll save nervous comefrom they

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_area_circle(r):
+        return 3.14 * r**2
