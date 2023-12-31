b33a146f7ca4babae65d573f272ec5c200385db9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:10 +0000

    university equal protest fact ask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
