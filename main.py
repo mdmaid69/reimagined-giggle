306da4bb7013b2b618c6d21e2de97e41d0b16e55
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:56 +0000

    dry sure apply stone boil

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def count_elements(lst):
+        return len(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
