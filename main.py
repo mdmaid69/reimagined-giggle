0b6e43f6ef829c6b7e5a37b7386a1a7b9a2e7318
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:57 +0000

    favourite giveup whether position deal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
