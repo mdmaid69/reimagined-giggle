1322ff5788bcfe2c43dbb13603576bc6f7aa9de1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:35 +0000

    colour past direct opposite tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
