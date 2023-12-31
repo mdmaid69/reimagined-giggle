05a22f0d5dfd6722f0bea5aea6dc50f3f59a7ed7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:35:37 +0000

    awake history well bridge announce

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
