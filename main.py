09e7bc8389c631c3638c78bae96f5db92438055a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:21:32 +0000

    passage cum many nigga shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def count_characters(sentence):
+        return len(sentence)
