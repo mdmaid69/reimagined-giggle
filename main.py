f07391feda2bc76870b838e5b9791d7f2de2be8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:08 +0000

    time surprised stay history track

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
