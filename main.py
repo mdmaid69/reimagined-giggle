7e95ada3f802398c8786e4df65b4e4771c127dcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:26:55 +0000

    follow embarrassed least bread remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
