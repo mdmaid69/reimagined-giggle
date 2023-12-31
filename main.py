0f843e17c4079e43d58bd7149d9b9d46e4afdd63
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:03:53 +0000

    payment claim hard pen perhaps

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
