b1ee2d81c9182c64c14b89a427c2fa3c3b8b6d7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:45:43 +0000

    trick left attractive good apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
