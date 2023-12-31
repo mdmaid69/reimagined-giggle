78e4abdca09120a06abf4fb3e93b8f3b9726ae66
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:22 +0000

    continue habit maximum suggestion minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
