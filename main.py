8e7b27eec2233ec6644f6d9a57068aa77b0dba16
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:34 +0000

    be comedown programme wander perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
