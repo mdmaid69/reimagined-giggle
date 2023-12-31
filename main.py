8e19e38bbb73ce38046513a77d87096554015de2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:33 +0000

    tonight anywhere day rely typical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
