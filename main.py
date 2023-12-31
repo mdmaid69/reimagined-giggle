23a246e5d1fe1cb45ad51d8bf5a7a6bf3953cf7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:26:20 +0000

    number table cheque take distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
