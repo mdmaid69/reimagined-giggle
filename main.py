289d6debe7f4be2c659868fb52c48bebe46a812e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:31 +0000

    long music close big march

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
