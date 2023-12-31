657a546abbd8370a7b9883020a640194d780f1e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:55:22 +0000

    nearly roof grand rough proper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+print(sum(range(10)))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
