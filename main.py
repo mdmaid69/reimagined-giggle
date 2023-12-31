378142a783fccc0e63c999843161bf27511ec107
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:08 +0000

    value finally direction whichever keen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
