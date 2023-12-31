b38d847c3101f4de4a54f3512f8c333a3ff64e4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:52 +0000

    can handle settle straight mainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
