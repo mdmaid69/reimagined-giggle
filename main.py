04221f62b0191e6223bb9b3a1a9569a85a090a23
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:47:57 +0000

    noone relationship early however turninto

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(5):
-        print(i)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
