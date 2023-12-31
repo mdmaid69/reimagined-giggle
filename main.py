c35c5588403bbd8a361155d3d1311dd34f78d947
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:01:07 +0000

    hit advanced unless great environment

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import random
+print(random.randint(0, 100))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
