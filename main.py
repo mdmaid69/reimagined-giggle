e0bda263c58eeab47dddc61154512a9721711268
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:39 +0000

    solve woman joke hospital begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
