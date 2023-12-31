d94255e5f7055a2d9c442b031a0c2a091adfd842
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:23:48 +0000

    use line Gg scrap mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import math
 def calculate_radians_to_degrees(radians):
         return math.degrees(radians)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
