136f25dded1dfaa84d4a2ff50c2404c86af571e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:57 +0000

    contents noise walk last underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
