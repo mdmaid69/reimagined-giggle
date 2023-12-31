41f16652b51e5c48424e77059f0ffdb8556946f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:30:51 +0000

    side plan cause Ee comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
