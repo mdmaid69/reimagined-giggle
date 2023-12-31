7d5e71ce569f9d8b0b43ee4ed9a03951e311f2d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:33:04 +0000

    skin settle shape theirs prepare

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
