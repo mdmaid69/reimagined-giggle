c503b0c83f5f08a0817a74b057d11631bd2c3001
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:34 +0000

    none cook good repair see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
