00ecfe81ee052c8844ec992d5ab2386c161ac5f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:33:52 +0000

    result perfect punishment hot entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def reverse_list(lst):
+        return lst[::-1]
