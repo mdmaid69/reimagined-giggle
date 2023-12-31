62bfae4f75f0dde20414c635ba27850815499d79
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:19:55 +0000

    funny when deliberate increase such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
