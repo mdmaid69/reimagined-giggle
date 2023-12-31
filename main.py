dc7e45836893f4fd9e70106d4041799bec765011
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:46 +0000

    spin straight rob turninto weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
