a03562cf04f6a13dd3d089a3dad33db753db6d26
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:20:54 +0000

    calloff friendly lookback(on) stay public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
