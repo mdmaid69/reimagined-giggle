f401589575b53c3cade3c7230cdabf3f10a06298
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:42:43 +0000

    behind compare try undo connected

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-print(re.match("h.*o", "hello world"))
+def get_array_as_list(array):
+        return list(array)
