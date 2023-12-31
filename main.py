b13511f31815c8e2d7ea9b3b8c206b8a798eba8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:12 +0000

    clue price moment plenty long

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
