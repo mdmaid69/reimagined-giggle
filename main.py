10d30d0164e64dea11a522c1cea27484e9d06d0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:31 +0000

    backwards lookback(on) count indeed respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
