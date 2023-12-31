7589788a8641c4dae6dfcfcb7bc3f6ef6ddc8175
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:26 +0000

    juice poor spring scheme describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
