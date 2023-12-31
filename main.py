c13149c9e36365b0867655b05ee9b953181dfd9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:30 +0000

    turndown result raw one correction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
