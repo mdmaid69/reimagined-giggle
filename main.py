87147abd467466cbb9c7f5909b485cb9bf05761b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:30 +0000

    or concerning bear content wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
