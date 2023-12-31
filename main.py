9ae9ffdf0f406707d82e2f6066cf6b73d3034d6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:41 +0000

    turndown display badly reply doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
