71974003987d045452c1b780ee58b2f41bd5f569
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:39 +0000

    picture occur steal argue laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+print([x**2 for x in range(10)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
