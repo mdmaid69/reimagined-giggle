5850af514deeb47e451dd1112cfb04eac383790f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:21:42 +0000

    one arrival children drop on

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
