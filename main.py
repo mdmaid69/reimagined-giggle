4bc389464efe412e8f29b236adbf73f0262c51f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:30:06 +0000

    refuse extra shrink pain dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
