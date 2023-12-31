15013445758019e9785b913ffbfb4fae47824933
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:00:38 +0000

    professional follow everyone grade negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def split_path(path):
+        return os.path.split(path)
