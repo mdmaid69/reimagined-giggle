ce57b9b80d6495c8d84a134517fda13e2fd6c14d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:52:28 +0000

    among appear advice mouth second

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-print("Hello, world!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
