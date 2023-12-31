9080851b713da7a5aaffcd72eddba84785e1b335
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:14:32 +0000

    getoff chance protection turn power

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
