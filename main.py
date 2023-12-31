de4c48ec8c0247bf4c309873d27b62abe315755b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:41:06 +0000

    behaviour shrink giveaway limit smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
