df656a4c6b20ebc1669fa747b60004f1486ff57d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:02:44 +0000

    half butter pause sink breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
