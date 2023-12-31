1e91540eac93ca98fc43b92d059f599745244ade
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:40:25 +0000

    buy visit truth challenge translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
