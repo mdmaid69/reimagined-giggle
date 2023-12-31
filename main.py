304f75a51b208c6af5dfe70c611356c8f46b4158
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:32 +0000

    want shop alone from possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
