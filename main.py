97a7d86f6aa52958150c378b3763b07f003d44c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:45:57 +0000

    memory and strange among on

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
