8ea64827aa634de05759fa48a485b4383237f991
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:39 +0000

    machine listenfor risk guess recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
