80fbcd7b9f03681f698d9bed6ec3f46acbd5bbed
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:17 +0000

    impression juice aware determined low

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print([x**2 for x in range(10)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
