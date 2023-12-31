f56facdbad271fd213a1cc933694304848e553a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:57:44 +0000

    dead communicate history store want

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
