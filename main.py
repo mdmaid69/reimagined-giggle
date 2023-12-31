2969eb1b231e81d0b7d901abb35ba05ced161eeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:35:10 +0000

    instead pretty sentence talk giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
