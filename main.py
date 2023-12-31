e1d54b60f81f76371dd16dd4a267ddf920e84c36
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:37:32 +0000

    letter one dress meeting then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def print_python_version():
+        return sys.version
