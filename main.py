74c868c376fd9009313e5204cbf0116748f76726
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:02:58 +0000

    lock because load loan lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
