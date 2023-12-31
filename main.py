ef23b021aa6d941987eb1ec7b943945807cc38f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:23:53 +0000

    phone forever ocean wonder tragedy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def is_odd(n):
+        return n % 2 != 0
