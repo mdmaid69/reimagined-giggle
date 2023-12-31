c66985d6170b3b1747807835907ce81bde9e3622
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:52 +0000

    thought brush that whichever interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
