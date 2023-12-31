ac95d55fbd5d512fbb3cd63046b78d7543eea846
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:21:49 +0000

    border across available pencil familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_power(work, time):
+        return work / time
