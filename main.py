9864515053a299842daefbf7d56f51f9472b7739
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:05 +0000

    move often on appeal mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
