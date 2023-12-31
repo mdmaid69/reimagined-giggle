a695e6a8417c49895f19a4a7a05dd9d6732a53b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:40:50 +0000

    compare performance everyone point hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
