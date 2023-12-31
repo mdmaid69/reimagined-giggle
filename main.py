5b309e86d7b829f61de6c74b88ec6ed4dd9bd981
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:44 +0000

    block city inquire Mm dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
