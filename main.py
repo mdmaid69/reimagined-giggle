2850cc7e59e8a8901c1fcd474ab804652c7f5ca4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:06 +0000

    determined record position service object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
