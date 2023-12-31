261a0fce4f7c2896dd90becba36483eb3420596a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:15 +0000

    dare turndown yourself wrap first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
