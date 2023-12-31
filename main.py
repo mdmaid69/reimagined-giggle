0dc87b59aa808084040e623f5d0827ad28272918
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:40 +0000

    runaway which type ride autumn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
