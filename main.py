452bc4123bca55d3adb7306c27ba12673fd85fbd
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:03:04 +0000

    splash previous practise visit instruction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
