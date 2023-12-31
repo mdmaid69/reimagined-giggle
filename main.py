847907e86506a2a587702bfed343abc4e219c611
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:14:53 +0000

    family season reply dance finally

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
