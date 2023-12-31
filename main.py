24880348771ce1e56a10f4c399e8c5eee02d5cba
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:56:00 +0000

    hear rule tongue work distance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
