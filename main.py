cfa9e17c6c90b24a9a1527b7f8dca8bfb8611a30
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:11 +0000

    combination whistle apart clear compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
-        return lst[::-1]
+def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
