d9fd82f69074e0437bbd7e82b533db5255878e1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:25 +0000

    secret female unkind shout rubbish

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import getpass
+def get_username():
+        return getpass.getuser()
