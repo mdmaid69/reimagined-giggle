eca57a4f0f30fe1e466686db29a2b9b35330f6e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:43 +0000

    substance giveout over whichever runaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
