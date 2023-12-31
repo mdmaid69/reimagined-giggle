772c7f79aaf6b136cf4ad6e454ff3af3b7ff83ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:40 +0000

    show feature beautiful turnon small

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
