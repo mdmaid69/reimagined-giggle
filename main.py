bb4b83140365400ba20184b2b11e37f835b0809e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:56 +0000

    knock finally breakinto clear embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
