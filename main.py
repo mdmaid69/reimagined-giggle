7ccc30ac36a44658749920e4993efd854c414bf2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:58 +0000

    Bb answer innocent person band

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
