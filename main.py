bc094547f8d9dc1b9bc782b2f52c22342c38462c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:08:27 +0000

    forgive performance abroad trust sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
