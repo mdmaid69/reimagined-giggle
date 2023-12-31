7373229d4baa9aeaeed33ee4117a9d3ff1f84aff
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:21 +0000

    insist hit test push plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
