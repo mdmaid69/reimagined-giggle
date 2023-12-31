75d02607e866ec3b76142b76c6580b5c024ceace
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:10 +0000

    ever lucky which organize speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
