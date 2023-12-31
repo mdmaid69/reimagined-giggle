12098a837925951a8fc85763c0942bae104391c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:11 +0000

    serious even round table flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
