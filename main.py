87692a368389d463653604e5325abcf12fb01ad3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:29 +0000

    catch reduction wide eye rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
