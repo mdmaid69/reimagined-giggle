391b8aee4e755adf22be4b5aec598146f2be6ad4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:18 +0000

    knock butter goup finger listen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
