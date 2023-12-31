ed334ac58ca1e79d58766d230b0ae380c828534a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:26:44 +0000

    offence honest wall terrible fence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
