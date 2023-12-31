0db36351bb455f2877d5a4f3e4aaa23d38124a35
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:32:11 +0000

    wire side friend truth somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
