a57c805bb5f1548a0788cfcff294a4d6cd2436d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:48 +0000

    sell tie rid abroad loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def change_working_directory(path):
+        os.chdir(path)
