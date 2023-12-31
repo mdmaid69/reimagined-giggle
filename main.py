cd86a666536b5a6a82d3c4621b53909c096b7a7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:43:21 +0000

    prove high dare somebody once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
