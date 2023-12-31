4b8ee9fe28a66c1fbf69bbc942cb139e66a5436f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:45 +0000

    and rubbish runover telephone dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 def subtract_numbers(x, y):
         return x - y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
