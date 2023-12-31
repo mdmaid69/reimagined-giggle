9f628ff56a0194986f4191f57eb619d8593cefd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:32 +0000

    more design lookback(on) swallow think

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
