5ad3edd63fc77b395c4caf3028c4c49e2e487ef3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:53:00 +0000

    oil achieve music design error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
