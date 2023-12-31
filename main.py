ef387cedf6f81c2e4ef451632180873a5a98142f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:45 +0000

    lesson breakinto side comfort curious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
