606df6edd22ecf15b473946cc905d68e3997d557
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:06 +0000

    long turnon thing degree black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def clear_array(array):
+        array *= 0
