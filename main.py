2a3b99654724a01cb36a823e1ecd8c6e9f89d2ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:05 +0000

    talk safe almost private pair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def reverse_string(s):
+        return s[::-1]
