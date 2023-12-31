310ae28898cfcf7c555c0435aa2e854c5a47df5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:30:43 +0000

    period squeeze serve typical wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
