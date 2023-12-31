c162cff64724be69806b7ae68986daf5568fb82c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:08 +0000

    achievement unlikely police whenever straight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
