328bcdd634d2b6f80146359a195242a93b5ce840
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:14:07 +0000

    with claim stop awake attitude

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
