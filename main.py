4ef427259d99db513e2ca6a8d1e7ad1fe6b27abb
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:22 +0000

    before crazy port beat prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_power(work, time):
-        return work / time
+import time
+print(time.time())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
