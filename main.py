ccc07e603a21267c6e708b639f3b5d5954fa6ae2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:46 +0000

    job whom reduction own interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
