b2c41fef2c3c95457b967c9d02951a4a653a6a21
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:54:50 +0000

    race negative callat habit share

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
