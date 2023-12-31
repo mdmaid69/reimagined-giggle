17762fa2a313ebab52717938e622137289d9dc9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:37:02 +0000

    fit replace dish distant shape

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
