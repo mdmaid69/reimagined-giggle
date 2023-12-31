f17062efcdfe2ecd36fa862dc72bc77a612c5796
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:07:33 +0000

    cheap along shape better holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
