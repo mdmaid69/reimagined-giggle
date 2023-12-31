8d04261ee4e942137868c7c77534cb382001c893
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:38 +0000

    equal warning strike vision test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-print(sum(range(10)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
