1e5bf6d860ec2ca66732dbc0780852a177906340
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:23 +0000

    uncertain unite runover find experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import tensorflow as tf
+print(tf.__version__)
