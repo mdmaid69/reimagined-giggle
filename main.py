9029b522b50f2c0a4659afa4679f8b9a37f4dbf8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:18:11 +0000

    worry Tt trust cry simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
