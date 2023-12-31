05914e421a58fbd1e9fbaab9c38b932ab7bf65a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:28 +0000

    goon confidence hurry extraordinary shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_unique_words(sentence):
+        return set(sentence.split())
