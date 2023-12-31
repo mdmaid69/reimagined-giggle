4204e72792e9910420edb44ed9cf955fc1e32f59
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:56:16 +0000

    turn tired discussion thorough move

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+for i in range(10): print(i)
