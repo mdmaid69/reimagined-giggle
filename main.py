356762dd709e667e34e1344397a74b314b184abc
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:30:35 +0000

    deep increase offend prepare characteristic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
