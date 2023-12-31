9d7c9b18eca7788508f66684bb37cfc3a195ca78
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:04 +0000

    future recent brain partner claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_counter():
+        return collections.Counter()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
