b258a783528e3ad038a90d005025a27b9549f630
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:35:02 +0000

    smell which trouble broad noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
