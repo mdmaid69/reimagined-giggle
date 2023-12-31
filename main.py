ef0947bc35b96121850bdbe5fb755873125ab8ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:10 +0000

    whoever cook market crash advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
