5a0bd0b9d7adcfa6fc013c9fed822748dc907652
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:23:19 +0000

    nonsense still every best getup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
