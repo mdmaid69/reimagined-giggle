be671b2f9c05371536a4f30e5aa0825b34d9b4c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:53:27 +0000

    next advise tidy copy claim

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
