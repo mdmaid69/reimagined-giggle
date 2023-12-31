bfae420b8e93d6b1f20de3b4599ddafa4d1aaf97
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:04:13 +0000

    collection steady sail feed transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
