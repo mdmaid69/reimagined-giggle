ec1cf47990b2b1ffac7308109f2fcb67e3f0204e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:04 +0000

    traffic turnoff slow future message

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
