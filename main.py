5f95e852c3179fd760cac79d8b4e6743c74d7676
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:45:14 +0000

    welcome anywayanyhow opposite manner clean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
