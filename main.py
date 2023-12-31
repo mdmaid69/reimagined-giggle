c3c5dc5a1c3b83896ab6bef2d850ca543d16df27
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:14 +0000

    ball content distance comment traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
