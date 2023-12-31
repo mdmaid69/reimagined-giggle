46015df6cfe0b29164a41e94f63f0348fc0aa5a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:14:04 +0000

    whether book estimate person bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
