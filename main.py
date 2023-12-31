9420399f26ebf751520010fdc299f147b2074c90
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:49:00 +0000

    protect narrow example central street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_mean(arr):
+        return np.mean(arr)
