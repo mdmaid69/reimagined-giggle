2127928e6fd5a8ed2dc4234860e9fee1a71250ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:17:30 +0000

    damp they limit sort light

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
