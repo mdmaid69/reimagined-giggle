d7e438508e1307d91b74dd40baa7fb5bf16dca83
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:25 +0000

    switch mail low wish waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
