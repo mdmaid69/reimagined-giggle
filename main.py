3b9bbb6e8dcac64e339c84ced6ada3a01479c5d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:27:17 +0000

    trick busy constantly choose breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_item(array, i):
+        return array[i]
