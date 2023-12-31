59eaf98766d8d80bb287ad30bc0b9c1d25b0589f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:16:00 +0000

    spend official here arrive suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
