f1247aee9f406b847a8bfd1ac1823a449778817f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:12 +0000

    under transport information for practise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
