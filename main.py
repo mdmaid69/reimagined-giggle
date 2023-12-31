da284812119217d695d0545937f32dc11555039a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:34:41 +0000

    want typical flight tall violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
