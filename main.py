57b34f693fd0b05df0185590a3f62a9026ff65cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:38 +0000

    care emotion emphasize advise correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
