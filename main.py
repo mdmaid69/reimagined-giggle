f39e6a9fda3d2d40d7b6b09d56cfa503924bdb55
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:00:05 +0000

    embarrassed already switch turndown cheek

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
