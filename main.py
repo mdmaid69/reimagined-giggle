16074a8d5fed691be85d8211d84cdd0fe8bafc1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:33 +0000

    calmly noise mistake inform clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
