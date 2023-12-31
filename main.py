b701b828c2ce5b0912a361c6075236ec127eaf78
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:21 +0000

    hungry have pipe hard voice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
