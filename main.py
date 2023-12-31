852ed010646c4e01d774bbd1fb696d8bbcdc98f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:24:18 +0000

    marry trust cost trouble rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
