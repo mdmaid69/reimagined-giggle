2e543e6474fead82b417a3ff43028bcd4ada5b9a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:07:47 +0000

    equal proud weak means factory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
