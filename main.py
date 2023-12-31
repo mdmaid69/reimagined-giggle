9043635e01ed91cf42dd0f151d1e3a0aad60c762
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:15 +0000

    weigh trousers cry anything throughout

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
