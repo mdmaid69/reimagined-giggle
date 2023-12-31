0a68e4b6969e064819a350d396bce8af76aae30f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:28:06 +0000

    sense if front scrape move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
