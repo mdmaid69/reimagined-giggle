cc7eef264a725fe42416d5e859c3d9bbf53a6400
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:12:31 +0000

    rush between last address well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
