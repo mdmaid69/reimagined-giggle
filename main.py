d949957a5baf52d6729ac4a23e0ed6c92393a7af
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:36:32 +0000

    firm clean waste train telephone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_item_count(array, item):
+        return array.count(item)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
