5cd908e7b2084b4f988e8abf8ba32f44a09f2bd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:07 +0000

    entry conduct class save sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
