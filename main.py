5438676c88c77678102c1ba36d153cb445334b00
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:22:08 +0000

    low separate sore natural trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
