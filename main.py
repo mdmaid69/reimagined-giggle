3921a44c9c0aca7d72a4d49be91c5bc5ab7fff66
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:43:32 +0000

    strength more soil instrument right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
