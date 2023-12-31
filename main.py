80c1b4c436ebeaffc6d89961ef56edb527486489
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:44 +0000

    include regional cold word care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+import os
+print(os.getcwd())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
