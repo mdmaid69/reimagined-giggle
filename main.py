e31e7e1b4840ca5099a20b328e229dbd4fb2150c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:43:48 +0000

    nigga unable left gooff substance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
