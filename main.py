72f3104dd48c6629a35adcc2a5eb06ec771f8135
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:15 +0000

    travel suppose transport unlikely hungry

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
 import csv
 def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
