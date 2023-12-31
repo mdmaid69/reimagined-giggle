5545d7c2b8cc5c14cf4648ed3f2ee48bd44f5e59
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:21 +0000

    one disapprove before private persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
