0ee8199aeb6e10a9ce8084df5729cdfbb0773e31
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:26 +0000

    makeinto undo pleasant wide patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
