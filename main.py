f99c41e9d6995c17b9717d8fcb198edc272db9c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:25 +0000

    ago sorry turnoff order store

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
