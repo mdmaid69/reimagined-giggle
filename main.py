918bf785a9078ee0aebc2db8e89bd6fc34f582e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:33:11 +0000

    minimum bound wave attempt aim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
