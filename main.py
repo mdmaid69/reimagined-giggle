2df6daa5f22781af9b7d68c021f21391453edf93
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:47:44 +0000

    round name attitude share refuse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def clear_array(array):
+        array *= 0
