c51e9e1de308ac754e8ecbc0c790b8f5b65e50be
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:25:32 +0000

    relation judge Zz pity pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_bool(array):
+        return bool(array)
