8e3bd72a90069a19f47dcc2e31931c5c3e5049f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:45 +0000

    belong stir base blow smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
