372e053b1a1a06ba59b9b82b71aa20889513ce0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:19:43 +0000

    shame identify shop phone land

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
