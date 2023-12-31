1e084a9d057c377d0c1e60ae39c2d8e8a38a9c47
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:37:50 +0000

    fix excuse indeed kick amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
