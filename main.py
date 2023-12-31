6e11dd87feb9e26923e981aca02c408d09cab6f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:17:57 +0000

    fill that bringforward love responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
