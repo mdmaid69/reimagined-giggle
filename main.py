dce3a5c5b4f32ad64c1942dd4ac9d469b8f4a770
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:15:11 +0000

    ring pool success out constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
