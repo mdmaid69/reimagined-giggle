1530baac7b13d7695c3b6e28f578d187014a1843
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:38 +0000

    level giveout vote contact duty

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_error_function(x):
+        return math.erf(x)
