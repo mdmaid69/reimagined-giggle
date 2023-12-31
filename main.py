8231b84a1efb43dc4199e165fbe6800e99bb50ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:55 +0000

    hope poison Ww rent bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_length(array):
+        return len(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
