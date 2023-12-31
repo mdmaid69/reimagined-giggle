b1bc2df9044b157b6160315ad7f69d5b0105fbfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:14:48 +0000

    put record let conclusion ever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def count_elements(lst):
+        return len(lst)
