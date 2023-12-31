dc56f2012c14f093a0eb36cd26204e18bfffd4d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:52:34 +0000

    office suggestion detail Vv only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
