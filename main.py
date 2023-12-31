8d2de36c63d11022a4858eabd89ef44ad8908a60
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:17:13 +0000

    find even image instrument story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
