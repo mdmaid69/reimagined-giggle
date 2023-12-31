720d00c6649acead185a95e253fd928607298ff4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:05 +0000

    wait carry giveout press space

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def reverse_string(s):
+        return s[::-1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
