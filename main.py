93cdf329ec35fcc370cddb920cd3e15c989ca985
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:08 +0000

    in print replace slowly law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
