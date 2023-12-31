6db27d95c6514794ad4e11ac1ebe8b9a0a9071f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:56 +0000

    so pick left container now

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 numbers = [1, 2, 3, 4, 5]
 print("Sum:", sum(numbers))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
