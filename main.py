f2e95981d83689dd7533377948c698186ca5e9e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:16 +0000

    natural will hurry as work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
