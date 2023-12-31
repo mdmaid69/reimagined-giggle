2e157ce652f9c6c7baca32006d2270d3540ed712
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:12 +0000

    yesterday address point public divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
