b547ad76c51861b17ff71c319c4c65dc3e4c284c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:07 +0000

    advanced blame brush hold else

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
