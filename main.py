44f7e1bac750baf65440d7e044c8cc069ae7b091
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:36:54 +0000

    town 567enemy so lower throughout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
