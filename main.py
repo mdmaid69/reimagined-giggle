6c9b59b325241ae9f05b006ab1c76fc395c05847
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:46:57 +0000

    suit impatient press smile material

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
