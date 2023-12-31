47465364f39382aff5326e5d8976f8e946eacd5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:00 +0000

    entrance bin dance ring uponon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
