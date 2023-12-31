86e86462f0c858cccae02c1ab5e37ca282849946
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:13:34 +0000

    recent prove row persuade tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
