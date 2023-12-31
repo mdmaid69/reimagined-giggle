a823714860fc2783c7b089c4077bce0c2e8c091b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:05:45 +0000

    branch scene forward want poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
