1ce382f20577f863e2f7b917284efdbdb3fc9e1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:07:06 +0000

    wherever colour mood shout typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
+import array
+def get_array_as_repr(array):
+        return repr(array)
