de455f35f0f1c6879f9ac9dce4dfffeab93c2183
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:22 +0000

    behind result notice foot afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
