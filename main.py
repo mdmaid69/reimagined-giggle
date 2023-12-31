b1cd4b7601527ddbc7371c461d5f1a703271ba12
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:41 +0000

    scale hope listenfor about ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
