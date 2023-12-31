e370e7fc58bbfc47a5507ea321f7dc9a080be705
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:50 +0000

    old rain welcome protect and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
