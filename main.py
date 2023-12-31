22e390190114cc1aa0cc43af57b822a28b08f251
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:15 +0000

    fork snow future avoid foreign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
