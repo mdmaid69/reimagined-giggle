c3610d56ba569680b49e1ac81cb9a8eaa38f65c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:55 +0000

    car danger dish report bang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
