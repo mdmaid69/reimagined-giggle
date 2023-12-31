baf014ff5d095f0d6c17b00ba10b29264a5e42de
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:40:09 +0000

    challenge nurse operation boat fear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
