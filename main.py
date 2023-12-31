e490c0e2f8c55bd538e71d389a95bc61e871c92c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:09 +0000

    list either by describe behind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
