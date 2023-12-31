38d7ea0dc20deee4e47aea285db87fc6a4e0f0ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:33:33 +0000

    570enjoyable mysterious practice talk open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
