b19419bab90d2878c441012a9322892668e9ae72
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:02:41 +0000

    bus contents choose early perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
