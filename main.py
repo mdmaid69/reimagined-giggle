afd2d106c34b07a6dcb860a66d57997b51ca47c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:55:05 +0000

    base available anxious receive bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def reverse_list(lst):
+        return lst[::-1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
