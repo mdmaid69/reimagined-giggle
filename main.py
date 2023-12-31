169b43e52d79ba7ec2ad51b0c9cb4354df4f1a16
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:23:40 +0000

    female talkinto excited failure children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_item(array, i):
+        return array[i]
+def reverse_list(lst):
+        return lst[::-1]
