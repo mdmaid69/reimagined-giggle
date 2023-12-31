d1196cb098d6c16e6593909d84751a4be07e97cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:12:54 +0000

    lesson conclusion mention rely plain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
