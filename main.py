e74bfa21b83d50b4398411278f9ccc0876cea19f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:16 +0000

    broken bet above calmly usual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-print(os.getcwd())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
