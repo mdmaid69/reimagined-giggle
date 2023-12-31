a6da4d3919aeb81205b138c4f395aea954f687cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:59:57 +0000

    spoil receive lay press wing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
