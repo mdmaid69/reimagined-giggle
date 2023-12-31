6b7b72c5bccff7d414156198b775398d65795ef0
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:24:07 +0000

    close content tragedy invite beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_username():
+        return getpass.getuser()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
