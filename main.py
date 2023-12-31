4e8dfb59bccb629a95b98349ffd29926228f5caa
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:42:04 +0000

    fork profit dangerous when protect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
