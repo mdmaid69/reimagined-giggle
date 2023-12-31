8105d531a9b7d9097f7bf90f83e126f3775e841b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:20:41 +0000

    substance order guilty free some

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
