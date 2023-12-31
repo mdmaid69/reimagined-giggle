5d686f5e51fc0e054db5feff1837b0f641327472
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:06 +0000

    defeat remark determined account national

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
