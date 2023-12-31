73601151c1ea06a7ea178274757425ed40387f01
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:19:14 +0000

    copy area break cut computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
