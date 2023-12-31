9cfa11965427b918d135aec5cd5b73baf410920e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:32:23 +0000

    chief coat poison theirs up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
