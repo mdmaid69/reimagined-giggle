837736ec2eeac27111d3b8a03543abe5bbe75343
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:57:00 +0000

    combination here fun frame example

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
