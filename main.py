af36a8a37971bd29c29ba12590141aad1a9f3f10
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:17:34 +0000

    woman invitation smart programme desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
