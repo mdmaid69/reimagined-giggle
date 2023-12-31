a85275ea0cfb32291cf82b59a84e1b1d2f11e412
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:54 +0000

    gate bored plenty calloff everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
