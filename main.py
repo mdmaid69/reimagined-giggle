f9fc3d32948c9c563fdbc978ef2435b22c33904c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:53 +0000

    solve pick perfect shrink lead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
