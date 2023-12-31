ce35c40592ce3cf60ca1399e00e639fd8ae8aeb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:51:54 +0000

    justice benefit calm bar exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
 import csv
 def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
