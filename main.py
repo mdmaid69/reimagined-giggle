856dcf6be1b8575f2d9df0b53e56ef72d782dda1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:07 +0000

    free bet rich Jj object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
