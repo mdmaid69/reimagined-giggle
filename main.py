d5eadc0d1716a129a095a68da4c4e86aa288e17b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:18:39 +0000

    blow delight impatient feeling behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
