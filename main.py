55b806445c9ca11bf1a404c7b2c2f7383f47cb4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:37 +0000

    sad poison yours floor straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
