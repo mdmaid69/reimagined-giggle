c2467fcf114f947ca01d55a9cabc4808d0312d91
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:46:12 +0000

    letter disagree dream angry this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def print_python_version():
+        print(sys.version)
