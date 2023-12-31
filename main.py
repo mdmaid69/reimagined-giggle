28ebe482a084250a924db777aa1f4dbee043eeef
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:37:54 +0000

    course fresh bringup goback(to) picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
