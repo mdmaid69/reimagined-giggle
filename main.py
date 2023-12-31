c1e570a113d019567343d79585a8761e47c6f665
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:24:31 +0000

    dry tap prevent carry unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
