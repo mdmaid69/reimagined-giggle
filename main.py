aba8e68e15d3e48ceec4366a6d74c9cb8d70b501
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:57 +0000

    over divide very goback(to) keen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import os
-print(os.getcwd())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import random
+def roll_die():
+        return random.randint(1, 6)
