caf73f0b5986f41e070631f22c3e8d9fac4d8823
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:21 +0000

    mark niggaacademy come include entrance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
