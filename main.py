52d5dd8a92dd5f3409ccf5086ec572b305ece17a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:46:38 +0000

    occur avoid accidentally choice offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
