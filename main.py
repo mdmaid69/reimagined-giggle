6d7a40e063bb857441ff7840ea89ca448de1795f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:13 +0000

    pencil bin lie over past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
