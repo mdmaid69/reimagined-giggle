f86620c909fe519f843a83b5a78900367cd5d425
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:39 +0000

    official tap shrink speech pity

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
