edbdc0a0c900b5dc1862e90efadbe8d3bf9072e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:19 +0000

    long hope abroad upset relax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def is_odd(n):
+        return n % 2 != 0
