abde60f7e926960e9064637f9db1200f46957f92
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:19 +0000

    office active edge nonsense tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
