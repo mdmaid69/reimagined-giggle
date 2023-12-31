df0c0483d28d60e40f54cf1b7ab0bec84127c9e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:40 +0000

    steep past minority advice expect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
