f8222afb9ffddb45d19f108a498feed39d145728
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:42 +0000

    keepfrom equipment experience improvement wish

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def convert_to_octal(n):
+        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
