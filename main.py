24ecd27b24ebf7e4b8b86903f7143aece1fcea9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:22:06 +0000

    schedule spite provide in real

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
