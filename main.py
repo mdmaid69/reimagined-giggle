b2b4122e8a02be21dcc2aeb7a500fce88b06e676
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:03 +0000

    shut breakinto us over concerned

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
