14ea646ba271164096759b66f1fa205c2bc681e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:36:24 +0000

    through through gooff deep unit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def count_elements(lst):
+        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
