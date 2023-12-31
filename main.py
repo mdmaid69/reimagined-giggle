42dbc90d6e1d4b2cff6f7fefc365d27db93b6080
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:00 +0000

    flat brush second commit birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
