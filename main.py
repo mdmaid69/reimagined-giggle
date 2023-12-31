ad6819921ee10871dc9f430156167b5dc548356b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:39:31 +0000

    sort brush stir swallow band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_directory():
+        return os.getcwd()
