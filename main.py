2b95e37a44f6090551726cb20b2763ae8f282478
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:19:58 +0000

    treatment march test miss wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
