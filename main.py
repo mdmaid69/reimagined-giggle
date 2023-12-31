35f3dcdc582b4db274017e9892261a4874bea14e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:09 +0000

    note press interfere straight Rr

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
