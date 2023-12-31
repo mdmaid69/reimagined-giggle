717e0f323fa1272d64ecc732d2f4d14ad7d97bef
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:52:10 +0000

    contrast aim character improve nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
