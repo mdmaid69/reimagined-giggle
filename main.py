cb4e8ad98c95b141a207ee855ddd258a672e642b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:57:40 +0000

    dance size punishment they love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
