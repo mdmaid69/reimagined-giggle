b35d4bdab3e8991bbb3de79beea4dd8f736d9e3d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:49:09 +0000

    debt intelligent crowded hit distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def create_queue():
-        return collections.deque()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
