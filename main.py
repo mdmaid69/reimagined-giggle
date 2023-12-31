4d1a309f1b66f5f2b6025fd4715b107ef4459e4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:40:29 +0000

    platform collection distance tired yet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
