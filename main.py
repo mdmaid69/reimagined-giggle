afc96489db314d25d70374ab04cdc47e168f4b98
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:37:18 +0000

    list slow deliberately swim Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
