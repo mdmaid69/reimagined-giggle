64274b2e963bf6e38ab3b2358378f1d73fe6b3a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:31 +0000

    loose intend harm best lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
