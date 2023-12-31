a6a3fec7741189c6460269e9740519f9725c1318
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:06 +0000

    plant someone argument charge Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
