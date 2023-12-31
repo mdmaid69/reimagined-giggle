e09ad4804df5975559887a89e57dd0cdeb7210bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:02 +0000

    settle calmly border sort future

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+print([x**2 for x in range(10)])
