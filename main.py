400b7e5185bac26da1e0c3054caa19ba64a8636a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:03 +0000

    gradually pocket immediate system necessary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+print(sys.version)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
