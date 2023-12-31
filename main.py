36867bf7a36aece8dc140efb472f62203cbbd496
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:18:47 +0000

    proper someone whisper so commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
