20c1b4cfd9def6863414b6049261b88a7e53b111
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:32:52 +0000

    nervous balance plan inform lot

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
+import numpy as np
+print(np.array([1, 2, 3]))
