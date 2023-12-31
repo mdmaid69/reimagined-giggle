f0a408f1f2d7310216bb85a38be4fc0c5fab78dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:46:50 +0000

    claim positive explanation invite burn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def multiply_numbers(x, y):
+        return x * y
