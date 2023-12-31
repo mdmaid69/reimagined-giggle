d37b97c4913f935a17916d0a03cf3c827247462e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:49:43 +0000

    into unlucky front dry interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
