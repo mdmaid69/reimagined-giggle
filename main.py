73d9fa58c4ad75ad115a7154842edd1ca2a716ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:58:58 +0000

    memory powerful zero church breakdown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+for i in range(10): print(i)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
