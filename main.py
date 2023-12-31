f0ed9f35ebda18d01838f235de618ec71d38f354
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:46:14 +0000

    open hold ago hit chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
