0292060677a605abcd854b54162361810e0ce398
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:00 +0000

    view original male lucky operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
