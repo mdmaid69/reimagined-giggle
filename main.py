7a88bf1603978127a1e11270fde876273d6238bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:23 +0000

    compare oil needle without fork

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
+def find_min(numbers):
+        return min(numbers)
