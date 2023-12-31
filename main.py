3b5d0971b878b01c1d89709f1d78c19f0f4ad013
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:37:40 +0000

    today chance return certain rob

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
