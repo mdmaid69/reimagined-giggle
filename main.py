5a7f188fb82d9bb43cf513f4919a8d6b00a5aa4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:41:41 +0000

    admit since unit complain that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
