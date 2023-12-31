fcbf6e81cd7ccb9a0682fa919c61f5c0336c419d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:02 +0000

    perform classroom getaway neck difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
