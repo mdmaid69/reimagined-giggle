1d0a26d22af7c14ceccc248aeead4c35b661c4f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:55 +0000

    minimum traffic contrast strike handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
