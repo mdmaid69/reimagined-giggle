c007b1311df000bc419c5984acb851df2d573c62
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:21 +0000

    round natural stone noisy deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
