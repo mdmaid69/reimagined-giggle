3db37cd757bb2e4937d2d0322eac9b672ea49f98
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:38:34 +0000

    instruction effect again loss substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
