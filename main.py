d3fb612f1aadef3ae802fc24dd109f3427851554
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:30 +0000

    ask tomorrow eager city afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
