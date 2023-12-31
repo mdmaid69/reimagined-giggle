770bdc89540844b9681eeb2dad2755495646c45f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:21 +0000

    564encourage lookup point but thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
