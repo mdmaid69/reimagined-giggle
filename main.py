83dd20116de0d6621bfe89dd5a270be01f545261
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:10 +0000

    independent present Zz sell away

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
