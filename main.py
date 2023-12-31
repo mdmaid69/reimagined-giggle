5c646035e133d488e4a59bfa8b72201aa9e0be98
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:00:49 +0000

    blame judge character only smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
