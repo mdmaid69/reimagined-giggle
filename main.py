414166858fc08fca2bd916c1556ba0724475df29
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:22:01 +0000

    see 564encourage embarrassed interfere trap

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
