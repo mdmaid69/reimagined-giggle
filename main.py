d90d5b531e8875f18bf6cbe98a0112d03c237dcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:02 +0000

    direction unable shake replace wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
