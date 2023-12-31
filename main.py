4eee1e17cfed0872b2274c7cf83717d94e7d4cd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:30:58 +0000

    plan and confusing mend class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
