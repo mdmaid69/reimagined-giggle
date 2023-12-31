c49e2f93ad393c85a3398531f25f12aab0aa6ad7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:14:59 +0000

    peaceful lead chief board burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
