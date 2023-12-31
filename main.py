dda004b877ab748978cb25aa0aeb0c52f252cbb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:56:31 +0000

    design except line wild escape

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
