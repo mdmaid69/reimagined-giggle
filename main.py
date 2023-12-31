3ac667220a718cb200752fdfeec9b2ef1bdfb75f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:38 +0000

    nigga old against complicated straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
