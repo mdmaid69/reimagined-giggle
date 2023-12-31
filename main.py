4d4d3935019cbaab74f64c9c9b3072774d55ed58
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:10:29 +0000

    relief doubt talkoutof cheek survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
+import getpass
+def get_username():
+        return getpass.getuser()
