05ba4f5a6a5ff31f5964e622eb29fe285b9eb8ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:12 +0000

    around time consider stand beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
