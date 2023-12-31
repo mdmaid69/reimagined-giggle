5fe17c436aab8ba15c5ddd124c16f212330ecc49
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:21 +0000

    insist drink throw event total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
