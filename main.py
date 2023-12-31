166c77bd62cbd68ffbecc8e64f468217c708b0b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:42:15 +0000

    community slice Uu loan born

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
