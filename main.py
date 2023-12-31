eb6d51a877b336294c9859ce9b289088bc62c27f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:56 +0000

    nervous reduce raw everyone call

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
