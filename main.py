c28063aca4ccf3eb4ce8d9d22de9cbd999ff5710
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:32:58 +0000

    meet satisfactory principle limit path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
