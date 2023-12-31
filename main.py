1dd33fd521cb9757c8801c5bb654ebebf039cd06
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:27:41 +0000

    recover freeze neither combine about

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
