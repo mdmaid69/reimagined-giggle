5eb57acb956f051df7471a62df20f4322c5587cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:23 +0000

    concerned throughout convincing never repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
