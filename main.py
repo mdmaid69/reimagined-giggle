8eb66352c299b2d4ad2b81d2302c411ac432c971
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:14 +0000

    kind shrink everywhere belong style

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_max(lst):
+        return max(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
