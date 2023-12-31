56aa5d85c6b26d0463cc9175c1ec6766cb876861
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:31 +0000

    forget prove move excuse almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
