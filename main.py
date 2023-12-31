56038358f804ffd10b3ba98cfb8e6f896176bca2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:38 +0000

    innocent ours fun return deep

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print(sum(range(10)))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
