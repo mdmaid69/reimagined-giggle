75037633c6d99afc7f3415b64ee6115e4a02bc36
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:36:20 +0000

    offend hello rain turnback lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
