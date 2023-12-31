3e8181a1a5d96a7f7cdf9441514c75c441503151
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:38:06 +0000

    result adjective same talkoutof left

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
