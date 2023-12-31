5ea6445830584338574512de091c79b549550a1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:21 +0000

    worried dance church shoot 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
