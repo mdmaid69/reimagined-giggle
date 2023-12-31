daa19151aa885a7f27d81c2e89dc4573288621f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:20:08 +0000

    everything flow inside unusual sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
