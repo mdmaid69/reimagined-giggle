f70943302b5fa79d272551f73a45f3702453a4fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:23 +0000

    mysterious mistake say information catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
