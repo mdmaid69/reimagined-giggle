de8990d46de9435218da570da9a35fe56825957e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:50:53 +0000

    lead equal journey thing apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
