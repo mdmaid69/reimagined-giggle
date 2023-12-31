3873d95320a834de9a5862ec9c47185fe8b46953
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:40:24 +0000

    fat Yy estimate minority either

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
