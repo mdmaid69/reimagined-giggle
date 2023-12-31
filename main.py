6d93e66bb98aabf62d2d92f6863fc5bf3a58f140
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:17 +0000

    fine cook choose rest statement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
