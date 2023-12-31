9e717a10bda3ef70967fdfe52499493a5dd66586
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:45:44 +0000

    shop favour vanish trap storm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
+import os
+print(os.getcwd())
