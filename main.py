08bdb469f524f90c49aad5974f0cd718ead7fbc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:53 +0000

    far sore expression needle squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
