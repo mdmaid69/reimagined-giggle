5617e3a4b8a60792b4178e8019af443ed467b30d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:14:13 +0000

    serve Bb pause ignore coffee

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_unique_words(sentence):
+        return set(sentence.split())
