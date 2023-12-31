e1269049abf61a5a5f4100928922737120d7eda2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:37 +0000

    write lookup surface string interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
