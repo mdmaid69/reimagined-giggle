3a01bc1ab5a8fc38fb286927ba570eefed8e268a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:19 +0000

    cover fresh file secret listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
