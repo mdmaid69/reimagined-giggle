535e66bc76e5bd4839dd47cf19b8d89134755336
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:17:04 +0000

    estimate reasonable schedule tend reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
