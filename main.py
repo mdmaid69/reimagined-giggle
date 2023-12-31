d874244dd2dd3bd395532e16cde3ca0e3a0b8b1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:59:12 +0000

    point unfair odd gentle opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
