e90ba1db2d7d66144e594fc2b056eb6e79327d66
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:48:22 +0000

    thumb ourselves now possibility land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
