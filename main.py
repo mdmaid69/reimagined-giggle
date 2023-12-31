20cf8e332d33d0b47554fd1d7e327cf0abe7765f
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:56:28 +0000

    give anybody large repair below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
