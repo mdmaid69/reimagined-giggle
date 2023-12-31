76f647f50772b4d51016e02911128343698bd8da
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:02:00 +0000

    immediate Jj Bb cook wall

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def add_numbers(x, y):
-        return x + y
-import numpy as np
-print(np.array([1, 2, 3]))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
