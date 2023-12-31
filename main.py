ccfe53940bbc2c01eb60d993bfce5a3a18b69e3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:10 +0000

    secret joke air almost popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
