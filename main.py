4f2c029da91f739ce81d5c994df75d502a7c9bf7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:41 +0000

    hospital whether only wake pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
