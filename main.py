656173a3a82e27b98de2d3072a746f0c39c2e2a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:56:35 +0000

    unite number considering convenient each

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_bool(array):
+        return bool(array)
