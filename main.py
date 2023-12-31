bb0c6e90d1facad5371493d727917bda17c58c26
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:25 +0000

    report afraid firm pencil squash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
