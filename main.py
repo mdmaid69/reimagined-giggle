b667a4555b98b71782113bc9700104afb2f357d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:49 +0000

    marry case lend sell giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
