0764f3e529bcb71f8c58313293ef8cb7476322e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:53:16 +0000

    drop correction exam exist patient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
