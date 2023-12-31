3c55c9bbbacb6a84c2da258def2a297b5af465a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:36:07 +0000

    interesting mark fairly pot pleasure

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
