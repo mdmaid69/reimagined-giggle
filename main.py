16b45848feab0c2d388cc116378c6c1a1c342a0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:03:41 +0000

    firm attitude mainly what comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
