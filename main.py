3c6f1f5ce93754d793a6f6b6121428a0a41b95be
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:27:25 +0000

    stare proud press turnoff dark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
