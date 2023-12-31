f67b9048e940f703f5562c4063fbd0681f67551e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:33 +0000

    defend wind extraordinary sign ever

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
