00cd3fee53c2e5fb104f990eed01b468e9c61676
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:37:17 +0000

    safe no violence club another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
