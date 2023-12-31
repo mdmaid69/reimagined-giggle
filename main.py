c75a522538632f7c60e925b7eb3c8ce2b0ef9c9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:52:41 +0000

    draw sticky thick congratulations Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
