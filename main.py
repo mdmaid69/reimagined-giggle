dde92ae1663accc353a28be51d9ebacf8072eb09
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:26 +0000

    divide please distance Cc scale

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
