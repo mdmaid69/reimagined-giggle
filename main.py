9edfdf81a1f02cab7a54e294c55f56e26ab89411
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:56:29 +0000

    either disappointed refuse custom will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
