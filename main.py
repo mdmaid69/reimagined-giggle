2b38b7eeb4eea63009aca2acfd5903e468c801e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:21 +0000

    praise shopping describe letter thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
