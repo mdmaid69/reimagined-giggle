c3646044eb9df3f16d5ef0bf66846da2ac894956
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:25:51 +0000

    feed switch godown spoil job

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_list_from_array(array):
+        return array.tolist()
