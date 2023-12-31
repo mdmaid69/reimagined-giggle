6fdab7c3ec47e02ec9857b28ac7d1a24348734a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:56:38 +0000

    match interruption service pay knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def find_min(lst):
-        return min(lst)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
