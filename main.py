0f9d3f841ffef1c5474c26c5ce3fdbb9eb25b3c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:28:17 +0000

    over however minority ignore angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
