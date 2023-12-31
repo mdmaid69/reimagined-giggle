67f983173f6d056fde64d92d43b3e5c991a9753e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:07:55 +0000

    exchange count rest geton correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_current_working_directory():
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
         return os.getcwd()
