e9cff246db378937c7bfe9805d42d7bcb7e54c5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:05:41 +0000

    hope tread aim pretend swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
