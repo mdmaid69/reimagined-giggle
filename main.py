658803b9339f115e422a1393111487b32518813a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:47 +0000

    suit put join board pleasure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
