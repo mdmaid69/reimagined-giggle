3042dd5dd54d9a8e6ef9a1ebacff7f21b2981fa8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:50:40 +0000

    careful bored label want related

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
