5ac109d42476fb7136c58459328628e0683a1523
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:07:23 +0000

    Dd much workon several wave

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
