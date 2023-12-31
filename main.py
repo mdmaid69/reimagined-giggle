8f63517357ec64a97d96ee1a6021e1709dfdf4f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:40 +0000

    available upwards interview Ll human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
