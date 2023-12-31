2bf21e4ce72db5675f24d7bdc24b7d4accce5e3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:45 +0000

    object Ee everybody storm taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
