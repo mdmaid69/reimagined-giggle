c986b93f40500ae13127a37ad1f01425aa891bb8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:07 +0000

    finish obtain there person reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
