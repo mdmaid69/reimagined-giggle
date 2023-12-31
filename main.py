522b767a4ecf195cacc9fcf2af1b6872648628e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:52:03 +0000

    pleasant promise everything its contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
