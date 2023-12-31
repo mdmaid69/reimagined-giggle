836374ddcf11b773a496b33ca283e38fe4bc49b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:49:49 +0000

    plan risk plastic branch fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
 import heapq
 def push_pop_heap(heap, item):
         return heapq.heappushpop(heap, item)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
