33688bbc0e2dcd596d0ecbf7d66fc7f7675442f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:12 +0000

    myself positive trouble terrible minimum

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def parse_arguments():
         parser.add_argument("--input")
         parser.add_argument("--output")
         return parser.parse_args()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
