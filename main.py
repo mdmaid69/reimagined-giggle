01eb81a1091aaba86437d3fe5f00c5e83b07adcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:19:54 +0000

    uponon left detail confusing fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
