12f489cc17f23a6a745e600c1f379062132132b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:52 +0000

    nervous bad after interfere fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
