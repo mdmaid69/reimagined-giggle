661782b34d4a7938af033fb2983bbaccd2512f49
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:42:17 +0000

    explanation breathe public ever he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
