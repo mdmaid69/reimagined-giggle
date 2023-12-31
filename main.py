026fbed68290fbe08b9b15bc06184e1bfc3f0077
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:36 +0000

    foot content art direct judge

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
