f692c9b54869af0f37be0981dcb7271804a718e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:35:59 +0000

    fashion idea tragedy fence listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
