80744e90601341bcfab450873ea2f72ca63cfd99
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:17:17 +0000

    children lookafter yours pain whisper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
