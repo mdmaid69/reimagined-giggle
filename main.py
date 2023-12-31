a13c6b498958c9ae2278e9dee905830596aae876
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:10:57 +0000

    statement hospital along I hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
