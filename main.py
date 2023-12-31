15045fb6b8dabb96df21778066864e55b24b35c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:01:49 +0000

    foreign mistake meaning relation increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+print("Squared:", [n**2 for n in numbers])
