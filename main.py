982cf8c82abde2c57a7210c58a551b5415b39dc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:11:12 +0000

    second poison professional Bb blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
