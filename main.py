2c03ce0cffa6066fc9f9eef746330026903685c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:57 +0000

    light anywayanyhow especially wish anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-name = "Python"
-print("Hello,", name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
