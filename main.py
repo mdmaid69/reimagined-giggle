58a62c15d3e1c8e184f2e3dda2f274abb3117500
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:54 +0000

    unit goon takeover female unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import re
 def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
