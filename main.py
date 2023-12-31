08f932b44760eee1cf46689bcb81374c5e75beff
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:44:27 +0000

    ago afraid turnoff class shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
