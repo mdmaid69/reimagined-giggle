bccd379b083556fe0bb3252d9ca7d64449130b55
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:49 +0000

    trousers home cover afraid good

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
