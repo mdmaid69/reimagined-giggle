ab7572ce50a135e38b0c7c31bcf571ec1c8d07c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:47 +0000

    store cover noisy spread improvement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
