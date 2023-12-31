f77962a08df687c1147c8d63663000a9df2efd3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:26 +0000

    rub judge dangerous work dream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+for i in range(5):
+        print(i)
