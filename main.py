205023827d354abd86df5e58b3e3283d9746dfce
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:04:04 +0000

    type handle branch broad news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-  def multiply_numbers(x, y):
-        return x * y
+import sys
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
