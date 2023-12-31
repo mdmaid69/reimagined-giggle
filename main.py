7361522fdedd16e872014872c5ba2a35585559f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:58:17 +0000

    or total makeup concern care

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
