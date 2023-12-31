f2ad89a6daa1cac3f9686c809e19a6a399d4c043
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:27:29 +0000

    sign important knock grade poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
