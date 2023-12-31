03a193aeca02963ec64f86cfeb1466c1342f09da
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:57 +0000

    565end fix through language taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
