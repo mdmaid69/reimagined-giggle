9f503dcf0902562045baf42ad0a7cabadb179ce0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:45:39 +0000

    protect proud when muscle steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
