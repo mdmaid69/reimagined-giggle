062c12ddf88225785653551480a3ffe74bc9b45d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:00:42 +0000

    pen yet Pp smooth state

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_index(array, item):
+        return array.index(item)
