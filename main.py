0ece9c2ba0470468a3236dd154db5704ae3ec6b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:12:18 +0000

    contact decision afterwards private fast

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
