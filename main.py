dcf552bc6edf1519c1d2b8bc7478ca212866144b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:17:28 +0000

    ahead turninto failure behind indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
