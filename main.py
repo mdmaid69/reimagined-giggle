afc713c163472e4477b3944576a9379333668a88
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:09 +0000

    healthy bin to system Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
