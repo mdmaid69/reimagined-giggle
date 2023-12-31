54adebad6826d9b112794df1ac4f925a769bc30a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:55 +0000

    getaway base Kk punish loud

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import random
+def roll_die():
+        return random.randint(1, 6)
