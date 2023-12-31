03b57a3fc236d9479d32f142fa31c7c408f04aa3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:14:21 +0000

    store protect natural clothes with

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-print(math.pi)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
