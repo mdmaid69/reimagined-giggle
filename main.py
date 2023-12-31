2993f0bf94883eaebc537f561b20c2f5a74b301b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:38 +0000

    view world settle accidentally force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
