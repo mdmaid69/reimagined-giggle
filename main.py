1ee90da85b4583de8a3cd47d215a8c71260cb817
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:24 +0000

    dish turnaway quantity nervous sail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
