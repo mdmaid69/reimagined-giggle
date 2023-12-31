485c695dba73f55fbeaa4a61821d989dd18c3a4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:25:22 +0000

    shock field godown nurse generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
