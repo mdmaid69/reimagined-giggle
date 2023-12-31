77f78f61f79bdc26a5dd790d150c32147b01cbe7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:56 +0000

    possibly bread abroad copy spoil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
