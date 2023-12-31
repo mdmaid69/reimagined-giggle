53bd1a6b7532488ab5424dc5ae8f54c3c9c6a72c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:05:35 +0000

    design post tip rather anybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def count_elements(lst):
+        return len(lst)
