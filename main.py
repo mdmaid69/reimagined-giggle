f10b3f857fa9bbbeb53299f29b00da548e54b254
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:39:00 +0000

    position foreign pencil wash creature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def remove_duplicates(lst):
+        return list(set(lst))
