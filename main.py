7169a88927fd6bfb5ad1f10b8c4cf00b8dbee1b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:30:53 +0000

    beside form depend smooth in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
