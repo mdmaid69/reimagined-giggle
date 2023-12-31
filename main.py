d878c620d6773f16068d76376ffd20f31d01bf93
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:37:27 +0000

    neat arm remark start impressive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_work(force, distance):
+        return force * distance
