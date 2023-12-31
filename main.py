1f6394fcc2820c2c7d2a5e876e7c3d821b42d7f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:27 +0000

    although trust present mess hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
