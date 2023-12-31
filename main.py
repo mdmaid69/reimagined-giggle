6fbcf492205578dc535a071545c8f8678c3f309f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:15 +0000

    century concentration in gap collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import heapq
 def push_pop_heap(heap, item):
         return heapq.heappushpop(heap, item)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
