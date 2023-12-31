9e53ae9da7dc3b251331dddaebd471dcffa18d96
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:38:28 +0000

    none friend rate quantity later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
