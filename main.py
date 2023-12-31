0237dba6806102904a3d6f03afe71c4b6b9cb52e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:54 +0000

    bush pupil reduce float grow

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_power(work, time):
+        return work / time
