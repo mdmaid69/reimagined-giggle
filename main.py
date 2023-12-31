cbd6d4ed559302d8940ef42799f48b8edbc34b57
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:01 +0000

    listenfor cross dance natural money

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import sys
+def exit_program():
+        sys.exit()
