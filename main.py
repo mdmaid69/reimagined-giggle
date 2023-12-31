0fbfcf0176dd09682314d307187126c6ca315a25
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:17:46 +0000

    brush tape record reason wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
