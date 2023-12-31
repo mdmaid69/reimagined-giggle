5075c57585bdf95d4e5c578ed36a2fab788a5508
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:38:17 +0000

    part succeed sense important waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
