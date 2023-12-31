20f7eb97f8c854ab2f75dae7e523dc6ec68abd25
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:18 +0000

    neck secret exciting terrible movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
