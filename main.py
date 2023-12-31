eb12a1dffd08330947ab811f57c7281fd741791e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:54 +0000

    age harm body goback(to) set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def exit_program():
-        sys.exit()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
