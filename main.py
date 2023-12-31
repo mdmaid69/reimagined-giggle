b5b979ce1c3469bd11b2679f4af031e7170d8393
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:05:39 +0000

    will tour march return real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_array_buffer_info(array):
+        return array.buffer_info()
