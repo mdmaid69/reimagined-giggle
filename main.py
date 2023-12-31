b9b3056ffb364319c4685bb110fd00ebd0ab0e84
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:42 +0000

    heart how turn of contrast

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
