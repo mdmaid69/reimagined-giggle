2b856c62aeee6953b53d5cc767bc78be830dcf98
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:54 +0000

    arm return introduce avoid cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
