7b9b09bef914e2873b63a7a3ea8b90cac6e407ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:38:00 +0000

    traffic neither major since there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+print(math.pi)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
