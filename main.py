1c2e15c2c4108da8e8809e0437ce81102dd48b81
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:33:49 +0000

    able rush fear set prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
