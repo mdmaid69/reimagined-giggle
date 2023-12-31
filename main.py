f3b7c07e03a0afcb25643cfc9c0c897130ee6cc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:52:04 +0000

    sudden intend tough hollow callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
