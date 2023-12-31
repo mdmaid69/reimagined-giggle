b108eeb1a2fe00f8e7b771dac2f0977689f2337c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:25:22 +0000

    here opportunity excellent look it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
