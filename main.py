8350429f15274c18d3d91e2dfb3693ef994f0434
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:36:53 +0000

    seriously estimate join race calmly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-print("Hello, world!")
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
