5e0749fda4e41e16e7a567b0dfa6b1e0d700d434
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:41 +0000

    rain play manner first large

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
