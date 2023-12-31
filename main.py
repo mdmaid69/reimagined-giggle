8aa5b494cfd8ee1fdadc6b8a99f64901a016b9a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:00 +0000

    gradually address just flat food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
