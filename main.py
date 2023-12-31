cc4603defb115d88f65e2fd0d51674c36b54889e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:49:51 +0000

    bush drop nice ashamed board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_index(array, item):
+        return array.index(item)
