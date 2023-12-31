c0809a37de863061a23066d5dc428f08488d8dbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:24:34 +0000

    job connected serious nice danger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import re
-print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
