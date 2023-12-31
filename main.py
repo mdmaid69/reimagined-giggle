2e1798c5632a90923450eea7ef71b7ed1f77f6bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:29:39 +0000

    stamp fly food hold hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
