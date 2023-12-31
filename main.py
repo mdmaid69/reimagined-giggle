1228c2d4c50ee00aab321e537c9ae571883f9daf
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:26:10 +0000

    thirsty house attention that cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
