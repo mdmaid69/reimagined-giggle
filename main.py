71fabbeefc69a4324f367831c4456b8904439088
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:53 +0000

    runinto Oo equal exaggerate concentration

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
