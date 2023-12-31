d934fdb2a826a2a3509c76ca9150170440755ade
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:33 +0000

    direction personal most 569enjoy attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
