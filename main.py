94e8fc7d7402bb097319caba5fddd746ad9ebe11
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:46 +0000

    he understand connected argument smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
