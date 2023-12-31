b1ab9cca47de71281ab74252fad8bddd00fabcc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:42 +0000

    peace lead freeze moon gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
