2d2164888a4d55ee8bfa44b90294827fc624e3e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:48:42 +0000

    its pin exam wrong out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
