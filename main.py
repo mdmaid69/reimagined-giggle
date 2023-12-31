687bf220f9c1dfc3425a20ccf7302fb26fa1037c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:56:51 +0000

    treat either cheat search bang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def remove_duplicates(lst):
+        return list(set(lst))
