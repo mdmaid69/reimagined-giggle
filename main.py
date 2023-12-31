b5f7546705c22c9ba0ba30a0f92cec9b044bd1c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:23 +0000

    really software offend tonight wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
