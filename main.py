eaa979c7d88d5b42099dde0496777f793fdbb314
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:14:59 +0000

    kill mess gather hurt plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+for i in range(5):
+        print(i)
