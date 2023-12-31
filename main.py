de28aa489d76c0f48487ae290bac6d2eae54e6ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:49 +0000

    experience precise want brush base

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
