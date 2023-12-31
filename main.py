ae71ad4ed48fa432ff877abe4ca6a75206712211
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:37 +0000

    basis wind storm nurse stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
