f20c52083b43a46d8f0deda33b8e8cc2cc799fa5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:29 +0000

    you smell pour colour matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
