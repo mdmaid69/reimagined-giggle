6b5eb198676d1c6f3d6bde93b56cdb811e9d39d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:05 +0000

    normal concerned receive oil sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
