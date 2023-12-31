b0aad2a92f01a44e84df627e7d163ce6ecfbbf9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:49:20 +0000

    cum environment turnback complete lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
