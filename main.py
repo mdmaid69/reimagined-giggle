b76411d98e7c7b4673ea027170c9b4fc9b0b9e9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:00:12 +0000

    this female Vv unlikely heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
