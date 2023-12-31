3968f8531a6a475ab7a0041be338521b042158c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:41 +0000

    good flow reasonable straight pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
