4b4e83ed84043157afc87fe1eb08af80425eb7dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:22 +0000

    pool cap contact contain better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
