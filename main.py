e6f53112a650eeee1b4300c02c384ca59b754e94
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:10 +0000

    pile loan bank bunch spend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
