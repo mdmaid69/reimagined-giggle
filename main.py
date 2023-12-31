61f98bf60769e3e254941c0a9e3157e3799dc2d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:27 +0000

    hit workon bored it next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
