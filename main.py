91c30c52cc82c40db0e39f18180c144d16d2efff
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:46:12 +0000

    collect live fork forgive should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def find_max(numbers):
+        return max(numbers)
