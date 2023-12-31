50edb09cf8250b4b7d596da25125e96075bacd05
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:29 +0000

    to temper frighten pool visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_speed(distance, time):
+        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
