d5bb96d8186acb7969c13282f80771a94ed1a112
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:04 +0000

    switch noone deep clue beside

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
