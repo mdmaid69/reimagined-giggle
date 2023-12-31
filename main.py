f5f3e72ebd3f5f956a9e7b00f1e3bb5f670d8476
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:45:35 +0000

    why flow and sport experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
