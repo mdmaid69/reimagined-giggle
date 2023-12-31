478d0d25d20558a46e799006264105944f13e3e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:19 +0000

    content false money normal deaf

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
