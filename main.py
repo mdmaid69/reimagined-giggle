39feb680f7de4b268eb149487728ce9a619384a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:47:26 +0000

    bet slide slight admire swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_exponential(x):
+        return math.exp(x)
