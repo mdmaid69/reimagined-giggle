1359482656afd36ba35265fc5be9992008b8546f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:32 +0000

    anyone swimming rest motion bored

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import os
+print(os.getcwd())
 import array
-def get_array_typecode(array):
-        return array.typecode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def extend_array(array, iterable):
+        array.extend(iterable)
