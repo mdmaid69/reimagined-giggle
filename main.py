1cce17b8f977d6a1f35e78c9d0bb9d7e4ad556b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:29 +0000

    purpose second exploration climb spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
