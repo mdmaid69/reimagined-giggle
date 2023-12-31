fd73799017396d1a454bad0909e873a5f80a58ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:31:39 +0000

    suck heat professional absolutely through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
