2eb75236511f042620ab28ad14796329ac134c87
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:46:07 +0000

    bill supply trap sound confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-print(datetime.datetime.now())
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_list_from_array(array):
+        return array.tolist()
