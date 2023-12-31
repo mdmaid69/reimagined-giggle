aeedef33cf7f784bee9f55eafecc1d018b97b9d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:45 +0000

    number centimetre sale murder possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
