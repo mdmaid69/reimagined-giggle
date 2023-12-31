fcba9015a6fa0ef574be119f393f22a9d67a3db8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:24:00 +0000

    minority wake spare your direct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
