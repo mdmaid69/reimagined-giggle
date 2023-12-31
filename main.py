c5e808a5dccce43208b25940e6c26a7c73c0660f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:49:04 +0000

    store round visit deserve sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
