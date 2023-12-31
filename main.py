a5747ce1383638c6bdd99b0619659c13cd9443e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:00:37 +0000

    project add to we increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_characters(sentence):
-        return len(sentence)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
