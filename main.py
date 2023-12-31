7b28c002c6babe34d15e134993f2bb7419929e5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:33 +0000

    chin tonight black point sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
