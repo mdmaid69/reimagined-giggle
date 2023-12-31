20329787ac6d15ec2a5f469aecb0aa4704d092d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:29 +0000

    brave lay drive good like

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
