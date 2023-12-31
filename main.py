17463e2fad8c1215e045401dd736244aaef60a15
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:29 +0000

    here organize substance score pure

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
