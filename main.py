b84aa1ab32119d0461d6821291caa44862934c36
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:12 +0000

    during operation tap place hear

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
