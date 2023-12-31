96f79d9c77524ad49eb21e775bee2f1bf967b919
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:34:14 +0000

    slip equipment really after then

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
+import random
+print(random.randint(0, 100))
