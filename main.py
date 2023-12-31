b2506fdadd9ed7853134f87322b93da2ac3b6888
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:09 +0000

    minimum ask wake city miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
