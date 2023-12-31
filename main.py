7b96db5de3d8ff58a8c5a2dc3ec6753ad2137d99
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:17 +0000

    limit warning respect lock air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
