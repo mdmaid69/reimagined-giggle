6c6b45ed7f500e8d9b678c0684d57fc000b5e3a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:20 +0000

    fire piece bus sleep none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
