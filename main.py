5cb4e64c3e9d0bd2bf8eb2e593ee43563b029a98
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:30:37 +0000

    door stick heat switch market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
