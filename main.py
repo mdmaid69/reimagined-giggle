edb6f0e14fae2b4f1d317e5866ea52ed90db589f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:44:38 +0000

    skin education hear 179bite attention

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_work(force, distance):
-        return force * distance
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
