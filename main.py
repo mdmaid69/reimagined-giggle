b4f527208f0d652d2e3183ff2aaeb99d28b41a4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:51 +0000

    depend through taste whistle official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
