be4ca8aaf301a00373aadbadf340a47ed453c8fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:36 +0000

    interview like pause fit announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
