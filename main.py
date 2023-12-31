af029c4c94970978e9673c5dce111ddcd74bb354
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:10:10 +0000

    suggest afraid connection always dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
