fedf95ca770b9d6fd53a2fa7d70f651514ba3e8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:46:10 +0000

    accident takeaway juice lie proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
