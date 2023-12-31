6b608b4228baa1b25ad0b27f64dae65ea39cb7b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:03:16 +0000

    grow fresh spray unhappy easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
