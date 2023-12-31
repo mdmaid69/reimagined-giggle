e06124bb3c779120d66a62a91c74deb56e16a3a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:50:22 +0000

    below clever project dangerous entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
