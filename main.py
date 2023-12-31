0b82ab4e331012074cb38ed0ecbc0c105eacb9b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:57:06 +0000

    age Yy answer nor cut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
