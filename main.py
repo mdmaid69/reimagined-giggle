5a11e4aae7ce3fdc55e50956b7bf16d8750ae9fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:36:44 +0000

    allow whether outside store satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def subtract_numbers(x, y):
+        return x - y
