e5114b8c14134302060cc69ca0ceff11e3f79682
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:36:37 +0000

    thirsty geton out door shrink

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
