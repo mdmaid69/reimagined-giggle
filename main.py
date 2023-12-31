102cc253d0e1c123824c0ac892ee7f7327888eba
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:57 +0000

    taste image hot content from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
