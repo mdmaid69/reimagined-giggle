cddf9a4a64bceae9012df2cdbbac99f16397cf39
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:32:35 +0000

    before check song underneath run

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
