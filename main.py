109b0cd5b9c7a0958bb89b46ef10613c771a4fc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:35 +0000

    colour valuable hide design early

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
