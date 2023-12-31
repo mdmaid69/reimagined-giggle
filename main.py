1132a15b1e5ff3b45fcadb207fcbf86d0b900e7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:53 +0000

    wet quantity declaration open busy

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
