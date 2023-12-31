c91947ebc9e9cf2aab6bafad64823d5118458947
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:32:09 +0000

    view quantity between plenty radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
