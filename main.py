7aacbd366792aa67206f352f012fbdc45f1ad0ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:09:19 +0000

    impression congratulations concerning middle television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
