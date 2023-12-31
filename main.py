0b1e0cc95c207024d33cd77690fc8d7f2ee5eb98
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:30 +0000

    flight central concentration keepout further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 def calculate_eps(net_income, shares_outstanding):
         return net_income / shares_outstanding
