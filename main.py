0d3719dd869af78cec3121817cba72a32e5889f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:07 +0000

    ride plenty treat embarrassing situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
