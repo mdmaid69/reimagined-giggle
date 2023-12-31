94a0cf786f4f7fcabe256d7e8d6af7e8a1e426cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:43 +0000

    level someone press take moment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
