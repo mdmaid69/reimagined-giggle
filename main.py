92b83f04778050594c8e064ac5c17dd6473acfe9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:50:59 +0000

    genuine stamp turn possible practice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
