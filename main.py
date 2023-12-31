940b089892f42a8d435ff6c169a56491c3ab6d72
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:33 +0000

    about though hollow direction person

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
