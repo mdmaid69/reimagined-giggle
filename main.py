8cc92871063253b63e8177a16c98bf2f8002f85b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:30 +0000

    among right unless commit tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
