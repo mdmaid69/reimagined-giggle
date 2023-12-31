54f795bdfc3e31ddcd046fff7d69511a065e5bdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:27:36 +0000

    bright grateful surface steam habit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def greet(name):
+        print(f"Hello, {name}!")
