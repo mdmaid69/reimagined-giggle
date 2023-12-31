d728eed1a0fefe0eb53a1e705e13b26a1fade53e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:38:29 +0000

    fact flow bringback brown comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
