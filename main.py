cb92a141cc370540d4a61180f091f2a39fcc5684
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:32 +0000

    address lonely face force successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
