b0d81ffad79a23db4895f7f0cd89d10b498ead25
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:07:10 +0000

    judge number benefit black far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
