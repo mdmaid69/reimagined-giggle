ca3078bebf7327ec2cd7c2139ff3d6dfef37ff0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:17 +0000

    beside organize visit reach centimetre

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
