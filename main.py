30bcd243164a8f6d0896f3d5445fab27d690e91d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:11:54 +0000

    replace moon passage ignore scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_density(mass, volume):
+        return mass / volume
