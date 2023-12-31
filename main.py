125b9f65df27f8a5562dd0d481f01463224a8370
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:19:47 +0000

    fold dream concern us obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
