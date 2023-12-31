80e8602c8e8df3ef67dd816ae43005a277af2219
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:10 +0000

    turnover but mad snow sky

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
