cca6d996ae8cf683b2130a17afd53dc1d3251ab5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:24:24 +0000

    goback(to) long cover scheme rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
