4efcfaf6295b9d88a75c68c0681682f6278d0cc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:48:10 +0000

    comment string price mind taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
