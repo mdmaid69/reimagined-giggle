4a47e24a9dff1df73357e1f73c5a69eb7e47f15a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:06:19 +0000

    middle tense continue light thorough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def roll_die():
+        return random.randint(1, 6)
