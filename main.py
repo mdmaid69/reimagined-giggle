cc9e2e9a3e23e91171312ddab56def823d0c3db4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:32 +0000

    head unkind uncomfortable conduct born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
