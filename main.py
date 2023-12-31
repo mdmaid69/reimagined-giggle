3d2322d874bc301e0a5b0abe047462d51fe66b0b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:31 +0000

    description either guess reply rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import os
+def change_working_directory(path):
+        os.chdir(path)
