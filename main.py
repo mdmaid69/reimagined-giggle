ef5b269bd6406c827bc42d38a5ead4fce47592a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:24 +0000

    match bury dish value rubbish

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
