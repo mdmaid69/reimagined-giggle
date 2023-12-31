021dd7923953e4a7cce713fdc1bc019436ab060c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:22 +0000

    go out centimetre shrink air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
