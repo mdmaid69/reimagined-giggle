15b7080e0b0333fcf6e7d2a7f7cf1358be8303f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:23:13 +0000

    spoil neither rent thankyou soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
