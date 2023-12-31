bba07d469f51004d25ec95270ad08b4a61e316bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:18:28 +0000

    judge agree shout receive shout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
