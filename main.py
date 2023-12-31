4d5ebca2761d2c6a9c70f89e5414ce690ef28a62
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:59 +0000

    giveup weekend bar brief escape

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import random
+print(random.randint(0, 100))
