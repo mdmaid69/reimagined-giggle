0fe7675eb44632c85aa5fc147b66fa1c03da422d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:43:06 +0000

    giveout offer runinto breakin person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
