9b604e5c3d8e998fca3c29c91312a7003c63ccc8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:32 +0000

    avoid bother Mm who absolutely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_stack():
+        return collections.deque()
