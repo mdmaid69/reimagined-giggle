fd9f26cdfca806b400e546e042d137026fec3592
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:44:34 +0000

    try extremely final expression underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_index(array, item):
+        return array.index(item)
