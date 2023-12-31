d813bd0630a5c0622a3a0773016533d48c77ad77
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:00:11 +0000

    fat clever should fat weigh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+name = "Python"
+print("Hello,", name)
