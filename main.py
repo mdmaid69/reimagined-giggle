395bc520c571d75ab510fd220257d37565bdd202
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:10 +0000

    basis shallow proud instrument calmly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
