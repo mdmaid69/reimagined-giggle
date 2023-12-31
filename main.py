8ebbd32c6ceb165f41eef310caec7869b1dd2cf3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:20 +0000

    suit fall naturally hope disgusting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
