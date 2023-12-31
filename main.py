38122cebe887c6c6d74a2a9d07cc83fa7019f216
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:18:48 +0000

    will kick light cheap after

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_power(work, time):
+        return work / time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
