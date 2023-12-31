601a5f44cedb67b0936aa1217933a66f841295d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:40 +0000

    floor telephone value since heavy

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print(sum(range(10)))
