0cc4a7a702f8e98c8aa3a98ea045dea789d48d07
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:41 +0000

    fact phone I communication description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_array_to_list(array):
+        return array.tolist()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
