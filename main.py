4ee66d6c055c32ca40cf176ca10cab9e1d88c3f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:04 +0000

    naturally sail improvement when correct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
