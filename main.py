63b057ff60d117f3214191e389b7e51f1f77f014
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:18 +0000

    explain exploration switch trust follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
