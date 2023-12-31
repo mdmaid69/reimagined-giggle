473a1a505592fd9fbb252c0f8fa940ef2966e47e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:32 +0000

    giveback surface to memory whisper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
