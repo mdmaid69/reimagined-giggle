8c556d29661b3af9050b1bee0a68ba39559b779f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:47 +0000

    sight wander support only truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
