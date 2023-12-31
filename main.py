e0baf33ff8e164c59c29b1b2cb48a9f3946af08e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:17:08 +0000

    involve weather nice shoulder record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_list_from_array(array):
+        return array.tolist()
