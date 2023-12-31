54132af2b8b814a5594728ea9850b42b470380bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:42:42 +0000

    female quickly first need class

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
