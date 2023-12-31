b698c51df6d3c19b5dd0e929ac086375682e6544
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:00 +0000

    consult rise underneath callback cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
