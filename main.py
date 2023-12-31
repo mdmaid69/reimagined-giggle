2bcd53db009254b35a5781fe06ad09801bf21461
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:23 +0000

    comeround factory wrong late urgent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
