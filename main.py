db2a7ac97eb4eab13e28df5321e0636a840d18c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:13:59 +0000

    divide joke discuss roof roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
