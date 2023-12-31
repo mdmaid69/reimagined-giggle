ca541d62a78a07e04c2f7eb7f6c716014e9029f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:30 +0000

    cross gas report able smell

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
