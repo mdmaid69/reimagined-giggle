5c003c11700bab72ae21e441f6863475e2ac2063
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:43 +0000

    family gap science forward effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_base_name(path):
+        return os.path.basename(path)
