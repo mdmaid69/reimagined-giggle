a83b369b0bb7babcb1158137c5a6bf810498a39d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:38 +0000

    giveout century slowly time above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
