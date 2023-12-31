c977e4b0eeed3516356e0b76a70609b59e5189bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:47 +0000

    until spray increase back flight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
