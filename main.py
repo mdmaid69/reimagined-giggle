3b75f614b2823477bce4843837ee058b211ec739
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:32 +0000

    mainly conversation yourself sorry pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
