6a2ed277a938c8cdf239358789b276be9314b8b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:53:18 +0000

    length active mean giveup production

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
