5f9fae44fa344122b58a504bb958b97fd136853e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:20 +0000

    pure thinkof normal swimming tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
