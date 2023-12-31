480aca8d1e27c307a2af5ce63c6cce265f5351a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:43:35 +0000

    reply equipment brief prefer although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_item(array, i):
+        return array[i]
