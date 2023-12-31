cd88b8d5efba017376768675f183ff5130d01a68
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:31 +0000

    minor exercise frame equal large

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def square_number(x):
+        return x**2
