e3a6d9d6c2e6440293ca8af5d216cdeaea0cae51
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:08:52 +0000

    block sure minimum copy level

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
