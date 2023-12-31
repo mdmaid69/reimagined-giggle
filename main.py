819c2522ab1463988fb2eb55b27582355a4f9dca
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:46:20 +0000

    more train concentrate slide affect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
