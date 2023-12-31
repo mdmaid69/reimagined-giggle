7f50421b18bf42157722acbcbb3825f62e5da866
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:59 +0000

    know talkback return stir Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
