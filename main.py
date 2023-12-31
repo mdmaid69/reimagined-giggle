e0eecc1421d7906feb4856a70cef21ba35097e1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:55:52 +0000

    frame later confusing interview you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
