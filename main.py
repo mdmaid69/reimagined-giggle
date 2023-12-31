eea644c0a90b0bca2db46485622e3de147367fcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:36 +0000

    responsible quarter practice turnup talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
