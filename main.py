7b5bcd514cf68ed5780773ed211b17a1efcfe865
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:36 +0000

    holdout swimming exploration wander Hh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
