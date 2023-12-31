dfd1cbbc2a3e5193630bfad414e196f16fe462ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:04:26 +0000

    object other organization shop situation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
