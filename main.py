affb6ae3b744c4fce80e8208ff14a2cc47812fea
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:52 +0000

    reply spend explain turn crisis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
