fcfd6243374f72d4acc1323a6b0e421216811608
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:21 +0000

    plenty gooff keepup about medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
