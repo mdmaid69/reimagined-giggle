c51f0683fb11d0a37f6c742e730484934e112904
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:15 +0000

    emphasis appearance neither feel cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
