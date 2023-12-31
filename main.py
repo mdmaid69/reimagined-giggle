a23dbede92eb92887a566b2d852266e899218cac
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:06:36 +0000

    guard actually side away emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
