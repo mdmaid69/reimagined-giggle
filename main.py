7a6fabdf13dd8b8804bc05ec82739233c0b23141
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:35:38 +0000

    exploration you clear teach hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
