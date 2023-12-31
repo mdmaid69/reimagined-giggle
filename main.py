abd206727dfe160e0b8116d13a70bf79f2eb6bbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:58:37 +0000

    flavour custom unless care extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
