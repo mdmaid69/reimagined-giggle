8bf48bf8deee8e6503cad06668321bed3582d94b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:25:18 +0000

    expression range shoot Qq joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
