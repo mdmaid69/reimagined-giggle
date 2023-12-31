4653d7072e80d0f5b763c96913287c723739463f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:40:29 +0000

    control only yes leave tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
