b4b71b474d4626eb7bd8b5cacacebd604c1b25e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:43 +0000

    write couple getup confusing trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
