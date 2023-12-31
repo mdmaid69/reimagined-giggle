4a4dba1cbfec5bd1de6fd5c3463a35a03cb7bdbd
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:51:13 +0000

    compare unless improvement second 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
