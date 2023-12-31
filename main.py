a3cd39466930288bb24caae6d8aadb6d69893601
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:14:34 +0000

    remark pause argument lookaround count

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
