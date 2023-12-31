70fa2be642c8639c9111a03c00a2ada104d6cb40
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:02:16 +0000

    accept entry perform turn freedom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
