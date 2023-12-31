0c6fe6febd3bc0a6186a7caa58e8fb780140a90d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:24:27 +0000

    stretch block giveout cinema especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_density(mass, volume):
+        return mass / volume
