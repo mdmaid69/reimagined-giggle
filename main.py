1a1704130ddb5d7c0ef429a9ceff0cf0e23143a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:40:52 +0000

    more limit fight hand wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
