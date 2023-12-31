888b20c93dd2e022dab5a74d5b6be229a3954a38
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:14:40 +0000

    estimate rather that control cross

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
