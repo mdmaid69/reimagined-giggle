adf845209156ef84bc5a9154c32c331a2e63e190
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:36:01 +0000

    it blind related sound callup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import sys
-print(sys.version)
+print(sum(range(10)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
