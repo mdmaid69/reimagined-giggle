ed98041820ff7fa9e6ed29110fb99ed74408418c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:09:38 +0000

    usual stiff guilty callout arrive

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import platform
+def get_os_info():
+        return platform.uname()
