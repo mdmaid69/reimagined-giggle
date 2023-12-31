ce029c49223108862f157e2eb42652b126620f48
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:13 +0000

    pause can control wing basically

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def square_number(x):
+        return x**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
