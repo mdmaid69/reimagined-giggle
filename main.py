22596bd0c8e16e70ebb3c7f7bd28c972d756bef6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:37:51 +0000

    offer march underneath song problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_tuple(array):
+        return tuple(array)
