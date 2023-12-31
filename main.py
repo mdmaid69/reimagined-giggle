1ec63d77cfeafca3d5fc3dc823061edbc674390f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:44:13 +0000

    round tap piece spite smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
