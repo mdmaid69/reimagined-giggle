6e315b125b472ba927bdbd8fa9ea5c9ee47a6d66
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:56:46 +0000

    perhaps exploration move upstairs together

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
