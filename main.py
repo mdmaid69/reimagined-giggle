d6e1d398b1db6477746d112adf627ea454beb59c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:52:12 +0000

    defend treat improve declaration important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
