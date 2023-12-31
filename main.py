99d7aae28a5f8aa8986a698479a0a2433cf5b197
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:48 +0000

    distant extraordinary tip base each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
