5445a5a49d756c2f69060cd32c9fd10226e6fb30
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:43:22 +0000

    corner sink scare callback street

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
