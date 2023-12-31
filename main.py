6298e53c160381137e58ffdaec95269cb8d030f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:11 +0000

    partner translate contrast pin foreign

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
