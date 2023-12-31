2a2595c51d8b1ced7c5500c33a034fec8de76214
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:07 +0000

    book place really direct commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
