4df77f0ba256b91b1791479e606654250b4727e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:42 +0000

    excited holdon regard than exact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
