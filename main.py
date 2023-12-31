005dc9b3f91ab1f7ce66c65e4fd09f548de4381c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:40:16 +0000

    towards load birth about space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def read_from_json(json_string):
+        return json.loads(json_string)
