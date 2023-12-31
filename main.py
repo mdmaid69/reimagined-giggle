ba090b6617a545efd46338a59928fa34efdb40b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:23:17 +0000

    movement between desperate confuse actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
