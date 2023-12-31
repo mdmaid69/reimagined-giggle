5af4041743823f180a5393d0d0727fc5d835cf6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:50 +0000

    young load rent speech this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
