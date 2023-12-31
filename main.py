4cf74406a55699b25abb9faef65b65ab477180e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:58:34 +0000

    relationship be smoke accommodation afternoon

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
