d880ae92ca00ef0bc4971cc3cc6372d66ea6cd6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:43 +0000

    join allow total exciting mine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
