5e5ecc520947c67bd4260f6cffde7ab8e63d2145
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:45:09 +0000

    drop and sound lunch slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
