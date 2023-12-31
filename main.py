2d5dfb36885ac8f387f6b9a6bda56c62e53163d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:45:23 +0000

    load rub myself anywayanyhow environment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def cube_number(x):
-        return x**3
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
