752303054c448d6b890876e73d5a103fccab9cf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:47:05 +0000

    improve hide themselves transparent tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
