e8d68b469284a725de8981a56d54b81202024786
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:49:06 +0000

    use between point into holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
