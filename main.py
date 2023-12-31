ffe2ef9e5015401b1d344adaaa3a36111753af67
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:23 +0000

    contain together original attempt hard

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print(sum(range(10)))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
