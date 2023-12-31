25b6b7d1fca3e7d456317fce8ffa7b580036edec
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:27:05 +0000

    easy worth appeal under neither

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
