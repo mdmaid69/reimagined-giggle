0b5da4a49c66b4eae242806e8259250ddbcdeba8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:53 +0000

    use offer exactly somehow confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def add_numbers(x, y):
+        return x + y
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
