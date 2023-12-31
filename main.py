e90f9c3e3eb0a4c51215b97e5232c9f1148ce4b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:59 +0000

    cover high nice report prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
