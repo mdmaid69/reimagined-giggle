c4a4be95a81e0531e6eda1441f760f49b55e3d2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:17 +0000

    still winter allow project under

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+for i in range(10): print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
