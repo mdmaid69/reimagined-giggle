5a92e59447297cb8d1091821ac84e3bb834eb5d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:19:36 +0000

    however thin firm simple eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_area_rectangle(l, w):
+        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
