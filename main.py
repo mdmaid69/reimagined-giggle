4c363d8586d002c32cfd86e691b7b2ad8a491445
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:45:52 +0000

    side special though we ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def square_number(x):
-        return x**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
