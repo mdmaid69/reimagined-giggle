98c1dc998c5527c8e7436bd57d2221a8f9e8718c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:32 +0000

    blue regularly 566end drive far

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print([x**2 for x in range(10)])
+def count_words(sentence):
+        return len(sentence.split())
