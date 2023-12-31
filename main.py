3ace38e327767fbf00e3b764a7c738e8fd15f4a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:17 +0000

    easy crash himself brush train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
