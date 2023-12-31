61f9a19d11b13b4dddc0902881825f1ff29b6557
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:52 +0000

    constantly right introduction concerning somebody

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
