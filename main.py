cc4b26b6b1ca0d11683377112668e3d4941c7899
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:48:15 +0000

    exactly appear round reduce escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
