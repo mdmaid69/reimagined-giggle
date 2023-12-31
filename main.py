0d6d10ae0c92e1103753494e14cb192a5cf27628
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:09 +0000

    next message emotion judge communication

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
