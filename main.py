0d92d891d99dd718bf70562c0f2eeffc19c1625b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:58 +0000

    near Ee further giveaway task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def is_even(n):
+        return n % 2 == 0
