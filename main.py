eba3989983e3b31fb6d19db302ab06f20b0ae386
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:33 +0000

    taste Kk present several whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def subtract_numbers(x, y):
+        return x - y
+def is_palindrome(s):
+        return s == s[::-1]
