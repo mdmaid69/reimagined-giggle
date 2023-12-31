32336021b2231d97963276499781748ccc4ad497
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:18 +0000

    butter influence use behind dance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
