a49c38a9c0546cdbeef69ed97ef14bc31ef77875
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:22 +0000

    try recently your immediately home

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
