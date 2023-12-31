a0bd512f8813549459d2d662de739d0540d8de8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:10:02 +0000

    gooff argue standard slice pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
