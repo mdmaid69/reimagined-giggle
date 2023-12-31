e99ba42a39601c130cdc74d939a74e7b8ca92423
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:39 +0000

    stone seriously bunch together recently

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
