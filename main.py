833b36939e16448a4110a68719a9bd39ec72d244
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:38:57 +0000

    live responsible collection close Bb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_palindrome(s):
-        return s == s[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
