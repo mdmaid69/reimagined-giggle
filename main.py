db95fa268b1f44d1ef5891a0bf579b81adacd55e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:32:12 +0000

    direction scene one tune indeed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def convert_to_hex(n):
-        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_tangent(x):
+        return math.tan(x)
