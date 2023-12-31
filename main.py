34ba850f5bb8e1b1b128ff1b4e4c98bf26954e69
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:05 +0000

    sound joke scared rid month

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def count_characters(sentence):
+        return len(sentence)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
