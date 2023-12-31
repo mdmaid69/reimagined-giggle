5f32d18310923a9615923467bc13292f509ead5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:51:01 +0000

    accident collection minor closely trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
