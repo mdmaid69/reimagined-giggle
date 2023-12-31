a9e28c8880eb42edda15ca105ae3ddc5005344ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:06 +0000

    detail work sorry my break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
