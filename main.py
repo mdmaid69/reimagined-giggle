62e8ad4ea6aa49b0c7e3c7a9e17c35cbb6e273c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:04:49 +0000

    division commit mention declaration hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
