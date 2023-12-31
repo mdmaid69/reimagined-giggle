06e3b0ca1be303087bfd42f5a0bc7a1f0a67d0ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:29:24 +0000

    reaction reach minimum spring building

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
