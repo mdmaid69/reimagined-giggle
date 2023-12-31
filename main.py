975d031e8ca421765fdf6d0bddf0ed780128b3a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:59:04 +0000

    uniform herself high satisfactory and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
