ea60b79b2d682983ed01d4292b3d79fc10b57455
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:05 +0000

    type imagination able own squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def reverse_array(array):
+        array.reverse()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
