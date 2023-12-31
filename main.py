15f2100f1c5811728eaa372c225fd9ead98444ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:56 +0000

    reduction start passenger way giveaway

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
