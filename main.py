5f31ab2074141aaffb472c28b547caba571e72e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:29:11 +0000

    away Rr world born any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sys
-def exit_program():
-        sys.exit()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
