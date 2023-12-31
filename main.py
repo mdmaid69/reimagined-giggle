9f34be5489d684e51d299578de9899281b49ecd4
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:05 +0000

    value wet rise recent free

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
