725e6b2746e4849765a6f81e0a35519987fd49c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:51 +0000

    search that key pressure lesson

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
