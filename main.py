88357d34d73b8b40ab8b53a6a6b73c795ea13ab6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:54 +0000

    weekend along pleasure notice plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
