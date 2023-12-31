296c7662b891ad34901a2c5293a306e8ed22f108
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:49 +0000

    neither coffee suggestion centimetre narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def reverse_list(lst):
+        return lst[::-1]
