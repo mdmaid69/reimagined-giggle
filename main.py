59de8002ca43ac68370af36e8d7800a8e32e3b95
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:34 +0000

    amount sorry hot spring listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
