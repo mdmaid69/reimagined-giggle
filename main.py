b5ee66689a6f24fff218ef6d299445be1ad1c6ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:25 +0000

    beginning bank comeback several date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
