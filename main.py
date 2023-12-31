299d2d52e6baff2ef5635bc96a2b95b29fbdf9f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:32 +0000

    earth birthday full stay mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
