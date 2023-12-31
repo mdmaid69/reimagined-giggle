6f2260ebe50113def515c8c5a7aa272573bbe063
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:20:10 +0000

    over herself cost act practical

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def add_numbers(x, y):
+        return x + y
