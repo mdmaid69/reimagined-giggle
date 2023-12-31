e44a922be0ab20cc450f39f95c9fe7b90e1ba75b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:20 +0000

    dress worst surprise store house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+print(os.getcwd())
