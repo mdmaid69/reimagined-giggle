b195bb1cf8e7552909bea034f869e0f8f9d571c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:30 +0000

    trip half accident score long

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import datetime
+print(datetime.datetime.now())
+print([x**2 for x in range(10)])
