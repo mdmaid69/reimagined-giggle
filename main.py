88e749f3aebaa5f647e09b32b79f985950478442
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:37:57 +0000

    summer explain train knock fast

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
