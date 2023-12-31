0a307fd1fb6274ce96a4c767835011175a7d2a96
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:11 +0000

    wire to present consult wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
