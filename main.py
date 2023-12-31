a56dc3102af7d692209a39b17089cb84babe5119
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:00:16 +0000

    damage front scene until scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
