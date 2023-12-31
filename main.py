5c03b04a3bcb499dce63ed57e48ba69027bb4ff5
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:07 +0000

    progress them more direction may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
