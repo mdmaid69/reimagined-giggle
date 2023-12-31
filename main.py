6faaaee60647edb583ec5c72e8d94b9f5300e8be
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:28 +0000

    either argue from possibly occasion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_density(mass, volume):
+        return mass / volume
