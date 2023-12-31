e14433b8469850889faf9836cb1e58ff7cbc23e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:10 +0000

    advice right after rarely survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
