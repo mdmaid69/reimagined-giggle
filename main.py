653c8b41e3143ba4f7a3b0cc232d659a060328ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:29:25 +0000

    benefit watch smile whether achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
