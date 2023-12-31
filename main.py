260cf57748127146285157cecfc94d43fb8154d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:14 +0000

    model late lookup mail aware

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
