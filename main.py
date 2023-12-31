dc423bdd2c2f9c37986b68fb15211e50adec5483
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:00 +0000

    tall secret tone own least

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
