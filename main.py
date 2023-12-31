4479001469f69bcc5b24049847eea4b1ad9a59e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:54:27 +0000

    release hungry form behind possibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+for i in range(5):
+        print(i)
