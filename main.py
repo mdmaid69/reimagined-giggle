9ca2e41083e48ffca9925a297dbe0f63334dacf6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:52 +0000

    case mix complete phone slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
+import array
+def append_to_array(array, item):
+        array.append(item)
