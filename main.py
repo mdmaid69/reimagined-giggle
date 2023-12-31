1094574f6f938fd95bfcf4cb0913989ea54fdd7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:15 +0000

    thinkof conversation switch declare us

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
