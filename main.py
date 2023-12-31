55f3ca1ac6942ca51b37e24a6d6756591cefa016
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:59:50 +0000

    furniture sport compete collection Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
