42b2c8b3599ea956ead62e079104572eaaeab934
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:30 +0000

    fortunate imagine medium press trade

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
