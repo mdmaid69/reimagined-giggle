0ec87b903c891e3976e21f9874ce08eee6e3c0da
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:09:40 +0000

    rather reaction theirs bottom getout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
