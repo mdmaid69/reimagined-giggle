e9019f55c1071b2078420f2e0b6dad7586f62d36
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:17:54 +0000

    half loud tooth tight lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import tensorflow as tf
+print(tf.__version__)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
