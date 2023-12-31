5980d179d6e6945684101e8d633ac7ba8e144dde
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:21:39 +0000

    please sick foreign than bye

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_array_as_str(array):
+        return str(array)
