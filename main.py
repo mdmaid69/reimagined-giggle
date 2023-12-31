da17f60175d12565c2075e87f464eee06e7b86aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:38 +0000

    nurse hide there official thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
