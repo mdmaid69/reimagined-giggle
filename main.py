62ac3eca41722fc768a9998b0f7ace143186c9dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:13 +0000

    peaceful visit schedule grateful 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_as_repr(array):
+        return repr(array)
