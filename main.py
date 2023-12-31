7d9f7f125b441cd9d16d5335e5a74aeaefc52396
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:03:01 +0000

    generally night violence already mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
