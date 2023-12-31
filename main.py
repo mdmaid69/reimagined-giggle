930df1ddbd58012de2f394cdae6013696b65d720
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:36:05 +0000

    surprise science mouth powerful tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def count_elements(lst):
-        return len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
