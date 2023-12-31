dec8b083f617bb659fb9aff91fa4a7a226ff0af5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:53 +0000

    long truth win concerned autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
