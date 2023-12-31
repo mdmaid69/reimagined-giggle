73208e5ce7d6c7673a244fff800b982109277585
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:38 +0000

    sentence welcome error circle runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
