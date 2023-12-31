1f0ecacd1d6afa01e4acfe50646523b9200fb4da
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:49 +0000

    rest thankyou mass turnon interrupt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def divide_numbers(x, y):
+        return x / y
