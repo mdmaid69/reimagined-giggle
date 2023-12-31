fcdff5e38878c525166cfa55f6a363d877af43d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:28 +0000

    dear well group doctor star

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_counter():
-        return collections.Counter()
+for i in range(5):
+        print(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
