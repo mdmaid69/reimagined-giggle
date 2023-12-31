331730d1517a155dfe0b90e7431ae629ca5eb01a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:55:09 +0000

    type empty result chief makeup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
