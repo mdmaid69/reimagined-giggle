96df999ebb3764de053f5534bb121d179bc327ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:48 +0000

    hurry scared just spread like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
