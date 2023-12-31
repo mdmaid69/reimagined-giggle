72cb42aefdb18f6306887bd5a448f8842c6ca615
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:33 +0000

    seat basically meeting none dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
