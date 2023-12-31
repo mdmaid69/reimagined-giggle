6a490384c8e947b9cbb2dd47fac262cc7f6e7176
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:32:56 +0000

    spare adjective snow down pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
