367397660b15263e58e0b7b9460c92b515195599
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:53:33 +0000

    estimate admire tip female fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
