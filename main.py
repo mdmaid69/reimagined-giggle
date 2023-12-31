4ee4850a98a1ca90e600f685efcb45589e9992ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:57 +0000

    crash fashion modern hole crash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.time()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
