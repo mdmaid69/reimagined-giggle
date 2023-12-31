b367dd0ea5b65b36972c9d7ca12fb9e57682b03e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:20:58 +0000

    tooth suffer before prevent dead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+print(re.match("h.*o", "hello world"))
+import os
+def remove_directory(path):
+        os.rmdir(path)
