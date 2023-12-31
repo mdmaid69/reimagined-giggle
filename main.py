e3952f9878030844a480cdc367b97ffee9b692e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:33:43 +0000

    so ever otherwise guilty neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def append_to_array(array, item):
+        array.append(item)
