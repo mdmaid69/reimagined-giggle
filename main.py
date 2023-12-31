4a29fab15e2ac5ae237d487628a105eea2e40ff7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:42:28 +0000

    excuse hospital impressive case pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-print(sum(range(10)))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+print([x**2 for x in range(10)])
