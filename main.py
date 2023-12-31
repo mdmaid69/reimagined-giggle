12b4a6974915f7f756f2ce5b279ea3aad2ec4305
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:21:35 +0000

    stretch lookback(on) love lookaround name

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
