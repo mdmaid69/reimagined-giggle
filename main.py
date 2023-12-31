4228a743e16b0318e09c126e82f693cf3b3fe4ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:23:11 +0000

    love town keep goodbye spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
