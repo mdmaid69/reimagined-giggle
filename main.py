c706aec5e1d2fe4006321ffb5f96c0e6402d21b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:37 +0000

    wet give instant lonely laugh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
