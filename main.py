7ddcc636f2698ad6fe64d0b514829757ca31cf21
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:28:24 +0000

    least come start neither laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_pressure(force, area):
+        return force / area
