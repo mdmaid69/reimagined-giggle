ded124010e5f563a82b22aeb3e2f1b54aac4d1cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:01:14 +0000

    patience reason wave region risk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
