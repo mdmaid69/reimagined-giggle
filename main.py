f79462456b078e993438a364b016cc5d30aaf89f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:14 +0000

    generous swing normal play breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
