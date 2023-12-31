b8fc8298f6d1e30ec9bcd348ae7637278feb3262
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:02:56 +0000

    book somehow gentle journey praise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
