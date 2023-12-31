8d16b2d92aa7f53e76bae04a6c86fd9158e23ee4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:25:24 +0000

    money thin tough tired scale

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
