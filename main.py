6df7dd01b4d42b107160bc5f2f8d92a8067211d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:22 +0000

    Bb considering independent time intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
