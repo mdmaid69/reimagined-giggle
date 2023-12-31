5b53f04319762cf207ecadb34faed59cbc3de0d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:14:48 +0000

    light swim fear spare somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
