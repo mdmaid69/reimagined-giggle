2828f14698adc670c0534e92673d1654b0a49d8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:00:34 +0000

    classroom moment month win later

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
