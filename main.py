37fe557ce95cf9ef75e135545d192cbebf98c70c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:05 +0000

    paper excellent answer heat dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
