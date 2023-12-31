2c90632eee59625747e9dbf894065495c400594a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:55:03 +0000

    early aim seat ready matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
