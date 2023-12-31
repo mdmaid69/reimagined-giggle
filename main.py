22b33d0c893b1129b59c6e313ce113614bad828f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:44 +0000

    fit solve call refuse ocean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
