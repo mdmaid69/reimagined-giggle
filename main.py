63315ef8253740fe0a64b7b5900868a5e77f455e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:14 +0000

    furniture turndown past healthy intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
