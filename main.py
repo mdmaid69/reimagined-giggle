7098fc789b4759dcc9f40bee6593bd32c1e08f95
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:41:16 +0000

    steal drop drink ours silent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
