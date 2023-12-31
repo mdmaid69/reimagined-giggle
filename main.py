07a1febcda9d0069766ddd59c43f34b9a61fb704
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:11:03 +0000

    behave gentle call thank cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sys
+def print_python_version():
+        return sys.version
