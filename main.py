85e6f1bd51dc191d84072e5e9cc1ec5c8caf8add
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:10:12 +0000

    switch accordingto which remark suckme

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
