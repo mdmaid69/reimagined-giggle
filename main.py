1fbac67049dec1dc86df878d975af5f479f36bec
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:52:18 +0000

    over regard floor direction sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_bool(array):
+        return bool(array)
