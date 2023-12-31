e880a4f999ee85c494666f03fb830399c911000c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:55:56 +0000

    actually form best play body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
