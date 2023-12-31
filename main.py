17bcfff1bf72d62563253bc47a5a633077d94609
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:22 +0000

    patient boat Zz awake crazy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
