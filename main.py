5c1475831d8cd2b5b975671ce5e86d21cc8a4142
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:48:11 +0000

    goon round save bath 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
