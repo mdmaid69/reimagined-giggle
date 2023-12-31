03c273961d612043db17b2d2059048278e0df6aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:30 +0000

    meal black provide nature kind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
