1f08f7065648df57b4d9d23a50ce8f421043bc9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:19:46 +0000

    even far stranger thinkof spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
