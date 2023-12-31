5b0f33b20fc9aa67c83a45af0be4ec9d684426dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:27 +0000

    field at take interest question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import datetime
+def get_current_date():
+        return datetime.date.today()
