861e053b66ddeecddf27e3227e6b33123813a7cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:26 +0000

    strict expression pair bank section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
