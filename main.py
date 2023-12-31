92b2804c4aefc3305df1427cd9ef2c975fa7c8cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:34:53 +0000

    song pull against sleep attach

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
