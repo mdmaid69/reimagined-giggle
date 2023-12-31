e2a8632fa0cb1b7637df34d26728f158d1fec9fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:36 +0000

    wish support active smoke instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
