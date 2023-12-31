4cba3d248dba888ffa9c72f3d3b28afe5b757275
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:10 +0000

    peaceful false into flat popular

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def count_characters(sentence):
+        return len(sentence)
