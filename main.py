01d8c0e9f9dd24c1e6d8b96018aeff7dd85a573b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:52:02 +0000

    succeed passenger on question at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
