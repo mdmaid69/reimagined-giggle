1c912d3db818eca0cf7ecc195efb325328315b23
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:12 +0000

    shock question tear free sort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
