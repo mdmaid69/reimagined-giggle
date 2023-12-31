16633a5ac923a9dde52e0dffa86430a77f2b7f87
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:22 +0000

    sore grade pencil mass lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
