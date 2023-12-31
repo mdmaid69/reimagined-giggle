ff9e48b69dc384b0ebc429f4738f793c6d20e3a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:25 +0000

    brush present university object whisper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
