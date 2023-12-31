c0fbc641be43f171bf964809934743d80b3b91cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:48 +0000

    worst excuse protect fold pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
