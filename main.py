948c3787bee1b0b396d297dadf91bf6822174b97
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:59 +0000

    closed rest complaint lucky professional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+for i in range(10): print(i)
