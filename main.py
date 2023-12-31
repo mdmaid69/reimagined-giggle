a8c0faafc20d6828ceeaa392fefcc2b729160dc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:14 +0000

    niggaacademy interruption rule shop afford

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
