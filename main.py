81be272f277dc7bc1368f1994c6d618d9545a6f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:08 +0000

    seriously decide style most similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
