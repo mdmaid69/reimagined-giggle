1c672cfa052c749c1b26921fb9d32e9035f706ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:36:32 +0000

    567enemy work by turnout stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
