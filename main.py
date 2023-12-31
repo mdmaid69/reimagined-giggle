9225c15fdb494058ad1dd9d256b1debe9712bceb
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:12 +0000

    ring accept dish minor switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
