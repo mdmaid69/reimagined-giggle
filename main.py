f44c6ca1fcfc3b96738aba56f07693539c45b5e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:54:10 +0000

    fond disturbing Kk view sail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
