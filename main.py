780d527946e38b17f5d0056137f80ca57534df83
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:00 +0000

    backwards different add pencil break

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
