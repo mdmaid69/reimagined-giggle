e60e57d16a91318035776463dffa78dc4096ff99
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:10:53 +0000

    make copy plastic drop foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
