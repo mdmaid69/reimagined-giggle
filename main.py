fce40fa666f7ddd51ac4a8fad27ce5244fa97041
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:48:07 +0000

    real disagree we sort keepfrom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
