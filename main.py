7adcc46f45a78fdc4d9db4cd266b896fa97186b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:20:40 +0000

    store turnover ocean stick direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
