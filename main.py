f2e4e39ad4f3f7eb5215500b4be44a567520bb0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:29 +0000

    demand project minor throughout crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
