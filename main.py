4fbf3cf095fd2560c8f668dafa199d46423b0f11
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:42:23 +0000

    tough string tense bad holiday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
