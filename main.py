188431bed0d13148e663b91c563abcf9137908ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:04:21 +0000

    worth cinema neither space stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
