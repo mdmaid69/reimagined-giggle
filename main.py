1b9b06e78e871e8f7a2fcad479f75612812ff2ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:44 +0000

    intelligent sense guess difficult exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sine(x):
+        return math.sin(x)
