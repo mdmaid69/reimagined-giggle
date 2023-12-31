92245b486c886d89beb5cd06562fb50cfc1147f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:44:36 +0000

    568energy figure start only unit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_work(force, distance):
+        return force * distance
+def calculate_work(force, distance):
+        return force * distance
