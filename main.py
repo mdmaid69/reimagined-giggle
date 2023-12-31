a1d050282a22f8790e36f7ec2887984b778a71dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:49:02 +0000

    fasten turnback television mine weak

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
