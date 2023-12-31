ff18acfe831400e7d75d3816ef05ef9bacc0253f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:55 +0000

    ring method illness lookaround open

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
