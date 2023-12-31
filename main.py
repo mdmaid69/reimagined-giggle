7f0a098fd1908a7a27c4cb8249f86766e37fff9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:17 +0000

    stir chat line detail our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
