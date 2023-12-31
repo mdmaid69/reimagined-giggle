c5eaafa48ce5ff8b408aa497df2c0aac62b0addd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:30:16 +0000

    comment else little great track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
