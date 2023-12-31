5424fcfcb522e8e7d8bbf7fd91dacd6fba133ceb
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:10:36 +0000

    dish woman matter target difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
