06957748412e1397901f027b4f3d7d7fee6ad8ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:43:34 +0000

    solve out understand cover exciting

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def reverse_array(array):
+        array.reverse()
