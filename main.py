31696990f1b00032668c55e877d2906abd0f28d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:17:36 +0000

    relation bridge rush tired lonely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_error_function(x):
+        return math.erf(x)
