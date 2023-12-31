0e60725743f9fadc10d26ec7f6ce0dbeb1b5a2ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:02:48 +0000

    burst fit wash about throw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
