6fecb5ec81aaef1bcf3e4afdf0ae2177da389dd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:31:55 +0000

    getback middle getup star previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import datetime
+def get_current_date():
+        return datetime.date.today()
