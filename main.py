487c47159351651866cfff35609b2ebc731175eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:13:48 +0000

    knowledge 565end middle capital cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
