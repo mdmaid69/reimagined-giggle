df585a0f135a25a8efa7cd0c75615e8dffb00dca
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:32 +0000

    risk team part treatment top

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+def exit_program():
+        sys.exit()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
