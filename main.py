5ca381129ae5dcee4814db839b1a44c245dbf1cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:27:14 +0000

    rent different fly steady emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
