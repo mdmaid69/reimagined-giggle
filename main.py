e07fafc9a7d9f8838942074921432e93ac90ff45
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:38:42 +0000

    steam opponent majority laugh last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
