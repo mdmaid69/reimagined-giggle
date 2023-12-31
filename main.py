0b3ea021fbcbc7a457975cca83ec5df6abe59c47
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:57 +0000

    callup smile rate finger neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
