5a57319d18a5d2826147d6f5e967d6bd97b626de
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:30:33 +0000

    who holdon the fall loss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
