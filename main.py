16b01129f3baed31d2a188127c2927ce2efe7768
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:05 +0000

    exercise declaration start welcome modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def calculate_work(force, distance):
+        return force * distance
