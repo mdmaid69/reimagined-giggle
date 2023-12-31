1bdf8ef1838e2618b802b11ca504452878b55f16
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:05 +0000

    thanks keepup vision kind quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_python_version():
+        return platform.python_version()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
