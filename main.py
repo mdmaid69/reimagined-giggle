41c40271e1b220dae513d9776a23e9d39d7ad8ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:53:58 +0000

    uncomfortable on injury trust frightened

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
