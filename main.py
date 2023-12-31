b1154238552181e2067b1eccafe8e30a961c93ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:42 +0000

    which mine divide getout takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+print(sys.version)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
