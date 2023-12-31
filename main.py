3550bced19378dc6cc681fb923740319cc022863
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:51 +0000

    hole card second interest trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_frozenset(array):
+        return frozenset(array)
