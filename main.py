1ae1117778f25e5017d967ccc8817d84f6862547
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:07 +0000

    plastic ill date cheque afterwards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_tuple(array):
+        return tuple(array)
