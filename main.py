52cebcea182d87405163ed223d50059a1797603f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:57:03 +0000

    badly though pleasant strange shop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
