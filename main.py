a5604d4049af18202475ac639bb4ed3ec869825d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:57 +0000

    approval exchange closely please classroom

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import random
-def roll_die():
-        return random.randint(1, 6)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
