154e9bcd77a492a2d2f5497bb14462da0a910ad6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:43 +0000

    you recover fire she bright

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import sys
-def print_python_version():
-        print(sys.version)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
