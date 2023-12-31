935df985a7bfbabc3e9ffd00d9971422a91ec6f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:16 +0000

    term sell least cook getback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import tensorflow as tf
+print(tf.__version__)
+def is_palindrome(s):
+        return s == s[::-1]
