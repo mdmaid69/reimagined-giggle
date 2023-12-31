0ad2ebd927590f2726f070000eae1dd0cfe3bd2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:20 +0000

    defend score earth narrow special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_user_string():
+        return collections.UserString()
