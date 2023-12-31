72374778299c644e027fe6ba1a4a2659746065df
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:18:21 +0000

    emotion cold read sure and

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
