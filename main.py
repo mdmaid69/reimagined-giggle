ebc4486e3e9e87f8574aac62e28f5281bd894141
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:39:39 +0000

    platform passage debt plate backwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+for i in range(5):
+        print(i)
+  def convert_to_hex(n):
+        return hex(n)
