1c938e7b0ccc7cb4c800c44635db11e983417797
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:48:59 +0000

    direction tough shelter tell play

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
