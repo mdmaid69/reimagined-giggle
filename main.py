9a37297034e2b2a6ab5047d3a7b34950d868e0f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:05:38 +0000

    modern split sign walk minimum

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def convert_to_hex(n):
-        return hex(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
