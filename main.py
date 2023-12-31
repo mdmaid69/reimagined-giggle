a442b102a6672c8bd0b0bbe6ba96d48fff26e3c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:36 +0000

    about field load fold off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
