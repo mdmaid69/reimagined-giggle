9daa0a23c303f8c10f12f2b0a04c4e0ae214521a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:18 +0000

    ball recent down care prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
