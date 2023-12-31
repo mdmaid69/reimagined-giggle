59b66def139d648db0d8f30bfd6c2ba8f9fb5732
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:54 +0000

    used read match rush stamp

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
