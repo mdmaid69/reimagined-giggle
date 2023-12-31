c06a53a69469ca68198a2b4269bb5e9ff98d83d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:10 +0000

    handle confused on talk constantly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-for i in range(5):
-        print(i)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
