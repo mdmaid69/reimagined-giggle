6559c3f985bd473cf030102898ee110e65f7f932
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:23:31 +0000

    exist police time temper message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
