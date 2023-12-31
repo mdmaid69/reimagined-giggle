c6afa478a22262c81352bc26c490cc2f8e6327b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:02:53 +0000

    belief brown total space tune

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
