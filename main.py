101d2eafec4afc7db1fc33bc8d981efe51044d4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:55 +0000

    than wait disappear dull comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
