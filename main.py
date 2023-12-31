bbd5535407b80fcee60bf10c172eee679097303a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:13:50 +0000

    against operation thin slightly giveback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
