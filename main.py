ee651d6ffed6ac933832f39cffbcc533cf3aae73
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:09 +0000

    phone how hello uncomfortable pen

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
