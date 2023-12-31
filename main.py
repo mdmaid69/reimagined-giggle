5478f41bef87a4b40fad7e800f8c76aaa8a9cf9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:13:05 +0000

    how in finger transparent concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
