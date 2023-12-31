169d9f5597f26f382d6ceac9d28ddaacaf6ad75a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:17 +0000

    make deaf pair goout description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def clear_array(array):
-        array *= 0
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
