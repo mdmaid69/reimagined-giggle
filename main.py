7f552657abbf02ee5ec2a0230ee4d23ad88f9fb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:33 +0000

    her Qq party switch load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
