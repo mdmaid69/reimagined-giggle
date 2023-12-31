1710cac10c01c7069ee3865e0f116e93d040347d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:43:30 +0000

    whole minute dick 179bite tend

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-import time
-print(time.time())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
