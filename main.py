dc2dd5bb973323e73fd8c3a3c0d29e889ad8adf1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:21:30 +0000

    fasten child beat minimum mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
