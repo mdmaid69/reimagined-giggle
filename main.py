f1ea8c7f9fcbd08fa794fd0d331d0eeddf189fb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:28 +0000

    hour maybe effect sure behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
