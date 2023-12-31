2418385885d0de7875b366baf1d088e2d60ab809
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:15 +0000

    keepaway sure surface aim content

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
