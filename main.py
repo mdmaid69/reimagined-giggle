066f49d0c8a2c6e9ed5f3406032ff088ac4b8a18
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:17:20 +0000

    market shut stay freedom flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
