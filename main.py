ea987be4e4bda3a6b39667f8112cebe5987b7940
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:30:35 +0000

    government container finish talkinto approve

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
