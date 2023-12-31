1faad758bbe122f0e42002973a6aa0cfb5af8ee4
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:09:04 +0000

    takeout tough thank young check

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
