c7a9e2e43cef2207d9ef4ec44a77f22b6fee9457
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:26:12 +0000

    speak exploration silent receive interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
