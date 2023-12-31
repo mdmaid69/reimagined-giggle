6724def8b0454f632687e96cf8f5af06a8c269a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:31 +0000

    cheque destroy art straight satisfied

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-import time
-print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
