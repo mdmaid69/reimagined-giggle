f5a16cd1905cbc783ceef51cd618a0317099dbe2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:12:15 +0000

    side invite either drive impressed

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
