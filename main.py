ac9385884b8caa13dd69e2dae69fac0a9f47ee01
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:51 +0000

    no list organize sleep club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
