74d34186f59e66e476f1aa152e6a976903189836
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:59 +0000

    announce otherwise even public usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
