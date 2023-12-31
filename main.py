9de787df12ec89d6897b2ecd17ed713478c68ca8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:43:44 +0000

    last task miss comefrom emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
