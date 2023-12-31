cc649c57d100c676b5f5a14aad30bf8ea829be4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:50 +0000

    voice nerve outside glass before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import math
   def calculate_square_root(n):
         return math.sqrt(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
