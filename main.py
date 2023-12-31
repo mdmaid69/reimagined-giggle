01f838c251b283cafe707d3e726aad372400428d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:46 +0000

    comeback order sort like figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
