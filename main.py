4a46ded802a7d06637584e5c2b6c002c3fc6ecc7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:58:59 +0000

    silent public sure lift all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
