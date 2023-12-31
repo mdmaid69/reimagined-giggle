02913d274c3824f5ebdb6dabb7c43684d14f49ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:43:20 +0000

    sometimes load roll defend through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
