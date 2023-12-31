7b21220b5d5e5988ae8933913b7c3b184009c2aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:40 +0000

    but bridge precise send drawing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_union(list1, list2):
+        return set(list1) | set(list2)
