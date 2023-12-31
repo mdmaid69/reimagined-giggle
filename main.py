5a23b686311666d50b16614a693911666123faa6
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:48:39 +0000

    appeal praise frame like everyone

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
   def get_file_size(file_name):
         return os.stat(file_name).st_size
 def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
