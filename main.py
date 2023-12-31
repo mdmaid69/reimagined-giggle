e03c6e50e240b5a07b4ddb560f4f621d9d5fa75a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:31:02 +0000

    mistake immediate excited leg criticism

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print(sum(range(10)))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
