4e71cff5440daa233d348294109c7a0764bac4b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:30 +0000

    disturb science confidence bury fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
