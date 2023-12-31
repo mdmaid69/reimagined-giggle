dc131bbf3cc83c5459cd46d71ba2bad399e956d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:13 +0000

    fairly dark easy like summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
