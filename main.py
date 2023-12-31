4280133137075e5463952b54ffa3115047f52721
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:12:49 +0000

    glad or describe negative notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_to_octal(n):
+        return oct(n)
