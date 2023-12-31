cbf8890df5fa0a3629fae9c209f071ebca2c19c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:43 +0000

    suit against runaway save passenger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
