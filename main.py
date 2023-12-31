ffc2aa4d41a472417420699f7969cee199854763
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:59:40 +0000

    constantly front official quarter phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def add_numbers(x, y):
+        return x + y
