8ebfc720976d3339450567717e29c2f3daaa2173
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:35 +0000

    pool moment little cover letter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
