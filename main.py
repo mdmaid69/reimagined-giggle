3a9ea2e3123e214f9c8564dff2b150586ba2f458
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:32 +0000

    matter interested nice behind holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
