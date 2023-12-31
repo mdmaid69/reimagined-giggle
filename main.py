77e547e2de237adf093fd709657d02d2a425b379
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:30 +0000

    good appeal mark noone otherwise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
