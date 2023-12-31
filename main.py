81c25608ecea44a222a612c22ec6ef3588d9e290
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:35 +0000

    hurry walk bored lookback(on) alone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
