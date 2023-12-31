60ca63eabfab88c287ca7de5bd2eb5d1ea801de4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:18:05 +0000

    manage friendly load mind class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import logging
+def log_message(message):
+        logging.info(message)
