edaadfc611915ca3787cc230e85888a6dbb63780
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:00 +0000

    handle Zz 573entertainment load hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+from collections import Counter
+print(Counter("hello world"))
