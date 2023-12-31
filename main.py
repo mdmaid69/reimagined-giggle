9c6a0c06daed49821e749056e8cdc49a9dea67b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:30:08 +0000

    never phone true compare prize

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-print(random.randint(0, 100))
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
