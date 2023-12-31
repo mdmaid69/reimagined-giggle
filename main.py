f7d3972b62851496a4606ac0d2cf641a8aa52a0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:00:20 +0000

    hardly taste satisfactory mend flat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def cube_number(x):
-        return x**3
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
