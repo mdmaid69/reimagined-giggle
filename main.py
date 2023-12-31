0fad2f12153a116994957932b99fa4ae13da71e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:12:06 +0000

    interested perhaps such speed rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
