786ad8b0bbdfb6e29d2a3ed226ed6a90e41ae1e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:59:55 +0000

    deliberately need address free touch

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-for i in range(5):
-        print(i)
-  def sort_list(lst):
-        return sorted(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
