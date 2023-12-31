73db61dc94d413660307504995510482048db1d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:50:03 +0000

    dig turninto Ff smoke love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
