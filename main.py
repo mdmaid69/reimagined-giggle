47de9e97d7340bbef5e0b6f30164322287a8d8c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:04:17 +0000

    sore takeback burn best shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
