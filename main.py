e2617b81da17e4bc8238e8ae9c959330fbb663d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:08 +0000

    chin even true opportunity it

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
+def is_palindrome(s):
+        return s == s[::-1]
