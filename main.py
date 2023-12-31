838a36519247c3949902bf4afda962c59d059e13
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:33 +0000

    begin summer play aim mail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def convert_to_binary(n):
+        return bin(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
