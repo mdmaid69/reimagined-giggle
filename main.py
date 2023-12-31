6e66272d06b8eb1e764874a4abe318b2353443ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:45 +0000

    179bite now word shrink knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
