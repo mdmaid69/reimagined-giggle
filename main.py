514c245465d041e6e6ecbfd2fdf5399b66ae41ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:55:38 +0000

    deserve convincing open length contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
