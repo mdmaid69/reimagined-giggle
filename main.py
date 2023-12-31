01945fa56d3dc1d54857e7e551cf35ac2f51973c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:15 +0000

    square importance definite similar stare

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+print(sum(range(10)))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
