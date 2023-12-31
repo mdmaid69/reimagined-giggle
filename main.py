42b24b2b7ce3cc3211e144c06e78adaf6a5a3b86
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:59 +0000

    broad workout peace crime through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
