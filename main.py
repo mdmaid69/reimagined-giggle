23f1c7d6bfd9fe52f34c064b79fa17e333f541a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:16:00 +0000

    slice pollution affect tread fast

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+print("Hello, world!")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
