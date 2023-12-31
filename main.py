760aa008ad916aa5862948a79c9b0f94d2127050
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:50:07 +0000

    emergency study occasion open company

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
