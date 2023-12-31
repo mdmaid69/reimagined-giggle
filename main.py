362339396d933baec3e8594e341770a8a352da24
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:31:25 +0000

    balance tread block awake practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import numpy as np
+print(np.array([1, 2, 3]))
