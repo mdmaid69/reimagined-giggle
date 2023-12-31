77a59d1e12f3480964ac007aff0439a4cee0177a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:03 +0000

    ball make congratulations guide rest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import re
-print(re.match("h.*o", "hello world"))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
