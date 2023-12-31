395a7ed279fd52619349242f0414b4844760119e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:17 +0000

    injury substance turnoff busy wealth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
