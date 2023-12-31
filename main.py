9faf50bfbce6a2a948e3c13620bd31276c0f66ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:17 +0000

    neat rush basis him choose

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
