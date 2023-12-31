7812feab32dd36972065c8084d5b135f1c5ff2bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:35 +0000

    lower back herself she practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
