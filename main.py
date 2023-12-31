e372dfd31a29b2009baebf3ce776412f2acfe379
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:43:12 +0000

    involve considering impressive cause used

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_to_hex(n):
+        return hex(n)
