61040dae73f4f147e95aa20e0cc3611f54207741
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:13 +0000

    trick course hospital or room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
