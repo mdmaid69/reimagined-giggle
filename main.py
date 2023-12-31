71616a85526d1c384261ef24746bf90901b1eacf
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:02:51 +0000

    track season breakin confused smart

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
