63a14f813879e9b47226cd0b45c528d150db19be
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:32 +0000

    stand jump shall quickly forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_density(mass, volume):
+        return mass / volume
