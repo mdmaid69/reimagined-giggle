e43f5ad91a4ad1581d6757488b5edd7a13e5233c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:23:55 +0000

    follow evidence entrance trip square

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
