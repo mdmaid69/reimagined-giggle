87adca484690f1177f7b5cf4b229a6328886834e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:21:45 +0000

    keepoff force top angry ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import sys
+print(sys.version)
