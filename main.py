7d8f4f613bcbdaaa37c567b377fac7a56000f805
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:17 +0000

    closely hurry prize arm final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
