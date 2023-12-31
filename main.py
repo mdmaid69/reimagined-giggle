18e2da49679e9d1abe6f4309aa19166bba1dbd31
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:34 +0000

    standard shade steer value give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
