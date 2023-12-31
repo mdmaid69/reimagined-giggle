d4ba85c3bcd33e1b83d6781094184e198e6d3bc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:45:12 +0000

    ready needle stone name him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
+def add_numbers(a, b):
+        return a + b
