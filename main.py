5b9698bda2c58dcae95cb71cadf1e5b8b8249f7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:11 +0000

    steer its result workon children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
