18d41fea0946c5997deeaac3551edcb9d4616bf4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:25:34 +0000

    wash science colour surface high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
