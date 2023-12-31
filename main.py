f13c2f030766d81c97f2052dfcaf0bd3c2e72877
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:55:56 +0000

    change major adjective near bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
