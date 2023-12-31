bd8fc54d96f43c276d8876b47489561d70e7190a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:31 +0000

    your pinch punish get method

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def divide_numbers(x, y):
-        return x / y
+  def square_number(x):
+        return x**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
