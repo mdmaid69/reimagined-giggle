44af122b3845228e2fa60c131776d53c00094edd
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:42:44 +0000

    string present land clue familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
