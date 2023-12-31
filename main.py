afceb2786b7bb165fe9a5caf84973b43f2284707
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:22 +0000

    excuse deal lead ability nose

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_set(array):
-        return set(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
