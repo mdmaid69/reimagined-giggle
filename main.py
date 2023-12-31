dcd003abb94d8a0c235186046e79d3515d03ffbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:27 +0000

    inquire before muscle object involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
