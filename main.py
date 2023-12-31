627169280ae56891f31f6b170009b9a462b577c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:34 +0000

    form fear whole fair underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
