26d214a4e09230b18214035283e07b853d6e8fe5
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:39 +0000

    tread confuse tear bit hurry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import datetime
-print(datetime.datetime.now())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def print_python_version():
+        return sys.version
