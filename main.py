d8f37ab59dc223ca41407f7df5ec3f5505e8ca5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:22 +0000

    now objection unable opposite kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_max(lst):
-        return max(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
