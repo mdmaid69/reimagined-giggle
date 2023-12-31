39feed1c9568fb98be8d62eef262db80f43ffad2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:59:54 +0000

    pencil leg change control workon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sys
+def print_python_version():
+        return sys.version
