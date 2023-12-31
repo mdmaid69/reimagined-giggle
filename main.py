0b5a9a9251387018f92a1d3f2df4ed2fa5f30bc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:13:33 +0000

    advice possibility calmly success point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
