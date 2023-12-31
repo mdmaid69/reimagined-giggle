e53730eaa5900a0fea8d0c290ad7affdf0e26a14
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:37:40 +0000

    report bite process hit Cc

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
