6f02a654a2ff3e054f4c73082ea1d012fe3f0a9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:23:49 +0000

    later basic most aim run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
