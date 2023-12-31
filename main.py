7d53cfd47c7771f97bb0776b9ddc2043d68ccb0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:04:35 +0000

    decision weigh storm know organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def create_stack():
-        return collections.deque()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
