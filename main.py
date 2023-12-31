c2c219d48e11be938ced986d2697cc2616964c01
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:35 +0000

    total morning factory consult along

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
