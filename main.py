77148ea050cdb04aaea01abc62e427915e12a1d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:45 +0000

    rush everybody lean grand mistake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
