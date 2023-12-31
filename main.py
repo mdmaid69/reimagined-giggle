9c53f3ccd102a73a79bd1d1893f9a7e97c36cd6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:12:18 +0000

    brief gentle favourite never let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
