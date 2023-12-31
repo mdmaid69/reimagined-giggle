91a7c0dd3bfd4d57c5c4bbf5795a9dcf69229ea7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:40:06 +0000

    practise war ought become purpose

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
