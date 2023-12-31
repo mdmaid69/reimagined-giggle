726ba3262a3b25e0b7a245ea11ba734edb78eaae
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:30 +0000

    steal belief basic stranger makeinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
