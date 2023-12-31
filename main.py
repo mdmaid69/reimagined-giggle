915062ea6f37662f1a2c8f93b8c7407621d615cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:00 +0000

    earn admire thankyou regret combination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def remove_duplicates(lst):
+        return list(set(lst))
