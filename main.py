48b7a7f901b454e5b4600027b9882a96e304af62
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:12 +0000

    neither talk behind sensitive effect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-print([x**2 for x in range(10)])
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
