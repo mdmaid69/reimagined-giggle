36d355d8fbf2132a20c4a6da2799a29c99610358
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:48:39 +0000

    pretty belong exchange hello period

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def find_max(lst):
+        return max(lst)
