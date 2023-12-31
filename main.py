e0c02d525e3ea42978aa9f24a6b6914e6d4ef9a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:55:29 +0000

    save he expensive lookback(on) Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
