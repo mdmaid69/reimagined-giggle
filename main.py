2143dbc0c300d86f71af13948209b98d84542814
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:37:40 +0000

    ourselves date tend visit oil

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
