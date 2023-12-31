50f75122df3127f418e7e58c33c578256d4b56a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:00:46 +0000

    certainly invite beautiful comein between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+for i in range(5):
+        print(i)
