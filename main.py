735bb12aa2eb31d177c5dccf9c1ae91de658474e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:30:54 +0000

    deaf maintain factory nor star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
