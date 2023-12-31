bdd7bf1dba5b0494b63d7f59d73457ab7371aeff
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:18 +0000

    best usual discussion heavy urgent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
