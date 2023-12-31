3a5585f16bad9ef456e8b8aef0b54449d2497d0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:14 +0000

    guess disgusting sweep love until

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import sys
-def exit_program():
-        sys.exit()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
