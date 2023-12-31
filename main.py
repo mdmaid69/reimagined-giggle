bec679bac2ae8be9181fe8fb42fcc9df8148dc43
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:44:33 +0000

    move I priority discuss chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
