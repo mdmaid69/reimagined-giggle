4d6eb2f09255bd45c640634a67adc8652b67b0c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:21 +0000

    company change sensitive twist runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
