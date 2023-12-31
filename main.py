3993f1d319acf5f8605ba78dfb5add36d9767571
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:00:45 +0000

    grade advise sail behind possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
