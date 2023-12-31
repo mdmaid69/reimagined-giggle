b8de1b5fadc1c665c11bbda2ba0278c812ac31cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:42 +0000

    bush about delight score rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
