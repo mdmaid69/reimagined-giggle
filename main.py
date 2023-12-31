7eb31f87e67059e38abbcab32baee89ea10a0121
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:04:39 +0000

    pure careful autumn reduction principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
