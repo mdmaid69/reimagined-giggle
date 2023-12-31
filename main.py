3e222dce5d2cdfc1b2f0c12a63629c909af95508
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:25:45 +0000

    woman instruction born squeeze send

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(lst):
+        return min(lst)
