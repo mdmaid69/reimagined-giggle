450a1a68db89089d37664d93faef6da43ded5629
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:40:28 +0000

    ocean spray Dd claim safe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_volume(length, width, height):
+        return length * width * height
