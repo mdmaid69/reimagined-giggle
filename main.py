f5382981f38e2fc20001c5bd78034ff115d7b41d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:08 +0000

    picture offend summer note used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
