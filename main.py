5c13f79beb7c017c1fa19ae86aa7e08a4fd0b3b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:09 +0000

    peaceful talk more computer mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_error_function(x):
+        return math.erf(x)
