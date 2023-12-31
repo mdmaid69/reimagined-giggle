63f96f630cf1dc2c3d65552860e0b02fac043455
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:59:17 +0000

    about round sign warn hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
