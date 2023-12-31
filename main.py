c6dcaba479a26602a17036a0ae6a5191c6fae2e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:28 +0000

    flow pause lead smart we

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
