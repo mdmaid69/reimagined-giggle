973a107e9e1c9a37a8c3e41844efb91e936edb0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:17:45 +0000

    list desperate Oo whisper space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
