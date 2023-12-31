5e64fedd400fccae96c13e5a06c7b2cfba0d94f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:49:35 +0000

    564encourage copy charge Tt centre

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
