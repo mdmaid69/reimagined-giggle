47f004a3dffb948dc29e312af8da7d4ba4c229eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:32 +0000

    basis band tear dream dear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
