acc8779dde24e8d643e6265a4f6b76c6b062b42b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:44:20 +0000

    information sure job towards callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
+import time
+def get_current_time():
+        return time.time()
