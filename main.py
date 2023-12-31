533dc2e2349cb1899dc3e88ce99092b419c788e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:24:37 +0000

    command scream worst Yy same

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+for i in range(5):
+        print(i)
