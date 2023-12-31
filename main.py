25e93efa9c3c102cfe6fa97c4b358d4d144ce6fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:20 +0000

    type stretch steam forever minimum

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
