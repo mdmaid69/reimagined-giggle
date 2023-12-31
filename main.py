8e76fc40abdab0b326d8a307de3ed1fcd948a16c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:39:22 +0000

    Yy lost around sensible pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
