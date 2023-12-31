56a528d64ddb88e526adcd13f42e7d4d5f5f7807
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:02:20 +0000

    discussion and close know change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def subtract_numbers(x, y):
+        return x - y
