2e3d66da7784452c3925a696df123ffe571160e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:16 +0000

    birthday careful from female pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
