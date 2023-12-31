a5ded3b3a067aed35dd9cac888a6c3dd15c80f58
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:28:29 +0000

    allow comeover relief good beyond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_typecode(array):
+        return array.typecode
+import os
+def get_current_working_directory():
+        return os.getcwd()
