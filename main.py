82075b226137eb877ad0d47c6cf30d91f0c81327
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:48:57 +0000

    grade indeed reasonable ready brain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
