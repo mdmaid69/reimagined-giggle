7043f19bb71565a72596cd06a08ee06ca08eee8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:09:30 +0000

    everything autumn mail interested left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
+import sys
+def exit_program():
+        sys.exit()
