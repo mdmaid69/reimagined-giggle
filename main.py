7676accbe1497001a5e74dd1fe7b4fc7141989fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:27:07 +0000

    favour show production thin project

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
