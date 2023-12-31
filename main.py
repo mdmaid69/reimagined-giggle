dd3a2c5d07002054a6378b3726426ccb5b701aa7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:34 +0000

    mail Yy inform report operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
