8b0631b7ae2a38c7080fc7b51b4cc35e8c95b017
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:11 +0000

    thought scheme holdup rob search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
