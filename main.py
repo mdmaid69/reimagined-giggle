21d4ec78987005f0a0f005d75df47c06f096a156
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:35:35 +0000

    central getaway lock narrow second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
