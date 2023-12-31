175245fb87f2cc959d7be0db876b7c7cb180bc87
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:39:54 +0000

    splash slowly married beat exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
