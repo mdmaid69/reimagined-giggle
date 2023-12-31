b70550fb61df839ce9f878f033ba86706de849d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:29 +0000

    square consult upset organize mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
