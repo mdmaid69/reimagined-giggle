724034fc60846af7bf0eb96fb37ecc83db193ffc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:09:04 +0000

    feeling gooff available spare bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
