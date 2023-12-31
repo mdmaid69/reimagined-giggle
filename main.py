072c7e5cea7247524c20fd2944a135efb61942a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:16 +0000

    mistake size mysterious funny keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
