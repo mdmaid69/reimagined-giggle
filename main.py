d63f2dceeea2527a551d03cf44a5d8c78c4f6687
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:54 +0000

    price improvement relationship grand opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
