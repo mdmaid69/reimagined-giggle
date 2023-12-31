23f6b7eceeac9532a42da5be14fce3e510dab325
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:14 +0000

    weekend shape store win bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
