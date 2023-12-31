1d4acd297a381b90590d902b83b62bbef4f2d6cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:08:23 +0000

    behaviour thin objection exactly search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
