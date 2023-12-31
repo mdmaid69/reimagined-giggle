9fea0c2f1b6714c074987121306f9c70c1868811
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:59:03 +0000

    place wide state cough claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_set(array):
+        return set(array)
