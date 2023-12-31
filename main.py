f4ad5632ec89a20a205bece2370993d43a3d9c2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:02 +0000

    fetch section date stir herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def reverse_list(lst):
+        return lst[::-1]
