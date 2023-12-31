3358e0a0505d4f87e8c23642dd152787f1ea7972
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:32:32 +0000

    full big raise suck snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
