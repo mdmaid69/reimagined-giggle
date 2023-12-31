f05a6cb5ecfe459d28f824ca342bef47c2474c32
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:01:51 +0000

    sensitive smoke miss friendly 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
