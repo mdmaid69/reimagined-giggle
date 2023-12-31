8c248535113689a2b61c21821ce601572d5f8c23
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:48:16 +0000

    order study copy characteristic bath

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
