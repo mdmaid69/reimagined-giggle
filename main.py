136c306bbf4e00675b287487bfb69f8c90335084
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:15 +0000

    Ss rid exactly runinto middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
