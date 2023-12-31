06eb199e207b9ce87cfcc56e8d81c0f30799a08f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:28:41 +0000

    development shallow comment score grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
