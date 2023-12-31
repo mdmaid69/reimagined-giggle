ad7a5a2c7aade4878cd32b4e299ef381b49ac1eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:58 +0000

    interview ocean whatever go shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
