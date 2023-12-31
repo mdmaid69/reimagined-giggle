766db9c39b7448b196bcc14dd9a5494255045e5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:25 +0000

    giveup question past backwards bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
