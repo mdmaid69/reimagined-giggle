477e742881dac4aa88375f1bcb30c0ac74434e10
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:47 +0000

    cinema shoulder level reserve purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
