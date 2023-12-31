0679cd62da48c75050be7ed5649e57e54ad98a11
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:06 +0000

    chat bend takeout entrance modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_buffer_info(array):
+        return array.buffer_info()
