10369a4a37b142fbb53ea09f8f76ffc336422e84
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:47:05 +0000

    message shock back Zz follow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
