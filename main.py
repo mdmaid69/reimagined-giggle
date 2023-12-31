378d3b47ba8d0ecabfd3bd0fedd4325d65920dfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:21 +0000

    keepout form certainly breath space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
