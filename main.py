c7778e03e178bd0dcfb3c9e39b23437b47fbaa48
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:09 +0000

    serious accidentally turn shout manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
