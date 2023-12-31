0a39a3fb9d39b6506d749303511c4cd06e7887c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:23:50 +0000

    build single everyone backwards right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
