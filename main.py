4450873abe7c84ed1706e6170320736eac3aefba
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:14:41 +0000

    equal news callback special point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
