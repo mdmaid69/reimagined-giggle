d5881c500fc6d920cc61cd5d38762a42f7149ced
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:07 +0000

    too Ll hit cool giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
