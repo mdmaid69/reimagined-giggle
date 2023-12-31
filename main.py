a95764a08d13af68652ad4e40f7f7cf99462586e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:12:17 +0000

    drop only bar free sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
