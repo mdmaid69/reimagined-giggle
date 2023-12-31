451c419741fddea29b27886257fdd5dfd1c1a135
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:01 +0000

    special part board everywhere several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+text = "Hello, world!"
+print("Characters:", len(text))
