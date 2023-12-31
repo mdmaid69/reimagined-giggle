1f65201de800bb541cba0860b638cc51150429cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:56 +0000

    job interview persuade target grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
