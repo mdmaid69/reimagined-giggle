d5f7348cb056d92813dfba889d349efcec1795f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:58:27 +0000

    you immediate taste cap speech

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
