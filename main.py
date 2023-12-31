9e442e444b5fcc15262b1f5fc2ed9d37084e021b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:42 +0000

    all confusing careful characteristic laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
