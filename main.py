46024520641fc96d35fbe125a34dbf49c4329c56
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:26 +0000

    Yy still score card Nn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
