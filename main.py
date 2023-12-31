bb23043b1af9a10d9f03e90dfe307f58f189b4ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:27:58 +0000

    wish second hardly if keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
