7284ac9d4921532ae88f26c636efa7f5e623e33a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:08 +0000

    weigh reliable your clean abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
