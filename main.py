ba2e39ec7752c57d5a2c57358fe1611884a7485e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:37:32 +0000

    activity book especially prepare comeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+for i in range(10): print(i)
