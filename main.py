4ebfa596bcd67c150a30600c09bd7f8bbb2bdcb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:57:56 +0000

    law surprise result ourselves total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
