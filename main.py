51d7c9ebea48b5ebddc651cea93dee7c399f97dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:32:26 +0000

    maintain tool wild doubt subject

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
