cc2fd5731baacca56b3ea6977b2c0d1082d9a54d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:49 +0000

    popular letter event conversation suffer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
