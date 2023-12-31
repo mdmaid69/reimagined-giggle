b501d0a7cebc1c0b9de6373b5858a0b848160f90
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:10:08 +0000

    especially keepoff study measure run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
