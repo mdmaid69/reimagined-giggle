b00db8d55649eda1766caccdbf963f95fd060578
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:51:12 +0000

    towards upwards short dead tap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
