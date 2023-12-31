9e553fcc8777dbf4dfab83082fcefee7e51e8dec
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:58:19 +0000

    heat single arrival university order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
