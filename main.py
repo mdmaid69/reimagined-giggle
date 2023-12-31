818ba44511679a7c0e1b82ef6081798c52f6fb83
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:08 +0000

    food 569enjoy exchange tomorrow heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
