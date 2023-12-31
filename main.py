913bdf5c46d8748043cbb11d232b831d6793f444
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:25 +0000

    frame Ii Qq borrow still

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
