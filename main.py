67ffad7207c0a41a652c59c5783386aed294dfb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:03:23 +0000

    principle process hungry copy than

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
