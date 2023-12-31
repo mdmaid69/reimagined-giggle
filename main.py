fd9b70dd852413c17f3a004e2c0c51654e74543b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:12 +0000

    match deliver pure off start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
