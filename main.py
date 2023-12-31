dd539e5057020bd1bdaac59bdde2a92985d94484
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:01:29 +0000

    sail short influence ill drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
