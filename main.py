f422457f2513b59c8a079f170c7e3a1b83573f28
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:59 +0000

    wide little pronunciation born pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
