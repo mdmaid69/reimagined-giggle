f8752966fedb9ccb0157efb9e3e8409578b75e44
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:53:03 +0000

    gate fix either single cross

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
