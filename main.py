ca7541d9e61105705cd3bc0399a1371c2366d2d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:46 +0000

    heavy question score left between

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
