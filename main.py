5d385aa96a9fb1c423d8587d936669a38fedfaef
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:41:03 +0000

    hole protection gooff tall throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
