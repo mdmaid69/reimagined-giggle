9cb3175c001f92d00111ef682e9159b7972eea5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:22 +0000

    wherever prefer Kk pass best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
