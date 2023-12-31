6f746a73b70ed80c4eb7ca4fe9a413d20cb63e48
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:34:15 +0000

    consist easy develop blame bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
