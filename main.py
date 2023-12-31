8dbbaacd42f8c61e5c55d7354198ff133c40344d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:14 +0000

    television low last pin ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import getpass
+def get_username():
+        return getpass.getuser()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
