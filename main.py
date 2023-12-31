e8788cb01199da7abdb9522def383e0b39737e3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:34 +0000

    block rub watch eye shape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
