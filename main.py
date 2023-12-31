39f1beadc94d0c06757a07435d9be41891ee8681
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:07:49 +0000

    increase afford breakin sometimes roof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def get_array_length(array):
+        return len(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
