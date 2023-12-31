935d8fc460e05de869e0489d99aad793e409bdfc
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:43:45 +0000

    last juice ought they clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_current_directory():
+        return os.getcwd()
