0a86a714f26db7d611c59a8d90411505b7a85930
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:22 +0000

    peace back everything embarrassed much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
