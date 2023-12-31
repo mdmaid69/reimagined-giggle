91885a73d067144b9b0344527cc1fbcf3f4eb732
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:05:50 +0000

    muscle whom necessary divide keepoff

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import platform
+def get_python_version():
+        return platform.python_version()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
