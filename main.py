f3fcdb371106690b7fda43e97b673389652628e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:39:54 +0000

    spoil night risk switch increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def convert_to_hex(n):
+        return hex(n)
+  def is_even(n):
+        return n % 2 == 0
