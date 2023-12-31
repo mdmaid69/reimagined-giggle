cb6ca2c45013bbfe69e519502b2b4a96b97318ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:26:57 +0000

    machine share foreign activity much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
