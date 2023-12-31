6f5277520e19c42cd7271a5e478b4c21e9fd57cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:11:53 +0000

    couple strict great contact target

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import matplotlib.pyplot as plt
   def plot_histogram(data, bins):
         plt.hist(data, bins=bins)
         plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
