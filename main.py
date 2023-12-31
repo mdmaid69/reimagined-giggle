234d27df8a7350c0d93cf555ab9bec2583f1b7b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:38:46 +0000

    copy through house score find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def cube_number(x):
+        return x**3
