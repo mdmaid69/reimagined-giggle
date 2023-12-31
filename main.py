e95169165655131a2bdea220d8a7a25edc4135b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:22:23 +0000

    event moment money repeat sharp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
