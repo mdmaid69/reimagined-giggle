136a264cf74e8bbd6e192c2067dabbc43f7b5c51
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:57 +0000

    one Hh exchange praise start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
