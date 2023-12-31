73d9816b61474cd74eefa4280976cace586b4920
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:10:49 +0000

    guess switch practice way release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def is_palindrome(s):
+        return s == s[::-1]
