9468aad9b7bc6dfc32813b8bde63544788887e66
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:53:10 +0000

    search exciting heavy try eat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
