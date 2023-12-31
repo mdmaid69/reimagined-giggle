9a33bdc6fca793e1925180a9ab21e4bd92323d0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:56:45 +0000

    description shut take willing not

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
+def find_max(numbers):
+        return max(numbers)
