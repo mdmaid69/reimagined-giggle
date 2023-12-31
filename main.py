128b8f5c408a5a67da1e407a1c0792bf85247df1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:50 +0000

    be careful ahead chance action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
