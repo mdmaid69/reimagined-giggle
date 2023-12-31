80f683768ba5f534dbba6f39a241b423f45c2cd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:37:46 +0000

    classroom unless damp bound supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
