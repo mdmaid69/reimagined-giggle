32ee58a2c4fcac79d9000889a7da3e0c1a91a7ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:52 +0000

    offend other set them error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
