afc3c7fb31b1eac2aa9ca32e25eeb683077dca12
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:06:39 +0000

    by objection provide petrol breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-print(os.getcwd())
+def reverse_list(lst):
+        return lst[::-1]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
