07d63f2ebf0ac2ea9971d429c9e41e59b8f1d566
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:48 +0000

    rough stiff basically Ss union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
