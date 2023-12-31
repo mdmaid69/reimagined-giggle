6d28bc25455fc1c4b3699d2f25bffdd26505ab8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:38:58 +0000

    path tool fact conclusion shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
