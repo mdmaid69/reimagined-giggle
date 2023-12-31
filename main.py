3bc67f596ab716587903381664d48d970210dfaa
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:15:43 +0000

    cold lay art method software

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
