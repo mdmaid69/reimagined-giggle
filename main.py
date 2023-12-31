4c9455227abf817cfc4449b8525d4d22d98f17b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:59:50 +0000

    convince holdout tell cruelty butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
