414fc945d0654a74b762748035b570f085e9d1ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:21:42 +0000

    thinkthrough about unlikely instant ticket

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+def get_current_time():
+        return time.time()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
