ba56722d2e3a25c323913a5e50eaa3686948c6c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:42:41 +0000

    talkback consist spend burn remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
