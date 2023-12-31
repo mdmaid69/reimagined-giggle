2ad06ba0b603a6084f403e3babb8f7947ed42633
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:48 +0000

    yours sorry before old in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
