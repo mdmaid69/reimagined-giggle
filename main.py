22eed8257f6c7cfb42f940879fa6ba87509c96ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:13 +0000

    influence introduction intend exit quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
