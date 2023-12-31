f5a9201f235b11cc4ab626745d9893675e00ba46
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:00:37 +0000

    worst length quality announce standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
