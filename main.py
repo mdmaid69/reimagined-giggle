5ded79027b02a2d380ec4c186e85ae7b2be6cf68
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:28:13 +0000

    race fact find decision man

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
