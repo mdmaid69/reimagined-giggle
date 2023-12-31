e884336283fe485417f74509b77e63e66fd73486
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:02:11 +0000

    professional disagree spot recommend press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
