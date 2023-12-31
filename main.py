020d573f8aef6c91d2e35d2a450ef5be37664c09
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:55 +0000

    belief part noone pronounce arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
