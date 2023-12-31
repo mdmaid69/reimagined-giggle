9ca369c944150170eef9ae161dc36d02e5869b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:02 +0000

    on past married help print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
