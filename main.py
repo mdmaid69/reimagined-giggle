f8124dfac77f7ae972396ce29eb4c5efb01129a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:57:19 +0000

    cause prefer surprise choice too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import platform
-def get_os_info():
-        return platform.uname()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
