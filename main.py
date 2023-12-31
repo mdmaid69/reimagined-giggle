4c3aaa4ee30248b276a004b702c07861ce906af0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:33:18 +0000

    workon keepaway change side ready

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_work(force, distance):
+        return force * distance
