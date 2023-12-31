f64825b6f2c54938a60c09024f24c7003084467a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:21 +0000

    colour machine keepoff fashion direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
