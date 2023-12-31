0552c6bc59c036b2d5bd0ff36bffda9f61f39750
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:23 +0000

    excuse arm available over poor

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
