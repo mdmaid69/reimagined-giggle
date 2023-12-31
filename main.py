b3ad4f64bc56daaf9a880df84a6ad0d5be170936
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:33:18 +0000

    callout warning importance somewhere bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
