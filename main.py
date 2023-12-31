086f91c37be4dd7ac791e08a0f9c2c297ce07f39
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:48:11 +0000

    hope judge boil lend innocent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
