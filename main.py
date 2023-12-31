08cae7c6d506386f394bbc5a8b266138e1e2e31f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:09:42 +0000

    patience early final science guilty

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_power(work, time):
+        return work / time
