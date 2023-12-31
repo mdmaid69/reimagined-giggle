6c4e7e4bfbcd1ee129fce5c8010cfeabdb903af7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:45 +0000

    measure lower odd use far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import array
 def remove_from_array(array, item):
         array.remove(item)
