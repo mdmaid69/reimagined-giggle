1aa40682979c2e158d7e04e7fc5f2b68c2b204ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:50 +0000

    his hurt hand handle impressive

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_max(lst):
-        return max(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
