24f86f9dae9b8e437fb6970faef43c86fee0f520
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:55:14 +0000

    rush fast clever replace neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
