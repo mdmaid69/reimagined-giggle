d173d13676f921fc78f3a2ebdf38dcb410f434ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:37 +0000

    there Oo dull bored blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
