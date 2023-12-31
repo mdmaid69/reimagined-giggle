b303b8089623af266a65bd0f44d7c137022b5a66
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:44 +0000

    evidence value report comeback side

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_area_rectangle(l, w):
+        return l * w
