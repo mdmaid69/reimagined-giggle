fc540cc543b00f956091a75b1ca2c44b4adc0986
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:07 +0000

    mistake plan breathe face man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-x = 10
-y = 20
-print("Sum:", x + y)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_odd(n):
+        return n % 2 != 0
