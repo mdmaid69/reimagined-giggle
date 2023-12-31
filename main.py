da9860ee17812041dacb48491272772fe7ebcd6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:01 +0000

    in money market especially awake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
