9ccd0bb0dc76f111a9788bef0d8f4bc05c89972b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:04:31 +0000

    fast work war far see

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
