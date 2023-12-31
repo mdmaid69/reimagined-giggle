b8fa910e97b38886def58ba340728487fca5d76e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:00 +0000

    main do anywhere alive spring

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
