3ff4e566104d6b7f89f03d4e8cd360c02f5368db
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:44:31 +0000

    hand regularly address doubt split

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def subtract_numbers(x, y):
+        return x - y
