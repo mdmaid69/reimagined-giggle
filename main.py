736b306afbd571a3354ac7ac46debaf61b24a70d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:55:45 +0000

    comein hardly profit confusing debt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
