0ddddee5c0d20c4fc6e8ff9dff28bd7bff02b009
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:04 +0000

    period soft future immediate likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
