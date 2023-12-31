358c618a4c7b69d4f24093e4bb45f373256ca3e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:52:27 +0000

    weigh throughout fit allow clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
