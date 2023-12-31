90505d775e3f59a27c5f3ea3143200da1126c109
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:10:04 +0000

    pay confusing occur study ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
