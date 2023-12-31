8dd0ece683624de2e789ae3882b3085659d63d5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:54 +0000

    share certain arrangement defend behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
