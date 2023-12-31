a627fc445bf9a0f4da9bdca049adcd9ba8513ed5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:44:52 +0000

    patient hour herself hard settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
