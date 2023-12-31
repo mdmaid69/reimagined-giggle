7cbb08dde694c312361e22313c26d4190156fcc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:41 +0000

    plastic everybody correct extraordinary politics

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
