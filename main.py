ae2b63314bf3cad3f745d31a42f96488ee38bffc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:47:46 +0000

    giveout beneath unless understand central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
