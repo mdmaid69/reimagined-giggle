6608f58c5e90c042985b9f2c155c27db8913c765
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:47 +0000

    oil neither bound stretch performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def convert_to_hex(n):
+        return hex(n)
