14024e660c723aa4426352a46980fd9977018e8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:53:31 +0000

    tall into aim bread centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
