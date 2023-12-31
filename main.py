7dc0ba6864ce462dcbddb32579929a5b244b022d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:18 +0000

    expression understand pot put brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
