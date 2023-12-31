9b65b3087f15f4f6b3464a37a65fbcb37de099eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:11:39 +0000

    cap cope wealth shame to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
