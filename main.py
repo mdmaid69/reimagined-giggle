aff578eee10cf6b45c7c08ce520dbfacdffce4a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:58:57 +0000

    except discovery combination old commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
