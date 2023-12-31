24f9d8ffb3f304bced3ba1d13d523a2c7cda8852
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:21 +0000

    connected before separate ear load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import platform
+def get_os_info():
+        return platform.uname()
