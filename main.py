d19aad5575fe25bff658863679dacb4211f8390e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:08:33 +0000

    crowd soil tend cause paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
