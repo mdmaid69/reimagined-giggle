df8e1c70f5710529b6141f0da366130c181cd40e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:19:21 +0000

    mass break basis agreement hear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
