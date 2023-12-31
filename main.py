c7be9b7ee16f52a07f2fddf006c5497f1f7dc90a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:17 +0000

    again busy down market left

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
