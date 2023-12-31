9e6d2d12797dbd4eb79379af716f13b8a8da56f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:54 +0000

    upwards respect lesson experiment dance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_list(lst):
-        return sorted(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
