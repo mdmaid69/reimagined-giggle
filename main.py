32f7f7117735b4340582cea8fe6ed1b9fb62ac3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:29 +0000

    strength crash command although custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
