0e8fe5809517072116f1cd77ee2071ab5295c3a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:57:22 +0000

    represent plan smoke pair development

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
