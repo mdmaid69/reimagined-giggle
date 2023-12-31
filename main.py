fe0602afee44e4a3bb695438c83842f9687b3083
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:31 +0000

    calmly crazy situation onto leave

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
