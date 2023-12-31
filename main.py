c02fd185483e0f76968dc9c7f9affcdba84bf60e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:19 +0000

    steep leg freeze attach except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def convert_to_binary(n):
+        return bin(n)
