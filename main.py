d2923e3465c5062b6e7063b744fae3a5413cfb35
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:14 +0000

    Oo experiment stand onto path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
