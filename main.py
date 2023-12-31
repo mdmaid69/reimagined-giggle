7aff1bc0bc9351eea595933b1eb9fd8f83ed8156
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:54:55 +0000

    Nn pupil flat help drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_item(array, i):
+        return array[i]
+def reverse_list(lst):
+        return lst[::-1]
