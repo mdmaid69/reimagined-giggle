72d720e7d808af8da865b52184d5d091ef97a90d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:10 +0000

    adjective react tooth pleased row

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
