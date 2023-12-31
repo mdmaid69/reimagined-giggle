b6109e0198d1c3e12e79afa19d2070973b1736e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:28:45 +0000

    drop naturally hurry geton soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
+import math
+print(math.pi)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
