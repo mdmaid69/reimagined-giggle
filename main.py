3d110f56ce7481cad62cde21dbba6a909d27ae27
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:50:48 +0000

    sea sticky feature snow transport

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
