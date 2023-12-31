f4f45d86fdb18dfab388f9138a243b06f1815f2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:10:58 +0000

    maintain confused nothing personal hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
