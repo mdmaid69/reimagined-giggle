b3f533d0bd6292c2c81876c40dd00293c4e03627
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:33:40 +0000

    unhappy summer reasonable desperate opposite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
