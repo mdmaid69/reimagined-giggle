e2e22bc51a1537a69fe1822cd1a1a8674e5156c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:42 +0000

    forward little proud touch sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
