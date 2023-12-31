58663ec38df4fb6d6c8216937863c7f359e2ad21
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:25 +0000

    if convince early means whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
