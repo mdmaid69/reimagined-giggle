10e2ff07a950c38cdcee2a81617187d7015277af
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:40:53 +0000

    need stiff certain land deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
