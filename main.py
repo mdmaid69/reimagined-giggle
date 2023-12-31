4423cd92d1551b30a4b9ee6d728a164584584ca3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:06:19 +0000

    high sister pair threaten reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
