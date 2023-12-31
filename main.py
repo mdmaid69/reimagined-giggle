e3763ed286248317a0fcdbd6e8657e826f33e0d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:57 +0000

    bar part shrink equal however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
