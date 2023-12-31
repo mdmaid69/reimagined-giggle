524102bbeea64b6ffa6bbd796fe42cc2be2f450c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:21 +0000

    will dream comeout even child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
