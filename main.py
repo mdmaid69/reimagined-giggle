eef35d58d5a994d365e8dfc2f6be0b5717573f4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:36:15 +0000

    cost interview help much clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
