352072f29dbcec45073298958b30507864a64878
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:22:21 +0000

    care switch defend believe sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-print(math.pi)
+def find_max(numbers):
+        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
