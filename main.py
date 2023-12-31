ca0191824d57a68ec0bf87bf28e7f985b759eaf5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:19 +0000

    form delicate 564encourage wide why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
