c4c729bfe644a036a76a0242c497073f4b6cb94b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:42:48 +0000

    however small sure cross attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
