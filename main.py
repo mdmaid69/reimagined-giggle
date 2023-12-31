4d810b69b755945e4e72817ced9fc39dd136916f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:54 +0000

    repair mean fear if back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
