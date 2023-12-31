e55b5bd1f4eabcfc11853037a3e98f4e942b3155
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:31:59 +0000

    mad ignore duty father skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
