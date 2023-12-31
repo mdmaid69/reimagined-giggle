0c70dbbcfd791f7b48889bc2b8cc958db66233d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:37:30 +0000

    mark problem order school favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
