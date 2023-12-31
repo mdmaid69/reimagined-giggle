3154559b44db65171de891eeb347db40e3653340
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:37:17 +0000

    actually press much sensible woman

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
