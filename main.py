60b9bc13d2390b13829717383081c0fcc07a0c54
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:34:45 +0000

    scream nose curious gooff account

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
