97443eec9015a6bfbdfff5cb5e98c4aab90223bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:50 +0000

    thin big worry reach offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
