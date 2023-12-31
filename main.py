1a30003e3297a7e572d3349edc86ce78fa37f12b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:10 +0000

    payment circle bound him friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+print(os.getcwd())
