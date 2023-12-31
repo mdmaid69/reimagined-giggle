9c1864418977b2470c1c7c1dfffcfbf3349669be
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:26 +0000

    such discussion rate pin can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
