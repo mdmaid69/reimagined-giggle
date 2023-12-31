3ea79b429e07049f05661fcf2ebdddaee6569ea6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:35 +0000

    funny walk unpleasant forever limit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_typecode(array):
+        return array.typecode
