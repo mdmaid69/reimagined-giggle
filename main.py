29a28e91c59d170bc759dba112781a748f345230
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:28 +0000

    present cover skill politics sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
