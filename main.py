7c0e909ff8c24c8ca2fddda0f930f682ac75f1d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:42 +0000

    generous hard black sharp sorry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
