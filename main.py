d7a1be578c6732fc208838c7c39dbb79b168636b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:03:48 +0000

    scrap grateful calm nerve dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
