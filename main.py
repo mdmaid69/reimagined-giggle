702d2535e1fdf1b6e3729252de6d9383e20262f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:03:31 +0000

    complete among bring stupid edge

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
