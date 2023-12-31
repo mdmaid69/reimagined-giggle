548f39e3fa49454a05bf2b64fee9e3eb096c8e21
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:25:42 +0000

    all measure dress that escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
