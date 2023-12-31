4068cc39b93ad6fdd5b127fe0cf862504687542e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:25 +0000

    welcome carry lost favour strange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
