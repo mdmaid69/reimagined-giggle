decff9e207f3dad4cea4d3e75ba8cdc3b7797c9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:31 +0000

    decision destroy mainly sink eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
