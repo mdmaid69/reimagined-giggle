db28060259b26a5235f6c87fc4a34b532819bd30
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:27:28 +0000

    letter row brown forever divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
