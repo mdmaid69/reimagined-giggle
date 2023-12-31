56c7308057c54247ca3f7cd02323eb6481b0f637
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:15 +0000

    sign allow bin check importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_base_name(path):
+        return os.path.basename(path)
