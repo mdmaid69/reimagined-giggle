0917b44725839b020c6b20598ebf4c93b8f85f3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:44 +0000

    joke drawing but reasonable bye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
