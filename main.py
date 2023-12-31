28b24d248f3ca349a231c5a152b1bd6c2e9367cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:28:49 +0000

    book touch hardly fall our

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
