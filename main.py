b78c333c2e8c73a78c2ef1868dc70af262d53603
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:43:18 +0000

    correct target choose job responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
