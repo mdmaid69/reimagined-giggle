2eb756213531f7f8b52e650dc054082597a25aff
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:20 +0000

    extraordinary scrap doubt mouth or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
