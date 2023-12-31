4589d24d1ccb7f559d960eea90d2a31bd646d9f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:35:19 +0000

    everyone news themselves authority point

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import time
+def get_current_time():
+        return time.ctime()
