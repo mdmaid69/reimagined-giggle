8be6869621fe592c5bee95e37f1712417737f1cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:21:58 +0000

    moment frightened shoot half advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import re
+print(re.match("h.*o", "hello world"))
