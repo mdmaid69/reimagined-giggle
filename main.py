dff35b2c1513f7921c290c2c2bed494a14e98482
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:20:07 +0000

    no sting story long avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
