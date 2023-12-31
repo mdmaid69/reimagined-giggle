4eb7c07d9961ecfab18ecf264abd65fa4b5ce61e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:05:29 +0000

    runinto silent cause getaway smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
