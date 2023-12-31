8a6adbf58c9877d85d423f771f9b83188f5fe1e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:09:28 +0000

    cause fence visit rest dull

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
