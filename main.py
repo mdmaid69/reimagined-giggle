5bc90fe063f34a8503eee72f20bbc51fecdc952b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:31:21 +0000

    minimum gap drink breakinto back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def convert_to_binary(n):
-        return bin(n)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
