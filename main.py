e6e1fdc5520e17fa2b853ee9faa5fda0f0fdcf2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:26 +0000

    bill tap lower promise measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
