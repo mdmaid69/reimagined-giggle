6233cd1bf522f2a3a5d9e52a8b47f942086214f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:26 +0000

    warm work accommodation dig exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
