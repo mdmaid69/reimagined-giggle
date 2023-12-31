7a097a97f58ca189b45a937f4d5c890f933180e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:28:49 +0000

    top commit about cut quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
