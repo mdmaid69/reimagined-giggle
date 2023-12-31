3e4ea573ebe6822caae4e6ed6a016c72e6b57bd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:36:29 +0000

    feeling meanwhile holiday lost university

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
