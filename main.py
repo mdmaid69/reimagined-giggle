fb796c5acccd36d8f65914932883d5f285b038eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:21 +0000

    sell speak day stone breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def add_numbers(a, b):
+        return a + b
