91ca5de36ddb063c0c225c525143c82a89b31c5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:13 +0000

    estimate divide flat you comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
