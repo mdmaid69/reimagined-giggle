7c82cd39c467129974ccecf3e7f05c6754092471
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:13 +0000

    congratulate trick ashamed fork circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
