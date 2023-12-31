2f93e5522fc1518593dfd1cd871eb7fc7ea38926
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:19 +0000

    action thick how walk small

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
