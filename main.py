01147141dc8bc09978aae34dc12fbcac68786cf2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:08 +0000

    change may mixture closely ashamed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
