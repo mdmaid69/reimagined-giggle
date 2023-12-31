10758fe49566379287a9f6b97558fd4be3992225
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:01:43 +0000

    contents food bye break wild

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
