f0deed82e640b9f523f4e6588e735660aadad64d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:52:11 +0000

    neck dinner book clear discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
