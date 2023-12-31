94d86bb3b4e7d7a217bb538de7f5e65f5893b5c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:03:07 +0000

    left clear Yy suggestion confuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
