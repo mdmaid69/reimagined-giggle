3ecb0a31911cf6d2b5991861a5ef67e77ac88512
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:36 +0000

    as phone hit roll pass

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
