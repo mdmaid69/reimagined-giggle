136f4f7a8ed379edd2a6a574c3900039dabf7829
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:28:33 +0000

    finally regional speak immediate copy

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
