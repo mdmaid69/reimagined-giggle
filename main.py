be7a1f980a3eebf127494f400c75e5cef4552f9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:01 +0000

    getaway throw runaway surprised for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
