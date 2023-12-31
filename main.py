28bfee26383a4e3ad92ecc4ee16f932dcbc8ce43
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:07 +0000

    harm basic style complete tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
