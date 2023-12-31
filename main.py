31d87a4fcae07d0fa6da097ae2f35763cb631295
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:13 +0000

    whistle expression conclusion form instead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
+  def square_number(x):
+        return x**2
