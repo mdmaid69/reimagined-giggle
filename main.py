5fda5fb883f851feec227db6072c110f4d48f0ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:38:05 +0000

    basic Oo hand duty bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
