0d7796837f1aaca2760af42496a395d831a7e86b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:13:08 +0000

    old suffer substance explanation slide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import json
+print(json.dumps({"name": "John", "age": 30}))
