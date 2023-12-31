576b69b10ac79fe1904efa54fad20c57c1b9411f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:55:26 +0000

    reduce square Dd cap half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
