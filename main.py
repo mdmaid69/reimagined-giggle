3542f26ff9df2ca2d237ccdca8b333ff79a27947
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:22 +0000

    mistake wish match health bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
