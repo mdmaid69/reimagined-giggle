52bd8c3fd9daa6fcd5c65b2ed8d507c783bb22c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:03 +0000

    strict lookup nonsense occasional disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
