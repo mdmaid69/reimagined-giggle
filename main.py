8b3eb9a071085c3895255eb9b4dde0c334ef72a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:51:40 +0000

    temperature task since will responsible

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_volume(length, width, height):
+        return length * width * height
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
