d04759321486f259d80f6f8dd42845036823c0d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:33 +0000

    Yy generous unhappy invite probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
