462a0cfc6a378f3420400ec9071b38627447df52
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:59:03 +0000

    happen shut apart choose immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
