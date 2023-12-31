07222341b58b9cbaf90ee6687a91122a3c5d3adb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:56 +0000

    aim drink fix shallow share

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_counter():
+        return collections.Counter()
