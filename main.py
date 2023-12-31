c9d09479fdc24bc9602e25b4a7e81ab85276748a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:39:51 +0000

    cum century discovery language street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
