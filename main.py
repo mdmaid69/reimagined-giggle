5c46f1fbead863006f50b6949deea535c56db27b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:54 +0000

    lean flight taste promise friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
