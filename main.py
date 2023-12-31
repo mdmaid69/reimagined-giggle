df21dee2ec056518dd5f9ee231db7e140b975bab
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:37:17 +0000

    572enter left stupid favour cook

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
