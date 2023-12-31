5feece5614060b6a3e1812ca0ed64b102a42c12e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:48:00 +0000

    standard increase die lay clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_factorial(n):
+        return math.factorial(n)
