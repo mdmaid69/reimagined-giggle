4c1806c9ee66bdf2722925c33183d796ec90f195
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:00 +0000

    if runinto label deep chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
