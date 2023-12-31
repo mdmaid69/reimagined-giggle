8c9ddc6048f988dbeec5d302a39baf2d107a4b3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:02 +0000

    level imagine safe underneath platform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sys
+def print_python_version():
+        print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
