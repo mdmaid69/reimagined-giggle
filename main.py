b65f496b32a6919acd380f86bf4570b2c5ee2425
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:07 +0000

    progress necessary face form the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
