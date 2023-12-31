fa053da137519a3bc3bcee36209a20c3ecd23e99
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:06:19 +0000

    branch forward correction movement his

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
