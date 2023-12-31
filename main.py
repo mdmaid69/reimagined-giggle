df7fe70ec3d831313b281eb6be40bfe24079c06e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:24 +0000

    cook upset post pleased tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
