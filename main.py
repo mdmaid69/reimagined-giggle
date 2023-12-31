1ecf9abe452347e972408436d10ab102eb66b4aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:49 +0000

    put thanks crowded behave comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
