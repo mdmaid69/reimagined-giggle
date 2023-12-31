91a299d9329003b4ab0dee8dd9c6970821796e94
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:41:45 +0000

    burst scare tie interfere react

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
