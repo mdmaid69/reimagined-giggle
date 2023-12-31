002716b42ece8ee5106f44b4c484a569dc21718b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:40 +0000

    only across shock recent ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import math
   def calculate_square_root(n):
         return math.sqrt(n)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def remove_duplicates(lst):
+        return list(set(lst))
