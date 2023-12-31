09e60f99f87e234d185dd96b07cea73053bab7fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:53 +0000

    free performance shrink sticky gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
