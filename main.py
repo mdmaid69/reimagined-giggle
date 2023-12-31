15f45590c96b10c6fe37aa28abbc7e8e5ca230fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:23:40 +0000

    bend nonsense able experiment breakfast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
