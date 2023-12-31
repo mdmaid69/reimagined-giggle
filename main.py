36ffa514ac9158762e88eaa6564fcec4bf73f6f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:36 +0000

    Nn very makeinto gooff disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
