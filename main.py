baf85e238afb2b243f57e5a8a7281eafa1d0bc05
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:54:41 +0000

    that Hh where pass disapprove

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-def convert_to_hex(n):
-        return hex(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
