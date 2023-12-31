048fe3f258ef4c44bd8804ebc4119c504b3d4529
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:48 +0000

    safety gentle man long bother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
