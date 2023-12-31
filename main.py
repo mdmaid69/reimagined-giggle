133b62776fd85d5e1d21550c7da4b553030df41c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:56 +0000

    use famous pollution properly think

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
