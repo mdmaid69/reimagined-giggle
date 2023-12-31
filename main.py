31009afca07e2ef71489ad5c2448b20864f28b09
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:35:30 +0000

    scale please permission past hate

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def convert_to_octal(n):
-        return oct(n)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
