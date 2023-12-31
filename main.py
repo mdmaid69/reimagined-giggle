30d6ab559c7eb7f65cb1d61ccf95b4b9c372e4ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:34 +0000

    lost fix pattern interested thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
