49b998ffb51de492251c162c7bbd950a74cf6114
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:04:47 +0000

    loss born project stop pleased

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
