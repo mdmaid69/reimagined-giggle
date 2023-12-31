7d413e7f477238f5cabaf3b540c0cf7e86576a6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:53 +0000

    target operate family threaten check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
