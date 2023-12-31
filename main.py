d23cebeb859c742e154f38c3c3b7b4ce420d8581
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:36:37 +0000

    never wind stare hurt mixture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import sys
+  def get_python_version():
+        return sys.version
