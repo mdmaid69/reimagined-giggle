d7d01fb8630c994abbb62fcb6a55aea06e842a0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:38 +0000

    any urgent suit avoid statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
