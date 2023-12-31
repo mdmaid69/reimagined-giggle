3be6a4087a506cdd6c991a6bac2c6b55a989015e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:48 +0000

    common bin physical bread left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
