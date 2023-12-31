3e95edc65db66505018cb4e1454a312f1f0f32be
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:23:50 +0000

    reserve decide order exciting judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  def is_even(n):
+        return n % 2 == 0
