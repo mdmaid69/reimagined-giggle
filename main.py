20898762d05159ed3a478e8267b443ac24511e7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:32 +0000

    no bite future operation least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
