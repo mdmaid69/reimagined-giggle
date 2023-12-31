31699bf06837389682c6b7df4f6fd8007952d8fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:30:58 +0000

    stage spoil owe wave else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
