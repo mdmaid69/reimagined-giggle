1aa740e4d3068c2b1ba3ff882141f28fd3da9629
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:00:18 +0000

    stir ago Nn matter check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
