779d4b5e5be652e52b8847d7059744f261b8342e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:11 +0000

    free bush odd complicated nothing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def split_path(path):
+        return os.path.split(path)
+  def sort_list(lst):
+        return sorted(lst)
