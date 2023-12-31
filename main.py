5a374930513bd86b4b12ce6a3f9724b6d0d78417
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:37 +0000

    first basic sharp offence chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
