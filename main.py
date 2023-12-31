2f492e1bc656f62704f458342d0905abd1984c30
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:27 +0000

    affect go cook scream smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
