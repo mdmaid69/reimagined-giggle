faf836f441d51f6a9c05f84106e03fa40614acaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:38 +0000

    here history section swim kind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
