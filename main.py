28c3ba00494bb7233dab025b7b696e96b11fc8e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:17 +0000

    little mark possibly strict scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
