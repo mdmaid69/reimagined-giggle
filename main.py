f21c45ead96ffc537d2d764712c7a348b77b2561
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:23:57 +0000

    sit war middle achievement head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
