d9220eeec5dbc1cc72c9425b79c58b17818bc78c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:36 +0000

    solve opposite difference air cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
