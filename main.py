f0bf6c0dc59ffb70ad29e4ab544d0a0e27c9dde8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:24 +0000

    rest comment set ask upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
