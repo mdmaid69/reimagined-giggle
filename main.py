05dbd5012a0903cd6aedca2a21770deaf78172e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:56:24 +0000

    uncomfortable fall advantage world his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
