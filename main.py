c4f4e85d2dcb5f836dfd7e1bcfc912303c7a119f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:04 +0000

    godown imagine occur split distant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
