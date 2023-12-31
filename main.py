ee48b65dfc94c8920469dc40d6bcc38abff6ca5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:21:13 +0000

    operation copy realize getback over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
