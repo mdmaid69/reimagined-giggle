429ee57a42a30e36ad96dd06697eb17ffcaf5584
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:07 +0000

    general small food taste instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
