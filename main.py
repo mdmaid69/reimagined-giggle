cc43864a831657f571e690847cb9dfd85fd2bb16
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:52 +0000

    lead plant tip children use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
