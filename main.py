bf7749aa2b1d37ade3c5d7afc2dcb4224ca96e6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:04 +0000

    system surface machine approval hang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
