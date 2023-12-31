93b6f0f7e0f52be109b369638069a6f0d84c44f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:19:29 +0000

    compete throw second use admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
