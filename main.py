14b63e02d71a4fb96c6d57ad68f4a9d8ca7bad54
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:29 +0000

    swim fight legal force debt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import random
-def roll_die():
-        return random.randint(1, 6)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
