8107863062b7e242fa7b26cfd44b3c9010bf0b9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:25:11 +0000

    pronunciation frighten transport figure onto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
