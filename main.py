e776fa45670480341cb5d5991a0b02eaa336d528
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:41 +0000

    terrible surprising part space government

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
