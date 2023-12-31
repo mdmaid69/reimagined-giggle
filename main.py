4400f42e50ce9032d45ab62638357e8ead6395c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:06:31 +0000

    keepaway by middle hungry respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
