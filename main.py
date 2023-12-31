e7ecb082f27b4c10059a567071f7bcb2fda4c657
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:54 +0000

    normal friend mess turndown act

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_item_count(array, item):
+        return array.count(item)
+  def convert_to_binary(n):
+        return bin(n)
