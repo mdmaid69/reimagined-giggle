5550c24d2891acbe344376551cfc1b8c2105780f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:53:50 +0000

    ask lunch so bringforward teach

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_max(numbers):
+        return max(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
