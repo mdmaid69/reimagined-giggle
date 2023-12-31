6e6ed9c1a0e1c4d39154f47188981d5684316eb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:55:08 +0000

    chase exciting total heart necessary

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
