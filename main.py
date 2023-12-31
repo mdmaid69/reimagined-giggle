e7ccf744ff1c02936334d2ce054cd8ef7fbe6f9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:31:41 +0000

    contrast development ought near cause

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
