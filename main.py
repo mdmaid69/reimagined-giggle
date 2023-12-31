a69ade94734f44d8d76f3dc2fcf0c7dc35d047a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:22:53 +0000

    relief pressure song pile fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_str(array):
+        return str(array)
+def count_characters(sentence):
+        return len(sentence)
