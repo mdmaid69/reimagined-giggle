76b1d6f41a538262aaa3e3582b9e54fa793f7f88
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:35 +0000

    cross lock change genuine moon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def cube_number(x):
-        return x**3
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
