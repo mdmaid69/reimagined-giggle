72748aaff0f94902ba61df6acc42b7b04abbc090
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:02 +0000

    respect idea debt represent hole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
