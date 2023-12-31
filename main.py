2c1937594b1ceb9cb9c5eceb0ed974df64fbefa9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:21:37 +0000

    translate separate interrupt seem familiar

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
