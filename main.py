9c92d6caecf432ac327f15b5d8b702743ff50ac9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:34 +0000

    entrance down goup deliberate defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
