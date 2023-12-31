e19d0b6eb26869f3cf0aaf9ebf39a7cf8ef9e038
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:15:56 +0000

    shop turnaway recommend decision storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
