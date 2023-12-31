8cfbb17293124243da2fd4d22be25321bbb5fc8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:35 +0000

    squash time shallow valuable sticky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
