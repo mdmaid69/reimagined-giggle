3b030aef924598d41a5f0b8f9b40d797b3efc405
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:02:07 +0000

    confident promise save across swim

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import sys
-  def get_python_version():
-        return sys.version
+def is_odd(n):
+        return n % 2 != 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
