0bff4b69d077317ff6c811a24bb83e5d4c438a2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:51 +0000

    down seriously nice whom brief

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
