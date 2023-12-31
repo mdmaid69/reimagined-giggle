9f7aa2a932dc851a06706c35b645089359592d99
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:00:36 +0000

    star thinkof myself thin suckme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
