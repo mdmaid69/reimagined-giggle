3f546a6fbcafd2114ea309de62c9ac37fe93c99d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:37 +0000

    long past never gooff shopping

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sys
-def exit_program():
-        sys.exit()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
