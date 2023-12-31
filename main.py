4db0de500b064f8628f6ce696290033a27bc1cc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:17 +0000

    nervous study purpose trick situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
