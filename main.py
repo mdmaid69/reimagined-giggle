07a8a2d5177f37b9fb673c2a758bbd4a33535a9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:08:23 +0000

    admire consult breathe home sweet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
