abf6b1b3b17000cdd2e0c041b2012aae930a7de3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:29:57 +0000

    job dress problem entry design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
