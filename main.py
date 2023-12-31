3f6866709420c63c59fba130183cd9509705c7ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:18 +0000

    gift whichever breakinto around why

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
