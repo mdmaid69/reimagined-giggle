f0f566aa303bfd917a72c07e8968aa9bb21ef6ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:42:35 +0000

    copy mind position cope vanish

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def convert_to_hex(n):
+        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
