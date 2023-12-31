2da165f7c9b34bbb41283c77fd4d86d348ebebca
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:29:39 +0000

    choose check act fact swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
