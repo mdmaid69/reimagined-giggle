28fd4a2cf1455303ccb180f99f29315bb96ad18c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:08 +0000

    future bring willing declaration loan

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import sys
-def print_python_version():
-        print(sys.version)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
