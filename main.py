df699a6363a3869c7ec12e4882e0794974085f79
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:01 +0000

    force mend part flow connection

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
