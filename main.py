ea2b40790a6afdb4a41b8dfcac7c833b4629d249
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:25 +0000

    square interesting OKorokay love steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
