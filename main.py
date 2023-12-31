e1a1ff86c2d9fdf6f41dcedb414b607fe61ffcab
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:10:03 +0000

    afraid far judge exactly fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
