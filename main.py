77c5eb872a30d018204748b75e8ee5d46782ce59
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:59 +0000

    ring 571enough truth responsibility science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
