6e08e964b3f2023e65f13ab677312e169da30f7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:30 +0000

    drink woman mess shopping trouble

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
