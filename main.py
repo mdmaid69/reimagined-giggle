dc8b0a15fb2988284c04aedcaf74db7797f8cc01
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:42:27 +0000

    kick team fat move fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import sys
+  def get_python_version():
+        return sys.version
