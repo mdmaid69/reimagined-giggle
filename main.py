1298bdb8bf5c5033ca2d96672d97063b0f9fb249
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:12 +0000

    unkind heat commit wet keepoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import platform
+def get_os_info():
+        return platform.uname()
