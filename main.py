e5d14473a13705171e21f452cab5661aff8ef4fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:47:18 +0000

    port degree ocean comeoff book

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
