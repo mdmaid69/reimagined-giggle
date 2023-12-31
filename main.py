c12dc4236030a9a0a861aa76150b71cb76ec57ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:21 +0000

    track as trap hold ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
