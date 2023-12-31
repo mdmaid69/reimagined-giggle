971329091cc09a53e1cce17ac98f04b2cd99a4a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:18 +0000

    heat task day suit 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
