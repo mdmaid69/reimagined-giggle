fbceaa9b7a5bc0b1dd03a46fe4ac83698f37f2ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:27:27 +0000

    power number holdup smile turnout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
