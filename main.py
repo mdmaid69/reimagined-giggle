80118b1a44bbc4b0481f6ede2658e75c8afd19bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:01:45 +0000

    similar mind permission away quality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
