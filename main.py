534e8d51f5075656ef9f5efdbcb52786d93b412b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:55:02 +0000

    ticket keepoff nose instead sentence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
