6b1f2b86ea9642de0bad5582ea735af307b28295
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:02 +0000

    Uu win theirs angry her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
