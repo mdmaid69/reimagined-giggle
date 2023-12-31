03912d507b2de57f0c0393a219758c68b51408b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:42:37 +0000

    crowd fun miss save work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
