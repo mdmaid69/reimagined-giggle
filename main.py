f72338103dc258156d404d20c5f8bb568cb729a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:03 +0000

    hide nervous far foot train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
