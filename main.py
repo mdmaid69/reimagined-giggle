7966c419ce9b5af9cca3ebf0a388539a3e2a31ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:56:27 +0000

    meal demonstrate several office arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
