e5dde2e81a911b18765a9a3cbbb96c3d0ade644f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:06 +0000

    willing callup mass great seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_min(lst):
+        return min(lst)
