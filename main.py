bac2fa24c2c3f9ea1471afa629e4f67c89167f46
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:11:29 +0000

    search run frequent anywhere bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
