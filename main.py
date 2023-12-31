fb2a760532b89c074b9d65bfc302bac5b01f2d70
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:24:44 +0000

    drawing off satisfied cover impression

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+name = "Python"
+print("Hello,", name)
