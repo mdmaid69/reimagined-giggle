842a9af24b7e34d4ae7e49cb9dc93c2801977b8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:18 +0000

    careful accordingto bound protection support

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-print(datetime.datetime.now())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
