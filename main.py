d410ba0526edcdc7d1496cf41d5e36824059bedb
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:56 +0000

    common badly Nn lookup fall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
