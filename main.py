b69ff738b56f0708e706e9c8477115ea0188851d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:28:28 +0000

    lead rain crowded benefit market

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
