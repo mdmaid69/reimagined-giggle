693eeb5c4adf1eada38b1c803d6664c72afbfc6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:35:34 +0000

    prevent mark past tune issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
