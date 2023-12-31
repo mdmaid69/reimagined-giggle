8cbc27acf64dbc2b9972898ef417280ac6deafbd
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:40 +0000

    brother history accommodation mouth that

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
