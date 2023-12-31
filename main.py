afc5d2487c3c7803336a8fd97a2af5610e1ac421
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:04:18 +0000

    flow possession intend loud tour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
