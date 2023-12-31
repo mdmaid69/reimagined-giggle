81ac82792361b0c4690dac4499ec6bf3fe06c2cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:18 +0000

    either read year official fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
