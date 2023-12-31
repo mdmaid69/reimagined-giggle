bedf95069339cfd32043e37762ec46fb6d3de96d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:45 +0000

    throughout we dig sting gap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print([x**2 for x in range(10)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
