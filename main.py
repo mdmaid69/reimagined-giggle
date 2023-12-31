a2389352d1bef89b0efea03978113369c16c4efd
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:26 +0000

    scream payment pure tap ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
