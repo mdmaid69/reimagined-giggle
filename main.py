865e7356e182124bbd0336d9a3b968e0b6fe08ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:17 +0000

    outside nearly stay measure slice

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
