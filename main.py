85f4fda74bc40adc88a06b862fa157379762916f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:56 +0000

    interview admit safety breakinto cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
