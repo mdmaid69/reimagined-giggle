6eadd14d311cb01e6a2f59998f776b22d3aabc9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:42 +0000

    basis adjective spring record dull

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def reverse_list(lst):
-        return lst[::-1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
