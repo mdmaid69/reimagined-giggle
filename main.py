eebf61024f256eb95ee60228030ddcfab504ccde
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:26:06 +0000

    breakfast food like 564encourage holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def reverse_array(array):
-        array.reverse()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
