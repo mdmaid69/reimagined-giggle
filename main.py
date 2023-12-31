be2a38fcbb17234fa081b301a2e06570e2e7cf07
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:13 +0000

    pretty cry pin his angry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
