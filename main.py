638b8e9c0f864a7408cfdb7bc9310ad7c4fcaaf2
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:35 +0000

    nor relax open stand several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
