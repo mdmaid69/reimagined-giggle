62c9ec181d2dfd13c31a30b5ee2d11c2d46cdb9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:24:04 +0000

    spring cry back past morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
