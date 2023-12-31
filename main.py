8ec07d8878c463768bc3d7f4102f362d0c1475d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:17:19 +0000

    memory quit flat congratulations she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
