15e40a4fbb5028eecd1d27d265a95213c9a6c46b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:08 +0000

    anywayanyhow break concern Itself more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_work(force, distance):
+        return force * distance
