10ca13df47e73366a2f018520642c5db7b2b7e8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:53 +0000

    fear bus us manage exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
