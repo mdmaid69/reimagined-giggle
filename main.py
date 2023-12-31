ba2e0ab56a9b12e8e296c486d41e9a5766ce51db
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:32:12 +0000

    spend unlikely emotion anybody Ll

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
