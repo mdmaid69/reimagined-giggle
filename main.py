b7a41e85015bf507ecb2bacc83cf57335356203a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:52 +0000

    trust available honest keepaway easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
