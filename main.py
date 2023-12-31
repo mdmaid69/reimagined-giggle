7caf89f1cea0bd695a872924bf4bd5f6a5ccfbe4
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:59:30 +0000

    delight bar word grand dance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
