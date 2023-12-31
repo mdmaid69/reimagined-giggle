0b64800722fbffa685cc0924dc08a172c2883046
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:26 +0000

    organize cheek wave urgent blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
