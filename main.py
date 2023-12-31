9c86cd4840cdc11ac08e72160c748d54aec62b39
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:09 +0000

    walk there write sure regret

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
