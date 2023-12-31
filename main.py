4ebf1b1ee076ee956f974795c1a284c43a646d39
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:39 +0000

    brown calloff vote finger colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
