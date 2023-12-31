4525c0dfe1602f369ae4d1e9582f590207fe75ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:05:29 +0000

    uncomfortable left minute disagree place

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def add_numbers(a, b):
+        return a + b
