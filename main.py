46eecb465b64ac7982ce39a268c91db52a300819
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:32 +0000

    reject surprise tradition me city

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
