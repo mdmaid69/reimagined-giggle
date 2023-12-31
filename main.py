bccaeb42c4145e55e1156edbe5fdb39e11875f04
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:23 +0000

    damage service exploration Tt tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
