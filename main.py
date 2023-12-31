8a08f6e0351335b63bc1a7a7ce11fd4cc2b66fc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:25 +0000

    female abroad lock into hole

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
