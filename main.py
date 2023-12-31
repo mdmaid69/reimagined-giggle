440452803aca8b60e94c45d6b0976fff40f1ff8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:34 +0000

    heavy before justice herself group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
