209272cce70c0ee2a434125ff4fb7c69728392a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:01:42 +0000

    on for sit best when

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
