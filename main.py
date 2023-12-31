ba464436bc2bfa01c3fe1ab94920a4c527db9e56
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:10 +0000

    contribute dance holdup could favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
