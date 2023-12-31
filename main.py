cefc7a08a61eb2519bc57d91311af2a80c27f222
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:36:02 +0000

    Uu cap need married thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-print(os.getcwd())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import sys
+  def get_python_version():
+        return sys.version
