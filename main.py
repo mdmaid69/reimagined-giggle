e4497b2da90223e8fba0fc51bebce228890f2275
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:35:51 +0000

    luck impressed duty result square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def convert_to_binary(n):
+        return bin(n)
