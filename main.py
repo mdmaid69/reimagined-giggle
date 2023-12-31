f762fd6b71389ab68f225ace3959b485b4fd661f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:29 +0000

    runover shrink joke thought receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
