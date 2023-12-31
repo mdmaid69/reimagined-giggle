b4e0c0c4854129aa4826e928dd7ef2d3b4fbbc84
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:44:01 +0000

    first cost dull Mm child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
