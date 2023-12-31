582c41cead37bd4e0806de876dc3acea9c64d9d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:18 +0000

    mood forever kick terrible both

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_max(lst):
+        return max(lst)
+name = "Python"
+print("Hello,", name)
