75468e2ae960ed8bd5e54d23eb4b19d47c2b0af0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:02:40 +0000

    undo yesterday middle stage confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
