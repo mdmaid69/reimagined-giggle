c5cfda72f54e661625d305a2971d6856edc2567d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:19 +0000

    majority connection this give nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import numpy as np
+print(np.array([1, 2, 3]))
