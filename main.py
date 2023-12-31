32d8d1b497ed6027187730977a11af6fca563571
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:35:58 +0000

    over give party appear load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
