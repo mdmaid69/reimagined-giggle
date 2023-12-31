9c7d0df174925ddf8c8bab823edfbf927168e06b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:04 +0000

    ought very add behaviour immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
