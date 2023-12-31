fbbbf70085313ad12dec5204d88daac39e5f7b3d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:53:16 +0000

    related miss rich care treat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_max(numbers):
+        return max(numbers)
+  def subtract_numbers(x, y):
+        return x - y
