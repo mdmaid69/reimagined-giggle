c223718f7decedd046f9229b6c00677fa3a163df
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:25 +0000

    improvement go deaf talkinto him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
