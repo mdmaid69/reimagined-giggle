b4e440fbe5ff4e6d87939e86930f49d17891e331
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:27 +0000

    567enemy worry include sell exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
