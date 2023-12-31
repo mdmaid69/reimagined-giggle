055509c8e817628c55d5ad2fc2b9581b1bcc500b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:27 +0000

    pump typical hungry any stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
