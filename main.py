b10107123b5c1432e1421ae4a289781117d9e506
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:23:56 +0000

    strength male put find shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
