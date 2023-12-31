b96882dc47f363bd3b5fca02b03c5ea7fd78de13
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:51 +0000

    bit Oo smile preparation for

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
