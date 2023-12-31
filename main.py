80dbe2fcf132b3fc03b3bd3b385bc9f06149cc57
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:00:28 +0000

    lock several petrol basis food

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
