73c36137f02c6002c36468f7d18c020f2e028220
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:52 +0000

    morning noisy break cheek only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
