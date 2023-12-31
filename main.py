b02547f62553c49b81743c29eec742cdf6ee5ab9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:35 +0000

    strength question store keepaway cry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
