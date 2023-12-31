012d2ee27894b370bb6567e2cb95f13ac92bba85
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:25:36 +0000

    feed complain opposite positive but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
