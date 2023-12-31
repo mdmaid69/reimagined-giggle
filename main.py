28c3461774e3416f36dc09bf177338386bd8f01f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:03 +0000

    bite temper backwards tune hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
