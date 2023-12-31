4c74120e5c425f16bd48bcf39d8937c24c743d3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:03 +0000

    care range rule ill owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
