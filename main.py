4545e6de1203fd59eb8458e1bfd7bd00a6422d71
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:22 +0000

    home whistle long represent wheel

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sys
+def exit_program():
+        sys.exit()
