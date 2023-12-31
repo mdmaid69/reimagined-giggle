6b4c2eeb1e793251ff8b92571eeaea6ce4645cef
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:34:44 +0000

    big callup natural recommend every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
