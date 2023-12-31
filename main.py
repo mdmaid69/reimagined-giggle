b52222c072ffb8665345887de032c817c182094e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:45 +0000

    complain lookaround so geton connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
+def add_numbers(a, b):
+        return a + b
