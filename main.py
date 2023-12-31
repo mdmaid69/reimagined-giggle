be8b0a2f2e8db183607bf74883bb884827dbc40a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:38 +0000

    already hide equipment history tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
