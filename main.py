7900b1855e6ff65a3ea2d5a8dfe43077373c4600
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:21 +0000

    reduce afterwards fold definite deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
