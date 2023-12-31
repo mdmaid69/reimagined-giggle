78b8691a8679cd8b9ba2f9c9dfd2a5623672f222
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:52:19 +0000

    pain partner frighten case strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
