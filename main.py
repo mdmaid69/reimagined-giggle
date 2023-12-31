9b8b6247e9525625bde9714c9b07e1c9dc4c863d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:40:57 +0000

    serve closed everything bear remember

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
