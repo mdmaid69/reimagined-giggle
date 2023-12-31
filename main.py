132f53c0e151351764f2caa2ac7cd876123b535b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:02:38 +0000

    turnout bag urgent normal active

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def count_elements(lst):
+        return len(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
