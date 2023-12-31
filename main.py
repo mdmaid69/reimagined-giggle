2fc8cb60f33a9887d01f5672c79f24ad8970ef53
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:28 +0000

    convince welcome takeback dress switch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
