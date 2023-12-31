5de78abd98cf8257a3536ee1626f421fdcb0c7bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:22 +0000

    attempt generous name sort environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
