72a01b14a84c650475d9aa687019f325a77297c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:08:13 +0000

    shock flavour prove bottom receive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
