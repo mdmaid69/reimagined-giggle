386e5b69bcc8c6ca0b44850f43435fa5ef09b751
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:54 +0000

    hate bush allow personal division

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
