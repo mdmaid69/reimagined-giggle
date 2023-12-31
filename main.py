bdb7c94cbbd151ea05de09816d40bd281b2f2e33
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:38:07 +0000

    university commit mean sign simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
