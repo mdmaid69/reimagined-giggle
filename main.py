3e11e62f65b445abf7c490bfd46707a3a46c9021
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:34 +0000

    swim month rid keepoff further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
