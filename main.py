8d79271dccd9bb32bef544bb73104505e521df1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:01:34 +0000

    secret company mixture cheat flow

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
