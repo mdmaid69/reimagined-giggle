aea26c2d6f1a1e845e8a831ec90601d64aabb7d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:44:19 +0000

    break fine system wrong either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
