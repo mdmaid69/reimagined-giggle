dc5eb7f669bdc4854bc245829be130e575aebf34
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:08 +0000

    Dd major walk peaceful full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
