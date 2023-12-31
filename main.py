e6fa09f9d839ea93b87b712bfdc0f26bf0b824d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:26 +0000

    swing miss union opposite prepare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
