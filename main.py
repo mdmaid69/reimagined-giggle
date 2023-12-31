7c01e9b49e64b10f59cf5b81254956f495770307
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:48:45 +0000

    laugh everything public interested turn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
