ade7a7536f540885a96b318d8ec07e99d5d1fde6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:43 +0000

    back passenger alone balance reserve

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
