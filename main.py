6968d8e259596f1cb3435361c8113e24d6836212
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:50 +0000

    patience branch well even part

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
