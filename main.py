d5f79108cf845d61a9761b9c40cf1028cdf63e0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:47:21 +0000

    protection fit team alone connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
