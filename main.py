43067cdc26ccb85964c026d5c0de571a572f557e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:36:28 +0000

    near manage shallow any valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
