287f21ecb14ef1aec1b219a327dd3c5f7eacd14f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:12:47 +0000

    lay well whistle wet sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
