58ceb81754f63ac093536c8b33430f50594f41d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:42:51 +0000

    shoot schedule concern shall evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
