af56934ce265939ada20529eb49e1acd01e161de
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:45:07 +0000

    scared fail turndown crowded each

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
