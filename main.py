8b0ba9313d2d39006eb0ad627f0f520a2de4513b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:23:36 +0000

    platform die patience touch Vv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
