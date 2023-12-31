7d52646013fcf5249f7e20a4f84d2ce629da5de0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:55:49 +0000

    punish raw raise traffic accident

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
