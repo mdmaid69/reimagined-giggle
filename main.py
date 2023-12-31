8cbe5cb0986413b0203aa9d2b3d64d5899fa85f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:18:34 +0000

    arrangement worst mysterious artificial improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
