6cdff8a9b5c8d9674665264dd16d02a6783d2a2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:36 +0000

    constantly workon drop declare woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
