f3f935985df9388d2af5111aea0c6dacbf103602
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:27 +0000

    hear thick surprise protest national

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
