1629051b33a04ea66f556a147d21f3c98a50df8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:55 +0000

    allow whistle lucky ring let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
