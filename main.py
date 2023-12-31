7917a230d92c7f7e85016f164fa06ac5a28de430
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:44 +0000

    herself communication geton sound right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
