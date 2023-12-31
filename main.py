01f15fac5237a340a53a11bf126ad8d3791382dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:39 +0000

    bother past think country steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
