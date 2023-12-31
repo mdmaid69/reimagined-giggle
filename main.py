d9b8ea38a00b3907b376fa92819bc4bf7e03b4c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:39:29 +0000

    turnoff chief anxious calmly moral

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
