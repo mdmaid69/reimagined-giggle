c8d2c652659c76fe978734c8654c399d00a8b3dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:39 +0000

    spring container without shoulder life

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
