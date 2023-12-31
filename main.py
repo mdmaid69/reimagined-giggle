89d829654fa71129ead665651e8673cdfa7250b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:44:29 +0000

    broad chase base fine uncomfortable

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_density(mass, volume):
+        return mass / volume
