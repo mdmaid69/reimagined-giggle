dda0bb9d634225bdd4b3cc7a3840a6076789b1d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:57:14 +0000

    identify pen keep listenfor thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
