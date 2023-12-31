f047f75fb4f636cd3a92f86cca88b027cd530d24
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:00 +0000

    pour character feeling whistle justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
