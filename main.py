f9c7568206166e1eba5f7f014a14a7d9da45d3cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:58:09 +0000

    over raw underneath feature turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def subtract_numbers(x, y):
-        return x - y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
