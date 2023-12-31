991b3eaae64df1744484586186240f165de140b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:56:55 +0000

    surprise sensitive draw over oil

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
