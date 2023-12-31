f00895616d2013000392e364c2fab5d9d5035347
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:39:37 +0000

    dull transport food next rush

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def calculate_area_rectangle(l, w):
-        return l * w
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
