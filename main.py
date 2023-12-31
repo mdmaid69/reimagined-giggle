503b1ee6c19174db5eb0e260fcfd454901195d8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:40:55 +0000

    vanish boring base Vv so

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
