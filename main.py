d0bfce3a12f5f03c93527470f9f0c9ea05faaea6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:55:31 +0000

    furniture muscle school prevent message

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_stack():
+        return collections.deque()
