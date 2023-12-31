69ff0a93d2d1019dad8096a890ab7244a57800c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:18:47 +0000

    deep Qq study want offer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
