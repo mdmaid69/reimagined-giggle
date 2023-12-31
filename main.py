03e17cfcec7596a29e09e4ae8fb631412f471743
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:09:39 +0000

    permission thought hear giveout several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
