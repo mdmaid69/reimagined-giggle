970f5f92631c1835bad93fdd98404f28a02f217c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:28:57 +0000

    this difference godown issue hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_stack():
+        return collections.deque()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
