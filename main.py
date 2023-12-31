b66358269c3a28135af5dd3e998b11473d0d51b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:06 +0000

    practical fortunately mine load into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import random
+print(random.randint(0, 100))
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print("Union:", set(list1) | set(list2))
