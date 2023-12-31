a631498f9361728adb8a4fc74f9876d070e30822
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:54 +0000

    unpleasant listen forward yet rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
