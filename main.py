d9df639281c85e9ef9be986d7ada5f00e2fb48e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:07 +0000

    seat tip excuse tape swallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
