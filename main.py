718199aa218d85b65371455cfcb20109b6eb71ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:37:27 +0000

    anyone something shock roof speech

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
