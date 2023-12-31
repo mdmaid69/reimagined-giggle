dbdb41df506b551d98cbfc61b7dd6fb08d9545a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:23:28 +0000

    scene delight activity pen turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_str(array):
+        return str(array)
