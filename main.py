4865f1434b54ae1b1aab69915b49c4021a4a6519
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:37:50 +0000

    pattern successful declare please port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
