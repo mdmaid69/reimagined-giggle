2cebd558ef3f7825aac37bc69e3c0ed5a611500c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:13:38 +0000

    matter pin fire walk maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def get_array_typecode(array):
+        return array.typecode
