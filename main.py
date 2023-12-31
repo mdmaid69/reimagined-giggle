de964fb14ed6ec50db88faf203145cf009083b63
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:52 +0000

    wire 569enjoy let private yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def square_number(x):
+        return x**2
