bcc7fa4ad7849fd0a57ae7350cfdc03230a15145
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:04 +0000

    afraid decide onto dead alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
