c9457682e9f230122f1f3ef137784b4f0b1488c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:18:20 +0000

    reserve sweet bright top back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_directory_name(path):
+        return os.path.dirname(path)
