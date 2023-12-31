4583a58d72fafd223461e58281497c4b50a49d99
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:47 +0000

    guess like oil guide sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def add_numbers(a, b):
-        return a + b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
