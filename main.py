5076af41dc008a898d6248f0963b6bf6fcb45429
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:01:56 +0000

    over loose path exciting turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
