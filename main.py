7e62c5191f753502365bca82f4dff0c012bcd6b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:22 +0000

    weigh sign possession score will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
+def is_even(n):
+        return n % 2 == 0
