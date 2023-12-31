466baefebd237a5dc9975e74126a936be4edc2c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:30:56 +0000

    estimate dick new sort miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sys
+def exit_program():
+        sys.exit()
+import os
+print(os.getcwd())
