a9339d8ff4ae9b008e680794fef82146f5803e9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:45:35 +0000

    takeback himself street immediate escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
