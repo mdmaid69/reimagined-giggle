c6679b4f322ee101c6413d9cb8e40b12e828d6aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:00:50 +0000

    owe noisy substance OKorokay boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
