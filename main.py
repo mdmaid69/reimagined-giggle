01cfe9b9e945292b0fa724c00bc9f07b211215d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:05 +0000

    extra trade suggest sit 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
