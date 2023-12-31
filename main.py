d091f097da892f2ad8ed04629010dac8606a7923
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:33:26 +0000

    floor object please face someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
