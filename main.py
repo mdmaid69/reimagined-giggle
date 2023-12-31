47a30a89609dde1c95ca7e4a9237501589dba18e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:05 +0000

    experiment trap arrangement group fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
