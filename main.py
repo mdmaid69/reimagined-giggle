b1ee503a061d65e700f0421ff1cba934d5e1a0fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:35:19 +0000

    meal delicate defeat mark female

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
