81f6276a84e29df041db4ec02549de3b624f40ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:29 +0000

    prepare cold object position rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
