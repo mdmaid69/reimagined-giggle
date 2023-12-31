3ba9d16e2d7b1b751126554201904af8cf0aa08d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:03:36 +0000

    kill lay organize scrape memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def clear_array(array):
+        array *= 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
