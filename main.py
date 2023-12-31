8187c7e0ed858ed7e6584e4447908c18e8d337df
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:31:18 +0000

    club home picture stamp anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_as_float(array):
+        return float(array[0])
