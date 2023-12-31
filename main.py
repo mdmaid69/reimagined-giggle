9c288955f1b3d9723bd94933c745390561e01533
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:50:55 +0000

    necessary mass force mistake towards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
