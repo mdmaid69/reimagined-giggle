7593053320a2c98ae320dd389ebe1f3c6d7634d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:02:14 +0000

    someone worried rough Ww transparent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import os
+print(os.getcwd())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
