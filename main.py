8d1db2aae349e371daafca3f264a18f971242e1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:42:19 +0000

    boring confidence fall reserve effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
