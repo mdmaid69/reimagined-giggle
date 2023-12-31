f15937e88051004d937b8e7ab51d12db0cd82344
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:24 +0000

    gift go report lonely reduction

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
