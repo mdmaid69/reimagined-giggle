93382fd356e99e33a8b6effff2b639e957b19345
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:04 +0000

    work scene own connected thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
