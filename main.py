5ee7621aed51b7482ac042b014d4da5e50c7d894
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:24 +0000

    mine programme stiff which back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_unique_words(sentence):
+        return set(sentence.split())
