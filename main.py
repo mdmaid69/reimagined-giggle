5fc977f6050a5e7d19c072141d71bb242b2ba16e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:20 +0000

    neither bridge illness bush look

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
