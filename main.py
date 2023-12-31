b71e27faddca16879e69c79f200d483c9fdb5739
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:08 +0000

    brother start side very thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
 def calculate_density(mass, volume):
         return mass / volume
+import sys
+def print_python_version():
+        return sys.version
