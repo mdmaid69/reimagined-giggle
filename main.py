28f8ac76cdf883db44aa9b7b11b37c06b7064ab7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:28:12 +0000

    entrance everybody danger join lost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_error_function(x):
+        return math.erf(x)
