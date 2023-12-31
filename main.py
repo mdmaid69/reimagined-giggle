8e2268248d5812749371c017e8d4ab4399266fb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:47 +0000

    better ago maintain opinion proud

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
