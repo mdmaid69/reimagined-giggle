aad249bb212971f66a63b0d77f8de3a5e5ecc0f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:27 +0000

    replace offend large past tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
