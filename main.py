45f81ea9419b940b0f5998859330672a1726ab89
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:37 +0000

    special present snow movement remain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def is_even(n):
+        return n % 2 == 0
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
