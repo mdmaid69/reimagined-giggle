b8751c4ca53f4e22270c7ab11b700975af804d46
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:00 +0000

    raise always damage weekend often

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(numbers):
+        return min(numbers)
