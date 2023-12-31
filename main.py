4df2de0811412e0021048159722e3e226e6590d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:12:46 +0000

    level play tax generally store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+for i in range(5):
+        print(i)
