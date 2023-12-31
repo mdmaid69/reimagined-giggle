731283b6b423b02d5bdde092c43635ecbdff1212
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:35 +0000

    objection guess soon teach keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def convert_to_binary(n):
+        return bin(n)
