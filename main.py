2ca360f72137f341cd2392edacd727fa5e7f4b43
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:14 +0000

    present strict careful succeed woman

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_as_set(array):
+        return set(array)
+def convert_to_binary(n):
+        return bin(n)
