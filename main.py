ddfa82d8c7e40233817bbaf152412fed43e0909c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:45 +0000

    storm original frighten spill sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
