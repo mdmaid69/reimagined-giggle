ec868d042eba6cceb18fec06acbc93cf0a113c18
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:52 +0000

    sorry protest large try wire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
