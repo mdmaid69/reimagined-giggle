2166db56a751c1b85f1cccf0108f36511329fd1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:34:45 +0000

    once believe lost tap think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import random
+print(random.randint(0, 100))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
