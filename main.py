12803a1b962c6a9753b67c3bf83cdf7a7d3ce611
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:39 +0000

    style expression generally rough foot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
