1fa751c4611e270e7543d398b301816afaa9fcd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:12 +0000

    cheap way transparent pump good

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
