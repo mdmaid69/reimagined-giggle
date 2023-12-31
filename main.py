228110bdeb74528fd5b1f2ce7e320681be478b4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:32 +0000

    disturb mistake Jj creature moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def check_if_array_contains_item(array, item):
+        return item in array
