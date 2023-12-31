229034ab641a7486df6fd86f80a6051a1f4aa232
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:00 +0000

    basis expression float relax healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def reverse_string(s):
-        return s[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
