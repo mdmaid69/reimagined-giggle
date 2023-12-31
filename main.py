e6a596e87a267f126c6f8df7baf741dccffb7da2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:53:22 +0000

    respect moon lunch easy account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import tensorflow as tf
+print(tf.__version__)
