726734f1d295628531f329677a5470472a6fcfe8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:55 +0000

    into whisper breakout increase spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
