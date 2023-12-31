67de804f02f5838001b02ae8ce88b56441916dd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:01:18 +0000

    type major emotion declaration busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
