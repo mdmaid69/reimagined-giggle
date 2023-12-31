b0fb90ac50bde3429c6593de55263ed417b14cde
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:16:15 +0000

    late true secret fasten drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
