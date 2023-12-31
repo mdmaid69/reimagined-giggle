9a7e548f03d1a17a42f475eb7675c83bfba4eb8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:42 +0000

    instrument discussion warning pity similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
