f1bbace562bc878085c00b2e99869af223ba34c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:43 +0000

    willing emphasize dance subject getaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
