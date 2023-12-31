4d32e725e32f1af400487463682bbc663aef0425
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:55 +0000

    ask dry flavour shoot reply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
