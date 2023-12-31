1fe1be01d74cc2e2728bf8b0d7e21e561ce2dc3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:12 +0000

    valuable that smart so right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def clear_array(array):
-        array *= 0
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_power(work, time):
+        return work / time
