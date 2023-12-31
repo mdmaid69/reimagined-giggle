29be6bf50573988679f71dbe52d8f5ab3aac9446
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:56 +0000

    thought shallow interruption area actual

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
