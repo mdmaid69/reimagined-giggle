0f436dde978bea0f99d35e758eea160d8223efcb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:57 +0000

    sweet whether weekend rule crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
