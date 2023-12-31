94396f4d4adaea1aa546d9fb4140d96f1ecac38a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:14:02 +0000

    pattern song protest difficulty cause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def is_odd(n):
-        return n % 2 != 0
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
