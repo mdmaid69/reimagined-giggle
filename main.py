d78bbe3a1bbf5382d2f00776f520f20d33f1282d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:12 +0000

    muscle discovery brief your exist

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
