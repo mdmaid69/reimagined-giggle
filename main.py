876ffdd9b810a5ffb5788169a29e8c372ec52403
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:22:24 +0000

    confuse form experience home intend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
+def is_odd(n):
+        return n % 2 != 0
