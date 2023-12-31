e0accda7e930ca18094d3c4405c0b6addcdfbbf5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:37 +0000

    cheat 564encourage bringdown short warning

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
