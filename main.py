065f4d91f59b20c80054da9c8b705dda0e29fb48
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:02:06 +0000

    underneath join cost drop content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import time
+print(time.time())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
