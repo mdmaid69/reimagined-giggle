98e3ec0b5cf9b4871d1e07087f9a49d0b04f21a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:38:09 +0000

    any row remain quick blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
