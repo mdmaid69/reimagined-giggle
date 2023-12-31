401a579c67eb5e0704ce9700ab770465aca6a562
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:48 +0000

    organization either I careful save

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
