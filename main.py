d679cf146186be9982a749749636d090d1151388
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:10:56 +0000

    walk sound somebody same pinch

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
