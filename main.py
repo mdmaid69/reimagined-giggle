c04023053c2ce1f90912bc8ea22b5552cb2b8fc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:14:04 +0000

    common light mad cinema pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
