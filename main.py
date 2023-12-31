fd516037e184f20e9c9f0241459f678e2cb8cdbd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:24 +0000

    funny takeout reaction underneath show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
+  def is_odd(n):
+        return n % 2 != 0
