4eae55dc97c7df59e7e4ac833f8552b2f8c3afef
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:55:40 +0000

    under point loan suffer to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
