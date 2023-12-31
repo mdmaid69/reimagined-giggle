0beb5c9cc7b59172422b3ee92722d653b3348765
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:25:09 +0000

    discovery never doubt shout live

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_density(mass, volume):
+        return mass / volume
