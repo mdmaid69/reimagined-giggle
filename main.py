6c695cbc38d0e92067e7b508ece2c9967b90858f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:25:25 +0000

    shut mind bad decision luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
