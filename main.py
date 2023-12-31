b320ceaa8401a71938041cc0384497514d5ee1ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:57 +0000

    row object blame refer type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import os
 def get_file_creation_time(filename):
         return os.path.getctime(filename)
