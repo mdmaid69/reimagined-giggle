14e2e3730fcb21b3bcaee80d6152b596f9d88e0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:30 +0000

    hold grow control other protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
