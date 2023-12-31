72e1d5137f31bbc8a28bdac7a75c42632771a451
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:18:48 +0000

    either design stretch downstairs on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
