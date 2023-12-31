237c2e4732c6f9c1e7513c6fe79772f3a8ca9c60
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:23:41 +0000

    separate holdout worst uniform lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
