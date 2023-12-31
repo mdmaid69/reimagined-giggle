d959587adb9f5ba72a70ca486e29e7d3408936f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:05:19 +0000

    authority persuade stone study hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
