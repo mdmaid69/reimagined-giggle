1f694d2594a24ec49f4dbe0fabb6152cd83ad770
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:38:27 +0000

    stranger private possession complete children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
