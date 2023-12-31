60b36d41d744d7dea7220f45613be4e1383c8101
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:19 +0000

    to limit catch yesterday intelligent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
