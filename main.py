7b8658896829ce52d27601636e438bd2e553f304
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:37:05 +0000

    decide worry sensitive land light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
