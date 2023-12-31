3e385a72ce051103e816f65c998e25c4f0980b05
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:43:29 +0000

    likely holiday shrink tone Cc

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-print(sum(range(10)))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
