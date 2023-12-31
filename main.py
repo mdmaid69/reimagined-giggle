9ae1d95984b3285bd5d19c4e622e1e9ddccf811f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:03:10 +0000

    Ww side much often foreign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
