3f09fbca394611e6c1c570fcfb97765e97e161e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:45 +0000

    mad quarter only sense pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
