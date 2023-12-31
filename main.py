0d46feb9d3e11be75431232342ac323f0f22ec2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:31 +0000

    their skirt clothes natural dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
