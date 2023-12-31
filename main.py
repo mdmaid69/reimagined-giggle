facf648606c5fd96316136495252500af94b58f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:12 +0000

    send breakup gooff wipe laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
