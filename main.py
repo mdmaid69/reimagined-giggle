570b978fa743ff291d8ca6ed072a1a88bb6db8f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:01 +0000

    typical port music me lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
