e637a9da42aef6709c7afe598e4c31425e8d6aed
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:51 +0000

    connected getoff opinion ilovedicks safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_combinations(n, k):
+        return math.comb(n, k)
