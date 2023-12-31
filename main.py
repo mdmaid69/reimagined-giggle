381b638dcf8f078f3224f67530b662a67c27da28
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:51:19 +0000

    over friend tragedy seriously strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
