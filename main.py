161e5d52d3be7928bc046b6818aae149cf699486
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:33 +0000

    cross personal bother weak flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
