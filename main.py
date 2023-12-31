26881efce249c56bf99bf7f856521e82b1339ef4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:52 +0000

    brain deliver pretty platform open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
