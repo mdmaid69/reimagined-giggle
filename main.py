2b64097908507715b7d10b0dec92ac3d61fb8d6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:05:38 +0000

    tradition swimming comment union moon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
