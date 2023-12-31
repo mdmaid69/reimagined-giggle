6189abe90b24621f4aa5a54b5bb1d1c9d805dddf
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:33:25 +0000

    medium set not community mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
