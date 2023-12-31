507e4946f727f22f56314e43d3ca5d069ee81c65
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:26:09 +0000

    everybody boil shrink dull cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
