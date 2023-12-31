69d567209d0ce9600db238ceea7ee4fc5b246320
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:03:04 +0000

    unkind average yet march Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
