895a8071369eaab5a2bf9f4608a1d3dcaa69775b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:11:50 +0000

    run among confusing away partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
