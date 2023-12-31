0cfa6efa8dc225b613daca39395d1f02e11bf8e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:37 +0000

    kill wrap past rest wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
