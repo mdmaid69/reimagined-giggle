265eefa4d212ec4499a065b8e32aa6ca7cd409dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:34 +0000

    owe content marry piece post

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+print([x**2 for x in range(10)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
