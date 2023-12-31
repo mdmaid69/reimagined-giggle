4bd77b2661b9ea8c37f6892fbf885c3257755a0b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:44 +0000

    pure considering club capital paper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
