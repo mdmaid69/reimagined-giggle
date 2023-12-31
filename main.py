8813b1630451668d0a5c77344484c6d87732c406
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:02 +0000

    in joke carry mean beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def count_characters(sentence):
+        return len(sentence)
