0d41f3cb8835c9a4cfb126a62f75296b5f22fd18
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:49 +0000

    doubt best range Qq house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
