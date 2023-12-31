5576a15a70b0480143350be19ba7712dcb90d93f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:41:00 +0000

    sell memory opposite advanced interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
