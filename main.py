e0f89636c70d77867333a1560daace466f68e207
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:14 +0000

    vote at close medium change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
