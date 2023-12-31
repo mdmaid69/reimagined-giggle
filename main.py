e8865eba17c3ba905d2ca8f4756ef9e0dc485e3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:44:04 +0000

    prevent prepare fill before image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
