d87d42834a0fbfb0b2eef6336561e13c4993fab8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:49:49 +0000

    copy middle at choice treatment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def greet(name):
+        print(f"Hello, {name}!")
