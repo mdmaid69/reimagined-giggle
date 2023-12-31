dd99b49a7c98707c3526ce10c02db5e0a4dd9edd
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:32 +0000

    paper majority during increase spite

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def clear_array(array):
+        array *= 0
