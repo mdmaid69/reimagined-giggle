1c77bb2d8aebb0f2c71a4463c4a8d76e9af0f30b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:30:04 +0000

    react preparation character study method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def multiply_numbers(x, y):
-        return x * y
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
