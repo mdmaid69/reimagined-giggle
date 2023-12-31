f11ec25ad615220c74e0aed378dbefe6633ecb71
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:16:48 +0000

    drive Qq another popular kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_queue():
-        return collections.deque()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
