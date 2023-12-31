6bd0a9d2d4d09c8981ed12add24d1a9dc31a4216
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:26 +0000

    that apparently lunch enthusiastic clear

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
