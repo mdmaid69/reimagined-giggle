e10c642a4904f184eafbeca88616ae7859ee67e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:10:57 +0000

    treat Yy matter minority as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def convert_to_octal(n):
+        return oct(n)
