53b25697a72063efd3b5e0fd6ab271d012a33c20
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:04:19 +0000

    earn gift date acceptable figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
