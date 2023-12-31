0b16c7d0ca38e5f97f47a84d9d70c64fe556d549
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:59:55 +0000

    fill object tough second yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
