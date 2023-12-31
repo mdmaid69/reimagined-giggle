88fd7f123eea75f4c045cb96c5409fa7718b4e80
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:19 +0000

    along wake young exactly male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
