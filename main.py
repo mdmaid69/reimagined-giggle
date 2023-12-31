af0d966705934c354aa491d056238c362330b3f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:37 +0000

    fast decide save win undo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import sys
+print(sys.version)
