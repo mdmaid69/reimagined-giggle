cec594f5366966b0ad44cbc256cd36e875cebc3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:22 +0000

    choice trial narrow want appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import tensorflow as tf
+print(tf.__version__)
