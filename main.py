9d91916a1205bbdd0a1c1c8d1c5fefce6798d121
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:58:15 +0000

    tape strict takeoff uncomfortable stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
