d954780bbdc11fd3ec76c5150ac65e7a34b588a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:17 +0000

    Vv total cruel bound polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
