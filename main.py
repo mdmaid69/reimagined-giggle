149b783a6b80340cd775eeb414f1795677bfae18
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:11 +0000

    somewhere pin trap care slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def cube_number(x):
+        return x**3
