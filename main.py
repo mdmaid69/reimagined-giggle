131756f0906e8fc0a0009e615ed50792ae7356cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:03:03 +0000

    law burn secret cook from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
