78afa0e1e2199d72e57ec29c5edb156504c64858
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:52:21 +0000

    borrow regard impressive tradition Ee

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_max(numbers):
+        return max(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
