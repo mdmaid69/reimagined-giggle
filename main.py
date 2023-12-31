63c93787d83a742247b23d14947ba35c77863415
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:11 +0000

    large getup pure nature hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
