780433dbd8308ef2ac07216b85623c89d31bf1d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:45 +0000

    recently so worth name file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
