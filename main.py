3c9e330999ffe30293a32d95a48ad088e9c24b95
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:12 +0000

    characteristic room declare factory talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
