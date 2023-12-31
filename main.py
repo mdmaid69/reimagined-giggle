9e0b7d19a3871349383291a64dc6234eaf33ca4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:00 +0000

    impatient coat high laugh real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
