fcd41280f120d6f6d70c7696d20b870b313e27ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:21 +0000

    nervous tend detail patience art

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
