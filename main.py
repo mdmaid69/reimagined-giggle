b0c1f4ba7f1eac5dc85820804046603177fa00e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:38:24 +0000

    area recognize experiment show stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
