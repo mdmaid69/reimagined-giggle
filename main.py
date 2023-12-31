a657d59426335f25e85b5f0f76909dc1948baf96
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:54 +0000

    loose press stamp project dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
