1ed9f2f5f4b19788c8d1babd5b40888e52d2c4bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:40 +0000

    except combination heart moon this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
