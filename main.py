a3c7083e1fd5cdb27df43d1e99394c662e3a34e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:17:57 +0000

    back that die rather shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
