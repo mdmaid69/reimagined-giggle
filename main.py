24d25dd7860f694f632fbb57a6b2e11698420f23
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:59:44 +0000

    worst weigh church trap zero

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
