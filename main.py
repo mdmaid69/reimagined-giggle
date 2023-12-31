a2fc82747e89c331dc7611128d9cd89362e59c62
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:25:04 +0000

    deliver walk mystery unkind software

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_even(n):
+        return n % 2 == 0
