56ac4ea8cddb535f1ac0d04e0a6e6ba0de424f2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:47 +0000

    tooth development bit operation busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
