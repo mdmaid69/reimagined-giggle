a0d41b6570b278ed64b35d9b8b4ca853189b94ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:17:28 +0000

    unless unfortunate centimetre manage now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
 def convert_list_to_array(list, typecode):
         return array.array(typecode, list)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
