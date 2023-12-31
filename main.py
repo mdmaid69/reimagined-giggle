2ae6638fc030e0deede7f2fca80237e2db701476
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:39:25 +0000

    air emergency towards whole bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_array_as_float(array):
+        return float(array[0])
