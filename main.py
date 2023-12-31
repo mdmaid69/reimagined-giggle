20e6f53869f40403e4981a6be28288f18d2b1d79
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:40 +0000

    near I evidence welcome suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 import itertools
 def get_combinations(iterable, r):
         return list(itertools.combinations(iterable, r))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
