0f56f642b0ef5a975739b0862292d451a4e2f40b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:14:11 +0000

    runover rubbish report fold second

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
