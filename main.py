94b179e1a874f1ca04b6b02edfdb84b9e83407d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:34 +0000

    church its prepare passenger watch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import datetime
+print(datetime.datetime.now())
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import sys
-def print_python_version():
-        print(sys.version)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
