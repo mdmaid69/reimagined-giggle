28ca07edb555c20465af1159214830c9b6355ed1
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:46:52 +0000

    whichever season pair miss embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def add_numbers(a, b):
+        return a + b
