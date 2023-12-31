23e30d842ad915b3028393cd215c6fda1db5bcef
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:34 +0000

    date praise tour cry cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
