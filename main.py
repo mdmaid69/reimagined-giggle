14d204a499763d0cda3ff52ff44a3d9dfa4f938b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:47:50 +0000

    love region account mass fact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def find_min(numbers):
+        return min(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
