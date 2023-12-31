c9388cfd6bb08f77f7e617ea784978537c424cfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:33:16 +0000

    trip praise through criticize development

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import sys
-def print_python_version():
-        print(sys.version)
+print(sys.version)
