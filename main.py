e7bc46269af7f23c027788048b24469a64e99cdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:19 +0000

    account 565end unpleasant test lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(a, b):
-        return a + b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
