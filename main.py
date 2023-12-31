97f125d89cebe05fd89ec1560c610d9b9d6ae39b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:55 +0000

    settle even mess prepare release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def greet(name):
+        print(f"Hello, {name}!")
