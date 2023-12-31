f1585ba12c66dfd3ec18558157c374384eb330ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:32:30 +0000

    rank once keepfrom advice count

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def convert_to_binary(n):
-        return bin(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
