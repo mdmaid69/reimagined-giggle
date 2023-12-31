40a8b61bb562356065a2a3bdd480208f4746384d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:14:31 +0000

    fortunately tour cheap report somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area(radius):
+        return 3.14 * radius * radius
