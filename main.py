bf9fae2b4f542e4bcc850343a78ad54be1ce6c7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:57:47 +0000

    badly book lower up pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def square_number(x):
-        return x**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def sort_list(lst):
+        return sorted(lst)
