9b3ff541fe0d7036a58b5351ee2570f7b55b3410
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:27 +0000

    petrol shopping fill joke angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
