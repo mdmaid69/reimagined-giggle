378427eecad3fe13d5eb4f814e26d9a6b7ddff10
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:40 +0000

    expensive punish quickly count performance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
