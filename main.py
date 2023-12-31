b7540106340067bf06265b17dff4ed89c94bc3eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:40 +0000

    intend before stay stage force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
