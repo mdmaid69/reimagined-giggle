cabfd2c4e908f45fa251fd56f1e2fa0ceb6c6e52
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:23 +0000

    claim journey offer method certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
