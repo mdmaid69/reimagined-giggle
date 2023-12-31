a8f04fe380b11da0c2a851129bc9f46490deb8a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:24 +0000

    detail interested somehow nice mouth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
