004e68b311d485bf41d3936a3e6433a6719872c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:36:16 +0000

    believe attempt shout rest material

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
