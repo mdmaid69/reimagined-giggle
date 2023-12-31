cc4ebd433ca77a7b090dc3518a294e320476c0d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:27 +0000

    damp still whatever minute balance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def divide_numbers(x, y):
-        return x / y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
