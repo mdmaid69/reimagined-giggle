6cf02a51e2175ee3685be9f227063fb40c94c9cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:53:49 +0000

    confusing modern same sick giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
