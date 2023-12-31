f73ffd4d200c388019693e51c4aa405880c8d666
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:23:14 +0000

    trade shade ignore bother gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
