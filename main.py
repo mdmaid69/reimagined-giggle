8ec19189e5d2691dfce77c556941c39c2b7e2011
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:04 +0000

    impressive uponon hole separate early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def square_number(x):
+        return x**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
