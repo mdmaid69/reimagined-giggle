32e05a19bb00cb02c5c750c6fa63ec20e68f4c84
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:20 +0000

    Rr invitation necessary market equal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
