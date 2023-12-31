7c19e486b5ee81029f94941808f998286efcac20
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:48:40 +0000

    community shut interview explain maintain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
