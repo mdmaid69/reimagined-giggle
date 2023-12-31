55e69917eacc5b3a6ed5c7bc936b4f98ff9d7a9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:45 +0000

    squash write its distance skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
