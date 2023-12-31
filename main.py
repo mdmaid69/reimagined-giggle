6c4146c8cc86858f40c054efc103569aa68a5759
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:20 +0000

    complaint awake action burn idea

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
