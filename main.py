53921e56e6610fdd4e12a1a3a1a66abbddafb996
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:47:28 +0000

    bill tradition lock proud frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
