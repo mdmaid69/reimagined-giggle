b10b001aa28eb697253850822a47824a6041ea2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:35 +0000

    range degree pour reach reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
