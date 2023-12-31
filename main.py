46cb86c600367a09c6efcd3c7939baf9ad38f0af
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:27:48 +0000

    interview pattern great harm mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
