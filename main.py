7ad1bfe2a0c9717b1b394f621fe551d6eb587ad1
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:48:09 +0000

    active recent opinion straight prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
