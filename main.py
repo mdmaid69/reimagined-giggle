4d8f99b57b3d8bb9f858b6277b8dea0989e9cc6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:38:48 +0000

    rely head software immediately excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
