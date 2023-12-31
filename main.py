447de4e08e31807b31086a10fe7450224536699b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:04:38 +0000

    shall think advise power embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
