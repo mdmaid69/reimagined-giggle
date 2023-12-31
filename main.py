c495fa75ab774447cd9306e2ebefc4e4329b43be
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:35 +0000

    turndown variety kick surprising travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
