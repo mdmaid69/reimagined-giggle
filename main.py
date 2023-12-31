164afe04089e01f6b98eb7d686f86d9317882618
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:26 +0000

    move prevent relief law star

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
