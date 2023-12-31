f64d52ef0994de62748a0b07f240308360b85061
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:06:13 +0000

    section stranger his wherever ability

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
