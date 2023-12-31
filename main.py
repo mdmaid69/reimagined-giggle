820d9a3291fdba1541c954311e045da40cf80723
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:30:57 +0000

    appeal mixture wise thumb production

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Words:", len(text.split()))
