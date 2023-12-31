e5174b2b34c0bb29da3237a4b63f9df32363d81e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:19:55 +0000

    foreign sore past appearance workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
