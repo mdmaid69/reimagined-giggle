81e64967fddaf8fdbaa933bab428b01b6909930d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:14:26 +0000

    want head respect fly lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
