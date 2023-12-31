32015b4479c5391eda039e287bbb1e8eccee15de
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:43 +0000

    card time allow mass challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
