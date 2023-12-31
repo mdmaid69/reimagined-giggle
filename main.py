e4fdda88abdfc21525035ca05a11c44a078ac856
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:12:48 +0000

    against expect fair excuse advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
