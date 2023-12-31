0184379832b3bea7f042f0da88277d6b083f149d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:21 +0000

    special list teach meeting get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
