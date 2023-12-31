a69cccb4e36fc7593be805348a2ef8637403a33d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:43:38 +0000

    greeting share square scared tidy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
