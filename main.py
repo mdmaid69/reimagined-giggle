cf1a0328c66f9cbae58e9ef9e107438474b0205f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:06 +0000

    mad ever female free test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def append_to_array(array, item):
+        array.append(item)
