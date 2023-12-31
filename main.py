cfa1c78945f992f0e632f8ce257b7ab1e2aeb75a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:21 +0000

    offer difference previous open sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
