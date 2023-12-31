0222df41a549fd97f0a4a765b7313c5184f74c82
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:44:42 +0000

    detail mark admit healthy behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_speed(distance, time):
-        return distance / time
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
