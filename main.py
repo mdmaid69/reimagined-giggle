2c375e1bf99ecb83974a7cd582ee5a1ad9928b23
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:04 +0000

    whistle old fire anyone ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
