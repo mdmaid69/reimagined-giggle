8e7c320ff599c384f099b05bdcba8259fb7c3365
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:05 +0000

    same seriously invitation bother correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
