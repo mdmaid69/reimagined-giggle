9be13898cd5ea6902710093da0b03085cbe93be1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:05 +0000

    account count gentle lower oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
