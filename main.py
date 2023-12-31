fdd8a96099c8f6579b87696d874ce3b6224a8412
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:25:30 +0000

    tight arm small consider either

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
