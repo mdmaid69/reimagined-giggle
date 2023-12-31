c374878c7cb1c3848130bb45f7be7cb6e588a988
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:04 +0000

    themselves this finish right mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
