425fdb315dc5c212d60d64d51c6f364d7b60c6db
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:48 +0000

    block relax heat service Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def get_current_working_directory():
+        return os.getcwd()
