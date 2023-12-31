616034b0aa524bf55af12ff421f17e9a53211b33
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:16:57 +0000

    statement mix keepout narrow unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
