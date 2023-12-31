2e9b025aa14d571bc4e3b1728b8475b9c2f1f510
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:57 +0000

    almost again runaway protect talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
