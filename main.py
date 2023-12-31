c3858278f98b218069d2c4adea3d352a9bf572d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:49 +0000

    trip replace emergency lost she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
