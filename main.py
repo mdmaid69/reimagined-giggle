dda06e4eee683aa6f2450e54dab262d408cb52fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:35 +0000

    touch smile in clearly comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
