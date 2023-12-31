69c3693982b6703ee69cf02de5596351e282d0ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:45 +0000

    harm agreement breakfast mix event

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
