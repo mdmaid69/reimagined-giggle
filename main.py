eb6db27424033b11c1540e4e43be3cc4a81791ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:52:03 +0000

    lonely neither even promise drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_list_from_array(array):
+        return array.tolist()
