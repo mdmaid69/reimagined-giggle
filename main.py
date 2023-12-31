423fac7b6c815864171f79b30edf789a3db39abc
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:05 +0000

    brush aim beyond present about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
