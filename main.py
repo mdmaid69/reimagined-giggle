7bcaac22f68c5e011da0cb72b12b311263cfc131
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:42:40 +0000

    calloff original range keepfrom already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(x, y):
-        return x + y
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
