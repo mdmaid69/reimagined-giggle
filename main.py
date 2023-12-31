4af8f36ad2c688f65654de3f1794e0b2f505e9c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:32 +0000

    square place star appeal forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
