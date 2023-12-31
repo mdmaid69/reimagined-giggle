9f15c33d447943134c400fa77d4a78ac360d9f3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:17 +0000

    safe wind write though cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
