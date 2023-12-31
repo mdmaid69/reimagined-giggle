5d0273257ca6f0f8cf1109878a84fe18a6f3aba2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:02:58 +0000

    summer fun thought success thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
