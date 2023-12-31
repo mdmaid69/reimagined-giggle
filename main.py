372f15398aa93bb25cfb719562ae32ec1547889f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:12:14 +0000

    advise risk rush welcome plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
