9340cd709e65c8f2c60083aab2cbef8a4c804483
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:09:06 +0000

    bringup calloff Ww complete thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
