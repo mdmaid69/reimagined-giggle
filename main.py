8368674c4e5dd4a0a4d0360beaafd44e17ec1238
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:53:13 +0000

    number cry sense one compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
