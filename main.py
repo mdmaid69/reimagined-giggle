8028e1c0c8d49e4909c7e0615d471113fff02e27
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:10 +0000

    similar declare confuse would quarter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+for i in range(10): print(i)
