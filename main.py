bdbc7dcc9c5667aa4bf6bc0eacd5620a32c8a07b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:46 +0000

    glass cheat passenger arm himself

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
