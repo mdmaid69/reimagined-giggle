ee8e2bea62e4743d402c7bdc346672d51c83362d
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:03 +0000

    season year proud female nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
