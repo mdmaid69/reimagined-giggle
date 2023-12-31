71f7b133ca7b9de7d968701f5347d77c3086f8cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:26:56 +0000

    answer swallow design thin Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
