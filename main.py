40a9428c7238e53db427749183341e427c6c3a00
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:31:26 +0000

    and situation illness rare phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def square_number(x):
+        return x**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
