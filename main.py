b154041439c23a8c6ba67e3870d9d195742eab97
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:31 +0000

    exaggerate upset quality cry and

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
