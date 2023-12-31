d0bba92747930bfab32c12ef1eae5a1b7bf7878e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:11 +0000

    constantly instant comedown whatever close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
