00aaf3a6bd8a634337c67da99b715e1947666dd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:16:51 +0000

    combination base unable enthusiastic produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
