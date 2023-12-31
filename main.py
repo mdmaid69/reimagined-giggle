76bb9341dbf724e8cfba7f078ffb5280a965c750
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:22:39 +0000

    hospital left Rr history wish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
