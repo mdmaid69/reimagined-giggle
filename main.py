5527047929c01e172e6e94103ce7bd41c61ba270
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:45:47 +0000

    explain separate mouth mine practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(a, b):
+        return a + b
