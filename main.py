2e69ea568c568e4073e811b59999c89d414b4da8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:10:43 +0000

    record else OKorokay flood appear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+print(os.getcwd())
