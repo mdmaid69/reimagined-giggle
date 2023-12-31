c3a2ddd5e77f319b62c06745d91f89774d69bca8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:33:40 +0000

    ability please inform transparent oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
