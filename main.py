b7eab4c7eefc12413abf444f468f59d63edf253f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:46:35 +0000

    likely blind shallow defend turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def divide_numbers(x, y):
-        return x / y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
