f972f0e19e37e55a69a2b83cd6ffadf5e04f0d21
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:15 +0000

    fall spoil dry recommend expect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
