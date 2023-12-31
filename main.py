9dc0a66e0764fc2b3e9e8b5321090babdace5f13
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:56 +0000

    friendly gas exercise even breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
