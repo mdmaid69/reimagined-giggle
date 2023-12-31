dbd43f20641511b03e545dbd0048169da96fdce8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:29:02 +0000

    oil platform hurry carry preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
