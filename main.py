4962e969df34a9e8f5193272a4b07333a680783c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:08:17 +0000

    stand tie wander form positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
