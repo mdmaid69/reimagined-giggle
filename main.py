0849f9b4104f20849847be105fe3fcc766087c2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:24 +0000

    dance notice clean cruelty motion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
