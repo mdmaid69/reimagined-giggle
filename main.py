f0ee46ed101bd4399fb904e5fecf0c71bd7503e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:11 +0000

    hurt born ago you check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
