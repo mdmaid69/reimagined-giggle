3df7afc026d5eec054dd606d11cf60be259906ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:08:17 +0000

    kind frighten plenty provide chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
