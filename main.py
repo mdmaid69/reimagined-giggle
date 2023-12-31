ead630e75540a05fcf87ccfde1bc4bfa178d2853
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:29:05 +0000

    father calloff run programme embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
