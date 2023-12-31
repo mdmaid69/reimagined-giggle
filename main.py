54bc8a0e7ba47746116b8ab8749bebdcbad43956
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:01 +0000

    method hour fasten telephone surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-import math
-def calculate_cosine(x):
-        return math.cos(x)
