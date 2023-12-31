ff1bf75ed9032c3298264ffcf79c19c6742ad6eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:10:50 +0000

    straight listenfor explain ill man

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+print([x**2 for x in range(10)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
