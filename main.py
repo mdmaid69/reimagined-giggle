7802a27fe0cb2837f1647c45b560be46dd0a223c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:08:26 +0000

    disagree drive suit down project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def get_time_since_epoch():
+        return time.time()
+import getpass
+def get_username():
+        return getpass.getuser()
