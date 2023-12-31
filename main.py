84370fccdb2e68b290cd8dec4fc35d47563a81e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:10:40 +0000

    crowd declare charge violence harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
+import os
+def get_current_working_directory():
         return os.getcwd()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
