3edab1b4cee83699c7f2e0ca1c279aa2d5248465
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:24 +0000

    goon climb transparent hour achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
