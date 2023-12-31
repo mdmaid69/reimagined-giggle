15df80e5a84fcfbb7246b85ab29d21208d63e69e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:02:26 +0000

    equal mix stage extremely tune

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
