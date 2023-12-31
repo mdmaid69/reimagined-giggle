c59675269df6fa7e7aa55dabcc7a5a90f6bfc5bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:58 +0000

    private size clue man concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
