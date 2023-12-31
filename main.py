9691adde7d9f34dd49708d9802378c4982ed210b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:29 +0000

    thanks hope single giveout about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def is_odd(n):
+        return n % 2 != 0
