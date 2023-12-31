5ddb840793d9587afdd579be907e51c214f2a336
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:13:22 +0000

    excited interview light ready throw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_max(numbers):
+        return max(numbers)
