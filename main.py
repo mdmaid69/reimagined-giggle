cf6c70893df3fb44a70e279351577384d258141e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:20 +0000

    care word thinkthrough wild different

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
