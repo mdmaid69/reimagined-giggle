fb58713212f2880ed7ba2df54211bb70c85b8ff3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:55:37 +0000

    stir sharp yes possession brain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def sort_list(lst):
-        return sorted(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def add_numbers(x, y):
+        return x + y
