58011b428f9c46f14fd968083e9d5a882bf7acdc
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:06 +0000

    note climb regularly result completely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
