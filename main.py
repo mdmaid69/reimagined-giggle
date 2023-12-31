954659b3fd458871881fff71eebbffe8d856c457
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:55:51 +0000

    sleep correct pronounce factory police

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
