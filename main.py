5432de8821908a346bb6bce91002261ff4278fe0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:49 +0000

    club criticism invitation rise check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
