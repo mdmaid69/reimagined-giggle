e415b22d28135bd7d639fd95e83ab0033d31a7e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:56:48 +0000

    Ee mention correct dance yesterday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
