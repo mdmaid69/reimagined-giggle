48cd50a0d5c0f4ef7611f69f10113c1cf9e48921
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:54 +0000

    judge centimetre spring life tonight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
