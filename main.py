05312a22ed078759326f0d02c7c674bfdc49bdac
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:59:51 +0000

    when 565end floor jump disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def subtract_numbers(x, y):
+        return x - y
