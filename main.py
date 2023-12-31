213afe60c46ef7d1d65425edcab76aa4d3084c6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:55 +0000

    criticize usual health reserve lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
