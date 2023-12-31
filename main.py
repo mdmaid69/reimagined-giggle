1181aca3910fed264f6bb9473940d1ce2d6eaef7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:29:04 +0000

    Bb pay tip habit thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
