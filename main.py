28457f20c62150d665289bbc6283b3c0ac0ca114
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:27 +0000

    new praise Pp slip spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
