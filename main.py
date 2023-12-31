739a0bb2f8d479addc40954990e05cd28b34452a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:11 +0000

    late spot recovery increase believe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
