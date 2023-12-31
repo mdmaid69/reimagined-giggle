39bdadac41756df519eb67d944083e95a5e24b1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:48:11 +0000

    failure who lend roll wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
