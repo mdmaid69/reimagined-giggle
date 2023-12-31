8fa5ac2c96763d76ac0f36d26eb430bb8f8f29ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:27:18 +0000

    translate pot direct week square

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
