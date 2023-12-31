36f34d436ddaa3bc742e7784235bb6174db5b45a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:44:36 +0000

    miss scene clear sail splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
