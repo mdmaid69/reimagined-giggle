abc0a07248ab1313318f7e6d4fb361043e70e751
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:59:50 +0000

    calloff go previous check exam

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
