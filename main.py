f5bc0ae9c46bf5957e3dc1853418c4bda1074e9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:12 +0000

    open squeeze light equal stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
