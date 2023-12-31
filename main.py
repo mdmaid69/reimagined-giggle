fa3813e0af65137f0737f7ee3af7805c6529a161
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:34 +0000

    dare car touch Ee mood

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
