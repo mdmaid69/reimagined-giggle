799047fa3dda324b09a33b9be6a79059de8097ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:31 +0000

    over let possible life medium

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
