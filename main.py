5713d51876558bba28199bcbef1b6b0f7f93b01e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:25 +0000

    except thorough study comein season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_str(array):
+        return str(array)
