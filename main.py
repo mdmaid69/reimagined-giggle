ca84e0211f7d5f4404f3e9f221861ea4bfc54f5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:24 +0000

    winter position sore both smart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
