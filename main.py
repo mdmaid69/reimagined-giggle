8dd3e7e91d9c5aac5ca56c17cfdf82cd1b885b9a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:33:01 +0000

    murder table choose his length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def square_number(x):
+        return x**2
