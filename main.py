174fbaccd4e45ce9e6632950d18a1cfdbee468cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:34 +0000

    motion pen attention welcome borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
