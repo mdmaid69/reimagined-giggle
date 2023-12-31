6ac12674386119db757ccc5666e094fc3048396d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:09:55 +0000

    choose friend pretty failure callup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
