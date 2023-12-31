32720a4a627e204609304cb515c5db364f1128ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:59:43 +0000

    weather other move respect born

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
