5d7d619bd377e99be7943e1de9d52d4d72f725ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:27:27 +0000

    foot crash your slow callup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
