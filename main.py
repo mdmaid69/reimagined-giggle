e9cbdf65b7d54b288a686f7250b207386035d818
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:52:35 +0000

    luck drawing hungry machine thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
