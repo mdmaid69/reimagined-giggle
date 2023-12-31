5fbf63bcb6ce5d0c472d1bd96fdb663edbd002b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:51 +0000

    second Ll respect collection forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
