45fd7d685f431bc6510a103f7e0585ab29910f19
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:06 +0000

    hope sort behaviour everywhere project

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+print(os.getcwd())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
