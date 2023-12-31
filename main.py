c902df393796efc44cef32bbce4aee292c2852a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:41 +0000

    home pinch ours completely interfere

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def change_working_directory(path):
+        os.chdir(path)
