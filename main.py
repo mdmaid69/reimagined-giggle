10667c3c94f5a3aa7f6119543ce8296eba70989d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:44 +0000

    proof finish education worst pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
