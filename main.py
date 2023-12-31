666922cdeab5b1d5c5e06c5c1b1ea7903c4692d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:13 +0000

    measure shock pretty trust fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
