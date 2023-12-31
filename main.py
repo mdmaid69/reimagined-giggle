ec643099c0071df2398678ae82f119c851756c7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:51:35 +0000

    yourself dance popular wash special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
