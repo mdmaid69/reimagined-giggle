00d79b5c416c7bedb2d0a623de88d7c44332f120
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:40:33 +0000

    fence Ii dream hurt passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
