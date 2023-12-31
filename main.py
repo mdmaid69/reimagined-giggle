1bc798b5dcf5d79ff00c7516139d0b671e0d340f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:52:13 +0000

    rarely trip talkinto slice relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_min(lst):
+        return min(lst)
