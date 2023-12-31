34380789fb72f0c7892ab534a0b13a8482539194
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:43 +0000

    score mail jump shine even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import time
 print(time.time())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
