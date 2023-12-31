4fd8e96d1fa06555a36623111584795da362fcb3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:24:37 +0000

    accident city command close behaviour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
