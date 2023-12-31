8a37c6e0daeafd0b06b9749ecf7c82ba6a289050
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:00:47 +0000

    glad adjective relax private gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
