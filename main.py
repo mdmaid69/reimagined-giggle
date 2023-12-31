c7426e58d198a20802bcd8a9f2b73b207cea4382
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:17:12 +0000

    offer food secret follow might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
