c83be46e9fc3db1ba40a9f3db0f84c5ca83ee186
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:14 +0000

    commit man makeinto exit degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
