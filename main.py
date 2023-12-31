797632beb57fb00f9fb7ff87d2b05aefa27cfc80
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:32:47 +0000

    future generous visit children turn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
