9c66f28493d5e343fef7ebaf87ef32b933e56739
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:00:12 +0000

    attention once there arm risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def subtract_numbers(x, y):
-        return x - y
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_bool(array):
+        return bool(array)
