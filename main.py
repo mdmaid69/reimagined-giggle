92e77a9bdc4b786d4684b2788995233766992485
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:49:41 +0000

    book act wall this destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
