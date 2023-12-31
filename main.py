420440319630410baefcdabf57d79fe5983112c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:35:25 +0000

    supply though answer surprising weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
