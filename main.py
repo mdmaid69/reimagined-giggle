0a9fa0728abeda854f7cf57a5dca153b44867805
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:06:33 +0000

    regularly demand clue spoil female

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
