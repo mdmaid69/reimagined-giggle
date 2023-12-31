33b9f3d90ae6aeffe6207d7312a08cea710c64da
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:46 +0000

    down sure anywayanyhow lonely welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
