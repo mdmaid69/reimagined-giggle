69d1e26224f9b1baf779045217e51dc1d116592c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:34:23 +0000

    middle anywhere exam exit tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
