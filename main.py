192d01b76df4ae20abedfef3c55514204821e1c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:37 +0000

    backwards goout hit cover Jj

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def reverse_list(lst):
+        return lst[::-1]
