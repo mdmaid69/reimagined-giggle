245013c241c1f0510756be94085c391ecd6780e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:06 +0000

    ago 567enemy refer shine they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
