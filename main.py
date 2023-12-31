9b06c7c0a8afa25247e642979c142041259b7fc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:27:31 +0000

    poor fix shade song responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
