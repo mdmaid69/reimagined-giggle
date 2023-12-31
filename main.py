151df9508802cc5dc9e8e6093afdaac2725022fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:26 +0000

    of report thank report room

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
