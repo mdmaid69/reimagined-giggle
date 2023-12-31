b00f2452dc2cf1b7bd79117eca299c69fb390ffa
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:42 +0000

    mail be fit make direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
