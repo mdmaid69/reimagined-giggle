3bbcdca456aff9ffe704bead991f52ee88e992c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:22 +0000

    become deliver giveout disapprove happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_current_working_directory():
+        return os.getcwd()
