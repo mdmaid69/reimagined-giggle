e69fec6086b9c5ac1ab4154189f10cfa4446bcac
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:28:26 +0000

    treat tape release Mm year

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_str(array):
+        return str(array)
