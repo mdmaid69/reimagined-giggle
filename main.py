ae9068b90256c2a4255e978df679d1c50be290d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:30:22 +0000

    range firm violent thought typical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
