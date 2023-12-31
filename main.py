4ce16b76eebc18349a9c4bb35032a60911feaaab
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:20:22 +0000

    offer neck principle advantage remark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
