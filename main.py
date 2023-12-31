f9988a983c21fa9caf678b7132aa0a7e8b12e616
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:04 +0000

    future stand process home murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
