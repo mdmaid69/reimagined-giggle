5517270f0fd2ca75afd347e309178f0c6119113c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:14 +0000

    choice of doubt oil another

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
