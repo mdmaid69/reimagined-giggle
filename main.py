aa2304cfc9148b6b9a6c33620313b3450436df96
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:47 +0000

    office account it empty takeaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import platform
+def get_python_version():
+        return platform.python_version()
+import time
+def get_current_time():
+        return time.time()
