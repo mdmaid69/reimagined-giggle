53b13de4e0226a6ac9798816040c54988bb25810
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:50 +0000

    development frame convenient command Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
