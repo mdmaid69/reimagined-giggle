c57cceddf086ff7e9d189f5fb54ce08ea41d8824
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:03:33 +0000

    mark tense eat comeover never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
