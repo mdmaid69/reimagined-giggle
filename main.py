6b1023d0d7fdeda711f8c4c68bd369045a9df43a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:09:48 +0000

    swimming pain loud worry question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
