3bbf1bae68769958a72c45634c7bc15fd44b198a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:40:15 +0000

    rain who pattern book 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_max(numbers):
+        return max(numbers)
