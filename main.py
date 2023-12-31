6e2b0131bdd3da68b192f2513fe790c211dc8859
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:42 +0000

    godown aim permission whatever far

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
