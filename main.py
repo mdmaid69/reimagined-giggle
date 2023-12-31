bebd9cc8be2b5c9b5f433064b326c6576448ddde
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:27:30 +0000

    off clothes command address connection

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
