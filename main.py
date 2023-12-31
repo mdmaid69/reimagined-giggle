0569aaeb5a94f1cd3e3a35acbf16a1e6dadf98f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:09 +0000

    trousers forward wing bunch total

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
