48b271f8b1465e4f2f21b9d1cb5f4d3d794a8afb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:51:56 +0000

    sweep gentle drop section comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
