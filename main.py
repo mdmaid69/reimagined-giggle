e5debe52d20c6e495ce5e88b5c7cb38ba94ec370
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:26:29 +0000

    university feel bar 567enemy add

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
