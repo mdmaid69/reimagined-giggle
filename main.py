607c7902074cbf55fb64222eb3f2019a2b01c450
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:41 +0000

    hard burn arm side later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
