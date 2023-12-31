fe5e8c7cceab08833b65ec52bf510a5b8dadd3ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:51:18 +0000

    question joke base block design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
