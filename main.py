6481ffeff46010e5e7e871631af74747fabf6df1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:14:34 +0000

    popular cold sharp connect majority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
