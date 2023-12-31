a0547315ddabd572117696e4016ffa8502476e97
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:30 +0000

    sky rain fail succeed thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_numbers(numbers):
-        return sorted(numbers)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
