e470783bfe20523f77f366b923c9a0c92912f963
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:00 +0000

    everywhere include sky wake cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
