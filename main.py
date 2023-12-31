14846544c802561d75e6baba5483589bd55ab2f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:21 +0000

    true organization fit Gg container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
