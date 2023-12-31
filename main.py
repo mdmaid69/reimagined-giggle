4e0862de79ccf89674cc56d4c96b53144217d39b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:10:29 +0000

    interest gradually get onto cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def square_number(x):
+        return x**2
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
