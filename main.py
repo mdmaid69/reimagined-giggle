bbfd303e73881e5ef45d265c34aaf11bc263f619
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:33 +0000

    programme history cry circumstance Qq

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def find_min(lst):
-        return min(lst)
+def calculate_arc_sine(x):
+        return math.asin(x)
