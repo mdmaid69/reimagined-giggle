da014e9ed8a2183f102b5559c38bd68f21682d2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:27 +0000

    answer professional earn culture material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
