850b89240e383e94fbe82e4d969524c83ec49b1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:51 +0000

    plant admire complete commit mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
