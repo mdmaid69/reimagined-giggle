a4039d5740d52d66eea4c1657dce7ec7cd986106
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:38 +0000

    telephone but closely measure concerning

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+print(sum(range(10)))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
