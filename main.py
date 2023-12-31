ee24bc5c00d9d80be694d21b878083f0bbd8bd3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:11 +0000

    murder independent backwards delicate petrol

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
