0a8cfc5f535c86f123199a98eea0c307fd8d4ec9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:07 +0000

    legal properly day food achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
