8cdd194089af1a310d920054676613e3e5d7574e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:32:38 +0000

    help relation talk sorry light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def cube_number(x):
+        return x**3
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
