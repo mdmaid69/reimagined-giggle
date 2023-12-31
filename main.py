abd757314d35f6798c37875fb3c11f6cc77cc93e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:56 +0000

    frequent secret holdon sell light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
