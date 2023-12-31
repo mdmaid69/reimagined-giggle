3a5d2a319fb3cec41fa7680ba6de53d7ef08ca72
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:29 +0000

    model realize remove care else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
