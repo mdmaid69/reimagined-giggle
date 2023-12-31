8a0ac6822fc3e49f315fbef3d274fbda4ff35995
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:49:22 +0000

    among doubt furniture contrast command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
