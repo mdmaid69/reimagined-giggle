aa465fe5b243089d11264356919d906cd96d06e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:24 +0000

    hold grateful avoid unpleasant attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
