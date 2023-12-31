a179406f88dbbbbc058752c9b261da6c7dd633a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:21 +0000

    book occur expect spoil scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
