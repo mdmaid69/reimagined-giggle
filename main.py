2c861ac1bb207e8c6e9daf9b2ac35a3dad55825d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:54 +0000

    bar check culture mess false

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
