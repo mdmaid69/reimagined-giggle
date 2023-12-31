f5c4bd26e491517076c7a32c30bc9a0d64bf5269
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:43 +0000

    borrow style attempt cool grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
