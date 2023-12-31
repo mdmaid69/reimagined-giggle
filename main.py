eba7b3a7d6413632cee2de1b2ccd45c62ccb6a3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:04:18 +0000

    roof create trial mother zero

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
