66a6dfc50473317b09160bebb8ae65a42802ba51
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:49 +0000

    line regional step welcome Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def is_even(n):
-        return n % 2 == 0
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
