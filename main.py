f1e72c935ed9019eca08a1c24a1cf6f36afe4a2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:18:36 +0000

    float wish branch public prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
