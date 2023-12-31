1165ddda9263e2c038e275ed1b62822f0b08b1ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:20 +0000

    height behaviour crash every communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+def convert_to_hex(n):
+        return hex(n)
