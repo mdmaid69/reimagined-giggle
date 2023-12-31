a76c9278fa53f418fc70f3ddc9327a299b122e46
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:41 +0000

    in there poison cut smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import platform
-def get_os_info():
-        return platform.uname()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
