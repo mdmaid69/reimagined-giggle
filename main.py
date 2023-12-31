ee85cf27c5c3b8d83f722f915ba797e4fa8a633f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:45:18 +0000

    conversation react centre pass form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
