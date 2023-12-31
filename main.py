1ab3ea5e576e4899705f7bb43addec7910548126
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:52:41 +0000

    means give television indeed close

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_density(mass, volume):
+        return mass / volume
