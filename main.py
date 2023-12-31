ef8ed33cec9d5999e64994dd99fb54eab906b143
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:03:36 +0000

    mean strike improve description under

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+print(json.dumps({"name": "John", "age": 30}))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
