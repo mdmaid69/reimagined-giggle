8fffa7d93d0bf577095a585ebcd377355e28c2d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:09 +0000

    thirsty thing various tradition closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
