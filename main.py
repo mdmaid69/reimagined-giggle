bc3bb9096d47981915bffa7d870862689dc9ebc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:20:13 +0000

    lay glass broad duty people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
