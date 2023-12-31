b148540b53dc1ca3f1b08bc9803b7ed0043c3ffa
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:01:59 +0000

    can convenient convenient strict post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
