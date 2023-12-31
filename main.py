568c70f9fc5c818d6dda165598fb0322961f7bfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:23:48 +0000

    morning acceptable back thinkover back

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
