f5d3c8dcc89eeddf6e44b9dbe962a5b6bada75f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:09:36 +0000

    right thumb whichever back deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
