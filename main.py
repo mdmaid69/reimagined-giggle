549bdfc9649b95ae9dbe6cc5823f488982e859e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:08 +0000

    turnaway broken round price pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_list(array):
+        return list(array)
