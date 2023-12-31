281c333d714dfd55d33bcf7aba751a1bd6096ff8
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:01 +0000

    capital he ashamed stranger become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
