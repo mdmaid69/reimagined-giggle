36c6ff1de82e55dd8b59717c9fb953246fb51e90
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:59 +0000

    decision warning home spend standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def change_working_directory(path):
+        os.chdir(path)
