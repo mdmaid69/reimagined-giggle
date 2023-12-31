149bbf3d167de4943429aa1fadbc6a95113ee00c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:39:24 +0000

    ever street pronunciation decision sport

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
