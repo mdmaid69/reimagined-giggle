e67c25ea9815730a38a682dc69e36fc5b446d158
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:48 +0000

    fair operate provide worth moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
