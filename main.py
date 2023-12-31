d9f09598583d543e71843fbb53c56862cb0000d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:02 +0000

    comeout uniform tell breathe butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
