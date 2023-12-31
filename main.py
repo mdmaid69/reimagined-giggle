3d2e4f061181e285c4320dac02a847a334e6e7ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:12 +0000

    except if callup brother gas

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import time
-def get_current_time():
-        return time.time()
-text = "Hello, world!"
-print("Characters:", len(text))
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_str(array):
+        return str(array)
