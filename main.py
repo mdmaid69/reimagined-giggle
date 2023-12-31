e7681dc3a4c26127ab9e6207c23abb07d7be59d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:33:36 +0000

    open emphasize dig visit contents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def divide_numbers(x, y):
+        return x / y
