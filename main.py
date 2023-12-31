8cf97c7501c18deb6e92c5c1b2d52636e36689cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:36:52 +0000

    stone until fact language more

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
