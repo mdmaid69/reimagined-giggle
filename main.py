eec3c5e3e02ebc862799f3264d126105b6964035
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:38:52 +0000

    your in Qq treat standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
