1455e90eae3646966936f005f2c69af0cf73cfea
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:24:11 +0000

    hurt anybody spot business ought

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
