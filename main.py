8d09be178e70f980f4fec0aad42d2a846fd6d4d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:00:24 +0000

    damage challenge near blue country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
