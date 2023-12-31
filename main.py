2bcd9eab6dc497cb329cc124c830aee5e89d4704
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:26 +0000

    strict meaning compete unable memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
