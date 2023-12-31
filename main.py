6cd9fd6f7e406c8ea6f23efad1edaa22dfa22518
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:34 +0000

    past morning few tidy hole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
