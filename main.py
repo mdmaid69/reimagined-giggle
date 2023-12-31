580522b557a2c36aaabc39678cd721c57adfccaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:18:57 +0000

    closed comeoff in hello trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
