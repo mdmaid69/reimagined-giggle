ad28aa36b43717dc4794f0031c800451993bf44c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:39:08 +0000

    slowly accommodation away fasten accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
