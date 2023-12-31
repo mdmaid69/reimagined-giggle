1437a486db781b05c18cb06329d3b9f61ff70454
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:52 +0000

    greeting breakup standard Dd year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def convert_to_octal(n):
+        return oct(n)
