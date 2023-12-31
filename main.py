e5cfc8fa65f1112d16ceb23913b73cf3722ac307
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:40:01 +0000

    slightly child luck only much

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
