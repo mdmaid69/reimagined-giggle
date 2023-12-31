c153f037f82002face9c0d5c782cb157e60195a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:31 +0000

    far talkinto sad somebody picture

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
