ae22a9f54b18278b7d542e07182073846dffa3d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:44:11 +0000

    nature attach education bus confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
