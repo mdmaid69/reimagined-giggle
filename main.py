2eed24c0479e558fe89f03ef2b9cba256d5c074f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:23 +0000

    disturb day excellent mean Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
