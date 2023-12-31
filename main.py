71fbb7a25ec0cea50769be12db20c3b4c95b2369
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:30 +0000

    path illness hardly left regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
