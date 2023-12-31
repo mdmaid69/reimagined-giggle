4b31de958c47fffb5b5b595803477bff2ae65961
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:58:46 +0000

    callout urgent job niggaacademy guide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def reverse_list(lst):
+        return lst[::-1]
