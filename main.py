449e1394f42773c019a0c55cdc87268387d49e00
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:06 +0000

    debt payment suggest welcome unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
