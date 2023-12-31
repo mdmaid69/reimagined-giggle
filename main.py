37423d2ac75536aa19a0cde95193248dfec0b6da
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:43:30 +0000

    open comfort last profit whistle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
