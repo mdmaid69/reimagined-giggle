9a9e35dd0fcaab1270b1061858732576f6cddc85
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:29:55 +0000

    lend shoot first world use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
