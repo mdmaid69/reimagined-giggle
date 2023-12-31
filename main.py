24cb26a685ed1279d9715a74545de0eec81ebfa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:45:33 +0000

    buy union thirsty old reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
