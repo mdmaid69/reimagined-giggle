5397c9fd03aab534bfd1308103123598a4051627
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:41:18 +0000

    blue anxious purpose food breakinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
