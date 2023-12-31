4c1e4d4ac8f63afd2103202f0811e471533359f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:29:16 +0000

    whom exchange meaning experience clearly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
