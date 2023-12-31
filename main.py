ef33cb95bced6ba31426ab5bbbdf701e27c669a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:30:29 +0000

    obvious couple rush aim live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
