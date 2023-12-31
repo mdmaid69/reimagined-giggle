67d23e8637574549fb6385b2a1613ac22921fcdb
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:13:59 +0000

    become runaway length ring tall

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
