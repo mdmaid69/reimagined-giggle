6454672573789b60e08b0edb9a51697cb16b4098
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:16:51 +0000

    makeinto do invitation finger situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def convert_to_binary(n):
+        return bin(n)
