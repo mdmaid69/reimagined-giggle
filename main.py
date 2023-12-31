c46aa1f7d3070949136a2c106c73bb4a34e6e3ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:35 +0000

    brother hurry loan basic combine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_max(lst):
+        return max(lst)
