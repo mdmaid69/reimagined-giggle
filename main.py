d065b87a36447f3d226d61d10a39245cfd301b82
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:15:41 +0000

    upwards hide of careful backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def is_odd(n):
+        return n % 2 != 0
