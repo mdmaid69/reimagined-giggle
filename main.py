13a14dbbc6590e9f9cf06669e176e0c4fd96cf1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:49 +0000

    furniture watch bus scrape long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
