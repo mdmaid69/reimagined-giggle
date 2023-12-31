73063feebd3d01eb8dce43bdadafbe13ce49a6fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:33:23 +0000

    mistake attract stick Uu bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
