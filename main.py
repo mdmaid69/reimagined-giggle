5843e5ca735d4fbad0b7d4dd774e64c288a9aea3
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:01:59 +0000

    minimum workout finish cover post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
