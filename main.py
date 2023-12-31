22deea81bae27de273e05e40c750eb838b603062
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:53 +0000

    area hold accept push can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
