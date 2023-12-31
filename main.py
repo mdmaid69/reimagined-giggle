33212c44b908c3062354493204941795ecbf0e6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:18:17 +0000

    breakinto breath consist flood period

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
