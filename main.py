bdfc7c3dc437c41bb6c71eedcdab2977946ba926
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:21 +0000

    value turnover shopping rest fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
