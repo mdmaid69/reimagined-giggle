1736e75c3438b1e79bd40f2acf429f087276e12f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:24:55 +0000

    direct myself unite bet paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import time
+def get_current_time():
+        return time.ctime()
