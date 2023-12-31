232ef5d28c75a3f945615c86922b4aa66bdf819f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:15:01 +0000

    copy command fortunate perfect seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+print(sum(range(10)))
+import array
+def clear_array(array):
+        array *= 0
