5db1e6cbf63273b691efe7e40082196baccb46bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:18 +0000

    brown city keepup scrap clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
