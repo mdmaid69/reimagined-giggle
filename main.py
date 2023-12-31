b58a2c6971415df69b424b576a71bed916491611
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:17:51 +0000

    cool level jump forget Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
