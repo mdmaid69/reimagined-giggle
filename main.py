43bf51c6290539da221e2ed109b02f009959b410
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:38:58 +0000

    pool school emphasize with computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_area(radius):
+        return 3.14 * radius * radius
