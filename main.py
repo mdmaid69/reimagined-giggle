9cf72ebe1f65ee19259e92c037ee95423eec504b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:15 +0000

    discovery charge owe able suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
+  def convert_to_binary(n):
+        return bin(n)
