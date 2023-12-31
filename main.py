d11a03906fef8ec578b6097ab63226e8194d1e7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:12:03 +0000

    hurry act that flavour then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
