da68e462c89677e26fdb83caaafdbd3c7044f854
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:13 +0000

    noone stage tomorrow declaration plate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def convert_array_to_unicode(array):
+        return array.tounicode()
