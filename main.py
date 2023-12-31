ca931879ad9a7d2d48988e1e1d90c02887a6c7f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:26 +0000

    raise badly system ought relief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
