0159ff0eb109e8e27b90d9945eb82484a8295919
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:57:58 +0000

    maybe instant getoff error extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
