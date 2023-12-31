6ad7a82ac97fc122c446ae6372b39519a2f890e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:45 +0000

    alone store chat wide deep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def cube_number(x):
+        return x**3
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
