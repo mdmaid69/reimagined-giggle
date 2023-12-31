c002cf3dd0f42706a8e4e174a1b89ca61f3e45c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:53:47 +0000

    note pot rain nerve uncomfortable

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
