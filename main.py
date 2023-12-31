4e21204ebf0e3dc0d3764a8b2104df742b3a87e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:04 +0000

    nerve reject confused find yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
