b1f281ede8502d15dee95468bda4c84be4739c72
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:02 +0000

    only set children takeaway old

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def count_characters(sentence):
+        return len(sentence)
