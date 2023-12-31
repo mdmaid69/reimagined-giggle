8464c85808cd191dc744d1f2cae90e6fd627bc0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:57 +0000

    appear emphasize ilovedicks bank coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
