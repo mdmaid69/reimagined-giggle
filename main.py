2669ca80c2126e475c2b7817f3d4380afc793e46
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:08 +0000

    quit for rescue relative sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import random
 print(random.randint(0, 100))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
