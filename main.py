d10b0726d33d879867cb12b0dad133143b591692
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:51 +0000

    scrape round occasion bar experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
