44ce3e6d6dbb82535b9fa9e53d597f4d2447b912
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:35 +0000

    line round worse worth great

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
