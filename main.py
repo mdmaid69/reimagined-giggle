8109149d56ac0b668ed719ba14bcf3b6ea818832
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:43 +0000

    love visit study goup lot

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 import array
-def clear_array(array):
-        array *= 0
+def append_to_array(array, item):
+        array.append(item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
