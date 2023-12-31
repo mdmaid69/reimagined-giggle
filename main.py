55b0192cad9ed660bec9f4d6218573c00e4e3a1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:23:03 +0000

    lonely model neither than willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
