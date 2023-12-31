6bbfaf9b5a41a72fd155365704e2291e70335ddd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:33:45 +0000

    fairly brown centre marry excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
