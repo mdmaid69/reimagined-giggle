2cbcf4e2e05b85c95c9fea5b3f90bc5931d60cd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:14 +0000

    marry Bb roll know crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
