d9567c7d7ad37ca5f4168dd7c7bc8aafabc89bf9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:18:51 +0000

    sell future reject punishment could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
