372994b28f908f2e607dc486cc7d241edc046175
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:12 +0000

    fault treat yours miss noisy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
