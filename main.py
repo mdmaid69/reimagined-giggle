e649d924803864a2dd259a9772a2a9fc4c5d5b69
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:03:07 +0000

    offend language back runover cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
