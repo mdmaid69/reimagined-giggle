1e48311222c0f1543c29d2d0a4effcb2c9526194
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:23:28 +0000

    estimate worth bound mention feeling

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
