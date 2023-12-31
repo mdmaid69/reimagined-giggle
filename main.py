7a6bb8fac549c08d5b72400431c831c717a07f72
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:38:41 +0000

    accident judge train straight live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def is_odd(n):
+        return n % 2 != 0
