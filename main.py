e4afb9ddd818aeabe645502144bfb06fccf0d304
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:03:03 +0000

    nerve transparent moral advice climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
