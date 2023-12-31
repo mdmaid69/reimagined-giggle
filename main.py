5c3126f6d548d9d08f39ada554f247e75332deb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:17:59 +0000

    ours set need only recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+print(os.getcwd())
