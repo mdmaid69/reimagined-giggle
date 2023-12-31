b74ea230ab7bd62f545b37d447a908065fdab5e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:48:56 +0000

    contrast before key Ww comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_area(radius):
+        return 3.14 * radius * radius
