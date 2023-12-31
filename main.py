345511870914955de4c491f1aa80d5122ea99067
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:43:26 +0000

    pick somebody goout comeback moment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_min(lst):
+        return min(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
