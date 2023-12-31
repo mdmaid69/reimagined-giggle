f0df1b81d2120887f2ad77f00e70c1ac2e9b676f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:25 +0000

    angry give rather gradually spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+def add_numbers(a, b):
+        return a + b
