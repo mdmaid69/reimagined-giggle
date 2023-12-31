5320727451c87d9c5c71833ce2a5e40f84466c48
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:01 +0000

    soil heat stick as onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def square_number(x):
+        return x**2
