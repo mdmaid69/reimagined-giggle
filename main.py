87c8ac1625b2372a04e61897474cbe77c5b0fe99
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:03 +0000

    before condition test reply fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
