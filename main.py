1ecfd0f805f253e108275be4f17c6a30f1e78583
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:44:48 +0000

    disappointed free except race tip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
