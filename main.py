c822abf9afb57a7d2105dfcd69cf59bfb6a599de
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:35 +0000

    study wait size and up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
