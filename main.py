dbf4a709038032d6a460d569313b7b6c3f76706a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:45 +0000

    subject switch later tie turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
