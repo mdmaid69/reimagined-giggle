eab2aa0e5c49351cb3d41114e58378575337a866
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:57:17 +0000

    mixture club if wander understand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
