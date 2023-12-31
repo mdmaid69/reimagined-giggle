4f23176f6e53b032d195dbc15f75056b0fe2fb30
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:21:04 +0000

    commit weigh honest brush cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
