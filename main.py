78069ce818ae96683bad705247424041a8761643
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:55 +0000

    left write mass attitude store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
