8e07dd3180e94d18d072b73cfdb26b23648986fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:08:34 +0000

    traffic poison target closely unusual

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
