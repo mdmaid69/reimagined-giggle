819d08002b9a2033a3bad1404ffe6dfd1cf363b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:19:21 +0000

    meet duty best loud mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def get_array_as_frozenset(array):
+        return frozenset(array)
