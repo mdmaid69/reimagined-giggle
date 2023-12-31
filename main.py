680cd8b2de126c75623d751aa2c6ac35e39b7a70
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:45 +0000

    border stick suit cum 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
