252432d772aaecea5200a3ca65af3f841e6726b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:17:48 +0000

    reject small meet deaf principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import os
+def get_current_working_directory():
+        return os.getcwd()
