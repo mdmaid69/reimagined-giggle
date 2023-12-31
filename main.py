d9d6b65e11bea5f9955c74d8e30b6041acb0c10d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:01 +0000

    record fat for slowly rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
