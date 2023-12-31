d0be91b6f92cde46db23d708ba6f14ed2f8da7e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:40 +0000

    professional Ee although oil turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
