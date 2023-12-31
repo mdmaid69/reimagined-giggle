629521de75d7496880b2d23f9145135b429ebcaa
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:00:22 +0000

    he remark easy professional that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
