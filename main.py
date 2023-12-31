6112df6f3c4f5bd3742f19f5d6f76b51eca1ed5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:36:34 +0000

    beyond terrible steam reduction sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
