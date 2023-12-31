696c84bb46529a51a9d154549b061a01f38ee892
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:45:49 +0000

    exciting thinkover suit Ss visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
