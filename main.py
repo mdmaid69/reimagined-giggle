81ca24d8fc726c62896f3d0e942e5d6a4fabc80b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:53 +0000

    organize react term immediately satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
