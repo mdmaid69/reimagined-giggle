c94163d401c8b8a6ade1e8a6f8294935222f433f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:57 +0000

    odd justice software change seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
