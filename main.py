d97a75a1f1273fa17f66b0ebbc552d6a8fa8315f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:44 +0000

    hello perform sting try unlikely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
