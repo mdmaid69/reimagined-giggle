3071fcdcf3820f39caf3e18047887ffcb4b7bb6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:12:07 +0000

    this basic burn soft container

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
