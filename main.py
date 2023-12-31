e72725f831dcf9a85e94a4ee87c4eaf46abe9a21
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:44:18 +0000

    help name instead damage touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
