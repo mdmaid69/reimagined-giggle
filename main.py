cfe9319afdd90b34553ceaf2bdd208dc57a84cc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:38 +0000

    regular slice fairly succeed giveup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def count_elements(lst):
+        return len(lst)
