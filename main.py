98263c02fb0e253dedf56f061dfdf8d78daf63a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:38 +0000

    hurry information into cross come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
