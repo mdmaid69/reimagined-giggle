d0f395440328c3887455295e22705ceec06f0a8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:11:26 +0000

    control forever feel little hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
