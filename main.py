27144415fde438033cb0514abf3abca16d2b5e0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:24:33 +0000

    surprising describe later minimum plan

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import random
-print(random.randint(0, 100))
-print([x**2 for x in range(10)])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
