1e2cf7f99fcfa6df956ed3fcfcb8dfff2140fa9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:16:27 +0000

    mother union right choose act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
