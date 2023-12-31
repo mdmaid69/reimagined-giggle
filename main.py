9b225292cb68f8d9aa20eb0b561bcc0b98d5826b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:47 +0000

    accident comeround set breakinto warning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
