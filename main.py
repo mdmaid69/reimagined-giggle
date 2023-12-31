aac759ca62a908cdb368a3ce62808266ec212bec
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:40 +0000

    middle because church comeoff risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
