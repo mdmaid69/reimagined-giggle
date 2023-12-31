664a2843ce2565731b06d55256c997a4566c76fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:06:22 +0000

    ground profit substance hide funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
