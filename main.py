30dcc0cbb65f3814884bff726657e022c8809c70
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:37 +0000

    interview tread pinch argue round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
