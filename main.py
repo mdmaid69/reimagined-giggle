0011d0a6afc12bbad1661889f0b4bde8ab29a7a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:45:53 +0000

    smile scream relative keepaway tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
