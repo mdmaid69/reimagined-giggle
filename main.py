a2430c5c41af51b3c1da589fe82dad5a41f61b51
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:27 +0000

    address whenever human shop goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
