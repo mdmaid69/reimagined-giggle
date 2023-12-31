2380baa615aeaf9264556df00f38f0ffcc3fc0df
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:06 +0000

    introduction lookaround treatment handle book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
