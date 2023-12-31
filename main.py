da99be750a4daa5a50aa58336fd1fecbb750e04a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:52:56 +0000

    reply fact characteristic fierce accordingto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
