08d932dc9787dde8543ed0537d2ba7314799dcfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:17:11 +0000

    pour easy myself desperate ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def reverse_list(lst):
-        return lst[::-1]
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
