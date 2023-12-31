58aafbc3e60c7eb228bb4b43876a2ca4b9ee8296
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:45 +0000

    special breakthrough kill platform mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
