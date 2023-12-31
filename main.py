3f0c2584d391d8ee6aeb780d056a58c69bd6e817
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:26 +0000

    them have fail room both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
