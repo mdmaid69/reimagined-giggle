d057ea7ea9da0de67c0ad0d69438e7ee0c21a8b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:49 +0000

    shout nonsense standard never uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
