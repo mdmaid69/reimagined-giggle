396f93e0a0b76ac49cafe59b90c3a1ef97898ff8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:12:53 +0000

    urgent right please bury thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
