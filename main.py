4170b32b7182971733ba5a209a9bafbacba3b10e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:38 +0000

    regional open remember back telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
