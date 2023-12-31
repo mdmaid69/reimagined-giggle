07693307b79078626534e3925dabb817550737ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:59:13 +0000

    confuse argument recently provide high

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
