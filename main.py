813a8c5c0cf6b4f2a54b3f05a4d2c41dfdfb27fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:27 +0000

    run influence problem sting profit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
