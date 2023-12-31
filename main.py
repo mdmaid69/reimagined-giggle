70c62719239d5ed15e27748f32f5f7b5207fc514
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:23 +0000

    trap rest middle desperate comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
