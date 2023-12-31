79384c5fcc339dded12730d15309a741dcc89aff
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:01 +0000

    develop account stare opportunity complain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def find_min(numbers):
+        return min(numbers)
