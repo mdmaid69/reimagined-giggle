af57dad2ba924927c3e4f3abe095f4bb101ff003
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:17:21 +0000

    relative whole reliable middle finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
