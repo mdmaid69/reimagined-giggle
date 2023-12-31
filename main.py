d2c8222faba9222df316d79ec869e34a2b6d0dd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:33 +0000

    feeling lunch steep hers plate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
