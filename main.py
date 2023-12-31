f96a6086706de248aed98feca88a5f24a9733eea
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:29:44 +0000

    grow cover polite calloff light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
