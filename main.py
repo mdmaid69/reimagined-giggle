3fc53d3dd5f5bc687e3dfc8bef7a48b5dae6d22c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:00:38 +0000

    pencil mix refer matter least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
