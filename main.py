d2e8c8fbe9410565156dd2f87b2a4587b0b3d916
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:14:40 +0000

    trust entrance no mail cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import sys
+print(sys.version)
