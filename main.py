e356e1a68931e04e3c6fc7eb9b8d82e5cf172c9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:17 +0000

    surprising sport opposite vision what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
