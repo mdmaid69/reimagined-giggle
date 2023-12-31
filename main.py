b558d085de1367749fa0c78fce7b7ed6b7039d35
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:09 +0000

    debt trap division further decide

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
