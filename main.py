2ccb7637cd999ef1d572de7317077ec35f66f9d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:27:29 +0000

    considering ago tragedy belief love

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_elements(lst):
+        return len(lst)
