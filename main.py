09b5e43f1e828fd5b767e3af55000cfe518fcba7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:30:41 +0000

    before use consist eye cinema

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
