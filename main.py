4175c7063fd000db6690bb330d889b4c72e4a3a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:06:07 +0000

    cover pass company scheme back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
