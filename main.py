ae7e118f9bf9b1f97a409775c92822c981fdb593
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:11:48 +0000

    mistake bottom bend meaning dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
