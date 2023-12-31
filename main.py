af03683022b8c6422b372f9e30aa0ffa0c2625be
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:06:35 +0000

    confused commit possible still union

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+name = "Python"
+print("Hello,", name)
