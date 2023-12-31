a093fbcf7269b25b38cc2b6e4939d701f443bebf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:03 +0000

    keepfrom mysterious great full near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(5):
-        print(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
