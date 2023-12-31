4bdb6bfb6d3c9ab9a0234466b33dd0a1024903a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:53:16 +0000

    sometimes talkback 572enter fault little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
