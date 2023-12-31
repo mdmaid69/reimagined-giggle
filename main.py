02ada5e7717105093a5ea26c0bb13fc8f2888939
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:27:56 +0000

    plenty sad mend depend beginning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+print(time.time())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
