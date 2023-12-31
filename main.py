a1d587b9a6e88cc6c116d5f7a09bcae63d4b18b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:48:16 +0000

    drink field workupto Ww guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
