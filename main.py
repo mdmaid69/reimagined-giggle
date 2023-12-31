3f808a49782765cff9cd34dcded68b2dadd35f81
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:31 +0000

    instruction so breath bush centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_typecode(array):
+        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
