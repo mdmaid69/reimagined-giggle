7843e3f5d7bbe8a958d25fdb8c39307c6c659f15
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:40 +0000

    sleep number disappointed strike weak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
