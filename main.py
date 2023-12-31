9e4df05be51f9e970547b552b756295573e1db1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:11:30 +0000

    eye lookaround left share curious

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_exponential(x):
+        return math.exp(x)
