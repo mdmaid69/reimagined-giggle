5e754462060cfe4341156e4350819b4d2f0efeb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:46:45 +0000

    anyone all interesting mean attract

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
