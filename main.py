a8ef38a11b8b94eee63c1efeb94d098cde784f31
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:08:29 +0000

    themselves left rent room thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
