684dfd029d938249e8d151137db3c4ac2a70c847
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:20 +0000

    hole receive report sense opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
