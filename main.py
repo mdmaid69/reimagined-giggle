3d89c2f30158e7506e03a4940684b04c55f5a667
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:34 +0000

    crash cook solve fortunately goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
