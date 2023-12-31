78a9dd9df978022e2d9912eed1673db0018d05f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:47:03 +0000

    man easy comment quantity tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
