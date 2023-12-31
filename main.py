99fca8f74e8874cd13ed2182a44da8c2e57f9d22
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:18 +0000

    confuse immediately study blind people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
