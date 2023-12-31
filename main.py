a89fb7ac466656e14dec33950e159c9e8fcd6650
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:10 +0000

    nothing worry experiment mend ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
