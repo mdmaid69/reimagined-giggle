8ba614a62c122f24092331046c1c062163bdcedf
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:22:09 +0000

    run suck suggestion position at

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
