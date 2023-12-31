b8ec9cdf9ddfffa221956b2041826468d6ce6e7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:29:28 +0000

    test collect ring brave learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
