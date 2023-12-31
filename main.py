2dcbd4ac71980e6935929ef8fd4b58dc68551659
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:14 +0000

    excited occasion school eat principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
