45153b98e276397ae6644efb24f0c48d6ebd2af9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:47 +0000

    stir drive deep mainly related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
