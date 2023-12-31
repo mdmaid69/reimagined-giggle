412c12656c79bbb5a01d07ff456bf4572a2766ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:52:21 +0000

    hand defeat oil artificial talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
