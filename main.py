8d89579f0139db4a56d3d56a2e08c8dd95f343cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:29:31 +0000

    female someone move life touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import time
-print(time.time())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
