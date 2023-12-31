c8b7251feffa7a3231694f04d699f360940cbcf3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:55:41 +0000

    fortunately force visit unlikely fail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
