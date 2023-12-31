858ebb6125ddc8fbfa3a20b3a005e94736622b22
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:31:24 +0000

    protest fond radio weak swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def delete_file(file_name):
+        os.remove(file_name)
