7423974a85839368330ab1d9660fec3092fa7e20
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:43:29 +0000

    nearly start matter what trap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
