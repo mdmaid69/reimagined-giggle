21db34118b4851fc4a55959894114f4c92f23641
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:16 +0000

    spite depend holiday although classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
