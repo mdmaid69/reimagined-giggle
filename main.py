b1b884d47bb0ce4f2f8d70ecb0cf80ab99c14bba
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:50 +0000

    soft characteristic consult development ask

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import sys
+print(sys.version)
