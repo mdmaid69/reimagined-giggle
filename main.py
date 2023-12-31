ec1bf016ba1ed945494da248fbac9d518d5598b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:42 +0000

    nurse result broken lock side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
