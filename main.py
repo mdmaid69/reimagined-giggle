981920ce4f127ffd1715f496420b73de12bc7599
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:44:31 +0000

    people exist embarrassed blame large

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
