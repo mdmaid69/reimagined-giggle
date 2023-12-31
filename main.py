0edbacd12501bd7099ade0a0a8776c45e2002591
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:56:18 +0000

    since rough thin emphasize centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
