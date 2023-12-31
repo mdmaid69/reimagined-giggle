0e0daea85d7f30f4be1e0d2a6a29ac21c2af134a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:05:42 +0000

    pocket operate patience performance organization

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_max(numbers):
+        return max(numbers)
