6c28739126ee31be35675d886a26d1d6876ba35b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:27 +0000

    ask bang 566end share answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
