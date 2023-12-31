5071d06a5f81ad00e8268d228ccae30557f6700b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:55 +0000

    exhibition guess single heat ride

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
