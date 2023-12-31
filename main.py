c87dbb48f0d2855017d5b7abf76113ac4d518cbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:43:14 +0000

    paper breakdown breakdown stay allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import re
+print(re.match("h.*o", "hello world"))
