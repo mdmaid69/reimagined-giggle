022ae36cb7fb3fcb810838c6e28563f2e2117e52
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:32:54 +0000

    disturbing since either live touch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
