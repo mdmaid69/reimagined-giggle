2019a10adba0d72a73e000a57ad92dbd3cd1adfb
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:51:45 +0000

    comment that pupil load return

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import sys
-print(sys.version)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+def roll_die():
+        return random.randint(1, 6)
