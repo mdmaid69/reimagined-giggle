25d5131ddeec68a067b7feda1c91229b6bef4914
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:37 +0000

    dead thing by breakup distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
