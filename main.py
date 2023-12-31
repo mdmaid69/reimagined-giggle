b441a9b0135ff5346fcf96589971a1b081b7a25d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:06:17 +0000

    safety smart kick sky recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
