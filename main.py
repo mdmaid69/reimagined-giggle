b76654a30e8b5b294596cefa714bd29d91379444
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:40:44 +0000

    thing embarrassed warm male clue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
