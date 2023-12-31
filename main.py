0f0a967bf496a104aee5ac455756d551402cb8e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:33 +0000

    trade track joke under excuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
