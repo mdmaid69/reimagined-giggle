92f65110961292ca84a857e28692a8265cd4de65
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:38:11 +0000

    cover shade hard often workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
