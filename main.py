6269c5ced2d82383b03f929289896805d3b894d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:06:26 +0000

    answer knock mistake crash work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
