fe9e8a0d13e1087f97b18848fb86a4e653305aa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:13 +0000

    event trick abroad giveaway easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
