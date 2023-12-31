0047479acd6f323fef855c51a07f0a3024248029
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:00 +0000

    actually welcome spring to accommodation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
