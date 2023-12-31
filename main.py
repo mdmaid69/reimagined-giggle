bd1cc9247bdca5f08b9369908048210f13f5b6f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:21 +0000

    anywhere issue judge surface shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
