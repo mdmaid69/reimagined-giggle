a4aaf152aa4edebf10b54eecee2b301861dda46e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:06 +0000

    holdout nowhere throughout exist question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
