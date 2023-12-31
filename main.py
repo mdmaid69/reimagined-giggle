9c278ae3e2955b40ea4934b3c8a3c9c45b485a61
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:54 +0000

    before grade answer very uncertain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import collections
+def create_stack():
+        return collections.deque()
+import sys
+def exit_program():
+        sys.exit()
