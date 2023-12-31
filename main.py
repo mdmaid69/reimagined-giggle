ae4fd05d39968c21fc338dfbcc3947c8574bbfd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:41 +0000

    work fail any check government

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
