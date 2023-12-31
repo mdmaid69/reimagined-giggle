25fd83c18e22b4d1480af04b43b6e121ccf7d11f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:17:30 +0000

    pinch tear Mm abroad yes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
