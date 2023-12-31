ad32f3782e17463ecdae6c7b8ea92a7f7cb1ee04
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:31:21 +0000

    computer worried least lucky coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def count_characters(sentence):
-        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
