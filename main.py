17135799b0a885a0bee883f73b4bcdc65cf175f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:56 +0000

    length guilty instruction team leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
