0037a063cb3a540234fe2ac5d5a36140b2c0a04f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:58 +0000

    burst mysterious wrong original influence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-print(sys.version)
+def convert_to_hex(n):
+        return hex(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
