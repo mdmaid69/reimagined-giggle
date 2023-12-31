c319596d3064a9b0d5c9c95ea8ae41d9e684cde3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:29:27 +0000

    once wet relax neat skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
