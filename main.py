63aeb583b92ede5c5db6fa183ec9dcb1358fd2eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:12:14 +0000

    show connected ticket eat children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
