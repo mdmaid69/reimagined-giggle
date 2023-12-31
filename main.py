e8ff10d3e30b3e95b2a179f32949af3b5896e163
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:15 +0000

    quickly break Ww tough season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_stack():
-        return collections.deque()
+print(sum(range(10)))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
