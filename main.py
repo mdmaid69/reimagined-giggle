22b76e97b46b8abfad77c8269180cc36d613d495
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:21:02 +0000

    different full top rich fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
