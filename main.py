db74d1b4f78e9296c02d6f97ac95448b860e4eeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:05:33 +0000

    undo post regular corner habit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def count_elements(lst):
-        return len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
