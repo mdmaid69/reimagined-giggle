31ba27e24bf8fd5dd9c68acc2f736f70b43ebda2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:49 +0000

    scale edge object communicate apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
