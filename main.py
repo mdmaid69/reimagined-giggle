bf0e8c877a0e2963bd1ea4db1e00a5513326f833
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:42:06 +0000

    flight shelter point react movement

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
