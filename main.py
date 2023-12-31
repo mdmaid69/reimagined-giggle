a44258a2f574ae69c7d51efe1508ca703a050de9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:57:04 +0000

    reaction error only belief owe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
