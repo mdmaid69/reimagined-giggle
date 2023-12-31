a0e039c40fa360ab1ac7bb98447d9c829f982eab
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:21:53 +0000

    children claim argue difficult pick

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def count_words(sentence):
+        return len(sentence.split())
