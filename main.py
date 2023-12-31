8f76653506b39ed39475529a4e5dd192bdb38348
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:19:48 +0000

    habit charge connection whoever control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
