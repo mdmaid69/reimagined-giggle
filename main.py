60720c1681d2d28b111eadbeb1c2295eefcce00a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:16:34 +0000

    children skin fast swim takeover

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
