78109d6254dd844603015e53d2cfbfe9db655d80
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:00:42 +0000

    snow odd close eat proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
