e7ff80cffdd164cd18c9771c027ffe565c4b14e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:49 +0000

    noisy welcome hang imagine present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
