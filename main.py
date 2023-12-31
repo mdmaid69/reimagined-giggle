2f990e05a447290b25fb9d4cc7ca01cd2b30dbf7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:19:22 +0000

    precise bored use need no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
