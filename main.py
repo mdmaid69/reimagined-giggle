046ec81e3da57799e4c0aa25e884e3a0242f1773
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:06 +0000

    answer Kk scheme future unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_list_from_array(array):
+        return array.tolist()
