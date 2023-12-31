361bf56d5ac20414fe373e23b19e510fdc27bb80
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:41:24 +0000

    surprising dress Ii Qq far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
