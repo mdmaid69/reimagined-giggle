4bf575e95dcb0fa5b334aca2086740ec3a4647ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:09:40 +0000

    failure move bus community swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
