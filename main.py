ae664e0042e61d4baefb64c14b0f980b0a9cbf3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:54 +0000

    land rest cold blue war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
