49caa91fcf15b3ee9caf6effc5515dc4ec92dc14
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:54:56 +0000

    surprising test container cheque be

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def square_number(x):
-        return x**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
