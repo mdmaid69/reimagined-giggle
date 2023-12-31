e29bd22030b5cb415aab8f8a63dec4032d06b32b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:31 +0000

    certain sticky choose spare when

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
