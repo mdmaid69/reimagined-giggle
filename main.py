ea71b630c7f581d68169cd7e90c07affca1b8bed
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:54:28 +0000

    rescue agreement group dead variety

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
