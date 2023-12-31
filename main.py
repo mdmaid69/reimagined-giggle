bf940eaf087053d11279c6956b5937187303ded0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:42:57 +0000

    this several rest bunch translate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
