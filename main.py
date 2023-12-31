d496318a7d70dc909bce5c2bc04fd30c13f4a72e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:52 +0000

    constantly sing so holiday take

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
