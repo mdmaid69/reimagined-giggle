b329f19d4cda45a6e115fe0b40677dd5d5e3046b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:20 +0000

    equal hardly spite usual test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
