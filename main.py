96aed18ac9bb9e49b94ed50930e4eca8bef58776
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:16 +0000

    wherever trust manage usual something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
