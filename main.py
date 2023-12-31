5b3d19078e5cca3dd5a8ce299b3d7eec32a00347
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:33 +0000

    cheek own listenfor why serve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+for i in range(5):
+        print(i)
