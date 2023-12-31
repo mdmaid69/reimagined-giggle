009a9a3ef8f4875eabfdc652fffa7d781dce2dc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:05 +0000

    blind mood hard write argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
