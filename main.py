5ff5280184e4e4159c398d1f2399a7dfe13954f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:57:54 +0000

    use touch study practise hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
