803e3afc64dd1ba42b7e995a17ddeb28dfdba7b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:59:48 +0000

    back emphasize male file effect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
