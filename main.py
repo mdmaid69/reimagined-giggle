3289b28e995466d7dda2da0ab92a9dc96f23cd6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:29:36 +0000

    side direct pleased belong pipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
