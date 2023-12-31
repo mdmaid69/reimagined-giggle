8a2f9223aada807010fe0c28acfdccc03c7e65ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:52:00 +0000

    friend call full rain 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
