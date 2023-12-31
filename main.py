cf63e7a1a1a49ec31221dec15f6a253e6872b37b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:59:27 +0000

    project shake sentence defeat become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
