631c6dbbba79ca2b9d30ca71634a1d0835de822c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:26 +0000

    neither willing bag field steer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
