79e9ac09b7bf4911f6dcc58844f70e5b3469ea02
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:48:17 +0000

    believe borrow watch concern climb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-print(math.pi)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
