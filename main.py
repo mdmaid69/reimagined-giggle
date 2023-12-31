df256cf6bb410717332d3cd1ede522a19f8d115e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:16 +0000

    base quality project criticism powerful

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
