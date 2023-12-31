34c5f6dd930b5088bfab4652c45e2f09f13bfbea
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:25:39 +0000

    tired blow class themselves hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
