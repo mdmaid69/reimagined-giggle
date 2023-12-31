93661a233ac97acb8db0506cad5d2a68934ef7d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:13 +0000

    bound performance strike great objection

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
