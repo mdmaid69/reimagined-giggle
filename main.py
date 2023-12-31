2421a3298cd35d7b11c9220c8f49437b587325b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:21:20 +0000

    bus match sharp rise same

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
