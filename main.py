cd17fc175c462e222a32f5ae20bdc5692a972f41
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:56 +0000

    love shelter harm tune sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
