f912b425dea2cef81126092b357332beaf8b1ff9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:26 +0000

    wind would basically fire direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
