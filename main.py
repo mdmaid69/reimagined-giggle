936e4a67c618da8129d92824b9b65031335e1f39
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:20:04 +0000

    true mass that excuse improve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def reverse_list(lst):
-        return lst[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
