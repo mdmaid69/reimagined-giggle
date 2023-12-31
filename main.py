093cd60097992d0ea2c7cff1aff8d5d17849b350
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:27 +0000

    tune hot please battery hungry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
