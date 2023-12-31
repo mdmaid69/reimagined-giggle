c1c874580e99f789f22240485c0eb4625196c4fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:37:47 +0000

    uncertain pass squeeze law hurry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
