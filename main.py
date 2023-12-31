57951bdf6342b33c9e1cb0bbe2520c0d2af93c80
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:14:22 +0000

    teach curious spot form summer

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_error_function(x):
+        return math.erf(x)
