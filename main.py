c444e7963637764415e060aefd479fd1fd54a02d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:36 +0000

    profit temperature kind bury brother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import numpy as np
+print(np.array([1, 2, 3]))
