ca65b1a929ad0dd2571ef0a3acd2d54725141abf
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:43:57 +0000

    pass emphasis row pretty certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
