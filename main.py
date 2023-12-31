e73e80b2de8da9a044a747617d435bffb0bcedee
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:02 +0000

    check tax mind generally bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
