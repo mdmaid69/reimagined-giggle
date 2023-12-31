1dbf71b3415cb40df3e53b0cc4197b0f4dff8540
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:25 +0000

    flight thank ground cover active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
