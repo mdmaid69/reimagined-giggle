f0b585a8f4e3887036aa3e3279eb145750f63905
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:54 +0000

    know meanwhile shall organization buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
