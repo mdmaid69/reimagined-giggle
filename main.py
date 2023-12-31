c34d171d418f88c2750b03ff3a213d1770e4df1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:01:20 +0000

    church soon easy exactly least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
