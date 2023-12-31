a905c3a7c9f4e1fa845370a76bc0713e54e71d38
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:13:53 +0000

    opposite over base season nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
