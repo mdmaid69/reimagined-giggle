42883393ab80953a2f1d66363f2077563797b68f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:02:16 +0000

    criticism friend notice hide sign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-print(os.getcwd())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
