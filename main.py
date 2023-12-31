f80a9ef9d77c671a696b38ca658ab8810a25dfe9
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:57:13 +0000

    position exactly expression careful nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
