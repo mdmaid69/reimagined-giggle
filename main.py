a93f57453b0861345907ac9b6230feeb646f8aa1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:31 +0000

    talk aim arrange majority lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
