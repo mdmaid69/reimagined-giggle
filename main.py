a3163fe6457d73cac0f6d75acdf0ec8b6b09cf87
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:23:42 +0000

    turndown near principle person replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
