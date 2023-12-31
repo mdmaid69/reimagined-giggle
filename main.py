b4bcc9e3c42b882ef493baeddbad985f4e3c347c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:29 +0000

    evidence angry office you fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(lst):
-        return min(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
