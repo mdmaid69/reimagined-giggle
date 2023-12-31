2d98b73bc910466d066a608e03ff1295ccdf2dab
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:30 +0000

    period steal board boring equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
