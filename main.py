cd69b7a04c889fe62ab7119828e4cb041c69542b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:00 +0000

    hear answer method cook agreement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
