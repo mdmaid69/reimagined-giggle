3d550dbb3a486b20298d0cfad53d89ef1252ab55
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:29 +0000

    gather danger her scale attempt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
