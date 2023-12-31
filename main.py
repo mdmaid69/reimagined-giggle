d018a34dfb8b7ecbc429188f81a8e102bf1c189c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:45:09 +0000

    service design male hold wash

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_elements(lst):
-        return len(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
