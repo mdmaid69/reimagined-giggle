442e7669ebd4112f7fd067d425d915f8331e4b36
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:08:19 +0000

    speed personal ill switch while

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
