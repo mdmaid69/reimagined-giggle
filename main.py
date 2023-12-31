56b7a8f59db12eb1a1bf16f013d5fbfaa76238ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:41:44 +0000

    shine car hello age through

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
