f4e1894c3b11ad470a673452faea5fbc10d32869
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:58 +0000

    bright will row imagination concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
