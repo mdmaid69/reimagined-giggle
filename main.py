4d254cfac2d166242376b5335fc3f9b414cdd9d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:32:56 +0000

    complicated phone science else total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
