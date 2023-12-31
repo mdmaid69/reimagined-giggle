cee4540c756a63d048ee6c5dc688ec46067f9d4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:50 +0000

    reject club trust Tt swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
