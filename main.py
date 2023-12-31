6cd6b04fb135d1ee0651fe4fbc7cc31a0936dfcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:40 +0000

    judge theirs themselves impression swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
