c9afc68fe3f5aa0efd33f5db6b147227cc770f35
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:23 +0000

    uniform moment professional confusing bringback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
