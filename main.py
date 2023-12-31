2297afd5b095b4e7b5f7f47aedf5c86622cd984b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:08:31 +0000

    lay prevent short unpleasant head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_counter():
-        return collections.Counter()
+import re
+print(re.match("h.*o", "hello world"))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
