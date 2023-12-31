bb4171c423adc82441ae5d16d1175d8ab05125c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:17:38 +0000

    trap whenever pity table turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def reverse_list(lst):
+        return lst[::-1]
