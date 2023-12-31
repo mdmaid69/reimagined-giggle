5bc8ea06bb80313868b39906e217958c97b78bfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:32 +0000

    wind march church sting remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
