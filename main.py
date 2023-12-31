581102cb38150d2c3430d0ee5048819fa21a831a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:34 +0000

    scheme want fashion hospital put

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
