82eed499ad79151a7fbdac6f13e0d8c1ffcf534e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:35:35 +0000

    along bill firm reject clothes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
