2481429bca5406ca26e23c7f8c3ff3d13e2e0086
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:02:46 +0000

    still dry mean mouth awake

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
