378649b7cd9e75fc2c6bcbc55db5ee6b39bd341f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:54 +0000

    speed arm rough cost light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
