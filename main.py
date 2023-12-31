8a59a77801b413604bf38e11d67826837a790eb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:25:30 +0000

    interfere people later previous seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
