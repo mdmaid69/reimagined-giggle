a506275a444b0f230eeaebe91e53ebdc4621d34f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:03:51 +0000

    knock comeout appeal mind past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
