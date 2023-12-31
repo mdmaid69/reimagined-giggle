e73a358716897a021604b788cc80b48141060137
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:25 +0000

    murder sticky bus disturb house

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
