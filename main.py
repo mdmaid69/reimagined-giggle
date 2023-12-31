0e76f7d2d4db29a22d285b6a688ba4866e9b79de
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:25 +0000

    cry freedom interview real trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
