39cf3707e06558edf3802b63909186fa68e19054
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:45:23 +0000

    translate emphasis occur everybody runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+print(sum(range(10)))
