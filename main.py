13138a5740d5d375a0e7ad07954ff9affcb318bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:48 +0000

    somewhere medium achievement admit soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
