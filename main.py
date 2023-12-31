b2af60d6de3dc627ee5a3dc83ebfdb15e6600128
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:50:04 +0000

    recovery needle measure 564encourage responsibility

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+def exit_program():
+        sys.exit()
