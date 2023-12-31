79b5046c05b8d7404800af1f64043fa0570a76c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:30:07 +0000

    damage shape Oo stone second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
