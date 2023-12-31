83a0004a87e7c61c0b8619bebaf6eb17457206ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:49:13 +0000

    exhibition raw bye she pretend

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import platform
-def get_os_info():
-        return platform.uname()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
