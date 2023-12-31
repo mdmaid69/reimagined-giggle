1356d9eb8b03adac08f25c4af0fdf10e54af90e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:21 +0000

    giveback passenger greeting laugh content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
