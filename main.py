dfd2f7881d8c355242ccab4708a611ec56ac7b13
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:56:12 +0000

    professional tax afterwards bottom attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
