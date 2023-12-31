1eff89b64afcc01a0e75c38321f3782215fe5216
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:43:11 +0000

    shape weak hang fond rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+import os
+print(os.getcwd())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
