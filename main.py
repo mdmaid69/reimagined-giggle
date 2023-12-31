7085b5cb6f56387c244e065aaf1049e041088028
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:03 +0000

    hungry underneath convince cope neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import numpy as np
+print(np.array([1, 2, 3]))
