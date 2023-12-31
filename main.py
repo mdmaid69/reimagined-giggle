460973b2f201b7c2e06069efe04449371ec8008d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:24 +0000

    570enjoyable acceptable bang watch Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
