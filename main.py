a653c53cace1d55aa5e373c1353525a2684258f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:50 +0000

    health but well middle seriously

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def convert_to_binary(n):
-        return bin(n)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
