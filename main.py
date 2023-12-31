fecea28411e6b6a390c3acfc156bc0c58a931c32
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:31 +0000

    objection best world along separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def find_difference(list1, list2):
         return set(list1) - set(list2)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_bytes(array):
+        return bytes(array)
