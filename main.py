e6d492eb56620f4cab4b2fdbd0319388795da42e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:27 +0000

    sort connected everything very piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
