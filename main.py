16a69f383c06da6f1135d74b61d252f7a7342d37
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:47 +0000

    prevent independent picture example still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
