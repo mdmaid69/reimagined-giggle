39a51ee3cae3c24e15f367accd204fbba6af297e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:28 +0000

    press slow miss turnaway fork

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
