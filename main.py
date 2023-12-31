f301f229b609b384caa4c515dd1239542d7f9e13
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:59 +0000

    rush interview find throughout lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_buffer_info(array):
+        return array.buffer_info()
