33f17503cbb5547e7890d35995493e18fca7d0fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:04 +0000

    office mine too abroad to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
