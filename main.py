a4d43f1a7e37563cba17bce035c7088f98293dba
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:38 +0000

    shopping design drive his of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+name = "Python"
+print("Hello,", name)
