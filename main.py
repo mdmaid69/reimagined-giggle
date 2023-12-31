f94fac93ccb7a00b158bb94915042d1a0c199f10
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:09 +0000

    imagine trousers stare short ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
