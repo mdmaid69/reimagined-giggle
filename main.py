642697b9bf8e9bfd8c7f5ead34fb9428edc5672e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:34 +0000

    hollow notice little month trip

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cube_number(x):
-        return x**3
-  def convert_to_binary(n):
-        return bin(n)
+def convert_to_hex(n):
+        return hex(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
