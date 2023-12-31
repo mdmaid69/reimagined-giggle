65f653ac95b4ca36944e1e747a929c080e22410b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:05:23 +0000

    error hungry must quantity responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
