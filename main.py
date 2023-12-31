8b52e679f5dc22378a11efa5f618432db7fae860
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:55:07 +0000

    confusing shadow morning skill feel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
