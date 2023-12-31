4c160625b6a404df1c6122670e391ce1b49c3157
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:24:37 +0000

    rarely false drive body mistake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+print("Hello, world!")
