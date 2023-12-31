432423ee9e87b940381e4fcf71c8668f4e1cdf0e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:10 +0000

    belong necessary arrange inside approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def exit_program():
+        sys.exit()
