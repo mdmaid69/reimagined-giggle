f4963b26fdfef791f6cede6237db7e866cfaef28
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:37 +0000

    I real point runinto some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
