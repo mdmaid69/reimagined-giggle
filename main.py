b1ba75231618a180a384f642a0bc8f9d9ae3e984
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:10:10 +0000

    wake ought suggestion ilovedicks meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
