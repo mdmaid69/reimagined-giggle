b7150312a3804a6a07590ae4b37daeb2f673f03d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:41 +0000

    ride disturbing price famous boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_even(n):
+        return n % 2 == 0
