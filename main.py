5d4b4253737df601acc7e8815488a583d5b603ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:09:36 +0000

    responsible sticky apply classroom I

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
