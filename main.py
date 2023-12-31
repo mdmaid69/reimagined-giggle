a822224f108d4b58e2ed0089b453e60c4bb7384c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:14 +0000

    repair in I stop anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
