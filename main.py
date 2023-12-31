f5158cb2c814cf5e92b6234a9afeff358fa6a177
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:12:03 +0000

    quickly born behind inquire song

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
