b52320eb24ac7f13e62e07a074c9d44e227a0d5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:09 +0000

    uncertain punishment pin popular meeting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
