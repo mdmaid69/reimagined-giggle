faa42c797d7801946b313f2b26d4d64e322991b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:39:15 +0000

    switch school none keepaway task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import time
-def get_current_time():
-        return time.time()
+  def convert_to_binary(n):
+        return bin(n)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
