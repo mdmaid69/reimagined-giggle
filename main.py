b42c64eac80d20f3ddaa96d8bcf78ac8e980fea8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:19:59 +0000

    section mouth journey shop anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
