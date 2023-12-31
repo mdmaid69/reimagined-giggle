3f318759410a66328c34a3ca8377c0f5ab08309e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:38:29 +0000

    pick surprised behind tip war

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
