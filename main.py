41ba8cdf0cb4d4f3be7c36216098620726750009
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:42:06 +0000

    continue home avoid correct capital

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
