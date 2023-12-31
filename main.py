06377996b3d0fe99b850af95d318ffeca7884c8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:07 +0000

    equipment aim opposite different opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
