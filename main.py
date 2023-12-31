1626974b5863dd353bda0f29a64ae1b57f8cda81
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:32 +0000

    put frequent goon defeat offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_force(mass, acceleration):
+        return mass * acceleration
