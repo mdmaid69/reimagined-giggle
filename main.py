5669fe3699ffbc83bf5ae7e542c70d7334e82015
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:43 +0000

    doubt entry full goodbye set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
