5728807e369f6061af0766e3843092cf3ae645d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:38:51 +0000

    Kk pay fit holdon family

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
