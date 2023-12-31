39f8466e4870f9ebce1bde52092b138540ae8ed6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:05:05 +0000

    sweet square thinkthrough content face

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
