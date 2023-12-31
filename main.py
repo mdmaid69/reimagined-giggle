59d9b1e9256064d867209f79d271ab6358266323
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:55:08 +0000

    Zz exercise tip involve live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def create_stack():
+        return collections.deque()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
