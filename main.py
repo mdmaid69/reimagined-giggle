76704579559f30cd6a2ab7bd83b546262f9a0bd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:39 +0000

    description system dull on object

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
