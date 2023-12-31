e47bbb382e2fc2deef8afcc348e6adb3ce94e7d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:09:37 +0000

    throughout tongue birth keen rush

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
