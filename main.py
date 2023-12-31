e8160613c3609dffa9eaffab0ca540d2d1bbb10a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:54:44 +0000

    worry imagine rub long process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
