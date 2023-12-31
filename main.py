8d088ac4541a0514c0ddb66951887e2efcd7daeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:18 +0000

    smooth spray chat breakfast until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def set_array_item(array, i, item):
+        array[i] = item
