ea50689942487536360396672e293f631546ad8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:12 +0000

    mend please kind sight uncertain

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
