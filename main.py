d31852620e6ce843d6850ce7a433bb3974f63453
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:26 +0000

    fix platform swallow giveback wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
