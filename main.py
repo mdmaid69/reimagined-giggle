9c05939988647ada712bb33085cc6ef3fcd8405e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:05:58 +0000

    read write sea track keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
