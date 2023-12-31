97c819b75ac395f6dbffcc1599b5cb3d487b9d4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:02 +0000

    flavour or anyone large study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
