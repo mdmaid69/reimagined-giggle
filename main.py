335cc0622f8153d3c57cc7094f95e1734f29d34d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:24 +0000

    workupto with pleasure mean wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
