9dfe8e8723b71676ade4378294833bc7d3236863
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:38:39 +0000

    generally poison wire separate bunch

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
