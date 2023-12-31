76479204aa7e167e735b6923be528b2de465cf53
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:55:38 +0000

    holdup true indeed precise apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
