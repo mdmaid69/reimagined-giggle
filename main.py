c556e5692fb45b05a23743ef29cbf07ce94bced5
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:49:14 +0000

    board mix regard Ii disturb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
