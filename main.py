68c4505d79896d801fedeb1d1fe61e352db90edf
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:21:03 +0000

    judge turn medium advice godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_density(mass, volume):
-        return mass / volume
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_as_int(array):
+        return int(array[0])
