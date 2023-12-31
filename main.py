a3e6a712298d1924b7c269d8c14e87650752b2f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:23:02 +0000

    565end eager either afternoon spare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
