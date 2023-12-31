61d3e128d2192b9eec00659c0b04207ead45fe61
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:06 +0000

    cover while yours 565end still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def subtract_numbers(x, y):
+        return x - y
