a3ca07b85ef90234ab4b51fb511bb1333bfe646e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:10 +0000

    move picture before herself satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
