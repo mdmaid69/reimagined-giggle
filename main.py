5bf163673a81ccbdc48dc61efde5eb94882be830
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:55:43 +0000

    block unit tooth kick exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
