0e86bf4a734fc38e10b259c46c74b5a0ff193ac9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:05:15 +0000

    communicate plate taste discuss listenfor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-for i in range(10): print(i)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
