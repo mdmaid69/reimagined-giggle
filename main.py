61d31f5bacd51c7e581cf08a574354ccccb809e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:50 +0000

    work long future joke reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sys
+print(sys.version)
