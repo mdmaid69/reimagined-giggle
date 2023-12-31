aec0aa2ea4d538a5b6f2ee3a2284a239069a4204
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:16 +0000

    guilty whatever inform usual schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
