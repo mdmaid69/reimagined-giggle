cbf4bfee827e2245e68e8c2b64bfa311a4523c40
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:32:24 +0000

    situation public beat moral before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
