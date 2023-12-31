3a7960bfa306aff9c74a78d441ba4dc7bea111b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:58 +0000

    card traffic Ii minority brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
