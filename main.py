78f5f8b1d21c02ef8b9706f16c5066b6072cbf8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:56 +0000

    sudden plant mysterious doubt comeback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
