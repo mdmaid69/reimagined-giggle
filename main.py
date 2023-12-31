42a6ad4ff28a02132ec28b1f6c0a69fb728712a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:01 +0000

    club great even low week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def calculate_area_rectangle(l, w):
-        return l * w
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
