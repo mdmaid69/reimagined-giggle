f737465a3070a3baa78c24686d2b3a2c8d2e3ed3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:43 +0000

    Oo anybody natural university themselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def is_odd(n):
+        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
