5e5b63639e3e9f4afa94f88db894eb7f4cbb228e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:19 +0000

    attempt future gift doubt feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
