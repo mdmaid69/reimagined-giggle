f1269a00cd728307b5a040bfc373164eebc70d0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:51:49 +0000

    style spring many throughout complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
