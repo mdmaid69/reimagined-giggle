6ffa39335aeb9657b400d36e93af3f583b48c806
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:38 +0000

    car whatever result sometimes fit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
