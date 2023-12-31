47e8eb5adf26babe7d30c815cef27fced0882ef4
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:46 +0000

    Bb type whether seat estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
