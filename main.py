22ae554a4f7b0be1739518f50360193015fcf211
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:32 +0000

    get stand stupid issue connection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def subtract_numbers(x, y):
+        return x - y
