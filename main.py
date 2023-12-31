04363210a8158d30e27c15e1df4caca2ce373a2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:41:28 +0000

    distance comefrom exciting shock test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
