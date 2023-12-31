473d3ad2ec693fad24bb64d494e8d38062e0381e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:26:58 +0000

    record giveout wide tap odd

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
