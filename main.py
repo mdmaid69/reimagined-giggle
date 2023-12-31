b4dabe1f25f8fd35f51cf16eb60caf3564446360
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:03:10 +0000

    club spread anybody immediate obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
