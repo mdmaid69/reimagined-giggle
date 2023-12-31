f96055e9bde790325dca55a3b5ae59437982aecc
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:36:27 +0000

    small rid transport connected peace

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def cube_number(x):
+        return x**3
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
