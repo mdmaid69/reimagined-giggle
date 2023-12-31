6dd50d1e32b8a757939c81578168e44ed8d91a25
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:17:05 +0000

    belong perhaps sound walk act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
