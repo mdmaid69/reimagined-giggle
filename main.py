3a9b08c253c75637f2055ea31ecfcc673324706d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:24:03 +0000

    show mass keep force achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
