fc8850142610c99151c804a6a695010654496116
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:45 +0000

    traffic fun breath mainly fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
