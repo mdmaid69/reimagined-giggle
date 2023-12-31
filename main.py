0f0258ee688cae9bb9883be3529ccf506f92b2d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:45 +0000

    stage print make meanwhile cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def find_min(lst):
+        return min(lst)
