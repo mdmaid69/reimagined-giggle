b04c6554ac86c0de25ad71d5ff253704641ce8e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:20:43 +0000

    turnout sick cruel constantly be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
