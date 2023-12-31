d0012a288a118ece7def69c4f49f0aff1b34043c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:33 +0000

    persuade list workout moral hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
