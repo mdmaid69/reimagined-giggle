85b4e915a10732dff95a765226ba9c6702153b7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:10:37 +0000

    rent go whenever concern set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def greet(name):
+        print(f"Hello, {name}!")
+name = "Python"
+print("Hello,", name)
