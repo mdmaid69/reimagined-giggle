6c9c9baf04bd91cb607d91db0b462619bab09990
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:28 +0000

    matter move whole quit quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
