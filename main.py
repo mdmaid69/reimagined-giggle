73b8b6119db1dd4ab3f133535d9d8d5c19d3899f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:53 +0000

    say kill reply tap advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_list(array):
+        return list(array)
