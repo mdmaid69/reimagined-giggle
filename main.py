c934303dae0430b2dcd719f4bd0e0512bd3dd6de
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:31:00 +0000

    target fall border school weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def clear_array(array):
+        array *= 0
