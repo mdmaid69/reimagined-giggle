9e19b06fa96189b6864833c64e01c172cfc1f179
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:42 +0000

    you wise exhibition Itself 568energy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_work(force, distance):
-        return force * distance
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
