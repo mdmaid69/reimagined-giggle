1c6dbc69c480e69b9c5d7c210970f4b1106d48d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:23 +0000

    total account special display curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
