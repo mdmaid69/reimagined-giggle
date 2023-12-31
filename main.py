cc17e1498ebcbd1dfee1e7e93bfc5806676d153f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:45 +0000

    like slice instrument row 572enter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
