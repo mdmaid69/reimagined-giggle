d28fe2588a17d0dfe2592c23d6ba01252b55d166
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:17 +0000

    bend write kill thinkthrough opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
