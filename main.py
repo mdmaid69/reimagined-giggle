9a3020a00853ab67a2196d5485462807551cba4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:55:02 +0000

    knock bang lunch trouble ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
