a86609e7c77c5f337e7e8352ecc532ace0c6fcce
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:52 +0000

    late real good trust square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
