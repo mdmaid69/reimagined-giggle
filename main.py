ba113e74e9f772c5e865337c5722496c90b03025
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:22:57 +0000

    hold giveback centre damage information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import sys
-print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
