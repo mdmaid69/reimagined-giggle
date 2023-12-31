77715aed447a4bce77cd794d3b53b8041190a9ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:53 +0000

    separate today hair decide consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
