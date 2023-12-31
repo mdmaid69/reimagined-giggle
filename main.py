5f0e9a272247d0272f2adb9fe2466c4e9cf5f99c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:06:31 +0000

    knock loss action hard to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
