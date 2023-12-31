83859370e44b3c9b1dbcbf9ed17ad2b4b10c2d26
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:06 +0000

    legal difficulty welcome reduce degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_float(array):
+        return float(array[0])
