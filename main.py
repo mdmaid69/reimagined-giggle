89e9cbfe465f334772a69beca7a8c375325c547a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:03 +0000

    accept possession furniture unless get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
