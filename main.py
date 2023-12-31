cae9c53a72473e679bcd18d274244fd15a6f71b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:59 +0000

    abroad steer may organize goup

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
