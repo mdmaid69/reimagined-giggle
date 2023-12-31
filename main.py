a1ab5ae1a378b662e6f9aecefbb5668aa75ffceb
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:16:54 +0000

    clear discuss dark wind peaceful

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
