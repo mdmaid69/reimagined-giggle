7f43d755e0f51b8ff676b291ce07dd58f1ae9ab1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:28 +0000

    fact move light least pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
