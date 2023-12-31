e0db2f3d18ff961c2993f584c425d6dfe10449b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:46:15 +0000

    finally private fun push after

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+print(sum(range(10)))
