e854f8a630e55cc41cddbb369fdd87ee229bb3bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:32:10 +0000

    drink giveaway show store by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_counter():
+        return collections.Counter()
