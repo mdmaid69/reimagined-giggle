f9258acdbc038d955b270a2965cf41e842aff578
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:12:31 +0000

    matter wealth comment result confident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
