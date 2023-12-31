283eafe4f5ba77ae3af13e15b8d6c41ab9686eb8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:23:36 +0000

    OKorokay straight needle job point

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
+def subtract_numbers(x, y):
+        return x - y
