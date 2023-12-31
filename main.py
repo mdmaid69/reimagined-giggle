96b1e8eb65fa74adf3d7b6d80697a1e7001c2972
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:49:11 +0000

    anxious after face sister exaggerate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
