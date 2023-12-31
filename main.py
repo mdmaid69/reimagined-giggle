16247f0a37f7164ba3cabc31c87383d9b312c719
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:07 +0000

    gift tax change nigga kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
