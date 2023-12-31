7ec17bfb2f1966c612b9d4af08871fdf8c44c169
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:50 +0000

    hollow once afford wave well

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
