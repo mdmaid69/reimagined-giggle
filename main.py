a6be87fa351145d3f0b0b2bf9ff57c86f6cfc162
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:36:00 +0000

    wild flavour lead satisfactory slow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_force(mass, acceleration):
+        return mass * acceleration
