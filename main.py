6c085d8a56459b7596caeb23f1182a061d6383ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:48:57 +0000

    ability raw still party protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
