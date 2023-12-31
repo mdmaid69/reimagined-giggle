59d0f1b7dda9f00dea997f8128b7e88098e79dd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:01 +0000

    fine lead destroy arm strike

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
