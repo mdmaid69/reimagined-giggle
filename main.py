d75a34023a14372d8ad90bd916601ec436634939
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:28 +0000

    bound splash identify affect section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
