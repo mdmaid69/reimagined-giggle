a83ef73ac00da48880a022b580441fc5a4fcc252
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:41:00 +0000

    build dig study bound somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
