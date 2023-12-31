d4d72d0fbcf3d275b17b73433de028ac021bfc53
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:42 +0000

    original high Qq listen excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
