16d71e5333232a9014015c2e7929dfcd5094db47
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:16 +0000

    look sure noone community OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+print(sum(range(10)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
