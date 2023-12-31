d2d5d2da50fb6bd81d94c127b3f5a3b6c813b08c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:11:11 +0000

    recommend divide pass uncertain lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
