dc499605c93d5f65e1ac039bdbf6936a80429305
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:50:13 +0000

    contribute music congratulations fork past

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
