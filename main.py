703a8456998593dd8d63fd995094d3ecd1909b68
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:36:18 +0000

    work goup range please advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
