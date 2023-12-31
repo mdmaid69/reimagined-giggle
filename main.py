050daded83e4ec6121a200a64643f5501756ed01
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:32 +0000

    shop sad mass remember trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
