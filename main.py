6df39d5e9e442ec2f0fbcae6d18cc09f3c67db38
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:24 +0000

    touch dare experiment chat convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
