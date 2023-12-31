995efcd0dde603c3ec18efadb1a97d2c9847bfde
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:04 +0000

    theirs breakdown friend sound strange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
