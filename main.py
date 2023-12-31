a93c11104298207bea92e66a9ef043d21e860c39
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:06 +0000

    good whom she last takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
