5520daf27d8c5f2c78570dd6cdde9bb64a2581d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:06:17 +0000

    accidental dish advice measure combine

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
