f719d6b0aa52cf08aa523be8eb257cd1e1f353e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:59 +0000

    myself recommend wise talkoutof very

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
