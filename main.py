c997a38bf240e7dff5947346a1c96c567b9cfa72
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:22:47 +0000

    callat lean public plenty mystery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
