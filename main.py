40682086e4e0f25ccce0b9b219d02d15a2b99cda
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:21:35 +0000

    hurt convinced valuable that possible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
