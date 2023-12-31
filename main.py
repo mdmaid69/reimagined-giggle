385a0abcb3ecb5d0782a50b2611071ec99efab7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:45 +0000

    street change colour respect allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
