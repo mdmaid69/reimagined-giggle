4cb2ed0a76dbf48fba06883d781606c460615c32
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:27 +0000

    hair contain sore shock change

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
