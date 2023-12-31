963ae87737ff241ad1257a2a884f167e169f2b4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:04:24 +0000

    course various live wire difficult

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
