6423beb277cc2756d2b339d97a2819191ecca862
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:06:31 +0000

    hour answer major secret upwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
