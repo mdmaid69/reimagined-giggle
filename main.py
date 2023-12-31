47c81481613f8a1c3a82527b48a672ed30313839
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:55 +0000

    choose can transport share niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
