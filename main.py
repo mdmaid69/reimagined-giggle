fae0df7c12e66be4ea28a3195a0ad115e5ce2cf9
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:08 +0000

    deep control occasional keepfrom takeaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
