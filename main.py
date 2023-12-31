fe1bcf2103479b27ad6f6f11a7de30c9e819220b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:03 +0000

    male down bet address further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
