29b608b26a605ae57bf8281d2a6e4407b495f90a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:07:37 +0000

    include leave work defend lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
