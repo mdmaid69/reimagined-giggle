efe5d3bc0c239796846af6ceb0c0a353c06512c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:34 +0000

    Ff provide but plain sister

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
