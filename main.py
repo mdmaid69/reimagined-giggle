5c85631767b3a0542cc18849474e0c1a41ffd32d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:11 +0000

    provide regular maintain could comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
