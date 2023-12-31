5ba4c1bcc438442a5d3f64d9df3f79dcca645fc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:41:18 +0000

    squash splash patient everything try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
