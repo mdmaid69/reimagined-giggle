7674bb8c0c7e2070e3efdf380b2601a366f4cf4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:08:03 +0000

    lay female doubt shopping press

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
