ecc759c67761d05d60f7e5cbaa31b10af726f326
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:24:14 +0000

    treat model ball sweet busy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
