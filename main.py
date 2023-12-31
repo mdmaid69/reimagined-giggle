27e87d71cef5317398f95dd779b4afc242e8b96b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:42:27 +0000

    exist neither fit disgusting form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
