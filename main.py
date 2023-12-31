44e004844ab7fcf1e13d73783a17f81d89cf21d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:29:49 +0000

    bang tight callback mind back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
