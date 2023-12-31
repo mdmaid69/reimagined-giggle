5de0c68526feb89c0a90738e52ad06f23deaef25
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:48:05 +0000

    through skin with bridge goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_speed(distance, time):
+        return distance / time
