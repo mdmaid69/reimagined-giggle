53d9965abdcb1e83a1774920f669ba577e8528f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:25 +0000

    abroad subject mix the term

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
