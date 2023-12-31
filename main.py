919d3eaf5ab6505aa8641654ddb14386367484ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:19 +0000

    strength free business fold remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
