1523ffe39d0d5f1b7feee25046a960dca203f6c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:09 +0000

    event seriously offer runover autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
