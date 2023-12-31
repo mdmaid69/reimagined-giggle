307e949b0fc560831061548a4f4d7184fe942242
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:38:34 +0000

    bang handle generous whole brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_combinations(n, k):
+        return math.comb(n, k)
