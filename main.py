ad0b1c06978045bcc893113cb1c09cde372b085d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:47 +0000

    introduce never pour sky suck

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
