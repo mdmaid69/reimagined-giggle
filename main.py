15eb42f5c090daa7859eb6d88ee1b7fc6c36905f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:55:09 +0000

    buy far our injury centre

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
