00f51b3c48442b656a51602452e7e9961e985762
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:20 +0000

    politics cross excuse walk believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
