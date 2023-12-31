f93b59f2f45f00121195b52a62da4c77bf142452
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:05:45 +0000

    drive expect lookafter shut damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
