6b1caac80a4c97500a4e66b18b82fcf1160ec709
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:55:41 +0000

    touch sometimes tall copy 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_floor(x):
+        return math.floor(x)
