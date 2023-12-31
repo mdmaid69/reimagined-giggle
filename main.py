b818f13203a8f104ed68df2c78bc9fa10acc8c82
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:09 +0000

    warn print last talk roll

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_index(array, item):
+        return array.index(item)
