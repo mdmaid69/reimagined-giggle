45fa17556e1bbf3fd6bc510075dfe290e1f1657b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:43:22 +0000

    stamp swing possibility neither excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+from collections import Counter
+print(Counter("hello world"))
