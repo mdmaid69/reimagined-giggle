8cc1324b9c2bf259999915e049b71e4e3a6a206c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:09 +0000

    loud valuable dream mean pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
