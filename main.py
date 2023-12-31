a78ce17a0ba04944bf124ee9da5c755ec394ea9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:50:03 +0000

    most immediate child snow tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def get_array_as_list(array):
+        return list(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
