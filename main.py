dc0d17a1e5d67527c5ea333fa0cf21a12ee89c80
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:11 +0000

    book duty keepout glad present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import math
 def calculate_euclidean_norm(v):
         return math.hypot(*v)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
