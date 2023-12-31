b465d199b3331c16278730256ae673f1aab13c93
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:02:00 +0000

    protection plenty pencil close challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
