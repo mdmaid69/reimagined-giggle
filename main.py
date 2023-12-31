7a4fc4be3e5e464ca14475dda467a1209cee6a08
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:39 +0000

    combination flight declaration weekend sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
