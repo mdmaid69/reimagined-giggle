7e79585111efef7ef83a47784dcb8bb37d55d54b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:21:49 +0000

    variety independent warning prepare report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print(sum(range(10)))
+import os
+def change_working_directory(path):
+        os.chdir(path)
