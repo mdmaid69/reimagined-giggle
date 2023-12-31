2d7f4e3acaf9e315ff42ca401e5fad9bb4bb486a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:22 +0000

    transparent brave drink secret terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
