b6c8a487279467abb60abfd8aa32dbfc8a132ba9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:27:14 +0000

    why lunch crowd pupil let

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
