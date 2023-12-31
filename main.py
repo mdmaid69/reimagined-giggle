2b258d9b81793e28c44c2debd4ee1bb0ef9dacb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:08:25 +0000

    handle score miss estimate collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
