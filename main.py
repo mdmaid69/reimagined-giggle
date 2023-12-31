b5aed4bb540c80a20ce7455a6f42fb049862c077
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:59 +0000

    better bus contact against band

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
