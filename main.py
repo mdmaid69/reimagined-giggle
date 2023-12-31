e76bc65a7f05260e65affb86579376669d7f3c5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:07 +0000

    loud free practical light pleased

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def add_numbers(a, b):
+        return a + b
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
