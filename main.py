5b312a4664600c7844438c150cd6c352894de9db
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:04:09 +0000

    wish involve bet deliberate various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bytes(array):
+        return bytes(array)
