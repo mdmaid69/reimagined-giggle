9a53a54f6cef2274ca054b19fbb5870f703a3292
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:17:17 +0000

    project underneath into interview surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
