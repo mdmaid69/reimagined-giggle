95fb29d3bcdfe2d4d448435fd8119b0534a93635
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:02 +0000

    simple sink unable mainly further

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
