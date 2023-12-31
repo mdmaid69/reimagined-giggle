30a339c39b9902274b09bef6842702274c5f3f0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:34:20 +0000

    challenge sure skirt breakfast glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+i = 0
+while i < 5:
+        print(i)
+        i += 1
