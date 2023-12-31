48d89ab5c67347ea9f2c284ce6317eac0db35dd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:06 +0000

    concentrate bite minimum communicate design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
