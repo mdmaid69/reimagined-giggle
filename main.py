f234d80100a7b97346a1cb6ff132213972d4fa5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:45:32 +0000

    whom design number dick strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
