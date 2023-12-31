93c1f001a1a76c411b33ba7f71920779648b8c88
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:24 +0000

    permission normal trust world state

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import sys
-def exit_program():
-        sys.exit()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
