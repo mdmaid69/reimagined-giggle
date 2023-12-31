504b66f1fa1703b1d3f95c1ab1f3990fe96b58a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:16 +0000

    flow play occasion environment ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
