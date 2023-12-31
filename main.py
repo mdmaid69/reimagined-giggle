77761455fd0d551bee71322c45551a9dcb707f86
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:21 +0000

    capital whoever spread unlikely she

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
