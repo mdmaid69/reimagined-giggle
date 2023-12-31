c9e32db1b75da349542f250e1b02dbad5a73ec31
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:35:15 +0000

    water OKorokay corner feature fire

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
