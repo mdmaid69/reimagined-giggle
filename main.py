bf40fdbad2ab3d0b59e1431af0e7b649cbbd10c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:23 +0000

    waste bus threaten present broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
