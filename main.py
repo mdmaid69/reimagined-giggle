dca64fadfc0679df1a0684aa520b06092a9e6236
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:49:11 +0000

    trip shade back wave search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
