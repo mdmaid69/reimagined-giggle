8e6902f193480d90f0b720c3dab67a066843e30a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:58:55 +0000

    steer bottom sound cool time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
