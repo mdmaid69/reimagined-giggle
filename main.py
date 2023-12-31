4d3a97597bec33262f44e79899862dfeec70db8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:44:46 +0000

    mystery cum error confused protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
