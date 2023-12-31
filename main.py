b8ef080caf7f04ba27255529a83d1e19583430bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:55:56 +0000

    regard ahead forget until form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
