4c22a7d124aac01e1a2d937cf1bd6e2943304d9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:38 +0000

    fact surprised joke often far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
