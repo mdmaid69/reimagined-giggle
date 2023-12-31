0be5ebabfc479067927f05b6a57b719e92ba62cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:28:55 +0000

    present kind easy frightened instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
