039c0b8980bbf893587607bd056978753b8f52aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:30:06 +0000

    unit roll careless fierce congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
