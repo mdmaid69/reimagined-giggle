1299870521bca61c6b74897da01e43f5d8780f43
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:50 +0000

    university street rather short suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
