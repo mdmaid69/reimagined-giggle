d149cc6caac2cf663ab978a21eed3f1ffbcb99fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:54 +0000

    pause violent share fence 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
