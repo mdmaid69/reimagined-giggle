5f228f0ffd419495c3a585c6fa70c996e9ea2fc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:54 +0000

    weak deliberately guess typical wipe

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
