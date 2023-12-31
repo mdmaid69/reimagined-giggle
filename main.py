cca1977dc3db4cb21627c16a2c8ad9552c232c4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:02:24 +0000

    wind later hear afternoon flavour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_set(array):
+        return set(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
