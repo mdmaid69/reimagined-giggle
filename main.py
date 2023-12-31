a98b83bc76030ed4e8d43b8cc95176ca61750995
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:35:33 +0000

    sight gooff rich already who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_bytes(array):
+        return bytes(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
