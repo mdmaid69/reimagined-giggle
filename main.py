9036d9b1c2eca062d482ebaeb31d16a1f7b3fe6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:57 +0000

    force possession corner vote winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-for i in range(5):
-        print(i)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
