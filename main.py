7c2ab8a230453cb5f88dde6c9cfe40aa35523227
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:11 +0000

    world joke guess female stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
