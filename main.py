3aeb614ccef7f6e86fb721999f1e6e21dff79d06
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:45:37 +0000

    church cheat under speed freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
