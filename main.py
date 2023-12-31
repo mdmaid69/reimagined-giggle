44afb7ead7a41358f681b356f576014b5de7b148
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:55 +0000

    wide callup welcome behaviour suppose

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
