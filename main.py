b4b477a7c4e77d3d4d5017836821d0b99369037d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:51:25 +0000

    distance change escape whole anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_octal(n):
+        return oct(n)
