adb0147be11282b11ae132a780043ea9ba862f54
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:47:02 +0000

    circumstance hand generous consider wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
