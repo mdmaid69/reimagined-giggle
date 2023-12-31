e89819c99d5025b11b1b1a815c4eae73b72ef4e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:04 +0000

    workout mainly head ability low

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
