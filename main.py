64600f21631fa6145a4b7817d7ed188719e28fed
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:17:25 +0000

    plain brush practise form change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
