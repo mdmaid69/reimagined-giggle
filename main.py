ae48699498f14c559a3b93d96c6e8f81b3086bfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:46:20 +0000

    national belong herself agree thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
