e304ca9e1b4160346dff3aff4d21dba82acda0b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:24:10 +0000

    year unkind extra achieve earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
