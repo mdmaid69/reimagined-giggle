bec5d1afca0b4180f0c440172c1c8b398762b868
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:44 +0000

    underneath seat breakinto distance thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_min(lst):
+        return min(lst)
