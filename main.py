4dd49d8da682a32529ea82465b9ec43cb8b7c15d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:08:57 +0000

    loan vote stop frequent nice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-print(random.randint(0, 100))
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
