b8ea36368feaf96b479289fca1312e19a88390ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:40 +0000

    watch business suck powerful relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def append_to_array(array, item):
+        array.append(item)
