b5b544c80b49a25be58ca8e9813506bb3befdbe0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:52 +0000

    Ss behind proper argue offence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
