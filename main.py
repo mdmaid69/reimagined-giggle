0bbddd22d050c4bf8954ec5b2a6e2ee248f20507
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:02 +0000

    worst breakfast us getup hers

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
