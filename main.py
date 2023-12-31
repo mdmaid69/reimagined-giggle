9b40d939feb5026b0560cdb77bb21d5ed08c404b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:11 +0000

    tell stick getoff person far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
