e0893ac7a4b74d34de42114846b4a1eeeed9cff2
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:58:21 +0000

    contrast maybe instant communication pocket

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
