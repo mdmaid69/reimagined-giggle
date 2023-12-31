b1d34c16ca74112f68518172ce54ec1cc97f4c6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:17 +0000

    closed shoot what from upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
