ef7ec926a9051e1c6a24ae0d969db0311b7233db
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:51:52 +0000

    occasional underneath Kk free steal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
