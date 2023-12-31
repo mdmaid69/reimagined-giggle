0acfc2aca70fc7ec6aff039979a1629a03bf8563
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:21:33 +0000

    able part voice check effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
