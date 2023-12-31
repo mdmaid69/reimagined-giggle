00bfe268a53a3689bbf7541cb5205a100fe51493
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:11 +0000

    level weekend difficulty guess instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_complex(array):
+        return complex(array[0])
