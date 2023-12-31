9b056a04453e756de6228116a1c9dfe4b873ab03
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:17:38 +0000

    secret them ready except Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
