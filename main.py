5026b2d8d74e15b79ffa82290a06910b0e2814d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:22:42 +0000

    impression contrast belong break fire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
