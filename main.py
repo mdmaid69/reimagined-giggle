829c809bbb5214d00768271cf1ae005a686881f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:48 +0000

    burn whenever sometimes everybody cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
