c0f3125b44b1b0dcc91a8965f94634663a18dbec
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:01:28 +0000

    warn secret carry correct spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(a, b):
-        return a + b
+  def get_current_working_directory():
+        return os.getcwd()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
