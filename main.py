92e5ea92bea2ba011383e160b7b6777bc8af46c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:50:58 +0000

    wake goon offend shopping weak

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
