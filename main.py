3cc2cf5b7d8f895032499c8677459e1dbec3e4fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:10:32 +0000

    control concerning someone favourite present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
