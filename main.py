2583b9735b9d7da50b9ec1fa149914ee878d2e45
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:37:31 +0000

    dangerous beginning city shopping used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
