782203a41a22c54a3f34fc26840a0d679c959748
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:33 +0000

    chance deep speech successful scheme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
