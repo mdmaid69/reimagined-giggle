5affa0588b1151bfc197caa37b246d8c0fa30e12
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:21:57 +0000

    extra taste lower niggaacademy exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import getpass
+def get_username():
+        return getpass.getuser()
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_variance(arr):
+        return np.var(arr)
