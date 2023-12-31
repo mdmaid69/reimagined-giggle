60e6af985b4cd2f1aa9a7f450a7fa61fa2edd236
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:39:59 +0000

    dry goback(to) disagree giveback various

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_str(array):
-        return str(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def sort_list(lst):
+        return sorted(lst)
