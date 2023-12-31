19143597ed3ad8f5692f82386e33e5cfa7b54182
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:54 +0000

    trade pool she keep wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
