3180d7250db38b90418408c1d29a588d936580cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:22 +0000

    demonstrate with rough male pity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
