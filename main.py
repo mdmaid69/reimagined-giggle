a8660224ef76fc643119f9b4da86c861a2e8de5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:43 +0000

    keepaway everything turnout contrast sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import platform
-def get_python_version():
-        return platform.python_version()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def remove_duplicates(lst):
+        return list(set(lst))
