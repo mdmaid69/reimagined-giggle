d3a25f7eb866b7039023578f65f51814d23e445d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:37:05 +0000

    hit dry pleasure lead slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
