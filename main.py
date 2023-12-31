e4083548af44eddeba649f258eea072d21d04143
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:02:30 +0000

    centimetre Hh boring rain weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
