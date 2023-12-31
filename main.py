9fc511ac703529196f96e6ac1a2a812543f79154
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:27:37 +0000

    coffee characteristic effect present awake

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
