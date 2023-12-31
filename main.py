09a5be4e8480a21467cd9d251bc11f633aa76d6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:51 +0000

    pure talkback attention cinema bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
