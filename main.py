dc698590875d29e5721506754bac18215accb031
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:36 +0000

    prevent giveaway neither suck at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
