5145e20ccb408d4ec31d55213974f82521c1b652
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:26:15 +0000

    or goup tongue wipe quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
