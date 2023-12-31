0f1102219c3ee9a91137764e1180963c4d08dbd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:13 +0000

    find float eat official breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
