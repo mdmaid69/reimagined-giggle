37a2a2dbfef35c2239d60304d4577ff3b6920948
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:29:44 +0000

    concerned use foot correction bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
+import sys
+def exit_program():
+        sys.exit()
