e934091ebc19c325d0b09ace68a48b9a5510fad6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:02:21 +0000

    trick nervous importance limit life

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def is_even(n):
-        return n % 2 == 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
