d5cee0ce0642bbac7d6c4bdb05854c68d52b250f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:55:15 +0000

    ask later equal danger block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
