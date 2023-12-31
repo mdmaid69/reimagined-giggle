1269237b20ef23f30d39da9e17c7a7958749009c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:26:01 +0000

    dream react stiff delicate 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
