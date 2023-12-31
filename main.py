3d19db005013df16c42891cc84b321dcdcb06e75
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:52:39 +0000

    clean suit correct crazy floor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import re
+print(re.match("h.*o", "hello world"))
+def find_max(lst):
+        return max(lst)
