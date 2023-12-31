9e8f0cb4d3fdbb46c773725a1728801afa9ab9e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:36:31 +0000

    steep scare the fashion free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
