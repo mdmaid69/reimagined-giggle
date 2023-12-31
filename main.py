d76b6a53a58aa3aebbeeea1b710ff53a5f6a5eba
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:41 +0000

    plenty open comfortable which petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
