37a9e0c93f12e638515139bff4530d536824530b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:40 +0000

    broken in callout public takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
