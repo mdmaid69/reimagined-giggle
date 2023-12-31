a9d8ec562c63daa2ead730b22a78ed3a35e8497e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:02:43 +0000

    national adjective still why rough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
