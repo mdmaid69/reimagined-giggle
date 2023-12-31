35b8796efaccd68ed293f6265648269e2f4158e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:59:18 +0000

    under fast giveback allow getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
