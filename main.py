c78d1f86ccdbbdd00e914965b34fea6d8a2cc70b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:57 +0000

    land correct waste 179bite certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
