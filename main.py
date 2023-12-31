1180f6caa22d09fc968d5cfae9c71d95edae2fce
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:46 +0000

    everything succeed along escape even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
