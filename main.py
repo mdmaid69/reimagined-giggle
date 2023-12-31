c3f1d55dd17fa78ff32fb718181ae5ad72bb22c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:03 +0000

    on borrow realize stage pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
