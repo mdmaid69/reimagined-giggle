e92a474a5afca58e076cf4c466fe98bb5b44616b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:43 +0000

    person decision arrival thankyou bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
