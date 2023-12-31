a205bc2a7c500eb07d6768ebd1d6f41a83088d27
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:51 +0000

    round crisis fat give air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
