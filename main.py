54a60994333c27140c41d48b56da7ce90facd313
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:23:58 +0000

    terrible creature cook healthy present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
