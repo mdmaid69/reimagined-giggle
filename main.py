f3a4548dadab36633241008eee22b85ffa18e132
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:22:12 +0000

    sleep popular road honest splash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
