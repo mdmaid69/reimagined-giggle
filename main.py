9213a6bccd8def49b7788c04601c08bdaad47fa3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:37 +0000

    list radio land poor sleep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
