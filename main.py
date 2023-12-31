21ddd73a9f9b9f2266b64a6d171b004ab01042d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:56:10 +0000

    neither final sing getaway open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
