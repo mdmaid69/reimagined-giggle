14d7e8d4e5d314192062e9f1a03e1832f4bcc4ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:54:48 +0000

    content afraid detail pump size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
