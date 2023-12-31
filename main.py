c90eaaa08cf775f3b46b2e891a2cb5ad73e3f52f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:18:19 +0000

    up Qq kind connection knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def create_stack():
-        return collections.deque()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
