314393632cbfe8f339339cd679af6b2b9ccbb1e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:03 +0000

    spot takeaway thing minor form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
