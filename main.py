eaaeccfc8bebf5b9fb8939925dfa32ef9a123082
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:17:19 +0000

    probably disagree unlikely home day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
