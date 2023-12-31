2b38c7613c1f601d5a47a9a9f8321fd6498e379d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:13:35 +0000

    test unpleasant professional ago smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
