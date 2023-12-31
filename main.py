1cff3fa33c72440490125fba5b4a135438eb076c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:24:03 +0000

    port too film spray distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
