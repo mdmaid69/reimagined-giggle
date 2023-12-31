c1eda75ddd4951bf86a2fa8c3cd97c9462fa4fa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:37 +0000

    safety ear whole pretty black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
