9a68e065a67e5ab305dd06dd568a1906beb6ef71
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:19:44 +0000

    majority tell deaf beat afford

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def is_odd(n):
+        return n % 2 != 0
