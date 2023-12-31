87c50285c8e4a2080d9ab33893e1aa5e4d48a442
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:20:09 +0000

    this squash settle achieve process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
