2d0e59ad2dfa93d4ec165fc6c120977394783ba2
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:38 +0000

    brother manage yes promise computer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
