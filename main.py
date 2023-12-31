295c34d0ec494ade08769c4268515b17b9abb6b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:21:17 +0000

    lesson contain forever live special

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
