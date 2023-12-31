a698258d81adc92c14a1416e301df7781968b515
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:48:19 +0000

    spend language 569enjoy serious excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def sort_list(lst):
-        return sorted(lst)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
