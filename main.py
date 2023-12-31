029336d584a88bd7a93e57287c9c44f3aa017f28
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:09:55 +0000

    surprised either last history be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
