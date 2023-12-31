f752c984621576fa0d99f6fa126a2ff1c118e7a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:35 +0000

    lift argument every tap rank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
