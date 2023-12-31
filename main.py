3dd2daf0912c6d960644ff6b4d17e57d3a5e0b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:06 +0000

    seat wise sound burst see

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print("Hello, world!")
