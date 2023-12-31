78e97d42638d7df6261b398668cf76feef9d6353
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:40:16 +0000

    throughout job cause total road

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
