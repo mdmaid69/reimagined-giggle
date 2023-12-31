76622d10b3702ae64ed0a4481329538252deb733
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:32 +0000

    Tt in achievement 179bite further

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
