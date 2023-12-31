b359f8c81fecc4b93b1cec7858f4972a9ed431bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:26:47 +0000

    copy send through avoid fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import time
+def get_current_time():
+        return time.ctime()
+import collections
+def create_user_list():
+        return collections.UserList()
