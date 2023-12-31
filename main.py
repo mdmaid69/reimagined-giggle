8adb71ce877833c7a9edf3b8cd1eb34b03f63d07
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:39:04 +0000

    job back write increase undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
