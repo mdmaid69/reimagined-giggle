8f260b17cb389ab51d6e3b38d53768a7166ae9c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:32 +0000

    play interview on impossible forgive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
