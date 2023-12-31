6b7334abae849a00f08943e83284ac6007b44b89
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:27:29 +0000

    lose Vv thinkthrough consider pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
