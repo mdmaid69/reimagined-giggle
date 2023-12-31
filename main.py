1711d0fcd0ed93adff47ff195074473fc765e537
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:59:57 +0000

    talkoutof shop absolutely thanks breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_list(array):
+        return list(array)
