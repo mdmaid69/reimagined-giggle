2834936205aff12b0d31f67a1bbfe91d7bd48e66
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:17 +0000

    turn silent down bringforward long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
