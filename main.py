3fb0c81809c9cfaa57461d1366e45f6cf12356da
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:36 +0000

    soon protest scale right forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
