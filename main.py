c7bb88c6bf74dd732ba8e1f7c40b83008c2b258b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:08 +0000

    recovery so challenge calm match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
