69adb37cfb391a6c86cf1c8a7c8872ff6c2f5fb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:58 +0000

    flat Kk wash pressure skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
