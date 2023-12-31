4cf9825e078113040c78ab150aa53fff8b0cc826
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:00 +0000

    love visit shoulder turnout sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
