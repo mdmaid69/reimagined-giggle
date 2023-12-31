e17eb300778ce7ed5752be947283077c47686143
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:17:04 +0000

    me judge big side capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
