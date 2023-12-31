9c9261df80e47550a5418b08166e87859a5d86e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:54:37 +0000

    fight safe blame general solve

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
