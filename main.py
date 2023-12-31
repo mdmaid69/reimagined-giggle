790da7ac18952927972479c53faeb13e35282bbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:46:51 +0000

    Cc runinto value car basic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_to_octal(n):
+        return oct(n)
+  def reverse_list(lst):
+        return lst[::-1]
