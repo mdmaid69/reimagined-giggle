08026ec6b9c3c01ea3310e799b11e3c2293b4e36
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:01 +0000

    terrible there very welcome gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
