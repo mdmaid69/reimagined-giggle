4a790ce38007c2731cf21c0c8a8fa7573fe73a44
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:39:18 +0000

    complaint board accidentally float pool

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
