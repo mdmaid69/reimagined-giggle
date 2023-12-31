a89e6a5d8986b7cd83bb336a87b39612e5e586ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:32 +0000

    tool negative at eat book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
