a8196a8276c2ed6d2985ddeecf486f17ff872f3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:31:39 +0000

    Tt right on mind smooth

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
