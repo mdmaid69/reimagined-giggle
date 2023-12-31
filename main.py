d7d0e6e80578e081b4d5e7fcb49d9d58c94ca20e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:18:01 +0000

    reserve press trap physical order

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_length(array):
+        return len(array)
