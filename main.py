22d62472066350db23f058e6d50e5b9e2dbb8798
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:36 +0000

    professional average please Mm meet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
