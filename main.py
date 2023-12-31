75100054b9147b4bbd4261c83685d34eb0989ec5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:37 +0000

    what apply substance school use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
