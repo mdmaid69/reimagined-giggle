99f795c81c2b72e9a8057dd674019efb2debbd22
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:59 +0000

    571enough safe friend money tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
