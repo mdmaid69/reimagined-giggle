6cc3aa8dd0ebe4bdbb55075f30cb52a775185521
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:38 +0000

    flavour uncertain whom authority hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def square_number(x):
-        return x**2
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
