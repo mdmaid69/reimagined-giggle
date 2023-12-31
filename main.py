c0ead2dbb88c480fe4e17ed7a846a17a33e79e61
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:06:44 +0000

    workon criticize equal quality belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
