ba6c412dba2f8a32440e478f53c255676e0e7363
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:36:42 +0000

    address hurt container blue neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
