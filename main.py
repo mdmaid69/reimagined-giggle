b016cebbec2b5e2ccbaff4f4223bdb865700a563
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:45 +0000

    speech alone such service steal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_memoryview(array):
+        return memoryview(array)
