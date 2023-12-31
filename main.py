034255c0339ed348b0a319db0d99deb169df2555
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:17:24 +0000

    convincing funny calmly form actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
