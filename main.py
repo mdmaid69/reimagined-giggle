559d8de5039cd916abba90ee5f4d2a88a44670bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:29 +0000

    sorry relax imagination guide shock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
