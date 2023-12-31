719221fac3e2bcb72a56d103a5b4e6fbd00fa74b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:47 +0000

    along refuse attach little balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
