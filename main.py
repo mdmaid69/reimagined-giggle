6b589e275c82b992beaa39d1c85be9b4a5fcdb14
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:52:21 +0000

    part public while careful will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
