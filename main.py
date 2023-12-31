f181192b3819f69de42110fffc998c9e62fecbde
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:52 +0000

    food shop fear control yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
