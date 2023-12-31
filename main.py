f2dd7d3d434c07b72cce287a3817b5b6c4f7c48d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:26:41 +0000

    over build bye goout exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
