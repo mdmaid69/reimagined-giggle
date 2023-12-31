29ccfc4b5fc10478eda7e88cc6e1c9a147d9576e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:50 +0000

    advise Oo present cheat type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_length(array):
+        return len(array)
+import time
+print(time.time())
