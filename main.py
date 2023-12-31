3aa82772d5cf957f2dd2ebd676a4a8cb5e669fd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:36:19 +0000

    coffee flow wash tidy side

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import numpy as np
+print(np.array([1, 2, 3]))
