2c679f6c2df2adfef26628b246b42b1ee6f68e53
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:02 +0000

    miss address estimate guard foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
