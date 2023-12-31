ea4281421979384964c28300a70c556c43393279
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:38 +0000

    patient window walk today sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def sort_list(lst):
+        return sorted(lst)
