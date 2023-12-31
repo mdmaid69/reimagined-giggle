993c1675d4f1a2b8dd6a3525bb7ca35bb2f21cc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:17 +0000

    regard bad education capital belong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def cube_number(x):
+        return x**3
