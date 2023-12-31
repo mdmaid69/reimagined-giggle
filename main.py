189e53dac2867d8e673038f6f278bde27e083b90
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:38 +0000

    account strange reason ocean shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
