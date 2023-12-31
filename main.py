f682e4d8754c375c33ecc854673c96e23aabbbdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:21 +0000

    hungry corner powerful reaction accept

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
