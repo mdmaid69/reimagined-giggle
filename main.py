2cc8fe1c927de05f4fb7745c0fb9dfc876642683
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:39 +0000

    lucky to true reply but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def count_elements(lst):
-        return len(lst)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
