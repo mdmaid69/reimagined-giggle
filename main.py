1b97e4bdc45ab39cd091cb8fe2854dc4087fd260
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:14:06 +0000

    dangerous matter brief squash suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
