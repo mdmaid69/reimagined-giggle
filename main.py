2ec06c0a4896b32b0cb732aa7463529ea8f34f2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:45:13 +0000

    later satisfied plan difference later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def square_number(x):
-        return x**2
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
