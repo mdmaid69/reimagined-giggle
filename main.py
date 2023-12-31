3d1a6636d9167023c10c4f1f5264088eee6af400
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:07 +0000

    life dish present wake direction

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
