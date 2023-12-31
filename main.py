4808bfaa8bb8d3d0299b5b10cb5cc26788c4d36c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:29:45 +0000

    Rr hear weak female exploration

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def find_min(lst):
+        return min(lst)
