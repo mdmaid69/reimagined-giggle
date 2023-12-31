ddd6bd02d1830cbaa61069d3f5660566d0027e2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:17 +0000

    murder Uu finish receive while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
