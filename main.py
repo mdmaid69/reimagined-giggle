1801ebce658dc3bb1c4c43c4cd2411c1262cb2a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:18 +0000

    chin Ss nowhere warm crime

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
