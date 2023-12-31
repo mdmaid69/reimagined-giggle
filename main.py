a99417997909e571f12b4377aa00a60c744443d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:55 +0000

    shopping hard tour pain amount

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def get_bytes_from_array(array):
+        return array.tobytes()
