717544e8083b1ea19ae51ccb8dfb340a78a5cc1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:46 +0000

    paper climb sister final thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
