a2c71314c1444bc2f1279458d6f079f3674a470d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:38 +0000

    breakinto holdon roof mean immediately

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
