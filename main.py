92dbbedde98a475ca9803e92c2fc275fd24cdf36
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:18 +0000

    reply apply compete holdup suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
