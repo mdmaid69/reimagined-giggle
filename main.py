21c5f11c2f4323d4934b518dcf34cbbfeb84de84
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:27:18 +0000

    emphasize forever its set choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
