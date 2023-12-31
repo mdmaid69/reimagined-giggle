57bff769b1e0f7f7b4de17d41f23bbdc007d2012
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:07:45 +0000

    nice tonight report last class

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import numpy as np
+print(np.array([1, 2, 3]))
