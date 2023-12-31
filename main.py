aa54b54347a7af7729005d88280491d7b982801c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:57:12 +0000

    community plant balance travel show

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
