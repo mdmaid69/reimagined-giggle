ac7eb8aee019fd2708db8104cb0d2b8ac7f13f6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:35 +0000

    know funny nature view Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
