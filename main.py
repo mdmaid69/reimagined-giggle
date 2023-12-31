b1438eb8c7a1ae7e5d5303296cdaf675784afe7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:48:36 +0000

    plenty pool careful bang ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
