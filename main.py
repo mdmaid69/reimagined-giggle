296fde7c0b4e62b0126d4c2bf5a4ee8afdf8701e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:48:00 +0000

    many sure afraid afternoon colour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
