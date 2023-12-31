b40017b8b451e54c8d0432ec4fb5ebfae2d7487c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:59:51 +0000

    job marry fold enthusiastic train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
