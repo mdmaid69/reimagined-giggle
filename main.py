f6ff84d09a9f6216b529ac491ca6d81288703a41
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:30:03 +0000

    getback bright pleasant cause soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
