53a5291c77c3fe198ec724a05277322fb5bfa619
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:20:28 +0000

    country tell thing empty realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
