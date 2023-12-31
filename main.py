e3be9a7436af4d5886b4baa4c64e1ddb27c4da84
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:12 +0000

    cheek cruel swallow create lookafter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import datetime
+print(datetime.datetime.now())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
