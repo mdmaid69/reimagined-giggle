3bc7cd8273089cdf34abfa292b626781cba84556
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:45 +0000

    few I interesting meanwhile say

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+text = "Hello, world!"
+print("Words:", len(text.split()))
+text = "Hello, world!"
+print("Words:", len(text.split()))
