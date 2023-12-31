e4b5f5613519f06053ef3e106f53de5703590aa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:02:17 +0000

    season unable environment splash yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
