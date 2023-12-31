4c65bf28150faf35cbbc7430ed011e3b1b9cd46c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:19:19 +0000

    hold wise able in can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
