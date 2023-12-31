d82620f5e3ef77c4c2aef6a4277450895d55adaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:55:08 +0000

    plenty bush path workupto themselves

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
