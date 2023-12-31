36714fa6f6ccce6b7da2ec3ee7679036f100796d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:23 +0000

    better pass introduction confused about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
