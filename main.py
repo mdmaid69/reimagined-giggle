97641b4f3a39bd3d36f7daf9eda762da913cd899
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:44:47 +0000

    eat fold pretty debt unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
