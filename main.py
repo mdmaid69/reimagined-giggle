339785bec82ba29ed93ef1b7d5923734fb5cf639
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:52 +0000

    takeout human shop ground everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-print(re.match("h.*o", "hello world"))
+  def delete_file(file_name):
+        os.remove(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
