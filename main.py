7db86c00148537bc5496826424ff7fe685163613
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:19 +0000

    shout hurry smell commit nervous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
