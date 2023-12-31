83a9f9f7f28bb6be5f9e7bc1f70a9686fc123676
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:27 +0000

    commit corner good compare scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
