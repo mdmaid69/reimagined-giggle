b3ad6510e17d3dee6e07f20f9d337a5e112864c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:42:04 +0000

    second quiet often Kk summer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def multiply_numbers(x, y):
-        return x * y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def reverse_string(s):
+        return s[::-1]
