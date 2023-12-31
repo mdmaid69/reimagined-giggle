2f8153160cd809530c149affe63cc2246fa1e105
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:48:42 +0000

    secret 567enemy sensible fine terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
