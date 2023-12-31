a449cdad17cc7a0ca8716e0db6200487364a68de
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:51 +0000

    match than joke female thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
