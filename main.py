fd62eddd38ea660e8a3621b9a9ebb343d386eb5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:46:59 +0000

    discover demonstrate ground callout programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
