59418edc227a393293b7292866c849daf7385a45
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:08 +0000

    unit amount create complain old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
