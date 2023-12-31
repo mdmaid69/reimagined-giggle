ef66f6fa5a742feadcf39911634a420c73f4098d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:19 +0000

    line proof breakup speak should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import os
+print(os.getcwd())
