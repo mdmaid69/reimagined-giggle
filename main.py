acf9ea2f4c1fb2573c83d032a45a52ecdfc70b6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:30:20 +0000

    develop lie late compare Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def divide_numbers(x, y):
-        return x / y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
