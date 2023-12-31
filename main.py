92356e0e4d606722038528d9efc2f4189fad3a3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:37:09 +0000

    mass fond production basically workupto

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
