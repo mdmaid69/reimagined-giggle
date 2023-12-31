6608b52ca4d4be7c5e3d59d0d6dc41e85c2be09b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:38:50 +0000

    dull really closely dare suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
