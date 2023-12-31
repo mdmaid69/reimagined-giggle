36a1597cacef6b632aeacf55294a4e5cb927b22b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:49:42 +0000

    generous nice firm culture date

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_error_function(x):
+        return math.erf(x)
