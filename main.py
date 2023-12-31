4aa031b8ebe969a7694eed7bc5d7923a03ebf010
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:01:37 +0000

    mess object recognize attitude fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
