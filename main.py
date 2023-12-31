735a2d0982bd85494cdf61a5cc02687d0c2907e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:43:51 +0000

    change strange holiday ourselves taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
