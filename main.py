4211d6e2241f897ef5be82d4f31d30ca19d0f5e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:08 +0000

    question everyone tool payment stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
