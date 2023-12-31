eb5553b5f196a5ffc27895156b540a42482b9d05
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:13 +0000

    increase appearance there because table

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+print([x**2 for x in range(10)])
