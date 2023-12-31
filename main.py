39fa076ad2a9b9f642f54bba6b20ec6142fed0d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:33:36 +0000

    572enter but dick stop important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
