29caca4cd0b6610a512820bdc947d444dd95fbae
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:32 +0000

    degree who none down admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
