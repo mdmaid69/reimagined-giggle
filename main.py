4e6977dbcf7ded18fb92858a2ba8ff5661dfaaac
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:08 +0000

    sink count ill piece test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
