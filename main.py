fef2a083f20cb9f4444ee519f1ca574b5edf453e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:46:15 +0000

    change place leg wander reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
