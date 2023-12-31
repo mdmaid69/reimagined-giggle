6f25e46848d4f0736077d631496a05c6ff2d53b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:03:36 +0000

    hand flow gift bus simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
