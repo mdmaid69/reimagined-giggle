33db9b2b9f31b61503c501618fd860feee7aecff
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:50:30 +0000

    clean tie tour our side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
