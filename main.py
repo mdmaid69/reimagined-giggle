debda93989991ce0b5f9118539ca4d2b41974c03
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:08:54 +0000

    steep simple bunch considering discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
