d7a0205c0d40d72e454754c534691e4116b45310
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:09 +0000

    second Itself minimum relationship shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
