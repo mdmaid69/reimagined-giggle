d234b4652680ed8cd9042631290c4d86dc6693b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:19:33 +0000

    excited food hang comeover roof

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_repr(array):
+        return repr(array)
