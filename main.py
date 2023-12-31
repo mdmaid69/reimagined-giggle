ae29bca29b4523c4a4c6701e680b4802ceebb34a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:15 +0000

    trap fasten murder workon 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
