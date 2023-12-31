d0c066934dc3b01bb8c46fc4281bd07b34d48d47
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:10:25 +0000

    standard command music my need

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
