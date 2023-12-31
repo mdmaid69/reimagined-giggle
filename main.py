30282f15dd9585e7809f3aab29264d9b7787dcfc
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:18:32 +0000

    match here write immediate few

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_even(n):
+        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
