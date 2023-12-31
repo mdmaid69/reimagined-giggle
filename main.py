78b1914317c8ae61f1173024d0ac0d14ab7abfce
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:50 +0000

    wealth sea knowledge laugh supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
