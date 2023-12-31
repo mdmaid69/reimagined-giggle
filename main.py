0d577677ddbea6fa48cedd5a211431e434345a42
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:26:31 +0000

    behaviour how deep combine young

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
