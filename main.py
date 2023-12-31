0d9e14cecdebf8753a3483ed31271677cce4db5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:11 +0000

    Gg might well runover begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
