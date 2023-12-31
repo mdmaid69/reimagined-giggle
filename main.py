30384074021b161b62cf9c85bc182b33afb64611
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:48:46 +0000

    sound whistle crime for relief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
