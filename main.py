5b6f01099bcc30b2d5adc9b826b32258d5106dc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:32:04 +0000

    shadow band part back whistle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
