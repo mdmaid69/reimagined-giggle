83c4670bfcc2ed3356a4f5836e0f73465c8f2a43
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:47:13 +0000

    result kind passage satisfactory spill

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_tangent(x):
+        return math.tan(x)
