a2961147dc3f4e9c29127f9714cbb449fb386640
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:32:35 +0000

    translate gap tie use shout

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
