a54f351f08f5c34536f362404fa3801c5d19336d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:01:33 +0000

    image pure kind left complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
