2d0bd73f71adf59193575169b5209a2fca6c3229
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:12:46 +0000

    certain fire abroad warning fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
