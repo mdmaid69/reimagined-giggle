717832c8aded1d53c61c0d0e7fe624e3267e1ac7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:38:22 +0000

    best put apart murder goaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
