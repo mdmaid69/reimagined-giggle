13f29088676a0d605dc3ef44c8d667af59eb7cde
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:44 +0000

    story safety takeaway woman giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
