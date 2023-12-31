64ffd4ae7c394b674a462b30cab2821cb0006142
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:16 +0000

    earth real occur century top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def reverse_string(s):
+        return s[::-1]
