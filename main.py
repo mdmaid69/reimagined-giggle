25e80b74c51060b39164747f94ef8e440171c2a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:03:49 +0000

    warn grade as yourself shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
