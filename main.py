eddb7f116ac244db20ee76ca7bfc85d35f0336cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:15:06 +0000

    stick brother throughout mail funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
