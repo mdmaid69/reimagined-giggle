c4664a4ec96f20ef96bca63ae65c1903c109c293
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:46 +0000

    question burst surprising transparent level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
