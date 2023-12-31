e50812dc4ccdd5aee588f395c85486b71c17356b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:22 +0000

    ago phone passage interview mother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
