aa75655a43d52896f7bd49a704951aaa10692896
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:06:12 +0000

    swallow proper normal emphasize authority

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def subtract_numbers(x, y):
+        return x - y
