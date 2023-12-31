7675091374eca8d7c9453a96991f07f7f33b2ced
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:00:25 +0000

    play gentle everywhere crowd so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
