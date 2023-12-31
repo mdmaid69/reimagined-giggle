ec2018bd163006661c4e272e421488594321927d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:07:46 +0000

    alive language imagination sight matter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def count_elements(lst):
+        return len(lst)
