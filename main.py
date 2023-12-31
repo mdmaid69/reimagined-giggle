0c418b21fafaef630134d9ddb1831d31e4fb9fa8
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:35:25 +0000

    track total see anywhere light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
