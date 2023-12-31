f93e698e8726d7bab2309fcf358982bbaa031bee
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:37 +0000

    centimetre part old its train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
