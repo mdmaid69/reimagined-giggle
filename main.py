a9b5ef6e17da4c28769ddb152170f6960f88b1c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:21:44 +0000

    decision otherwise rest like stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
