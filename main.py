41a03f0de3656fa174afba19c7d09b05b2ead014
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:15:51 +0000

    father offend scream declaration long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import sys
+def exit_program():
+        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
