2692348a2865cbc15a983d1794481ba62b49a4b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:08 +0000

    concern unlikely book however ourselves

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def reverse_array(array):
-        array.reverse()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
