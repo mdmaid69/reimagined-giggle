020f12a8e0868efba7a10332a3786ad163adbaa7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:12 +0000

    tough movement hurry summer scream

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
