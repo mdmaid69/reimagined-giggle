4d81a4e5a26bd337d566d32d2ffd0b931ad95967
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:11 +0000

    risk roof tend fork 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
