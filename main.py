138ccabc9d8e35566172a4682a4d6344a051981e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:58:27 +0000

    turnaway impression part office breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+x = 10
+y = 20
+print("Sum:", x + y)
