08560a12bafc26b528e7db455f4e2836cf4dba35
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:08:19 +0000

    noisy car problem runinto building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_list_from_array(array):
+        return array.tolist()
