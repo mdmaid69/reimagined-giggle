570d0db430d0d38f0266972397b0b723721d24b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:42 +0000

    trade excellent machine spot regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
