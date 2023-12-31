e33278c5d196bb49cc4ab23b8aa08f427e809f12
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:52 +0000

    long fashion key by save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
