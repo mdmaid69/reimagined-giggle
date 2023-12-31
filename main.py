0a8656b2ef1867c2fae194c21b20bc85487dc38c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:54:11 +0000

    occasion exact yesterday face offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
