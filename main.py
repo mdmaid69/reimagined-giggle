4892c4ea4d3069094c8dfa96bc6121a619d921fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:46:25 +0000

    such no she hour mystery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
