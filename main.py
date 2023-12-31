c1f4141b4464d686a5b8db44527c7efac9f4c35d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:42:18 +0000

    where them practise original deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
