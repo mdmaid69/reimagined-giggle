7fa0f8c8fc3accedfd53d5fa33c72415b8bd0fbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:48:28 +0000

    would hot attractive patience concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
