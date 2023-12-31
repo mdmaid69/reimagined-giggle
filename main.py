eb91acfbbf4374de34151015ad0ac8199c026fe3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:27 +0000

    save bad disturb talk dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-text = "Hello, world!"
-print("Characters:", len(text))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
