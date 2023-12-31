f7b6a14fedf7de914596228716b15d0359af5aa5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:08:09 +0000

    only hot rise mainly breakthrough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+def calculate_density(mass, volume):
+        return mass / volume
