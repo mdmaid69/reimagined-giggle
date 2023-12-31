eb3490486868a03c1fb87afb4851ba65625b0425
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:20:03 +0000

    company fault come bet bound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import sys
+def print_python_version():
+        print(sys.version)
