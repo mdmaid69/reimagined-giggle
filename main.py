7fd29d4bf0bb4316dcba964d3c6346384899b994
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:17 +0000

    doctor warm piece front build

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
