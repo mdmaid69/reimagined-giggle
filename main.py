578c01f7593dccbe3ecae54967287c3794730e7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:38:33 +0000

    570enjoyable evidence personal cough wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
