e61a9bfe370dc9c8d450b02e61b1be68602220b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:49 +0000

    thin simple earn float she

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
