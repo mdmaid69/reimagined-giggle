0f1c176cd4c24666e143af259818b5e065679af0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:58:12 +0000

    learn room modern disturb listenfor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
