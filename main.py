95ee04f58edd0c6f1873c5b2f333c3e3e5453887
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:45 +0000

    turn proud dig sensible return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
