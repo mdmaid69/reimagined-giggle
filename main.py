5ae292f5f5b54c8e84609b84dc23e6b5a20f527b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:20 +0000

    speech equal rescue boil rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
