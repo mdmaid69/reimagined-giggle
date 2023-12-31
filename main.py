ac43a690371bd92f06fe7cc8fed91a11421515ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:35 +0000

    record score and help produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
