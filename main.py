f3565f8c9192d3ad0ad4abeec2c529a81192273e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:04:04 +0000

    song she fat produce allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
