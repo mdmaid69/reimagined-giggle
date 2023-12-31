e4d5ae223db8759f0493550b513a8c29959ad130
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:01 +0000

    hardly success quantity thank worry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
