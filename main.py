826fa9fc66d4d11de1a3ae2549efade021554495
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:31 +0000

    piece upstairs subject butter up

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
