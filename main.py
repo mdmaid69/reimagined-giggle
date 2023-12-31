a88a2b6ecf98c4ad7f6bc065d3b0ce31a4fcf1b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:36 +0000

    somewhere avoid measure question himself

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
