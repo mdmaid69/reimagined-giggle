e817dd99c62965b8cf9d65070e7b06b017e132d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:56 +0000

    statement floor add trip afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
