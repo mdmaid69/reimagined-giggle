418dd097398ced8ae5589a1f59f4dc5986d9b7fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:36:45 +0000

    leg their sweep fault preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
