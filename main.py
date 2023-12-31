26d720f5f88c39f805e6cb72a091d2fd6068f1e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:30:27 +0000

    certain connection to computer catch

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-def greet(name):
-        print(f"Hello, {name}!")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
