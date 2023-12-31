3dcc5dc29b0801d9b5de971d9660e780ce5af7ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:29 +0000

    birthday boat ordinary payment through

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 for i in range(10): print(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_string(array):
+        return array.tostring()
