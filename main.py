33eade41525b101b306d0d702f37f1b5964c0972
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:34:39 +0000

    clearly put unable reply make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
