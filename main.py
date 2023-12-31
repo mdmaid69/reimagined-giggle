8d91ab85b1e549a03a1e0c4745e6b8815c23ba51
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:20 +0000

    she public breathe invitation properly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
