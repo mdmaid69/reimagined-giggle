e496b0b41aec2301e1fe267a29d9031ceab98c37
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:02:36 +0000

    section wish sit interview catch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
