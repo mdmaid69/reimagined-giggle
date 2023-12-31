ce580db2924284206d6cda44f494479853e31edd
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:50:44 +0000

    owe patient confident pile murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
