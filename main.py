b6e9fedfafbe4d25ff5a7a04d050cc6a410d504f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:10 +0000

    general attract advice knock prove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
