38214290aace58305e28f7f85b1d8f2debbf67d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:48:05 +0000

    stretch there chance best back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
