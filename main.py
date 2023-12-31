a4b86920d00a5f794a44d98857441729fb9f20e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:51:02 +0000

    company lookup takeover slowly roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
