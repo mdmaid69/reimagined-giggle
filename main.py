d413ca0a90fc3b4fcceaaa03e18b3695b51279fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:21 +0000

    knowledge artificial Jj snow fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
