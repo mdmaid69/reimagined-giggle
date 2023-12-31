278407b2be90c90f19623c213aa2bb7f30e552e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:21 +0000

    post improvement list sky scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
