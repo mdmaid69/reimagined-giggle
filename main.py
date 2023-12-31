8816cc2a18a93d70b4d0fa24f17b3fc11a6c47a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:03:46 +0000

    either somehow show along fashion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
