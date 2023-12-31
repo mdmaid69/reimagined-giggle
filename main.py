9115be94292fdb32524ab3e945a9a9ce24efbe9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:05:17 +0000

    mistake hate attempt pot calmly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def greet(name):
+        print(f"Hello, {name}!")
