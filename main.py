c27c582214d9eb778f2904664fc3382d18c416dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:58 +0000

    land relax satisfied concentration why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
