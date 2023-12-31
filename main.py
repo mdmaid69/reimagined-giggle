c15e2b3235d2ffa0d4b82bb84e10f4f2f08c04e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:51 +0000

    many sleep no birth year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
