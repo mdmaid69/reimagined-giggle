c3f551b7e5a3e960cd9a78b31adb19eca02d2b8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:24:53 +0000

    tidy people shopping 566end sometimes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
