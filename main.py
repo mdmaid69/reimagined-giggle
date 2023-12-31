0df36ca7c5b372617a7d49807ebd738b9b6d6d83
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:41 +0000

    precise recent scare plan lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_power(work, time):
+        return work / time
