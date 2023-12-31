e7b1048eeb15a7dcea80db1cbffc3c14c49ccb29
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:32:13 +0000

    marry surprise comment control runinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
