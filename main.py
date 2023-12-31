825f5e50546c7e3c5ffa4476f9b811e82b102a52
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:17 +0000

    ring murder traffic major curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
