bbff1290a9a7bcdb8bc6528ce2be719769d3068d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:36 +0000

    afternoon near regret thought fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
