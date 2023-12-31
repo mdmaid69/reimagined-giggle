f2a1fd71a6a37eba8b2421ccf9dc3872f1efa4c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:33 +0000

    offend shelter yourself space makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
