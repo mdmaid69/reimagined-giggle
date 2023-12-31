8b4e280eb20018e726880a5570fbdeebf43e626a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:25 +0000

    temperature stir suggestion beautiful attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
