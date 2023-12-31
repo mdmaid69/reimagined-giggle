e04d1b34468845112555e7662b57d0e306929f41
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:01:54 +0000

    treatment minority Mm hers agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
