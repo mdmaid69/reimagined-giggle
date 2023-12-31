922ce4842cf5dd277f1bb436eb13f707efabfb0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:01 +0000

    careless fast possibility lower at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
