f266f7b16f4f1d3c796bf0eeb88f93a6a4576047
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:47:34 +0000

    represent song night before be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
