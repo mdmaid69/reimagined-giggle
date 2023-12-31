3f22dce62f39147fc58d646b51f96a4463757289
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:54 +0000

    complicated physical fall face can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
