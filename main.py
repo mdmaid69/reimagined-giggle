1aee445e5122c6cce4d42127c9d86c114980aa06
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:50:02 +0000

    upstairs female trousers factory sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
