f37cd0c955165bb2309c312b002f7064bb952666
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:50 +0000

    improvement know complain absolutely able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_speed(distance, time):
-        return distance / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
