c0a57126d56c505a1bdb50e0ecc00b472f81b396
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:05:53 +0000

    yours care wrap exit Kk

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
