9345aa65983a001ecb0db7f213d3245b9079b4de
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:51:38 +0000

    towards whole church exit platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def count_elements(lst):
+        return len(lst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
