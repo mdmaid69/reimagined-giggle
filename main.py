ed1274c36c85ce6fe2aa8dcd4cbe5d453954bc91
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:14:52 +0000

    exit date visit sense shut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
