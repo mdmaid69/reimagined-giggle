2940d258632094f4dfa6afadad6c2d6c99aeaba9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:39:41 +0000

    direct temperature wide song complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
