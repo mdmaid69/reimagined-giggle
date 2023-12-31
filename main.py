6746d61bd85c0a087b273e8ff4c216950014b9f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:36:48 +0000

    never exchange time keepfrom reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
