a8fc2fa07bb573b5dddac1d55ba52e1acb8ab19f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:39 +0000

    before correct command relief above

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
