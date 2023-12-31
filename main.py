41099b57b48122fd29b7843bbd75bf442701ad39
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:24:28 +0000

    crime popular favourite pleased return

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_pressure(force, area):
-        return force / area
+for i in range(5):
+        print(i)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
