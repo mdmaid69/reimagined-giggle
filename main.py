d3ada50a8ca61186c3984e245f0eaf1f9e904136
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:32:33 +0000

    complete allow occur serious value

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+for i in range(10): print(i)
