bd8a9a1448af0efcc9e9c68581f0d5ad3a7f2134
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:58:32 +0000

    touch exam and find man

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
