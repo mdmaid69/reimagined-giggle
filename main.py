73b6f41826c50e6a7d238881c646dfd5e6f6d6ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:35:17 +0000

    Oo lucky gas certainly close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
