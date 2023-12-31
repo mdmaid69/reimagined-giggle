3a19d1f278a664a1f2f57d69c29492de3ff1834b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:25:11 +0000

    convince pause ground pity sound

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
