c776cb5bce37f1657e3036073f552292d36d60db
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:18:24 +0000

    offer bye drink confused sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
