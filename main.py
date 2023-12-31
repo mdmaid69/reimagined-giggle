bdb66bdcebb9831839b4bb7a569cf7417e56786e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:35:33 +0000

    570enjoyable waste load takeback account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
