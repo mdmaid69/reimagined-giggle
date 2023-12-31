22038e856397e4afd56cdba75150fa444d049cc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:06:39 +0000

    dig computer bringback string bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
