51652e0a3d7a039e24434e180b761080081a05b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:23:17 +0000

    correct breakin talkoutof born bury

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
