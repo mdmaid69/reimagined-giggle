ee7efa9c9fa005dcb2f213bd8b35ad6f47ba1a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:37:33 +0000

    majority state approve suit complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
