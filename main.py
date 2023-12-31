2f75d5435e9872ad485965dee486b156ae48a770
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:08 +0000

    remain sing someone myself declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
