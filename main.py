1d8769483fac360fd02f060e376f95b4e5c582c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:06:10 +0000

    accordingto nature workupto by politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def greet(name):
+        print(f"Hello, {name}!")
