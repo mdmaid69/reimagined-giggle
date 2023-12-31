769a3f9af902838e867e0ad986577ef3bd67c8be
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:57:04 +0000

    school doctor mess land wrong

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_power(work, time):
-        return work / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_volume(length, width, height):
+        return length * width * height
