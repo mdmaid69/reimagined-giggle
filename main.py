d15422dfe9589f9693f691387baabc9a42fbb526
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:48 +0000

    Zz well formal himself software

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
