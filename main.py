4af4040ce418a8d9b919600c73b13065ffb9bc75
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:48 +0000

    contents smell post appearance meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
