7ba10d5bfdadecebe7d844f7a270caaccd331731
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:32:23 +0000

    home 570enjoyable live force zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
