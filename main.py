a966f4be9f4eabb675a5d7658b794aef6661b2c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:47 +0000

    basis issue city any give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
