e16bfb5c7fc723a47e401f7f55016c3cbe070a8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:06 +0000

    571enough keen occasion bottom 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
