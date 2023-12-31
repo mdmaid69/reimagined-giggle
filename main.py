84fbf2cc5d6f6e52b94d344a77978f4e5eb40f23
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:06 +0000

    effect case sound very until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
