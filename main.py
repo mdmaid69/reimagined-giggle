38494ce59749cf9d9205fe2ba4ed1843a0f886bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:41:55 +0000

    stretch criticize pile memory passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
