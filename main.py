f94609a5a4c31d0540a8af44b25726da5dd64fbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:23:23 +0000

    considering last possibility dull circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
