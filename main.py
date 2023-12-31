d719f2e09a8bd1f2435d340e34f211663b1b13df
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:17 +0000

    rush murder activity exchange gooff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
