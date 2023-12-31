c6cbd9ff631a1cece567e581ba29698152545334
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:14 +0000

    positive lead stage meet breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
