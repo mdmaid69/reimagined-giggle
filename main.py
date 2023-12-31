81b21d9e94219c2396f6f7f9e1785930f9625a11
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:55:41 +0000

    opinion fresh politics turnoff secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
