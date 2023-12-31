f9c285e6c0a5c2403603b1fb1090e91764249251
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:40:20 +0000

    total instruction except variety poison

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import random
+print(random.randint(0, 100))
