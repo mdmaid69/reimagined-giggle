fa6eb1f2f0c79a27df0c1be5f434a8341e3dc84d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:43:52 +0000

    add bright limit form excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def clear_array(array):
+        array *= 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
