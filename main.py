3b214e8895da16e0bd5ad877e9dceef3b52721cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:32 +0000

    expect forget get great everywhere

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def greet(name):
-        print(f"Hello, {name}!")
+def get_array_itemsize(array):
+        return array.itemsize
