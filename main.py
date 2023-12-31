c8217552cdc50da51a50ef228110e8317f3e5c9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:20:10 +0000

    use priority married it print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
