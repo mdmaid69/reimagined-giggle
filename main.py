f673c20ccd94f7cedadc0aeffad5ed5cf19ba0fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:42:00 +0000

    underneath bang tax giveaway tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
