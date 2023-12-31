d5822087daae786c78368b2cb8751d1ab748f3d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:05:02 +0000

    colour mother speech patient telephone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+print(json.dumps({"name": "John", "age": 30}))
