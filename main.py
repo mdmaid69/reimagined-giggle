78d5d75f55731142614cadcb7473d6bbe73ca0dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:22:14 +0000

    rarely OKorokay pot you meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
