380da5d15f550ba1c6ebd6ccf8dce7b360a0f691
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:33 +0000

    custom band capital possibility polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
