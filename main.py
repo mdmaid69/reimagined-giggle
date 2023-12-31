ecbc71dcad732732f228ed53624d3ddcc28d7157
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:18 +0000

    569enjoy famous politics none top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
