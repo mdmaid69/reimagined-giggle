6829203b704228db5b53ed075415892b669e1f2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:02:14 +0000

    tonight make close point ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
