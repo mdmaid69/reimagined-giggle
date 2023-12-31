9f758015f6dcb5c5088bf96b26ccf0cbfb79ecb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:43 +0000

    pronunciation perhaps tense success community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
