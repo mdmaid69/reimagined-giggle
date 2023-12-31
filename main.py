4c1946ab2c6ee73c2f84930270318cd4c32bec57
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:53 +0000

    occur concern match destroy turndown

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import time
+print(time.time())
+def divide_numbers(x, y):
+        return x / y
