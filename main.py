a8b0c6373e61e0de12db68ade4c5847bea685b4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:21 +0000

    public precise basic under strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
