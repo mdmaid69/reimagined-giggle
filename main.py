61ecdd2af73df42468dd958665410afd2d780c03
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:27 +0000

    angry situation movement extra prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
