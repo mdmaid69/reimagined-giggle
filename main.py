0faddba0f605ca0686080f9859f9e93a9d827e64
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:24:59 +0000

    respect short introduce thankyou confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
