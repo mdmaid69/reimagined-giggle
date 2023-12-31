c161d816bff39c2128218d4eccdc6023f9f284fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:19:45 +0000

    judge once black tomorrow similar

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
