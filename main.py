50c09b3fa6333672aca38631003ad0aa5e5511f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:59:14 +0000

    tidy murder explanation none close

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
