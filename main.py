c4d35c9cd9a1e129240c20ceb41207b4ca61401c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:35 +0000

    join exactly road every check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
