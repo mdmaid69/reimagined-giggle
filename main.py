3f185fc2211560147d39626d739c147f954fc478
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:33:04 +0000

    replace instead marry tip become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def reverse_list(lst):
+        return lst[::-1]
