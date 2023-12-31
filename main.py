9a0a2c8f0e5a0f7e22dce0f5d2ad179467081c1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:19:35 +0000

    sport meeting total answer strength

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
