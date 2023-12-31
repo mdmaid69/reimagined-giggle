5a819081da8ba668d03195acd9dccf3f70ce2ab9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:25 +0000

    manner Ll scared shock finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
