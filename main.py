5a3f3b359357558035d096d9086995abf7697291
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:02:27 +0000

    heart offend hers pen criticism

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
