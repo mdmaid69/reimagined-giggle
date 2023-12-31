a93bd7d4ecbc679f7c44e74dbf071ee3da50111a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:56:41 +0000

    Cc know all sight classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
