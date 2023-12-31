505eaa6da3718f18b04bdd383a3199ccf8b6f5b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:16:25 +0000

    frequent lock set doubt turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
