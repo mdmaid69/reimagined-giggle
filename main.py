694787f7295a7ee4722af5da706c8dfaf890c570
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:04 +0000

    especially rarely turninto suck loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-print(math.pi)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
