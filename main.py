110e1b277e3fe97ed94f4ba1722f9bc4ea0203a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:42:46 +0000

    collection single myself platform lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
