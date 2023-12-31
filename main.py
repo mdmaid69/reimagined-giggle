a1d2fc9c48d4785cea8cb5da2dc3180c48f1d250
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:15:37 +0000

    lock development store false search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
