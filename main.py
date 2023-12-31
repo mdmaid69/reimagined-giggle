3855818d10fca01c615e0585c4f1dc0654247b92
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:01 +0000

    difficult intelligent control within worse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
