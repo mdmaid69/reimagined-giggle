905bb8613fd9107f4c70f258c9d3ba865b6265ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:44 +0000

    door laugh Ll his bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
