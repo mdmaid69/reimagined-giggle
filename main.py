d8458942bb95e5ae3c800994a1fad86d36da225f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:44 +0000

    unite joke rain study collect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
