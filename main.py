19510dcd096d2ec99591865592c7b009214af879
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:57:37 +0000

    goaway almost load fetch pull

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
