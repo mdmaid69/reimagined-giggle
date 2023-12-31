da12a410850d0d628970988bce5bbe03624d6b69
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:29:32 +0000

    lunch nose soon lonely related

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
