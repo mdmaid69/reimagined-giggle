b88c3d82824297f257b008b668b58ca129d87511
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:37 +0000

    179bite under moon original murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import json
+def convert_to_json(data):
+        return json.dumps(data)
