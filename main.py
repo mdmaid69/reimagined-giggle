044c96ebefcea0a434502efb8f7240dca8f9e570
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:59:02 +0000

    explanation dance sad Ll calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
