7e615dcfc2f66332071ec40a43978b1b8fc6c9b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:31 +0000

    cost rise week praise convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
