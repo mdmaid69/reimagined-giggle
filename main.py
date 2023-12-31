11d2957b0906358eb1aaa8525c0ad7336be259d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:16:26 +0000

    Dd some government circumstance female

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-print(math.pi)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
