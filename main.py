3e4207f04222b091afbb61926758b4d109cd4388
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:12 +0000

    pencil firm job convinced positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
