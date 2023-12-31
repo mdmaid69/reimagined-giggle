cefb97e70e1d1d483ecf11ad32f1efe7db37b1bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:15 +0000

    natural telephone successful act thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
