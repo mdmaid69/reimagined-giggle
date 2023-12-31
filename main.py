f38003c3cc1870da0a0a1116a2f76a772df0f8e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:09 +0000

    interview dance professional recently awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
