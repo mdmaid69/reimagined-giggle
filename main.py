3ac4f215dbf96b296058648ef9ce34b0a75df9c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:44 +0000

    cook computer among young train

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
