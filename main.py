36832f575b165eafa9c97d313df02336c6e42765
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:33:57 +0000

    runout left strength form flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
