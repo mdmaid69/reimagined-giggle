abd2fcadc3ad7006d4add4436d48ece2b21ac29d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:58:44 +0000

    hard common show sweep rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def find_max(lst):
+        return max(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
