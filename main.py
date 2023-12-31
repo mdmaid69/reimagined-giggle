4c59c6205ef0cc16ab85b7103e48b6881c834ce3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:23:15 +0000

    final demand test worse favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
