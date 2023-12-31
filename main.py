9d259f11808867806b16fbdc51517a675e8015d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:45:27 +0000

    lost race report Ff return

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
