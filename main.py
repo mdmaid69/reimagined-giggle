b949c4d1669c0313b7bba01679765c4a017d65a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:33 +0000

    under near convince yourself waste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
