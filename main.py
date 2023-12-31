c71d60bd9f8dd4b3a4f8042292882edaa5ea23cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:22:37 +0000

    visit male and perform whenever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
