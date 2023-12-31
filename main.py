9ea6a9ec981e45df3488d028063da488019f9519
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:51 +0000

    comefrom valuable dry proud bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
