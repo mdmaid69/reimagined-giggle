9cb080d68c3a52bfc08adb8593ebfe3194791617
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:25 +0000

    dry club control front impatient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
