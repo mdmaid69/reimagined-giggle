71cb5d89837f5aa39fcee4b42a4f2d065ad71781
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:14 +0000

    aware unite giveout whistle common

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import sys
+  def get_python_version():
+        return sys.version
