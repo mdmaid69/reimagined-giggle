73252af0ca2c421a50b10fc54592bc0b6ec205a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:59:05 +0000

    experiment keen Mm holiday confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
