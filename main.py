45eea089394c175c7fb0f14f33f70b2b31faca1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:43:28 +0000

    neither run start card square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
