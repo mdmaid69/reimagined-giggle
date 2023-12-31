5fd9a6cfd71dd69364b14ce29c3cc449fd7ec94a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:20:51 +0000

    deaf behind one 565end straight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def add_numbers(a, b):
+        return a + b
