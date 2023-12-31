9daa47de1e40d5de7e8fbecb222fe932bce23c2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:29 +0000

    forever drink instruction grateful lie

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
