5a000fae3f0d5572472ffcb495b09e7459c11388
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:16 +0000

    thinkthrough as determined match clothes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
