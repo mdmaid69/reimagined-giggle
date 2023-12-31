7c9cac329455f377d55390f4855bf606329f2528
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:17 +0000

    typical autumn stretch club post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
