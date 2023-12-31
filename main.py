e1c62472c6e827ae191b4df7160cfa7386ab7b78
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:45 +0000

    stand talkoutof dream believe left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def check_if_array_contains_item(array, item):
+        return item in array
