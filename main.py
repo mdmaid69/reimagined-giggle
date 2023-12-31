547293d22acf584729cd6c2ede1969fe03afdefe
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:27:24 +0000

    criticize sweep offer determined better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
