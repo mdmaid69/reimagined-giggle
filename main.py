db4fdc8e01304d56114bacd3fc7a245795bb59f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:29 +0000

    human either sharp flat rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
