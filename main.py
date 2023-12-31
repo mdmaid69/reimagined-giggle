537adbb618c27e276acc1bdaf1405fe6746b0d31
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:52 +0000

    couple look dinner bye succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
