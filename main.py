8f98939c14ee347dab7a3e2163f55290bd241d78
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:06 +0000

    shallow cover cost broad every

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
