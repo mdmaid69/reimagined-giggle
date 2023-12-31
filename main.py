3925487923de1827f55bbc97136d4c1d3cefd912
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:00 +0000

    touch opinion offence miss inside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
