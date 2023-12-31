ba54c990502334a19b0a304f2834a9ba7889f1c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:39 +0000

    release rush rescue material change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
