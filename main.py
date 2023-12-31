597c7352935f151492e8ab346e427435ff5d5a9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:49 +0000

    Qq turninto runinto hide present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
