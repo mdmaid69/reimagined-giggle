fed7b7c12cb5c931e7520e908acd176a0c17099c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:50:05 +0000

    born sight bear influence steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
