c2367945e0e39c53ecc8cab8baa7d2fb40b79b9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:06 +0000

    message which soft workupto wash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-for i in range(5):
-        print(i)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
