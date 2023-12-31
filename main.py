a552b06940d57e4694f947d59e76c830490a0502
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:24 +0000

    advanced talk fault their sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
