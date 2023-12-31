490d3833a9578077e789cf39b8a559d653396f8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:55:45 +0000

    advice stone thing tight ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
