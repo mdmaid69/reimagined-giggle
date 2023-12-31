2b84d08ac3aa30b3fe8e111d70b9f38f75567eaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:22:56 +0000

    female powerful rest science deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
