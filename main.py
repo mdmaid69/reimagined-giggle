f8d4c0e1c31a17075ed8ff6aa3c75cffd6020ed7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:55:20 +0000

    charge breakinto somewhere alive base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
