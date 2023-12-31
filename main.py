cb1713c420e8204cd15c2478f70af6ea66d7c510
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:21 +0000

    beautiful further ill fast hit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
