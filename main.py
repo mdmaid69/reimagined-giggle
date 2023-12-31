f3901f2db197d1e4f767629fd04c3acea63ec437
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:35 +0000

    protest juice community scale boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
 def get_array_as_int(array):
         return int(array[0])
+import sys
+print(sys.version)
