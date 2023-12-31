b578c1c2b74cb8a9ff96c11acc621a38c276fae8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:05:29 +0000

    capital cause challenge breakin scared

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+print([x**2 for x in range(10)])
+import array
+def get_array_as_list(array):
+        return list(array)
