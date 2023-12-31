4d3fea224faa90216f9d6d02ef8fbc87b759b9cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:14:23 +0000

    judge estimate statement transparent weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
