e7397b01800bbaa39970e541d1333317af749336
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:45 +0000

    lay become tap work evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
