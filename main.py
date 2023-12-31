a812dac5dc7ff214b09d9e965c3f92ef82bb7ac5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:11:10 +0000

    challenge wall post expression life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
