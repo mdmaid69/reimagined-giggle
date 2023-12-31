6d53d5f92a6b7846c0b42c6ba3165f40570971e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:37 +0000

    scene zero boat name gooff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_current_directory():
+        return os.getcwd()
