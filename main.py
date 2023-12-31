922bcba4c6915bc401b976ec8b4a6c90c291fc83
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:40 +0000

    limit train tone repeat coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sys
+def exit_program():
+        sys.exit()
