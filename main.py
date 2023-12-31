e8bb2ff3f3079fc9fe22412086516e9791adadf6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:18:28 +0000

    partner unkind satisfactory sentence crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def get_array_as_tuple(array):
+        return tuple(array)
