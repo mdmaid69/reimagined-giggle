376228c38685fd7ea4a8f6059dd8f4633aab5856
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:40 +0000

    hurt rough sleep secret preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
