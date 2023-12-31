b2fc3c532193730ff2fd8899121200e586d9d19a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:15 +0000

    trust minority evidence drop something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+print(random.randint(0, 100))
