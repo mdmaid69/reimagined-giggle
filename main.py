aa3ac277d4981a281c2ff3227132a08d683211c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:34:25 +0000

    slight nor receive accept tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
