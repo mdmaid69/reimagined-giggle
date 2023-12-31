45884e4e20abe75312e7437aabfeacd2266598ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:31:38 +0000

    send defeat fire different anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
