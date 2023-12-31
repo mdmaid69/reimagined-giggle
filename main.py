5e1f0db28e55987e4f456329f55dac35e4b2d2b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:19 +0000

    crash command fun Itself guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
