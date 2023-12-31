a50e68f9926de248b7ee5437744b078f19a9fdbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:25:19 +0000

    maintain fit Bb bad bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
