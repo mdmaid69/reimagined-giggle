bc33721a88786da2ca6ecac96c25468aa4f3afee
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:31:58 +0000

    shine to be shake control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def cube_number(x):
-        return x**3
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
