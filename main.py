990e6f66d4d3956f5e667e04a5fb1616e7792761
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:49:45 +0000

    imagination Tt we suit order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def get_current_time():
+        return time.ctime()
