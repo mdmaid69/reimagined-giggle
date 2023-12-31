2cae95b43d8ae6bbf1732f80bde1182729e12806
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:34 +0000

    behind satisfied breathe behaviour somehow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
