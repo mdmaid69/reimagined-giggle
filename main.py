9da277c22fbf10c8c3bff0b46e0bd1fba69f1a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:45:36 +0000

    fond than aware poison form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
