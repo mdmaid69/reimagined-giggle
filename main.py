174ad4559b5d9e09437a47779dc109496a953bb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:54 +0000

    as comfortable possible negative guard

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
