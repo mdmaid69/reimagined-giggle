a5cb7146ff1cb2b306220b40ef7c2243026f9876
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:14 +0000

    tear connection walk height best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print(sum(range(10)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
