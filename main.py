6381e702025197777c27f88c409ab693fec15afb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:59:13 +0000

    568energy instead welcome air height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_ceiling(x):
+        return math.ceil(x)
