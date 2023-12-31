c2b4b1cf04d093609477c2f2b07c6ada02db4629
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:41:20 +0000

    live compete guess game there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
