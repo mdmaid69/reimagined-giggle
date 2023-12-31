da354f22e53ca11b7dab1886175bd51b26711b9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:03 +0000

    bread wonder shock steer let

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
