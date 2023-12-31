4550835a0c44c2b9d104c5aa3db8b0f492c84084
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:14 +0000

    comein uponon arrangement smell smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
