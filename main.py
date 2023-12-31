481a253662f3b7e7887050adb6eac7f1329ca9a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:22 +0000

    frequent will try after tread

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
