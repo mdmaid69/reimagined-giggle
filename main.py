8dbf92e2f852f574cb5977fcc6b1f439ea2bb96b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:33:22 +0000

    clearly into brush regular extraordinary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
