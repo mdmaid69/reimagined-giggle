36ee06a57d97b578dc6a552cec82cb3473155b7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:49:39 +0000

    wash frighten downstairs watch keepoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
