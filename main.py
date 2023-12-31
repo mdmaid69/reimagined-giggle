454d6fe99ef4aa5e0c97d6edf05fb5d1da3a3537
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:18 +0000

    act strict attach ball system

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
