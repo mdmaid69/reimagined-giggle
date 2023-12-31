aa26bed2bd0f02cabb641c86e70505f4e9fd56e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:30:46 +0000

    fly level call exist straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def is_palindrome(s):
-        return s == s[::-1]
+def append_to_array(array, item):
+        array.append(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
