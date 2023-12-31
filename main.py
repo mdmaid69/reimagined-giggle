57e27faa57f0d49ce580867f14ff2970ce075321
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:09:00 +0000

    trial lie move party rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def reverse_array(array):
-        array.reverse()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
