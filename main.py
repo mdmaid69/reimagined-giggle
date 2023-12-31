3a40ad8279c70dae68e8556769e339377533b4c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:06 +0000

    available full achievement team own

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import tensorflow as tf
-print(tf.__version__)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
