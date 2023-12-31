4682297ff50b1f1d84eeee90a29388f5ae7b7f74
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:10:28 +0000

    herself pronounce together press or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
