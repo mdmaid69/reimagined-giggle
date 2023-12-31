e66c76f154cf145cd9db1789903cc59cf505b390
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:55 +0000

    score generous handle sing start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_queue():
-        return collections.deque()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
