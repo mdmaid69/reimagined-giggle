7ccf01db79e7f62bfd6486fc6caade4a61462ce8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:05 +0000

    back system moral hard shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
