dd7904c3bd2cf07bdb7b28330da8d8a52a86d87a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:35 +0000

    where run deserve habit afterwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
