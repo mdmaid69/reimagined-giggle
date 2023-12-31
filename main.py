5b7ab795de5eaba246d33d1f6d526b0c9ed82529
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:50 +0000

    send noone description polite insist

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
