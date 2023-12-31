de687a8cceebcbb53c91288c3ae94986e98dd126
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:34:31 +0000

    move comeoff ability sense take

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(10): print(i)
