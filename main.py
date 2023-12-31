429040a8ad4c3de32b776b1e12e534a2d7017454
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:30:47 +0000

    firm patient relax gentle underneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
