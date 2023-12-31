336a86b977cb847efb9227bae6f5ecc441bdbaf9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:56:46 +0000

    remove throw Cc open face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
