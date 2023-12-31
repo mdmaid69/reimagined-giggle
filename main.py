89676af4b1868cfbc5dd6f631b77a7eaf75cbb34
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:10:57 +0000

    speech tradition shelter bother estimate

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_max(lst):
-        return max(lst)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
