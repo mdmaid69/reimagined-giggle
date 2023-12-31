6f1f4d6262da8b1adb0f10bcbd4d5950703fd776
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:14 +0000

    street crime cut community explain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
