27287d8bff42121089d1b06d32ca316aad7f7699
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:48 +0000

    sweet there somehow corner tell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_work(force, distance):
+        return force * distance
