f4d2e2f18c6b05664dc96209756bb4c6ff9b9c47
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:19 +0000

    influence earn name include community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
