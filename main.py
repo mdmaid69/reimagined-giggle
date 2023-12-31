642042abf038357b349889643475031949394a23
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:37:29 +0000

    convince line cinema whatever bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def count_words(sentence):
+        return len(sentence.split())
