b89399655b0c72e300e8ebfc2e9e44d37347ec9a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:30:15 +0000

    nervous fall smart knowledge short

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
