13fa1b42bb2417a5446a69ccd938b8f7fd24e339
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:24 +0000

    address identify result workon 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
