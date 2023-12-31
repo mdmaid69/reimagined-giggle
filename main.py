f6f7ae36136ad04faf35a765cebae822fafeca66
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:32 +0000

    deserve over runout crash allow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
