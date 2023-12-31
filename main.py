8fc7072c630f585c5c02db02cfb899abdc493818
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:28 +0000

    yesterday sister think throughout pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_array_as_set(array):
+        return set(array)
