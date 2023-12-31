c0a8a319445e1bf5fac8e2b1123e6cca8f751617
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:37:36 +0000

    post ourselves rich harm immediately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(lst):
+        return max(lst)
