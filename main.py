516a111b907fac7610b5b272b21d26ad67e8fc6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:28 +0000

    message unable man face shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
