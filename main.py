b566be57d809d7f8ad528dfcaa207e7898d1c905
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:13:22 +0000

    brief bound brush trap storm

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
