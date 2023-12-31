88127b14bfa24f030e3f3f04eec524188557bf98
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:34:00 +0000

    sharp wash create schedule complain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
