e98836bcb5b5d2c8e9301fc3428e7a04b78b893e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:42:23 +0000

    breath shout wide clean open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
