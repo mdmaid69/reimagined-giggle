8a3b8b4189f4c764207f4bf431a9ceeb3b8c5d7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:08 +0000

    lift crash centre opposite takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_priority_queue():
+        return collections.deque()
