7df12a32d26589cdf8ab5805f0c7620af5fb109c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:36 +0000

    down enthusiastic past survive date

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
