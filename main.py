f3cc9153e3c74b4a62daadd2d9387ae511034e8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:03 +0000

    rich outside kill rather balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_str(array):
+        return str(array)
