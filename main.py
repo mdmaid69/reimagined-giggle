ee1783650f048142b0f151d016eac557d8dac729
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:47:48 +0000

    accident guess original famous clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
