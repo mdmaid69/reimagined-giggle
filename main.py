7561a1ff78516cc876e0fc27fd493ccddacc1a17
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:17:26 +0000

    notice while birthday tread complain

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
