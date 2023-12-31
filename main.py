70b0ad13f20d73c13d0436e5d833a11c6911f574
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:56:28 +0000

    ashamed doubt without pleasure fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
