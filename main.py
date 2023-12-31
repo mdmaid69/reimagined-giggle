516c1f0add8a25fabefef7dc67c6ae375bf4a201
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:28 +0000

    violent talk sky morning formal

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def convert_to_octal(n):
+        return oct(n)
+  def subtract_numbers(x, y):
+        return x - y
