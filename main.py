04ec9b3f770f392d7d2dd2526962dbae585d6db1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:14:24 +0000

    centimetre congratulations suckme hold memory

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
   import csv
   def read_csv_file(file_name):
         with open(file_name, "r") as file:
           return list(csv.reader(file))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
