6770c9c67193884b51cdda18dbd683408a1b1c12
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:41 +0000

    people father press bread suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def clear_array(array):
+        array *= 0
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
