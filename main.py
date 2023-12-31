3828342cf3d4aa921f5ac2f1676e132552711b7a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:50:09 +0000

    settle home whichever this hate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
