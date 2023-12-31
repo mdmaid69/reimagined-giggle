7b21766ddf331280758625c799ab9acd026aefc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:49:35 +0000

    real continue basically bit until

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_string_from_array(array):
+        return array.tobytes()
