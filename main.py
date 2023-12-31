d89c8fb3f5219965f87b9ad0b67910eac656da75
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:22:50 +0000

    wherever whoever trick list stretch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+print(time.time())
