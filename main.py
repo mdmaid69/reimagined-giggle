58d227ad4c9780f66798497c17027ddf5868b370
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:12 +0000

    him even science come age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
