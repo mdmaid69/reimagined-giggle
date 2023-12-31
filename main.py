7418122ce8bcb2fc0f89acbe39449e71e0685d7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:54:39 +0000

    centimetre unkind mind bag from

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_even(n):
+        return n % 2 == 0
