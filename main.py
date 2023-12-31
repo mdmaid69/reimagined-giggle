85f40e630d620fe493d014ab8b4cca06b1fe69db
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:43:01 +0000

    help design call thought yes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
