4eda6d7b25d9d13493971c2bf673bf2e07dcf42a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:50 +0000

    during worth eat blue we

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
