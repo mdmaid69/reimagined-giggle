61c5ebae1e1e26150eaa06e8b6ab8114ec91d09c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:08 +0000

    listenfor lookafter giveaway weekend lay

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
