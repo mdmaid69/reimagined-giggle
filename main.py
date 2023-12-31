5f5f606b4984ff1bda222ffed31b75fe5601c757
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:52:24 +0000

    worst Pp responsible cook main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
