fe19e5f40821705f8ff96dafcb1d120038c3d28c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:55 +0000

    hold hope principle twist activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
