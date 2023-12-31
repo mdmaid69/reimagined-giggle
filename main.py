a7627288a43db399cead70d3b21e0cd1162a0173
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:31:31 +0000

    hang full geton well dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
