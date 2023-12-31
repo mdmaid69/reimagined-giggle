6f96009d11257fb45b11de010d11e7304b3aa24c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:58:34 +0000

    beyond mass allow nurse smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def is_odd(n):
+        return n % 2 != 0
