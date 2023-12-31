5747141ba96212133f5d4e234af5dae656b68165
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:12:21 +0000

    suck education criticism match last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
