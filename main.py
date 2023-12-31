8c8487d70c0fb7ab7a38d94d447312100a9651bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:04 +0000

    amount accordingto and bringforward defend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
