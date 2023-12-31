e633175046551b5dcf2fa4571cceeb8e4d3cac46
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:37:17 +0000

    roll free issue straight giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
