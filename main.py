0769255523915e8a5e55eba1a77c63992bea2b58
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:49:32 +0000

    history detail direct sudden create

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_work(force, distance):
-        return force * distance
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
