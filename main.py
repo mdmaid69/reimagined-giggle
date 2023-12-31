8cc7c37ba3a570015c6d9be176d5867b6e0bd071
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:16 +0000

    appearance but clothes visit truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
