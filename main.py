fca4c5915e37987e25eb6eba7adf63e0aaaddece
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:27:55 +0000

    will holdon address possible that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
