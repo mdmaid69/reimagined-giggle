201a2b899381d2fcd14ccebbe20c13705f8e4400
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:18 +0000

    tie defeat aim brain figure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_item(array, i):
+        return array[i]
