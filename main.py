98131fbe66cf1271fad4618303473ca59998290c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:46 +0000

    illness greeting communicate pollution dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
