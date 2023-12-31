cbd2d8054fc61d3afb88b491cfddd7d390340c5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:54:07 +0000

    fat boil sort worth about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
