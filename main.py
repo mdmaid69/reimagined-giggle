c3eb7457edfc391957c7dc355a70c2fe4fbd69cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:25 +0000

    total pure second judge organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
