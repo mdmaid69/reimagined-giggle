c56d7792f57a161136c1e95915962d257184b51e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:56 +0000

    bread familiar turnaway meet almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_floor(x):
+        return math.floor(x)
