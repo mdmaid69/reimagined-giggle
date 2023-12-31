3f8787d8b9a2cf25453b96198d472dedb132622d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:16:39 +0000

    rare cough partner appeal alone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
