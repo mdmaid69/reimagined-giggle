2751716b02c4fd50625e0d85a15674baff99fd2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:15 +0000

    press listen discussion trap tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
