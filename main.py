2130435ba9dec5044d3f2a0444866a2ead05bdd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:53 +0000

    so evidence equipment software we

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
