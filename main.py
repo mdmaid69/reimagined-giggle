e7b69535c3af620806542955183efa91b1d2194d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:53:34 +0000

    treatment bringup before argument maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
