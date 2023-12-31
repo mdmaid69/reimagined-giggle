12a090557fc06ff9b9ddf5f28f65b98d9c045361
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:31 +0000

    uniform whole after throw full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
