37dd73af2852d854f969eab41bf4baf583a4f26c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:24:38 +0000

    I common interesting keepup entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
