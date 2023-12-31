a369df91a62258a62653d7edece6f3531e0f1c87
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:43 +0000

    food regular weak weigh limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_username():
+        return getpass.getuser()
