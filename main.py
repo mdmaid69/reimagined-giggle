58debf9bdaacbcf5554809aab7166f3f91943318
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:55:41 +0000

    approval temperature and comeout stage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print("Hello, world!")
