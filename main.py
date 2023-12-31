cbfbb403f0038cdae78e5331d2ebb629319078ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:07 +0000

    persuade mass dark exam please

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def convert_to_hex(n):
-        return hex(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
