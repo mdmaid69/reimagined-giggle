7df35f35a042b95fcf49a30cd3a56a68cefa0118
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:56:46 +0000

    load firm hold callout treat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def find_min(numbers):
+        return min(numbers)
