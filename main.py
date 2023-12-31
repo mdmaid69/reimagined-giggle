ddb90653feeeb21f08a3cfbc51b46596c3865ba5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:58 +0000

    Ii harm inquire hit see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
