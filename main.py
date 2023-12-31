7354c06d07e6c82b28b8162fb0d4004c51664501
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:23 +0000

    although try lie pollution unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
