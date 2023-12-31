2cacb9dfb899c6757c6289a9be30e84b4d05738e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:35 +0000

    satisfactory clearly area famous brother

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
