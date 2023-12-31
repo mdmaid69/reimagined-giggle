c661d9f821fa30d1e70777cb8c369577cd446361
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:05:19 +0000

    sing slow should service recently

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
