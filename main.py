755a642e5880be458b7ad75f7b3deba2804d5c2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:10:27 +0000

    visit tonight mistake send tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
