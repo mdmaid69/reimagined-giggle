7f5c8b099aef68c1dc053c68bb9e080c9310fb87
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:42:45 +0000

    train refer smile flat content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_length(array):
+        return len(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
