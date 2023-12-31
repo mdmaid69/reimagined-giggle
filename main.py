fca76b37aca725829745d72465b707a1d8beb505
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:58 +0000

    computer somehow spray union own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Words:", len(text.split()))
