6f96431f24b07cb5974e76c8505e53c7d509a676
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:29:09 +0000

    comeup tight press talkoutof kind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+print("Hello, world!")
