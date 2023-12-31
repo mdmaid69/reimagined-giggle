bf66df9915ab33e4b0736fe789d5df8818da51c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:58:48 +0000

    team present limit combine long

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
