cccecb13e813adbdfdd9989313364cab9935f778
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:33:20 +0000

    spray room grand ashamed equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
