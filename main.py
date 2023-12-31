de70c76e82baa910432fc21dce9ba59e2c9bc13d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:54:09 +0000

    urgent Cc effort achievement inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
