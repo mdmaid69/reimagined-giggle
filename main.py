1a0db924c55960480b51b3099078dcd58506e756
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:02 +0000

    advanced suggestion accidental owe object

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_pressure(force, area):
-        return force / area
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_list_from_array(array):
+        return array.tolist()
