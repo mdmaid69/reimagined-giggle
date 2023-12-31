29d9594512abf771be73478be2460cd1ceb148ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:47 +0000

    development own beginning compare rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
