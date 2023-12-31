0f5dcad4ad5c31fe292d0e67d94b8e1868a3443a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:09:38 +0000

    future last wash bottom insist

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def find_min(lst):
-        return min(lst)
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
