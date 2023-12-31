dae1220dc2105efa486c5ad965e6977f8f154f33
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:16:29 +0000

    guard brown Ee tone mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
