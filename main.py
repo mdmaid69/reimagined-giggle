a4c1450496d83709f45763759288b74f6501d79c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:45:27 +0000

    space responsibility principle trick story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
