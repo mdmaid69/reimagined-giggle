1bb16c27c87546dea2618259d61c821abb8ee028
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:22:17 +0000

    sweep properly name offer although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
 import itertools
 def flatten(iterable):
         return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
