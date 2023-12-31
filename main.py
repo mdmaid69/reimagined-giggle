1c812dc0b0540ede346bcc0fa4785ce625bb1cfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:34 +0000

    room sometimes accident spoil breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
