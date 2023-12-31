49d68197e4848d14f809b2fc986845d75b3ba4eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:34:12 +0000

    plant himself first closely practise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
