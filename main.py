35ae2845294dfb177bc26891796104b6bfd065e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:29:15 +0000

    keepfrom storm somehow culture reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
