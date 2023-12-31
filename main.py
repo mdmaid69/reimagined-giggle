09e3f37a2f7a14cd67768d102dcb4f77a52f7f79
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:43:20 +0000

    callat recent flow go clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import sys
+print(sys.version)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
