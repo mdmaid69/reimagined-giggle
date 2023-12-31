c25bf0f1f042e43104e2334ec116cf5ff5591138
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:59 +0000

    dance so tour shallow matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
