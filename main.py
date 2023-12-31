183cb4c7a53f08581be32bff49cc040bf26c1cf1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:39:37 +0000

    whistle recovery carry smile production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
