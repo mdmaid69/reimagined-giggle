a439ec2330f7a7f87f44f528f82e44d5bf627815
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:16:46 +0000

    child lucky lock behind giveout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+print(sys.version)
