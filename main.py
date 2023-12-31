6ccbb8830924c382c5ae44106683986b3aed4ed0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:30 +0000

    black waste belong slow listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def divide_numbers(x, y):
+        return x / y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
