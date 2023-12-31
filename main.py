4ff1481614ea81ad7a09efc9df5b90b919cc98d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:12:44 +0000

    contact fear cool cause towards

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
