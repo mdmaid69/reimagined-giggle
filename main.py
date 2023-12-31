9f3338748c339f4b746d9560845eb38b410a6166
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:22 +0000

    getout connection purpose plenty hers

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_array_to_list(array):
+        return array.tolist()
