0b30b1c01c8428decf3af2890426dba4aa7ab750
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:10:32 +0000

    school uponon quarter appeal release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
