fc1638b3daf2059aed02fd578d7eaf2be7e6b17d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:22 +0000

    home book them memory lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
