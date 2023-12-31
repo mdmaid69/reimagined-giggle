ceb12575c04f083a43e8b3a1cc67bdb9449291cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:39 +0000

    claim practise behave matter street

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
