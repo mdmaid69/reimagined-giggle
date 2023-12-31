b1a6c27e912960650688afb7dbf8ab2da841cb24
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:27:15 +0000

    gift mind chase exam force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
