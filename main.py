a8d103c05fdcbd320530f7b6dcf86d6512f10453
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:36:58 +0000

    blue extra appearance fit pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
