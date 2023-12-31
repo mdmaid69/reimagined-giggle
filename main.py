cf85d77fdf2246bae7d726cb0b31b21e083256cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:48:41 +0000

    interview pause transparent shame pollution

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def find_max(lst):
-        return max(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
