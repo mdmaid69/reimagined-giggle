e954744cdbfbda7a77df8b52dce37c8ece773b5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:12 +0000

    settle sale coffee image combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
