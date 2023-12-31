397846dc2a4e599ba86c735a5c6bffedb9608f20
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:41:36 +0000

    cut not suffer learn spend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
