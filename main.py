bd8c152e3ab01e26e8a99f4c1a8539a3b93d218d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:16:17 +0000

    ground tidy goback(to) season month

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
