a0630293fa2f02961bf94251390295043b41b9a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:50:39 +0000

    I watch hand exaggerate distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
