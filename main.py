a6ee88306de944fab26d8dfdebf96e70b655c28c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:56:09 +0000

    confusing warning in part violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
