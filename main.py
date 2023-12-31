e687a05160e0e032a6911985435182876d3d56f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:42 +0000

    near arrange accommodation deserve present

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def subtract_numbers(x, y):
+        return x - y
