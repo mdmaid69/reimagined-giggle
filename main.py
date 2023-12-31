612eb27eeea91d5eeabbf997d94f6eac22eedd16
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:38:51 +0000

    flavour each comeoff better all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
