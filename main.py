fd6ff59b0a7c49a124d2d9c8e0b408503dd7ece8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:23:32 +0000

    stamp takeoff something ready wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
