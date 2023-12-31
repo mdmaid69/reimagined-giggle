203d4b519f15cc8a297d390cc6eecd9ce66be31c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:13 +0000

    open report want estimate control

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
