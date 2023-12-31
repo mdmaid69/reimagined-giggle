9fbab017869ccc73caf09ae446117267b5715ec9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:07:08 +0000

    man roll noisy below because

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
