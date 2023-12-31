5aa8cf2e888bed64f5fd6ea565993027038e6ae1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:47 +0000

    mistake science ahead team left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
