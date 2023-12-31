99c9a77d3b0c3f48c62ced4f85297a6e076a3294
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:38 +0000

    loud bound friendly declare natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
