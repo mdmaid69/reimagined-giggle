c30a3286c37a4bd03e2ff518bc8392ed60ee5744
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:06:26 +0000

    move suit run behind either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_pressure(force, area):
+        return force / area
