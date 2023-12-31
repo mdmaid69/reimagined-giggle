b80da6dfdd134b22474b6d2ae196ad1008ecb608
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:44 +0000

    play swing change even love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
