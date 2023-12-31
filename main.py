5ee1177d30151cfb084f98a2c5069964aa35836f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:12 +0000

    my spite fierce cry takeout

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import sys
+  def get_python_version():
+        return sys.version
