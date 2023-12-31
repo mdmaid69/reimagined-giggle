c90ae96df64d2f3c27be20ef1669df64748120eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:09:30 +0000

    until away main rank pleased

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
