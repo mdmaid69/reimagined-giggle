ccbf698ec3289fb36c8dfd8f1a52e175fd5eba5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:29 +0000

    slight kill trick suit suppose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
