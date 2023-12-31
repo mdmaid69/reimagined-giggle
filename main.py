07b846b210c923ce6c1b93a6c575971c7f2122cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:18:53 +0000

    battery bus surface rush acceptable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
