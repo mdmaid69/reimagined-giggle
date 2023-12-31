74fbec8f31904c72c08b9deb2b9d55a68d7752cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:31 +0000

    neat contain lock dick approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
