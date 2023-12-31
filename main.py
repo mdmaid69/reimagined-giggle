fd11bf9568a9479465764891d3478738cad68a65
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:30 +0000

    alone force dish bright sting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
