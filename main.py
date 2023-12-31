ad539ee28a9c2822bd3962fee9a4d82324ab6dee
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:15:01 +0000

    surface church fashion birthday 568energy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
