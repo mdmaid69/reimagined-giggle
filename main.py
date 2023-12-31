c43a3293871a55bf55506d1029fd7cc1633d8455
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:58:59 +0000

    this gift save definite finger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
