9ddbdf5580ca3aec37f4a7e95efa030172e74e60
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:40 +0000

    downstairs pause yesterday close too

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
