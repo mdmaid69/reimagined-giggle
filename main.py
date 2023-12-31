d72ea144d5c8c47bdb262fa2ba23035490d58afa
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:56:24 +0000

    ground score boat sweet Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
