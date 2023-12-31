88f1e59567123a94f2f3f1e4ee2cd08ef61cf154
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:41 +0000

    callback they wind relative operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
