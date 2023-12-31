f78ed400623d1833efefb7ac534b6f22dad443ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:16 +0000

    other official down soak write

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-print(sum(range(10)))
+name = "Python"
+print("Hello,", name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
