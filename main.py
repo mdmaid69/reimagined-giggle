52e86673cc92e73b183cd748614220d4ce536715
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:50:07 +0000

    style variety tread stick height

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
