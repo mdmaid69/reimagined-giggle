c6e7fe274d323d9932b932f1eee2e00afa22e3a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:39 +0000

    remark area would priority leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_typecode(array):
+        return array.typecode
