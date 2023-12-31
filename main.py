7403d23451ef47a8600403ae0fe438afaf62a782
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:29 +0000

    sell achieve mood delicate share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
