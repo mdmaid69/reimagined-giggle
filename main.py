f724f704438227003c650c7477cf0e4790185399
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:40 +0000

    count spare good pump majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
