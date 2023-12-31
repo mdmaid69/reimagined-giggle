f7c0eccf223f1aee6e7a65f485c21407d1a7f0ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:06:07 +0000

    knowledge instruction instant large later

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
