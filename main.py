fb383ed849ff187d2b817411d89eb700bdee3c2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:39:03 +0000

    tell treat manage live slight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
