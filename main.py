dfc1ecb0b3918b65d5b33c8c6636fd017c3a9e8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:47 +0000

    ago inquire sore patience occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import os
+print(os.getcwd())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
