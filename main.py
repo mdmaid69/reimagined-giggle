fee6d131f26c3726423c2e7e232ee581a3386902
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:05:57 +0000

    hope tax Oo quickly definite

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print("Hello, world!")
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bytes(array):
+        return bytes(array)
