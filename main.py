a2b72816eb0ddc59c01735e3f16df2e34e0e0619
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:38 +0000

    city effect nervous improve performance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
