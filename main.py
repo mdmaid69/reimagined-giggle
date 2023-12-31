2516f8c10ff288f45d8d605313e86ed305b94a36
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:21:30 +0000

    finish official about mine priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
