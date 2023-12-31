80db89a7e63406491ddbd270084948d37a7eca53
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:58 +0000

    belief run material water sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
