ed0618e8d1829fa146559abbb92a0976caa8c289
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:48 +0000

    object adjective war press short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
