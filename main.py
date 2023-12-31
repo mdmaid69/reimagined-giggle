b858b1dd70e825e525f60bf9aa972c8c3ffd5261
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:27:44 +0000

    marry noisy stay badly who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def count_elements(lst):
+        return len(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
