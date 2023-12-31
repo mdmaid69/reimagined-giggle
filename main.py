509b8cb3ec8859c797b1f99af9ac5bb42cb8f7d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:35 +0000

    workupto pile combine crime boat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_odd(n):
+        return n % 2 != 0
