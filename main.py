8a425951aec2a4446e655355a93d41899e74e9dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:45:33 +0000

    waste once measure have defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
+def reverse_list(lst):
+        return lst[::-1]
