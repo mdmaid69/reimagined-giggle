4e1b50a45e76e516107a60eb09b2be43cca698fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:04 +0000

    accommodation put calmly drop discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def split_path(path):
+        return os.path.split(path)
