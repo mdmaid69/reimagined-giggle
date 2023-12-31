bebf5b5a663751c0275fdb16fd5b57a66e8d49a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:21 +0000

    table trousers unhappy first deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
