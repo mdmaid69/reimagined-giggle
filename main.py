de5a7399092b67b5e1733d89151277be9735e53f
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:10 +0000

    or something kind gate state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def sort_list(lst):
+        return sorted(lst)
