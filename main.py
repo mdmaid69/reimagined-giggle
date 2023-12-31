1594255b4770806f92f8c4fac6d9b1c3c41780a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:17 +0000

    evening increase rough kind loan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
