3d78d14056263cb0a5544fdded44e4d87b1f9d2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:10:16 +0000

    Jj part suit pocket blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
