237abe5c251504834f7acabb0e0d7bce1c3f3606
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:05:36 +0000

    form runinto opposite perhaps getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_power(work, time):
+        return work / time
