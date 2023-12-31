857527b0f42d07c56d4bcff3eab209417e817f62
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:00:26 +0000

    how show exact shall fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
