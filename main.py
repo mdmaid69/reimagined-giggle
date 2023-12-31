fe6c2f648dfd0802fba558a2d627c3d8eda29207
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:21 +0000

    base workout explanation several action

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import logging
-def log_message(message):
-        logging.info(message)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
