bf75672a643b0ae6b2afe99d58ab8b83ee4d865d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:17:16 +0000

    off street borrow search introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
