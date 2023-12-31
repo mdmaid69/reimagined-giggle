825b1b2176bafa44ce413064e747e65397e06987
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:04:29 +0000

    present man talkoutof protect country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
