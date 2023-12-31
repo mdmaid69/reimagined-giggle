c6a74d30aa880047be04625ca1272dce9ee47e64
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:00 +0000

    challenge worse measure pattern getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
