a0ac4c6f68d28357096937b49364036623b5bfc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:48 +0000

    cause why your shake soft

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+for i in range(10): print(i)
