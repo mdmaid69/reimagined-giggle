41ec5fb8e3b125ce3ed24b74c1d22dafb21bef0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:24 +0000

    catch comfort public benefit giveout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def square_number(x):
+        return x**2
