ebfd7c9a6ca1f98a0718b4a660ea8faa89cabc27
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:13 +0000

    giveaway vote because rate accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+print(os.getcwd())
