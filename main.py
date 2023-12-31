719b2ecb5098c86a1e01b29f4bf685f6f967d0e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:01:52 +0000

    standard sea position holdout blame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
