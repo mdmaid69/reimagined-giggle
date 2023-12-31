31e9d3a1588b01f615c7327ae930ec85b9f25cff
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:27:52 +0000

    grow quality unlucky like creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
