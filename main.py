a2060172877052ed4900ff3f3daf8f8b465bb66f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:41 +0000

    content expression blind back legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
