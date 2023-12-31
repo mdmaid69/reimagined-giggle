b87882c39f7cc93fb078443677a44a7b79eeb771
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:04 +0000

    weather pollution strict plate Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import numpy as np
+print(np.array([1, 2, 3]))
