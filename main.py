ce59f15789a326af7284325ce7a3e937e5383a66
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:05:44 +0000

    imagine extremely action than change

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
