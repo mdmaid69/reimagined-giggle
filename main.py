3979764118a3a9a8d9281cbf90c49a8cc983bc4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:11 +0000

    anyone lock image show disapprove

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
