3dc78235205fbe75999070ecf0a1a33fdeecd522
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:18:33 +0000

    track otherwise main opinion takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
