ff4c86542fd5db5a57b70f0c6256f0b821a00749
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:51:23 +0000

    giveback lift embarrassing all shake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
