2fe28dd39673feb5ce347f7579825f490e435e18
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:02 +0000

    sticky open calmly sense sting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_unique_words(sentence):
-        return set(sentence.split())
+def is_odd(n):
+        return n % 2 != 0
+def find_max(lst):
+        return max(lst)
