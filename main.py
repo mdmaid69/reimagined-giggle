7f08999f2786e74cbe6c6c8ce0fd13e7eb660855
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:30 +0000

    interest answer hurry square nervous

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def is_palindrome(s):
-        return s == s[::-1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
