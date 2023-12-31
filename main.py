07f7bb5617615fb4173c4b157d01eb4d4cf32615
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:01 +0000

    slow ours healthy empty gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
