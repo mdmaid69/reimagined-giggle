6289621f6e62d5c5fb8aa76bb3eb11afd3aa0f7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:41:05 +0000

    see pause watch schedule reaction

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
