53babd97a1a6b7df5a17334e60a70f3d04036bf1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:40 +0000

    design balance discussion section phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
