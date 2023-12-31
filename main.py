6b7bc11cb39a829da26c827d7fece8eb6527ba9a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:36:26 +0000

    date beginning organization meet gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
