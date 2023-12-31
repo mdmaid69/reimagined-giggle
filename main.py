1dfab21f27b868ece2a94856a80a28b5115130bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:38 +0000

    care towards funny whom unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
