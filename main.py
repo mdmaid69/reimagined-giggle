0623aaf92ef3f49300aaf86a892b337e0318b651
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:38:05 +0000

    home respect harm receive print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
