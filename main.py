821804ad927d906cd587388c12151e69f8764d62
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:16:58 +0000

    myself switch sit guess formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
