e47ecadb6deca5bdf6fa2d525286fe62f50cdee6
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:39 +0000

    danger practise rate direction pain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
