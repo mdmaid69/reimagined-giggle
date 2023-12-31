5892ce5d34556938df38b77eb4041377aa6e6c8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:23:08 +0000

    hit grow butter we trade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def convert_to_binary(n):
+        return bin(n)
