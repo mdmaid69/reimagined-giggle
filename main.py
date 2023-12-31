95842a9b705c19d2546ee986b3460c4ba92336f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:37:48 +0000

    balance country offer concerned snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
