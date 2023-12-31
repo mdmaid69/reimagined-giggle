d8b91f162e6bde3c2fb310cd7de3c9c6f37d1e19
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:00:49 +0000

    pleasure deaf getoff holdon drink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
