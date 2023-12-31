2adf31b92b13d4df7d0a311e7bdcac7789c36c80
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:38:02 +0000

    rid us sign somewhere purpose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
