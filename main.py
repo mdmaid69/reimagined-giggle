add26d5ffbcfc687d33ee240ce0505d93db42b9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:56:10 +0000

    inquire dig should please difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
