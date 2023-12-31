5400012df5309104317428d239682100bdf7ebcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:39 +0000

    beside notice weather turn bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
