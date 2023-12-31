e930b13c024a5642ad4dbe24f6455f137b815311
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:26 +0000

    also system software system band

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_acceleration(speed, time):
+        return speed / time
