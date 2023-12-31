d67706dce84e82b31dcdc62607cf0219096f0127
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:38:45 +0000

    public old impressed everybody pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_list(array):
+        return array.tolist()
