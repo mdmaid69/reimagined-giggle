1d50d712774f9356b7b7dd4f5ccbc5ce24274597
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:45 +0000

    innocent pair break now nowhere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
