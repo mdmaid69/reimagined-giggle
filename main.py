c1a9e7502f49404458b6865ed41b7bf44c8b18f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:38:35 +0000

    under branch spread 572enter OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
