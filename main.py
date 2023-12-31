acb4a5ae79796f3e16adcb6c4147f4db61ef7595
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:02 +0000

    rush paper glass getback smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
