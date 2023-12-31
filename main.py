06bfaabe4285c2d880a276c5738f57bd3fac6891
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:59 +0000

    careless famous whom used afraid

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
