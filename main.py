554a2a5b7cd99f217913f44ee81f988cc6e5c737
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:54 +0000

    fold produce piece into dear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
