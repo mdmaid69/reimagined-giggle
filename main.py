eea9ef8a80bf6ce25ac9144abfd8a601edbbe09d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:28 +0000

    bend visit protest environment deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
