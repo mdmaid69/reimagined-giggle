249f0592e72fdf7349ab69da73591b374525f966
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:50:33 +0000

    wish interesting muscle number keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
