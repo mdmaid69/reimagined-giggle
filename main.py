86dcfbfc05623df99376cbd4bc4d9ba8f1e92546
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:20 +0000

    deliver heat runinto invitation getout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
