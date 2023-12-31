b93c2bce110becff13321f3a06fa862b5246440e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:33 +0000

    deaf home have stick have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
