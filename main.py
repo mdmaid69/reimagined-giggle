13363fb9fc905d84340a8c50011452a8e6deac4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:33:15 +0000

    godown train heart smooth church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
