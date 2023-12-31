b78c4430394e20571de93943d20153a1b89080fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:08:06 +0000

    scared negative boil professional tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
