2e107ea1bfae4908ca9ef2f6bde6a9289bebf0d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:07 +0000

    speak go rain thought waste

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
