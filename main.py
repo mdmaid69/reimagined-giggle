7f73ead2f358eeb1eec03e2cab62066f32f76e25
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:19 +0000

    in memory win score blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
