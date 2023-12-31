ea2ed5c70a543505a439dab40b221d703024d44a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:42 +0000

    play relationship after inform prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def delete_file(file_name):
+        os.remove(file_name)
