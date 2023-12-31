0c7c8f32b0abea3e6ca11ee096b026e1cb6f0cb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:08 +0000

    definite juice woman about past

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
