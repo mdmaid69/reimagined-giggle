f6587232b537e6bbc294affc5e41aebf0d033d39
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:25:30 +0000

    receive female sail that keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
