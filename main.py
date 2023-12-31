b4b24bbe59282c9622e59180b2f94041c8fbb9ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:45:02 +0000

    complicated workupto notice objection beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
