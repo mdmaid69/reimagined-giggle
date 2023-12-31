df21587552836e7a2c352612bb5441f9860b4492
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:19:18 +0000

    blue case clear condition bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_typecode(array):
+        return array.typecode
