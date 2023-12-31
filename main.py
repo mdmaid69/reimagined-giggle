75f3d5b13405d0d8ce094d1ebd94dd6e950f4b41
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:53:12 +0000

    conversation polite petrol ticket occur

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+def find_max(numbers):
+        return max(numbers)
