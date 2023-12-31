364c6c819f7b21a35a6ecc0c096c2862762e90eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:43:50 +0000

    as mouth left tense child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
