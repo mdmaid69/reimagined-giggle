01af8c08c2265fece6930100cd306268818dc4e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:56:54 +0000

    plenty tough birth except waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
