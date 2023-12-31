41f8951e0e941b30fefb593f09d45629ac591d2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:10:08 +0000

    slice none change yes fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
