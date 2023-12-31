40e250486b714dab1a7917a6b093a0ae5f1dd2f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:41 +0000

    order dig doubt body understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
