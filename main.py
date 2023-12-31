727e90100b9c5d0647a9f014359dfb275c49f2f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:58 +0000

    exit variety cough anxious basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
