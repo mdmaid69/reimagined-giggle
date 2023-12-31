2a6c7084792d6b0c889cfe91638d832daa821935
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:09:12 +0000

    start influence blame time back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
