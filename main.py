03c99c791fb070511514e02b7795f7687a9d901f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:57:57 +0000

    but wish special consult backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
