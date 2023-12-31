c53ef5d7c64b021e43a4d5dcc92a4d4d122499d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:44:11 +0000

    note prefer survive crash blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
