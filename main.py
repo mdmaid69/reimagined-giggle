f316052942d34daee7d775a8877e577d4fd0448b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:32:19 +0000

    game interested cheque stay become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
