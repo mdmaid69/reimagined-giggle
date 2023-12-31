beffac8ad503330352c7df461970dbfb12ef3b06
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:24 +0000

    cut flow approve middle cover

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
