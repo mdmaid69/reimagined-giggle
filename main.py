6b22dc467022858d37371c81215253147c54e606
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:27 +0000

    thing steady shop form on

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def reverse_string(s):
+        return s[::-1]
