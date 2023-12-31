f28ed8fc42bd7da57d03754458acc99fc04db170
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:11:44 +0000

    spite up minute true over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
