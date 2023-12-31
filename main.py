b1e575ccac02975ddcaccfbac9f392e26860e4e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:39:15 +0000

    keep almost thinkthrough pinch pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
