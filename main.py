258c7982d45becb138f1658c60d997e7a635c4de
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:44 +0000

    fast 567enemy affect brother fear

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_list(array):
+        return list(array)
