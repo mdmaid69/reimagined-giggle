16652fdaa1b029264728b4d8aa39b19317f6ff18
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:43:24 +0000

    everywhere scream odd tour tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def sort_numbers(numbers):
+        return sorted(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
