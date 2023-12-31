f90dd1084e3c7471010d29556e4ab214e27c4d94
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:24 +0000

    available instead suck let decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
