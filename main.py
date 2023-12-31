2881deb70b77e691fb19e7b551feb378c0d8c456
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:08 +0000

    rare community since divide equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
