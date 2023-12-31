34ee2a410d37b334ee9d211d56a5cc764cfa780d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:50:16 +0000

    thank history platform holdon crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import collections
+def create_counter():
+        return collections.Counter()
