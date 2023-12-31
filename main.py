d793cbf847a804da43e3d86ced982016962fb30f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:25:47 +0000

    gas quantity face direct realize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def is_odd(n):
-        return n % 2 != 0
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
