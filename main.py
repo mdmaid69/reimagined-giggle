2775d28c93dfc84220604de57c78b7aec359192f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:49 +0000

    tear cinema coffee word eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+print(sum(range(10)))
