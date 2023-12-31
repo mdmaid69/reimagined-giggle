82c0c0ea5c49197fb5623a2d5eabcc8cf98d062a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:15 +0000

    command voice instead within trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
