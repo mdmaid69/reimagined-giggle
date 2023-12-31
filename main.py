3e4b278bdf496875c59f243157f380a2d7a5dfe3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:13:27 +0000

    reduction discuss train fear mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
