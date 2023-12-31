fde5bfdc19db0795ffc4a1b88f6f05bf266c4112
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:52 +0000

    read positive getback difficulty pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_density(mass, volume):
+        return mass / volume
