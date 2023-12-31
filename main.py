9815f841dfb02f7a23392a35e21b3aaf3b673b1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:38:15 +0000

    shelter include strange anything operation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_min(lst):
+        return min(lst)
