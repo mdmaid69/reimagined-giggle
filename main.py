ccf01ad201bd50797ce1bccd419e7074ecd33f38
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:25:08 +0000

    keepoff sound inform instruction persuade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
