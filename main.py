7ea75700a638c39a0eff82b336ab5b2ffc4708ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:32:32 +0000

    basically runout now pour speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-print(datetime.datetime.now())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
