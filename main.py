1a70b11ee1eaea00324c371affeb8b9882156f54
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:38 +0000

    team comeup suit correction swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def reverse_string(s):
+        return s[::-1]
