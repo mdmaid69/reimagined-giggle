2e638d0ed0136adee8935236548d402817310077
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:44 +0000

    kind control contents regard heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
