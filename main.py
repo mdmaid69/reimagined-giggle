ef76cf71e5742963d3274de4365a2e6fba66cff9
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:43:58 +0000

    which human passenger gooff myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
