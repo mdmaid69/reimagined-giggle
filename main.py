4020e482ef533a35d2f8e29c28d1a08102eb0ba0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:04:57 +0000

    as speed face joke degree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
