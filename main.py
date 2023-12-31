9934474c260fffdeb743561faca98346d05a6c2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:15 +0000

    makeinto exist attitude fill throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
