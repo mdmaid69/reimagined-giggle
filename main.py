ed06f7ca24e681cb21b1a7e25dcae3a6ab513b25
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:29 +0000

    off full importance calloff any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
