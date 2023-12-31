d46a2b319fee384445a9b71109badaefe12ed0de
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:55:13 +0000

    rain none present family twist

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
