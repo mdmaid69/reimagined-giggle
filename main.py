eaea97b2977560b358598e8f18f94448419dc4d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:40 +0000

    favourite excuse average together sometimes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
