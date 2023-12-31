fedd86c1866c9431106802e27f891393219bfd42
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:04:45 +0000

    also squeeze think release goon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
