e59c730031ff34e73fa440664d488e9a644c3055
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:59:49 +0000

    skin sing side name target

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
