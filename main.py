b9a049bac8a06b0082b0cb122410c056b42bb37e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:24 +0000

    female simple everyone visit vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def square_number(x):
+        return x**2
