486954b488ab5530bee7024ee2ffbf7530e802fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:10:36 +0000

    border thought uncomfortable spot form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
