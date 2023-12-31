81b8399a3605b9419ab1a97ca4745239dd7c147c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:03:53 +0000

    beat familiar drive vanish face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
