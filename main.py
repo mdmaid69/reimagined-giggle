7a043e46def25bde54c0e4d80579c678b4df3eae
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:06 +0000

    shape plenty bus number claim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def subtract_numbers(x, y):
+        return x - y
