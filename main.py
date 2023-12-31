6ab16dd6852cbdb728752821e6766e747087974e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:38:42 +0000

    family to partner thinkthrough lookback(on)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_length(array):
+        return len(array)
