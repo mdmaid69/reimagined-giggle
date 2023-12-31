6a1b07bc7c5799dccbaf923f3fe8eeda548b5b24
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:39 +0000

    charge second female advantage people

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
