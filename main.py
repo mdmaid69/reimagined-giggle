99147edb85d09ed08b995f652b9faedb124b9993
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:02:36 +0000

    comefrom runinto unhappy work total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
