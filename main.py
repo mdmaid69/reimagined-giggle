96ae011504e80eaf80aa738b00b46a8c9be8b020
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:56:41 +0000

    grow surface wash beat represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
