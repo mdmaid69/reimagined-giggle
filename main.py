96766b121c3865be4f6fcc3d81c36ee82afc5269
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:16:02 +0000

    other furniture sleep outside dance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
