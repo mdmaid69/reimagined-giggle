041dc1232a07cfe805c24eb2fa82ecdbcad16db1
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:21:11 +0000

    fill decision regret light country

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
