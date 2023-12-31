b06334b8108236cef1072a29fadbb5c3c3dd8abf
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:08 +0000

    spot plain rest recover wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
