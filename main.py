b4ecc2b939ab91aadb34aa06ff569e29de7d2a4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:57 +0000

    marry sign possibility turnout fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
