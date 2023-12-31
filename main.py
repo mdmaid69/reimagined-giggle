2b2f89cab921ebf7ea96b95382e7c0c42127e307
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:11 +0000

    everything organize rather minor holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
