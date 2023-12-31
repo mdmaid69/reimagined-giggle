a7358d31894775b99de063a32de820dd8b4c3c60
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:45 +0000

    before accident giveup improvement within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def remove_duplicates(lst):
+        return list(set(lst))
