c9a7c1d392829652ef18703694fbac6369439946
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:16:11 +0000

    lonely hold right vision equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def change_working_directory(path):
+        os.chdir(path)
