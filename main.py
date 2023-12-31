e8a46202a79766087a2437b4a3f52adf86766613
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:16:34 +0000

    stir practice please with holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
