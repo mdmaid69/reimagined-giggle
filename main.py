d7bbee444d5e6f9517c326d212cb66d79ca61d0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:10 +0000

    comedown ring minimum by wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
