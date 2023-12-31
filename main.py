3c39efeb10c231b95b51c0a5eb178ef98c2b56e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:27:26 +0000

    friendly fit replace workupto someone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
