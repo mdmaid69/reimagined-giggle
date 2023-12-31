fef243fba1aab11d5fe79bef1d56c51ce125b943
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:51:37 +0000

    himself bus collect minimum exercise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
