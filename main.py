e717de29271135785072a2d8ca873bfbff9cc927
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:00:07 +0000

    pity invite not there favourite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import time
+print(time.time())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
