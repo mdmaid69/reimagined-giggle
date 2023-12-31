547d4397fe48f5f7a6c46d3b9d36a9542abe9c6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:31 +0000

    mistake talk circle car wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
