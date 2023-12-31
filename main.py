d571f23493c1a70aa1d6eed4dca414c60d6e1ce6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:29 +0000

    conclusion maybe answer someone exploration

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_set(array):
+        return set(array)
