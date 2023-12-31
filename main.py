f68caa8b92694a7b5d9a871b25c86fb59ce65360
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:14:05 +0000

    wall straight steer sleep watch

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
