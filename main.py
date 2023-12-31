0e39393fdf9547debc6a34795e66e089f6b267fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:53:12 +0000

    defend goodbye survive size fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
