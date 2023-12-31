5378fd073b2467679050ab8c92adc944c87d292c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:36 +0000

    answer make comfort fact soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
