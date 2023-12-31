66eaef950c3463c925193344f9a12c4bd559a0dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:43:46 +0000

    pass worse mass divide apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
