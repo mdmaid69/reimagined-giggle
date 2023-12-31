62b6761a1339d49187304adc54da65479b16d76f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:04 +0000

    odd remark meet tax town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
