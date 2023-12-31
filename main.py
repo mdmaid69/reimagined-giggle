910a5241ce084e374246d4178e9242ea90904848
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:22:33 +0000

    less congratulations declare speed exactly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def reverse_array(array):
+        array.reverse()
