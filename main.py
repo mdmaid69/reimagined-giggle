ee1b5fddb28ada094df08a8f6db0c654b484a7c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:51 +0000

    before lead already keepout search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
