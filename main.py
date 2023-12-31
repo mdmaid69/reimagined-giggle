2752e2126f51ef9ee583140a7902dd06dadf68bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:42:01 +0000

    somehow shout score activity post

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+def exit_program():
+        sys.exit()
