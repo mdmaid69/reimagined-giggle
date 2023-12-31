2f3a253a9465be59648d9ea5e731361deea86ab0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:59:46 +0000

    refer sense art top separate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
