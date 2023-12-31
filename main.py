ad7043d68aa1a9b70424d40aee03f100d6960775
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:05 +0000

    search scale hers majority important

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
