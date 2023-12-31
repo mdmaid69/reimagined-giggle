0fed482d46ed41c1087f0467c3f9b42b9092bc2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:18 +0000

    image afterwards usual on payment

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
