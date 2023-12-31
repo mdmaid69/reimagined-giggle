44e1a5b7273e9169366fcaaaf26e2691c965e6dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:46 +0000

    design town squash game might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
