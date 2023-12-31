720392c5513f8a804e44bf292b1b2481d85099f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:21 +0000

    impossible vote grow gooff away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
