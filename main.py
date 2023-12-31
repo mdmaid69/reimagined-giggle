6fb7b5da2e5cfd76bdbff5b3f8d427baa3b5f1b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:52:29 +0000

    manner hit famous impatient approve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
