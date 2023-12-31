ec25bea05b24e6700b9f9f977672667422fdc728
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:17 +0000

    will meeting holdout return nerve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
