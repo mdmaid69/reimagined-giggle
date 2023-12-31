aa88a0ee75954419dcbdb0fa485bc9ccba93b075
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:05 +0000

    instead glass plenty workupto ocean

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
