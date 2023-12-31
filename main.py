2f285fbcf06d92411c8d7c1568eae337b7ec9363
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:43:09 +0000

    trap generally lift thorough concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
