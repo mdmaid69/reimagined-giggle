0879b50065e40a56cb717fc7036550505c9ca559
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:22:54 +0000

    winter broad television refuse borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
