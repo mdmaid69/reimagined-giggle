fb27970440ebdb0d1fca5d89144089993261ea93
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:20:39 +0000

    against hold month slow science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
