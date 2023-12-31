ac48b2774351c053abc84ef1aaa7865022e6e131
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:34:24 +0000

    shut about bang exhibition upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
