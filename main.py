f250cfc02f5bf5812bb24c8c5790f20cfbdd3656
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:19:01 +0000

    compare although bag there official

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
