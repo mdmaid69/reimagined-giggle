50f5e6f0ffad8f23cba45f84fcd2dedf6bd2563a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:02 +0000

    doctor pin early convince conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
