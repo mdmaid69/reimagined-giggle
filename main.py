64262ed4036348080c79955406a71c947f26fc7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:05:08 +0000

    experiment touch nature breakout married

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
