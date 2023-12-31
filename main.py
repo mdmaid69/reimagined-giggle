85b6b9eaaab5733f28549448884a47ceee105758
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:35 +0000

    Uu mad call nowhere introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
