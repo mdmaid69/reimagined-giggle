aea1cb1097dce9ac0c5df5a97a9d821cc387b5e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:28:01 +0000

    complicated woman important finally everywhere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def divide_numbers(x, y):
+        return x / y
