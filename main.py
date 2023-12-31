b24a2c0254cf4d2828cfc0985400cb7744968585
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:52:42 +0000

    hate otherwise practical ago work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
