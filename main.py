95b3872336b53da93caebe357ee61ce4f4268c80
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:55:14 +0000

    go hit keepoff may of

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
