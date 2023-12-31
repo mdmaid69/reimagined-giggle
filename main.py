1231385163b26ab22a1ffb29fb7e595f1a9cefc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:13 +0000

    similar closely public family close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
