aeb9828884556a1a7dce56f1f30b8429f3abbb35
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:32 +0000

    573entertainment lookaround prevent strength late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
