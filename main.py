3b91560aa1872e28ffa5abeaa949fa48792733bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:35:46 +0000

    pause catch crime smoke receive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import collections
-def create_user_list():
-        return collections.UserList()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
