222e51e5983c6415df5712f8b463cefb5b2fc33a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:52:28 +0000

    still fly Ss satisfied fierce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
