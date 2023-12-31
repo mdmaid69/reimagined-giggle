f9f760fe96408ef079d34c8f6d97c922dd9e996b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:47:30 +0000

    means first flavour fall then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
