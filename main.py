401758bfd99cbae963cfacb496653882217946d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:08 +0000

    holiday unless think cheek recently

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def clear_array(array):
+        array *= 0
+def multiply_numbers(x, y):
+        return x * y
