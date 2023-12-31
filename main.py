9d6375bf5c5fe99cea418422eb7a296a4cf1d6db
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:59:58 +0000

    shout smooth exist spread consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
