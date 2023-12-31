facb3ee175b88129ce019e01d3ac836aae24a021
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:20 +0000

    friend ordinary win cause journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
