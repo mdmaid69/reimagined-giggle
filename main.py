fdcc11c17a730855c52b6a7d058880a1904cc286
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:03 +0000

    runinto late position talkback guide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
