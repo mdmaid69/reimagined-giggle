11e10f4195fd317c6650dec389ee812ed3db884a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:54 +0000

    environment ear subject love married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
