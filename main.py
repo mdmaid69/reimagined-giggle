51993babeec58827e7ee72adcf3151de993d98f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:09 +0000

    work season city fear last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
