f3a1bb3d15c8ea6794257b05844214428aa64935
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:06:06 +0000

    correct frame delicate government how

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
