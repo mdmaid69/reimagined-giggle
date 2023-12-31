a1a3ad8eda32177aaf68aa3b8e3cd1b74f582595
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:05 +0000

    blame honest bad song thanks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
