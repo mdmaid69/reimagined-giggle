32b42197a09043174e67e55ce3c4a6a3cecb1bb8
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:29:14 +0000

    risk memory long pleasure store

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
