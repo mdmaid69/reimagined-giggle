f0a2853e5944cff2075b37daf9192e38f78a2ee8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:50 +0000

    performance trust principle tense right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
