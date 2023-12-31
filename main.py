6b4a4219fa0c152d827bb53eb90dd09a8095e1a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:00:27 +0000

    spring hardly splash curious guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
