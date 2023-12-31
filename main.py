7e0b729cb5e24d796331b639775d7334264456d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:03 +0000

    cold attractive myself single command

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
