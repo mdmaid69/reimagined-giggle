4a0a0cb41a0766cf6b82461733d9c6fe1f4a9bb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:05:07 +0000

    my intend more earn pot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
