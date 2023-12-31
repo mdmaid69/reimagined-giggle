12d99f4fd2175f65623cd9b7e55dfc77caa4c0e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:13:11 +0000

    into intelligent education start family

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
