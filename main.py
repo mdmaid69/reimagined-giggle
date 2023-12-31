5b7b586ca0867b84a816aa00940973ac1e1bb246
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:56:35 +0000

    shout turn rent experience break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
