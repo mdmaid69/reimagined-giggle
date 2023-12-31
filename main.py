b36ba6e0cad359f6f2df08b396b7a7d59f9d32d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:10:30 +0000

    speech minimum motion closed trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
