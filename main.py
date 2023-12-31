276c4b5cca63a51803f061a427b5478748ebf76c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:09 +0000

    majority describe demand turnon children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
