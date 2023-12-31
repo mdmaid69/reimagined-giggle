6cbb9b33f3070c9becf0beebc336b4d9f23304ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:27:00 +0000

    murder software idea himself post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
