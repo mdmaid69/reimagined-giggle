2986fcdc3c7b9461abb10cbe645328dd2a67be5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:52 +0000

    avoid children visit understand really

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
