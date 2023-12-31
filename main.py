1c3c04b184afab9536eef886c96187688d06f6ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:58:12 +0000

    love father furniture course start

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def print_python_version():
-        print(sys.version)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import logging
+def log_message(message):
+        logging.info(message)
