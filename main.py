df18e394fbe4cd144816a4a8104e2601ef77ac49
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:32 +0000

    lookafter university spray tone temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
