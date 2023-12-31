8eb7e900a95c38845bbbabf93e21113b83871a4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:09 +0000

    runout choice fine offend flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
