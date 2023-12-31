ae4f06f68cdd825c62dd5dfc941cbda277c6bd5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:57 +0000

    fast justice gooff name should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_array_to_string(array):
+        return array.tostring()
