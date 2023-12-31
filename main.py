b19d3b9e79a2c7e32ef92adf56ac617be9d1f0ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:05 +0000

    father loose own imagine ask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def divide_numbers(x, y):
+        return x / y
+def is_even(n):
+        return n % 2 == 0
