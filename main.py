6e39e0e0183ae65c98d8f582c761290c096ff36b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:46:56 +0000

    top no determined contain interested

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import random
+print(random.randint(0, 100))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
