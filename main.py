bec369c33f89dc20f053db9b9f1cb334f0adbab9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:20 +0000

    furniture difficulty flow consider church

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
