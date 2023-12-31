d8be8c7a1473146fb607dd23e83d76f00b19b982
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:42 +0000

    minimum somehow act butter must

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
