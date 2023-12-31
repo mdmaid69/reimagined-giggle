1af26ed41ea9b752e8eb19523061830a43cdf0d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:14 +0000

    positive home unit tongue turnoff

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
