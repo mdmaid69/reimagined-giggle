503489a803d66646ef6713e71af1b79f4b621f65
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:05:28 +0000

    record proof mess die speak

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def sort_numbers(numbers):
-        return sorted(numbers)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
