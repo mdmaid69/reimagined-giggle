01fe6bb0c5a70209a57aca30e78616fababa9a62
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:40 +0000

    attract worth left present before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
