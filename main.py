8c43f8e7337171b2e69ec4fdca04a0dde984c223
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:47:13 +0000

    crisis lift rather ourselves quantity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
