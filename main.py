c9b2773087265e352468ba49d135f7121bc81524
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:36 +0000

    badly difficult gooff team sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
