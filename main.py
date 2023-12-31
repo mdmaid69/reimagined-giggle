2d7cae6dd2e4802748adee56e03d4592691177b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:47 +0000

    cool remain between fill sometimes

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
