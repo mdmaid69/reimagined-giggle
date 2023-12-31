9845bc2dcc233beffbd9e978c68626dc96d2485d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:29 +0000

    upset speak each family stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
