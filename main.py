ef187b8f7c7b244bc072705a0266981cb9749879
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:16:23 +0000

    getback draw squash central half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def get_current_working_directory():
+        return os.getcwd()
