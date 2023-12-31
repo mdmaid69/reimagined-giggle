b688565bb1dcb7eb2d44549b7f97761a86cb9673
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:19:39 +0000

    suggestion warn sell meeting close

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
