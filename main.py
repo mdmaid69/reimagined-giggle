27084e25e99dbf194ffdfe272d62e131582425d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:24 +0000

    fast downstairs information Mm takeout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
