cd2126b8fc1471b41ac13636c77b3efa2eab190b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:50:45 +0000

    turnaway business supply create drive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print(sum(range(10)))
