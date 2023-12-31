f4815484399a1efd03dac9f630e8cc3c997b84d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:28:47 +0000

    aim whenever breakup plenty choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
