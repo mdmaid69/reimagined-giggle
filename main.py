f272fa6452ab02be561564a0f83c695a88863b90
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:34:44 +0000

    surface certain slide heat left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
