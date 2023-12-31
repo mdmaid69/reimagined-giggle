2fe0b6442f8c6bf8a9d55bacdc85ea806aba22f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:25:08 +0000

    broken punishment gas brush laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
