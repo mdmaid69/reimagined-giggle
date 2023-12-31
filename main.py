4c44e081638a14b1e8522e8ffa6397b777c0b003
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:47 +0000

    getoff stage especially very evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
