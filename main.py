2439af0f8f415bb673958abd8da3113cdf74f22f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:12 +0000

    demand crazy switch foot woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
