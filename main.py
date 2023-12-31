42f0da6ea5cb2d8f8d26bcbf4b9276108dd94a6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:57:55 +0000

    excuse characteristic load crowd sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
