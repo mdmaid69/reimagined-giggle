947808cf92a2957f4feddad38cebeaa33db650d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:39:06 +0000

    contrast ever shout balance many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
