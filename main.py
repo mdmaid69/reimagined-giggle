0703453442c4088a308d4d3726a174ab26985e9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:59:17 +0000

    cook taste needle hungry include

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
