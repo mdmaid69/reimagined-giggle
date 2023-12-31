7d6fbb259f286b1281db5af4e6b5854275bfb79f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:08 +0000

    beat paper intend telephone cost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+print(json.dumps({"name": "John", "age": 30}))
