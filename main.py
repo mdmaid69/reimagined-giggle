2c9096fb5cac8c98bac31c7c4ce558f1f742d7e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:46:22 +0000

    makeup common makeinto top lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_logarithm(base, x):
+        return math.log(x, base)
