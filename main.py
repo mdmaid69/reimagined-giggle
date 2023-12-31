0c0c8552abfc58d89d9861c6648a606d425f1eb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:19 +0000

    by shout blame high dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
