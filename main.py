239c6de17dc52d1f63b33894991e9a200169a96c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:36:13 +0000

    crash tongue fashion everything event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
