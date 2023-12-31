da7dee4448678e14fa900eea7a89e65502446fad
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:48:38 +0000

    extra until comment month disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_sine(x):
+        return math.sin(x)
