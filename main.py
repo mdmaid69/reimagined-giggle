2fafb3371ed25c98db7d56daaa5646ea32b1c497
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:59:41 +0000

    complain maximum giveout sing no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
