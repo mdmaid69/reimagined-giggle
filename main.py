b1e05fce8c694d30517bece71b9e0f5f5d55530c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:06 +0000

    stay guard waste television outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
