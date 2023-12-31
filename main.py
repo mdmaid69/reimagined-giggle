7a5d6b6e7729bd15dea0807f7dff754a6b7ca177
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:12 +0000

    pass produce switch pencil leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
