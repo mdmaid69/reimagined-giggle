6e4c781a16a1a16c18d9dd8de05c369bb352a046
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:28:38 +0000

    show reason closed platform division

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
