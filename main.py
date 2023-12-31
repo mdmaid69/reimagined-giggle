69127faf53af4d49aa817a12b0e863da7245cc83
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:31 +0000

    suckme write rubbish build agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
