e08cf6323dc23fb67a7f0b012906edadb2f40b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:53:02 +0000

    original confused plenty damp famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
