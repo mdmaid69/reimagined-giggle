c9521288c0484231804681073bb8ce98ca859942
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:10 +0000

    miss waste brush disturbing interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
