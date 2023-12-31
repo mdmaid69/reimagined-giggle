c682b7a84e9fe25a30e3b128209984ca39f6d038
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:26:51 +0000

    yours bad face exciting increase

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
