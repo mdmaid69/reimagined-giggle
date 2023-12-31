380c2c8d5ce12ddc9f1864999640b0001e94e6a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:38 +0000

    present history dark knock wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_gid(file_name):
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
