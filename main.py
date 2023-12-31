ab95b7571a6d34ccf2f053c9365deda451d88e02
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:57 +0000

    keep large injury delight regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
