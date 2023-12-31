42dea5b578fbcf88959bcd774fdb3bd60f869239
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:21:00 +0000

    drawing dangerous theirs turnover mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def is_odd(n):
+        return n % 2 != 0
