9264b64f8af34f5254cc5b021c2779acc998fac0
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:28 +0000

    grateful takeaway hospital wave block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
