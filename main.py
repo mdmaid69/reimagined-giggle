8e04e9641b97150defeb085566b12e99c75680be
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:12:39 +0000

    considering sick either team muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
