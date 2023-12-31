3caba7f47697d404eae55b3848a5b309d9094de5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:43 +0000

    shape tooth disgusting fire Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
