a038ee86125bc6b99f3dd1282e1b8d3d7635c731
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:11 +0000

    sound course can fail moment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
