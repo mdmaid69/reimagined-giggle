3756575e891d9c2046dda3393e3591f64d5e5917
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:35 +0000

    listenfor protest ordinary thought delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
