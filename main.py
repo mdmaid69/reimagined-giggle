d20a577955a14bfc7418a081852dde10a89f83c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:30 +0000

    flavour sticky judge protection firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def count_elements(lst):
+        return len(lst)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
