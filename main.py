f8b85a072d073bd241728011e2d415be851dbf85
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:01:27 +0000

    miss cry polite fire drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+from collections import Counter
+print(Counter("hello world"))
