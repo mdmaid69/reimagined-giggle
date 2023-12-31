c2bfd66352162d997ab81a29b5ef8ac83a0f3763
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:01 +0000

    think should unable weather important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def reverse_list(lst):
+        return lst[::-1]
