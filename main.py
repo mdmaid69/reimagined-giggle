4fd43e8c36ce4021a873a9f06467c8ade6761638
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:59 +0000

    muscle indeed side practise same

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import sys
+def exit_program():
+        sys.exit()
+def convert_to_hex(n):
+        return hex(n)
