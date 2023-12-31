8b09091e3dcae7b34224e1f9b93da679ca6060a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:59:10 +0000

    above disturb thin otherwise prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def count_characters(sentence):
-        return len(sentence)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
