07d2c242a5bd7ad70109ce29957b92d72e54a054
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:18 +0000

    shame by Gg statement its

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
