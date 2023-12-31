b5d948f519acb44dc41f731cf7b32f6f91873943
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:03 +0000

    flight quarter tradition smoke they

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
