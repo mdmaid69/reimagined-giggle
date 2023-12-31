83f752c892ea10450a7de0855e0a340e184fa897
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:45:18 +0000

    house thumb demonstrate emphasis steep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
