abd8a34349c7b72c9610f887f20a111355ae7697
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:47 +0000

    name grow lock punishment kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
