6abb3c26a4edc35d6117ac23d82de30f67cf9f83
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:34 +0000

    free face develop waste private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
