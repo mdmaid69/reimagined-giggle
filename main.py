762b19ca4204d93dea8ffa6785316c3a75472d9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:32 +0000

    callat sign talkinto culture about

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
