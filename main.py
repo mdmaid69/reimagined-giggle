cf291a4fffe6453ea306454fd2fe3697178655a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:56:34 +0000

    store majority hour school official

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
