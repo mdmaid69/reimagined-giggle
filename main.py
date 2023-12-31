fdea16e4c9a759ef6efaade64e7f35ab33b2f221
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:20 +0000

    advanced if contrast 565end left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_as_set(array):
+        return set(array)
