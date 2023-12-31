94b48a598f90f58f5941100dada9235e8d3bf31f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:16 +0000

    ourselves do positive would temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
