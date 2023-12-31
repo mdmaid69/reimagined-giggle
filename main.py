ec000badce38a3c1e8b64439413001169d36d1e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:24 +0000

    embarrassed create person edge worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_density(mass, volume):
+        return mass / volume
