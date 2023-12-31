c8999e459cb1fe1a3b793147c8f82cd2af6c0aa5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:15:41 +0000

    better open pay contrast feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
