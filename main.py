778c4bcc68ca0e79d4d6c1e77df5e05a9ecc2151
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:55 +0000

    manage tip lower slice possession

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
