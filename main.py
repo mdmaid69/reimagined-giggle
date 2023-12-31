c1b243f816ed32554f72bd77d6839ddcf7c79658
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:19 +0000

    smart laugh suffer pull outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
