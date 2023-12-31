2ae68876ef7f7fadffcaf750a08a6b8873598bbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:51:32 +0000

    exam once mistake string otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
