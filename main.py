267348a8eae71c63bc92f1bca522441d9d5c5392
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:42 +0000

    right no fetch bridge whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import heapq
 def merge_sorted_iterables(*iterables):
         return heapq.merge(*iterables)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
