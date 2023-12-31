c543c3ac5d42e61d2248f578d3dcc2505b04a192
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:27:51 +0000

    other without trip hang several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
