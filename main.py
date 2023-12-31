57942bffb139189d0c815f8308f7b34d9f2c69cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:52 +0000

    everywhere environment perform recommend himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import time
+def get_current_time():
+        return time.ctime()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
