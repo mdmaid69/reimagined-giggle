0239ec3a23fefe840d9f0abc97e94942e3996f4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:44 +0000

    spill major wrap Zz takeaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
