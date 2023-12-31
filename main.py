55b6ece57c940688c7de38ad71e5085a623b6e7e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:31:09 +0000

    ability declaration wish land takeback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
