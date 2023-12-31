605e7aacc81a734dffd69dbfc80c4eeebf24e2dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:55:50 +0000

    autumn communicate move unite today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import tensorflow as tf
+print(tf.__version__)
