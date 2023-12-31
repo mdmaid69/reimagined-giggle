7c71f7adc690f0e45c602c8d193bf5042bf8530e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:17 +0000

    change fire away regret blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
