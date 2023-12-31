73f36a3835848e680949c957f16c6ae03ee1b366
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:04 +0000

    hit downstairs must challenge slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_typecode(array):
-        return array.typecode
+name = "Python"
+print("Hello,", name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
