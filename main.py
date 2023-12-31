83d444f3e7e8915e84223fb3d418f345ddd25758
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:34 +0000

    bringback system value one official

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
