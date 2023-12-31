8c40962cff43fee5dc8feeea850fcea4573ba9c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:14 +0000

    answer drop about invite whenever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
