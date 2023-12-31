7e23c17e51d167d46377807ef9691a37c433fcf6
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:57 +0000

    charge maybe store tooth regard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_pressure(force, area):
+        return force / area
