aa5acd7df3578c9dc589b3fe61a0d40e01556c11
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:41 +0000

    damp charge talk his lead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
