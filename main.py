5b02bca9240e20ca40bbb44527cb0689825de720
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:53:24 +0000

    hope schedule 564encourage through meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_area_circle(r):
+        return 3.14 * r**2
