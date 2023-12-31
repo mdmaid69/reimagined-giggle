cf38569f7c28c81630f53775ba0dc4b8d771d2ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:05 +0000

    even instrument common impossible sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
