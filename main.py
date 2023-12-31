b2bb6c7d35453aa65e7ac7f37cca3afee48a6dfa
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:41:34 +0000

    against increase error trip pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
