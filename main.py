42ac812fe428d6827feba07aaf5c090e96435125
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:49 +0000

    popular win play before before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
