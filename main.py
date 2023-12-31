468a338d928c8e973dbc83e55f727e360c096b3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:39:55 +0000

    switch object meet embarrassing advise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
