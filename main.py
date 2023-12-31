8d7fb3cec912b3d3d0d0f1be104cf1318440df94
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:58:35 +0000

    secret story well worst accident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
