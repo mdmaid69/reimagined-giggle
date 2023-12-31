fba897d67961d5d89353835da6e0d7435ac55f69
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:43:07 +0000

    risk argue for probably get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
