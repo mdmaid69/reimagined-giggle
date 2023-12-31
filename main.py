770d30771256990ef9ac0b7549fb7ed2723e7422
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:40 +0000

    petrol extremely everybody software day

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def print_python_version():
+        return sys.version
