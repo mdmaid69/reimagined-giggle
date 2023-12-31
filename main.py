59c080234d17d6d01cf60dc02547014ab5599ca9
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:28 +0000

    different hers fetch refer culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
