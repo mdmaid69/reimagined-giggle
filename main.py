27ac36eeaf5471f4fd711ac5848db26f04e7a0f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:44:29 +0000

    gradually wipe tragedy tense score

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
