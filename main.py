435a12c90f0661ad9bd9e4955e0a9d97de4f3978
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:26:03 +0000

    object fasten possible vision separate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
