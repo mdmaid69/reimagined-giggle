239c7c71245b474791ba5edf2f2b330923fef717
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:56 +0000

    single earth centimetre objection be

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
+print(sum(range(10)))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
