78f8abcafd727742691f496eda8d6374d345097e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:51 +0000

    influence entrance influence hit goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
