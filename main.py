52f91a2ece52a81a37b4caf8f0ce616acb1c5c3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:06:03 +0000

    place stage state bringdown line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
