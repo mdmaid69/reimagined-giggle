03a40c84c98f98220e38e16ce5064162d6f1c053
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:29:33 +0000

    regret trap welcome truth talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
