3acb4735b234fb7aed1a837f7a2678b8ff35ef94
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:42:34 +0000

    pay number true now joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
