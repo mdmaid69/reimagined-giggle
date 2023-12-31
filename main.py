e22c95aa3447441602f13583d0b4ab4967847946
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:55 +0000

    keepaway deliberate thinkof notice wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_as_str(array):
+        return str(array)
