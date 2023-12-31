2d20729ef75c5ed92899bad788a31b4ae1187e50
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:30 +0000

    contact prize interfere increase commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
