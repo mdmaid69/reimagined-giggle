47aed555aa0968aac204cc57b749c3f52be1e62d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:48 +0000

    unlikely juice spray glass must

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
