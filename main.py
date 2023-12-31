3ca1d08666d3eb3c828cf25a75089391b11f671c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:44 +0000

    needle must onto spread indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
