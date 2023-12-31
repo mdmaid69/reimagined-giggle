5568b22a2242e4470fcca8faafe53db7b12ff5af
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:18:34 +0000

    plant rob complete uncertain urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def count_elements(lst):
-        return len(lst)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
