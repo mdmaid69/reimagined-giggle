287269bcc979e4446ce47a052f6cc17db747e3b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:58:38 +0000

    noisy difference attention tour exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
