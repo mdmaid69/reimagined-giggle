f5d6c20a02bea5dcdc118ef784b8fa9eb95d4502
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:59 +0000

    what public mark classroom opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
