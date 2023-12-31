56b281a92dc9f3e7df762cd6d9f2e45f29a6e2b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:12 +0000

    young eye print polite personal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
