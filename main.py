d8d6c89bf91d3a21ef185713a877c448be93ee47
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:13:53 +0000

    beneath pair loud pay platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
