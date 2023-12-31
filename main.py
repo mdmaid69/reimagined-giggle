05a5a7660d1a7efc1696474bbfb5f6506ed05668
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:23:50 +0000

    original reaction comeoff judge giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def remove_duplicates(lst):
+        return list(set(lst))
