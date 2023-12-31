fb603958ef92c8a583ee99f9b86015f65b643fe6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:53 +0000

    inform level Ii pool rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
