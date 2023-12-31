9671b74d6b4a451ccb19bb75c1ba7e7edee751cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:34 +0000

    objection basic music burn giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import collections
 def create_stack():
         return collections.deque()
