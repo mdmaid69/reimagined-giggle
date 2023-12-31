e8c7ab68ed174a62bd772822967907106ac94c29
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:26 +0000

    away about interrupt loud callat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_min(lst):
+        return min(lst)
+def calculate_work(force, distance):
+        return force * distance
