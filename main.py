a1e774a4822f8c0d1ac3c1ac6bd9ea6d5aa136e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:12:44 +0000

    damage deserve minority slightly arm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_density(mass, volume):
-        return mass / volume
+import re
+print(re.match("h.*o", "hello world"))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
