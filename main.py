61e1cd36f83119701e3ef257f6445da7a6c4b628
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:28 +0000

    grow task best police surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
