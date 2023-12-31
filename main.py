0829fdfb9add2de753a2f2b48eaae298e9635f20
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:15:58 +0000

    treat now advanced deliberately method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
