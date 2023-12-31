64170af4b02e583d686a7165db9e46bd4e1bf5b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:04:07 +0000

    market voice commit pen degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
