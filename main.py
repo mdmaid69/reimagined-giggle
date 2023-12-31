793b7821ddd3a0e0b93a7bf16fa08d54f7121270
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:46:18 +0000

    read any pretend air trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
