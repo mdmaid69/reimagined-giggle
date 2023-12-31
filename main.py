c262d6174c8864fa155db4c2b87d954c03961306
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:29:19 +0000

    waste centre really pour touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
