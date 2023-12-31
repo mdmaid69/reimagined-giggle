9c1afcf8895eb44fd0cb4279d08c5f485338356a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:40 +0000

    summer spoil occasional basically difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_directory_name(path):
+        return os.path.dirname(path)
