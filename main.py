5c81fbaa0ce15d0e469b54182cf7925f9539efd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:53 +0000

    aware power understand lesson reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
