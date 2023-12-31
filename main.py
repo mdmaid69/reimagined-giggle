f886ff7c7dcf27f283225291b1a14ad6f54dc7d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:08:33 +0000

    season collect circumstance polite off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
