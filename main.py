7886ae89294f32109d8a079150bd50b5c9fa83ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:22:37 +0000

    separate home disgusting that concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
