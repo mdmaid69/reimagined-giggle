bb7fd56227aaea1032da57c92c079bd07e816255
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:24 +0000

    send drive answer process colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
