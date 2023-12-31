66d7d3106d4302d33ba20fe52a28b7a4ae53f7d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:39:33 +0000

    child clearly thinkover pencil intend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
