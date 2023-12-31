1fcd9a04dfaa6265b12ee8edee34b04e667d62bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:42:36 +0000

    tone speed wind Uu separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_rectangle(l, w):
+        return l * w
