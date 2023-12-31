73bbbcebc61f63e9ab0897c2604d0bb7bc53a85f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:33:44 +0000

    schedule goodbye burn funny swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
