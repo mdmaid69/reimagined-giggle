ed3731e941cf1b6fa7211dc6bae8b35a18d5bb04
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:26 +0000

    bend false together pot produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_bool(array):
+        return bool(array)
