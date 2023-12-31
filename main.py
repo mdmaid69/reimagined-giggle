ecfe0ac1f32f7fbb759301f954e53cb894ee05c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:08 +0000

    section rent finish less search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
