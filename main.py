812d23580c31198b6aaea17551cf409b79aa1dc8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:14 +0000

    advise fetch disappear transport strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
