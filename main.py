a79f8ccb561963fcef894877b5ad2708a6c42dd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:11:49 +0000

    dick crash slice major behind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_power(work, time):
+        return work / time
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
