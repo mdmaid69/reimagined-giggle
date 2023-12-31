cae8888b668836c7da96506de37334044c51a249
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:32:13 +0000

    marry receive bring tie urgent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_min(numbers):
+        return min(numbers)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_set(array):
+        return set(array)
