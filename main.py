abc6ba5716f4ec1d1837ffbbe8695c6d32b83b85
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:23 +0000

    respect operation prove brain sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+print([x**2 for x in range(10)])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
