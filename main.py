4f517a1b4fb8be2f98de165d05eba52878af835b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:02:21 +0000

    feature cruelty cap sea direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import math
-print(math.pi)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
