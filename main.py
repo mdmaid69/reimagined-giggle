fd461ed321f7218c5e0ca9895fdccb5008af0ac9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:36:35 +0000

    look drop understand keen obvious

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
