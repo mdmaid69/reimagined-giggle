9dd01c461520951da1a7fcea38e46aad253b64e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:49:12 +0000

    room know doubt base foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
