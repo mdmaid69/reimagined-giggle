b3b5a28cfb3ee9c5930f3e168e038aa0da5f4174
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:52 +0000

    fall hope result mend still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_power(work, time):
+        return work / time
