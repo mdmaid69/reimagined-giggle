09c81b61fbb50578dc99cfa837a271c7adcbcdc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:13 +0000

    minute original machine look under

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
