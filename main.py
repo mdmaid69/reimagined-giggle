d356079d65d2c49719379e21f4c9430c01086342
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:34:31 +0000

    create suggest mystery each hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
