a58850155ae74e3526fd57848bb1978085dff920
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:10 +0000

    blow hang capital purpose rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_typecode(array):
-        return array.typecode
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
