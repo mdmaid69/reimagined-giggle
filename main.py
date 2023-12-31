1a8ad4b23b366d6651953d7c75344513a18fed90
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:37 +0000

    apply call through wrong this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
