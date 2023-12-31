decd7b31a175915ad714301a2b410d4d4b32d846
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:51:55 +0000

    guess spite dare wet it

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
