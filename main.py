3106b270bc4f918854cf0ed393605dabae0f907e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:50 +0000

    to fresh estimate interfere voice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
 def calculate_power(base, exponent):
         return math.pow(base, exponent)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
