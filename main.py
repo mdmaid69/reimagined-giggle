e52de72969d9986e7d361219e88bfd07c763c95e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:51:16 +0000

    friendly wild since year over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
