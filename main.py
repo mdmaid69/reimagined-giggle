ff1663e45ab294461dc5bcaf0c51999d167e1f1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:10 +0000

    sleep person plate small divide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
