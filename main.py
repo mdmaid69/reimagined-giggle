5e2076338e6d55518c3b685aa2e9f1824ba38087
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:50 +0000

    recommend say card curious worried

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
