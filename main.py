3fb710db25738754fdd9bb8f8f0f8ed78cc240c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:25 +0000

    luck importance file wire nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
