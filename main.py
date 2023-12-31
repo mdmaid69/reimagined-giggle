b7a16dfb8aeea57c401925574da559b46d9bd4d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:35:43 +0000

    square land important call choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
