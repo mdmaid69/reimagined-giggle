7dbbbc519be8f35e9e70b26d7df35b115110da8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:49 +0000

    whether keepup 570enjoyable children break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
