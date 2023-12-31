2d7befd389ffec94820e64cb5e0274e1d24a67a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:39:30 +0000

    talkinto sick pain state spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
