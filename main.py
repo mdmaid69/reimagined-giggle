e1dd3adffca6c7a2dd2d14811c55d4d19a34de7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:07:11 +0000

    branch accidental justice approve force

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_even(n):
+        return n % 2 == 0
