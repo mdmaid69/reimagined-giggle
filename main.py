1b96889258d7c73809f7eed29fbab0251ddf0654
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:21:55 +0000

    base on interest conversation suit

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
