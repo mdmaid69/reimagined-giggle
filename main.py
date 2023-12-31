51817e95928203d402d5681996e1cd6024e6213e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:04 +0000

    second spread communicate achievement sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
