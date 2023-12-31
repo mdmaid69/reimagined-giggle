dbadd8e2037a7b9a534aed31802b230a8a96d963
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:41:47 +0000

    slice obtain often lower price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import datetime
+def get_current_date():
+        return datetime.date.today()
