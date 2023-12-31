cf558ec0223d42240115c3c6893d7ecbea66a4ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:35:20 +0000

    organize note blow anywayanyhow aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
