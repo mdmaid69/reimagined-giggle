96e3cabfe27a0c71bad3f495ab36539b66b8dd60
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:10 +0000

    willing pipe lean have control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_length(array):
-        return len(array)
-import time
-print(time.time())
+def convert_array_to_bytes(array):
+        return array.tobytes()
