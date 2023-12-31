efc818bfff5a4f73e13f1441b6ff716c84267b77
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:07 +0000

    holdout bright bet still doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
