3b74209578f9313cb4afd92670f9779b04229e6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:39:07 +0000

    play regret 565end comment match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
