388f423254fd2ca4ce29fcc2643979c5035c3726
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:24 +0000

    age frequent everybody view join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
