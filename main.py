a27242e8e34a3c15c6a650f0e0d6efe50f176362
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:14:36 +0000

    notice switch summer appearance something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
