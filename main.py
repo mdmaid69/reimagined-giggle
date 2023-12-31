0f90e56caa524010ed009c4ff353dccfe8a5a70c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:49 +0000

    form store fun formal square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_int(array):
+        return int(array[0])
