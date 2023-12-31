8635b860f548c695bcb2acfeaaa9215b0544b4a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:24:26 +0000

    sight closely exciting swallow perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
