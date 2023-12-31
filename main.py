6d71b40ac16f4c27e4b54c9810da421f944ce3ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:27 +0000

    turnon split such off makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
