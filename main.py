a5f5f2af9c9e458a2412c53a54044a33f9d05244
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:42 +0000

    Rr often borrow might extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
