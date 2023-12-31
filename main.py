b648901bf43a209f593c048428300e4be2c6f35c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:55:40 +0000

    female giveup now side otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
