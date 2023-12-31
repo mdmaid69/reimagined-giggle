bd5ff57770b334f792d9fe39260aba6e9caa400e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:07:14 +0000

    sick more will complete health

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_as_list(array):
+        return list(array)
