da29f782c67ea99ac11a21bd244d667d62df6fe8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:45 +0000

    create honest while snow dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
