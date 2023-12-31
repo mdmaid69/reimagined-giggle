f71ba610bcd9a0fd6aadf38bd95fb5f72262be22
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:43 +0000

    answer cruel makeinto neither pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def reverse_list(lst):
+        return lst[::-1]
