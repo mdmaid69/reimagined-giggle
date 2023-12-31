d60172bc1cbfdce2cd7fba1d4bc6e083eb6a54a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:43:43 +0000

    means impressed recognize tie waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
