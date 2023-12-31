573ee6f1404f23ab46c2985e50f358800ce8bd29
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:34 +0000

    grade somehow 573entertainment gas complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
