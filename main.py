7a829af1dcce753595b45b291ae503906644428d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:49 +0000

    keepfrom 573entertainment approve degree ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
