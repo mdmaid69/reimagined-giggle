1815e16913678894317837c3da8e45c7f050cf0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:33:28 +0000

    lookback(on) only bend go like

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import random
+print(random.randint(0, 100))
