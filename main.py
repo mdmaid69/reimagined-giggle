7e3bc8d258c8b8d990b9b1e89494902cde28ba29
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:21 +0000

    wall lunch spin comeout outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
