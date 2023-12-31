edb91e4f6a293a9c7bb3bee072aae54c41cd2936
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:06:59 +0000

    separate surprising it order sudden

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import getpass
+def get_username():
+        return getpass.getuser()
+print([x**2 for x in range(10)])
