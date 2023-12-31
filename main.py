ea5e4ead0f1203e9eb959b4c4a9fabba2b09ef74
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:01 +0000

    joke creature goout shout finger

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
