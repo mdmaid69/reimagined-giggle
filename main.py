49a15e91d95aa001f304843196c5c38dc4a9faf4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:38 +0000

    material however instead minimum love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
