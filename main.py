3c340e2ba152fa54721a52da0d1fd600a1c35e1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:00:33 +0000

    centimetre her other there although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
