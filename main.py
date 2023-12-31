5ac044bc097038f7718a5c129bf13e1ae1ca6f8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:51 +0000

    flood mistake justice birthday know

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
