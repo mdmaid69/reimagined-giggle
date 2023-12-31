6d4869e302536eb4ba1407ad126d9b72263bdd98
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:28 +0000

    expect face idea together protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
