2cdf4c338f537ad3692fb9ad3e659fab456ec9cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:48 +0000

    laugh telephone value make main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
