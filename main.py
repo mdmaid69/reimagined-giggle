10559d1bda14869dcfed6fb5f39454006f1503c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:15 +0000

    call workon roof sink ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
