fff3e6f953b3446800ae79ab9b4da246ae1db379
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:37:57 +0000

    communicate wander Ll ability tax

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
