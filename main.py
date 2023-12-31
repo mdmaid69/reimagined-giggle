08e80ff766bb6aaa89dd60d5d1203685014b6535
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:44:46 +0000

    club produce window survive film

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
