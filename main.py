734d785ff005908ba58870f2b8cf7744dd68fc64
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:46:37 +0000

    replace keepoff by cap rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
