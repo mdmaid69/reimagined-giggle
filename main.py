41c0750d10be055297bdac28f2610e0098f3b0e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:23 +0000

    waste difficulty ride play hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
