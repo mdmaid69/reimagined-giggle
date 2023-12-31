cadecb4b327f56e701bdb97be68959a1a40d9752
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:14:17 +0000

    dinner store butter search together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
