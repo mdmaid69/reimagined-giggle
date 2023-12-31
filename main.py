9c7a765d5eeed30a699ebeb234b5f09b59cc8915
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:02 +0000

    pain please protest last objection

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
