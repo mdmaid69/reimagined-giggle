19c4beabc4e6b565ae9c9fc884bbffca2e662e9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:30:48 +0000

    description environment impossible remark wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
