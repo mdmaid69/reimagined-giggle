fb5118c4da40d3132f6a4e47b48f79edd6355e03
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:54 +0000

    match joke date unite obvious

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_min(lst):
+        return min(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
