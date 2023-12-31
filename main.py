5511a6fc1fa4e9e96352aeb893686333d738b13b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:50:39 +0000

    somehow run crowd raw kill

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import random
+print(random.randint(0, 100))
