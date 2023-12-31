7d4436d94fab7c31240d31f613728c1958b0ba90
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:11 +0000

    battery later man difficult preparation

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_work(force, distance):
+        return force * distance
