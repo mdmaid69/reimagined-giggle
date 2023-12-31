b8e78199c1b6b322bda25ed1d9b31c5bc33bb7c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:52 +0000

    fall have block here display

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def convert_to_hex(n):
+        return hex(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
