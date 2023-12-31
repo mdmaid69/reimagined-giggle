325848b856413ecb7c2efccc387c6c66884e03ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:15 +0000

    decision declaration ordinary defeat distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
