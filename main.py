289eb19c7deb44b0a8cb76f60cde1f7f29877c2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:14 +0000

    year face hurry convincing politics

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
