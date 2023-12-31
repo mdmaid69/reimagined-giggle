8c25ca90f969cf2dcb9d874b9e64d61162da6844
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:07:16 +0000

    cause once love instruction level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+def log_message(message):
+        logging.info(message)
