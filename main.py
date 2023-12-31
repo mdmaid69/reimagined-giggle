06ced89b26ec07eac195c9d3c0c754cd1f0ae4d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:27:39 +0000

    building splash black by evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
