e713df61f23f090c61c8117bded3034bae446a39
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:42 +0000

    comfort meeting programme pair air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
