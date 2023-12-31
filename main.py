6c494aad961046b7f3998cc5db9aa76e01a6ee25
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:41:59 +0000

    phone wide closed whisper joke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
