9e50d807607db173f2eaf57d41d2cc95fe4e704e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:33 +0000

    person broad weigh keepout cry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
