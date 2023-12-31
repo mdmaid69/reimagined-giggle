98bc3ad96311e03accd9939a15cebf656bf223c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:17:38 +0000

    even shadow trick butter swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
