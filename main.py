9ab5674cab4c5a36dc412eece08effb6e0298ea3
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:17:31 +0000

    possibly heart demand luck why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
