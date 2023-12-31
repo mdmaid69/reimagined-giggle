97c03d49c3e0a4960d02da0134dd745728b8bb0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:32:39 +0000

    bag turnup soak balance forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
