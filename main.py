3fb625c39627da8ee59218e4dd1939f0c659128e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:13 +0000

    bury reply takeover either clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
