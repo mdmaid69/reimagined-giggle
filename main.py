bf0372a3a029fa54d6be89a973985981cc092b7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:00 +0000

    work shout coffee fit calm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(numbers):
-        return min(numbers)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
