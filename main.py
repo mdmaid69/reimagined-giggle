139b0d7a973754433e0da610622d71ed65673791
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:27 +0000

    foot no pretend influence unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
