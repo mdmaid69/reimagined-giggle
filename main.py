a526f14e375f8b868be67c39a258f127b1c7599a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:21:39 +0000

    mistake opportunity quickly convenient black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
