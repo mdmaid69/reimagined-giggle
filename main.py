9790685e1602cc70b4a242a3c51d574dc5c8d90a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:16:58 +0000

    frighten between base direction birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
