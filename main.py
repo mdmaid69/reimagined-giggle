673aa805cb03261fb2874c8c930fd4427ab49107
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:41:40 +0000

    criticize suggest tomorrow glass case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
