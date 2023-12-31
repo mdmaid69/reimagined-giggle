24adc8df7b15d726057d7ad4ca2ba7de2104fb4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:57:54 +0000

    fasten clear attract outside measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.ctime()
