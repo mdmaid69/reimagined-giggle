04683fbef67e09734c326ad6448f57fedae0fdb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:18 +0000

    mood mail shout rob touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
