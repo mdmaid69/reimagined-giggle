ac4abae8a981dc39bcea4de835859fce110de6e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:12:14 +0000

    person minor success remove start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
