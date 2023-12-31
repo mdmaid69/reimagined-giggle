324ad3a03748683f24381bc7e65a9cfb98e6450b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:45:46 +0000

    comefrom holdout rather refuse town

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import os
-print(os.getcwd())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
