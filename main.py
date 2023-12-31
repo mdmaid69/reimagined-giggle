c0538573beb8d34867b708583df93c907b1e70d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:53:06 +0000

    agree violent lie door mind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_speed(distance, time):
+        return distance / time
