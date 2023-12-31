7476da7a81020ab5a386a5e4000b973ea9522351
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:12 +0000

    release someone squeeze ground Gg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
