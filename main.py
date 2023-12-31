654892cd855ff261a3fa804288a8ddbadef5eeac
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:26:25 +0000

    niggaacademy bet also war alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
