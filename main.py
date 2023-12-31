c2484db4f394ec2b9b1d28107cca9ec38a5bfcf6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:33:28 +0000

    record seem debt comfort be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
