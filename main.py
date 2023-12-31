36ae799d44893319920943024bf610af52c9aa43
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:14:38 +0000

    murder equal instrument comeout in

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
