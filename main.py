6a20f485c1c24a7564ef40eeb12d69b2dfb64dc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:41:53 +0000

    memory shoulder workupto crash near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
