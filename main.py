cfc91610497ec1b68de3dcbaae3a897e0b7e2fe0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:22:18 +0000

    spill buy basis blue health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
