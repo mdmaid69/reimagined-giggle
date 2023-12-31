181dff312db254d4c61f0246a8df0a14b0933419
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:41 +0000

    watch tool dry list swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
