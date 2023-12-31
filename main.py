4ae2d8ef8d994a2798ec352ac4ba8311b2583bec
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:51 +0000

    pronounce nigga nothing exist say

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
