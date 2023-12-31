29690bf93457cff5057bc4ca7bd22881e54cfebe
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:39:05 +0000

    embarrassed odd goout now hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
