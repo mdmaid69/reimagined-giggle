0a65713870b26110828f6800bc9377934398cb79
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:25:28 +0000

    report hurt pot advice seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def is_even(n):
+        return n % 2 == 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
