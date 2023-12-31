b02daace489032788bdb532739a7c928268bc2b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:42:07 +0000

    alive Kk error both somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def print_python_version():
+        print(sys.version)
