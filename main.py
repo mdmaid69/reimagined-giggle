bf3db62f72fc75585f8020047a5d92801eae70ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:17:01 +0000

    that clear hold invitation little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
