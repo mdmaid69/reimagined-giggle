3a128733cdb49fcaee90cf5d17d07bff82f47713
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:22:13 +0000

    reaction prefer silent responsible fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
