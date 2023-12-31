f7d6ad5d45b446e6500625580d77fda11147312d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:51 +0000

    makeup relationship substance responsible watch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
