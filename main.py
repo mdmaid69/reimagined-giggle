d6f343c62b249887a4e49e44686851b02f5fd409
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:51:47 +0000

    dance cheque stir whole goaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import numpy as np
+print(np.array([1, 2, 3]))
