22671c8a958c0d3a9df9b300521721492ca6d1cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:03 +0000

    arm exam goback(to) unite cover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
