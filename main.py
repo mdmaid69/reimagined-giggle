2b79245683489477c46613de8a8c8960c0f5940e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:35:25 +0000

    seem during bright unkind supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def reverse_array(array):
+        array.reverse()
