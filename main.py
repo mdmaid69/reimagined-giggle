86c85f76ede5b442f0ccbf9a062618f8c63fe26d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:44 +0000

    judge valuable tone variety only

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
