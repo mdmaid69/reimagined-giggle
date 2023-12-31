7117575b90d91abfb166b7bbfff584086c1ecb01
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:21:46 +0000

    modern dick intend warm consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
