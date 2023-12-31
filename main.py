7f9387771b80a553c64cb32d9752fa65a462c2a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:00 +0000

    rich shock kill cheek rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
