362d07e68ff1426c4e80f92bdec594118a72e9cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:35 +0000

    limit measure regional sensitive total

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
