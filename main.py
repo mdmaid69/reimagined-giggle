1e82af3a1e8c065ed47e83b86f7428d0671ef6fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:36 +0000

    listenfor borrow contents affect save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
