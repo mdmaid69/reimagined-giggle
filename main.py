7cd5cbdc21a10bd52eb5e640eeeca5e839b3c6d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:48 +0000

    include later still whether mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
