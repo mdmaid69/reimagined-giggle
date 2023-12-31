047cc023d300b5bdc6e7bc1f5248adf5d1c0b6c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:13 +0000

    sharp poor cut normal circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def convert_to_binary(n):
+        return bin(n)
