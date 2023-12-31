014f8573acb947fb80d2bee1b3d99849bc9b449a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:00:09 +0000

    force workupto view Qq awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
