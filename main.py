d5a72b6b6aa9e41e84104ad18f8fc9aaf2939094
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:43 +0000

    way ago question bright event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
