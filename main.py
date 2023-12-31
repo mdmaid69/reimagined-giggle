cf753d0ecb32b7293e630bd455f4594ef19e3dd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:14 +0000

    development minimum lock scheme match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
