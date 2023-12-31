804ebf5cd1ac0b6a503cfc1167db1f86c91cbe35
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:37:19 +0000

    justice yours shine answer wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_user_string():
+        return collections.UserString()
