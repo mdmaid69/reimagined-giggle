ae77a6637324f033cc97f01ced9b34c0c4f57c0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:27:37 +0000

    consist equal declaration slide spite

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def square_number(x):
+        return x**2
+def is_even(n):
+        return n % 2 == 0
