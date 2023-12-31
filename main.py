4d3b4e6bf61b308808fd5092ce6ea31c3f4e346d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:46 +0000

    direct pencil land past joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
