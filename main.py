6c36eec5549f9883f4463bcf1f566e151df695d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:53 +0000

    translate practise case way tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
