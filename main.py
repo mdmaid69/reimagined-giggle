3ed4533349d0c6b9a0dd96c51d641327b9b21db9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:25:50 +0000

    deep soon formal control equal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(numbers):
+        return max(numbers)
