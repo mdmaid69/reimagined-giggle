e357364ad5b35a3d9da92f25b5563e9bed12b5f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:45:47 +0000

    fortunate like call taste operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_priority_queue():
+        return collections.deque()
