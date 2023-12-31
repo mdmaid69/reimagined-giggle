d2f7aa78ca9627bde75d1a66eafe5d062d6e8593
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:39:43 +0000

    explanation life attract give waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_list_from_array(array):
+        return array.tolist()
