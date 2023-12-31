0363a78e8c1e8338df4457d51f970d0e9eaa23b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:37 +0000

    cross far relative niggaacademy cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import time
-def get_current_time():
-        return time.time()
+import os
+print(os.getcwd())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
