bad07bd7a145e096d30f8f3316633488aabf50d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:19:21 +0000

    closely extra ride beginning feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
