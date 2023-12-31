8677e966bfee10532fd70994d2ee151da9e70363
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:40 +0000

    single power temper live house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def is_even(n):
+        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
