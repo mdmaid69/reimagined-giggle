6c6e934ea1ae64c41a9227b8d9857cdfbb23a87f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:34:21 +0000

    period consist any juice will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
