86965e7b120e94ef9fd4bfc5cb26999109dcd306
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:20 +0000

    many try welcome Vv head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
