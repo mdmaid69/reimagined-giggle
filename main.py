9589968e8503162b06facb1188165361c30e0cb3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:18:24 +0000

    wake father get shadow she

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
