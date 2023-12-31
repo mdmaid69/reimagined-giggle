5b4d3ad0abd30ee047a92942efa9eac083ecc93f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:00 +0000

    lonely brush change majority outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
