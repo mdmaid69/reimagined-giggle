57a16a46253490c7a1204e9381bd5ce4e6b3799a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:46:30 +0000

    door moon guard acceptable march

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_string():
+        return collections.UserString()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
