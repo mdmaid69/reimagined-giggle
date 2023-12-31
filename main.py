e77fdebd6917540a743c852c496ac2921da40915
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:49 +0000

    male actually quality appear fault

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
