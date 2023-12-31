665e74cda60b4f48e23680031e23c120407b2e35
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:29:22 +0000

    Itself her niggaacademy laugh modern

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_max(lst):
+        return max(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
