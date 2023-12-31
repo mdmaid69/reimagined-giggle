e283a2a60f201d6efcd76f905328ce14802d2ffb
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:55 +0000

    type exact ready pay love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
