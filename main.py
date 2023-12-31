7fe459dcab6dd2f2f5fe5642ea75fd6836649611
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:19 +0000

    die cut hard spoil action

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
