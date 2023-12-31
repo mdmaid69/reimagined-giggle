8390471885ad582f6f5739f8025112625cfbeffd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:19 +0000

    runout ready walk again song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
