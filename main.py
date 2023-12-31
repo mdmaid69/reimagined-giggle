864e95bd45001e076521e7ac1403c0a5381d10f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:54:19 +0000

    beat song platform address nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
