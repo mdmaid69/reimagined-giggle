900bdb77bff15018bbdfe5e014150139aaf1384f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:10:44 +0000

    together wish really mad proof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
