1f316aa0012566fa59cc9af78a2c59f586b353b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:15:47 +0000

    fat absolutely judge sign call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
