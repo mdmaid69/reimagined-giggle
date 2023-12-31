e6765af9220a23cb93e29a46ae740d168b1b996f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:19:49 +0000

    stamp important brother uniform connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_python_version():
-        return platform.python_version()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
