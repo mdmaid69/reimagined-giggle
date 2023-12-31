6eaa01b7870b0e48b5abd8ee5e20bbf5fe46144d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:58:21 +0000

    act my direct mainly flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import collections
-def create_stack():
-        return collections.deque()
+def create_user_string():
+        return collections.UserString()
