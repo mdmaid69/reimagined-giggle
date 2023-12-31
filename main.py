3cb80c797dbdfa6bf1d6badc8dc6fdb11b33877b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:54 +0000

    yes dish frame idea accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
