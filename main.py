fa48a3cd56edcafeb63dcb87366ca5426dbc5dbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:50 +0000

    heavy loan who system listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
