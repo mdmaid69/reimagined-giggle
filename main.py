c3b66daabcce7ab79d0fbdab33eddb5f8edbe333
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:05:43 +0000

    serious pride height true spin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_priority_queue():
-        return collections.deque()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
