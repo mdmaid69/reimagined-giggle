d273b563b31d50cfd5d74671c35a8912238d0f0b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:01:54 +0000

    scream needle apart direct nervous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
