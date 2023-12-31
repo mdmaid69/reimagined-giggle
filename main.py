a40b4f062d38f75fdfc53e6b17b612b3c2aa3d48
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:37:13 +0000

    as pool regional at disappear

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
