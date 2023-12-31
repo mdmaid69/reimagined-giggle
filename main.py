6e2ec5ee5412cade6dbb9d77911c1997f43d9c6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:18:16 +0000

    sport admit even scared positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
