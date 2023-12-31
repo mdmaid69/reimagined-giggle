baee5d16356c80a6ed5d8d8c54660a1b3f4cf3d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:58:05 +0000

    travel try recover insist round

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
