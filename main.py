3d76bdf9542a003db462f0ee2e5fbf9fa733fcca
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:33:20 +0000

    prove bored save whom official

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
