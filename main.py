548cbec68f05aa02912d64323cb992c454f217b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:36:21 +0000

    bring strict lookup each her

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def append_to_array(array, item):
+        array.append(item)
