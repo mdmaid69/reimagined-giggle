b9a740e1f104e4c3303ffbef238074022a380c90
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:01:39 +0000

    length hope beat even remember

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
