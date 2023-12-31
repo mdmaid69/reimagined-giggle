916205d85769ebf14d368d74a02748acf5daf79a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:24:56 +0000

    safe explain occur pain smart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+print(sys.version)
