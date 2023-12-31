cc6c2f21af1a52507ce27a67a7b682fe8ea70f11
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:33:52 +0000

    miss free murder left emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
