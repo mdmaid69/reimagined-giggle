a40893d34f996a8ec38fe2eade4921acbfb2f6bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:26:06 +0000

    smart only nose skin any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
