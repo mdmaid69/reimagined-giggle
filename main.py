73969f54eb5327508078fb9f4ef67aa98a247f5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:36:17 +0000

    vote joke enthusiastic order embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
