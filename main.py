fa504f15d3a2fdf1e535968cf1b703636793d98f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:00 +0000

    snow wash steady holdout one

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
