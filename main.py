0923486feef00a2a00977cc6c0a8170451f272a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:56:31 +0000

    measure thorough practise ball shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
