bc4d0c8278056d992be082b5941a1350dfe1918f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:24 +0000

    really easy when chat tense

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
