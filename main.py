2debc83ce1d63cd00f111b8935394591a4aed6d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:59:07 +0000

    century Gg culture detail change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import collections
+def create_counter():
+        return collections.Counter()
