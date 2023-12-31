426d30917ec9761dcebcd20632f47718eebc47d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:57:04 +0000

    so pool fold brown discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
