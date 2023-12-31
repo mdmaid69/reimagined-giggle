5edc7b514f14965ec532c559e23eae6800f3c49f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:33 +0000

    sorry bang simple office dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def divide_numbers(x, y):
-        return x / y
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
