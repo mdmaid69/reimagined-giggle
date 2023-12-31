31618bae84e9ff6f9974af05d2759450e2f3b522
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:46:42 +0000

    mood hang nature breakthrough mass

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
   import matplotlib.pyplot as plt
   def plot_graph(x, y):
         plt.plot(x, y)
         plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
