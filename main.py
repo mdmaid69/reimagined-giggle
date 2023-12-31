27eacfcd50a8d2b04d420c9584c1892a3df30911
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:58 +0000

    cross accidental quickly congratulate play

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_acceleration(speed, time):
+        return speed / time
