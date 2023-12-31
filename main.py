6a1402c9852f27ef099f899f45dcccfa2f3462c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:32:10 +0000

    between entrance along search kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
