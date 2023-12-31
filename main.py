5e752d1a39039896c1829741320075a5672974c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:43 +0000

    border positive sail neat replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_repr(array):
+        return repr(array)
