105e29ae2f2afde87e56681746c4194aaa9b29dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:39 +0000

    shape substance return sad pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
