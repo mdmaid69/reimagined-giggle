57dd5cbab5da733a71f9b1c3ab216215b3eb3d87
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:06:23 +0000

    avoid mainly hair fat straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def convert_to_hex(n):
+        return hex(n)
