6c7e14ea03ed40df860dd731e4084ed6a3ebc253
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:45 +0000

    classroom write wish lean takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
