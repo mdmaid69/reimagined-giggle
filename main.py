4884509fa9b4e2f9db56a73871798b3d977f1a54
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:17:30 +0000

    present fall slightly long object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
