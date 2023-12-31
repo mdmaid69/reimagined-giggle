176c77283424bb1bc764e08c991a96796551bbd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:41:10 +0000

    might afraid talk under confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
