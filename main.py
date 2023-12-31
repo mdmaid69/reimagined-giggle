3db828c00d7e292291ebdaad21b41503359edbd9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:33:30 +0000

    mend favour early hurt our

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_power(work, time):
+        return work / time
