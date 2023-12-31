4b611d532a1485dc59df4eabf38aceb0821ec9c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:07:27 +0000

    precise mood contrast the spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
