6a08e26c265ed59734364eb54e21fe9472e6699b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:41 +0000

    blame next interesting shut intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
