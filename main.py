8c36bf7afc97df1f5884dab0f144784287bbdc3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:32 +0000

    at any narrow 179bite approval

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
