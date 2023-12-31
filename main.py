4577833c1ecbbe4b9a2f60aa275c5874ffe41ed7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:03:00 +0000

    dinner pipe claim surface method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
