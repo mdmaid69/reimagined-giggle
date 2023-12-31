0ddb4abee1bbc4165176616a2c539e9cf3a1ad32
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:52 +0000

    tense miss giveout switch value

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
