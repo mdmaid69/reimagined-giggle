52b813347b07599770d574200820feb635e3a716
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:08 +0000

    grade crazy wise soil course

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
