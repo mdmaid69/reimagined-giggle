b025c904e8adb77b49f76417bc4cfbf825d997ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:15:43 +0000

    improve judge ocean glass concentration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
