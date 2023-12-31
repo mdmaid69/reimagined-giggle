2a6fa2d7f2804d192977377c6959b55fec9412ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:50 +0000

    tongue somebody careful frequent share

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
