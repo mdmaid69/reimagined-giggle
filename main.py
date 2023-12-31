2fe67292dfc054134a8f61b5beab3b3d1508c778
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:24 +0000

    turnback shame combination star practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
