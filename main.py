45570ab71a21ab625f797d06967ebb2d261a2ff5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:49 +0000

    both on kind limit right

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
