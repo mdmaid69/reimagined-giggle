6bd949f4ac1eb65498253f3c97d3486fe7db4945
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:44:57 +0000

    tend fall watch bringdown connected

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
