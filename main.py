4db75f54d3ff95824f866e7d15041cd17029827f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:38:32 +0000

    few bringforward true normal emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
