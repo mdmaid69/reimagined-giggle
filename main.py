4db407f002b1791d306629feebd588ed8814b003
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:04:17 +0000

    uncertain cheque excited spring shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
