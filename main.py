24d6a19c96ba3044d810f8c8ca4f090897f0db58
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:16:59 +0000

    bag provide appeal contents calloff

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def cube_number(x):
-        return x**3
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
