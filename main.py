d2296eec25cd6f4505d88675e04663824dc4680e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:03 +0000

    throughout birth picture adjective result

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
