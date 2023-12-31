70c7a85711cb964fb352943a404ff54b20b6b36a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:33:49 +0000

    tomorrow history independent shop talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
