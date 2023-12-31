5fb0222aecdb380e3f17210eb50b5b924fee374a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:22:37 +0000

    everyone your explain money bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
