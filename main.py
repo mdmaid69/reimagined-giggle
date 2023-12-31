ab0f515dde9f5335b96b27176aa61a5db9c842fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:28 +0000

    often guess by right discuss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
