a2d5df8f039e7c983ddbb88fa11c8fd09b119899
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:34:51 +0000

    healthy badly Ll alone turn

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
