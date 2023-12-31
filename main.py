52b700bd3905c1fa4c28a4d3a5e9f89ba19a5487
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:44 +0000

    talkoutof shout row hello of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
