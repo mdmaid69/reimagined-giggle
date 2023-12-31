ef0d760877cc08ee622625335d7773747d336961
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:25 +0000

    mark little write sound downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import sys
+def exit_program():
+        sys.exit()
