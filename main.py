54b4142dcfa31e7b35c72fa8acd7e491d20455df
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:01 +0000

    drink share hurry turndown average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
