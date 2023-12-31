976e6cb4b0ecdf753032795a33ca7ef075a3987d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:08 +0000

    undo mouth journey limit year

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+print(math.pi)
