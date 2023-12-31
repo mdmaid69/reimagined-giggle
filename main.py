1e80b0b2977daeac8d579e77713411745be7b42b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:12 +0000

    hers road both central break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def count_elements(lst):
+        return len(lst)
