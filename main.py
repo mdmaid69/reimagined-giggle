0eec73c3887d56bae85583c941235533fb8f3d1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:05:25 +0000

    separate season expression danger breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
