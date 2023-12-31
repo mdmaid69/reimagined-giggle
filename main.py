0ea6cdd82caaf6f2a4cee49ab21f57eb9a01343c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:54 +0000

    sail target last other car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
