36b0f8e7038f0e58e79d5e8bc151b4806df19260
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:49:45 +0000

    wall tool just cook interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def reverse_list(lst):
+        return lst[::-1]
