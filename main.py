cdeefaa290e51b76f0958141145bd07f150b394a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:20:18 +0000

    precise right tough still it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
