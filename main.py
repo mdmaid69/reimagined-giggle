7c43e43ceeef5f6cc0cd906c556967ab87c57b2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:35 +0000

    content play load supply table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import http.server
 def start_http_server(port):
         http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_area(radius):
+        return 3.14 * radius * radius
