efd4f5a189e988bf4ee61fba1c5b94e5bd0d9a09
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:41:35 +0000

    add Oo admit trip shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
