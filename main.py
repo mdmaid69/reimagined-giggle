e37430c8455a69613c39bab126892be79e678ada
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:11:17 +0000

    fashion emphasize hate interview duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
