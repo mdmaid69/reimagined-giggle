966840e99b98ef57d2e0671a5f9fc4f9a7f2730f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:28:56 +0000

    turnaway face thankyou busy separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
