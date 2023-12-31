bfc056101b16a4da777156d67948589628e93140
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:55:54 +0000

    become beginning dance represent plenty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-def get_current_time():
-        return time.ctime()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
