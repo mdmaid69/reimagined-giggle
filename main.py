d00950f2e5c17a67207908dd951bf2ce146a69cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:47 +0000

    family accommodation appeal walk morning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
