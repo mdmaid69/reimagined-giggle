13b5156eb70c8daee8f16c28fac9b21200f8658b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:16 +0000

    569enjoy traffic half time mass

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
