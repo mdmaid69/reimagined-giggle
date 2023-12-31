30759e52812565f055f0f71cf18b90504db1da2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:17:42 +0000

    union look mother stay ashamed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
