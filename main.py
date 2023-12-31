1e39c68d4d3a426d3252de6ffb958be07fc12a5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:18:30 +0000

    creature genuine cold many brush

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
