57aa82f8d3588fff972b2299ebfdacb6e5d44487
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:47:28 +0000

    stone behaviour off Bb touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
