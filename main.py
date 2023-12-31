40e39ec3aa51d61e5cab665988bee8d60e39dd47
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:31 +0000

    union shadow instruction little value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
