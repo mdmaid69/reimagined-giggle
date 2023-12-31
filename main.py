80608618be684b915f3644ea4220486d227c4728
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:10:37 +0000

    everything ours cope brain next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
