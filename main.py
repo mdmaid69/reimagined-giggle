99ef660cde9eabe81ce99c5083bafe40b92cea98
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:39 +0000

    wait leave common contain poor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
