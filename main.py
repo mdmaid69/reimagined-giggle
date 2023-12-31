7e1994227674d95e40b151d0de74e5c25488ac96
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:31 +0000

    harm succeed offence tongue oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def remove_directory(path):
+        os.rmdir(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
