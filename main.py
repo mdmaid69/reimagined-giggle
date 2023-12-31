aba3eb990800277109d10044f621f5c15529a4ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:11:27 +0000

    bread until day flat sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
