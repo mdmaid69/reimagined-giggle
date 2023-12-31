2b02c0b53df7441844c0aeb1bc1d06b5509087e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:03:15 +0000

    save half pain shut closely

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import numpy as np
-print(np.array([1, 2, 3]))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
