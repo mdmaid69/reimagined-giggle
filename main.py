33ce2c38ba30c41d8742e688db0a0a5afbc8a71a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:51:43 +0000

    correct real lend whisper as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
