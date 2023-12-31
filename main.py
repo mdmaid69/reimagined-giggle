02462a852227feacb0c9d3756bc3e044b82d49e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:48:23 +0000

    teach sea careless badly related

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
