69d502becfeddbdd29be04d3dd027993f0eb81ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:36:36 +0000

    model care broad past badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
