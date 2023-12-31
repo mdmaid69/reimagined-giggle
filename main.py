363e44be19029f51e6ab2251824574d5efc00d5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:50 +0000

    comeout couple available lookup lunch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
