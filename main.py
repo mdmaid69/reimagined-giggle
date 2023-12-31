48add4f82c8cd83e553847131b5931331f2ef462
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:44:29 +0000

    computer fun wash spoil star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
