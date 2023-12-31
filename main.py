7087cf2f35b7ebd2cb6805726c16dd8e552365da
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:47 +0000

    leg determined throw full difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
