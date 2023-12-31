f926eb0a16f9e4c45ae87b25a6477f52f1434345
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:21:45 +0000

    content air uniform situation open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
