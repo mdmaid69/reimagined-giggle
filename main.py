c048727ea9131bea32f2c6e3c0c14dcc94b9e3af
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:49 +0000

    top name generous feel definite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
