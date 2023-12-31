4d23c510ede877863f603dfb8a366fc630240cb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:56 +0000

    listenfor immediately weak warning too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
