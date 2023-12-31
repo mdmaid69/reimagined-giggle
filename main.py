8ee4ba57c7a1806fe65b19931a5db5ce5a191575
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:08 +0000

    wrong Kk keepfrom type measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
