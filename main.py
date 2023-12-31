5cb13581b4a3137c51a9abca2beff3024488e190
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:14:20 +0000

    slowly arrive buy how beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def add_to_python_path(path):
+        sys.path.append(path)
