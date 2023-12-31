c513eb8f32d41f12645c97d412cf50537d5ebc9a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:49:45 +0000

    muscle top complicated raw factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+print(time.time())
