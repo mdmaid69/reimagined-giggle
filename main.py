1556f63ca48224672e9021ad1c4d9390d360e9f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:06:55 +0000

    weak complete fortunately into thin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
