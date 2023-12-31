6a215b204f2170d1fb9a69e5305f5f8c297ce161
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:13 +0000

    mass wherever school serve plan

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
