6c913e4051658e00785419693e783a668a2c6956
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:13 +0000

    preparation recent trust embarrassing criticize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_binary(n):
+        return bin(n)
