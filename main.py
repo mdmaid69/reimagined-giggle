c8b0af12c8cd7cc507a93f96be7801c984aa781d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:32:01 +0000

    accidentally without entry right drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import re
+print(re.match("h.*o", "hello world"))
