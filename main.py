83583b8918ae30c8593d21f6b897148bc4675f20
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:20 +0000

    whistle ready apologizeapologise post list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
