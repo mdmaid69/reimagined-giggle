67aea5bfb594efeea5b98916e063ee5ed69251ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:00 +0000

    book serious punish wave entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
