cfeb06af5d2bf5a35ed0925a14512a15bdd5b963
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:18:45 +0000

    sometimes raw decision progress unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
