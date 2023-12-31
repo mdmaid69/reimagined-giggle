04169e3e3a672e7739ba6f735a0c7c2708d37512
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:01 +0000

    whenever why legal bury fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
