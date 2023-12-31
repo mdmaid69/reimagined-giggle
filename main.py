35068661aa2959cd2f14ab7f8409c88f902c44d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:22:17 +0000

    paper dare major Qq suffer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
