b1d836eea5650ce5151fc01fd09d15468ee0d4fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:22 +0000

    cross brush march search spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
