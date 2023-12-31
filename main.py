3ec47943bd3f4e3c1c31a643a9da6a0702604d6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:58 +0000

    drawing friend foreign later suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
