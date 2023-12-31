1ad5140e55b84fea41515124a5e310a2b686c315
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:10:58 +0000

    second schedule stop protection certain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
