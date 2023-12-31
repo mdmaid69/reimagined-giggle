71a888d0ddbca2005b16e1252f52717bd014c66e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:55 +0000

    Tt leave comeback swim habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+print(sum(range(10)))
