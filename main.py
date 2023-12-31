40d3b4df86d64c9bb13fb4c24361617c0e5e1fca
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:51 +0000

    peaceful contact change miss nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
