84515618a0d3cca9fdcda18d1b2571b6cfe64c96
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:59 +0000

    ear city hand never wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
