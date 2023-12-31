b261d89c8035c92c4a49a7041ca0de68dd9f8123
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:26:24 +0000

    frighten organize attach lookaround reserve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
