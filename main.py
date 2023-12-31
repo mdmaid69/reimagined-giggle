56335ebfe61b718cf39faf45d54de15eed9f45d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:56:25 +0000

    classroom letter means maintain must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
