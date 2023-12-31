67b054b05475b3517ed38809a088f284ab0e7785
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:45:42 +0000

    justice tense nearly pronunciation noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def count_characters(sentence):
+        return len(sentence)
