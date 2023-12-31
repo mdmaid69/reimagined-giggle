39424c3c412e9f74d903dc8d009e8750ea819e6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:46:21 +0000

    eager week tape hide cover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def cube_number(x):
+        return x**3
