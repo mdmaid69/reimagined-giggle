6e0e36a142d5b2fc772f619c5561f7f54b066dc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:44 +0000

    divide content hope bit crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
