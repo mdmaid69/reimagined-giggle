08f56f7365a9b7f2a9cfaae64f2d33533e04b3a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:02 +0000

    shock ago join reduce pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
