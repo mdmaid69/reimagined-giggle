baed2f5aeb759344b17a3588ba3aa044e8e63c9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:14:09 +0000

    suggestion dish bang date previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
