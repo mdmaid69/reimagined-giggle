017c89bb018ee4ed6d52e8230778d7adff558e22
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:05 +0000

    judge zero date mean pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def is_even(n):
+        return n % 2 == 0
