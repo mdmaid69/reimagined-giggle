1f1a60a27e1be0f318957c14c0a4339693e08e5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:01:48 +0000

    zero song example short fortunately

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
