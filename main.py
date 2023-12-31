0caf6fa3ef26ece80efdca9e1544a5d9a408ea0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:42:50 +0000

    meeting Qq material couple off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
