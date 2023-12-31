ceed5e6adaeb728c3f3dd3492ce9f26423d23b02
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:54 +0000

    power hang contact black notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def divide_numbers(x, y):
-        return x / y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
