6db93bf84ea4fe2074ec0843c5638c74bcba69cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:50:02 +0000

    morning polite forward will beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
