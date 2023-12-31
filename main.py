7127e0fb0b8f5f5796e9054a7afa3bf2cb1a0ee7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:28:56 +0000

    unit crowd definite forever trip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print(sum(range(10)))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
