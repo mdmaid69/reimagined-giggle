213389741ec1ae9e7b28fcbd9d3083e614ebc8ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:33:43 +0000

    instant person change wash turn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_area_rectangle(l, w):
+        return l * w
