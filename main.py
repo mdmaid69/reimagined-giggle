a9d374651df1833a52084d7b9ff7a924d2fab9c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:38:14 +0000

    sting along seriously scale risk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
