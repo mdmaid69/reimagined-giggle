9ff8a052d1b9929254873d066549a5b4de2ebc21
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:07 +0000

    OKorokay wake keepup marry instruction

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
