ce0535f38f42ef05e67d8431f4334d4cdaf72a67
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:07 +0000

    spoil wheel deep getup surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
