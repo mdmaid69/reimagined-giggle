79421f968e6674b413d9d70ad91299b1898a816f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:07 +0000

    often apologizeapologise cheat spend average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
