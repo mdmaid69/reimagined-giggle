da85b528e7632ef637280f9f49a1b167c25f5182
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:30 +0000

    anything apparently already hit treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
