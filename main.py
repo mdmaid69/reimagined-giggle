a572deb8ebcff8e124a61bbd5fc49b35c793ded0
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:40:14 +0000

    dick floor possible crazy accommodation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
