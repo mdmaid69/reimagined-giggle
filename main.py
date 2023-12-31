5df9115de21962f878a49215ba18945eddc99bd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:54:56 +0000

    conduct change turn perform across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
