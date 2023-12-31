1951f30e21acf738672b4bb2c245e4e87c6eeb17
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:48 +0000

    line name explain follow law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
