d5f35c40f6267a3c588b1d24eab757d0315ae3d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:12:42 +0000

    edge the bring tape exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
