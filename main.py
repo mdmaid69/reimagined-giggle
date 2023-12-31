83a4dca68b7ab741b30d605a57f37aecfbe84e56
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:14:11 +0000

    too our sentence light minority

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
