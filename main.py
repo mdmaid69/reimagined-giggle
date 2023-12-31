8399c8593d7e5b99c2b99f726540b3173a41dd04
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:49 +0000

    drink behind indeed with its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
