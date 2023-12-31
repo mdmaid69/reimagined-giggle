f7139a795bcb75b5a9a49bfb8b3dd0a86cc8a620
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:36 +0000

    shine attention concentration promise example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_list(array):
+        return array.tolist()
