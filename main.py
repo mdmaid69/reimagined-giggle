f1ab2c8b5cf4631202a9438b923be4d6dafc8383
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:14:40 +0000

    as tidy damage takeback 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def square_number(x):
+        return x**2
