1d9a75f99d95ec2a74ec1c9b2e4a4ab192f76375
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:58 +0000

    unhappy partner giveup shout risk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
