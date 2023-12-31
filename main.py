baab8c94eb493bbf3c78cf49f15456190c16d28f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:16:43 +0000

    section distance general vote partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
