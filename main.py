c7f6aab066191c6ecb2b866c4a925592ca1e2dd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:15 +0000

    pretty ahead combination exact window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
