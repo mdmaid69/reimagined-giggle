b3d3b250718e1fb91b816d883f221b381892ab47
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:01:32 +0000

    supply conversation you more punishment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
