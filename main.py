a969321d9c8d467391b8a98bcf1fb688286bd5ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:42 +0000

    grow noone workout later independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
