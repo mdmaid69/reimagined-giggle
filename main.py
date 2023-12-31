eb77abbff5819592ce42d6431ec085d1a9c0f600
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:31:11 +0000

    her experiment can thanks raw

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
 def calculate_bessel_function_of_second_kind(n, x):
         return math.yn(n, x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
