54038a6151ce5d6e79ba4048b877f6d3892d0cfc
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:47 +0000

    spend hold here nothing interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
