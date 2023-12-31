d2c258b639f724b6ff9497989432ffd0acef596d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:11:37 +0000

    bite attractive subject dull cold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
