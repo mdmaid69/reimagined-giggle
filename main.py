58bce529b35cf8ccd7fd03dbae4391d43203166e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:20 +0000

    coat noone burn reply react

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+for i in range(5):
+        print(i)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
