a2cab4cd606ea85f672f077fecfdc2cbe0a38fb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:50 +0000

    hardly blame interest target of

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
