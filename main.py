e7e71025e96bc34ebea05980d1d087f9d96cccdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:56 +0000

    somebody perhaps reduction bang second

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
