e17d1ba6ef0a3927a580aefd27c0193a42f7d36b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:45 +0000

    sore seem curious classroom arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
