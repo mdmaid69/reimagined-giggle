7c996498f64204cd8d1dfdca4182b2263f99ceaa
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:32:28 +0000

    tone speak fit bush ever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
