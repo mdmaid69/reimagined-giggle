993ead8f839172dd4ba75814acac2f469f1fcbf6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:10:29 +0000

    dress match although idea repair

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
