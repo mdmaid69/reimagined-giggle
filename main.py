b80c75340d2e1b815552de9ca5b74ce9d80a7f50
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:30 +0000

    onto dinner comfort black personal

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
