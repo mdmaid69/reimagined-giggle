6008737dfad0eca22c631ad1829c4c1da2bc0aad
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:22:55 +0000

    bar old travel soft lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
