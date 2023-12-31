5911ca7eb89fb737ee8c83e3b5dbb3c5de74ac91
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:33 +0000

    advanced tap number light reasonable

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_odd(n):
+        return n % 2 != 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
