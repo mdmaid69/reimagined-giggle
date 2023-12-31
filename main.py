5e22cfafc88dff209856e6e6b13f97298ef081a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:24 +0000

    there suit active foreign OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def is_even(n):
-        return n % 2 == 0
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
