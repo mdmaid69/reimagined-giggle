2bbaa7a36fa58ae40074e4f914fd73bf90674cb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:03 +0000

    tear disapprove Bb accident guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
