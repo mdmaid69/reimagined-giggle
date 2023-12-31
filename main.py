93b2f3e12db962ce5376e99b4138ae8dc56ce8de
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:56:41 +0000

    print control patient as crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def convert_to_hex(n):
+        return hex(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
