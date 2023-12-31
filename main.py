daf3969d3a84fe29ec49edc452bc5524e45f29d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:16:41 +0000

    Bb workon justice however married

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
