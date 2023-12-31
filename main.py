04671a9153b209f86e600c91e1103d2c7d54b386
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:59:10 +0000

    maximum tragedy nervous explanation easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
