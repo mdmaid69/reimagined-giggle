0be6dedbf35fb688cde0eaad93a597c60279bde6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:50 +0000

    injury positive father human fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
