acbdbbd3ccb4d822729584ea06b8bba7961767ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:05:53 +0000

    reserve within typical hope deserve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
