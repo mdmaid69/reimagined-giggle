7852bc248cf2b02bf5977c6034bc43a6b3a9d415
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:20:48 +0000

    correct free fact proud public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_as_bytes(array):
+        return bytes(array)
