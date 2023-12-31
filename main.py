f39b70c1b37951c0073b7f9b5cae1efae2cce252
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:16 +0000

    task eat belief month meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
