6187b5f0ca9ee8b578b28a7a2f64f2af27854007
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:52 +0000

    opposite below seem anywayanyhow dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
