c71b7a933383fada0b6c867ec284f7f9a1f5c291
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:56:29 +0000

    file key foot low calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
