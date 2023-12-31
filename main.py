757e76c2a8182f037dc9337e3ef3ffae1fbe4a16
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:11:02 +0000

    press keen naturally rely flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_area_rectangle(l, w):
-        return l * w
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
