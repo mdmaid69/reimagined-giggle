df39e49529dbaa4f5022805df6f1e9a2dfde0126
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:52:36 +0000

    write female advice clear recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
