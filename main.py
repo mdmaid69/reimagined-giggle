7d0edb91c7a027316edeb8d93537de6203d56112
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:52:11 +0000

    put full laugh heart find

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-name = "Python"
-print("Hello,", name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
