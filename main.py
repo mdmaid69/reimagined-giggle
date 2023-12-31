f105e4b32bc247d80b7dd5895dc183518dd562a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:08 +0000

    exciting excuse sudden makeinto why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def is_odd(n):
+        return n % 2 != 0
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
