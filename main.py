e59930c57ef8bfed58eef77801899dd7fea201a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:18:50 +0000

    Ww satisfied breakin test burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
