5349c1f94f5ae30c02afad64da2794171e70f51d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:01:38 +0000

    other message giveback slice mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
