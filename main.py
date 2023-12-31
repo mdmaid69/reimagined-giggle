f1fa319c1230f0f09a32fb4dee10f607a98b7662
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:10 +0000

    lot discovery period instrument direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
