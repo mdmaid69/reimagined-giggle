6efcdedcc7f5182ac26e9f1bbf9028511d0e3ae7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:56 +0000

    sit press achievement reliable reaction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
