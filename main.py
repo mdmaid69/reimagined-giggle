6addbe71f925b428af5dd870a16189fc2f52c634
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:14 +0000

    degree unpleasant doctor necessary straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
