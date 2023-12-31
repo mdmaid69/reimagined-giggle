86db4773e007d4de4da42e0b3f1c9258afd1f6d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:27 +0000

    steam border attempt drink sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
