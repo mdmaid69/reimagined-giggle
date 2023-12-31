10aaa00dd25ace16bdf1c61882785d9a408b92c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:12 +0000

    war active bottom board violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_list(array):
+        return list(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
