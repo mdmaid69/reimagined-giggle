2adda939efed5abd2c14f5542483e256e0999377
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:09 +0000

    minor party uncertain bring nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
