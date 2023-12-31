e70ecd0b135f80fc4f6cae528d07aff18c6f98d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:19:34 +0000

    battery card payment development runinto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
