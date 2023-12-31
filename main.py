bf9f8cd93d6d23883133bf77b243f09e60505ece
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:05:54 +0000

    least take cause ocean certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
