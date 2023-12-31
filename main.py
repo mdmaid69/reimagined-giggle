e5db27b0d3c643de33c8f6ccbb0b2ed3daec30c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:24 +0000

    meet excuse bridge goodbye share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
