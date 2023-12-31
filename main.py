61273a9e0679ee3988658a5131b0ca0443ca8636
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:12 +0000

    exactly base lose receive scale

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
