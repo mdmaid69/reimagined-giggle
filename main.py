dee63e052ec4202260a6e4d054a3c5ec3a767772
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:37:27 +0000

    whom content central maintain miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
