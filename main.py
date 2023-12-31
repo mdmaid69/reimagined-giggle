2384149eb8f6b68da83792aced1ed60c7713c003
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:12:06 +0000

    rush typical lookup many bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
