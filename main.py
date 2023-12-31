60a540f2944d925db558bfad761c3f7d3f2dafa1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:57 +0000

    fetch demonstrate stretch temperature full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
