8b623ae773a8ab2e0c16749160b71f0b52c66bab
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:04 +0000

    force slight convincing sleep safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
