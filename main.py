7516b9a3ca1ce4dfd5d7e2b45fec9f0c6fcfaf68
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:39 +0000

    beginning surprise settle human early

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
