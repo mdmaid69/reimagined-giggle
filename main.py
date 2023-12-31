003154344ebfed330c58421eae7d44618c4f55ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:22:06 +0000

    relation discuss forward rate play

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def multiply_numbers(x, y):
+        return x * y
