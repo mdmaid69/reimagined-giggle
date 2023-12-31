128d05521e51720cb99a69d8247c3774d758955f
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:00:30 +0000

    action personal solve giveup wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
