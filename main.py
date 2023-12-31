1241c09b96c6e884963dbf8c8fcdc520fca7529e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:46 +0000

    front common hole quarter ball

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
