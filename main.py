77d98f94711a0b3996bb10b37843ef0810600ca2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:14:46 +0000

    here already image window behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def cube_number(x):
+        return x**3
