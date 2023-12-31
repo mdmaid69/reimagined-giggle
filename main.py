3a709bc9ccf20cf154211edb46ad17a0f43e12d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:52:58 +0000

    exhibition excuse cut catch profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
