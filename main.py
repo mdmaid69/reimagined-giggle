a429ee50e3a6c65b27fff6cb1756973fa4879bbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:52:23 +0000

    like ability bored getaway swim

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
