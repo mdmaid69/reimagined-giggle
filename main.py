91f98095e0e5fdfdf33ca366a3a1264ab681fff4
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:28:41 +0000

    shelter exchange patience phone scale

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
