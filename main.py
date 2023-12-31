bfc6709ba3c50ceaa082c6a88fd381be9dc71e86
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:28:15 +0000

    apparently spring keepfrom slowly tough

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
