abfa68257ca69b3563016ed0f71695d86802a8c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:21:32 +0000

    fill tidy left rub giveup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
