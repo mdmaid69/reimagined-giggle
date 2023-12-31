83f6ce4925ee88e4303bf42a12a375b7ad3f3dd4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:00 +0000

    mistake method bye past false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
