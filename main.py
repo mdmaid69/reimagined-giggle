28527b632e82bac354051cb8a7fdd8c914907754
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:10:12 +0000

    contrast perform maximum praise unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
