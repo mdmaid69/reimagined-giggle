706af2694920847bce214246ec6d42e6c9d96764
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:48:08 +0000

    block trick tread beginning contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
