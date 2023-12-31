41e5c62059c393c8b5b4f967bb164d56e4059ed2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:12:06 +0000

    stiff fasten cinema pipe score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
