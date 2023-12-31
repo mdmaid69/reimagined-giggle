1fd6083ecad7e3e5a8acb80152d9c00a580b8747
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:22:38 +0000

    will subject easy connected everyone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_power(work, time):
+        return work / time
