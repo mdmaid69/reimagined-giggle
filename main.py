aeeebc27a5076179b4dfc25cab1c2c9f81deafbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:27:35 +0000

    dinner Qq wild division whom

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
