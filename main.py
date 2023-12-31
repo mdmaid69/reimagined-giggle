90185f37faa36b2e8955d41e7a994978a1136e85
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:12:52 +0000

    store scream instrument invite search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
