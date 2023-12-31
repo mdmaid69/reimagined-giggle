af452ee5030d010a57d775ce2f5ccd39da471889
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:35 +0000

    regret available argue factory contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
