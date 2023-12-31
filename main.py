6845c48fc1003ebc1c74c240d8f5e9f182ee92ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:28:09 +0000

    thinkthrough car pain kind keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
