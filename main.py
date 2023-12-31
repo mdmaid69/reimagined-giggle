202921cb361cbb7eba7726ee88f2d26230307ae0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:41 +0000

    back thin old move load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
