3d93e111b1318834aa9e589eebc51ae8639a55e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:19 +0000

    maintain breathe all write young

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def add_numbers(x, y):
+        return x + y
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
