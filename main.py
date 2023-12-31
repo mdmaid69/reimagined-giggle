33b92a18b9379c157074cf98775616311e9695dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:04 +0000

    occasional concerned positive remember bury

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
