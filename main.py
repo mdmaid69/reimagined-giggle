17b5ec96f2f4db714c543b09ab9b3ee50556273c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:23:21 +0000

    earn dish principle we party

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
