2156d52b121981de02f005c873e76a17af12d77a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:36:33 +0000

    getaway badly every theirs flow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
