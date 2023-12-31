395a696860355099b76b6160d86fa2692bd0f5f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:22:27 +0000

    they speak address union authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+import os
+print(os.getcwd())
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
