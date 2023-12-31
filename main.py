cbe63decb8676771f19a6e7b112e3b2b2e46025a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:12:04 +0000

    high lesson smooth pick to

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-import time
-print(time.time())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
