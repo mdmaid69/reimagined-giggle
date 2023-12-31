2c4361866f94d430e27a60f2002190692dc37bd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:39:52 +0000

    injury payment shape low realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_str(array):
+        return str(array)
