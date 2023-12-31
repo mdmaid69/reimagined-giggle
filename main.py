166fc1ef861ceeaff7ec566c0d1a76779563d20a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:51:27 +0000

    poison makeup challenge drink no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
