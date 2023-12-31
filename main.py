2fb7b9b0d6f76ffc754d7b79d4348ca1a8b801d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:13 +0000

    speak tooth narrow reserve protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
