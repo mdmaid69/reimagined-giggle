4c039713ab9c5c1a5f3e13e163cc5c29a973f9ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:49:35 +0000

    properly frightened violent successful judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_pressure(force, area):
-        return force / area
+import numpy as np
+print(np.array([1, 2, 3]))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
