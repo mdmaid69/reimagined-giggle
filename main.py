90a43cbeb820e1cbaf9c031c997a2cc2d8726922
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:00 +0000

    run weather pipe to false

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
