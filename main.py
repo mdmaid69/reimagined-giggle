f7a3f1841ef72582eca2da43159f891b712ff048
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:42:22 +0000

    fear worse doubt plate ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
