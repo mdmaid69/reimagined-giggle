407197ce7441bc1157cff76535e861caa014279d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:57:35 +0000

    artificial free problem compete unless

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_repr(array):
+        return repr(array)
