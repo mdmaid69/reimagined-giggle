d7bdfa97edf30ee45b95b2e02bde04bf470bf736
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:54 +0000

    under you trap clothes telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
