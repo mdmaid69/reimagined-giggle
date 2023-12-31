0b1313afae3bac4d07c0faeb2b22b4695d9a121a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:51:05 +0000

    wise convince choice special keen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
