9371981e5b39cb4023b035105af10135dedda6f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:55:03 +0000

    emphasize connected thirsty pencil performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
