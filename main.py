c8aa76b64fd98bb441d07279acfe8af4204f3cd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:55 +0000

    control prefer either relative unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import heapq
 def get_smallest_elements(iterable, n):
         return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
