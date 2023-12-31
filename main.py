38f8e74b11bd54aa20217e41ea423a4824506d8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:33:09 +0000

    beneath move hurry himself welcome

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
