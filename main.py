84be3c3325fb117a4fa9e42b1514e76e105e0471
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:24:53 +0000

    pressure defeat silent Hh long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
