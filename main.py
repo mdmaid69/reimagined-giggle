2685060aaecdbafb0cb1fae0af18202f5ba492b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:11:00 +0000

    remain light laugh target period

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
