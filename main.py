78b8f2f739bba45091f1b82f92b471124f474a67
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:42:27 +0000

    however pool detail boat question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
