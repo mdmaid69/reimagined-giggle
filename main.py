c241a6de407f7920d82b449c4ab2d9ff78a6a92b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:43:44 +0000

    callup butter task there equal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
