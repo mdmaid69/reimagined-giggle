e1ca08073ef3d75c749f485ebbe78a78de341cc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:38:44 +0000

    fun boil empty some stiff

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
