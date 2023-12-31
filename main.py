42437988f52274d5eee9f924e22782955249af24
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:05:04 +0000

    afraid morning pattern pretty coffee

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
