22ad369a1df12eec1fcf9201b5b4e053ce4ef3ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:34 +0000

    pollution environment rough gradually wall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+from collections import Counter
+print(Counter("hello world"))
