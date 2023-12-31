2706e813673e0a31dc150722888543a54d1ff71c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:52:27 +0000

    open file themselves any worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
