ecb129cd620323d8bfa5d8a636a1bd8da1b7d593
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:23:29 +0000

    none float attempt after secret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
