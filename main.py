e85d9a12ae7f5e6f7c0db0e0f56126ee8fd8c851
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:38 +0000

    runaway free broken wire stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
