b914f3af9159668c248aedc04423b4fd4400a4e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:47:25 +0000

    interfere compare possible recommend Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
