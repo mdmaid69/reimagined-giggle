54236694eedc08952918a69249b35983de21cc3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:10 +0000

    birthday spoil form head Yy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
