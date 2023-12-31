9a5219acc860fd3439f29d709da26c1028a08bef
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:07:06 +0000

    immediate make onto niggaacademy roll

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
