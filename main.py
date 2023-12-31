c5ae517a437294798d588db227fb56b78b7a794c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:22 +0000

    wheel friend matter study so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
