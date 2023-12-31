f62a8ac884c6e294071ea2479aa73784ce7170c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:58 +0000

    justice fear even warm broken

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
