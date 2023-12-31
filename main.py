395488186e41648c58147fe0099552e45d14208a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:03 +0000

    attract search before direct soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
+  def calculate_area_rectangle(l, w):
+        return l * w
