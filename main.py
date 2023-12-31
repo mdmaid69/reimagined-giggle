0377f6ad2ef7a3591e9083292c7fcc82e5776bfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:41:20 +0000

    fight thankyou brief goodbye busy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
