ff70c22a6e734a99d311fca2cdae80157338ed70
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:28 +0000

    surface write 572enter area illness

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
