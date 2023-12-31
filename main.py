a695f28e39da43742846b958425308d9dd47bb40
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:14 +0000

    sting strength actually expensive hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
