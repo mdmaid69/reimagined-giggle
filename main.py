7deba75e61726ed918c2b205766bb8912b5b3e73
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:13:03 +0000

    track doubt comeoff Bb chief

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_area_rectangle(l, w):
+        return l * w
