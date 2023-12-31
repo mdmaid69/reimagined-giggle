8ddb725d9d7bfad0fd7b81cda47fe92707363b5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:37:25 +0000

    tie search foreign it mind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
