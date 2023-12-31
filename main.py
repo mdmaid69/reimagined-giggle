024608778adc26b35fed673625d9abfff0dd176b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:59 +0000

    everyone search before temper telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def count_words(sentence):
+        return len(sentence.split())
