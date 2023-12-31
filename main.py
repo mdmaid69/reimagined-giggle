2ef7f902ec0e96a2b112461f3b9f22132e50b915
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:16 +0000

    attach treat rescue mess temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
