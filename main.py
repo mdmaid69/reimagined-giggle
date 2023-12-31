034c93359292bfe3848eee3b3101ff115dca4516
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:22 +0000

    fact gradually introduction like seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
