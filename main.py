cd222daaf55ebd95fddfbec12a64ef6562009642
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:30 +0000

    answer recognize pattern here settle

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def square_number(x):
-        return x**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
