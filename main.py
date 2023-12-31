150f52e52977f37fa69485aae058167a18d33d2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:11 +0000

    cry peaceful defeat ago walk

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
