7def87c41241c72337110e3eb5910bf165fe3452
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:14 +0000

    567enemy relax uniform pay check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
