debd08cf6744696ab694c8b9bdf344417a79b285
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:51 +0000

    score approve blame weather familiar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import random
-print(random.randint(0, 100))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
