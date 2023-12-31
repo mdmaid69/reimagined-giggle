686bea8cf018fd048d1af622a0087fb35c1403ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:55:45 +0000

    behind lie suckme skin mention

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
