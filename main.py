10502bfff4539116a089545cba50c4eaa15bca5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:19:42 +0000

    part radio bad slight recognize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
