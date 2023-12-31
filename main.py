6c536d1b247c12ee237125688e62c1e5c961e6bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:34 +0000

    forget far runaway thin corner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
