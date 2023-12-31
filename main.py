6d5ea5b918335227af7b1f23ef75d47cab9d621e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:11:53 +0000

    crash breakin target damage anywhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
