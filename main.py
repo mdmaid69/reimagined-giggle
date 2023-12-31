9c68461aa8c6f832cbacd8525c5401780524cf32
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:28:13 +0000

    among disappointed odd shop brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
