f7f374ddb739a7fae941ccfff0f3d3b970c47772
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:35:08 +0000

    wander bad bit roof regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
