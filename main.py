442e7eeaa264283c2ebcb5d560c7e462939349eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:10:08 +0000

    goout hard natural squeeze eye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_pressure(force, area):
+        return force / area
