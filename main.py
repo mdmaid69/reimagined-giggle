3b8770b251a8fa3de7722fe483628c678c84d6ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:19 +0000

    mark takeback effort leave dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
