58abfda03e34ac059e11b52ead74ac7f5cd5c16d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:58 +0000

    eye unfortunate foreign write fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
