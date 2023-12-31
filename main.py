76d3a82029dbabca6a3a5253f08d5ad4ac8fbe63
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:49 +0000

    crime string proper teach reduction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def divide_numbers(x, y):
-        return x / y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
