47a888f41fc1f31571d4803e8ffa29eb5829f7db
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:05 +0000

    slip rare goback(to) favourite thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
