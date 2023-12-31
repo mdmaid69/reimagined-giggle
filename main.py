e99ed78206f3f0bacfa2544521748139341c0e31
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:29:25 +0000

    your house shout march cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
