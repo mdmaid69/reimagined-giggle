a2021a74f7c4fffcbca88bf932d651a1e560b5d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:32 +0000

    stamp organization fear generous description

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
