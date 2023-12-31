0b0a21a746c02b0731bc4efb6cecc895ac7e9598
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:00:34 +0000

    steer off nice need soil

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
