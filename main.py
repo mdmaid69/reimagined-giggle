d50ca050133c7fe8b91a196b29b3b32d980ac87c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:57 +0000

    equal angry art shout future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
