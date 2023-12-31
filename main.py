0f3b73e84a2faad571c643b682edfb648a96c027
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:01 +0000

    reach squash do nigga week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
