1665a3061afd39a6914aec1eaaf38adb459e9b91
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:52 +0000

    last long weekend meeting naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
