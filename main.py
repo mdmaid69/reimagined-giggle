7482a853cec109af525251fc9e589d2cf2a379f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:37:31 +0000

    decision advantage comedown help ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
