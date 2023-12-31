012ae3e7415108b030b511ac53cee1f5989b1f62
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:31 +0000

    hurt glad circumstance act generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
