95c462d39591094c3d406e1e0d3f505e7413399e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:09:24 +0000

    wave pile she plenty sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
