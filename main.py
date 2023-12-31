7d17b75e0e9fe38aef945ce451a19a6e0f70361d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:02:12 +0000

    friendly generally boring path occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
