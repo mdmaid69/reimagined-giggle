a797ad1d954bf62c178045f7c0bafc78b3b67b8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:38:24 +0000

    father full seat freeze listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
