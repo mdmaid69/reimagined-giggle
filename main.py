bff1a9d93ea2b5b49b51b7d98a13f6ab68bc010b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:09 +0000

    boat bite object performance Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
