534182a506950f82a1b7020b5f2f5ad99086f97b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:23:19 +0000

    sensible bringback trial hand practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
