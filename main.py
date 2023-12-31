c7549bff630e5b2039f40550bfcc47fa9cf0c87d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:05:17 +0000

    advantage form possibly murder spite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_palindrome(s):
+        return s == s[::-1]
