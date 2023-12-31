4d4c993bc9df240d471f9a3ebf2111bac6b87c89
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:29 +0000

    deliberate respect search wish story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
