bcedef9b7959fb092361ba52fdc1c1fe98d790fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:39:28 +0000

    call wrong scream lookaround too

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def is_odd(n):
-        return n % 2 != 0
+def reverse_string(s):
+        return s[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
