6937122d0781df96b091292bdd0cd886e682b753
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:22:34 +0000

    swallow only complicated influence pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+print("Hello, world!")
