8e400c5a46bee6b800bc24b4df6a8643265abd6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:42:43 +0000

    measure home heavy cost pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_speed(distance, time):
+        return distance / time
