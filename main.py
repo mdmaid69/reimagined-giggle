4f0ed99007c921fb8cfc797d01ef0e000861ba9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:12:38 +0000

    successful private reliable afterwards frame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
