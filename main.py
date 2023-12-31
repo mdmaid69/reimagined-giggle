761c32737a6950d0bdb868bae3ed873d882ef117
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:47 +0000

    squash fast trap human fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
