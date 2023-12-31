1344c97a351ecb77cbc8fa67d1f1d0876a865439
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:32:56 +0000

    cry breakthrough rich operation base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
