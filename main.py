172e2c1a3aeae179bb4f9b4aa771a1d07205300c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:24:18 +0000

    future callback shoot straight either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
