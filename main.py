2c1bee322523e94645c635739c58008c07bf9bcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:05 +0000

    politics spoil obtain joke sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
