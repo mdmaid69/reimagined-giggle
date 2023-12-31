c1400b402011c791a9d4ade7144ed0c84ab296b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:22:48 +0000

    yes closely division throughout rely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_pressure(force, area):
+        return force / area
