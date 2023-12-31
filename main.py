24227fcea89343033d4d226b18d85184df230278
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:36:09 +0000

    Ii illness contribute hide grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
