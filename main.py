08f622abb4f026f170dc771365b68936e6335fc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:38:57 +0000

    embarrassed honest name better march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
