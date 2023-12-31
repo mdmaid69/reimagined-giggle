c3d8aa0801a22f42f41835596e9fad3710f0360f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:02 +0000

    pleasant square goodbye broad teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
