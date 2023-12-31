d9f9514ff38463ca29a3a2a0c4eed6ecd1abf42b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:35 +0000

    rush drop afraid guard form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
