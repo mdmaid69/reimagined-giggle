22e51d0545beb96fc618d368cbf874b34dc3f2b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:28 +0000

    normal insist surprising switch sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
