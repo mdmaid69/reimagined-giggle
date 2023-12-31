b4de5a7b6677f4b5f44c4c775f67636a20464fc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:19 +0000

    juice brave invite rather waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
