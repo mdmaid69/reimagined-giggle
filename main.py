e8b6d12a9e3a553d8293214f6d106a9976eb327d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:10 +0000

    coat result fill pure computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
