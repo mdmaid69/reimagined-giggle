599f3d2c20c16e98f95aa4dcb903109dcfba5195
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:05:19 +0000

    573entertainment upstairs wide make among

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def count_elements(lst):
+        return len(lst)
