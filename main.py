cc6330770ccbbedecc83efa173d2d7de7a711027
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:09:49 +0000

    respect feature mark swimming remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_queue():
+        return collections.deque()
