8f5df31cbce9113be0aa3e504a699b746b3f231f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:11 +0000

    style hope interfere journey which

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
