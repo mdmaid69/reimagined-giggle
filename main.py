33e24c4d5384048d3897541615f6b10310129145
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:10 +0000

    speak raise judge appearance goup

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
