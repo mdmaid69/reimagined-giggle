d7ec95cea2eb586c9ab8bc74c68952de83979246
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:57:40 +0000

    mend breakup colour cinema brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
