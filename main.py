8e1a510b3e17868af041f6e280238eaac53499dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:30 +0000

    bet explain whoever practise that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def count_elements(lst):
+        return len(lst)
