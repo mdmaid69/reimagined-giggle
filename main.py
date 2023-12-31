08d61c95c5e4e0b0ce209cd1e92b44956fc6730d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:27:54 +0000

    conclusion rise break bring because

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def cube_number(x):
+        return x**3
