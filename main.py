0a783b4dfc3b908c3e4e272672ea13e9c9483aa8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:08 +0000

    gate entrance him bottom where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
