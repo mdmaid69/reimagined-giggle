ca06d43c899afe4e0b9696e0f6206db522623dc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:11:05 +0000

    thirsty regularly spoil replace preparation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
