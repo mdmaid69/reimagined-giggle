613da604eb6b991b0ae28fe662d0c992c3d93173
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:07 +0000

    shoulder create gentle relation shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def remove_duplicates(lst):
+        return list(set(lst))
