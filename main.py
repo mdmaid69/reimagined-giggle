ed8e857dca71cb7d8c904e4c725d953d4396ad30
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:32:05 +0000

    emphasize violence many crime attach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_average(lst):
+        return sum(lst) / len(lst)
