22cc8570343bb6ff2952b1d67b00187db2905190
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:29 +0000

    mean arrival wild goout suddenly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def count_elements(lst):
+        return len(lst)
