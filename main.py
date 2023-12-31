0838615d047125c43e771d9c2c57828796c79e43
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:08:12 +0000

    phone fall clean ignore thirsty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
