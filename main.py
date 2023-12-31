f1f8ad1c9b8bcaa00069cc01bb5c7b688ef1a7bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:06:05 +0000

    family human luck professional walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
