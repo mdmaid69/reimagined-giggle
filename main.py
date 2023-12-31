a22f733ce89fb4f6fbeadafb8e6569225370e9c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:56:50 +0000

    over entry cough dish might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
