19ee65366ebc42f9ccfc8fafb5c17b7b643942fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:33:28 +0000

    dangerous contents workout spot avoid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
