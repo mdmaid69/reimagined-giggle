7f678dc42c87a9ea6b8fd1320b27e4fb97eaea44
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:45:56 +0000

    police me market direct firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
