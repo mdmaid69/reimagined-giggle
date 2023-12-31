e14f235076918a5d480e94dd9123c5c20ed34eb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:29 +0000

    sorry trousers fortunately friendly price

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_floor(x):
+        return math.floor(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
