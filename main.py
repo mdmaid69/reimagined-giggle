1536402c0b8bca24c0527467de376297ca2106ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:23:34 +0000

    course arm loud expect Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
