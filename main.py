c8ce790802763e7b5fd125b09bf9a672600618d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:28 +0000

    comefrom anyone let then connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
