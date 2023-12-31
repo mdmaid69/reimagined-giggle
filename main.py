7d2f36ce42ae40a86afc30c91430c59a29fe44cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:40:17 +0000

    back protection obvious holdout back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def reverse_string(s):
-        return s[::-1]
+def calculate_exponential(x):
+        return math.exp(x)
