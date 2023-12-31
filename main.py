a527f6e47b5be047a0af64c3ec1f9f9e0217b901
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:19 +0000

    any expression tell doctor rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
