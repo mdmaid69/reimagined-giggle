50dfd5de3a67037494fc542fa19ceb53eb5ff068
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:36:09 +0000

    congratulate deal mood pity print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
