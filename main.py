2427be26a750cc0184c0b2646a6004ae97708c41
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:45:46 +0000

    equipment scheme nothing minority apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
