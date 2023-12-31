e33c503eb7c0df34e183077e4890f494666b8edf
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:18 +0000

    beyond play study public that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
