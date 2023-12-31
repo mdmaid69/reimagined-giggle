b5966a4373866ca0691cd75dfb06b19b4b8b2482
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:36 +0000

    show sound sign lend number

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
