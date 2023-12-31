a1b31312ce0491ab22cdad27290568c59ee508f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:11:44 +0000

    dick action protest phone swallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
