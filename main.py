ec2a4f89b59b2611998907d71c6f0014a60d3e69
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:28:13 +0000

    lesson platform whoever mix light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
