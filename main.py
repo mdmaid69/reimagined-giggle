9a6e4671f0d9fcc174839e769871a06fac46887f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:21 +0000

    mistake child shop weigh score

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
