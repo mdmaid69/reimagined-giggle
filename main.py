8f73af15f917e8315ae3cf6eaf98a103dd30db6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:51:42 +0000

    pretty full wipe head fall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
