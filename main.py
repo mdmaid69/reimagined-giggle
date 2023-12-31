e696ee9b3bb6a0023a6a70323d4321f494cffa2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:42 +0000

    well breakup design surprising accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
