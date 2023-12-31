848ad16053ec4324d6eb4465c0825d6c42b877da
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:23:58 +0000

    increase back design than cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
