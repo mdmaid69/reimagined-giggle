37d263929ffe44c98f58d1472b3e3fa65221f018
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:55:30 +0000

    she careless used below bend

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import json
+print(json.dumps({"name": "John", "age": 30}))
