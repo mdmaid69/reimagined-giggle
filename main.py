6c4a5f8b23932e9183de868bf1230894ffed4d01
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:52 +0000

    recently strike hospital wing hide

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
