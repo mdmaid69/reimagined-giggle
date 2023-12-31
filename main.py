5d5e87bd5a16f6b0f4e0f41a89d871d413b92b56
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:52:02 +0000

    female all hurry flood offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
