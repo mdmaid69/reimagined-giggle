e47ae74295d2fce1af350b4228cc08bd92d96063
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:53 +0000

    attitude protest demonstrate judge edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
