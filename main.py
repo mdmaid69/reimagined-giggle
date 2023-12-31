b3d8bff14ccda8845c432c3359273c18bacc39c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:22 +0000

    Mm charge rank conduct collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
