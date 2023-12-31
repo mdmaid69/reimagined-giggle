fe6dad49f0e4d5375a9594738b2c24d9e068062f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:44:21 +0000

    various sink drive snow sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
