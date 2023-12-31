cdb434c2ef8124160feb1d6aacdaa38a4dcd3f3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:36:14 +0000

    cheque programme geton week move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(lst):
-        return min(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
