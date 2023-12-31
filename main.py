1abcceaba82dadd46afe231571900d061810bff4
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:25 +0000

    worth underneath against equal declaration

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
