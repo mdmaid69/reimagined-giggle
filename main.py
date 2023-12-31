5e691bbfeabc8772d8f93c3bf6684397fbedb3cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:38:32 +0000

    cost uncertain anxious lonely spite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
