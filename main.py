37e8c53d20b57766e38a2ce75efd0c67a78d5554
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:04:24 +0000

    guilty ball principle dull record

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
