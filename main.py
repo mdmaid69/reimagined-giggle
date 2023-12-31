45be760aec7d41482f61e3f737f04ed25e4670fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:26:54 +0000

    compare forever insist song Ff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
