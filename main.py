691ade4b6899a31183db9c99b329bc978b60e581
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:58 +0000

    represent impressive scream failure top

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-print(datetime.datetime.now())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
