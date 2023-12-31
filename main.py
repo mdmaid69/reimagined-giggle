5d2d8d6e1da359ae9a42da8dd7c8afd026312eee
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:39 +0000

    cross butter cheap border mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
