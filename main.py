335557f5344bd21a6665ad25c3267797f19082eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:59:14 +0000

    full to fun accommodation know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def is_even(n):
+        return n % 2 == 0
