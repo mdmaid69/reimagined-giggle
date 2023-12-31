d1c9168d1a4a51ee7f8df8fbb6f5e7be4bcc4c10
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:05 +0000

    disapprove need present last second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
