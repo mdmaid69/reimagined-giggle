8f07f1932f7dce995bddc54fa05bdc45ec032f51
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:10:41 +0000

    fat difference sport habit claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def find_max(numbers):
+        return max(numbers)
