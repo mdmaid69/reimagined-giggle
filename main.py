47eef9c9203e788cd8fb7d5dda83fd7bfe2c9318
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:01 +0000

    perform what error star burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
