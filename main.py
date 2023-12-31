e446473e4540d1d9e3c29b54570054db98813e2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:12:50 +0000

    nor doctor wipe yours command

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
