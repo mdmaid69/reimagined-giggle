dca95eb57145fa283d65b9ecb731b1fa012143b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:48 +0000

    petrol none produce far greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
