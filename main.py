534ad8aafcef97196b4a15293a4b80d8758e0c0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:42:18 +0000

    excuse bringdown light thinkof hide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def is_odd(n):
-        return n % 2 != 0
+for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
