fae0282170793eb128831434d854f072b860707a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:24 +0000

    excited silent still fight repeat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def convert_to_octal(n):
+        return oct(n)
