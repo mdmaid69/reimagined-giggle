823ca2c6a51b3e98916205a06d40b17cbc443fda
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:55 +0000

    threaten emotion church level hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
