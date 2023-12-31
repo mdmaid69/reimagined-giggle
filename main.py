42d4d5b6388a983dc11e4458f580509c1699ed5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:54 +0000

    old left Mm weather hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
