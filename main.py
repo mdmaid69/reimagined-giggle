be7d51e20cb8e85bdc8071663594eaa57002c0b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:37:00 +0000

    several proud decision admit trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
