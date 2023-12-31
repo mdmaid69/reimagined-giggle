c26930005e9828fc4d7e93a331ebd64d274c6973
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:51:37 +0000

    then confident air compare trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
 import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def append_to_array(array, item):
+        array.append(item)
