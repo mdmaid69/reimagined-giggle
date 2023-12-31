c9ea30c866c9e9958d9e95749d1b1a131f3b77a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:35:18 +0000

    mess goout even waste dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
