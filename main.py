b1d7caaf4a1115b8637f5c0454e5478135be2ef1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:33:31 +0000

    light miss prepare left strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
