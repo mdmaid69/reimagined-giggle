183d65d677acd61232c616b49cd50b442b5cc9b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:55 +0000

    praise direct getoff when surprise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
