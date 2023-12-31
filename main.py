f15277390b50cd0944101c4af260d4e3f37e5e9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:24:11 +0000

    tap in Jj chief old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
