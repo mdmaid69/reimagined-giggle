48fe4496720b7513d0c2f7ad42a830c275c1da93
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:33 +0000

    some bottom sudden heat Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-print(sys.version)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
