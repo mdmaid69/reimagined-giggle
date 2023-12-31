37f6b1e59ebfcf6cf491c4c3a009da86039d211a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:45 +0000

    just paper answer furniture coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
