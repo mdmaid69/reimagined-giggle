d53357224372ce6c35b5d8bddce5d19e5706a06a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:07 +0000

    high fold offend perfect across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
