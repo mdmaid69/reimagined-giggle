56483fb146eed522cf767a9bda4825bbefb3c34d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:41:00 +0000

    Cc limit apparently turnon crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def clear_array(array):
+        array *= 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
