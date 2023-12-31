ab20bd8a3f8c279fd827551ecc5c7c98faf1eb65
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:29:29 +0000

    interfere whichever prefer lead failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
