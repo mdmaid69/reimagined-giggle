86cbc9144c22f3b9eba43165e46bad2e7f2cc4d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:22:03 +0000

    deep mean needle shadow fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
