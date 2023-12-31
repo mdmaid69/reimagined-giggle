f11a8ac2a98439b1df2bcea7352bb83b7afe9da3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:24:34 +0000

    drink production provide everywhere job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_set(array):
+        return set(array)
