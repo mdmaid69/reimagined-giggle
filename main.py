8d6b13fceee5b94e2cf65bb61a5cba5e84008353
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:05 +0000

    how allow greeting completely wind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
