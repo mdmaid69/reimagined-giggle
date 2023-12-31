0fa5acc8d50c42a2e004f5913d48673b78e59f1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:45:50 +0000

    cross criticize otherwise comeoff inform

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
