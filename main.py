4301a8f27a8594d091e1607390d7bc062c5743c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:45 +0000

    true peaceful event coat settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
