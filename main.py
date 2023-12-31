5373ca716aa017f8fcbea64da442cc55cc068aa6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:23 +0000

    prefer normal stamp region distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
