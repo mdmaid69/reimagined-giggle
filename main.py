8872f3c3766dd87d3997132b0546327a06375705
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:58:59 +0000

    winter head instant improve arrangement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
