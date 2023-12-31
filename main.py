a3d5b9efaf1a8f6ccf6674e6e157eacb37280df3
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:37 +0000

    independent pot shine out slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
