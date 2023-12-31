e4b346b051a330242f2a04ecec7e989371dd9e2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:15 +0000

    promise arrangement breakthrough rain sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def reverse_string(s):
-        return s[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
