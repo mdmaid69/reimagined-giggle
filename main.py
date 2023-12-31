b92cd79c63a54183e51ea3083e679c7a80280f66
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:37:00 +0000

    objection moral ever rarely general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
