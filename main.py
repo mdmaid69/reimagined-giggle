8eb35316e273662f371b4213a5f89d781ff9e5d0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:56 +0000

    ever patience warn powerful head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def find_max(numbers):
-        return max(numbers)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_float(array):
+        return float(array[0])
