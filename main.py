8e721b20bcc7e55cc5cdbee95d5b4569ec5f8667
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:19 +0000

    wise thinkthrough spread strange between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
