ca2ddb5c6f9c81e2719ae6690cc72c6de4a3bf1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:26:45 +0000

    round brave sticky cinema bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
