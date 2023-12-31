6018c6404bb02b0a0ac70b94df377b5b3052479a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:31 +0000

    raw public justice common accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
