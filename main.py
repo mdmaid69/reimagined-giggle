7941affb1d030ab8dd8cf1e007f5ae52c52950f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:15 +0000

    warning valuable complaint rubbish raise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
