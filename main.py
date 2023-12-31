3810c11fde4aab31aa6187ca67d35438948c4e6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:11 +0000

    autumn long change spoil emotion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def reverse_list(lst):
-        return lst[::-1]
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
