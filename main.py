2c0d4da5079445f8f5f76cd686cb4e5062623556
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:42 +0000

    clothes boring communication steady capital

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
