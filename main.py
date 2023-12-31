52339394d5ecbd3a640785bf4fa5469b2aa76374
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:02 +0000

    major hope settle appear getoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def reverse_array(array):
+        array.reverse()
