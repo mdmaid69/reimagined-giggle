68477fab145ddcb50ce13a8504e813a1866e8ad3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:12 +0000

    relax spill exciting possibility try

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_str(array):
+        return str(array)
