76800075f1771d61d31355ecc9885187bf91043d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:43:27 +0000

    keepaway question impossible real load

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
