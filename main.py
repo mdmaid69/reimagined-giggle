e7f8da7192d03248b21c42bcc98b33fa46a1b7be
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:29:41 +0000

    pay there such meaning injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
