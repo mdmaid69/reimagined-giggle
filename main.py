f4d37caba4401db8ce81e4b676cadb5812431454
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:25 +0000

    press turndown charge conduct light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
