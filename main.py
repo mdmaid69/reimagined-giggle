3a8a2a91767620474f1f1d1e75d246aa8cc32237
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:00 +0000

    error row die base by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
