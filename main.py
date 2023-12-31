d0c03527f3a51563815d1801b83b13b4e092d51b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:30 +0000

    instrument ready strange kill shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
