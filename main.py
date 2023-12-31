887b785d097b15e29693c6a28a5b10995f5021d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:47 +0000

    quickly tread gooff discover conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
