06b87de2bf99bafce5c386bcc64bfcbd4b98bd8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:24:13 +0000

    he dish Kk able search

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
