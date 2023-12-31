a294058250f25efbc69ceb5e4f2e0c0d71651294
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:47:13 +0000

    bother copy raw thought though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
