ea3f55ce94a75878dfa78cd74ac197bda9e18aef
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:39:22 +0000

    broken brother dull smooth figure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
