527f1fc4d4880f7c2d95da66a87845945b2cf6e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:46:38 +0000

    getaway before goaway straight uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
