0e9ade4c0a16a6ff4691099a5d6031395c1cd6ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:29:37 +0000

    real heart demonstrate politics future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
