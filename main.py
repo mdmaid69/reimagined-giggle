7958eeca3f7953104a946c5c6532b6d3e15450da
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:08:37 +0000

    bunch patient confusing neither plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def sort_list(lst):
+        return sorted(lst)
