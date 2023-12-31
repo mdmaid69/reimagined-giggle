8c66b8b0b2e74d354eab398d3cb19adfc5ee3ae9
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:48:28 +0000

    unit prepare division our weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
