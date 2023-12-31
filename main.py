abc95dd2881bdb67ea7a64e942c7a4020689a6f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:38:20 +0000

    address contact long past live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
