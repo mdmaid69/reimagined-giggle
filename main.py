715bc96a851e88c7c5e803b8c95e5799f5f27b0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:45:58 +0000

    minimum unfortunate spare concentration demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
