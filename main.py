85ace1403c1ed679878a5c3934c973cf8be58eb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:59 +0000

    autumn somehow wave wave conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
