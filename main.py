790ddf26c4f1f5f68d40b475e74c0729bb0ad379
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:52 +0000

    fine opposite tip breakout react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
