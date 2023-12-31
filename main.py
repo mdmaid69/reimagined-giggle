1dfe12ae164d7959cfecc50d9ffda0fb83ba9ed6
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:41 +0000

    side more stamp loud child

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def clear_array(array):
+        array *= 0
