47d6bd199a5ea416f1f69716c932ec4ede6dc796
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:33 +0000

    introduce watch before keepaway perform

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
