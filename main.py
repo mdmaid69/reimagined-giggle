bee355d755c2823f1de91b6b64aba4c19298713b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:07 +0000

    brush pleasure except waste task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
