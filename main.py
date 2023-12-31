a84d5ce9e1aef76277719674043ef6e085fbfce1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:29:37 +0000

    sit combination mood talk giveup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
