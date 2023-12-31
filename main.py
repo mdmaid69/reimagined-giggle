095d07fa71178b409404337b03c3b774bdc4403c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:27:33 +0000

    quickly cut uncomfortable anything lookup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
