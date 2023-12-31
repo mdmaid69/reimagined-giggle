318d0d30e8ac83bdefd5a195c2eb76fdf412409a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:09:59 +0000

    play storm catch in disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
