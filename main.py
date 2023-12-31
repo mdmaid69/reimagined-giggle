36385f6900c6aa97f784f74853333b5f538957ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:48:03 +0000

    past yours tool play yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-import math
-print(math.pi)
