33236aa456b847c4ddc39e0623a455748635524b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:20 +0000

    used need risk hope fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-for i in range(5):
-        print(i)
+def calculate_work(force, distance):
+        return force * distance
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
