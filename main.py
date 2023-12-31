5aca6424e2167b5b34734332961a5a40f736339b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:37:00 +0000

    clear cheek listen stop sleep

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
