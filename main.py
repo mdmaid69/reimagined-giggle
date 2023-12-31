2c1bf01dac0119a6aed989dcb71ff368bd3818e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:39 +0000

    snow familiar clear either rely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
+import re
+print(re.match("h.*o", "hello world"))
