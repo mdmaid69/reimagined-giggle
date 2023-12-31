d6aae365b7c8b7f6f868ef2c04c760ab1e550980
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:24:49 +0000

    affect affect book popular criticism

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_string_from_array(array):
+        return array.tobytes()
