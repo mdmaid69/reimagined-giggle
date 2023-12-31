494cb8e02a9c611c1b6b16c1c614ec029661f5f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:18 +0000

    file explain switch style idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
