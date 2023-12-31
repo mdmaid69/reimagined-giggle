07f934959b8e001c43ed67d0a98b24684b157805
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:42 +0000

    appear take bite power holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+print([x**2 for x in range(10)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
