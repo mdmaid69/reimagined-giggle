7ea9e1324147814ec6be5e0d0ddf291cf65cb46b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:13:15 +0000

    takeback rough shadow practice dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
