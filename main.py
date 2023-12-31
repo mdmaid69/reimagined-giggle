90cea4756193f10c2ee246198154db0f2fcb814c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:41 +0000

    window invitation nose mouth dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
