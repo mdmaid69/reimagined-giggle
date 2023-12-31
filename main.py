bf249ce69094a9e433517159530ef65bbf705b65
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:47 +0000

    limit hurry usually by cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def multiply_numbers(x, y):
+        return x * y
