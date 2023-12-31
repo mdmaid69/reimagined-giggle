eff82ce96c5c3792621266326e6d86ff327d1756
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:04:01 +0000

    takeout interested promise spend react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
