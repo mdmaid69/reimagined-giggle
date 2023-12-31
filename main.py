f7c88d85739e909b9b46b06f231104b2b4e08758
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:01 +0000

    popular company type like suck

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def clear_array(array):
+        array *= 0
