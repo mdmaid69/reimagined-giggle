37b85686e987df4813f54205a0c5da4f900dddc8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:00:54 +0000

    rent piece getout display objection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def count_elements(lst):
+        return len(lst)
