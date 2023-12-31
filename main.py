c48dc8eecfde5456640edf8e4dd1bc959eaa9d7b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:12:56 +0000

    familiar plenty trick before play

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
