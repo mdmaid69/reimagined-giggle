e52a0011d300c0763bc47720db5c3ed7ce2b3aa2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:16 +0000

    finally which remove modern enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_rectangle(l, w):
+        return l * w
