19a176cdc86a2a50409fd6cab111d79a57369ac1
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:57:32 +0000

    contrast broad rather radio comeup

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
