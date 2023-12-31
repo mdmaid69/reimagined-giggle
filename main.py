1e179b07218290065427fa0deb8a488054f2356d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:13:42 +0000

    name rise over anybody he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
