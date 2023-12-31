3f09bea9befc2630d651266ff054ed2926dab56f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:50:40 +0000

    difficulty tense confident activity calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_list():
+        return collections.UserList()
