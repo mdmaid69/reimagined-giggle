7cc55fb970f4accc0be9d4766f1c48caad84c08e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:37:08 +0000

    high lean type properly rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_list(array):
+        return list(array)
