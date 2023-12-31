1d47008f5b5a804bad9b85998be877eaaf7b8adc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:30 +0000

    trouble film bound reliable by

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
+def is_odd(n):
+        return n % 2 != 0
