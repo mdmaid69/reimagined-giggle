de4af42858b70f5c67c07007a3bf764393e1bfe8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:52:42 +0000

    produce furniture wide trousers meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
