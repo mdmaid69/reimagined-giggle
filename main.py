abd57bd8557ad824b4d2e59a9cad01d673482574
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:57:27 +0000

    you step really trip war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
