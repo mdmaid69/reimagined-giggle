fb41931e8d376fe2ea4bf9fb1a38e2c591a9522d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:22:15 +0000

    lie mystery who learn dangerous

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import threading
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import queue
 
-def print_hello():
-        print("Hello, world!")
+q = queue.Queue()
 
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
