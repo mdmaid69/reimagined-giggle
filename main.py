8fe7476a6a418e8311062acff16dc7b970f65423
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:12:36 +0000

    tight hungry help bunch model

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
