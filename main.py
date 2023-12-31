809ed90f278d72f64ecc76977389322815b9ade4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:31 +0000

    survive fly want store exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
