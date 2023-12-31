d5943eb94ffca0e5090d3f79029e31769320b973
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:55:06 +0000

    recommend war different only honest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
