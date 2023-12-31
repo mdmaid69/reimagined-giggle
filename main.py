0e90c2db2466b0e405ba753e5523168b97e733b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:58:18 +0000

    narrow broken shout safe suit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
