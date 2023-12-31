3aa437219703595168f171ac1dc6732314d87d61
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:25 +0000

    original beat protection talkback train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
