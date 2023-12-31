f8af57671c1a5a1f9bcc6f5430a54d40c36db152
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:02:35 +0000

    workupto crowd taste whisper giveup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
