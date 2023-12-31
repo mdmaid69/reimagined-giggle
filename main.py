9481fdbc73b7c3351b1ce40a50bfca5d16a1c632
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:33 +0000

    bottom record future brush beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_tuple(array):
+        return tuple(array)
