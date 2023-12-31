aa7f0a19d035b4dd1c05a1a02df89a84e4f7dd05
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:44 +0000

    talkback there arrival let translate

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def remove_from_array(array, item):
+        array.remove(item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
