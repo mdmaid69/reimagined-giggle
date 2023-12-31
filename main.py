c994c97eaea841a7d1f396818615be054e84fe09
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:14:19 +0000

    mix takeoff nice summer callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def reverse_list(lst):
+        return lst[::-1]
