ee53b377520562a8cccc5bedad79638ad3bc4ec4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:01 +0000

    please arrive cap moral walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
