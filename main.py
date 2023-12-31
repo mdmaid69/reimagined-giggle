568a2feaa5cc7831cf59e784a66b32a34b0969a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:45 +0000

    mend extremely estimate challenge position

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
