686ec935255b0ba24e4c0ff219b7ebd4cf4bdb85
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:01 +0000

    giveup obvious scared long pool

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def is_even(n):
+        return n % 2 == 0
