5732b8b7027c95af35528d8e9a1e52c1131f567d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:33:10 +0000

    once praise another except reason

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
