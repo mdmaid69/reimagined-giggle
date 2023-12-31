c25e9046681034d334e0812cf25cdc6d669bdb29
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:55 +0000

    put swallow total matter if

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
