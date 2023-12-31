5e76d274839016891f3d3f46d2d662fccbe6c83a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:01:17 +0000

    land natural land whichever shock

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
