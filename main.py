3b69a234094315b410291169892b069c4b7cb779
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:39:33 +0000

    quiet afford mess Itself wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
