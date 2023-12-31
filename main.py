a69c12ff034bb6048f0d78da0baafcddabf51972
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:22:40 +0000

    lock who feature date command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
