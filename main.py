843349415656e6dc62378ca798743f50e52235c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:31:35 +0000

    safe underneath yours cross obtain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
