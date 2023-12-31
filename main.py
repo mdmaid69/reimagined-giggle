38a878300fda45970be1cafcde0a2323127b3b70
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:12:35 +0000

    used must frame occasional rate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
