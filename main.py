ef4f470331c81edf981ea6f6cd99b5911cd1922b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:00 +0000

    at butter breakfast from church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
