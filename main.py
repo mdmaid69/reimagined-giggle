c6f921ebcffd5f79b9770473799844a2c6a80dea
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:28 +0000

    estimate clearly place release well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
