36f2b2181cfab3820a9aea004a7fd69ad9857052
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:55:41 +0000

    coat spill forever window attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
