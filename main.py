33e4b0aa1f0985b03ab3b17f6feb99f0eb9da178
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:02:57 +0000

    means much dance habit anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
