01a014b0a2ad4ab9d2ee925e498783a97135c7f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:09:32 +0000

    excellent forward position woman settle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
