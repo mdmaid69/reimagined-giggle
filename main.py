f555bd88efe6b597a719a3648a2586db810a8523
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:01:46 +0000

    bury spare roof pull reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
