bdeb4570c7304b07d1b29d281788e46136ad12ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:04 +0000

    sad pick succeed dig cruelty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
