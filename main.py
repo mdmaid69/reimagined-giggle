5ba86bb18bc144c6f6d6e3b652a396c1102651dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:36:00 +0000

    common friendly risk law completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
