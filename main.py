48e1ca2d47dba5d5e4af6e73c78859ccc9c8fedf
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:58:42 +0000

    bath complicated favourite exchange mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
