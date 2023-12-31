bb14c70e634ac8372d6cfd7a550602817c9d5682
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:47 +0000

    repair guess hope yours mind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
