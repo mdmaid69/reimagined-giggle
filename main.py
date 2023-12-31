9210a11f114cc104ccf755778c4512f16458b0ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:55:52 +0000

    my sorry prepare depend 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
