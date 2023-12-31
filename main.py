b1c79fb962662053475c48e4464542d2eba1a1bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:28 +0000

    probably odd doubt often praise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
