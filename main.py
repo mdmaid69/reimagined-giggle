f0203fc71cd3e198fd479dd5658fadd8457f1f30
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:35 +0000

    street unfair insist point long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
