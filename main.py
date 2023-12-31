50e26e8d0f7926dd85638efcd7f13c89053babee
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:26 +0000

    sign brother fashion space fail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_list(array):
-        return list(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tensorflow as tf
+print(tf.__version__)
