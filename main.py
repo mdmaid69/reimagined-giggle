bc8d9e22c2d8f383217916beab1fdf7973f34dbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:15 +0000

    plan hurt contact but hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
