a3ded815578dcf40e9243f5a706a43f9c0452245
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:39:29 +0000

    but intend accept mean destroy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
+  def convert_to_octal(n):
+        return oct(n)
