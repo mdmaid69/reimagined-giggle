a4909c7adb3effa012e2b35eebe3aa3549234ac6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:35:43 +0000

    uncomfortable human little formal any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_bool(array):
+        return bool(array)
