6b4d18c151541e955578bf1829dbe753db297e2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:49 +0000

    sorry petrol workupto slide important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_current_working_directory():
+        return os.getcwd()
