44b19bd86274b4208a124393a42fd3e25b47c349
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:29:17 +0000

    explanation meanwhile seem talkback deserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
