afb7444a374c091e2a0217ff21df5f3956400ce9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:07 +0000

    heart both clothes remember start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
