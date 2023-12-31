2823e5f389b1c19d684c44e218ea160dcc6b91c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:22 +0000

    uncertain pause butter term production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_min(lst):
+        return min(lst)
