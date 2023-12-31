47a8ab38c6a6d7f4b162848fdb71375e39e46dce
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:50 +0000

    valuable I appear recognize hit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_bool(array):
+        return bool(array)
