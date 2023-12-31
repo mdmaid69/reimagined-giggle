642cbe1882782c47af53d6d4bb136c0e16181ef8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:49:54 +0000

    talk decide possession nature except

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def count_elements(lst):
+        return len(lst)
