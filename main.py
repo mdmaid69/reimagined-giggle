7fc235e2fe62d112e45bd5559bf9d5bbe7fa0d9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:58 +0000

    introduce dull pause meaning getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
