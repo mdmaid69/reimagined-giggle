83c96444dcc335c6c0f19f30354889f805cb71b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:08:02 +0000

    substance ability rain comeback marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
