bcb235727f1c49637e81032898ad64fee645d2f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:34:21 +0000

    light swallow central need write

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
