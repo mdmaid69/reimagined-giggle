dceaf52d83e5fefecca0ea76a1ac63780c3ef538
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:37 +0000

    child takeback gather place treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
