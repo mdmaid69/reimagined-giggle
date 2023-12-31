2325e2ed2ceb8595818435d28e790e3f566a1b66
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:49:50 +0000

    walk scrape beginning card now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
