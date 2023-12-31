11512efb03eed948a792c0fb2b5fb7b770c60d16
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:42 +0000

    fat from slide ride attempt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
