b6696bac2f9262ff7d5a0dda942bc4fefd3a3c40
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:25:16 +0000

    slight so lookback(on) basis escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
