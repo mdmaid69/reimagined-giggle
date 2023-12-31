b58e9c369285f09e046403227b00db64f28f182a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:59:39 +0000

    drink tap basic immediate once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_area_circle(r):
+        return 3.14 * r**2
