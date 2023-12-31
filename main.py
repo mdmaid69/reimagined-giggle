345a6bd4528d1846b60cad04493cdac611aa0e6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:55 +0000

    accidental much hard night disapprove

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
