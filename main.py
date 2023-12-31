aef895dca3e238def1ea533b84d8cada5e2bf1c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:04:01 +0000

    clear relax finally notice hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
