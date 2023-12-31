7cdf8e17bb0dae536b458f1e9237697fef4f1bbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:23:39 +0000

    character male attract below plan

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
