0f7d35597ff1031e6d56b4a236e3072ca963c8ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:20 +0000

    drive experience later love guard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import getpass
+def get_username():
+        return getpass.getuser()
