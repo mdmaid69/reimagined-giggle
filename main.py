426543046152b2b7e82ae8d69d9b6a457c91153d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:35 +0000

    together typical fall runinto wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
