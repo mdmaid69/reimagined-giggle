0a090154ac83f48c6793430a09f6c22706041c2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:33 +0000

    keepup satisfied spend breakdown laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
