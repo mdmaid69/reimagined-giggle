222ff1dd796857742b69bf45c5478920180b71d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:53 +0000

    crowded news glass late protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_queue():
+        return collections.deque()
