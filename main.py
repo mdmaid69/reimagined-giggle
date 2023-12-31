3224b34b2f90ade1be685581691171f8f2823a2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:17:49 +0000

    single fork middle Ff all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
