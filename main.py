52f10198a91ba9e9644e2dce685c555afd7233e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:02:08 +0000

    container just regular any famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
