614c3d8e28afe8da34640992fdd78b6987e2c33c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:37 +0000

    just dish foot extremely depend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
