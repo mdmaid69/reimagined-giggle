3826682f6c35b92408a26f57cfabc022012d396a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:32 +0000

    subject excellent him into police

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
