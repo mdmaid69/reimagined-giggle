59b6617eed3b69fdd94e8d3522c5f25e53c309ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:46:05 +0000

    mess worth ought turnup climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
