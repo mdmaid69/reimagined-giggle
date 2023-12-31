728e62c2f47b7ea70d54f47ac78a85139e9d6474
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:49:04 +0000

    their careless term mail wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def set_array_item(array, i, item):
+        array[i] = item
