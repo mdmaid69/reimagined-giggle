caaa8286933c3564e3dd3701be26ac40ff802f36
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:43 +0000

    speed comein brave prove teach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
