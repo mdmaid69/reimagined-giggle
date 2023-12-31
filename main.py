c4137de34a3584538e61750bb33978d839f0b19a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:20:36 +0000

    cover argue area comfortable female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import getpass
+def get_username():
+        return getpass.getuser()
