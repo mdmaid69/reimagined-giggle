d69f4a81d00771bf005ee62c77ad1ff32d14d4a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:49:53 +0000

    crash reply blue crash bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
