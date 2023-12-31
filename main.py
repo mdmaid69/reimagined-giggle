662017c5f33f78ef856916e836070600ce9c74ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:08 +0000

    offer road disappear whisper piece

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def is_odd(n):
-        return n % 2 != 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
