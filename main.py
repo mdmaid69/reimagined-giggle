c50246ef5f4c2dfc9fcc7fb6bcc197e27c8fa103
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:28:37 +0000

    especially chat escape cope yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
