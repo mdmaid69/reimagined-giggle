d870b090e79b6e2aadb16628d7a0b683f28fc148
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:16 +0000

    trick getoff off speed just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
