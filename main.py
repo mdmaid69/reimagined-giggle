44af4d3c3a14599cd156b7ad19e72bff142a75b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:18:15 +0000

    show agree their preparation fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
