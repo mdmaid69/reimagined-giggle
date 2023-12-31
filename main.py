77e147413f7ea9fae4067ca382083482b13e11ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:08:55 +0000

    mind forget whichever involve shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
