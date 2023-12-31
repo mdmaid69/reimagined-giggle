14590c4535a997d8ae90e27b12ed8b3c46017f50
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:46 +0000

    happen develop scared phone join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_int(array):
+        return int(array[0])
