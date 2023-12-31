39d51ae649867c6ab0e869ee921508a088a11108
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:24 +0000

    ball also sensitive dance holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_str(array):
+        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
