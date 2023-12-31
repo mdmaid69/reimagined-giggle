6f8a826f5a45b71bef3b4657bcf586bac4fed507
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:54:18 +0000

    workon brown our police crash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
