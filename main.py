b49c78f78b1001cb9495d59113030406be9302ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:41:25 +0000

    size soon frequent ahead silent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def exit_program():
-        sys.exit()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
