5c7eb7c43e267342162b71cf2272984f5f718137
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:43 +0000

    responsibility before nearly unlikely keen

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
