b3ba31beeff8f7a6216f3014615084d4c544839f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:34 +0000

    put disturb objection comeout upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def divide_numbers(x, y):
-        return x / y
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
