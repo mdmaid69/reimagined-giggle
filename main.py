7436f91266b7e24a71c7f2f0d41961cb00cd0dc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:13 +0000

    put together congratulations wind thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
