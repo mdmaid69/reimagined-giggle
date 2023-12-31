d1210db1f2b5643ea65ad23534851e28b9f526ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:03:24 +0000

    them calloff open meanwhile late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import time
-print(time.time())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
