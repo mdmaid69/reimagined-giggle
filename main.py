a97bec518f49a51da1e9fb26b382e19ba5bf1c57
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:36:19 +0000

    feature phone any such wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
