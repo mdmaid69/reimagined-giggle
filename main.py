91fdd43b1cbecc62151b52cc38d8d4ac6550fde9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:44 +0000

    build length freeze ocean regular

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def append_to_array(array, item):
+        array.append(item)
