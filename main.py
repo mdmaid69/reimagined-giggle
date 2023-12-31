17743b470de8c96cfed1da4215f56759f093a251
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:11:50 +0000

    try regularly familiar live attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
