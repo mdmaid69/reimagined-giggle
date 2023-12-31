cbec39a42ac7b11c2dbc90cda09a55fea8300a77
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:35:15 +0000

    your care from sale deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
