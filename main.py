68c8b46f68000f69682d7609fd98237227cca482
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:03:53 +0000

    anywhere correct clever phone whether

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def reverse_array(array):
+        array.reverse()
