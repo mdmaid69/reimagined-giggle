970946d1927db01d83d3d44d3849b643223bff72
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:45:27 +0000

    building eat physical art wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+print(os.getcwd())
