16b87b914e5d5b1b144c9df868bbce188046db88
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:33:01 +0000

    bright steep should round fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
