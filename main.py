36909434f228648409684923f2d09851cc240369
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:19 +0000

    hour block love everything ring

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
