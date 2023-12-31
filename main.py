6e9daaf190a0a5708417d3b7a2862cdcd34f2123
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:41:05 +0000

    sore rich minority age brush

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print(sum(range(10)))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
