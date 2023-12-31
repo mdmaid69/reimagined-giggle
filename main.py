366c7b255b12bc9f7f987cc127c53f063201afd8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:17 +0000

    belief Jj environment threaten free

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
