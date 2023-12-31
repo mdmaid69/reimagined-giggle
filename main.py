cee5ea5ff64000a9c6369245441ba840c4f6f610
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:10:18 +0000

    crash ourselves practice scrape term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
