3c19507f6183208b2b3496ee2cc833432b972653
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:52 +0000

    breakin decision peaceful responsibility event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
