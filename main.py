4e722d980e1734733226cbc4bb07b93c1450984f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:48:47 +0000

    there runinto terrible poor smooth

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
