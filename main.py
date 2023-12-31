fec3f030ec431b3fd93ad870cf3675c2755996f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:30:16 +0000

    copy content within shop yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def multiply_numbers(x, y):
+        return x * y
