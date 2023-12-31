2f75a5854b66199e6cf53668aeb8e58b0601d989
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:03:44 +0000

    excellent region violent unhappy border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
