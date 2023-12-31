347360d0d737c6856b942c0cbf84e4eb4f762044
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:16:00 +0000

    follow favour already now himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
