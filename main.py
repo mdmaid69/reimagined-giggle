24118d6c0f0bca7d64b5e00874b0169da58eeab5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:48 +0000

    moral politics arrange miss estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
