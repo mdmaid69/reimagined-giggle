c78dbfb6c379f41364f5f1b55870b81fccf1b2d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:29:18 +0000

    cut murder sure clean absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
