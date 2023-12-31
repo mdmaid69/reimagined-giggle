1eee41be0326fe9376878b4cd616939ba8724a26
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:54:03 +0000

    man protest top fact nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
