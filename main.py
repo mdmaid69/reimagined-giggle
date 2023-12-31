0a625c42fb7ee2059071a1a8747a961dd53bcc29
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:33 +0000

    jump sport mystery anywayanyhow raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
