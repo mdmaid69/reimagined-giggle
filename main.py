8fb460ad361b949b8c9b8e8e41aa9dd1885f3c07
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:51:48 +0000

    pupil sure weak cry print

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
