a5ffef2084fdf4bda14cf48f553f461c4d6735fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:40 +0000

    because needle peace uncertain back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
