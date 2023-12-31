d46700e13804c0a556eec47be5e9a94e3b69845b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:16:15 +0000

    careful check cost attempt sense

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_power(work, time):
+        return work / time
