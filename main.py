21bc4426216d1a4027db84686378dc4fb6d42156
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:06:57 +0000

    sound spring wild perfect makeinto

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
