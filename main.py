1ba4e1f2b67e37764e042cae748a7b8b197980cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:59 +0000

    miss hear needle sister suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
