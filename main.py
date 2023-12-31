9174009460e8974e349d8dbeb95def54980a78d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:31 +0000

    reply far runinto expensive side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
