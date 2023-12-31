9ee7adcdac01961dc0173ef9be465ee21aacb0a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:02:35 +0000

    perform cut before exit almost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def remove_duplicates(lst):
+        return list(set(lst))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
