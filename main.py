3c2e05652a1dc4f9ac42aaddc55c25649976c761
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:06 +0000

    cap head concentrate stranger beautiful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def convert_to_octal(n):
+        return oct(n)
