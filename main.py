3902e8d50682d1d2069d57cf18f116dd0cb7424d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:48:48 +0000

    cheque wealth fit settle prevent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
