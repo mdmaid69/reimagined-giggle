76baa9aaa5c567c5e434b2449cd6f131352b1e2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:29:05 +0000

    should shape principle which goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
