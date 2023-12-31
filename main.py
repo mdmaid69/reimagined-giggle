488654a0e5fbbeb52aaeb22c55c488c73aefe074
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:21:19 +0000

    expression second club kick appearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
