293c29ccc43c988084246ba626cb3f1a594be099
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:18:12 +0000

    remove successful certain bury rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_square_root(x):
+        return math.sqrt(x)
