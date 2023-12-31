c9a04716799e4c15ee693422a7e8454980e8696b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:00:03 +0000

    conduct 569enjoy speech interrupt nearly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_power(work, time):
-        return work / time
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
