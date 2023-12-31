e63a254749b54cbe6ae1472774b0739689306b42
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:01 +0000

    right prize discover act improvement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
