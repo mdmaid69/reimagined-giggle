eb2cdfeaebb5f035b87dadbbc0486b19bac45ce1
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:15:58 +0000

    kind blow deep ignore accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_unique_words(sentence):
+        return set(sentence.split())
