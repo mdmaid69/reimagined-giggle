6959df10ba9828259ea725fca902b32db7894089
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:54 +0000

    answer 573entertainment down instrument direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
