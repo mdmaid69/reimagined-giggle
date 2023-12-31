4d4b0043fc7d09c519d4bfe57e5ef3e3467f222b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:11:46 +0000

    move meanwhile surface find offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
