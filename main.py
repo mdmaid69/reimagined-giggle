f4d257cac78f0b19f4e69824d297115d5a17f581
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:45:39 +0000

    bar town bright ours serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def remove_duplicates(lst):
+        return list(set(lst))
