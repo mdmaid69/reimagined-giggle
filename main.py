45df79417c492f8d5bc220fcd7ae429b387c9e65
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:06:51 +0000

    condition tomorrow funny sleep tense

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
