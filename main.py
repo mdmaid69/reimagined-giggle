2cb0e622309f7b9dacd6701024dc18aface08267
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:25:33 +0000

    slowly probably joke back let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
