46ddf4319e71eaa1e33086f63333c1156601083a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:14:44 +0000

    fear Ff whistle geton so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
