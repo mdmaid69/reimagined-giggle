ad94a97c8594ac00633b59862da350ffe53043cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:00 +0000

    steer boil burn connect breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
