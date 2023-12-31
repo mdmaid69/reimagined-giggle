17483fd28100de43c12b95640417a41595e35821
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:43:07 +0000

    off temper sudden care play

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
