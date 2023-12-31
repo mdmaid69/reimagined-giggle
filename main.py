442139050802bdae9b261566a4848ce970165fb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:07 +0000

    top matter keepout nonsense down

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
