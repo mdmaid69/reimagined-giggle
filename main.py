e32cf2efac28dede14309f7cb955ba63bc3ec64f
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:36:00 +0000

    effect regional choice compete steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
