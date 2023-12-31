c678f3d7b5e7f4277dfcc0339a3c2600e6d3bba5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:45:58 +0000

    wild soft race fair destroy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
