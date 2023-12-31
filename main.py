08de1a52e08efffcd83e645ccbc9bdcca7dcbc26
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:07 +0000

    correct strike famous walk entry

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def count_characters(sentence):
-        return len(sentence)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+for i in range(10): print(i)
