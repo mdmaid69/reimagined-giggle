887b5dd8d59c77258d47da078ba2ead86fc54116
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:34 +0000

    none cap open later over

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
