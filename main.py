682adda0d546477beaeb465b11bffc77eda5b481
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:57:45 +0000

    develop deliberately leave maintain fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_today_date():
+        return datetime.date.today()
