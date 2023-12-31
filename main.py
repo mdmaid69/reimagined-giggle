9da294a209ebc38e3a14423cb42357eefe9b60cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:30:08 +0000

    tear relax ground apparently cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
