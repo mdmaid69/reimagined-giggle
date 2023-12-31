53ab7efd7dd8b722f8acca8f426f5d611418d71f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:30 +0000

    correct action sensitive fly measure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
