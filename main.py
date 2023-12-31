cf425bde7d7fee64dcc726b069a89a8c4a938fb3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:40 +0000

    shade ilovedicks affect unpleasant dance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def multiply_numbers(x, y):
+        return x * y
