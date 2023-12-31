3f84b405117bb839e0851d3e63f676ff7c1bf640
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:35:41 +0000

    us protect date closed choose

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
