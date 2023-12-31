0ccbf74e4ac55c5ea3601bf7ac082ec8fea19d12
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:50 +0000

    exciting Ee sick table bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import sys
+def print_python_version():
+        print(sys.version)
