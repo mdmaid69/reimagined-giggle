bd286044dc80762180b004fb41e4d02c2566881d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:49 +0000

    spread best love frighten steal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_acceleration(speed, time):
+        return speed / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
