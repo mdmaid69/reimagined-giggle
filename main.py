3f39564b2354980d4055f4664e030db01d6ba3c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:32:14 +0000

    switch burn condition keepoff heat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
