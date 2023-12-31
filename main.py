6b0e6276e0169730f5bcba1a9d9e61b3287bdf83
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:14 +0000

    ground throughout recently believe profit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
