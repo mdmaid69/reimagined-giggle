d30b2e292ed106754ee91b3455a28593593a9aca
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:18 +0000

    second thinkof hear watch after

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def sort_list(lst):
+        return sorted(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
