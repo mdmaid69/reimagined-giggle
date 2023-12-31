35301228019a09e04f8e93e883338db60357c8d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:51 +0000

    entry now straight snow reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
