7f094e076cc54f82a87a9ce370cb063654b27012
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:06:32 +0000

    different favour smoke everyone bother

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
