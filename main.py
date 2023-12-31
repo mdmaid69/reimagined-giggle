f8efee6c1ebdd0a5c1134c8b2d7c359226e01ef7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:38:41 +0000

    well close fire succeed demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_sine(x):
-        return math.sin(x)
