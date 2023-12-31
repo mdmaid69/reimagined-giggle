2162ee4ee20554b3797fac99f4491e8acc348f20
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:18:11 +0000

    square afternoon keepaway season detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_priority_queue():
+        return collections.deque()
