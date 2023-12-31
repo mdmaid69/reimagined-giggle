6822373d6a0dd17feed747d175bb171b2b76d88b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:47:52 +0000

    Jj jump plastic interfere patient

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
