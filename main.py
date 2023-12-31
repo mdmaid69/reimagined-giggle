e9c34fe0266b1c80e20abf1600961957cb34a0d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:40:54 +0000

    avoid promise mess whistle thick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
