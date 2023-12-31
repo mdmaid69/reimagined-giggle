35624808e4a3685d74d3ffc72303df28e9acd81b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:52:08 +0000

    common factory send about tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
