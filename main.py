971ec2b73cd755e5d508346288b2a5fb0088f6fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:14 +0000

    respect minute grand thumb build

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
