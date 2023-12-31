cab9ae3fd308512ab23edc904fa1852cec8993b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:01:13 +0000

    stamp ball pencil body long

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def is_even(n):
-        return n % 2 == 0
+def subtract_numbers(x, y):
+        return x - y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
