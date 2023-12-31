6d758c45da6fefd970adac7af4ab71a0b88cb670
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:54:02 +0000

    calloff choose fortunate mother born

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
