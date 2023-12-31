3a9a7c4686811256bdc6663db56c25a3f68ff8db
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:06 +0000

    broken clean at ball year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
