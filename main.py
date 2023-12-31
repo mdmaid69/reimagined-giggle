fe78b51e9b7c3d02143e14c5f1c7643ee68bd503
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:10 +0000

    quickly promise lunch union anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
