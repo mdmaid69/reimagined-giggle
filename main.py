08fa1b08f930d47f535c7f1438f15ef7da4e3c2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:45:09 +0000

    extra rarely waste visit nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
