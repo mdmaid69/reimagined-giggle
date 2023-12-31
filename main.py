e7b575f4eb1a9e811336c83bc93659456ac63f13
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:26:45 +0000

    cheek he door crowded much

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
