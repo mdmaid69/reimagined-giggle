01c9beff2cd245ad17032ef87d99e9aef5ac510e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:33 +0000

    drop top reduce bright behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
