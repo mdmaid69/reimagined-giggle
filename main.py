4c4d7959c3364f383e3d60cec1542ed2af6c8df3
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:06:59 +0000

    bored either seriously brush please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
