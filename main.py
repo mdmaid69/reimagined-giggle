5073c09096d9043b410bed0e05fbc42a5f0378c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:20:43 +0000

    cry war may beside whether

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
