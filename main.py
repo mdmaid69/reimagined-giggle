631b3b852a1bb00fb0b009cfa9e422717938f8bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:02:21 +0000

    tired high list arrange issue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
