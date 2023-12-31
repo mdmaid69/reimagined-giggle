c1a9aa311bb28bf0b3ba048c616e7447637c70ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:48:18 +0000

    permission custom close father turninto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
