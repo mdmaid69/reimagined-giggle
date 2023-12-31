c26ed4ce0c5850738c3f906c70573ea7c15821d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:15 +0000

    lost fit declaration guide guess

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
