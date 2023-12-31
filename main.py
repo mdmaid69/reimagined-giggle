9db24c38f735f2ff2e791bff65845acba3b7c5a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:40 +0000

    awake freedom impressed birth violence

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
