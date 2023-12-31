0ceea60ce6f130d962f58cbc95c254471b1b4f6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:42:09 +0000

    fact maybe myself influence stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
