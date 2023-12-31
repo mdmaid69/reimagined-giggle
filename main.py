d454f737bdb20e54f1df50ff4a924cad83383193
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:05 +0000

    clever cross declaration grade recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
