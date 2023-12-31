ca6af7ae2b99a3603d4cf9c87ee54da1de968779
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:00 +0000

    position classroom dig bound over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
