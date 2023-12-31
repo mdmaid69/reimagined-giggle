41dcf43bdc54bb5439ff85ae72a34480a12793e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:51:34 +0000

    treat season before discuss hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_speed(distance, time):
+        return distance / time
