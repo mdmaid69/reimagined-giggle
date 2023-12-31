583b0d92d875fe5c5e9de32e15c40f1f5bf2f979
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:07 +0000

    flavour false permission hope breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
