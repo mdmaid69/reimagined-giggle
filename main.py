056d723e31fe5782da5783d79829a7c52164555c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:33:59 +0000

    touch behind lay dream he

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
