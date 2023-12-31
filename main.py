d4567b591737ac19be7eac79a2d9a250b7b48cc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:43:10 +0000

    offence slowly comedown past carry

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
