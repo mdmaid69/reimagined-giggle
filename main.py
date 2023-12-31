3981a3cf15998fea330dbfedc344028e13dac6d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:32:43 +0000

    exercise mixture lookafter rubbish choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
