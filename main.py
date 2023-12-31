443fee9e6b039e2abd6bb5fd79c96e6b6e88d28a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:33:24 +0000

    turnover goout most other ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
