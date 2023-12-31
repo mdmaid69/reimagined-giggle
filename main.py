55af87d28a7a0e035a220eb97cb541732b8cf335
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:27:38 +0000

    fond eye learn 572enter count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_min(numbers):
-        return min(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
