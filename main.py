3e1b18ec18a2abb42d02b4a6a37782b382b40330
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:57 +0000

    walk especially agreement safe dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
