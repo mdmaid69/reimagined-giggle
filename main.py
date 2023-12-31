1e641fd374eb0c6a275698fb21776905bf21528b
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:14:24 +0000

    onto or near Yy Ss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
