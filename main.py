fb6fd088ddd2b544e7d907a1ea4b7cf63444119a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:15 +0000

    fit deserve day getoff pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
