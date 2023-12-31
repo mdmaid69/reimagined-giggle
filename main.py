50a5dedf0ae7a864838834c6122b5b74f606eae6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:34 +0000

    reasonable float downstairs steal hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def count_characters(sentence):
+        return len(sentence)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
