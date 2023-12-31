1490876e3ad2467710187649e0dd68f94302ff52
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:54 +0000

    let finger threaten journey spread

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
