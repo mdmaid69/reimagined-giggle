dd0082f3168b45dd56e37e83c108f20f1e07ff48
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:44 +0000

    woman pull surprised pipe black

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
