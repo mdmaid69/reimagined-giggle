a782af6f288d33e35be41b1562d2b74b140da32b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:31 +0000

    left right concentrate somewhere borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_list(array):
+        return list(array)
