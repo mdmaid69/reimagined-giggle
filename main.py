79885cb2c07b070e551e515a73e26296cae2a8a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:03:10 +0000

    nonsense simple moment pot bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
