f8f9dd485e264194e1e55bbb86ec94bbaee9a479
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:12 +0000

    behaviour case sink formal rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
