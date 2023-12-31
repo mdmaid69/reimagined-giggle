69c4c5b2b6a2dae52e50f86db1ac3e212d1ae19f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:36:07 +0000

    store human really slight occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
