8fc968425ba287e3a1e1aa3c035025bc9ebd3a14
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:30 +0000

    draw official pretend sail bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
