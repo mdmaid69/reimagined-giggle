aaa2fbe29cefeaaea512aa4ab588131fc8f97acd
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:47:50 +0000

    wash reduction respect fill future

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
