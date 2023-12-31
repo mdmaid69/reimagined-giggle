2ff9f3ceefd50feb2592627a909496f9001ae470
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:40:34 +0000

    bury careless protest person report

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
