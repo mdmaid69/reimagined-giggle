2b2e5361e766fe02e20809b5d05760fcfe5f7b23
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:18:35 +0000

    disturbing comment best method heavy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
