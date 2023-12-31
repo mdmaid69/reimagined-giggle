e50ee0c0b4f49d56b1ef19d7f827dc46e9d09cd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:58:44 +0000

    almost stranger start lose although

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
