e34a38823881c3f4f0c4af53b59dc5a7388c9b64
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:59 +0000

    order increase read raw hurt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-def calculate_acceleration(speed, time):
-        return speed / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
