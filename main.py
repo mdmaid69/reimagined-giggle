2cfb185a093b64d7c83b193b476122665923087c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:36:34 +0000

    lookback(on) modern thankyou might report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def count_words(sentence):
+        return len(sentence.split())
