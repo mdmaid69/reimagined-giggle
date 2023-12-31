691bf1bbdece7ad7bf8cd3449cabea79547dfab0
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:51:11 +0000

    course group search hold survive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_force(mass, acceleration):
+        return mass * acceleration
