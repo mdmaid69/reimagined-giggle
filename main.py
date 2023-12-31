4edb6666baa71f195e9e0a4133d06c09b285c6d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:04:03 +0000

    nature stop possibly draw within

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
