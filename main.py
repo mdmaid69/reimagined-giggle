e7134235c9772d2668cb0204e14b524b035319e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:01:30 +0000

    deal store impressive phone theirs

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
