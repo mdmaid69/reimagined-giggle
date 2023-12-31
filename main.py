8b7e06474071232f632af9b7b777b09f231977e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:21:27 +0000

    rank afraid course Hh general

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_max(lst):
+        return max(lst)
