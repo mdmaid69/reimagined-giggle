3ceb8390c4d0caa0c54965e1c79a265de435a7cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:52 +0000

    similar keep connected sort right

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_power(work, time):
+        return work / time
