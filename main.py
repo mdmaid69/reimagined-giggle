2ad7acabebc68036c150adcabde84348cce3ea2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:27:12 +0000

    underneath regret beat attempt 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
