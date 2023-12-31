39c221d42ea18b7ffd63845eb11b08a895e7c74c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:53:21 +0000

    fun guess hardly various detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
