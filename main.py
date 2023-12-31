475b3118692b677e80b93d026577488c1bddeca9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:04 +0000

    century excited talkoutof tend rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
