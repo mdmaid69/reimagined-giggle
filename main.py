bae9522a90e120356d8f1f49b64b8113be0c3834
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:24 +0000

    approval weather exciting disturbing myself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
