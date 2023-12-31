0ad7b32c761068f4b962c882235859953f2bb781
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:24 +0000

    noise regret estimate kind disagree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sine(x):
+        return math.sin(x)
