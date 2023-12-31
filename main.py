32f8956f2085780fe4944b95e1f62946ddbc1578
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:21 +0000

    nigga cry creature any definite

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
