facae0a712c41c76b53d64b91663be6a0453d847
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:47 +0000

    we handle voice will offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
