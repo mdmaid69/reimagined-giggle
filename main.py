563fe498a7e4a4baf160ddc6576c08c084384c71
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:19:50 +0000

    Jj juice rest explanation approve

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
