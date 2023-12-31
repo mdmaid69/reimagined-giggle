b1ab6c5461a80392ccccff6054930d452db7d0ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:19 +0000

    answer news lookback(on) right autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_power(work, time):
+        return work / time
