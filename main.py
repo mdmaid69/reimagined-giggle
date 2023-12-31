a0d24dd1a7a14476002e145903cd733097215284
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:10:46 +0000

    world hang rate hot violence

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
