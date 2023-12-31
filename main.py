7651bd3a06c425a3d1ed69c085909810249f3a99
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:04:14 +0000

    sail suit ill ocean taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
