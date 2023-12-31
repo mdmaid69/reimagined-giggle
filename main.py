45b7892b13ce110379851f17afc7b38f7c63f41e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:36:32 +0000

    serious kind with regional frighten

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import sys
+def print_python_version():
+        print(sys.version)
