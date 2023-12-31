3d5dda549b78cd937d4296728babbd6c2b0c48a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:06:19 +0000

    basically earth remove tour label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
