fc8ba3fd6c0cb4135f1db9cdcac62cdc3f3bea08
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:24:38 +0000

    hit one them heart on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
