ebb8b6763530e79f0a9a0e79f5cef81d3480554e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:52 +0000

    narrow include address side difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
