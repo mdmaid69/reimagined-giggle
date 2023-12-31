baa9c421443bec490459bd8ca0a993647928f9d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:52 +0000

    fond have worse geton attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
