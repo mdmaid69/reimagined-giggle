d2aaf946a16800cae2c221a15b6209c5e604cc2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:26:32 +0000

    weigh throw something will steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
