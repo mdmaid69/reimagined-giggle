bbbc05a0a2aee0e063a915d36b6f7aadf170de42
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:13 +0000

    hear combine hardly something original

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
