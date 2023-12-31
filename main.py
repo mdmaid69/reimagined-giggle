9588619be3a7f150ffe53ca4fd1db762647c8b6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:44 +0000

    ashamed shallow imagine Vv positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
