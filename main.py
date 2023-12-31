3e663e3e1134fe9d7c9c550c556701a8fe561bb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:38 +0000

    pride awake perform effect surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
