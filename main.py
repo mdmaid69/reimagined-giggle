4320f07d1aea316fa25b41923abd430eb4abc8f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:50 +0000

    quiet broken godown Zz machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
