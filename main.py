758f0522f75d087a21533d3080116dfb139369f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:51 +0000

    fear area rather concentration care

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def convert_to_octal(n):
+        return oct(n)
