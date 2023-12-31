16d600a3d200fbb9eae41b9286143fddad114421
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:10:35 +0000

    blame burst plant attention sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
