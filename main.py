d1b43c143a4f432ec8a563e26d4b1159aa582fb8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:45:42 +0000

    improvement heavy much involve shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
