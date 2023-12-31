38df1358356e2c79a18a952b2957705bf9eebad4
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:55 +0000

    stupid hate day up while

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
