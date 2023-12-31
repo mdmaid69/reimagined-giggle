8db73b05d0c4b0bbc0775d757d092232de267d61
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:28:16 +0000

    boat sort long most it

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
