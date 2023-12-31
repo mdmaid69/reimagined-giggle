167335eca4bdf72bf9c3b6cc6374f1b916e113b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:14:38 +0000

    go quit music follow pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
