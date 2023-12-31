5bfba88283dc0ed68d0fb023c1e1312fa42ef48a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:37:46 +0000

    during wave score collection dull

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
