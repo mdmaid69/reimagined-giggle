7d6cac493c45d7ce602ac3bbd4ef507df94a9981
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:24:31 +0000

    bag serve fold emergency build

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
