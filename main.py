5495aeac902b739f0ae92003db3ca3cfb18d6041
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:23:10 +0000

    job gas spare rob rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
