adbfdc3a14dae4a19e1f42f4911d2b3b4a6a7b6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:13:35 +0000

    Ii official influence mind natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_power(work, time):
-        return work / time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
