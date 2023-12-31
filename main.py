0905f5fb3d8433569ddfe4a2eab1368eb90d6764
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:25:32 +0000

    criticism shout hot often bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
