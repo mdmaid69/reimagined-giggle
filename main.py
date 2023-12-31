51030f581e04a173cfd7979af8b7fec23a3a0814
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:53:37 +0000

    awake respect branch fun shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
