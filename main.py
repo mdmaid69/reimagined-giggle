5db996b1bb8e2f2bc7a97d61a68dd8ffd9410e66
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:42:20 +0000

    birthday expect fierce car afterwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
