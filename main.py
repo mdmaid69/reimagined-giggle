537b407895741402fa12ae616c43a073771a8261
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:18 +0000

    conversation average nurse dream right

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
