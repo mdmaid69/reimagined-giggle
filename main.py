448563a16090885870fbcbc8e75aad2a4e5dc924
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:25:35 +0000

    secret cover relative write watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
