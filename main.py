b38748a9e0b0a091084af72a1138ff0247a113a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:37:52 +0000

    method variety callout none sink

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
