4eeee0134b632cd03dede9ffb418df4998df0d6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:07 +0000

    spill number sick wire choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
