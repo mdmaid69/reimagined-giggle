483d0778c41c4d035a6fabc6f8c8a2d7a3729f6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:15:10 +0000

    name pollution comeoff come swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
