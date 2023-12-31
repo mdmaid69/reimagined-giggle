0851dcbca28bb6473edc395384bb93da93f93a61
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:28 +0000

    squash raw only ourselves follow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
