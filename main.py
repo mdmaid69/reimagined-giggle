366f7207ae3f0f07dfbed736af9331a8487f7145
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:25:17 +0000

    circle attention popular set snow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
