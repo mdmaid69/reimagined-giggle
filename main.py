31d44aa63466d2f4f896ee62313f105e50673528
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:44 +0000

    rain ourselves ready skin suddenly

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
