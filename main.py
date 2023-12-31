cc7b23916f2098a10a6e10daf98f1310a702729c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:48 +0000

    cinema damage accidental why government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
