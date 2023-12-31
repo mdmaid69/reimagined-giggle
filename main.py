c2e8e9156b7f2b1bbed36336186298adb43bf21d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:56:10 +0000

    paper comein underneath normal unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
