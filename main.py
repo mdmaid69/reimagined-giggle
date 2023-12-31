d2af892073bc653911007c646c28ddc15bad8047
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:32 +0000

    fair closely reason turnup suggestion

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-for i in range(10): print(i)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
