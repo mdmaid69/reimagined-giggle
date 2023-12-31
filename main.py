289870d8563b5216b2513736df5bd3c6836c5dc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:25:10 +0000

    speed distant face backwards dress

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
