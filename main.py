2c9480e0eee4c1ada03466983bfcf22dc769fafc
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:28 +0000

    his wide bus knock behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import sys
-def print_python_version():
-        return sys.version
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
