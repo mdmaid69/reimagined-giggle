bb8866c4c243820e3ae77d0a6264ce5c6ca93527
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:28:12 +0000

    hard Hh workon getback patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
