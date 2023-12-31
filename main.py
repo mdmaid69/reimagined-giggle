524f48a6085c8bc9a40528d81c2d2c179badf876
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:43:31 +0000

    roll sense soft such label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
