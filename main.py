3ce5face536c05a78529884941cddc47ec30292d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:18:27 +0000

    happy strict interested government further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
