be9530e13c1038aab859ba9d58715a0d8b5f708e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:00 +0000

    565end twist height turnup arrange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
