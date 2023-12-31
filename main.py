32c35ee5e205e9339e72adbea262d936a64d2a1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:59:26 +0000

    571enough shoot think switch put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
+from collections import Counter
+print(Counter("hello world"))
