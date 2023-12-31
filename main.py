fc46da85aa7db2cb4f1aab5f3a81ca303e7463ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:12 +0000

    lead task still female question

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
