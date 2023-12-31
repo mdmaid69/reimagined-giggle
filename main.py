f4ea5b0b7a54c8d68323489028d44950836e2fdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:44:00 +0000

    control easy city track comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
