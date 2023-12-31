b98fa5e18b88852a98db1209b3423fc2ae324a46
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:27:29 +0000

    feature moment taste range straight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_odd(n):
+        return n % 2 != 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
