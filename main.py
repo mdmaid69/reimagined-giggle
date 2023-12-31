2edba7464d56577275ea3a985d507d448265cf98
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:48 +0000

    common day card cover love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def append_to_array(array, item):
+        array.append(item)
