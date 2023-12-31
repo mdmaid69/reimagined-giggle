a880336cd42d5b493d38634b474a105768079483
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:37:21 +0000

    report holiday label violence Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
