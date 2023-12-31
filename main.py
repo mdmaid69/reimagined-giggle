4828f68656f387f78b0e1682d8a300feadee8cd4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:30:12 +0000

    reply body bad still congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def square_number(x):
-        return x**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
