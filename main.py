8a35a4bada8a81ba0a7d62aefb7548b072d1931c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:32:32 +0000

    ask convince strange touch offence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def convert_to_hex(n):
+        return hex(n)
