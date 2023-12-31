a71b4a7de92af3932b79063c7edabf1f644fcb06
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:59:33 +0000

    disapprove speak such blind pocket

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_distance(x1, y1, x2, y2):
+def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
