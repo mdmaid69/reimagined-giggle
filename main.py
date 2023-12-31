8fd206194fe974e29f7d357aa14161eb1395a501
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:27:18 +0000

    dream please all fortunately track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
