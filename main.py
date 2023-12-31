e9f64c35c42dde0a34ffbf94c59f5ac8795953f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:10 +0000

    Gg switch separate convenient moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
