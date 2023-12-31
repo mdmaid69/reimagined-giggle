d1ade2bfad3f0bb38cd5b7056ecc578895c4c7e5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:46:08 +0000

    keepout taste professional tool brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
