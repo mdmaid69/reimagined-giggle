3de5b11a53ea6250e5f64a0cb39cbf067a415eea
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:33 +0000

    successful wander will height grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
