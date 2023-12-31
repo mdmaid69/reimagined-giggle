fe1bf61d3536201ad88c1abdec5fa899554bf230
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:35:00 +0000

    swallow difficult listenfor this body

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
