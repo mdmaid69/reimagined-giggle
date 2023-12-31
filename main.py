59405bb66059a30d7b8f2fef17240798bd6d4f2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:25:39 +0000

    back onto side priority pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_index(array, item):
+        return array.index(item)
