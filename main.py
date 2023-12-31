9c638bbf2093a8eafb4ec5a5db2aa8bb4b5b991b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:32 +0000

    coffee moral terrible compare vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
