ad0457c37e32cccb8d843573e5899a9ad93923f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:16:02 +0000

    task mysterious complete turn tell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
