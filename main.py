52d213d3015e87fcf95517335486cf17d31495e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:47:03 +0000

    pour spray trip centimetre ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def is_odd(n):
+        return n % 2 != 0
