617ecd7f85db0ac4733507377ddb88eb03dd807e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:45:24 +0000

    used news on no murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_as_str(array):
+        return str(array)
