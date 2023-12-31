3149368fc87a0305d05c34ecde8fbc302af2eef8
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:01 +0000

    drink without direct thank getup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_work(force, distance):
+        return force * distance
