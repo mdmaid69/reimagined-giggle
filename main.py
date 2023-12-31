c80812c2f8d8495758e8fa4fb839644d4cc52afb
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:10:57 +0000

    for afraid effort recommend indeed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
