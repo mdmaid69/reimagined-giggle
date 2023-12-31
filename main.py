71e9e6333400acb0150f706980b2b2aad46aa0a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:03:20 +0000

    brief study arm whatever off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
