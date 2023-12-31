30ef2afa91e82e336c2913f7f90caa948340f65b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:36:27 +0000

    frequent anywayanyhow afternoon want science

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
