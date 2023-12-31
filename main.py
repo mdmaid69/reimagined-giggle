c0e1f9b18b78b6304764c6ea73c1d107510e36df
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:43:25 +0000

    male fit collect concentration design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
