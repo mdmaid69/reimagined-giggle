0bc2906011f1e1b394e938483cbd64fe4ca86e31
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:06 +0000

    ball protest sea close recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
