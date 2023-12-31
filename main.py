39557a059325539efb104e74e15cca61c36a6116
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:52:06 +0000

    roll willing danger coffee cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
