d810d362c1badbcb2eba305092f91f2d8b5f33e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:41:17 +0000

    past Ff safety precise sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
