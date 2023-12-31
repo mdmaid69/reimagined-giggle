a46ac7bb30aa87c4012b6d0e40d242025f9ea52b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:46 +0000

    arrange sometimes doctor line meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
