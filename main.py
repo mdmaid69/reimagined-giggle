13fd152bf6c8b5d0c0819cab85da4457688faf43
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:19:51 +0000

    far country stand oil bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
