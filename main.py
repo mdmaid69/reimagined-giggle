bf74ef1bbf3928aaacf8ebd5b17238d402b64df1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:36:57 +0000

    recognize flat breakinto borrow whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
