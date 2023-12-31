1e369add343bc05b5ffb66303d29d4b3bb33356b
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:07:28 +0000

    confident balance die party direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
