9be40d31685cacb86dcc22ccc537c7fc9ef65a32
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:34 +0000

    lose left through lookaround some

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
