3f2f5ec2eee9eef7f4073ca0e657c8ff3ede17f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:33:48 +0000

    willing breakthrough provide loud runinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
