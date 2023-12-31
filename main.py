2702dadb5a9387d12f9ee046d031f8508bedb111
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:38:01 +0000

    this can squeeze gap wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
