dd66213c95dc656f612070f74c428bc27351d767
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:57 +0000

    develop figure right sort live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
