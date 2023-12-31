2f68ca74f2ab2646dcf2f57ecb48fffe2185c9c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:25:20 +0000

    Vv choose whole waste fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
