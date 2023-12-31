b73ffb3c5806e23fee492eaa92c910ebb7824303
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:03:39 +0000

    plan mouth card lonely wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
