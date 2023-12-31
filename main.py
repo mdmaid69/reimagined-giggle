b9f59ce67f7cd03b52aa2f78ad46dcc97e8dd8cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:49 +0000

    authority easy please familiar Zz

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
