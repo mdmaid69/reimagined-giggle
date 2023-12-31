a7b431c71c1e284c3362e2c84175c5b7cc99d3fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:05 +0000

    better congratulations wise ourselves kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
