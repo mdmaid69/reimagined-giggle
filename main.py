c1da7fc6ca0b9df99a9e6639259d524138b197c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:40:52 +0000

    sensible Oo here build development

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
