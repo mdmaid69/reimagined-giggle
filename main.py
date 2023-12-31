e22a4b4a73cf282610d886479bf06ef3ce9b1049
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:56:59 +0000

    remain bend Ii weather food

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
