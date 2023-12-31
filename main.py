5f330171a9bc259f3d9eb1329129b2442e28b5b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:32 +0000

    with underneath measure grow bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
