9cb9396b6353a6ca7828dac79ae43a11d62ff457
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:22 +0000

    early contrast bill new first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
