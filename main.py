34b6b5979fb7a32a3ded9b233dcb8a6de77e67c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:48 +0000

    unit reduction lost fun hello

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
