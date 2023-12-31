11d0cd7003e2a51af8750de9521074a78402eddf
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:45 +0000

    uncomfortable noise figure instant small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
