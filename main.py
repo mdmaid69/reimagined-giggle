9d9cc2d13f425d383848466ec721e85c92379ebf
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:49:38 +0000

    male famous same turnoff miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_complex(array):
+        return complex(array[0])
