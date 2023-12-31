9886269e2ddc34c3fd3859b17c1f29357360504f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:01:05 +0000

    region unite report stage flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
