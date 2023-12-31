6505637f88de5a80edea7d9f9884b82bf7ab3c39
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:18:40 +0000

    lock lend act quickly arrangement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_power(work, time):
+        return work / time
