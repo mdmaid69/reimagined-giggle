e149caf43075abf308dce1fa998374adf6dcf5d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:47 +0000

    night rich comedown regret precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
