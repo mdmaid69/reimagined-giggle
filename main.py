e4abff81640bb3edb39bf4d3173f86d1b5eb98d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:38 +0000

    afford wave getaway obvious sharp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
