028dd7b3a0ea0c60dadda9b2b3d28568c98ac9c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:51:35 +0000

    breakout near lose press explanation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
+def count_characters(sentence):
+        return len(sentence)
