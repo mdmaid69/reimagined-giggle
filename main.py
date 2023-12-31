2e89d022ed58648b976efbc8fe37db0c59958190
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:26:33 +0000

    tidy urgent discover size long

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
