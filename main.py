8f62026dc3fce9a4e47927f1a7ba4877e3325ebe
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:40 +0000

    across control typical road reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
