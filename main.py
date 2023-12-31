758b8834b215096e2f6fc22f8ab34b2cf10eef47
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:30:30 +0000

    way hers cost change professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def subtract_numbers(x, y):
+        return x - y
