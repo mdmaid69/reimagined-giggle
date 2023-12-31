f9f756c5bcdff41c56a95c7de588371d912ed3e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:37 +0000

    row constantly sound plan time

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
