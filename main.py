8dcbcd447453df41fcd8f2c8bcf41646e1bcd28b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:51:58 +0000

    sit broken blame represent weigh

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
