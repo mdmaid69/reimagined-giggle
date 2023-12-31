9dbd449ffdf96adf2c3c7411c43b6fbffc1dcd49
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:05:24 +0000

    identify stand friend ready before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
