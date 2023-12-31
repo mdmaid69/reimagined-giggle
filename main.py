13afbe57341a12fd146c1c3282e3d47a1af2d73e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:43:31 +0000

    father turnover exchange thinkthrough traffic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
