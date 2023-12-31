3c9be464cd67b4edd26d2a3428c9a4b63e0b9a5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:52:29 +0000

    team pollution fault pride tragedy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
