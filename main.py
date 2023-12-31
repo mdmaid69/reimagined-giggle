3f81a1ba728d2a4f2296c14c9e361fc835ab2c6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:35 +0000

    yours ground standard accommodation comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
