50101cff8cf5106c5c798a646daf0ddcf4bd90c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:41 +0000

    along impression hello freeze battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
