7462012e28b9005eba018abe5f8502eb9e6f11a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:35 +0000

    or goout try woman classroom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
