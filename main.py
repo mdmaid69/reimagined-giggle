9c9071dcd101f5059d34eec4e19b4976d6c698a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:57:08 +0000

    spite read now best bush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
