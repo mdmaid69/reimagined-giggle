f7526f85f4d621aeafb408a3600d928a38ed732a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:13 +0000

    exchange lay sound party raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
