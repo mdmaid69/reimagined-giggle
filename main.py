562f4be71e4497be375c5c67c361ace6b9076c6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:43 +0000

    adjective bored direct problem little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
