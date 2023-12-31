8165bca736dd5086347df96f3561473adc375a5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:46:12 +0000

    secret eye naturally whenever father

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
