35e89749578eba9e004a379b338c26a8c984d94f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:05:11 +0000

    count period flavour no backwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
