563de43402c8aa34732d047abcf8266c14bba0e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:52:23 +0000

    day second nurse petrol song

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def get_current_time():
+        return time.ctime()
