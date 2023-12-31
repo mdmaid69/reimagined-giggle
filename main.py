c1ebc89ecd2a0202ad7431554728c7a1dcfcf611
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:40 +0000

    game rush then mail impressed

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
