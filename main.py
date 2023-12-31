6e7e91787a8ab5ca5e73197f870ba7b7e5b1aced
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:06:24 +0000

    affect you silent bright careful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
