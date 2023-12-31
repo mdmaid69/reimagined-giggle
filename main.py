8c588ee0f640b7d312a45991e40e80d47601ff50
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:55:42 +0000

    tax 571enough confidence belong tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
