77fca3bf7cff86a2e0f060483a511ddb6be04d49
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:45 +0000

    free about standard while contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
