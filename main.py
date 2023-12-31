ba9d6edaa9faf79aaaf3326f282cccf059c4dd9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:21:51 +0000

    need smart build cap crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
