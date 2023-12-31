8c559c591797a8e60f8a68b2f90cf541b6f3c44a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:02:51 +0000

    trick argument mystery please government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
