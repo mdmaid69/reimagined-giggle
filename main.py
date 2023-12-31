adc56b186391b6d6b21663905490fa4bce0dd30c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:05:48 +0000

    friendly circumstance fit fast secret

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_float(array):
+        return float(array[0])
