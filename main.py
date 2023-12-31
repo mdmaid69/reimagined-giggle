31f6c5780d819894b52fe4f5817f1cfad2a54527
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:14:48 +0000

    welcome already round bit contents

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
