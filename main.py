13767d76c8b13e8b24265cd9ba3aafe05d4548be
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:16 +0000

    sure sore easy dance desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
