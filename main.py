26de8a2f71ce68134027954eecef5d40f312c270
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:45:09 +0000

    because cope closely build geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
