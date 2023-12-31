bfd27a33cf36285aac172e4aa2944d37e2bde269
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:23 +0000

    show consider Gg discuss fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
