3fc74a6ca4b0355c8b67c9cab1c487dd8ad7698f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:32 +0000

    worth protection 570enjoyable steal interest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
