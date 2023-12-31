52d68779539afaa6152221a7998c6bfa8755bb26
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:11:54 +0000

    Nn pretty less evidence Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_as_list(array):
+        return list(array)
