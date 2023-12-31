218e6bd0690b9e5ecc06cae741c3794c60176554
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:31:49 +0000

    what block case period send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
