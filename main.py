818b38b2009990396d9cba963badedad2f5d5404
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:29 +0000

    dress fit space price happen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
