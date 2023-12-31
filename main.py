a28dcdddb3873c8e354e8d52b23187e37eb4f935
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:54:42 +0000

    walk speak spend round seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
