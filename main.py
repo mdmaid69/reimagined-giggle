6f24c6ba2de5034e312a35c7760026d084cfdce0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:37 +0000

    order listenfor several eat behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_user_dict():
+        return collections.UserDict()
