fe5186ca75c1e06175646d4f66270a1150ab9947
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:04:18 +0000

    petrol wonder course then university

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
