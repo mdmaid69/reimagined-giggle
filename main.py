4986748e59b9df2cb6f38e44b19230891d5f6071
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:10 +0000

    television boil appeal point whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-for i in range(5):
-        print(i)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def convert_to_hex(n):
+        return hex(n)
