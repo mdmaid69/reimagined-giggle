4f588bac955744a445a26190b2f0f526f2a8523a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:19:35 +0000

    where treat pleasure limit card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
