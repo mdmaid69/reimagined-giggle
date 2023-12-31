90e3deef3002b052543c7e0397bdc7961d9f8a47
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:20 +0000

    goon guide base turn clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
