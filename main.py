64a9cbeb3e2d7790eb6960ff2a9e1c579c6486de
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:40:36 +0000

    limit takeout whichever fact famous

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
