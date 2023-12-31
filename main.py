c2f79f6f24392811310129eb45cb9976c6f6a813
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:46:47 +0000

    turn frighten mark Gg react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
