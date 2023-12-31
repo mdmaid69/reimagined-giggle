86029bf86b738da28c06b9d02304c794d6606f72
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:39 +0000

    572enter off tonight hope frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
