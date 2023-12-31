0959e0e1af95e024c3a7d3cbcfc5baedc0f492d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:51:02 +0000

    cinema wind precise type curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
