e907069cc3f335aca7de024625bc503d1077b415
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:36:52 +0000

    progress sure bridge bringup attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
