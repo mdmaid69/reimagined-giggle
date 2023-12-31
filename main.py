4322cacddec52ebfa4223c22ccaee226feb9d340
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:50:58 +0000

    second message high border accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
