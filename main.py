44ecd808c993aee3183f1cc362f9a392a7327638
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:24 +0000

    dance university road noisy actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_acceleration(speed, time):
-        return speed / time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
