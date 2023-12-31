bfef9884b8061636e8ca78edb866841506855c29
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:10:44 +0000

    bringdown breakthrough cheap powerful warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
