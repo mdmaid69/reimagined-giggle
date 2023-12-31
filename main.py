fcbcc7679b63df0fc839af256f365828d36642b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:57 +0000

    space OKorokay hit use keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
