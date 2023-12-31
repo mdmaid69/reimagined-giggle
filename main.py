173217a6af08ed1db16aa062f10d45db47fe9184
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:49:52 +0000

    off original shape path past

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
