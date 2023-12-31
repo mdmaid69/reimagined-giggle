2bd6f58bfbbf7f1f0108ccc2bf1a84f619105f93
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:27:09 +0000

    home quality listenfor prove thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
