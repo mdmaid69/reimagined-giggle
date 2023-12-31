7d04b374eec7c2ae7b58d3d6fab98241638a1d4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:35 +0000

    get steal through school appear

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_area_rectangle(l, w):
+        return l * w
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
