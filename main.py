1e89aa92976722123bf20a73b8412df9d7bd9a79
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:23 +0000

    steam listen young discuss excellent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
