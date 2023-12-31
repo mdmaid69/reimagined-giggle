7ebdbde879220a42d6a7b909cf81b0ed72c7d1d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:18 +0000

    generous distant never realize slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
 import array
 def get_array_as_bytearray(array):
         return bytearray(array)
