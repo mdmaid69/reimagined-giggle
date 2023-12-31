247e403b9199d88a73877019f99054b875c98284
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:06 +0000

    degree measure towards direct platform

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def sort_list(lst):
+        return sorted(lst)
