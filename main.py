0668aab0f0ae1c1d74aad2841d5f0abc0f2395b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:19 +0000

    tongue dress attempt summer influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
