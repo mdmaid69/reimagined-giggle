2260b06ea132b3ca36501ee371f46fcb8d924678
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:28:29 +0000

    return born ought concerning someone

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
