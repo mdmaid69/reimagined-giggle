c0c3d2654c6a70969c86247bb1d16e2017b602d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:41:28 +0000

    walk strict report chin pay

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
