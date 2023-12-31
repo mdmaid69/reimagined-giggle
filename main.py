675291dd3492ea06cca7f0be1a5ce8fd24f51a53
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:03:58 +0000

    floor mend rid after answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
