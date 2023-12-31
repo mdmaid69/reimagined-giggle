2e3243c1683dfa1065ddc47166719397b9021df1
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:05:37 +0000

    job luck accidental hurry towards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-print(math.pi)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
