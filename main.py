5d100387d603515e63ccea1a05b26e1d0ed8db99
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:35 +0000

    improve wait bag language hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
