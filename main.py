b098f62eca7e3248cebe7a50f99294092ca2fca2
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:17 +0000

    area godown comedown pretend classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
