385c777cf5808accb692580f1cc904e66a5e16d8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:23 +0000

    pronunciation address start sudden catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
