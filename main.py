4f9a94a042a4f5802ee740847997bbdae2464c69
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:55:20 +0000

    share fortunately furniture it fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
