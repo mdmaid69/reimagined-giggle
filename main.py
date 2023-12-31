ee903c59742170dd0b83385278990be7aacb1038
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:44:51 +0000

    steer spite roll pile fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_frozenset(array):
+        return frozenset(array)
