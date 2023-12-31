5d34bbc4a668379a1cb242d9a5415269658b927c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:46 +0000

    565end pin allow gate attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
