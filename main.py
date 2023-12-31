a9f39fe80f108f71c4c249d32674ebbbe3175f4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:27:15 +0000

    contrast subject event never meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def square_number(x):
-        return x**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
