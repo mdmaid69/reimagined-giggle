c11130891bd44a35f5d6c2fb3ff1d03b20b7e168
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:50:10 +0000

    brief male compare anything window

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print("Hello, world!")
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
