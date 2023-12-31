25174bf574972d116682c6986e6ec9235ae6a7f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:56:01 +0000

    practise know name temperature very

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import sys
+  def get_python_version():
+        return sys.version
