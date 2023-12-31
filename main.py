acfc8fd754a56966132dc2f21a295c4f06500e08
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:52:10 +0000

    rob clear alone under sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
