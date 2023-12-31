04205722a1b224dc651cd6de267743e896c85783
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:58:19 +0000

    gate soak be have break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
