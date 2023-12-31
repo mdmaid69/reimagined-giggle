4f2cbda2115283f9dcaac143a3daa779cd4c0978
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:20:02 +0000

    shut risk tread score hear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
