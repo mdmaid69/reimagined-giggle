e0a209929fcc398fc2fe8db820191aa95927c2a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:21 +0000

    add sink pain suggestion towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
