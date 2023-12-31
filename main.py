395f9c7daf43be07b4e93d868e105a447f4a1e1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:48 +0000

    press giveup telephone plenty beat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
