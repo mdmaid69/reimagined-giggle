f0edd7d2bd70ded9257a6fc97fc6d94bb7772934
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:49 +0000

    month business arrange left themselves

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
