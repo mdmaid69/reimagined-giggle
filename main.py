2bdf61150a23c47b3b73adde56f9a27cc615d0d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:59:27 +0000

    boring dress slight male gentle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import time
-print(time.time())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
