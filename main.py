c0c0ead45cd4a3cb14cfd4ac06c26b81a01db295
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:28:15 +0000

    accidentally disapprove persuade both deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
