5fef2ac5eb87c2ae614bb9de9008fa6a2020623c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:03 +0000

    unite winter argue 569enjoy tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
