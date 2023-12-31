80b747e6b7567dd89b6a2a65e0f086c763b4590d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:18:13 +0000

    lonely mood sore door anxious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
