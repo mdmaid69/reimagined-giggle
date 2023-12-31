fcf191390b626c358fa13fcc14d857fb2986f89a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:02 +0000

    medium collection some sport accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
