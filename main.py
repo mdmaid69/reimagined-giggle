31a39bb36b65f84effcd133604634b7f9278b027
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:01 +0000

    drop account suddenly lonely trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
