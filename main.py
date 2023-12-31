7ddf21ca5686376d187dc4c869f033cb4a462eeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:34 +0000

    but Ii duty uponon exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
