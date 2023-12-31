7af09349362b194b2b63c23ed72162cfa53a148a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:35 +0000

    draw almost run holdon regular

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
