0902287342423204d0e76dcf57fdbc2ff7ca89fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:29:53 +0000

    sale tidy shut throw forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
