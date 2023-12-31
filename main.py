c5b83a9ff26c79fe15fc3565c30bd1f86d52b35b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:55:38 +0000

    loud talkinto food radio change

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
