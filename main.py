76ef4b42fa87ab64f8460d32263a96102af12a76
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:16 +0000

    think petrol country ahead character

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
