bd6a1b9a1c38491e5dad1a6e34f5b551eb5069e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:35:08 +0000

    collection of free hardly extremely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def reverse_string(s):
+        return s[::-1]
