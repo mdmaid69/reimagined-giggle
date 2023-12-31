93c2343caf019aafbcd17c931a9d22cc0d7d944b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:46:28 +0000

    gooff beside time inquire direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
