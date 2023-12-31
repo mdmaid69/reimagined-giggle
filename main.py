f0594a8a4aa0109462b81517d11fdb300e5f04e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:28:33 +0000

    avoid glad shout appeal wealth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
