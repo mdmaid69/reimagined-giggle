ba9e8f91b5f2bfb6e496e728761fd4377e07bea9
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:50 +0000

    sore doctor check bottom to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
+import time
+def get_current_time():
+        return time.ctime()
