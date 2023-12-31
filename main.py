c1001b537364629a3a3115f97d4ab212e1e9da92
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:47 +0000

    fork basis punish there calmly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_int(array):
+        return int(array[0])
