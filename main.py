a71eb0dacbb533f45c1537de8ba11f08fb2ba26f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:50 +0000

    succeed definite during strike difficult

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
