036ca5ec6513d0e977d25b1cab6491a6e0f06a8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:30:49 +0000

    unless hollow why spoil position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
