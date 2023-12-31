fdf353807b16893191a16f6ed32234a12f8583d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:14:32 +0000

    tip comeround strike guilty stare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def get_array_as_complex(array):
+        return complex(array[0])
