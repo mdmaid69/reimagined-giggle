7f305754a738f02a45b19fda8f4825a18bc03de9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:33:07 +0000

    fairly last supply spot best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
