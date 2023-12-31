df00a4e9a3a7ee2d9be7f875085f9743f89f1375
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:16 +0000

    height behind both about funny

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
