63e5017e506894b17bdf2a6160df0a1eafcc56f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:46 +0000

    copy much guilty will mother

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def get_current_working_directory():
+        return os.getcwd()
