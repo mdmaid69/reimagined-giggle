8a48b7d820864705a49754a5b4317f3db3709673
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:51 +0000

    turndown unlucky sit there callback

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
