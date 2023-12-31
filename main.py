0e82eaf89c014e969e3c83ff9199d36404b4a8a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:38:56 +0000

    furniture thirsty squash contribute me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
