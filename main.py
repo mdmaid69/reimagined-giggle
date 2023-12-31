dc7f712d0a0bdf7da8e7526af38c0db42414bc4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:21:55 +0000

    nurse demand practise gap bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
