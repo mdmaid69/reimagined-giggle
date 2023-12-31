c79fa506f97a69275971d118c633067fdd996821
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:43 +0000

    scrap there birthday birth ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
