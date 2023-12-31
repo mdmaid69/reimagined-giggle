2214a1b7f3dfff158069e74e13b4122d461f981f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:10:48 +0000

    accidental gather business finally criticize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def convert_to_octal(n):
+        return oct(n)
