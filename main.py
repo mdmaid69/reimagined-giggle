11a17783b0aa8054c832e2a9cd80d3426b13076f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:55:28 +0000

    somehow nearly guess pen fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
