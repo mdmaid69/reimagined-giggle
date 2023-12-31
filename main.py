e39e7a451b14fadcd91fabcc4a7dfa4bc863c47e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:48:31 +0000

    straight wake deliberate rush object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
