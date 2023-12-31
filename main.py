eace6f37eedab8b79a4613c71c03a1511af06f04
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:35 +0000

    by important swing positive match

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
