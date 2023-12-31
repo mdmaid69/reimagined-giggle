a7db72be088ea1f3ae2233e91cbd8c925b6f3240
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:57 +0000

    must short blame plant future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
