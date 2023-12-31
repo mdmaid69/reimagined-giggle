33e24c49a290e8d3c6001ab69f4d5e9aaf468845
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:16:04 +0000

    badly spring split disagree software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
