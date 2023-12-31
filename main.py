2ea82a51c1cbb0aabd8fe483ac18730f867bc83b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:05:10 +0000

    finger character scrape remark waste

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
