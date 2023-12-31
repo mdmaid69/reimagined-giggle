04f13406fab384407694b2a86efd53710389a571
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:41:48 +0000

    explanation after yourself emotion like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_rectangle(l, w):
-        return l * w
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
