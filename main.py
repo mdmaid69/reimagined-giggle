03d7a2997ed9669e20ab234de47d6a5e7c02d5df
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:04:45 +0000

    school attach join excuse path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_set(array):
+        return set(array)
+import time
+def get_time_since_epoch():
+        return time.time()
