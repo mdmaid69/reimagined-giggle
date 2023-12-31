6a33c9dcbf5c261cc5f204967ee946fa39ee7455
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:53:05 +0000

    bath keepout about unusual dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_base_name(path):
+        return os.path.basename(path)
