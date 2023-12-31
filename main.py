84ff87078c60bff608ecf54c974b9d242d562286
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:27 +0000

    report bottom treatment people within

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_list(lst):
+        return sorted(lst)
