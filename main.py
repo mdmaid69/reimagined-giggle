078a7384019d340288d11161c8dc59b4b4c07d0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:49 +0000

    tear attractive idea contrast bridge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
