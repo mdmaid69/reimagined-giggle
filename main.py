b2377c9609525b0b48ffa22a197c9d7c4041fd6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:06 +0000

    accidental word store image finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
