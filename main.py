9e04bc61f6fad6e7bc0bb857384433f2f5776f90
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:56 +0000

    apparently soil while bottom ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
