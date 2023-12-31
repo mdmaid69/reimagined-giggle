929950638206b0b5a3572c9c2191c7ed853f4995
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:17 +0000

    never punishment border seem present

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
