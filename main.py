6fa42243fa329cac9df304daf13a19d20243da76
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:13:38 +0000

    rain pretend aim equal ought

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def cube_number(x):
+        return x**3
