9ac5ddfacae762deef25f449e4d7542fd7d4e3fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:49 +0000

    add only please hope mix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
