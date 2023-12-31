72f5bac8ef1faa67abd0ad6f3abb2b50fa6b11bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:44:46 +0000

    previous store fly even confused

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import time
+print(time.time())
