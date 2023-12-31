750aad67ba01478dc15723007a6608f8139b9a46
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:53:48 +0000

    still within price sticky present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
