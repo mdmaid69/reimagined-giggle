78fd1b51f6571bd8e0c5d05bfea420795b98affa
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:54:31 +0000

    dead hold instrument flow cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
