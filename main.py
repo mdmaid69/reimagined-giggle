24bb9f3bf54b149b849593f2303b25b56322a327
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:13:53 +0000

    funny by board clear national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
