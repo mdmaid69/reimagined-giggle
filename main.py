733c8bce16c1bf34ae415f6453b8aa34bbc2eb21
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:18:26 +0000

    lay go schedule me when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
