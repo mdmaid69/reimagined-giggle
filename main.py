3d5c89ba08616bc2e09272f2c90c856899c038a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:00:30 +0000

    season dish waste satisfied hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
