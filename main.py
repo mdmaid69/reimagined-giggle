4c2493672913b7b0a6279a43d29e21e424d88c43
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:01:08 +0000

    today anxious react OKorokay 573entertainment

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_typecode(array):
+        return array.typecode
