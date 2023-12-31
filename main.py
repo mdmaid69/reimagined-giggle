02e598b5fcf6f081610f06d599b79e61c5dce4d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:41:15 +0000

    seriously game any poor promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
