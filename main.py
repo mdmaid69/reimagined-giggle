acf8d6bd7d00fe3dee7f1c661d6ecd0947126bfe
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:41:22 +0000

    glass region doubt noisy trousers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
