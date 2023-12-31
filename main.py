55aaf39917931b640dc364dd7d23a414f8003a8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:55:33 +0000

    she hand feeling determined satisfied

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
