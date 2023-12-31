12f22c3c3587864c68c8c5b8732c0d7c91652456
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:34:55 +0000

    cheat present capital gradually rob

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
