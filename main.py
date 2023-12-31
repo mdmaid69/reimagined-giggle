03aa21eea4a1e032e780e13a9b81cdfa71f8e4d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:41 +0000

    read close winter trip wind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def square_number(x):
+        return x**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
