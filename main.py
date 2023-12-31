9188acebcebf6c547c132b4cbca8a2000639084b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:20 +0000

    pour chance tape sad promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
