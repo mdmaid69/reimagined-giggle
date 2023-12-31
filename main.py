5ef09888416d50e075bff62c783d1152eed3b3e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:46:53 +0000

    reduction combination meeting upstairs high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
