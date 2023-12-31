3e0bf3ca028ab2d3700d86b427ca533f12d85099
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:58 +0000

    class load relationship count finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
