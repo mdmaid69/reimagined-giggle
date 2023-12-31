bd3b2e9fc04858f4f3ae62a9570f880946ec7b4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:25 +0000

    record through our responsibility scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
