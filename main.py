eefafab4b93ff0d4604e3d34118deae7a15b3d8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:35:29 +0000

    activity art already also away

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
