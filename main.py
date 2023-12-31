19d476571ec9b15fe3866605ee195b952866bc8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:30 +0000

    complicated awake Itself word appeal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
