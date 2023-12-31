6bd5eefebe0113cd84b8afb96c4e60795820ac06
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:03 +0000

    cook careless justice keepout left

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_power(work, time):
+        return work / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
