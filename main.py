ecbae6333b3490cc599981d2a885e4de80f09001
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:44 +0000

    whisper mean change rob while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
