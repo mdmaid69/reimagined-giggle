c6e5dc9d569d83a7c5d1acff7cf3e33389ec5506
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:34 +0000

    add runout interesting plenty over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def print_python_version():
+        print(sys.version)
