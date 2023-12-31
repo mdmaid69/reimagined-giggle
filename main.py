f4bc55a5332e4e65fddbeb3c15c48d4bfd6df204
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:12 +0000

    fact treatment stop probably show

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
