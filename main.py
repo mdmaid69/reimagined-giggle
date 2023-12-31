38352b71ddbe5341e71b8b2b41dba137f07115b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:18:55 +0000

    hole wish smoke middle objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
