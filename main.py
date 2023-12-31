17efc08abb5fd3e394d62582f204e025b5cfdaa1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:19 +0000

    across send convenient play recent

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
