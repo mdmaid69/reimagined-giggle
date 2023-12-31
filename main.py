68187688bd289374a0c6acd75772534defcb5d9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:09:16 +0000

    getaway stop zero which steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def clear_array(array):
-        array *= 0
+def check_if_array_contains_item(array, item):
+        return item in array
