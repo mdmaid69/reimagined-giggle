46810c5ab8fe0b492859818944e20d8539802211
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:10:04 +0000

    talk can sensible plant side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
