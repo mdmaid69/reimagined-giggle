1495ec74acd5f48b92ad0c6e0b8c5f595efe676f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:12:53 +0000

    pass grand battery bush thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def find_min(lst):
+        return min(lst)
