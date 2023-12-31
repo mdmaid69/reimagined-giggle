198a555f20eac5cd90866551ff3fed34e759dc3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:48 +0000

    otherwise hair report shut terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def convert_to_binary(n):
+        return bin(n)
