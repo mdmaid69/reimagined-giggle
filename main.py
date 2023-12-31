bd37098f14f11231f9773c58900c7a948d734361
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:27:26 +0000

    react ear appeal story matter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
