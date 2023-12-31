0ef05c2f00ea3f82c196994247789baef91ec6b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:19:38 +0000

    fresh callat coat comeout cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import sys
-def print_python_version():
-        print(sys.version)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
