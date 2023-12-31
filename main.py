6164c28a9b8f385749dd0c3ac5255a696e46d49c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:14 +0000

    yesterday breakfast shine rank back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
