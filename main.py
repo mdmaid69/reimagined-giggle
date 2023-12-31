4f659101026ed6ba74703790e17a151d74c2b94d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:19:40 +0000

    succeed petrol cause rob miss

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
