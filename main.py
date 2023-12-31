d3f8f4432a7b3ba19494c9d4bc254983b743e9ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:51 +0000

    together sink gooff side share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
