5270dfa5c2b5af4cef472d8b46013b5a238ebcd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:01 +0000

    snow wind proud respect call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
