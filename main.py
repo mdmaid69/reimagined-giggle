5d155bcc14d12c825267f53e79a02d0782b527ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:46:02 +0000

    defend well pair strike design

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
