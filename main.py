11663b8547b4079390a83ad371c082f8cfe6233a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:52:10 +0000

    problem divide break culture father

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
