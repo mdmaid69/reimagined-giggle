e2cbbee40005d54866ed502c0febcc7eaf70ad82
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:40:41 +0000

    tooth nerve move appear relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
