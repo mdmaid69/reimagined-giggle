4114d8e2f3a00578b2f840cda346b9b6209bf196
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:14 +0000

    565end everybody bet test rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
