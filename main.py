3f5771d3ff626873cac632a2d4b1d5debba9fd3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:01:50 +0000

    cross ball suppose bringback frighten

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
