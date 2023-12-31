0b4c4841efe20ab6bd3c4942368aa640df0c28be
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:47 +0000

    scrap suit object fit autumn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
