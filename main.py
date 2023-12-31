cee743ea4b561c976759a5695969b76b2e81de65
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:56:33 +0000

    please your report makeinto hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
