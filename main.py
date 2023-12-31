83302de4e3ae25749027c86e22a91c268f54d700
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:16 +0000

    well week interesting burst solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
