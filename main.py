feadad3e0062b59851f489a5d45856c36b8ca020
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:36 +0000

    decision talkoutof cope shock quantity

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
