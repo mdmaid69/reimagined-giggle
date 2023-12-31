e518296bb83ec2c555e3373e261fcc7ce85ec27c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:06:05 +0000

    cinema figure past may muscle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
