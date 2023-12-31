0d31dda63c09591df47789ca733023e4e1b8b245
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:12:49 +0000

    deaf allow substance expensive cross

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_speed(distance, time):
-        return distance / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def find_max(lst):
+        return max(lst)
