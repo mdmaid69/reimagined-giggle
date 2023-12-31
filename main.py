d87c68b2a93c17e1efcbc43de264f4f422ab72ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:44 +0000

    result there type mark influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
