b83b1cc824f3506ccd6d09def011a062fc400a24
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:09:53 +0000

    introduction noisy estimate sell it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
