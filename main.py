fed6337ff8d4d81205c2e30eb78b3bfa25256557
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:16:05 +0000

    turninto around remain before no

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_area_rectangle(l, w):
+        return l * w
