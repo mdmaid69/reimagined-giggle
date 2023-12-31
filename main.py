a0e22bcf4e8b7babe067ba866097d6ca5a797178
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:16 +0000

    bringback sensible Qq light workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
