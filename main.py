c32044625ea6c6d4bc502ef30240a1cc6a1b429e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:50 +0000

    similar party cheek only satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
