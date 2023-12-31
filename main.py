8d2e612ced59dc6cefb13cea4c56f8db4cc1b022
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:21 +0000

    brief extraordinary thick certainly outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
