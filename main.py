dce163f569f17d1ee65cb651136c7d654073aff1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:56 +0000

    petrol ear backwards stop appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
