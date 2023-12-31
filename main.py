79d085504e8827c706966e595029a674a2ee86d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:08 +0000

    catch money cheap when mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
