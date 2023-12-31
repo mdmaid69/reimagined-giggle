89d61e416ffe13b9723b896ee02050a6bfafa20b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:06:14 +0000

    less fortunate fold night 566end

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def subtract_numbers(x, y):
-        return x - y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
