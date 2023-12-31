bf29cc068d4a3bd30b470e166f3bff301ecfc53f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:16 +0000

    education hello uncertain mine obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
