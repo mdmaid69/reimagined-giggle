9ece3cf2ee2e19849be31cfc5fe39bfa39c1eca7
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:18 +0000

    beneath interview loud none upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
