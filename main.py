d5cd7720826b30c6e93692061414e590b9ee8aaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:53:15 +0000

    original pencil last guard have

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
