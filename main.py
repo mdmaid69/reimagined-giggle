3ee31f14fc096f23c42102f83a343cdbb02d1d17
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:21:56 +0000

    usual schedule shut concentration typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
