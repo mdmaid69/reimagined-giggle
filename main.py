eb1b098ee18a9d2d899d2d9e82c6eeb894c6cf36
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:40:10 +0000

    correct muscle pressure shallow appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
