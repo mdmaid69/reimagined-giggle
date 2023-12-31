2d8c5c5c95be6e02f2b7b35dcfa1e1df779efa8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:07:22 +0000

    talkback window drink children news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
