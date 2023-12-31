86fea03de3b7a1cb134d396a1d2aefd3a5800c1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:58:52 +0000

    extremely create order spoil side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
