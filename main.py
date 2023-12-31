4cfe3c51e4abc745d2ce7adc5dd8add0b03d9e1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:33:59 +0000

    load mad rest instruction you

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
