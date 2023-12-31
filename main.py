876dc01c0475711bd02188ecc0fe044fa7204628
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:17 +0000

    contain produce cook head agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+for i in range(5):
+        print(i)
