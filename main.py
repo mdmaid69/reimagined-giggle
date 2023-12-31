3678ec7d30ff811ec05831dfff39dcaecc8aee7c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:55:51 +0000

    dangerous not kind miss coffee

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+def square_number(x):
+        return x**2
