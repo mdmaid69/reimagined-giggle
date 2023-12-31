4ed6d062f73dc006b8182ef406454149984f73e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:01 +0000

    goup confidence earn secret Kk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-print(os.getcwd())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
