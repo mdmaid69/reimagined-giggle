bee538ecabfacb15fe1b62cd95889ca8eb946c93
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:19 +0000

    swim string prepare model operation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
