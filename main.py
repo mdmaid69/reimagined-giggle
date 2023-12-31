ff4025adca902435d7bd975ab0254703f593c122
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:11:26 +0000

    slightly bye cry relief pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
