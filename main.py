99d98d2ed1d535470d4bde8cf7ac5431d77b9c5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:47 +0000

    confused part pupil tragedy score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
