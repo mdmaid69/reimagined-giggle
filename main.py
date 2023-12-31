cac3f43dad3ce0f0c1d8fc7a3caee9e39c3441af
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:05 +0000

    turnback platform classroom preparation mistake

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
