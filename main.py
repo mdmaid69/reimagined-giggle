32b5785f939b425b2f9af73e3f409a95a61dcc43
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:06 +0000

    cover majority serious everybody judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
