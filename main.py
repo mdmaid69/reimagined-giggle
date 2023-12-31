b21b859eed560c83b90362efe1110f8f7710e669
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:09:00 +0000

    present mistake friend sort trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import sys
+def exit_program():
+        sys.exit()
