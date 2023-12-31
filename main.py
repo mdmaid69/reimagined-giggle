cc292a8c735b0e8ff47ab0c7dab39c8793e117d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:48:10 +0000

    emotion improvement platform goback(to) skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
