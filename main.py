6e9c683aeaf1403eaea2f7ce9385be133a8f954d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:49:23 +0000

    influence workon block waste problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import sys
   def get_python_version():
         return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
