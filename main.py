dae36cefa00133b115a43a02aac1f9a58e6f3205
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:40:24 +0000

    taste city conduct judge exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def reverse_list(lst):
+        return lst[::-1]
