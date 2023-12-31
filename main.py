6a43825dc2699b8511ea6671388e9f5553a1f906
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:18:30 +0000

    say about part average tone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
