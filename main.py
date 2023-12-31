ead5eb6fba55b63e6e2dcaa90d26bab35de584db
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:20:04 +0000

    book major protect excellent everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
