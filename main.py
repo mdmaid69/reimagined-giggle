0601e1acd9287d453a54fedb4dbf89268fb09f53
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:20 +0000

    approve fold close marry favour

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+name = "Python"
+print("Hello,", name)
+import collections
+def create_user_list():
+        return collections.UserList()
