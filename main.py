e560fe307b9d060ade4a18c5c7b55e912630350c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:21:36 +0000

    OKorokay embarrassing swallow visit valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
