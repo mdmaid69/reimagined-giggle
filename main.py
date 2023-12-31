2592224682bda78293531cb0e98d283b4575070f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:33:52 +0000

    doubt unable kind no thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
