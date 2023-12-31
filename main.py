245d41efb946e9a19f0a3fec84b3d8fe847f1d34
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:17:11 +0000

    how proof autumn choice thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
