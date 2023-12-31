4d02d04a1e8721722519571236e606a3bd43e1bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:46:38 +0000

    can keepoff trip radio content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
