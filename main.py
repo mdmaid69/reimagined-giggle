f73bb444e462539002d73493765152e755b1791d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:10:31 +0000

    innocent simple order collection tour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
