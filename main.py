8b52a91a12b5b2b5ee55c775813680d407b04088
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:25 +0000

    wire middle promise bridge as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
