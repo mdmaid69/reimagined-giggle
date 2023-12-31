a2ea8d1acebab6c2feaa51d51e0af65e18e66053
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:44:41 +0000

    debt clever balance upset sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def is_odd(n):
-        return n % 2 != 0
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
