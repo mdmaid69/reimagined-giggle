91c04db0c3ec0fb9e50a5fed532c1d2344cadaba
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:08 +0000

    congratulations talkoutof not communicate risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
