1840e5088fd66de6575b29b8a2068dd2bb44a950
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:25 +0000

    physical 572enter rarely nose to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_circle_area(radius):
+        return math.pi * radius**2
