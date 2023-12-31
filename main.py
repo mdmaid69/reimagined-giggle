3151a0a10e421ea93a7f855ef7a3221d197db62d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:51 +0000

    clothes professional feeling me near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
