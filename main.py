19c1ce1cd6c3799ad4ac58fc3cedf1fecf993609
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:15 +0000

    lost snow play phone practise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
