d7e387af54e78fa7b0a373817dd1e953b1c6db98
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:21:40 +0000

    uncertain stand mark gradually uponon

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
