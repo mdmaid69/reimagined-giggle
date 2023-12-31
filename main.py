d74fca80305bc51b4f72c1d73633ce293392313e
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:31 +0000

    float frame offer thumb otherwise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
