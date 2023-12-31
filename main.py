590d7e3646a4cb0421cbc56487ac0a12aa8814af
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:16 +0000

    trade poison necessary store straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
