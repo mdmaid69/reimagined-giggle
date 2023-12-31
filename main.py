af2674601621ce74989f99fe9524fb3af692e131
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:10:16 +0000

    match square possibility situation stick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
