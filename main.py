a5726b92c81eb0f40e11dc49867657f44cdecd4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:10:16 +0000

    pencil talkoutof either bring answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def find_max(lst):
+        return max(lst)
