43c1abebf33641f3a45601bea00f1a5d388e5c9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:16 +0000

    basic judge hide match rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
