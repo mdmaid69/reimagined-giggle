e84a3d1ca086a4fe3dfea725420d9b22e1d30bc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:26 +0000

    them divide finger meal step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
   def get_base_name(path):
         return os.path.basename(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
