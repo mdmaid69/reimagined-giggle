269cad56d08d5be2802f3f016f42eeb0b47f726b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:23:58 +0000

    getup study giveout row yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
