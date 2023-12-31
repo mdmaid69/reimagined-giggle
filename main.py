a33a83678de291ce3e28a99824ef05b565ef6408
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:37:15 +0000

    upwards danger spill target slip

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import logging
-def log_message(message):
-        logging.info(message)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
