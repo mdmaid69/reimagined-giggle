85194c603fc868accd2896b2dd670d8beec6b714
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:01:47 +0000

    discover upset notice term several

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
