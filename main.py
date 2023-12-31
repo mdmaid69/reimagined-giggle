604dde7e6fb4cf479385ff978f22f9c63c699ac7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:49:58 +0000

    appeal sink Bb better long

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def square_number(x):
+        return x**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
