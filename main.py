a7dcd0da1b0eeb11301b28e3766c7caceadd3728
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:45:11 +0000

    scrape soft minute usual pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def square_number(x):
+        return x**2
