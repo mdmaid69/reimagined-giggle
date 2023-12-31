00011ae860ad0448d8e30316ec45f1c4f17024e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:08 +0000

    564encourage dead thank Zz arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
