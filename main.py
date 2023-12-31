1620b034fb1fb0bb170b946f4826fe215dbc5a97
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:54 +0000

    friend bother discuss visit party

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+  def is_odd(n):
+        return n % 2 != 0
