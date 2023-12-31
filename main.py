6b01004573c01d497c31a502437d990696872cf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:56 +0000

    suddenly grand cut save passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
