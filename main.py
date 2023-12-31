e956969b3aa49bbb97544d74425ddcb544818c98
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:51 +0000

    play doubt Ww wave show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
