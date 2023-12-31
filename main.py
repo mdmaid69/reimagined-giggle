ad67b0d0ea9ed9135c85e4887c853996080946e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:45:28 +0000

    float goback(to) bush everything result

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
