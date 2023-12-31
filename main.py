4993f3fbb12edd8de9f4f8ccba4a8cb450790d9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:32:37 +0000

    last tongue bother whistle back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
