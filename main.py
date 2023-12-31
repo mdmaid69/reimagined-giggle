5e0c276572b75bb4ae643b984c3ff4f85551d33f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:19 +0000

    respect waste young past surprised

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+text = "Hello, world!"
+print("Words:", len(text.split()))
