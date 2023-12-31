069132bd0fd61e8105ead428ad8b791f99b90eb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:23 +0000

    call cheat mix lay fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
