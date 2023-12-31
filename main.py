6745ea2ef122b8e8c07f68c96e4deb011ee03a60
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:04 +0000

    sleep together better tap country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
