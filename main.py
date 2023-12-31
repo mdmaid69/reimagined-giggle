c1b7ba05d04d972883b48a57fc6077db803ae790
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:54:42 +0000

    sound any whole miss finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
