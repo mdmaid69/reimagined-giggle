06ff9cd4111d978a51abd419d96f55169db11eb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:12:11 +0000

    unusual reason floor peace combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_current_working_directory():
+        return os.getcwd()
