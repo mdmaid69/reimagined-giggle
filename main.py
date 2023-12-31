8f678472b0b3b8382a7e90f6167b429076ef5cde
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:12 +0000

    light spite probably address possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
