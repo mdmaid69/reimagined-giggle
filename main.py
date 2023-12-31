7ee911ad8a7a5335d853a0f8f216fff6d3949681
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:21:31 +0000

    interruption autumn gooff keepoff national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
