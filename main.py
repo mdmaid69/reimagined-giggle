3e6d1cf577bf9d58c459ef9aad37820519e51c9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:49 +0000

    material considering comment to line

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
