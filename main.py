c5ed354190bfbbc8ba4c2eafb96f8927f4f3aeb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:26:04 +0000

    possibly kick situation terrible convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import collections
-def create_counter():
-        return collections.Counter()
+def reverse_array(array):
+        array.reverse()
