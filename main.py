3220503ef64be184bf68abd9fa93c8fc4f1d2e2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:35:46 +0000

    want address afford middle only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def cube_number(x):
+        return x**3
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
