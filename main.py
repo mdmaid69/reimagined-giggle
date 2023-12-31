2e7e90de8dbbe846fc89abe2b6a3a499af542cc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:16:09 +0000

    invite process point rise worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
