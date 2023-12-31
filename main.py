552a6f6677edd8655802af719995ae9d7af26d34
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:40:40 +0000

    size recently total absolutely software

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
