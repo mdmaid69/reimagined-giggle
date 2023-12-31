e3c6abdf815aa4f3bb7ecb22fe3c8578754a57ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:45:25 +0000

    load deep some lend building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
