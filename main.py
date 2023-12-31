934785f15f9144ed6c3a2d32e05dc40250a78c83
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:56:37 +0000

    love figure bunch jump still

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
