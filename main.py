cfe9f7b2799ce3d1bfcdad4b11047eb68b7a5a70
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:41:19 +0000

    objection odd none arm able

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_work(force, distance):
+        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
