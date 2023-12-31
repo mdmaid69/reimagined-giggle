ff80181014a0cd7dadb0a77fcb522ed08e533d3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:06:53 +0000

    begin escape sorry through obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
