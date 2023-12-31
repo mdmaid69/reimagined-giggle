26276c29aa12de9bedbaa5bd89bd8d45e5da6845
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:39 +0000

    peace copy warm left neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
