ae20cd66791dfd78a21d11acb011455c7f702609
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:08:36 +0000

    closed ignore best train embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
