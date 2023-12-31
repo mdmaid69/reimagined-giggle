8703b1b07558ce4aca8abd63b9c767ad1ef99113
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:15:07 +0000

    569enjoy female book place provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
