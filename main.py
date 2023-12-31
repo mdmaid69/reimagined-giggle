f7b03824c4b3bc2ba562e6c666539905cec45f6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:50:28 +0000

    weigh makeinto regard high minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
