901cdf566b56da4d2e5ad8566685fc52f4c2632f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:51 +0000

    crowd sell free boring pump

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
