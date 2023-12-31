ee246856fb05b9b6de469f0edea3e807e9ab0afd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:49:41 +0000

    train obvious clue back promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
