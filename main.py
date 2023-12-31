08e051c559f7e37b5ad88e035f279d70cdcd7fdc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:01:23 +0000

    surface sell answer somebody lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
