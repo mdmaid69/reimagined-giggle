9d14eb46af9e79cf6545e491b4fc39350c6455d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:55:11 +0000

    well he tight mend air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
