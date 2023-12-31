6733df1897936d9e6fa0bc282d4a48a1cad316ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:37 +0000

    improve pure suck room mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
