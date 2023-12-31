4341b898f4b0570e65dcbbd5e5228bc9c6e0577f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:53:30 +0000

    young price thinkover runover rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
