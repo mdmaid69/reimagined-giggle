40cada28a133ca835bafc70e3d96a48394ad3a9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:17 +0000

    actual afford schedule turnover view

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
