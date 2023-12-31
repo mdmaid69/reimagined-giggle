4be42b3fb5a22fe9fca73ff286126c3eb13c76b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:50:39 +0000

    basically appeal laugh together join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
