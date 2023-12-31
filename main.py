311de1bfac84b61bdc8128c074d91dafd23c7dae
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:21 +0000

    threaten bite custom set rule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
