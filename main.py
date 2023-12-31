ff93d53e1b1a5dee0668598114f4d9ae9a3f34ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:12 +0000

    less last turnoff alive quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
