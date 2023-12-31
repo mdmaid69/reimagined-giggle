1b2ec21b6be3a5cb2f66dc66adc059a14d53e434
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:00:22 +0000

    alone plenty average instead lead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-print(random.randint(0, 100))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
