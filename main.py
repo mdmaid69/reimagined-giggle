234480f3390d58eadd77cfd7ebb0ec043177c697
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:14:51 +0000

    style mistake thinkof recently on

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_force(mass, acceleration):
+        return mass * acceleration
