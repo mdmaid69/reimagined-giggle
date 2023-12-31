25f5d785769bb5397565092b52e4e586726d17f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:09:13 +0000

    smooth attach lesson content acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
