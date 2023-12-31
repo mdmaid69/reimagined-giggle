9f12b15a7f3dd270d228e333b96b14ef7a7ce160
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:12:19 +0000

    test turnon positive whichever squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_queue():
+        return collections.deque()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
