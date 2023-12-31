ebb8c0dd73233b2a1a978beb84d4b1871cf6d196
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:32:09 +0000

    job over cross century turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
