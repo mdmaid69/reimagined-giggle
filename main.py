918646e1d95854df9a39f85c7c1c5d396c6daa06
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:05 +0000

    before neither collect listen once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
