9b701e76e5dda298ae5ae99d61c59a2b8eaf267c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:44:20 +0000

    once height than speed out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
