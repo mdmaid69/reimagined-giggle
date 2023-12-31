172714eb3e2999a9b8b9de9585cf11924e6b2730
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:23 +0000

    to giveup the home combination

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
