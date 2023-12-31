9389882a84eb71b371adb9e7ee0f217b8e4ae989
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:28 +0000

    sure flat steam comeover action

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
