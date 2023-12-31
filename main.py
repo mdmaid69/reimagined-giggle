72255377f052ce5222723c7243cf2f8d5171635b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:49 +0000

    guilty reply right draw supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
