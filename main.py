e896dc8648aee86fce07df3cc003e8c857e9ff45
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:31 +0000

    promise important really wait interruption

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
