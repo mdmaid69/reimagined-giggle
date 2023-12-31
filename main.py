c35c3a0ff983ab18dc22bf5007e7669ac6feb7e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:04:00 +0000

    team happen discover danger cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
