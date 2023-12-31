c55b2bee8761d5902d0878e3ebf6cdb5ab267ca1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:00:00 +0000

    suffer turn occur relief react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
