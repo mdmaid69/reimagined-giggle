0818aa581f12903a941ee4498a6661fef438bc10
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:39:58 +0000

    shop proper slide however late

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_even(n):
+        return n % 2 == 0
