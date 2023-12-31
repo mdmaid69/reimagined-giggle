dac358487e8b1bd596004dcb5937eae7e1fe91c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:37:53 +0000

    569enjoy company command little nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
