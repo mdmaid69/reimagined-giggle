71bbcea97597125ffc923f458bfb9c54ee21b7f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:50:38 +0000

    where run yet loose claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
