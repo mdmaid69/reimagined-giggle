04e02d3d71a5ba4479921b50e762a1d031324f2b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:03:41 +0000

    about opinion reduce find whether

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_directory():
+        return os.getcwd()
