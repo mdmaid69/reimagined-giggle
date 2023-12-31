e9f40d93a14c53dfb466df2b86b8f92901c1fc80
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:19:55 +0000

    likely describe rubbish in balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
