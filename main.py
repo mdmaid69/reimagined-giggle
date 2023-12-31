6be375b7644156f927ca0909e05c1fceeea7945f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:40:32 +0000

    mother national wait count separate

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
