1c6558c6db1dd5d469c7bef9d478106a5d5a8b9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:24 +0000

    ready more concerned notice walk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
