f5e3a8d52e80d6c815198982f65031cb0be22dea
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:15 +0000

    more present type movement left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
