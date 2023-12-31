cd34004239dcc4d1355af4328c21347f126e530c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:50 +0000

    contribute dry wind common stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
