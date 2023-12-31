4a87551ddaebde648f5630e5de28d2b0a00a554c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:12:31 +0000

    draw there sport closed tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
