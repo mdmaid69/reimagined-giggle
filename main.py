44748f824adfff5852b8f2e2e710d432cf4942e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:52 +0000

    sore act proud matter press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
