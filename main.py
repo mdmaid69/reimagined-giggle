a8e9420a9a36b223ee321e1dd47b9eff395a60dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:12:32 +0000

    height cap crisis mad hang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
