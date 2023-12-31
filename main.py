b9d5ebb3c8596c33c783f3a6e756c8524969e20b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:23:40 +0000

    programme goout closed paper part

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
