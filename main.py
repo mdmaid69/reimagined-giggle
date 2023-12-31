d77b288d2adfba23a1f6b057a6410f5a1b559d57
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:02:53 +0000

    prefer objection contain active whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
