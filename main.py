09833932d7c6154f7d33c720ccd5cf5b812b7781
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:47:26 +0000

    between dare but badly nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
