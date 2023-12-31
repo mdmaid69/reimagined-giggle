1f5936048a525248f25a33d2efa60f0522b58f37
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:48:32 +0000

    price live date transparent hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
