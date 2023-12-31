b02a25b5ae6157d8ceee17dc893fe0e84c51366b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:21:46 +0000

    owe age smoke interesting cut

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
