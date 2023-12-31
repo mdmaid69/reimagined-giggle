5287279c41a29323ed9bfdea662149f2d4e3e7a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:05:49 +0000

    shade hello behaviour maybe straight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
