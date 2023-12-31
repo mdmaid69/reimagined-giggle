28fb7b9d267ca8d7f809074418ca72d0b2d011e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:13:35 +0000

    rate lead brief swim arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
