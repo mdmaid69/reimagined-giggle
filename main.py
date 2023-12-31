7f24e3bb3fccfd29b6b1c4589625811d1ad9f1ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:01 +0000

    rely narrow fortunately interview beginning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
