298f39f3019f0dbf9e663928c60f47d6d99482f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:19 +0000

    aim interfere ear strength performance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
