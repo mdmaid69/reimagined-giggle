2e9d46d0260aaa27666c695120e518e1f003f697
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:14 +0000

    niggaacademy difference hold stone swing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
