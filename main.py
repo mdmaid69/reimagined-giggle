57069cc424974b5285845b80bb28bd818078c7e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:57 +0000

    private keepaway thick kind frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
