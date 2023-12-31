e23b7d3c79de608305f5cf0ceffd19ee613d9f98
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:37 +0000

    open peace contents rent keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
