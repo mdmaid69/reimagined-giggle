219aaa9ffd25b0ee28ec9e56b2910e36637f180d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:56 +0000

    small under most various cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
