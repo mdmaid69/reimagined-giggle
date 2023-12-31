ca5b1541c874610f8b550e909412f3cf81876a38
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:14:40 +0000

    drop again your difficult male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
