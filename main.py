823e21bdbe6853f911c9413debaebd188db343ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:01:13 +0000

    innocent give dare underneath best

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import re
+print(re.match("h.*o", "hello world"))
