b5abf7eb049547c52197255422b9d26d84597f10
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:50:57 +0000

    advantage scared lookup nor difficult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
