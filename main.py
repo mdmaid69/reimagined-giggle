240a03efd1564388cd1924875be5f5a1982a7c42
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:43:28 +0000

    Zz land breakfast period birth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
