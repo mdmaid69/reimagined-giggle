c3d0037af2915482e5fd4d7be1e869302abfd31a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:01 +0000

    thinkof ashamed fit kind subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
