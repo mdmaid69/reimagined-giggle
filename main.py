8db3f546cc268358d9c369dca6cb7633e8fe384e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:49 +0000

    179bite tall far Hh excellent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
