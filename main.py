caa99180f7c8364a4993f572f9ee5d3a4668ad5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:16:38 +0000

    there moral lead treatment style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
