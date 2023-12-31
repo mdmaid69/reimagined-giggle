ec11b22ec0369d8801a93ed623e342598cbb2792
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:09 +0000

    dig honest recognize lunch related

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
