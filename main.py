46f3fbb82f0c4ff63934f1ca665e9fbf693e6416
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:57 +0000

    tooth laugh advanced bread keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
