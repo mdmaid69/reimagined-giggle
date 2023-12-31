dbb3fc249610630b932763911ab4b0bd96e25fcb
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:03:12 +0000

    bet fortunately fear measure spot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def reverse_list(lst):
-        return lst[::-1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
