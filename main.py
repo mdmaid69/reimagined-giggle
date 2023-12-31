80e87094ccd7322874cca80baa1ff3dec89a7f2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:50:35 +0000

    quick future transport thirsty disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
