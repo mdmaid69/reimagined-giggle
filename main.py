9fbfe33e786d641fef8a711f0fb34269fe581e38
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:37 +0000

    control go kind cool choose

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_min(numbers):
-        return min(numbers)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
