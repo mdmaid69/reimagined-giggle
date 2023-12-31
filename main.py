72ac7ed155999e7ca566e45f959aa9b5c5fd6a32
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:57:10 +0000

    operation police temper please certainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
