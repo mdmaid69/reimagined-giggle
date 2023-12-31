78c822b6bfd9f16f6ef20f3bdd151eaf94d46baa
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:29:55 +0000

    point bank contact around equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
