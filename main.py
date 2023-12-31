ec5924bec7e8ae3adc614f180eb78435dd358f68
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:36 +0000

    smile row positive anyone certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
