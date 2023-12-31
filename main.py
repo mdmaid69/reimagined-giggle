3d057f3605dcb6de607a99d1ecf10ed8108e5835
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:55 +0000

    Vv corner town roll 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import tensorflow as tf
-print(tf.__version__)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
