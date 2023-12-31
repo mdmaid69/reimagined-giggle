21cd87ac7e4e22a2c7b0adfd9a3ca0c4f44fb0c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:16:18 +0000

    school unit crisis vision everything

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
