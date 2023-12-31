d680296c47ed93b12e0d12e0868a12cc5ba25710
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:28 +0000

    formal spread business degree store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
