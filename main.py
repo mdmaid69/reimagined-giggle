dcf4f437a1213b59a5aaad234e635394b6be4924
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:21 +0000

    violent uponon emphasis rob ago

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_bool(array):
+        return bool(array)
