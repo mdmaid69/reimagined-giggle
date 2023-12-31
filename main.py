722e55e9a87c8d2afe19802d69ae7ddde2cb3c67
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:59 +0000

    cheque of flood material interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
