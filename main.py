799bf55bf5ef65e7d1c8297cd42611b326929aeb
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:29 +0000

    relative often sign either neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
