1c5451e2fe08a605ffab38f22df83383f69f3e8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:03 +0000

    sore turn snow although fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
