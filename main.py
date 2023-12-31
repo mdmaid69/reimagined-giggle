540a7ad93330f85b21fb9daa93963ba6b2721507
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:37:50 +0000

    feed soil step cold pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_binary(n):
+        return bin(n)
