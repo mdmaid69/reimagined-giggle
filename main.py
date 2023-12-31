465f4a55ec4b3125b24cb823cfacf33f4f5fa758
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:28:05 +0000

    zero each now measure furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+for i in range(5):
+        print(i)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
