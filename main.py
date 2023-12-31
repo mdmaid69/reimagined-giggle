b65014bd07b4bd1e2563ef8a19af29e5f0b8ef49
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:08 +0000

    will reduce rarely off truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
