9b691c213073fcb43caa48d3f0574b1d761045ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:16:01 +0000

    certain careless corner splash science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
