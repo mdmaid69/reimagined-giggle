f0a653a465a75959bacecc8e088b1b0695cb42b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:25:10 +0000

    nigga whatever anything Mm interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
