0850a95184145cba83b782bc871a8009cb45feac
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:06:21 +0000

    custom 566end lower either fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import random
-def roll_die():
-        return random.randint(1, 6)
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
