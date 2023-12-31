3e6eed7765e5c3bf2bf625d1208660521e177904
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:41:06 +0000

    notice activity recover or thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
