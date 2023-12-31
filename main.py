376cb64013d1585412fcbcddec7f8d56cd255175
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:02:48 +0000

    everybody section half command light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
