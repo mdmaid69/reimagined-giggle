d103af354eda5bd297846d2bf1c69792e417025f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:48 +0000

    far instant apparently score full

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
