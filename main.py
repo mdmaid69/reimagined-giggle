879b22ac3c898d9a0a92435d1eb60ef869718491
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:16 +0000

    follow mess wake complain stay

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
