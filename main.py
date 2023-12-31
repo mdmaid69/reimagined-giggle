faf6a1311ec4d06c383040feebc69efef832f627
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:39 +0000

    prize brain tune perform try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_tangent(x):
+        return math.tan(x)
