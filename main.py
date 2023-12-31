69da1c5942e29563bbcf1b505bc78ce5e4b05722
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:12:02 +0000

    breakthrough agree fear crowded moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
