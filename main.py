be96245fe2ce54c05bf0b97bc822c07a76e4f86e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:47:13 +0000

    disturbing bringback professional hurry unable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
