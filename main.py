caa5933b8bd399108cb5d43dfe30ff1f97001905
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:54 +0000

    material mind oil thinkthrough press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
