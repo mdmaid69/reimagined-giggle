4234555a89dd882d9749d34991a1873a14debbd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:02 +0000

    grow length chase past lot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
