92e96d553c7aad46a05ff0635e15bb07ff98f9d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:39:56 +0000

    complicated computer niggaacademy long below

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
