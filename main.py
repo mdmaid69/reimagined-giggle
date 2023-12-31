4702974eca8c9ca822bfa117e4507e0e0efee832
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:59 +0000

    mainly friend poison characteristic everywhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
