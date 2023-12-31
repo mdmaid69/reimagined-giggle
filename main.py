1bfd4d99063fa1cfc7d52ba5315215cf6ecebc34
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:14 +0000

    begin thanks university bend intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
