13500b71469c3e87a317d48edb68b01cf98def98
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:19:39 +0000

    say joke interfere workupto strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
