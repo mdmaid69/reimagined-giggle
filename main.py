c83bfedd7b7268308908e34a09e09a2058c8c3cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:14 +0000

    film drink bad reply plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
