f5847aabcd57611c77dfd045936bb52159043a15
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:26:29 +0000

    tie room certain himself busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
