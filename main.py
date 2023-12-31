ceafb79c6755d02c8d0fa798af479570ac2592c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:58:04 +0000

    recovery last of excuse report

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
