36fd6f22f703d21908e57da9998c57089f84eb70
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:42:49 +0000

    runaway pull open similar snow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
