14c7fa74da6748247745ee9453c761c1400a01fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:07 +0000

    between steady leg cost relative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
