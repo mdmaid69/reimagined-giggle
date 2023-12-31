1384d72f1e911b4ed338efb7076d219cb2d5e52c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:12 +0000

    speech round spring shut intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
