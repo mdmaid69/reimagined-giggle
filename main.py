d0d443e1ce98d20fcc78d2dc268e599f58d2a1f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:16:50 +0000

    doctor charge previous doubt suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+  def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bool(array):
+        return bool(array)
