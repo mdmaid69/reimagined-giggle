a7e106eaaf18a4628acc627917fb71ece42d6266
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:54 +0000

    yes success can centimetre chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
