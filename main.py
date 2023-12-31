3a8211963c53d17d994bef75aaf1ceaefdd46eab
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:06:56 +0000

    will lookafter myself roll yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
