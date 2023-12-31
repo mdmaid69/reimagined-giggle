6eeb332a04522f9d647de6980ed1bae104127871
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:46 +0000

    meanwhile far again will on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
