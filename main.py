f1b111ceb50fdbb855b6147566e457bbc7c75072
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:54 +0000

    love original thinkthrough rest judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_even(n):
-        return n % 2 == 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_list(array):
+        return list(array)
