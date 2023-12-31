34008025f25704658339bd95d2fb1f5905d3492d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:10 +0000

    office ready breakdown cry mine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
