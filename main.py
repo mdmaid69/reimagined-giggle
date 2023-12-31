fc068d73e2e09629815a724c6a7ae20e253f4044
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:14:29 +0000

    will glass together mainly note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
