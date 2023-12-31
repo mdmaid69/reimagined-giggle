642c5bc1fd2285e7b6d2d8b535f67cee0eb7c5e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:45:40 +0000

    possession over last laugh though

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
