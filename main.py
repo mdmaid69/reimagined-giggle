4326a8d0db23fde0bd5eb369c855584870c0d915
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:27:19 +0000

    wait thumb bottom afternoon standard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
