d4e999636924a6252d7fd2e90fab4d9afe32cb2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:01:39 +0000

    between dig fact inform problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
