87012c0d76c11398594de2c3b748b272d5d4c414
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:37 +0000

    obtain dangerous spill pen attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
