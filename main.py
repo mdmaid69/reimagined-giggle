20edb4e60a99fbb182c268eeb8d1476a6e400d03
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:56:20 +0000

    string too coffee 566end goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
