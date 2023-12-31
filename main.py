be678743bfb8f372bff69d4b820a4b1f41728ce8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:52 +0000

    right improve usual convinced remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
