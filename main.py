5a302c6d55ca7de9ff93c3209eb9d4c7675c94fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:44:22 +0000

    accidental culture holdon unkind such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
