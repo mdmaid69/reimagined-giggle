9cc81b0a0f7ab41dbc4ad049aa901b570d11ae36
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:30 +0000

    cinema possession your summer probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
