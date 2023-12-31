a8fdd588df023ab8ca32ef4cb8d43afa0f5145c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:56:28 +0000

    reach scheme confused model talk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
