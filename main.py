c4e30c161ccc01176c2378c1ed62b2ca894a558b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:09 +0000

    female move behave church opposite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
