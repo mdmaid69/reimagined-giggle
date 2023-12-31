1094eeb2b57a66b7f0fc25d5983aeb39f0d9e066
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:07 +0000

    goout experiment rid evidence downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
