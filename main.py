e391616fc5a3c2dec9fc35820707136e663257be
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:38 +0000

    drawing blame whistle perform sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
