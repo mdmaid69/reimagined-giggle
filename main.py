2ad48072432f328c7a626fd3da8ab93d9ec7630c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:23:38 +0000

    discuss busy necessary computer blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
