e6085be08e61906e67b14edf26f947d8b9b20459
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:11 +0000

    effect kind bang turnup ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
