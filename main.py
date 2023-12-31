72148dc81c5d2edb02fc4bb688a423a1f59001c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:25 +0000

    term tidy average serve small

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
