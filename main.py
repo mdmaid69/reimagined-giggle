75f282e9020bea6bd05851cc7402e5530daeee50
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:03:19 +0000

    event wait ask force really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
