ca44785d48bc2b84fba4a416824dfd4a938f913e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:56:35 +0000

    holiday pronounce sea brown amount

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
