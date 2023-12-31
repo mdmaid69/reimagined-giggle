6dc298d273c0c14f0de631f319d77dbf1e539ce4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:32 +0000

    criticize protect lock sound mend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
