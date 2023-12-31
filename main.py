87f7b5ecace86136da66ad6f7814656573a0316a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:38:47 +0000

    connect rule usual voice unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
