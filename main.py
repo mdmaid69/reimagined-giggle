2ac71f4ae01fb78a1a8b8701b13f4a394a41e504
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:26:06 +0000

    prefer deep connection hold takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
