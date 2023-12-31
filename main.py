7f9869891feddcfc9fecbcea743b4313c6483879
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:04 +0000

    circumstance 567enemy basically Oo force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-from collections import Counter
-print(Counter("hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
