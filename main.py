f355ba8da222b5a098a1d87b5fd56fe626904bcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:38:23 +0000

    only close guilty obtain question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
