ed835caec68ccf97715609a802868138b2ce4b96
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:20:42 +0000

    tomorrow relationship 565end attach some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_exponential(x):
+        return math.exp(x)
