d71f1b7836f8756a14d1705375b4e5b063fbb88d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:07 +0000

    there you transport head ever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-print(math.pi)
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
