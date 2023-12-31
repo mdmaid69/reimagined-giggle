4b5b3d3e32ebc99bdebc5bf5c0ff9dbf2f2defa5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:01 +0000

    original find kind positive borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_user_list():
+        return collections.UserList()
