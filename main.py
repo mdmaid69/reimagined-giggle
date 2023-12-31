abcfa831c5a9b5b4327131334d01e28f4e9dd47a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:51 +0000

    joke doubt getoff the border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
