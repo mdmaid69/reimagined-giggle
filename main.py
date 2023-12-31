20db565992d9d6fd9b499e77ffdfb466cef9353f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:53 +0000

    describe jump limit spin activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
