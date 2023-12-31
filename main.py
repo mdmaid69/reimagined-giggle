8fc258d585d0c69d1e51445b495e4e902ea937dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:34:33 +0000

    drop pause brush callback take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
