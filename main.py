089d8fb1c75b7ad1660e5fe13b9369927e472c24
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:23 +0000

    arrival family second pretend frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
