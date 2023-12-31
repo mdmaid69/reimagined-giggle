fbe017e89db14f656a9811847e14b4f2767a8c61
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:08 +0000

    aim summer claim over command

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
