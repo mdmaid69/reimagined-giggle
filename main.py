7fbe273c0aefc08037e62cabbbddd6f3d2b0fd88
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:43:21 +0000

    change close different trap hope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
