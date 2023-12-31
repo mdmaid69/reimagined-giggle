ffa65dbc15cf2e4d2a2c16e7bdd9a7cd61792eec
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:28:56 +0000

    means simple manner week confused

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
