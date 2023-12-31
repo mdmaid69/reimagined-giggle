0a7cea4fb869420662becc35d63bb9c749b80bda
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:28 +0000

    responsibility slice but answer total

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
