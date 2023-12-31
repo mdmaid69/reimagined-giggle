8590f961bb07e91d9968fd8875f5f070f888234e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:42:18 +0000

    crime original very shallow takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
