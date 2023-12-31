ee63cb9f8084b81f9576a25b3e4f54505b922908
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:58:53 +0000

    nearly last Ww knock confident

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-print(sys.version)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
