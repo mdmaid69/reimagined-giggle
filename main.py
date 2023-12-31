2458d40f501ebc67fa2628e6c25a77db65369908
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:26 +0000

    gate dangerous persuade drive follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
