ef0c62a41a3320fec3f707826c56da539ae70d8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:02 +0000

    pity pronounce band break careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
