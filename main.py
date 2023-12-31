3507f2f3ecaae6456a2dc0a8e140dc3fc6c6fb97
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:16:16 +0000

    Itself recover project write judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
