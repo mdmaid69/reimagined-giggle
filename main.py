3fc885816db56b30bf276bc9f5b8e0079a9e97ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:08:26 +0000

    borrow guess set someone performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
