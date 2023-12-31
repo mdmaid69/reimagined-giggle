39f900d1e64ceeea3e2f7c9baddfb90dae53e028
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:40:39 +0000

    whatever Ee Hh tonight interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
