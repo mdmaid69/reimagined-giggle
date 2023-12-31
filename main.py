eccc9b35d6f046d70f0eba7353273fc6049e3d3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:45 +0000

    replace breakdown gift feed advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
