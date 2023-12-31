a05783e2f112edee97aa0ae8a5f9d5a661a1f24d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:57 +0000

    ring noone stiff actual long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def square_number(x):
+        return x**2
