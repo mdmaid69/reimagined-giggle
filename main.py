c542527e3edfd782d3859c43a5418beca53f6ac4
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:36:46 +0000

    onto neck help together decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
