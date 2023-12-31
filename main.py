8efe9027eae6243c4e3b3af300d3ad4323e57152
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:14:54 +0000

    pencil collection mistake play creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
