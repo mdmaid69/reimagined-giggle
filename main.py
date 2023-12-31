7d074a4e499531a1728fff32b3498adc703e2914
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:02:57 +0000

    vision steer hello spray behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
