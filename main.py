c96af4f47ddaaf7b39aaf974257c92cfc3dec8f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:28 +0000

    smoke mind notice least straight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_area_rectangle(l, w):
-        return l * w
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
