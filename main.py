2a3e910611408df1d5db1a1d65ac7428b79084b1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:26:44 +0000

    wire thanks awake right unite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
