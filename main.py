41248c710cc850e563fb3c4d7306d153738c6be2
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:41:11 +0000

    head cut spite frightened scrape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
