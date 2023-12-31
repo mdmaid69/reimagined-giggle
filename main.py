ec828e27f31a6fe70998f365e4ca9300564d5123
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:52:11 +0000

    prove design gate cut spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
