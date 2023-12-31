18899d5c4bff0614a3626002fce6a0ab93fe5543
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:44 +0000

    unkind friend exist least burn

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
