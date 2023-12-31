78a7af40f509cb81ad0a4d589613fb01991a1dad
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:23:26 +0000

    occasion speech basically during shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
