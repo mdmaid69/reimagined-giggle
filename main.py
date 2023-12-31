fec05984f20b2fd9fde6fdfd8889e03241037423
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:37:22 +0000

    shake dance figure grade noisy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
