1f42e829eeb8c1bd50b4066a03010ce45754fbe4
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:13:06 +0000

    kick tear possible tie choose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_area_rectangle(l, w):
+        return l * w
