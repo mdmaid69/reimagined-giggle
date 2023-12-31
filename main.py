0724bab3e75e5be49bdfb63a65ba54095a5eef41
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:25 +0000

    block theirs at command suffer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def reverse_string(s):
+        return s[::-1]
