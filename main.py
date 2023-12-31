d58383e659e0da5cc9d7710be1f5445ff8ac42c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:52:59 +0000

    finally responsible telephone stand whisper

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
