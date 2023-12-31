1606e6a6b6a21919aba89f97d06e8892330fe56d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:35 +0000

    trip alive arrive provide for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
