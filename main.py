7d5778c791823314b237b405f27db0e097a99923
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:12:50 +0000

    Zz combination object study breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
