3123d8232d82c6cc186eba01dcc1e23e12837ecb
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:20:54 +0000

    along over talkback forget tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
