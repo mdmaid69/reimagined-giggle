eee8f588bdf7015c4ab2464b5db4618407930543
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:48 +0000

    every this century drawing impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
