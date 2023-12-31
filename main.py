aa3513d74f067facb5c39971175400996b2983a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:52:04 +0000

    ashamed dream pause education sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
