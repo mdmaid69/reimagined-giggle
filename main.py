3c253f20dbb68e7fc724cfb7a0998ef8e81ed81e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:09 +0000

    spill check weekend Vv violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
