2b69c12972844b7b32f26f2f458f8aec0461ac9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:57:52 +0000

    ear middle watch dig feature

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_area_rectangle(l, w):
-        return l * w
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import datetime
+print(datetime.datetime.now())
