3d59a047f3ef72f529cbed5bd2e8b993e078f665
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:01:33 +0000

    maintain home nature cheap quiet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
