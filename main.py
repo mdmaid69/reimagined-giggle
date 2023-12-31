de065308a3ebe762dd59c480e50faf5191999929
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:46:09 +0000

    occasional limit wonder extremely safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
