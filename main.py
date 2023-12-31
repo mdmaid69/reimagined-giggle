8b832318a2cb74f950adba58b34e6cdc3f6f22d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:18:13 +0000

    order trousers towards interruption offence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
