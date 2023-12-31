1fc722a1f1b84cd40be246b32d836ee8f3020ff8
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:24:49 +0000

    board uniform spill uponon bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
