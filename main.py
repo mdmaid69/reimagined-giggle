e7a2dfb4302444d754c2b5772db52236339901da
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:24:46 +0000

    far congratulate prefer announce contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def remove_directory(path):
+        os.rmdir(path)
