2528c2f2941919b994affa78a6c0d08b8220d93e
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:38:15 +0000

    unite sorry limit ride comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
