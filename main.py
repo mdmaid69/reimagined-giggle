eda51a54fa765ee25307ef8b4a3d163fd62b54e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:15:32 +0000

    container excuse goon medium mass

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
