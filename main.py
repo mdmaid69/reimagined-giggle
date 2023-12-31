4bb7c0c0757aca6179055b65eae5cb1932f7dad0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:04:20 +0000

    introduce limit chat morning perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
