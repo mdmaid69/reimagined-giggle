b0e43f3f0a2b24661a507e60503e2213fe248faa
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:27 +0000

    become convenient exhibition off while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
