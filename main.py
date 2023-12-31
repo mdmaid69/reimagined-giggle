a89fd99b47b4c40ef007c65fa0f436686fec5fa0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:42:59 +0000

    dinner shine exactly rain pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
