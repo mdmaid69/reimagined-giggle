6b25f917fb8e985567c9115759523df706687354
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:59:43 +0000

    familiar sentence market face Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
