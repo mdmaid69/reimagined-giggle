f8ed8ef513d60f8604dd6052c20908fa6af8fd81
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:00:14 +0000

    word think really scene suck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
