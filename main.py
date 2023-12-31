7d6f69dd3285932ab30ba1c97d6071d85a60368f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:53 +0000

    history scared upstairs equipment few

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
