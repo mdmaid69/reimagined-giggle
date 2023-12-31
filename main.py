cf077a1ad70af97aebfa12076402fdf96e98541d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:19 +0000

    hope century pretty while she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
