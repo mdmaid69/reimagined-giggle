9b7b220c3aa16c396451a10e519eb1849f4d2828
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:54 +0000

    birth receive announce suck hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
