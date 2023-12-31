6fa2d6b4a5a0db803a5ceec6bda69dc7840593f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:58:56 +0000

    stamp show touch thumb spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_item(array, i):
+        return array[i]
