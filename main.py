40dadac14f5556516c655376d8e87e5de4eaa3ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:08 +0000

    important check about explain friendly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+for i in range(5):
+        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
