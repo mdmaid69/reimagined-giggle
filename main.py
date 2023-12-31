8ec0f4bd6ffc40cc58e250635938b4f0cc478c85
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:16:07 +0000

    leave comment spoil regular tradition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def cube_number(x):
+        return x**3
+def calculate_power(work, time):
+        return work / time
