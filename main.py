eaf8698656e79fed788ffa17774ff2b58934fee8
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:23 +0000

    become remark combine block because

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
