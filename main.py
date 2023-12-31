9a7c320eb020208a93240234a661ef6c11a02de8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:38 +0000

    represent breakfast crime sale broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def convert_to_hex(n):
+        return hex(n)
