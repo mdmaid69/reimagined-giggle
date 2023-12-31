f74ce7d9c6eabad127ad8eebfa9dcdc64eb1248d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:04:12 +0000

    use gather suck cry tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
