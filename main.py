e6ceed03712ad962cff6481fffffa7b99b040193
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:36:53 +0000

    agree expensive determined reject mistake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
