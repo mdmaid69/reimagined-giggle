0f445cb88762bf4affe68cd279249e5378e194b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:36:20 +0000

    whether manage visit wire term

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
