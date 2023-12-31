388a27ae6c8030af8130113a5a363fd2802427d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:52:08 +0000

    sound respect judge large corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
