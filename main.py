cf3e507bcee9a79abdd962b4869a9f60089ce387
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:54:26 +0000

    sell offence main talkback get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
