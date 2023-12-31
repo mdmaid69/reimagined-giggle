b97d76c404f98e436abad7635a5a9e941ad1eb33
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:27 +0000

    objection tall declaration throw sight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
