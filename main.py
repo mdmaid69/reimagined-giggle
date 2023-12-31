3b9d88a62bdabee3a79a0b3d9c8171ff209de407
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:35 +0000

    certainly meanwhile quick family variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
