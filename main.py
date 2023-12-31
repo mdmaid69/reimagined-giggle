f5d5674f16995ddd0f4acf072b5c622d36f6b955
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:14:22 +0000

    future goodbye over sleep fit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
