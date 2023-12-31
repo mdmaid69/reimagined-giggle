e83c822c9f1d0ff9dce0beb78cf8636f8f783901
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:41:27 +0000

    type pocket frame demand score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_set(array):
-        return set(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
