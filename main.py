add6a9469505a86083f1547e83c6d769eb252d06
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:25:42 +0000

    stupid cruelty sort recommend sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import time
-print(time.time())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
