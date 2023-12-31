bb93641972c984e27662a79e876b053f3c4bae99
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:14:07 +0000

    press describe skill obvious keepoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
