6dc82047f4dab20b5784fd0a41cd8a47f583f22b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:23 +0000

    float wait also claim reliable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
