953b7a33a2212bcba1660b0c39fdbae0c8a36049
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:27:01 +0000

    glass law Ee confusing add

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
