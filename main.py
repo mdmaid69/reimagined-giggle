5112cf29e98fb066a46d953c47dd0eede6daae50
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:59 +0000

    beautiful come whether trust standard

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
