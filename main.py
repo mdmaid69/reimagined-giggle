a8458a19483ffcec4ef77223c4269a82149ed31e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:02 +0000

    want please replace impatient treatment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
