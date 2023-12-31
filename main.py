863431d83e60fb58876088090cbbb9788e7a6a35
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:35:04 +0000

    stay message argument without available

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
