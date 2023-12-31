59aa07af20a07582d561568cd7e40ef61d3df56d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:10 +0000

    concentration each freedom display brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_list(array):
-        return list(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
