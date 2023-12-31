522fee87927ac1f9ba8803e4dc706c578d35418c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:05 +0000

    probably level down pleased although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
