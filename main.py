49aff749a904e69d1ab0bea58022cde165c08f69
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:35 +0000

    modern check criticism physical result

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
+import re
+print(re.match("h.*o", "hello world"))
