9e456e2b917a8f0533a96d5e35cab422f253a26a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:55:58 +0000

    pretty month mix sentence stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
