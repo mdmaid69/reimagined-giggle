1b598bd85a1efbbbac97e87741dcb4661153cd6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:34:48 +0000

    tip frequent lift well breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
