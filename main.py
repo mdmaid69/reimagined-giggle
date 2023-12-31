ea36eaacdb956e34c6fc7375104d0cdd2cca4539
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:28:12 +0000

    last argue answer anything fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
