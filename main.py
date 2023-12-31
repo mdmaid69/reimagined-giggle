f47a2a590a5c0bcf9be7022ab323a45291bdc33c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:34:20 +0000

    doubt hold ours complain bringback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
