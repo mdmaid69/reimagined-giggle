9d19269236da55da880b28324bb4c9ce8091e844
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:18:09 +0000

    frequent through knowledge fierce create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
