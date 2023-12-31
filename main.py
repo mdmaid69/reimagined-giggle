eacc29488bb38f302cc4ebb341beccdc63de819f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:09:03 +0000

    or curious bored argument Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
