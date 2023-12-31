a8de35ec40b5cdcc540da203cf2bfde6d2ace7c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:08:53 +0000

    mend hand through family term

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
