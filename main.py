b17e61ca9e3b7f5398b3400d26a4b3f4e8d81953
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:21:27 +0000

    pencil guess holdup rank attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
