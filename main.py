14568b67a358b42f5ac6ddaabaa31f82c91dfe48
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:37:59 +0000

    vote happy he badly border

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
