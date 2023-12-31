d965f83ec0776d03dab861a98a91e6fa5441ef46
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:38:17 +0000

    holdon print recognize opponent exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
