5e45225be19ebbfc96a5c27df7f925f31fea3346
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:18:05 +0000

    unkind criticism thank limit Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
