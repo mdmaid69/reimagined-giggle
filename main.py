efb8a04e692af1549ea1439d2130f757bc256cba
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:28 +0000

    Jj modern takeback ticket split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import getpass
+def get_username():
+        return getpass.getuser()
