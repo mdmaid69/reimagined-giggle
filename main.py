e88083433aa9c3550cc76ac3af61739f5ea608ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:03:32 +0000

    pinch classroom breakfast shade frighten

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
