48a3ea37fd46becb19980a262d665bc7315efbad
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:23:43 +0000

    interruption close know slowly himself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def sort_list(lst):
+        return sorted(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
