75c97100586c56a36ec4ec4dfad5a8ea63f5a7b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:32:05 +0000

    attractive rubbish couple getaway nerve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
