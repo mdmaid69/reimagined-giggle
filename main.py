86f146f60a5d42f49da4513e110a00c0abfd62dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:56 +0000

    fortunate close whichever outside spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+print(os.getcwd())
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
