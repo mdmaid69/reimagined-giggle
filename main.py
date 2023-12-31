55bbda23f52d1515b97128e7d1c30fa35ca8ca4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:50 +0000

    unhappy tell each deserve design

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
