be2417e2d4d705156ec6e2c2411cdad921721df9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:27:39 +0000

    before tone shut thin so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
