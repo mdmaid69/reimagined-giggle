e8c5b3270b7ccd7b343a831f2e78367159588f8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:51:26 +0000

    busy watch empty skill sad

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def append_to_array(array, item):
+        array.append(item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
