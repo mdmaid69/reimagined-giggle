ace8b940e09c1a16e58d825e06c69460f0b6ab0f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:07 +0000

    rise front range edge study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_directory_name(path):
+        return os.path.dirname(path)
