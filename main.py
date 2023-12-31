904614e50532f6eb475db5147e3e881d34f481b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:18:31 +0000

    bringup shoulder shopping do complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
