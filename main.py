b24136cc8a68a204ef7360ab95b7001b2d0d6d6d
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:30:59 +0000

    left bet upset swallow late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
