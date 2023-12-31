fc205141581a6b65bf960c08187bbfe0fe025fe0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:03:18 +0000

    bet loss popular need muscle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
