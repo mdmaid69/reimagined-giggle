9302f3734be414acab6c6e209b89260d1350bf73
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:20:02 +0000

    bringup hospital introduce constantly machine

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
