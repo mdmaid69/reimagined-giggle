cd237d894a26d7a4b7ec60704823a16cfce2b3eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:06:42 +0000

    life quarter meaning people crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
