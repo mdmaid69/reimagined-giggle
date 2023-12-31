b15b4e98ba4f7d868f718b60749b396c42eabc3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:44:40 +0000

    always thing turnaway note prevent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_power(work, time):
+        return work / time
+import numpy as np
+print(np.array([1, 2, 3]))
