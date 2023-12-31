2d8f42a2b9154bccd9683a917208dad397b6bc8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:41 +0000

    town wait temper Vv risk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def split_path(path):
+        return os.path.split(path)
