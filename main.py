df34c84de040818f60903a3867ff2ea4c11dc4e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:16:05 +0000

    subject range fast thinkover frighten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
