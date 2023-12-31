12122c4cd5115f9f4b7a35be1e56ffa48d9dc919
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:46 +0000

    level never fine key consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
