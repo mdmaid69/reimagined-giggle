5409adac093878fb509bb5335474b4527e9618cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:19:27 +0000

    tongue drink purpose tear ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
