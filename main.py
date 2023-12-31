a6b0978570988c0af573f8852c8114fdd8c9090a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:35:00 +0000

    that detail exchange of vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
