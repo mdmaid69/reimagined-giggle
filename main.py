b83fe763fc23b524111315b5f6b70c0eda7fbdf5
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:32:05 +0000

    comein interview important base treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_work(force, distance):
+        return force * distance
