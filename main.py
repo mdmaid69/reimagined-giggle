a3e13ac24c888b6ef9a0511dd8690e3d9d98037f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:22 +0000

    on ashamed slight waste awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 def calculate_acceleration(speed, time):
         return speed / time
