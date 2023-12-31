cd72a7e794c9fd83052d2a784858d4a9ec073e62
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:23:11 +0000

    lost service love funny getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
