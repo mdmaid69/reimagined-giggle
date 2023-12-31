10d39d7d9464933df6a983be06871e56283d4780
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:35:06 +0000

    turnover only direct recovery badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import collections
+def create_user_dict():
+        return collections.UserDict()
