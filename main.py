4a5e9ada19551063d6941193cf3375564915170d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:55 +0000

    OKorokay exist birthday mess easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
