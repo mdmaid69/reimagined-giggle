6b7165cd6deb7c099bc0965a24cba4d9b202efa4
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:33 +0000

    light friendly no least hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def convert_to_hex(n):
+        return hex(n)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
