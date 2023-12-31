d556768e937a4bd7f91b52d57256d98e9bbd601d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:42 +0000

    used smooth single pour bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
