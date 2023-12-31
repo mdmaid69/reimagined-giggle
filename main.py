37ada4ce178f12e30f9fafb5915ec2415f20cec5
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:20 +0000

    manage pick point punish stop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 def add_numbers(x, y):
         return x + y
