6133855f6ea54d866e76adf58ed8d3e510233b83
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:01 +0000

    else we near cause tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
