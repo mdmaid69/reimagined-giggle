fad94137d34755607156cd8bdb6e318c926a77fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:38:10 +0000

    very pump his area important

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
