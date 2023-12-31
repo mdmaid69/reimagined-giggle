cee5b39d357ea7e905a7cffa8a5ec63ddbd94272
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:10 +0000

    apologizeapologise worst demand trade preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
