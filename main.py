97b5453a7e9ee91dd4519b16d8944a8067a5a591
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:51:46 +0000

    equal previous sit switch anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
