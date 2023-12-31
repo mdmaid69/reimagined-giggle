4a403a28ca7be029140d93418000882cd0a275e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:09 +0000

    weak neck exaggerate possibility point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
