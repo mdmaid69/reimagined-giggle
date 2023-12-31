ed2970eaf00a441575abe1fd18d42e81e7d15081
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:27 +0000

    stamp them attach entry sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 def remove_directory(path):
         os.rmdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
