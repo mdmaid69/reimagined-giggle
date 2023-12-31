2f34c538481a48ed920b8db5fe7fa2f253e9cb5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:25:22 +0000

    grade neck label negative scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
