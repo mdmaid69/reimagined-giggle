8d36ae53549551cdc411af4cce703b8950d6af07
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:56 +0000

    geton convinced impression weak Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
