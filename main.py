70b7a82c22a2c54506ef70f77e5d24b971c6e978
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:23:33 +0000

    bored lookafter callup neck declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
