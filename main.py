247d637c8f9ad72e9ffa49ca3c152abbf0030a4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:12:45 +0000

    dare tooth crash greeting sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def cube_number(x):
+        return x**3
