82c42883249bc5563e62febd5f16821369ebde93
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:33:37 +0000

    unable glass create race hers

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
