fb6541673516137341f88c7d9b782b4b5fc8495d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:57:33 +0000

    steady even strong cover wealth

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_absolute_value(x):
         return math.fabs(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
