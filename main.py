e4430199eca4928d40a7883ea3af6bde819bd3ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:44:09 +0000

    rule entry nice regular nowhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
