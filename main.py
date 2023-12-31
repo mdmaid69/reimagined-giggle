9123ee5d9dcd22d11a9663e73d2ba63ca290e93e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:34 +0000

    goon star difficult means year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
