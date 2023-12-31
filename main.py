c408fda816bbea26d40f1b78bf6716541152faa5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:48:40 +0000

    law quiet OKorokay appeal shame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
