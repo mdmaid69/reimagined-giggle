6d4495828a953f1dd7c2efbddc4bc241fef66d4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:10 +0000

    moral plastic everywhere realize slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
