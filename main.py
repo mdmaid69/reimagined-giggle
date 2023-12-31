f999f0e8da998571adaa295802a2123153e738be
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:17 +0000

    maybe realize press callback impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
