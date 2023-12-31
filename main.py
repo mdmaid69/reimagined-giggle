77b3a788a2d6d0a911740d42e7e7b97cd020c4f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:25 +0000

    against responsibility drop genuine invitation

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
