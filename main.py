4b8f004299779c6853642c4379ed90f6ac053e68
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:05:10 +0000

    balance reject sit schedule human

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
