d693438013d9ce2d29331699c6aa6614274bfea7
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:02 +0000

    sport branch paper announce in

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
