36b564768192e4a508f587c17f4cbf885b11d5d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:41:11 +0000

    probably good themselves figure tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
