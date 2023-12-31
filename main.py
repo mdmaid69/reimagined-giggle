a489053c747b06c85f41062917793aa6c12c8eca
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:57:54 +0000

    wide exaggerate arrive contact apparently

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import datetime
-print(datetime.datetime.now())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def exit_program():
+        sys.exit()
