67bac157401b38e7acb62c9dc09442fc6718c34f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:50:45 +0000

    broad much lend confident upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
