2666c418000e81da432c9905e8284394940e6f2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:58:51 +0000

    seem community empty rather appearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
