425a8ccc5a544d3af2dcde5a857f1a9214117b96
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:34:16 +0000

    entry truth suddenly everything present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
