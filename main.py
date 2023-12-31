9fd2b8565a269ae5fa62eebafcabe99ff653a103
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:54:01 +0000

    throughout bury typical bang wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
