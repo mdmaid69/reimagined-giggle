3d395284a33b71ad977ae2a18de56880aad2e4f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:06:32 +0000

    wealth education giveout flat touch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
