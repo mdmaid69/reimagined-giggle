633da5a5b7ae12614b3da3dbda1ba83ef18a6f59
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:31 +0000

    equal straight vote friendly progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
