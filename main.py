aaabfc1361af01d60df9782db0e7ff8d8bf8ac9a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:51:47 +0000

    concerned fair accommodation judge board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
