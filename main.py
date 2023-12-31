ba2fe531f7cd2736f97db7e404b36d38f521bf28
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:17 +0000

    surprised callback children attention innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
