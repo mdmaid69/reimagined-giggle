2b44ab790e2487719efa957d6a3faf3024083319
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:46 +0000

    few awake content advanced plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_today_date():
-        return datetime.date.today()
