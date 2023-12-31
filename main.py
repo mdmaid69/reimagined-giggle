823ba8e1b820068bf3f93d7b8a3b723b874f5283
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:48:31 +0000

    joke free walk stranger evidence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def append_to_array(array, item):
+        array.append(item)
