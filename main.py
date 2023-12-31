1109b839127bdf7dc2756d8458e7a0c6e74a0600
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:33 +0000

    name lonely light put find

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
