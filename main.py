f692c2d30c17c81b5b8d7ad8ffd090f73c3b8c16
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:49 +0000

    fierce pretend serve crime vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
