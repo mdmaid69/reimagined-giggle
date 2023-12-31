247b533856cd51056a23ce926783ed4fce85bdf9
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:45 +0000

    top each contrast custom consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_list(array):
-        return list(array)
+def reverse_array(array):
+        array.reverse()
