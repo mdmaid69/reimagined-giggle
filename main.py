c080504a91135c772b559e82ecb328c2493c12a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:04 +0000

    description advice powerful mention run

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
