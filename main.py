f0e843a5ae16ceb382d7ddde259a0eead7bfbc6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:21:09 +0000

    address join willing problem reject

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
