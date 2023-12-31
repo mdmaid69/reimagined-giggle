54bb1034a7506bb1a20b88a4de758a04f4a05a1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:19:04 +0000

    border heavy uncomfortable escape top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
