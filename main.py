d927757513ba1df4f01695beb57bc04f7979297f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:19:06 +0000

    moon only limit nerve stupid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+print("Hello, world!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
