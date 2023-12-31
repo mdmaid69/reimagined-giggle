431197212ab20c28982b27696a4ef3508a7f01be
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:40 +0000

    question careful variety enthusiastic physical

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-print(sum(range(10)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
