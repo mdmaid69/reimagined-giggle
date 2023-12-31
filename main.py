bb34c3a3c9217eddff60abdc2209659815996b0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:14:55 +0000

    sweet which introduce exercise where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
