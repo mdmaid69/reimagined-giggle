01b5c25d2c56d1eaed790672b354c3009ea5c86f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:48:44 +0000

    month borrow loan fun flavour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
