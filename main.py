3528dc9a2109553c9d08fec5df88ae3e7a132cbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:45:18 +0000

    instruction phone between far stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
