3d95579dd9d2dee4a9bb1550d15a677d50ef6407
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:54:03 +0000

    lost sight smooth equal holiday

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
