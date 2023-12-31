134d9bf092aea35efd6882dd638381fdf7365242
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:17:33 +0000

    swallow top pot stone machine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print([x**2 for x in range(10)])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
