299e148ecbde373c1b5b540827158a9c34376a78
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:07:10 +0000

    steal permission strike distance pupil

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_power(work, time):
+        return work / time
