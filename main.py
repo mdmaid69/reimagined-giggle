bc2f79d9a8c5e1eb3661f3b129a62cab9f67c39a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:25 +0000

    curious heavy pencil preparation why

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_work(force, distance):
-        return force * distance
+def get_array_typecode(array):
+        return array.typecode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
