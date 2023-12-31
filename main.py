0217d17d93e2d6aee3d4e6237314db0628f47ced
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:02:43 +0000

    worth language unfortunate female sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
