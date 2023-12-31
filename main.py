555a703811d09e6319dbd14deba850dc944a9733
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:13:12 +0000

    responsible cost coffee scrap cook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
