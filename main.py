7a5706162b5aa92c9f8b33ec597d8b816f932f21
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:19:34 +0000

    satisfied system nose surface defeat

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+print(math.pi)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
