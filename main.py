daf64aabc20c8ddac1b77df2930fc7e93a6c0e16
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:58 +0000

    flow thirsty hers reject cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
