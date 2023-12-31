790b9fc82d99ac0dee5a1c24c9413a7350a21863
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:05:00 +0000

    makeup young buy doctor keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_max(numbers):
+        return max(numbers)
