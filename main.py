75675fc399bf3f15272166704e4a506d9b57fb04
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:58 +0000

    group secret fit choose part

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(numbers):
+        return min(numbers)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
