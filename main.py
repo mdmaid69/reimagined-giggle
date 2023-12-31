44087b4067c3b2932b71684c302b7b1f564e7636
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:26 +0000

    surprising condition meanwhile check busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
