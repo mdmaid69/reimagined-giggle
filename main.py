aa66e2d9e772fedad98b020dcc1b349dfdb4c4f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:04:22 +0000

    equal prevent frighten hard least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
