ed18640f1c0790f963a13d752b8c9f441651e4d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:20:06 +0000

    until order connection discussion reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def delete_file(file_name):
+        os.remove(file_name)
+  def convert_to_octal(n):
+        return oct(n)
