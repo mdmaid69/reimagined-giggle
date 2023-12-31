b67accc9ba55d6a713ff3784c2311e02326bd0a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:31 +0000

    wave price attempt would noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
