7b64627311dfd7a091ce756be1f6f8f1a6b25333
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:31:25 +0000

    total say chase reserve sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
