9e0688a8a146100462da4c97f4dd6f3278c02761
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:21:34 +0000

    Hh thin wave whistle him

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
