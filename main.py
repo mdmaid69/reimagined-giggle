616ed5ce2aa5b682078b7ebeb6c1c27e3177333c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:15:54 +0000

    mistake record yet turnup cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
