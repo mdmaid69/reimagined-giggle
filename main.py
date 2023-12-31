0dfcee69da996cbc16de7363468173d659daca4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:33:31 +0000

    classroom there music to public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
