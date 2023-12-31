e57194a252080699f9275bb5f2c2a0b9e9c983dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:43:32 +0000

    speak best history push funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
