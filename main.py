7229bdcada453ada275afb0475af416d52bf4b58
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:43 +0000

    cause flight discovery payment Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
