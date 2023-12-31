138464909156ded870132efe6f4563b4c2253989
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:21:29 +0000

    swing achievement concerning wire thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
