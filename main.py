83121bfb70177c9651226c6a0f2e94c9464b17a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:32 +0000

    trade cinema way confident shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
