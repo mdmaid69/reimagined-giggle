92a3765ffaa73cdcd080569f31d5fc3a6d153bb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:58:42 +0000

    ask prepare minute as include

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+print(sum(range(10)))
