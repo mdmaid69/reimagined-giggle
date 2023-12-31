101146b160cd4f82c6afefe2866d4407e709abdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:11:07 +0000

    complain top press beyond blame

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
