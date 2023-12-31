f3070dae2e723f24913e6859dbf238686043b9e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:04:20 +0000

    comedown ilovedicks poison genuine yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
