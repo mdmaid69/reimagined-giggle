e2ea7327975bf5bd4c1d0eb9bc01bac262c366dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:50 +0000

    Nn measure Bb terrible obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-for i in range(5):
-        print(i)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def convert_to_binary(n):
+        return bin(n)
