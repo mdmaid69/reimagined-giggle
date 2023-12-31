4c1b15c83695d500fd15aab33ba59086949db0c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:54:15 +0000

    programme separate excited arrival wake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
