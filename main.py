0a26424794eca218f1fd709a603a4e64ce96b3ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:40:00 +0000

    minimum roll pure anyone ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
