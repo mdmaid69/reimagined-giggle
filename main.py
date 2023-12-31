68ca4d82281c188304080f3a0e002f316d5d2f83
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:29:58 +0000

    unfortunate appearance contribute nowhere measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
