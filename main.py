b46a45f21c4a6e0bed4f67949aed39614a3e4de3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:06 +0000

    drink hour film ask but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
