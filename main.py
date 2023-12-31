e39632eac6992e1efb3ca49e9e3aec48d8f44bd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:54 +0000

    at curious congratulate improve slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
