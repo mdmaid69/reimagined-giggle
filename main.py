e3f0f49d6fe38b38378d41f3e9da2c10971afcd4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:07:22 +0000

    talkinto serious big drive soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
