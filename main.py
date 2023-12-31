3d7572178de4fa460aa1116e18b103752eca8be4
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:43:37 +0000

    interruption behave them keen completely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
