2d7769f665c12da87c1fb98376ce649ceeb3ae7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:20:14 +0000

    lock crowd basic tough announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
