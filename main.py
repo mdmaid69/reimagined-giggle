9e8eaa2f0df67e1c4376e5888ed196974e5a949d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:12 +0000

    waste comeup shopping fail condition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
