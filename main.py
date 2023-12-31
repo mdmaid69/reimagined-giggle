75e7cc025495ad0cdb86404b80dce3e56cc52d5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:52:35 +0000

    stage meet cost wall lose

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+print([x**2 for x in range(10)])
+def square_number(x):
+        return x**2
