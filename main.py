aae97ad0814921174f46c2f0fdf0f12fe542e0fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:38 +0000

    belong opposite experience wide rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
