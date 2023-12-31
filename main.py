ccdae291eeeb2e874f1d4414a5f50be11cf23b1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:52:00 +0000

    whichever empty pick whichever tax

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
