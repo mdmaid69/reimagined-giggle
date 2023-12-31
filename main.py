e97774d9e3b216d3536c116979832a2d31a4b2e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:52:27 +0000

    needle frame various traffic actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
