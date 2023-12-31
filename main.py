a78f381d21acbc49f66adafc9c9b38adeef219c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:14:14 +0000

    continue married pull work favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
