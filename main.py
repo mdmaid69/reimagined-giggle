d8796e1187cf85421d7185a2c73cb08f07e55332
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:06 +0000

    fit grand convincing recently project

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
