12b9f00c24c219c99a9b948b858c91390b149219
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:36:16 +0000

    building squash either comment clothes

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def reverse_string(s):
+        return s[::-1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
