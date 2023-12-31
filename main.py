12a7a71750cd194fc098bf382802b689f635b613
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:23:38 +0000

    point welcome luck support second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def remove_from_array(array, item):
+        array.remove(item)
