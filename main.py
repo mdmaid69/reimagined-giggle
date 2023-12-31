c558e50ee27b927a33be3d7182203ea356870245
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:09:11 +0000

    obtain practice otherwise to rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
