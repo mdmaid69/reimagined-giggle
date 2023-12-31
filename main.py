1e9f5da22722b0f82297277087225f625c8c3bc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:14 +0000

    error doubt Itself believe birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def roll_die():
+        return random.randint(1, 6)
