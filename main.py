82e417d8ca891f8c4ecb661c13d353ff9de01fd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:42 +0000

    fail possible sea patience failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Words:", len(text.split()))
