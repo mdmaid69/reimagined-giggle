1b6c3c9cd002482ba34ba86c2f216bc4f7278b53
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:34 +0000

    start block shelter quit early

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
