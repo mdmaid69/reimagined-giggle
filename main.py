d4631accd3d150d1909352d907ae8691cf938d76
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:44:11 +0000

    myself have her strike bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
