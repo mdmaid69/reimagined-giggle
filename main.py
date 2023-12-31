6302a77d8b99d98368d2dc508e07f8e26539d4f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:16 +0000

    weigh design of refuse fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
