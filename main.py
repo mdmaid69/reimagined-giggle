b7b894d03e4f7fb412d7178211f7abbe198dfd2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:26 +0000

    hour burn all clever accommodation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
