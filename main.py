8b2c39c30f3e6907f36f45b590c7fe2d2b3db096
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:29 +0000

    gap excellent its somebody anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
