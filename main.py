f0979de5764c0fb8d35b61fe75fe2589d941245b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:04:28 +0000

    bottom bright my gooff disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
