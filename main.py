5db8d73f72d64d9d298deb36cfef40735de4b5bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:08 +0000

    start bush answer tense get

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
