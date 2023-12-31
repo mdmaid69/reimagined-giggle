97e6a27febb7763a6d6fb9888e49e00752e975c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:09 +0000

    whole absolutely wall pain pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
