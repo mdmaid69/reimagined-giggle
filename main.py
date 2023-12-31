a9d574339ce94bc972d75884e3b6cf32e566c91b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:48 +0000

    state already building unkind brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
