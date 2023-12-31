000709c0624a88c9edbe7d2f043c147d005ee8cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:36:43 +0000

    better scrap thankyou workon refuse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
