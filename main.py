1a05af2e8f5c99b422d7aee25bebd592a6f2f44b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:56:39 +0000

    direct ear back further match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
