c5af292f7fac37e46f495d8ca395e25b2f869556
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:19 +0000

    attention search will middle evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def append_to_array(array, item):
+        array.append(item)
+  def reverse_list(lst):
+        return lst[::-1]
