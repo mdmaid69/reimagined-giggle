26228ac84aac1d2b8fb46170edd41189d4982bf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:43 +0000

    extraordinary coffee doubt hang play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
