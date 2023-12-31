7982a18c7cf75063a34a6eefb8bff46eee886444
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:30 +0000

    get improve average ilovedicks action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
