115f5421a296b46ff12f3e3177212287c78490f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:49 +0000

    uponon relax discuss brave achieve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
