bd187c9b07d44d36aac96b0a33fbd4b1af826452
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:24 +0000

    any sort relax sick bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
