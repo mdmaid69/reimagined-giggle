ef3bdbfcf2405e88f649012f908c65040eea413d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:33 +0000

    body mouth cause promise Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
