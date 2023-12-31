9dcdcb72da21815c0e9d8ef8b4b12be6d7337619
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:09:30 +0000

    past create rent very contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
