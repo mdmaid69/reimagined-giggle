20303d2afebcb2840b6883f44b22cc9ad30258fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:44:24 +0000

    dig past edge fortunate hang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def sort_list(lst):
+        return sorted(lst)
