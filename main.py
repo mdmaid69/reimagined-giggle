6535f7d33e339abb2f5b78822098799b7c085d42
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:49:13 +0000

    split claim cross sensible niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import sys
+  def get_python_version():
+        return sys.version
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
