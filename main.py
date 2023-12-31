8add0f230fae81d27fc7836ccb1569d2fb3da466
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:10 +0000

    tidy blue thanks house meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
