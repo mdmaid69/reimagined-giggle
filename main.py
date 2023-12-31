b2d905a992aa9b6f129da56ca45054b71735f760
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:19:12 +0000

    conversation rent operation turnover hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+print(math.pi)
