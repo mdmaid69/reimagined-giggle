d2ab465c004cecdf13997adb6d3abc77fdeac32d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:57:17 +0000

    discuss because physical obvious punishment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
