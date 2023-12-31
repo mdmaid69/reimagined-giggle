65f74bf61ae6c89d900f2d8b660a62cd50d6cd5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:51:06 +0000

    be far over often relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
