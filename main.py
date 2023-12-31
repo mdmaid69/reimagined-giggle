109c5391e00c44489ad81b7bc12e0f6b518daa55
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:23:43 +0000

    future edge mix cheque could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
