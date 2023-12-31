960aae74b3c27b4383247dc8c188f016d0e4b73b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:26:48 +0000

    comedown noisy instant act doctor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
