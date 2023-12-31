db3ae98638c44d84debf2b41c26b470243c913c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:36 +0000

    comeback hope exploration material available

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
