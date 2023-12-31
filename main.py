0c4107858d41f8247825e6d38b2bffe04c752693
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:26:43 +0000

    we punish contents colour letter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_density(mass, volume):
+        return mass / volume
