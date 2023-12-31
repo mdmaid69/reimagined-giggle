6ee47612c3e788937e05c3731e01fa2112c05e18
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:32:41 +0000

    stamp fat abroad loss shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import logging
-def log_message(message):
-        logging.info(message)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
