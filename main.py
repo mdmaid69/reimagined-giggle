b2bed1e16537a0f2da4283ccf3130cffa9962186
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:18 +0000

    stir father base high purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
