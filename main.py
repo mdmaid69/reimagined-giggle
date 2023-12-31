07275636bff6964283a65f30906ed7e6684a8d55
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:44:26 +0000

    increase form attention detail murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
