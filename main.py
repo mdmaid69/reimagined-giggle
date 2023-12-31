4e590f2115fd614f7481329c6d283f99d2039e30
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:51 +0000

    skin abroad fresh company picture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
