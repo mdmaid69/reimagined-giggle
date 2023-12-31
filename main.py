dbf5782e98b720396617c681c6a09e941a404d97
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:26:32 +0000

    drive anywhere scale fit shout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def reverse_list(lst):
+        return lst[::-1]
+def convert_to_binary(n):
+        return bin(n)
