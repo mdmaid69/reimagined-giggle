fea52a1a1077494de0283e57e496d4543eb1f4ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:53:43 +0000

    wish commit total beneath bringup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
