a611f775d23c9cd0074b05321745410df993e5c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:40:03 +0000

    state complain become head effort

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def multiply_numbers(x, y):
-        return x * y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def is_odd(n):
+        return n % 2 != 0
