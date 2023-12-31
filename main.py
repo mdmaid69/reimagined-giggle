3e84341ac90ee8fcb9729db04d9ab418b32f2e73
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:34:12 +0000

    drive deep keepaway waste quarter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
