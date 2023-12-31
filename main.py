36d9d3a35d5d07f1b7b444276b70cc839e3bd206
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:04:06 +0000

    both name drop sister command

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tensorflow as tf
+print(tf.__version__)
