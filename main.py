3ad52af5ecd08ac5794cc5b90b734e36a9127efb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:13:02 +0000

    error square risk physical back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def sort_list(lst):
-        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
