4398b83a3e7ee47c9f3ca574eb8d7562c9786b12
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:15 +0000

    depend uniform charge general film

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
