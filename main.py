8f4d6265aa9019e69e12bb13c4d79e1188fc20fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:28:08 +0000

    system even thick only far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-from collections import Counter
-print(Counter("hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
