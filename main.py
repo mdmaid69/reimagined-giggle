d13fe43912384beddbd9aa869e87f881a985a837
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:58:42 +0000

    getaway room typical expensive that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
