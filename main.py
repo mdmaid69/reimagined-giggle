e5474f947e8a1bb58deed37dbb2658c79f346ba6
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:58:34 +0000

    giveout involve underneath calmly neither

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
