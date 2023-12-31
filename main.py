a95589204a237507d84720153d51292eaeaa5405
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:16:21 +0000

    minor mother strange quality dish

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-print("Hello, world!")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
