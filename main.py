a5a77fb8233f2428204f4e1550990414a3c532c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:12 +0000

    supply mysterious new empty concerning

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def sort_list(lst):
+        return sorted(lst)
