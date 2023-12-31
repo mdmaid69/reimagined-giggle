10e696392fe8f80240d631e8b4242eeebfae88cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:54:20 +0000

    public appearance evening supply model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def change_working_directory(path):
+        os.chdir(path)
