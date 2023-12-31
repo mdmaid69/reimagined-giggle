74c92061482b86594f608bbc40111f1c7e2abb60
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:55:45 +0000

    argue somebody ticket announce open

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_list_from_array(array):
+        return array.tolist()
