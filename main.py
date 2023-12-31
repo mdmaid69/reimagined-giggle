c0fdf51a628390fa39a71c10191e33c60100facc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:25:59 +0000

    active naturally hot face steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+x = 10
+y = 20
+print("Sum:", x + y)
