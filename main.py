bf7189f73acba871f196b9a80fc971d38bced9a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:51:03 +0000

    reasonable land among float which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
