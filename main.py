ed38eb36378c301d28c38c84b7da26339f1ed831
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:06:50 +0000

    theirs how line when snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
