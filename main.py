a916e9f0b8f44587c68fa8272f2077c2ab8ecf93
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:25:03 +0000

    official left pick middle firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
