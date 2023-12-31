071342d38192059d52a08e16a8c2508163a2a957
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:28 +0000

    true estimate include lucky confident

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
