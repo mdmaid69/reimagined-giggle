ea535c0c064fa34319acfdbc1728b2cb4afaed7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:06 +0000

    hard loan drink brother pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
