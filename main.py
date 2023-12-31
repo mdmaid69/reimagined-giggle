155e67b9a20d855581be797901bb5f0a8199b4f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:22:37 +0000

    tall reaction anyone recover reply

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
