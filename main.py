877511d440c61d4c2359b0d94d3d7259c68e88fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:56 +0000

    earn damp contribute beside practise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
