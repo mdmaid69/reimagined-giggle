9e0f1a9937233572020b50772214b3aefe7fc4f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:58 +0000

    finish destroy increase extremely weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def append_to_array(array, item):
+        array.append(item)
