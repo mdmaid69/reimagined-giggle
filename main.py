68a6cd4597c24fcc7451e2da21d50ea45f26ceb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:39:58 +0000

    machine need talk set give

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
