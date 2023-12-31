41fda72a07d48b09ea9702f0435d5d1c6bfa8c3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:20:13 +0000

    difficult runaway urgent sting in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
