9f59961c0964638839021363054eae97aa7fe35e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:15:56 +0000

    common correction rather square fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+  def convert_to_hex(n):
+        return hex(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
