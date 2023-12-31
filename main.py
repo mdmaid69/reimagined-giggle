5c0f00dc4547ef471b209fc375b13af82cb3bd2f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:21:10 +0000

    completely instrument go keepfrom hard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
