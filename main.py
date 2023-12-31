c05e419867213d010bd58dd37568e68136a19cc7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:42:53 +0000

    too needle fortunate insist gradually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
