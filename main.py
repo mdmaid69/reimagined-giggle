195c64f36c268f2f5860ac4e970554fba0282f4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:08 +0000

    journey anywayanyhow anybody butter contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
 import array
-def append_to_array(array, item):
-        array.append(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
