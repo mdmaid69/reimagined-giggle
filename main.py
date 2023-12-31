5b6347f5675ccbb554232f598dbb839aed7069b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:43 +0000

    wing no uniform flight during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
