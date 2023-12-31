d25702b0fe4f706fa19e676bff1144cff87d51cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:56:46 +0000

    pause first suppose spoil work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
