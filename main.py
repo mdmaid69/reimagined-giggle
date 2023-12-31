7b68e195391238f340545b0d78cb427b7d69766c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:31:26 +0000

    she again future blow disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
