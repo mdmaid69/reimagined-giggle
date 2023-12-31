479cf3fe8803fba614648fa4ef29e9e7e9968a8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:55:44 +0000

    hold church past laugh second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_directory():
+        return os.getcwd()
