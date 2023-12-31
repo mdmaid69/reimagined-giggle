d5efab54d07fee730fbb009384bcb6dfcce0841e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:34:58 +0000

    plan list nice mine half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
