15f445d8f5bc587830b8d773d82c25a3c4bbb178
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:41:33 +0000

    unite shoulder turn squeeze doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
