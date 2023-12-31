113834f6bf1697b405ff3401342ac488d90d49b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:50 +0000

    suppose every wing clean 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
