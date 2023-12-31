59887ae265d94ff3d2d7c2b53ff1d244b2da06c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:16:33 +0000

    second probably shelter recently progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
