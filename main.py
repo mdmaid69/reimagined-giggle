6ffe9d86f95dd925db359a0fb7e2fa58e462fa18
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:32:41 +0000

    run force waste advantage apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
