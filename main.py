f08e3c8bbe99b4c41c6579332bc466130ba828fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:52:05 +0000

    contact understand giveout circle disturb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+print(sys.version)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
