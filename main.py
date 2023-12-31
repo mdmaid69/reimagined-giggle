454b4d7abd262ad71585e92844c577a431627dd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:30:46 +0000

    you shade of sound tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
