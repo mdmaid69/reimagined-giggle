4522a486fcf445558e2dc8382f3755e63bdda90c
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:50 +0000

    whistle patience store tonight mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
