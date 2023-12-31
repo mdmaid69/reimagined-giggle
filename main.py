3ac370147e411498df6033f884b4d09f8b3bbafc
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:44:41 +0000

    thinkthrough how sky classroom wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
