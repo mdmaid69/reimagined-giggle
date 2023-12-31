a008a465e8139c304706ba07f5089add2c78bd50
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:12 +0000

    so interested moment Yy so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
