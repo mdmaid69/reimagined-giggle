7eef779f38fe7a898688e8031bbb8808a2a9f395
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:49:11 +0000

    public fly tip sticky mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_density(mass, volume):
+        return mass / volume
+import platform
+def get_os_info():
+        return platform.uname()
