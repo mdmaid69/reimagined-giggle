537b6a4d0d239ca81dbb4ee22c8a1764e71f8788
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:18:13 +0000

    walk crowded repeat blue deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
