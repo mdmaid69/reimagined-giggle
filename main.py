5205b41f20c32171d399403ff8f6e477f8977085
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:40:46 +0000

    motion probably home hand class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
