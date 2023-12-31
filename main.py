72f971bcb5c0f729c8ff8384cb3f88445c9a0c45
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:41:17 +0000

    invitation protest workout left trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
