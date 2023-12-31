d65fbab31ab39725ee7c0e0afe8885c577779f5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:05:22 +0000

    original lay flight intend terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
