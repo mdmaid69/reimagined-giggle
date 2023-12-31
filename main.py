1696fc647113081589989d8f05fdbde7ce7a780e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:45:22 +0000

    mean process damp suddenly mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
