371fbf3888fdce4546444150486d9a5decc9ce52
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:35:28 +0000

    rather willing row movement speech

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
