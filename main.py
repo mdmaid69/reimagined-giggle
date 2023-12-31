5bedefdba327dc42f41ef4ecf792bccf6f830b88
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:02:25 +0000

    late hers somebody unfair next

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
