6023984ae3c7f431c797cbe2e448a9a33bbbda6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:46:49 +0000

    muscle continue tidy contact anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
