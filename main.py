e82a53862f724ee92743d30a556741dbc2bf9e2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:57:37 +0000

    telephone this owe scream neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
