d5ba49333bd11402106d063ab27ef8413b18636d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:09 +0000

    demand list stiff organization pile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
