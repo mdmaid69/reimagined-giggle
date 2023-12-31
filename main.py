5764cdc14c4130ee8e68fc79a5479446e559ebda
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:38:46 +0000

    worried environment care worse plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
