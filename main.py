910df90a1a6038e03b8a77cdce0a9ae4556f4332
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:32 +0000

    OKorokay takeover sting translate agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
