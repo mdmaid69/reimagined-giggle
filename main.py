01aaaaeb2ca5ba02184515156b69b1c580ce9256
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:39 +0000

    rare tense available practise strict

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
