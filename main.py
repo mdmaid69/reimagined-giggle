3d0f7ca92a830e775e558ccf7ca42c484c2d5956
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:58:43 +0000

    score declare corner pen wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
