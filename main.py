ecc6e1c9083fe86bbf1707693e9ef237dd29eec3
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:01:24 +0000

    demand keepup beginning cold worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
