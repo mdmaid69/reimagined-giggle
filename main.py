67b7c37ba8571cb2f368cc257ef69da6f5656d04
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:35:31 +0000

    why plant half vision save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def subtract_numbers(x, y):
+        return x - y
