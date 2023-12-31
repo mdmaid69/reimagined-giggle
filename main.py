1e50931fdc5a3e5d5fd30144b8cb3f5f47fe0817
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:49:00 +0000

    keepoff light 565end 566end march

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+for i in range(5):
+        print(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
