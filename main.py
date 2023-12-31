ab6e6b6613c66f96c11b69c9a58118bd244bd71d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:47 +0000

    turnon object loan interview centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
