dc8312d3368d8c1b65dd76b85b6b4555f654aedb
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:09:01 +0000

    land above brush win hungry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
