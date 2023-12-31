fc682155bc4c377b99b77d0e99305f6467645111
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:34:09 +0000

    journey top gift disappear check

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
