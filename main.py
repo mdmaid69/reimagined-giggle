d2661d76f3eac1064928e1c4c62821b9873a97ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:49:57 +0000

    Ii mood design float happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
