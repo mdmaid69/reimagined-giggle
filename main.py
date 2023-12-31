20c27324a1abf8a4ed13ec2c80793de192326806
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:00:02 +0000

    fail ball unlikely believe realize

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_speed(distance, time):
+        return distance / time
