58cecbc439b338ee4204a76b7c35874420fd57e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:56:51 +0000

    watch immediately fail person wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
