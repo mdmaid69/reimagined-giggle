851f0086a9c1a8e5a7d8542a20e62e4b2ec56022
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:24:57 +0000

    soak thinkof whichever together little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
