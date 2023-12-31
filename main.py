ed89068890be4ebcc64634968702ea698b5534fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:30 +0000

    coffee our school warn offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
