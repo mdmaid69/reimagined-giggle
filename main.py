04505c830347333a06f2b176ba06ce5162b26eb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:53 +0000

    frequent creature comein spoil bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
