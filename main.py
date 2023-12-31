cea89a455b4496d54397fd7be0d33b0cb83b3e70
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:15 +0000

    period release description argument bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
