d905c289bf2133dfea03745b41ad711795d0dc6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:48 +0000

    print surprised wet giveaway I

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def split_path(path):
+        return os.path.split(path)
