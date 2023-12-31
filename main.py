81d82d320fdebdfbfbc91b330be46ef793ae59f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:37:20 +0000

    fortunately comedown right secret expression

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
