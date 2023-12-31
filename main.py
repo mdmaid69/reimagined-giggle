9f951ed452f7552ac5911ab100416eab1e9f21f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:57 +0000

    beside will Dd strength Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
