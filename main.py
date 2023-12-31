1f68c39afeab195baecc9be39aa8e4ff4a14e1e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:18 +0000

    discuss opportunity art busy gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
