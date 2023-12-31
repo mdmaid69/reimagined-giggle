66c7d17d5f15173207a3ae77b9d8506ce6005a42
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:10:54 +0000

    almost oil war university shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
