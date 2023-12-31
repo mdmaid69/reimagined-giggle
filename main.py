013d96879e620bc94205f6b4f4d330db6c5a8afc
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:15:57 +0000

    medium holdup scheme interrupt whoever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_power(work, time):
+        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
