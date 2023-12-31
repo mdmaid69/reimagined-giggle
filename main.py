4765c4e276035ad14f3e9d2c1cf42df15d2d4a12
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:53 +0000

    passenger turnout early concentrate destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
