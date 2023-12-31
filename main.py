977fc3c97a7bfe1d7631411daeca6ec99d441684
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:13 +0000

    duty record fact below boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
