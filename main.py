eb85f6512be04d017e99e8c64ff1be0845b77cc9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:23:34 +0000

    calm instruction by sharp drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
