eb3d04cedeee83b008c64b3f45f2950ccde57582
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:44:52 +0000

    set exaggerate plastic demand quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
