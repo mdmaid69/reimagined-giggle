961a2fab75574187c0f93d08b52bfe30a06507ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:39 +0000

    suppose money object sometimes person

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
