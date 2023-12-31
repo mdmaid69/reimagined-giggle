8579984f733a9807cae706c424ced3a4e5b05d6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:00:46 +0000

    low awake use development aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
