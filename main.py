fca62d3b87173dc102a25f771a24feeeadd36b28
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:19:51 +0000

    I workout touch beyond cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_counter():
+        return collections.Counter()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
