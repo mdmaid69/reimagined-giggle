6e3a269f9f89f8a65db5771ab3247c7ad496953f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:02 +0000

    cinema point pure drink live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
