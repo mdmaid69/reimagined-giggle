f0374199a32919788608cff5b1cd0c3e721d0630
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:58 +0000

    hope recovery when long appeal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
