ada71494f392451b918538208efac6612b4070e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:14 +0000

    come exhibition necessary holiday once

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
