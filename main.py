4046eb95fac6746b440a5c1d6a3b3a06bbfe66c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:27 +0000

    break miss tense believe attitude

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
