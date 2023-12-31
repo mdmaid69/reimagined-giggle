f8a0761658e68f7968319a8c8fefe02f41c98f5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:01:11 +0000

    follow process spread male profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_username():
+        return getpass.getuser()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
