981941594f070b43cc545a47b86a7781b21df73c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:34 +0000

    confuse instant content like hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
