467ff7cfd3859ba3b75cb7be4fadde05cd0ef146
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:01:41 +0000

    treat rent long follow buy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def square_number(x):
+        return x**2
