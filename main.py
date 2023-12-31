7dd77be22881e425e2c30f253758947447fdcb90
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:13:04 +0000

    stamp method brother town hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
