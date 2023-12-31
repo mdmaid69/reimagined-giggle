54395d82efb917c7c2f45785f58e6d3809530f5e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:31 +0000

    error choice plenty cover study

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
