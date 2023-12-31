c78c03ca1f8d313b5851b7bb8479a0cb05fa9238
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:39:03 +0000

    warning goup more successful concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
