f5d5b8267d3305537550eef519170e9271af9519
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:35:19 +0000

    let ago slowly ill fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
