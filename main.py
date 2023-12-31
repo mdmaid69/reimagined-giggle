fc3c949b77d70be3d92238c9d6b78106d5a5f585
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:07 +0000

    tonight beneath date doctor development

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import platform
+def get_os_info():
+        return platform.uname()
