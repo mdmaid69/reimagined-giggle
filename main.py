af353deae0018b259e6c8559e75fa5d3155b5d6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:16:58 +0000

    dull influence stare strike tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
