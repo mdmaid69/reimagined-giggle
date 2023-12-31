c063d4fe8f76106aa0a0883e60081c07c161c881
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:52 +0000

    crowd if programme away train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def add_numbers(x, y):
-        return x + y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
