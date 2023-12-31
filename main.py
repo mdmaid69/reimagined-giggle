df7673eeca62bb8e368fb50935b2f91c616814e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:54 +0000

    schedule rough accidental region doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_rectangle(l, w):
-        return l * w
+for i in range(10): print(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
