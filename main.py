ed6fb7373f4fb6516b768dcabf9dcb277a5884ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:27 +0000

    form everyone male drink beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-print(math.pi)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
