0df4db10a22db69ce1548b7bc94ec2c464b7e43b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:37 +0000

    naturally concerning sting cross pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
