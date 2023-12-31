13ec2f6d6772cb15dfceb3b9109b1c1b52465bf7
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:46:56 +0000

    communication reaction job float strange

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def convert_to_binary(n):
+        return bin(n)
