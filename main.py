6c38f58fee1f6ed51253506a054bdb2ba27e87cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:11 +0000

    neck formal uncomfortable dance cover

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
