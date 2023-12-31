1e7e6f6c4a70541f37574df371ab3f0da485d677
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:05:42 +0000

    regret sing connection stage replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
