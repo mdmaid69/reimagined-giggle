5ba4c7e2e1d4043db05c9ea9824dd2814e68b2a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:27 +0000

    sweet giveaway Ss permission must

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
