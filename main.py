d1d2e3cbf73acd32513dd9d17031711942758e52
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:29:09 +0000

    abroad pass capital clean connected

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
