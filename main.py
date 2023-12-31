59ae50219d094c841c3f67eb89f29e51fc0745d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:30:17 +0000

    loose band smoke emphasis picture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
