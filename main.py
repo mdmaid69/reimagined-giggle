5be42872e38e5988a0d667b39cfa66b82adacddf
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:35:28 +0000

    lose voice start lean same

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def is_even(n):
-        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
