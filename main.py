7a2c87e12d4d23926f98215ba685ef5874b6f6bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:42 +0000

    part state name turnback criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
