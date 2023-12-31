a52a84e48210af9aad58968aa27a3b456a009832
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:29:08 +0000

    walk own advise classroom in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
