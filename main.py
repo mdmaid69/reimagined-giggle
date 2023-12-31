8e9ac3523306a1fa9ecf1ade486efe109795d057
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:31 +0000

    lose keepaway punish exhibition close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
