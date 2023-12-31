a9645ff33c657e79f8bf11e49cfab4925f5e9c5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:00:14 +0000

    reach drive mistake thought where

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def count_words(sentence):
-        return len(sentence.split())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
