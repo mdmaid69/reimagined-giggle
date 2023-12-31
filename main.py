6a6a4294f1791271227b552fba17d338ae1505f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:34:26 +0000

    really lost pain keepoff across

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def convert_to_octal(n):
-        return oct(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_length(array):
+        return len(array)
