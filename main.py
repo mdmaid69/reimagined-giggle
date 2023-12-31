a873f73e65ca4fd3860f573b3adbbc8d7a0d036b
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:36 +0000

    neat appeal calmly walk suffer

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+print(sys.version)
