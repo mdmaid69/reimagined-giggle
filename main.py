224b62d3a3ee252f0b3c14c8a56d42156e3e4692
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:31:25 +0000

    mine possible rent breakup law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
