49debc1b66eab44ae3608856610d6a1a6c4de445
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:05:12 +0000

    fly demand payment piece guess

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def remove_duplicates(lst):
+        return list(set(lst))
+def cube_number(x):
+        return x**3
