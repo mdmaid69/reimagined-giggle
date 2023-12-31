bc47c7ed5d671137c1bb80937afe6e79ad8dac5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:53 +0000

    reserve uponon fill smell really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
