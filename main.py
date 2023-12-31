8dc8cbcac68d133a9c519c4bcb23ecb4ea1fd783
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:21 +0000

    false sort travel objection their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
