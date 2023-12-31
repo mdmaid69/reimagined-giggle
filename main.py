253fd72301aea1f1e4dcac2f072edfd9bb4d1908
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:55:57 +0000

    trade persuade rough innocent foot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
