2fb2655c91cd1a02691dfed5d14be88cf30d911a
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:05 +0000

    emergency ground patient step turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
