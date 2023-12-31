8895c9f098fb0fb8ec0f8c7f1e541ea340d58e9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:48:23 +0000

    smoke period light sign healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
