8224a01b6d7274f3ccdc2250c507199c30167ea2
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:23 +0000

    anything intelligent deserve action immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
