c05fb7a4d2f3306f321e2b6b0fa51b24f3fe17c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:24:07 +0000

    deliberately if survive free measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
