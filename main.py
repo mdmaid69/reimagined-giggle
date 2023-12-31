b0081e249d773fcf7f582ea0f797f9dd6d991f36
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:59:08 +0000

    unlucky nerve earn prevent computer

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
