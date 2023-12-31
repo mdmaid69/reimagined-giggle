b72c9a8169e56cb3cded7b17e27816fac449d4c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:50 +0000

    completely study pick meet heat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import logging
-def log_message(message):
-        logging.info(message)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
