52d3c5d3b51dac1abddf9e82508eecd43a52e333
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:42 +0000

    especially sight to once scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   def add_numbers(x, y):
         return x + y
