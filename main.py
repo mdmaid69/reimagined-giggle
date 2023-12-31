58e3f076ebcb5135e9b46c0b25dbf4d91d870169
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:23 +0000

    each anything ring turnover world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
