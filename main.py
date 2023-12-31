e354f741df560934f05eacf81810f3684e9944ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:20:19 +0000

    valuable let more measure it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
