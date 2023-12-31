168c5bf757d616baed299b58ad48e695ac7b2571
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:48 +0000

    used properly arrangement might hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
