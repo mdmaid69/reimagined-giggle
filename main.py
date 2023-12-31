da58decec17696beabb38e1f4b251d81164b6c4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:37 +0000

    wrong worried issue rain company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
