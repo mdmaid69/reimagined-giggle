1dce5752b35a695d56583dc0793eabc0bf05e115
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:59 +0000

    crisis surprised influence behaviour change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
