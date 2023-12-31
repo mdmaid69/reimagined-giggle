7521f6708b984fb750f14a980f823203dc2dc2e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:44:50 +0000

    reach afraid first never throw

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
