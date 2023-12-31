71403219c0f0be62e329c6ba35495f1e5c2dd481
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:07:39 +0000

    ball translate trap big talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
