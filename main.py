e6ddf46501e032fb22fb464112c9de5f1d3564ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:31:40 +0000

    you earn long quiet beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
