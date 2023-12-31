645efdc56a40b6642fee6004b4c7761fe86b0318
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:19:34 +0000

    Kk treat owe him rush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
