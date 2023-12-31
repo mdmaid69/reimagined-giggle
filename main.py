963935d699df3f1370defaa51486292636fa91b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:58:56 +0000

    murder debt know score thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
