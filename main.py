ba2a1962bcefc473d0553fe2aceb401e398c0113
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:53:09 +0000

    hurt opposite beyond ticket practical

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def sort_list(lst):
-        return sorted(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
