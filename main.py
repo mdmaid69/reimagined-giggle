8c5a80bbad444ffa4d603e288863ec4e366b774d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:19:12 +0000

    although rid now sharp school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
 def iterate_over_array(array):
         for item in array:
         print(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
