f6f06435ca395e309c2507b741c8349c9a72a5e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:14:07 +0000

    thanks pretend minority welcome left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
