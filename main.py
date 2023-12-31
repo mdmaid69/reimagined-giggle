cb9f3053540aab915b150c952cffb535f141eb61
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:50:23 +0000

    modern shoulder activity large answer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
