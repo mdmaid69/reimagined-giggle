bc43e31d3f672deaf17eb936134c2ae8bb145477
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:27 +0000

    party reach information type container

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def remove_duplicates(lst):
+        return list(set(lst))
+def sort_numbers(numbers):
+        return sorted(numbers)
