28096e87c9838857f19497230e620ef5d1de766f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:08:03 +0000

    hers practice anybody murder church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_current_directory():
+        return os.getcwd()
