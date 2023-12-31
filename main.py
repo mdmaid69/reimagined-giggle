02011ef2d7427ff86aa36f3272c482e7fcb72ee0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:12 +0000

    collect gather bringdown sometimes consist

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
