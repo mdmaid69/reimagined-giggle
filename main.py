db3d9657bb78d05df3ea379f08b9071f27dfa44a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:07:11 +0000

    hello cause exam want win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
