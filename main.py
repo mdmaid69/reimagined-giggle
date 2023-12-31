d46c34d18d89b664ce6443801ae3973a75b42a04
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:23:59 +0000

    that bringup then then part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
