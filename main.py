11c4f5dfcd188f839e7fdf745059176d83aec08d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:28 +0000

    concentrate goup keepfrom holdout read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
