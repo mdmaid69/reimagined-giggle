2925e40f288422c18957ec0119799bf3c4405af0
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:56 +0000

    profit telephone Qq software degree

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
