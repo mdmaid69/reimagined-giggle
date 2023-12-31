c5100d91f8912a0e4bc75fc3b0b5c746e906fb8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:01:16 +0000

    disturbing lead married seem closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
