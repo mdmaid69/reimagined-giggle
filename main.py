7cd34382731964880e0e181f7ec62cf4c7cf47af
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:23 +0000

    goodbye beat hard difference sad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(lst):
-        return min(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
