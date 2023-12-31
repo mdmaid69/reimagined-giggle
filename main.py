cb746ab0ae2b74542fb8a72ab2b516630af79fb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:49 +0000

    cut exam thing wish division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
