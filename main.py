6c9793d589c13e63644e261a312f7cfe4382bb55
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:32:54 +0000

    hate rest container Ii pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
