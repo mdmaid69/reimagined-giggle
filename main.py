b249e42e5340779f54a5ba3796c15302629432ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:28:22 +0000

    everywhere present subject rather talk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
