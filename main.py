8c7334321fd0f888a3134ef932b42d2365a4bd3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:27 +0000

    pattern convince press relationship person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
