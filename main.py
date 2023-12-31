a7e8b55e4aab743a1d0d75ade5ca2a78ea35c3b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:42 +0000

    exam none advice study know

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_speed(distance, time):
+        return distance / time
