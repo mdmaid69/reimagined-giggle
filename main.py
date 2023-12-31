46049c1771dfc7baac3563737e9365ca478fee75
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:16:03 +0000

    day temper difference somehow various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def count_words(sentence):
+        return len(sentence.split())
