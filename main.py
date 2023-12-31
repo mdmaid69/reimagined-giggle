05871b6614d77c0fa39de9ab8e5a926339ec92a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:50 +0000

    judge height record glass table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
