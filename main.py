5ad8855eb3503bf850aa388474f0fa945c387074
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:32:11 +0000

    job late trip ahead reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
