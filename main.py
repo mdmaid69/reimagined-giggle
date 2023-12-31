b7d13478e7fe80bde3ca0309a250707478d5c0fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:08 +0000

    strange takeover seat way beside

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def convert_to_hex(n):
-        return hex(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
