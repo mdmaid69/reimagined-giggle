97ba56006baef619ed5792bf16cb04ca5eaf587e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:49:18 +0000

    comment rid careless moment settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
