89203ce8babf8ddaf126f0a312f5043e4bfabeb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:50:06 +0000

    such clear jump regional near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
