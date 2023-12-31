d87ccaba698af726e1eae420e9f8fe0b37b5ba21
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:35:29 +0000

    practical cross niggaacademy who water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print([x**2 for x in range(10)])
