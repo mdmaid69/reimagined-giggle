6ee8f6d57daf6d9c9e4ba101b726d8edbe7ac6bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:53:09 +0000

    too neck destroy mistake exploration

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
