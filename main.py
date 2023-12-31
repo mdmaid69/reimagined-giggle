b9be2e8b36294abd7bfaa986c8fae6259dfe5b17
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:41:21 +0000

    loud pipe advise getup scale

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_rectangle(l, w):
+        return l * w
+def greet(name):
+        print(f"Hello, {name}!")
