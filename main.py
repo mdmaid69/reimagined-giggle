18d5d57070bfb1975ae8ae0a57e1bf59bc088d2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:02 +0000

    probably appeal afford comefrom thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
