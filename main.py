b4df2b3bfdab3f0b00b8fb8ec2910902a5165a09
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:03 +0000

    justice expect sudden within disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
