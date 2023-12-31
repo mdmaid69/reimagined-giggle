5ab905fd621d08f628d4f07ccc991739ccd739b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:17:16 +0000

    star before system here sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
