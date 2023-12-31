66efcac38402e2560adce33b4933c1a4d4841ab6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:29:04 +0000

    prevent takeback drawing squash nature

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_max(lst):
+        return max(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
