d1fb1590b9ad5b01306ad3bb263a3c433fa8c379
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:31:58 +0000

    pretty punishment however scream occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
