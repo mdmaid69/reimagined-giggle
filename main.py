11a5c4ae6724276764be2426800f127802118ad0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:44:55 +0000

    yesterday count shape organize talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
