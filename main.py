c6f0c23a3f353e75c9b9af9f4215ffce2431f7bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:45:58 +0000

    that produce however lookback(on) price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
