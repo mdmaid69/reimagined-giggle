30ec19d5e6b71177210f6f5e22359092547ecba2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:22 +0000

    imagination splash constantly more far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
