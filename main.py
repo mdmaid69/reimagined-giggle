168f46906ad02b70bf3c0d2d8a0bf7f6d70173a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:14:01 +0000

    sad too point suit brush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
