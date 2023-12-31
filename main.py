20b4f522b1a08c742d59e9745842db5dbd223a65
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:41 +0000

    innocent less come dress model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
