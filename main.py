a9ce404e30a82b597ef2c191d7b9e098f7bd1e05
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:08:20 +0000

    high reject own bend crowd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
