e5d5a6e5dbf5152db66e346e0825b7ac6a022dce
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:04:27 +0000

    cough decide because exam typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
