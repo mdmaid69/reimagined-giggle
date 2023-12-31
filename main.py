43986c868c09884c63dc1ac3ee4d10ccb36723eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:15:03 +0000

    method open flow recent part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
