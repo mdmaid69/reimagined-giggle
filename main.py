0405e6a1726c325fd1fa6a0592ae0f71c5efc2d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:17:41 +0000

    afterwards shine invite sticky freedom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
