c73eea285875cdf1a26e53b2a7b6f2899b339d81
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:54 +0000

    language principle sink rather smell

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
