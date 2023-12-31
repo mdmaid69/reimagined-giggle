32b9c5d215553e60c9efc46a070a23f0a4d27d03
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:05:11 +0000

    unhappy breakup wash once disapprove

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
