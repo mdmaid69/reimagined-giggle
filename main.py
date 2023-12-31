398eb6a67428034b1aa0a14e78bc0bc945f89391
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:47:09 +0000

    after maybe deal cheat contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
