ac365682320a264623e91116f36806e2ddc4c8cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:59:55 +0000

    course better ticket powerful high

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
