eb9f86e9d56ce335121f0d42e5ba552aabf99a04
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:24 +0000

    correction rescue push wake especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
