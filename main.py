6f5808378a23ccb9ee87dca1b97143a05a3ec7d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:17 +0000

    inside shelter majority certainly short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
