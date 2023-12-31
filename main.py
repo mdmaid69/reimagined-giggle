3c7da4e11e390e286317b21b5b65b3f361e41e44
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:44 +0000

    love point will impression priority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
