e42d4e17dfacd22bdc40b59c776c8fa15d12d432
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:59:56 +0000

    over number round conversation tough

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
