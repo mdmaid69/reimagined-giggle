649ccf932d2a9ee39a3f50aaf88c04e077012481
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:22:53 +0000

    bad glad treatment other point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
