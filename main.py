cdd38d364ccc9451200d4539e90d9b4162e11759
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:53 +0000

    but Nn beyond cost afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
