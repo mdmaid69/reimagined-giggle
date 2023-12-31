5c1cb79876cf4c11ff563270ae170945d95ed0e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:26 +0000

    keepout dance cause pinch chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
