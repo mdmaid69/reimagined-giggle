ce24076d3dae0fbf1900063a73b5043ecefeac8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:16:33 +0000

    fierce close fix blame beneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def remove_directory(path):
+        os.rmdir(path)
