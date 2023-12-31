db6ba59e1ae9e92993e7dacf0c105b2e64b72320
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:49:52 +0000

    me turndown list successful reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
