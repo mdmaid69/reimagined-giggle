f5ee7cd123d132df174d02ab6d55a8c7dc7c647e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:59 +0000

    whistle collect trouble straight next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
