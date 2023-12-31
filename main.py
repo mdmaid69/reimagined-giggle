3ea51d0be0a0a4c0c813dfb0d6a453a001acbb32
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:36 +0000

    somewhere passage used boat fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def square_number(x):
+        return x**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
