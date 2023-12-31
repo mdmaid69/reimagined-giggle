d6c0cd2f79520bae7db844690c97f278f4f2c91d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:44:23 +0000

    threaten empty still perhaps sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
