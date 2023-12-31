6a4f45ed05ec7877d1a7fd6150c16c92fc8998af
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:53:00 +0000

    crime hurt bite shallow feature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
