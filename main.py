5a73e591a559f03fc958b0a1e9938809a9c1f4df
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:27 +0000

    wish owe uncomfortable tip gas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
