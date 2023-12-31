f5df32a1d3df2e9b0dbc2466f4f305c73746c413
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:48:21 +0000

    thing really loud especially impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
