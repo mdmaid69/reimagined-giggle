130b6a543239da72fd63067f54ce393298762d2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:32:18 +0000

    common afternoon tie whistle change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import platform
+def get_os_info():
+        return platform.uname()
