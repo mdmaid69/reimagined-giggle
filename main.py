2a4f34099566787f092b7fb2c0fdd7bc33c27cef
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:44 +0000

    scrap must classroom ourselves through

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
