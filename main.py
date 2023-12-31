7eac8b31c262b67cdbf8555f3ec400d346ca7037
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:48:49 +0000

    tone attract hold negative platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+for i in range(5):
+        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
