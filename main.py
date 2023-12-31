3976f311adb204a5180bf43c520d48577f3812df
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:01 +0000

    song holdon mess cruel drawing

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def convert_to_octal(n):
+        return oct(n)
+def convert_to_hex(n):
+        return hex(n)
