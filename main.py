59aec594e1cb95e114ab48bd84972a515e2a5d2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:53:59 +0000

    reject opponent hope club track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
