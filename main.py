e5acadb6db0b8cb935046fa4adc09cc8c456df09
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:46:07 +0000

    possession nature attempt mend pressure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
