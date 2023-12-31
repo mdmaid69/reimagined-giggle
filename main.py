e2f5bf44953503f9717b9dbe1d27b69b3d2097e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:33 +0000

    century ocean bank ordinary unlikely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_length(array):
+        return len(array)
