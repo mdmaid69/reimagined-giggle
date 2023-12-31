c0ebbd8f16a1782522170d905457c0ee906eb2c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:05 +0000

    drive around wave steal noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
