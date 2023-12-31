2a14f3a3e26a43fa98016bc37d5a16c1d1e4bb46
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:32:51 +0000

    arrangement takeout attach politics tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
