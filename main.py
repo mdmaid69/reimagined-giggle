d26281df4b4c301fc44ec37b52eae4cea1fe08d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:38:12 +0000

    peace that only law region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
