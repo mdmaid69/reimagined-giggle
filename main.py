6b090d6b86bb42180195094f4760d817082eedb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:20:26 +0000

    in figure refer factory mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
