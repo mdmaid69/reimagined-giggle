71389d5a65ddf891e20e8c15ff0b7d6e747969b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:16:50 +0000

    keen reduce grade trap lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
