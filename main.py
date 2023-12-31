1da67a2e65f5340709b3c2cfe66e406459c2068b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:24 +0000

    from precise sleep cover future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+print(os.getcwd())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
