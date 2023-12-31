babd87cf420e4c316474196e5ca2ee90d1f3275d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:17 +0000

    stamp book hot aim belong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+for i in range(5):
+        print(i)
