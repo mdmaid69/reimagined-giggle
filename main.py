3fec9ce41e3bf7a91556d6fe000b034b6a504710
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:25:57 +0000

    doubt summer squash small calloff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
