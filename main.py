3728335e93565f698380c01a04e8d4e1823639f0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:21:11 +0000

    familiar traffic earth female Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
