d246531eab4c237a8c3c18a674c2c4955cffdc5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:53 +0000

    favourite race today speech each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
