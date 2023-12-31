16f31feedb085fa82dff7fb0d5e334d5b745a52e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:17:57 +0000

    emphasize mass explanation book mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
