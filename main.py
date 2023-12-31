7204f5d40c021a83a7d7932f3e941b02a3ef2db7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:29 +0000

    estimate mother branch behaviour comeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_min(numbers):
+        return min(numbers)
