eb323fcb21e9b330c3615054045f84adcf346d29
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:45 +0000

    bag score total Jj empty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
