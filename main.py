08a41c068ac4c47fc933c1d8af70c0e08d46e142
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:44:01 +0000

    relief sleep late tape fit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
