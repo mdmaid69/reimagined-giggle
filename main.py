16edfddfb67033e1bc154c355dd3328bc433f8ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:22 +0000

    tap welcome soil eat paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def find_max(lst):
+        return max(lst)
