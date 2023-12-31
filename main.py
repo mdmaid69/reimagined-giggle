df0edc6eae12debdf22e70a06c3dc86f26bb4925
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:37:26 +0000

    especially thinkover recognize moment moral

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
