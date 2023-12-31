92bce7ff0cea2a4fe234937ae070cabbea91aef8
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:20 +0000

    lookup guide Qq relax show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
