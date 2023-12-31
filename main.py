4f2ba449ff20ecb6aa3b0fb4eeb2bdcb8f770e86
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:15 +0000

    cut nowhere rent destroy car

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
