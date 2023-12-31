3770585b982f2dc91c0ad783ecb5ad4ca09dd4b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:03 +0000

    degree believe record between high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
