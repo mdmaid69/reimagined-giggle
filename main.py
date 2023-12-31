950f89ddb5b29381fffb13953b097622dafb15ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:32 +0000

    wind empty strike with search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
