123e13cdb0361d72535d87054ebe9b5d215eb2c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:16:30 +0000

    plastic control patience decide hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
