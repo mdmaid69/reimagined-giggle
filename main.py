d9bcce7901882f93537a363254ec1c28d5994604
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:23:56 +0000

    rest release friendly idea callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
