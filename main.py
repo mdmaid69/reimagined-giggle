127c4ba81ad5454327a543be89a936d5145361b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:16 +0000

    past advantage direct excuse only

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
