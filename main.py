23d7be47203392c8da6d96f97e74182d992829d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:41 +0000

    same loose onto that shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
