8df1b8eb16272f7900a1cab1f60296620bbe4b42
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:08 +0000

    describe fine Cc also base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
