16718ba065badb86eec0d0d9c8221b0899026176
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:26:10 +0000

    unless not stand lock contribute

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
