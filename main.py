7fa75d9a463d020662b230b350ae424b38f357ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:12:25 +0000

    confusing early getup stop seem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import datetime
-print(datetime.datetime.now())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
