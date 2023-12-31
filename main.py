22f9ef345a4f0dc94c3b0cb2392162b29955b12f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:46 +0000

    free speech compare accidentally keepaway

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def cube_number(x):
-        return x**3
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
