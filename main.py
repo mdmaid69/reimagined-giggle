341a04c42c79484e969ab4b7a6a942545dee89f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:23:51 +0000

    poor bin wire mass mad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_work(force, distance):
+        return force * distance
