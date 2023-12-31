aa1b226674eb91808bb5107d8c11fa8324977624
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:26 +0000

    thin apparently laugh clever shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+def calculate_power(work, time):
+        return work / time
