395981ec0147b37225a54bb787b7f1f181286a53
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:07:20 +0000

    talkinto welcome pour style school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
