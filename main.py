774c584e19daa36e7291f377b8b04b0092b2eeb3
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:08:37 +0000

    temper strong consist child no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
