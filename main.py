015d058ffe27151b9c84c7525c5a4f3352b703e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:27 +0000

    lost lot message mood let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
