b72a0c6d3d404899a2fe3b35fc38f4528b354e08
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:38:21 +0000

    city not half communication hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import logging
-def log_message(message):
-        logging.info(message)
+for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
