41945c8c8289e3308a1557a656cbd08839ded2ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:37:26 +0000

    blue false cross attempt many

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
