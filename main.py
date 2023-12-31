9309c29a450da3940f86514564437bd5c7801d1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:03 +0000

    later dinner yours finally anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
