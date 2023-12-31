8a882f9c1b8d6d3ff7ea092d20b4d8a51bb2ddcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:38:23 +0000

    office aim uponon something prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_itemsize(array):
+        return array.itemsize
