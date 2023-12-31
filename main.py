a28fb8d6dfc197949823081047302b7ae6967f2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:09 +0000

    by performance occur unit ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def sort_list(lst):
+        return sorted(lst)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
