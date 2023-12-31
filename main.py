65fdd2027151bb45e77e638a9d83897a28a8b618
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:36:58 +0000

    until below should print concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
