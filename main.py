6df2f114516cb279897d11fb3b13953cd730a4e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:55:38 +0000

    flow offer guard artificial best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import sys
+  def get_python_version():
+        return sys.version
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
