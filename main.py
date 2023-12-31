a75a4245b7f3e59ad8cbff7a861708e8e309a553
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:04:58 +0000

    message come remain getaway change

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
