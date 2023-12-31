99efaac354d20767a4c8ebdadf6f1da985a7033c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:43 +0000

    fairly profit worse satisfactory rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
