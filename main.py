a2b1e649bd262d333f9b2cfb1890157091802a38
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:31 +0000

    soon matter upwards patience brush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sys
+def exit_program():
+        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
