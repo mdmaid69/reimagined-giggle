c261502946928d16314f490882eb04c8bfe372df
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:05:24 +0000

    laugh account well radio shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
