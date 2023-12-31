20b17b2b6764f45c291f2c3cd75fc464c6c9a59b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:50 +0000

    sudden enthusiastic slice 571enough tall

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
