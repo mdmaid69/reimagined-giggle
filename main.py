556732763fe6af2baaa5f6e9bad14d55050fbe08
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:03:25 +0000

    born shut design model chase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
