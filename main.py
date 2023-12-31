66263ed23beff717844288abb497bb1a88b7e657
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:43:01 +0000

    section summer win strict free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_rectangle(l, w):
+        return l * w
