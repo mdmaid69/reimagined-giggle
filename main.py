8cc337f667d1de85a3f07def35db9421bb6f1c1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:05:34 +0000

    Qq change talk ocean comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
