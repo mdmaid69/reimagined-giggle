c30bea633cbe4e12caf8f78673aa3ccefebe2a5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:29:08 +0000

    key breakdown print convinced weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import array
 def get_array_as_bytes(array):
         return bytes(array)
