31ce373924cd2d386188fe138933ec750bfa0396
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:55:28 +0000

    respect sense sort meanwhile mouth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
