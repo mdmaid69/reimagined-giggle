2b9da0ad714c3947a2cdd5bd30810b69b6e81af6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:56:49 +0000

    capital hair cause influence clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
