63be874ffb5b1ce1c1ebb2d6624dbfe47ca1a7b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:18:29 +0000

    lie start they remove absolutely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_sine(x):
+        return math.sin(x)
