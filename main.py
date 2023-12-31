82f35184217ae989524ca03770503f1fa01ebb35
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:55:49 +0000

    underneath consist rest fine justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def subtract_numbers(x, y):
+        return x - y
