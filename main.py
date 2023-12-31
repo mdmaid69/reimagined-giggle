b5935e8562d15e1d574acd09037033466683cf9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:26:44 +0000

    organization attract scared strike however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
