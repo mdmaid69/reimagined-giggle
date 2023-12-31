d0a7fd60e0bdb536cbfc325d0af0dfa89e747a9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:50:52 +0000

    uncertain stranger talkback near neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_string_from_array(array):
+        return array.tobytes()
