014db9a8ab18f2b5929f2722c50eca8a1c55bfaf
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:02:18 +0000

    fond invite criticism opposite today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
