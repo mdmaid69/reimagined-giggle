1aff853b359cdc8484ff10a8edd5dff5976c67ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:25 +0000

    offend sea delight interruption proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def square_number(x):
+        return x**2
