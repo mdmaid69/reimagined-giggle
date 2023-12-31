b74cdb81d2a59e43fd926c5188252bc098f810df
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:05:34 +0000

    steal pride foreign concentration concern

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
