c84425dfa381d9bac684be001c1c45f8afdba77d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:54:02 +0000

    drink move post total upwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
