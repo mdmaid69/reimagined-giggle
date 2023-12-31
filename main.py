cf0bf5895c95d89a9a45c3e5990ccc53dabc0a73
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:02:10 +0000

    noise into serious guard interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
