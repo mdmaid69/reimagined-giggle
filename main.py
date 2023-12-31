6631878d2606465385c2699ffa75b90149ef7411
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:26:46 +0000

    no callout pot vote lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
