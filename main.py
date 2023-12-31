5fb2d6f1d8fc25b692bea063c284c0976a1c87b4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:42:54 +0000

    yourself disturb proud giveup space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
