f5451edd708f6529e13da2e203a279faf859b01c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:02:07 +0000

    breath measure accordingto mind detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
