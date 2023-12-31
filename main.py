ea89699d4fb08268fa2e83d672d03fc50dcbd23f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:31 +0000

    attractive steady limit average regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def sort_list(lst):
+        return sorted(lst)
