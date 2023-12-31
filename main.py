ce54214143b8dbdc824a39db67d027a05fd5c5aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:48:49 +0000

    condition keep violent their extraordinary

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def append_to_array(array, item):
+        array.append(item)
