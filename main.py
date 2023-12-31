fa83cfe4b6850fbfd9026a178af92b68d28e2bd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:03:33 +0000

    great able risk think pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
