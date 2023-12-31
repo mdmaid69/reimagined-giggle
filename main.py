1eec743e7cc0f12f070f1914265539241ba9c6a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:30:56 +0000

    peace everywhere reserve mention once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
