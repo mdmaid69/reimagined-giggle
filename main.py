2b2e700400dfb9a1f56775c0028bce40b46e05a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:42 +0000

    community fold care party excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
