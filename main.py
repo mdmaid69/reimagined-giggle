49bc5fe7c8d18f06f4124aff90a40e3adb452cdd
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:53:37 +0000

    trust payment whether cruelty roof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
