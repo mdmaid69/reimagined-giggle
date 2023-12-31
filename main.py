d93ff5e9e68c68909200234e651d37f096c15d3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:55:21 +0000

    cinema company related stone flat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
