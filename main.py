f3e63b9f4b1867bd80999e171d90bda196e5ca19
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:25:09 +0000

    form arrangement mystery hurt rid

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+print("Hello, world!")
