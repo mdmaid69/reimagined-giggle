5e4c6dee4c04702f5ff86ad45f732244cfcf4d7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:16:02 +0000

    different firm quick brain bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def square_number(x):
+        return x**2
