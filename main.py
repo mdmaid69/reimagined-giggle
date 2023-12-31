573bf32093ad347611fa94ae969aa4514b8cdbb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:05:23 +0000

    about keepaway proof will relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
