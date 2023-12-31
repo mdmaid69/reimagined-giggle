5fd1e1cc91275cba7ff79ab0760adb8933b00855
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:57:44 +0000

    name pull back smoke would

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
