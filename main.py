9d07f590a49120c3db00bd54eb9d58e2fb6453b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:50 +0000

    pollution peaceful disturb private top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
