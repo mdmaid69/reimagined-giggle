231249148f56cbe0548dacd5fe616c42eb173fb7
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:31 +0000

    address move complicated model spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
