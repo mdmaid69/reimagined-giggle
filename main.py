35555ccfb150dc98a1bdc3c195ac51f77cd83dff
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:48:23 +0000

    female holdup condition new sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+print(sum(range(10)))
