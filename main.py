2f0ca8a3d5c4ec5c45c874ef8b11f7283a404069
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:36:05 +0000

    base opponent spin clear flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
