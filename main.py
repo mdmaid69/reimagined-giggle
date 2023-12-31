3e56f74ba191a870ad89886a5deab8397ecd947b
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:27 +0000

    bored fashion wide dream sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
