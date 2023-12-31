6909255420935a682d1bfbf367ea0d669f86219d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:48 +0000

    come comeout getaway room radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
