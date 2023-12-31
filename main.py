b7970ac35890b79318da1a6940a59a1741f7c942
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:47:32 +0000

    back culture fall flat road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
