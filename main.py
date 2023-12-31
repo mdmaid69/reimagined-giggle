b44310adaba45cae82fde64ba34a7280e96c3945
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:55:19 +0000

    I well noisy talkoutof sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import numpy as np
+print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
