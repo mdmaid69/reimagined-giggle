61052bfcf822ef1cf6f840c778b517735bef500f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:37:41 +0000

    waste teach valuable price operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
