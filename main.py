a33dae0ba1aa442a5602ac47d9f4cc8fd38a0dcc
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:52 +0000

    smell kill effort light makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
