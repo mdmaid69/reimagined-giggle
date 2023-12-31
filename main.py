61fb7412dc70661246785dd59b3f925f8b6aa487
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:15:43 +0000

    loud blue born experience pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
