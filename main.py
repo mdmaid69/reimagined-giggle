980fa9aae658cf4007cf268bf2c0ad860d4e464c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:06:16 +0000

    warn punishment possible doctor stand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
