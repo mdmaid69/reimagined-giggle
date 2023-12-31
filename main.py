a190c5924927aa54239a2585fe720cd7e0140bfb
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:47 +0000

    weak disappear breakfast strict wonder

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_power(work, time):
-        return work / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
