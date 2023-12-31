c16a22c5f0484e17bd58eb12630995b2fab3efd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:17:47 +0000

    remark race paper command future

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
