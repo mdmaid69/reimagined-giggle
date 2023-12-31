8f6f687251e93bcd4732d01fbb457a73dee4851b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:25:38 +0000

    load takeaway spring else play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
