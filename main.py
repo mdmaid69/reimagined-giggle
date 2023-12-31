567ebe67d4a92f7d0e85f387f1ec815f626380c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:50:31 +0000

    offend smooth grow habit keepout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
