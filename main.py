f37f6e209afa84444e64811c9a56c6af6ab0015f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:38 +0000

    change aware evening generous noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
