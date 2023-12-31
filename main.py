278f067e55153a32d8170ed48c4fcf4627d7bc1b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:13 +0000

    repeat translate rest short news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
