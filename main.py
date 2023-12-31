3b97b955101bd342d499fa25bdf1898788f08db2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:08:58 +0000

    variety go opportunity from principle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def convert_to_binary(n):
-        return bin(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
