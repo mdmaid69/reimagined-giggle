eea35ebcf863dc4bbb97f3fbb0fdcf8b4513c6a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:43:56 +0000

    reduction whole match present peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
