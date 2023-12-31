2296c2063a620f696240a82bba942c800b4173a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:41 +0000

    shout mean shoulder children head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
