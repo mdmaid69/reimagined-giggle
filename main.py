3f62bdf2135a288cebfb73fcc651870f53488f12
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:55:16 +0000

    switch back injury task explanation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
