d53b93f6174eb5b14e1fed14c5bfb9257d83bf7a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:46:49 +0000

    weekend protect pretty change base

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def append_to_array(array, item):
+        array.append(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
