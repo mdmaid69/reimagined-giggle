d62eb95d55ea8cd931fac6f4540e96813456a326
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:23:44 +0000

    unite name family touch hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
