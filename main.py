133604015cc9f0bb24b38e32ae2d7886aa7a0e90
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:23:51 +0000

    increase unusual settle lose hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
