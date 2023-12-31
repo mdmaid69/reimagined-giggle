214329302de4bbe901f85a686f4c043f90fd5047
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:18:43 +0000

    turnback acceptable thick human worth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
