5cc4ebccc70dd1a205ac9dbd3fd4b4ec66e05757
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:10:24 +0000

    after rush think know pure

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import time
+print(time.time())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
