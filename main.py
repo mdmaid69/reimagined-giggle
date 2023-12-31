3a02b1183c7472c518acbf363a821aa123973cc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:42:04 +0000

    quality everybody runaway several repair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
