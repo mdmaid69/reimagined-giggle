5564815137c146cbaba081c079a10f368d40bee9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:55:45 +0000

    home bring push fasten dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
