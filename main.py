622bfb66b283147cd01b70bedd7bca6522e249b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:16:03 +0000

    immediate nor frightened turnback mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
