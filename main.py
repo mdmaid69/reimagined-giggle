538a36a62a2f33f826b3cb1aac70ca1213520ffc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:31 +0000

    act true comeover fine young

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
