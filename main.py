02fb1975df39a696da77d6963b3d95a636bb7ce8
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:17:36 +0000

    lay almost keepfrom lost breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
