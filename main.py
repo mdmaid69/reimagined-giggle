73cfd7387f2f961ca736e264e9b89d959ce2224e
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:47 +0000

    separate pleasure on shopping pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
