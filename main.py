f8c54fa90b392ab3aaa01c4274d03fec95b63c85
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:45:00 +0000

    argue approve laugh trust waste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
