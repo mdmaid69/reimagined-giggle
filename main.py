bc4b1c08929a0afb018f017f0137950aaccfb190
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:36:41 +0000

    approve crash charge movement talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import time
-print(time.time())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
