a1e1cb25010f7d9c375dd1d9ac8f25338e796b53
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:17:09 +0000

    surprise kick getback air aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
