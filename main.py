2ab8a01c308b55b9efe1d9795e3b181d738a800a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:28:59 +0000

    travel workon thin clean stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
