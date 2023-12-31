2e1fecf74cc9ba0efcbf07f18ee59b433ac54a75
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:12:15 +0000

    reply occasional stare brush rescue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def sort_list(lst):
+        return sorted(lst)
