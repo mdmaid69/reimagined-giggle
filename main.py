6c33d82b859057b38033acc6c9a78bd6cc38321c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:10 +0000

    give wash silent straight scared

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-import sys
-def print_python_version():
-        print(sys.version)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
