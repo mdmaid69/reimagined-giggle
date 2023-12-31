e293cda5c4dd4ca1754b6c233db68251169f5111
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:13 +0000

    holiday recent Bb middle breakfast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def sort_list(lst):
+        return sorted(lst)
