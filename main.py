9ecc87aa2a75fbad6cd40cb195e53fab9d56866c
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:36:15 +0000

    address wrong passage me fit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
