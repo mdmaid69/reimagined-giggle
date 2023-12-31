1ca6b0be0dedc9489373f302767d5af2bc586666
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:22:12 +0000

    space advise most classroom radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
