fbdbb58d870f1b421a080f5b217480f982d7c3c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:36:29 +0000

    need entrance taste pick contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
