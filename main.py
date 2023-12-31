a7e0d32abd418cdcbb7b3c9f9f89d444ff948a05
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:24:35 +0000

    bear large giveout advanced frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
