69fe869b3acf83071bba407ba635177f276ceb90
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:27:14 +0000

    confidence branch discovery trade clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
