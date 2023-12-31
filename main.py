3f2e144bd1a502aa446e329dbacfbb0e897378ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:33:47 +0000

    price persuade admit schedule coat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import random
+def roll_die():
+        return random.randint(1, 6)
