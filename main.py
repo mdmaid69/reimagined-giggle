7862f11bc87d1451e312f6bf645e913c2fd80e55
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:27:10 +0000

    here herself slice office treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
