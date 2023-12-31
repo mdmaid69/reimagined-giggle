235686555b9f087054bf30dc41bda5cdddf26a1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:01:07 +0000

    hour impression important sing normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_itemsize(array):
+        return array.itemsize
