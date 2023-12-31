4e77595046c3a77dc80f890d26f83743cbc1ff64
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:10 +0000

    shout friendly use fortunate worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
