eba0da08fd1d20450eba65f75de813e40290dff6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:11:58 +0000

    sensitive bill would treat foot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_float(array):
+        return float(array[0])
