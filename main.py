29d9e1bcddaa799eddc7ffb61c4aafe18971be04
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:03:10 +0000

    although look plan money cap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+print(sum(range(10)))
