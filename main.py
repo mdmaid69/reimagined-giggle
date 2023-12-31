1b4e9487ce69bc8228f16c4252c6f86d515d289c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:59:33 +0000

    style foreign night opposite war

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
