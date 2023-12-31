3f7a276f7ab983cc2b6d98f1f2ce98246c4f9a9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:34:34 +0000

    tool apologizeapologise fortunate beautiful cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
