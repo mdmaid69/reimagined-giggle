9f9c1641eaf1687eed40a7722779edaf007abeae
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:42:13 +0000

    both point serve weather herself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def sort_list(lst):
+        return sorted(lst)
+def count_elements(lst):
+        return len(lst)
