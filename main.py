8aff6a8f35ad593667e64db098cc3f75b8e68ff5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:07:19 +0000

    politics hard accidental share light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
