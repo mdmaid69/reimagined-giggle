469fd0506b6034d272e8793dfdcffa06f52f97b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:26:31 +0000

    sea lunch mad system nearly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def convert_to_hex(n):
-        return hex(n)
+def convert_to_octal(n):
+        return oct(n)
+  def convert_to_octal(n):
+        return oct(n)
