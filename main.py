dded7fed68e07501b5f09da8d31dcffbe23f25e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:41:02 +0000

    pay poison boil achieve upstairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import sys
-print(sys.version)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
