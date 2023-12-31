a2719dbb3807db0b3c82d6f6c6fbc87727575c0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:30:40 +0000

    change gradually backwards before result

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
