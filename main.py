294ed05dcdc11a669af1b4d661faf08d93ff82d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:03:43 +0000

    wave skill occasional any conversation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
