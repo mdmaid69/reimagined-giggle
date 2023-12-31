e0d96e37824c21325de009529b8b8ad3426355c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:40:51 +0000

    threaten voice thank market do

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
