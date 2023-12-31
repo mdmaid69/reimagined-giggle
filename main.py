cae6a922097709234bfd45779193ed4079d00d8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:38:26 +0000

    concerned many rare scheme near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_area(radius):
+        return 3.14 * radius * radius
