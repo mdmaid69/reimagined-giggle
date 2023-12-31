fc7e5c6b26fce82d1ef6f6ccfb19f87cff026e72
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:31:31 +0000

    pleasure thank taste empty continue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def cube_number(x):
+        return x**3
