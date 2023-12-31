2eb7c84d59251ee810ff10aa5094b31d8ce7443c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:56 +0000

    criticize above walk wait prepare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
