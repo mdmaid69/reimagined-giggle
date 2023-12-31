198c09db4a1acb4b88f3c8a9ad753c06be517418
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:53:25 +0000

    welcome and apparently weather design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_power(work, time):
+        return work / time
