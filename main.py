5704236c8dbcf174b446efae0b1f96149a902b90
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:41 +0000

    smooth our size safety whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
