a0bc439b3e6aaa2b86f9d0a77dc3401737d8aca8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:42 +0000

    message reach likely ready bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_float(array):
+        return float(array[0])
