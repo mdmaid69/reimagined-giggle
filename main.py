0fcff032c62c477d7bd65fdaa68d1cc95f34ea46
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:32:46 +0000

    tie generally mend badly destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
