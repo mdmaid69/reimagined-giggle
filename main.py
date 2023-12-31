510ff4a40acfae9c4e0553751f6c945a27498857
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:22 +0000

    tool Ll violent harm famous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
