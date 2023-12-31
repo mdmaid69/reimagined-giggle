4ac1b38a7fc10b3ecaf38a13d28f58abc0df03a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:22:32 +0000

    round apologizeapologise disturb other concentration

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_string_from_array(array):
+        return array.tobytes()
