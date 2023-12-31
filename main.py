73a29f3457755c886be850adb5fe71a38f01e386
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:40:47 +0000

    commit plan include minor fight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
