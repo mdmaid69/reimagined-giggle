56be19a845d190dc30d10a15a92117a7046d06b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:10:44 +0000

    turnover quiet brave fault different

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
