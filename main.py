b22dcd70148426c2430817c8c172a45878eaccf7
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:42:51 +0000

    lot ability wipe information unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import datetime
+def get_today_date():
+        return datetime.date.today()
