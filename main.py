a130436ecdd03da578b9fc975af462d4bdb1a5c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:12 +0000

    unless after section especially ocean

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
