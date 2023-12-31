61a9fd4074c51bc3018a31cfdf738204229e103e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:46 +0000

    himself complaint actual wind study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
