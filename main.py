ac18b7c1a90c3ecf5f0aa55148de7fb1715620bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:43:28 +0000

    fierce approval tax unlikely difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
