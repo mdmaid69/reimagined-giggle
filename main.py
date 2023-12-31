fe25ee025cc96ce9e2ac584ca7dbb83b45933d49
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:52:53 +0000

    leave level aware getoff off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
