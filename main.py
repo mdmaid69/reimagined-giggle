aa423a125fa31acafdbf3a6adb0e7b766d2508e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:37:27 +0000

    possession they you gate taste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
