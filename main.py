ade31e8e397546f56d66c3f38aeda30720de16f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:17 +0000

    taste meal absolutely instruction call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_bool(array):
+        return bool(array)
