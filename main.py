2ce0f5844950f14e17167c5166e72b534a3a3261
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:22:57 +0000

    someone embarrassing crisis it uponon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
