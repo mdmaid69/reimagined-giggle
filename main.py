ba1adbac0be1bdbd626edf29b8e4e8859b81ea92
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:58:00 +0000

    pretty sound content naturally downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
