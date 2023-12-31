ecf63fe69ddec9899361324ec21900d424dbc10b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:27:35 +0000

    almost unfortunate complete trip programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
