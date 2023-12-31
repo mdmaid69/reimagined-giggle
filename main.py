d3dd80356ada62a0b13acca06999e6f371c1b0b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:15 +0000

    dear even receive into choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
