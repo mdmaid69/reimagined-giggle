2b7b23ac1a430c235a28139f600a2855cdac7ea3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:51:29 +0000

    step badly law rescue chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
