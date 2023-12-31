e161fead973810e53a29c4ced7d5592c3b326bd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:35 +0000

    regional beneath their provide surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
