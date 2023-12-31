9e9aa5a920bdab378dc3d3fd390b372b6602335b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:54:34 +0000

    drop borrow dance treatment protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import random
   def generate_random_number(start, end):
         return random.randint(start, end)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
