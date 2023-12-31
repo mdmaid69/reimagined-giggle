2c6ab93ed973ea2b68c80e51f653be13855dd42f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:43:04 +0000

    translate authority anxious Zz Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import heapq
 def pop_from_heap(heap):
         return heapq.heappop(heap)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
