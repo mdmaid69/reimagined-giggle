8bee88d7e850e7687cd5d6c8b2e0603c7678dcd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:45:51 +0000

    pinch whistle responsible bottom detail

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
