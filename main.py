990f2a7b001db9d6667acdf02e50cced694513ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:09:28 +0000

    centimetre only object instead recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
