5623830d0a991c89a1319b3b9b409522fcdafa87
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:12 +0000

    disturbing play shock likely attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
