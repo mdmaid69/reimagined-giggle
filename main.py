e96bf318a54c36d1e03fd0872a2d07d9955190ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:28 +0000

    trouble match poor name fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        return sys.version
