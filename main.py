b2e2e8e2448422e784b81b902885b13bd20528eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:39:25 +0000

    however Cc secret swimming shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
