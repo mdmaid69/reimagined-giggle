24b757b342fab891a2b549fa072d420942479b23
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:16:23 +0000

    grow frightened lift be offer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
