1ebc0e3ae98d30db181205a203ac2623e88e966b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:14:38 +0000

    complete roll grade object tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
