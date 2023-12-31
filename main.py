b2e4ffb9af2d6e1a49158a42319495450715f270
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:15 +0000

    experience basic comeround grow listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
