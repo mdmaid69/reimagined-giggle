90b97ae18300a724c2a833fa65de274fd52e6fa8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:36:55 +0000

    year something part shelter argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
