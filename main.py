a3151b2be41d054b72ed9fda79349734c08fdd2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:33:04 +0000

    offer fasten minor moon distant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
