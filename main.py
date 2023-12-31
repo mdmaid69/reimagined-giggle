767d3ea0660948f45461f7d445b159ab3f72ff8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:35:19 +0000

    determined communicate doubt turnback practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def cube_number(x):
-        return x**3
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
