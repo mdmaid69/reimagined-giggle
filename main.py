0fed77b6bfc526f69b87ff1e229a9a479d250474
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:23:19 +0000

    fresh genuine active what level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def square_number(x):
+        return x**2
