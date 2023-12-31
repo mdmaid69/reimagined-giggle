66615d39c83ce6b49b6c5a98a8cb7835a3d5cab4
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:30:40 +0000

    valuable finish union keepup giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
