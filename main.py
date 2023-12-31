6a5407294ead61b109b18a14aa8ac877fda0e122
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:17 +0000

    pride tomorrow exaggerate compare paper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
