ad8031a9a93c02613cae3066ebfcd156102272ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:33:46 +0000

    this mend practice valuable before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
