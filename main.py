d4b1ef221770e392574247814a291625858e2fc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:20:14 +0000

    real but rare wind clever

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import sys
-  def get_python_version():
-        return sys.version
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
