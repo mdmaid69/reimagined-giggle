3486d4f65a17ed87795d7b34233298f352d44038
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:45:49 +0000

    concentrate fear creature lesson should

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
