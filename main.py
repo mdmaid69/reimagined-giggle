089882ae667b2f4cbc60943f4ec863e2da261018
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:15:53 +0000

    finger stick card life keepout

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_max(lst):
-        return max(lst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
