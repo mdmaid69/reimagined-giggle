7453da5f721bb960f2af728278a649b3a59fb3d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:33:51 +0000

    doubt during she tie burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
