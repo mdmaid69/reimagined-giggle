209d46dbd49cad506cf4f271ba14c96ce797f828
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:11:39 +0000

    suppose soak slide land watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
