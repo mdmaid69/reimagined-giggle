83cdaabecf1789c2a89363ceaa7dc4bd0740d2bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:53:02 +0000

    hit float nonsense open thing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
