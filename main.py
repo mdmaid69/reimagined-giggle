36d3c7e20d18d2b4ef1b240b652b9346cc2ece75
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:47 +0000

    seem pleased respect protest convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_max(lst):
+        return max(lst)
