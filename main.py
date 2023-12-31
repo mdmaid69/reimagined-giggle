eb786b3e9a398f615b078213cee5a45ebb1ee3cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:59:28 +0000

    dry later discussion thinkthrough giveout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
