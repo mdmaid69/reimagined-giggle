c6ea840de7e4fa01746ff110dd002afe785edac6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:22 +0000

    popular as exchange before everyone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def count_characters(sentence):
-        return len(sentence)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
