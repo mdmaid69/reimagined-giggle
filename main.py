6ed453acbe4b566970c5ccab0d156fdd90f33364
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:57:39 +0000

    old report break relief pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
