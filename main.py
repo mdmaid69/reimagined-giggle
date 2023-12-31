2f6a72e4297e5a470dde995e6c272fc7e6edb866
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:29:48 +0000

    earn disappointed safe judge worried

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
