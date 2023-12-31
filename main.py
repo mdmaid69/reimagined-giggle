4ccdd35fb8637f0915462f0bce763c8090c92e56
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:56 +0000

    cause nor change repair slow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
