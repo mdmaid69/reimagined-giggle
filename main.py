abb88b80cbd8133e32726d876d70b6d489bece90
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:18:51 +0000

    sister nice complete town flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_acceleration(speed, time):
+        return speed / time
