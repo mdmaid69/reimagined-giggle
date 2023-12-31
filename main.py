8e75368a4facf50446a2fa76f383e2d740d071c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:19 +0000

    drawing meanwhile game well important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
