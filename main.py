2bd0387d200ea1be3f40df7555846556140cac64
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:31:44 +0000

    education talk quickly blind flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
