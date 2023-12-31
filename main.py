54a261a02bfd51207de5ef9a6282a32b5ef3c4f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:01:05 +0000

    explanation record start stand everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
