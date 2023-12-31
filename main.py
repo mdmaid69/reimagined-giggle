b458011e55ef287ca8e2274da29d5bb91414810b
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:18:30 +0000

    variety label bar hardly thankyou

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def count_elements(lst):
+        return len(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
