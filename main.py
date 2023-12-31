d855e0fed2bde0d0b36109e6fe4220ee24ab3ea3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:45:15 +0000

    middle exchange ride shout prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
