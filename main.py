cfa6824c180e78284d34f7b7362d7a662e870b30
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:18:22 +0000

    sell history how maybe boring

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
