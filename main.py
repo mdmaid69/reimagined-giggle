0601adb841014cc709a3a4dfb064800d725146b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:04 +0000

    murder breakfast right unit pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
