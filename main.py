a00ac5ac360519c2135343f12297f2bcbd324d79
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:12:11 +0000

    tune turnoff mark either sweet

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
