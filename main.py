17c5e786fbb597de49b06488f8d03475b38a6e99
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:32:53 +0000

    offend charge always past scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
