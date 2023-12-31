e0495d6b37618491ece0144938566aa6d8523a4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:09:11 +0000

    murder undo empty base middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
