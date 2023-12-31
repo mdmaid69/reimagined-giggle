c974ffe372d65570342e49c8f09284e55438a0ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:44 +0000

    holiday comeround fact black him

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+print(datetime.datetime.now())
