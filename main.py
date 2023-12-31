d48bff47776da018acb2ddcbc71cc1f0a0d030f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:50:44 +0000

    behind splash smile relief congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
