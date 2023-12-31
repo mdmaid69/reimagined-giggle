7cabedeb5b480b1143c2493fe9a75e6b2528b8b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:14:34 +0000

    impression habit within wish last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        return sys.version
+def square_number(x):
+        return x**2
+for i in range(5):
+        print(i)
