97c6ce4c1914384eb1ff743ab9d8b69008b0d745
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:07 +0000

    plain than concerned place everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
