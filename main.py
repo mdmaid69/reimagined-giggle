ef3d935b0ff60c9ae0461a80277e0e8abdbed3df
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:59 +0000

    once lookup lookafter suggestion frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
