b775380f54e0821439546f9ff37f350bfe9f489e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:27:19 +0000

    fair art there press whisper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+print([x**2 for x in range(10)])
