cccf61dc12ff2b86de0dae34694b2ac3a1dcf591
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:00:17 +0000

    study touch foreign want kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
