518db58371971f4cddd1635dac143cdb343d3ee2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:07 +0000

    necessary dish suck without weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
