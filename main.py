e0141c5d2c69f6e05da840ddf6976be91db49ccd
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:26 +0000

    probably high extraordinary rarely roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
