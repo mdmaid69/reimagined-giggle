054ce573fe7b2843bc27399e7280cc54c3ffebf3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:03:37 +0000

    oil progress original Itself national

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
