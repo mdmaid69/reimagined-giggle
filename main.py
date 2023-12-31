ccbab1e858dc84143f03bcd6f025009f8b7d9f6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:53:33 +0000

    breakinto bush each seriously comment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
