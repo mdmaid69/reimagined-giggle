4d53d70e24b2ac2fce6859b0d8c468d6eec7ce42
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:14:39 +0000

    quantity uncertain bring may middle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_average(lst):
+        return sum(lst) / len(lst)
