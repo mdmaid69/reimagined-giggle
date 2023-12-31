7260d28c6376efce52478c0da2e84773e9e96522
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:08:41 +0000

    rush because impression search quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
