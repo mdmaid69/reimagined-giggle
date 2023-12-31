f12bc7888bbe1c225be595d996d39be578f60f40
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:01:44 +0000

    division deliberately side late calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
