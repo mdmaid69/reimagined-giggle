27bc83aed1a503fdd53196f35e5a2cf33744d14c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:25:06 +0000

    crowded loose Zz mad Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
