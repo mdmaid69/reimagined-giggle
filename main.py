b7005e9cfa1a8c33aaacf8cc1891dc6c7a184c78
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:57 +0000

    mend Zz should responsibility freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
