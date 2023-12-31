db677a4c790ccea86013686f6830bd0f5188af15
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:59:28 +0000

    used answer importance complaint act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def sort_list(lst):
+        return sorted(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
