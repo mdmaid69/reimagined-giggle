c120a8edf0a57144399b47f1c9c8f2a4da4adc9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:46:10 +0000

    store pity spite comment getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
