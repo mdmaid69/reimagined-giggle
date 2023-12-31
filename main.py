067b5fa2bcd36e189f54ea4596ea547a6ab48f98
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:12:55 +0000

    runinto fortunately attempt peaceful relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
