5dfd66d4c779b6c67ec0ee558a6bcc2a8d1160ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:16:25 +0000

    exchange show drive tonight ring

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
