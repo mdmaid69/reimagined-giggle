85c383b63e9ba736e258f5e5328079a646997e29
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:49:58 +0000

    mass against play bye damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def remove_duplicates(lst):
+        return list(set(lst))
