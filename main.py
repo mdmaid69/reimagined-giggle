f2c47d0646f2c400c79fe35709043dfc9c96d962
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:33 +0000

    gift direct hour worse limit

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def convert_to_binary(n):
-        return bin(n)
-def greet(name):
-        print(f"Hello, {name}!")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_item(array, i):
+        return array[i]
