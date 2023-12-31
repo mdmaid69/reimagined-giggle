8b3c90d0817f9b9fb208d8cb67c5023ef1b4a755
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:18:20 +0000

    grade condition include slightly completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
