8319a99d49bca33e89ea1c76a9c44637ac17ce10
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:39:58 +0000

    himself limit keepout scheme spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
