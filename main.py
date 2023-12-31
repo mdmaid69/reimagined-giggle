8524f58dbdb0183d67a312596310a834988b037b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:19:47 +0000

    wet doctor keen afternoon godown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_today_date():
+        return datetime.date.today()
