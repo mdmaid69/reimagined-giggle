1c14f543b942879864d617d3949c781b0dddfc96
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:41:40 +0000

    breakout after once cap slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
