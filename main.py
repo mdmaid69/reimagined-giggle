fd2351cb4bd70563efb3b35443717ea822d732ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:04 +0000

    worth various lesson country value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_error_function(x):
+        return math.erf(x)
