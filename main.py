76b05638e39254cf576489a1cd02ab8b091324e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:25 +0000

    generally march debt anyone door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
