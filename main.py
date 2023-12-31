eee117341597084496b55b863f7b79e45b7876f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:15:10 +0000

    duty up mine church practice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
