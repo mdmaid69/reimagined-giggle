17d3a35e1a1ed57860fe425f9280c05ec14399e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:18:34 +0000

    mark hurt without never patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+def find_min(numbers):
+        return min(numbers)
