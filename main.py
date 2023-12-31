47d52c4695a00144b8631002646eac4b87e9b807
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:42 +0000

    available vanish soil 571enough file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
