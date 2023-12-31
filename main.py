93a3fdf93af75ff8e039d79ce29386f25ec15ec6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:40:39 +0000

    beneath search square takeback Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def delete_file(file_name):
+        os.remove(file_name)
