7d0d82f04fd74430686b5e50092911341d04d610
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:52 +0000

    rush cover can gradually opponent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
