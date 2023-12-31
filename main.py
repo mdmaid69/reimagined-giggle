e01ac915e2c1d5d6480de87dd7b692b8849b86d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:24 +0000

    bored offend tap short attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
