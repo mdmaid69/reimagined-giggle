e893bbc5592f509d8431db3cc7911c6ac1f945a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:31:47 +0000

    flavour with plain rent write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Words:", len(text.split()))
