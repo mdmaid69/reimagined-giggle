eb1c378a58f4a0557af253c1b6c1aa2475df068e
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:51 +0000

    pronunciation some appeal circle crime

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
