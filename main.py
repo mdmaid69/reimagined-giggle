6a92f8df9241fb4dd7dbe42e22e6a328a43686fd
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:35 +0000

    wind recover whistle song empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
