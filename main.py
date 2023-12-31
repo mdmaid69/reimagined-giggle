f2fbf1c09cde286c6dd7ab957788efd67dcef09f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:22:46 +0000

    experiment tool tall lost game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
