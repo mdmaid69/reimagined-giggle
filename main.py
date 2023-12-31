8414fe01f3a7c6d777b6b85e8e51fa581fe59128
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:18 +0000

    cross slight moral legal plan

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
