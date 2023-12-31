a2eec4043fe2f120ce4a5411f60d33bb9b40ea2d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:09:06 +0000

    discover simple else union available

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
