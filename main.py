bdaab7fbe6454eb5ddab12741f47ffb416340191
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:08:38 +0000

    street evidence wrong store first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
