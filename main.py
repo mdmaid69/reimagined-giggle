775c3f80ae701475e75f755058ad3c67fdcdb13a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:46:21 +0000

    stare muscle dinner himself in

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
