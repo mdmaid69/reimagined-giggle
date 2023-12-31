f6effaf179b660157db461b8eb127632561b12db
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:59 +0000

    exist past complain desperate drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import sys
-def print_python_version():
-        return sys.version
+def calculate_density(mass, volume):
+        return mass / volume
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
