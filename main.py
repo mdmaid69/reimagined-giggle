9b5b30058caf521e37d353cb14a76fe3369cb6c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:10 +0000

    deserve guide except read judge

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def cube_number(x):
+        return x**3
