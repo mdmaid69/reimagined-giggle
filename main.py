b43705d11b6147e6bcaf906cbe118127b3505c97
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:03:09 +0000

    tend mess offence congratulations change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
