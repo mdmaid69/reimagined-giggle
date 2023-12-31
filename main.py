01aa9d87df5cc0a1a2e8b781ef4a2a60327039cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:46:57 +0000

    combination boring pleased border near

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
