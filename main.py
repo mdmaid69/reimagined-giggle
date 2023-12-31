adac9f2064080d7ac8fe3818ebc1e95ff008adbb
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:33:51 +0000

    cry path stage treat skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
