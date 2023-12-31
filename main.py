6536f3cc2774d2bba21041fc3d9bec6cdbb2121e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:27:15 +0000

    front race hope squash pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
