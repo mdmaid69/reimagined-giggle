1318ae1fb4561a11d495499c2341615500aa7799
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:52 +0000

    crowded blow them suggestion favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
