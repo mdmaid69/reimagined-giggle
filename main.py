d416cdda004729168e19bcf1a3a97078709d88f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:37 +0000

    around satisfied refer calm desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
