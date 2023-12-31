6c6f6ee02c2609ea907fd8023b5cf3a6b34433fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:03:57 +0000

    obvious fall hurry aim spite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import collections
-def create_user_string():
-        return collections.UserString()
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
