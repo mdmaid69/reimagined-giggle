7bfe554d3afe880c9c8a678a5355e52f99c95cb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:33:53 +0000

    exist crazy down borrow morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
