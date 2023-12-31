7cedc7e4648bc723dd81b66ecc96c78089f0fa22
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:23:17 +0000

    record invitation wide Ss content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
