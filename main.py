620578faa56621d5d80f1babc3474686010d7696
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:54:23 +0000

    separate violence about terrible cold

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
