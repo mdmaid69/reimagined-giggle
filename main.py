45841c2560b58bc06146e0a9cb68fe6c890e9c9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:06:03 +0000

    order spring reason test difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
 def calculate_hyperbolic_cosine(x):
         return math.cosh(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
