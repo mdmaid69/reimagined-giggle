eca6d73d9cc2cba79057483a61eb6105337f13e9
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:13:43 +0000

    watch maybe dull produce spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
