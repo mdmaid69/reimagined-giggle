f0d287ea8f538b66a9804d2e4b75eb3524a49034
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:51:50 +0000

    detail blow runaway tune wise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
