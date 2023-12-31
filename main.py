2af4e4652920394a86b1dd9b079bc1d428cd8dc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:46:01 +0000

    temperature respect responsibility challenge naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
