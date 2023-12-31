2e216fcc668219fed2969b5e52d4d58d2d405bf7
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:33:00 +0000

    build cover borrow bank judge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
