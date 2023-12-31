933da9d5124358c0807c8aa673334269bb30d163
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:32:28 +0000

    hope development attempt believe hope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import getpass
-def get_username():
-        return getpass.getuser()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
