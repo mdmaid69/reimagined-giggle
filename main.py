fa6e0f77ac9d7aaaad89fd82ee3c6713b3a4fcc9
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:11 +0000

    pity protect drink warn television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
