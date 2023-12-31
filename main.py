426fe0fa3318efc8c10630b6b6415ccde6540633
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:02 +0000

    attract glad importance speak remember

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
