c0e7a5d0636e2adbf4f97a125ffd87d7243d0f7f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:01 +0000

    spot experience proof exciting none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
