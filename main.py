2b61c5c16299d78420bcc8dc80ab6a517cb589a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:27:32 +0000

    smell embarrassed coffee realize certain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
