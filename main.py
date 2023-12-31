67aa90ecc620fe3c456276bb7941a9faa32b6c76
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:19 +0000

    present holdup this comein ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
