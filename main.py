c4025ec674c84d6cbb0d039ccc484de53202519f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:19 +0000

    while miss most swimming underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
