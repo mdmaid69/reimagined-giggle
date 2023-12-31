4c5630122421b1614cad64c5230beb21437c0971
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:39 +0000

    gentle comeround free type gradually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
