a07db9dc88b0144a32b314dee0f5a6bab5475011
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:39:57 +0000

    boil hit your boat authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def count_characters(sentence):
+        return len(sentence)
