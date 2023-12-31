29184cf29f612d733bf23fbaabde9d7e04a7c840
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:32 +0000

    matter squash murder favourite praise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
