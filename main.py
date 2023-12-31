b245075ce6f09ba2fe6b2923e4a17b9d6f2e4bc8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:43:56 +0000

    promise recognize operate pattern rid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
