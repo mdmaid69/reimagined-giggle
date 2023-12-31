adc9e324e5425d23c2afcdc0c5ba62481571ddd6
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:35:49 +0000

    impatient trust off throw delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
