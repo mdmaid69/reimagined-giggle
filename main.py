2d622ec3044ac45220b3d49dbba6956ec1b4f07a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:59 +0000

    method soft bottom brief hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
