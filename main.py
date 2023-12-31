3062e606677a16f03c37f9d384b4a07f70e84e6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:28:26 +0000

    arrange Nn absolutely union beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def reverse_list(lst):
+        return lst[::-1]
