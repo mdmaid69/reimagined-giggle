6194fa82d4e47d45a5d976d434b3e216be7a96ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:19 +0000

    also takeout bit turn light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
