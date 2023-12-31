8737285a0eaad248e0a8779c3b6d86f1925ec605
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:55:45 +0000

    poison sensible rest behind watch

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_work(force, distance):
+        return force * distance
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
