956da77c5bf5fa78ee7073027de259b6e9302c50
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:27:58 +0000

    none typical stranger appear connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
