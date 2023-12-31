035cb969d8d6187446c2817abd5aaf36a794b199
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:48:58 +0000

    under female neat 179bite total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
