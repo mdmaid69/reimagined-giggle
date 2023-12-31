baf4f7d53689ab07fbe1bac78ba757f825ac06fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:55 +0000

    pleasure artificial place they drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
