92746fe95ac7993d5f58f5b38021f40a5787855a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:35:35 +0000

    exploration unless several open butter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def reverse_list(lst):
+        return lst[::-1]
