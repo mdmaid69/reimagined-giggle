f11a403b0a0ed3a6f111b33e77b68ab79b434f10
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:28 +0000

    measure easy point dry phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
