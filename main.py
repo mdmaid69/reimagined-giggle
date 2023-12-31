1e59e3c23ca6514ce05b4144b5ac65cf2f2f6ec1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:28 +0000

    loan into survive comeround day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
