72bddc5101f6d0dcb760ef9b6ba57cc714be706e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:26:43 +0000

    approval car nature people wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
