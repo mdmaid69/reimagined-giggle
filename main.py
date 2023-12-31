186c75bb5ccd56ef57dd651350b6a24b4d827d42
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:49 +0000

    stranger difficult obtain punish used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
