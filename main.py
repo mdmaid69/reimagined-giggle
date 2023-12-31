80511b07d4407f83fdb0160a38eda80af71ca456
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:56:20 +0000

    team beside little joke cool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import sys
+def print_python_version():
+        print(sys.version)
