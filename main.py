6ce43f0a6cb25b9b39bc8c017d4bead32ffb6bd2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:05 +0000

    approval can stage crime obvious

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
