19c0278c85451749523dde1e2f05382df306b58f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:11:21 +0000

    sale loose calm debt recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_index(array, item):
+        return array.index(item)
