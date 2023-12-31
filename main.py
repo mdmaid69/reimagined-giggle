61bf283dd318fc6287683c2d25ef69f4fb6d88e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:04 +0000

    national real bringforward move promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_work(force, distance):
-        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_odd(n):
+        return n % 2 != 0
