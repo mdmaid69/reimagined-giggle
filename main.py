846da911dd0ffdd75201f6c96a346ab347c75343
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:33:11 +0000

    range bet extremely hello lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
