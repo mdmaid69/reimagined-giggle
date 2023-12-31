8b681a826bfa08e6efa60f04fc275efccf79fbbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:17 +0000

    workupto cost tradition cook official

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
