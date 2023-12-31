e18f0f5a345bb0d17c33cd3a9e810809884d396c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:33:06 +0000

    personal at simple add curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def count_elements(lst):
-        return len(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
