dd306bc75a40e91bdcc03376379517348e75e1c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:12 +0000

    tune loose every another string

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
