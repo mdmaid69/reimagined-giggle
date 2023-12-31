ac4d45396b4317eae3ac7d1f66e172fcae3e9a6b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:09:24 +0000

    show charge indeed realize talk

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
