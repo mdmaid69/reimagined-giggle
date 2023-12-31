d32a6efe0062e41b481a489c37a1b6219264aafd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:31 +0000

    play left fix petrol male

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
