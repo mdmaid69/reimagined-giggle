3300c8d318569555771665b2ce422b003613384d
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:43 +0000

    check cover dig fetch cheque

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
