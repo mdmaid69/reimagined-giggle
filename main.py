4cfa2d17abdf0ec1237ffa6725583058cb3f7d4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:48:34 +0000

    base decide save smell birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  def sort_list(lst):
-        return sorted(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
