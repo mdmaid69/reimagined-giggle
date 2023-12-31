db9c3ec61ceadd9eb74673f94b71689bbb059f0d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:42:08 +0000

    win shut quarter needle expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
