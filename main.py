d229c032d05b3b97991d88391be8e91e445ba0af
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:31:13 +0000

    put television strong but last

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
