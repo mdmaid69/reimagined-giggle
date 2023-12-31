538b578bb1a285defcf57ecfd1c50232b09505f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:12:33 +0000

    relation so thinkthrough Ss rob

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
