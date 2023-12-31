d7ab75c6955058e007846b0be367394c6a415356
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:37 +0000

    listenfor arrive tap physical lookaround

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
