35e4db51d9a1ae261ffa3935585e9a7e3cc70721
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:03:59 +0000

    crowded first force month punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
