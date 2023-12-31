b593a339a9e993e1ed3f68115fed74afee493396
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:17:41 +0000

    you fall roll challenge because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
