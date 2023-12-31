3e0e7e54c8610d5b57525ba484b4cd88e0030702
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:32:02 +0000

    getaway today difficulty protect round

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
