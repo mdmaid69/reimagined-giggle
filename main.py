d2d72c7b6707858328f2c93668a07cb407350689
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:42 +0000

    meet below major shut slice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
