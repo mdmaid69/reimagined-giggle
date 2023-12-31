32c4c49ce6418f00d9ab0b135254fa061cfd33bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:53 +0000

    why up post welcome perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+print(datetime.datetime.now())
