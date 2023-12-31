bd89c666823b7508b91953c2cb8152070546055a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:57:54 +0000

    accidentally else much that safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
