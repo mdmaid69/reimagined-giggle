2dd09dab445c7bbdff7078dae0a8a9cafb002778
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:17:02 +0000

    final preparation manage refuse workupto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
