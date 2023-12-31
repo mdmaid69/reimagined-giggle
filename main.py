25012e9cb09bcced8ae0580bb51adc0648ef0aba
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:19 +0000

    arrive kill borrow gentle computer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import sys
+def exit_program():
+        sys.exit()
