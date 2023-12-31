20e6ca66e4468c9b2a6530cf7da4305fe8a1a790
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:48:01 +0000

    by takeaway bad search convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_floor(x):
+        return math.floor(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
