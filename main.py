757a75c610f2e52e0971a4c01318f989e376f909
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:14:35 +0000

    extra final free stand apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
