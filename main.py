d0179e0135db5def319413d0d7b5f282ffd19613
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:31:56 +0000

    disapprove connected almost woman concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
