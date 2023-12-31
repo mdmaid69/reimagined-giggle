ded05c1b22d0d1c5cd90fe80c5e39bb3aa1e96fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:44:48 +0000

    easy steady flow complain new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
