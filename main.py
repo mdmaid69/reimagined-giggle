72b1d8957e732bfcf5817b64210816ec3614d978
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:17 +0000

    used prefer wide book who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
