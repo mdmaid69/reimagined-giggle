edacfaa0488bdd3dfcac682c5a94cb6cd63efb28
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:50:24 +0000

    mess direct calmly stop tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import sys
+def print_python_version():
+        return sys.version
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
