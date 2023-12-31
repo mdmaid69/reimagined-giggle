3111e344fa70d188850235c7aeda259a4a1563ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:27:02 +0000

    face cheat only sleep giveaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
