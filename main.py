dfdc43c3aedcb86642e84cfeca35bde6db2c4881
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:43 +0000

    punish talk form face straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
