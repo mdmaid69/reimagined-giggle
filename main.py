b57242444f81cfbcdc0d618d80b81ba7a5809b48
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:01:39 +0000

    lucky foot business step crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
