7ce11b740e6cbb8f09ba60e48199ad9f18ead3cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:31:33 +0000

    cross pleased nearly sick programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sys
+def exit_program():
+        sys.exit()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
