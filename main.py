5525c25df044db27115dfc84d50a3552256ece35
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:22 +0000

    winter rest father look secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_item(array, i):
+        return array[i]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
