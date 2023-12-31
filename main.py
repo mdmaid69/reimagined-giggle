27126fd6466c041ff979b98a0eb0cb2b11470dc9
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:30 +0000

    punish open choose shopping pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
