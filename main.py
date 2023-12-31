8e6c3357ab3ac459d4ee2d83ef024f1f17b98c64
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:15:11 +0000

    relative goout mine accident expression

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
