43e155cdf0c7752687379ea90e695cf20473a02b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:10 +0000

    takeback still address ask region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
