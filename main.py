a7b84074f8f35c130e93aed415370e2f93f608f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:24 +0000

    discuss cheque turnon regret matter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
