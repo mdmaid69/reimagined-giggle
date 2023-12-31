018ed031c2b93190d53c737b94331c1e2490388b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:44:47 +0000

    responsibility powerful release fold correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
