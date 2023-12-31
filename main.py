8c64157cefd02d73bfa312b77ce5f3e5118bd95b
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:18:39 +0000

    want onto detail except region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
