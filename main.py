f685563c55075b1bb812d3f62e23e34768d12a05
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:54:41 +0000

    print chat sister out centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
