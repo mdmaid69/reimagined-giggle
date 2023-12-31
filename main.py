153245f52d0c49f33baba54721d0656dc26f0811
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:00:57 +0000

    fall promise degree gooff meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
