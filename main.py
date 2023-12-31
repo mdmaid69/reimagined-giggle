16f095578bae2cb560dd7aa21a0f726db649005e
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:01 +0000

    journey brave but anxious trap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
