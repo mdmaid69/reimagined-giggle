b0d34241ff5ddcb118ebc8dba250cea9e5a9ca79
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:14:36 +0000

    keepfrom compete rob must Kk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_min(numbers):
+        return min(numbers)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
