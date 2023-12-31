263130e73ce6546f0533c9e8aac2d565e12a196f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:02:37 +0000

    advise performance anywayanyhow explain near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
