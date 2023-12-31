e1292ab2abe67221df46872bf0f3f33e0d9340e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:54 +0000

    project search quick shrink now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
