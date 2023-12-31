1f954e382eefd50df6d31607a662c6da7a58a45b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:02:02 +0000

    unlucky float bus respect recently

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def create_user_list():
+        return collections.UserList()
