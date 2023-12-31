22d29d18d247305bcf3536654f8aba251c07c4cc
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:07:50 +0000

    equal raise badly female smile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
