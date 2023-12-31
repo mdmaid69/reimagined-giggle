82dc3e97b1aaaf49a5eccc8912b7bc74422648bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:07:22 +0000

    city life mention become close

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+print("Hello, world!")
