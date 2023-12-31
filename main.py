e697fdef1426cc9e5384e65556e9ebb480c87d42
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:56:44 +0000

    reply impressive lot Pp advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
