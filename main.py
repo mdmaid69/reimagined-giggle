90a31e2c9695da054e9fb3417224cc7402082cf6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:18 +0000

    most work work badly zero

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import platform
-def get_python_version():
-        return platform.python_version()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
