b0da1e6a04cd207a88f8931d633ea17fd370b4c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:14:59 +0000

    general serve mine hand dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
