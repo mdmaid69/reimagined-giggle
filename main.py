8962fc84ff4bb113a3de42061700c26c2a92904d
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:47 +0000

    so left breath complicated harm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def divide_numbers(x, y):
+        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
