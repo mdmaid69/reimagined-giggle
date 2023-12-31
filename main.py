52399794a9a82c70a44a3a51166cb7172b03b877
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:38:29 +0000

    vote abroad moment rather natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_tangent(x):
+        return math.tan(x)
