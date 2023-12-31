9ab47561a03023d6cb8437771191dfd0fb9ffc37
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:50 +0000

    payment even fast wise rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
