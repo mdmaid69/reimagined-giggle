b772b4df7577951484eae9f60f19e0b55fc19f49
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:07:07 +0000

    record Yy gentle interesting public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
