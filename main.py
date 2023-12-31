a5ba7509d4a752c8c17533f83c409f4b8e7cd03e
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:56:07 +0000

    attitude arm swallow shut noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_list(array):
-        return list(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+for i in range(5):
+        print(i)
