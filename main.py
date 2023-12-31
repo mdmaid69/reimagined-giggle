c75fcfc685022b5bfcd7c8fb3d4fe5b4ffa2d52e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:10 +0000

    odd congratulate pot price before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
