7b4ef54c66a3e79d682316ff3e42694c2f729f8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:15 +0000

    your form claim motion sweet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
