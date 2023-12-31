30196316d90c3f53cc63aaf030e43dcba70ecf6a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:34 +0000

    reply information by equal rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
