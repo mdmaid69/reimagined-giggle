4076d886e4c60e1b34da0accc2f5f93edb4e8f6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:59:15 +0000

    shape satisfactory callup proper hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
