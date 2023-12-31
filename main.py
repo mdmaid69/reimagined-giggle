e1cdd43c2b5ea21b42b0ded2ea9f7e2815d1aa68
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:47:38 +0000

    physical correct polite love interview

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
