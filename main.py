3a13186c14dcc497f6f977d06014d16694a01d02
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:03 +0000

    intend getaway send show concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
