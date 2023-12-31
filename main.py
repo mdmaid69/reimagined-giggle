404f90faa36008675db6abe3ac42d839237f56f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:47:04 +0000

    proof past Ll lean move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
