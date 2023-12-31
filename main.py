3b9f7be746a2dadb72a9f02440a8c151afc67340
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:47:42 +0000

    clean characteristic tough suit odd

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import csv
 def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
