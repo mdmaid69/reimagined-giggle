50b5b91ac796d3eca1dd7ac89df96884152c7d1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:04:10 +0000

    statement comein invite needle 564encourage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
