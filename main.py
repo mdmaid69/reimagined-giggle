fc8756533001a47781037fe2f589a13ff24e5c87
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:49 +0000

    curious television state wide reach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
