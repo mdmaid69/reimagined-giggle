b174ad4f71417b03df92b97a98f1c2402c188930
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:36:26 +0000

    march demand frame mess light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def convert_to_hex(n):
-        return hex(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
