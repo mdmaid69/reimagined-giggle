1eb9f39848d2a6a0fa10b886cc54c3d7024f7dba
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:59:15 +0000

    hold true fight promise cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
