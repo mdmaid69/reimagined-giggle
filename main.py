34168e06e399fcbb659d7b7ec65f59bbd4e6369f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:11:16 +0000

    break Jj rough art feature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
