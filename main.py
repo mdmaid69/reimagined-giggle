89a39226b2b6d095b1357dddda6d64149953d3d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:56:37 +0000

    activity belief mainly receive immediately

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-print(math.pi)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
