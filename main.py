6a1099c21e899cffa29f898ab69e172b24923192
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:29:01 +0000

    succeed organize bet ball though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
