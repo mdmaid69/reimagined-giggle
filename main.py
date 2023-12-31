da29f069a30b89f1f299a6f9c55cd793f69db5db
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:26:36 +0000

    welcome our argue get dare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
