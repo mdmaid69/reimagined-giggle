0452fb2df984f5ae7a33e7a3762bda4f8d8298bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:27 +0000

    sail with right kick with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
