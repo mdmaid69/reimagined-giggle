c520a36c43a6ecd010ade4c9d2d946e0049783ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:27:10 +0000

    identify gentle window command punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
