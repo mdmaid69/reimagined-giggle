a16029fa8edd2a3c85847fb6f87ec2a5504c7332
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:40 +0000

    lift view if thick conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
