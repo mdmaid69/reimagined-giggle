438e72dac322e7d99f5ff6dc19d2971138eecd17
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:37:17 +0000

    split arm manner before maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
