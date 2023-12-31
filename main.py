5a1c9b135105b0c54dc97c43d637846d52503cb1
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:17 +0000

    dead bright debt clothes trousers

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
