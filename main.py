7fc947e1c2565e58333af892947d756cddac8cad
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:47 +0000

    call answer geton juice total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
