aa6943b931ac330bfcb7f47f06172e7dd389441b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:25 +0000

    impressive sad authority summer strong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
