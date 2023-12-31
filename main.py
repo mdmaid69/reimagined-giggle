385513f7d777eddbdf1fa06b3ded1d4de5eab67d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:46 +0000

    phone major equal violent paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
