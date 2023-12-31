150c8c6eaeedd6db71688b4969981bbcacbfd352
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:08:24 +0000

    judge deal Vv maybe excellent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
