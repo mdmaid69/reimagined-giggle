d86241dcc55a0355591f57dff4a83691d6340893
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:54:42 +0000

    hair after line doubt ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_priority_queue():
+        return collections.deque()
