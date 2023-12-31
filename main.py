0e5edadf6563cd6e400009d3747f4c2fb7e02808
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:16:24 +0000

    breathe ahead on excuse search

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+print(math.pi)
