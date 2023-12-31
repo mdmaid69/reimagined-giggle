0f2bd17beda2b692ac492047e7274620198c3614
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:06:41 +0000

    right tour careful soft division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
