0ad7bcbe5bea813047843dadaafb0ae0cd3ef177
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:28:49 +0000

    love nowhere mess demonstrate train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
