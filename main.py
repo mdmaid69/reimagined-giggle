2f4916a068a2ab8bc4815a563bb6c8ee9e7a43f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:54:26 +0000

    belong unfair afraid care like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
