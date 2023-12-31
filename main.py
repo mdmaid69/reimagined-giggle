69f91f79a24cafeeaa6b35bc90232ed72d89bf4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:08 +0000

    natural hit serious bottom know

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
