38c86e009de4f2b49e7a7895960707a2ceb660d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:40:21 +0000

    certain hurry involve contrast Ss

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-print([x**2 for x in range(10)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(lst):
+        return sum(lst) / len(lst)
