d4ee3d684558ec240a0213ccd37b85af7d70c5da
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:55:46 +0000

    miss relief smell surface union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_item(array, i):
+        return array[i]
