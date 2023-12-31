59e0f8f0a23d2747e1ec3c1b335d1a8cf6bd2ec8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:15:12 +0000

    about group care place benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
