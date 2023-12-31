65c9ea1bcfcfa5fcaad78cb0179adbcdfe72012b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:07 +0000

    emotion goback(to) cut earn exhibition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
