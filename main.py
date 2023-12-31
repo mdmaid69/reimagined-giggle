d54b0ebf20a52c58238ec6486f31fd9dead62a07
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:37 +0000

    risk Yy holdon OKorokay exam

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
