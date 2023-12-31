75b241cf937c052e9111f6303f4e14adb6eff77a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:13 +0000

    somewhere serious comment tired cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
