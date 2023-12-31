14467b2171699c053849bb90e429ec68d163b078
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:43:08 +0000

    test confuse in vote lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
