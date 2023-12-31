b06408c4429faabd76c14b9887775ea296045a9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:20:31 +0000

    Qq lock fail shadow human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def convert_to_octal(n):
+        return oct(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
