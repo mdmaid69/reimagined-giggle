ac26f950eb428ddecf0a2931c17b9778f707800b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:08:26 +0000

    fork person finger always debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
