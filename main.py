0cc22a776f4477adf6e94d70d0819770697f3b26
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:30:31 +0000

    argue worried which life mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
