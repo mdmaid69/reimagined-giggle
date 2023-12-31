caeb187ecb9003a39ba78dcfd89bb3f787157cde
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:57 +0000

    finger round introduction carry path

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def divide_numbers(x, y):
-        return x / y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
