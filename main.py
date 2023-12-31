3a8dbd16cfe9f249f41c3625a8d910bb7eef3628
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:13:39 +0000

    street wash book grade communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
