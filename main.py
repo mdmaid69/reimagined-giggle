73b075c6f49eee330093337b9a472488216bc97f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:52:07 +0000

    sit Zz easy train bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
