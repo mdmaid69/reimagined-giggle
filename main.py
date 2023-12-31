47c546277a7c8f19cf2af160eb64231a8791732a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:48:09 +0000

    open runover past except ourselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+  def convert_to_binary(n):
+        return bin(n)
