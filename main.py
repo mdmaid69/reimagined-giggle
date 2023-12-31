07a72f066b2ee2b84bb8bacec96419b2af3a597a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:59:20 +0000

    work address turn nothing hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
