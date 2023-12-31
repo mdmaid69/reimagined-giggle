15d8ab269c1c5d99f343984d9dc2ed19909edca6
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:24:57 +0000

    runover score bound claim school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
