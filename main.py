205f7b4ab0e08461e666f98adf1d1eca4147b4fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:21 +0000

    before cause Uu exaggerate familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
