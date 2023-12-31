83612181f3fda3832e85514883ed8be390f014b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:35:42 +0000

    live actual straight reserve one

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
