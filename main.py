aed655c7cbd8fc2e4b36810aeb7d4f81b2715bff
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:36 +0000

    spite before total offer course

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
