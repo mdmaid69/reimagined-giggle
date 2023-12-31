b4ccea59ee1e631b46d38607a40ea403f45550f1
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:38 +0000

    talkinto less spread capital declaration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
