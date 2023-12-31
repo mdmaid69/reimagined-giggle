2f45e5c2970682b7e71fbe0ee939146edb91f174
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:15:19 +0000

    lock occur fear pretend safe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_str(array):
+        return str(array)
