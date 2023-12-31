33c68c641c3ae4ad88aaa340e8e65fab7a24503b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:02:35 +0000

    tip innocent most pretend everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
