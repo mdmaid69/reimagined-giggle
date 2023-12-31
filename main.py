f7f6aa0d63ba6707041ea1e4933a80f66226b22c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:44 +0000

    talk cut onto back skirt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
