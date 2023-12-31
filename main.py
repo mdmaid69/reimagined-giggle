a9137a636273e9a175033a3ae6671e4ff04f7db4
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:51:25 +0000

    pressure study defeat centre coffee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import numpy as np
-print(np.array([1, 2, 3]))
+print(sum(range(10)))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
