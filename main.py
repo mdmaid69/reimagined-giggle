efecfe0906de55235c288c5da87b94964df33486
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:04 +0000

    force pretend appeal transparent money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
