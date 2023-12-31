027546219412b6ef2eb2425b372957d9618696e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:20:09 +0000

    juice scream interruption innocent reply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
