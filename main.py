3379ec98e4737b6b10fd9b653f4976b6aa29c17d
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:47:09 +0000

    many discover somehow snow future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
