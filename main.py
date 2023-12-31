a7ad110ef81b29d0de4a70e36f05639a200a065b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:19 +0000

    than eye just glad glass

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
