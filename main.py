14e3a9a353b1009ccb6098510a59e360c744bd31
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:17:26 +0000

    half rough relative call accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
