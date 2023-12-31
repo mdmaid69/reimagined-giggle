36143e5f35510761933861d2a3abc4b7c03b2c61
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:41:00 +0000

    fly sign disturb plan do

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
