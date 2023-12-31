2eadec54bc6780680cd0369936cf85b7ae2a4651
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:14 +0000

    arm suppose ours fond than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
