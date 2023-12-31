e8c4628d7253219ff141d5f08d2549b380e5acde
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:17 +0000

    question change fashion feed move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
