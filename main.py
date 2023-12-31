5901ef7d5a77803e5cba2af079e4d0b96d8104ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:23:31 +0000

    teach minor needle trade mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
