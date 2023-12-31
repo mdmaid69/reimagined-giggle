7bbcc5d4b55e7d4ea1c0549f4cf5b825ade6ba30
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:09:48 +0000

    suffer sleep 568energy accommodation find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
