35989fd8724bf29fd4a1e4581f55214b3d73c592
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:49:19 +0000

    runaway recognize lot large before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
