fa2795d62176bbb5e619d7a2b93cdf009c45be0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:27 +0000

    workupto priority false identify impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
