90ae8e8c58eebef3e236eff132e54d8dd6346358
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:54:51 +0000

    use book both at niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
