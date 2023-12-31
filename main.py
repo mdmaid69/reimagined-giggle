1e3030f5ead53978cd44f10153e710f94a2f8d8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:43:28 +0000

    later send regular empty comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
