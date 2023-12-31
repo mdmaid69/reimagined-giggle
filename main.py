45b8b33e196221e1c224a4b7756d59e0ecd6642d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:58 +0000

    hang Ll raw offend surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
