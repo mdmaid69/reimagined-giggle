27e7f61db409e6d6fa45bbef78994851ef959358
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:26:47 +0000

    flat moment I formal young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def clear_array(array):
+        array *= 0
