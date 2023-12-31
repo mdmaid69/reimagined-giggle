d097de8a05c85fd78f004e464a24fa5b3cad43fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:31:16 +0000

    every display rise share measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
