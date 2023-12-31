e9967bb27118b2b78ac1c868ce5a626a6166445f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:47 +0000

    trade offence very 564encourage shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
