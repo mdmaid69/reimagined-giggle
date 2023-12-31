55de68971fd4f6c79dbd5c6b8e904e2bfc91ee04
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:50:57 +0000

    jump point fit shall mind

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_power(work, time):
+        return work / time
