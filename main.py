471766fc8d5b61f5cb05cd94fcc11b6b7d443baa
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:34:06 +0000

    major impossible wealth break children

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
