ee249365fa488ef658547726c28e044fffd0e570
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:46:29 +0000

    certainly impressive proud table sound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
