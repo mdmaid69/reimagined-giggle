5d56b654a24bda9115e78f3989b4191caa1839e1
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:01:43 +0000

    police appeal bottom excuse surprise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_palindrome(s):
+        return s == s[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
