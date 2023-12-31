91d892c52f472455f1f37781e779b4edcca11cd9
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:14:57 +0000

    turnoff journey entrance interest concentration

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
