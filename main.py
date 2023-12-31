e5a9db30462234860a060017c837b0290560d3f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:18:41 +0000

    climb ordinary punishment Tt slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
