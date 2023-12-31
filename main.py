5a2e03ce0cc61174420250d1e836ed963032b407
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:12 +0000

    suck Dd equal steep doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
