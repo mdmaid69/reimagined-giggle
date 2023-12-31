a6c5472996bea7620d84dd9050761c92a6b1b09d
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:27 +0000

    remark entry satisfactory rich would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
