6be0bb8036496a29cfae28f5ffc8216b4c39da1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:32:34 +0000

    block file top build score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
