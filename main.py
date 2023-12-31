8c2e845084cf074abc397a6d538fb47d6fccc6b3
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:59:05 +0000

    dance town computer community smile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_area_rectangle(l, w):
+        return l * w
+def remove_duplicates(lst):
+        return list(set(lst))
