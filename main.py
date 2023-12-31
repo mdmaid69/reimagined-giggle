0200efdfca29b8178d71f1ff1eb94da44cec9a39
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:42:01 +0000

    along that break protest have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
