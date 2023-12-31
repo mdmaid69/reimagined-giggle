a3abccb362f54d63f7d6c6a0d78eb0c27f191239
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:50:18 +0000

    getup outside watch large produce

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def square_number(x):
-        return x**2
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
