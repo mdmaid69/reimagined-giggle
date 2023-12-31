6181ae91fd07d82ff99fa8e019182a2c73939379
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:10:26 +0000

    walk plate wet single method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
