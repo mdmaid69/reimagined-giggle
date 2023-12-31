17145643e2dda174c0fe0eb05dc50b89333da33b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:10:06 +0000

    year identify fit apart effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
