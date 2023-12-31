c8e1a5c0c6e08f4812e50d51c83b8661006e8cad
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:10:44 +0000

    imagination before badly 565end complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+        return os.stat(file_name).st_size
