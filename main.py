713436a97ad7d0e9a4fe4d4645516b5be43cd9a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:25:24 +0000

    voice tradition opposite sleep miss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
