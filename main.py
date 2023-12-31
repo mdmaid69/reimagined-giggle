7499b5516b385da0eea6ee6641b8c454bc1b9115
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:29:56 +0000

    study tooth argue job fall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+print(sys.version)
