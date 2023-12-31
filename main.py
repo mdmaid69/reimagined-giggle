11573993948b7ff13eaced5f3fccb13d403e7422
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:48:27 +0000

    low bringdown nigga national warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
