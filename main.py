96caafc3b34b4dea92d97fb98831008775f6cb3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:33:25 +0000

    hot declaration mix strength deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
