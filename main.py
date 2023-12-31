df7ab4d1fd85242ca3ea55aa66fcf7925cb96988
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:59:10 +0000

    uncertain nature sore complaint let

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
