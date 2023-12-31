109846c1b4998a8cb8d72f76ad576825e065741e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:29:07 +0000

    immediate relationship rent find say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
