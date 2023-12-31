7dafd6035676448b37ba4f9a8bc97efb6c9237cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:43:02 +0000

    fortunately sit copy drop measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
