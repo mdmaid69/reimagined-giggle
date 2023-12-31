64b7ae5a9dbfdc90e167a5ee0ea69e52ee7cae10
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:40:58 +0000

    here foreign yet Ll wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_index(array, item):
+        return array.index(item)
