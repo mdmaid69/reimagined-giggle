ffdaa4c43af82ee9582072051886659a9b60e9fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:47:53 +0000

    business needle record condition original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
