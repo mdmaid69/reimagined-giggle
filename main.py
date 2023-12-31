0f49383b9bd148f6a1d008ee86228ad26f1fd8a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:25 +0000

    it common spite keepout paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
