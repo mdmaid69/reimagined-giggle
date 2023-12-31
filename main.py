bd857b16f4ff5482ea7f9d4f786e9ff7b61c5fd5
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:28:52 +0000

    tonight tell label mine every

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
