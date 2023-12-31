79eac37004a5664e54d9c9014053a0d6990360db
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:31 +0000

    impressed lookaround church raise practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
