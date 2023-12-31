eb575633b21c24fa9de80ab3f7fc02d805deab74
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:39 +0000

    treatment objection result different hurry

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
