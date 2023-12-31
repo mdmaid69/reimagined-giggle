5262f1724e42ec4b5698efc7f03d97869a42adb9
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:43:08 +0000

    house before spend allow private

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
