63c08be9ae63f648f576f1201a90ee1e11c9f510
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:33 +0000

    slow cum cheap violent teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
