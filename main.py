a897b5b34f673193ad545d803daf232c7d0fb658
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:59 +0000

    mainly father die state away

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
