b76c924870d31eb1569e6f95444a74b3628d733a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:30:04 +0000

    once full surprised worried question

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
