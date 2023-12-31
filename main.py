8fa542b636954e08cfe94397f27b0c635d21a75a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:53:31 +0000

    error sleep odd sail street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import heapq
 def push_to_heap(heap, item):
         heapq.heappush(heap, item)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
