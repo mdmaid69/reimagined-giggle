45990be3a7ca25030f99a164d7de5f40231f5ce9
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:27:19 +0000

    bright mass successful split allow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def count_words(sentence):
-        return len(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
