4b2c9cd2aa958afd587132ad8cd83205d159f3b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:48:26 +0000

    size turnover comfort other Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
