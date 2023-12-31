2bffd8605c681081646a21564a8018f6ef6e3519
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:21:41 +0000

    back argument lock load stone

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def create_user_dict():
+        return collections.UserDict()
