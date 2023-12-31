cde418cd4cf10a3b796706d82fb59aaa39c51e96
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:05:46 +0000

    tend along part wind criticism

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
