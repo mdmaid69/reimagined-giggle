a547ff87a812a7dece7e708cdab644f221ee6415
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:46 +0000

    invite cum effect feed mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
