df9a717fd5cb7d661329f0c09df6d3120f610df7
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:18 +0000

    only possibility describe test few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
 def calculate_inverse_hyperbolic_sine(x):
         return math.asinh(x)
