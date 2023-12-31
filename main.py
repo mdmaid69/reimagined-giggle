a212f084067fffd19fa34377102fcd16d99c1cad
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:01:51 +0000

    correction fix shock your autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
