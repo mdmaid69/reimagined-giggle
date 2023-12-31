e64a6c583d03346df56d18fd8ad0d0af646c7aec
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:50:38 +0000

    offend relative equipment rain takeoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
