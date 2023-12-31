4e50f7058a71710cd78e8e5e288779f853378a6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:10 +0000

    hope pain Yy address value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
