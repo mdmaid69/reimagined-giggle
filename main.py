a6a060af489218e87dd7ce7fd0b7f97bbdde0409
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:00:41 +0000

    that reject above unpleasant takeover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
