968c02a62ea6fdecff2877bf4497ac9779969ea8
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:59:13 +0000

    complete born material fast yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
