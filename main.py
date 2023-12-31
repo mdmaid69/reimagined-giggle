ab07abdb17e4eccbe1db4c699020d4e126f5d056
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:56:19 +0000

    adjective argue funny scale limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+print([x**2 for x in range(10)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
