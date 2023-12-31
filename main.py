d20c5b86196f8ca956a5ffc2e9ca6638c9dc5713
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:05:56 +0000

    able belong change fight tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_item(array, i):
+        return array[i]
