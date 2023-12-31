77ca07de3e4fc5d19abb10a5092a1f51fcc376e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:28:21 +0000

    mouth exam brush event truth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
