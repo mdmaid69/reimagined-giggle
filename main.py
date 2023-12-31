3da9b79d734ac87909cf7384c5e48ee5830d5c9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:43:18 +0000

    callback wait watch land problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
