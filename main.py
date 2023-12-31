1bebfba665603d0a8644d01751793f82180e70cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:41:24 +0000

    shut state getout definite start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
