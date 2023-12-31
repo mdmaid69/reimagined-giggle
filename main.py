5bc00b2a1fabcdc44b4c43bfe506afaf5796f016
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:15 +0000

    accidental left contents yes recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
