97a6a3a7943b1253f74162fe4ec120d56d1d1161
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:11:30 +0000

    communicate used guess order wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_length(array):
-        return len(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
