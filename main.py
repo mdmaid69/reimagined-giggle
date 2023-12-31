d04f3825dfa17eec615fbd59b33ce2d33185e5fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:29:55 +0000

    clean walk always clear Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
