304ecaf5cc503e5869a3e9b247320308422a56e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:42:01 +0000

    loss computer piece exist left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
+import getpass
+def get_username():
+        return getpass.getuser()
