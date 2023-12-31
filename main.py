7e57e89d55adc437509699a6952278cf2790e465
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:56:33 +0000

    arrange involve hour pipe prefer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_list(array):
+        return list(array)
