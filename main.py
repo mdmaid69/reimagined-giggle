dbe57dadc1bddb6deb67e35a3651a04acbf69028
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:43 +0000

    moon neat fall use invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
