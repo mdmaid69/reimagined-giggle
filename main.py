1ed9561764c11412554cc7093587b3630120d09e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:14 +0000

    risk dull pollution bit contrast

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
