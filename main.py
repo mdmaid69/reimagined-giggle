140410da4b8ad12fcbb87db8d4874760ece6538a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:33:33 +0000

    fire certainly tongue several mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
