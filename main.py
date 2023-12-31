40804e83269c15642f5cf7a4487ece673e355ce5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:52:46 +0000

    everywhere wherever beginning press tear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_priority_queue():
-        return collections.deque()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
