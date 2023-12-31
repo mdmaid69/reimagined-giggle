2f5e36e7e898f9ccd848e645b860197e355fbb2c
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:36 +0000

    miss exit free over object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
