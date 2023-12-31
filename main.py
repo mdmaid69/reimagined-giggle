77555729ba7d35e91adc175a8d5460b03a51f607
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:20:50 +0000

    rescue pronunciation table past excuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
