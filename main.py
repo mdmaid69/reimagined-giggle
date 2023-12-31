828647d415fb188996128e055b534466e0724aa9
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:50 +0000

    lookback(on) bet goodbye opposite confused

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
