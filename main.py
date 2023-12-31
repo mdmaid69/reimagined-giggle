f37749b65b27d6f17e3a0fb35b751eff3d01a9b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:27 +0000

    final world crowded giveaway concerned

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
