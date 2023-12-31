77cdd0de238d2de9a5ad8125a53af36e2110b38d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:50:36 +0000

    look model meeting pupil similar

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
