c60ba0b5465c1104129548ca72c42a6190f46bac
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:09 +0000

    keepout confident too workupto small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
