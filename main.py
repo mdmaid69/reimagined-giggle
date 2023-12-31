28a93d011b69c87cb5baa37432bddbd673169413
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:33:41 +0000

    someone trust surprising turn evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_acceleration(speed, time):
+        return speed / time
