9cc4ad4425313ab03eefc6833ded04bad45ffa0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:37:51 +0000

    between include say ride over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-print(math.pi)
+import re
+print(re.match("h.*o", "hello world"))
+def convert_to_hex(n):
+        return hex(n)
