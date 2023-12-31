891ebb575014290fa6953938e0f921dce0cce9d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:08:01 +0000

    quit point theirs giveaway aim

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def find_min(numbers):
-        return min(numbers)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
