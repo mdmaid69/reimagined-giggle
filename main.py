aadb8d58309c22b14e4261b26d70ce05209404ba
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:02 +0000

    makeup fine afterwards crowd silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
