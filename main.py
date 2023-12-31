f9c30ec8189af1363fefd44916227f95ca3e0034
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:19:18 +0000

    centimetre beat contain herself thorough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
