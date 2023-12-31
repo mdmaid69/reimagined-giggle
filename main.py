df22b3fbac67345bdfeef605e3dc99d9c5069301
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:56:20 +0000

    unlucky exact ticket everybody say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import sys
 def exit_program():
         sys.exit()
