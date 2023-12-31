4bb88bf6da3554fc95c007c72358f9f7e20f1a81
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:46:39 +0000

    yours everybody same direct remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
