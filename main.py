f8f3e6bd45cf0827801203a38e858e19b6a4f3f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:17 +0000

    when remove exciting left pass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def is_even(n):
+        return n % 2 == 0
