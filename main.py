cd58a8753f1b77007a847038a8be5524a7fe7c9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:09:46 +0000

    mess car comeback unite plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
