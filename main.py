9f50f25779054bb1cd4cc8fa5faa392bdc38130a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:07:19 +0000

    discover godown sky right telephone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+name = "Python"
+print("Hello,", name)
