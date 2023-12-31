f2926100fde3c0d716836d04b6a87f520b1ed4f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:17:40 +0000

    report especially return over hard

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-def is_odd(n):
-        return n % 2 != 0
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_octal(n):
+        return oct(n)
