261d2e9975c0d9bae010e6ac720fe5730532b1d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:19:29 +0000

    home thank consist healthy pleasure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
