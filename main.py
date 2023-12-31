fd220b02716daeac07aae9fe5777b108b77f4221
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:16:09 +0000

    branch fetch class enthusiastic so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
