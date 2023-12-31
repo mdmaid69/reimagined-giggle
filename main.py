0a2c349db12f9a90b2bf74138e0788eb3a6fba55
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:47:20 +0000

    lie maximum different worried because

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_as_repr(array):
+        return repr(array)
