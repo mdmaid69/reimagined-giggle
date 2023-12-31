fd56db2c86569a1f36aa8fdd08d75d42bbd809f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:45:46 +0000

    side immediately tired far culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_str(array):
+        return str(array)
