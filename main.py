815bb5b79d45029a9b5f62dfedb46f8e9a2d1524
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:38 +0000

    leave step concentration grade loose

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
