2e735fe771384d68bbc3d00981b9a63bc1c9b973
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:30:36 +0000

    mixture birthday shout downstairs pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
