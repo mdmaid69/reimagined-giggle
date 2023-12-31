a6d45c8eedbeab905a2fe5a47bf6e34c5f7f8cb0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:26 +0000

    emergency preparation actual compare estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
