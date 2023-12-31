830e5f6b0c63210603d7bab63c03e49744ff0dac
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:08:32 +0000

    makeup plan clear wash depend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def is_even(n):
-        return n % 2 == 0
+from collections import Counter
+print(Counter("hello world"))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
