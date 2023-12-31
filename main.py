6bb9d3ffe1d5b4fe74aa90e678069b3758ef5dc2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:09 +0000

    couple appeal consult pump whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
