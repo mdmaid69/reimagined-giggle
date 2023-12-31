c7e564d191384e18b1d6478344d94a4c4118ae86
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:45:35 +0000

    gather form available wake train

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def reverse_list(lst):
-        return lst[::-1]
+def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
