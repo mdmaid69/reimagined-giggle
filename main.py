09efd5f12e52852fd6a3c2c815e08af7a88f8262
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:52:01 +0000

    lost mood far Qq ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
