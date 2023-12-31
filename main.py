410370ffe9a8b59081b80489c1bf1f45597dc1c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:39:27 +0000

    average positive bite bored suppose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def subtract_numbers(x, y):
+        return x - y
+def sort_numbers(numbers):
+        return sorted(numbers)
