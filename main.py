885fbebfe4756d01379f5453cb3b4c232fb797f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:59 +0000

    steer bridge instrument his cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_floor(x):
+        return math.floor(x)
