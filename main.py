49f098fbfac82f9927d1ecab776ef581b9c401d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:43:15 +0000

    blame blind rain not oil

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Characters:", len(text))
