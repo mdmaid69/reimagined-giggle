1989aea00fe86f3b8d75b0dfe5eda7d2cc56b5ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:18:32 +0000

    favourite you satisfied pour switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
