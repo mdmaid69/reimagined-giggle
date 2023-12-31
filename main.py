d01049f45ff2da96ee64f2c9c7fbbf7748f2c15d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:55:51 +0000

    drop on set large finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def reverse_list(lst):
+        return lst[::-1]
