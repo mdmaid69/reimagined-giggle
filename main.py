2861354a362a00c7bcece4d178bb00fa3be9ef8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:13:40 +0000

    compete suffer breakdown instrument giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
