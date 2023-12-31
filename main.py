cdee2f85a30ef2e2ea5283dbacd1ec22ccbce993
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:10:27 +0000

    limit care anybody loose judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
