46ce5d14616282601c281b6596aec20e3130854b
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:10:38 +0000

    generally soon pull bunch extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_sign(x):
+        return math.copysign(1, x)
