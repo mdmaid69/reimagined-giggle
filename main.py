9fc26569880dcfc1ead4aa8aec57216fcd848046
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:47:51 +0000

    fat scare needle at properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import numpy as np
+print(np.array([1, 2, 3]))
