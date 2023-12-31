4417dbae2325bb5a4f95b23c3946371b3103cd28
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:17:53 +0000

    speak police comein please describe

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def is_odd(n):
+        return n % 2 != 0
