59014b969bc290b73f8bf2131244756763d590e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:05:48 +0000

    forgive squash follow lookafter stamp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def subtract_numbers(x, y):
-        return x - y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_current_working_directory():
+        return os.getcwd()
