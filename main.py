238369e08503031b34af871ef1287759a4343349
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:00 +0000

    previous stretch early bit cause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
