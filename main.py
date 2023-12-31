b0289d86616e97a96700214d5f751d2d27aa59e4
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:59:23 +0000

    will represent doctor appearance some

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_density(mass, volume):
+        return mass / volume
