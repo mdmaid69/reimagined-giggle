0334cb78f4d08c40b8e6d3015e67b87d1eab9a00
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:57 +0000

    field light tense pour regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
