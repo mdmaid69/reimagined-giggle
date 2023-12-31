a4cc737689efef3a117be056dc3e0124032ccd39
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:21:04 +0000

    pretty keepfrom act nature emotion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
