1acb53d178eab7226dd46257aaea6622ed8402ab
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:45:54 +0000

    judge catch winter the poison

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
