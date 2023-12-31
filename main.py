859a51ee7f256f434ff3bcaf7fb616051c907865
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:37:39 +0000

    rough pretty curious challenge keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
