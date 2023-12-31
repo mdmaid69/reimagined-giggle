6c3f5282612224afc7fcce04cec0de1e699a6fcf
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:25 +0000

    switch demand collect somebody so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
