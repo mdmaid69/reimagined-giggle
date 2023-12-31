f49db6ee50a0e14fb2cebca60c1bb872f9f71f6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:03 +0000

    good fail police splash meet

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
