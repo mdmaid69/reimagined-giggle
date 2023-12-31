4d3e452746a49f8690171daf62444cd6634cc9ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:52 +0000

    know scheme post yet lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
