6c9995579fe6a3a00019d4dd658a7b750f218fd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:25:54 +0000

    vision careless separate rest last

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import numpy as np
+print(np.array([1, 2, 3]))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
