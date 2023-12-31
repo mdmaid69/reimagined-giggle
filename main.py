9908993b5c54db631eac4e55cb0612b841bac5ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:05 +0000

    grateful borrow warn plant block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
