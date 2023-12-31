b5edc7acba77e5615c063aecf06b00b22f4e4773
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:09:26 +0000

    advanced land herself spread formal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_palindrome(s):
+        return s == s[::-1]
