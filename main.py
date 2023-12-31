4deecb02db796d3f923a51703c982869a0e62b30
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:03:16 +0000

    century myself type opinion sad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
