4e82052a51f24e95885d3b60b6c689d723958d69
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:47:15 +0000

    cover snow course but Ee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def find_min(numbers):
-        return min(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
