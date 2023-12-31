1296efdeb55c2b4ac20b15869064b2d19646eef7
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:15 +0000

    full they conversation freeze available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
