120f21dde4ade4f3ba3a37963c53a581b7919f71
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:03:35 +0000

    mess passenger holiday refuse tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
