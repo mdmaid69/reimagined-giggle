35576d7475a51dec42ae709d8201cc1690abcfe2
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:10:06 +0000

    bottom future attempt put Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
