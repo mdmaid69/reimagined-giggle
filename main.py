520194ea10990e64b88cffef043c72b1984b2854
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:37 +0000

    real raw short tense here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
