4e8e10f3bef6a42687fb1c1efafc61531964670a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:54 +0000

    value drink ring collection be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
