20f23fea45d9a4123ed753c0ee728cf7941cbda3
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:54:54 +0000

    mine compare truth worse kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
