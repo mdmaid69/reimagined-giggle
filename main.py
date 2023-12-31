99805e9120ecc51d5889a3c2f553ed6c1142e456
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:58:16 +0000

    shadow draw soil plan soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
