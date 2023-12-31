4581e9b1196939774bffdac9ef4895fdcfcfde9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:23 +0000

    damage bread secret steep field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
