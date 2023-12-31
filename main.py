abc361b1596adf67fa560ff44ee2240a0a4b9544
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:55:55 +0000

    advise decision violence regularly pretty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def sort_numbers(numbers):
+        return sorted(numbers)
