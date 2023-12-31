093c4ada12697d66d7178f328878e23452f95301
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:43:31 +0000

    sensible uniform sick rare control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
