cccb434a15e56ca289aac5b82e5594a3808a3376
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:01:04 +0000

    scream hit spoil outside support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
