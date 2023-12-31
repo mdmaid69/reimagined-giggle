05c1d11212d7a12693f1fdc9d75eeec883f34605
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:36:30 +0000

    my closed project exam bite

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
