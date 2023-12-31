75ab5393c1c1d1197a8cfcbdd44d33dd70b550e2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:26 +0000

    although second introduce correct square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
