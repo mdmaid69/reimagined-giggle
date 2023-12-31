55b25126e0d9e657ac0a57fd0fdde31997c62306
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:22 +0000

    giveaway shrink gather feature number

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
