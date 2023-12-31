73ace5534dcb6f24aa140734c17a5abcd84d9861
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:30 +0000

    cap word interrupt again flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Characters:", len(text))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
