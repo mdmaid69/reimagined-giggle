580894309d67259678daeec1cdf35a5dea30215d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:55:48 +0000

    aware steady comein reasonable in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
