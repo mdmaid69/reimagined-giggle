c101a8a54a0fdece1d4ef624233e95c02055e8bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:17:16 +0000

    exploration form together crime strict

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
