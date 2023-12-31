bde3078d546416f33da37111f443ab74cca34339
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:24:37 +0000

    sail everyone wherever wander condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
