f1c14e60ace4401b6b5b6d53feb8ba4a3934bac0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:24:27 +0000

    fierce Tt matter find correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
