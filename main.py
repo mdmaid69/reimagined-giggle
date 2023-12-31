1b51ded2e25dc9c25c4b83d9689f2c5b3a42e43e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:14:44 +0000

    attempt getout reach chief knock

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
