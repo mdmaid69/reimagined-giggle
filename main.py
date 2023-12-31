e1896748ee50daf497913b4b8c8a3d4866e268f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:29:18 +0000

    majority wind ashamed slice usual

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
