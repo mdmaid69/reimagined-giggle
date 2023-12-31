5faa30e1e45f3f4c0de796d6ea259267e17c0749
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:32:56 +0000

    skirt radio partner crazy past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
