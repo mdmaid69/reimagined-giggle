856f52c24d3f8edc6070ddf66f6e334bfe3d9514
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:27 +0000

    forward born turnoff unhappy believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
