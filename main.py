cfc5c771d5153eccd04bccbb463889e15882e450
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:32:21 +0000

    measure neither large honest material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
