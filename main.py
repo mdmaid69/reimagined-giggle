20e583c08b70184f3157e66d36feb23a6bfda732
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:49:18 +0000

    dress recovery passage main prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def find_min(numbers):
-        return min(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_length(array):
+        return len(array)
