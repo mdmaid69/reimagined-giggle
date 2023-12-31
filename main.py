3262525dfb659985adf64cd9b2f382a99f3d4049
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:04:32 +0000

    normal check border effect answer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
