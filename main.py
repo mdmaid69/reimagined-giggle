29f33cf1b76090dfbbf11cdfce7fb92efd81fa36
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:52:38 +0000

    reason preparation loose office improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import numpy as np
   def calculate_correlation(arr1, arr2):
         return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
