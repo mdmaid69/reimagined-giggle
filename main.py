35725ecae0edeca455496a8f000ee8deb44ca72d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:24:55 +0000

    twist combine Tt rough communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
