cf26fa10fdc55d828bceb2aee573f01f54d0fdb6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:32:39 +0000

    sensitive better goup instruction precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import logging
+def log_message(message):
+        logging.info(message)
