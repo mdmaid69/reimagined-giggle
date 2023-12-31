51aa3eae45eb383488befa546d538c3a4070769a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:04:37 +0000

    clear from pipe skirt relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
