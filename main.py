23434a7734be719a6a1829b56fdbb9c64f703153
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:44 +0000

    your criticism well once comfort

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
