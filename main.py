430af9516da6a7ddd193e7fe8f66b58c80112f83
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:51:18 +0000

    failure train runinto second environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
