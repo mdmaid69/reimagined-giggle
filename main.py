13844b9b97e47886c3661a8abe2794b834af8f68
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:33 +0000

    whole quick everybody often exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
