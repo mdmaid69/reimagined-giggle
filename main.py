0872c8a451c626e6166bb08450873ca88f8d0da8
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:31:32 +0000

    slice convincing least start thankyou

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_min(lst):
+        return min(lst)
