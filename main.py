fca0c0ed6e283ff3ea58dd77b60b8256f69b17e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:40:24 +0000

    favourite slightly breakin worry scared

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-name = "Python"
-print("Hello,", name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
