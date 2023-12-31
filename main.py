be129ac05798b9808c5d5e5302f6f4a8ff1843b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:40:34 +0000

    improve approval spare ear hollow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-import random
-print(random.randint(0, 100))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
