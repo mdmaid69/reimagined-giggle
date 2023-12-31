36ab00b20e81aee7afa2e06cd4c72dfab3c03aea
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:09:23 +0000

    Itself give than drink hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def sort_list(lst):
-        return sorted(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def convert_to_binary(n):
+        return bin(n)
