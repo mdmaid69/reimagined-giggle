e6628715972480fae3937397484731176e865c0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:39:46 +0000

    public neck tonight introduce exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
