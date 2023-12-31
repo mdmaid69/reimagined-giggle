302c75afe237180407010e6ce95f01be447ff358
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:27:02 +0000

    cost upwards people acceptable dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
