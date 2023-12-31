cd0431f4f7a57e5205d552faefeb6478f331bd3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:39:35 +0000

    bang finish watch my sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
