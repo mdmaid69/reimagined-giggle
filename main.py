e3e55a990ea5330e49ee51dff0d20a3f0f52017f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:59 +0000

    over plenty lead bread himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def append_to_array(array, item):
+        array.append(item)
