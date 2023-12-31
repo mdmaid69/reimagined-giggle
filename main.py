bc87c1ee5615cc59fe2618115bc06a6b9023693c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:09 +0000

    fold lost slice turnup yourself

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import time
+def get_current_time():
+        return time.ctime()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
