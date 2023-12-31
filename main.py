e3e5b02227d0f04f38464f7bc61e3139b3b18878
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:08 +0000

    minority cheap fight it legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
