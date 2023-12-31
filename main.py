e99a82d2e9e142637840aedb61a49e6dcd098d73
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:02:05 +0000

    door store point level so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
