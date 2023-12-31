4290c593473565adb0dd9fad78e44d77658c2370
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:47:59 +0000

    company walk sea onto start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
