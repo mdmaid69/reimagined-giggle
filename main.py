0ac186ff1f9f184ec9f546fa082ad89af4c07d40
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:57:38 +0000

    temper urgent middle organize Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
