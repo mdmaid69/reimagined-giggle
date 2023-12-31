9d03635e927640b5ec8151655c8db03c1570610f
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:31:42 +0000

    brush once giveup shopping pot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import time
-print(time.time())
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
