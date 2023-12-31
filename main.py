6b5e4673e2f15f50cb7d434736ccc67db9c706f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:13:24 +0000

    use pronounce deliberately fierce disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def multiply_numbers(x, y):
+        return x * y
