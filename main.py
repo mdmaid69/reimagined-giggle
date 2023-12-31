2c094a73201e5b2c03acc3842da3dd9836bd952a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:08:40 +0000

    behaviour uncertain once show tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
