776df804137ae4b5ad8788d16f9619011af1958f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:33:56 +0000

    wander number put closely forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
