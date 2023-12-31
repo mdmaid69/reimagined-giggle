16cda9ce7835d278a53936cef292661eddbd667e
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:56:45 +0000

    check reserve bet clearly nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
