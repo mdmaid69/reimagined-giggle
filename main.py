7f31ae3f629b94eaadc433dc3a5b5afcac3f640e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:32 +0000

    shrink production plastic nonsense please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
