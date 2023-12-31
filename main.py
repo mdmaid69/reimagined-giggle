a76e1d31e499d54e6606f176f340bbb82f1f271c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:09:27 +0000

    write pot breakinto ask leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def add_numbers(x, y):
-        return x + y
+import math
+print(math.pi)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
