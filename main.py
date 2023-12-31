9d592e59631eac98e206d445a02981c4aad3649e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:54:34 +0000

    believe answer major level objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def square_number(x):
-        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
