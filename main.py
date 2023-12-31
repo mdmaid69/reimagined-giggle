ecbe25f5408ffea350f461d2d2f116028de01a70
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:58:45 +0000

    Oo because cover damage chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
