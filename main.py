1536e437feb0f730b57ef6005fde69565d7574dd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:51 +0000

    that clean outside sick float

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
