00031cc14117a255c9e5b30704a1bb6d87eafb42
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:48 +0000

    dinner lot touch doubt control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
