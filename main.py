340de5724c9c9d6f534500e57e994c187db55c5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:49:49 +0000

    scrap tidy forever mine occur

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
