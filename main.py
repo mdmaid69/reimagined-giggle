8fe331f62383a98fa6256145343b4bffabaad21e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:36:23 +0000

    ball force careful little connection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
