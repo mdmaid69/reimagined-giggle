ddbb0eaf667d974fa6394fb89de358d7a8ac111c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:08:16 +0000

    heart name under free long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
