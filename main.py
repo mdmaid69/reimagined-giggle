cab952131786a73d6217423b4c825637c1448e30
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:42:38 +0000

    blind chat thumb approve strange

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
