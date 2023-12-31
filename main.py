50c646dac02bac4d4374843e2250811bc6bf1608
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:27:03 +0000

    less copy second also sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
