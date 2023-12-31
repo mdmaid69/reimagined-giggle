9060fecc83044ddf501e0e1335b2db8ae54df234
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:32:38 +0000

    complain path common excited visit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
