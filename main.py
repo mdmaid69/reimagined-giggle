a2f9b5993aa3f11dbe5cb0be5ffaadc6bcea0fb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:49:22 +0000

    your Bb undo autumn star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_power(work, time):
+        return work / time
