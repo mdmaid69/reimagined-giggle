330f09665969e0d4a6b8bd2094602179e9fbd55a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:21:37 +0000

    replace bored concern sharp influence

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
