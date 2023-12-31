51c8ebec8faceedbf4fdaafb590f0b9142b3bd5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:16:50 +0000

    score nervous report bringback temperature

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def multiply_numbers(x, y):
+        return x * y
