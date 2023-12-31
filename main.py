8559aa5d7728d8871e6e0d4bd4143ad25c1f91cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:24:21 +0000

    pick crisis twist without care

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
