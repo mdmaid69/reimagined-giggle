12c504c65654baa46e2f697c18c4c0092a08d295
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:37:02 +0000

    block separate perfect expression trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
