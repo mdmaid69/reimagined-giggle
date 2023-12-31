a5978df7af54ca3414bee42edbf27446c05307b9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:33 +0000

    law under store transparent miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
