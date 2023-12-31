ea0bf74d01c1b4b8a96cca86432de6d09c13a996
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:35 +0000

    yours modern maybe than pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
