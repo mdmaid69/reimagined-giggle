82493afb3fd309fd77330e481e1794c91e719068
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:47:57 +0000

    back unfair ocean distant case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
