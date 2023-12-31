48114890e354527ba59a2cfecc538652526b1a51
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:41:48 +0000

    niggaacademy factory slip bound their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
