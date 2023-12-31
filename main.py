3cc5b232f07fd0f54c98e4b12dd3b15a2ca820c7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:48:40 +0000

    nearly during crash fashion their

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
