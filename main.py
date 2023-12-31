8a7aa326899f9022e3b3abb8c6073bcac6440d4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:52:59 +0000

    drive work level fierce keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
