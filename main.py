c1317bfcdbd0ca856e393d6670bc31fcb924fe99
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:57:25 +0000

    busy absolutely purpose concern interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
