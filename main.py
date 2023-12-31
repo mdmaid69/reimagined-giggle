91f10acf01e3184f2a752330098c72150be60bec
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:08:20 +0000

    fear politics but count furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
