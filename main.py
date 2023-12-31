d0d95089a9331bb01fe1f2f3208f2087fc7caa10
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:42:02 +0000

    sharp cost town sit point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_pressure(force, area):
+        return force / area
