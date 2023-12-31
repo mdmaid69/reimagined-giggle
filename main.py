b3e1ea98c7bb6cc1b68734876f69fb3524e5046a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:50:24 +0000

    statement terrible type bin then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
