c186d89c326998c26e3e4e7c9617e5ae96a4ad91
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:56 +0000

    draw fierce shout behind television

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def cube_number(x):
-        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
