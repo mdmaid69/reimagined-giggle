005ffa1046c87ba2a5c96bf9f669943bda19307b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:25:05 +0000

    thing path loose himself scream

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
