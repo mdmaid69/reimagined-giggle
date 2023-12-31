1ab8c5adc5b646dd84ae3b8bb586b01a4df7e82a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:31:55 +0000

    effect wealth holdup straight pause

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-print([x**2 for x in range(10)])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
