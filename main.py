df43d97420a1cc91796becd6d8d77987b349f55f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:14:19 +0000

    sure seriously achieve leave estimate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
