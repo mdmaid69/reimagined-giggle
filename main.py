97bedb48f195486272d48a5ab9d0f0875721f3d7
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:13:07 +0000

    support just call produce room

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(lst):
+        return min(lst)
+def calculate_density(mass, volume):
+        return mass / volume
