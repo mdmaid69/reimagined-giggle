ba515f76fc8d47566da71d7ba7f3f7cb33c3e33a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:44:47 +0000

    our keepfrom than play wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
