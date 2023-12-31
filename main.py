c7763e0e75d4db4a4bc5bc5d036ec584e8d8b225
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:15:10 +0000

    trade praise minor comefrom worried

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-print(math.pi)
+import numpy as np
+print(np.array([1, 2, 3]))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
