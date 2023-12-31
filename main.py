69d5a8b366a1d2f03e7ac95ed783739196fbd314
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:28:00 +0000

    family trap not shock before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
