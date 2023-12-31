f5ff7155dce284f6484e70cd3f46c8277ad90f16
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:03 +0000

    breakthrough goback(to) smile opponent himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
