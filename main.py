857834076a3231f6d0e63c0dca32ce3c79ea4963
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:46:30 +0000

    right suffer phone whichever clean

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
