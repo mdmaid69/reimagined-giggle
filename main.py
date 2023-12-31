9352d4f715eddb3693a8c88e8e84995850ab4b92
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:51 +0000

    niggaacademy city tread turnout correct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
