df206d87b9c60312014d634ae99118f620a0ce1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:07:20 +0000

    desperate hungry except crazy age

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
