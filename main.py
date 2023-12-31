5aa6e75bcf8332c6ade8f09581237f4eeb536432
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:31:35 +0000

    accidental tradition grow some drop

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
