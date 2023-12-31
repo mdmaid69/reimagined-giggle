9929ecbbf87d340389d261035625f5e64ca70beb
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:29:35 +0000

    score eat opposite produce broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def subtract_numbers(x, y):
-        return x - y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
