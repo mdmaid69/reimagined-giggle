2755192f3ba1005fdb586f577f56ffca4562f448
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:21 +0000

    pull slice shame else rub

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
