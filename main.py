40ad2b9d0177c4a85e753e9df8c6d9e2d5553cb2
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:55:10 +0000

    sport pay which use eye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
