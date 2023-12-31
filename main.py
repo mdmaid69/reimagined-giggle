cf0155f9fb9e01cb69df2ec012ff0262728baef8
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:02 +0000

    goout dear process it serve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
