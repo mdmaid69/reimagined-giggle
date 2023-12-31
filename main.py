c592c63f11bfeb4c35e27b0d57990c6231f724ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:24:21 +0000

    recent quarter promise base when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
 def calculate_neper_number_to_power_x(x):
         return math.exp(x)
