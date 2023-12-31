0a8429cde4e6b13f96d38aa8eedbfcafd12e80ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:14 +0000

    mistake that kill prize opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
