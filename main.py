0332612adc7fa5a0de700395b0e9f9bfda7c0a36
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:12:25 +0000

    spring next funny party beside

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
