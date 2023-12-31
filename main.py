cab37ce37528bd4d03e470ad43af266691a98df9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:43:54 +0000

    print branch accept callback correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
