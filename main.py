bb1db0863071096738e373e458ebd09d99161fd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:08:56 +0000

    sticky sign tap cheat summer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
