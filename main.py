d0a71af49bcd617166bb87d00f468c81ecd270ad
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:53:19 +0000

    discussion judge address responsibility neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
