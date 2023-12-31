e8b18d600909eb54b1fea5b6843875538c5306a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:05:22 +0000

    bored pay sell flight belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
