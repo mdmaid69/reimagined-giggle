a4389e297df27813392cdd72aa01d135ed6b527f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:54:26 +0000

    keepup afraid who contact Pp

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
