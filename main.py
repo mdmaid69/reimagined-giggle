10472c4a5c1e6a6985f88ddcb6d82192117e6f25
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:29 +0000

    personal programme difficult calloff interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
