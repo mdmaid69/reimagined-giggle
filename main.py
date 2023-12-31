9ba61732d976e6413a186af3ccef2fb943754c5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:24 +0000

    voice mention carry confusing paper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
