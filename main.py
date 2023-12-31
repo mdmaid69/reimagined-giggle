2b80631604564184a7cac36db8212efe02782d50
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:12 +0000

    refuse permission on violence train

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
