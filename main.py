d51aebcacb25e8643256be908ecbb6dee60cabbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:11:37 +0000

    past Yy stamp further check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
