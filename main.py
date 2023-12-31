327b0417d2a148c863649682decfce684eebad62
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:06:58 +0000

    small any oil fasten complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
