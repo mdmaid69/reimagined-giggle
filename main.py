8b081afd70ca7c7e8650cec1384178d1a08b6260
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:02:09 +0000

    hair roof description yet getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
