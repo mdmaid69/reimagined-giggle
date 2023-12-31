bf1884377fdfcb7ca9147969d69db24ec64abb40
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:36:35 +0000

    past wheel awake intend little

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
