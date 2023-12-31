2c23b619b16bc28651fcd9589c66205c2d674612
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:20 +0000

    convince us progress there shake

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
