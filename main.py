1c68c77c07a4dcd265d802b7d87539254086a81d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:44:13 +0000

    main yes clear buy every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import tensorflow as tf
+print(tf.__version__)
