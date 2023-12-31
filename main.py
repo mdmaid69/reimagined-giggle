935a56a4001271b8b78e65a3fd7421bf2d964b11
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:18:53 +0000

    compare system seat number workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
