aabc4e22bd832dd9bb80ea0775ea4beab9ae87cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:43:59 +0000

    talkinto crazy you occasional pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
