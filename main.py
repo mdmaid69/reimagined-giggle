e0ed230e531a10eab556f1d96dd5dbc232148e15
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:38:31 +0000

    true crash question middle runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def cube_number(x):
+        return x**3
