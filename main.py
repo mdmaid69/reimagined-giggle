df02e150a1b1de1b17a1c61b9c829abc4aed81cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:58:14 +0000

    call everywhere Cc noone naturally

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
