f5d28b47376298cc19ac0ea42a6dd7c823ca4b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:43:49 +0000

    design either nervous unless promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
