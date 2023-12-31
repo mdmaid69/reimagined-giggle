fb23f0ef71e09c20092a85a74ebda01a0cc5616b
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:01 +0000

    damage hope remark age somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+print(os.getcwd())
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
