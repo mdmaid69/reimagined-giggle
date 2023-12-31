060f1ba8815836c9b373ff57283349a1d75d67d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:43:58 +0000

    work by comein step wonder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
