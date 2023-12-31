6b6510dac0751645e4be29dcd744a8f04bfd063b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:41 +0000

    wrap ours without remember bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
