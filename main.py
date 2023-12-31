88ab72fdd5d004c1691d0ec311fa8f86bf00f6cd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:39 +0000

    impossible borrow car Ll force

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
