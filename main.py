002864c79bfd252588a244b3b13487af257f4cce
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:53:22 +0000

    comeround cut report group climb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_power(work, time):
+        return work / time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
