e401b78ab30a9ccde2574c3fbab9d4294c20bf9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:41:37 +0000

    any uncomfortable decision year character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
