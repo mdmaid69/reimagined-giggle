2d556b54feb4ff46bbd6815cc6cb045e69c5eaf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:50:15 +0000

    disapprove underneath constantly his mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
