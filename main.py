7374a2092ff9d34d8a3ff144ab2271ab13b2a659
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:06 +0000

    big settle cheat wear trick

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
