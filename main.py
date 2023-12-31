ce89aa826b90fc03f8968a3f778dbcac3e220e74
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:41:36 +0000

    reason habit total whether attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_speed(distance, time):
+        return distance / time
