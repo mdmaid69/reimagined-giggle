3551b69c49b30b73a00ea23ec02e5aa5aee919a9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:48:12 +0000

    workon office extraordinary stretch piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
