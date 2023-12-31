ece2dc79c728379e139e6cd8fda72f3027b7ee90
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:20:47 +0000

    false land separate deliberately pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def print_python_version():
-        return sys.version
+import platform
+def get_os_info():
+        return platform.uname()
+def reverse_list(lst):
+        return lst[::-1]
