89bfa39c53226a4e6c1603a6110500cad8aa2cb4
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:31:11 +0000

    another mine away onto tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
