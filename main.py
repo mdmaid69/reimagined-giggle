46cb2eb56d4d144de3824e4c28fe1cde990c8567
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:40:07 +0000

    holdup contact space relation listenfor

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
