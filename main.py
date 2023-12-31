68b8d3015f1d86f4c32051aed732faf52737d88f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:25:09 +0000

    education sister improvement miss track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
