a02435235cb6edba13a72b1e3a079d926a2b30b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:56:13 +0000

    about concentration one statement stamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
