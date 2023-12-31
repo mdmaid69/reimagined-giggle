a40eaa269b962958ec1f4267c51f95033ba2e3cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:43:26 +0000

    worry sound left foreign ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
