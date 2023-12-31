7045aa4e7c737dd1e1d83b0937e10321d4671451
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:44 +0000

    with correction thumb onto keepup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def print_python_version():
-        return sys.version
-import collections
-def create_user_list():
-        return collections.UserList()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
