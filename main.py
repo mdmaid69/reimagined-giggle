c4a156906bd674934fb357a529231d25c093eab1
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:12:19 +0000

    action mean blind speech purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
