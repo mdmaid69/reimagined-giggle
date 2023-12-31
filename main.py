3e12846f7e4eb9e7503a8fb742c3558a91c2c1f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:11:34 +0000

    worst blame suggest congratulate poison

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
