08158a3d9130374f85a2760446a302681606b5dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:42 +0000

    television naturally refuse stupid plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
