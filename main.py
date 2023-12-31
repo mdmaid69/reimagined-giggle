2e827edc7d4f1a5df9747e3011100e3c4e9f51fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:53:16 +0000

    Yy error rush pin chance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
