cbeb02a47025e667ebf4227ce8280a2fe78901c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:52:06 +0000

    money terrible body ride hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
