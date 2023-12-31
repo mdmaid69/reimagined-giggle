4140eca0628094ef7cf0c5ffa355b4b6dabe107e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:22:51 +0000

    store release another hit interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
