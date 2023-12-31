3ddae804046f9348ddcb3120dbfd2fdf9753f9c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:25:51 +0000

    person calloff smile knowledge worth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+i = 0
+while i < 5:
+        print(i)
+        i += 1
