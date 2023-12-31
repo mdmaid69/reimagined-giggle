71d8a8099bcc599df4eaef9c748e817c4948b522
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:02:40 +0000

    save nose hang remark determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(lst):
-        return min(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
