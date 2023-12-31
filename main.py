9a124e36a112008a0295f02f3ce2b88737a38d13
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:29:45 +0000

    concerned scrap print tune go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
