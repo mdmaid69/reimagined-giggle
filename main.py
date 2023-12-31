65297788130d5b271c53dafd3a83e9d6591665ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:43:45 +0000

    pretty nor please place stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
