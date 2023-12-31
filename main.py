ec4b94ff15d2ac82ab9b062ed322454adc15b8ac
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:28:06 +0000

    crash glass habit callback settle

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def count_elements(lst):
+        return len(lst)
