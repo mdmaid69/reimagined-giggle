986d45bab13d7e954c8f2154fbc87d0e917e0c04
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:36:19 +0000

    funny less person complaint now

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
