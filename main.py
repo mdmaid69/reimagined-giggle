d37fb1cc2a98ae565251199101604d3204bd403d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:58:38 +0000

    pull note appear pool journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
