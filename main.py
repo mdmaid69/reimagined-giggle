a6f51d9566425876f16efd02ec079a86b350b8c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:04 +0000

    pay police distant wise moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
