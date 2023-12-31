c16c5b0e4d2ebbc7cfa91a2842f993f6915762b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:56:39 +0000

    shock sting mail story comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
