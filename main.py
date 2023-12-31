1db4173469b384c928b32956fe1d449ced162e9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:14:24 +0000

    even both admire straight drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
