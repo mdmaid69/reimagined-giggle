f14d300598f633f7b9d4ea78e66067bbba306a26
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:49:04 +0000

    fashion use good do border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
