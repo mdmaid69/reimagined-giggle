245a1c7676025be705ecfae66e4ca5cf67cbdfdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:41:12 +0000

    carry pinch so now board

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
