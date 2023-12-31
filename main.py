d6d4fe3e34526e53346069e65ce2854a96227eed
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:50:19 +0000

    sharp behaviour pick hope rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
