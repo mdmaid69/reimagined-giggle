646c12359e6d342cc09ddb39b5b97e582513b8a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:35:10 +0000

    nowhere giveout shout hide want

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def reverse_string(s):
-        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
