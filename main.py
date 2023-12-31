8d1096187970910f1dd351885dc016dff2287f3b
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:49 +0000

    weekend mainly murder board steady

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
+def calculate_work(force, distance):
+        return force * distance
