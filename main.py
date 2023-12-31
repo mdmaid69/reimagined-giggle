075ae970a1d2a298b60567eda7f9c6635b704fb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:06:23 +0000

    rain delicate rubbish whoever runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
