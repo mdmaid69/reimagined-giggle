feaf7c768f8023f8d9bff31f04890e7c5596c7d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:44 +0000

    share argue instead foreign space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
