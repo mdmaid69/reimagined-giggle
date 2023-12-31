2d95dd72d131b7829c6ceb6f96839e948fc9d9d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:51 +0000

    round hear objection fear free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
