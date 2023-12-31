0d95d5c4fe64cfa9a98ca857787af6a98182060c
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:02:04 +0000

    roof destroy callup commit coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_current_date():
+        return datetime.date.today()
