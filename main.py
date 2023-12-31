725f5b857e6230c7db44926c65ef73a11091cc38
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:14:42 +0000

    fortunately apologizeapologise concerning pile result

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
