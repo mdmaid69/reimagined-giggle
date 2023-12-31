321d8313a198597a31ed5be28ec268f833906034
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:37:52 +0000

    market rough success definite nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
