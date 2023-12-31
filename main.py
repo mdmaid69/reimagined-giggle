84198aa4c7fd801190bbfee41bd499436ff5e7e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:05 +0000

    goon listenfor bye order direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
