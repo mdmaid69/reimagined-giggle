4616ffe6938d7e095d30f9d4e8f512a58738ffa9
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:50 +0000

    once Ff combine follow dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
