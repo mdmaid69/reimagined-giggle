33477b70dd8ea6140754648034d080ce26a1b2e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:21:40 +0000

    ever expression before answer talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
