8ffa6997cee04a3dc911238a3b69bccbf45ed38c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:57 +0000

    sell basic explanation situation love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
