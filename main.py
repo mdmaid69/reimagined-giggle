ed4de3c9747054814c79c9237e07a5ff76cd7ca6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:53:58 +0000

    typical prefer promise pour information

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_str(array):
+        return str(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
