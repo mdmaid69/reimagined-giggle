efa83c1a0da23ff774d5dfc1b069387bdb23afea
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:41:56 +0000

    congratulations furniture this arrival crisis

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
