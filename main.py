c6b68b43df2c570595a7e68dad2bb9d7a924572a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:57:45 +0000

    schedule trust dull load wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
