c40f4684319e153a8a59bd04a102f574fcdbec6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:36 +0000

    vision often guard worried then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
