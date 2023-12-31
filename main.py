e59053974d035361707e43a58f34e1c180270d95
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:55:04 +0000

    equal owe happen strike finger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def is_odd(n):
+        return n % 2 != 0
