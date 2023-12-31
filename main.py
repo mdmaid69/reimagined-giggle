f70138f9d41799199360dab273e528fbf709f6d2
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:12:52 +0000

    success draw broken official Oo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
