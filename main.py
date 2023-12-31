58e8f1502f65272545489230766039627f4b5666
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:28 +0000

    judge each motion angry knowledge

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
