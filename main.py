66cfffa5d507df546deedff64a5d072765fb80a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:32:18 +0000

    big duty pause sink calmly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
