aa30698b98029e6a9d8776d081cbcbb56031a012
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:00:47 +0000

    direction body special interview under

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import random
-def roll_die():
-        return random.randint(1, 6)
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
