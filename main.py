cdf200221fb08ea7fc58b38f332dedc4176a2b04
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:32:22 +0000

    strength train near bored wear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
