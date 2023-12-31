e89168563a68544b2a7fa031620c906c8ca021a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:32:39 +0000

    keepoff object measure murder fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
