0946bde2559c643f2775ccebc66756aa747e74de
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:45 +0000

    stamp each show cause begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
