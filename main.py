7acdb5c6007ec65729607b4693bdc2ad813c0783
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:13:24 +0000

    legal sink criticize crime previous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
