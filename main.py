9e21d58636330988b293d33cc8f5b1e818730ec4
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:16:04 +0000

    formal floor be like somebody

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
