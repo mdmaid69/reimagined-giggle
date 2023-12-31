bedff59d473b381718eeeb558b5227ef728101c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:09:39 +0000

    evidence doubt evening miss please

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
