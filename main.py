b663b611ec1cfa44ff1756675879c61968a44b92
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:19:50 +0000

    smile true reasonable getout glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
