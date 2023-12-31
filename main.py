1ece041033b96d56ff0b92d93b1c144f9bd3d4ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:27:13 +0000

    read prove switch off intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
