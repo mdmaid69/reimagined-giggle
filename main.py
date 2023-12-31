c61ed31d38e8420ee8c78088c2a9af7e9612ea28
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:15 +0000

    flat either mother relative pot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def print_python_version():
+        print(sys.version)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
