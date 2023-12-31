cbc6d197f8cc46e4e8d40250b58913180a66ceae
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:36:42 +0000

    cruelty conversation appear smile abroad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
