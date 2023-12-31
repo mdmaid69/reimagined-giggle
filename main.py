38913ed0a7d920840971df64463135e08e45768a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:56:02 +0000

    doubt Cc possibility mixture comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
