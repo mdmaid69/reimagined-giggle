c9dbbbe1ed95867c1865a717896f763b00781eef
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:58:51 +0000

    promise coat us behind definite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_username():
-        return getpass.getuser()
+def reverse_list(lst):
+        return lst[::-1]
+def convert_to_octal(n):
+        return oct(n)
