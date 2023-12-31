625c7bec46db856ee3e6e17a4697d34ae718443d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:02:40 +0000

    popular bus sit habit dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
