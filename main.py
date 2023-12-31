0563218a5e74ef87b5a4823bd496b5d912c649a5
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:21 +0000

    protect everywhere different however interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
