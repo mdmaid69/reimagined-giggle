6428ed9e67aafe7fd84a44f0e4b2b0d35b9c2fd0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:18:24 +0000

    familiar well blind brief work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
