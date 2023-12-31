8f29fb9116e8f856891db641c10d91c345d04773
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:33:14 +0000

    winter angry read that look

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
