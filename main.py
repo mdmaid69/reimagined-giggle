49fa810155cbd58eee312f94f0c87116f1da4b3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:25 +0000

    used taste night birthday nurse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
