bdee198f80f56326c2e4d1753399f9208f8dccc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:47:07 +0000

    negative doubt improve wire luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
