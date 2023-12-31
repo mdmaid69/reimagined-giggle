333d6147fa1aa80e08edcff8317c4b6a6576d8ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:51:52 +0000

    save make amount cry opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
