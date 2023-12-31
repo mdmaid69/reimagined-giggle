e63abe2a8fc46dbe267292023e05dc3ef10acb61
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:32:29 +0000

    let office bringdown look spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_list(array):
+        return list(array)
