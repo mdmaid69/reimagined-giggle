df46e10bf193c02db4c5a4f8ab91b452c9c57758
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:01:46 +0000

    cry put later destroy cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_max(lst):
-        return max(lst)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
