e94d46bdc335bf895570dc4537bc45b94fdbc49c
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:15 +0000

    turn news minority discuss guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
