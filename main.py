057c98cbae9adfa115353412bb5edd0766b701cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:42:52 +0000

    interview question draw onto laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_str(array):
-        return str(array)
+def append_to_array(array, item):
+        array.append(item)
