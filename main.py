a75c123d213f5263a1bd3449afb6f66f318a61e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:07:22 +0000

    attach person wealth rest us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def add_numbers(x, y):
-        return x + y
+import sys
+def exit_program():
+        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
