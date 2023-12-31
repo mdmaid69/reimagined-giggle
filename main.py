482fafef2ef3fcad1e42948a9448b501d3e1cc67
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:11:48 +0000

    term pity quantity nature down

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def count_elements(lst):
+        return len(lst)
