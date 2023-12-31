34601572d06fb45f0dfd87c7faf8317c6be1e04f
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:40 +0000

    independent part bang instant ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
