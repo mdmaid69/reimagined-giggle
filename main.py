cc0680def9a5709f28584de12c33669f4ec56d74
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:40:08 +0000

    habit agreement trust system mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
