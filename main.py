b0cd784f04d5477311093170d07cae610eebf7c6
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:13:24 +0000

    ability crisis attract Nn blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
