dacc0c4bb56318f55d9817b6ca816688deadd767
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:15:18 +0000

    break such other any swallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
