93224737c85287c158940270ab6a6a682e12ae94
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:13:13 +0000

    borrow at Qq few getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
