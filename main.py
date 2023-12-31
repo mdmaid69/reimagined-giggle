3e1ea531e8bc90560db3f0e221d42691ecb0cc73
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:59 +0000

    shape ask container deep both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
