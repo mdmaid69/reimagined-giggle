1e05f7c053c413d7974a01aab788b83de0f25517
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:57:40 +0000

    slice exaggerate shop push sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
