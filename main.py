dc22980d2850cef7e378d8cd125c726a0ca93f79
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:30:02 +0000

    brain mend arrival shade bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def convert_to_hex(n):
+        return hex(n)
