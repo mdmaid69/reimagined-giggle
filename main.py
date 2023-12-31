4a816ef2d0470a658820cf438f36e908b1cdd1c1
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:30:09 +0000

    rescue long custom ask confidence

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_hyperbolic_cosine(x):
         return math.cosh(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
