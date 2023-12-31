5885a6ed4ac44d4b9d97562ecf5e3e54a0af14c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:39:20 +0000

    star shake will in himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
