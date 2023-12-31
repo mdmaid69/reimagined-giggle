8ed769a1d80f3c1ee369f1830bd44d29b200c279
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:18:35 +0000

    he way remark trick lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+name = "Python"
+print("Hello,", name)
