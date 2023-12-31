1388eff15d23f0df131a7bdf980cf27743e017e7
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:48 +0000

    along concentrate flat too same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
