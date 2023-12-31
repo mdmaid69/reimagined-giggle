5a2ce87bf236fd042f16899916c571318c0b9dcd
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:22 +0000

    force unit urgent receive keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
