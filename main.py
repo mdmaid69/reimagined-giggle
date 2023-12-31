d7b53f8f0141afdbf8d1c0bf085402fc6dfe9b53
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:12:46 +0000

    gift aware style recognize state

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import getpass
+def get_username():
+        return getpass.getuser()
+import getpass
+def get_username():
+        return getpass.getuser()
