a09b0039bfa33a9443c59b45413cef02a5375ec4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:58:02 +0000

    taste drop obvious remove pattern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+def calculate_area_rectangle(l, w):
+        return l * w
