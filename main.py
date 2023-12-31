ee795628d8aad62c73db3744a7dac078ef2e5cf9
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:14 +0000

    strike land bite force slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
