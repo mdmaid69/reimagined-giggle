26e3a533ac56d2a29c43b4dad2fa7463af29e04b
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:28:24 +0000

    final pen good get both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
