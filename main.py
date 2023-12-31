54088947aa92cc4bb91e9bb3070d11b193c81148
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:02:52 +0000

    transparent past colour spread direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
