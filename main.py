6fc616e73444b98ebcde5dabefa780cc37f6ad96
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:23:00 +0000

    free personal under scheme period

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
