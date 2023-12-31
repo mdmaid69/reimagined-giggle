b0e7005e1b562d3e7a39aadcf7ea972d382c7698
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:48:48 +0000

    bet crazy 565end union innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
