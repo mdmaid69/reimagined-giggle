84a6050d5f60c7c4054c89c16d657b616eef836e
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:45:56 +0000

    generous division carry arrangement broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
