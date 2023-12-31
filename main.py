b60888848004cf3de6978f7ac15ffee8e21e5b91
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:15:17 +0000

    oil radio send will smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
+  def reverse_list(lst):
+        return lst[::-1]
