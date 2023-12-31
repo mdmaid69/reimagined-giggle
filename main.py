93acdd893ac674198d47ea74759d50652b8a38b5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:29:13 +0000

    happen deliberate turninto escape wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
