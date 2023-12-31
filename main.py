74cb8a29a6515811d64426fa92b79aba24d0092e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:48:50 +0000

    grand genuine congratulations damage discussion

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def sort_numbers(numbers):
+        return sorted(numbers)
