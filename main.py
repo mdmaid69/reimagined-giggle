d36d8e4f30d5073c053b5ce99c69cb5539bab068
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:00:01 +0000

    total explain thinkof comfort on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
