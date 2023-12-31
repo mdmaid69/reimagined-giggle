4ddc108b535c25cd747b7c3589876e0b98bb18f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:57:39 +0000

    interested mail ever do climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
