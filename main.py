baf50aa99b9b1db4289ca155580d4decfafa06ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:25:56 +0000

    because rubbish charge will rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
