957cbcbc249cfe910853a350533382f080fa2592
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:56:16 +0000

    male consult should dark track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
