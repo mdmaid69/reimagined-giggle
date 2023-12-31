0b0404d8385b9794859d9055d823d88cfd50b7f2
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:20:35 +0000

    statement rest father formal instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
