f9abbc409028fe412e53c871a20203f44fff3f6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:18:58 +0000

    sorry owe Dd suffer woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_min(lst):
+        return min(lst)
