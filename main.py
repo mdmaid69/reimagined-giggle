847a5de3c4e1a7e70755f5a095a470d9fb822ef2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:35:02 +0000

    near uncertain although round exercise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-for i in range(5):
-        print(i)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
