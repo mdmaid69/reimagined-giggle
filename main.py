a12a2bfad45d24976ceaf4e9df826d7f4d180593
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:31 +0000

    behind matter precise slightly regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
