2b034e3526472dd4a7550724a55d2a30861938c5
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:10:55 +0000

    moral division add main pencil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
+def count_words(sentence):
+        return len(sentence.split())
