9d8760fd2ac27626fb91b881fec499bbd9f25e1d
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:45 +0000

    programme test Yy born street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def reverse_array(array):
+        array.reverse()
