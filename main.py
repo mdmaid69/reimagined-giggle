be167c2eab9268921a847d1b1adfa1217c7efe34
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:02:23 +0000

    scare scrap confused tradition either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
