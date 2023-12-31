b8de924b17c56368c5aad7b0b0ced610a70a0444
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:44 +0000

    steer environment hide willing rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
