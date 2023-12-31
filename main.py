eb34d4ede937d2a13970546f3b535fb3307e961e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:55:51 +0000

    suggest bread pure model group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
