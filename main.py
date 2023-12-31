66de7acdbe73379e26c11f898a50f1e31576c492
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:51:02 +0000

    keepoff equal spread middle prize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
