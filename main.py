cd046c3d3edbecc9817d52cb746e500c8b192148
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:58:04 +0000

    brother heavy pain pressure genuine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
