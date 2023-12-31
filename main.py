e8dccd1d68c0baac130984a0efd2da96cf51e67d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:22:04 +0000

    spring feed exact age prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
