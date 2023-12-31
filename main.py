ce508842bb3719800104128ab0c2980241284b99
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:21:15 +0000

    any claim rule work service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_bytearray(array):
+        return bytearray(array)
