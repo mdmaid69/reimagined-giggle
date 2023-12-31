fc7deb6385859258acd7938247bd257ea67bebe8
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:23:02 +0000

    form talkoutof rule common else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
