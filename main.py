123137dcdcd281249980ebfe533918982d0afb6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:51:11 +0000

    workout combination fight sore anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
