b628c55f436328a39cf46c0c2e7bd21fc2306853
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:42 +0000

    lend care improve fashion close

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
