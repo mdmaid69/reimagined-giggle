f476a0c1bd7c4d411dc1d84f97d8bb87dd9b7009
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:11:58 +0000

    unpleasant contain be scream free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def divide_numbers(x, y):
+        return x / y
