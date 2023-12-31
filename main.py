7ac3de73e5010c08396e284e9b4a37667695d0d6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:25 +0000

    stretch any other go able

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
