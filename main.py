ee3d10caafb9cd7dabf1488afcdce01b56b6e600
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:45:31 +0000

    gather not chat never equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
