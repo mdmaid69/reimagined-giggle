4bb2f21b1906708549d45d0b08155d96c274ae67
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:45:16 +0000

    state by attractive beneath cook

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def reverse_array(array):
+        array.reverse()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
