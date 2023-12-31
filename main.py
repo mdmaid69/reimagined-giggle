001a02dd082c347cb298aca3ed6d7670a917372b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:56 +0000

    confusing punish search try meeting

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
