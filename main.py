a2bc3e16aeffec950b5e0f826cf4d4933baf6311
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:11 +0000

    change think that explain acceptable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-for i in range(5):
-        print(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
