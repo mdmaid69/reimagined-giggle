dabb3ee8c8d18f3cd4c288de492e5e935337c958
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:41:01 +0000

    Rr profit excellent right runout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import collections
+def create_priority_queue():
+        return collections.deque()
