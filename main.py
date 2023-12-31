1e33d452049dc4029f31bef2ba90b158dc81cb56
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:48 +0000

    report tie education centre invite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
