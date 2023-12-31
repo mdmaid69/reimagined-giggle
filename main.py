a603cb0e51d7737418c8477e11a1f02f840f64aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:49:34 +0000

    clean swing satisfied Cc comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+print(sum(range(10)))
