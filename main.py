141c073e7a54f8c0b0b4262828f1dd61130bf0a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:36:45 +0000

    exactly embarrassed kind full object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
