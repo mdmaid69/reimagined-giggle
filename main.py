dedf7a89ce1282d2cb437c559e37a30d023c8e46
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:03:39 +0000

    after slide black innocent steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
