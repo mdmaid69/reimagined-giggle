4330ef6efa0fe6022dd8a4c14368eb0e8dd6fb6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:30:41 +0000

    Ee complaint urgent environment model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def sort_numbers(numbers):
+        return sorted(numbers)
