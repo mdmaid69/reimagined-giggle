2e4c737a75db60e9134002952e58e2837a3afb24
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:53 +0000

    doctor breath corner lead terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
