7f47d5024e415b030e4995f0978512b5bb8d1b1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:37:04 +0000

    extraordinary curious union imagine problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def subtract_numbers(x, y):
-        return x - y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_min(numbers):
+        return min(numbers)
