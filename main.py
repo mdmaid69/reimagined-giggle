77417853640631a295c0706f827c9c97ecc22e6f
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:41 +0000

    see schedule mind harm whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
