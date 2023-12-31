2469a773b9a0eb1d58d66db817e579cf41ea55f9
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:04:49 +0000

    class practical passenger aim depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
