bfe8d6456301ff18ed48a492ff2f4d61699e7c4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:36:59 +0000

    shout profit excited not amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
