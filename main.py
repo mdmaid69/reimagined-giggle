a81bdf4d91287e997648401fe8cbbd7699a15b03
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:50:06 +0000

    think like street disturb breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def is_odd(n):
+        return n % 2 != 0
+def count_characters(sentence):
+        return len(sentence)
