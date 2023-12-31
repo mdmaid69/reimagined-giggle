b3c11450ae84bcc1e658553538e6efbd82cd64ec
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:06 +0000

    will copy future allow slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
