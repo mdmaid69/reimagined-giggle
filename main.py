470b02c91d6df099d3ea86468fefae4174726a4e
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:43:43 +0000

    minute thorough valuable level mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
