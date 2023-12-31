03c268580ade8b8ccf46c3a2ba03049779fced51
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:04 +0000

    view note private light occur

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
