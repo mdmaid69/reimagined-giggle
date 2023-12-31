f7755e492e4fc77f2f8d4e2f9b5edc8fdafb57d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:19 +0000

    after whenever squeeze change gather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import logging
+def log_message(message):
+        logging.info(message)
