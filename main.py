09e9903f07b9e59e77f773ac3c7c57dfc5b5e273
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:07 +0000

    attempt takeoff at emphasis perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
