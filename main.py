3b6e0435002ef10a50b361a0df1ff4762d0cfdf0
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:37 +0000

    responsible 179bite drink lay possible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
