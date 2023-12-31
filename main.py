600e06e207d3888a8d909cb6dd27595f642bde45
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:52:02 +0000

    reliable forward somehow tall else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
