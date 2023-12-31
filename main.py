cfc11c5e54e80f0390cce41712c2836c68790d30
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:47:06 +0000

    window entrance compete almost strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def exit_program():
+        sys.exit()
