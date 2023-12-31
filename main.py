4d67755be67c15f53a596e69fb2f4b24d3b7d3c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:38 +0000

    bother cut unable contain may

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
