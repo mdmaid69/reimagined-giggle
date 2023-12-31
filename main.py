74530b4d2dc6bb896d3fc7cdf6135e2e68ba4b90
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:29:11 +0000

    rain board uniform person confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
