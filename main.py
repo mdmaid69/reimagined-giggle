c79b03c3c8650edd96f47f5b2176e652bc9f11c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:12:37 +0000

    wing depend risk national forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
