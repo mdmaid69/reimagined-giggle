e11c289d920c11502d49e89a89fcda4a15ff32c9
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:07:22 +0000

    friend mail row contain container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
