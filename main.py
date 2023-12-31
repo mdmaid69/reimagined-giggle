c9b33be1f55e998f550b2a54a8b8dae9ba20f36e
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:57:07 +0000

    name getback fierce trade war

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
