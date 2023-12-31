647f859c36ce4eae359c1fcc1b50347e37f101ae
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:30:28 +0000

    cheek foreign bringup several Yy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
