87788b4529ac7e4fde5208a5e27d008a1bdaf629
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:29 +0000

    constantly spoil change quick holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
