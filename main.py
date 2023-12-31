542a6caadfe56109fab80574c28f400b7d059f3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:33:10 +0000

    bar acceptable judge bury grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
