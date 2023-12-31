13901ea7fea2b41111591d63b2396c38a596c235
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:25:55 +0000

    born concentrate steal boring better

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
