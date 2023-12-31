0df84d51dba4a5c0c86394b8d5b6599d4d2dd4c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:37:51 +0000

    fault otherwise fear ready temper

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
