49dcf3f59a434e9f3ba8e7fbbd3ae1feb46539e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:58 +0000

    tool delicate influence workon cruel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
