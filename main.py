4b7d88496ddee594da195832ce8feac8f3f75900
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:19:58 +0000

    bother book characteristic accident hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
