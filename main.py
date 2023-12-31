37e35cd3fcc26056b969e6dc5fdaa43dbc212e4b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:53 +0000

    entry weak concerning stretch getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_error_function(x):
+        return math.erf(x)
