ecf0c7708f1425de23b1c6766144746d5d8df197
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:40:11 +0000

    tap sport recent practice beneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(10): print(i)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
