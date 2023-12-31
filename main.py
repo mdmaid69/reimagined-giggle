99d839e450fd6c2238f512d8d81a2bfe3b8ff5df
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:31:44 +0000

    turninto fault hang calloff swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
