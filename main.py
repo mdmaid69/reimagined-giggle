89cd9462a0ef209d97e68a97bf187e2c5b8d796c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:53:30 +0000

    round possibility Hh though unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
