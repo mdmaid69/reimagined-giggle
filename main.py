8960951baeb0f220cdbc37a1d1ba463f0c424158
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:27:06 +0000

    head let beyond damage surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+for i in range(5):
+        print(i)
