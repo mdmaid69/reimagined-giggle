f6cbff8ba64d5b403240763e0f2cf5556cccfe98
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:28:28 +0000

    blow behind pinch preparation schedule

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
