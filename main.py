d194fbac300bbfb4df3dab8cd9a9f2eaa535dc8f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:02:03 +0000

    image offence push immediate getout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
