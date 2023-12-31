e0dfbba2e926b9207305832c1b58c419b2b07d49
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:25:22 +0000

    team getaway travel return dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
