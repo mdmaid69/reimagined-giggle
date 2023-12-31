bea8a83556b0277d6a971573c5e4198b703a22df
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:55 +0000

    sign doctor damage thankyou so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
