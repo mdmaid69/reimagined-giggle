ea8fa9779c19e0e8ed5c57ece30003d6d1b8e502
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:53:08 +0000

    pin cap dress ring lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
