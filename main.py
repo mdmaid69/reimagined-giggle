96500b0b7ec4a1cf2fd5fbc62bec6a4dcefbf358
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:15 +0000

    calmly closed not road if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
