8b114c786a8c7831cc98610f19cd5f3025bb4807
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:39:45 +0000

    patience damage guard interview slip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
