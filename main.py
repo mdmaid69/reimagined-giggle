15c4e414e4aa53724a633405a465b32cb23f8f87
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:18:54 +0000

    dream passenger foreign live pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
