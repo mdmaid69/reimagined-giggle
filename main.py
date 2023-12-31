7f1d8d72bafb2b2f97db2078cfb5035ceb107317
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:03:06 +0000

    begin respect perform secret clean

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
