2e2d5a4e2088a628bdfc979a9e41b0d12fe23b21
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:33 +0000

    yourself getaway clue complicated suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
