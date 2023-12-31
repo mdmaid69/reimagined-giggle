efa179c03882d07ba874af674a98ce9b1c53b9fe
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:07:56 +0000

    away book age deal tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
