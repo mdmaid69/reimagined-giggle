9cddd373ce1ee4ea8e84dbf7f9e1da3ccf067750
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:58:02 +0000

    leave level way none hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_priority_queue():
+        return collections.deque()
