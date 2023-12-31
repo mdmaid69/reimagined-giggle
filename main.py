1e1d419ee9265334eb4d9340da0aa84c6b6568b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:08 +0000

    figure art bringup bother mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
