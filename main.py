f6c59670fee01a4b6ac79935452685c3785a10b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:29:23 +0000

    apologizeapologise OKorokay eye someone schedule

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
