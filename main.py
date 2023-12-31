85570f8c2deff55b958f1c8f359b1183ae40984e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:35 +0000

    black shock in label well

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
