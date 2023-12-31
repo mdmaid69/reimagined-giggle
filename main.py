93fe2c52696f315ea0c401b39432f42a05dc9e5b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:10:35 +0000

    sharp exchange spot unit within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
