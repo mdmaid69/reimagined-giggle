8c2c01f342d0f0636413dd7b6b7fc454ea424ea3
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:03:42 +0000

    cool today mind emphasis separate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
