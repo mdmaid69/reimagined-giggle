e1a01c5ce5129bfe42689288467ccfc8271a4a92
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:22:26 +0000

    welcome see please lock earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
