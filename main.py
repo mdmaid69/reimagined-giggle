f5b88f9e42f9221046ed2c14408c25f08d4b0bba
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:12 +0000

    Mm some sensible company taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+for i in range(5):
+        print(i)
