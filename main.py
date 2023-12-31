2f84070be38c5ee749f0233e76df713ae394b32e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:44:26 +0000

    period finish traffic Tt turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def square_number(x):
-        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
