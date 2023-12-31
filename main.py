b61eeaf067f153ae23e5cf8137d0f4ec55bedc54
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:43:29 +0000

    hers common bang turnaway silent

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
