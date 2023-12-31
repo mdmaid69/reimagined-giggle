c96293e82b68b8fdb925e523813d6cb6460b1206
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:55:32 +0000

    hole suck punishment behind anything

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def count_elements(lst):
-        return len(lst)
+def get_array_as_set(array):
+        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
