b23bfb7e409b598435454f5b41f4a2af39ee9d10
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:06:47 +0000

    share ear vanish forever for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def split_path(path):
+        return os.path.split(path)
 import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
