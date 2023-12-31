dd9494dbb5ca6063327df3f6333bd2e1c393ad8b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:49:13 +0000

    hardly disappointed confusing bank any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
