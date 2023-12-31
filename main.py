557c204126949e8e1adafbb7552e0d3ba3325d4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:41:47 +0000

    begin well so building organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
