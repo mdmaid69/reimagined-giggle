b68fa4d43c2ff811f8796781e86853316d4e9818
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:53:14 +0000

    surface hurt fond rise pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def square_number(x):
+        return x**2
