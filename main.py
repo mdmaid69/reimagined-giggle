a561dbc0518e71882d6b686fb6922c1664297c1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:51:54 +0000

    error impression refuse record pipe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+print(math.pi)
+def calculate_pressure(force, area):
+        return force / area
