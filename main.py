0b5fef63df229f91310b6f4c06a53191d170e38c
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:39:14 +0000

    wind cold sad drop talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
