478027ce4248981d39c31c60ddca92966fce7739
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:21 +0000

    measure grateful cool search join

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_power(work, time):
+        return work / time
