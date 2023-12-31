5a3200029639ea61fcdb191d7d327a7c36c62dc3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:10 +0000

    pen rush contents next mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
