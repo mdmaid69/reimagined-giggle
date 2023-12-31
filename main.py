c3df03a49d691801f82e0e2cfb0eeaa6ee4557f5
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:10:20 +0000

    performance convince vanish contribute statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
