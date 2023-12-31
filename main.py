82b355110276d95e148e3f2ac8d56206e9d072e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:57:15 +0000

    sting major centre identify stupid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
