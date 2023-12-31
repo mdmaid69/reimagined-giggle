0c37706afe1a46b531afa7bbedf368a0e5442021
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:03:22 +0000

    survive translate space forward content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import time
+print(time.time())
