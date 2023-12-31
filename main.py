43df895c3adc5ab769e9aea987360483bdae1609
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:07:24 +0000

    getaway opponent moon black cheque

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
