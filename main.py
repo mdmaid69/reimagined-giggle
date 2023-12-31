12795bb24f04834f5ad3475088142f82a3e3aeae
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:29:27 +0000

    on pump might desperate intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
