a472516adea664ed3529e7e66b5b89c644f2fd3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:44 +0000

    so forget manage history form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
