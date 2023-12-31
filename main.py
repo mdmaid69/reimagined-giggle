898f763fae51d5d892d4b47bc66cbd89b441c88a
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:35:31 +0000

    ignore time decision sticky tight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
