05acbf5f40cc85b16f964a1431c4f5afe071b4ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:34 +0000

    spin term backwards force weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
