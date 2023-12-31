9428425b0b293049431134c833d6c25376b7b23c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:00:26 +0000

    image rarely near sort mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+for i in range(5):
+        print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
