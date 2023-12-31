f296198ef434e225f843e68ede3db90f16e85f10
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:49:02 +0000

    forgive deal quantity crash else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
