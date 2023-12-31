5e53ef9f91b904b13dfa98d7c1f2ee616bd6eb1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:19:08 +0000

    absolutely convinced runout favourite surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
