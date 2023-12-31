d25a38076377bb6704379b6896c9a34818825511
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:09:27 +0000

    luck accordingto why relation practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
