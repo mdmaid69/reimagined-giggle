31e9d8ea82c6013905aa27287c12bdf9ecb0e7ef
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:06:49 +0000

    train equipment lock progress advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
