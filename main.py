83b59141b2f7794f86f5dd1579b972b989a4b552
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:47:17 +0000

    expression only depend prepare crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
