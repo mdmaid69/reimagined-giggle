89a24db0237d4c20b118fc3640f52bcdb21814b2
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:35:16 +0000

    attention unlucky table feeling not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
