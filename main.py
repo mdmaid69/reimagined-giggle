1510ce8fe6a8a62fda062d01b0da2330d6b18a9e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:32:37 +0000

    follow battery original wear flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
