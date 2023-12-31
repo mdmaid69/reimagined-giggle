4cadd3bbd79f20b1f83043ec0104a794a36fb590
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:19:11 +0000

    shock with remark contents pen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_work(force, distance):
+        return force * distance
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
