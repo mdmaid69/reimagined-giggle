284c05bddfc5dc3e7d473e27dbd3c5d931defc31
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:33:02 +0000

    paper attitude support practice method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
