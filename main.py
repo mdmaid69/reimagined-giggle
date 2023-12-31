5431ef52ab90bbc93b52dbcb81229c93e2f49376
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:38 +0000

    cheap my cool lucky report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
