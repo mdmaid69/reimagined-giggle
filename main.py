0ce5d27a9ae3ddbfece73443ca197d1200065046
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:53:53 +0000

    attract violent note stare sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
