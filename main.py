fc864dd2235f7499530d13dc7ae64d7f12564dd1
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:01:31 +0000

    obvious takeout mine giveaway pile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
