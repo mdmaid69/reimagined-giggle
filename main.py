89b3288a412fc7baf16a78fda0069f4dca1a309b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:05:38 +0000

    innocent raw night scrape region

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_power(work, time):
-        return work / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
