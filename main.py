6d0772255de9d6a8170980f579c62ab26f0433dc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:25:58 +0000

    name cover fast argue stone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def multiply_numbers(x, y):
+        return x * y
