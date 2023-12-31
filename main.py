dd28ca927319202d80d6083004f5dbf0f91ba3ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:13:40 +0000

    organize Ss body word among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
