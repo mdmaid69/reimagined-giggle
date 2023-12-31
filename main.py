a7628dcc7951cd29c1e8b75ade6c02b4ddd2c521
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:19:29 +0000

    position smart behave exciting experiment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
