f2ebfafb4064bd9c76bb787eddfd13c6a64fd665
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:44 +0000

    fond risk pencil between Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
