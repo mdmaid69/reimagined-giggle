172d79dddf815c0873afe608159907a9ecd2621a
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:05:43 +0000

    stone immediate city do stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
