5b8e9d8fde5cb5173f2f1b8b519afd129202e57d
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:17:19 +0000

    bag luck reject female shout

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
