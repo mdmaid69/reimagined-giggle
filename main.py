a2e2528b7a3d0086412f3b6f2fd2a1c5db0e3d85
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:50:28 +0000

    address touch pause suggestion getaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Uppercase:", text.upper())
