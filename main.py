32271afafb7cbe0938705b85036b7455ac6b0512
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:24:37 +0000

    reach class put object exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_speed(distance, time):
+        return distance / time
