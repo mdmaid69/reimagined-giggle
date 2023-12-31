cae32165ddd488dfd8d1440558e9eeb45049461d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:39:36 +0000

    proof Ss flight scream join

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def square_number(x):
+        return x**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
