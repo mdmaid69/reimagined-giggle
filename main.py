51a6d44b5379ba0e7275ac6cde4ace3d658485fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:05:16 +0000

    involve runinto before dance wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
