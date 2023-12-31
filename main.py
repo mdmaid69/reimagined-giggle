076f7e389b3ea59cfff416557a0e50988ded2052
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:03:41 +0000

    workout history dream storm breakfast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
