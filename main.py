611e7905d1bb20b34ec4568d69ed4806902c2575
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:43 +0000

    fence mysterious immediate storm already

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
