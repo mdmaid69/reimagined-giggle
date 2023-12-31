9afa97f7ea055e2f6837f9d06b0aadf9d9103434
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:30:00 +0000

    correct waste alive spill ring

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
 def get_array_as_tuple(array):
         return tuple(array)
