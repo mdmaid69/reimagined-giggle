c94656243b75b8c4cf38d6fcb6bad2bc6906621e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:44 +0000

    mark exaggerate meeting popular typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
