20ebfaaf2a262a7b9da63ceaed0f6e9758045ac0
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:05:45 +0000

    listenfor represent interview march react

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
