2af68a42831997693a5fcb3b14617ae10a3ad048
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:24 +0000

    kick broken wish knock argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
