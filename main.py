f1f2dc67216945843ff0c315d2309bd8caf99db0
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:52 +0000

    start central care sore genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
