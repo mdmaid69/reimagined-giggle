904d5f68865adc6fa3e91533e2dd55056b268921
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:44 +0000

    lock strict close age exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def split_path(path):
+        return os.path.split(path)
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Words:", len(text.split()))
