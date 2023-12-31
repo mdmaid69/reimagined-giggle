31dc39fa7db2befb74e09442266a48daa7fc703a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:01:20 +0000

    fear difficult careless eager invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
