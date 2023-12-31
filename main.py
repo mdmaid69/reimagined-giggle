5c968e91eefb1156f3595eaa06b171cacf291653
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:40 +0000

    promise equal conversation lonely tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
