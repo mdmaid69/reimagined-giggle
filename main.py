e46c635cd2a5c6e7504a355f1550a769d5bfd195
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:24:12 +0000

    absolutely instant somebody talk conclusion

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
