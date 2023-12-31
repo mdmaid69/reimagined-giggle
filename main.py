c60874d32cb300ac8ebe3be16c1ee43a1a49cb33
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:49:35 +0000

    priority agree back variety second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
