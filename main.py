1f5996dc12573a70bfb7b0af97d3505c58b35656
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:03 +0000

    disgusting compare boil aim hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
