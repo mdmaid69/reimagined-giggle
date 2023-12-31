0cdaad41a96a480aa8054efe9d4f9e870f3af6bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:24:52 +0000

    fetch nearly approval nose Itself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
 import datetime
-print(datetime.datetime.now())
+def get_today_date():
+        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
