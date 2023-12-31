1c79352e526e4ee3e140c6732766973cf3f3f233
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:05:56 +0000

    name plain blind everywhere accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
