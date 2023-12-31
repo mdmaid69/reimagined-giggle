211ce4655b4e7929831443c9f959ae5d6a793bc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:34 +0000

    interrupt quarter set difference stupid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+x = 10
+y = 20
+print("Sum:", x + y)
