70a4d397ff53b28e33e1a594e83e0a3dacf51d46
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:43:59 +0000

    spring damage tone bear slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
