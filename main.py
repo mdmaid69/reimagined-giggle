a6192ef8ce56e1984107fcd5c0e3c32ba674c069
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:51:08 +0000

    runinto laugh nothing space remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
