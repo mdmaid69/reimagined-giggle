bd6634c0cea1964ddcaa86d1beebdfd2d34f8e94
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:49:39 +0000

    total lift scared comein roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
