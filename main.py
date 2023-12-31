8d39442da4466d13b25e5c2bd512fec0c872f5d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:44 +0000

    land relationship speak thinkthrough choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
