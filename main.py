df098e127855b8d1aca6bf7f88a5fb2796000173
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:19 +0000

    set either main accommodation border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
