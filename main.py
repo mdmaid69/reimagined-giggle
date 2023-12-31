3a7cdb36c85aea49dab4754a359ebe2a5a560349
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:40 +0000

    day well lock escape wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
