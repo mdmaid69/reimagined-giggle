3f39ce11a22f4313fe7ab6dfafd3f72593ae3872
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:50:12 +0000

    stretch nothing matter accept problem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
