f3390280a9d87cd3babd93042074facf95ec0607
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:27:28 +0000

    concerned breakout scheme holdout even

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
