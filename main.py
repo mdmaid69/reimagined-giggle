5cf09eba7f3547df7e209904afffece6a244bb8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:49:14 +0000

    ourselves object makeinto plenty see

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
