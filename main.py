eecd6ebb35793e5965c3ab684c6bbae2e0ae6b23
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:16:16 +0000

    care hair file clean danger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
