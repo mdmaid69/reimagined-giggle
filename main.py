46762d84097ddcf62badb9a29ab43bf3a5e84ec8
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:52:46 +0000

    lost know crowd uncomfortable great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
