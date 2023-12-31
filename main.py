9565937300ca28c236e612fb5e8b3373ece48930
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:50:26 +0000

    possibility thought centre father contribute

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def square_number(x):
-        return x**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
