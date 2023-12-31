7828872fcae40d669c6066afb5cd8e18dd3941aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:19:25 +0000

    takeaway over opinion breathe although

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
