261344afdcd758f808f64d2d760ed7b52a322deb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:34:58 +0000

    Pp listen affect shopping side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
