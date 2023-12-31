c25585bdeead9a9a700a96679e122aa646f33820
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:41 +0000

    accommodation probably imagine choose open

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
