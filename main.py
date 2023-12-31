7667d2b836a55949c5c56f91bc0a4353fec73863
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:02:02 +0000

    opportunity speech several path comfortable

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
