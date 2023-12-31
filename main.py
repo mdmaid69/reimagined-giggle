8c99791ae680f2046f0a6e418e91055ec0e473d3
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:19:10 +0000

    original person neither raise equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import datetime
+def get_today_date():
+        return datetime.date.today()
