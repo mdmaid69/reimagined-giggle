1447b1fbd8c2ef81b6caa36f73a798bd196b719a
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:50:15 +0000

    to promise along ought centimetre

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
