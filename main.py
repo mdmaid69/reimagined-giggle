d5844c67a1726e55d1f29131e0839929e7f82edd
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:38:20 +0000

    beginning light correct miss pride

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
