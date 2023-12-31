a6adce7172a985e7a44fdc75f326f8271b42af50
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:46:55 +0000

    possession admit season 572enter handle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
