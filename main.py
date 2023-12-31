852f2bbbc45f443b3b68dd7c08436d8b6b4c0f2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:35:57 +0000

    runinto describe complete put begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
