878b6eaae24d500d8897482f3793ff4f82506b7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:31:35 +0000

    trap generally exciting little spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
