46f6fe0e40ce57aea5fb267e39739fc044ebdc3e
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:34:01 +0000

    bury lock zero politics wonder

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
