2e443c52da025c921b9eec78191712f2b9a7b693
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:16:27 +0000

    realize uncomfortable urgent shame themselves

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
