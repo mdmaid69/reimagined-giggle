7ee203f3128aaaf3ef8e0d3768b7da79561a773f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:03:34 +0000

    history goback(to) upstairs loan over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
