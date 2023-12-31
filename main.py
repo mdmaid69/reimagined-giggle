2eb67ac05e962bd0569fd820c93f03de57a5f742
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:49:21 +0000

    exit frame tall agreement emphasis

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+print(sum(range(10)))
+import json
+print(json.dumps({"name": "John", "age": 30}))
