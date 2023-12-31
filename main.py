efd59696f47254177095fff54bd52ac44acbba63
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:20:38 +0000

    instruction turnon noise fat upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
