92448c225131562cd0d5ce9d5fd037ca028ed513
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:18:47 +0000

    risk smell area gather pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_string_from_array(array):
+        return array.tobytes()
