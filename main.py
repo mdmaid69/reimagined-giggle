089ef4a5c423825a5959e632e3ad887b57f60169
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:33:54 +0000

    behaviour destroy scrape book heat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def convert_to_hex(n):
+        return hex(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
