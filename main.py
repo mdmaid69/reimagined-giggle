12157f92a489d394d8056cd55365137c87488e26
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:12:36 +0000

    number town between mention concentration

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
