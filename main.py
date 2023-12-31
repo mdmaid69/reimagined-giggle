317152eb9dc1cd18a36f8a61398c6ef5d16245e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:44:38 +0000

    brave sweet part and Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_array_as_bool(array):
-        return bool(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def get_array_as_bytearray(array):
+        return bytearray(array)
