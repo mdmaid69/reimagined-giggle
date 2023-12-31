fec4596d01bf2c486ab401c3d728ba964c722860
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:46:47 +0000

    reject them thinkthrough along step

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
