83485ede981910e6cad651cb7ede6d87a6956156
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:14:36 +0000

    freeze gate sink song defend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
