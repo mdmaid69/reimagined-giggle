2f6768804413c5c554a89bb042a585a3314c098a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:35 +0000

    I decision roof short plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
