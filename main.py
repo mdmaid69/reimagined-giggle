fe26f0dfb75446207640f621d1ab7815fba16a3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:04:04 +0000

    Jj impression noone previous ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Words:", len(text.split()))
