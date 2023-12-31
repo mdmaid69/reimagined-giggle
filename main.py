99adcdbd128aba1f830f272e504e4daab9bb74e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:31:39 +0000

    second father pocket shame fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
