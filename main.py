bdbbade87ce1ffd8ddd4820241f19a7c25997d8a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:41:45 +0000

    form music choose platform walk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def remove_directory(path):
+        os.rmdir(path)
