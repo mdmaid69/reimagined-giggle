a0a7057ef3f18d2f0329f420891ab34f2f8e2aa7
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:00:42 +0000

    nonsense dark noone word desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
