39b4af6426a68c043a85e69efb24ca87ef786199
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:04:41 +0000

    shut upwards post honest test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
