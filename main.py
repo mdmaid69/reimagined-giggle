f2e989cc924f774a4e6339559b8766d8a88f5430
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:26:14 +0000

    round straight first drop offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
