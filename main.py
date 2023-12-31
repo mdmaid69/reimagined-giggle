bb6230a8a7a5053eded03f0aa009ad91cd3e227b
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:54:55 +0000

    original thirsty respect content how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import time
+def get_current_time():
+        return time.ctime()
