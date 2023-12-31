7539676af38c5b891598932034020f8c533f4f13
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:56:28 +0000

    bound careless switch wrap beautiful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def multiply_numbers(x, y):
+        return x * y
