1ab036e31e283fc5e5c80d3c7bb09b35ce3c6cff
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:03:18 +0000

    habit distant demonstrate kill deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
