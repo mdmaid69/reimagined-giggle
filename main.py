6f7ff2d572af3105ee7ea4fa4984ae2bacfe4107
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:18:45 +0000

    month position deliver couple old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
