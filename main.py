83df2352502f40bb7e27f70f04e2e18b2fe26348
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:33:06 +0000

    patient high comment onto while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
