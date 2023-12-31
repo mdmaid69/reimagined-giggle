58c1da5235498ae95f33de7a55dc1da738d88159
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:48:24 +0000

    hurry country to rule risk

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
