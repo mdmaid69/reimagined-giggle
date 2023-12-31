3784555ccb8e88c03935b0671c99945c807e7fc9
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:38:13 +0000

    chin car soft part square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
