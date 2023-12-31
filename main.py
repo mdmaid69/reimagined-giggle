dad71b8e33286ab52e2f09b11f9057810bbd54a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:30:19 +0000

    sign tend no rush joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
