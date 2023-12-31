2c7a0179d1b72ed355b5ab28961649fe93ed4c4f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:46:32 +0000

    schedule risk spray delight clothes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
