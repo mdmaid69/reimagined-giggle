5b2d7b38eb62efe25c3e21a62d783446517b4cdf
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:54:32 +0000

    store carry story original value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
