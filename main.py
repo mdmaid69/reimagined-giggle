d7a4e5fef9bda5d169b26c21e3cf521355dfba47
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:42:57 +0000

    worth straight important ordinary hide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_user_list():
-        return collections.UserList()
+print([x**2 for x in range(10)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
