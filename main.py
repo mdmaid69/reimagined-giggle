378a4aeccd7e31cbdeb77fc09a1101e6fc98d646
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:07:27 +0000

    beyond impressive almost refuse godown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
