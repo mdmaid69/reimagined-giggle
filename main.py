3667248abb75199b4efc7c85145c942416a05250
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:17:02 +0000

    male ashamed smell than listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
