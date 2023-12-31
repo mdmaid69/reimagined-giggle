dc7f39a5c1fd790768899bf410897f84bfa0988c
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:41:26 +0000

    spin pay live design attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def is_odd(n):
+        return n % 2 != 0
