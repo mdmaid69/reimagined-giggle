0e4944e7ae92431c0cc3b4b98ad8bfe53b846264
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:50:54 +0000

    similar difficulty contents instruction male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import collections
-def create_queue():
-        return collections.deque()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
