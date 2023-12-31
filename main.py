1eb7070a6bb6629323e2f167f76159d070f74630
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:08:34 +0000

    I this once sort container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def is_odd(n):
+        return n % 2 != 0
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
