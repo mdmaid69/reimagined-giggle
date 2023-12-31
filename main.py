686d5616a25267473a7675decfb7e02735619c69
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:46:19 +0000

    push grow Uu stop rarely

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
