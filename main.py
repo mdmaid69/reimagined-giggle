efa051a89f0b25f56f957d1e567d451fd9840c59
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:35:40 +0000

    switch curious his mystery strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
