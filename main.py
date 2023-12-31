7b0cce92dbc7d40cd2fec5e4bfd3a190bd43f316
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:05:53 +0000

    complete allow home preparation pipe

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
