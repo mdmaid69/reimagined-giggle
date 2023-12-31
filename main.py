a48614e5345ceef245eeeb42f0c9c02031dc1b9f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:10 +0000

    chase close case middle order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def subtract_numbers(x, y):
+        return x - y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
