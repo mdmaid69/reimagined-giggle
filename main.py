4d79977fc9b78caa80834d2301b8788f042b4146
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:19:18 +0000

    health rob eat face rank

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
