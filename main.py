3d18d5a8847450aa9dad663c7b85ebdadfa78894
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:39:39 +0000

    comfortable very lead sweet solve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
