48be3907fbc720f57ce776ad4274e2224e268c9b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:55:47 +0000

    range conclusion replace allow full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
