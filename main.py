58d6b358610fdc44c35c4105b49439a34bc20f2e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:26:37 +0000

    impressed arrangement time rather gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
