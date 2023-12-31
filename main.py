32f94c228e04ab82a187ccfa798b10909f66a984
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:34:06 +0000

    unlucky all happy now give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def change_working_directory(path):
+        os.chdir(path)
