938ca6403ade95b6114651934955ccd522c449e0
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:11:44 +0000

    workout Yy cut trial study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
