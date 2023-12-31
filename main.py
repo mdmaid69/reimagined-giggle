ad734ce6239fb6954a1b301c8c38ce0ae14c82f7
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:35:23 +0000

    upstairs answer week judge miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
