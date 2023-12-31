a4ab732b081225ad8274a25e5902f40d537749d1
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:42:00 +0000

    stamp personal write considering yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
