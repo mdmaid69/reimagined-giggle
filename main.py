19a9dea13cc145ae891073e17b77f9c9ddb8bf86
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:36:26 +0000

    still split ask rescue feed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_area(radius):
+        return 3.14 * radius * radius
