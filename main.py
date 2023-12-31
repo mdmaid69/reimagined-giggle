4b886b0ba9417252f7754708ac50104f284a3315
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:51 +0000

    grade dead sorry light forget

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
