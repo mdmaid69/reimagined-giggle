8048a5990d918e6004f29e9fdd4c6f3ddf98fd95
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:07:03 +0000

    turnback tape influence process guilty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
