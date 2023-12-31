67180efc61ded2d188202a737116c556d5d1acfc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:04:28 +0000

    standard file prevent cheat break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
