1f66f236ebc9e50ee2d8692d00fdfcad62ed15a6
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:16:21 +0000

    pay possibility control food report

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
