6f74fbfdd3bda94d4e74adadd756503293446974
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:54:15 +0000

    winter degree wipe organization estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
