5cc3830bdec380052891bd9acb2941ed860142a1
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:01:03 +0000

    friend patient base tragedy rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_sine(x):
-        return math.sin(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
