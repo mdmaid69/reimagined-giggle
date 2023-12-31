8c5f9f15a767692adeec326e317567b66e50a082
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:00:51 +0000

    how need splash broken Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
