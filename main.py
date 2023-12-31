790eb641dcb9da64ae3604ab8ee0c8b7709b8e02
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:38:07 +0000

    work pretend wet genuine always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
