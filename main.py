0a2acc08f4c1f3d85d74f628ba8876c94e6ca905
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:41:16 +0000

    on gap or hit quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
