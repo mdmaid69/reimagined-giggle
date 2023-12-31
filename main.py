7abd7c47ce15b9571832e273c9a9e639768b5530
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:19:49 +0000

    loan meeting cinema chin drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
