d6527a3138137904e620961753d55f95a7431112
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:09:38 +0000

    pool runover shut equal card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
