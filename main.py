847b488f223d24c9a1bf33b24980f885bbbfa70b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:42 +0000

    system song apparently sting crash

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
