ea6d749fa65ebeb86a0ac10e3a2a2578dd203fbe
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:47:16 +0000

    score row cook church cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
