44837834d8ff5c71e93cf200d26f6d5cb1d4341f
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:43:11 +0000

    surprised crime quantity extra contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import os
+print(os.getcwd())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
