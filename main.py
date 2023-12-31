1c76b120beb628385d6547a29984834d77da1f1e
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:02:51 +0000

    realize off connected goup sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
