64f5a266ce17c81e18e5b60c029194210c60aed0
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:36:48 +0000

    school risk arrive influence close

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
