d1bd00ad0bb2560c24fa117ac43b2d952af622bf
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:37:06 +0000

    score spin telephone finger promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
