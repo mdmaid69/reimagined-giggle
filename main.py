23c364052b048e086fcfbae30920c0b58f608483
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:11:05 +0000

    port lie throughout lock ticket

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
   def delete_file(file_name):
         os.remove(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
