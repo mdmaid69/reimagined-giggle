3ed75b3ca2a9bd0de95708b860f7dc23915770a7
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:25:07 +0000

    mystery bath before load correct

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
