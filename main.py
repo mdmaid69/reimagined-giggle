716f2ebaf5d60daf0f0e110652ded55c2dd3dc3a
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:22:44 +0000

    male creature exaggerate test fairly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
