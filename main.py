ea7715a3839350b6c2327ec3599878e66997cd71
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:50:20 +0000

    expect sea danger perfect building

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
