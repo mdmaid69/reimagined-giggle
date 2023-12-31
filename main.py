bc5b6a777f063f047c7205d7650a2b93f7039a18
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:21:26 +0000

    picture her sale stamp press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_work(force, distance):
+        return force * distance
+import os
+def remove_directory(path):
+        os.rmdir(path)
