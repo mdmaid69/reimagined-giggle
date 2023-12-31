859049ce709092cb9395951a0cf09721d9675d0a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:31 +0000

    rule rest depend thinkof hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
