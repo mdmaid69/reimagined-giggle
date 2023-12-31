4749e1da4978cd28471b0eef3fd30d17e1234ee6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:12:36 +0000

    avoid above underneath across fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
