f2c15e2930775ddb7b5514722f3a90836d75eafc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:56:02 +0000

    free group dance determined roof

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
