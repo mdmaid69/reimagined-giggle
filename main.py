fbcab0fae3bf478136dd3736d32d552ed786e99d
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:30:04 +0000

    more Itself squeeze succeed noone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
