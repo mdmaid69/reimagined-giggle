7b75c90ea89d5848cf7c9fbe5f531338791673ce
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:38 +0000

    future at earn dull nigga

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
