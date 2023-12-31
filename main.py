38fa8300f510c401e762abd4eaeee29c9c5c9d02
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:11:46 +0000

    relationship act gate hers giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
