27932f8f8d09d26d1f73f206ed0ba1ca3abc6b38
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:20:35 +0000

    at past knock reaction stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_time_since_epoch():
+        return time.time()
