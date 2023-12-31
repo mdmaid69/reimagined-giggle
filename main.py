18aa6ae9fc934d3a304227cdd5dc20a755843bad
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:53:31 +0000

    time buy sudden fairly remember

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
+def convert_to_binary(n):
+        return bin(n)
