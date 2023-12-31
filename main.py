7eceee161b6a2fe89374da0c6ec2baa1fbd13bc0
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:58 +0000

    act than insist contain proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
