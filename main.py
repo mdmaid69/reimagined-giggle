5286eb59fc7de828c9b6b9c35b79e2da482a16cb
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:28:20 +0000

    bored very worst call careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
