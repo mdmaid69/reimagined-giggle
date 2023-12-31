467b2816b58d8a4e46693f22aa6cad505b165f5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:06:16 +0000

    frightened about runinto also badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import matplotlib.pyplot as plt
   def plot_graph(x, y):
         plt.plot(x, y)
