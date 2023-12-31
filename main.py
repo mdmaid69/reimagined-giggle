703943b9fc2d719f3c37f7fb7b8c2a10edeb8361
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:00:22 +0000

    emergency beautiful surprising law original

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  def square_number(x):
-        return x**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
