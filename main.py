dbaa81b9520b782ea6edcf39fbf8071f08de7dfd
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:18:27 +0000

    excuse mystery react mine relation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
