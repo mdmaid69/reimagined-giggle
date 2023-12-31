47023232234f58c1ee9dff6ab3f9de07db7dec5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:10:13 +0000

    plan task simple height indeed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-for i in range(5):
-        print(i)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
