21ccf94e33fa2b8006d6c7048c83157d1de32dd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:56:54 +0000

    active sentence news extremely class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
