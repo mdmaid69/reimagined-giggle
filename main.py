53a5e234f0217d7fc78dd8fbc069508250d23109
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:32:43 +0000

    line passage suppose accommodation ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
