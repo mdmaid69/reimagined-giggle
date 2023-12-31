a4cc1220d39eb84b651bf11fd233c59301a84e22
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:00:58 +0000

    professional trade old inside check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def is_even(n):
+        return n % 2 == 0
