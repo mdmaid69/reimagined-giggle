a3bbe3e94a18aa74a5a09203f34afcb81aaf7ca5
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:11:15 +0000

    brush summer phone punishment pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
