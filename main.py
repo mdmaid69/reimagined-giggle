c1a1aedbfee4bdb0f7c847696fd5d0336bc2109f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:08:36 +0000

    choice on shock tradition plain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
