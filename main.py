e7ad32ca1260ad9158da66819d2dfffb1ffe0391
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:56:00 +0000

    tap Hh you tape instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
