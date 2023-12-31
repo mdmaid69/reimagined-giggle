60dbe99d0bb109bb315ad410b0dc09d4f45c5d94
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:26:57 +0000

    before nearly provide sign recommend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def check_if_array_contains_item(array, item):
+        return item in array
