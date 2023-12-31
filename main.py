4d8e11254e77af383795594f75bb52a1348c5c18
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:49:09 +0000

    already knowledge future gap splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
