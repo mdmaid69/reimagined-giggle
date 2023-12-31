224e3ad666319797f2f0b9873e9b2df55367fad8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:06:58 +0000

    suckme sticky eat wrong off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
