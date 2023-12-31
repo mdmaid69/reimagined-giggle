b69d6e1730d6924aba525e71152b8b29b788e2f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:55:19 +0000

    embarrassed Rr whole opposite listenfor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
