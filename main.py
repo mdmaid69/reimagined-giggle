51fe254ae602cee62bd013bde7544fc0dae28c5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:38 +0000

    switch good silent breakout address

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
