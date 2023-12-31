9c63a30aadef27be41491c6d6d5fa23e14a71032
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:24:26 +0000

    runout matter contents warm visit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
