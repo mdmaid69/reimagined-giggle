cb3ce8036f098f44c73752d1bd39a42a27daad5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:44 +0000

    probably scheme pour makeup dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
