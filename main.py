df7d3bf02222976f80a895f5d2405b4a47364c60
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:29:26 +0000

    connection delicate sensitive contain genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
