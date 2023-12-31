7f05a90fc011dbb3a666036efa1554de022d748a
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:37:28 +0000

    normal shopping proof board may

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
