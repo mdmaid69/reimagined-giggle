5eb3fe9c5ea35916543550ed54d15d943c82ec09
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:30:36 +0000

    invite fire track thinkover basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
