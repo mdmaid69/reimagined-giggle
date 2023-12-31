c6a494ef042296ab5fefb4fd021b475aaa243ebf
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:26:38 +0000

    throughout phone enthusiastic sure sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
