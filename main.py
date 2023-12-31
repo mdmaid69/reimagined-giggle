2eee99711add080572061ab826f428f634e9550d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:30:16 +0000

    cinema broken main storm wise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
