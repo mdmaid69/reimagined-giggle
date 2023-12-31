ed3e873b98b6dff9b8dfde2413329648bd3a2248
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:40:55 +0000

    generous bring spill consist thought

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
