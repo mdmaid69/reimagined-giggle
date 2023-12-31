6452a0c0ea9fd4757c8f8b5d8abd2b9a521082bd
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:23:53 +0000

    affect mean unfortunate takeaway sting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
