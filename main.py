6fee84449e64be56edf0e2f9e0091b0731a4537b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:22:28 +0000

    attempt education splash vision star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
