97358e7b72cf7b6dda063ce646459235eccb61aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:02:14 +0000

    sleep score attention load intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import re
+print(re.match("h.*o", "hello world"))
