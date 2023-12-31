31ad73b76aad4199000964131e7d7aff462852b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:59:22 +0000

    connected delicate emphasis community far

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+for i in range(10): print(i)
