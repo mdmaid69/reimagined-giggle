f3d260ffb2fec3ca44e7234fe4f53e79e7b2d530
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:04:31 +0000

    unable takeover telephone mark breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
