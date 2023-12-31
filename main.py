26a54a36c9bbf37bce4191f7b2cf85c24c119180
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:23:19 +0000

    check vanish then estimate damage

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def sort_numbers(numbers):
+        return sorted(numbers)
