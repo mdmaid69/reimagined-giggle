b68c76aef889ce6fbd9da3308ef850a51407133a
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:19:51 +0000

    start workon emphasis offer treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
