1c930c186f05ce3b7973f8c2417fbd7b8639dce2
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:59:53 +0000

    make congratulate before close rob

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_volume(length, width, height):
+        return length * width * height
