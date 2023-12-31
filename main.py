d70b5a426704e79f9085d13783d2b8200249cd88
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:25:39 +0000

    lock expression another couple importance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
