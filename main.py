9bcd9bb5017a7ad10c8560120a45f98566760b8e
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:22:49 +0000

    sort real independent not task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
