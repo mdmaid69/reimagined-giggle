d04bb511be5ffae0f57d5b633960221db653826f
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:46:44 +0000

    cover agree side coffee list

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
