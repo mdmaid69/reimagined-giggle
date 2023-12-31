b55ab5b5a5035ad224f069db6a84e326c2546d64
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:47:38 +0000

    prepare you please possibly colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def multiply_numbers(x, y):
-        return x * y
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
