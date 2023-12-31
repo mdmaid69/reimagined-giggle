e2d15b31594c568775d60a1c9723e45b323eeb60
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:59:08 +0000

    upstairs cross crash speech weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
