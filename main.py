b380bc0a714585750e4b03bd17b260e40528058a
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:16:07 +0000

    home body on rush need

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
