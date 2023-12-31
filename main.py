8ff5e2ed32e15fdbe4fef7a9051aab98752a2c5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:28:37 +0000

    another getoff instruction amount without

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import datetime
+print(datetime.datetime.now())
