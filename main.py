ca47bb7378adb4448dd68bd8f1bba2e29996e1d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:52:31 +0000

    confused tradition neither tip kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
