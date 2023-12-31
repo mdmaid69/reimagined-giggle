06a7995228d8a1963e8e73fcd34d4ff1fa97cb47
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:49:20 +0000

    avoid roll avoid exam shut

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_power(work, time):
+        return work / time
