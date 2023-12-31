5ca1a11278902f8d3e760c1e4f8bc387574cb195
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:08:15 +0000

    breakdown block apparently advantage shelter

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_min(numbers):
-        return min(numbers)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
