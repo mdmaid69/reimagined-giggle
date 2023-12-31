5b63b31ee44133349a7b601be9b4a4c998de7a8d
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:34:23 +0000

    base behaviour face muscle importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
