39c9f45d5935fadbf6adfac954711c12813f1a5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:07:30 +0000

    I sister badly dangerous disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
