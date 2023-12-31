ddc9e885a74d402fc3e100d51c76b2cc0a192157
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:43:08 +0000

    area order them steep workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_factorial(n):
+        return math.factorial(n)
