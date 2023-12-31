f52a6607f4f61b1603e73c68b3dbfc3fc30fffe1
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:04 +0000

    culture mess 573entertainment knock home

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
