c0011af0796c4924d101be3bc08da83d132c58c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:31:13 +0000

    delight slice keen force thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
