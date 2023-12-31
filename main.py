c095ba05d8215777218f7504b7f635c3a753193c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:30:49 +0000

    up pour limit turndown sign

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
