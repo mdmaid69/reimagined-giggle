2216039e18ed69d04fc4522db1c2ce83247f7c63
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:19:08 +0000

    copy but face compare form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_counter():
+        return collections.Counter()
