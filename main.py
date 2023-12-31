32185e9f3c4ecea7e8d84afb08b264f505d6b6d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:32:55 +0000

    sale passage abroad another cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
