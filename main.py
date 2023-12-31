a0f7347af0da71c4d25f3fc7f077b1c66b9ad16a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:18:28 +0000

    prepare minute key condition breakin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
