b13485e7c0a24ab3dfdc6ec59d5914c936bcd4ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:45:54 +0000

    concern sense pattern main country

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
