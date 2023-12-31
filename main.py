390fdcab7416de944ae738b2151d9b96a4d11c49
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:48:57 +0000

    either forget slight Vv bear

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
