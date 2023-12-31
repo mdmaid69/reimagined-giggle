8fdfea44ef1b6a0df3a4297bc5f642e1d4e3e04b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:45:55 +0000

    comeover turndown physical up sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
