c36e99fca1e1dbd36ecbfbb81ede5bbdb5d662c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:13:58 +0000

    explain invite dry hurt find

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
