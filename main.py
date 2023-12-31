4e5e4d6b5aff6f589dd1b6cb93436ed30694e08f
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:27:44 +0000

    command dry answer they battery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
