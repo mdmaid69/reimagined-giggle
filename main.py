fc27ab4e14e9f1c3bc4d19efb2f9f7a83817a490
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:22:08 +0000

    form shelter provide flood joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
