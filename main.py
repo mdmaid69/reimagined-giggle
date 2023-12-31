cd724d9403c46a97de00b3a8e4e93b9c699a57f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:09:49 +0000

    getoff balance doubt limit pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def multiply_numbers(x, y):
-        return x * y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
