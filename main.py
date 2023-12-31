23400b05d3ed1ad76975ea6b7cb78bcff5ecbfa2
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:51:52 +0000

    opponent leave discover sweet frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
