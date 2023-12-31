ff86e6e628db3229d65eb07faafe6edb0a7c4508
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:31:38 +0000

    leave score sound minor invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
