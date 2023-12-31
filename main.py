ec89a0b424625b10c103ff9f2c4f2005c84b4e6c
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:35 +0000

    education stranger schedule branch fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
