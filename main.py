8980504860bbe15d066ed7f0823cb23dbf2095bc
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:16:03 +0000

    relative wide innocent excuse Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
