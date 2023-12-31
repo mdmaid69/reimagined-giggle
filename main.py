2285736a1f4fced32b6e457d3846e2f83614e001
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:39:46 +0000

    past love not shout read

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
