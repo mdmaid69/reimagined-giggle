6bdd2e9b22c4dad2c80a94a64b375a61daed5f4a
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:01:24 +0000

    offer beat organize description thirsty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(x, y):
-        return x + y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
