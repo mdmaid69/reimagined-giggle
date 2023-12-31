47cb2d0ede3ad299c0ec8f427abcae3d22828122
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:02:53 +0000

    worry genuine breakthrough hit organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
