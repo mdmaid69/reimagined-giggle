e60de75bfd5ab3a1348dc7d12902793a8ae5c783
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:01:22 +0000

    pay instant string tell circle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def add_numbers(x, y):
+        return x + y
