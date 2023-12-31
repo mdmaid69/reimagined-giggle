60e6851d2971acc1e27f4865fd6c476d24c0c601
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:30:46 +0000

    wash couple benefit human comment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
