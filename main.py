592a5c02f7d42cd4a15119c329aea7c20dff7007
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:48:13 +0000

    guilty male several lend giveaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
