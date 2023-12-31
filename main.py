a18b737ebf21a84bccd2943623566fa526ced230
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:39:16 +0000

    reply load report slide active

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
