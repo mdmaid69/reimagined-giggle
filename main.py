bc583a2eda0c77e7fa618635db39ceeb5d271af6
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:43:02 +0000

    basis rid keepout payment escape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
