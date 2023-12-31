cf0901d0016188d5cf0701164bd592430c2d41f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:47:25 +0000

    union interview demonstrate attach 571enough

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
