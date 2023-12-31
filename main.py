39059d69d4a0401a129c37d457f09a5c3a0a91fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:38:26 +0000

    brief transport refuse forever arrange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
