724a8a6688a846d5ab63a30a34785044d6fb1f94
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:45:23 +0000

    attract slightly wealth problem burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
