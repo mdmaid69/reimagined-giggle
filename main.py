3110227f65f35b23954978f570bd8e9a5720089d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:18:24 +0000

    read declare very comment left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
