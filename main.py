866f35466a69e78803bb5ad166eaf16a2af74c5c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:10:49 +0000

    fresh tune lunch last will

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
