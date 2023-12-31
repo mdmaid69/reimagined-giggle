b434084405d7c264b60c5cecb58b3d129e0fe0c8
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:41:13 +0000

    well embarrassed check thick thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
