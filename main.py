cf71ffd5d90babe34305468aa87e211f870b29f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:45:29 +0000

    seem modern regular slide your

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-print(os.getcwd())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
