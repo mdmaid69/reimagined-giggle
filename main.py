983d3957022faab6542947edbbe418538a1fab7d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:55:58 +0000

    shock file inside hot left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def reverse_array(array):
+        array.reverse()
