6ede3f6277e0919c6a20de5bd1b4e210b75cbd1f
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:17:38 +0000

    wide fine lower choice talk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
