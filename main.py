8252c24ce40de92c7cf89b4f6b50fa7cfe351d06
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:34:26 +0000

    impossible please afterwards into tidy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
