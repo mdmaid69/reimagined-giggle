7b62fa5612ba6322262262ec443cd4a65915804d
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:20:48 +0000

    limit soak step belong store

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
