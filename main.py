f9e2ea0ff9e24b1700a1866813cfb30cb4319125
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:21:26 +0000

    unkind on little flight action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
