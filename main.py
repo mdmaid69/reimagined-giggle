a93606cf82e93b0059d8866382c310e9655a6391
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:50:23 +0000

    pot during tough sell improvement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
