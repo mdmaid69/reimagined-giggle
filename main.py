2e28a0f879bd063c96d8a64d50d64bc1bede35c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:25:28 +0000

    over shame joke mend used

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
