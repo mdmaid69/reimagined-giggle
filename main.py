1616a54ac06c3f02a737c5d98bd4bf5299207ca6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:06:22 +0000

    band pour later recently alive

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
