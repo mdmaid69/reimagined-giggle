4802618d39addd9b76eabb97c7680482a9479d11
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:52:53 +0000

    opinion accordingto better profit bag

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
