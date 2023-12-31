f5af4aafad3ffef9f1b51e4ad2355114c0b52485
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:53:52 +0000

    disturb high reasonable declaration why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
