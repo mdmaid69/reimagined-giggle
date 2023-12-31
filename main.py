36167117f3b327d78f9b0ec7f3c50d8ec973a11b
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:25:28 +0000

    natural whole agreement broken embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
