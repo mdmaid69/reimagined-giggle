5b678fa06a78daee676c249236a7befb1a758575
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:51:31 +0000

    honest minimum same enthusiastic explanation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def square_number(x):
+        return x**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
