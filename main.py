68509def382dde47576d7c90dd843becd74cd18f
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:23:35 +0000

    copy delicate information Rr frightened

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
