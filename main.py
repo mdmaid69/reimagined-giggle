b802b2d8e673d98f2893dd682f69b8da62a0ddc4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:00:16 +0000

    development realize bite remark maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
