71d3298c07c952b93585bc71fdbd4d456a56c2fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:47:27 +0000

    fair block journey fairly nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
