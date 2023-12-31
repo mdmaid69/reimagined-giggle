3dc4a5f4462538875e2f8d10cb2cb27c9ac4b9a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:28:34 +0000

    intend ocean right lost maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
