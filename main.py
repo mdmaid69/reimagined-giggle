8ce36b07074e72bcbfe6724be2483bc0e5458dce
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:41:34 +0000

    reason apart scrape swim approve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
