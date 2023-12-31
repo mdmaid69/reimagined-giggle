c433b5c93ebb13b47852bc3360b4f43355dac2aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:05:14 +0000

    platform final conduct light couple

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
