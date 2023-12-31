928a6400270d0284fb75ac37b078a0368b82b66e
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:24:11 +0000

    bend maintain fault lay face

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
