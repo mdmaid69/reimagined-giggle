ca0891af5c58f11bc39dcc2600b71bec9c7ac50e
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:42:58 +0000

    respect I cheap occasional drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
