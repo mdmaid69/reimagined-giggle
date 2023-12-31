a033a2049ea34fefd66f76a51165e3eafa76e2aa
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:28:32 +0000

    rest middle characteristic takeback interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
