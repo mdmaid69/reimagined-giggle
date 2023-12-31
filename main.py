2ac9e0f5345dde22bade061aca2fdcc748e81773
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:03:19 +0000

    pass ring road secret Pp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
