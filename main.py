04088e0c551244ce6b760f708ac64fb05096871f
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:13:26 +0000

    cough pretty front cut easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
