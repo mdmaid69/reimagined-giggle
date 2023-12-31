1beff11f4a7d6de87cda002ec7388a625f114a37
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:18:04 +0000

    issue wash behind praise physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
