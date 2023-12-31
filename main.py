985f087e575e116f801bfb0a8d74ceda16b0103d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:45 +0000

    house impressive firm sure his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
