7141f90c827efcc2d2835848beaea64b7258b6c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:01:16 +0000

    thing obvious disgusting gradually but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
