fa6edc1bfb153ef55943d6c584612ccee70b3a49
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:05:44 +0000

    fit truth heat hollow over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def square_number(x):
+        return x**2
