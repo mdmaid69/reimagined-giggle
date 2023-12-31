9864a3d981138aa094d0be706393d2c1e3519f28
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:02:54 +0000

    land steer tragedy proof whether

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
