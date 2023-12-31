e0042a22d110c9c56fdadaa7460ec35d7451b1a0
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:57:18 +0000

    similar pretend confuse lock error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
