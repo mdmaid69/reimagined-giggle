0b37a42fcbcb514b5d1fc4b7d4e369855d8e9d92
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:51 +0000

    desperate point far organization soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
