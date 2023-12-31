0e25a39d6a44fae6c781e43e99ddc5a97f42ad26
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:14:47 +0000

    swing offence failure danger equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
