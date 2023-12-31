a75123f313beecc3a940a9312f8cc133221b4a87
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:40 +0000

    simple mad bunch whom likely

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
