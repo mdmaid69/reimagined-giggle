f617299ccc899cf9fe23a14cc76118f8cf4f1637
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:50:19 +0000

    cut patience cool Ss control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
