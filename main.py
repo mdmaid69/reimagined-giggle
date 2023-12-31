79400c9989f308cb03775f13237b8064a84a151d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:04:14 +0000

    minimum new immediate cook worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
