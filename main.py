898724060a7ec2c2d7bfb26e9daac0c95d8783eb
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:23:18 +0000

    receive fasten platform careful pin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-def cube_number(x):
-        return x**3
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def reverse_string(s):
+        return s[::-1]
