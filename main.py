f492075b73d14cd489031be96f9259bcefea32f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:34:04 +0000

    circle top group recognize shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
