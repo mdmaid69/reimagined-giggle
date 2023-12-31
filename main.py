58613dd0cc14a140699a7127443d5204e73cad08
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:17:56 +0000

    everyone battery achievement worth exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
