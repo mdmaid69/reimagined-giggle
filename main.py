117f5659a52cc3fbcb076dfbcc7cc3cf6d68ba32
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:24:45 +0000

    speak matter someone ordinary dear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
