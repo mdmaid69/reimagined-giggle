5d1bbb7ad7eb180a32604e902e77d84c12a7e5e8
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:10:01 +0000

    attach connect hole he scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
