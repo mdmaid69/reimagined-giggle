48036b675b4918c0c74a9b24e420e1ad4f71912d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:11:54 +0000

    closely shut ready 179bite conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import platform
-def get_os_info():
-        return platform.uname()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
