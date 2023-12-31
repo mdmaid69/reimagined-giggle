69306ab0c4c89978379a62d913a1a316e9f66e38
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:26:18 +0000

    trade win blue burn too

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def square_number(x):
+        return x**2
