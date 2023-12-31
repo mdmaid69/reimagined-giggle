8577aa6fd177b61d17687dd5db0da669e3581b6e
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:34:08 +0000

    similar deliberately stage report steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
