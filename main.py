e3d78af689ed9e3be164ebdd70c2a6cdb3a7d1ee
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:13:22 +0000

    wide easy whoever charge pronounce

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
