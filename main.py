d31fb1fb79a2e9e2fd391a59e4ede0cb40551495
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:25:43 +0000

    innocent steady book file strange

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
