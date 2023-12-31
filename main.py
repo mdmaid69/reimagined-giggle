32a2406032f81f51ef0a78d510bc128493ed5bc5
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:39:44 +0000

    memory sign confidence train tap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_speed(distance, time):
+        return distance / time
+def find_max(numbers):
+        return max(numbers)
