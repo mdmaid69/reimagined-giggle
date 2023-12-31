25593ca416f0f9281b954ec9c193e90b17e7b8ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:55:56 +0000

    comedown from beat relative female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_max(lst):
-        return max(lst)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
