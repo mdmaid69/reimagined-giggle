b62dda62a6e70d3becf649a68ac9242f984962a8
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:38:52 +0000

    entrance alone hole dick waste

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def add_numbers(x, y):
+        return x + y
+def subtract_numbers(x, y):
+        return x - y
