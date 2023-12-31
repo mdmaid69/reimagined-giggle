b5d1fcec64c4ef1dd42a3b80a4c3d0a6e652a62d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:01:03 +0000

    reject translate identify flavour neither

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
