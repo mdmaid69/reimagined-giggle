899a76918d77ed182648144d0bae0892148bee48
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:18:29 +0000

    twist dangerous excuse harm rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
