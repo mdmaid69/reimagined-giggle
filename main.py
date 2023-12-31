b6a5bf24a8aca70b3fc0552434f22d169473c79d
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:29:28 +0000

    how artificial Ee side development

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
