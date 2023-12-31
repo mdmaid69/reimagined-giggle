126cdbc813af9d22b8ea606952436ad25e6f2730
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:34 +0000

    quit Bb debt innocent concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
