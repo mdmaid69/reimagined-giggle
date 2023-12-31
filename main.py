af0b1acde0b1c74adcfea30fd839911154ceae87
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:31:12 +0000

    better moral burst tap club

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
