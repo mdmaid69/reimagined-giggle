0db484689fe5e2b3155d47b240d05a26d67917cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:44:23 +0000

    spite funny late they damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
