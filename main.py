4b38ac972b5063a5f01c158ea2e49718092a6098
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:38:09 +0000

    crowded practise judge half standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_volume(length, width, height):
+        return length * width * height
