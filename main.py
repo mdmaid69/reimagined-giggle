7ef3ebf8736b9b179cabdee0e362a3fed1ef9cc6
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:14:34 +0000

    call interesting distance us dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
