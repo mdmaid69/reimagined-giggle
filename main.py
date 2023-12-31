8f61ba54b586bc3b28e3c7e7d0791817f18428a3
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:08:43 +0000

    exam loss sensible line minimum

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
