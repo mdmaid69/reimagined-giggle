a4510a29a6b0933b15db9a99f9d2d6d3ae94d2c2
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:36:46 +0000

    plant flat car surprise along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
