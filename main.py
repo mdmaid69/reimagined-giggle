fcd88a89cbd39a64afc6228cf105676aee27f54f
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:21:02 +0000

    crowded sound bored injury shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
