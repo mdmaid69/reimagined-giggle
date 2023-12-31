ba8089d75104a905714f2bbbb0e9d88e92be7428
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:26:13 +0000

    human close yours ourselves language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
