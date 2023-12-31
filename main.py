558affdea0908d89924d36cbd858b30ba3335882
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:39:32 +0000

    description conclusion character theirs open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
