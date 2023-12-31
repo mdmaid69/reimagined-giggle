d3a47432d430a9c59e1485b78eea11581d4c51bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:22:04 +0000

    reliable start motion evening convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def get_array_item_count(array, item):
         return array.count(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
