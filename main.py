8cd9d9a9a7443cc6c29f7cf939fc2843d27809f4
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:11:50 +0000

    design teach protest part money

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
