e9736824a611288085902ff642c15b8083fc6131
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:41:27 +0000

    silent possibility shrink quality open

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import tensorflow as tf
+print(tf.__version__)
