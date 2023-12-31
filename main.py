f756a8996159e94891474d51a5e50d90ed927594
mdmaid69 <robot@alexsyw.me>
2023-12-31 21:40:47 +0000

    cheap try far comment crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
