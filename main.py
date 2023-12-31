9e1ccfdfb4934a3e3addc6df329239e4e0839c1a
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:59:49 +0000

    keen slowly please home close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
