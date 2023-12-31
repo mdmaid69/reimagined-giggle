a1ac8ba942701a12eb4f59b088bac73e931a5bd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:44:00 +0000

    use challenge knock 571enough dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
