1a7730846ee9589b4504f7f6871ee066fa69ac1c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:51:35 +0000

    legal change anyone slip temper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(10): print(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
