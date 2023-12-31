4fbed51535a4607aa3cf9721402b486a5d982957
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:23:59 +0000

    coat instant club waste than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def sort_list(lst):
+        return sorted(lst)
