1c5367a8ba07a88419d2b08569d728b740dc22f8
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:49:30 +0000

    raw negative arrival badly do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
