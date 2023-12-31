6509c8d558ea2b42708ef3f5ddde06bb82a65416
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:15:14 +0000

    whether work answer turnover rule

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_work(force, distance):
+        return force * distance
