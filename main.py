05dda571d8d49c13111c6941e1b8a3d9c54aa6db
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:15:07 +0000

    war cause rank progress maintain

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
