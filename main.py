c9727746d07c403ac8d8f41c4c15db364c61a13d
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:39:50 +0000

    decision less string drive attempt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
