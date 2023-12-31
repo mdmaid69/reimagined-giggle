40e29e05084623283f56ecc7621443c1a8ff3ac3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:06:01 +0000

    down out yours bye mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
