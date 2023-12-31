d815db82606d296847ad619efacdb3ae46dbd124
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:33:54 +0000

    wonder press dear pass true

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
