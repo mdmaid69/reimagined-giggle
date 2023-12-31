6508ecac4be4ce491d5d7d45386d357ad6b1398c
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:52:07 +0000

    rush must chance comeback bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
