fd0cd77720239f07e13269b35151d859f472f011
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:43:30 +0000

    however getup downstairs peace disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
