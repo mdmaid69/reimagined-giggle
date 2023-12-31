6485498b68de04dfaf01d72109fb99692093ffd4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:55:19 +0000

    free control start tread frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+  import sys
+  def get_python_version():
+        return sys.version
