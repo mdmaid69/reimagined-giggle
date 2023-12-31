00034feafc46e9861a5f30bc48b90fb192593a77
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:05:37 +0000

    likely computer 571enough silent violent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
