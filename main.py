244bbf5b0a0d1c12bac02f8d02b96077a574c34b
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:36:39 +0000

    steam similar nonsense anxious the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import time
+print(time.time())
