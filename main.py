7891ba5b6e7c888795fad9f44b3ba983f033a9ca
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:12:48 +0000

    judge pencil quantity total proof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_itemsize(array):
+        return array.itemsize
