41027d5acf8af33e3bfe760e319149d64df65b06
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:24:09 +0000

    original threaten her leg spray

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
