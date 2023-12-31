77a411ee19c379aad79966831bf38e040783c29b
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:33:35 +0000

    act solve appeal clever strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def split_path(path):
+        return os.path.split(path)
