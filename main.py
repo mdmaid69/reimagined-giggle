bc2d6712db05c9a13d57a718bee319d7668acd4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:30:42 +0000

    plastic advise first pretty different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
