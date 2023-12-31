d6b49ab07b44b654023c3c398817b4443909c6db
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:07:40 +0000

    bunch ticket shame by impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
