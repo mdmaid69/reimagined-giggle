6feb34ff21b4394a9f4c71e97454a8e2492ef31b
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:37:30 +0000

    sister spill detail spray dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
