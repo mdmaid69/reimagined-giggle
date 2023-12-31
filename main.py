d08252e9532cbc26956ee4b30ac70c9910c93bca
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:26:15 +0000

    temper cap mystery genuine callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
