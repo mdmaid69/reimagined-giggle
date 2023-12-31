f7fec86de2ba544d98dc772f0031eec80359c07d
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:43:47 +0000

    long human basically invitation himself

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
