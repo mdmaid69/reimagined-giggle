e0b9b82024cdd00cd1b54e0a695bb917cf3cffd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:57:30 +0000

    detail market cover convincing go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
