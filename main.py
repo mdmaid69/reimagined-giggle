f204c9b31507d5cc63dc771ea8d285db9ccbcb26
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:16:21 +0000

    opponent offer succeed interruption compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
