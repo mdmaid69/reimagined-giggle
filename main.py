769ce83bb0eeb8cadac40a23705dde24419977cf
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:52:37 +0000

    it public rare cruelty ago

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
