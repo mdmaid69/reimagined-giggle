7b9c8ec2816a588e133cf32c5fa2295c8a0257be
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:06:11 +0000

    pressure lay responsible needle health

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
