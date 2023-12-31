541122e92eed07729ebff83ac5f8dd5e5e50779f
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:08:57 +0000

    woman development treat rain centimetre

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
