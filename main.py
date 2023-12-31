d6ced2397c8d67898883a959f5b3d7b8dae384b0
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:51:56 +0000

    pressure sometimes important substance inform

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
