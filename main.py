4907cb8c11a730d4a63f476ae511eb3648261858
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:43:36 +0000

    like value strength dry gift

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
