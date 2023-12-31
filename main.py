db230eb3b91a1f3a378148e6c0fa016e21d0116a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:10:56 +0000

    swallow uponon on control hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
