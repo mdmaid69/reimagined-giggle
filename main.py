b61151672f456369109e70af9fd83cef610ae52b
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:15:58 +0000

    risk doctor turn correct bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
