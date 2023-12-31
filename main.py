fe0d71573f739e6940b635ea94207f8ca50fcc3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:00:11 +0000

    dress sore either star holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
