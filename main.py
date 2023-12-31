885aa2064af55dbcbe6f51f47812e13583d8414f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:39:12 +0000

    instant comeup once rate loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
