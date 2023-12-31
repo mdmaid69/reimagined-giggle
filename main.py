ac80039f44074715ffdf645e6db2acc0c4a8389d
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:12:06 +0000

    trust brother either Ii bet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
