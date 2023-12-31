c1062a8ec0cf628ea6255311343ba285b7377cbf
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:07:36 +0000

    spare moment aim loose police

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_floor(x):
+        return math.floor(x)
