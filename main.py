4fe1ea9ee28583b6e844caa01d41a4fb9e9bfb5f
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:22:58 +0000

    realize born calmly book concerning

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
