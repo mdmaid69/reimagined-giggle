e7ce3becadbba9d14d790184e8562ba6df7b7c5d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:58:37 +0000

    tense cheek emphasize spring battery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
