a8b92a36a54d7acdc4eac50eec8c628b4c6edf9d
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:52:37 +0000

    keepoff among pure everyone serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
