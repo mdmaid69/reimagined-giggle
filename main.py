f7db3227ee2d06d4c6e1177ec55c5883377594fa
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:53:10 +0000

    achievement cruelty show clearly estimate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
