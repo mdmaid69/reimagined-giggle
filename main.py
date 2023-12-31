de858500c2c721ad25471791c04bb77be7eb858a
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:46:30 +0000

    eye introduce goodbye left negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
