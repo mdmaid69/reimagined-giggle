07265526981de534fe296e81006e19eeb9cd3dd3
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:44:33 +0000

    fast precise exchange phone load

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
