b059bb98cf0013ea8546df079b84ccddcf427ac2
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:04:25 +0000

    hope sweep provide fast chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
