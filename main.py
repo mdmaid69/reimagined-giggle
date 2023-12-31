04bc19341cc3aeeba475090e787aa86bda283c19
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:54:14 +0000

    glad rush week artificial reject

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
