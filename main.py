a28edfe57dde5edbb43ede33915f0c2333dae009
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:43:14 +0000

    school explanation vanish row seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
