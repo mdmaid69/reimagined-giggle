5240648dce1cc38973bfd54e3db24587f7c6977b
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:50:21 +0000

    road bad reach quality mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
