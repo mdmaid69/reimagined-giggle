94b3cf9e0ae25d008bb7363143cd7f8223f4d502
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:30:04 +0000

    how please family do another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def convert_to_hex(n):
-        return hex(n)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
