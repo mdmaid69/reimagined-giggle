a02231291b74f0640379bee7a19150cb29ea43ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:58:52 +0000

    room spring wall thirsty programme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
