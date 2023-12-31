548c573c4abc12e095008fdb3ee3af77dc60cbb5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:02:03 +0000

    success properly fail main sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
