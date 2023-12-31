5bb31bb325a5560fbca6a6d8687a4fbc9e930968
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:53:57 +0000

    drawing walk party pressure benefit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
