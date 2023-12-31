47f0a9c9187308dc4248fcc4ff8f478d906a4a09
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:00 +0000

    know check cross cover fairly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import numpy as np
+print(np.array([1, 2, 3]))
