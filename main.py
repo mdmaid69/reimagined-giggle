c7b9c0b3ca709d333209465226a76021b07916bb
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:27:21 +0000

    Vv fetch passenger splash father

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
