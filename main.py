20520aafa24a1dd3bade122ff12fe3361f1b9550
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:36:35 +0000

    square gather information disturb advantage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
