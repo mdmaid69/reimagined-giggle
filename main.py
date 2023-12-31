466cfffdd642ad67a670a3c22e19da521a4c2fe0
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:27:06 +0000

    period excellent Ff lookafter science

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cube_number(x):
-        return x**3
-def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
