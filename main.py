8a006157026aa2e12af935894e8d702ececfc552
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:10:05 +0000

    tomorrow official turnover whoever between

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
