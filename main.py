9bc7ae2699e7548f9618165093a25077d091e538
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:55:16 +0000

    turndown giveup present lucky skin

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
