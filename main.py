7d4faa6b56bd9d301163d2035096aaa8da3f66e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:38:42 +0000

    harm swallow Ee closely ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
