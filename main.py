92817a4ef0560b708b85b2b48b73f83d8570e2fb
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:43:39 +0000

    she chat unkind apologizeapologise image

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
