ba63fee9fced410962fad05296bae4e5e9dd6453
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:50 +0000

    trap be above naturally nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
