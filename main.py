01fe77cb1ce97f9947775f7aabf19002376c583a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:34:54 +0000

    to treat birth cheek warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-print(sys.version)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
