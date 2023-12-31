c074181ae06d5a05fc3cbd20de0c0321ab2fc634
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:39:50 +0000

    understand voice Gg survive exactly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
