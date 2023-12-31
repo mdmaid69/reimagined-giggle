f096e0cecb10aabc98c2db0da94abfd0aa422600
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:48:54 +0000

    thank class turnon cum wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
