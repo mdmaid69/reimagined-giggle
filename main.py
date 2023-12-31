8ab07106b846645a4d29a408800e2359c2e4e71f
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:54:50 +0000

    place official contact bring 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
