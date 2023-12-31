e144baf1b877e4939f8afadd0d5fc5932a9f7da5
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:56:14 +0000

    upset turnover butter strike snow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def count_characters(sentence):
+        return len(sentence)
