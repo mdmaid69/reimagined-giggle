c6edf93999cecdb9be30d1f019d513cf7da11551
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:43:59 +0000

    join house region dress circumstance

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+print("Hello, world!")
