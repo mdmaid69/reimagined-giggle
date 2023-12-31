4e45dd10eef767a3d0cc8237dff60a22735e2d2a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:21:25 +0000

    fasten criticize regular fact allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+print(math.pi)
