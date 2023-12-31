3f5be3cc48731d6a0f9301667c4c182de65bc0e3
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:49:43 +0000

    switch Uu get nowhere funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
