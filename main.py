5ba2ba26011774d74bb4b7dbd263d389a48b5b97
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:25:10 +0000

    dead land dry breath comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
