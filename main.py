f825dc51635be23e6086eb1b9c836f2c7a5b1e4d
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:37:20 +0000

    fork takeout sight habit wind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def reverse_string(s):
-        return s[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
