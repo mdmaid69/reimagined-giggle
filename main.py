6bb72c36962d160f6842f07a0cbdc10b32f621af
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:04:56 +0000

    rely bring gas commit likely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
