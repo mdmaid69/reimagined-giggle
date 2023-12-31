35c0e3251c9ac77687db65b2bb8d4eed6955081a
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:13:40 +0000

    reach mind sore rich interrupt

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def cube_number(x):
-        return x**3
+for i in range(5):
+        print(i)
+def calculate_pressure(force, area):
+        return force / area
