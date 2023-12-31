9089a85c1a3d427e0a23d46cc105adb4e2cd2226
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:20:43 +0000

    store group little talkinto Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
