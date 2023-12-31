6434f0c65eea9c6127f1259d5b228948d82b176d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:03:36 +0000

    room turn phone brush fear

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import os
-print(os.getcwd())
-for i in range(5):
-        print(i)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_str(array):
+        return str(array)
