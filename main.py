cfc2d0472ca07b856e66a50f636473c29ca0cf14
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:22:57 +0000

    switch size OKorokay exchange belong

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
