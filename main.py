e06f21d227eb2957bb05ea36070771ed8f8bef3c
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:24:55 +0000

    good live keep positive weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_priority_queue():
+        return collections.deque()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
