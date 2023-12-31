492f4cb7b0ab7af425125645c406273b851d0220
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:33:28 +0000

    almost back least bored uncertain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def reverse_array(array):
+        array.reverse()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
