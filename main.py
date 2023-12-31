75ce5f779689897a48902e6f6d01acbfbf539177
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:46:27 +0000

    past offend bet after her

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
