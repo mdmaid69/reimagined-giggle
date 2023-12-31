0e0b7d7c539c8e71c82ebd759c4fe2986401a982
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:41:40 +0000

    change reach measure hit suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
