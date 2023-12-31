e5a4eb122f5ecca11aa4ea0771401f483efcf871
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:02:57 +0000

    desperate failure minimum drink sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
