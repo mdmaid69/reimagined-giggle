31e879b9addafe7f42d237b20e0d9b4cb309e0c0
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:59:42 +0000

    lock comeoff progress inquire keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def find_min(numbers):
-        return min(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
