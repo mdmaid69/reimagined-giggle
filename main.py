557f3142b941521796664f90658a76b1a00ce89d
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:04:26 +0000

    feed pen wander fly narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
