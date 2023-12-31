20381fa543d2546cc9652cd451327c5c6fcae8b8
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:58:30 +0000

    goon sure gooff guess reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
