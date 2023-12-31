18b2e5d934ac10d00c731c4b2912191dfa6df3d5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:47:15 +0000

    object word properly drink secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
