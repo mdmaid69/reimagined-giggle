f770000f44537727d16b195c4b22953c8f9f7423
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:12:36 +0000

    consider dry top since crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
