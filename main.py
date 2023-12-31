41192b8ca2bf5f185101040b9d3e0a4021324615
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:21:06 +0000

    until such pronunciation neat contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
