89c9d22c1d6179b1ceb3680a3c2e92ed3613ef12
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:42:53 +0000

    sound die claim plan lead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
