bfcbf9318fd2ae6900ffc9af6b67ae56d6c43e69
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:58:53 +0000

    pride piece possibly mark 571enough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
