568b6db7a050e7dacb21fa0555ef76709a459e87
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:50:41 +0000

    where square record slice opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
