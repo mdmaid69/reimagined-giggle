e67aa5aaa7a84bed53182541acd5802f736694ed
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:59:47 +0000

    price worst patience bringdown pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
