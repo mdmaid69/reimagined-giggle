1388bcd734ccbdfa48b0ab30a57eba0ba104cefd
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:35:11 +0000

    taste should mind difficult only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
