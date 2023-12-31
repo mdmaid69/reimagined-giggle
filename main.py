cc3b3bb570e0389b7493bf288f70a03195c86c4c
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:01:09 +0000

    nothing bring underneath block suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_max(numbers):
+        return max(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
