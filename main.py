e8300f87a49403246658a11ec27764632098679e
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:04:42 +0000

    tune popular callat yet deserve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
