05e63ac2714eb7f85b8642561bba2f54465f3942
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:32:09 +0000

    telephone stamp forever professional taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-print(os.getcwd())
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
