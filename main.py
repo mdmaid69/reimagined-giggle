60d9413a6b51079582d776c17dcb88c4cc9380d9
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:19:55 +0000

    opportunity cut punish talkinto comeoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
