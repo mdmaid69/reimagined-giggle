a59241a73167ebd39298fc27b041069544044613
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:32:08 +0000

    fix pressure drive react know

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
