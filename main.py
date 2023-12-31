718f58ee55a43d9bc7bf805b32000bbd64d81c5a
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:47:25 +0000

    be goodbye ask pot rely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-  def is_even(n):
-        return n % 2 == 0
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
