68cf43f49edc8211609e15e5c36563e0c77474f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:57:59 +0000

    sweet lend combination side none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
