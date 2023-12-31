fc357f9e491423b7fbe56056597a780113b1d847
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:42:54 +0000

    emotion turnoff cause deaf switch

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
