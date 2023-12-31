69029556882aab18d5e5d676d0aaca2a07be4989
mdmaid69 <robot@alexsyw.me>
2023-12-31 18:45:46 +0000

    emphasize direction building object afternoon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sys
+def exit_program():
+        sys.exit()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
