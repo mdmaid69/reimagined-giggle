ae1defd3b96a986c55ab2d3773bc49b8cd19cbf8
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:38:16 +0000

    family threaten block step subject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
