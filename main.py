847fb82abcaf904c0d32a8db63e5a84aefb2d615
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:41:13 +0000

    bus label what pump blue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def is_even(n):
+        return n % 2 == 0
