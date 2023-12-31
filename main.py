6089b2af312844fe893868a08c06cf39006d28fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:46:25 +0000

    even shock official split rich

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
