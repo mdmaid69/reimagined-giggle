1aee30d9a13ecdbffaca7b838cafb09edf2f476f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:48:34 +0000

    key chance appearance niggaacademy Kk

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
