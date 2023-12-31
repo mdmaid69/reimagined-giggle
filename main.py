2408a78e1db5e4b16a78657454de2fb47659684e
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:35:56 +0000

    future free right know sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-print(datetime.datetime.now())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def reverse_list(lst):
+        return lst[::-1]
