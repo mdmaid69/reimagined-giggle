c3d2749d6bd5d4ac2ddf396b6d4c162ad655f274
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:09:23 +0000

    figure move benefit drive though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+print(math.pi)
