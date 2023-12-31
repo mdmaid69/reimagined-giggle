14604ee6aa1effbe964c4bb7dd2d05c8e0e30f88
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:11:24 +0000

    correct within great there skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
