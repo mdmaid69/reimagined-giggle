c69b7bc54dfa0746eda01346b75d4072b55cf2b7
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:04:11 +0000

    then land nice punishment mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_max(numbers):
+        return max(numbers)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
