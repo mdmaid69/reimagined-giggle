ea2bbbaf373e528740049345ea568e4008a3744a
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:52:47 +0000

    medium obvious weigh turn rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
