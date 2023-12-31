fd9625ea7d861901032bbbfe9d432cccb98bebbc
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:20:48 +0000

    comfort Bb tape workout dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
