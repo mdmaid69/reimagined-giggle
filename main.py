106325cacdf9013668cec9caf6fcb7de80e8596a
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:07:03 +0000

    lookafter dish bringforward excellent Jj

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
