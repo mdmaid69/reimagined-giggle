46a1445e6bff6867a4acbb09bd550d4c99d3dccc
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:41:40 +0000

    runout deserve section opponent original

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
