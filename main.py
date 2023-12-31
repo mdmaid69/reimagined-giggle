f311cb4f82de7129d7e976819009add3a5350a86
mdmaid69 <robot@alexsyw.me>
2023-12-31 06:13:05 +0000

    fence unite estimate design free

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def greet(name):
+        print(f"Hello, {name}!")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
