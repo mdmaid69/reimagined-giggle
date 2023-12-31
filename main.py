7c01cbf4e490d4e07608d345979c3536ee8985ea
mdmaid69 <robot@alexsyw.me>
2023-12-31 09:29:20 +0000

    considering noisy geton keen needle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
