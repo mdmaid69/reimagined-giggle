ef850826f8ade17ad559485e269ba356f060708c
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:06:37 +0000

    at address want flat hour

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
