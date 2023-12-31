e50934ca2170934fff2f9c58660b418409c69b66
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:56:14 +0000

    reaction spoil 569enjoy secret myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import sys
+def print_python_version():
+        print(sys.version)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
