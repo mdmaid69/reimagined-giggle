c2c03cc4ac9db43d1093ebf141c43885246b6fc1
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:32:16 +0000

    whistle supply repeat dead test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+print(sum(range(10)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
