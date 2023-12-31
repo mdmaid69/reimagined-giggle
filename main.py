9e6ae3e94403aa5371c04609527db52d5daded9c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:21:02 +0000

    polite danger goon aim trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def split_path(path):
+        return os.path.split(path)
+def is_palindrome(s):
+        return s == s[::-1]
