07564ee770056fcca40d1f46e555e5cfd1a407fc
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:16:09 +0000

    real means reduce fly remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_date():
+        return datetime.date.today()
