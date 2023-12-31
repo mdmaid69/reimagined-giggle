9a7f042071af80a133e5aca2c0f9e13a702809a4
mdmaid69 <robot@alexsyw.me>
2023-12-31 11:14:40 +0000

    week repair cruelty band place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
