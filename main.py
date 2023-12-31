71961525c71ffd56ad29e449f88565f9b1705086
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:23:50 +0000

    probably unfortunate apparently unit explanation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
