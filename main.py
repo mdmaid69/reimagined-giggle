78888cc7f12e93ff4f5db4fc70baed6a2b6458b6
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:33:48 +0000

    division average include measure language

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
