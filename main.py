f279888e89ee4cbdc980c86a09d74c971be8ae35
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:30:50 +0000

    affect temper pity lookaround extra

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
