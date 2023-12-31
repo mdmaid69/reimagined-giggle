5f75938ece75a80d77cb7df6d59941a568929d43
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:59:57 +0000

    equal relief track unusual downstairs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
