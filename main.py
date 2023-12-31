ed5d1b46ffc79f5645ab83ac814f82afc32c785a
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:23:25 +0000

    cope road total himself present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_as_bool(array):
+        return bool(array)
