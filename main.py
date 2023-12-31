b81d9c7163d4de922bf75832c3cc0f3977cd6fe5
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:32 +0000

    else spin rest as fire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import collections
 def group_by(iterable, key_func):
         return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
