451040b543afff5598d2d0bea730f5bbc398c1f3
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:28:04 +0000

    rule weak match juice yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
