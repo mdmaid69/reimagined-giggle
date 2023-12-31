d64f4389f3bdd27f7cb157375e5461677bc8071c
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:51:55 +0000

    holdout image plan poor through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
