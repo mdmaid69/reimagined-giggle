fcef1018c4941edfaa9fd0be09337dede8b64e81
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:09:04 +0000

    software food emphasize experience human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-print(time.time())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
