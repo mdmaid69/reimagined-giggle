11e8d7d1315c65ce73faff21df92b48eb51cc58e
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:38:00 +0000

    shoot scale aware attempt set

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
