271ca00cd8383f2c3b03d40430beb094e74db80d
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:52:56 +0000

    square wash road software sad

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_user_dict():
+        return collections.UserDict()
