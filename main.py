b064f2aeb7f8c33077a32e54fcde9353ccc42c98
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:13:26 +0000

    history influence badly clear over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
