46b604732b07b3438cfa48a9f037822970dfbd12
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:15:27 +0000

    married pair suck seem ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
