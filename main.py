42b3871cba8383b8c2bb002a9f349dab7ff0232b
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:09:01 +0000

    aim stir pause content continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
