b74220a9d91a7a261a778437041d282a0e4329ff
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:35:11 +0000

    life limit agreement charge style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
