8644e50a54be469846a85a60bf7f7501379e1af5
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:54:25 +0000

    wind know holdon wash more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 import shutil
 def move_file(src, dst):
         shutil.move(src, dst)
