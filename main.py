2e97abc3804aff28153f22d5b4138cb4c750fdba
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:30:04 +0000

    occasion character rob runout number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
