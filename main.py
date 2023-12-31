0b11e90e7b4f7410d03bf0d3f25f7328a6e9f74d
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:32:46 +0000

    independent food she keepoff about

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def is_even(n):
+        return n % 2 == 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
