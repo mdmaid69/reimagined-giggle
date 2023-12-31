47b2fb8e65e2aeb7f336fd4c7c7eba124bf326d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:20:40 +0000

    value usually improve embarrassing related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
