c200673acbec7d207acf9c797caaf59297c7d33a
mdmaid69 <robot@alexsyw.me>
2023-12-31 12:31:11 +0000

    balance tune material through keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
