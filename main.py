0549b1d0e509d1cc0a93a307549a6f7e51120682
mdmaid69 <robot@alexsyw.me>
2023-12-31 07:09:41 +0000

    touch protection float deliberately within

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def cube_number(x):
+        return x**3
