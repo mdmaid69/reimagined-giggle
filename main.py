c04d52d7b1f70c84188f4c91b339c36c0c19d178
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:09 +0000

    escape warm fact send opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_power(work, time):
+        return work / time
