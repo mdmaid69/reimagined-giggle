7b9256cdf8f8ef3ff884fce85222bdaf0a20b6a2
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:33:05 +0000

    disturb Bb aware trade raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
