a9efdef60675fc93dd12ce0f7aedcf4d9085ed39
mdmaid69 <robot@alexsyw.me>
2023-12-31 17:13:48 +0000

    dead moment drive pronounce space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
