764c56c2789c66e04225ae1915b167fb3544d574
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:22:33 +0000

    receive if answer child approval

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def convert_to_json(data):
+        return json.dumps(data)
