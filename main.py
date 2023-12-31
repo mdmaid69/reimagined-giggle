37727b0ef9095b4db2009a3f0142792e1ab0a561
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:13:11 +0000

    rough round splash long hollow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
