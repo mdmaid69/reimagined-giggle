a86cb89455e33676f0e0077be96834802a09ea3f
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:34:29 +0000

    creature blind mainly cook joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
