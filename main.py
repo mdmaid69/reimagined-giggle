993e58298a3a8027665d5e48ebca5d080fb46433
mdmaid69 <robot@alexsyw.me>
2023-12-31 15:08:24 +0000

    note success excuse worst greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
