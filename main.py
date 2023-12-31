64f23471d7bb493751bde8e7abde129f0748c5e6
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:21:16 +0000

    giveaway nearly force top more

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def greet(name):
+        print(f"Hello, {name}!")
