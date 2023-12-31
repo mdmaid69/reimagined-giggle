5a8156455c2046b9d4d1df37f479cb2e8d175136
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:41:31 +0000

    practical variety second post present

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
