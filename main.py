2e75661306eec82cfec5a8018ff70c9ceedf7bac
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:23:01 +0000

    constantly buy criticize over dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
