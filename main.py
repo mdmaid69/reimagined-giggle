3245f9e66d980e3e4fbda647aaca9e7f78361362
mdmaid69 <robot@alexsyw.me>
2023-12-31 04:15:27 +0000

    again sound storm embarrassed nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
