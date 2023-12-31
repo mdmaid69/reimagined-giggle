04975ef85069b6d256647cf189ff99a749e3ae72
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:56:35 +0000

    protest suppose bet half performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import math
 def calculate_neper_number_to_power_x(x):
         return math.exp(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
