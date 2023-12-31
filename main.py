c9fbeabffee0c2b14ac2036e698c6708a4ac2440
mdmaid69 <robot@alexsyw.me>
2023-12-31 20:26:43 +0000

    hurry of this where temperature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
