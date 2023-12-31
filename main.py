1c9dbbc25130e617d167704474b3f1a0253d8157
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:13:27 +0000

    fall high sign circumstance comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
