d0aeba50d3a85cdefd1a72ae3e50de0bab506cd7
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:32:05 +0000

    letter common hate everyone classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bool(array):
+        return bool(array)
