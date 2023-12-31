123a9b9927e00fd4fa55ea53125c872addb81570
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:16:42 +0000

    than or disappointed mess tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
