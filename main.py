c6d70fcb298d71726d85ffd8771a93e9d226fa8c
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:23:56 +0000

    gap strange cut skirt tap

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
