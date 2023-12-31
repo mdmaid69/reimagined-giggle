c9dbc3d7d55efda9a3d340773bcb052d219b3f0c
mdmaid69 <robot@alexsyw.me>
2023-12-31 19:15:03 +0000

    exhibition thank ever hide chin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
