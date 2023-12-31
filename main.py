d0d52f7500ac1670783bc856d4f487d1dbc9a3c3
mdmaid69 <robot@alexsyw.me>
2023-12-31 01:37:34 +0000

    chief line lock communicate into

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-def print_python_version():
-        return sys.version
+import numpy as np
+print(np.array([1, 2, 3]))
+def add_numbers(x, y):
+        return x + y
