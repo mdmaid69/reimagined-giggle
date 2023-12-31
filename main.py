c52ef3a8aacc1bc69c39b3a0df7897dbdf2721d4
mdmaid69 <robot@alexsyw.me>
2023-12-31 13:22:42 +0000

    clean amount distant regularly congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
