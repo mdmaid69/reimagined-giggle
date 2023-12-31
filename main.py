9e3bbb193a8605f76b0de47f26b5ca486497bf69
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:47:28 +0000

    ball noone giveaway deal both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_min(lst):
+        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
