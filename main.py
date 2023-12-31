9f4aa8de4379416f1c539f248d06a2df30e66f74
mdmaid69 <robot@alexsyw.me>
2023-12-31 16:23:56 +0000

    support suit both break use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
