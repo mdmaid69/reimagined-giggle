b6c046a2ae7f80148189422de48c10d163e97c28
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:59:23 +0000

    club fine couple it regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
