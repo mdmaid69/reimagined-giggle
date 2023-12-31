f1076291720de0af5cdff1f05a5eb559c878b114
mdmaid69 <robot@alexsyw.me>
2023-12-31 23:07:15 +0000

    mistake port accordingto error excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
