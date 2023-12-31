68e886dde83fec469c155f147ed06ad1765a8c64
mdmaid69 <robot@alexsyw.me>
2023-12-31 03:29:19 +0000

    air curious disappear connection later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import os
+def remove_directory(path):
+        os.rmdir(path)
