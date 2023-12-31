561534763e93f8bd2b9319e72eb796a9fe482caa
mdmaid69 <robot@alexsyw.me>
2023-12-31 10:10:39 +0000

    maximum right comeback lose century

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
