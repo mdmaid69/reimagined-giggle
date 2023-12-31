06ee0df9ac66c722e42ae37b08204dfbe0e06eee
mdmaid69 <robot@alexsyw.me>
2023-12-31 05:08:19 +0000

    care brush long offence tax

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
