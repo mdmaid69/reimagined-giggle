b265736ea55ee380a5b8646636633039b34092c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 22:40:17 +0000

    cut regard production back while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
