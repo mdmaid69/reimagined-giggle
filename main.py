e3f13a8d7cf55cd9d0a6d736da507ebc9c0ef635
mdmaid69 <robot@alexsyw.me>
2023-12-31 00:32:44 +0000

    leg when pause right Ss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+print(math.pi)
