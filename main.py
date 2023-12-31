9b9b018dbcb117362ebca1116883e2bcd639c5f6
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:13 +0000

    city vision matter period black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tensorflow as tf
-print(tf.__version__)
+for i in range(5):
+        print(i)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
