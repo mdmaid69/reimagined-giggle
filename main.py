a3e73969c702ec7b03c533aee8d3e6cf4749e4da
mdmaid69 <robot@alexsyw.me>
2023-12-31 14:36:55 +0000

    566end it crash obvious shock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
