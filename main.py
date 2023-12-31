d4d020ac752dd5140589902ad9a0f5e48da6e618
mdmaid69 <robot@alexsyw.me>
2023-12-31 08:17:42 +0000

    unhappy trap exam band bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
