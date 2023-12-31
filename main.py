e977f63bc60249210f055bd89184e147f85c46c4
mdmaid69 <robot@alexsyw.me>
2023-12-31 02:20:29 +0000

    neither home friend hole quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(lst):
-        return min(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
