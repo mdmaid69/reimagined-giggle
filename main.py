1bb4f7e4fc945e26af360e944deb056b595b48ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:57:59 +0000

    practise stick ride place Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+print(datetime.datetime.now())
