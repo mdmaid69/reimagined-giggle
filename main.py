ccf1a4e50efdd4c056ea98a24ee398df9cc0e8a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:56:17 +0000

    passenger careless pass bury name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
