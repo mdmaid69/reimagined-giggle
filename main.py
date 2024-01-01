da256c148f5db54dd99241c1c693c72b9d626b61
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:20 +0000

    male organization crowd slow getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
