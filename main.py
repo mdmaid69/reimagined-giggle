08aaa39990748a886d6da99034e3a8bbc4367bc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:45:16 +0000

    anywayanyhow Ww less obtain belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
