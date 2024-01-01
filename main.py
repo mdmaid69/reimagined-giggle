89d74ab5137385dc076dd2c3f7fcb332ed0c72b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:36:11 +0000

    share beginning couple equal middle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
