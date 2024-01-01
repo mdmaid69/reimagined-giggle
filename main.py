385f6b10eb5a64b3d45879e5c3aae087544dfec9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:40 +0000

    friend sound left flow steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_itemsize(array):
+        return array.itemsize
