93513747cc148ee92ef426cce7110bca743f9dc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:36:25 +0000

    yourself carry declaration successful before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def calculate_area_circle(r):
+        return 3.14 * r**2
