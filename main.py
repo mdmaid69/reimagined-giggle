624cb46b7c2d0c4238aceb0e44b8ab3287255dbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:15:35 +0000

    drive ignore rise brush from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
