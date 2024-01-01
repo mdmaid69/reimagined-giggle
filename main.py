a2e17d696216a7e65b80939aeb695082926b1fd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:37:04 +0000

    stay by guard hospital claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
