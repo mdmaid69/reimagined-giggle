44bdb768fb7e169679eefc0a4b58fd2669c3d3d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:05:25 +0000

    peaceful long from neat offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
