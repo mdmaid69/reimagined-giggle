147a623f528ed6e6edcd1eaa32e66652f83de400
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:44:20 +0000

    failure Jj return depend lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
