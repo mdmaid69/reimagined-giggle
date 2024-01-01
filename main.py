a8035ad425427207610f10b772b3d4bb9436c254
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:19:39 +0000

    taste little one no worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
