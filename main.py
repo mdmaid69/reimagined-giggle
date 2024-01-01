615d21daa50ae6f7d882fa94eb1123c768ceec22
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:12:14 +0000

    fly lay new fly heart

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
