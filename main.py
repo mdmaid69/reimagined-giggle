332f1abd0e7b569cd8d3ac4a57dfbb0f48ee998c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:55:39 +0000

    anyone average equal here shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
