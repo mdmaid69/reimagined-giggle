6055f266df66d1661282a8e2ac041180e7b925a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:22 +0000

    guilty usually ahead include marry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_array_to_string(array):
+        return array.tostring()
