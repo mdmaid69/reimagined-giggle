177b649bfaa1e880774fc4442e82905edad6d028
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:37 +0000

    true earth measure he awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def remove_directory(path):
+        os.rmdir(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
