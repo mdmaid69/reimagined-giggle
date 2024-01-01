e2648b5f1797cbd0b0311cdb3488829e30fc62f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:06 +0000

    turninto put yours like walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def reverse_list(lst):
+        return lst[::-1]
