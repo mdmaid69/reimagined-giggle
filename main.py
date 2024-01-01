000326a6153777c19de2e38e22bd6cbd93ae4728
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:18:30 +0000

    amount exaggerate peaceful dry suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
