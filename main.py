5febb0ebe45c4f01384c27e95bc20ab6e4558c9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:47 +0000

    Pp bath plenty bet fly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def subtract_numbers(x, y):
-        return x - y
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
