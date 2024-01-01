d3aa0d0b8b17767c28a01040bbb5707193f568db
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:18:21 +0000

    mine joke front stamp identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
