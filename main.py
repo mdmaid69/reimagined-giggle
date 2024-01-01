72d15192a1fd92e7692ec0e0bf44097eb7497754
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:06 +0000

    complicated base another breakin government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
