5aaa8bbe30c0a55b8ddd32ffbf8cf598d3b2c1b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:18:40 +0000

    birth any star average clue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def get_array_as_list(array):
+        return list(array)
