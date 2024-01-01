26490acdd457ea13a00e3fcfdf6b7f332583ade6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:40:28 +0000

    food game open release everybody

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
