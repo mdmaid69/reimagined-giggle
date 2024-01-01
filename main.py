77091bce213b3c74a3dd0b61610f80532f0c4d91
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:11:48 +0000

    distance neither giveout win size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def sort_list(lst):
+        return sorted(lst)
