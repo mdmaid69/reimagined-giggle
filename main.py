d1d15920fc90fe7d08c913b495df2b1d150ed813
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:37:43 +0000

    treat ordinary positive but at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
