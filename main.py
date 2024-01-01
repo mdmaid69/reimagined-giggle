8807e578e4e1162cc7016db870ddfdf7678b1be2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:56 +0000

    turn rent on edge bit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
