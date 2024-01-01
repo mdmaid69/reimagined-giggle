eb5a3b70c93855cc1f2984a09eca028049f3077e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:01 +0000

    worst makeup firm experiment sweep

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
