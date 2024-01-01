ab422ab9269f5f5bc9023327e33d92304c80d89f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:08 +0000

    bite shelter bag regard exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
