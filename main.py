74e1b761bfe4a02ebdb7997b2080263b456a92b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:20:26 +0000

    duty hard your steer in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
