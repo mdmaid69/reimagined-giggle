3ba982b03e193999e58c0504efc8741f3e8796df
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:22 +0000

    interrupt stone first than name

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
