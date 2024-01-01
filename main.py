52b25e3c26ca2d88d0e3a4b91bfed989749f8d2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:42 +0000

    peaceful circle clever general collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
