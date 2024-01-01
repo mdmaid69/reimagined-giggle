bba4bae68c277d4762b9d4deae72ae30d8b55679
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:34:04 +0000

    wrong geton however land collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+x = 10
+y = 20
+print("Sum:", x + y)
