0db37ea274c8725dbbcda10ff52423f5855d3eb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:40 +0000

    centre mention quit cool bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
