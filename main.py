f65b606710a0409933459c025ab03566d99421d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:25 +0000

    police crisis main getup question

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
