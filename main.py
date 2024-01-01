0bec04a5ca1cc612e3de4e8c17c6ca09cfcab165
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:59 +0000

    fork ready change trap roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
