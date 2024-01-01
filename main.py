9d7d1481497010eac49834d4a573b38fef8513ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:29:21 +0000

    juice every confidence sharp central

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
