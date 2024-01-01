df5b0acb1d166391b8b60bc424cf35339c1a77d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:45:52 +0000

    own criticism scared quick pollution

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+  def remove_duplicates(lst):
+        return list(set(lst))
