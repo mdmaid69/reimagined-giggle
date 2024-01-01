8411b26394b55522a395f17e97f95d2730188f7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:11:17 +0000

    sign muscle flow defend actually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
