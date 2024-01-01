d8e823e4d1fc786f3fb1c4170df49b43fdd03551
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:55:58 +0000

    separate possibility act ill provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
