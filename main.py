fbe6726050b33abfb20f80c2bdde804cacf7b5a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:31 +0000

    dull get which Gg reject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
