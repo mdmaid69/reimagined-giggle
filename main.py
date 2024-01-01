e224b8e2ed82ebd0e5b138adb6b5b1e0f846a7fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:25 +0000

    create slide fact information neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
