3f78795d00434991d39822bb56ec3e73535c0ce8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:57:36 +0000

    once keepoff combination sail have

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
