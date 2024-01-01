c5efcffc007c254c5baafbef9f61284d273fb152
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:33:33 +0000

    opponent rough fall steal give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
