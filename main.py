c922b7f18fe9fca3c3819ca88a0fc0f23f6e8990
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:24 +0000

    pretty judge why scrape tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
