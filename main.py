475c84f1fb4c1ea64f37f8bfb089d585c4993acd
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:35:35 +0000

    bright thorough safety string couple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
