e182a1145a530b652bf1613c9f74e688ce738f9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:38 +0000

    left finish will by present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
