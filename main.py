d890a81b8d770d238c8c417fa689554c373c5383
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:15:01 +0000

    confused than neither guide male

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def is_odd(n):
+        return n % 2 != 0
