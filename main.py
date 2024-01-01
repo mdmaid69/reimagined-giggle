16e90aca59b3dea1ca53cab6839404f49411677a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:35 +0000

    whistle possession listenfor point plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
