c335ffbdbe219428329ecd92879dc6285d5cd411
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:53 +0000

    risk sure smoke usually black

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def is_even(n):
-        return n % 2 == 0
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
