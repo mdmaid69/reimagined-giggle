e3b9b63288ba90a2d5e55838b0f49ce2dccaf34b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:35:54 +0000

    debt distance lot trust us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
