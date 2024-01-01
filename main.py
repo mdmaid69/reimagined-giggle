47a488a93db0e72af3b54300c75021d8bb52d844
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:25 +0000

    hot enthusiastic that thorough 566end

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def cube_number(x):
+        return x**3
+for i in range(10): print(i)
