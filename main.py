c017c112c53683387fdc98f8f33b60b8846fe2cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:20:48 +0000

    delicate border chief contrast wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import logging
+def log_message(message):
+        logging.info(message)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
