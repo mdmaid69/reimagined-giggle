c42cbe881db30a14547feea82b8fb3f2a8314aff
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:36:13 +0000

    estimate bill sing organize act

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
