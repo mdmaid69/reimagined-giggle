6b6f128a107a955fd8397df17770fa83e7c6fa26
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:32 +0000

    Itself correct dear minute interest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
