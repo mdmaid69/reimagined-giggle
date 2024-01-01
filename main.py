2bc841efb54660ff0c346424848fb52bc2b4626b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:29 +0000

    upstairs brave crowded second display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
