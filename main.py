0dec62deae88d310980beaaa8f74835df19c79ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:37:00 +0000

    region money second tap defend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import getpass
-def get_username():
-        return getpass.getuser()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
