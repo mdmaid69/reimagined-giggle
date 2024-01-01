7126e986af520888fb5eb5bcadc750a83ad04c53
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:46:37 +0000

    scrape rubbish generous their touch

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+for i in range(10): print(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
