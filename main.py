d1c885d6e5fbce7b86a33f159bf9120b665fa22c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:19:59 +0000

    card put write free appear

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
