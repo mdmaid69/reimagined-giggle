6e1b9a754d56c17221e540a105093dec96453987
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:13:19 +0000

    constantly problem rain further Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
