f2d62681c73e1fc48a738b8694d64a20bd8766c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:07:35 +0000

    him this firm goon bye

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
