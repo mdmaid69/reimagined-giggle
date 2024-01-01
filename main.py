98e556cfc2e579f4993c45b926434e497e21175e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:42 +0000

    fork excuse produce not characteristic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_perpetuity(payment, rate):
+        return payment / rate
