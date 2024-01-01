f136f792a092369943ed34d2690ba5743e28c475
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:23 +0000

    rush advantage drink everything last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+print(sum(range(10)))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
