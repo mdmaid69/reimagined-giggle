351195e4cd6b88883dc39cfc896bfabb5cc0d906
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:13:14 +0000

    lesson fall love coat win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
