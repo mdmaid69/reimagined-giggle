0a31d4f91ce19477d75cce66283d2034319a7978
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:59 +0000

    grow build true model cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
