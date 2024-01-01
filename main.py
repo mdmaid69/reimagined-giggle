c109b4f4f4e7ecc53328a805304111587c585116
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:40:21 +0000

    part stamp trousers amount yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
