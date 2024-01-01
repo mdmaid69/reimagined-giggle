4bf11ddbfb8ce4335aa5a2b7b3a635ccba054fc8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:10 +0000

    Zz lookaround trial community sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
