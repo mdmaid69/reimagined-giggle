4b0830ccd46381ed80d6708340e61ae177e389a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:35 +0000

    record dish neat comein hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
