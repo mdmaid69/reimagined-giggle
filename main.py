c12e1dd9adcdbf721fd5065a1a2f51d4ae39504e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:43 +0000

    television know begin interrupt open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
