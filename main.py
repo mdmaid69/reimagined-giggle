7dd167d272df80a54d4bc7c8bdc194ef4bb33361
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:38:32 +0000

    break path Ff bright burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def is_even(n):
+        return n % 2 == 0
