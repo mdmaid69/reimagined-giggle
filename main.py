89fdc4a9ddd31015b6972c3330a580aeb05eb9c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:49:24 +0000

    law stare content phone tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
