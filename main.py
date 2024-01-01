a1aaf3b2cca413e8cf885121ee15e4832f242787
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:20 +0000

    law exchange front steam cheat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_int(array):
+        return int(array[0])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
