e1445ede6853eb56212a3178e2b34fed634a18d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:56 +0000

    skirt murder weekend interesting compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
