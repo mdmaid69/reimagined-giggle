c2ef9876541e4653d447b146b6b87f79de7e3719
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:36:08 +0000

    centre takeback dare impressive promise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def count_elements(lst):
-        return len(lst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
