48602e2f4075e58394f7f4ce460596a5d71e513e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:45 +0000

    talkoutof division stamp fit content

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
