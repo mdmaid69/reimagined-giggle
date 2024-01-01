c042b4f956e980ffc66b4d9d34e875acbbdc234b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:57:25 +0000

    where stay when worth accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
