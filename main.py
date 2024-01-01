d4e63263a825ad2ad341d2c6a5f4fc48c4fbc1c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:46:05 +0000

    grade term dress send at

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
