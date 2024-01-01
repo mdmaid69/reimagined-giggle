935500157c8d2b2dd901c9f66599a6bdc8d6791b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:04 +0000

    motion reliable equal tap dark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
