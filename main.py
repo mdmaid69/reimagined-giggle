10b4f5dab23cce044a5ee3a25572518e0cd7e66a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:44:00 +0000

    570enjoyable declaration difference favourite principle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sine(x):
+        return math.sin(x)
