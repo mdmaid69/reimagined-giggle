118ab546b1eb5b68ca52ee3f9f7a4c1e5b9449c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:02:09 +0000

    cinema related decide whenever direction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
