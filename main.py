740444b9f2d4d62d421b3004ea43486593a6759d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:04 +0000

    today afford shape tax pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
