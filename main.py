678a53b2b130fb045685319777e7e899bff2f538
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:34 +0000

    shoot Cc clever weak quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
