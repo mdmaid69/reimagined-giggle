0894b7f67a46721c7daa88cdda2e3b22a30d868b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:11:14 +0000

    comeoff food mind belong friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sys
+def print_python_version():
+        return sys.version
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
