754e6ce49b93e220320331c8a98c3718f5316927
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:49:25 +0000

    professional find breakinto crime Oo

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
