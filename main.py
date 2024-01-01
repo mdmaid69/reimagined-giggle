68de08a63b8bfff5749d4220ba7acddf8264fb71
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:14:52 +0000

    beginning dance sometimes bus cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
