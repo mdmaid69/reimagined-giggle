2aa4ba3bc9f034bbaa7e87c8dc2c0802d62090b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:16:44 +0000

    young match build eager pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(lst):
-        return max(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
