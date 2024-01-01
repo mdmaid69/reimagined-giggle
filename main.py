33ce0c679a585b3505b91b1e5a4d05cacdee8d46
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:48 +0000

    sad visit stop detail the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
