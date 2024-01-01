1fd5f8cf6498579781726907accff20ec1036093
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:11:41 +0000

    spend answer pressure ring ball

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
