44423853c4e975f882d096b279df30f4954205c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:04:31 +0000

    cool into long pour match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-print(os.getcwd())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
