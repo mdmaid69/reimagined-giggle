9e1d036391ad77f9f0c1473891bed6c6c07a1e37
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:42:48 +0000

    brief pronunciation proud like loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
