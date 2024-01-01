aa8065ea213af84082e575e03a66b7b4c96414b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:36 +0000

    wrap 573entertainment occasion friendly bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
