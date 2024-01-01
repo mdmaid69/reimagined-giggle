696f1696240dbc98766b4020ee11805248d01ae4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:59:41 +0000

    small roll offend opposite emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
