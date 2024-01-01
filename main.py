fa70cdcb1104e74271d4db248b659d61d7bb4dfc
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:19:17 +0000

    mine round refuse minute talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
