7a7e87aa53d2939f3dcbae9b5e97282e59323018
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:19 +0000

    ever destroy glad rubbish conduct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
