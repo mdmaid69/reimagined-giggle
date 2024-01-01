730b70eb77e2886564f521187ed6f91438998db3
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:48 +0000

    sick talkback attempt method live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def sort_list(lst):
+        return sorted(lst)
