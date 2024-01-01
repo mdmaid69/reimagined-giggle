3b627a6538100f1baa628bf22017ae08caddeaf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:01 +0000

    blind crime lesson pair similar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
