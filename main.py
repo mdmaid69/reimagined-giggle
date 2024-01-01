6326e117d98762fba47e157a39bc76c086e4971e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:44 +0000

    harm early quiet urgent keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
