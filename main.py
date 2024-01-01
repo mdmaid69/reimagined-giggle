40ca109fe948775e8694c0893365cf6e4cc0c67f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:28 +0000

    subject near where heavy badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_item(array, i):
+        return array[i]
