f0bfd4d4f754a66a8ee77bccf86532e8355cd921
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:34:42 +0000

    allow tread pipe trip lookup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
