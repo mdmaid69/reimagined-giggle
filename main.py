e6e611d10e701c8ecb1ab83a27977cb5a0231916
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:54:12 +0000

    afterwards hang spoil favourite cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def subtract_numbers(x, y):
+        return x - y
