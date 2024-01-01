08739cc6f4f81633996b247e330d1833a65bf79e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:13 +0000

    include to along thorough back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def count_elements(lst):
+        return len(lst)
