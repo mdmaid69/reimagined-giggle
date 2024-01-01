9582ff9a6c45df2b63c73e884333c2aa54e7f915
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:22 +0000

    blind correction hungry 568energy snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
