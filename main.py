a9767321934d8f999a6e4b1e968b04785e214bec
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:58:36 +0000

    objection importance probably tap distant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
