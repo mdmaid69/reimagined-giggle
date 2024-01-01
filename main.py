4bd080ed028e4b70fd69fba2ce8e12e466a628b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:28:37 +0000

    beside record quantity emotion lunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
