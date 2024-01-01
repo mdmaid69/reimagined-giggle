a17f25d34da046154d502f620a780f4e9af38320
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:02:46 +0000

    old lose hair interview connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
