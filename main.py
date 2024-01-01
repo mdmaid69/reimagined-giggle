396e45a7cbf1d3fb798d119846d050b9995503b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:44 +0000

    unkind thirsty common nonsense peaceful

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-print(sum(range(10)))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
