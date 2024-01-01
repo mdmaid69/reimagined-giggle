e2a7b52cb3c2fabe3029f9497d04b5d54a071a7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:56 +0000

    defeat attempt promise equal common

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-print(math.pi)
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
