3f674f30980fe4bc44f56970cd3f5756fc493a14
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:37:14 +0000

    best date brief winter rub

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
