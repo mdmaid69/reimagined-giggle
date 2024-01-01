adc2728239ae3e12eb4f5e56c1cec07746a7f9be
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:20:04 +0000

    perfect unlikely bag treat this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
