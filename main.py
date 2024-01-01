0db7c6e1d291a22f38e7bdf4358541edab0f7b81
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:32 +0000

    Kk comfort record conduct see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_min(numbers):
+        return min(numbers)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
