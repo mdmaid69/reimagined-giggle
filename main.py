a903bf1eed88ffcca0f89ad5e337e6228e6278da
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:35:48 +0000

    receive immediate hold turnback big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_bool(array):
+        return bool(array)
