13bda4527eff5efea8dc4b05c271052b0987c111
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:19 +0000

    instrument after invitation inform skin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+import tensorflow as tf
+print(tf.__version__)
