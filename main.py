6f4d8bba89cb5db61b771ad18ecbf93045f8b0fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:41:29 +0000

    aim swallow lunch best tooth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
