a34da4da3071a9d2c731e7368114b77a526aacd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:58:57 +0000

    range sensitive post beneath pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
