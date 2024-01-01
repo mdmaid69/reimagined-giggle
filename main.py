f94fe91e656d438deb888dcc18d4878c37a538ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:01:31 +0000

    question tax workout insist build

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+print("Hello, world!")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
