a97faead019485651c59210a76f3bd1f40827a0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:14:32 +0000

    lock health price relative direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
