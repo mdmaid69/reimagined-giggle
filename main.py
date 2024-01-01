c65bcef1e81618b90cca05e19d4fe901650c4265
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:13 +0000

    difference summer value switch announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
