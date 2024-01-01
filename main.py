4d6ca4a753bb2f9137356bcf8e1dc8946403f8e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:34 +0000

    several phone terrible drink stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
