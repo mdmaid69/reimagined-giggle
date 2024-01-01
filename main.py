0145a0b8ff0b2a8ba5ed2f576f8d659ddf266809
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:50:24 +0000

    pain busy nurse beginning start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
