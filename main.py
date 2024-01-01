56bff0d3fb1d9e7086c6359384f6b63eed0a4881
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:12 +0000

    emotion about contain above getout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
