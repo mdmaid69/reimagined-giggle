790dad7634900d7574ac95444b2896f2cf6c012e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:59 +0000

    talk damp so illness extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
