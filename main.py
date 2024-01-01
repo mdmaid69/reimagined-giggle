d2d33afecb0bd2cdfcba36cc45e0806925cd200b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:27 +0000

    exploration sign important peaceful marry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+name = "Python"
+print("Hello,", name)
