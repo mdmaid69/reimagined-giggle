ef430f5b2ee005d4ced210679e4034a2b3e332f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:10:13 +0000

    voice please bush flight fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
