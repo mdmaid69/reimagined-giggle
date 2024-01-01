42de4434860d98f10af517cd42d69af10a31ade2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:59 +0000

    cry probably admire shape height

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
