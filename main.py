e1a1eb6bbffa6e22d165e3d95e4224e89311bb4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:48:33 +0000

    tend join pin separate shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
