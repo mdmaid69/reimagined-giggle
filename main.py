2486a1e3c2124fb76629f92ab80d56c498633e7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:58:36 +0000

    window admit wind message confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
