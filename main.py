cad210801055289e2ce3163f710d759ee0ca9194
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:05:12 +0000

    group slice passage Vv name

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_list(array):
-        return list(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import random
+def roll_die():
+        return random.randint(1, 6)
