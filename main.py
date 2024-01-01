d1b59266881c3fb14eb3c616c1e31e2f0c492545
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:40:35 +0000

    exploration turn luck understand match

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
