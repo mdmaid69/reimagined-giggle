3d2f53696b9e91c328c607a849c2ef315db33b82
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:24 +0000

    them programme drive imagination than

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import platform
+def get_python_version():
+        return platform.python_version()
