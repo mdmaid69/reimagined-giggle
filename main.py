3e9bf79cbcad3a853d821788be73ab3411f6e1cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:28:35 +0000

    under tight pressure actual card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
