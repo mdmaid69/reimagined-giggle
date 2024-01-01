6a8135440ad2023a7f69c7389734efb050445c0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:24:12 +0000

    power married pay pure gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
