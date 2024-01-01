9f06c54473c01953914f4ee767935308d98844a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:39 +0000

    cope show vanish sting normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-print(math.pi)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import random
+print(random.randint(0, 100))
