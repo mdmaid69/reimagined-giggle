e5d43de52757399b582e58b2c7bf5b798035d519
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:53:04 +0000

    either that university bite punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
