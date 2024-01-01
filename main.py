6a9fa35c007f025f57b546ee4fae35799a98eded
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:44:03 +0000

    beneath neat fetch past since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sys
+print(sys.version)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
