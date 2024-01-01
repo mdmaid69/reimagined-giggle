fd3d27c5caeb6d24d76b0ff7e87b4f8fcaa61586
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:18 +0000

    that score lunch strike mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
