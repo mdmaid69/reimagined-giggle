c2b0eb03ff5946afc81bb7659e2345d876316f1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:11 +0000

    569enjoy getup true replace indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
