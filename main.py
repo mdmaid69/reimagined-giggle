3bf40eb1648f0c6c94e2534383e18bf02951dc09
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:45:19 +0000

    purpose steam sport if argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_max(numbers):
+        return max(numbers)
