4f9e797834dbeb3d4c28fba93f4c15b30808e5e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:08 +0000

    standard cruel improvement tip people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
