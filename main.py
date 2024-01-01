108204766fd07859e9254c7753f8221e4d88421b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:52:17 +0000

    control agreement towards bet fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
