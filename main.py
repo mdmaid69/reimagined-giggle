4c7a80840d4e3b484dd3206fb22d5c59d5439dba
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:01 +0000

    advise discover sport communication bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
