851a91de8a6f2bbeacd82d1cbb217e332bacfd93
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:04 +0000

    announce scheme sweet listenfor dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
