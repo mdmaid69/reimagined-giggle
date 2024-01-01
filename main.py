43c54d029137cf8ee434c0e327d153830945939b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:19 +0000

    type disagree hold everybody deliberate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+print([x**2 for x in range(10)])
