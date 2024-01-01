371ca0d9d2b95ff4564a5dc4524ec9a88ab97a87
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:22 +0000

    building advise callback turninto either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import collections
+def create_user_list():
+        return collections.UserList()
