d899682dc2dc619d23f021f3d4df0d6fbc663f20
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:34 +0000

    hungry thin task murder want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
