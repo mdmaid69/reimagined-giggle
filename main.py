9dbb1ba42b3ad73a23e4e64c569801199220aca4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:18:10 +0000

    approval join petrol tend light

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
