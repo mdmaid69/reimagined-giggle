fa4f191a3a4244fc60d0b8bafe9ae869a0ae2144
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:36 +0000

    plastic partner usual pass crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
