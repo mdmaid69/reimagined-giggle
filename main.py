abe2abe7ab6e7d2dc23748eef54153cbc0e333ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:44:18 +0000

    convenient about delight damp board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
