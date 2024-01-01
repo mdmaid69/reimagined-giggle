21e7a2d0b02e41dacab383bf498efe070820f498
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:50:10 +0000

    friend last exit standard nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
