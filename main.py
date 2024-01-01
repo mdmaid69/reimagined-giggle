74ec190402dd5bb9b45640e596ebe8f9fd409f59
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:49 +0000

    sight second noone choose makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
