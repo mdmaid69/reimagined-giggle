52e14871ab15a37783d952b57edc5f509fd9d8fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:54 +0000

    false grow politics unfortunate nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
