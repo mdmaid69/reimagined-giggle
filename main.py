fb2c10dfb41d82bb6bfc855f57c47dd36b25b4f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:53 +0000

    collect dream thing comeover holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
