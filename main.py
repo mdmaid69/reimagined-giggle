ce5f271c94519eb523b7e451155ce93d3759a71c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:39:49 +0000

    love no poor clever shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+print([x**2 for x in range(10)])
