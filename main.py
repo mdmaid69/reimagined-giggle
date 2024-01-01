70f5cfef959b0b16d4953618b3b8a910ef6b7f6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:11:04 +0000

    life wake distant thinkof present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
