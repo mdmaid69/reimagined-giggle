00baa15e1cd29e576484f65103f9bff4b1b70eea
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:19 +0000

    load string tip round if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
