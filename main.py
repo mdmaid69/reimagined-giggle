06f8b8aad665324d594b8ba16b2ee80d914856b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:51 +0000

    space next mixture indeed grand

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
