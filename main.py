7234878b360d7388ddbdfdf1b4e88666613b16f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:33:15 +0000

    check base he argue settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
