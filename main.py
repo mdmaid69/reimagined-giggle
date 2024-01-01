b0aa4b79124871db7b90f7c5db74ac851c38c16c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:37 +0000

    lookaround standard broken call congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
