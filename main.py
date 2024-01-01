fb31d2bcf0e612edee9a84ea5466635ed3fc9dba
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:09:57 +0000

    warning form slow calloff tend

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def is_palindrome(s):
+        return s == s[::-1]
