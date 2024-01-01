dd2964577f2ea710fad5d8d07652e6389d6d569b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:08:58 +0000

    feed exaggerate aim praise treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
