98ad0eea8fc6162643873fd66fbb11f5ef3484fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:48 +0000

    clean continue and unite ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
