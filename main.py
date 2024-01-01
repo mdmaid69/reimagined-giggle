3c8bed15d4dfb7e0b3373342a6b5178a58050b62
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:54 +0000

    sensitive whenever school positive spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
