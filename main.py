03c5eeab257c4aac8498c4015c56d2c2cb00e6d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:31:52 +0000

    thirsty development influence return hair

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_floor(x):
+        return math.floor(x)
