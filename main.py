f267a3d7c8ef6b7ec370a19059443b9351ef11d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:55:31 +0000

    able scare comment interrupt manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-print(math.pi)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
