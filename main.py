3e81966c7657ae47ae5a90bc7addac80e7235920
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:41:18 +0000

    black judge us this people

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sys
+def exit_program():
+        sys.exit()
+import os
+def remove_directory(path):
+        os.rmdir(path)
