c73c1bb8c96294c448b800646c6cafa7a77b813a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:35 +0000

    reply increase picture start imagine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
