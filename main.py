3ab7c18c9671d8c21307047e262778886fd0c4c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:43:24 +0000

    punishment grade loose forget delight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
