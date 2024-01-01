59aa38eb73ac176960e505847ce3cb7eeb61160a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:37 +0000

    minimum suddenly increase answer still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
