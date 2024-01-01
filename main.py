be7e4c322e6f046c427ab3ceb6a18621da65d314
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:34:20 +0000

    pattern Nn fast cool software

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
