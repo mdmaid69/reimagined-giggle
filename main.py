fb2e4890e5eac48613c16472a850558eb6b07be3
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:00:56 +0000

    callout minute fat disappear world

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
