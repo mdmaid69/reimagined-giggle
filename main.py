cdd8525f69af8e5b13dbf04510b5ccc40a1f619f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:10:44 +0000

    pollution this clean accidental square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
