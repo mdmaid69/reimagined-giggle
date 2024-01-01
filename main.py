343463909f22280ec4b4c18ca32ec857d8b1c7fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:53:38 +0000

    mood pair listen mass consider

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def print_python_version():
+        return sys.version
