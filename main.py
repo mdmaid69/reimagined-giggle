b470420bb1cd124dea1b4aa52a82ddfd2aedef1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:31 +0000

    adjective anywhere further please clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def is_odd(n):
+        return n % 2 != 0
