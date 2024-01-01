b525ab7ca4f68b853667be925254f749e7d0a9bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:02:41 +0000

    deliberate demand lonely slowly determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
