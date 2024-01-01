6f5ce9877c672a98c27852b85cac737019413eb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:58 +0000

    bite into Cc whether anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
