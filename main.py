fdbfa35dcb354575093e754ac6e0630861e296f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:52:41 +0000

    phone firm thankyou tell sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
