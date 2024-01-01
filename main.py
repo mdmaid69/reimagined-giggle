b73f21aabfaa1f6f842d62e215cb6dcd9064a36b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:04:40 +0000

    like waste goodbye cry anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from collections import Counter
-print(Counter("hello world"))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
