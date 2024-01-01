7aae613f3f574931460e36b2fa3c4f06f2043ee2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:23 +0000

    wide none congratulate present bag

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
