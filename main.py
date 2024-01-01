a981380aacaf8652befe9e40ef811cc113a337e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:16 +0000

    none hurt answer during careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
