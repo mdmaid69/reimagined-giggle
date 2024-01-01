21cd160a8c64e95f85432337755aff426f3c1b97
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:42 +0000

    uniform nonsense profit Kk shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
