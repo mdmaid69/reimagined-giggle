1417bedce2a0222c53042a1f06f6ce8c2a7c56fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:55:30 +0000

    permission either nerve good unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
