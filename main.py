aef8e9a8a27dff64d059db5987210a9bef30ca89
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:50:29 +0000

    threaten impressive spray twist basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
