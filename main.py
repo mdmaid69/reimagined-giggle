94b147fe0611e0df50637c7da86fcbae9c606fa2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:27 +0000

    sort raw perhaps active scared

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
