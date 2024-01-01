8708210ed9678a4c34da81e3e423c54168676618
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:51 +0000

    couple branch develop die breakout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
