51b13959553bba69419b9d32bfa3d9a9a74e122a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:16:09 +0000

    heat recently laugh attention half

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def convert_to_binary(n):
-        return bin(n)
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
