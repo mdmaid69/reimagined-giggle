47ccc2eab28cfaa3397e177e9319afe55d3b3cca
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:20 +0000

    side holiday no include modern

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
