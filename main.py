0be32fa829f0ff4e41cf5af9901272a71f5ad030
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:23 +0000

    introduction second story although butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
