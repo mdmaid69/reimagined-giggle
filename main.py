81d5375f88aac44a9d432b6be9629c647d97e46e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:13:11 +0000

    tidy later meaning strength responsibility

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import os
-print(os.getcwd())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+text = "Hello, world!"
+print("Uppercase:", text.upper())
