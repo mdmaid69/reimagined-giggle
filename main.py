c481690a890f70c2b8ccc686649560002560d7a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:13:09 +0000

    size pool thinkthrough thankyou wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def split_path(path):
+        return os.path.split(path)
