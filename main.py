bec3d78d9d10380e317a32aedf57e891d092f339
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:25:14 +0000

    transparent ocean funny upstairs tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
