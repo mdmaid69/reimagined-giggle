160a0f81a41ce12545c4f093783004e82727a926
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:38:55 +0000

    strike under again successful on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+for i in range(5):
+        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
