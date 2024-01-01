7fb545769ed4b68b064dc84c57a6e61810060347
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:16 +0000

    stick react detail familiar vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
