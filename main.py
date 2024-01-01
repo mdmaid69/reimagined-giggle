0f82bffead5e0490af86a40dfe8b3a709592fcf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:28 +0000

    sort getup sink comeout smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
