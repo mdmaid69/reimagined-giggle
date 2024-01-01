0ec995f21e97157b853ad4e98d165312b57ccf63
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:49:10 +0000

    at frighten present seat of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
 def remove_from_array(array, item):
         array.remove(item)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
