9700ca756efe95a8a3fb1b635f2fe2deb675cf30
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:48:23 +0000

    knowledge shop noisy miss test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
