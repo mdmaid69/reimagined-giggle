9ebe59a4a7be27b217f2b6edd7ccf3213c5108d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:48:10 +0000

    trust often closely contact take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
