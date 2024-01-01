b4ed1f56b468f473fc1503b495f0a6e713321a2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:40 +0000

    chin himself low live frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_max(numbers):
-        return max(numbers)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_area_circle(r):
+        return 3.14 * r**2
