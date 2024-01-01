3aa354e1544014469cfcb6b033d07d2f816f04a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:34:40 +0000

    bringback certain support suckme enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
