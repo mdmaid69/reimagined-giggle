b968a7c32e18a3bf824c00214c42cc917181e644
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:46:26 +0000

    here lesson weak scrap petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import sys
+  def get_python_version():
+        return sys.version
