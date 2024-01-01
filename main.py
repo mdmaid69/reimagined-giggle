bb9e96bb67458bf44f3fd6fdbd1c2c1f80735fb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:16 +0000

    boring bury official unhappy ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
