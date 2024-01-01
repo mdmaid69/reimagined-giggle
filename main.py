eac3822ead70febb27c486eaf78808f4a03dee6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:32 +0000

    greeting drawing agreement move getout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_pressure(force, area):
+        return force / area
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
