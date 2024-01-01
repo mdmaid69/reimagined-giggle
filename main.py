3fa3266395373e0c3e82f1948f70f036dde5056f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:02 +0000

    any guess workout funny snow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
+def convert_to_octal(n):
+        return oct(n)
