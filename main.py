aa633c4fa535bf96fd16fb33b43efadfdcc27434
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:13:41 +0000

    call home angry vision suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
