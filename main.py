85babcf1dd0f2b61567bc409ad2afbea2754faa6
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:30:39 +0000

    sort comfortable inside perhaps maybe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def reverse_string(s):
+        return s[::-1]
