e37303865f684668aab59357592b4d157df0e50a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:32:41 +0000

    order claim genuine church ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_username():
+        return getpass.getuser()
+import getpass
+def get_username():
+        return getpass.getuser()
