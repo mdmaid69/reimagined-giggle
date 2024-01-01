99841554903523176b7fe5956a3c307a738b755a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:43 +0000

    past choose still nose hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def remove_duplicates(lst):
+        return list(set(lst))
