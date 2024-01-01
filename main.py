3218c72fbf84604edb981d8ca9bb434ebb56e0ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:03:57 +0000

    dry discovery limit church past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_power(work, time):
+        return work / time
