3b70c68a22b78e1c23297e288401ba1fb5a9cd2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:46:10 +0000

    she public advanced extremely shall

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import time
+print(time.time())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
