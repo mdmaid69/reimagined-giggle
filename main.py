bf9a48560b8af8fc257d53064d5cfa44060d19fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:49 +0000

    abroad speech total bright sport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
