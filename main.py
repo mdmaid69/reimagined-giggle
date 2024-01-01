86dc3f3fb16a685973ae75944896f92873b6dd81
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:11 +0000

    whoever broad Kk quantity company

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+print(sum(range(10)))
+import numpy as np
+print(np.array([1, 2, 3]))
