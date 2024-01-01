2d32c916f4a8cc0db8a0acfad3c9076389369901
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:21:01 +0000

    declare behind loose contrast gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
