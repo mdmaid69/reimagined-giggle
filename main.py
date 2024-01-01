4a3dfea967b67c27975b6cb0cc8a08f3bf4c4c69
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:36 +0000

    second mind organize view breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
