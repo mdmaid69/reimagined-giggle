090386d8a3a1bcbfa0df6a3271f2da3f59535922
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:20 +0000

    dance afraid finish perfect remember

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
