dae0cfb970598c153456fc6f8e07efa7cb0af27f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:21 +0000

    criticism mess add interview bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def reverse_string(s):
+        return s[::-1]
+import collections
+def create_stack():
+        return collections.deque()
