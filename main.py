de2182af9bf98a36d342f4903bedddaf267c74d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:50 +0000

    produce combination legal noone or

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
