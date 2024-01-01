34a3e5ade64926ad8ead76a95a0acf7cfdb52acf
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:26 +0000

    collection official lock sign start

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import random
+print(random.randint(0, 100))
