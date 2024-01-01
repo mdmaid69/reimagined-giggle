60ef58eee659ba2eb33696aaa111340a2cd3df17
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:24:44 +0000

    mother like thirsty amount interested

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
