75922c2f4c57108b7ba6c51c54099e9cd1de795f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:58:55 +0000

    prove wish clothes turnaway trick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
