627bb1e866bfb69619c5253c976b1bafde88398d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:13 +0000

    dear once waste short picture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
