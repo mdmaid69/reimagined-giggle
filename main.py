5ed7555a54c75c4639493788dab452a429e77a1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:59 +0000

    acceptable gentle less as exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
