b4fa0971b28090b8c8bea1bf4a66d168686999f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:30:08 +0000

    basic circle height everywhere worry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def is_odd(n):
+        return n % 2 != 0
