840cf1c69604c3e900f926a1cc46f7f99aeae994
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:28:13 +0000

    thick variety late shine join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
