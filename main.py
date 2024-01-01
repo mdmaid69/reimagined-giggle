f70ac156aeac14570ab7643814e52b247d737109
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:56:52 +0000

    fresh regional patience tidy holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
