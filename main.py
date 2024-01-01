0200b45d53c0e789e6eac54d6f5910ba1492355e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:55:06 +0000

    avoid about deliberately hurry range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-from collections import Counter
-print(Counter("hello world"))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
