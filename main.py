32998b44bfbed71a768e47a1e2646fc15d1fb81e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:31 +0000

    wrap stick behave now before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
