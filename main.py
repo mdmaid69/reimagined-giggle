28d679348cd35620b60fb927be3be2a5f7247ce6
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:17:50 +0000

    feature skill environment note shut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_min(numbers):
+        return min(numbers)
