e3187fe616462acfa63351826a136ad565fdf5d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:53:29 +0000

    centre front goup right matter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
