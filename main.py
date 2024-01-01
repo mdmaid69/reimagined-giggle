e6d9ec603b2ff2d44b22acd24cd63f2ad4a57335
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:50:35 +0000

    shadow coat track cost excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
