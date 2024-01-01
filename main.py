db942186c4518a59f09ce7472c80c4aa784a58b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:29:29 +0000

    excuse colour dance unlucky react

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
