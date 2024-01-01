d5e16ab9382165dcd19116799cb4abf3a8a08cf6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:52:24 +0000

    minimum capital weigh heavy certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
