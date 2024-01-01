6d1079c48eb61dd3258f906796167cdba36a4a9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:33:19 +0000

    ticket into dig receive nonsense

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-print("Hello, world!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
