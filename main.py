12587d2bb13a69cb82fa44fc8e0a320344a6b6f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:38:01 +0000

    visit further rank hear traffic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-print([x**2 for x in range(10)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
