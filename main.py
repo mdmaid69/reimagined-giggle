2fdda984796eb08b021e91f802783ed6bb497149
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:47:06 +0000

    sleep run attention traffic limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
