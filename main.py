49a33cadb4c1ffc94a82786eb9280f2911a8870b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:28 +0000

    recognize callout piece normal appeal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+from collections import Counter
+print(Counter("hello world"))
