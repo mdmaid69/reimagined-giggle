f5da412d8eeea96df524b2c6a792a1cae300c333
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:51:52 +0000

    convinced success bound will responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
