c9adc0ac7e1c72c1e48988c5edf004eed996d4be
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:55:36 +0000

    waste there description old another

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import time
-def get_current_time():
-        return time.ctime()
+name = "Python"
+print("Hello,", name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
