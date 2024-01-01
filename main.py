1f652d055a50f223db30a2d7eba3b267da8baa91
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:16:36 +0000

    measure mend we comein continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_counter():
-        return collections.Counter()
+  def convert_to_octal(n):
+        return oct(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
