9c9cf61d1a8ff23539ca54ee091ceb428bb127a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:27 +0000

    history 573entertainment happy flight drink

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-print([x**2 for x in range(10)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
