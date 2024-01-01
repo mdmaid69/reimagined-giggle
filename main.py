12e8ffbfa70891aefe3ec443d14aa94c7fc3d0cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:52:22 +0000

    average if stick turninto unfair

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_work(force, distance):
+        return force * distance
