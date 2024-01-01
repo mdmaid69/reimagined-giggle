aaee9bdca134a57d7314252fd78aa62b010fddb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:56 +0000

    flavour recovery grateful respect often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+print(math.pi)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
