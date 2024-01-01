dc9c9626b05c1c81924e87134fdd37ce879cd2b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:36 +0000

    same charge church shape lonely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_work(force, distance):
+        return force * distance
