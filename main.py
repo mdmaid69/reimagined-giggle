12c558e88096b3ebc8f3eec18d0381f522e79611
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:22:58 +0000

    base know news love each

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-print(sum(range(10)))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
