c72a120dd5736103c63899bfc0a5a12769fff712
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:39 +0000

    public main show cost nervous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
