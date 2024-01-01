5688e8877c55d2b650a1506a3e7fad4bdd16e340
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:16:40 +0000

    patience wall crime quit touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
