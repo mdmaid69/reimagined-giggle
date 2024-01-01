bef9adf5816de519b30867719a027c06d9e85822
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:54 +0000

    turninto spend direct accept beat

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_pressure(force, area):
+        return force / area
+def convert_to_hex(n):
+        return hex(n)
