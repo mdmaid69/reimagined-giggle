86f6da377792760d103d35ea4df669483e8d79ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:50 +0000

    neck month speech look listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
