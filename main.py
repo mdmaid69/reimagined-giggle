668728b7acf27e9f92ded44dda2f22bf4824da59
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:51:02 +0000

    shadow wall nigga bored behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
+  def add_numbers(x, y):
         return x + y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
