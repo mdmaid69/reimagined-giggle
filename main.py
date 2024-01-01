d527152dabd2b024a69e7ba91c27fbf0d57c4e68
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:59:11 +0000

    though tone now between open

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import time
+def get_current_time():
+        return time.time()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
