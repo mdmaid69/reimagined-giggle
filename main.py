5a0c190a562e09091fcb3e47b0fa5d1872d0c73d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:25 +0000

    hard ear impossible favourite help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import numpy as np
-print(np.array([1, 2, 3]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def sort_list(lst):
+        return sorted(lst)
