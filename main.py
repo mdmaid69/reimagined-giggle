462c6def6bef02270b0f9eed2b6e75afb2bb9289
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:08 +0000

    official powerful confusing deal stick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
