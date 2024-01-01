d3881cf6d24bf89b37c0fe0fa93f6c2c8cafb119
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:57 +0000

    blame vanish positive laugh finger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
