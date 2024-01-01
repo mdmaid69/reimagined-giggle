2044a074be649ff21e780e69e9b99349bc9f91be
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:04:15 +0000

    receive behind I defeat objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import time
+def get_current_time():
+        return time.ctime()
