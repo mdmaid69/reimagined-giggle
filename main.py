3516a11ac1749631215b43c7aef2077b21c9b81f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:55:58 +0000

    gate section fierce and bread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def square_number(x):
-        return x**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
