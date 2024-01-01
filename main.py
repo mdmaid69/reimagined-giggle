429b66d36eb1ac75d14a8d0dc24593af4acb6476
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:44 +0000

    minimum translate miss worried firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_min(numbers):
+        return min(numbers)
