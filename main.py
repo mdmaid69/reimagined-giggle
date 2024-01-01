bf083982139b83f8c0ad14b4bfe187ec83d7850d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:51:49 +0000

    level consider waste tight badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
