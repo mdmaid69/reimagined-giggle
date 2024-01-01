16a66026b761f69dac397629b421ef53d39320e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:02 +0000

    apparently rare sweet factory date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
