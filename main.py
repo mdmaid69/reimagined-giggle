2841142296aabc1fe09c63609a894b85e19f5aa8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:46:19 +0000

    smell successful payment hold 572enter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
