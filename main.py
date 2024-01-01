3bdc553015575571fa70553d27f46368c3e46d64
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:44:08 +0000

    reason address explain imagination cry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def reverse_string(s):
-        return s[::-1]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
