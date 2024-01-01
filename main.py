82f43c8c4a8314c28513db3d84c97c718180c4dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:18 +0000

    star crash pretend spring pleased

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
