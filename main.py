3b69d0ceae3381b3c7bf2dc0c6f0b28708428ba9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:12:11 +0000

    would rubbish detail naturally name

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import getpass
+def get_username():
+        return getpass.getuser()
