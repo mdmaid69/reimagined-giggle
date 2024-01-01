81bdf966f77b945b71cebadceeeb38b90886cd93
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:52 +0000

    information sense vote nurse together

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
