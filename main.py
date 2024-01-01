06c5a9712588a494dcb01b36e2ce7ee9efd713a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:52 +0000

    lookaround pull suckme cinema tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_elements(lst):
+        return len(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
