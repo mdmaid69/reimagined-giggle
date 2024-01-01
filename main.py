fda4513ece57d3ac00661f72d524dea584da2b02
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:32 +0000

    cheek activity feed me fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def convert_to_hex(n):
+        return hex(n)
