1d957f5492fa00427083efb54507db6d6029e644
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:07:52 +0000

    back low cost sale borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def cube_number(x):
+        return x**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
