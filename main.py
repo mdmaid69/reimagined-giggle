01ce3b096c52c3cdf2aa98f3a419d7c4bc38d4af
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:18:11 +0000

    beneath fly false guilty detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def reverse_list(lst):
+        return lst[::-1]
