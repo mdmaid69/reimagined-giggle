996949fc2455659b0049d648c287ee0b42f59521
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:36:10 +0000

    provide exam custom second both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def is_even(n):
-        return n % 2 == 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import numpy as np
+print(np.array([1, 2, 3]))
