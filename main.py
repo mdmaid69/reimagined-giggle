655b65cb45917b65fb94f66d8920cc36753a9963
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:00 +0000

    harm commit result fall contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
