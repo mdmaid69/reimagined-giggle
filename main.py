eb590b8930c73d6eeeb474f23b9313cea9fcca78
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:33 +0000

    strike workupto breakin fire minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
