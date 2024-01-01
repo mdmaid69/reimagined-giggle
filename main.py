ed63a8926d5ee95c8bbe79cbbe857076b9662411
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:01:39 +0000

    pocket deliberate experiment temperature shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_power(work, time):
+        return work / time
