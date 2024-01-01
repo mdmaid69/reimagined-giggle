8c643cbe23f2a4fd03c7a8619136732a162e86eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:05:57 +0000

    throughout breath reasonable large sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def remove_duplicates(lst):
-        return list(set(lst))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
