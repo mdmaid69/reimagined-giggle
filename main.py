371e710bba10cf9a3336e7ec70d1af3b9313bf2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:14 +0000

    it print suck mistake involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
