823c84a196b8241140ece6bd63b2343e30ab9f36
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:19:59 +0000

    mother outside jump neither sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
