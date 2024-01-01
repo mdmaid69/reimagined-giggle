9bc826bdba5a88ee8d57d5918365722935d71060
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:52:52 +0000

    violence since spare correction continue

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-print(sum(range(10)))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import datetime
+def get_today_date():
+        return datetime.date.today()
