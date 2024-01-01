49eeb083c2749597c6198349022988527cb8ed7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:13:20 +0000

    thirsty serve kind switch direct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
