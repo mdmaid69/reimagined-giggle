d0dcb102ea56af5fd3607bd2a86dadd21883dc7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:18 +0000

    exploration table spin impression cruelty

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.ctime()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
