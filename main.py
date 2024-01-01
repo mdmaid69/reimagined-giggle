094cb66909eec66812ce9aa6157621a3798b094e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:32:47 +0000

    tune few patience by spring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def multiply_numbers(x, y):
+        return x * y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
