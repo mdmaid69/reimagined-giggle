e85807b3cf30a09028a158ac17d76c49c03d4f87
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:16:17 +0000

    left discovery air march change

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
