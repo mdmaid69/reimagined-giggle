6774b7a41afdab2239244d00115810b7d140b505
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:52:38 +0000

    someone goodbye spoil mail date

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
