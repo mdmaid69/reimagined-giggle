36641e49e03a35455e2bcd774b2ceb6376fb454c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:49 +0000

    breakin as increase fly extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+from collections import Counter
+print(Counter("hello world"))
