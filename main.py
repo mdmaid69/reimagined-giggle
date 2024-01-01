ccade83a9e245e0260a246fef17b0c14dd14943b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:30 +0000

    suit beat bother meet passenger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_factorial(n):
+        return math.factorial(n)
+print(sum(range(10)))
