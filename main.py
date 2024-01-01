727af642dbc94f40e63c3147f46e6739358e0170
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:10 +0000

    rob too dance pot announce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import os
-print(os.getcwd())
+def change_working_directory(path):
+        os.chdir(path)
