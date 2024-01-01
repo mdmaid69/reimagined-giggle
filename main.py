21f825a6aee9894b10d9192ee3325850b08bb645
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:16:27 +0000

    storm 569enjoy tomorrow complain spend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-print(math.pi)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
