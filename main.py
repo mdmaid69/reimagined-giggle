fb5245b395701cbcfea19206d2f34ebbdeeb91d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:59:05 +0000

    visit join television opinion full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
