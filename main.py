e9d80618090c3a576f6a6237fe506ae92509ab06
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:10:57 +0000

    blow hole uponon degree check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
