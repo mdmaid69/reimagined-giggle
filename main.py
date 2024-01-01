5e1ed8b8c1ec9b20263f535047944fedb662f30c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:37:26 +0000

    fairly same load call slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
