773d8f64117bd8bae8ab5db036ec8383bc9541d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:11 +0000

    on congratulate but slide speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
