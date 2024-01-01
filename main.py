d76e8dfe3eedbfbbe469560871309ea8f0f0bd57
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:16 +0000

    myself willing juice piece proper

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_acceleration(speed, time):
+        return speed / time
+print([x**2 for x in range(10)])
