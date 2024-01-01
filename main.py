ace64145c72d81a09fbf696c6b4f0a61bf108151
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:34:42 +0000

    table who plant everywhere comeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-def get_time_since_epoch():
-        return time.time()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
