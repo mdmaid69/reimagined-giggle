a9c86a984ae8e5e489cf4b6b7c2476c7e9e47eb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:21:15 +0000

    somebody getback almost performance loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sys
+print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
