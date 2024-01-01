6390a054992f6dc67d1aee59245013a38a8922a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:54:02 +0000

    habit sad be bringback eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def subtract_numbers(x, y):
-        return x - y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
