324f811662925036830aaebdcd9203716ecf11e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:26 +0000

    ocean cover work detail opinion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
