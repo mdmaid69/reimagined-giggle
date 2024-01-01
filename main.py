908a96d37559e6c0be0a4376c5426db5a3283406
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:36:03 +0000

    one worth sea used takeaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
