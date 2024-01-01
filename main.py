b109368dee9ac5ccc60c291fb053d9fad5911406
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:08:14 +0000

    surface used successful contact entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
