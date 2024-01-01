b4035be88ae0f8e4966e610dbca27109ac92d6dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:41:36 +0000

    never challenge complaint back search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
