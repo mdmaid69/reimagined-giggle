95b5cd11e84a3817f68a96948ff33cd2c4c79896
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:43 +0000

    female nor opportunity fire feel

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
