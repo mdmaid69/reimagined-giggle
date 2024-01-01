73474de94e3cc23ea15908d2c79fd7d1fb89b9dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:04 +0000

    disapprove judge dead evening develop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_typecode(array):
+        return array.typecode
