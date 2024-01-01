bf81deec312e67274f3c6eaf180839b1b531b519
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:24:21 +0000

    study commit official contain offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-print(sum(range(10)))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
