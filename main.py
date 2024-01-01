28c17cd76624e855280843f4acdc782c4a150093
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:19:22 +0000

    protest measure along goup definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def square_number(x):
+        return x**2
