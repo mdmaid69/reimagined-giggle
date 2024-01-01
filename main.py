8945a67293cd37ce85e9f81c209637a287fa4f18
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:38:22 +0000

    sad charge adjective appeal find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
