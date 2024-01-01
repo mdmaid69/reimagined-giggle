ea8e51a7e8541af6e28f394b13926e2d77aa639e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:45 +0000

    excuse platform between connect rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
