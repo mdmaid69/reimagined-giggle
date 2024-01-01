0aaa7fcdced59d22e333b0a512786b83cbdb6def
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:19:55 +0000

    thorough power matter pause bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
