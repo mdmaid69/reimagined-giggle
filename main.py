1e6fd9be298bc3f5c259c61434ac7374bf479968
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:12:38 +0000

    mark discuss today hit wonder

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_max(lst):
-        return max(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
