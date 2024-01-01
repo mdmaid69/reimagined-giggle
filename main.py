9ecb929f2d83c7b228f948aea55992bf965bdf38
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:27 +0000

    attempt no small question condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
