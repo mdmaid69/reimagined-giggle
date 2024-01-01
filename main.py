ea75bce41adb38591349311feb6fba1c306e12c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:39:15 +0000

    give heart sight previous naturally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
