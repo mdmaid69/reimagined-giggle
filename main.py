4654b3803019bbbb4cb0b6ecaca7c88cfe5895d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:33 +0000

    list will bringforward progress need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
