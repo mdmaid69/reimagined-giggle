9e602df3f60ebc45c20e0221070441540e363be8
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:43 +0000

    further grand hurt regret understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
