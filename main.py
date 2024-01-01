83c82294d6c01001b2bf849fef8d473196ffe85b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:07 +0000

    prepare question influence track however

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def convert_to_hex(n):
-        return hex(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
