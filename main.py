d8816a34ea46c3fb53a4be71a48420a02fbf6c85
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:37:18 +0000

    hit plastic them high classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
