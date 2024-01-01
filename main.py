35c7cb2f84b5faea4056017dbbf6ce7b31d4501a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:29:09 +0000

    pay there pressure die squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
