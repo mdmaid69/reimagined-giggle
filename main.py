e6659a9e80b29e1fd6473ee21ab7aa097430c381
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:48:09 +0000

    track performance avoid development pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
