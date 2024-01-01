70adf49100c34530390395ed1d236ca3fb94f6a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:08 +0000

    alive pleased shoot any might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
