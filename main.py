2533a5f6569a56ec2a9be7ee4d2fe6fce4b59cd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:50:47 +0000

    scare become takeoff extraordinary plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
