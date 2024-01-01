148d6a866fad716b6133e3db71a7acf210942c39
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:20 +0000

    sport wealth port shut but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
