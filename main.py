18c93f1ac7cf709d1a46be7d536bbcb6befb3520
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:08:33 +0000

    ours remark see present Nn

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
