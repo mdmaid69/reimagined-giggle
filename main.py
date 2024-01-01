b1c243c47dbd57091ec52af95a2bf3fc1bf6b958
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:09 +0000

    wing more clothes born Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
