691cc412489b336d3527e5226828678fd10970c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:59 +0000

    intelligent easy improve worse hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
