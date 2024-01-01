866049584e9f59a025ed178f23d184be0c09568b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:49:44 +0000

    substance lock wet string character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
