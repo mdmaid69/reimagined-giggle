7b52edfd9c99f84a33a0880bcfc33c1da96c53ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:09 +0000

    often young collection look yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
