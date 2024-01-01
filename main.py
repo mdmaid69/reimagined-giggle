6a6f599c77466c560bada6edc17cb2622ce487fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:12 +0000

    couple test declare intend lend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
