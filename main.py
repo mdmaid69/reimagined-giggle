5eb548a9856867a39b0822a1ffc7e81815972ef5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:15 +0000

    deep immediate forgive suckme nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
