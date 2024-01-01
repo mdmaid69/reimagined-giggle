29b43bebb36402f9f9c004b202d0ea3f1b57e508
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:02:45 +0000

    unlucky bit best determined cruelty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
