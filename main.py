8bb382cb6daf05af9e8b499449926c0504d7974a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:34 +0000

    afternoon workon loan stage tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
