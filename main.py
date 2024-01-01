fc08ce0438390e662bda277d1b9af1c967edb5fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:13:45 +0000

    573entertainment university strange constantly different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def add_numbers(x, y):
+        return x + y
