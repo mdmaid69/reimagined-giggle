dd40669e0e2ecbaa2868ea194de7a7adcbad29f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:35 +0000

    deal information even tomorrow joke

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def convert_to_binary(n):
+        return bin(n)
