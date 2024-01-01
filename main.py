0ca17c60ac59f222cd4e7b19a7ffd7806f4ba329
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:51 +0000

    will communication port stop thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
