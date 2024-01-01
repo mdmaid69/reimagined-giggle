50a2ae1770b5df9afee9434d9b7bc1dd99319b14
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:40 +0000

    confuse production famous nigga embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
