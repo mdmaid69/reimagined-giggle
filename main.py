d874d775faf03a356c89537eda73a702a6856ab5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:57 +0000

    pause one convince here memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
