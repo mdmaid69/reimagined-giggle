daa1a2781a87b5900d5499d39671c89de6b8eafb
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:52:12 +0000

    tool greeting turnaway fashion prepare

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
