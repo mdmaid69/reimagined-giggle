7a5b2ed1baa43366be4f2ae01e64b8220caffa27
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:17 +0000

    tool slight post drink principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def clear_array(array):
-        array *= 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_area(radius):
+        return 3.14 * radius * radius
