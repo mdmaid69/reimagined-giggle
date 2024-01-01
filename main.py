3c5d9e304d0af63b84307501e2a8045a2051838c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:57:33 +0000

    float emphasis consult sentence unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def count_words(sentence):
+        return len(sentence.split())
