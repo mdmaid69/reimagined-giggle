d945048ab3a1a9f1a8757461b15139935c77799d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:20:03 +0000

    class complete usually declaration care

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def count_words(sentence):
+        return len(sentence.split())
