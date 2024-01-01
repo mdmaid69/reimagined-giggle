3734c0526348e82b77057341e29a201401b5fe8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:14:54 +0000

    deep absolutely disappointed appear only

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
