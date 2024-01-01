2c4a3e0c5904dc40f024aed292561c436ba3dc59
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:39 +0000

    manage easy expect move threaten

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def print_python_version():
-        print(sys.version)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
