8f07856e24da2da177ec7ea7555d849180fb6e9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:32:59 +0000

    principle ours catch offence fire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
