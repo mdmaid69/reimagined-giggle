90396361981813fbd821471c46d6e11991e1fd67
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:04 +0000

    involve rob male minimum middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
