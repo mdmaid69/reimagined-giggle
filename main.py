cc1e0553fc9a3bcd36936bcfb528411b822248e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:47:48 +0000

    insist even no correct inform

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
