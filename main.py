a512515f6e55639b10311e9fca6bdf3416d01d5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:38 +0000

    reach behind his state injury

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
