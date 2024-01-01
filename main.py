17c3cb4ce62749fa276a93c0625abb3e3d4ac0ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:18:04 +0000

    turninto confused control accordingto rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
