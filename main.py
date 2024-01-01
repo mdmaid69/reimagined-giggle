741913aa51f28ab97f7f86d009d27ba74166bbb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:48 +0000

    ask impatient feeling broad corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
