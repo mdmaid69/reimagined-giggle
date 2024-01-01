720a6adfa57cfeb0986d9c7beb5935004e05ab4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:01 +0000

    little little reduction watch well

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
