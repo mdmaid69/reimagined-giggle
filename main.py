c51c7e1d8c1c3245c33ba2ca73e5d7430381e316
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:55:55 +0000

    attitude material birthday benefit success

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_list_from_array(array):
+        return array.tolist()
