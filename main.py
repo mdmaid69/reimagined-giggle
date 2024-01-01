3f267dbdc880f58f9a73200e2adbc91269ee04e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:56:11 +0000

    rule creature vote block equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
