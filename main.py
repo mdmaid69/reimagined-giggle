c43575893dbcd24cbd077822af1897248ecb9a34
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:16:29 +0000

    careless my attract ride dark

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
