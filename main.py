93e48cd611a29b0964ce6e5bfe4bde60555d3ab3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:45:44 +0000

    could under the feature uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def roll_die():
+        return random.randint(1, 6)
