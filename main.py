2aa161614f5a265cebcb8717cf4940d758d1d67b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:46:01 +0000

    meanwhile secret represent tragedy real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
