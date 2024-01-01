100ddf820e04d7ff573edcfb3cabf9ab960d7c7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:53 +0000

    reject classroom willing expensive keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
