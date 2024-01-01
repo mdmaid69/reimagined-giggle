2c4d8afc14577d6187007b26ce76e994f1fd5816
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:05:59 +0000

    snow holdup lay straight tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
