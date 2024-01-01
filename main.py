2a0caad8fe7f2aebd253a294cc244ceac09175b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:55:12 +0000

    production change history arrive admire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import re
+print(re.match("h.*o", "hello world"))
