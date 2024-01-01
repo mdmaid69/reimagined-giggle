5ff03707832482c85f60eac1b9caea8d4d2ee307
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:40:46 +0000

    care especially remember less create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
