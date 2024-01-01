7d7ea1b71ad90a1b3313e8a94feb43de88dde959
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:18:40 +0000

    naturally return rise bath chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
