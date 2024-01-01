17b56a966a99ee45479127dc5324a965701f6063
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:13 +0000

    wish before welcome line suit

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def subtract_numbers(x, y):
-        return x - y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
