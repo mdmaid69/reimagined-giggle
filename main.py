1088d2bfbbe3b438901a1e775a81d007bbf3f265
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:23 +0000

    reaction strange famous active floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
