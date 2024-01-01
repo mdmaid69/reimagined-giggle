1508124611c96eafff506c80167ccd4a8d511a32
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:49:32 +0000

    activity breakout pretty emphasize sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
