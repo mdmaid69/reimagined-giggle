edc340f74df8c65ee841b98e9cf583d164f89992
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:26:44 +0000

    offer forward tonight cold brave

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
