67903fd5126c41fe542c489e752966e0fb92526c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:42:24 +0000

    murder divide tread handle prize

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+for i in range(10): print(i)
+def remove_duplicates(lst):
+        return list(set(lst))
