47af47d6938d726cfa783d72f17d8191d76c8ffd
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:55:24 +0000

    lucky winter thumb alone second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
