41851b7a6f4d9941f5641739775185e79eab4295
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:56:31 +0000

    warning loan think call process

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import time
+print(time.time())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
