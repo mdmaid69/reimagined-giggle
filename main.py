482930736dfe746055048df2d8614bf62770cc47
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:19 +0000

    shelter turn plant catch live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
