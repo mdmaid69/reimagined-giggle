71a56fbf51ae5a039948af9d06718100a35958ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:00 +0000

    responsible turninto software sight content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
