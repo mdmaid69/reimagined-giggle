477ec4ba25618b2878b4415af351e58a7249dc28
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:07:37 +0000

    love under confused lower reach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
