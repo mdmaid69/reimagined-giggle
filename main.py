6407213b4ea5af2ccd9cfe09caf42a8e7f48fdd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:01:53 +0000

    claim miss attach male side

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+print(re.match("h.*o", "hello world"))
