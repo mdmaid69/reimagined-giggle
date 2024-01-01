27fa7ab9e433bdfae4ae8e173e33f219f722db2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:27 +0000

    performance kind variety to sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
