ff8624fca23d9131ecc7f705541a13cbd60752a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:22 +0000

    bread entrance manage drink phone

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
