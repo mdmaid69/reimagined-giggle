5dbab9b5cccaaef58227d8c343e2747d3ecd5c01
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:39:05 +0000

    delicate battery strange advice foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
