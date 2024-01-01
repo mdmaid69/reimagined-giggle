50ce64186a7ff655c073ec9ec0f4767ffd5a02a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:21:34 +0000

    reserve sticky along label sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
