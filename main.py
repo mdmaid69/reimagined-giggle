fbc5ee03ed222b8a59ac165fb770a6c0b480ffbb
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:46:02 +0000

    match or comment book declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
