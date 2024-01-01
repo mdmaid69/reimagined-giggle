f1e37f4d28ca5b0ec5428129477faa9009e0ff61
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:48 +0000

    later glass birth fear once

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def count_elements(lst):
-        return len(lst)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
