b6ba44f0372421fc46c60e044143703065fc757a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:00:17 +0000

    contain decide pay maintain report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
