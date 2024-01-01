1982f665a8a0ccab29bbb4bf674dcdcfb30174a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:06:34 +0000

    except serious position laugh oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
