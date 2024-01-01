2f08cf69e47d54b6e38d0f1deff63cceaf08c0aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:51:55 +0000

    bringdown Qq war right ring

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
