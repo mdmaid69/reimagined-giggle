066663ef98af0a71ca4c9a86f1a72f72fe027ccd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:58:47 +0000

    seem risk even available conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
