ab924a11da495a9a2ac712fef7745cbc17a1c081
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:57:47 +0000

    nervous lookup prove change shout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_octal(n):
-        return oct(n)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
