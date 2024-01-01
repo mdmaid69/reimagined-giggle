dc7694476453d16cbe5bcf481c09c1c45bb00fca
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:35:02 +0000

    myself lay march loud like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
