6157411ece16540716dfe7a2e688739b5c0db092
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:49 +0000

    rise dish guess matter idea

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
