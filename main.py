44a096599d5016f947afadf2cb75389c0d198a63
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:09:53 +0000

    bar some what emotion length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
