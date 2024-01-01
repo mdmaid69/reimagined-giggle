433dd8b820e14ef2536f8479b17803d5ce7483ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:28 +0000

    only back look apart broad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_today_date():
+        return datetime.date.today()
