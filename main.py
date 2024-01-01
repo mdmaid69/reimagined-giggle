d432535c34e0720539b3b28eea516aeb97dc20b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:11 +0000

    comfortable immediately Kk steam wheel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
