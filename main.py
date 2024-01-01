7c6ac32eaee24d27fbeb968c9b0749eafa9ef40c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:52:27 +0000

    complaint why reduction nonsense apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
