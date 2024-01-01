26883b910365ac200ca59f9a83f79ef89da8b7ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:37 +0000

    movement lift shut law concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
