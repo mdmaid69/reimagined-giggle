8aaa3e7280ba6898c2556845a3eb9428b60136a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:45:34 +0000

    polite return Yy creature badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
