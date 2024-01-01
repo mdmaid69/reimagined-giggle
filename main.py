42822f8acd28d6f8dcc6efd22dcd72646af68908
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:14:43 +0000

    warn recent brother talk tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
