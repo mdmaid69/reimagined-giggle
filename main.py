11f49265bfe6164e59042fc6775207fab264c0f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:14:33 +0000

    pipe sharp unhappy height improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
