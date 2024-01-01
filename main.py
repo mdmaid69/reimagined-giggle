d266f696695cbc8858d60c82c2796b1fcfd5ff30
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:04:43 +0000

    touch last description rather myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
