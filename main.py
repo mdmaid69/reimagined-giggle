09d0ad3038cf406922b50d9f42ca783fdff22f41
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:18:45 +0000

    neck trip talkoutof increase star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
