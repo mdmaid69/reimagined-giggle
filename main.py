eb2e97398cdb7b9f097747bd05b579afb19e722d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:08:38 +0000

    dance bill dead human road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
