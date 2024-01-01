fce33c8f52df00a9cff9f7cab50fc0134edb8745
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:07 +0000

    judge news ready world behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def is_odd(n):
+        return n % 2 != 0
