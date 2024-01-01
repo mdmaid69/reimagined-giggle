43db96fe47717b09e15484d00679bb1dcdf72b9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:50:59 +0000

    realize interview cry push sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
