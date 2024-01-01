6f073042e52bacd650a9321ea154c3a8a3671985
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:00:43 +0000

    scare rent straight extremely grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
