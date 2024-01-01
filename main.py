60e905c28175511c2f99daba4fb846d7acfe9fb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:11:23 +0000

    spring breakinto live connection similar

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
