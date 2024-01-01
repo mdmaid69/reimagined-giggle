b9ce60b559312c8744bd791000a0ada941244461
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:47 +0000

    sticky so round crash simple

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
