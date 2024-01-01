6662e9edbcc59ce932e2e89cce13ee1419e90d58
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:38 +0000

    nature direction switch people feeling

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
