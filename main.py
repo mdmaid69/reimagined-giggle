f08daedbd90fff1af96fad04a4d80ac138e00f5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:22 +0000

    quantity spring bin justice narrow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
