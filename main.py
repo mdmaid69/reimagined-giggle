e90632bd1829bf4ac3553cef52b5ff5f48ea0c31
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:13 +0000

    base just Nn beginning well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
