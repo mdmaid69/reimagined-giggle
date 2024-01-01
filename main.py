86e77b5c4f10efa6f90f4ee47ee68b84974bc81c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:22 +0000

    coat Pp Dd extraordinary extremely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+def calculate_pressure(force, area):
+        return force / area
