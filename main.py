648dd41de8d37932256732825ec4b388d9c83455
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:52:10 +0000

    burn design company flight we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
