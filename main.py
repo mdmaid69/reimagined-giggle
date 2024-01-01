5d5f4a4a5649cebdde04e734baf5f92ec5f79eaf
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:45 +0000

    strong pollution temperature by concerning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sys
+def print_python_version():
+        return sys.version
+import numpy as np
+print(np.array([1, 2, 3]))
