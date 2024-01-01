5a96612497ce7f6cb88e0071729ef9f908891cbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:08 +0000

    makeup occasional city step fit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
