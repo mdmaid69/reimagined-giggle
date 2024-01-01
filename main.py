f2f2a76f3976a952aeac13352945739c74fdf107
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:54:42 +0000

    empty anything remove line important

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+print(math.pi)
