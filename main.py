b0397da9cd4d7a746f0bde1317deb165647559ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:58:14 +0000

    seem stick pride cheek mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
