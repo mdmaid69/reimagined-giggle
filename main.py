04a35462d1eddd9876d88fb535092da0a62930cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:12 +0000

    pull upstairs inquire undo protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
