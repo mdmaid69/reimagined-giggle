54532b141272f3c15d2c86f1d39ae4dd28914070
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:49:40 +0000

    stir case wander confuse 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
