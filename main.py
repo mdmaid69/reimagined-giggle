e6eab77673e19a585dcb1c6f0ac8f9353130cb87
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:50:17 +0000

    rely train callat choose Ll

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def get_array_item(array, i):
+        return array[i]
+def calculate_speed(distance, time):
+        return distance / time
