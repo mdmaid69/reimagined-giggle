997267986a4ad5152cec2047fd2cbce41c8bcc1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:43 +0000

    some gate wonder feed coat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_list_from_array(array):
+        return array.tolist()
