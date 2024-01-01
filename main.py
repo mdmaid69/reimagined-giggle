28beb7d44b759c2efbdd39f79f0ba85aa6dcfc93
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:17:07 +0000

    affect refer swing Pp at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_speed(distance, time):
+        return distance / time
