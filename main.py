3305800e8e5427cf733e2fd99d0b412adfd83995
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:16 +0000

    boat apply practice face forward

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
