586d27e8544a59199502277d0c6d34447abf069f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:04:44 +0000

    whatever personal danger circumstance trap

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
