2ecc6deb21f3924c9ecbea65404c0b60523d7dbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:33:38 +0000

    calmly along wash match seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
