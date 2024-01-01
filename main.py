df9a15dae6cdeee8a7ab8d92e08b18a278611ef3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:27:29 +0000

    see supply drawing bear movement

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_min(lst):
+        return min(lst)
