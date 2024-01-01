c319e4b6455697ce0816ff790b864f6bf665c233
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:04:25 +0000

    contrast smile admit stir description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def is_even(n):
-        return n % 2 == 0
+def get_array_as_bool(array):
+        return bool(array)
