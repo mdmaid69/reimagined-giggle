165e96b1eab07cd5fbbe4de6222c5bfdd8fed633
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:37:09 +0000

    thirsty dream 570enjoyable bin grade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
