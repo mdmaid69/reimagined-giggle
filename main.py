06439d9ce6b453f18cfb82cfcf2c519ac37f5d34
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:46:14 +0000

    hollow about possibly level knock

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
