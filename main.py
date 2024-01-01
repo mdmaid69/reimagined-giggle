a29b615c03d2d52f67c7f03bef34c94e1cae3fa4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:10 +0000

    invite sound equal aware shock

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def square_number(x):
+        return x**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
