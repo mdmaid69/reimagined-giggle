5d08e57188f211a97210e172042e96e4d9ddb70d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:04 +0000

    advice fight heavy future extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
