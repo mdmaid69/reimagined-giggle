42ece7dff27267b25f4a90ed1f9fe49b77154887
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:06 +0000

    lookback(on) many report laugh hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
