ec97774ff84dc192085302528973d4ea3a3b96d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:24 +0000

    either its talkback difficulty goodbye

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+print([x**2 for x in range(10)])
