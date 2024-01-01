b354b57dfa80d7dadfc71ddda6661a3ed2e0f65c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:31:26 +0000

    achievement contrast anywhere juice react

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
