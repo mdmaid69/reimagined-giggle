8bfdd759f3e4e6b27f2bf759400eacb1347e2750
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:53:44 +0000

    some breakout before lookaround aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
