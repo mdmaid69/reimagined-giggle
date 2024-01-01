89ce76017e2a112cd97053bf3ae6ff26ad1dc110
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:41:38 +0000

    callup spend pocket introduce later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
+def create_queue():
+        return collections.deque()
+import collections
 def create_stack():
         return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
