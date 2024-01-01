5268667b933a389164336b3162a5e89f6bab0d3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:24:20 +0000

    own exciting mind mad talkback

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_itemsize(array):
+        return array.itemsize
