e900ae4649f551e1f42d56c34ff59dee444874b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:19 +0000

    secret bunch convincing reach this

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-print(datetime.datetime.now())
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytes(array):
+        return bytes(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
