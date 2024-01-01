1f27e1cb5663c81241afc113987244b385390295
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:42 +0000

    lost partner rush knowledge piece

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+print(sum(range(10)))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
