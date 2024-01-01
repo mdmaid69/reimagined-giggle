0e2f20cdd92490373815ba823d11f4fa159ab40a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:36:06 +0000

    account train arm moral professional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
