31ce757103e0040acd8d3e5ea6b0fc3cf9a16f01
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:05:14 +0000

    unlucky combination far fall few

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import random
-def roll_die():
-        return random.randint(1, 6)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
