9295aa68c484116690bd4ef47914d80c76854578
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:49:53 +0000

    appear active scare brown professional

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
