8b42777ae2fbc52abef1c21353a7d0a5ab54d903
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:26 +0000

    unhappy unless attempt decide fall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
