8b7ef35a246396e090b4cd47176177bf84fc5809
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:45:12 +0000

    cool generous protest tomorrow feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
