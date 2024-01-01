6ddb61bd6ff3e19deab5965d3a5f8bd2914d1bbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:07:46 +0000

    Itself physical time basic make

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
