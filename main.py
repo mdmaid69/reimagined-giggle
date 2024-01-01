c39e7549ac9b01a205834f9a579ee0c609e24b10
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:19:02 +0000

    can even drink giveaway black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
