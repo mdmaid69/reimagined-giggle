fa41713b11b62339ab0ff36b5cad747bfdae2477
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:57 +0000

    woman consider bringback turnaway kind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
