b36a60edf4ef575bda8b3e3086a5a44c10adc57d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:29 +0000

    check left different calm store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
