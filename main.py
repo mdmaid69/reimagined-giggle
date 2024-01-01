805698882571027346466386b9ebf509ca865d6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:12:39 +0000

    smart behind improvement square gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
