a4fbc1bee4bf7ff1609131fdd2410ad762bc51ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:05 +0000

    often original various passenger stupid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def multiply_numbers(x, y):
+        return x * y
