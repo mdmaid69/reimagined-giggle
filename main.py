bd8108954375037f9057b20e41cc36039ecb216d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:58:01 +0000

    uncertain so soak shop goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
