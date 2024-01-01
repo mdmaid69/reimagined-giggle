8dccfea95499259ed16c7e3ae00d584f7efa4d1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:10:04 +0000

    dark tend suggestion dream rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
