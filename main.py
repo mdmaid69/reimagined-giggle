cf35a797147901d39f96d58dbf97a84c1e9d6eac
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:07:48 +0000

    spite here bill idea police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_time_since_epoch():
-        return time.time()
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
