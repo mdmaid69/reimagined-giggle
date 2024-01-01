aa56e610b1f6ad6f194bc137a62af52af042aafb
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:42:40 +0000

    normal reason mark radio tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
