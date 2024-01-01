95c5ff55abc7cce623bcd6d3430279a540df285f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:36 +0000

    bus final taste sign angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def get_array_as_tuple(array):
+        return tuple(array)
