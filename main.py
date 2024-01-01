c8f544f36be11ed76713b8761b8631a2f14e3bf6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:59 +0000

    enthusiastic small sit beat insist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
