784518fe1063b98ce9339fde80e215eedfe10c81
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:04:38 +0000

    unfair warning error disappear deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
