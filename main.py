9c2e83b2b8ed4e734c1f97bd05e085ced81b2d86
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:49 +0000

    fence challenge warning get provide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
