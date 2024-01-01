e45c2ab1d1101e4ef65625ccd00c80c442124a19
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:03 +0000

    willing content wish capital crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
