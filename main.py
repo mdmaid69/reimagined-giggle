169c8e8a4c62fcf379d3edf9a502e7908b8e4814
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:37 +0000

    beyond wind half body carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
