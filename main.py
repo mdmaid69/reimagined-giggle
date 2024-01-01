3dd1b4449848a57fc866806557275d8950d11679
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:13:51 +0000

    hit phone birthday sport doubt

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
