bba3f2f4af2a58f95cf1749bc74d5731d1ea79e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:11 +0000

    thinkof responsible one picture noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
