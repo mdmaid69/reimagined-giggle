dc7407275c2dc130ffe6b3149f8c9bb11403792c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:37:34 +0000

    protect through 570enjoyable fair announce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
