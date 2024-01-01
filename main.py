a4a61bf32db1c836c4e042b0e303c1bfd54b50eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:55 +0000

    middle damage wall freeze half

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
