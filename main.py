acb8a3c250bcd03f368fc15f47e4ee18cc9c0aab
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:43 +0000

    circumstance hold laugh travel funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
