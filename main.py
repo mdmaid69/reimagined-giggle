275235e6c2f64f10f2d751c5bfb35b557cf28bd3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:20:57 +0000

    loose fail surprised left yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
