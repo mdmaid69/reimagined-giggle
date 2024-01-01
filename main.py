16f9a97a08a99a1cd72da713a1485fc4fce9f401
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:34:25 +0000

    very become chin here slide

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
