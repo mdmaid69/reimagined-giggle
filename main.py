477b4a319a3076caf3d34a08bc7084d23cb38366
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:28:38 +0000

    there glad shrink 567enemy smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def add_numbers(a, b):
+        return a + b
