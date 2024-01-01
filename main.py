06e5723598502dc562bbb497565036eebb0ab503
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:57:09 +0000

    violence serious crisis protest past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
