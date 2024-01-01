ca3dae26dc9ce1c62221ff3854962d1f55ba3db9
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:43:11 +0000

    turnup either communication food before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def add_numbers(x, y):
+        return x + y
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
