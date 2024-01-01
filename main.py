b3bf51c7ea7d75196af651de8718d3357145199b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:47 +0000

    curious getback expensive under factory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
