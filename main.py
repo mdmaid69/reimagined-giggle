c41c576720c449030ee74ce7775075e690f083a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:10:39 +0000

    difference lean zero tip single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
