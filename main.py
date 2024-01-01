4c19f4e33930b271880609a9811ebf5b064bca29
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:49:43 +0000

    meet always succeed feature contents

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
