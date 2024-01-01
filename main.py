7c352c1043be2f9bdb8856e2950709b222965ba5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:15 +0000

    several old offence copy unable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def split_path(path):
+        return os.path.split(path)
