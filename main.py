bec52fad31318a782afddf423bcb6218eda194ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:24:03 +0000

    lay occur discussion long step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
