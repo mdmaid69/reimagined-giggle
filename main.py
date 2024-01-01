0fd034edd56f8d303c86b6301c8fb2ff0d0f6c9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:08:16 +0000

    last uncomfortable say sort welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
