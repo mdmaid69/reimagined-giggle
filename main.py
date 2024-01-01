ecf41ce79a7c7e6fd43248c06df5f48d4ca506d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:06:07 +0000

    hurt present rarely offer deep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
