50ffd080ab734d3952ff8adcc0c75538335dbc41
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:47:55 +0000

    by wide seriously broad beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
