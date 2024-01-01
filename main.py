25bf40a6ccdcf112d54ace0491c32d453029b49b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:26:49 +0000

    dry everything memory develop before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
