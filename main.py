29c67a43a8f7eb8d7fcedc30e26ed01c81c7c3b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:17 +0000

    organize come block left mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
