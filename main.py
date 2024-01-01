4c4b5dcc420908bdcd751875583b7a1d684421f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:45:16 +0000

    benefit substance middle rain season

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
