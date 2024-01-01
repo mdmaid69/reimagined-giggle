0374470ea02f60b2d9c734b932bbaf50a860b0d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:09:54 +0000

    factory compare part comefrom mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
