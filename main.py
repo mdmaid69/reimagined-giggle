d4dddaa535b05331909d5f05ca81d073536c3aed
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:43 +0000

    open waste earth circle stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
