45f5e4155f742c67c36182e6616d6382918e3df1
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:25:49 +0000

    hurry takeoff number birthday unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
