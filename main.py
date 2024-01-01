07bead908a97505ffaac680c3d7dd3d51a561067
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:12:26 +0000

    debt stage wash me light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
