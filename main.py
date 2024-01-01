2302c40b764961d9b3945d4d7b078a785e55b6ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:38:25 +0000

    tour shrink feel rich cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
