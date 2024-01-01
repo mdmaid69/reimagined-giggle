c028c71fd77281bd47b39b6f109fc6c1d7c37286
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:48 +0000

    that tongue brave freeze muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import datetime
-print(datetime.datetime.now())
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
