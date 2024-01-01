33209bf13b0ef02c435594236123af1a0afbe709
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:30 +0000

    recent much read tidy furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
