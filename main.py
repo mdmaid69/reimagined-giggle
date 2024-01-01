4c20a44b8b5be5fc04205164881c5be1c0d7ad83
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:12:40 +0000

    mess steep round distance food

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
