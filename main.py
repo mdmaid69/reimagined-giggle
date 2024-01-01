2632a9d2f2affb73ee08358a49fac840b0bb24f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:18:52 +0000

    appear Itself crowd for back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
