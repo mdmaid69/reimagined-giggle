9476720a5b2380954dcb11e0ea81d0a97b20ae4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:44 +0000

    clothes split appearance gentle guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_factorial(n):
+        return math.factorial(n)
