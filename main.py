ffa255bf87bfa873c670413f092616c38308fbf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:02 +0000

    house fortunately nose prepare noisy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def sort_list(lst):
+        return sorted(lst)
