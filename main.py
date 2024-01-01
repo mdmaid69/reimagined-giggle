bfc4164bb519ac84ec31e51ecb8abcc4f502628a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:12:44 +0000

    promise trick pay coffee perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
