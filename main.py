9bdcc7ab9be19a2a7f89091e6f27304c38216a8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:05:22 +0000

    favourite young muscle image previous

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
