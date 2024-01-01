ac11af2d7a3ebe54e2f3aa381e63368d28ab4364
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:34 +0000

    near hot blame complain noone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
