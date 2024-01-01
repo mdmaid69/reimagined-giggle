fec9f3e3f3c4f3c36832f5f9491814343637613b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:30:23 +0000

    police so breath state natural

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
