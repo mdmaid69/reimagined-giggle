1f69e5b18908580da75370e6c986b1f631c4c8c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:26:03 +0000

    somehow moon scrape realize minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
