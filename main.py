e372810c43aae7db8be4640f548c100b66e8431b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:26:18 +0000

    determined sweep container left be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
