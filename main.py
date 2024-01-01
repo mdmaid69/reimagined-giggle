16dee5ff01599bdd72d7275be51aad723f0869a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:59 +0000

    border healthy fasten place well

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_work(force, distance):
+        return force * distance
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
