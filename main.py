c45d03b0fb4d1d4d2bbf62dc4f9325e9c7e3b80c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:18:44 +0000

    cost blind broken task properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
