d4c478a7f41049ca44f99170ad0b7f8a484f16c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:51 +0000

    secret song ill intelligent makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
