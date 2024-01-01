9ef57b884111902cc1f416a304d0253c68f9d64c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:20:46 +0000

    brief above ought tongue Tt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-print([x**2 for x in range(10)])
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
