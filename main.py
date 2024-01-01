49a21e6c1bbad858fccab104af41307a2c4a1a51
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:57:48 +0000

    closely unkind surprised pass mean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def find_max(lst):
-        return max(lst)
+for i in range(5):
+        print(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
