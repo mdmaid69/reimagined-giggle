6c6288775184737e79bb0f11860a7762283bb91f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:25 +0000

    scream shop pronunciation reduce introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
