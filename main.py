a3db93fc069ebc5b3cd7ad5c97089d93e3d37de0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:06:49 +0000

    dare suggestion tragedy freedom label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
