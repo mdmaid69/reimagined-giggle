ada517c9711d4433b83d983efb6c38202c68300e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:17 +0000

    sit yet recommend us sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_float(array):
+        return float(array[0])
