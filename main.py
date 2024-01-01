a02e591e2752eba35e79990448e6053119b18a82
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:30:14 +0000

    their like way time authority

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import collections
+def create_queue():
+        return collections.deque()
