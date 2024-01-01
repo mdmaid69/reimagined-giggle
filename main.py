15f0fefb9e76ef8ee37428973d2fed59d3c77ae6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:13:44 +0000

    concern why like news impressive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def reverse_list(lst):
+        return lst[::-1]
