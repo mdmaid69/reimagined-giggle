656ff711ca7a79dc6a890b5dd41384936d9ca986
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:09 +0000

    lookback(on) major calmly stiff admire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
+for i in range(5):
+        print(i)
