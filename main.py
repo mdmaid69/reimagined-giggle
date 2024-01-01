a6bbb30b34be8f7574181f9658e9c40cd6dacf48
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:23:06 +0000

    welcome suggestion shall standard important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
