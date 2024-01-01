637eddb94a729b44ab2f29b3af3b419b2cdf5427
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:33 +0000

    heat song owe discuss crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
