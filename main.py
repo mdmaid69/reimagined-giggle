22fc3ada77727b1aa2d6c47bb2a86c5a904b0678
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:48 +0000

    against communicate lift foreign theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
