3d7c086603b21ff9230d4d66a7c89d783db82e90
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:21:33 +0000

    scrape jump advantage warn expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
