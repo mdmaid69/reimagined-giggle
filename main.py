880e1e1da4660659e7e9137619ce8b24f1106366
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:56:37 +0000

    in above cope way motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
