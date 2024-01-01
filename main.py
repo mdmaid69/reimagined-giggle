e0ff9c570ee514d84fe9be49af8acfb441ad3f50
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:21:22 +0000

    adjective long clue model collection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
