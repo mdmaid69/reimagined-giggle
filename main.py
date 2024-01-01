f67cfdd34ca4095c822eb6e08bfc82b2456cab8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:22:11 +0000

    exercise service record steer address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
