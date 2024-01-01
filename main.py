09835e0cdfb8f90ab9626b4465aaa3088e8f155a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:21 +0000

    mend again bush sticky proud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
