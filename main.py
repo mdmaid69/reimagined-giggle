4e3adfd8bf57b2391988936d3086f323369fe484
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:04 +0000

    remember spring mind tragedy who

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
