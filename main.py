5e7dc88de2f48343c80f357194fe7f3af81eeb46
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:38:36 +0000

    keepup taste thirsty measure by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
