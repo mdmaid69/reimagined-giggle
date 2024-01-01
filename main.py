29c4db8a40cb2028afe9b48bc52e0ca87f317eaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:20 +0000

    answer slice turnup joke turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 def get_today_date():
         return datetime.date.today()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
