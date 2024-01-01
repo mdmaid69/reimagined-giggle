53aa6b0831635462e77717227b1748a0c6506a84
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:42 +0000

    talk until child she waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
