f99800376ae70f2f68721ce99029aafb578e9b11
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:27:45 +0000

    platform police my sticky somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
