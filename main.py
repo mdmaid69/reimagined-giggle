b5edb32e183dbe378322ab0a2294f73d8abe57d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:08:16 +0000

    lot will relation sign taste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
