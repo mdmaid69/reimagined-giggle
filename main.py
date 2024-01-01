3a3bc023c7f30b7494eb90425cbcc0d43f58ea28
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:38:20 +0000

    sore confidence blame takeoff target

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import tensorflow as tf
+print(tf.__version__)
