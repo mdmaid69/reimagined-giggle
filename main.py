68111466ca38d7dd6f531299d177e94d1bb7c91b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:26 +0000

    opposite look introduction form recognize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def is_odd(n):
+        return n % 2 != 0
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
