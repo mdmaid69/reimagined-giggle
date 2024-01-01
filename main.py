3f0fadc21da510d7f1f06ecdaa3eb6d43abb65eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:19:14 +0000

    by half likely none movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
