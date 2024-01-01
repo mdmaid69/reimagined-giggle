0ea1fc416fd5dc1aa9d0d54f8726a333e11d4785
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:14 +0000

    lock advise whenever stick business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
