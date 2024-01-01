4a8572eeb3ff12bf16bd3664733edc4c8b1acdd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:35:39 +0000

    better supply string minimum 566end

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_base_name(path):
+        return os.path.basename(path)
