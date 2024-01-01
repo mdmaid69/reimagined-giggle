50eaaeda5b6cfe088acc4dc30ad2f342cda7c7c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:21:55 +0000

    duty able wise exercise basically

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
