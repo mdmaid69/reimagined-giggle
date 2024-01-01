b49f87c00f1a550047a5a45fb748e711af356075
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:16 +0000

    only little disappear extremely blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_max(numbers):
-        return max(numbers)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
