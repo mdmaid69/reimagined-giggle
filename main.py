9095da8d2227e5576041c1976248f324a7793cfc
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:39 +0000

    her admire judge patience ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
