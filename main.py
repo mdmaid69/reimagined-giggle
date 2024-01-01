824fe8ce96fc45e7389d47b7031733c42b30bd76
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:32:23 +0000

    service platform or must game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
