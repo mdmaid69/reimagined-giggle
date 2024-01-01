8205ebab529f156b1f38332c47ffbdda7dcb3263
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:26:57 +0000

    concern cover legal weigh include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
