18b5fe6b5cea117a1e2a195bbf62911be1cb6b4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:29:01 +0000

    downstairs shadow need not they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
