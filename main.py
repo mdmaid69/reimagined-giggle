8f5773b07adb014362d20d640d4db38d784d2b15
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:12:51 +0000

    dream effort unfortunate reason cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_max(lst):
+        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
