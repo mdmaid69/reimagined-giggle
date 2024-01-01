7f235940743dbdc79d5ac8b2971fbace9a3f891d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:40:23 +0000

    suppose desperate debt foot love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def change_working_directory(path):
+        os.chdir(path)
