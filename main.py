e58c0ae3f9c95b9f5b8abd635d70825a2d5a6231
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:57:11 +0000

    show care reserve all excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
