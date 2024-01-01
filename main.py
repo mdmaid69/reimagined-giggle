ac00c568779b761aaee741584edde59efd474bf5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:53 +0000

    hope too contact majority 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
