4413ae947e98b43c1a0baa553fedf259596011e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:05 +0000

    open awake sometimes contents language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def cube_number(x):
+        return x**3
