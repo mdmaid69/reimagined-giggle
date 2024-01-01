a92ceefa0352197a5354226ac87efee3ea4e4857
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:54:00 +0000

    part fix Qq transport experiment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
