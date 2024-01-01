d637b3e0085f7b8b90ca09b4889f8e021f332147
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:56 +0000

    contact little old moon second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
