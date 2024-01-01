c73404a7e31a74a283cdb1880db87d404a0fcccd
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:12 +0000

    habit scheme murder piece permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import math
-print(math.pi)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
