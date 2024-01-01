0c780e215bf6cc6b531faa738fe13c154e207f88
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:30 +0000

    how path organize tie reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
