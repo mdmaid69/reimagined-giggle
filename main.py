db6c57da62b0ed08382cb8bc69e01f9b49c5597e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:57:04 +0000

    mother roll shock typical 568energy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
