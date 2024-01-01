f91bd2681b4af4f43a56514cb95cd77fe1666b3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:52 +0000

    everywhere confuse dark available good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
