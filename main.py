72de82288ed1b74d38fc6fac38aadf8851bc6141
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:24 +0000

    image spin shadow benefit workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
