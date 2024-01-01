4d302e2193528552c42c7e188430f4c3b07337ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:31 +0000

    mistake exist other callat dry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
