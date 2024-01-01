118a242b175a7074537393249e5d0abb578b35f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:05 +0000

    my shallow thankyou should knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+text = "Hello, world!"
+print("Characters:", len(text))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
