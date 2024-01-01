bd851e188cb32612baac89128785fe247ba26ea6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:39:35 +0000

    shade always where wall who

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+print(sum(range(10)))
