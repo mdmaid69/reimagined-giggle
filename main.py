02bd0932cf8500549b94040618b7f2955a2c8709
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:55:34 +0000

    unkind bad season begin physical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def convert_array_to_string(array):
+        return array.tostring()
