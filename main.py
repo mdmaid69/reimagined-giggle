3372dd8f17ce3f3f90aafd7aad7dba0bcde3ee55
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:41 +0000

    impressive brave deserve ought impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
