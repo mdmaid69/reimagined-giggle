440d8cfe8bd1f7ba8a4061fc934fa4638fbb61fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:01:40 +0000

    love accidentally collect board prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
