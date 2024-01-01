133bc5e3ef330b5d9e73d309b4ded663d3fc0ac4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:33:35 +0000

    demonstrate here holiday trust left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
