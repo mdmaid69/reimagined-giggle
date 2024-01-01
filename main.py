9560a223d5846148721f0e4964a8628566cc3d40
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:14:01 +0000

    sad tap pass usual but

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
