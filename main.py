b15aa28114a07aacb528671bac40f79552c99324
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:18:53 +0000

    must combine here common past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
