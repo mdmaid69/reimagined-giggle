768c4afaf28d401bfdb85f471ea3797910fc0cc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:26:53 +0000

    weigh just unlucky line draw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_length(array):
-        return len(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
