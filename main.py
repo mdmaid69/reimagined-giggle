3457556185760a414f5a1771981bde91e5950abc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:49:10 +0000

    seriously cook send belief importance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def add_numbers(x, y):
+        return x + y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
