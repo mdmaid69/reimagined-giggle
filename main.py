b36b13a7a5cb3525710fb4ba11b1898c77eba2c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:31 +0000

    exact upset trap operation television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
