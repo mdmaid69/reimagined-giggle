52572c1e461ac7f1d02f3fbb6c1bdd3cc04107eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:04:24 +0000

    nearly your know out towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_circle(r):
+        return 3.14 * r**2
