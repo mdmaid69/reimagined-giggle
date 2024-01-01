a78059c25a28c2d90253aeb461f13be0b8dbfd6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:23 +0000

    criticism sometimes few deliberate force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
