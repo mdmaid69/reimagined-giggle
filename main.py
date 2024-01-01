6a53ffa3df49bf1747af0d80a74247569dea53dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:47:33 +0000

    off organization side nice little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
