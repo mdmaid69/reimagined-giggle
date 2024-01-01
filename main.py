a393c5265373182905228dc80fd9a083afdb42b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:13:11 +0000

    run receive window giveback believe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
