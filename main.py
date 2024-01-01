0c41462b3e962b88e4547eec736ea8b4e7d09dd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:47:11 +0000

    answer disappear send lucky hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
