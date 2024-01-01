17ea6d0a640768260e7f8473e470836ae52e9480
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:07 +0000

    comeover kill strength breathe person

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
