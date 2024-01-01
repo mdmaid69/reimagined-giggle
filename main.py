61dfd26057465f7e06b0e7fc016f05d8ef6765a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:53:41 +0000

    circumstance help beat niggaacademy repeat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import sys
+  def get_python_version():
+        return sys.version
