03b626de97e7b88c8f38a5c14ab2be5f2bb0db68
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:08:16 +0000

    expect gift in recently get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def sort_list(lst):
-        return sorted(lst)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
