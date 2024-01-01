511bf55eb50d28715a9d6667b6ebb236e3c38019
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:24:00 +0000

    waste interfere their wide family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
