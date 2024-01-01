6864bc501910489bd74e978537737c86d76b30ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:36:15 +0000

    plant ocean greeting earth century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+text = "Hello, world!"
+print("Words:", len(text.split()))
