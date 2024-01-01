ddbaead9e40ff7faa322fac4b54193ef7e5db474
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:49 +0000

    trick always extraordinary press gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def remove_duplicates(lst):
+        return list(set(lst))
