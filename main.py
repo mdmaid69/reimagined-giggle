7a36367abeb95181c2c6b0c16a99ff68d9ff649a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:01 +0000

    school direct nonsense beside short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
