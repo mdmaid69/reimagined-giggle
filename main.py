ab7aab89042b44f68a494916094abfe89caf6631
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:39 +0000

    179bite message recognize evening turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+print(sys.version)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
