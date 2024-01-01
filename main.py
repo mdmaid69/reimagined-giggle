56008882395b4ce88352c10cd3fbbd57c783c715
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:11 +0000

    include but vision frame concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
