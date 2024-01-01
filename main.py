6a9b176fb5e301d35cf1d2ba65deb1ef11d96cf4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:48 +0000

    movement for disturbing each serve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
