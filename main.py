33aefa175d95503095ee30941c4bbd62fbfbce7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:42 +0000

    mad sight shame Pp take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_typecode(array):
+        return array.typecode
