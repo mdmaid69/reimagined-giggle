e952ba94532e86036b96abafd78a19b5c9252b76
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:27 +0000

    cope create pot transport keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
