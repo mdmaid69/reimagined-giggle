e65d3581f48b2cef8d8408b30d7fb79dda4cd229
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:37:42 +0000

    find situation shut emphasize before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
