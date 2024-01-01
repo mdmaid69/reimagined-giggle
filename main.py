010e92d4269156eeb876071ea667aa235f01cc53
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:55:57 +0000

    effect flood wonder address new

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
