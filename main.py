abe88cc6052cd006aad7e814766d7fa010e95548
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:16:17 +0000

    straight priority up today mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
