ea5a4ac6c502057f2aee8ddc65dfffa3c22aa166
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:31:40 +0000

    concerned accordingto cost settle event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
