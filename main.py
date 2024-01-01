768fd95c4f9bdec1d85d31e331f2da512656e9d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:06:42 +0000

    recognize careful age scrap smell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
