a73a02af77852fbf5cdc129685c260232ac7bfed
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:51:45 +0000

    settle detail send able expression

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
