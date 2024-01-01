299a865fa50dda534388c188ec498453051843d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:42:59 +0000

    none pity destroy fly total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def extend_array(array, iterable):
+        array.extend(iterable)
