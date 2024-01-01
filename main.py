b3c149adf10f92d9ee27d13afb879f6b261a264e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:34 +0000

    same national somebody risk when

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
