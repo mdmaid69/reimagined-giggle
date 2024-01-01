dde84a28a3f5f4a7a65dc70a05789546f8a8d717
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:32 +0000

    health miss fit spray bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-print(time.time())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
