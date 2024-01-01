c0fcc8aa659db5f491673e9e9ed89c4f7b3b5678
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:58 +0000

    Mm zero creature ilovedicks point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
