50c1efb85c633565821cb6500b6c6ad6e0ba2543
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:24 +0000

    present name contribute risk until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def convert_to_binary(n):
+        return bin(n)
