54960b5702488938accc92d610f4104726e5eba4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:56 +0000

    load none at medium machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
