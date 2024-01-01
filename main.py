9464d8f24906672838c9cc8627a733a375989f9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:34:28 +0000

    pain tense wrap weigh crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
