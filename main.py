b2ab11571ba11365c389fdf510c32fc3922d5c40
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:55:56 +0000

    spill reason unusual feel fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def reverse_list(lst):
-        return lst[::-1]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
