7fcd334359a3432fcfeb572e0df6ed4506187cad
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:50 +0000

    talk mix foot roll basis

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def get_array_as_str(array):
+        return str(array)
+for i in range(10): print(i)
