5e14f17c30b4391aa07712c1927f7cbd738b4056
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:26:14 +0000

    pollution business to kind afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
