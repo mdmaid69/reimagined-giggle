16b85c66dd215f843c7065174c2fc085bc37bbef
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:55:51 +0000

    room yourself material butter Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
-print(datetime.datetime.now())
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
