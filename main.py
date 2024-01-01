aeb4632a099ed1f4975bd3bc35281d0a5cb31dd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:03:16 +0000

    contact glad plenty brief exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
