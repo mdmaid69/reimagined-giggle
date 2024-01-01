05a74725629db41dd6392abef95e26fab41550c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:25:47 +0000

    branch business whistle refer long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
