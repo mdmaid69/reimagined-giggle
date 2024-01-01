29cc78a892514e31800a33b60d4e02fd287261fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:53 +0000

    shoulder rise getback start make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
