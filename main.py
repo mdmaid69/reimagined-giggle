71b97fb324068a9b704a6dea036ce76ab3f42b00
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:28 +0000

    judge make emotion foot shout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_factorial(n):
+def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
