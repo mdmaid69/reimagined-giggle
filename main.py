92e986bc60adf0d16252b4420bd785ab1785997d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:41 +0000

    Ff issue quality by wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
