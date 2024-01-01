2f8ae35485de5680844e15e3fc0f78ae87800672
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:54:00 +0000

    profit happy night talkback popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
