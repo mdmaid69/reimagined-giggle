ac113dae96131f602921dcba4c5c72cf99812b15
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:22:06 +0000

    charge weekend week few smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
