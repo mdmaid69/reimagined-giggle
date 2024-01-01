b510fc420ae06182c1326032af75e14fb2edee4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:57 +0000

    among fast scare trip drive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+print(sys.version)
