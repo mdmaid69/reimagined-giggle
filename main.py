979446359e465dd8b18b90205a8e8c1ec2cc0c89
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:54 +0000

    rain perhaps description rain Tt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
