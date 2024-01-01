90ade122525cac3713883d7d2603475566c6c39f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:49:26 +0000

    practise wear safe about reliable

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
