002ebceac20de8ac5353582ffd5009d04cfcd304
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:50:08 +0000

    trip night possible entrance arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
