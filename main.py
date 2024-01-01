5f288ac7b62ffb9cb991bb2e626713ec355dbd81
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:28:34 +0000

    dare refer sick be tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
