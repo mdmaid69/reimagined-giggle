3380d7ca6bf5d5bc61e4a34d6451c43136564af3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:40:46 +0000

    shoot married protect thinkthrough light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
