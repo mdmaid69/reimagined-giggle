e36cd6cebbaf7de38d9d5f115118a18dde596583
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:59:25 +0000

    true strength pot hers pity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
