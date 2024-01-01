415633f88c7ec0dfa49126267bca18f57440e1e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:59:07 +0000

    strike weigh unhappy hospital satisfied

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def reverse_list(lst):
+        return lst[::-1]
