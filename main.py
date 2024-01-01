6b1764999e1fdb664207b4141c0b14bbe38f54e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:25:56 +0000

    judge cap popular side cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_work(force, distance):
-        return force * distance
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
