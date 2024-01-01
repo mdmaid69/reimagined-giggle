5f4f72349da3f1760da406a0839b5eca5ac2db82
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:21:16 +0000

    forever range dish rise patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
