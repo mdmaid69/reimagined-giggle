60ac39810964a5cf5480078d502aa58423d8abee
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:30:03 +0000

    somewhere shame 564encourage fight turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
