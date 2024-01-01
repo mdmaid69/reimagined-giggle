1797872fdc3dcd3897efab148197b492878b0e4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:54:11 +0000

    by nerve here its love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_length(array):
+        return len(array)
