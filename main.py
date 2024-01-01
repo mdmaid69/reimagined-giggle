0c487db0b642bd06d33c05af05f94c5fa232b5df
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:20 +0000

    dick target suggestion offer Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
