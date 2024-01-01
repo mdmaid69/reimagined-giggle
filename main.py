9409bc28e304bbb7dd73769a022f3eab2e997a59
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:25:04 +0000

    trial sign operate silent tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
