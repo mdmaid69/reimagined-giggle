a7a1a764c38a77f3db3d249b0e67f5d38f0a2e28
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:59 +0000

    anyone plenty scream sure knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
