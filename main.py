e7492813f51fdbab4ab5fa64de93339f2866ee64
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:06:54 +0000

    Zz aware wealth near total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
