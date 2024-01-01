563c24002becf342d88a7db839b920e0b5cf24ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:19:58 +0000

    shame come Bb disappear challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_complex(array):
+        return complex(array[0])
