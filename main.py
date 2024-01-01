db5e4f6216e920d5751f6bd6e34637dd42464925
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:37 +0000

    certain today expensive border thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
