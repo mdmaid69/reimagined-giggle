6e522b945d2547a11d1a2ad2545f9bbadb1e5bc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:04:53 +0000

    level direct ignore borrow national

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
