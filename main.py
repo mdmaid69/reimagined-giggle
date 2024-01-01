cff3d4f4a7e5b02ceda4a252b86d9bc234c6d71f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:18 +0000

    war tone band failure best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
