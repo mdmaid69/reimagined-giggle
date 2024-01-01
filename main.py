c09e1b6594da5c037e782a6804a85f9e2b4f5d8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:52:22 +0000

    mad getaway want free copy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
