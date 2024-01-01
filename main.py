31a1fa887c27e1ed69a5a8e3a2706653341b4b72
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:51:09 +0000

    Ww think reason book holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-for i in range(5):
-        print(i)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
