2bcecf8c8beca65eb850fe58074f3d90e376e804
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:17 +0000

    standard maybe theirs load another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
