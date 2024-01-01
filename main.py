bd215c732c61fe03dcf8cdfc64ed1cc6178147a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:53:49 +0000

    forget habit satisfied opposite matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def sort_list(lst):
+        return sorted(lst)
