773b679dd1ebdad8980f7437ccaa5eb881e3adbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:58:30 +0000

    stick interview make include sensible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
