63c8613f0bac48361ea16f888a6a7eebe188666a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:18:52 +0000

    murder contrast distant marry record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
