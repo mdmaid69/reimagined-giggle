d938333e8b57a5571a16b16209cda79820483a57
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:02:51 +0000

    offer refer normal sure boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
