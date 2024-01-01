ef08bb76a29adf1c15cd44ffae0417bbe99c0d8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:35:11 +0000

    wash care term treat open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
