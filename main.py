75b258d06b5f89952a141821f80988d746c2f14b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:33:23 +0000

    afford characteristic card test fill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_user_string():
+        return collections.UserString()
