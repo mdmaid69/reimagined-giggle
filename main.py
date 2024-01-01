871ebf5b33b0b2f410b4cacaa81a23f029852d64
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:00 +0000

    rid blue introduce crowd recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
