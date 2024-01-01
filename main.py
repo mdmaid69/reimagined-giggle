342458a0ea9ecaa923c7ef77e3fe4ab28025dc5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:51:37 +0000

    process bit increase fall consider

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import array
+def get_array_index(array, item):
+        return array.index(item)
