1efa65dfb72a6e5f0d03bab7103868748f0db305
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:19 +0000

    report sleep cause demand change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print("Hello, world!")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
