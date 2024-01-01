181ef1ac700b121d9397f8fbc46cb0ad43d3d545
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:18:30 +0000

    trial actual winter but turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import time
+print(time.time())
