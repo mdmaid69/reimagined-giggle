36fc8dfd7f3e0b40c3813d53a88cad25518fdda2
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:00:02 +0000

    trip generous mess base pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
