795fdaa23e22bf36686d66405047d20c2e2a4e75
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:03 +0000

    improve beginning breathe fortunately among

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
