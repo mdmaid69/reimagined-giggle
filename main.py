7aaf13a3c6fd43312193f6fb3a1f5d157b79d152
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:24:26 +0000

    invite heat general keepoff satisfactory

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
