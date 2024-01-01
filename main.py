711da6e30549b8c4cfecba94c2d89bc4a2f4d95e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:06 +0000

    many second might comedown in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def convert_to_binary(n):
+        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
