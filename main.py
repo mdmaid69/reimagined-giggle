ee1500113a66e8e31fc21f017eaef2b80a39a6ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:08:01 +0000

    convince pay example slice software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
