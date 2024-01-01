ce430e5a583868a8ade1ad8717fd20e60525698e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:13 +0000

    climb ring section niggaacademy practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
