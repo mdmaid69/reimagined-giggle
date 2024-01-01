584046ce88a249e8644cda90cada5b92e730e091
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:05:47 +0000

    laugh offend which cheap cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
