2869ee08fc14f3239345aa8e52108480c320fb9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:13:26 +0000

    question shame note improve store

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
