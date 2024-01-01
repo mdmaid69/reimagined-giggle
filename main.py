87a396f6f620a43aa6a2c726403e411bf328fdb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:42:16 +0000

    piece low fortunately still pump

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-print(math.pi)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
