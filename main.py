18d745679d3bde435a0f04173eb40184219f7109
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:48:44 +0000

    pump stiff century temper summer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
