099129285146f0cd1b8ef689a9890e536f9eaae5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:23 +0000

    previous uponon fact crowd holdout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
