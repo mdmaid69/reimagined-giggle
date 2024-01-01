1c08940501ae147b9e1e16c4f22fd3662ba8eaed
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:50:33 +0000

    Yy consult actual when listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_permissions(file_name):
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
