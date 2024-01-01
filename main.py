b18ccdbd11c9b7c2a945a5f981bc4a02fd7fbefa
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:54:47 +0000

    round attach win strange display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
