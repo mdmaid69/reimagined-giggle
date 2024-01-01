9c416e623373680cb5f42571663f3d1cc4754751
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:22 +0000

    hate cry Dd makeup pencil

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+text = "Hello, world!"
+print("Characters:", len(text))
