b6eea0ccfcf193129cde126e6d835269889cb65f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:38 +0000

    in wish war instrument age

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
