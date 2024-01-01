2fa734014d3faabfa2e8c5ef01646b4251dde380
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:45 +0000

    order relative after more it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
