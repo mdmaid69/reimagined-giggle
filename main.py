214ee07a248d04380ed2c56add68dbab0c203722
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:28:03 +0000

    phone male fetch legal cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
