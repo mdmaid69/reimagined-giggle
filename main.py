f6efc824806bf5c433bb8d682e304efc74197df1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:44 +0000

    generous present danger finally wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import numpy as np
+print(np.array([1, 2, 3]))
