c911b472a5f02ac681a90cf98664e3e6d1fae4e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:38:02 +0000

    phone equal even pot bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
