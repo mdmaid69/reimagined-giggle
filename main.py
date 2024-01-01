802569b24d9ed84757ad54295ee57bb66ea711cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:28:39 +0000

    shopping level model regional debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
