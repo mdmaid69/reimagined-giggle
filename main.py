f017694ba9c678d13bf669e256254622848b4129
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:39:38 +0000

    some now duty than consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
