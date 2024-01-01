32967494daa6c6450f18eec23e1dcb84760e722d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:42:44 +0000

    prize uponon maintain against takeaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
