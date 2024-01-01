66b544b0a455c8572416449ae875ce5e3005200a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:27:06 +0000

    stop roll far blue correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
