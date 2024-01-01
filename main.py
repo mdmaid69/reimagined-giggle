6f1622be8eddb08fe52c4eea1b91722c45d0eb6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:04:53 +0000

    body tongue mouth attractive nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
