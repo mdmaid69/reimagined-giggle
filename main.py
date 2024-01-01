4bfd42ba836aa7cf6221c5bc67959827e853f5d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:39 +0000

    win copy seem send job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
