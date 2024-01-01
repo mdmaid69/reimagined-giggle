edb382d4b602eae8bc85ad36c8e90f5110bf519e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:10:01 +0000

    confidence contents finish breakinto person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
