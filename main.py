167ff1dd0121ae2428616f71d2e410b51a14d664
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:41 +0000

    keep switch frame eat that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
