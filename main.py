bd7fd84e260e5c461c96b0ae0b338d8e8e751b13
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:04 +0000

    contain belong uniform official Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
