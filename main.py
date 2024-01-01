ff75d81cf56f924f8e64743b870b1aabc661dac3
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:34 +0000

    congratulate average narrow past well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
