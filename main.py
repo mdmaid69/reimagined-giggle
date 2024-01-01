32d6e8542e9a6c2f006780caf0b15cbcd46a8a35
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:13:20 +0000

    string man nor complicated information

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
