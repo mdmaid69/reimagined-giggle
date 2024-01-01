15251edb00e134f8755841c9a0461b4b80e9e855
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:39:59 +0000

    mistake disapprove warning arrive begin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
