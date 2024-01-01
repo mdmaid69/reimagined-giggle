52a96d470b3f071d15908bb41b5ec334f9c1f4f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:00:14 +0000

    spend lock Pp back rate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def reverse_array(array):
+        array.reverse()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
