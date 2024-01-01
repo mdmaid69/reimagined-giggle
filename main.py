befa6296c30f8df4fe112cfe1bf4eee01c603eec
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:52 +0000

    really note getaway convenient offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
