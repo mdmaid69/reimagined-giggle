cdc449c0f858c75cbc572db2b31f90fe09ce7e06
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:01:24 +0000

    Ll takeaway vote comeoff hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def count_words(sentence):
+        return len(sentence.split())
