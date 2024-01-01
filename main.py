1c78430a2dcbf12b1d4883e0aa1ca34c19b49cb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:18 +0000

    cross standard statement onto 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
