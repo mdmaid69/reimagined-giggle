66de5162dc30addf2c428aa1694eb02a1b019d7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:45 +0000

    talkback letter content uncomfortable loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
