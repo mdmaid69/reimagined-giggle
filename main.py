b6b03ec7e3e9e65d35ded875d7aea17fb44a5800
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:59 +0000

    my calmly word welcome birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
