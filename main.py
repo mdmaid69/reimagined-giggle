c2ff96ab11948c2685bc36b58caee3ddb1848388
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:29 +0000

    serve weak confidence ring play

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_work(force, distance):
-        return force * distance
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def remove_directory(path):
+        os.rmdir(path)
