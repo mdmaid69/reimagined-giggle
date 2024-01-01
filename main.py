36d393ac33de60eb834073084093ff7f7108e188
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:02 +0000

    cost hers fast classroom compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
