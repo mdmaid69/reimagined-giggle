fabdb8e308ef89ca071c6f7f22987bb5e19835b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:17:13 +0000

    wide fill pronunciation personal Ll

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import sys
-def print_python_version():
-        print(sys.version)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
