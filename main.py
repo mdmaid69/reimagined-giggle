c4a59f20450f56c8eaeceb42204c7b8a512b930b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:14:49 +0000

    you there myself none hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
