3b3379753cd5a844b275c2dc2ce3d13c0f9f2866
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:54:50 +0000

    purpose because television mess mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
