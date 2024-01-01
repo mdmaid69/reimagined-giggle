841eef8e1814f161e5f098eda5231fdc95c93e30
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:25 +0000

    wash force example office increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
