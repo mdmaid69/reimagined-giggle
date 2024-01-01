4682044b8089f4f3ed6602a75b1e2ec9221e147b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:51 +0000

    almost spring breakinto remember mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
