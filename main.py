c14b18041c4eb9ac5d5fd429532df70e7f42a874
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:29:21 +0000

    town near task embarrassed sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
