350aba570512ac2759faadaf30bd471c355623dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:37:08 +0000

    able rain arrive rule scale

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+print(time.time())
+def calculate_density(mass, volume):
+        return mass / volume
