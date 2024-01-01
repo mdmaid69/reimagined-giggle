b1a13eff1d872ef73eb1eb85c9c343fbd22e6c8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:48:23 +0000

    understand many easy recently watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
