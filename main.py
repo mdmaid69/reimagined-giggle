8241eae39be541d638029fea86d2e3fb005747bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:10:34 +0000

    me explanation pair surprise close

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def is_odd(n):
+        return n % 2 != 0
