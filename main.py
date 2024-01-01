9c99094cf0c6d9db2c4f76b93f5049ac3ce38986
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:09 +0000

    fill unfair concerned blame impatient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
