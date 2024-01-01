80f6418b82134766e6c735be2929ae5641aee4eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:27 +0000

    distant represent breakthrough dull split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
