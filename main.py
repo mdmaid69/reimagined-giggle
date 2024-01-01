c527bd7efc4d34c047fd80cf1854b65d71ad669b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:43:51 +0000

    trust effort wild noone suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
