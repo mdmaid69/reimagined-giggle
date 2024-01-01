3e6333a74b082a24d2ee1e6b400689c50ddbe1a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:35:30 +0000

    swallow meeting smooth length snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
