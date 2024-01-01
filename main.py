dd0b984d352b4077c8cd541c20618f4871b08101
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:12 +0000

    receive take move man fasten

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def get_array_as_set(array):
+        return set(array)
+import os
+print(os.getcwd())
