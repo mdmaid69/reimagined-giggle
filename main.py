31ddfe903db1022c28a87f4de4ecc32b62d86d73
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:14 +0000

    goback(to) service translate along bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import tensorflow as tf
-print(tf.__version__)
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
