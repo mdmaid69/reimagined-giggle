702b6ee2d94b1fd8dfea670abe262ef38a45b246
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:02:35 +0000

    breakthrough no agreement mystery Ii

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
