f85690a2b7188d7ba2d137826cada2f69ab1729e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:19:22 +0000

    gather stupid copy control waste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
