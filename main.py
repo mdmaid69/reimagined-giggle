b9bbd7ffa109c37b429bd1e5c3ae98aa9314d532
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:39:18 +0000

    any clean hospital achievement lookup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def is_odd(n):
-        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+print(time.time())
