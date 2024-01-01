bb298fdc3953fdda684f2f873cddff7a2ed0e237
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:17 +0000

    coffee divide offend key available

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   def subtract_numbers(x, y):
         return x - y
-def add_numbers(x, y):
-        return x + y
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
