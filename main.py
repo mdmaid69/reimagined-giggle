83e0a5709745111d77e69f25dd8239be88478653
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:16 +0000

    operation use connection book hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_pressure(force, area):
+        return force / area
+  def reverse_list(lst):
+        return lst[::-1]
