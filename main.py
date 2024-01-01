a050e4833a0103f0d9dee385a6a19c5246fc84ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:42 +0000

    skirt copy number stupid important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
