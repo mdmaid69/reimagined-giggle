0576941843c1c249306373f5e56589ac54d64b7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:19:38 +0000

    freeze fill related willing limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
