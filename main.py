e6dcf05881626f3061c6833f14917f4e8f761678
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:35:28 +0000

    march attractive table target turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
