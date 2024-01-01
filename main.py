f459db26257d5f62d79727b4832a1bd0c7c10b40
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:22 +0000

    make read road shallow polite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
