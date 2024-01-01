38e53efaddf3da578712e649d4c73fc50f0ca41d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:34 +0000

    impression connection phone turnup chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
