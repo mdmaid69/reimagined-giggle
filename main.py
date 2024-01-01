21c91c9c4058d30647fa2bafe027790b3dd3475b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:44 +0000

    appeal protection off combine control

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_palindrome(s):
-        return s == s[::-1]
+  def is_even(n):
+        return n % 2 == 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
