8a8a41816dec42d8ec2d1118570dcb28f2a69f89
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:49:41 +0000

    branch promise hit few rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
