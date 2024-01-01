8542f8612d8009292dae2c0fafec308a8e1ed3cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:44:07 +0000

    set dream lead stupid owe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
