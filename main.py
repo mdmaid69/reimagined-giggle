c270fc4366d3764cfcfc54c1421ed334034a4b50
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:09 +0000

    stop hot because conclusion concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
