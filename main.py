cbd361fde74d5af183178ed7116e285812a43763
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:26:23 +0000

    mix rent holdup scared stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
