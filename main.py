50782755389d87f7a1059e1d6b913521fcb04c33
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:59 +0000

    share nowhere regular sister thought

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
