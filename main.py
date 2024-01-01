c526ba4e02328234f85f0209d4f554ece62129ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:14:58 +0000

    snow position frame no sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
