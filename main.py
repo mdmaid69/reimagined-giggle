37714504a2603707a5dec09d04b52e462f8dcf1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:12 +0000

    hang bright nose score still

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_error_function(x):
+        return math.erf(x)
