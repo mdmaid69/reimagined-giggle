e5287480dcf7d81ab679ac6cf21b3e61f7d69e58
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:11:27 +0000

    nose bringdown getout pure deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def is_odd(n):
+        return n % 2 != 0
