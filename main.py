310ee55a54ba028fcb634ef7a13c6cf4de6fe599
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:25:37 +0000

    description exercise discussion ask favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
