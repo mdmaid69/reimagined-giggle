4d5c2f2bbcc67ac6b07449b32427bdb4e7812083
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:04 +0000

    available talk move drop break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_list(array):
+        return list(array)
