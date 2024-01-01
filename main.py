bf722b7f1a10091850aae47ea73233e85e90ba9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:36 +0000

    separate this morning spin bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
