4dad6f03dd945d07a73a9b1458fb49c97f526789
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:42:31 +0000

    likely day lie pure challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
