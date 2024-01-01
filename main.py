cd433c491774efa7da821b7f5a8953007f7c6fd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:40:31 +0000

    emotion show callat trousers turninto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
