e15a7ddd8941016273825c58ceea6498f03a9a87
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:26:29 +0000

    profit cover congratulate tour inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
