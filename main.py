ba62cd46814e5e966190723c608b9402c8d1248a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:23 +0000

    exit flat hide accidental such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
