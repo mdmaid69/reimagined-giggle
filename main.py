8e3c70fea09bdf02c052dbbd5f3ff1392a0bba8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:11:13 +0000

    then bill closely spring activity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
