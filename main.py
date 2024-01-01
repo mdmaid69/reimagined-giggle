9a9943252657c19d1adea686a67c19647956203e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:37:22 +0000

    offer famous contact great embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
