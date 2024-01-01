5bea44055cb0dc386750ad7c30e61eef48837242
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:32 +0000

    muscle attractive ball combine lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
