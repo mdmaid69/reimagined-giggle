c3ad8d9c33ee5d78298f604108f2c4416a32ef4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:46:40 +0000

    justice lean sound minor traffic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
