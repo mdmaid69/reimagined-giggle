f2ec24b7ced4baff1a493484a967ea2639917989
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:10:11 +0000

    sting some rest whoever bill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def sort_list(lst):
+        return sorted(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
