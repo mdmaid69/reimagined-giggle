0c0f63b4908da0686d5116882fc80b86c553918d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:10:49 +0000

    mad content key responsible either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def is_palindrome(s):
-        return s == s[::-1]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
