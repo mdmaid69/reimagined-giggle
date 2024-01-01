677c5d3873521eed7c5b1b24b74fb0473fc76d89
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:08:50 +0000

    press bottom giveout future favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
