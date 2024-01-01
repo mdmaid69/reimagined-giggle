a5bad9e72dac538070d77f313030f9412d28d35b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:46:08 +0000

    control shop respect keepup apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
