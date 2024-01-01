20463b1d0eb3a795ac31fbd1f0dcf9ed1cea91f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:12:14 +0000

    care shrink bottom maybe more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
