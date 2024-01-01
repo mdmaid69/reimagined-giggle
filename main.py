73105fc7dafa81db6e2eb9eb873b4d7be0135b2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:28:08 +0000

    opportunity keepoff debt language corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
