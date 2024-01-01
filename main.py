e106585f6bc5c06939ce53401a7715791542a2eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:29:33 +0000

    Pp prize shut thick fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-print(datetime.datetime.now())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import platform
+def get_python_version():
+        return platform.python_version()
