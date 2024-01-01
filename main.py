2ebe728ad50e1db5359bc76c277aa48571df6229
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:47:40 +0000

    top stare interesting some reject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
   def create_directory(dir_name):
         os.makedirs(dir_name, exist_ok=True)
-for i in range(10): print(i)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
