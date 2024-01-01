207b1636cc7690b6b260e8f842a2ccded265c29f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:20:31 +0000

    able straight exit cheek dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
