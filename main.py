6825bf903f2336e432b65a637bdf4aaa6d241727
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:19:12 +0000

    argument tooth game paper oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
