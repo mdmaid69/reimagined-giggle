73ac7b6d3868136b9f4a6ed8ed1c20a4afea7dec
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:50 +0000

    spill certain sensitive theirs zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
