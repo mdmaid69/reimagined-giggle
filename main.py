15b7caa79bf4efd221f022204891835e90988e66
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:19 +0000

    bringdown again kill gooff wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
