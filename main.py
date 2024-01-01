58cc843c247a229080c3ee36ad23a982585515d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:35:20 +0000

    warn apologizeapologise touch damage burst

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
