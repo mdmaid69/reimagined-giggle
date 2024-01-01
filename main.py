4b7e678be992a7ba3d5abf524b0a3baf4d1f736f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:22:06 +0000

    regard wish regularly clearly either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
