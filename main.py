b5ce320007c17c18d987f8797dfd04f3781eb8a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:04:06 +0000

    bite base perform supply ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
