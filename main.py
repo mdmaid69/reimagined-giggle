a6dd90810aa42f1f5a0358bec9cf926ffb8f2365
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:40 +0000

    project getup man dream concentration

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
