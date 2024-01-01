e3354e111d8149849f4d6504cc7f1cb90a57f30d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:26:18 +0000

    with mention also benefit letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
