d9359fdfd7384b844a6e571cf345f739cfe20ca5
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:59:27 +0000

    authority cut sing only around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
