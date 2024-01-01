507e0a585e4f67de4a1a60a7b215d9efc8660125
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:37:34 +0000

    laugh so clean fast evening

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import platform
-def get_python_version():
-        return platform.python_version()
+def is_odd(n):
+        return n % 2 != 0
+  import sys
+  def get_python_version():
+        return sys.version
