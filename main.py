036d1cf5f10e710eb00082405dd5d4aec4ffaf4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:21:28 +0000

    firm store run thought shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
