ce5cc39b710771853a7d9af9aaf92890562ee3fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:55 +0000

    recovery hollow wealth plenty fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
